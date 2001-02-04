34d1ddba74ec3e3c951fa0c3a9f6931f7c59d268
Eric Jones <eric@enthought.com>
2001-02-04 01:44:53 +0000

    __init__.py makes cow a package.

---
+++ Lib/cow/__init__.py
@@ -0,0 +1,9 @@
+import sync_cluster
+from cow import *
+
+Mrange = range(1,17)    
+import cow # cow stands for "Cluster Of Workstations"
+server_list = []
+server_list.append((sync_cluster.host,10000))
+
+local = cow.machine_cluster(server_list)
