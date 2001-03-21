c04daefbe8efe986688a2e4e1634b8cb59bd2c7e
Eric Jones <eric@enthought.com>
2001-03-21 03:08:19 +0000

    another attempt at cleaning out pyunit-1.1.0

--- util/cvs_rmdir.py
+++ util/cvs_rmdir.py
@@ -1,15 +1,16 @@
 #! /usr/bin/python
 import os, sys
 
-def cvs_rmdir(dir):
+def cvs_rmdir(dir,depth=0):
     import os
     path = os.path.abspath(dir)
     all_files = os.listdir(path)
-    
-    for i in all_files:
+    indent = '   ' * depth
+    for i in all_files:        
         if not i == 'CVS':
-            if os.path.isdir(i):
-                cvs_rmdir(i)                            
+            print indent, i
+            if os.path.isdir(os.path.join(path,i)):
+                cvs_rmdir(os.path.join(path,i),depth+1)                            
             else:    
                 cmd = 'cd ' + path + ';rm -r ' + i + ';cvs rm ' + i + ';cd ..'
                 print cmd
