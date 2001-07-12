c1e084309c159224b1196aa1cd6c1cc12dcf8136
Travis Oliphant <oliphant@enthought.com>
2001-07-12 00:08:27 +0000

    Added documentation for io.

--- Lib/io/__init__.py
+++ Lib/io/__init__.py
@@ -1,3 +1,17 @@
+"""Binary File input and output.
 
+  Classes
 
+     fopen -- a class for easily reading and writing binary data.
+
+  Functions
+
+     fread -- low-level reading
+     fwrite -- low-level writing
+     bswap -- in-place byte-swapping
+     packbits -- Pack a binary array of 1's and 0's into an array of bytes
+     unpackbits -- Unpack an array packed by packbits.
+
+"""
 from mIO import *
+from numpyio import packbits, unpackbits, bswap, fread, fwrite
