636a0d878930046d89e3bab38144866d48cdffc3
Eric Jones <eric@enthought.com>
2001-07-17 21:04:48 +0000

    initial attempt to refactor stats module removing duplicate functions for lists and arrays.  Also, some renaming and interface changes going on.

---
+++ Lib/stats/tests/test_new_stats.py
@@ -0,0 +1,115 @@
+"""* Test functions for misc module
+
+*"""
+
+from Numeric import *
+import unittest
+from scipy.scipy_test import assert_array_equal, assert_equal
+from scipy.scipy_test import assert_almost_equal
+from scipy.scipy_test import assert_array_almost_equal
+import scipy
+import scipy.stats.new_stats as new_stats
+
+
+##################################################
+### Test for sum
+
+class test_geometric_mean(unittest.TestCase):
+
+    def check_1D_list(self):
+        a = (1,2,3,4)
+        actual= new_stats.geometric_mean(a)
+        desired = power(1*2*3*4,1./4.)
+        assert_almost_equal(desired,actual,decimal=14)
+
+        desired1 = scipy.stats.stats.ageometricmean(a,dimension=-1)
+        assert_almost_equal(desired1,actual,decimal=14)
+    def check_1D_array(self):
+        a = array((1,2,3,4),Float32)
+        actual= new_stats.geometric_mean(a)
+        desired = power(1*2*3*4,1./4.)
+        assert_almost_equal(desired,actual,decimal=14)
+
+        desired1 = scipy.stats.stats.ageometricmean(a,dimension=-1)
+        assert_almost_equal(desired1,actual,decimal=14)
+
+    def check_2D_array_default(self):
+        a = array(((1,2,3,4),
+                   (1,2,3,4),
+                   (1,2,3,4)))
+        actual= new_stats.geometric_mean(a)
+        v = power(1*2*3*4,1./4.)
+        desired = array((v,v,v))
+        assert_array_almost_equal(desired,actual,decimal=14)
+
+        desired1 = scipy.stats.stats.ageometricmean(a,dimension=-1)
+        assert_array_almost_equal(desired1,actual,decimal=14)
+
+    def check_2D_array_dim0(self):
+        a = array(((1,2,3,4),
+                   (1,2,3,4),
+                   (1,2,3,4)))
+        actual= new_stats.geometric_mean(a,dimension=0)
+        desired = array((1,2,3,4))
+        assert_array_almost_equal(desired,actual,decimal=14)
+
+        desired1 = scipy.stats.stats.ageometricmean(a,dimension=0)
+        assert_array_almost_equal(desired1,actual,decimal=14)
+
+class test_harmonic_mean(unittest.TestCase):
+    def check_1D_list(self):
+        a = (1,2,3,4)
+        actual= new_stats.harmonic_mean(a)
+        desired =  4. / (1./1 + 1./2 + 1./3 + 1./4)
+        assert_almost_equal(desired,actual,decimal=14)
+
+        desired1 = scipy.stats.stats.aharmonicmean(array(a),dimension=-1)
+        assert_almost_equal(desired1,actual,decimal=14)
+    def check_1D_array(self):
+        a = array((1,2,3,4),Float32)
+        actual= new_stats.harmonic_mean(a)
+        desired =  4. / (1./1 + 1./2 + 1./3 + 1./4)
+        assert_almost_equal(desired,actual,decimal=14)
+
+        desired1 = scipy.stats.stats.aharmonicmean(a,dimension=-1)
+        assert_almost_equal(desired1,actual,decimal=14)
+
+    def check_2D_array_default(self):
+        a = array(((1,2,3,4),
+                   (1,2,3,4),
+                   (1,2,3,4)))
+        actual= new_stats.harmonic_mean(a)
+        v = 4. / (1./1 + 1./2 + 1./3 + 1./4)
+        desired = array((v,v,v))      
+        assert_array_almost_equal(desired,actual,decimal=14)
+
+        desired1 = scipy.stats.stats.aharmonicmean(a,dimension=-1)
+        assert_array_almost_equal(desired1,actual,decimal=14)
+
+    def check_2D_array_dim0(self):
+        a = array(((1,2,3,4),
+                   (1,2,3,4),
+                   (1,2,3,4)))
+        actual= new_stats.harmonic_mean(a,dimension=0)
+        desired = array((1.,2.,3.,4.))        
+        assert_array_almost_equal(desired,actual,decimal=14)
+
+        desired1 = scipy.stats.stats.aharmonicmean(a,dimension=0)
+        assert_array_almost_equal(desired1,actual,decimal=14)
+
+def test_suite():
+    suites = []
+    suites.append( unittest.makeSuite(test_geometric_mean,'check_') )
+    suites.append( unittest.makeSuite(test_harmonic_mean,'check_') )
+    total_suite = unittest.TestSuite(suites)
+    return total_suite
+
+def test():
+    all_tests = test_suite()
+    runner = unittest.TextTestRunner()
+    runner.run(all_tests)
+    return runner
+
+
+if __name__ == "__main__":
+    test()
