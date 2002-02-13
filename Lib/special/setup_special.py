5ab9fb779be50b57e0228a1fe5dd975d3939e288
Pearu Peterson <pearu.peterson@gmail.com>
2002-02-13 10:23:00 +0000

    Added mach, made locally buildable

--- Lib/special/setup_special.py
+++ Lib/special/setup_special.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import os
 from glob import glob
 from scipy_distutils.core import Extension
@@ -6,12 +8,14 @@ from scipy_distutils.misc_util import get_path, default_config_dict
 def configuration(parent_package=''):
     if parent_package:
         parent_package += '.'
-    local_path = get_path(__name__)    
+    local_path = get_path(__name__)
     config = default_config_dict()
-    
-    config['packages'].append(parent_package+'special')
+
+    if parent_package:
+        config['packages'].append(parent_package+'special')
     c_misc = glob(os.path.join(local_path,'c_misc','*.c'))
     cephes = glob(os.path.join(local_path,'cephes','*.c'))
+    mach = glob(os.path.join(local_path,'mach','*.f'))
     amos = glob(os.path.join(local_path,'amos','*.f'))
     toms = glob(os.path.join(local_path,'toms','*.f'))
     
@@ -20,7 +24,7 @@ def configuration(parent_package=''):
     config['libraries'].append(('cephes',{'sources':cephes}))
     
     # Fortran libraries
-    config['fortran_libraries'].append(('amos',{'sources':amos}))
+    config['fortran_libraries'].append(('amos',{'sources':amos+mach}))
     config['fortran_libraries'].append(('toms',{'sources':toms}))
     
     # Extension
@@ -32,3 +36,7 @@ def configuration(parent_package=''):
     config['ext_modules'].append(ext)
 
     return config
+
+if __name__ == '__main__':
+    from scipy_distutils.core import setup
+    setup(**configuration())
