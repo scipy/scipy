3732bb4a69a4eb47b7ea701d1c4647516bc919be
Travis Oliphant <oliphant@enthought.com>
2002-04-06 18:17:24 +0000

    Changed version to have _ instead of - between subversion strings for compatibility with rpm.

--- Lib/scipy_version.py
+++ Lib/scipy_version.py
@@ -7,5 +7,5 @@ from __cvs_version__ import cvs_version
 cvs_minor = cvs_version[-3]
 cvs_serial = cvs_version[-1]
 
-scipy_version = '%(major)d.%(minor)d.%(micro)d-%(release_level)s'\
-                '-%(cvs_minor)d.%(cvs_serial)d' % (locals ())
+scipy_version = '%(major)d.%(minor)d.%(micro)d_%(release_level)s'\
+                '_%(cvs_minor)d.%(cvs_serial)d' % (locals ())
