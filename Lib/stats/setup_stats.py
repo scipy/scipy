24c4bf1a48ddde5712c58b213f55d66dd549b309
Pearu Peterson <pearu.peterson@gmail.com>
2002-05-06 20:41:17 +0000

    Made stats locally buildable

--- Lib/stats/setup_stats.py
+++ Lib/stats/setup_stats.py
@@ -1,16 +1,20 @@
+#!/usr/bin/env python
+
 import os
 from glob import glob
 from scipy_distutils.core import Extension
 from scipy_distutils.misc_util import get_path, default_config_dict, dot_join
 
 def configuration(parent_package=''):
-    if parent_package:
-        parent_package += '.'
+    #if parent_package:
+    #    parent_package += '.'
     local_path = get_path(__name__)
+    test_path = os.path.join(local_path,'tests')
 
     config = default_config_dict()
-    config['packages'].append(parent_package+'stats')
-    config['packages'].append(parent_package+'stats.tests') 
+    #config['packages'].append(dot_join(parent_package,'stats'))
+    config['packages'].append(dot_join(parent_package,'stats.tests')) 
+    config['package_dir']['stats.tests'] = test_path
 
     # Extension
     sources = ['randmodule.c','ranlib_all.c']
@@ -19,3 +23,7 @@ def configuration(parent_package=''):
     config['ext_modules'].append(ext)
 
     return config
+
+if __name__ == '__main__':    
+    from scipy_distutils.core import setup
+    setup(**configuration())
