a2c8bc57f99c660aa364de548e80bf27d965e70f
dmorrill <dmorrill@localhost>
2002-05-10 18:30:42 +0000

    Made changes to allow SciPy to build correctly under cygwin

--- Lib/xplt/setup_xplt.py
+++ Lib/xplt/setup_xplt.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
 import os
-from scipy_distutils.misc_util import get_path, default_config_dict, dot_join
-from scipy_distutils.misc_util import dict_append
-from scipy_distutils.core import Extension
+from scipy_distutils.misc_util   import get_path, default_config_dict, dot_join
+from scipy_distutils.misc_util   import dict_append
+from scipy_distutils.core        import Extension
 from scipy_distutils.system_info import x11_info
 
 
@@ -33,8 +33,10 @@ def configuration(parent_package=''):
     from glob import glob
     gist = glob(os.path.join(local_path,'gist','*.c'))
     # libraries are C static libraries
-    config['libraries'].append(('gist',{'sources':gist,
-                                        'macros':[('STDC_HEADERS',1)]}))
+    gistdic = { 'sources': gist,
+                'macros':  [ ( 'STDC_HEADERS', 1 ) ] }
+    dict_append( gistdic, **x11 )
+    config['libraries'].append( ( 'gist', gistdic ) )
                                    
     file_ext = ['*.gs','*.gp', '*.ps', '*.help']
     xplt_files = [glob(os.path.join(local_path,x)) for x in file_ext]
