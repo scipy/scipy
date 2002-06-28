bdedceea87e70d0eebeb7832c8dea34f7d023824
Eric Jones <eric@enthought.com>
2002-06-28 04:39:38 +0000

    put protective try/except around zlib calls so that platforms without zlib don't get errors trying to import scipy.

--- Lib/io/dumb_shelve.py
+++ Lib/io/dumb_shelve.py
@@ -1,5 +1,10 @@
 from shelve import Shelf
-import zlib
+try:
+    import zlib
+except ImportError:
+    # Some python installations don't have zlib.
+    pass    
+    
 from cStringIO import  StringIO
 import  cPickle  
 
@@ -20,11 +25,18 @@ class DbfilenameShelf(Shelf):
             r = zlib.decompress(compressed)
         except zlib.error:
             r = compressed
+        except NameError:
+            r = compressed    
+            
         return cPickle.loads(r) 
         
     def __setitem__(self, key, value):
         s = cPickle.dumps(value,1)
-        self.dict[key] = zlib.compress(s)
+        try:
+            self.dict[key] = zlib.compress(s)
+        except NameError:
+            #zlib doesn't exist, leave it uncompressed.
+            self.dict[key] = s
 
 def open(filename, flag='c'):
     """Open a persistent dictionary for reading and writing.
