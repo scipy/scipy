9073df199fda9279a7e3561f47c82a351a8d742d
Pearu Peterson <pearu.peterson@gmail.com>
2003-10-22 13:13:03 +0000

    Incr. micro version number. New style importing hooks will follow soon (see DEVELOPERS.txt).

--- Lib/scipy_version.py
+++ Lib/scipy_version.py
@@ -1,6 +1,6 @@
 major = 0
 minor = 2
-micro = 0
+micro = 1
 #release_level = 'alpha'
 release_level=''
 
@@ -8,5 +8,9 @@ from __cvs_version__ import cvs_version
 cvs_minor = cvs_version[-3]
 cvs_serial = cvs_version[-1]
 
-scipy_version = '%(major)d.%(minor)d.%(micro)d_%(release_level)s'\
-                '_%(cvs_minor)d.%(cvs_serial)d' % (locals ())
+if release_level:
+    scipy_version = '%(major)d.%(minor)d.%(micro)d_%(release_level)s'\
+                    '_%(cvs_minor)d.%(cvs_serial)d' % (locals ())
+else:
+    scipy_version = '%(major)d.%(minor)d.%(micro)d'\
+                    '_%(cvs_minor)d.%(cvs_serial)d' % (locals ())
