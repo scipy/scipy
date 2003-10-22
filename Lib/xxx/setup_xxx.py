1234437183b8179cf8d79e6cdf2aed5a5b43fac0
Pearu Peterson <pearu.peterson@gmail.com>
2003-10-22 22:18:54 +0000

    Impl. example of SourceGenerator usage.

--- Lib/xxx/setup_xxx.py
+++ Lib/xxx/setup_xxx.py
@@ -1,12 +1,15 @@
 
+import os
+
 def configuration(parent_package=''):
 
     # The following three lines constitute minimal contents
     # of configuration(..) that is suitable for pure Python
     # packages.
     package = 'xxx'
-    from scipy_distutils.misc_util import default_config_dict,dot_join
+    from scipy_distutils.misc_util import default_config_dict, dot_join, get_path
     config = default_config_dict(package,parent_package)
+    local_path = get_path(__name__)
 
     # xxx contains pure Python sub-package yyy
     config['packages'].append(dot_join(parent_package,package,'yyy'))
@@ -14,9 +17,28 @@ def configuration(parent_package=''):
     config['packages'].append(dot_join(parent_package,package,'yyy.tests'))
 
     # xxx has extension module ..
+    from scipy_distutils.core import Extension
+
+    
     # xxx has f2py generated extension module ..
     # xxx has swig generated extension module ..
 
+    # xxx generates source code
+    from scipy_distutils.misc_util import SourceGenerator
+    def gen_spam(target, sources):
+        fin = open(sources[0])
+        body = fin.read()
+        fin.close()
+        fout = open(target,'w')
+        fout.write('python module spam\n%s\nend python module spam' % body)
+        fout.close()
+
+    pyf_file = SourceGenerator(gen_spam,
+                               target='spam.pyf',
+                               sources=[os.path.join(local_path,'spam_src.pyf')])
+    ext = Extension(name=dot_join(parent_package,package,'spam'),
+                    sources=[pyf_file])
+    config['ext_modules'].append(ext)
     return config
 
 if __name__ == '__main__':
