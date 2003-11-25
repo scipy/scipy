ab95239558b79429cf0b03d46a5d7d9d07fe71bc
Pearu Peterson <pearu.peterson@gmail.com>
2003-11-25 07:55:04 +0000

    Disabled non-standard malloc.h include

--- Lib/sparse/SuperLU2.0/SRC/sp_coletree.c
+++ Lib/sparse/SuperLU2.0/SRC/sp_coletree.c
@@ -2,7 +2,7 @@
 /*  Elimination tree computation and layout routines */
 
 #include <stdio.h>
-#include <malloc.h>
+/*#include <malloc.h>*/
 #include <stdlib.h>
 #include "util.h"
 
--- Lib/sparse/SuperLU2.0/SRC/util.h
+++ Lib/sparse/SuperLU2.0/SRC/util.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <malloc.h>
+/*#include <malloc.h>*/
 #include <assert.h>
 
 /* Macros */
