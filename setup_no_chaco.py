66c1eb875a0aff4f39f50cd83b8fc9a1930e27db
Pearu Peterson <pearu.peterson@gmail.com>
2003-12-09 21:17:02 +0000

    Added missing sys import

--- setup_no_chaco.py
+++ setup_no_chaco.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
+import sys
 from setup import setup_package
 
 ignore_packages = ['chaco','kiva','freetype','traits']
 if sys.platform=='win32':
     ignore_packages.append('xplt')
+    ignore_packages.append('sparse')
 
 if __name__ == "__main__":
     setup_package(ignore_packages)
