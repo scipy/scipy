124806200db90359463b799414c7af718cb864cd
Travis Oliphant <oliphant@enthought.com>
2004-04-13 21:23:51 +0000

    Fixed masking of convolve by Numeric.convolve

--- Lib/signal/__init__.py
+++ Lib/signal/__init__.py
@@ -6,7 +6,8 @@ from info_signal import __doc__
 
 import sigtools
 from waveforms import *
-from signaltools import *
 from bsplines import *
 from filter_design import *
 from ltisys import *
+from signaltools import *
+
