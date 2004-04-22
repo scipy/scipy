f4dfc7e6ad111efbdd80b0a1b30b6025676d2449
Pearu Peterson <pearu.peterson@gmail.com>
2004-04-22 15:55:11 +0000

    Incr. micro version to 1. Enabled CVS version info in version. Enabled arch-dependent Fortran compiler flags.

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ def setup_package(ignore_packages=[]):
         from scipy_version import scipy_version
 
         # Uncomment when making releases:
-        if not command_sdist: scipy_version = '0.3'
+        #if not command_sdist: scipy_version = '0.3.1'
 
         packages_path = ['Lib']
         if command_sdist:
