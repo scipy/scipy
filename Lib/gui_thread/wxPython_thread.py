b269868ad7a824540d14d616893300af5c7b7bef
prabhu <prabhu@localhost>
2004-10-02 18:53:53 +0000

    ENH: Modified to work with both wxPython 2.4.x and 2.5.x.  Also cleaned up the code.

--- Lib/gui_thread/wxPython_thread.py
+++ Lib/gui_thread/wxPython_thread.py
@@ -7,7 +7,6 @@
 
 __all__ = ['wxPython_thread']
 
-import re
 import os
 import sys
 import new
@@ -16,19 +15,16 @@ import thread
 import inspect
 import atexit
 import imp
+import glob
 from scipy_base import ParallelExec
 
-def get_extmodules(module,pexec,state0=None):
+def get_extmodules(pexec):
     # .. that need wrappers
-    if module != 'wx':
-        assert not sys.modules.has_key(module),\
-        module+' is already imported, cannot proceed'
-    if state0 is None:
-        state0 = sys.modules.keys()
-    if module == 'wxPython':
-        pexec('import '+module,wait=1)
-        
+
+    state0 = sys.modules.keys()
+    _do_import(pexec)
     state1 = sys.modules.keys()
+
     return [k for k in state1 if k not in state0 \
             and (not hasattr(sys.modules[k],'__file__') \
                  or os.path.splitext(sys.modules[k].__file__)[1][:3]!='.py')]
@@ -88,43 +84,65 @@ func_code=%(func_name)s.func_code"""
     return new.function(func_code,globs,func_name)
 
 
-def _import_wx_core(wx_pth, pexec):
+def _do_import(pexec):
+    """This imports the wxPython modules.  This code handles both
+    wxPython 2.4.x and 2.5.x versions.
+
+    """
+    sfx = [x[0] for x in imp.get_suffixes() if x[-1] is imp.C_EXTENSION]
+    suffix = '.so'
+
+    # Handle the imports for wxPython-2.5.x
+    flag = 0
+    for path in sys.path:
+        wx_pth = os.path.join(path, 'wx')
+        if os.path.exists(os.path.join(wx_pth, '__init__.py')):
+            for x in sfx:
+                if os.path.exists(os.path.join(wx_pth, '_core_' + x)):
+                    suffix = x
+                    flag = 1
+                    assert not sys.modules.has_key('wx'), \
+                           'wx is already imported, cannot proceed'
+                    # Now do necessary stuff for wx.
+                    _import_wx_core(pexec, wx_pth, suffix)
+                    break
+
+    # wxPython-2.4.x
+    if not flag: 
+        assert not sys.modules.has_key('wxPython'),\
+               'wxPython is already imported, cannot proceed'
+        pexec('import wxPython', wait=1)
+        path = os.path.dirname(sys.modules['wxPython'].__file__)
+        suffix = os.path.splitext(sys.modules['wxPython.wxc'].__file__)[1]
+        exts = glob.glob(os.path.join(path, '*' + suffix))
+        for x in [os.path.splitext(os.path.basename(i))[0] for i in exts]:
+            pexec('import wxPython.%s'%x, wait=1)
+        
+
+def _import_wx_core(pexec, wx_pth, suffix):
     """Imports the core modules for wx.  This is necessary for
     wxPython-2.5.x.
-    """
-    # Find the suffix.
-    suffix = '.so'
-    flag = 0
-    for x in [x[0] for x in imp.get_suffixes() if x[-1] is imp.C_EXTENSION]:
-        if os.path.exists(os.path.join(wx_pth, '_core_' + x)):
-            suffix = x
-            flag = 1
-            break
-    if not flag:
-        return 0
 
+    """
     # Create a dummy module in sys.modules to inhibit importing the
     # actual one.  We will reload(wx) to get it right later.
     m = new.module('wx')
     m.__file__ = os.path.join('wx_pth', '__init__.py')
     sys.modules['wx'] = m
 
-    # Now import all the modules manually.
-    pexec('import os.path, imp, glob')
-    code = """\
-libs = glob.glob(os.path.join('%(wx_pth)s', '*' + '%(suffix)s'))
-done = []
-for i in ['_core_', '_controls_', '_misc_', '_windows_', '_gdi_']:
-    p = os.path.join('%(wx_pth)s', i + '%(suffix)s')
-    imp.load_dynamic('wx.' + i, p)
-    done.append(p)
-libs = [x for x in libs if x not in done]
-for i in libs:
-    p = os.path.splitext(os.path.basename(i))[0]
-    imp.load_dynamic('wx.' + p, i)
-"""%locals()
-    pexec(code, wait=1)
-    return 1
+    # Now import all the extension modules manually.
+    pexec('import imp')
+    libs = glob.glob(os.path.join(wx_pth, '*' + suffix))
+    done = []
+    for i in ['_core_', '_controls_', '_misc_', '_windows_', '_gdi_']:
+        p = os.path.join(wx_pth, i + suffix)
+        pexec('imp.load_dynamic("wx." + "%s", "%s")'%(i, p), wait=1)
+        done.append(p)
+    libs = [x for x in libs if x not in done]
+    for i in libs:
+        p = os.path.splitext(os.path.basename(i))[0]
+        pexec('imp.load_dynamic("wx." + "%s", "%s")'%(p, i), wait=1)
+
     
 def wxPython_thread():
     class AttrHolder: pass
@@ -134,37 +152,19 @@ def wxPython_thread():
     call_holder.main_thread_id = thread.get_ident()
     pexec = ParallelExec()
 
-    # Check if 'wx' namespace based modules are used.
-    mod_name = 'wxPython'
-    state0 = None
-    for path in sys.path:
-        wx_pth = os.path.join(path, 'wx')
-        if os.path.exists(os.path.join(wx_pth, '__init__.py')):
-            assert not sys.modules.has_key('wx'), \
-                   'wx is already imported, cannot proceed'
-            state0 = sys.modules.keys()
-            if _import_wx_core(wx_pth, pexec):
-                mod_name = 'wx'
-            else:
-                state0 = None
-            break
-    #print [x for x in sys.modules.keys() if x.find('wx') > -1]
-    
     # Create wrappers to wxPython extension modules:
-    for name in get_extmodules(mod_name,pexec,state0):
+    for name in get_extmodules(pexec):
         module = sys.modules[name]
         if module is None: # happens with gui_thread.wxPython
             continue
         sys.modules[name] = wrap_extmodule(module,call_holder)
-        #print name
     
-    if mod_name == 'wx':
-        pexec('import wx; reload(wx)', wait=1)
-        #print [x for x in sys.modules.keys() if x.find('wx') > -1]
-        import wx
-    else:
+    if sys.modules.has_key('wxPython.wxc'):
         pexec('import wxPython; reload(wxPython)', wait=1)
         import wxPython
+    else:
+        pexec('import wx; reload(wx)', wait=1)
+        import wx
 
     pexec('from wxBackgroundApp import wxBackgroundApp')
     pexec('call_holder.call=wxBackgroundApp()',wait=1)
