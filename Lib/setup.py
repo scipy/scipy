00fd818e2dab5daa0a8d7280d59201d13b2c124b
Pearu Peterson <pearu.peterson@gmail.com>
2005-10-14 13:14:12 +0000

    Added linalg and __scipy_config__.

--- Lib/setup.py
+++ Lib/setup.py
@@ -5,11 +5,13 @@ def configuration(parent_package='',top_path=None):
     #config.add_subpackage('sandbox')
     config.add_subpackage('utils')
     config.add_subpackage('io')
+    config.add_subpackage('linalg')
     config.add_subpackage('special')
     config.add_subpackage('optimize')
     config.add_subpackage('stats')
     config.add_subpackage('interpolate')
     config.make_svn_version_py()  # installs __svn_version__.py
+    config.make_config_py('__scipy_config__')
     return config
 
 if __name__ == '__main__':
