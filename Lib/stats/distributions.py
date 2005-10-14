8a8ad9f7442fe2bae834ff23b5a07e336f7837a2
Pearu Peterson <pearu.peterson@gmail.com>
2005-10-14 22:34:48 +0000

    Added missing pi import.

--- Lib/stats/distributions.py
+++ Lib/stats/distributions.py
@@ -13,7 +13,7 @@ from scipy.base import alltrue, where, arange, put, putmask, nonzero, \
      ravel, compress, take, ones, sum, shape, product, repeat, reshape, \
      zeros
 from scipy.base import atleast_1d, polyval, angle, ceil, insert, extract, \
-     any, argsort, argmax, argmin, vectorize, r_, asarray, nan, inf, select
+     any, argsort, argmax, argmin, vectorize, r_, asarray, nan, inf, select, pi
 import scipy.base
 from scipy.lib import mtrand
 
