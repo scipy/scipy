3e149944e27c9b55a7d7f248a70bb04021aefa3f
Pearu Peterson <pearu.peterson@gmail.com>
2005-10-26 20:54:06 +0000

    Added missing fftpack.helper.py

---
+++ Lib/fftpack/helper.py
@@ -0,0 +1,19 @@
+
+import types
+from scipy.base import array
+from scipy.basic.helper import fftshift, ifftshift, fftfreq
+
+def rfftfreq(n,d=1.0):
+    """ rfftfreq(n, d=1.0) -> f
+
+    DFT sample frequencies (for usage with rfft,irfft).
+
+    The returned float array contains the frequency bins in
+    cycles/unit (with zero at the start) given a window length n and a
+    sample spacing d:
+
+      f = [0,1,1,2,2,...,n/2-1,n/2-1,n/2]/(d*n)   if n is even
+      f = [0,1,1,2,2,...,n/2-1,n/2-1,n/2,n/2]/(d*n)   if n is odd
+    """
+    assert isinstance(n,types.IntType)
+    return array(range(1,n+1),'i')/2/float(n*d)
