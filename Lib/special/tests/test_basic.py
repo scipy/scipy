efbb772daa07240a806461c46398f04d8747a297
Pearu Peterson <pearu.peterson@gmail.com>
2005-10-26 21:59:48 +0000

    Disabled special tests that run forever.

--- Lib/special/tests/test_basic.py
+++ Lib/special/tests/test_basic.py
@@ -5,6 +5,7 @@
 #F   Means test is failing (F)
 #EF  Means test is giving error and Failing
 #!   Means test is segfaulting
+#8   Means test runs forever
 
 ###  test_besselpoly
 ###  test_jnjnp_zeros
@@ -27,6 +28,9 @@
 #F   test_sph_jn
 #F   test_sph_in
 #F   test_sph_jn
+#8   test_sh_chebyu
+#8   test_sh_jacobi
+#8   test_sh_legendre
 
 import os
 import sys
@@ -1788,7 +1792,7 @@ class test_round(unittest.TestCase):
         rndrl = (10,10,10,11)
         assert_array_equal(rnd,rndrl)
 
-class test_sh_legendre(unittest.TestCase):
+class _test_sh_legendre(unittest.TestCase):
 
     def check_sh_legendre(self):
         # P*_n(x) = P_n(2x-1)
@@ -1837,7 +1841,7 @@ class _test_sh_chebyt(unittest.TestCase):
         assert_array_almost_equal(Ts5.c,tse5.c,12)
         
 
-class test_sh_chebyu(unittest.TestCase):
+class _test_sh_chebyu(unittest.TestCase):
 
     def check_sh_chebyu(self):
         # U*_n(x) = U_n(2x-1)
@@ -1861,7 +1865,7 @@ class test_sh_chebyu(unittest.TestCase):
         assert_array_almost_equal(Us4.c,use4.c,12)
         assert_array_almost_equal(Us5.c,use5.c,11)
 
-class test_sh_jacobi(unittest.TestCase):
+class _test_sh_jacobi(unittest.TestCase):
 
     def check_sh_jacobi(self):
         # G^(p,q)_n(x) = n! gamma(n+p)/gamma(2*n+p) * P^(p-q,q-1)_n(2*x-1)
