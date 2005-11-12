c73d4f61ca7042006fe6b954fda35131f98a9c16
Travis Oliphant <oliphant@enthought.com>
2005-11-12 06:37:52 +0000

    Fixed version number.

--- Lib/scipy_version.py
+++ Lib/scipy_version.py
@@ -2,11 +2,11 @@ major = 0
 minor = 4
 micro = 2
 
-try:
-    from __svn_version__ import version as svn_revision
-    scipy_version = '%(major)d.%(minor)d.%(micro)d_%(svn_revision)s'\
-                    % (locals ())
-except ImportError,msg:
-    svn_revision = 0
-    scipy_version = '%(major)d.%(minor)d.%(micro)d' % (locals ())
+#try:
+#    from __svn_version__ import version as svn_revision
+#    scipy_version = '%(major)d.%(minor)d.%(micro)d_%(svn_revision)s'\
+#                    % (locals ())
+#except ImportError,msg:
+#svn_revision = 0
+scipy_version = '%(major)d.%(minor)d.%(micro)d' % (locals ())
 
