267fb968a6e27ac9be05972884f446bd7df239a8
Travis Oliphant <oliphant@enthought.com>
2005-11-13 03:45:02 +0000

    Removed svn stuff for version number.

--- Lib/scipy_version.py
+++ Lib/scipy_version.py
@@ -1,12 +1,12 @@
 major = 0
 minor = 4
-micro = 2
+micro = 3
 
-try:
-    from __svn_version__ import version as svn_revision
-    scipy_version = '%(major)d.%(minor)d.%(micro)d_%(svn_revision)s'\
-                    % (locals ())
-except ImportError,msg:
-    svn_revision = 0
-    scipy_version = '%(major)d.%(minor)d.%(micro)d' % (locals ())
+#try:
+#    from __svn_version__ import version as svn_revision
+#    scipy_version = '%(major)d.%(minor)d.%(micro)d_%(svn_revision)s'\
+#                    % (locals ())
+#except ImportError,msg:
+#    svn_revision = 0
+scipy_version = '%(major)d.%(minor)d.%(micro)d' % (locals ())
 
