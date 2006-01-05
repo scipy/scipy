13bf8289158c4ba5e810a30a6b1456540b2da7c5
Pearu Peterson <pearu.peterson@gmail.com>
2006-01-05 13:33:49 +0000

    scipy __init__.py clean up and initializing __doc__

--- Lib/__init__.py
+++ Lib/__init__.py
@@ -16,30 +16,31 @@ Available subpackages
 ---------------------
 """
 
-import os as _os
-import sys as _sys
-SCIPY_IMPORT_VERBOSE = int(_os.environ.get('SCIPY_IMPORT_VERBOSE','0'))
-
 try:
     import pkg_resources as _pr # activate namespace packages (manipulates __path__)
     del _pr
 except ImportError:
     pass
 
+from numpy import show_config as show_numpy_config
+if show_numpy_config is None:
+    raise ImportError,"Cannot import scipy when running from numpy source directory."
+from numpy import __version__ as __numpy_version__
+
+from __config__ import show as show_config
+from version import version as __version__
+
 import numpy._import_tools as _ni
 pkgload = _ni.PackageLoader()
 del _ni
+
+import os as _os
+SCIPY_IMPORT_VERBOSE = int(_os.environ.get('SCIPY_IMPORT_VERBOSE','0'))
+pkgload(verbose=SCIPY_IMPORT_VERBOSE,postpone=True)
+del _os
 
 from numpy.testing import ScipyTest
 test = ScipyTest('scipy').test
 __all__.append('test')
 
-from version import version as __version__
-from numpy import __version__ as __numpy_version__
-__all__.append('__version__')
-__all__.append('__numpy_version__')
-
-from __config__ import show as show_config
-pkgload(verbose=SCIPY_IMPORT_VERBOSE,postpone=True)
-
-del _os, _sys
+__doc__ += pkgload.get_pkgdocs()
