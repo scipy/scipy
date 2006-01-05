4d950b0a5c13f1d990ce20e60b5bf614c413a10c
Pearu Peterson <pearu.peterson@gmail.com>
2006-01-05 22:27:47 +0000

    Using fft_opt info in scipy.fftpack setup.py

--- Lib/fftpack/setup.py
+++ Lib/fftpack/setup.py
@@ -10,14 +10,7 @@ def configuration(parent_package='',top_path=None):
     from numpy.distutils.system_info import get_info,FFTWNotFoundError,\
          DJBFFTNotFoundError
     config = Configuration('fftpack',parent_package, top_path)
-    fftw_info = get_info('fftw') or get_info('dfftw')
-    if not fftw_info:
-        print FFTWNotFoundError.__doc__
-    djbfft_info = get_info('djbfft')
-    if not djbfft_info:
-        print DJBFFTNotFoundError.__doc__
-    #djbfft_info = None
-    #fftw_info = None
+    fft_opt_info = get_info('fft_opt')
     
     config.add_data_dir('tests')
 
@@ -26,21 +19,17 @@ def configuration(parent_package='',top_path=None):
 
     sources = ['fftpack.pyf','src/zfft.c','src/drfft.c','src/zrfft.c',
                'src/zfftnd.c']
-    extra_info = []
-    if fftw_info:
-        extra_info.append(fftw_info)
-    if djbfft_info:
-        extra_info.append(djbfft_info)
+
     config.add_extension('_fftpack',
                          sources=sources,
                          libraries=['dfftpack'],
-                         extra_info = extra_info
+                         extra_info = fft_opt_info
                          )
 
     config.add_extension('convolve',
                          sources = ['convolve.pyf','src/convolve.c'],
                          libraries = ['dfftpack'],
-                         extra_info = extra_info
+                         extra_info = fft_opt_info
                          )
     return config
 
