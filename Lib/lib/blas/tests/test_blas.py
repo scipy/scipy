ffe3f6c453d0b1a08add716229cff988c7973a03
Pearu Peterson <pearu.peterson@gmail.com>
2006-01-25 20:14:53 +0000

    Added gemm(1,[[1,2]],[[3],[4]]) tests.

--- Lib/lib/blas/tests/test_blas.py
+++ Lib/lib/blas/tests/test_blas.py
@@ -178,12 +178,23 @@ class test_fblas3_simple(ScipyTestCase):
             if f is None: continue
             assert_array_almost_equal(f(3,[3],[-4]),[-36])
             assert_array_almost_equal(f(3,[3],[-4],3,[5]),[-21])
+            assert_array_almost_equal(f(1,[[1,2],[1,2]],[[3],[4]]),[[11],[11]])
+            assert_array_almost_equal(f(1,[[1,2]],[[3,3],[4,4]]),[[11,11]])
         for p in 'cz':
             f = getattr(fblas,p+'gemm',None)
             if f is None: continue
             assert_array_almost_equal(f(3j,[3-4j],[-4]),[-48-36j])
             assert_array_almost_equal(f(3j,[3-4j],[-4],3,[5j]),[-48-21j])
-
+            assert_array_almost_equal(f(1,[[1,2],[1,2]],[[3],[4]]),[[11],[11]])
+            assert_array_almost_equal(f(1,[[1,2]],[[3,3],[4,4]]),[[11,11]])
+            
+    def check_gemm2(self):
+        for p in 'sdcz':
+            f = getattr(fblas,p+'gemm',None)
+            if f is None: continue
+            assert_array_almost_equal(f(1,[[1,2]],[[3],[4]]),[11])
+            assert_array_almost_equal(f(1,[[1,2],[1,2]],[[3],[4]]),[[11],[11]])
+        
 class test_blas(ScipyTestCase):
 
     def check_blas(self):
