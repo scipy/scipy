16e7cc67ce3fe8a9416c57421a65a38af4f049d1
Travis Oliphant <oliphant@enthought.com>
2006-01-31 20:08:22 +0000

    Add test for cspline1d_eval.

--- Lib/signal/tests/test_signaltools.py
+++ Lib/signal/tests/test_signaltools.py
@@ -5,7 +5,7 @@ set_package_path()
 import scipy.signal as signal
 restore_path()
 
-from numpy import array
+from numpy import array, arange
 
 class test_convolve(ScipyTestCase):
     def check_basic(self):
@@ -26,6 +26,20 @@ class test_wiener(ScipyTestCase):
         correct = array([[2.16374269,3.2222222222, 2.8888888889, 1.6666666667],[2.666666667, 4.33333333333, 4.44444444444, 2.8888888888],[2.222222222, 4.4444444444, 5.4444444444, 4.801066874837],[1.33333333333, 3.92735042735, 6.0712560386, 5.0404040404]])
         h = signal.wiener(g)
         assert_array_almost_equal(h,correct,decimal=6)
+
+class test_cspline1d_eval(ScipyTestCase):
+    def check_basic(self):
+        y=array([1,2,3,4,3,2,1,2,3.0])
+        x=arange(len(y)) 
+        dx=x[1]-x[0]
+        cj = signal.cspline1d(y)
+
+        x2=arange(len(y)*10.0)/10.0
+        y2=signal.cspline1d_eval(cj, x2, dx=dx,x0=x[0])
+        
+        # make sure interpolated values are on knot points
+        assert_array_almost_equal(y2[::10], y, decimal=5)
+
 
 if __name__ == "__main__":
     ScipyTest().run()
