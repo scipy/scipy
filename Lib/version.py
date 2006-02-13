9a3d9c6dd7cda4103a48c4c4bcc412426516bc59
edschofield <edschofield@localhost>
2006-02-13 12:56:31 +0000

    Updates to docs for 0.4.6 release. Removed README.txt (duplicate of LATEST.txt)

--- Lib/version.py
+++ Lib/version.py
@@ -1,14 +1,2 @@
-version = '0.4.5'
-
-import os
-svn_version_file = os.path.join(os.path.dirname(__file__),
-                                '__svn_version__.py')
-
-if os.path.isfile(svn_version_file):
-    import imp
-    svn = imp.load_module('scipy.__svn_version__',
-                          open(svn_version_file),
-                          svn_version_file,
-                          ('.py','U',1))
-    version += '.'+svn.version
+version = '0.4.6'
 
