0beefb64e326cff6a092b86769cce70c9582ce1a
Pearu Peterson <pearu.peterson@gmail.com>
2006-03-30 18:16:07 +0000

    Fixed umfpack setup script.

--- Lib/linsolve/umfpack/setup.py
+++ Lib/linsolve/umfpack/setup.py
@@ -5,27 +5,21 @@ def configuration(parent_package='',top_path=None):
     import numpy
     from numpy.distutils.misc_util import Configuration
     from numpy.distutils.system_info import get_info
-    import os.path as op
 
     config = Configuration( 'umfpack', parent_package, top_path )
     config.add_data_dir('tests')
 
     umf_info = get_info( 'umfpack', notfound_action = 1 )
-    if umf_info:
-        print 'Umfpack present, ok.'
-    else:
-        return None
 
-    scipyInclude = numpy.get_numpy_include()
-    umfpackInclude = umf_info['include_dirs'][0]
+    umfpack_i_file = config.paths('umfpack.i')[0]
+    def umfpack_i(ext, build_dir):
+        if umf_info:
+            return umfpack_i_file
 
     config.add_extension( '__umfpack',
-                          sources = ['umfpack.i'],
-                          swig_opts = ['-I' + umfpackInclude],
-                          include_dirs = [umfpackInclude, scipyInclude],
+                          sources = [umfpack_i],
                           libraries = ['cblas'],
-                          extra_objects = umf_info['extra_objects'] )
-    # config.add_scripts( 'test_umfpack.py' )
+                          **umf_info)
 
     return config
 
