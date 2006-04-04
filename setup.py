bc6100f003941bc7ae54f63adea8af817acfdf0f
Pearu Peterson <pearu.peterson@gmail.com>
2006-04-04 12:28:21 +0000

    Fix pkg information requests.

--- setup.py
+++ setup.py
@@ -5,25 +5,16 @@ os.environ['NO_SCIPY_IMPORT']='SciPy/setup.py'
 
 def configuration(parent_package='',top_path=None):
       from numpy.distutils.misc_util import Configuration
-      config = Configuration(None, parent_package, top_path,
-                             maintainer = "SciPy Developers",
-                             maintainer_email = "scipy-dev@scipy.org",
-                             description = "Scientific Algorithms Library for Python",
-                             url = "http://www.scipy.org",
-                             license = 'BSD',
-                             )
+      config = Configuration(None, parent_package, top_path)
       config.set_options(ignore_setup_xxx_py=True,
                          assume_default_configuration=True,
                          delegate_options_to_subpackages=True,
                          quiet=True)
 
       config.add_subpackage('Lib')
-      config.name = 'scipy' # used in generated file names
-
       config.add_data_files(('scipy','*.txt'))
 
-      from version import version as version
-      config.dict_append(version=version)
+      config.get_version('Lib/version.py') # sets config.version
 
       return config
 
@@ -39,7 +30,16 @@ def setup_package():
     sys.path.insert(0,os.path.join(local_path,'Lib')) # to retrive version
 
     try:
-        setup( configuration=configuration )
+        from version import version as version
+        setup(
+            name = 'scipy',
+            version = version, # will be overwritten by configuration version
+            maintainer = "SciPy Developers",
+            maintainer_email = "scipy-dev@scipy.org",
+            description = "Scientific Algorithms Library for Python",
+            url = "http://www.scipy.org",
+            license = 'BSD',
+            configuration=configuration )
     finally:
         del sys.path[0]
         os.chdir(old_path)
