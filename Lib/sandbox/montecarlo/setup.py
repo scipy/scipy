847189be1c5d44c4b70085709e3560744837c404
edschofield <edschofield@localhost>
2006-04-19 12:37:46 +0000

    Improved portability of setup.py in montecarlo

--- Lib/sandbox/montecarlo/setup.py
+++ Lib/sandbox/montecarlo/setup.py
@@ -1,7 +1,7 @@
 import numpy
 from numpy.distutils.core import setup
 from numpy.distutils.misc_util import Configuration
-from os.path import join
+from os.path import join, dirname
 
 def configuration(parent_package='', top_path=None):
 
@@ -10,18 +10,16 @@ def configuration(parent_package='', top_path=None):
     # This code requires 'randomkit' to have been built using 'add_extension' in
     # numpy/random/setup.py.
 
+    random_lib_dir = dirname(numpy.random.__file__)
+
     config.add_extension('_intsampler',
-              include_dirs = [numpy.get_numpy_include(),
-                    # The following path needs to be extracted in a portable way.
-                    # This points to the default installation location used by
-                    # config.add_data_files().
-                   '/usr/lib/python2.4/site-packages/numpy/random/'],
+              include_dirs = [numpy.get_numpy_include(), random_lib_dir],
               libraries=['randomkit'],
-              library_dirs=['/usr/lib/python2.4/site-packages/numpy/random/'],
-              runtime_library_dirs=['/usr/lib/python2.4/site-packages/numpy/random/'],
+              library_dirs=[random_lib_dir],
+              runtime_library_dirs=[random_lib_dir],
               sources = [join('src', f) for f in
-                        ['_intsamplermodule.c', 'compact5table.c']]
-              ) 
+                        ['_intsamplermodule.c', 'compact5table.c']])
+    
     config.add_data_dir('tests')
     config.add_data_dir('examples')
     config.add_data_dir('doc')
