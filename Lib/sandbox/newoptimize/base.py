81b63fafd7c6858da7be3675a304642e74d50dd6
Robert Cimrman <cimrman3@ntc.zcu.cz>
2006-04-28 14:31:35 +0000

    Added to sandbox.

---
+++ Lib/sandbox/newoptimize/base.py
@@ -0,0 +1,57 @@
+##
+# 16.02.2005, c
+import numpy as nm
+import scipy.optimize as sopt
+import scipy.optimize.linesearch as linesearch
+import pylab
+
+import glob, re, time, sys
+
+##
+# 22.09.2005, c
+# 24.10.2005
+if sys.version[:5] < '2.4.0':
+    def sorted( sequence ):
+        tmp = copy( sequence )
+        tmp.sort()
+        return tmp
+
+
+# Some usefull definitions.
+
+##
+# 02.01.2005
+class Struct( object ):
+    # 03.10.2005, c
+    # 26.10.2005
+    def __init__( self, **kwargs ):
+        if kwargs:
+            self.__dict__.update( kwargs )
+        
+    # 08.03.2005
+    def __str__( self ):
+        ss = "%s\n" % self.__class__
+        for key, val in self.__dict__.iteritems():
+            if (issubclass( self.__dict__[key].__class__, Struct )):
+                ss += "  %s:\n    %s\n" % (key, self.__dict__[key].__class__)
+            else:
+                aux = "\n" + str( val )
+                aux = aux.replace( "\n", "\n    " );
+                ss += "  %s:\n%s\n" % (key, aux[1:])
+        return( ss.rstrip() )
+
+    # 08.03.2005, c
+    def strAll( self ):
+        ss = "%s\n" % self.__class__
+        for key, val in self.__dict__.iteritems():
+            if (issubclass( self.__dict__[key].__class__, Struct )):
+                ss += "  %s:\n" % key
+                aux = "\n" + self.__dict__[key].strAll()
+                aux = aux.replace( "\n", "\n    " );
+                ss += aux[1:] + "\n"
+            else:
+                aux = "\n" + str( val )
+                aux = aux.replace( "\n", "\n    " );
+                ss += "  %s:\n%s\n" % (key, aux[1:])
+        return( ss.rstrip() )
+        
---
+++ Lib/sandbox/newoptimize/log.py
@@ -0,0 +1,73 @@
+from base import *
+
+##
+# 20.04.2006, c
+class Log( Struct ):
+
+    ##
+    # 20.04.2006, c
+    # 21.04.2006
+    def fromConf( conf, dataNames ):
+        obj = Log( dataNames = dataNames, seqDataNames = [], igs = [],
+                   data = {}, nCalls = 0 )
+        
+        for ig, names in enumerate( obj.dataNames ):
+            for name in names:
+                obj.data[name] = []
+                obj.igs.append( ig )
+                obj.seqDataNames.append( name )
+        obj.nArg = len( obj.igs )
+        obj.nGr = len( obj.dataNames )
+        
+        try:
+            obj.isPlot = conf.isPlot
+        except:
+            obj.isPlot = True
+            
+        return obj
+    fromConf = staticmethod( fromConf )
+    
+    ##
+    # 20.04.2006, c
+    # 21.04.2006
+    # 26.04.2006
+    def __call__( self, *args, **kwargs ):
+
+        finished = False
+        if kwargs:
+            if kwargs.has_key( 'finished' ):
+                finished = kwargs['finished']
+        
+        ls = len( args ), self.nArg
+        if ls[0] != ls[1]:
+            raise IndexError, '%d == %d' % ls
+
+        for ii, name in enumerate( self.seqDataNames ):
+            self.data[name].append( args[ii] )
+
+        if self.isPlot:
+            pylab.ion()
+            if self.nCalls == 0:
+                self.fig = pylab.figure()
+                self.ax = []
+                for ig in range( self.nGr ):
+                    self.ax.append( pylab.subplot( 100 * self.nGr + 11 + ig ) )
+
+            
+            for ig in range( self.nGr ):
+                self.ax[ig].clear()
+            for ii, name in enumerate( self.seqDataNames ):
+                try:
+                    self.ax[self.igs[ii]].plot( self.data[name] )
+                except:
+                    pass
+            for ig in range( self.nGr ):
+                self.ax[ig].legend( self.dataNames[ig] )
+            pylab.draw()
+            pylab.ioff()
+
+        self.nCalls += 1
+
+        if finished and self.isPlot:
+            pylab.show()
+            return
