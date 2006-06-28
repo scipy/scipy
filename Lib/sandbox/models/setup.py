ffe9627a2b31dddcbb305786dae876bb1f89b4f6
jonathan.taylor <jonathan.taylor@localhost>
2006-06-28 21:11:00 +0000

    change sandbox.models setup file so it can be built and installed into existing scipy installation

--- Lib/sandbox/models/setup.py
+++ Lib/sandbox/models/setup.py
@@ -1,11 +1,12 @@
-def configuration(parent_package='',top_path=None):
+def configuration(parent_package='',top_path=None, package_name='models'):
     from numpy.distutils.misc_util import Configuration
-    config = Configuration('models',parent_package,top_path)
+    config = Configuration(package_name,parent_package,top_path)
 
     config.add_subpackage('*')
    
     return config
 
 if __name__ == '__main__':
+
     from numpy.distutils.core import setup
-    setup(**configuration(top_path='').todict())
+    setup(configuration(top_path='', package_name='scipy.sandbox.models'))
