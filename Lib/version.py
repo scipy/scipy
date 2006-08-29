f37050e329fb7e0f893b6668f2848ce749a5222f
Travis Oliphant <oliphant@enthought.com>
2006-08-29 03:40:28 +0000

    Use NumPy scheme for development versions of scipy.

--- Lib/version.py
+++ Lib/version.py
@@ -1,13 +1,15 @@
-version = '0.5.0'
+version = '0.5.1'
+release=False
 
-import os
-svn_version_file = os.path.join(os.path.dirname(__file__),
-                                '__svn_version__.py')
+if not release:
+    import os
+    svn_version_file = os.path.join(os.path.dirname(__file__),
+                                    '__svn_version__.py')
 
-if os.path.isfile(svn_version_file):
-    import imp
-    svn = imp.load_module('scipy.__svn_version__',
-                          open(svn_version_file),
-                          svn_version_file,
-                          ('.py','U',1))
-    version += '.'+svn.version
+    if os.path.isfile(svn_version_file):
+        import imp
+        svn = imp.load_module('scipy.__svn_version__',
+                              open(svn_version_file),
+                              svn_version_file,
+                              ('.py','U',1))
+        version += '.dev'+svn.version
