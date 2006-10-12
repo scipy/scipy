6a1d3ff4eca28693cb8cc24a35d3fb18e24ebc96
Tim Leslie <tim.leslie@gmail.com>
2006-10-12 06:50:41 +0000

    rearrange the SurvivalTime class hierarchy

--- Lib/sandbox/models/survival.py
+++ Lib/sandbox/models/survival.py
@@ -1,17 +1,18 @@
 import numpy as N
 
 class SurvivalTime:
-    pass
-
-class RightCensored(SurvivalTime):
-
     def __init__(self, time, delta):
         self.time, self.delta = time, delta
 
     def atrisk(self, time):
+        raise NotImplementedError
+
+class RightCensored(SurvivalTime):
+
+    def atrisk(self, time):
         return N.less_equal.outer(time, self.time)
 
-class LeftCensored(RightCensored):
+class LeftCensored(SurvivalTime):
 
     def atrisk(self, time):
         return N.greater_equal.outer(time, self.time)
