17dd389b04e42d5f46695cae0668fcf1cb07772c
Robert Kern <robert.kern@gmail.com>
2006-11-26 05:06:00 +0000

    Move the odr package out of the sandbox and into the main package due to requests.

--- Lib/setup.py
+++ Lib/setup.py
@@ -12,7 +12,7 @@ def configuration(parent_package='',top_path=None):
     config.add_subpackage('linsolve')
     config.add_subpackage('maxentropy')
     config.add_subpackage('misc')
-    #config.add_subpackage('montecarlo')
+    config.add_subpackage('odr')
     config.add_subpackage('optimize')
     config.add_subpackage('sandbox')
     config.add_subpackage('signal')
