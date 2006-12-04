c5bcd219f6873ef1cbb2c3e3f8a9efce15053f69
jonathan.taylor <jonathan.taylor@localhost>
2006-12-04 04:42:51 +0000

    setup changed to install _bspline in the relevant site_packages tree

--- Lib/sandbox/models/setup.py
+++ Lib/sandbox/models/setup.py
@@ -1,3 +1,4 @@
+
 def configuration(parent_package='',top_path=None, package_name='models'):
     from numpy.distutils.misc_util import Configuration
     config = Configuration(package_name,parent_package,top_path)
@@ -6,8 +7,19 @@ def configuration(parent_package='',top_path=None, package_name='models'):
 
     config.add_data_dir('tests')
 
-   
+    try:
+        from bspline_module import mod
+        n, s, d = weave_ext(mod)
+        config.add_extension(n, s, **d)
+    except ImportError: pass
+
     return config
+
+def weave_ext(mod):
+    d = mod.setup_extension().__dict__
+    n = d['name']; del(d['name'])
+    s = d['sources']; del(d['sources'])
+    return n, s, d
 
 if __name__ == '__main__':
 
