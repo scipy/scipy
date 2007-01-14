b0d049ed35a5e34c06b6adcd0f738b072fcf4d9d
Jarrod Millman <millman@berkeley.edu>
2007-01-14 12:35:28 +0000

    use absolute imports and define __all__

--- Lib/sandbox/models/__init__.py
+++ Lib/sandbox/models/__init__.py
@@ -2,15 +2,17 @@
 # models - Statistical Models
 #
 
-from info import __doc__
+from scipy.sandbox.models.info import __doc__
 
-import model
-import formula
-import regression
-import robust
-import family
-from glm import model as glm
-from rlm import model as rlm
+import scipy.sandbox.models.model
+import scipy.sandbox.models.formula
+import scipy.sandbox.models.regression
+import scipy.sandbox.models.robust
+import scipy.sandbox.models.family
+from scipy.sandbox.models.glm import model as glm
+from scipy.sandbox.models.rlm import model as rlm
+
+__all__ = filter(lambda s:not s.startswith('_'),dir())
 
 from numpy.testing import NumpyTest
 test = NumpyTest().test
