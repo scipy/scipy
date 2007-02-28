7c9996a293c4da6458ff52f6c1fb6311c852a3d3
mattknox_ca <mattknox_ca@localhost>
2007-02-28 21:20:03 +0000

    fixed typo in Matt's email address

--- Lib/sandbox/timeseries/tmulti.py
+++ Lib/sandbox/timeseries/tmulti.py
@@ -3,7 +3,7 @@
 Support for multi-variable time series, through masked recarrays.
 
 :author: Pierre GF Gerard-Marchant & Matt Knox
-:contact: pierregm_at_uga_dot_edu - mattknow_ca_at_hotmail_dot_com
+:contact: pierregm_at_uga_dot_edu - mattknox_ca_at_hotmail_dot_com
 :version: $Id$
 """
 __author__ = "Pierre GF Gerard-Marchant & Matt Knox ($Author$)"
