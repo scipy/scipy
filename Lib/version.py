6999836eeee578b6bcaaf2c2d08f394828298697
Robert Kern <robert.kern@gmail.com>
2007-04-04 17:27:42 +0000

    Allow for change in numpy r3660 to not generate __svn_version__.py when outside of an SVN checkout.

--- Lib/version.py
+++ Lib/version.py
@@ -3,6 +3,7 @@ release=False
 
 if not release:
     import os
+    version += '.dev'
     svn_version_file = os.path.join(os.path.dirname(__file__),
                                     '__svn_version__.py')
 
@@ -12,4 +13,4 @@ if not release:
                               open(svn_version_file),
                               svn_version_file,
                               ('.py','U',1))
-        version += '.dev'+svn.version
+        version += svn.version
