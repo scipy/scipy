3b346990e69e704233942dbc754f8f598683fce9
Robert Kern <robert.kern@gmail.com>
2007-04-15 05:16:13 +0000

    Remove dependency on scipy.linalg in favor of numpy.dual

--- Lib/odr/odrpack.py
+++ Lib/odr/odrpack.py
@@ -320,7 +320,7 @@ class RealData(Data):
     covx and covy are arrays of covariance matrices and are converted to weights
     by performing a matrix inversion on each observation's covariance matrix.
 
-      E.g.  we[i] = scipy.linalg.inv(covy[i])  # i in range(len(covy))
+      E.g.  we[i] = numpy.linalg.inv(covy[i])  # i in range(len(covy))
                                                #   if covy.shape == (n,q,q)
 
     These arguments follow the same structured argument conventions as wd and we
@@ -376,15 +376,15 @@ class RealData(Data):
         """ Convert covariance matrix(-ices) to weights.
         """
 
-        from scipy import linalg
+        from numpy.dual import inv
 
         if len(cov.shape) == 2:
-            return linalg.inv(cov)
+            return inv(cov)
         else:
             weights = numpy.zeros(cov.shape, float)
 
             for i in range(cov.shape[-1]):  # n
-                weights[:,:,i] = linalg.inv(cov[:,:,i])
+                weights[:,:,i] = inv(cov[:,:,i])
 
             return weights
 
