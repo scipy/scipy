e30db54e0770764b2567e53acf068c6a5b229a64
fullung <fullung@localhost>
2007-04-17 13:23:03 +0000

    Fixed ARPACK includes for Windows.

--- Lib/sandbox/arpack/setup.py
+++ Lib/sandbox/arpack/setup.py
@@ -19,7 +19,7 @@ def configuration(parent_package='',top_path=None):
     arpack_sources.extend([os.path.join('ARPACK','LAPACK', '*.f')])
 
     config.add_library('arpack', sources=arpack_sources,
-                       include_dirs=[os.path.join(['ARPACK', 'SRC'])])
+                       include_dirs=[os.path.join('ARPACK', 'SRC')])
 
 
     config.add_extension('_arpack',
