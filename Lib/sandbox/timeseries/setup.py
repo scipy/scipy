cf1cef94ec46decb469d5aaf553c0de00147489b
mattknox_ca <mattknox_ca@localhost>
2007-05-09 17:10:27 +0000

    updated to reflect changes to C code

--- Lib/sandbox/timeseries/setup.py
+++ Lib/sandbox/timeseries/setup.py
@@ -10,11 +10,13 @@ def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration, get_numpy_include_dirs
     nxheader = join(get_numpy_include_dirs()[0],'numpy',)
     confgr = Configuration('timeseries',parent_package,top_path)
-    sources = join('src', 'cseries.c')
+    sources = [join('src', x) for x in ('c_lib.c',
+                                        'c_tdates.c',
+                                        'c_tseries.c',
+                                        'cseries.c')]
     confgr.add_extension('cseries',
-                         sources=[sources,],
-                         include_dirs=[nxheader],
-                         )
+                         sources=sources,
+                         include_dirs=[nxheader, 'include'])
 
     confgr.add_subpackage('lib')
     confgr.add_subpackage('io')
