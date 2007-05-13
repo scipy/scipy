806b1b19a332ae5b0b895752212ecbecb801bf7d
ondrej <ondrej@localhost>
2007-05-13 16:46:21 +0000

    Patch implementing nonlinear solvers, see the ticket 402.

--- Lib/optimize/__init__.py
+++ Lib/optimize/__init__.py
@@ -11,6 +11,7 @@ from anneal import *
 from lbfgsb import fmin_l_bfgs_b
 from tnc import fmin_tnc
 from cobyla import fmin_cobyla
+import nonlin
 
 __all__ = filter(lambda s:not s.startswith('_'),dir())
 from numpy.testing import NumpyTest
