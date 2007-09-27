5b159d0cb9f86364cc1729a8e3c032146b44064a
dhuard <dhuard@localhost>
2007-09-27 14:03:00 +0000

    Added tests for stats stub.

--- scipy/sandbox/dhuard/stats.py
+++ scipy/sandbox/dhuard/stats.py
@@ -1,32 +1,36 @@
 import scipy.interpolate as interpolate
 import numpy as np
 
-def scoreatpercentile(data, per):
-    """Return the score at the given 'per' percentile of the data.
+def scoreatpercentile(data, percentile):
+    """Return the score at the given percentile of the data.
     
-    Example
-    >>> scoreatpercentile(randn(100), 50)
-    will return the median of the sample.
+    Example:
+    	>>> data = randn(100)
+	    >>> scoreatpercentile(data, 50)
+    	
+    	will return the median of sample `data`.
     """
+    per = np.array(percentile)
     cdf = empiricalcdf(data)
-    interpolator = interpolate.interp1d(sort(cdf), sort(data))
+    interpolator = interpolate.interp1d(np.sort(cdf), np.sort(data))
     return interpolator(per/100.)
     
 def percentileofscore(data, score):
-    """Percentile-position of score relative to data.
+    """Return the percentile-position of score relative to data.
       
     score: Array of scores at which the percentile is computed.
     
     Return percentiles (0-100).
     
     Example
+	    r = randn(50)
         x = linspace(-2,2,100)
-        percentileofscore(randn(50),x)
+        percentileofscore(r,x)
         
-    Return an error if the score is outside the range of data. 
+    Raise an error if the score is outside the range of data. 
     """
     cdf = empiricalcdf(data)
-    interpolator = interpolate.interp1d(sort(data), sort(cdf))
+    interpolator = interpolate.interp1d(np.sort(data), np.sort(cdf))
     return interpolator(score)*100.
 
 def empiricalcdf(data, method='Hazen'):
@@ -46,11 +50,10 @@ def empiricalcdf(data, method='Hazen'):
     i = np.argsort(np.argsort(data)) + 1.
     N = len(data)
     method = method.lower()
-    
-    if method == 'weibull':
-        cdf = i/(N+1.)
-    elif method == 'hazen':
+    if method == 'hazen':
         cdf = (i-0.5)/N
+    elif method == 'weibull':
+        cdf = i/(N+1.)
     elif method == 'california':
         cdf = (i-1.)/N
     elif method == 'chegodayev':
