45ab30dc1af2e0e1c0d62b8bb0e9f626b7b01cb4
Travis Oliphant <oliphant@enthought.com>
2007-11-07 20:36:40 +0000

    Add missing files to ndimage.segment

---
+++ scipy/ndimage/segment/objectdata.py
@@ -0,0 +1,25 @@
+
+import numpy as N
+
+objstruct =N.dtype([('L', 'i'),
+                    ('R', 'i'),
+                    ('T', 'i'),
+                    ('B', 'i'),
+                    ('Label', 'i'),
+                    ('Area', 'i'),
+                    ('cX', 'f'),
+                    ('cY', 'f'),
+                    ('curveClose', 'i'),
+                    ('cXB', 'f'),
+                    ('cYB', 'f'),
+                    ('bLength', 'f'),
+                    ('minRadius', 'f'),
+                    ('maxRadius', 'f'),
+                    ('aveRadius', 'f'),
+                    ('ratio', 'f'),
+                    ('compactness', 'f'),
+                    ('voxelMean', 'f'),
+                    ('voxelVar', 'f'),
+                    ('TEM', 'f', 20)]
+                   )
+
