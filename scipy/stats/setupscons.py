09cac53147e30f7c653129b7dad37c3872e13e70
David Cournapeau <cournape@gmail.com>
2007-11-26 05:32:08 +0000

    stats now builds with scons

--- scipy/stats/setupscons.py
+++ scipy/stats/setupscons.py
@@ -6,7 +6,7 @@ def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
     config = Configuration('stats', parent_package, top_path, setup_name = 'setupscons.py')
 
-    config.add_subpackage('models')
+    #config.add_subpackage('models')
     config.add_data_dir('tests')
 
     config.add_sconscript('SConstruct')
