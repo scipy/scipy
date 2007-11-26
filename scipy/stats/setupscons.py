678ffa5d88e91036f3c19d7d5d636c83b5ca91ee
David Cournapeau <cournape@gmail.com>
2007-11-26 06:24:52 +0000

    add forgotten mvn extension in scipy.stats

--- scipy/stats/setupscons.py
+++ scipy/stats/setupscons.py
@@ -4,9 +4,10 @@ from os.path import join
 
 def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
-    config = Configuration('stats', parent_package, top_path, setup_name = 'setupscons.py')
+    config = Configuration('stats', parent_package, top_path, 
+                           setup_name = 'setupscons.py')
 
-    #config.add_subpackage('models')
+    config.add_subpackage('models')
     config.add_data_dir('tests')
 
     config.add_sconscript('SConstruct')
