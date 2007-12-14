7a0941a19b454f6cdc9d796721e6e92c600f7934
Jarrod Millman <millman@berkeley.edu>
2007-12-14 23:45:19 +0000

    cleaned up imports

--- __init__.py
+++ __init__.py
@@ -1,4 +1,3 @@
-
 #
 # io - Data input and output
 #
@@ -7,17 +6,22 @@ from info import __doc__
 
 from numpyio import packbits, unpackbits, bswap, fread, fwrite, \
      convert_objectarray
+
 # matfile read and write
 from matlab.mio import loadmat, savemat
-# netCDF file support
-from netcdf import *
-from npfile import npfile
-from recaster import sctype_attributes, Recaster
-from array_import import *
-from data_store import *
-from pickler import *
 
-from mmio import mminfo,mmread,mmwrite
+# netCDF file support
+from netcdf import netcdf_file, netcdf_variable
+
+from npfile import npfile
+
+from recaster import sctype_attributes, Recaster
+
+from array_import import read_array, write_array
+from data_store import read_array, write_array
+from pickler import objload, objsave
+
+from mmio import mminfo, mmread, mmwrite
 
 __all__ = filter(lambda s:not s.startswith('_'),dir())
 from numpy.testing import NumpyTest
