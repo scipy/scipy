4845bac4bc33445049762b92b9728e088114a255
wnbell <wnbell@localhost>
2008-01-07 21:41:21 +0000

    modified multigrid test

--- scipy/sandbox/multigrid/simple_test.py
+++ scipy/sandbox/multigrid/simple_test.py
@@ -1,8 +1,17 @@
-from multilevel import *
 from scipy import *
+from scipy.sandbox.multigrid.sa import *
+from scipy.sandbox.multigrid import *
+from scipy.sandbox.multigrid.utils import *
+from time import clock
 
-A = poisson_problem2D(200)
-rs_solver = ruge_stuben_solver(A)
+A = poisson_problem2D(500)
+
+start = clock()
+sa = smoothed_aggregation_solver(A)
+print "constructed solver in %s seconds" % (clock() - start)
+
 b = rand(A.shape[0])
-x,residuals = rs_solver.solve(b,return_residuals=True)
+start = clock()
+x,residuals = sa.solve(b,return_residuals=True)
+print "solved in %s seconds" % (clock() - start)
 print 'residuals',residuals
