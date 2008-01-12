4d8d9113641459d4f1aa7cbfe2d8d8c25c8b6dee
Matthew Brett <matthew.brett@gmail.com>
2008-01-12 01:54:24 +0000

    and refixed sparse init for nose tests

--- scipy/sparse/__init__.py
+++ scipy/sparse/__init__.py
@@ -15,5 +15,5 @@ from construct import *
 from spfuncs import *
 
 __all__ = filter(lambda s:not s.startswith('_'),dir())
-from numpy.testing import NumpyTest
-test = NumpyTest().test
+from scipy.testing.pkgtester import Tester
+test = Tester().test
