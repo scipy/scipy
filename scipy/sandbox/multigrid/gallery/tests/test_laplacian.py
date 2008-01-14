cb6123524d48d1a9f0b94712af9e9f4884bbe9ca
wnbell <wnbell@localhost>
2008-01-14 21:31:13 +0000

    expanded poisson() unittest

--- scipy/sandbox/multigrid/gallery/tests/test_laplacian.py
+++ scipy/sandbox/multigrid/gallery/tests/test_laplacian.py
@@ -8,6 +8,7 @@ class TestPoisson(TestCase):
     def test_poisson(self):
         cases = []
 
+        # 1D
         cases.append( ((1,),matrix([[2]])) )
         cases.append( ((2,),matrix([[ 2,-1],
                                     [-1, 2]])) )
@@ -16,10 +17,35 @@ class TestPoisson(TestCase):
                                     [ 0,-1, 2,-1],
                                     [ 0, 0,-1, 2]])) )
         
+        # 2D        
+        cases.append( ((1,1), matrix([[4]])) )
+        cases.append( ((2,1), matrix([[ 4,-1],
+                                      [-1, 4]])) )
+        cases.append( ((1,2), matrix([[ 4,-1],
+                                      [-1, 4]])) )
+        cases.append( ((1,3), matrix([[ 4,-1, 0],
+                                      [-1, 4,-1],
+                                      [ 0,-1, 4]])) )
+        cases.append( ((2,2), matrix([[ 4,-1,-1, 0],
+                                      [-1, 4, 0,-1],
+                                      [-1, 0, 4,-1],
+                                      [ 0,-1,-1, 4]])) )
+        # 3D
+        cases.append( ((2,2,1), matrix([[ 6,-1,-1, 0],
+                                        [-1, 6, 0,-1],
+                                        [-1, 0, 6,-1],
+                                        [ 0,-1,-1, 6]])) )
+        cases.append( ((2,2,2), matrix([[ 6,-1,-1, 0,-1, 0, 0, 0],
+                                        [-1, 6, 0,-1, 0,-1, 0, 0],
+                                        [-1, 0, 6,-1, 0, 0,-1, 0],
+                                        [ 0,-1,-1, 6, 0, 0, 0,-1],
+                                        [-1, 0, 0, 0, 6,-1,-1, 0],
+                                        [ 0,-1, 0, 0,-1, 6, 0,-1],
+                                        [ 0, 0,-1, 0,-1, 0, 6,-1],
+                                        [ 0, 0, 0,-1, 0,-1,-1, 6]])) )
+
         for grid,expected in cases:
             assert_equal( poisson(grid).todense(), expected )
-
-
 
 if __name__ == '__main__':
     unittest.main()
