abea3b647d09207e11aa2a4acf7822b98c08bf78
Robert Kern <robert.kern@gmail.com>
2008-01-28 17:02:36 +0000

    Remove // comments from C code.

--- scipy/fftpack/src/zfft_fftw3.c
+++ scipy/fftpack/src/zfft_fftw3.c
@@ -1,5 +1,5 @@
 
-// This cache uses FFTW_MEASURE for the plans, and do not copy the data.
+/* This cache uses FFTW_MEASURE for the plans, and do not copy the data. */
 GEN_CACHE(zfftw3,(int n,int d)
 	,int direction;
 	fftw_plan plan;
@@ -7,19 +7,17 @@ GEN_CACHE(zfftw3,(int n,int d)
 	,((caches_zfftw3[i].n==n) &&
 	    (caches_zfftw3[i].direction==d))
 	,caches_zfftw3[id].direction = d;
-        // This working buffer is only used to compute the plan: we need it
-        // since FFTW_MEASURE destroys its input when computing a plan
+        /* This working buffer is only used to compute the plan: we need it
+           since FFTW_MEASURE destroys its input when computing a plan */
 	    caches_zfftw3[id].wrk = fftw_malloc(n * sizeof(double) * 2); 
 	    caches_zfftw3[id].plan = fftw_plan_dft_1d(n, 
 	        caches_zfftw3[id].wrk,
 	        caches_zfftw3[id].wrk,
 		(d>0?FFTW_FORWARD:FFTW_BACKWARD),
 		FFTW_ESTIMATE | FFTW_UNALIGNED);
-	,//fftw_print_plan(caches_zfftw3[id].plan);
+	,
     fftw_destroy_plan(caches_zfftw3[id].plan);
 	fftw_free(caches_zfftw3[id].wrk);
-    //fflush(stdout);
-    //fprintf(stderr, "aligned count %d\n", countaligned);
 	,10)
 
 static void zfft_fftw3(complex_double * inout, int n, int dir, int
@@ -56,8 +54,6 @@ howmany, int normalize)
 		for (i = n * howmany - 1; i >= 0; --i) {
 			*((double *) (ptr)) *= factor;
 			*((double *) (ptr++) + 1) *= factor;
-			//*((double *) (ptr)) /= n;
-			//*((double *) (ptr++) + 1) /= n;
 		}
 	}
 }
