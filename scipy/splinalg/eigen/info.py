124f1d4f780c58c0fae7d664877f41ce7c39e370
hagberg <hagberg@localhost>
2008-02-06 18:28:08 +0000

    Add info, __init__, and setup to splinalg.eigen

---
+++ scipy/splinalg/eigen/info.py
@@ -0,0 +1,18 @@
+"""
+Sparse Eigenvalue Solvers
+=========================
+
+There are submodules of splinalg:
+    1. arpack: spare eigenvalue solver using iterative methods
+
+
+Examples
+========
+
+
+
+"""
+
+#TODO show examples
+
+postpone_import = 1
