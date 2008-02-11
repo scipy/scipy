53a2457564c313cb1846a5972ef916289b86c234
Matthew Brett <matthew.brett@gmail.com>
2008-02-11 23:27:05 +0000

    Trimmed nulltester, no functional change

--- scipy/testing/nulltester.py
+++ scipy/testing/nulltester.py
@@ -8,12 +8,9 @@ See pkgtester, nosetester modules
 '''
 
 class NullTester(object):
-    _msg = 'Need nose >=0.10 for tests - see %s' % \
-           'http://somethingaboutorange.com/mrl/projects/nose'
-    def __init__(self, *args, **kwargs):
-        pass
     def test(self, labels=None, *args, **kwargs):
-        raise ImportError, self._msg
-    def bench(self, labels=None, *args, **kwargs):
-        raise ImportError, self._msg
+        raise ImportError, \
+              'Need nose >=0.10 for tests - see %s' % \
+              'http://somethingaboutorange.com/mrl/projects/nose'
+    bench = test
     
