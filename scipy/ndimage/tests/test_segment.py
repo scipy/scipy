25aca679611567a986e2b6fd8cc10bac17741a86
tom.waite <tom.waite@localhost>
2008-02-13 01:32:07 +0000

    Assert test for boundary length and compactness of the aorta and ventricle

--- scipy/ndimage/tests/test_segment.py
+++ scipy/ndimage/tests/test_segment.py
@@ -15,14 +15,28 @@ class TestSegment(TestCase):
         get_shape_mask(edges, objects)
         get_voxel_measures(sourceImage, edges, objects)
         get_texture_measures(sourceImage, edges, objects)
+	# measure the compactness and object boundry length
+	# Ventricle measure
+	assert_almost_equal(objects[7]['compactness'], 0.25657323, 4)
+	assert_almost_equal(objects[7]['bLength'], 1215.70980000, 4)
+	# Aorta measure
+	assert_almost_equal(objects[13]['compactness'], 0.91137904, 4)
+	assert_almost_equal(objects[13]['bLength'], 198.338090000, 4)
 
     def test2(self):
         sourceImage, labeledMask, ROIList = segment_regions(filename)
+	# measure the compactness and object boundry length
+	# Ventricle measure
+	assert_almost_equal(ROIList[7]['compactness'], 0.25657323, 4)
+	assert_almost_equal(ROIList[7]['bLength'], 1215.70980000, 4)
+	# Aorta measure
+	assert_almost_equal(ROIList[13]['compactness'], 0.91137904, 4)
+	assert_almost_equal(ROIList[13]['bLength'], 198.338090000, 4)
 
     def test3(self):
         regionMask, numberRegions = grow_regions(filename)
-        regionMask.max()
-        #save_slice(regionMask, 'regionMask.raw')
+	number_of_regions = regionMask.max()
+	assert_equal(number_of_regions, 21)
 
     
 if __name__ == "__main__":
