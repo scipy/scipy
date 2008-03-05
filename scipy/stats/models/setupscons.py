b0f0f029cf72537df3d3f56e0b2e37d8d2ecab12
David Cournapeau <cournape@gmail.com>
2008-03-05 12:48:09 +0000

    stats now builds with numscons.

---
+++ scipy/stats/models/setupscons.py
@@ -0,0 +1,27 @@
+
+def configuration(parent_package='',top_path=None, package_name='models'):
+    from numpy.distutils.misc_util import Configuration
+    config = Configuration(package_name,parent_package,top_path)
+
+    config.add_subpackage('*')
+
+    config.add_data_dir('tests')
+
+    try:
+        from scipy.stats.models.bspline_module import mod
+        n, s, d = weave_ext(mod)
+        config.add_extension(n, s, **d)
+    except ImportError: pass
+
+    return config
+
+def weave_ext(mod):
+    d = mod.setup_extension().__dict__
+    n = d['name']; del(d['name'])
+    s = d['sources']; del(d['sources'])
+    return n, s, d
+
+if __name__ == '__main__':
+
+    from numpy.distutils.core import setup
+    setup(**configuration(top_path='', package_name='scipy.stats.models').todict())
