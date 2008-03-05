28f9e8415cb096d873943ecbb258ddeccc2207ec
David Cournapeau <cournape@gmail.com>
2008-03-05 08:14:54 +0000

    optimize now builds with numscons.

---
+++ scipy/optimize/setupscons.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from os.path import join
+
+def configuration(parent_package='',top_path=None):
+    from numpy.distutils.misc_util import Configuration
+    from numpy.distutils.system_info import get_info
+    config = Configuration('optimize',parent_package, top_path)
+
+    config.add_sconscript('SConstruct')
+    config.add_data_dir('tests')
+
+    return config
+
+if __name__ == '__main__':
+    from numpy.distutils.core import setup
+    setup(**configuration(top_path='').todict())
