a861e5079ca3c98f86cf7e595345c31ec66c9a6c
David Cournapeau <cournape@gmail.com>
2008-03-19 13:16:32 +0000

    Update scons build for sparsetools.

---
+++ scipy/sparse/sparsetools/setupscons.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from os.path import join
+import sys
+
+def configuration(parent_package='',top_path=None):
+    import numpy
+    from numpy.distutils.misc_util import Configuration
+
+    config = Configuration('sparse',parent_package,top_path, 
+                           setup_name = 'setupscons.py')
+
+    config.add_sconscript('SConstruct')
+
+    return config
+
+if __name__ == '__main__':
+    from numpy.distutils.core import setup
+    setup(**configuration(top_path='').todict())
