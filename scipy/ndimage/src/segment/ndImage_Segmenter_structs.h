517af5d8bc0260e466a919f12284cb5858775e81
tom.waite <tom.waite@localhost>
2008-03-26 23:36:52 +0000

    completed Law's texture feature code.

--- scipy/ndimage/src/segment/ndImage_Segmenter_structs.h
+++ scipy/ndimage/src/segment/ndImage_Segmenter_structs.h
@@ -4,31 +4,6 @@
 #define bool unsigned char
 
 typedef struct{
-    int x;
-    int y;
-}POINT;
-
-typedef struct{
-    int x;
-    int y;
-    int linkIndex;
-    bool haveLink;
-}bPOINT;
-
-typedef struct{
-    int left;
-    int right;
-    int top;
-    int bottom;
-}RECT;
-
-typedef struct{
-    char filterName[20];
-    float Mean;
-    float Variance;
-}tTEM;
-
-typedef struct{
     int numberKernels;
     int kernelLength;
     int numberFilterLayers;
@@ -46,51 +21,6 @@ typedef struct{
     int Area;
     float cX;
     float cY;
-    // filled in BuildBoundary
-    int   curveClose;
-    float cXBoundary;
-    float cYBoundary;
-    float boundaryLength;
-    float minRadius;
-    float maxRadius;
-    float aveRadius;
-    float ratio;
-    float compactness;
-    // filled in VoxelMeasures
-    float voxelMean;
-    float voxelVar;
-    // filled in TextureMeasures
-    float TEM[20];
 }objStruct;
-
-typedef struct{
-    int numberPoints;
-    int curveClose;
-    int classify;
-    float boundaryLength;
-    float minRadius;
-    float maxRadius;
-    float aveRadius;
-    float ratio;
-    float compactness;
-    float voxelMean;
-    float voxelVar;
-    RECT rectangle;
-    POINT centroid;
-    bool isWithin;
-    bool closedCurve;
-    bool criticalSize;
-    int Label;
-}boundaryIndex;
-
-
-typedef struct{
-    POINT xy;
-}blobBoundary;
-
-
-//
-// prototypes
-//
 
 #endif
