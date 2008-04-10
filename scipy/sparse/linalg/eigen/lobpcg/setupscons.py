82cae922e7fc6125881d55771a4051d2f3a1fe32
David Cournapeau <cournape@gmail.com>
2008-04-10 22:59:42 +0000

    Add logpbc subpackage for scons build.

--- scipy/sparse/linalg/eigen/lobpcg/setupscons.py
+++ scipy/sparse/linalg/eigen/lobpcg/setupscons.py
@@ -2,12 +2,11 @@
 
 def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
-    
-    config = Configuration('eigen',parent_package,top_path, setup_name = 'setupscons.py')
-    
-    config.add_subpackage(('arpack'))
-    config.add_subpackage(('lobpcg'))
-    
+    from numpy.distutils.system_info import get_info
+
+    config = Configuration('lobpcg',parent_package,top_path)
+    config.add_data_dir('tests')
+
     return config
 
 if __name__ == '__main__':
