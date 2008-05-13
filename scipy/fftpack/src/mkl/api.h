b120fcef6a57a6b68e630619862374913de5f8dd
David Cournapeau <cournape@gmail.com>
2008-05-13 09:07:33 +0000

    Forgot to add one file for mkl backend.

---
+++ scipy/fftpack/src/mkl/api.h
@@ -0,0 +1,16 @@
+#ifndef _SCIPY_FFTPACK_MKL_API_H_
+#define _SCIPY_FFTPACK_MKL_API_H_
+
+#include "fftpack.h"
+
+/*
+ * straight FFT api
+ */
+void zfft_mkl(complex_double * inout,
+		 int n, int direction, int howmany, int normalize);
+
+void zfftnd_mkl(complex_double * inout, int rank,
+			 int *dims, int direction, int howmany,
+			 int normalize);
+
+#endif
