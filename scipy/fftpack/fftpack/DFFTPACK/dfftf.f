ac186e3be2c660fd5cb2591288b0dd40f4b53ce5
David Cournapeau <cournape@gmail.com>
2008-05-15 14:09:14 +0000

    Move sources of dfftpack in subdir

---
+++ scipy/fftpack/fftpack/DFFTPACK/dfftf.f
@@ -0,0 +1,7 @@
+      SUBROUTINE DFFTF (N,R,WSAVE)
+      IMPLICIT DOUBLE PRECISION (A-H,O-Z)
+      DIMENSION       R(1)       ,WSAVE(1)
+      IF (N .EQ. 1) RETURN
+      CALL DFFTF1 (N,R,WSAVE,WSAVE(N+1),WSAVE(2*N+1))
+      RETURN
+      END
---
+++ scipy/fftpack/fftpack/DFFTPACK/dsint.f
@@ -0,0 +1,10 @@
+      SUBROUTINE DSINT (N,X,WSAVE)
+      IMPLICIT DOUBLE PRECISION (A-H,O-Z)
+      DIMENSION       X(1)       ,WSAVE(1)      
+      NP1 = N+1
+      IW1 = N/2+1
+      IW2 = IW1+NP1
+      IW3 = IW2+NP1
+      CALL DSINT1(N,X,WSAVE,WSAVE(IW1),WSAVE(IW2),WSAVE(IW3))
+      RETURN
+      END
