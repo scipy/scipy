937ae6c171fa5ae6f2e0718123a04c3d9e7b63e7
David Cournapeau <cournape@gmail.com>
2008-05-15 16:05:29 +0000

    fftpack backend now exposes both fft and convolve.

--- scipy/fftpack/fftpack/__init__.py
+++ scipy/fftpack/fftpack/__init__.py
@@ -1,3 +1,9 @@
 from _fftpack import zfft_fftpack as zfft, \
 	zfftnd_fftpack as zfftnd, \
-	drfft_fftpack as drfft
+	drfft_fftpack as drfft, \
+	zrfft_fftpack as zrfft
+
+from convolve import convolve_fftpack as convolve, \
+	convolve_z_fftpack as convolve_z, \
+	init_convolution_kernel_fftpack as init_convolution_kernel, \
+        destroy_convolve_cache_fftpack as destroy_convolve_cache
