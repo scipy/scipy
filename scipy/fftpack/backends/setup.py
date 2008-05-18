557666df37e735aef11b542d371bed15b9e27080
David Cournapeau <cournape@gmail.com>
2008-05-18 10:52:48 +0000

    Build extensions in backend dir instead of building in each backend.

--- scipy/fftpack/backends/setup.py
+++ scipy/fftpack/backends/setup.py
@@ -1,15 +1,43 @@
 #!/usr/bin/env python
-# Created by Pearu Peterson, August 2002
-
 from os.path import join
+
+from numpy.distutils.system_info import get_info
+
+SRC = {}
+SRC['fftw'] = [join('fftw/src', i) for i in 
+                   ['zfft.cxx', 'drfft.cxx', 'zfftnd.cxx']]
+SRC['fftw3'] = [join('fftw3/src', i) for i in 
+                ['zfft.cxx', 'drfft.cxx', 'zfftnd.cxx']]
+SRC['mkl'] = [join('mkl/src', i) for i in 
+                ['zfft.cxx', 'zfftnd.cxx']]
+
+def build_backend(config, name, config_name = None):
+    if config_name is None:
+        config_name = name
+
+    info = get_info(config_name)
+    if info:
+        config.add_library("%s_backend" % name,
+                           sources = SRC[name],
+                           include_dirs = ["common",
+                                           info['include_dirs']])
+        config.add_extension("%s._%s" % (name, name), 
+                sources = ["%s/%s.pyf" % (name, name)], 
+                extra_info = info, libraries = ["%s_backend" % name])
+
+    config.add_subpackage(name)
 
 def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
     config = Configuration('backends', parent_package, top_path)
         
-    config.add_subpackage("fftw3")
-    config.add_subpackage("fftw")
-    config.add_subpackage("mkl")
+    # It is a bit of an hack: we build the extensions here, but we add
+    # subpackages. The reason why we build the extensions here is because we
+    # share files between backends, and scons cannot use sources outside its
+    # top directory.
+    build_backend(config, 'fftw', 'fftw2')
+    build_backend(config, 'fftw3')
+    build_backend(config, 'mkl')
 
     return config
 
