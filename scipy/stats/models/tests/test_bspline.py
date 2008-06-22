696546b69865cf61e5e6f5c3d9ce0a9339e3d69f
David Cournapeau <cournape@gmail.com>
2008-06-22 15:14:43 +0000

    Do not run bspline tests if _bspline is not available.

--- scipy/stats/models/tests/test_bspline.py
+++ scipy/stats/models/tests/test_bspline.py
@@ -6,17 +6,21 @@ import numpy as N
 from scipy.testing import *
 
 import scipy.stats.models as S
-import scipy.stats.models.bspline as B
+try:
+    import scipy.stats.models.bspline as B
+except ImportError:
+    B = None
 
 
 class TestBSpline(TestCase):
 
     def test1(self):
-        b = B.BSpline(N.linspace(0,10,11), x=N.linspace(0,10,101))
-        old = b._basisx.shape
-        b.x = N.linspace(0,10,51)
-        new = b._basisx.shape
-        self.assertEqual((old[0], 51), new)
+        if B:
+            b = B.BSpline(N.linspace(0,10,11), x=N.linspace(0,10,101))
+            old = b._basisx.shape
+            b.x = N.linspace(0,10,51)
+            new = b._basisx.shape
+            self.assertEqual((old[0], 51), new)
 
 
 if __name__ == "__main__":
