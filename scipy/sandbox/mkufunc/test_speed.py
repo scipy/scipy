dcc71733552cb469f76663c9aaea2378c10d10f5
Ilan Schnell <ilan@enthought.com>
2008-06-27 22:29:20 +0000

    Added blitz

--- scipy/sandbox/mkufunc/test_speed.py
+++ scipy/sandbox/mkufunc/test_speed.py
@@ -2,7 +2,8 @@
 from math import sin, cos
 import time
 
-from numpy import arange, vectorize
+from numpy import arange, vectorize, allclose
+from scipy import weave
 
 from mkufunc import mkufunc
 
@@ -11,27 +12,38 @@ def f(x):
     return 4.2 * x * x + 3.7 * x + 1.5
 
 
-ufunc = mkufunc([(float, float)])(f)
-
 vfunc = vectorize(f)
+
+ufunc = mkufunc([(float, float)])(f)
 
 
 x = arange(0, 1000, 0.001)    #print "x =", x, x.dtype
 
 start_time = time.time()
-y = 4.2 * x * x + 3.7 * x + 1.5
+b_y = x.copy()
+weave.blitz("b_y[:] = 4.2 * x[:] * x[:] + 3.7 * x[:] + 1.5")
+b_time = time.time() - start_time
+print 'blitz: %.6f sec' % b_time
+
+start_time = time.time()
+n_y = f(x)
 n_time = time.time() - start_time
 print 'numpy: %.6f sec' % n_time
 
 start_time = time.time()
-y = vfunc(x)
+v_y = vfunc(x)
 v_time = time.time() - start_time
 print 'vectorize: %.6f sec' % v_time
 
 start_time = time.time()
-y = ufunc(x)
+u_y = ufunc(x)
 u_time = time.time() - start_time
 print 'mkufunc: %.6f sec' % u_time
 
-print "speedup over numpy:", n_time/u_time
+print "speedup over blitz:",     b_time/u_time
+print "speedup over numpy:",     n_time/u_time
 print "speedup over vectorize:", v_time/u_time
+
+assert allclose(b_y, n_y)
+assert allclose(v_y, n_y)
+assert allclose(u_y, n_y)
