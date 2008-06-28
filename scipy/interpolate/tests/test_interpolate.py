c86bf80ddcbce8afe19f1469b2c1295dd9c0e5b5
Pauli Virtanen <pav@iki.fi>
2008-06-28 13:54:11 +0000

    interpolate: Fix #289. Make interp1d order axes in the result correctly when y.ndim > 2. Fix a bug in splmake that was triggered when y.ndim > 2. Add corresponding tests.

--- scipy/interpolate/tests/test_interpolate.py
+++ scipy/interpolate/tests/test_interpolate.py
@@ -29,7 +29,7 @@ class TestInterp1D(TestCase):
 
         self.y210 = np.arange(20.).reshape((2, 10))
         self.y102 = np.arange(20.).reshape((10, 2))
-
+        
         self.fill_value = -100.0
 
     def test_validation(self):
@@ -125,13 +125,30 @@ class TestInterp1D(TestCase):
             np.array([2.4, 5.6, 6.0]),
         )
 
+    def test_cubic(self):
+        """ Check the actual implementation of spline interpolation.
+        """
 
-    def test_bounds(self):
+        interp10 = interp1d(self.x10, self.y10, kind='cubic')
+        assert_array_almost_equal(
+            interp10(self.x10),
+            self.y10,
+        )
+        assert_array_almost_equal(
+            interp10(1.2),
+            np.array([1.2]),
+        )
+        assert_array_almost_equal(
+            interp10([2.4, 5.6, 6.0]),
+            np.array([2.4, 5.6, 6.0]),
+        )
+
+    def _bounds_check(self, kind='linear'):
         """ Test that our handling of out-of-bounds input is correct.
         """
 
         extrap10 = interp1d(self.x10, self.y10, fill_value=self.fill_value,
-            bounds_error=False)
+            bounds_error=False, kind=kind)
         assert_array_equal(
             extrap10(11.2),
             np.array([self.fill_value]),
@@ -145,25 +162,28 @@ class TestInterp1D(TestCase):
             np.array([True, False, False, False, True]),
         )
 
-        raises_bounds_error = interp1d(self.x10, self.y10, bounds_error=True)
+        raises_bounds_error = interp1d(self.x10, self.y10, bounds_error=True,
+                                       kind=kind)
         self.assertRaises(ValueError, raises_bounds_error, -1.0)
         self.assertRaises(ValueError, raises_bounds_error, 11.0)
         raises_bounds_error([0.0, 5.0, 9.0])
 
+    def test_bounds(self):
+        for kind in ('linear', 'cubic'):
+            self._bounds_check(kind=kind)
 
-    def test_nd(self):
+    def _nd_check(self, kind='linear'):
         """ Check the behavior when the inputs and outputs are multidimensional.
         """
-
         # Multidimensional input.
-        interp10 = interp1d(self.x10, self.y10)
+        interp10 = interp1d(self.x10, self.y10, kind=kind)
         assert_array_almost_equal(
             interp10(np.array([[3.4, 5.6], [2.4, 7.8]])),
             np.array([[3.4, 5.6], [2.4, 7.8]]),
         )
-
+        
         # Multidimensional outputs.
-        interp210 = interp1d(self.x10, self.y210)
+        interp210 = interp1d(self.x10, self.y210, kind=kind)
         assert_array_almost_equal(
             interp210(1.5),
             np.array([[1.5], [11.5]]),
@@ -174,7 +194,7 @@ class TestInterp1D(TestCase):
                       [11.5, 12.4]]),
         )
 
-        interp102 = interp1d(self.x10, self.y102, axis=0)
+        interp102 = interp1d(self.x10, self.y102, axis=0, kind=kind)
         assert_array_almost_equal(
             interp102(1.5),
             np.array([[3.0, 4.0]]),
@@ -197,6 +217,23 @@ class TestInterp1D(TestCase):
             np.array([[[6.8, 7.8], [11.2, 12.2]],
                       [[4.8, 5.8], [15.6, 16.6]]]),
         )
+        
+        # Check large ndim output
+        a = [4, 5, 6, 7]
+        y = np.arange(np.prod(a)).reshape(*a)
+        for n, s in enumerate(a):
+            x = np.arange(s)
+            z = interp1d(x, y, axis=n, kind=kind)
+            assert_array_almost_equal(z(x), y)
+            
+            x2 = np.arange(2*3*1).reshape((2,3,1)) / 12.
+            b = list(a)
+            b[n:n+1] = [2,3,1]
+            assert_array_almost_equal(z(x2).shape, b)
+
+    def test_nd(self):
+        for kind in ('linear', 'cubic'):
+            self._nd_check(kind=kind)
 
 class TestLagrange(TestCase):
 
