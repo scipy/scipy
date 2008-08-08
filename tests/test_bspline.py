18693aee2c556af8225a229ec0aea0d433828296
chris.burns <chris.burns@localhost>
2008-08-08 21:10:03 +0000

    Add a test for gram function.

--- tests/test_bspline.py
+++ tests/test_bspline.py
@@ -15,7 +15,9 @@ class TestBSpline(TestCase):
         new = b._basisx.shape
         self.assertEqual((old[0], 51), new)
 
-    def test2(self):
+    # FIXME: Have no idea what this test does.  It's here to simply verify the
+    # C extension is working (in a technical sense, not functional).
+    def test_basis(self):
         b = bsp.BSpline(np.linspace(0,1,11))
 	x = np.array([0.4, 0.5])
 	v = b.basis(x, lower=0, upper=13)
@@ -34,6 +36,12 @@ class TestBSpline(TestCase):
                       [ 0.        ,  0.        ]])
 	assert_array_almost_equal(v, t, decimal=6)
 
+    # FIXME: Have no idea what this test does.  It's here to simply verify the
+    # C extension is working (in a technical sense, not functional).
+    def test_gram(self):
+        b = bsp.BSpline(np.linspace(0,1,11))
+        grm = b.gram()
+        assert grm.shape == (4, 13)
 
 
 if __name__ == "__main__":
