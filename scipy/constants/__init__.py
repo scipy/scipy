6820c4008f6373a2efb2238aa68ea94318911095
Ilan Schnell <ilan@enthought.com>
2008-08-18 20:58:28 +0000

    Moved my project into root of branches.

--- scipy/constants/__init__.py
+++
@@ -1,4 +0,0 @@
-
-# Modules contributed by BasSw (wegwerp@gmail.com)
-from codata import *
-from constants import *
--- scipy/ndimage/info.py
+++
@@ -1,9 +0,0 @@
-__doc__ = """
-n-dimensional image package
-===========================
-
-This package contains various functions for multi-dimensional image processing.
-"""
-
-postpone_import = 1
-depends = []
