984086353bd2fb87a400b764ce4656d292184c96
Ilan Schnell <ilan@enthought.com>
2008-08-23 18:44:21 +0000

    Improved documentation of the examples.

---
+++ examples/README.txt
@@ -0,0 +1,21 @@
+
+
+benchmark.py
+    Some speed comparisons between dirrerent ways of vectorizing
+    and calculating a simple polynom.
+
+compdec.py
+    A small example illustrates how PyPy's translator package can
+    be used to write compile decorator.
+
+mandel.py
+    A program which uses fast_vectorize to calulate an image file
+    of the well-known Mandelbort set.
+
+mandel_c.py
+    A program which illustrates how to create a Ufunc by writing
+    C code into weave.
+
+primes.py
+    A Python function for counting primes is first executed as a
+    regular function, as well as after "fast_vectorization".
--- examples/compdec.py
+++ examples/compdec.py
@@ -1,9 +1,10 @@
 """
-I have written a class which allows using the compiled version of a
-Python functions simply by adding a decorator to the function.
-The nice thing about doing things this way is that all the code is pure
-Python code, and switching between the compiled and uncompiled version
-of the function is as simple as possible.  
+This small example illustrates how PyPy's translator package can
+be used to write compile decorator.  This is done by a class which allows
+using the compiled version of a Python functions simply by adding a
+decorator to the function.  The nice thing about doing things this way
+is that all the code is pure Python code, and switching between the
+compiled and uncompiled version of the function is very simple.
 """
 from pypy.translator.interactive import Translation
 
@@ -22,13 +23,14 @@ class compdec:
 
         return self.cfunc(*args)
 
-@compdec
-def is_prime(n):
-    if n < 2:
-        return False
-    for i in xrange(2, n):
-        if n%i == 0:
+if __name__ == '__main__':
+    @compdec
+    def is_prime(n):
+        if n < 2:
             return False
-    return True
+        for i in xrange(2, n):
+            if n%i == 0:
+                return False
+        return True
 
-print sum(is_prime(n) for n in xrange(100000))
+    print sum(is_prime(n) for n in xrange(100000))
