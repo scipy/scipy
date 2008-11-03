23ae9977267f9c38304c633798ed014516310694
David Cournapeau <cournape@gmail.com>
2008-11-03 10:31:08 +0000

    Prefix every internal library with sc_ to avoid name clash with existing libraries.

--- scipy/special/setup.py
+++ scipy/special/setup.py
@@ -16,25 +16,25 @@ def configuration(parent_package='',top_path=None):
         define_macros.append(('_USE_MATH_DEFINES',None))
 
     # C libraries
-    config.add_library('c_misc',sources=[join('c_misc','*.c')])
-    config.add_library('cephes',sources=[join('cephes','*.c')],
+    config.add_library('sc_c_misc',sources=[join('c_misc','*.c')])
+    config.add_library('sc_cephes',sources=[join('cephes','*.c')],
                        include_dirs=[get_python_inc()],
                        macros=define_macros)
 
     # Fortran libraries
-    config.add_library('mach',sources=[join('mach','*.f')],
+    config.add_library('sc_mach',sources=[join('mach','*.f')],
                        config_fc={'noopt':(__file__,1)})
-    config.add_library('toms',sources=[join('amos','*.f')])
-    config.add_library('amos',sources=[join('toms','*.f')])
-    config.add_library('cdf',sources=[join('cdflib','*.f')])
-    config.add_library('specfun',sources=[join('specfun','*.f')])
+    config.add_library('sc_toms',sources=[join('amos','*.f')])
+    config.add_library('sc_amos',sources=[join('toms','*.f')])
+    config.add_library('sc_cdf',sources=[join('cdflib','*.f')])
+    config.add_library('sc_specfun',sources=[join('specfun','*.f')])
 
     # Extension _cephes
     sources = ['_cephesmodule.c', 'amos_wrappers.c', 'specfun_wrappers.c',
                'toms_wrappers.c','cdf_wrappers.c','ufunc_extras.c']
     config.add_extension('_cephes', sources=sources,
-                         libraries=['amos','toms','c_misc','cephes','mach',
-                                    'cdf', 'specfun'],
+                         libraries=['sc_amos','sc_toms','sc_c_misc','sc_cephes','sc_mach',
+                                    'sc_cdf', 'sc_specfun'],
                          depends=["ufunc_extras.h", "cephes.h",
                                   "amos_wrappers.h", "toms_wrappers.h",
                                   "cdf_wrappers.h", "specfun_wrappers.h",
@@ -48,7 +48,7 @@ def configuration(parent_package='',top_path=None):
                          sources=['specfun.pyf'],
                          f2py_options=['--no-wrap-functions'],
                          define_macros=[],
-                         libraries=['specfun'])
+                         libraries=['sc_specfun'])
 
     config.add_data_dir('tests')
 
