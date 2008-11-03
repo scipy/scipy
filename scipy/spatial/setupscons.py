e1fc18a3b342023e4bb05dc2ffc77a5fc8cc9969
David Cournapeau <cournape@gmail.com>
2008-11-03 09:54:38 +0000

    Update setup.py file for numscons build.

--- scipy/spatial/setupscons.py
+++ scipy/spatial/setupscons.py
@@ -4,7 +4,7 @@ from os.path import join
 
 def configuration(parent_package = '', top_path = None):
     from numpy.distutils.misc_util import Configuration, get_numpy_include_dirs
-    config = Configuration('cluster', parent_package, top_path)
+    config = Configuration('spatial', parent_package, top_path)
 
     config.add_data_dir('tests')
 
