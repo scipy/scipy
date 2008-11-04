6f2abaea5195b39651d33922ab0e66367aa334bd
David Cournapeau <cournape@gmail.com>
2008-11-04 16:32:55 +0000

    Add spatial to numscons build.

--- scipy/setupscons.py
+++ scipy/setupscons.py
@@ -21,6 +21,7 @@ def configuration(parent_package='', top_path=None, setup_name='setupscons.py'):
     config.add_subpackage('optimize')
     config.add_subpackage('signal')
     config.add_subpackage('sparse')
+    config.add_subpackage('spatial')
     config.add_subpackage('special')
     config.add_subpackage('stats')
     config.add_subpackage('ndimage')
