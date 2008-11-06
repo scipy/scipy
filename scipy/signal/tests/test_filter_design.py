1ec0db6ec33aa2cd561a276b5396429a807691c6
David Cournapeau <cournape@gmail.com>
2008-11-06 11:54:13 +0000

    Add a simple test for tf2zpk.

---
+++ scipy/signal/tests/test_filter_design.py
@@ -0,0 +1,21 @@
+import numpy as np
+from numpy.testing import TestCase, assert_array_almost_equal
+
+from scipy.signal import tf2zpk
+
+class TestTf2zpk(TestCase):
+    def test_simple(self):
+        z_r = np.array([0.5, -0.5])
+        p_r = np.array([1.j / np.sqrt(2), -1.j / np.sqrt(2)])
+        # Sort the zeros/poles so that we don't fail the test if the order
+        # changes
+        z_r.sort()
+        p_r.sort()
+        b = np.poly(z_r)
+        a = np.poly(p_r)
+
+        z, p, k = tf2zpk(b, a)
+        z.sort()
+        p.sort()
+        assert_array_almost_equal(z, z_r)
+        assert_array_almost_equal(p, p_r)
