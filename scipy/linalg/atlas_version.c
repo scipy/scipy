3a51dab510e6e1c9f1e89696c81ed28e0de0aa9e
David Cournapeau <cournape@gmail.com>
2009-01-19 10:24:57 +0000

    Do not print atlas build info at import; add a version function if printing is necessary.

--- scipy/linalg/atlas_version.c
+++ scipy/linalg/atlas_version.c
@@ -1,24 +1,43 @@
 #ifdef __CPLUSPLUS__
 extern "C" {
 #endif
+
 #include "Python.h"
-static PyMethodDef module_methods[] = { {NULL,NULL} };
-PyMODINIT_FUNC initatlas_version(void) {
-  PyObject *m = NULL;
+
+void ATL_buildinfo(void);
+
+static char doc_version[] = "Print ATLAS build info.";
+
+static  PyObject *version(PyObject* self, PyObject* args)
+{
 #if defined(NO_ATLAS_INFO)
-  printf("NO ATLAS INFO AVAILABLE\n");
+        printf("NO ATLAS INFO AVAILABLE\n");
 #else
-  void ATL_buildinfo(void);
-  ATL_buildinfo();
+        ATL_buildinfo();
 #endif
-  m = Py_InitModule("atlas_version", module_methods);
+
+        Py_INCREF(Py_None);
+        return Py_None;
+}
+
+static PyMethodDef module_methods[] = { 
+	{"version", version, METH_VARARGS, doc_version},
+        {NULL, NULL, 0, NULL}
+};
+
+PyMODINIT_FUNC initatlas_version(void) 
+{
+        PyObject *m = NULL;
+        m = Py_InitModule("atlas_version", module_methods);
 #if defined(ATLAS_INFO)
-  {
-    PyObject *d = PyModule_GetDict(m);
-    PyDict_SetItemString(d,"ATLAS_VERSION",PyString_FromString(ATLAS_INFO));
-  }
+        {
+                PyObject *d = PyModule_GetDict(m);
+                PyDict_SetItemString(d, "ATLAS_VERSION",
+                                     PyString_FromString(ATLAS_INFO));
+        }
 #endif
 }
+
 #ifdef __CPLUSCPLUS__
 }
 #endif
