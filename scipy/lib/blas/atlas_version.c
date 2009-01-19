29ba0bb27aa2c2696f5388a3c2ebc14ec3fc213b
David Cournapeau <cournape@gmail.com>
2009-01-19 10:26:55 +0000

    Revert change 5504 - I applied it to the wrong branch.

--- scipy/lib/blas/atlas_version.c
+++ scipy/lib/blas/atlas_version.c
@@ -1,43 +1,24 @@
 #ifdef __CPLUSPLUS__
 extern "C" {
 #endif
-
 #include "Python.h"
-
-void ATL_buildinfo(void);
-
-static char doc_version[] = "Print ATLAS build info.";
-
-static  PyObject *version(PyObject* self, PyObject* args)
-{
+static PyMethodDef module_methods[] = { {NULL,NULL} };
+PyMODINIT_FUNC initatlas_version(void) {
+  PyObject *m = NULL;
 #if defined(NO_ATLAS_INFO)
-        printf("NO ATLAS INFO AVAILABLE\n");
+  printf("NO ATLAS INFO AVAILABLE\n");
 #else
-        ATL_buildinfo();
+  void ATL_buildinfo(void);
+  ATL_buildinfo();
 #endif
-
-        Py_INCREF(Py_None);
-        return Py_None;
-}
-
-static PyMethodDef module_methods[] = { 
-	{"version", version, METH_VARARGS, doc_version},
-        {NULL, NULL, 0, NULL}
-};
-
-PyMODINIT_FUNC initatlas_version(void) 
-{
-        PyObject *m = NULL;
-        m = Py_InitModule("atlas_version", module_methods);
+  m = Py_InitModule("atlas_version", module_methods);
 #if defined(ATLAS_INFO)
-        {
-                PyObject *d = PyModule_GetDict(m);
-                PyDict_SetItemString(d, "ATLAS_VERSION",
-                                     PyString_FromString(ATLAS_INFO));
-        }
+  {
+    PyObject *d = PyModule_GetDict(m);
+    PyDict_SetItemString(d,"ATLAS_VERSION",PyString_FromString(ATLAS_INFO));
+  }
 #endif
 }
-
 #ifdef __CPLUSCPLUS__
 }
 #endif
