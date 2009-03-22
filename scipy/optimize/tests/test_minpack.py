1bca1f4b2d3757f3147429cb0d8601f3d2c98909
Pauli Virtanen <pav@iki.fi>
2009-03-22 20:22:57 +0000

    optimize: add new tests for fsolve (contributed by Yosef Meller)
    Also move leastsq tests to test_minpack.py.

---
+++ scipy/optimize/tests/test_minpack.py
@@ -0,0 +1,129 @@
+"""
+Unit tests for optimization routines from minpack.py.
+"""
+# Authors:
+#
+#   Andrew Straw (April 2008)
+#   Yosef Meller (March 2009)
+#
+from numpy.testing import *
+import numpy as np
+from numpy import array, float64
+
+from scipy import optimize
+from scipy.optimize.minpack import fsolve, leastsq
+
+class TestFSolve(TestCase):
+    def pressure_network(self, flow_rates, Qtot, k):
+        """Evaluate non-linear equation system representing
+        the pressures and flows in a system of n parallel pipes::
+
+            f_i = P_i - P_0, for i = 1..n
+            f_0 = sum(Q_i) - Qtot
+
+        Where Q_i is the flow rate in pipe i and P_i the pressure in that pipe.
+        Pressure is modeled as a P=kQ**2 where k is a valve coefficient and
+        Q is the flow rate.
+        
+        Parameters
+        ----------
+        flow_rates: float
+            a 1D array of n flow rates [kg/s]
+        k : float
+            a 1D array of n valve coefficients [1/kg m]
+        Qtot : float
+            a scalar, the total input flow rate [kg/s].
+            
+        Returns
+        -------
+        F : float
+            a 1D array, F[i] == f_i.
+        """
+        P = k*flow_rates**2
+        F = np.hstack((P[1:] - P[0], flow_rates.sum() - Qtot))
+        return F
+    
+    def pressure_network_jacobian(self, flow_rates, Qtot, k):
+        """Return the jacobian of the equation system F(flow_rates)
+        computed by `pressure_network` with respect to
+        *flow_rates*. See `pressure_network` for the detailed
+        description of parrameters.
+
+        Returns
+        -------
+        jac : float
+            *n* by *n* matrix ``df_i/dQ_i`` where ``n = len(flow_rates)``
+            and *f_i* and *Q_i* are described in the doc for `pressure_network`
+        """
+        n = len(flow_rates)
+        pdiff = np.diag(flow_rates[1:]*2*k[1:] - 2*flow_rates[0]*k[0])
+        
+        jac = np.empty((n,n))
+        jac[:n-1,:n-1] = pdiff
+        jac[:n-1,n-1]  = 0
+        jac[n-1,:]     = np.ones(n)
+        
+        return jac
+        
+    def test_pressure_network_no_gradient(self):
+        """fsolve without gradient, equal pipes -> equal flows"""
+        k = np.ones(4)*0.5
+        Qtot = 4
+        initial_guess = array([2., 0., 2., 0.])
+        final_flows = optimize.fsolve(
+            self.pressure_network, initial_guess, args=(Qtot, k))
+        assert_array_almost_equal(final_flows, np.ones(4))
+
+    def test_pressure_network_with_gradient(self):
+        """fsolve with gradient, equal pipes -> equal flows"""
+        k = np.ones(4)*0.5
+        Qtot = 4
+        initial_guess = array([2., 0., 2., 0.])
+        final_flows = optimize.fsolve(
+            self.pressure_network, initial_guess, args=(Qtot, k), 
+            fprime=self.pressure_network_jacobian)
+        assert_array_almost_equal(final_flows, np.ones(4))
+
+class TestLeastSq(TestCase):
+    def setUp(self):
+        x = np.linspace(0, 10, 40)
+        a,b,c = 3.1, 42, -304.2
+        self.x = x
+        self.abc = a,b,c
+        y_true = a*x**2 + b*x + c
+        np.random.seed(0)
+        self.y_meas = y_true + 0.01*np.random.standard_normal(y_true.shape)
+
+    def residuals(self, p, y, x):
+        a,b,c = p
+        err = y-(a*x**2 + b*x + c)
+        return err
+
+    def test_basic(self):
+        p0 = array([0,0,0])
+        params_fit, ier = leastsq(self.residuals, p0,
+                                  args=(self.y_meas, self.x))
+        assert_(ier in (1,2,3,4), 'solution not found (ier=%d)'%ier)
+        # low precision due to random
+        assert_array_almost_equal(params_fit, self.abc, decimal=2)
+
+    def test_full_output(self):
+        p0 = array([0,0,0])
+        full_output = leastsq(self.residuals, p0,
+                              args=(self.y_meas, self.x),
+                              full_output=True)
+        params_fit, cov_x, infodict, mesg, ier = full_output
+        assert_(ier in (1,2,3,4), 'solution not found: %s'%mesg)
+
+    def test_input_untouched(self):
+        p0 = array([0,0,0],dtype=float64)
+        p0_copy = array(p0, copy=True)
+        full_output = leastsq(self.residuals, p0,
+                              args=(self.y_meas, self.x),
+                              full_output=True)
+        params_fit, cov_x, infodict, mesg, ier = full_output
+        assert_(ier in (1,2,3,4), 'solution not found: %s'%mesg)
+        assert_array_equal(p0, p0_copy)
+
+if __name__ == "__main__":
+    run_module_suite()
--- scipy/optimize/tests/test_optimize.py
+++ scipy/optimize/tests/test_optimize.py
@@ -1,11 +1,12 @@
-""" Unit tests for optimization routines
+"""
+Unit tests for optimization routines from optimize.py and tnc.py
+
 Authors:
- Ed Schofield, Nov 2005
- Andrew Straw, April 2008
+   Ed Schofield, Nov 2005
+   Andrew Straw, April 2008
 
 To run it in its simplest form::
   nosetests test_optimize.py
-
 
 """
 
@@ -261,45 +262,6 @@ class TestTnc(TestCase):
         ef = abs(fg(xopt)[0] - fg(x)[0])
         if ef > 1e-8:
             raise err
-
-class TestLeastSq(TestCase):
-    def setUp(self):
-        x = np.linspace(0, 10, 40)
-        a,b,c = 3.1, 42, -304.2
-        self.x = x
-        self.abc = a,b,c
-        y_true = a*x**2 + b*x + c
-        self.y_meas = y_true + 0.01*np.random.standard_normal( y_true.shape )
-
-    def residuals(self, p, y, x):
-        a,b,c = p
-        err = y-(a*x**2 + b*x + c)
-        return err
-
-    def test_basic(self):
-        p0 = array([0,0,0])
-        params_fit, ier = leastsq(self.residuals, p0,
-                                  args=(self.y_meas, self.x))
-        assert ier in (1,2,3,4), 'solution not found (ier=%d)'%ier
-        assert_array_almost_equal( params_fit, self.abc, decimal=2) # low precision due to random
-
-    def test_full_output(self):
-        p0 = array([0,0,0])
-        full_output = leastsq(self.residuals, p0,
-                              args=(self.y_meas, self.x),
-                              full_output=True)
-        params_fit, cov_x, infodict, mesg, ier = full_output
-        assert ier in (1,2,3,4), 'solution not found: %s'%mesg
-
-    def test_input_untouched(self):
-        p0 = array([0,0,0],dtype=float64)
-        p0_copy = array(p0, copy=True)
-        full_output = leastsq(self.residuals, p0,
-                              args=(self.y_meas, self.x),
-                              full_output=True)
-        params_fit, cov_x, infodict, mesg, ier = full_output
-        assert ier in (1,2,3,4), 'solution not found: %s'%mesg
-        assert_array_equal(p0, p0_copy)
 
 if __name__ == "__main__":
     run_module_suite()
