e4e4639d1d9bbfc418f380bb8f5a499d2cd7e7f8
Matthew Brett <matthew.brett@gmail.com>
2009-05-15 04:45:21 +0000

    Cleanup up info, removed deprecated functions

--- scipy/io/info.py
+++ scipy/io/info.py
@@ -2,30 +2,15 @@
 Data input and output
 =====================
 
-  Classes
-
-     npfile -- a class for reading and writing numpy arrays from / to binary files
-     Cache
-     DataSource
-     Repository
-
   Functions
 
-     read_array -- reading ascii streams into NumPy arrays
-     write_array -- write an array to an ascii stream
-     loadmat -- read a MATLAB style mat file (version 4 and 5)
-     savemat -- write a MATLAB (version <= 4) style mat file
-
-     fread -- low-level reading
-     fwrite -- low-level writing
-     bswap -- in-place byte-swapping
-     packbits -- Pack a binary array of 1's and 0's into an array of bytes
-     unpackbits -- Unpack an array packed by packbits.
-
-     save --- simple storing of Python dictionary into module
+     loadmat -- read a MATLAB style mat file (version 4 through 7.1)
+     savemat -- write a MATLAB (version through 7.1) style mat file
+     netcdf_file -- read NetCDF files (version of ``pupynere`` package)
+        This version cannot write NetCDF files.
+     save_as_module -- simple storing of Python dictionary into module
                that can then be imported and the data accessed as
                attributes of the module.
-
      mminfo   -- query matrix info from Matrix Market formatted file
      mmread   -- read matrix from Matrix Market formatted file
      mmwrite  -- write matrix to Matrix Market formatted file
