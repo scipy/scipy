efc44c084a8a10841545df0258bc8e4d552bb815
David Cournapeau <cournape@gmail.com>
2009-06-21 09:56:05 +0000

    Release 0.7.1.

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ MAJOR               = 0
 MINOR               = 7
 MICRO               = 1
 ISRELEASED          = True
-VERSION             = '%d.%d.%drc3' % (MAJOR, MINOR, MICRO)
+VERSION             = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
 # BEFORE importing distutils, remove MANIFEST. distutils doesn't properly
 # update it when the contents of directories change.
