2be68ef15d985467a1ee5ecf0e37835847105fec
David Cournapeau <cournape@gmail.com>
2009-08-22 19:10:47 +0000

    Remove stsci from scons build as well and make a note in the release notes.

--- scipy/setupscons.py
+++ scipy/setupscons.py
@@ -25,7 +25,6 @@ def configuration(parent_package='', top_path=None, setup_name='setupscons.py'):
     config.add_subpackage('special')
     config.add_subpackage('stats')
     config.add_subpackage('ndimage')
-    config.add_subpackage('stsci')
     config.add_subpackage('weave')
     config.make_svn_version_py()  # installs __svn_version__.py
 
