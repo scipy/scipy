eb366e01271798c5290793eafb84c90ec8331b89
David Cournapeau <cournape@gmail.com>
2009-09-04 03:53:08 +0000

    Forgot to include numpy header before using NPY_INLINE.

--- scipy/cluster/src/hierarchy.c
+++ scipy/cluster/src/hierarchy.c
@@ -33,6 +33,8 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+#include <Python.h>
+#include <numpy/ndarrayobject.h>
 
 #include "common.h"
 
