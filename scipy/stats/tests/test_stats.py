Fixed tests that fail; 0.42264973081037421 is actually the closer number to 0.422649730810374235490851... (longer expansion computed with Sage\'s numerical integrator and WolframAlpha.com).  Hooray for 5e-17 improvement in accuracy! ;)

scipy/stats/tests/test_stats.py
@@ -1141,7 +1141,7 @@ def test_ttest_rel():
     #test zero division problem
     t,p = stats.ttest_rel([0,0,0],[1,1,1])
     assert_equal((np.abs(t),p), (np.inf, 0))
-    assert_equal(stats.ttest_rel([0,0,0], [0,0,0]), (1.0, 0.42264973081037427))
+    assert_equal(stats.ttest_rel([0,0,0], [0,0,0]), (1.0, 0.42264973081037421))
 
     #check that nan in input array result in nan output
     anan = np.array([[1,np.nan],[-1,1]])
@@ -1222,7 +1222,7 @@ def test_ttest_1samp_new():
     #test zero division problem
     t,p = stats.ttest_1samp([0,0,0], 1)
     assert_equal((np.abs(t),p), (np.inf, 0))
-    assert_equal(stats.ttest_1samp([0,0,0], 0), (1.0, 0.42264973081037427))
+    assert_equal(stats.ttest_1samp([0,0,0], 0), (1.0, 0.42264973081037421))
 
     #check that nan in input array result in nan output
     anan = np.array([[1,np.nan],[-1,1]])