5f685de479f541a7ccaa21d658f14fa1ea3d8f0f
aarchiba <peridot.faceted@gmail.com>
2009-10-24 21:40:28 +0000

    Fix von Mises distribution outside -pi..pi (Ticket #1032)

--- scipy/stats/vonmises_cython.pyx
+++ scipy/stats/vonmises_cython.pyx
@@ -46,7 +46,8 @@ def von_mises_cdf(k,x):
     k = np.atleast_1d(k)
     x = np.atleast_1d(x)
     ix = np.round(x/(2*np.pi))
-    x = x-ix
+    print x, ix, x-ix*2*np.pi
+    x = x-ix*2*np.pi
 
     # These values should give 12 decimal digits
     CK=50
@@ -71,6 +72,6 @@ def von_mises_cdf(k,x):
     result[~c_small_k] = von_mises_cdf_normalapprox(bk[~c_small_k],bx[~c_small_k],C1)
 
     if not zerodim:
-        return result+(2*np.pi)*ix
+        return result+ix
     else:
-        return (result+(2*np.pi)*ix)[0]
+        return (result+ix)[0]
