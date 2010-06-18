72ac954658b94b0103cb139ab8ea00bc214fc819
rgommers <ralf.gommers@googlemail.com>
2010-06-18 13:10:38 +0000

    Remove deprecated objects in scipy.io on Python level.

--- scipy/io/data_store.py
+++ scipy/io/data_store.py
@@ -15,37 +15,10 @@
     1
 """
 
-__all__ = ['save_as_module',
-           # The rest of these are all deprecated
-           'save', 'create_module',
-           'create_shelf', 'load']
+__all__ = ['save_as_module']
 
 import dumb_shelve
 import os
-
-from numpy import deprecate
-
-def _load(module):
-    """ Load data into module from a shelf with
-        the same name as the module.
-    """
-    dir,filename = os.path.split(module.__file__)
-    filebase = filename.split('.')[0]
-    fn = os.path.join(dir, filebase)
-    f = dumb_shelve.open(fn, "r")
-    #exec( 'import ' + module.__name__)
-    for i in f.keys():
-        exec( 'import ' + module.__name__+ ';' +
-              module.__name__+'.'+i + '=' + 'f["' + i + '"]')
-#       print i, 'loaded...'
-#   print 'done'
-
-load = deprecate(_load, message="""
-This is an internal function used with scipy.io.save_as_module
-
-If you are saving arrays into a module, you should think about using
-HDF5 or .npz files instead.
-""")
 
 
 def _create_module(file_name):
@@ -59,12 +32,6 @@ def _create_module(file_name):
         f.write('data_store._load(%s)' % module_name)
         f.close()
 
-create_module = deprecate(_create_module, message="""
-This is an internal function used with scipy.io.save_as_module
-
-If you are saving arrays into a module, you should think about
-using HDF5 or .npz files instead.
-""")
 
 def _create_shelf(file_name,data):
     """Use this to write the data to a new file
@@ -76,13 +43,6 @@ def _create_shelf(file_name,data):
         f[i] = data[i]
 #   print 'done'
     f.close()
-
-create_shelf = deprecate(_create_shelf, message="""
-This is an internal function used with scipy.io.save_as_module
-
-If you are saving arrays into a module, you should think about using
-HDF5 or .npz files instead.
-""")
 
 
 def save_as_module(file_name=None,data=None):
@@ -100,4 +60,3 @@ def save_as_module(file_name=None,data=None):
     _create_module(file_name)
     _create_shelf(file_name,data)
 
-save = deprecate(save_as_module, old_name='save', new_name='save_as_module')
