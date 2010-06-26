b7971d68256b23646de1ef648181da5ceacd67f8
Stefan van der Walt <stefan@sun.ac.za>
2010-06-26 18:53:07 +0000

    ENH: Add very basic tests for codata and constants.

--- scipy/constants/tests/test_codata.py
+++ scipy/constants/tests/test_codata.py
@@ -1,6 +1,9 @@
 
 import warnings
 
+import codata
+import constants
+
 from scipy.constants import find
 from numpy.testing import assert_equal, run_module_suite
 
@@ -25,6 +28,16 @@ def test_find():
                                 'natural unit of momentum in MeV/c',
                                 'natural unit of length',
                                 'natural unit of time']))
+
+
+def test_basic_table_parse():
+    c = 'speed of light in vacuum'
+    assert_equal(codata.value(c), constants.c)
+    assert_equal(codata.value(c), constants.speed_of_light)
+
+def test_basic_lookup():
+    assert_equal('%d %s' % (codata.c, codata.unit('speed of light in vacuum')),
+                 '299792458 m s^-1')
 
 if __name__ == "__main__":
     run_module_suite()
