BUG: Ticket #1214: optimize.newton could return an incorrect value if the function coefficients and initial guess were integers.

scipy/optimize/tests/test_regression.py
@@ -1,14 +1,24 @@
 """Regression tests for optimize.
 
 """
-from numpy.testing import *
-import numpy as np
 
+from numpy.testing import TestCase, run_module_suite, assert_almost_equal
+import scipy.optimize
 
 class TestRegression(TestCase):
+    
     def test_newton_x0_is_0(self):
         """Ticket #1074"""
-        import scipy.optimize
+
         tgt = 1
         res = scipy.optimize.newton(lambda x: x - 1, 0)
         assert_almost_equal(res, tgt)
+
+    def test_newton_integers(self):
+        """Ticket #1214"""
+        root = scipy.optimize.newton(lambda x: x**2 - 1, x0=2,
+                                    fprime=lambda x: 2*x)
+        assert_almost_equal(root, 1.0)
+    
+if __name__ == "__main__":
+    run_module_suite()