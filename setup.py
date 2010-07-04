e96c9dfb2d0b8ba6833b7844961ae8d5536a7a2d
rgommers <ralf.gommers@googlemail.com>
2010-07-04 08:49:18 +0000

    REL: Change version number for the 0.8.0 RC1 release.

--- setup.py
+++ setup.py
@@ -43,8 +43,8 @@ Operating System :: MacOS
 MAJOR               = 0
 MINOR               = 8
 MICRO               = 0
-ISRELEASED          = False
-VERSION             = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
+ISRELEASED          = True
+VERSION             = '%d.%d.%drc1' % (MAJOR, MINOR, MICRO)
 
 # BEFORE importing distutils, remove MANIFEST. distutils doesn't properly
 # update it when the contents of directories change.
