ee7bd715eccba3c1a2dc7932c36a1fd6636a8c7d
Pauli Virtanen <pav@iki.fi>
2010-09-12 00:51:49 +0000

    3K: Fixing up sparse.linalg.dsolve again

--- scipy/sparse/linalg/dsolve/py3k.h
+++ scipy/sparse/linalg/dsolve/py3k.h
@@ -6,6 +6,7 @@
 #if PY_VERSION_HEX >= 0x03000000
     #define PyUstring_FromString PyUnicode_FromString
     #define PyInt_FromLong PyLong_FromLong
+    #define PyInt_Check PyLong_Check
 #else
     #define PyUstring_FromString PyString_FromString
 #endif
