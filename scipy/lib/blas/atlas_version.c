49e716afbacacfbf017334077539bc29d4c4a168
Pauli Virtanen <pav@iki.fi>
2010-09-14 23:08:08 +0000

    BUG: lib: make atlas_version.c's Py3 compatible (fixes #1280)

--- scipy/lib/blas/atlas_version.c
+++ scipy/lib/blas/atlas_version.c
@@ -1,4 +1,5 @@
 #include "Python.h"
+#include "numpy/npy_3kcompat.h"
 
 static PyObject* version(PyObject* self, PyObject* dummy)
 {
@@ -20,14 +21,41 @@ static PyMethodDef module_methods[] = {
     {NULL, NULL, 0, NULL}
 };
 
+#if PY_VERSION_HEX >= 0x03000000
+
+static struct PyModuleDef moduledef = {
+    PyModuleDef_HEAD_INIT,
+    "atlas_version",
+    NULL,
+    -1,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL
+};
+
+PyObject *PyInit_atlas_version(void)
+{
+#define RETVAL m
+    PyObject *m;
+    m = PyModule_Create(&moduledef);
+#else
+#define RETVAL
 PyMODINIT_FUNC initatlas_version(void)
 {
     PyObject *m = NULL;
     m = Py_InitModule("atlas_version", module_methods);
+#endif
+    if (m == NULL) {
+        return RETVAL;
+    }
 #if defined(ATLAS_INFO)
     {
         PyObject *d = PyModule_GetDict(m);
-        PyDict_SetItemString(d,"ATLAS_VERSION",PyString_FromString(ATLAS_INFO));
+        PyDict_SetItemString(d,"ATLAS_VERSION",
+                             PyUString_FromString(ATLAS_INFO));
     }
 #endif
+    return RETVAL;
 }
