47aeda942bcf40fa80107433db33f21717a78e99
warren.weckesser <warren.weckesser@localhost>
2010-11-13 20:20:24 +0000

    TST: signal: simplify a test--in the old version, the warning was actually triggered in the call to bessel(), so the line containing tf2zpk in the test was never reached.

--- scipy/signal/tests/test_filter_design.py
+++ scipy/signal/tests/test_filter_design.py
@@ -1,12 +1,13 @@
 import warnings
 
 import numpy as np
-from numpy.testing import TestCase, assert_array_almost_equal
+from numpy.testing import TestCase, assert_array_almost_equal, assert_raises
 
 from scipy.signal import tf2zpk, bessel, BadCoefficients
 
 
 class TestTf2zpk(TestCase):
+
     def test_simple(self):
         z_r = np.array([0.5, -0.5])
         p_r = np.array([1.j / np.sqrt(2), -1.j / np.sqrt(2)])
@@ -28,12 +29,6 @@ class TestTf2zpk(TestCase):
         filter coefficients."""
         warnings.simplefilter("error", BadCoefficients)
         try:
-            try:
-                b, a = bessel(20, 0.1)
-                z, p, k = tf2zpk(b, a)
-                raise AssertionError("tf2zpk did not warn about bad "\
-                                     "coefficients")
-            except BadCoefficients:
-                pass
+            assert_raises(BadCoefficients, tf2zpk, [1e-15], [1.0, 1.0])
         finally:
             warnings.simplefilter("always", BadCoefficients)
