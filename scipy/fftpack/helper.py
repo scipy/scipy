c2425eef60391bd65c4a6eff9b7e4bee95eebfdc
warren.weckesser <warren.weckesser@localhost>
2011-01-29 19:02:01 +0000

    ENH: fftpack: Don't use plain assert for argument validation.

--- scipy/fftpack/helper.py
+++ scipy/fftpack/helper.py
@@ -3,7 +3,7 @@ __all__ = ['fftshift', 'ifftshift', 'fftfreq', 'rfftfreq']
 from numpy import array
 from numpy.fft.helper import fftshift, ifftshift, fftfreq
 
-def rfftfreq(n,d=1.0):
+def rfftfreq(n, d=1.0):
     """ rfftfreq(n, d=1.0) -> f
 
     DFT sample frequencies (for usage with rfft,irfft).
@@ -15,5 +15,6 @@ def rfftfreq(n,d=1.0):
       f = [0,1,1,2,2,...,n/2-1,n/2-1,n/2]/(d*n)   if n is even
       f = [0,1,1,2,2,...,n/2-1,n/2-1,n/2,n/2]/(d*n)   if n is odd
     """
-    assert isinstance(n,int)
+    if not isinstance(n, int) or n < 0:
+        raise ValueError("n = %s is not valid.  n must be a nonnegative integer." % n)
     return (array(range(1,n+1),dtype=int)//2)/float(n*d)
