5230bc8e7c847ff11c379198393658cf884d1786
rgommers <ralf.gommers@googlemail.com>
2011-02-19 08:57:31 +0000

    TST: explicit file close to avoid ResourceWarnings under Python 3.2.
    Thanks to Christoph Gohlke.

--- scipy/spatial/tests/test_distance.py
+++ scipy/spatial/tests/test_distance.py
@@ -88,7 +88,9 @@ def load_testing_files():
     for fn in _filenames:
         name = fn.replace(".txt", "").replace("-ml", "")
         fqfn = os.path.join(os.path.dirname(__file__), fn)
-        eo[name] = np.loadtxt(open(fqfn))
+        fp = open(fqfn)
+        eo[name] = np.loadtxt(fp)
+        fp.close()
         #print "%s: %s   %s" % (name, str(eo[name].shape), str(eo[name].dtype))
     eo['pdist-boolean-inp'] = np.bool_(eo['pdist-boolean-inp'])
 
