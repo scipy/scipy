e56d51cbc3433bd48ba077d4e7ad39e7c1513cbc
Skipper Seabold <jsseabold@gmail.com>
2011-04-08 11:14:19 -0400

    Changed print warning to warnings.warn in quadpack.py

--- scipy/integrate/quadpack.py
+++ scipy/integrate/quadpack.py
@@ -282,7 +282,8 @@ def quad(func, a, b, args=(), full_output=0, epsabs=1.49e-8, epsrel=1.49e-8,
             else:
                 return retval[:-1] + (msg,)
         else:
-            print "Warning: " + msg
+            from warnings import warn
+            warn(msg, Warning)
             return retval[:-1]
     else:
         raise ValueError(msg)
