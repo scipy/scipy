6c114327e477b9eb6f040ee121d99638b08dd3bd
Thouis (Ray) Jones <thouis@gmail.com>
2011-06-02 21:26:06 +0200

    fix maximum_position for float labels

--- scipy/ndimage/tests/test_measurements.py
+++ scipy/ndimage/tests/test_measurements.py
@@ -796,6 +796,18 @@ def test_maximum_position06():
         assert_equal(output[0], (0, 0))
         assert_equal(output[1], (1, 1))
 
+def test_maximum_position07():
+    "maximum position 7 - float labels"
+    labels = np.array([1.0, 2.5, 0.0, 4.5])
+    for type in types:
+        input = np.array([[5, 4, 2, 5],
+                          [3, 7, 8, 2],
+                          [1, 5, 1, 1]], type)
+        output = ndimage.maximum_position(input, labels,
+                                          [1.0, 4.5])
+        assert_equal(output[0], (0, 0))
+        assert_equal(output[1], (0, 3))
+
 def test_extrema01():
     "extrema 1"
     labels = np.array([1, 0], bool)
