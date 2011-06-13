eff9aa2d6ad6a6c8de4f9a754c8df3fc9a58255a
Warren Weckesser <warren.weckesser@enthought.com>
2011-06-13 00:59:28 -0500

    ENH: stats: add a few functions for working with contingency tables.

---
+++ scipy/stats/_conting.py
@@ -0,0 +1,219 @@
+"""Some functions for working with contingency tables (i.e. cross tabulations).
+"""
+
+# Author: Warren Weckesser, Enthought, Inc.
+
+import numpy as np
+from scipy import special
+from numpy.testing.decorators import setastest
+
+
+__all__ = ['margins', 'expected_freq', 'conting_test_ind']
+
+
+def margins(a):
+    """Return a list of the marginal sums of the array `a`.
+
+    Parameters
+    ----------
+    a : ndarray
+        The array for which to compute the marginal sums.
+
+    Return Value
+    ------------
+    margsums : list of ndarrays
+        A list of length `a.ndim`.  `margsums[k]` is the result
+        of summing `a` over all axes except `k`; it has the same
+        number of dimensions as `a`, but the length of each axis
+        except axis `k` will be 1.
+
+    Examples
+    --------
+    >>> a = np.arange(12).reshape(2, 6)
+    >>> a
+    array([[ 0,  1,  2,  3,  4,  5],
+           [ 6,  7,  8,  9, 10, 11]])
+    >>> m0, m1 = margins(a)
+    >>> m0
+    array([[15],
+           [51]])
+    >>> m1
+    array([[ 6,  8, 10, 12, 14, 16]])
+
+    >>> b = np.arange(24).reshape(2,3,4)
+    >>> m0, m1, m2 = margins(b)
+    >>> m0
+    array([[[ 66]],
+           [[210]]])
+    >>> m1
+    array([[[ 60],
+            [ 92],
+            [124]]])
+    >>> m2
+    array([[[60, 66, 72, 78]]])
+    """
+    margsums = []
+    ranged = range(a.ndim)
+    for k in ranged:
+        marg = np.apply_over_axes(np.sum, a, [j for j in ranged if j != k])
+        margsums.append(marg)
+    return margsums
+
+
+def expected_freq(observed):
+    """Compute the expected frequencies from a contingency table.
+
+    Given an n-dimensional contingency table of observed frequencies,
+    compute the expected frequencies for the table based on the marginal
+    sums under the assumption that the groups associated with each
+    dimension are independent.
+
+    Parameters
+    ----------
+    observed : array_like
+        The table of observed frequencies.  (While this function can handle
+        a 1-D array, that case is trivial.  Generally `observed` is at
+        least 2-D.)
+
+    Returns
+    -------
+    expected : ndarray of type numpy.float64, same shape as `observed`.
+        The expected frequencies, based on the marginal sums of the table.
+
+    Examples
+    --------
+    >>> observed = np.array([[10, 10, 20],[20, 20, 20]])
+    >>> expected_freq(observed)
+    array([[ 12.,  12.,  16.],
+           [ 18.,  18.,  24.]])
+    """
+    # Typically `observed` is an integer array. If `observed` has a large
+    # number of dimensions or holds large values, some of the following
+    # computations may overflow, so we first switch to floating point.
+    observed = np.asarray(observed, dtype=np.float64)
+
+    # Create a list of the marginal sums.
+    margsums = margins(observed)
+
+    # Create the array of expected frequencies.  The shapes of the
+    # marginal sums returned by apply_over_axes() are just what we
+    # need for broadcasting in the following product.
+    d = observed.ndim
+    expected = reduce(np.multiply, margsums) / observed.sum() ** (d - 1)
+    return expected
+
+
+@setastest(False)
+def conting_test_ind(observed, correction=False):
+    """chi-square hypothesis test for independence of observations.
+
+    This function computes the chi-square statistic and p-value for the
+    chi-square test of independence of the observed frequencies in the
+    contingency table `observed`.  The expected frequencies are computed
+    based on the marginal sums under the assumption of independence.
+
+    Parameters
+    ----------
+    observed : array_like
+        The contingency table. The table contains the observed frequencies
+        in each category.  In the two-dimensional case, the table is often
+        described as the "R x C table".
+    correction : bool, optional
+        If True, *and* the degrees of freedom is 1, apply Yates' correction
+        for continuity.
+
+    Returns
+    -------
+    chi2 : float
+        The chi-square test statistic.  Without the Yates' correction, this
+        is the sum of the squares of the observed values minus the expected
+        values, divided by the expected values.  With Yates' correction,
+        0.5 is subtracted from the squared differences before dividing by
+        the expected values.
+    p : float
+        The p-value of the test
+    dof : int
+        Degrees of freedom
+    expected : ndarray, same shape as `observed`
+        The expected frequencies, based on the marginal sums of the table.
+
+    Notes
+    -----
+    An often quoted guideline for the validity of this calculation is that
+    the test should be used only if the observed and expected frequency in
+    each cell is at least 5.
+
+    This is a test for the independence of different categories of a
+    population. The test is only meaningful when the dimension of
+    `observed` is two or more.  Applying the test to a one-dimensional
+    table will always result in `expected` equal to `observed` and a
+    chi-square statistic equal to 0.
+
+    This function does not handle masked array, because the calculation
+    does not make sense with missing values.
+
+    See Also
+    --------
+    scipy.stats.expected_freq
+    scipy.stats.fisher_exact
+    scipy.stats.chisquare
+
+    Examples
+    --------
+    A two-way example (2 x 3):
+
+    >>> obs = np.array([[10, 10, 20], [20, 20, 20]])
+    >>> indtest(obs)
+    (2.7777777777777777,
+     0.24935220877729619,
+     2,
+     array([[ 12.,  12.,  16.],
+            [ 18.,  18.,  24.]]))
+
+    A four-way example (2 x 2 x 2 x 2):
+
+    >>> obs = np.array(
+    ...     [[[[12, 17],
+    ...        [11, 16]],
+    ...       [[11, 12],
+    ...        [15, 16]]],
+    ...      [[[23, 15],
+    ...        [30, 22]],
+    ...       [[14, 17],
+    ...        [15, 16]]]])
+    >>> indtest(obs)
+    (8.7584514426741897,
+     0.64417725029295503,
+     11,
+     array([[[[ 14.15462386,  14.15462386],
+              [ 16.49423111,  16.49423111]],
+             [[ 11.2461395 ,  11.2461395 ],
+              [ 13.10500554,  13.10500554]]],
+            [[[ 19.5591166 ,  19.5591166 ],
+              [ 22.79202844,  22.79202844]],
+             [[ 15.54012004,  15.54012004],
+              [ 18.10873492,  18.10873492]]]]))
+    """
+    observed = np.asarray(observed)
+    if np.any(observed < 0):
+        raise ValueError("All values in `table` must be nonnegative.")
+
+    expected = expected_freq(observed)
+    if np.any(expected == 0):
+        # Include one of the positions where expected is zero in
+        # the exception message.
+        zeropos = list(np.where(expected == 0)[0])
+        raise ValueError("The internally computed table of expected "
+                         "frequencies has a zero element at %s." % zeropos)
+
+    # The degrees of freedom
+    dof = expected.size - sum(expected.shape) + expected.ndim - 1
+
+    if dof == 1 and correction:
+        # Use Yates' correction for continuity.
+        chi2 = ((np.abs(observed - expected) - 0.5) ** 2 / expected).sum()
+    else:
+        # Regular chi-square--no correction.
+        chi2 = ((observed - expected) ** 2 / expected).sum()
+    p = special.chdtrc(dof, chi2)
+    return chi2, p, dof, expected
