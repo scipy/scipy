43f2756b685bdf7dce70a8b83cff420b9c82384c
Pauli Virtanen <pav@iki.fi>
2011-06-29 11:54:31 +0200

    DOC: constants: add missing info.py

--- scipy/constants/__init__.py
+++ scipy/constants/__init__.py
@@ -4,6 +4,13 @@ from info import __doc__
 from codata import *
 from constants import *
 
+_constant_names = [(k.lower(), k, v) for k, v in physical_constants.items()]
+_constant_names = "\n".join(["``%s``%s  %s %s" % (x[1], " "*(66-len(x[1])),
+                                                  x[2][0], x[2][1])
+                             for x in sorted(_constant_names)])
+__doc__ = __doc__ % dict(constant_names=_constant_names)
+del _constant_names
+
 __all__ = filter(lambda s:not s.startswith('_'),dir())
 from numpy.testing import Tester
 test = Tester().test
