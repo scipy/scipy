d08a3fabbe8f4c0330970e3e87e7786aec39599a
Pauli Virtanen <pav@iki.fi>
2011-06-29 22:30:19 +0200

    BUG: constants: make sure iteration variables don't overwrite exported ones

--- scipy/constants/__init__.py
+++ scipy/constants/__init__.py
@@ -4,10 +4,11 @@ from info import __doc__
 from codata import *
 from constants import *
 
-_constant_names = [(k.lower(), k, v) for k, v in physical_constants.items()]
-_constant_names = "\n".join(["``%s``%s  %s %s" % (x[1], " "*(66-len(x[1])),
-                                                  x[2][0], x[2][1])
-                             for x in sorted(_constant_names)])
+_constant_names = [(_k.lower(), _k, _v)
+                   for _k, _v in physical_constants.items()]
+_constant_names = "\n".join(["``%s``%s  %s %s" % (_x[1], " "*(66-len(_x[1])),
+                                                  _x[2][0], _x[2][1])
+                             for _x in sorted(_constant_names)])
 __doc__ = __doc__ % dict(constant_names=_constant_names)
 del _constant_names
 
