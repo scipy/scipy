35bf862388c30edc54f534a96554e0735212d7a6
Pauli Virtanen <pav@iki.fi>
2011-09-11 16:37:31 +0100

    BUG: optimize: use get_blas_funcs from scipy.linalg

--- scipy/optimize/nonlin.py
+++ scipy/optimize/nonlin.py
@@ -117,7 +117,7 @@ from scipy.linalg import norm, solve, inv, qr, svd, lstsq, LinAlgError
 from numpy import asarray, dot, vdot
 import scipy.sparse.linalg
 import scipy.sparse
-import scipy.lib.blas as blas
+from scipy.linalg import get_blas_funcs
 import inspect
 from linesearch import scalar_search_wolfe1, scalar_search_armijo
 
@@ -657,8 +657,8 @@ class LowRankMatrix(object):
 
     @staticmethod
     def _matvec(v, alpha, cs, ds):
-        axpy, scal, dotc = blas.get_blas_funcs(['axpy', 'scal', 'dotc'],
-                                               cs[:1] + [v])
+        axpy, scal, dotc = get_blas_funcs(['axpy', 'scal', 'dotc'],
+                                          cs[:1] + [v])
         w = alpha * v
         for c, d in zip(cs, ds):
             a = dotc(d, v)
@@ -673,7 +673,7 @@ class LowRankMatrix(object):
 
         # (B + C D^H)^-1 = B^-1 - B^-1 C (I + D^H B^-1 C)^-1 D^H B^-1
 
-        axpy, dotc = blas.get_blas_funcs(['axpy', 'dotc'], cs[:1] + [v])
+        axpy, dotc = get_blas_funcs(['axpy', 'dotc'], cs[:1] + [v])
 
         c0 = cs[0]
         A = alpha * np.identity(len(cs), dtype=c0.dtype)
