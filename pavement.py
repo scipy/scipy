35fca4bdceaaba3165226a8a3e5201b87149e0d1
Ralf Gommers <ralf.gommers@googlemail.com>
2011-09-12 18:09:09 +0200

    REL: some minor fixes to the release scripts.

--- pavement.py
+++ pavement.py
@@ -87,7 +87,21 @@ from paver.easy import options, Bunch, task, needs, dry, sh, call_task, cmdopts
 sys.path.insert(0, os.path.dirname(__file__))
 try:
     setup_py = __import__("setup")
-    FULLVERSION = setup_py.FULLVERSION
+    FULLVERSION = setup_py.VERSION
+    # This is duplicated from setup.py
+    if os.path.exists('.git'):
+        GIT_REVISION = setup_py.git_version()
+    elif os.path.exists('scipy/version.py'):
+        # must be a source distribution, use existing version file
+        from numpy.version import git_revision as GIT_REVISION
+    else:
+        GIT_REVISION = "Unknown"
+
+    if not setup_py.ISRELEASED:
+        if GIT_REVISION == "Unknown":
+            FULLVERSION += '.dev'
+        else:
+            FULLVERSION += '.dev-' + GIT_REVISION[:7]
 finally:
     sys.path.pop(0)
 
--- release.sh
+++ release.sh
@@ -1,6 +1,8 @@
 #! /bin/sh
 # script to build tarballs, mac os x and windows installers on mac os x
 
+export PYTHONPATH=''
+
 # Check we're using the correct g++/c++ for the 64-bit 2.7 dmg.
 # We do this because for Python 2.6 we use a symlink on the PATH to select
 # /usr/bin/g++-4.0, while for Python 2.7 we need the default 4.2 version.
