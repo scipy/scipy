0f2e47502177c1654933ef9c15ce34b0088359a0
ckuster <ckuster@carrollu.edu>
2011-10-01 10:58:37 -0500

    Edited scipy/stats/stats.py via GitHub

--- scipy/stats/stats.py
+++ scipy/stats/stats.py
@@ -2148,15 +2148,15 @@ def f_oneway(*args):
     .. [2] Heiman, G.W.  Research Methods in Statistics. 2002.
 
     """
-    na = len(args)            # ANOVA on 'na' groups, each in it's own array
-    tmp = map(np.array,args)
+    args = map(np.asarray,args) # convert to an numpy array
+    na = len(args)              # ANOVA on 'na' groups, each in it's own array
     alldata = np.concatenate(args)
     bign = len(alldata)
     sstot = ss(alldata)-(square_of_sums(alldata)/float(bign))
     ssbn = 0
     for a in args:
-        ssbn = ssbn + square_of_sums(array(a))/float(len(a))
-    ssbn = ssbn - (square_of_sums(alldata)/float(bign))
+        ssbn += square_of_sums(a)/float(len(a))
+    ssbn -= (square_of_sums(alldata)/float(bign))
     sswn = sstot-ssbn
     dfbn = na-1
     dfwn = bign - na
@@ -2165,7 +2165,6 @@ def f_oneway(*args):
     f = msb/msw
     prob = fprob(dfbn,dfwn,f)
     return f, prob
-
 
 
 def pearsonr(x, y):
@@ -3544,30 +3543,30 @@ def kruskal(*args):
     .. [1] http://en.wikipedia.org/wiki/Kruskal-Wallis_one-way_analysis_of_variance
 
     """
-    if len(args) < 2:
+    args = map(np.asarray,args) # convert to a numpy array
+    na = len(args)              # Kruskal-Wallis on 'na' groups, each in it's own array
+    if na < 2:
         raise ValueError("Need at least two groups in stats.kruskal()")
     n = map(len,args)
-    all = []
-    for i in range(len(args)):
-        all.extend(args[i].tolist())
-    ranked = list(rankdata(all))
-    T = tiecorrect(ranked)
-    args = list(args)
-    for i in range(len(args)):
-        args[i] = ranked[0:n[i]]
-        del ranked[0:n[i]]
-    rsums = []
-    for i in range(len(args)):
-        rsums.append(np.sum(args[i],axis=0)**2)
-        rsums[i] = rsums[i] / float(n[i])
-    ssbn = np.sum(rsums,axis=0)
-    totaln = np.sum(n,axis=0)
-    h = 12.0 / (totaln*(totaln+1)) * ssbn - 3*(totaln+1)
-    df = len(args) - 1
+    
+    alldata = np.concatenate(args)
+
+    ranked = rankdata(alldata)  # Rank the data
+    T = tiecorrect(ranked)      # Correct for ties
     if T == 0:
         raise ValueError('All numbers are identical in kruskal')
+
+    j = np.insert(np.cumsum(n),0,0)
+    ssbn = 0
+    for i in range(na):
+        ssbn += square_of_sums(ranked[j[i]:j[i+1]])/float(n[i]) # Compute sum^2/n for each group
+        
+    totaln = np.sum(n)
+    h = 12.0 / (totaln*(totaln+1)) * ssbn - 3*(totaln+1)
+    df = len(args) - 1
     h = h / float(T)
     return h, chisqprob(h,df)
+
 
 
 def friedmanchisquare(*args):
