e741dfc4852544bb724bea1022dbc2eefd2df8f3
Johann Cohen-Tanugi <johann.cohentanugi@gmail.com>
2011-10-25 14:36:45 +0300

    BUG: optimize: add a NaN check in scalar_search_wolfe1 to prevent infinite loop

--- scipy/optimize/linesearch.py
+++ scipy/optimize/linesearch.py
@@ -145,7 +145,7 @@ def scalar_search_wolfe1(phi, derphi, phi0=None, old_phi0=None, derphi0=None,
         stp, phi1, derphi1, task = minpack2.dcsrch(alpha1, phi1, derphi1,
                                                    c1, c2, xtol, task,
                                                    amin, amax, isave, dsave)
-        if task[:2] == asbytes('FG'):
+        if task[:2] == asbytes('FG') and not np.isnan(phi1):
             alpha1 = stp
             phi1 = phi(stp)
             derphi1 = derphi(stp)
