BUG: sparse/arpack: use only double-precision routines if running on OSX
There are currently unresolved crash-bugs with OSX's Veclib in the
single-precision routines. This commit brings in a temporary workaround
to allow us to proceed with the 0.10.0 release.

scipy/sparse/linalg/eigen/arpack/arpack.py
@@ -40,6 +40,9 @@ __docformat__ = "restructuredtext en"
 
 __all__ = ['eigs', 'eigsh', 'svds', 'ArpackError', 'ArpackNoConvergence']
 
+import sys
+import warnings
+
 import _arpack
 import numpy as np
 from scipy.sparse.linalg.interface import aslinearoperator, LinearOperator
@@ -49,6 +52,19 @@ from scipy.linalg import lu_factor, lu_solve
 from scipy.sparse.sputils import isdense
 from scipy.sparse.linalg import gmres, splu
 from scipy.linalg.lapack import get_lapack_funcs
+
+
+def _darwin_cast(typechar):
+    # This check is required, for now, because we have unresolved crashes
+    # occurring in single precision Veclib routines, on 64-bit OSX.
+    # When these crashes are resolved, this restriction can be removed.
+    if sys.platform == 'darwin' or True:
+        if typechar in ('f', 'F'):
+            warnings.warn("Single-precision types in `eigs` and `eighs` "
+                          "are not supported on the OSX platform currently. "
+                          "Double precision routines are used instead.")
+            return {'f': 'd', 'F': 'D'}[typechar]
+    return typechar
 
 
 _type_conv = {'f': 's', 'd': 'd', 'F': 'c', 'D': 'z'}
@@ -308,6 +324,8 @@ class _ArpackParams(object):
 
         if tp not in 'fdFD':
             raise ValueError("matrix type must be 'f', 'd', 'F', or 'D'")
+
+        tp = _darwin_cast(tp)
 
         if v0 is not None:
             # ARPACK overwrites its initial resid,  make a copy
@@ -897,10 +915,10 @@ class SpLuInv(LinearOperator):
         # careful here: splu.solve will throw away imaginary
         # part of x if M is real
         if self.isreal and np.issubdtype(x.dtype, np.complexfloating):
-            return (self.M_lu.solve(np.real(x))
-                    + 1j * self.M_lu.solve(np.imag(x)))
+            return (self.M_lu.solve(np.real(x).astype(self.dtype))
+                    + 1j * self.M_lu.solve(np.imag(x).astype(self.dtype)))
         else:
-            return self.M_lu.solve(x)
+            return self.M_lu.solve(x.astype(self.dtype))
 
 class LuInv(LinearOperator):
     """