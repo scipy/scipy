8971e947da0e8f2902a62ae86caf1cae4a2a0cb7
Stefan van der Walt <stefan@sun.ac.za>
2011-10-31 18:35:17 -0700

    ENH: Fix up LSMR test suite.

--- scipy/sparse/linalg/isolve/tests/test_lsmr.py
+++ scipy/sparse/linalg/isolve/tests/test_lsmr.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 """
 Copyright (C) 2010 David Fong and Michael Saunders
 Distributed under the same license as Scipy
@@ -16,16 +15,16 @@ Systems Optimization Laboratory
 Dept of MS&E, Stanford University.
 
 """
-import unittest
-from math import sqrt
-from numpy import arange, concatenate, identity, zeros, ones
-from numpy import transpose
+from numpy import arange, concatenate, identity, zeros, ones, sqrt, \
+                  transpose, hstack
 from numpy.linalg import norm
+from numpy.testing import run_module_suite, assert_almost_equal
+
 from scipy.sparse import coo_matrix
 from scipy.sparse.linalg.interface import aslinearoperator
 from lsmr import lsmr
 
-class lsmrTestCase(unittest.TestCase):
+class TestLSMR:
     def setUp(self):
         self.n = 10
         self.m = 10
@@ -34,7 +33,7 @@ class lsmrTestCase(unittest.TestCase):
         Afun = aslinearoperator(A)
         b = Afun.matvec(xtrue)
         x = lsmr(A,b)[0]
-        self.assertAlmostEqual(norm(x - xtrue), 0, 6)
+        assert_almost_equal(norm(x - xtrue), 0, 6)
 
     def testIdentityACase1(self):
         A = identity(self.n)
@@ -56,7 +55,7 @@ class lsmrTestCase(unittest.TestCase):
         xtrue = transpose(arange(self.n,0,-1))
         self.assertCompatibleSystem(A,xtrue)
 
-class lsmrReturnsTestCase(unittest.TestCase):
+class TestLSMRReturns:
     def setUp(self):
         self.n = 10
         self.A = lowerBidiagonalMatrix(20,self.n)
@@ -67,16 +66,16 @@ class lsmrReturnsTestCase(unittest.TestCase):
 
     def testNormr(self):
         x, istop, itn, normr, normar, normA, condA, normx = self.returnValues;
-        self.assertAlmostEqual(normr, norm(self.b - self.Afun.matvec(x)))
+        assert_almost_equal(normr, norm(self.b - self.Afun.matvec(x)))
 
     def testNormar(self):
         x, istop, itn, normr, normar, normA, condA, normx = self.returnValues;
-        self.assertAlmostEqual(normar, \
+        assert_almost_equal(normar, \
                 norm(self.Afun.rmatvec(self.b - self.Afun.matvec(x))))
 
     def testNormx(self):
         x, istop, itn, normr, normar, normA, condA, normx = self.returnValues;
-        self.assertAlmostEqual(normx, norm(x))
+        assert_almost_equal(normx, norm(x))
 
 def lowerBidiagonalMatrix(m, n):
   # This is a simple example for testing LSMR.
@@ -91,27 +90,27 @@ def lowerBidiagonalMatrix(m, n):
   #
   # 04 Jun 2010: First version for distribution with lsmr.py
   if m <= n:
-    row = concatenate((arange(m, dtype='intc'), \
-        arange(1, m, dtype='intc')), axis = 1)
-    col = concatenate((arange(m, dtype='intc'), \
-        arange(m-1, dtype='intc')), axis = 1)
-    data = concatenate((arange(1, m+1, dtype='double'), \
-            arange(1,m, dtype='double')), axis = 1)
-    return coo_matrix((data,(row, col)), shape=(m,n))
+    row = hstack((arange(m, dtype=int), \
+                  arange(1, m, dtype=int)))
+    col = hstack((arange(m, dtype=int), \
+                  arange(m-1, dtype=int)))
+    data = hstack((arange(1, m+1, dtype=float), \
+                   arange(1,m, dtype=float)))
+    return coo_matrix((data, (row, col)), shape=(m,n))
   else:
-    row = concatenate((arange(n, dtype='intc'), \
-        arange(1, n+1, dtype='intc')), axis = 1)
-    col = concatenate((arange(n, dtype='intc'), \
-        arange(n, dtype='intc')), axis = 1)
-    data = concatenate((arange(1, n+1, dtype='double'), \
-            arange(1,n+1, dtype='double')), axis = 1)
+    row = hstack((arange(n, dtype=int), \
+                  arange(1, n+1, dtype=int)))
+    col = hstack((arange(n, dtype=int), \
+                  arange(n, dtype=int)))
+    data = hstack((arange(1, n+1, dtype=float), \
+                   arange(1,n+1, dtype=float)))
     return coo_matrix((data,(row, col)), shape=(m,n))
 
 def lsmrtest(m, n, damp):
     """Verbose testing of lsmr"""
 
     A = lowerBidiagonalMatrix(m,n)
-    xtrue = arange(n,0,-1, dtype='double')
+    xtrue = arange(n,0,-1, dtype=float)
     Afun = aslinearoperator(A)
 
     b = Afun.matvec(xtrue)
@@ -148,4 +147,4 @@ if __name__ == "__main__":
     # Comment out the next line to run unit tests only
     lsmrtest(20,10,0)
     print("Continuing to run some unit tests...")
-    unittest.main()
+    run_module_suite()
