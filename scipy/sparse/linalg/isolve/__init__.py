5c41066e9c93c417253cbde325a18079c1c69d1a
Stefan van der Walt <stefan@sun.ac.za>
2011-10-31 19:07:51 -0700

    ENH: Add lsmr to isolve module.

--- scipy/sparse/linalg/isolve/__init__.py
+++ scipy/sparse/linalg/isolve/__init__.py
@@ -5,6 +5,7 @@ from iterative import *
 from minres import minres
 from lgmres import lgmres
 from lsqr import lsqr
+from lsmr import lsmr
 
 __all__ = filter(lambda s:not s.startswith('_'),dir())
 from numpy.testing import Tester
