cfbe9253e98b6f6d914ccd37448702dfa8640952
Jeff Armstrong <jeff@approximatrix.com>
2012-01-03 09:22:17 -0500

    Fixed handling of 'scale' parameter in *trsyl.  Added further tests for flapack's *trsyl.

--- scipy/linalg/tests/test_lapack.py
+++ scipy/linalg/tests/test_lapack.py
@@ -5,7 +5,8 @@
 
 from numpy.testing import TestCase, run_module_suite, assert_equal, \
     assert_array_almost_equal, assert_
-from numpy import ones
+
+import numpy as np
 
 from scipy.linalg import flapack, clapack
 
@@ -25,7 +26,7 @@ class TestFlapackSimple(TestCase):
             assert_(not info,`info`)
             assert_array_almost_equal(ba,a)
             assert_equal((lo,hi),(0,len(a[0])-1))
-            assert_array_almost_equal(pivscale,ones(len(a)))
+            assert_array_almost_equal(pivscale, np.ones(len(a)))
 
             ba,lo,hi,pivscale,info = f(a1,permute=1,scale=1)
             assert_(not info,`info`)
@@ -43,16 +44,48 @@ class TestFlapackSimple(TestCase):
             assert_(not info,`info`)
             
     def test_trsyl(self):
-        a = [[1,2],[3,4]]
-        b = [[5,6],[7,8]]
-        c = [[9,10],[11,12]]
+        a = [[1, 2], [3, 4]]
+        b = [[5, 6], [7, 8]]
+        c = [[9, 10], [11, 12]]
+        
         x_expected = [[0.5, 0.66667],
                       [0.66667, 0.5]]
+                      
+        x_expected_t = [[0.5, 0.58333],
+                        [0.83333, 0.41667]]
+                        
+        x_expected_m = [[-1.125, -1.0],
+                        [-1.25, -1.875]]
+        
+        # Test single and double implementations, including most
+        # of the options
         for p in 'ds':
-            f = getattr(flapack,p+'trsyl',None)
-            if f is None: continue
-            x, info = f(a,b,c)
-            assert_array_almost_equal(x,x_expected, decimal=4)
+            f = getattr(flapack, p+'trsyl', None)
+                
+            x, scale, info = f(a, b, c)
+            assert_array_almost_equal(x, x_expected, decimal=4)
+            assert_equal(scale,1.0)
+            
+            x, scale, info = f(a, b, c, trana='T', tranb='T')
+            assert_array_almost_equal(x, x_expected_t, decimal=4)
+            
+            x, scale, info = f(a, b, c, isgn=-1)
+            assert_array_almost_equal(x, x_expected_m, decimal=4)
+        
+        # Test complex operation (a bit simpler...)
+        ac = [[0+2.j,],]
+        bc = [[0+4.j,],]
+        cc = [[0+12.j,],]
+        x_expected_c = [[2.0+0.j,],]
+        
+        for p in 'zc':
+            f = getattr(flapack, p+'trsyl', None)
+            
+            x, scale, info = f(ac, bc, cc)
+            assert_array_almost_equal(x, x_expected_c, decimal=4)
+            
+            x, scale, info = f(ac, bc, cc, trana='C', tranb='C')
+            assert_array_almost_equal(x, -1.0*np.asarray(x_expected_c), decimal=4)
 
 class TestLapack(TestCase):
 
