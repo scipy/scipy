d1050283cc9cd498d8489e4550d72791e3d66de2
Denis Laxalde <denis.laxalde@mcgill.ca>
2012-01-13 16:45:09 -0500

    ENH: add constrained optimization solvers to the minimize interface
    New methods L-BFGS-B, TNC, COBYLA and SLSQP in `minimize` for constrained
    minimization. Additional parameters are `bounds` and `constraints`. As
    described in the previous commits, `constraints` is either a dict or a
    sequence of dict if several constraints are to be handled.
    
    Check if provided parameters are relevant with respect to the choosen
    methods. Returns warning if not.
    
    `show_minimize_options` has been updated accordingly.
    
    Tests for `minimize` with these new methods have been added.

--- scipy/optimize/tests/test_cobyla.py
+++ scipy/optimize/tests/test_cobyla.py
@@ -1,21 +1,38 @@
 import math
 
-from numpy.testing import assert_almost_equal, TestCase, run_module_suite
+from numpy.testing import assert_allclose, TestCase, run_module_suite
 
-from scipy.optimize import cobyla as co
+from scipy.optimize import fmin_cobyla, minimize
 
 class TestCobyla(TestCase):
+    def setUp(self):
+        self.x0 = [4.95,0.66]
+        self.solution = [math.sqrt(25 - (2.0/3)**2), 2.0/3]
+        self.opts = {'disp': False, 'rhobeg': 1, 'rhoend': 1e-5,
+                     'maxfev': 100}
+
+    def fun(self, x):
+        return x[0]**2 + abs(x[1])**3
+
+    def con1(self, x):
+        return x[0]**2 + x[1]**2 - 25
+
+    def con2(self, x):
+        return -self.con1(x)
+
     def test_simple(self):
+        """ fmin_cobyla """
+        x = fmin_cobyla(self.fun, self.x0, [self.con1, self.con2], rhobeg=1,
+                        rhoend=1e-5, iprint=0, maxfun=100)
+        assert_allclose(x, self.solution, atol=1e-4)
 
-        function = lambda x: x[0]**2 + abs(x[1])**3
-        con1 = lambda x: x[0]**2 + x[1]**2 - 25
-        con2 = lambda x: -con1(x)
-
-        x = co.fmin_cobyla(function, [4.95,0.66], [con1, con2], rhobeg=1,
-                           rhoend=1e-5, iprint=0, maxfun=100)
-        x1 = 2.0/3
-        x0 = math.sqrt(25-x1*x1)
-        assert_almost_equal(x, [x0, x1], decimal=5)
+    def test_minimize_simple(self):
+        """ Minimize with method='COBYLA' """
+        cons = ({'type': 'ineq', 'fun': self.con1},
+                {'type': 'ineq', 'fun': self.con2})
+        x = minimize(self.fun, self.x0, method='cobyla', constraints=cons,
+                     options=self.opts)
+        assert_allclose(x, self.solution, atol=1e-4)
 
 if __name__ == "__main__":
     run_module_suite()
