755eeff069214d13b17a84ead29de75807edb0f1
Travis E. Oliphant <teoliphant@gmail.com>
2012-02-05 18:57:33 -0600

    TST: Add tests for axis keyword for circular statistics.

--- scipy/stats/tests/test_morestats.py
+++ scipy/stats/tests/test_morestats.py
@@ -315,6 +315,57 @@ def test_circstats():
     Sval = 6.520702116
     assert_allclose(S, Sval, rtol=1e-7)
 
+def test_circmean_axis():
+    x = np.array([[355,5,2,359,10,350],
+                  [351,7,4,352,9,349],
+                  [357,9,8,358,4,356]])
+    M1 = stats.circmean(x, high=360)
+    M2 = stats.circmean(x.ravel(), high=360)
+    assert_allclose(M1, M2, rtol=1e-14)
+
+    M1 = stats.circmean(x, high=360, axis=1)
+    M2 = [stats.circmean(x[i], high=360) for i in range(x.shape[0])]
+    assert_allclose(M1, M2, rtol=1e-14)
+
+    M1 = stats.circmean(x, high=360, axis=0)
+    M2 = [stats.circmean(x[:,i], high=360) for i in range(x.shape[1])]
+    assert_allclose(M1, M2, rtol=1e-14)
+
+def test_circvar_axis():
+    x = np.array([[355,5,2,359,10,350],
+                  [351,7,4,352,9,349],
+                  [357,9,8,358,4,356]])
+    
+    V1 = stats.circvar(x, high=360)
+    V2 = stats.circvar(x.ravel(), high=360)
+    assert_allclose(V1, V2, rtol=1e-11)
+
+    V1 = stats.circvar(x, high=360, axis=1)
+    V2 = [stats.circvar(x[i], high=360) for i in range(x.shape[0])]
+    assert_allclose(V1, V2, rtol=1e-11)
+
+    V1 = stats.circvar(x, high=360, axis=0)
+    V2 = [stats.circvar(x[:,i], high=360) for i in range(x.shape[1])]
+    assert_allclose(V1, V2, rtol=1e-11)
+
+def test_circstd_axis():
+    x = np.array([[355,5,2,359,10,350],
+                  [351,7,4,352,9,349],
+                  [357,9,8,358,4,356]])
+    
+    S1 = stats.circstd(x, high=360)
+    S2 = stats.circstd(x.ravel(), high=360)
+    assert_allclose(S1, S2, rtol=1e-11)
+
+    S1 = stats.circstd(x, high=360, axis=1)
+    S2 = [stats.circstd(x[i], high=360) for i in range(x.shape[0])]
+    assert_allclose(S1, S2, rtol=1e-11)
+
+    S1 = stats.circstd(x, high=360, axis=0)
+    S2 = [stats.circstd(x[:,i], high=360) for i in range(x.shape[1])]
+    assert_allclose(S1, S2, rtol=1e-11)
+    
+
 def test_circstats_small():
     x = np.array([20,21,22,18,19,20.5,19.2])
     M1 = x.mean()
