fdc8c62fc5802650eb0877eef24c4b8183d93990
Jake Vanderplas <jakevdp@yahoo.com>
2012-02-06 07:59:26 -0800

    add depth-first search

--- scipy/sparse/csgraph/__init__.py
+++ scipy/sparse/csgraph/__init__.py
@@ -7,10 +7,11 @@ __all__ = ['cs_graph_components',
            'floyd_warshall',
            'dijkstra',
            'cs_graph_laplacian',
-           'cs_graph_breadth_first']
+           'cs_graph_breadth_first',
+           'cs_graph_depth_first']
 
 from graph_components import cs_graph_components
 from graph_shortest_path import\
     cs_graph_shortest_path, floyd_warshall, dijkstra, construct_dist_matrix
 from graph_laplacian import cs_graph_laplacian
-from graph_traversal import cs_graph_breadth_first
+from graph_traversal import cs_graph_breadth_first, cs_graph_depth_first
