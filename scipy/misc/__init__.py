be19150695d7896386b4549cd2e1cd35f55a3754
Ralf Gommers <ralf.gommers@googlemail.com>
2012-02-18 21:58:51 +0100

    BUG: misc: fix possible issue when PIL is not installed.
    See PR-164.  This is a partial backport of 4f29f4f0.

--- scipy/misc/__init__.py
+++ scipy/misc/__init__.py
@@ -50,10 +50,12 @@ del sys
 
 try:
     from pilutil import *
+    import pilutil
     __all__ += pilutil.__all__
 except ImportError:
     pass
 
+import common
 __all__ += common.__all__
 
 from numpy.testing import Tester
