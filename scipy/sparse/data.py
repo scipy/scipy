f1cce6dcce3252f8f895151fd8fb0db11b2ba90b
Lars Buitinck <L.J.Buitinck@uva.nl>
2012-05-09 23:15:07 +0200

    ENH: move new sparse ufuncs from instances to class

--- scipy/sparse/data.py
+++ scipy/sparse/data.py
@@ -8,7 +8,6 @@
 
 __all__ = []
 
-import types
 import numpy as np
 
 from base import spmatrix
@@ -20,26 +19,6 @@ from sputils import isscalarlike
 class _data_matrix(spmatrix):
     def __init__(self):
         spmatrix.__init__(self)
-
-        # Add the numpy unary ufuncs for which func(0) = 0
-        # to this class.
-        for npfunc in [np.sin, np.tan, np.arcsin, np.arctan,
-                       np.sinh, np.tanh, np.arcsinh, np.arctanh,
-                       np.rint, np.sign, np.expm1, np.log1p,
-                       np.deg2rad, np.rad2deg, np.floor, np.ceil,
-                       np.trunc]:
-            name = npfunc.__name__
-
-            def create_func(op):
-                def func(self):
-                    result = op(self.data)
-                    x = self._with_data(result, copy=True)
-                    return x
-                func.__doc__ = ("Element-wise %s\n\nSee numpy.%s "
-                                "for more information." % (name, name))
-                return func
-            setattr(self, name, types.MethodType(create_func(npfunc),
-                                                 self, self.__class__))
 
     def _get_dtype(self):
         return self.data.dtype
@@ -90,3 +69,24 @@ class _data_matrix(spmatrix):
 
     def _mul_scalar(self, other):
         return self._with_data(self.data * other)
+
+
+# Add the numpy unary ufuncs for which func(0) = 0 to _data_matrix.
+for npfunc in [np.sin, np.tan, np.arcsin, np.arctan, np.sinh, np.tanh,
+               np.arcsinh, np.arctanh, np.rint, np.sign, np.expm1, np.log1p,
+               np.deg2rad, np.rad2deg, np.floor, np.ceil, np.trunc]:
+    name = npfunc.__name__
+
+    def create_method(op):
+        def method(self):
+            result = op(self.data)
+            x = self._with_data(result, copy=True)
+            return x
+
+        method.__doc__ = ("Element-wise %s.\n\n"
+                          "See numpy.%s for more information." % (name, name))
+        method.__name__ = name
+
+        return method
+
+    setattr(_data_matrix, name, create_method(npfunc))
