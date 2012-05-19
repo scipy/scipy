b4ef007ac50495fb1d5faf3d03bfcc7a0d83bcaf
Pauli Virtanen <pav@iki.fi>
2012-05-19 21:04:36 +0200

    TST: optimize: add tests for newton

--- scipy/optimize/tests/test_zeros.py
+++ scipy/optimize/tests/test_zeros.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
-from math import sqrt
+from math import sqrt, exp, sin, cos
 
 from numpy.testing import TestCase, assert_almost_equal, assert_warns, \
-                            assert_, run_module_suite
+                            assert_, run_module_suite, assert_allclose
 
 from scipy.optimize import zeros as cc
+from scipy.optimize import zeros
 
 # Import testing parameters
 from scipy.optimize._tstutils import functions, fstrings
@@ -28,6 +29,23 @@ class TestBasic(TestCase) :
         self.run_check(cc.brentq, 'brentq')
     def test_brenth(self):
         self.run_check(cc.brenth, 'brenth')
+
+    def test_newton(self):
+        f1 = lambda x: x**2 - 2*x - 1
+        f1_1 = lambda x: 2*x - 2
+        f1_2 = lambda x: 2.0 + 0*x
+
+        f2 = lambda x: exp(x) - cos(x)
+        f2_1 = lambda x: exp(x) + sin(x)
+        f2_2 = lambda x: exp(x) + cos(x)
+
+        for f, f_1, f_2 in [(f1, f1_1, f1_2), (f2, f2_1, f2_2)]:
+            x = zeros.newton(f, 3, tol=1e-6)
+            assert_allclose(f(x), 0, atol=1e-6)
+            x = zeros.newton(f, 3, fprime=f_1, tol=1e-6)
+            assert_allclose(f(x), 0, atol=1e-6)
+            x = zeros.newton(f, 3, fprime=f_1, fprime2=f_2, tol=1e-6)
+            assert_allclose(f(x), 0, atol=1e-6)
 
     def test_deriv_zero_warning(self):
         func = lambda x: x**2
