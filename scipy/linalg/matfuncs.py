c4f2c036924fd99e866dc4633850e39ff216da09
Ralf Gommers <ralf.gommers@googlemail.com>
2012-05-28 22:23:00 +0200

    BUG: fix Python 2.4 compatibility in scipy.linalg.

--- scipy/linalg/matfuncs.py
+++ scipy/linalg/matfuncs.py
@@ -26,7 +26,7 @@ feps = np.finfo(single).eps
 
 def expm(A, q=False):
     """Compute the matrix exponential using Pade approximation.
-    
+
     Parameters
     ----------
     A : array, shape(M,M)
@@ -48,7 +48,7 @@ def expm(A, q=False):
     A = asarray(A)
     A_L1 = norm(A,1)
     n_squarings = 0
-    
+
     if A.dtype == 'float64' or A.dtype == 'complex128':
         if A_L1 < 1.495585217958292e-002:
             U,V = _pade3(A)
@@ -75,21 +75,21 @@ def expm(A, q=False):
             U,V = _pade7(A)
     else:
         raise ValueError("invalid type: "+str(A.dtype))
-    
+
     P = U + V  # p_m(A) : numerator
     Q = -U + V # q_m(A) : denominator
     R = solve(Q,P)
     # squaring step to undo scaling
     for i in range(n_squarings):
         R = dot(R,R)
-    
+
     return R
 
 # implementation of Pade approximations of various degree using the algorithm presented in [Higham 2005]
 
 def _pade3(A):
     b = (120., 60., 12., 1.)
-    ident = eye(*A.shape, dtype=A.dtype)
+    ident = eye(A.shape[0], A.shape[1], dtype=A.dtype)
     A2 = dot(A,A)
     U = dot(A , (b[3]*A2 + b[1]*ident))
     V = b[2]*A2 + b[0]*ident
@@ -97,7 +97,7 @@ def _pade3(A):
 
 def _pade5(A):
     b = (30240., 15120., 3360., 420., 30., 1.)
-    ident = eye(*A.shape, dtype=A.dtype)
+    ident = eye(A.shape[0], A.shape[1], dtype=A.dtype)
     A2 = dot(A,A)
     A4 = dot(A2,A2)
     U = dot(A, b[5]*A4 + b[3]*A2 + b[1]*ident)
@@ -106,7 +106,7 @@ def _pade5(A):
 
 def _pade7(A):
     b = (17297280., 8648640., 1995840., 277200., 25200., 1512., 56., 1.)
-    ident = eye(*A.shape, dtype=A.dtype)
+    ident = eye(A.shape[0], A.shape[1], dtype=A.dtype)
     A2 = dot(A,A)
     A4 = dot(A2,A2)
     A6 = dot(A4,A2)
@@ -117,7 +117,7 @@ def _pade7(A):
 def _pade9(A):
     b = (17643225600., 8821612800., 2075673600., 302702400., 30270240.,
                 2162160., 110880., 3960., 90., 1.)
-    ident = eye(*A.shape, dtype=A.dtype)
+    ident = eye(A.shape[0], A.shape[1], dtype=A.dtype)
     A2 = dot(A,A)
     A4 = dot(A2,A2)
     A6 = dot(A4,A2)
@@ -130,7 +130,7 @@ def _pade13(A):
     b = (64764752532480000., 32382376266240000., 7771770303897600.,
     1187353796428800., 129060195264000., 10559470521600., 670442572800.,
     33522128640., 1323241920., 40840800., 960960., 16380., 182., 1.)
-    ident = eye(*A.shape, dtype=A.dtype)
+    ident = eye(A.shape[0], A.shape[1], dtype=A.dtype)
     A2 = dot(A,A)
     A4 = dot(A2,A2)
     A6 = dot(A4,A2)
