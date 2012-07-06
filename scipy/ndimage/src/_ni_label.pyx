4a687c8cf3dafcb6bd92d77c9c39b2d8dc95a3ae
Thouis (Ray) Jones <thouis@gmail.com>
2012-07-06 11:33:18 +0200

    ENH: halve .c size of ndimage.label, but 2-3 times slower.

--- scipy/ndimage/src/_ni_label.pyx
+++ scipy/ndimage/src/_ni_label.pyx
@@ -48,30 +48,73 @@ ctypedef fused data_t:
     np.float32_t
     np.float64_t
 
-# needed for double-specialization of copy_nonzero_line
-ctypedef fused data2_t:
-    np.int8_t
-    np.int16_t
-    np.int32_t
-    np.int64_t
-    np.uint8_t
-    np.uint16_t
-    np.uint32_t
-    np.uint64_t
-    np.float32_t
-    np.float64_t
+
+######################################################################
+# Nonzero test for fused types
+######################################################################
+cdef bint fused_nonzero(data_t *p) nogil:
+    return (p[0] != 0)
+
+
+######################################################################
+# Load from fused type pointer, returning np.uintp_t
+######################################################################
+cdef np.uintp_t fused_read_label(data_t *p) nogil:
+    return <np.uintp_t> p[0]
+
+
+######################################################################
+# Store np.uintp_t in fused type pointer if possible, returning True if
+# overflowed
+######################################################################
+cdef bint fused_write_label(data_t *p, np.uintp_t val) nogil:
+    cdef data_t tmp = <data_t> val
+    if val == <np.uintp_t> tmp:
+        p[0] = tmp
+        return False
+    return True
+
+
+######################################################################
+# Function specializers
+######################################################################
+def get_nonzero(np.ndarray[data_t] a):
+    return <Py_intptr_t> fused_nonzero[data_t]
+
+def get_read_label(np.ndarray[data_t] a):
+    return <Py_intptr_t> fused_read_label[data_t]
+
+def get_write_label(np.ndarray[data_t] a):
+    return <Py_intptr_t> fused_write_label[data_t]
+
+
+######################################################################
+# Typedefs for referring to specialized instances of fused functions
+######################################################################
+ctypedef bint (*nonzero_func_t)(void *p) nogil
+ctypedef np.uintp_t (*read_label_func_t)(void *p) nogil
+ctypedef bint (*write_label_func_t)(void *p, np.uintp_t val) nogil
+
+
+######################################################################
+# advance a numpy array pointer (mostly to avoid casts below)
+######################################################################
+cdef inline void *step(void *p, int stride) nogil:
+    return p + stride
+
 
 ######################################################################
 # Copy one line of the input array to the output, setting output to FOREGROUND
 # wherever the input is nonzero
 ######################################################################
-cdef void copy_nonzero_line(data_t *inl, data2_t *out,
-                            np.intp_t L, np.intp_t si, np.intp_t so):
+cdef void copy_nonzero_line(void *inl, void *out,
+                            nonzero_func_t nonzero, write_label_func_t write_label,
+                            np.intp_t L, np.intp_t si, np.intp_t so) nogil:
     cdef np.uint_t i
     for i in range(L):
-        out[0] = FOREGROUND if (inl[0] != 0) else BACKGROUND
-        inl = <data_t *> ((<void *> inl) + si)
-        out = <data2_t *> ((<void *> out) + so)
+        write_label(out, FOREGROUND if nonzero(inl) else BACKGROUND)
+        inl = step(inl, si)
+        out = step(out, so)
 
 
 ######################################################################
@@ -98,17 +141,20 @@ cdef inline np.uintp_t take_label_or_merge(np.uintp_t neighbor_label,
     if neighbor_label == BACKGROUND:
         return cur_label
     if cur_label == FOREGROUND:
-        return neighbor_label
+        return neighbor_label  # neighbor is not BACKGROUND
     if neighbor_label:
         if cur_label != neighbor_label:
             cur_label = mark_for_merge(neighbor_label, cur_label, mergetable)
     return cur_label
 
+
 ######################################################################
 # Label one line of input, using a neighbor line that has already been labeled.
 ######################################################################
-cdef void label_line_with_neighbor(data_t *line,
-                                   data_t *neighbor,
+cdef void label_line_with_neighbor(void *line,
+                                   void *neighbor,
+                                   read_label_func_t read_label,
+                                   write_label_func_t write_label,
                                    np.intp_t stride,
                                    int use_previous,
                                    int use_adjacent,
@@ -117,138 +163,130 @@ cdef void label_line_with_neighbor(data_t *line,
                                    np.uintp_t *mergetable) nogil:
     cdef:
         np.uintp_t cur_label, i
-        data_t *prev, *next
+        np.uintp_t prev_label, adjacent_label, next_label
+        void *prev, *next
 
     # special case the first element
-    next = <data_t *> ((<void *> neighbor) + stride)
-    if line[0] != BACKGROUND:
-        cur_label = <np.uintp_t> line[0]
+    next = step(neighbor, stride)
+    adjacent_label = read_label(neighbor)
+    next_label = read_label(next)
+    cur_label = read_label(line)
+    if cur_label != BACKGROUND:
         if use_adjacent:
-            cur_label = take_label_or_merge(<np.uintp_t> neighbor[0], cur_label, mergetable)
+            cur_label = take_label_or_merge(adjacent_label, cur_label, mergetable)
         if use_next:
-            cur_label = take_label_or_merge(<np.uintp_t> next[0], cur_label, mergetable)
-        line[0] = cur_label
+            cur_label = take_label_or_merge(next_label, cur_label, mergetable)
+        write_label(line, cur_label)
+
+    # loop over all but last element
     for i in range(1, L - 1):
         # increment pointers
-        line = <data_t *> ((<void *> line) + stride)
+        line = step(line, stride)
         prev = neighbor
         neighbor = next
-        next = <data_t *> ((<void *> next) + stride)
-        if line[0] == BACKGROUND:
+        next = step(next, stride)
+        prev_label = adjacent_label
+        adjacent_label = next_label
+        next_label = read_label(next)
+        cur_label = read_label(line)
+        if cur_label == BACKGROUND:
             continue
-        cur_label = <np.uintp_t> line[0]
         if use_previous:
-            cur_label = take_label_or_merge(<np.uintp_t> prev[0], cur_label, mergetable)
+            cur_label = take_label_or_merge(prev_label, cur_label, mergetable)
         if use_adjacent:
-            cur_label = take_label_or_merge(<np.uintp_t> neighbor[0], cur_label, mergetable)
+            cur_label = take_label_or_merge(adjacent_label, cur_label, mergetable)
         if use_next:
-            cur_label = take_label_or_merge(<np.uintp_t> next[0], cur_label, mergetable)
-        line[0] = cur_label
+            cur_label = take_label_or_merge(next_label, cur_label, mergetable)
+        write_label(line, cur_label)
+
     # special case the last element
     # increment pointers
-    line = <data_t *> ((<void *> line) + stride)
+    line = step(line, stride)
     prev = neighbor
     neighbor = next
-    if line[0] != BACKGROUND:
-        cur_label = <np.uintp_t> line[0]
+    prev_label = adjacent_label
+    adjacent_label = next_label
+    cur_label = read_label(line)
+    if cur_label != BACKGROUND:
         if use_previous:
-            cur_label = take_label_or_merge(<np.uintp_t> prev[0], cur_label, mergetable)
+            cur_label = take_label_or_merge(prev_label, cur_label, mergetable)
         if use_adjacent:
-            cur_label = take_label_or_merge(<np.uintp_t> neighbor[0], cur_label, mergetable)
-        line[0] = cur_label
+            cur_label = take_label_or_merge(adjacent_label, cur_label, mergetable)
+        write_label(line, cur_label)
+
 
 ######################################################################
 # Label one line of input after labeling it with any neighbors.
 # returns updated count of regions.
 ######################################################################
-cdef np.uintp_t label_line(data_t *line,
+cdef np.uintp_t label_line(void *line,
+                           read_label_func_t read_label,
+                           write_label_func_t write_label,
                            np.intp_t stride,
                            bint use_previous,
                            np.uintp_t L,
                            np.uintp_t next_label,
                            np.uintp_t *mergetable) nogil except 0:
     cdef:
-        np.uintp_t cur_label, i
-        data_t *prev
+        np.uintp_t cur_label, prev_label, i
+        bint overflowed
 
     # special case first element
-    if line[0] != BACKGROUND:
-        if <np.uintp_t> line[0] == FOREGROUND:
-            # create a new region
-            line[0] = <data_t> next_label
-            mergetable[next_label] = next_label
-            next_label += 1
-            # check that we aren't going to overflow
-            if next_label != <np.intp_t> <data_t> next_label:
-                with gil:
-                    raise NeedMoreBits()
+    cur_label = read_label(line)
+    if cur_label == FOREGROUND:
+        # create a new region
+        cur_label = next_label
+        mergetable[cur_label] = cur_label
+        overflowed = write_label(line, cur_label)
+        if overflowed:
+            with gil:
+                raise NeedMoreBits()
+        next_label += 1
+
     for i in range(1, L):
-        prev = line
-        line = <data_t *> ((<void *> line) + stride)
-        if line[0] == BACKGROUND:
+        prev_label = cur_label
+        line = step(line, stride)
+        cur_label = read_label(line)
+        if cur_label == BACKGROUND:
             continue
-        elif line[0] == FOREGROUND:
-            if use_previous and (prev[0] != BACKGROUND):
-                line[0] = prev[0]
+        elif cur_label == FOREGROUND:
+            if use_previous and prev_label != BACKGROUND:
+                cur_label = prev_label
             else:
                 # create a new region
-                line[0] = <data_t> next_label
-                mergetable[next_label] = next_label
+                cur_label = next_label
+                mergetable[cur_label] = cur_label
                 next_label += 1
-                # check that we aren't going to overflow
-                if next_label != <np.intp_t> <data_t> next_label:
-                    with gil:
-                        raise NeedMoreBits()
-        elif use_previous and (prev[0] != BACKGROUND) and \
-                line[0] != prev[0]:
-            line[0] = <data_t> mark_for_merge(<np.uintp_t> line[0],
-                                              <np.uintp_t> prev[0],
-                                              mergetable)
+        elif use_previous and (prev_label != BACKGROUND) and \
+                (cur_label != prev_label):
+            cur_label = mark_for_merge(cur_label,
+                                       prev_label,
+                                       mergetable)
+        overflowed = write_label(line, cur_label)
+        if overflowed:
+            with gil:
+                raise NeedMoreBits()
+
     return next_label
+
 
 ######################################################################
 # Remap region labels through the mergetable
 ######################################################################
-cdef void remap_line(data_t *line,
+cdef void remap_line(void *line,
+                     read_label_func_t read_label,
+                     write_label_func_t write_label,
                      np.intp_t stride,
                      np.uintp_t L,
                      np.uintp_t *mergetable) nogil:
     for i in range(0, L):
-        line[0] = <data_t> mergetable[<np.uintp_t> line[0]]
-        line = <data_t *> ((<void *> line) + stride)
+        write_label(line, mergetable[read_label(line)])
+        line = step(line, stride)
 
 
 ######################################################################
-# Function specializers
+# Label regions
 ######################################################################
-ctypedef void (*copy_nonzero_line_func_t)(void *inl, void *out,
-                                          np.intp_t L,
-                                          np.intp_t si, np.intp_t so) nogil
-ctypedef void (*label_line_with_neighbor_func_t)(void *line,
-                                                 void *neighbor,
-                                                 np.intp_t line_stride,
-                                                 int use_previous,
-                                                 int use_adjacent,
-                                                 int use_next,
-                                                 np.intp_t L,
-                                                 np.uintp_t *mergetable) nogil
-ctypedef np.uintp_t (*label_line_func_t)(void *line,
-                                         np.intp_t line_stride,
-                                         int use_previous,
-                                         np.uintp_t L,
-                                         np.uintp_t next_label,
-                                         np.uintp_t *mergetable) nogil
-ctypedef void (*remap_line_func_t)(void *line,
-                                   np.intp_t line_stride,
-                                   np.uintp_t L,
-                                   np.uintp_t *mergetable) nogil
-
-def get_funcs(np.ndarray[data_t] input, np.ndarray[data2_t] output):
-    return (<Py_intptr_t> copy_nonzero_line[data_t, data2_t],
-            <Py_intptr_t> label_line_with_neighbor[data2_t],
-            <Py_intptr_t> label_line[data2_t],
-            <Py_intptr_t> remap_line[data2_t])
-
 cpdef _label(np.ndarray input,
              np.ndarray structure,
              np.ndarray output):
@@ -284,16 +322,13 @@ cpdef _label(np.ndarray input,
         # XXX - trigger special check for bit depth?
         output = output.view(dtype=np.uint8)
 
-    funcs = get_funcs(input.take([0]), output.take([0]))
     cdef:
-        copy_nonzero_line_func_t copy_nonzero_line = \
-            <copy_nonzero_line_func_t> <void *> <Py_intptr_t> funcs[0]
-        label_line_with_neighbor_func_t label_line_with_neighbor = \
-            <label_line_with_neighbor_func_t> <void *> <Py_intptr_t> funcs[1]
-        label_line_func_t label_line = \
-            <label_line_func_t> <void *> <Py_intptr_t> funcs[2]
-        remap_line_func_t remap_line = \
-            <remap_line_func_t> <void *> <Py_intptr_t> funcs[3]
+        nonzero_func_t nonzero = \
+            <nonzero_func_t> <void *> <Py_intptr_t> get_nonzero(input.take([0]))
+        read_label_func_t read_label = \
+            <read_label_func_t> <void *> <Py_intptr_t> get_read_label(output.take([0]))
+        write_label_func_t write_label = \
+            <write_label_func_t> <void *> <Py_intptr_t> get_write_label(output.take([0]))
         np.uintp_t next_label, src_label, dest_label
         np.flatiter _iti, _ito, _itstruct
         PyArrayIterObject *iti, *ito, *itstruct
@@ -342,14 +377,19 @@ cpdef _label(np.ndarray input,
         with nogil:
             while PyArray_ITER_NOTDONE(iti):
                 # nonzero input -> output
-                copy_nonzero_line(PyArray_ITER_DATA(iti), PyArray_ITER_DATA(ito), L, si, so)
+                copy_nonzero_line(PyArray_ITER_DATA(iti), PyArray_ITER_DATA(ito),
+                                  nonzero, write_label,
+                                  L, si, so)
 
                 # Take neighbor labels
                 PyArray_ITER_RESET(itstruct)
                 while PyArray_ITER_NOTDONE(itstruct):
-                    if not ((<np.int_t *> PyArray_ITER_DATA(itstruct))[0] or
-                            (<np.int_t *> (PyArray_ITER_DATA(itstruct) + ss))[0] or
-                            (<np.int_t *> (PyArray_ITER_DATA(itstruct) + 2 * ss))[0]):
+                    neighbor_use_prev = (<np.int_t *> PyArray_ITER_DATA(itstruct))[0]
+                    neighbor_use_adjacent = (<np.int_t *> (PyArray_ITER_DATA(itstruct) + ss))[0]
+                    neighbor_use_next = (<np.int_t *> (PyArray_ITER_DATA(itstruct) + 2 * ss))[0]
+                    if not (neighbor_use_prev or
+                            neighbor_use_adjacent or
+                            neighbor_use_next):
                         PyArray_ITER_NEXT(itstruct)
                         continue
                     valid = True
@@ -374,10 +414,11 @@ cpdef _label(np.ndarray input,
                         else:
                             label_line_with_neighbor(PyArray_ITER_DATA(ito),
                                                      PyArray_ITER_DATA(ito) + total_offset,
+                                                     read_label, write_label,
                                                      so,
-                                                     (<np.int_t *> (PyArray_ITER_DATA(itstruct)))[0],
-                                                     (<np.int_t *> (PyArray_ITER_DATA(itstruct) + ss))[0],
-                                                     (<np.int_t *> (PyArray_ITER_DATA(itstruct) + 2 * ss))[0],
+                                                     neighbor_use_prev,
+                                                     neighbor_use_adjacent,
+                                                     neighbor_use_next,
                                                      L,
                                                      mergetable)
                     PyArray_ITER_NEXT(itstruct)
@@ -386,9 +427,11 @@ cpdef _label(np.ndarray input,
                 # be conservative about how much space we may need
                 while mergetable_size < (next_label + L):
                     mergetable_size *= 2
-                    mergetable = <np.uintp_t *> PyDataMem_RENEW(<void *> mergetable,
-                                                                 mergetable_size * sizeof(np.uintp_t))
+                    mergetable = <np.uintp_t *> \
+                        PyDataMem_RENEW(<void *> mergetable,
+                                         mergetable_size * sizeof(np.uintp_t))
                 next_label = label_line(PyArray_ITER_DATA(ito),
+                                        read_label, write_label,
                                         so, use_previous, L,
                                         next_label, mergetable)
                 PyArray_ITER_NEXT(iti)
@@ -405,15 +448,17 @@ cpdef _label(np.ndarray input,
                     mergetable[src_label] = dest_label
                     dest_label += 1
                 else:
-                    # we've compacted every label below this, and the mergetable has an
-                    # invariant (from mark_for_merge()) that it always points downward.
-                    # Therefore, we can fetch the final lable by two steps of
-                    # indirection.
+                    # we've compacted every label below this, and the
+                    # mergetable has an invariant (from mark_for_merge()) that
+                    # it always points downward.  Therefore, we can fetch the
+                    # final lable by two steps of indirection.
                     mergetable[src_label] = mergetable[mergetable[src_label]]
 
             PyArray_ITER_RESET(ito)
             while PyArray_ITER_NOTDONE(ito):
-                remap_line(PyArray_ITER_DATA(ito), so, L, mergetable)
+                remap_line(PyArray_ITER_DATA(ito),
+                           read_label, write_label,
+                           so, L, mergetable)
                 PyArray_ITER_NEXT(ito)
     except:
         # clean up and re-raise
