78922b680f20509e84210f3a5acba17c17ce19a4
jtaylor <jtaylor@cs.toronto.edu>
2012-08-07 22:41:35 -0400

    A more involved test for MST code.

--- scipy/sparse/csgraph/tests/test_spanning_tree.py
+++ scipy/sparse/csgraph/tests/test_spanning_tree.py
@@ -1,18 +1,37 @@
 """Test the minimum spanning tree function"""
 import numpy as np
 from numpy.testing import assert_
+import numpy.testing as npt
 from scipy.sparse import csr_matrix
 from scipy.sparse.csgraph import minimum_spanning_tree
 
-def construct_sparse_graph(N):
-    np.random.seed(1234)
-    csgraph = np.random.random((N, N))
-    ind = np.random.randint(N, size=(2, N * N / 2))
-    csgraph[ind[0], ind[1]] = 0
-    return csr_matrix(csgraph)
-
 def test_minimum_spanning_tree():
+
+    np.random.seed(1234)
+    
     for N in (5, 10, 15, 20):
-        csgraph = construct_sparse_graph(N)
+        
+        # Create a graph as above where every edge is >= 3 except at
+        # most N that are 2.
+        graph = 3 + np.random.random((N, N))
+        ind = np.random.randint(N, size=(2, N * N / 2))
+        graph[ind[0], ind[1]] = 2
+        
+        csgraph = csr_matrix(graph)
+
+        # We thus expect at most N+1 in the spanning tree.        
         mintree = minimum_spanning_tree(csgraph)
         assert_(mintree.nnz < N)
+        
+        # Now set the sub diagonal to 1, to create a known spanning tree.
+        idx = np.arange(N-1)
+        graph[idx,idx+1] = 1
+        csgraph = csr_matrix(graph)
+        mintree = minimum_spanning_tree(csgraph)
+        
+        # We expect to see this pattern in the spanning tree and otherwise
+        # have this zero.
+        expected = np.zeros((N,N))
+        expected[idx,idx+1] = 1
+        
+        npt.assert_array_equal(mintree.todense(), expected, 'Incorrect spanning tree found.')
