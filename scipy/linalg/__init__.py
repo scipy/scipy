8ffd440280766a852478f3a7309575df8e663f00
Jake Vanderplas <vanderplas@astro.washington.edu>
2012-08-16 09:03:55 -0700

    add pinvh: hermitian pseudo-inverse

--- scipy/linalg/__init__.py
+++ scipy/linalg/__init__.py
@@ -31,6 +31,7 @@ Basics
    lstsq - Solve a linear least-squares problem
    pinv - Pseudo-inverse (Moore-Penrose) using lstsq
    pinv2 - Pseudo-inverse using svd
+   pinvh - Pseudo-inverse of hermitian matrix
    kron - Kronecker product of two arrays
    tril - Construct a lower-triangular matrix from a given matrix
    triu - Construct an upper-triangular matrix from a given matrix
