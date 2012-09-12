5d4bad8efc2ee33b72b130ecc3e05d48677e13e0
endolith <endolith@gmail.com>
2012-09-12 10:06:35 -0400

    ricker docstring not rendering correctly on web

--- scipy/signal/wavelets.py
+++ scipy/signal/wavelets.py
@@ -251,7 +251,7 @@ def morlet(M, w=5.0, s=1.0, complete=True):
 
 def ricker(points, a):
     """
-    Also known as the "mexican hat wavelet",
+    Also known as the "Mexican hat wavelet",
     models the function:
     A ( 1 - x^2/a^2) exp(-t^2/a^2),
     where ``A = 2/sqrt(3a)pi^1/3``
@@ -263,10 +263,12 @@ def ricker(points, a):
     points: int, optional
         Number of points in `vector`. Default is ``10*a``
         Will be centered around 0.
+
     Returns
-    -----------
+    -------
     vector: 1-D ndarray
         array of length `points` in shape of ricker curve.
+
     Examples
     --------
     >>> import matplotlib.pyplot as plt
@@ -277,8 +279,8 @@ def ricker(points, a):
     100
     >>> plt.plot(vec2)
     >>> plt.show()
-    """
 
+    """
     A = 2 / (np.sqrt(3 * a) * (np.pi**0.25))
     wsq = a**2
     vec = np.arange(0, points) - (points - 1.0) / 2
