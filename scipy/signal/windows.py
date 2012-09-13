0a34d290d4967e9ffa037b9072fe26b7179e8f57
Ralf Gommers <ralf.gommers@googlemail.com>
2012-09-13 22:29:05 +0200

    DOC: strip matplotlib output and unnecessary calls from signal docstrings.

--- scipy/signal/windows.py
+++ scipy/signal/windows.py
@@ -196,33 +196,20 @@ def blackman(M, sym=True):
 
     >>> window = scipy.signal.blackman(51)
     >>> plt.plot(window)
-    [<matplotlib.lines.Line2D object at 0x...>]
     >>> plt.title("Blackman window")
-    <matplotlib.text.Text object at 0x...>
     >>> plt.ylabel("Amplitude")
-    <matplotlib.text.Text object at 0x...>
     >>> plt.xlabel("Sample")
-    <matplotlib.text.Text object at 0x...>
-    >>> plt.show()
 
     >>> plt.figure()
-    <matplotlib.figure.Figure object at 0x...>
     >>> A = fft(window, 2048) / 25.5
     >>> mag = np.abs(fftshift(A))
     >>> freq = np.linspace(-0.5, 0.5, len(A))
     >>> response = 20 * np.log10(mag)
     >>> response = np.clip(response, -100, 100)
     >>> plt.plot(freq, response)
-    [<matplotlib.lines.Line2D object at 0x...>]
     >>> plt.title("Frequency response of Blackman window")
-    <matplotlib.text.Text object at 0x...>
     >>> plt.ylabel("Magnitude [dB]")
-    <matplotlib.text.Text object at 0x...>
     >>> plt.xlabel("Normalized frequency [cycles per sample]")
-    <matplotlib.text.Text object at 0x...>
-    >>> plt.axis('tight')
-    (-0.5, 0.5, -100.0, ...)
-    >>> plt.show()
 
     """
     # Docstring adapted from NumPy's blackman function
@@ -414,33 +401,20 @@ def bartlett(M, sym=True):
 
     >>> window = scipy.signal.bartlett(51)
     >>> plt.plot(window)
-    [<matplotlib.lines.Line2D object at 0x...>]
     >>> plt.title("Bartlett window")
-    <matplotlib.text.Text object at 0x...>
     >>> plt.ylabel("Amplitude")
-    <matplotlib.text.Text object at 0x...>
     >>> plt.xlabel("Sample")
-    <matplotlib.text.Text object at 0x...>
-    >>> plt.show()
 
     >>> plt.figure()
-    <matplotlib.figure.Figure object at 0x...>
     >>> A = fft(window, 2048) / 25.5
     >>> mag = np.abs(fftshift(A))
     >>> freq = np.linspace(-0.5, 0.5, len(A))
     >>> response = 20 * np.log10(mag)
     >>> response = np.clip(response, -100, 100)
     >>> plt.plot(freq, response)
-    [<matplotlib.lines.Line2D object at 0x...>]
     >>> plt.title("Frequency response of Bartlett window")
-    <matplotlib.text.Text object at 0x...>
     >>> plt.ylabel("Magnitude [dB]")
-    <matplotlib.text.Text object at 0x...>
     >>> plt.xlabel("Normalized frequency [cycles per sample]")
-    <matplotlib.text.Text object at 0x...>
-    >>> plt.axis('tight')
-    (-0.5, 0.5, -100.0, ...)
-    >>> plt.show()
 
     """
     # Docstring adapted from NumPy's bartlett function
@@ -518,33 +492,20 @@ def hann(M, sym=True):
 
     >>> window = scipy.signal.hann(51)
     >>> plt.plot(window)
-    [<matplotlib.lines.Line2D object at 0x...>]
     >>> plt.title("Hann window")
-    <matplotlib.text.Text object at 0x...>
     >>> plt.ylabel("Amplitude")
-    <matplotlib.text.Text object at 0x...>
     >>> plt.xlabel("Sample")
-    <matplotlib.text.Text object at 0x...>
-    >>> plt.show()
 
     >>> plt.figure()
-    <matplotlib.figure.Figure object at 0x...>
     >>> A = fft(window, 2048) / 25.5
     >>> mag = np.abs(fftshift(A))
     >>> freq = np.linspace(-0.5, 0.5, len(A))
     >>> response = 20 * np.log10(mag)
     >>> response = np.clip(response, -100, 100)
     >>> plt.plot(freq, response)
-    [<matplotlib.lines.Line2D object at 0x...>]
     >>> plt.title("Frequency response of the Hann window")
-    <matplotlib.text.Text object at 0x...>
     >>> plt.ylabel("Magnitude [dB]")
-    <matplotlib.text.Text object at 0x...>
     >>> plt.xlabel("Normalized frequency [cycles per sample]")
-    <matplotlib.text.Text object at 0x...>
-    >>> plt.axis('tight')
-    (-0.5, 0.5, -100.0, ...)
-    >>> plt.show()
 
     """
     # Docstring adapted from NumPy's hanning function
@@ -655,33 +616,20 @@ def hamming(M, sym=True):
 
     >>> window = scipy.signal.hamming(51)
     >>> plt.plot(window)
-    [<matplotlib.lines.Line2D object at 0x...>]
     >>> plt.title("Hamming window")
-    <matplotlib.text.Text object at 0x...>
     >>> plt.ylabel("Amplitude")
-    <matplotlib.text.Text object at 0x...>
     >>> plt.xlabel("Sample")
-    <matplotlib.text.Text object at 0x...>
-    >>> plt.show()
 
     >>> plt.figure()
-    <matplotlib.figure.Figure object at 0x...>
     >>> A = fft(window, 2048) / 25.5
     >>> mag = np.abs(fftshift(A))
     >>> freq = np.linspace(-0.5, 0.5, len(A))
     >>> response = 20 * np.log10(mag)
     >>> response = np.clip(response, -100, 100)
     >>> plt.plot(freq, response)
-    [<matplotlib.lines.Line2D object at 0x...>]
     >>> plt.title("Frequency response of Hamming window")
-    <matplotlib.text.Text object at 0x...>
     >>> plt.ylabel("Magnitude [dB]")
-    <matplotlib.text.Text object at 0x...>
     >>> plt.xlabel("Normalized frequency [cycles per sample]")
-    <matplotlib.text.Text object at 0x...>
-    >>> plt.axis('tight')
-    (-0.5, 0.5, -100.0, ...)
-    >>> plt.show()
 
     """
     # Docstring adapted from NumPy's hamming function
@@ -784,33 +732,20 @@ def kaiser(M, beta, sym=True):
 
     >>> window = scipy.signal.kaiser(51, 14)
     >>> plt.plot(window)
-    [<matplotlib.lines.Line2D object at 0x...>]
     >>> plt.title("Kaiser window")
-    <matplotlib.text.Text object at 0x...>
     >>> plt.ylabel("Amplitude")
-    <matplotlib.text.Text object at 0x...>
     >>> plt.xlabel("Sample")
-    <matplotlib.text.Text object at 0x...>
-    >>> plt.show()
 
     >>> plt.figure()
-    <matplotlib.figure.Figure object at 0x...>
     >>> A = fft(window, 2048) / 25.5
     >>> mag = np.abs(fftshift(A))
     >>> freq = np.linspace(-0.5, 0.5, len(A))
     >>> response = 20 * np.log10(mag)
     >>> response = np.clip(response, -100, 100)
     >>> plt.plot(freq, response)
-    [<matplotlib.lines.Line2D object at 0x...>]
     >>> plt.title("Frequency response of Kaiser window")
-    <matplotlib.text.Text object at 0x...>
     >>> plt.ylabel("Magnitude [dB]")
-    <matplotlib.text.Text object at 0x...>
     >>> plt.xlabel("Normalized frequency [cycles per sample]")
-    <matplotlib.text.Text object at 0x...>
-    >>> plt.axis('tight')
-    (-0.5, 0.5, -100.0, ...)
-    >>> plt.show()
 
     """
     # Docstring adapted from NumPy's kaiser function
