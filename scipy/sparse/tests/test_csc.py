3f4d1f511e49657d58bc51b6949be8fba431819d
Jake Vanderplas <vanderplas@astro.washington.edu>
2012-10-22 08:31:46 -0700

    TST: remove asserts and yields

--- scipy/sparse/tests/test_csc.py
+++ scipy/sparse/tests/test_csc.py
@@ -1,25 +1,9 @@
 import numpy as np
-from numpy.testing import assert_array_almost_equal, run_module_suite
+from numpy.testing import assert_array_almost_equal, run_module_suite, assert_
 from scipy.sparse import csr_matrix, csc_matrix
 
 
-def _check_csc_getrow(i, X, Xcsc):
-    arr_row = X[i:i + 1, :]
-    csc_row = Xcsc.getrow(i)
-
-    assert_array_almost_equal(arr_row, csc_row.toarray())
-    assert type(csc_row) is csr_matrix
-
-
-def _check_csc_getcol(i, X, Xcsc):
-    arr_col = X[:, i:i + 1]
-    csc_col = Xcsc.getcol(i)
-
-    assert_array_almost_equal(arr_col, csc_col.toarray())
-    assert type(csc_col) is csc_matrix
-
-
-def test_csc_getrow_getcol():
+def test_csc_getrow():
     N = 10
     np.random.seed(0)
     X = np.random.random((N, N))
@@ -27,8 +11,26 @@ def test_csc_getrow_getcol():
     Xcsc = csc_matrix(X)
 
     for i in range(N):
-        yield _check_csc_getrow, i, X, Xcsc
-        yield _check_csc_getcol, i, X, Xcsc
+        arr_row = X[i:i + 1, :]
+        csc_row = Xcsc.getrow(i)
+
+        assert_array_almost_equal(arr_row, csc_row.toarray())
+        assert_(type(csc_row) is csr_matrix)
+
+
+def test_csc_getcol():
+    N = 10
+    np.random.seed(0)
+    X = np.random.random((N, N))
+    X[X > 0.7] = 0
+    Xcsc = csc_matrix(X)
+
+    for i in range(N):
+        arr_col = X[:, i:i + 1]
+        csc_col = Xcsc.getcol(i)
+
+        assert_array_almost_equal(arr_col, csc_col.toarray())
+        assert_(type(csc_col) is csc_matrix)
 
 
 if __name__ == "__main__":
--- scipy/sparse/tests/test_csr.py
+++ scipy/sparse/tests/test_csr.py
@@ -1,5 +1,5 @@
 import numpy as np
-from numpy.testing import assert_array_almost_equal, run_module_suite
+from numpy.testing import assert_array_almost_equal, run_module_suite, assert_
 from scipy.sparse import csr_matrix
 
 
@@ -7,7 +7,7 @@ def _check_csr_rowslice(i, sl, X, Xcsr):
     np_slice = X[i, sl]
     csr_slice = Xcsr[i, sl]
     assert_array_almost_equal(np_slice, csr_slice.toarray()[0])
-    assert type(csr_slice) is csr_matrix
+    assert_(type(csr_slice) is csr_matrix)
 
 
 def test_csr_rowslice():
@@ -27,23 +27,7 @@ def test_csr_rowslice():
             yield _check_csr_rowslice, i, sl, X, Xcsr
 
 
-def _check_csr_getrow(i, X, Xcsr):
-    arr_row = X[i:i + 1, :]
-    csr_row = Xcsr.getrow(i)
-
-    assert_array_almost_equal(arr_row, csr_row.toarray())
-    assert type(csr_row) is csr_matrix
-
-
-def _check_csr_getcol(i, X, Xcsr):
-    arr_col = X[:, i:i + 1]
-    csr_col = Xcsr.getcol(i)
-
-    assert_array_almost_equal(arr_col, csr_col.toarray())
-    assert type(csr_col) is csr_matrix
-
-
-def test_csr_getrow_getcol():
+def test_csr_getrow():
     N = 10
     np.random.seed(0)
     X = np.random.random((N, N))
@@ -51,8 +35,27 @@ def test_csr_getrow_getcol():
     Xcsr = csr_matrix(X)
 
     for i in range(N):
-        yield _check_csr_getrow, i, X, Xcsr
-        yield _check_csr_getcol, i, X, Xcsr
+        arr_row = X[i:i + 1, :]
+        csr_row = Xcsr.getrow(i)
+
+        assert_array_almost_equal(arr_row, csr_row.toarray())
+        assert_(type(csr_row) is csr_matrix)
+
+
+def test_csr_getcol():
+    N = 10
+    np.random.seed(0)
+    X = np.random.random((N, N))
+    X[X > 0.7] = 0
+    Xcsr = csr_matrix(X)
+
+    for i in range(N):
+        arr_col = X[:, i:i + 1]
+        csr_col = Xcsr.getcol(i)
+
+        assert_array_almost_equal(arr_col, csr_col.toarray())
+        assert_(type(csr_col) is csr_matrix)
+
 
 
 if __name__ == "__main__":
