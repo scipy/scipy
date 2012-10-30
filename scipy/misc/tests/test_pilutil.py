BUG: fix pilutil.bytescale scaling behavior.  Closes #1748.
Thanks to kingson for the patch.

scipy/misc/tests/test_pilutil.py
@@ -39,10 +39,19 @@ class TestPILUtil(TestCase):
         assert_equal(im2.shape, (30,60))
 
     def test_bytescale(self):
-        x = np.array([0,1,2],np.uint8)
+        x = np.array([0,1,2], np.uint8)
         y = np.array([0,1,2])
-        assert_equal(misc.bytescale(x),x)
-        assert_equal(misc.bytescale(y),[0,127,255])
+        assert_equal(misc.bytescale(x), x)
+        assert_equal(misc.bytescale(y), [0,127,255])
+
+    def test_bytescale_keywords(self):
+        x = np.array([40, 60, 120, 200, 300, 500])
+        res_lowhigh = misc.bytescale(x, low=10, high=143)
+        assert_equal(res_lowhigh, [10, 16, 33, 56, 85, 143])
+        res_cmincmax = misc.bytescale(x, cmin=60, cmax=300)
+        assert_equal(res_cmincmax, [0, 0, 64, 149, 255, 255])
+
+        assert_equal(misc.bytescale(np.array([3, 3, 3]), low=4), [4, 4, 4])
 
 def tst_fromimage(filename, irange):
     fp = open(filename, "rb")