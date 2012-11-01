BUG: fix broadcasting in special.hyp0f1

scipy/special/basic.py
@@ -2,9 +2,10 @@
 # Author:  Travis Oliphant, 2002
 #
 
+import numpy as np
 from numpy import pi, asarray, floor, isscalar, iscomplex, real, imag, sqrt, \
         where, mgrid, cos, sin, exp, place, seterr, issubdtype, extract, \
-        complexfloating, less, vectorize, inexact, nan, zeros, sometrue
+        less, vectorize, inexact, nan, zeros, sometrue, atleast_1d
 from _ufuncs import ellipkm1, mathieu_a, mathieu_b, iv, jv, gamma, psi, zeta, \
         hankel1, hankel2, yv, kv, gammaln, ndtri
 import _ufuncs
@@ -469,14 +470,18 @@ def hyp0f1(a, z):
     It's also the limit as q -> infinity of ``1F1(q;a;z/q)``, and satisfies
     the differential equation :math:``f''(z) + af'(z) = f(z)`.
     """
-    a = asarray(a)
-    z = asarray(z)
+    a = atleast_1d(a)
+    z = atleast_1d(z)
+    a, z = np.broadcast_arrays(a, z)
     arg = 2 * sqrt(abs(z))
-    num = where(z >= 0, iv(a - 1, arg), jv(a - 1, arg))
+    old_err = np.seterr(all='ignore')  # for z=0, a<1 and num=inf, next lines
+    num = where(z.real >= 0, iv(a - 1, arg), jv(a - 1, arg))
     den = abs(z)**((a - 1.0) / 2)
     num *= gamma(a)
-    den[z == 0] = 1  # Avoid RuntimeWarning in next line
-    return where(z == 0, 1.0, num / den)
+    np.seterr(**old_err)
+    num[z == 0] = 1
+    den[z == 0] = 1
+    return num / den
 
 def assoc_laguerre(x,n,k=0.0):
     return orthogonal.eval_genlaguerre(n, k, x)

scipy/special/tests/test_basic.py
@@ -21,12 +21,14 @@
 #    test_sph_kn
 
 import numpy as np
-from numpy import array, isnan, r_, arange, finfo, pi, sin, cos, tan, exp, log, zeros, \
-        sqrt, asarray, inf, nan_to_num, real, arctan, float_
+from numpy import array, isnan, r_, arange, finfo, pi, sin, cos, tan, exp, \
+        log, zeros, sqrt, asarray, inf, nan_to_num, real, arctan, float_
 
-from numpy.testing import assert_equal, assert_almost_equal, assert_array_equal, \
-        assert_array_almost_equal, assert_approx_equal, assert_, \
-        rand, dec, TestCase, run_module_suite, assert_allclose
+from numpy.testing import assert_equal, assert_almost_equal, \
+        assert_array_equal, assert_array_almost_equal, assert_approx_equal, \
+        assert_, rand, dec, TestCase, run_module_suite, assert_allclose, \
+        assert_raises
+
 from scipy import special
 import scipy.special._ufuncs as cephes
 from scipy.special import _ufuncs_cxx
@@ -1205,6 +1207,10 @@ class TestHyper(TestCase):
         assert_almost_equal(h2,h2real,8)
 
     def test_hyp0f1(self):
+        # scalar input
+        assert_allclose(special.hyp0f1(2.5, 0.5), 1.21482702689997, rtol=1e-12)
+        assert_allclose(special.hyp0f1(2.5, 0), 1.0, rtol=1e-15)
+
         # float input, expected values match mpmath
         x = special.hyp0f1(3.0, [-1.5, -1, 0, 1, 1.5])
         expected = np.array([0.58493659229143, 0.70566805723127, 1.0,
@@ -1214,6 +1220,17 @@ class TestHyper(TestCase):
         # complex input
         x = special.hyp0f1(3.0, np.array([-1.5, -1, 0, 1, 1.5]) + 0.j)
         assert_allclose(x, expected.astype(np.complex), rtol=1e-12)
+
+        # test broadcasting
+        x1 = [0.5, 1.5, 2.5]
+        x2 = [0, 1, 0.5]
+        x = special.hyp0f1(x1, x2)
+        expected = [1.0, 1.8134302039235093, 1.21482702689997]
+        assert_allclose(x, expected, rtol=1e-12)
+        x = special.hyp0f1(np.row_stack([x1] * 2), x2)
+        assert_allclose(x, np.row_stack([expected] * 2), rtol=1e-12)
+        assert_raises(ValueError, special.hyp0f1,
+                      np.row_stack([x1] * 3), [0, 1])
 
     def test_hyp1f1(self):
         hyp1 = special.hyp1f1(.1,.1,.3)