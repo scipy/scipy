4a1d7d003aa45b330a7e64ea3b14c50602dc162f
Pauli Virtanen <pav@iki.fi>
2012-11-17 14:59:53 +0200

    MAINT: lib,lapack: remove atlas_version module
    This module is not used in Scipy, and is not listed as a part of the
    public API.

--- scipy/lib/lapack/setup.py
+++ scipy/lib/lapack/setup.py
@@ -95,18 +95,6 @@ def configuration(parent_package='',top_path=None):
                          extra_info = lapack_opt
                          )
 
-    # atlas_version:
-    if os.name == 'nt' and ('FPATH' in os.environ or 'MKLROOT' in os.environ):
-        define_macros = [('NO_ATLAS_INFO', 1)]
-    else:
-        define_macros = []
-
-    config.add_extension('atlas_version',
-                         sources = ['atlas_version.c'],
-                         extra_info = lapack_opt,
-                         define_macros = define_macros
-                         )
-
     config.add_data_dir('tests')
 
     return config
