c915f871fb21cd31bb06a2492bbacf95dd0cf277
François Boulogne <fboulogne sciunto org>
2012-11-30 18:29:43 +0100

    DOCSTRING: spline degree is k, not s

--- scipy/interpolate/fitpack2.py
+++ scipy/interpolate/fitpack2.py
@@ -111,7 +111,7 @@ class UnivariateSpline(object):
     >>> from scipy.interpolate import UnivariateSpline
     >>> x = linspace(-3, 3, 100)
     >>> y = exp(-x**2) + randn(100)/10
-    >>> s = UnivariateSpline(x, y, s=1)
+    >>> s = UnivariateSpline(x, y, k=1)
     >>> xs = linspace(-3, 3, 1000)
     >>> ys = s(xs)
 
