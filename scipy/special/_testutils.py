BUG: special: avoid raising overflow in test utilities

scipy/special/_testutils.py
@@ -191,13 +191,17 @@ class FuncData(object):
             nan_x = np.isnan(x)
             nan_y = np.isnan(y)
 
-            abs_y = np.absolute(y)
-            abs_y[~np.isfinite(abs_y)] = 0
-            diff = np.absolute(x - y)
-            diff[~np.isfinite(diff)] = 0
+            olderr = np.seterr(all='ignore')
+            try:
+                abs_y = np.absolute(y)
+                abs_y[~np.isfinite(abs_y)] = 0
+                diff = np.absolute(x - y)
+                diff[~np.isfinite(diff)] = 0
 
-            rdiff = diff / np.absolute(y)
-            rdiff[~np.isfinite(rdiff)] = 0
+                rdiff = diff / np.absolute(y)
+                rdiff[~np.isfinite(rdiff)] = 0
+            finally:
+                np.seterr(**olderr)
 
             tol_mask = (diff < atol + rtol*abs_y)
             pinf_mask = (pinf_x == pinf_y)