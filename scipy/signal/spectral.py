1194b6451709728c94f032e5839a9e745eca0ec4
Ralf Gommers <ralf.gommers@googlemail.com>
2012-12-26 23:40:13 +0100

    MAINT: signal.spectral: fix integer division and some formatting stuff.

--- scipy/signal/spectral.py
+++ scipy/signal/spectral.py
@@ -10,45 +10,38 @@ from _spectral import lombscargle
 __all__ = ['periodogram', 'welch', 'lombscargle']
 
 
-def periodogram(x, fs=1.0, window=None, nfft=None, detrend='constant', 
-        return_onesided=True, scaling='density', axis=-1):
+def periodogram(x, fs=1.0, window=None, nfft=None, detrend='constant',
+                return_onesided=True, scaling='density', axis=-1):
     """
-    Estimate power spectral density using a periodogram.  
+    Estimate power spectral density using a periodogram.
 
     Parameters
     ----------
     x : array_like
         Time series of measurement values
-        
-    fs : float, optional 
+    fs : float, optional
         Sampling frequency of the `x` time series in units of Hz. Defaults
         to 1.0.
-
     window : str or tuple or array_like, optional
         Desired window to use. See `get_window` for a list of windows and
-        required parameters. If `window` is an array it will be used 
+        required parameters. If `window` is an array it will be used
         directly as the window. Defaults to None; equivalent to 'boxcar'.
-
-    nfft : int, optional 
+    nfft : int, optional
         Length of the FFT used. If None the length of `x` will be used.
-
     detrend : str or function, optional
         Specifies how to detrend `x` prior to computing the spectrum. If
-        `detrend` is a string, it is passed as the `type` argument to
+        `detrend` is a string, it is passed as the ``type`` argument to
         `detrend`. If it is a function, it should return a detrended array.
-        Defaults to 'constant'
-
+        Defaults to 'constant'.
     return_onesided : bool, optional
         If True, return a one-sided spectrum for real data. If False return
         a two-sided spectrum. Note that for complex data, a two-sided
         spectrum is always returned.
-
-    scaling : { 'density', 'spectrum' }, optional 
+    scaling : { 'density', 'spectrum' }, optional
         Selects between computing the power spectral density ('density')
         where `Pxx` has units of V**2/Hz if `x` is measured in V and computing
         the power spectrum ('spectrum') where `Pxx` has units of V**2 if `x` is
         measured in V. Defaults to 'density'
-
     axis : int, optional
         Axis along which the periodogram is computed; the default is over
         the last axis (i.e. ``axis=-1``).
@@ -56,14 +49,13 @@ def periodogram(x, fs=1.0, window=None, nfft=None, detrend='constant',
     Returns
     -------
     f : ndarray
-        Array of sample frequencies. 
-
+        Array of sample frequencies.
     Pxx : ndarray
         Power spectral density or power spectrum of `x`.
 
     See Also
     --------
-    welch: Estimate power spectral density using Welch's method 
+    welch: Estimate power spectral density using Welch's method
     lombscargle: Lomb-Scargle periodogram for unevenly sampled data
 
     Examples
@@ -78,7 +70,7 @@ def periodogram(x, fs=1.0, window=None, nfft=None, detrend='constant',
     >>> N = 1e5
     >>> amp = 2*np.sqrt(2)
     >>> freq = 1234.0
-    >>> noise_power = 0.001 * fs / 2 
+    >>> noise_power = 0.001 * fs / 2
     >>> time = np.arange(N) / fs
     >>> x = amp*np.sin(2*np.pi*freq*time)
     >>> x += np.random.normal(scale=np.sqrt(noise_power), size=time.shape)
@@ -92,7 +84,7 @@ def periodogram(x, fs=1.0, window=None, nfft=None, detrend='constant',
     >>> plt.show()
 
     If we average the last half of the spectral density, to exclude the
-    peak, we can recover the noise power on the signal. 
+    peak, we can recover the noise power on the signal.
 
     >>> np.mean(Pxx_den[256:])
     0.0009924865443739191
@@ -109,11 +101,12 @@ def periodogram(x, fs=1.0, window=None, nfft=None, detrend='constant',
     The peak height in the power spectrum is an estimate of the RMS amplitude.
 
     >>> np.sqrt(Pxx_spec.max())
-    2.0077340678640727 
+    2.0077340678640727
+
     """
     if window is None:
         window = 'boxcar'
-    
+
     if nfft is None:
         nperseg = x.shape[axis]
     elif nfft == x.shape[axis]:
@@ -128,15 +121,15 @@ def periodogram(x, fs=1.0, window=None, nfft=None, detrend='constant',
         nfft = None
 
     return welch(x, fs, window, nperseg, 0, nfft, detrend, return_onesided,
-            scaling, axis)
+                 scaling, axis)
 
 
 def welch(x, fs=1.0, window='hanning', nperseg=256, noverlap=None, nfft=None,
-        detrend='constant', return_onesided=True, scaling='density', axis=-1):
+          detrend='constant', return_onesided=True, scaling='density', axis=-1):
     """
-    Estimate power spectral density using Welch's method.  
+    Estimate power spectral density using Welch's method.
 
-    Welch's method [1]_ computes an estimate of the power spectral desnsity
+    Welch's method [1]_ computes an estimate of the power spectral density
     by dividing the data into overlapping segments, computing a modified
     periodogram for each segment and averaging the periodograms.
 
@@ -144,45 +137,36 @@ def welch(x, fs=1.0, window='hanning', nperseg=256, noverlap=None, nfft=None,
     ----------
     x : array_like
         Time series of measurement values
-        
-    fs : float, optional 
+    fs : float, optional
         Sampling frequency of the `x` time series in units of Hz. Defaults
         to 1.0.
-
    window : str or tuple or array_like, optional
         Desired window to use. See `get_window` for a list of windows and
         required parameters. If `window` is array_like it will be used
         directly as the window and its length will be used for nperseg.
         Defaults to 'hanning'.
-
     nperseg : int, optional
-        Length of each segment.  Defaults to 256. 
-
-    noverlap: int, optional 
-        Number of points to overlap between segments. If None, `noverlap`
-        = nperseg / 2.  Defaults to None.
-
+        Length of each segment.  Defaults to 256.
+    noverlap: int, optional
+        Number of points to overlap between segments. If None,
+        ``noverlap = nperseg / 2``.  Defaults to None.
     nfft : int, optional
-        Length of the FFT used, if a zero padded FFT is desired.  If None, 
-        the FFT length is nperseg. Defaults to None.
-
+        Length of the FFT used, if a zero padded FFT is desired.  If None,
+        the FFT length is `nperseg`. Defaults to None.
     detrend : str or function, optional
         Specifies how to detrend each segment. If `detrend` is a string,
-        it is passed as the `type` argument to `detrend`. If it is a
+        it is passed as the ``type`` argument to `detrend`. If it is a
         function, it takes a segment and returns a detrended segment.
-        Defaults to 'constant'
-
+        Defaults to 'constant'.
     return_onesided : bool, optional
         If True, return a one-sided spectrum for real data. If False return
         a two-sided spectrum. Note that for complex data, a two-sided
         spectrum is always returned.
-
-    scaling : { 'density', 'spectrum' }, optional 
+    scaling : { 'density', 'spectrum' }, optional
         Selects between computing the power spectral density ('density')
         where Pxx has units of V**2/Hz if x is measured in V and computing
         the power spectrum ('spectrum') where Pxx has units of V**2 if x is
-        measured in V. Defaults to 'density'
-
+        measured in V. Defaults to 'density'.
     axis : int, optional
         Axis along which the periodogram is computed; the default is over
         the last axis (i.e. ``axis=-1``).
@@ -190,8 +174,7 @@ def welch(x, fs=1.0, window='hanning', nperseg=256, noverlap=None, nfft=None,
     Returns
     -------
     f : ndarray
-        Array of sample frequencies. 
-
+        Array of sample frequencies.
     Pxx : ndarray
         Power spectral density or power spectrum of x.
 
@@ -205,18 +188,18 @@ def welch(x, fs=1.0, window='hanning', nperseg=256, noverlap=None, nfft=None,
     An appropriate amount of overlap will depend on the choice of window
     and on your requirements.  For the default 'hanning' window an
     overlap of 50% is a reasonable trade off between accurately estimating
-    the signal power, while not over counting any of the data.  Narrower 
+    the signal power, while not over counting any of the data.  Narrower
     windows may require a larger overlap.
 
     If `noverlap` is 0, this method is equivalent to Bartlett's method [2]_.
 
     References
     ----------
-    .. [1] Welch, P. "The use of the fast Fourier transform for the
+    .. [1] P. Welch, "The use of the fast Fourier transform for the
            estimation of power spectra: A method based on time averaging
-           over short, modified periodograms", IEEE Trans. Audio 
+           over short, modified periodograms", IEEE Trans. Audio
            Electroacoust. vol. 15, pp. 70-73, 1967.
-    .. [2] Bartlett, M. S. "Periodogram Analysis and Continuous Spectra",
+    .. [2] M.S. Bartlett, "Periodogram Analysis and Continuous Spectra",
            Biometrika, vol. 37, pp. 1-16, 1950.
 
     Examples
@@ -231,7 +214,7 @@ def welch(x, fs=1.0, window='hanning', nperseg=256, noverlap=None, nfft=None,
     >>> N = 1e5
     >>> amp = 2*np.sqrt(2)
     >>> freq = 1234.0
-    >>> noise_power = 0.001 * fs / 2 
+    >>> noise_power = 0.001 * fs / 2
     >>> time = np.arange(N) / fs
     >>> x = amp*np.sin(2*np.pi*freq*time)
     >>> x += np.random.normal(scale=np.sqrt(noise_power), size=time.shape)
@@ -245,7 +228,7 @@ def welch(x, fs=1.0, window='hanning', nperseg=256, noverlap=None, nfft=None,
     >>> plt.show()
 
     If we average the last half of the spectral density, to exclude the
-    peak, we can recover the noise power on the signal. 
+    peak, we can recover the noise power on the signal.
 
     >>> np.mean(Pxx_den[256:])
     0.0009924865443739191
@@ -262,7 +245,8 @@ def welch(x, fs=1.0, window='hanning', nperseg=256, noverlap=None, nfft=None,
     The peak height in the power spectrum is an estimate of the RMS amplitude.
 
     >>> np.sqrt(Pxx_spec.max())
-    2.0077340678640727 
+    2.0077340678640727
+
     """
     x = np.asarray(x)
 
@@ -295,8 +279,8 @@ def welch(x, fs=1.0, window='hanning', nperseg=256, noverlap=None, nfft=None,
     if not hasattr(detrend, '__call__'):
         detrend_func = lambda seg: signaltools.detrend(seg, type=detrend)
     elif axis != -1:
-    # Wrap this function so that it receives a shape that it could reasonably
-    # expect to receive.
+        # Wrap this function so that it receives a shape that it could
+        # reasonably expect to receive.
         def detrend_func(seg):
             seg = np.rollaxis(seg, -1, axis)
             seg = detrend(seg)
@@ -310,7 +294,7 @@ def welch(x, fs=1.0, window='hanning', nperseg=256, noverlap=None, nfft=None,
     if np.isrealobj(x) and return_onesided:
         outshape = list(x.shape)
         if nfft % 2 == 0: # even
-            outshape[-1] = nfft/2+1
+            outshape[-1] = nfft // 2 + 1
             Pxx = np.empty(outshape, x.dtype)
             for k, ind in enumerate(indices):
                 x_dt = detrend_func(x[..., ind:ind+nperseg])
@@ -319,17 +303,17 @@ def welch(x, fs=1.0, window='hanning', nperseg=256, noverlap=None, nfft=None,
                 # as real values, packed r r i r i r i ...
                 # this indexing is to extract the matching real and imaginary
                 # parts, while also handling the pure real zero and nyquist
-                # frequencies.  
+                # frequencies.
                 if k == 0:
                     Pxx[..., (0,-1)] = xft[..., (0,-1)]**2
                     Pxx[..., 1:-1] = xft[..., 1:-1:2]**2 + xft[..., 2::2]**2
                 else:
                     Pxx *= k/(k+1.0)
                     Pxx[..., (0,-1)] += xft[..., (0,-1)]**2 / (k+1.0)
-                    Pxx[..., 1:-1] += (xft[..., 1:-1:2]**2 + xft[..., 2::2]**2)\
+                    Pxx[..., 1:-1] += (xft[..., 1:-1:2]**2 + xft[..., 2::2]**2) \
                                     / (k+1.0)
         else: # odd
-            outshape[-1] = (nfft+1)/2
+            outshape[-1] = (nfft+1) // 2
             Pxx = np.empty(outshape, x.dtype)
             for k, ind in enumerate(indices):
                 x_dt = detrend_func(x[..., ind:ind+nperseg])
@@ -342,9 +326,10 @@ def welch(x, fs=1.0, window='hanning', nperseg=256, noverlap=None, nfft=None,
                     Pxx[..., 0] += xft[..., 0]**2 / (k+1)
                     Pxx[..., 1:] += (xft[..., 1::2]**2 + xft[..., 2::2]**2) \
                                   / (k+1.0)
+
         Pxx[..., 1:-1] *= 2*scale
         Pxx[..., (0,-1)] *= scale
-        f = np.arange(Pxx.shape[-1])*(fs/nfft)
+        f = np.arange(Pxx.shape[-1]) * (fs/nfft)
     else:
         for k, ind in enumerate(indices):
             x_dt = detrend_func(x[..., ind:ind+nperseg])
@@ -359,5 +344,6 @@ def welch(x, fs=1.0, window='hanning', nperseg=256, noverlap=None, nfft=None,
 
     if axis != -1:
         Pxx = np.rollaxis(Pxx, -1, axis)
+
     return f, Pxx
 
