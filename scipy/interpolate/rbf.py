5131c12ae6ced34645d172f5a89e9e7aac52452f
Pauli Virtanen <pav@iki.fi>
2013-01-05 17:59:44 +0200

    PY23: interpolate/rbf: fix function code handling + callable()

--- scipy/interpolate/rbf.py
+++ scipy/interpolate/rbf.py
@@ -47,7 +47,8 @@ import sys
 from numpy import (sqrt, log, asarray, newaxis, all, dot, exp, eye,
                    float_)
 from scipy import linalg
-import collections
+from scipy.lib.six import callable, get_method_function, \
+     get_function_code
 
 __all__ = ['Rbf']
 
@@ -145,20 +146,20 @@ class Rbf(object):
                 raise ValueError("function must be a callable or one of " +
                                      ", ".join(functionlist))
             self._function = getattr(self, "_h_"+self.function)
-        elif isinstance(self.function, collections.Callable):
+        elif callable(self.function):
             allow_one = False
             if hasattr(self.function, 'func_code') or \
                    hasattr(self.function, '__code__'):
                 val = self.function
                 allow_one = True
             elif hasattr(self.function, "im_func"):
-                val = self.function.__func__
+                val = get_method_function(self.function)
             elif hasattr(self.function, "__call__"):
-                val = self.function.__call__.__func__
+                val = get_method_function(self.function.__call__)
             else:
                 raise ValueError("Cannot determine number of arguments to function")
 
-            argcount = val.__code__.co_argcount
+            argcount = get_function_code(val).co_argcount
             if allow_one and argcount == 1:
                 self._function = self.function
             elif argcount == 2:
@@ -197,7 +198,7 @@ class Rbf(object):
         # attach anything left in kwargs to self
         #  for use by any user-callable function or
         #  to save on the object returned.
-        for item, value in list(kwargs.items()):
+        for item, value in kwargs.items():
             setattr(self, item, value)
 
         self.A = self._init_function(r) - eye(self.N)*self.smooth
