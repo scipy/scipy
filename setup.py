f19c889767e3e607590f01f908e61ce013c0e38a
Pauli Virtanen <pav@iki.fi>
2013-02-09 14:31:03 +0200

    BLD: fix the setup.py build_sphinx command
    This is perhaps the easiest way to build the documentation, so it's good
    to have it working.

--- setup.py
+++ setup.py
@@ -133,6 +133,20 @@ if not release:
     finally:
         a.close()
 
+try:
+    from sphinx.setup_command import BuildDoc
+    HAVE_SPHINX = True
+except ImportError:
+    HAVE_SPHINX = False
+
+if HAVE_SPHINX:
+    class ScipyBuildDoc(BuildDoc):
+        """Run in-place build before Sphinx doc build"""
+        def run(self):
+            ret = subprocess.call([sys.executable, sys.argv[0], 'build_ext', '-i'])
+            if ret != 0:
+                raise RuntimeError("Building Scipy failed!")
+            BuildDoc.run(self)
 
 def generate_cython():
     cwd = os.path.dirname(__file__)
@@ -162,11 +176,15 @@ def configuration(parent_package='',top_path=None):
 
     return config
 
-
 def setup_package():
 
     # Rewrite the version file everytime
     write_version_py()
+
+    if HAVE_SPHINX:
+        cmdclass = {'build_sphinx': ScipyBuildDoc}
+    else:
+        cmdclass = {}
 
     metadata = dict(
         name = 'scipy',
@@ -177,6 +195,7 @@ def setup_package():
         url = "http://www.scipy.org",
         download_url = "http://sourceforge.net/project/showfiles.php?group_id=27747&package_id=19531",
         license = 'BSD',
+        cmdclass=cmdclass,
         classifiers=[_f for _f in CLASSIFIERS.split('\n') if _f],
         platforms = ["Windows", "Linux", "Solaris", "Mac OS-X", "Unix"],
     )
