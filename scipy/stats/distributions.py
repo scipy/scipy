57a1f0c9f094d08777c3a02192e4b98396275383
pbrod <per.andreas.brodtkorb@gmail.com>
2013-03-13 01:12:31 +0100

    Restored nnlf function and added _penalized_nnlf

--- scipy/stats/distributions.py
+++ scipy/stats/distributions.py
@@ -1692,7 +1692,27 @@ class rv_continuous(rv_generic):
         x = asarray((x-loc) / scale)
         cond0 = (x <= self.a) | (self.b <= x )
         if (any(cond0)):
-            # old call: return inf
+            return inf
+        else:
+            N = len(x)
+            return self._nnlf(x, *args) + N * log(scale)
+        
+    def _penalized_nnlf(self, theta, x):
+        ''' Return negative loglikelihood function, 
+        i.e., - sum (log pdf(x, theta),axis=0)
+           where theta are the parameters (including loc and scale)
+        '''
+        try:
+            loc = theta[-2]
+            scale = theta[-1]
+            args = tuple(theta[:-2])
+        except IndexError:
+            raise ValueError("Not enough input arguments.")
+        if not self._argcheck(*args) or scale <= 0:
+            return inf
+        x = asarray((x-loc) / scale)
+        cond0 = (x <= self.a) | (self.b <= x )
+        if (any(cond0)):
             goodargs = argsreduce(1 - cond0, *((x,)))
             goodargs = tuple(goodargs + list(args))
             N = len(x)
@@ -1701,7 +1721,7 @@ class rv_continuous(rv_generic):
             return self._nnlf(*goodargs) + N * log(scale) + Nbad * 100.0 * log(xmax)
         else:
             N = len(x)
-            return self._nnlf(x, *args) + N*log(scale)
+            return self._nnlf(x, *args) + N * log(scale)
 
     # return starting point for fit (shape arguments + loc + scale)
     def _fitstart(self, data, args=None):
@@ -1726,7 +1746,7 @@ class rv_continuous(rv_generic):
                 x0.append(args[n])
 
         if len(fixedn) == 0:
-            func = self.nnlf
+            func = self._penalized_nnlf
             restore = None
         else:
             if len(fixedn) == len(index):
@@ -1744,7 +1764,7 @@ class rv_continuous(rv_generic):
 
             def func(theta, x):
                 newtheta = restore(args[:], theta)
-                return self.nnlf(newtheta, x)
+                return self._penalized_nnlf(newtheta, x)
 
         return x0, func, restore, args
 
