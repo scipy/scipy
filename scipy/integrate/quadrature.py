d89356631f49a504e90f39350a0373b7c344381e
Andrew Nelson <anz@d121131.ncnr.nist.gov>
2013-04-24 12:30:22 -0400

    ENH: integrate: added the startiter optional parameter to scipy.integrate.quadrature, specifying the initial order for Gaussian quadrature.

--- scipy/integrate/quadrature.py
+++ scipy/integrate/quadrature.py
@@ -115,7 +115,7 @@ def vectorize1(func, args=(), vec_func=False):
 
 
 def quadrature(func, a, b, args=(), tol=1.49e-8, rtol=1.49e-8, maxiter=50,
-               vec_func=True):
+               vec_func=True, startiter=1):
     """
     Compute a definite integral using fixed-tolerance Gaussian quadrature.
 
@@ -136,10 +136,12 @@ def quadrature(func, a, b, args=(), tol=1.49e-8, rtol=1.49e-8, maxiter=50,
         Iteration stops when error between last two iterates is less than
         `tol` OR the relative change is less than `rtol`.
     maxiter : int, optional
-        Maximum number of iterations.
+        Maximum order of Gaussian quadrature.
     vec_func : bool, optional
         True or False if func handles arrays as arguments (is
         a "vector" function). Default is True.
+    startiter : int, optional
+        Starting order of Gaussian quadrature.
 
     Returns
     -------
@@ -165,7 +167,7 @@ def quadrature(func, a, b, args=(), tol=1.49e-8, rtol=1.49e-8, maxiter=50,
     vfunc = vectorize1(func, args, vec_func=vec_func)
     val = np.inf
     err = np.inf
-    for n in xrange(1, maxiter+1):
+    for n in xrange(startiter, maxiter+1):
         newval = fixed_quad(vfunc, a, b, (), n)[0]
         err = abs(newval-val)
         val = newval
