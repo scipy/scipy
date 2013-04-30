3acbbd1a8f7bb9cb7aae8a3568874ed6756949c1
Daniel Smith <smithd.daniel.br@gmail.com>
2013-04-30 14:17:31 -0400

    Added a test that caught a bug in CSC/CSR. Details in master branch.

--- scipy/sparse/tests/test_base.py
+++ scipy/sparse/tests/test_base.py
@@ -1283,7 +1283,7 @@ class _TestGetSet:
             for j in range(-N, N):
                 assert_equal(A[i,j], D[i,j])
 
-        for ij in [(0,3),(-1,3),(4,0),(4,3),(4,-1)]:
+        for ij in [(0,3),(-1,3),(4,0),(4,3),(4,-1), (1, 2, 3)]:
             assert_raises(IndexError, A.__getitem__, ij)
 
     def test_setelement(self):
