12dd8005d7ed1256772329e84231218ef8bbb0df
alex <argriffi@ncsu.edu>
2013-05-11 21:15:37 -0400

    TST: do not import expm ecplicitly

--- scipy/linalg/tests/test_matfuncs.py
+++ scipy/linalg/tests/test_matfuncs.py
@@ -17,7 +17,7 @@ from numpy.testing import (TestCase, run_module_suite,
         assert_allclose, decorators)
 
 import scipy.linalg
-from scipy.linalg import signm, logm, sqrtm, expm, expm_frechet
+from scipy.linalg import signm, logm, sqrtm, expm_frechet
 from scipy.linalg.matfuncs import expm2, expm3
 import scipy.linalg._expm_frechet
 
