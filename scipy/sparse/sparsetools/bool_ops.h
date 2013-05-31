3b8d0f4c622b10d1c01e0ee7314238af543d7685
Blake Griffith <blake.a.griffith@gmail.com>
2013-05-30 21:46:23 -0500

    ENH: Added bool_ops.h file which wraps npy_bool data to handle bool operations.

---
+++ scipy/sparse/sparsetools/bool_ops.h
@@ -0,0 +1,13 @@
+#ifndef BOOL_OPS_H
+#define BOOL_OPS_H
+
+/*
+ * Functions to handle arithmetic operations on NumPy Bool values.
+ */
+
+#include <numpy/arrayobject.h>
+#include <iostream>
+
+typedef npy_int8 npy_bool_wrapper;
+
+#endif
