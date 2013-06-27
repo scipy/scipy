be0b21bec036fe46be1f0dcb56d2e18296acce46
Blake Griffith <blake.a.griffith@gmail.com>
2013-06-26 19:41:25 -0500

    ENH: new bool_ops.h

--- scipy/sparse/sparsetools/bool_ops.h
+++ scipy/sparse/sparsetools/bool_ops.h
@@ -7,6 +7,42 @@
 
 #include <numpy/arrayobject.h>
 
-typedef npy_int8 npy_bool_wrapper;
+class npy_bool_wrapper {
+    public:
+        char value;
+        
+        /* operators */
+        operator char() const {
+            if(value != 0) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+        npy_bool_wrapper& operator=(const npy_bool_wrapper& x) {
+            value = x;
+            return (*this);
+        }
+        npy_bool_wrapper operator+(const npy_bool_wrapper& x) {
+            return x || value ? 1 : 0;
+        }
+        /* inplace operators */
+        npy_bool_wrapper operator+=(const npy_bool_wrapper& x) {
+            value = x || value ? 1 : 0;
+            return (*this);
+        }
+        npy_bool_wrapper operator*=(const npy_bool_wrapper& x) {
+            value = value && x ? 1 : 0;
+            return (*this);
+        }
+        /* constructors */
+        npy_bool_wrapper() { 
+            value = 0; 
+        }
+        template <class T>
+        npy_bool_wrapper(T x) {
+            value = x ? 1 : 0;
+        }
+};
 
 #endif
