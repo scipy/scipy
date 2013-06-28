1eb15a942910167581d957e648fbf24b400eba1b
Blake Griffith <blake.a.griffith@gmail.com>
2013-06-28 15:11:51 -0500

    ENH: Added macro to check at compile time that np_bool_wrapper is the correct size.

--- scipy/sparse/sparsetools/bool_ops.h
+++ scipy/sparse/sparsetools/bool_ops.h
@@ -1,11 +1,15 @@
-#ifndef BOOL_OPS_H
-#define BOOL_OPS_H
-
 /*
  * Functions to handle arithmetic operations on NumPy Bool values.
  */
-
 #include <numpy/arrayobject.h>
+#include <assert.h>
+
+/*
+ * A compiler time (ct) assert macro from 
+ * http://www.pixelbeat.org/programming/gcc/static_assert.html
+ * This is used to assure that npy_bool_wrapper is the right size.
+ */
+#define ct_assert(e) extern char (*ct_assert(void)) [sizeof(char[1 - 2*!(e)])]
 
 class npy_bool_wrapper {
     public:
@@ -44,5 +48,7 @@ class npy_bool_wrapper {
             value = x ? 1 : 0;
         }
 };
+
+ct_assert(sizeof(char) == sizeof(npy_bool_wrapper));
 
 #endif
