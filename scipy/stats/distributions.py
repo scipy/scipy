91c4aa2043eb14bf466b26be837233bd70ce934d
Zhenya <evgeni@burovski.me>
2013-07-10 15:23:19 +0100

    MAINT: Lift the stats method to rv_generic.
    Changed betwen rv_continuous and rv_discrete were:
    * rv_c had guards for nan**1.5; kept.
    * rv_c had a shortcut for no valid arguments; kept.

--- scipy/stats/distributions.py
+++ scipy/stats/distributions.py
@@ -711,6 +711,118 @@ class rv_generic(object):
         return vals
 
 
+    def stats(self, *args, **kwds):
+        """
+        Some statistics of the given RV
+
+        Parameters
+        ----------
+        arg1, arg2, arg3,... : array_like
+            The shape parameter(s) for the distribution (see docstring of the
+            instance object for more information)
+        loc : array_like, optional
+            location parameter (default=0)
+        scale : array_like, optional (discrete RVs only)
+            scale parameter (default=1)
+        moments : str, optional
+            composed of letters ['mvsk'] defining which moments to compute:
+            'm' = mean,
+            'v' = variance,
+            's' = (Fisher's) skew,
+            'k' = (Fisher's) kurtosis.
+            (default='mv')
+
+        Returns
+        -------
+        stats : sequence
+            of requested moments.
+
+        """
+        args, loc, scale, moments = self._parse_args_stats(*args, **kwds)
+        # scale = 1 by construction for discrete RVs
+        loc, scale = map(asarray, (loc, scale))
+        args = tuple(map(asarray, args))
+        cond = self._argcheck(*args) & (scale > 0) & (loc == loc)
+
+        signature = inspect.getargspec(get_method_function(self._stats))
+        if (signature[2] is not None) or ('moments' in signature[0]):
+            mu, mu2, g1, g2 = self._stats(*args, **{'moments': moments})
+        else:
+            mu, mu2, g1, g2 = self._stats(*args)
+        if g1 is None:
+            mu3 = None
+        else:
+            mu3 = g1*np.power(mu2, 1.5)  # (mu2**1.5) breaks down for nan and inf
+        default = valarray(shape(cond), self.badvalue)
+        output = []
+
+        # Use only entries that are valid in calculation
+        if any(cond):
+            goodargs = argsreduce(cond, *(args + (scale, loc)))
+            scale, loc, goodargs = goodargs[-2], goodargs[-1], goodargs[:-2]
+            if 'm' in moments:
+                if mu is None:
+                    mu = self._munp(1.0, *goodargs)
+                out0 = default.copy()
+                place(out0, cond, mu*scale + loc)
+                output.append(out0)
+
+            if 'v' in moments:
+                if mu2 is None:
+                    mu2p = self._munp(2.0, *goodargs)
+                    if mu is None:
+                        mu = self._munp(1.0, *goodargs)
+                    mu2 = mu2p - mu*mu
+                    if np.isinf(mu):
+                        #if mean is inf then var is also inf
+                        mu2 = np.inf
+                out0 = default.copy()
+                place(out0, cond, mu2*scale*scale)
+                output.append(out0)
+
+            if 's' in moments:
+                if g1 is None:
+                    mu3p = self._munp(3.0, *goodargs)
+                    if mu is None:
+                        mu = self._munp(1.0, *goodargs)
+                    if mu2 is None:
+                        mu2p = self._munp(2.0, *goodargs)
+                        mu2 = mu2p - mu*mu
+                    mu3 = mu3p - 3*mu*mu2 - mu**3
+                    g1 = mu3 / np.power(mu2, 1.5) #mu2**1.5
+                out0 = default.copy()
+                place(out0, cond, g1)
+                output.append(out0)
+
+            if 'k' in moments:
+                if g2 is None:
+                    mu4p = self._munp(4.0, *goodargs)
+                    if mu is None:
+                        mu = self._munp(1.0, *goodargs)
+                    if mu2 is None:
+                        mu2p = self._munp(2.0, *goodargs)
+                        mu2 = mu2p - mu*mu
+                    if mu3 is None:
+                        mu3p = self._munp(3.0, *goodargs)
+                        mu3 = mu3p - 3*mu*mu2 - mu**3
+                    mu4 = mu4p - 4*mu*mu3 - 6*mu*mu*mu2 - mu**4
+                    g2 = mu4 / mu2**2.0 - 3.0
+                out0 = default.copy()
+                place(out0,cond, g2)
+                output.append(out0)
+        else:  # no valid args
+            output = []
+            for _ in moments:
+                out0 = default.copy()
+                output.append(out0)
+
+        if len(output) == 1:
+            return output[0]
+        else:
+            return tuple(output)
+
+
+
     def entropy(self, *args, **kwds):
         """
         Differential entropy of the RV.
@@ -1658,114 +1770,6 @@ class rv_continuous(rv_generic):
             return output[()]
         return output
 
-    def stats(self,*args,**kwds):
-        """
-        Some statistics of the given RV
-
-        Parameters
-        ----------
-        arg1, arg2, arg3,... : array_like
-            The shape parameter(s) for the distribution (see docstring of the
-            instance object for more information)
-        loc : array_like, optional
-            location parameter (default=0)
-        scale : array_like, optional
-            scale parameter (default=1)
-        moments : str, optional
-            composed of letters ['mvsk'] defining which moments to compute:
-            'm' = mean,
-            'v' = variance,
-            's' = (Fisher's) skew,
-            'k' = (Fisher's) kurtosis.
-            (default='mv')
-
-        Returns
-        -------
-        stats : sequence
-            of requested moments.
-
-        """
-        args, loc, scale, moments = self._parse_args_stats(*args, **kwds)
-        loc, scale = map(asarray, (loc, scale))
-        args = tuple(map(asarray, args))
-        cond = self._argcheck(*args) & (scale > 0) & (loc == loc)
-
-        signature = inspect.getargspec(get_method_function(self._stats))
-        if (signature[2] is not None) or ('moments' in signature[0]):
-            mu, mu2, g1, g2 = self._stats(*args,**{'moments':moments})
-        else:
-            mu, mu2, g1, g2 = self._stats(*args)
-        if g1 is None:
-            mu3 = None
-        else:
-            mu3 = g1*np.power(mu2,1.5)  # (mu2**1.5) breaks down for nan and inf
-        default = valarray(shape(cond), self.badvalue)
-        output = []
-
-        # Use only entries that are valid in calculation
-        if any(cond):
-            goodargs = argsreduce(cond, *(args+(scale,loc)))
-            scale, loc, goodargs = goodargs[-2], goodargs[-1], goodargs[:-2]
-            if 'm' in moments:
-                if mu is None:
-                    mu = self._munp(1.0,*goodargs)
-                out0 = default.copy()
-                place(out0,cond,mu*scale+loc)
-                output.append(out0)
-
-            if 'v' in moments:
-                if mu2 is None:
-                    mu2p = self._munp(2.0,*goodargs)
-                    if mu is None:
-                        mu = self._munp(1.0,*goodargs)
-                    mu2 = mu2p - mu*mu
-                    if np.isinf(mu):
-                        #if mean is inf then var is also inf
-                        mu2 = np.inf
-                out0 = default.copy()
-                place(out0,cond,mu2*scale*scale)
-                output.append(out0)
-
-            if 's' in moments:
-                if g1 is None:
-                    mu3p = self._munp(3.0,*goodargs)
-                    if mu is None:
-                        mu = self._munp(1.0,*goodargs)
-                    if mu2 is None:
-                        mu2p = self._munp(2.0,*goodargs)
-                        mu2 = mu2p - mu*mu
-                    mu3 = mu3p - 3*mu*mu2 - mu**3
-                    g1 = mu3 / mu2**1.5
-                out0 = default.copy()
-                place(out0,cond,g1)
-                output.append(out0)
-
-            if 'k' in moments:
-                if g2 is None:
-                    mu4p = self._munp(4.0,*goodargs)
-                    if mu is None:
-                        mu = self._munp(1.0,*goodargs)
-                    if mu2 is None:
-                        mu2p = self._munp(2.0,*goodargs)
-                        mu2 = mu2p - mu*mu
-                    if mu3 is None:
-                        mu3p = self._munp(3.0,*goodargs)
-                        mu3 = mu3p - 3*mu*mu2 - mu**3
-                    mu4 = mu4p - 4*mu*mu3 - 6*mu*mu*mu2 - mu**4
-                    g2 = mu4 / mu2**2.0 - 3.0
-                out0 = default.copy()
-                place(out0,cond,g2)
-                output.append(out0)
-        else:  # no valid args
-            output = []
-            for _ in moments:
-                out0 = default.copy()
-                output.append(out0)
-
-        if len(output) == 1:
-            return output[0]
-        else:
-            return tuple(output)
 
 
     def _nnlf(self, x, *args):
@@ -6610,111 +6614,6 @@ class rv_discrete(rv_generic):
             return output[()]
         return output
 
-    def stats(self, *args, **kwds):
-        """
-        Some statistics of the given discrete RV.
-
-        Parameters
-        ----------
-        arg1, arg2, arg3,... : array_like
-            The shape parameter(s) for the distribution (see docstring of the
-            instance object for more information).
-        loc : array_like, optional
-            Location parameter (default=0).
-        moments : string, optional
-            Composed of letters ['mvsk'] defining which moments to compute:
-
-              - 'm' = mean,
-              - 'v' = variance,
-              - 's' = (Fisher's) skew,
-              - 'k' = (Fisher's) kurtosis.
-
-            The default is'mv'.
-
-        Returns
-        -------
-        stats : sequence
-            of requested moments.
-
-        """
-        try:
-            kwds["moments"] = kwds.pop("moment") # test suite is full of these; a feature?
-        except KeyError:
-            pass
-        args, loc, _, moments = self._parse_args_stats(*args, **kwds)
-        loc = asarray(loc)
-        args = tuple(map(asarray,args))
-        cond = self._argcheck(*args) & (loc == loc)
-
-        signature = inspect.getargspec(get_method_function(self._stats))
-        if (signature[2] is not None) or ('moments' in signature[0]):
-            mu, mu2, g1, g2 = self._stats(*args,**{'moments':moments})
-        else:
-            mu, mu2, g1, g2 = self._stats(*args)
-        if g1 is None:
-            mu3 = None
-        else:
-            mu3 = g1*(mu2**1.5)
-        default = valarray(shape(cond), self.badvalue)
-        output = []
-
-        # Use only entries that are valid in calculation
-        goodargs = argsreduce(cond, *(args+(loc,)))
-        loc, goodargs = goodargs[-1], goodargs[:-1]
-
-        if 'm' in moments:
-            if mu is None:
-                mu = self._munp(1.0,*goodargs)
-            out0 = default.copy()
-            place(out0,cond,mu+loc)
-            output.append(out0)
-
-        if 'v' in moments:
-            if mu2 is None:
-                mu2p = self._munp(2.0,*goodargs)
-                if mu is None:
-                    mu = self._munp(1.0,*goodargs)
-                mu2 = mu2p - mu*mu
-            out0 = default.copy()
-            place(out0,cond,mu2)
-            output.append(out0)
-
-        if 's' in moments:
-            if g1 is None:
-                mu3p = self._munp(3.0,*goodargs)
-                if mu is None:
-                    mu = self._munp(1.0,*goodargs)
-                if mu2 is None:
-                    mu2p = self._munp(2.0,*goodargs)
-                    mu2 = mu2p - mu*mu
-                mu3 = mu3p - 3*mu*mu2 - mu**3
-                g1 = mu3 / mu2**1.5
-            out0 = default.copy()
-            place(out0,cond,g1)
-            output.append(out0)
-
-        if 'k' in moments:
-            if g2 is None:
-                mu4p = self._munp(4.0,*goodargs)
-                if mu is None:
-                    mu = self._munp(1.0,*goodargs)
-                if mu2 is None:
-                    mu2p = self._munp(2.0,*goodargs)
-                    mu2 = mu2p - mu*mu
-                if mu3 is None:
-                    mu3p = self._munp(3.0,*goodargs)
-                    mu3 = mu3p - 3*mu*mu2 - mu**3
-                mu4 = mu4p - 4*mu*mu3 - 6*mu*mu*mu2 - mu**4
-                g2 = mu4 / mu2**2.0 - 3.0
-            out0 = default.copy()
-            place(out0,cond,g2)
-            output.append(out0)
-
-        if len(output) == 1:
-            return output[0]
-        else:
-            return tuple(output)
-
 
     def _entropy(self, *args):
         if hasattr(self,'pk'):
@@ -7594,4 +7493,3 @@ class skellam_gen(rv_discrete):
         g2 = 1 / var
         return mean, var, g1, g2
 skellam = skellam_gen(a=-np.inf, name="skellam", longname='A Skellam')
-
