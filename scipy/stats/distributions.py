ef8ff31dec29016ac5abe2831642e5c72eac9a5f
Samuel Drapeau <samuel.drapeau@gmail.com>
2013-07-23 00:13:42 +0200

    ENH: add Normal Inverse Gaussian (nig) distribution to scipy.stats (distributions.py).
    The Normal Inverse Gaussian distribution gains quite a lot of momentum in the last years as a fat tail distribution (finance, Levy processes, extreme values, etc. see http://en.wikipedia.org/wiki/Normal-inverse_Gaussian_distribution)
    
    I am mathematician and newbee in Python. Out of curiosity I coded with scipy a method to compute risk measures with nig distribution as base. Therefore I added the nig ppf to the library of continuous distribution functions in distributions.py. This is not difficult and I copied the code used for other distributions. However, being new to python, this may be not optimal.
    
    I crossed checked with Matlab and R and so far it matches the result obtained there.
    
    I am still thinking about a method to compute the cdf as well as the ppf since there are no close form for them and the performance are very poor in comparison with the equivalent in R or Matlab (This is central for our applications).
    
    Being new to python, git and all the stuff I hope that I did the things at least halfway right.
    
    ~

--- scipy/stats/distributions.py
+++ scipy/stats/distributions.py
@@ -5102,6 +5102,52 @@ class nct_gen(rv_continuous):
 nct = nct_gen(name="nct")
 
 
+class nig_gen(rv_continuous):
+    """A Normal Inverse Gaussian continuous random variable.
+
+    %(before_notes)s
+
+    Notes
+    -----
+    The probability density for a Normal Inverse Gaussian distribution is
+        nig.pdf(x, m, a, b, d)=
+            (a*d/pi) * kv(1,a*np.sqrt(d**2+(x-m)**2))*
+            exp(d*sqrt(a**2-b**2)-b*(m-x))/
+            sqrt(d**2+(x-m)**2)
+    where kv is a Bessel function of second order.
+    m is mean, a is tail heaviness, b is asymmetry parameter
+    and d is scale parameter (see Wikipedia)
+
+    %(example)s
+
+    """
+    def _argcheck(self, mu, a, b, d):
+        return (a > 0) & (d > 0) & (np.absolute(b) < a)
+
+    def _pdf(self, x, mu, a, b, d):
+        return (a * d / pi) * \
+            special.kv(1, a * sqrt(d ** 2 + (x - mu) ** 2)) * \
+            exp(d * sqrt(a ** 2 - b ** 2) - b * (mu - x)) / \
+            sqrt(d ** 2 + (x - mu) ** 2)
+
+    def _logpdf(self, x, mu, a, b, d):
+        return log((a * d / pi)) + \
+            log(special.kv(1, a * sqrt(d ** 2 + (x - mu) ** 2))) + \
+            d * sqrt(a ** 2 - b ** 2) - b * (mu - x) - \
+            log(sqrt(d ** 2 + (x - mu) ** 2))
+
+    def _stats(self, mu, a, b, d):
+        gamma = sqrt(a ** 2 - b ** 2)
+        mean = mu + d * b / gamma
+        variance = d * a ** 2 / gamma ** 3
+        skewness = 3 * b / (a * sqrt(d * gamma))
+        kurtosis = 3 * (1 + 4 * b ** 2 / a ** 2) / (d * gamma)
+        return mean, variance, skewness, kurtosis
+
+
+nig = nig_gen(shapes="m, a, b, d")
+
+
 class pareto_gen(rv_continuous):
     """A Pareto continuous random variable.
 
