575b5f1b72aac3f1d7028b25fc2a745339920931
Samuel Drapeau <samuel.drapeau@gmail.com>
2013-07-23 15:15:29 +0200

    ENH Normal Inverse Gaussian in scipy.stats
    File distribution.py
    
    According to the feedback I changed
    1- removed spaces around ** (pep8 refuse that I remove \ for end of
    lines)
    2- removed m (or mu) as this is redundant with loc. Correct stats
    accordingly
    3- removed the redundant definition of logpdf and replace with
    log(self._pdf(...))
    
    Still open:
    a) name (nig, norminv_gauss, norm_inv_gauss ?)
    b) inclusion of init tests (I don't know which parameters I should
    provide for for the distribution in the distribution list in
    test_continuous_basic.py
    
    General Problem
    The computation of cdf and ppf is pretty slow in comparison to matlab or
    R.
    From my numerous tests, it seems that this has to do with the performance of the numerical integration

--- scipy/stats/distributions.py
+++ scipy/stats/distributions.py
@@ -5110,42 +5110,37 @@ class nig_gen(rv_continuous):
     Notes
     -----
     The probability density for a Normal Inverse Gaussian distribution is
-        nig.pdf(x, m, a, b, d)=
-            (a*d/pi) * kv(1,a*np.sqrt(d**2+(x-m)**2))*
-            exp(d*sqrt(a**2-b**2)-b*(m-x))/
-            sqrt(d**2+(x-m)**2)
+        nig.pdf(x, a, b, d)=
+            (a*d/pi) * kv(1,a*np.sqrt(d**2+x**2))*
+            exp(d*sqrt(a**2-b**2)+b*x)/
+            sqrt(d**2+x**2)
     where kv is a Bessel function of second order.
-    m is mean, a is tail heaviness, b is asymmetry parameter
+    a is tail heaviness, b is asymmetry parameter
     and d is scale parameter (see Wikipedia)
 
     %(example)s
 
     """
-    def _argcheck(self, mu, a, b, d):
+    def _argcheck(self, a, b, d):
         return (a > 0) & (d > 0) & (np.absolute(b) < a)
 
-    def _pdf(self, x, mu, a, b, d):
+    def _pdf(self, x, a, b, d):
         return (a * d / pi) * \
-            special.kv(1, a * sqrt(d ** 2 + (x - mu) ** 2)) * \
-            exp(d * sqrt(a ** 2 - b ** 2) - b * (mu - x)) / \
-            sqrt(d ** 2 + (x - mu) ** 2)
+            special.kv(1, a * sqrt(d**2 + x**2)) *\
+            exp(d * sqrt(a**2 - b**2) + b * x) /\
+            sqrt(d**2 + x**2)
 
-    def _logpdf(self, x, mu, a, b, d):
-        return log((a * d / pi)) + \
-            log(special.kv(1, a * sqrt(d ** 2 + (x - mu) ** 2))) + \
-            d * sqrt(a ** 2 - b ** 2) - b * (mu - x) - \
-            log(sqrt(d ** 2 + (x - mu) ** 2))
+    def _logpdf(self, x, a, b, d):
+        return log(self._pdf(x, a, b, d))
 
-    def _stats(self, mu, a, b, d):
-        gamma = sqrt(a ** 2 - b ** 2)
-        mean = mu + d * b / gamma
-        variance = d * a ** 2 / gamma ** 3
+    def _stats(self, a, b, d):
+        gamma = sqrt(a**2 - b**2)
+        mean = d * b / gamma
+        variance = d * a**2 / gamma**3
         skewness = 3 * b / (a * sqrt(d * gamma))
-        kurtosis = 3 * (1 + 4 * b ** 2 / a ** 2) / (d * gamma)
+        kurtosis = 3 * (1 + 4 * b**2 / a**2) / (d * gamma)
         return mean, variance, skewness, kurtosis
-
-
-nig = nig_gen(shapes="m, a, b, d")
+nig = nig_gen(shapes="a, b, d")
 
 
 class pareto_gen(rv_continuous):
