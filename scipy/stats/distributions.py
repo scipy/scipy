d0b724c0a4a0aa71178d4ee7189ba50d993ee012
Zhenya <evgeni@burovski.me>
2013-08-21 09:56:48 +0200

    BUG: np.inf**2 fails on python2.6; replace by np.inf*np.inf

--- scipy/stats/distributions.py
+++ scipy/stats/distributions.py
@@ -384,14 +384,14 @@ def _moment_from_stats(n, mu, mu2, g1, g2, moment_func, args):
             val = moment_func(3,*args)
         else:
             mu3 = g1 * np.power(mu2, 1.5)  # 3rd central moment
-            val = mu3+3*mu*mu2+mu**3  # 3rd non-central moment
+            val = mu3+3*mu*mu2+mu*mu*mu  # 3rd non-central moment
     elif (n == 4):
         if g1 is None or g2 is None or mu2 is None or mu is None:
             val = moment_func(4,*args)
         else:
             mu4 = (g2+3.0)*(mu2**2.0)  # 4th central moment
             mu3 = g1*np.power(mu2, 1.5)  # 3rd central moment
-            val = mu4+4*mu*mu3+6*mu*mu*mu2+mu**4
+            val = mu4+4*mu*mu3+6*mu*mu*mu2+mu*mu*mu*mu
     else:
         val = moment_func(n, *args)
 
