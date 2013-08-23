2b12f3fec7095d43ab95cc69279e184172f89144
Ralf Gommers <ralf.gommers@googlemail.com>
2013-08-23 14:10:39 +0200

    REL: set version to 0.13.0b2-dev

--- setup.py
+++ setup.py
@@ -48,8 +48,8 @@ Operating System :: MacOS
 MAJOR               = 0
 MINOR               = 13
 MICRO               = 0
-ISRELEASED          = True
-VERSION             = '%d.%d.%db1' % (MAJOR, MINOR, MICRO)
+ISRELEASED          = False
+VERSION             = '%d.%d.%db2' % (MAJOR, MINOR, MICRO)
 
 
 # Return the git revision as a string
