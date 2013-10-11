04471888e06c1ecb9ed11b508dc706418ef4bd64
Pauli Virtanen <pav@iki.fi>
2013-10-11 22:35:56 +0300

    CLN: sparse: clean up code in _expokit.expm a bit

--- scipy/linalg/_expokit.py
+++ scipy/linalg/_expokit.py
@@ -8,7 +8,7 @@ import numpy as np
 from scipy.linalg._expokit_f import dgpadm, zgpadm, dspadm, zhpadm, \
                                     dgexpv, zgexpv
 from scipy.sparse.base import isspmatrix
-from scipy.sparse.linalg import LinearOperator
+from scipy.sparse.linalg import aslinearoperator, LinearOperator
 
 
 def expm(A, t=None):
@@ -38,48 +38,44 @@ def expm(A, t=None):
     # dgpadm does an internal check for square matrices,
     #  so assume ldh == m
     # constant input variables
-    if hasattr( A, 'shape' ):
-        m   = A.shape[0]
-        #ldh = A.shape[1] 
-        dtype = A.dtype
-    else:
-        m   = len(A)
-        #ldh = len(A[0])
-        dtype = type( A[0][0] )
-    #if t is None:
-    #    t = np.array([1.])[0]
+
+    is_sparse_op = (isspmatrix(A) or isinstance(A, LinearOperator))
+
+    if not is_sparse_op:
+        A = np.asarray(A)
+
+    m   = A.shape[0]
+    dtype = A.dtype
 
     # output integers needed to get result:
     iexph = np.array([0])
     ns    = np.array([0])
     iflag = np.array([0])
-    if isspmatrix(A):
-        # Sparse matrix routines.
-        matvec = lambda v : A.dot(v)
-        # Please check usage of LinearOperator:-
-        Av = LinearOperator( (m, m), matvec=matvec)
+
+    if is_sparse_op:
+        # Krylov estimation
+        Av = aslinearoperator(A)
         itrace = np.array([0])
         tol = np.array([1e-7])
         v = np.ones(m, dtype=dtype)
-        anorm = np.linalg.norm(A.todense(), np.inf) 
-        if dtype in ('float64', 'float32', float,):
+        anorm = np.linalg.norm(A.todense(), np.inf)
+
+        if np.issubdtype(dtype, np.complexfloating):
+            w, wsp = zgexpv(v * 1 + 1j, tol, anorm, Av.matvec, itrace, iflag, t=t) 
+        else:
             # See src/expokit.f for documentation
             w, wsp = dgexpv(v, tol, anorm, Av.matvec, itrace, iflag, t=t)
-        elif dtype in ('complex128', 'complex64', complex):
-            w, wsp = zgexpv(v * 1 + 1j, tol, anorm, Av.matvec, itrace, iflag, t=t) 
-        #print('\nA {0}: {1}'.format(A.shape, type(A)))
-        #print('w {0}: {1}'.format(w.shape, type(w)))
+
         return np.reshape(w, (m,m), order='F')
-        #return wsp[m*(m+1)+m+(m+2)**2:]
-        #return np.reshape(wsp[m*(m+1)+m+(m+2)**2:], (m,m), order='F')
     else:
         iexph = np.array([0])
         ns = np.array([0])
-        if dtype in ('float64', 'float32', float,):
-            wsp = dgpadm(A, iexph, ns, iflag, t)
-        elif dtype in ('complex128', 'complex64', complex):
+        if np.issubdtype(dtype, np.complexfloating):
             wsp = zgpadm(A, iexph, ns, iflag, t=t)
-    if iflag[0] != 0:
-        raise IOError("Expokit error ({0}) in routine {1}".format(iflag[0]))
-    return np.reshape(wsp[iexph[0]-1 : iexph[0] + m * m - 1], (m,m), order='F')
+        else:
+            wsp = dgpadm(A, iexph, ns, iflag, t)
 
+    if iflag[0] != 0:
+        raise IOError("Expokit error ({0})".format(iflag[0]))
+
+    return np.reshape(wsp[iexph[0]-1 : iexph[0] + m * m - 1], (m, m), order='F')
