13163813cb8373413949a20cfc273d50333a56c2
Zhenya <evgeni@burovski.me>
2013-10-17 11:21:27 +0100

    TST: interpolate: refactor tests common for PPoly and BPoly

--- scipy/interpolate/tests/test_interpolate.py
+++ scipy/interpolate/tests/test_interpolate.py
@@ -404,6 +404,81 @@ class TestLagrange(TestCase):
         assert_array_almost_equal(p.coeffs,pl.coeffs)
 
 
+class TestPPolyCommn(TestCase):
+    # test basic functionality for PPoly and BPoly
+    def test_sort_check(self):
+        c = np.array([[1, 4], [2, 5], [3, 6]])
+        x = np.array([0, 1, 0.5])
+        assert_raises(ValueError, PPoly, c, x)
+        assert_raises(ValueError, BPoly, c, x)
+
+    def test_extend(self):
+        # Test adding new points to the piecewise polynomial
+        np.random.seed(1234)
+
+        order = 3
+        x = np.unique(np.r_[0, 10 * np.random.rand(30), 10])
+        c = 2*np.random.rand(order+1, len(x)-1, 2, 3) - 1
+
+        for cls in (PPoly, BPoly):
+            pp = cls(c[:,:9], x[:10])
+            pp.extend(c[:,9:], x[10:])
+
+            pp2 = cls(c[:,10:], x[10:])
+            pp2.extend(c[:,:10], x[:10], right=False)
+
+            pp3 = cls(c, x)
+
+            assert_array_equal(pp.c, pp3.c)
+            assert_array_equal(pp.x, pp3.x)
+            assert_array_equal(pp2.c, pp3.c)
+            assert_array_equal(pp2.x, pp3.x)
+
+    def test_extend_diff_orders(self):
+        # Test extending polynomial with different order one
+        np.random.seed(1234)
+
+        x = np.linspace(0, 1, 6)
+        c = np.random.rand(2, 5)
+
+        x2 = np.linspace(1, 2, 6)
+        c2 = np.random.rand(4, 5)
+
+        for cls in (PPoly, BPoly):
+            pp1 = cls(c, x)
+            pp2 = cls(c2, x2)
+
+            pp_comb = cls(c, x)
+            pp_comb.extend(c2, x2[1:])
+
+            # NB. doesn't match to pp1 at the endpoint, because pp1 is not
+            #     continuous with pp2 as we took random coefs.
+            xi1 = np.linspace(0, 1, 300, endpoint=False)
+            xi2 = np.linspace(1, 2, 300)
+
+            assert_allclose(pp1(xi1), pp_comb(xi1))
+            assert_allclose(pp2(xi2), pp_comb(xi2))
+
+    def test_shape(self):
+        np.random.seed(1234)
+        c = np.random.rand(8, 12, 5, 6, 7)
+        x = np.sort(np.random.rand(13))
+        xp = np.random.rand(3, 4)
+        for cls in (PPoly, BPoly):
+            p = cls(c, x)
+            assert_equal(p(xp).shape, (3, 4, 5, 6, 7))
+
+        # 'scalars'
+        for cls in (PPoly, BPoly):
+            p = cls(c[..., 0, 0, 0], x)
+
+            assert_equal(np.shape(p(0.5)), ())
+            assert_equal(np.shape(p(np.array(0.5))), ())
+
+            # can't use dtype=object
+            assert_raises(ValueError, p, np.array([[0.1, 0.2], [0.4]]))
+
+
 class TestPPoly(TestCase):
     def test_simple(self):
         c = np.array([[1, 4], [2, 5], [3, 6]])
@@ -436,11 +511,6 @@ class TestPPoly(TestCase):
         assert_allclose(p(0.3), 1*0.3**2 + 2*0.3 + 3)
         assert_allclose(p(0.7), 4*(0.7-0.5)**2 + 5*(0.7-0.5) + 6)
 
-    def test_sort_check(self):
-        c = np.array([[1, 4], [2, 5], [3, 6]])
-        x = np.array([0, 1, 0.5])
-        assert_raises(ValueError, PPoly, c, x)
-
     def test_vs_alternative_implementations(self):
         np.random.seed(1234)
         c = np.random.rand(3, 12, 22)
@@ -454,14 +524,6 @@ class TestPPoly(TestCase):
 
         expected = _ppoly_eval_2(c[:,:,0], x, xp)
         assert_allclose(p(xp)[:,0], expected)
-
-    def test_shape(self):
-        np.random.seed(1234)
-        c = np.random.rand(3, 12, 5, 6, 7)
-        x = np.sort(np.random.rand(13))
-        p = PPoly(c, x)
-        xp = np.random.rand(3, 4)
-        assert_equal(p(xp).shape, (3, 4, 5, 6, 7))
 
     def test_from_spline(self):
         np.random.seed(1234)
@@ -696,50 +758,6 @@ class TestPPoly(TestCase):
             res = res[~np.isnan(res)]
             assert_allclose(res, 0, atol=1e-10)
 
-    def test_extend(self):
-        # Test adding new points to the piecewise polynomial
-        np.random.seed(1234)
-
-        order = 3
-        x = np.unique(np.r_[0, 10 * np.random.rand(30), 10])
-        c = 2*np.random.rand(order+1, len(x)-1, 2, 3) - 1
-
-        pp = PPoly(c[:,:9], x[:10])
-        pp.extend(c[:,9:], x[10:])
-
-        pp2 = PPoly(c[:,10:], x[10:])
-        pp2.extend(c[:,:10], x[:10], right=False)
-
-        pp3 = PPoly(c, x)
-
-        assert_array_equal(pp.c, pp3.c)
-        assert_array_equal(pp.x, pp3.x)
-        assert_array_equal(pp2.c, pp3.c)
-        assert_array_equal(pp2.x, pp3.x)
-
-    def test_extend_diff_orders(self):
-        # Test extending polynomial with different order one
-        np.random.seed(1234)
-
-        x = np.linspace(0, 1, 6)
-        c = np.random.rand(2, 5)
-
-        x2 = np.linspace(1, 2, 6)
-        c2 = np.random.rand(4, 5)
-
-        pp1 = PPoly(c, x)
-        pp2 = PPoly(c2, x2)
-
-        pp_comb = PPoly(c, x)
-        pp_comb.extend(c2, x2[1:])
-
-        # NB. doesn't match to pp1 at the endpoint, because pp1 is not
-        #     continuous with pp2 as we took random coefs.
-        xi1 = np.linspace(0, 1, 300, endpoint=False)
-        xi2 = np.linspace(1, 2, 300)
-
-        assert_allclose(pp1(xi1), pp_comb(xi1))
-        assert_allclose(pp2(xi2), pp_comb(xi2))
 
     def test_extrapolate_attr(self):
         # [ 1 - x**2 ]
@@ -764,7 +782,6 @@ class TestPPoly(TestCase):
 
 
 class TestBPoly(TestCase):
-
     def test_simple(self):
         x = [0, 1]
         c = [[3]]
@@ -837,7 +854,7 @@ class TestBPoly(TestCase):
         c = [[3], [1], [4]]
         bp = BPoly(c, x)
 
-        for extrapolate in [True, False, None]:
+        for extrapolate in (True, False, None):
             bp = BPoly(c, x, extrapolate=extrapolate)
             bp_d = bp.derivative()
             if extrapolate is False:
@@ -849,7 +866,6 @@ class TestBPoly(TestCase):
 
 
 class TestBPolyCalculus(TestCase):
-
     def test_derivative(self):
         x = [0, 1, 3]
         c = [[3, 0], [0, 0], [0, 2]]
