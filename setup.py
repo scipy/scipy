5720ec5cb2467a34930c773ebd94b0d6e1410df4
Ralf Gommers <ralf.gommers@googlemail.com>
2013-10-19 11:43:08 +0200

    DOC: finalize 0.13.0 release notes.

--- setup.py
+++ setup.py
@@ -48,8 +48,8 @@ Operating System :: MacOS
 MAJOR               = 0
 MINOR               = 13
 MICRO               = 0
-ISRELEASED          = True
-VERSION             = '%d.%d.%drc1' % (MAJOR, MINOR, MICRO)
+ISRELEASED          = False
+VERSION             = '%d.%d.%drc2' % (MAJOR, MINOR, MICRO)
 
 
 # Return the git revision as a string
