2080cb717e341f9a2ea9d72397ba0167b80919bd
Warren Weckesser <warren.weckesser@gmail.com>
2013-12-16 15:40:25 -0500

    MAINT: special: Fix whitespace in cephes/pdtr.c

--- scipy/special/cephes/pdtr.c
+++ scipy/special/cephes/pdtr.c
@@ -38,9 +38,9 @@
  * See igamc().
  *
  */
-/*							pdtrc()
+/*  pdtrc()
  *
- *	Complemented poisson distribution
+ *  Complemented poisson distribution
  *
  *
  *
@@ -78,9 +78,9 @@
  * See igam.c.
  *
  */
-/*							pdtri()
+/*  pdtri()
  *
- *	Inverse Poisson distribution
+ *  Inverse Poisson distribution
  *
  *
  *
@@ -119,7 +119,7 @@
  *                     k < 0
  *
  */
-
+
 /*
  * Cephes Math Library Release 2.3:  March, 1995
  * Copyright 1984, 1987, 1995 by Stephen L. Moshier
@@ -134,13 +134,12 @@ double m;
     double v;
 
     if ((k < 0) || (m <= 0.0)) {
-	mtherr("pdtrc", DOMAIN);
-	return (NPY_NAN);
+        mtherr("pdtrc", DOMAIN);
+        return (NPY_NAN);
     }
     v = k + 1;
     return (igam(v, m));
 }
-
 
 
 double pdtr(k, m)
@@ -150,8 +149,8 @@ double m;
     double v;
 
     if ((k < 0) || (m <= 0.0)) {
-	mtherr("pdtr", DOMAIN);
-	return (NPY_NAN);
+        mtherr("pdtr", DOMAIN);
+        return (NPY_NAN);
     }
     v = k + 1;
     return (igamc(v, m));
@@ -165,8 +164,8 @@ double y;
     double v;
 
     if ((k < 0) || (y < 0.0) || (y >= 1.0)) {
-	mtherr("pdtri", DOMAIN);
-	return (NPY_NAN);
+        mtherr("pdtri", DOMAIN);
+        return (NPY_NAN);
     }
     v = k + 1;
     v = igami(v, y);
