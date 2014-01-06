9354de9555512c01f5003cd551d3a85a21e477de
Jörg Dietrich <astro@joergdietrich.com>
2014-01-06 14:40:02 +0100

    API: Speed up and implement both version for discrete distributions for k-sample Anderson-Darling test
    1. Change call signature to have array of arrays and optional keyword to
       specify which version of the k-sample AD test should be computed.
    
    2. Get rid of all inner loops and list comprehensions by using
       np.searchsorted.
    
    3. Both version given by Scholz & Stephens for discrete sample can be
       computed now.

--- scipy/stats/tests/test_morestats.py
+++ scipy/stats/tests/test_morestats.py
@@ -84,7 +84,7 @@ class TestAnderson(TestCase):
 
 
 class TestAndersonKSamp(TestCase):
-    def test_example1(self):
+    def test_example1a(self):
         # Example data from Scholz & Stephens (1987), originally
         # published in Lehmann (1995, Nonparametrics, Statistical
         # Methods Based on Ranks, p. 309)
@@ -93,14 +93,30 @@ class TestAndersonKSamp(TestCase):
         t2 = np.array([39.2, 39.3, 39.7, 41.4, 41.8, 42.9, 43.3, 45.8])
         t3 = np.array([34.0, 35.0, 39.0, 40.0, 43.0, 43.0, 44.0, 45.0])
         t4 = np.array([34.0, 34.8, 34.8, 35.4, 37.2, 37.8, 41.2, 42.8])
-        Tk, tm, p = assert_warns(UserWarning, stats.anderson_ksamp, t1, t2,
-                                 t3, t4)
+        Tk, tm, p = assert_warns(UserWarning, stats.anderson_ksamp, (t1, t2,
+                                 t3, t4), discrete=True)
         assert_almost_equal(Tk, 4.449, 3)
         assert_array_almost_equal([0.4985, 1.3237, 1.9158, 2.4930, 3.2459],
                                   tm, 4)
         assert_almost_equal(p, 0.0021, 4)
 
-    def test_example2(self):
+    def test_example1b(self):
+        # Example data from Scholz & Stephens (1987), originally
+        # published in Lehmann (1995, Nonparametrics, Statistical
+        # Methods Based on Ranks, p. 309)
+        # Pass arrays
+        t1 = np.array([38.7, 41.5, 43.8, 44.5, 45.5, 46.0, 47.7, 58.0])
+        t2 = np.array([39.2, 39.3, 39.7, 41.4, 41.8, 42.9, 43.3, 45.8])
+        t3 = np.array([34.0, 35.0, 39.0, 40.0, 43.0, 43.0, 44.0, 45.0])
+        t4 = np.array([34.0, 34.8, 34.8, 35.4, 37.2, 37.8, 41.2, 42.8])
+        Tk, tm, p = assert_warns(UserWarning, stats.anderson_ksamp, (t1, t2,
+                                 t3, t4), discrete=False)
+        assert_almost_equal(Tk, 4.480, 3)
+        assert_array_almost_equal([0.4985, 1.3237, 1.9158, 2.4930, 3.2459],
+                                  tm, 4)
+        assert_almost_equal(p, 0.0020, 4)
+
+    def test_example2a(self):
         # Example data taken from an earlier technical report of
         # Scholz and Stephens
         # Pass lists instead of arrays
@@ -123,10 +139,40 @@ class TestAndersonKSamp(TestCase):
         t13 = [487, 18, 100, 7, 98, 5, 85, 91, 43, 230, 3, 130]
         t14 = [102, 209, 14, 57, 54, 32, 67, 59, 134, 152, 27, 14, 230, 66,
                61, 34]
-        Tk, tm, p = assert_warns(UserWarning, stats.anderson_ksamp, t1, t2, t3,
-                                 t4, t5, t6, t7, t8, t9, t10, t11, t12, t13,
-                                 t14)
+        Tk, tm, p = assert_warns(UserWarning, stats.anderson_ksamp,
+                                 (t1, t2, t3, t4, t5, t6, t7, t8, t9, t10,
+                                  t11, t12, t13, t14), discrete=True)
         assert_almost_equal(Tk, 3.288, 3)
+        assert_array_almost_equal([0.5990, 1.3269, 1.8052, 2.2486, 2.8009],
+                                  tm, 4)
+        assert_almost_equal(p, 0.0041, 4)
+
+    def test_example2b(self):
+        # Example data taken from an earlier technical report of
+        # Scholz and Stephens
+        t1 = [194, 15, 41, 29, 33, 181]
+        t2 = [413, 14, 58, 37, 100, 65, 9, 169, 447, 184, 36, 201, 118]
+        t3 = [34, 31, 18, 18, 67, 57, 62, 7, 22, 34]
+        t4 = [90, 10, 60, 186, 61, 49, 14, 24, 56, 20, 79, 84, 44, 59, 29,
+              118, 25, 156, 310, 76, 26, 44, 23, 62]
+        t5 = [130, 208, 70, 101, 208]
+        t6 = [74, 57, 48, 29, 502, 12, 70, 21, 29, 386, 59, 27]
+        t7 = [55, 320, 56, 104, 220, 239, 47, 246, 176, 182, 33]
+        t8 = [23, 261, 87, 7, 120, 14, 62, 47, 225, 71, 246, 21, 42, 20, 5,
+              12, 120, 11, 3, 14, 71, 11, 14, 11, 16, 90, 1, 16, 52, 95]
+        t9 = [97, 51, 11, 4, 141, 18, 142, 68, 77, 80, 1, 16, 106, 206, 82,
+              54, 31, 216, 46, 111, 39, 63, 18, 191, 18, 163, 24]
+        t10 = [50, 44, 102, 72, 22, 39, 3, 15, 197, 188, 79, 88, 46, 5, 5, 36,
+               22, 139, 210, 97, 30, 23, 13, 14]
+        t11 = [359, 9, 12, 270, 603, 3, 104, 2, 438]
+        t12 = [50, 254, 5, 283, 35, 12]
+        t13 = [487, 18, 100, 7, 98, 5, 85, 91, 43, 230, 3, 130]
+        t14 = [102, 209, 14, 57, 54, 32, 67, 59, 134, 152, 27, 14, 230, 66,
+               61, 34]
+        Tk, tm, p = assert_warns(UserWarning, stats.anderson_ksamp,
+                                 (t1, t2, t3, t4, t5, t6, t7, t8, t9, t10,
+                                  t11, t12, t13, t14), discrete=False)
+        assert_almost_equal(Tk, 3.294, 3)
         assert_array_almost_equal([0.5990, 1.3269, 1.8052, 2.2486, 2.8009],
                                   tm, 4)
         assert_almost_equal(p, 0.0041, 4)
@@ -135,10 +181,11 @@ class TestAndersonKSamp(TestCase):
         assert_raises(ValueError, stats.anderson_ksamp, np.ones(5))
 
     def test_no_distinct_observations(self):
-        assert_raises(ValueError, stats.anderson_ksamp, np.ones(5), np.ones(5))
+        assert_raises(ValueError, stats.anderson_ksamp,
+                      (np.ones(5), np.ones(5)))
 
     def test_empty_sample(self):
-        assert_raises(ValueError, stats.anderson_ksamp, np.ones(5), [])
+        assert_raises(ValueError, stats.anderson_ksamp, (np.ones(5), []))
 
 
 class TestAnsari(TestCase):
