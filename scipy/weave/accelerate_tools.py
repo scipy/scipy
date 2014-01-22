scipy/scipy

commit 021e0ee1564a5798a8f25edb131d6a2ea4375712

ENH: weave: replace md5 with SHA-256
use more secure hash algorithm -- NIST approved SHA-256
update documentation related to this change

scipy/weave/accelerate_tools.py
@@ -12,7 +12,7 @@ from __future__ import absolute_import, print_function
 
 from types import InstanceType, XRangeType
 import inspect
-import scipy.weave.md5_load as md5
+from hashlib import sha256
 import scipy.weave as weave
 from numpy.testing import assert_
 
@@ -364,7 +364,7 @@ class accelerate(object):
         return fast
 
     def identifier(self,signature):
-        # Build an MD5 checksum
+        # Build a SHA-256 checksum
         f = self.function
         co = f.func_code
         identifier = str(signature) + \
@@ -372,7 +372,7 @@ class accelerate(object):
                      str(co.co_consts) + \
                      str(co.co_varnames) + \
                      co.co_code
-        return 'F'+md5.md5(identifier).hexdigest()
+        return 'F' + sha256(identifier).hexdigest()
 
     def accelerate(self,signature,identifier):
         P = Python2CXX(self.function,signature,name=identifier)

scipy/weave/build_tools.py
@@ -235,7 +235,7 @@ def build_extension(module_path,compiler_name='',build_dir=None,
     # dag. We keep having to add directories to the path to keep
     # object files separated from each other.  gcc2.x and gcc3.x C++
     # object files are not compatible, so we'll stick them in a sub
-    # dir based on their version.  This will add an md5 check sum
+    # dir based on their version. This will add a SHA-256 check sum
     # of the compiler binary to the directory name to keep objects
     # from different compilers in different locations.
 

scipy/weave/catalog.py
@@ -85,13 +85,13 @@ def getmodule(object):
 def expr_to_filename(expr):
     """ Convert an arbitrary expr string to a valid file name.
 
-        The name is based on the md5 check sum for the string and
+        The name is based on the SHA-256 check sum for the string and
         Something that was a little more human readable would be
         nice, but the computer doesn't seem to care.
     """
-    import scipy.weave.md5_load as md5
+    from hashlib import sha256
     base = 'sc_'
-    return base + md5.new(expr).hexdigest()
+    return base + sha256(expr).hexdigest()
 
 
 def unique_file(d,expr):

scipy/weave/platform_info.py
@@ -96,13 +96,13 @@ def compiler_exe_path(exe_name):
 
 
 def check_sum(file):
-    import scipy.weave.md5_load as md5
+    from hashlib import sha256
     try:
         f = open(file,'r')
         bytes = f.read(-1)
     except IOError:
         bytes = ''
-    chk_sum = md5.md5(bytes)
+    chk_sum = sha256(bytes)
     return chk_sum.hexdigest()
 
 

scipy/weave/md5_load.py
@@ -1,11 +1,0 @@
-# Import correct md5, irrespective of the Python version
-#
-# `hashlib` was introduced in 2.5, deprecating `md5`
-from __future__ import absolute_import, print_function
-
-try:
-    from hashlib import *
-except ImportError:
-    from md5 import *
-
-new = md5

commit 55114de4aa297c6936985c1c347eb05b25584584

BUG: fix weave issue with too long file names with MSVC.  Closes gh-3216.
This issue was introduced in 021e0ee15.  It's MSVC specific; MSVC
has a 260 char file name (including full path).  So truncation the
has to 50 chars, plus filename prefix (``sc_`` or ``compiler_``), leaves
200 chars for the path which should be enough.

scipy/weave/accelerate_tools.py
@@ -364,7 +364,7 @@ class accelerate(object):
         return fast
 
     def identifier(self,signature):
-        # Build a SHA-256 checksum
+        # Build a (truncated, see gh-3216) SHA-256 checksum
         f = self.function
         co = f.func_code
         identifier = str(signature) + \
@@ -372,7 +372,7 @@ class accelerate(object):
                      str(co.co_consts) + \
                      str(co.co_varnames) + \
                      co.co_code
-        return 'F' + sha256(identifier).hexdigest()
+        return 'F' + sha256(identifier).hexdigest()[:32]
 
     def accelerate(self,signature,identifier):
         P = Python2CXX(self.function,signature,name=identifier)

scipy/weave/build_tools.py
@@ -236,8 +236,8 @@ def build_extension(module_path,compiler_name='',build_dir=None,
     # object files separated from each other.  gcc2.x and gcc3.x C++
     # object files are not compatible, so we'll stick them in a sub
     # dir based on their version. This will add a SHA-256 check sum
-    # of the compiler binary to the directory name to keep objects
-    # from different compilers in different locations.
+    # (truncated to 32 characters) of the compiler binary to the directory
+    # name to keep objects from different compilers in different locations.
 
     compiler_dir = platform_info.get_compiler_dir(compiler_name)
     temp_dir = os.path.join(temp_dir,compiler_dir)

scipy/weave/catalog.py
@@ -92,7 +92,9 @@ def expr_to_filename(expr):
     """
     from hashlib import sha256
     base = 'sc_'
-    return base + sha256(expr).hexdigest()
+    # 32 chars is enough for unique filenames; too long names don't work for
+    # MSVC (see gh-3216).  Don't use md5, gives a FIPS warning.
+    return base + sha256(expr).hexdigest()[:32]
 
 
 def unique_file(d,expr):

scipy/weave/platform_info.py
@@ -103,7 +103,7 @@ def check_sum(file):
     except IOError:
         bytes = ''
     chk_sum = sha256(bytes)
-    return chk_sum.hexdigest()
+    return chk_sum.hexdigest()[:32]  # truncation needed, see gh-3216
 
 
 def get_compiler_dir(compiler_name):