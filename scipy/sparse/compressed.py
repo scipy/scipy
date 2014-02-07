317a13ae19e3196c0adf1af93639e862a5b12928
Pauli Virtanen <pav@iki.fi>
2014-02-07 12:51:34 +0200

    ENH: sparse: implement spmatrix.maximum and spmatrix.minimum

--- scipy/sparse/compressed.py
+++ scipy/sparse/compressed.py
@@ -507,6 +507,42 @@ class _cs_matrix(_data_matrix, _minmax_mixin, IndexMixin):
         return y
 
     #####################
+    # Other binary ops  #
+    #####################
+
+    def _maximum_minimum(self, other, npop, op_name, dense_check):
+        if isscalarlike(other):
+            if dense_check(other):
+                warn("Taking maximum (minimum) with > 0 (< 0) number results to "
+                     "a dense matrix.",
+                     SparseEfficiencyWarning)
+                other_arr = np.empty(self.shape, dtype=np.asarray(other).dtype)
+                other_arr.fill(other)
+                other_arr = self.__class__(other_arr)
+                return self._binopt(other_arr, op_name)
+            else:
+                try:
+                    self.sum_duplicates()
+                except NotImplementedError:
+                    pass
+                new_data = npop(self.data, np.asarray(other))
+                mat = self.__class__((new_data, self.indices, self.indptr),
+                                     dtype=new_data.dtype, shape=self.shape)
+                return mat
+        elif isdense(other):
+            return npop(self.todense(), other)
+        elif isspmatrix(other):
+            return self._binopt(other, op_name)
+        else:
+            raise ValueError("Operands not compatible.")
+
+    def maximum(self, other):
+        return self._maximum_minimum(other, np.maximum, '_maximum_', lambda x: np.asarray(x) > 0)
+
+    def minimum(self, other):
+        return self._maximum_minimum(other, np.minimum, '_minimum_', lambda x: np.asarray(x) < 0)
+
+    #####################
     # Reduce operations #
     #####################
 
