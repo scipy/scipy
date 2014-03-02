6cc19490001e92add90a6e3333ebe8339e1785c3
Skipper Seabold <jsseabold@gmail.com>
2014-03-02 00:25:27 -0500

    TST: Fix tests

--- scipy/optimize/tests/test_bvls.py
+++ scipy/optimize/tests/test_bvls.py
@@ -1,28 +1,29 @@
 from scipy.optimize import bounded_lstsq
-from numpy import arange, dot, float32
-from numpy.linalg import norm
-from numpy.testing import (TestCase, assert_array_almost_equal, assert_equal,
-                           assert_)
+from numpy import arange, dot, random
+from numpy.linalg import norm, lstsq
+from numpy.testing import (TestCase, assert_array_almost_equal, assert_,
+                           assert_allclose)
+
 
 class TestBVLS(TestCase):
-
     def test_bvls(self):
-        X = arange(25.0, dtype=float32).reshape(-1, 5)
-        b = arange(5.0, dtype=float32)
-        y = dot(X, b)
+        random.seed(1)
+        X = random.randn(25, 5)
+        b = arange(1, 6.)
+        y = dot(X, b) + random.randn(25)
         bb, res, nsetp = bounded_lstsq(X, y, bounds=[(0, None), (1, None),
                                                      (2, None), (3, None),
                                                      (4, None)])
-        assert_equal(b, bb)
-        assert_(res < 1e-7)
-        assert_(norm(dot(X,bb)-y) < 1e-7)
+        expected, _, _, _ = lstsq(X, y)
+        assert_array_almost_equal(bb, expected)
+        assert_allclose(norm(dot(X, bb)-y), res)
 
         bb, res, nsetp = bounded_lstsq(X, y, bounds=[(None, None),
                                                      (None, None),
                                                      (None, None),
                                                      (1, 2),
                                                      (2, 3)])
-        assert_(bb[3] == 1)
+        assert_(bb[3] == 2)
         assert_(bb[4] == 3)
 
         # smoke test
