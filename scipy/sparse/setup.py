875bb0a1c7529da0e620bf9248d2c8b24ee5ab3e
Pauli Virtanen <pav@iki.fi>
2014-03-08 18:11:02 +0200

    BLD: sparse: update sparsetools build

--- scipy/sparse/setup.py
+++ scipy/sparse/setup.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 from __future__ import division, print_function, absolute_import
 
+import os
+import sys
+import glob
+import subprocess
 
 def configuration(parent_package='',top_path=None):
-    import numpy
     from numpy.distutils.misc_util import Configuration
 
     config = Configuration('sparse',parent_package,top_path)
@@ -17,6 +20,24 @@ def configuration(parent_package='',top_path=None):
 
     config.add_extension('_csparsetools',
                          sources=['_csparsetools.c'])
+
+    def get_sparsetools_sources(ext, build_dir):
+        # Defer generation of source files
+        subprocess.call([sys.executable,
+                         os.path.join(os.path.dirname(__file__),
+                                      'generate_sparsetools.py'),
+                         '--no-force'])
+        return []
+
+    depends = [os.path.join('sparsetools', 'sparsetools_gen.h')]
+    depends += glob.glob('sparsetools/*.h')
+    config.add_extension('sparsetools',
+                         define_macros=[('__STDC_FORMAT_MACROS', 1)],
+                         depends=depends,
+                         include_dirs=['sparsetools'],
+                         sources=[os.path.join('sparsetools', 'sparsetools.cxx'),
+                                  get_sparsetools_sources]
+                         )
 
     return config
 
