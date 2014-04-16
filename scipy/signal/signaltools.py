0f91329c3633f344785f0f5e1c4026f7ca5c80b4
endolith <endolith@gmail.com>
2014-04-15 23:11:04 -0400

    DOC: Add a 2D cross-correlation example. Currently this is slow, but the template needs to be large or it won't match reliably. Normalized cross-correlation would probably work better, but then it wouldn't be a demo of correlate2d...

--- scipy/signal/signaltools.py
+++ scipy/signal/signaltools.py
@@ -625,6 +625,33 @@ def correlate2d(in1, in2, mode='full', boundary='fill', fillvalue=0):
         A 2-dimensional array containing a subset of the discrete linear
         cross-correlation of `in1` with `in2`.
 
+    Examples
+    --------
+    Use 2D cross-correlation to find the location of a template in a noisy
+    image:
+
+    >>> from scipy import signal
+    >>> from scipy import misc
+    >>> import matplotlib.pyplot as plt
+    >>> lena = misc.lena() - misc.lena().mean()
+    >>> template = np.copy(lena[235:295, 310:370]) # right eye
+    >>> template -= template.mean()
+    >>> lena = lena + np.random.randn(*lena.shape)*50
+    >>> corr = signal.correlate2d(lena, template, boundary='symm', mode='same')
+    >>> fig, (ax_orig, ax_template, ax_corr) = plt.subplots(1, 3)
+    >>> ax_orig.imshow(lena, cmap='gray')
+    >>> ax_orig.set_title('Original')
+    >>> ax_orig.set_axis_off()
+    >>> ax_template.imshow(template, cmap='gray')
+    >>> ax_template.set_title('Template')
+    >>> ax_template.set_axis_off()
+    >>> ax_corr.imshow(corr, cmap='gray')
+    >>> ax_corr.set_title('Cross-correlation')
+    >>> ax_corr.set_axis_off()
+    >>> y, x = np.unravel_index(np.argmax(corr), corr.shape)
+    >>> ax_orig.plot(x, y, 'ro')
+    >>> fig.show()
+
     """
     in1 = asarray(in1)
     in2 = asarray(in2)
