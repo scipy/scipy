b491d1badb851e83bcef8c0608ab729844571d60
alex <argriffi@ncsu.edu>
2014-05-04 16:47:58 -0400

    STY: two blank lines between function definitions

--- scipy/optimize/tests/test_linprog.py
+++ scipy/optimize/tests/test_linprog.py
@@ -72,9 +72,9 @@ def test_linprog_upper_bound_constraints():
             [1,1],
             [1,0]]
     b_ub = [10,8,4]
-
     res = (linprog(c,A_ub=A_ub,b_ub=b_ub))
     _assert_success(res, desired_fun=-18, desired_x=[2, 6])
+
 
 def test_linprog_mixed_constraints():
     # Minimize linear function subject to non-negative variables.
@@ -84,9 +84,9 @@ def test_linprog_mixed_constraints():
            [-1,-1],
            [-2, 1]]
     b_ub = [2,-1,-1]
-
     res = linprog(c,A_ub=A_ub,b_ub=b_ub)
     _assert_success(res, desired_fun=5, desired_x=[2/3, 1/3])
+
 
 def test_linprog_cyclic_recovery():
     # Test linprogs recovery from cycling using the Klee-Minty problem
@@ -95,11 +95,10 @@ def test_linprog_cyclic_recovery():
     A_ub = [[1, 0, 0],
             [20, 1, 0],
             [200,20, 1]]
-
     b_ub = [1,100,10000]
-
     res = linprog(c,A_ub=A_ub,b_ub=b_ub)
     _assert_success(res, desired_x=[0, 0, 10000])
+
 
 def test_linprog_cyclic_bland():
     # Test the effect of Bland's rule on a cycling problem
@@ -108,13 +107,12 @@ def test_linprog_cyclic_bland():
                      [0.5, -1.5, -0.5, 1],
                      [1, 0, 0, 0]])
     b_ub = [0, 0, 1]
-
     res = linprog(c, A_ub=A_ub, b_ub=b_ub, options=dict(maxiter=100))
     assert_(not res.success)
-
     res = linprog(c, A_ub=A_ub, b_ub=b_ub,
                   options=dict(maxiter=100, bland=True,))
     _assert_success(res, desired_x=[1, 0, 1, 0])
+
 
 def test_linprog_unbounded():
     # Test linprog response to an unbounded problem
@@ -122,55 +120,49 @@ def test_linprog_unbounded():
     A_ub = [[-1,1],
             [-1,-1]]
     b_ub = [-1,-2]
-
     res = linprog(c,A_ub=A_ub,b_ub=b_ub)
     _assert_unbounded(res)
+
 
 def test_linprog_infeasible():
     # Test linrpog response to an infeasible problem
     c = [-1,-1]
-
     A_ub = [[1,0],
             [0,1],
             [-1,-1]]
     b_ub = [2,2,-5]
-
     res = linprog(c,A_ub=A_ub,b_ub=b_ub)
     _assert_infeasible(res)
+
 
 def test_nontrivial_problem():
     # Test linprog for a problem involving all constraint types,
     # negative resource limits, and rounding issues.
     c = [-1,8,4,-6]
-
     A_ub = [[-7,-7,6,9],
             [1,-1,-3,0],
             [10,-10,-7,7],
             [6,-1,3,4]]
     b_ub = [-3,6,-6,6]
-
     A_eq = [[-10,1,1,-8]]
     b_eq = [-4]
-
     res = linprog(c,A_ub=A_ub,b_ub=b_ub,A_eq=A_eq,b_eq=b_eq)
     _assert_success(res, desired_fun=7083/1391,
                     desired_x=[101/1391,1462/1391,0,752/1391])
+
 
 def test_negative_variable():
     # Test linprog with a problem with one unbounded variable and
     # another with a negative lower bound.
     c = np.array([-1,4])*-1  # maximize
-
     A_ub = [[-3,1],
             [1,2]]
-
     b_ub = [6,4]
-
     x0_bounds = (-np.inf,np.inf)
     x1_bounds = (-3,np.inf)
-
     res = linprog(c,A_ub=A_ub,b_ub=b_ub,bounds=(x0_bounds,x1_bounds))
     _assert_success(res, desired_fun=-80/7, desired_x=[-8/7, 18/7])
+
 
 def test_large_problem():
     # Test linprog simplex with a rather large problem (400 variables,
@@ -178,6 +170,7 @@ def test_large_problem():
     A,b,c = lpgen_2d(20,20)
     res = linprog(c,A_ub=A,b_ub=b)
     _assert_success(res, desired_fun=-64.049494229)
+
 
 def test_network_flow():
     # A network flow problem with supply and demand at nodes
@@ -196,6 +189,7 @@ def test_network_flow():
     b_eq = [0, 19, -16, 33, 0, 0, -36]
     res = linprog(c=c, A_eq=A_eq, b_eq=b_eq)
     _assert_success(res, desired_fun=755)
+
 
 def test_network_flow_limited_capacity():
     # A network flow problem with supply and demand at nodes
@@ -218,6 +212,7 @@ def test_network_flow_limited_capacity():
     res = linprog(c=cost, A_eq=A_eq, b_eq=b_eq, bounds=bounds)
     _assert_success(res, desired_fun=14)
 
+
 def test_simplex_algorithm_wikipedia_example():
     # http://en.wikipedia.org/wiki/Simplex_algorithm#Example
     Z = [-2, -3, -4]
@@ -227,6 +222,7 @@ def test_simplex_algorithm_wikipedia_example():
     b_ub = [10, 15]
     res = linprog(c=Z, A_ub=A_ub, b_ub=b_ub)
     _assert_success(res, desired_fun=-20)
+
 
 def test_enzo_example():
     # http://projects.scipy.org/scipy/attachment/ticket/1252/lp2.py
@@ -246,6 +242,7 @@ def test_enzo_example():
     _assert_success(res, desired_fun=317.5,
                     desired_x=[66.25, 0, 17.5, 0, 183.75, 0])
 
+
 def test_enzo_example_b():
     # rescued from https://github.com/scipy/scipy/pull/218
     c = [2.8, 6.3, 10.8, -2.8, -6.3, -10.8]
@@ -260,6 +257,7 @@ def test_enzo_example_b():
     _assert_success(res, desired_fun=-1.77,
                     desired_x=[0.3, 0.2, 0.0, 0.0, 0.1, 0.3])
 
+
 def test_enzo_example_c_with_degeneracy():
     # rescued from https://github.com/scipy/scipy/pull/218
     m = 20
@@ -269,6 +267,7 @@ def test_enzo_example_c_with_degeneracy():
     b_eq = [0, 0]
     res = linprog(c=c, A_eq=A_eq, b_eq=b_eq)
     _assert_success(res, desired_fun=0, desired_x=np.zeros(m))
+
 
 def test_enzo_example_c_with_unboundedness():
     # rescued from https://github.com/scipy/scipy/pull/218
@@ -280,6 +279,7 @@ def test_enzo_example_c_with_unboundedness():
     res = linprog(c=c, A_eq=A_eq, b_eq=b_eq)
     _assert_unbounded(res)
 
+
 def test_enzo_example_c_with_infeasibility():
     # rescued from https://github.com/scipy/scipy/pull/218
     m = 50
@@ -289,6 +289,7 @@ def test_enzo_example_c_with_infeasibility():
     b_eq = [1, 1]
     res = linprog(c=c, A_eq=A_eq, b_eq=b_eq)
     _assert_infeasible(res)
+
 
 def test_callback():
     # Check that callback is as advertised
@@ -327,6 +328,7 @@ def test_callback():
     assert_(callback_complete[0])
     assert_allclose(last_xk[0], res.x)
 
+
 def test_unknown_options_or_solver():
     c = np.array([-3,-2])
     A_ub = [[2,1], [1,1], [1,0]]
@@ -338,16 +340,19 @@ def test_unknown_options_or_solver():
     assert_raises(ValueError, linprog,
                   c, A_ub=A_ub, b_ub=b_ub, method='ekki-ekki-ekki')
 
+
 def test_no_constraints():
     res = linprog([-1, -2])
     assert_equal(res.x, [0, 0])
     _assert_unbounded(res)
+
 
 def test_simple_bounds():
     res = linprog([1, 2], bounds=(1, 2))
     _assert_success(res, desired_x=[1, 1])
     res = linprog([1, 2], bounds=[(1, 2), (1, 2)])
     _assert_success(res, desired_x=[1, 1])
+
 
 def test_invalid_inputs():
     for bad_bound in [[(5, 0), (1, 2), (3, 4)],
