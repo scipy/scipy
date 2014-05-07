f66b0deb9e9e1c302fbd71eb52862fbf50c6c2c7
alex <argriffi@ncsu.edu>
2014-05-07 15:22:15 -0400

    MAINT: linalg.orth memory efficiency

--- scipy/linalg/tests/test_decomp.py
+++ scipy/linalg/tests/test_decomp.py
@@ -13,14 +13,15 @@ Run tests if linalg is not installed:
 """
 
 import numpy as np
-from numpy.testing import TestCase, assert_equal, assert_array_almost_equal, \
-        assert_array_equal, assert_raises, assert_, run_module_suite, dec
+from numpy.testing import (TestCase, assert_equal, assert_array_almost_equal,
+        assert_array_equal, assert_raises, assert_, assert_allclose,
+        run_module_suite, dec)
 
 from scipy.lib.six import xrange
 
-from scipy.linalg import eig, eigvals, lu, svd, svdvals, cholesky, qr, \
-     schur, rsf2csf, lu_solve, lu_factor, solve, diagsvd, hessenberg, rq, \
-     eig_banded, eigvals_banded, eigh, eigvalsh, qr_multiply, qz
+from scipy.linalg import (eig, eigvals, lu, svd, svdvals, cholesky, qr,
+     schur, rsf2csf, lu_solve, lu_factor, solve, diagsvd, hessenberg, rq,
+     eig_banded, eigvals_banded, eigh, eigvalsh, qr_multiply, qz, orth)
 from scipy.linalg.lapack import dgbtrf, dgbtrs, zgbtrf, zgbtrs, \
      dsbev, dsbevd, dsbevx, zhbevd, zhbevx
 from scipy.linalg.misc import norm
@@ -2098,6 +2099,27 @@ class TestOverwrite(object):
 
     def test_svdvals(self):
         assert_no_overwrite(svdvals, [(3,3)])
+
+
+def _check_orth(n):
+    X = np.ones((n, 2), dtype=float)
+    Y = orth(X)
+    assert_equal(Y.shape, (n, 1))
+    assert_allclose(Y, Y.mean())
+    Y = orth(X.T)
+    assert_equal(Y.shape, (2, 1))
+    assert_allclose(Y, Y.mean())
+
+
+@dec.slow
+def test_orth_memory_efficiency():
+    _check_orth(10*1000*1000)
+
+
+def test_orth():
+    for n in 1, 2, 3, 10, 100:
+        _check_orth(n)
+
 
 if __name__ == "__main__":
     run_module_suite()
