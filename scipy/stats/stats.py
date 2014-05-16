fb7229b7298c5a3c98c2fcf4ac8c871191468a57
Alex Hagen <mr.alex.hagen@gmail.com>
2014-05-16 16:51:26 -0400

    added cramer von-mises two sample test

--- scipy/stats/stats.py
+++ scipy/stats/stats.py
@@ -169,6 +169,7 @@ from __future__ import division, print_function, absolute_import
 
 import warnings
 import math
+import sys
 
 from scipy.lib.six import xrange
 
@@ -3899,6 +3900,50 @@ def ks_2samp(data1, data2):
         prob = 1.0
     return d, prob
 
+
+def cramer_vonmises_2samp(x, y):
+    """
+    Computes the Cramer von Mises two sample test.
+    
+    This is a two-sided test for the null hypothesis that 2 independent samples
+    are drawn from the same continuous distribution.    
+    
+    Parameters
+    ----------
+    a, b : sequence of 1-D ndarrays
+        two arrays of sample observations assumed to be drawn from a continuous
+        distribution, sample sizes can be different
+
+    Returns
+    -------
+    T : float
+        T statistic
+    p-value : float
+        two-tailed p-value
+    """
+    
+    #following notation of Anderson et al. doi:10.1214/aoms/1177704477
+    N = len(x)
+    M = len(y)
+    assert N * M * (N + M) < sys.float_info.max
+    
+    alldata = np.concatenate((x,y))
+    allranks = rankdata(alldata)
+    ri = allranks[:N]
+    sj = allranks[-M:]
+    
+    i = rankdata(x)
+    j = rankdata(y)
+    #Anderson et al. Eqn 10
+    U = N*np.sum((ri - i)**2) + M*np.sum((sj - j)**2)
+    #Anderson et al. Eqn 9
+    T = U/(N * M * (N + M)) - (4 * M * N - 1)/(6 * (M + N))
+    Texpected = 1/6 + 1/(6 * (M + N))
+    Tvariance = 1/45 * (M + N + 1)/(M + N)**2 * (4 * M * N * (M+N) - \ 
+    3*(M**2 + N**2) - 2*M*N)/(4 * M * N)
+    zscore = np.abs(T - Texpected) / np.sqrt(Tvariance)
+    
+    return T, 2*distributions.norm.sf(zscore)
 
 def mannwhitneyu(x, y, use_continuity=True):
     """
