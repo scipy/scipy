569fb6622dc2e3ef4086cc8d170d708808519f6c
Marcos Duarte <duartexyz@gmail.com>
2014-06-03 04:29:15 -0300

    add regression test for signal.filtfilt
    correct added regression test

--- scipy/signal/tests/test_signaltools.py
+++ scipy/signal/tests/test_signaltools.py
@@ -865,9 +865,9 @@ class TestFiltFilt(TestCase):
         y2 = filtfilt(b, a, np.swapaxes(x, 0, 2), padlen=0, axis=2)
         assert_array_equal(y0, np.swapaxes(y2, 0, 2))
 
-    def test_a_coeff(self):
+    def test_acoeff(self):
         # test for 'a' coefficient as single number
-        out = signal.filtfilt([.1, .5], 1, np.arange(10))
+        out = signal.filtfilt([.5, .5], 1, np.arange(10))
         assert_equal(out, arange(10))		
 
 
