MAINT: fix eigs and eigsh error messages, fix a related possibly off-by-one validation issue, and add a test

scipy/sparse/linalg/eigen/arpack/arpack.py
@@ -48,8 +48,8 @@ import warnings
 from . import _arpack
 import numpy as np
 from scipy.sparse.linalg.interface import aslinearoperator, LinearOperator
-from scipy.sparse import eye, csc_matrix, csr_matrix, \
-    isspmatrix, isspmatrix_csr
+from scipy.sparse import (eye, csc_matrix, csr_matrix,
+        isspmatrix, isspmatrix_csr)
 from scipy.linalg import lu_factor, lu_solve
 from scipy.sparse.sputils import isdense
 from scipy.sparse.linalg import gmres, splu
@@ -499,8 +499,10 @@ class _SymmetricArpackParams(_ArpackParams):
         if which not in _SEUPD_WHICH:
             raise ValueError("which must be one of %s"
                              % ' '.join(_SEUPD_WHICH))
-        if k >= n:
-            raise ValueError("k must be less than ndim(A), k=%d" % k)
+        if not (1 <= k < n):
+            raise ValueError('symmetric arpack params require 1 <= k < n '
+                    'where k=%d is the requested number of eigenvalues '
+                    'and n=%d is the order of the square input matrix' % (k, n))
 
         _ArpackParams.__init__(self, n, k, tp, mode, sigma,
                                ncv, v0, maxiter, which, tol)
@@ -681,8 +683,12 @@ class _UnsymmetricArpackParams(_ArpackParams):
         if which not in _NEUPD_WHICH:
             raise ValueError("Parameter which must be one of %s"
                              % ' '.join(_NEUPD_WHICH))
-        if k >= n - 1:
-            raise ValueError("k must be less than ndim(A)-1, k=%d" % k)
+
+        # require k < n-1 ???
+        if not (1 <= k < n):
+            raise ValueError('unsymmetric arpack params require 1 <= k < n '
+                    'where k=%d is the requested number of eigenvalues '
+                    'and n=%d is the order of the square input matrix' % (k, n))
 
         _ArpackParams.__init__(self, n, k, tp, mode, sigma,
                                ncv, v0, maxiter, which, tol)
@@ -1205,9 +1211,10 @@ def eigs(A, k=6, M=None, sigma=None, which='LM', v0=None,
                           'This may adversely affect ARPACK convergence')
     n = A.shape[0]
 
-    if k <= 0 or k >= n:
-        raise ValueError("k=%d must be between 1 and ndim(A)-1=%d"
-                         % (k, n - 1))
+    if not (1 <= k < n):
+        raise ValueError('the eigs function requires 1 <= k < n '
+                'where k=%d is the requested number of eigenvalues '
+                'and n=%d is the order of the square matrix A' % (k, n))
 
     if sigma is None:
         matvec = _aslinearoperator_with_dtype(A).matvec
@@ -1483,8 +1490,10 @@ def eigsh(A, k=6, M=None, sigma=None, which='LM', v0=None,
                           'This may adversely affect ARPACK convergence')
     n = A.shape[0]
 
-    if k <= 0 or k >= n:
-        raise ValueError("k must be between 1 and ndim(A)-1")
+    if not (1 <= k < n):
+        raise ValueError('the eigsh function requires 1 <= k < n '
+                'where k=%d is the requested number of eigenvalues '
+                'and n=%d is the order of the square matrix A' % (k, n))
 
     if sigma is None:
         A = _aslinearoperator_with_dtype(A)

scipy/sparse/linalg/eigen/arpack/tests/test_arpack.py
@@ -10,16 +10,16 @@ import warnings
 
 import numpy as np
 
-from numpy.testing import assert_allclose, \
-        assert_array_almost_equal_nulp, TestCase, run_module_suite, dec, \
-        assert_raises, verbose, assert_equal
+from numpy.testing import (assert_allclose,
+        assert_array_almost_equal_nulp, TestCase, run_module_suite, dec,
+        assert_raises, verbose, assert_equal)
 
 from numpy import array, finfo, argsort, dot, round, conj, random
 from scipy.linalg import eig, eigh
 from scipy.sparse import csc_matrix, csr_matrix, lil_matrix, isspmatrix
 from scipy.sparse.linalg import LinearOperator, aslinearoperator
-from scipy.sparse.linalg.eigen.arpack import eigs, eigsh, svds, \
-     ArpackNoConvergence
+from scipy.sparse.linalg.eigen.arpack import (eigs, eigsh, svds,
+     ArpackNoConvergence)
 
 from scipy.linalg import svd, hilbert
 
@@ -637,6 +637,22 @@ def test_svd_v0():
     u2, s2, vh2 = svds(x, 1, v0=u[:,0])
 
     assert_allclose(s, s2, atol=np.sqrt(1e-15))
+
+
+def test_n_k_validation():
+    # check that combinations of k and n are accepted or rejected as expected
+    for n in 3, 4:
+        for t in float, complex:
+            A = np.ones((n, n), dtype=t)
+            for f in eigs, eigsh:
+                for k in 0, n:
+                    for ev in False, True:
+                        kwargs = dict(k=k, return_eigenvectors=ev)
+                        assert_raises(ValueError, f, A, **kwargs)
+                for k in range(1, n):
+                    w = np.absolute(f(A, k=1, return_eigenvectors=0))
+                    assert_allclose(np.sum(w), n)
+                    assert_allclose(np.max(w), n)
 
 
 if __name__ == "__main__":