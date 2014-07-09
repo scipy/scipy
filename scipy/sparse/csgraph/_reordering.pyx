ed232971d50f7095f7b22a6a8bc634a9b68f728c
Paul Nation <nonhermitian@gmail.com>
2014-07-09 19:29:41 +0900

    set type of row_match after multiplication

--- scipy/sparse/csgraph/_reordering.pyx
+++ scipy/sparse/csgraph/_reordering.pyx
@@ -196,7 +196,7 @@ def _maximum_bipartite_matching(
     cdef np.ndarray[ITYPE_t] queue = np.zeros(n, dtype=ITYPE)
     cdef np.ndarray[ITYPE_t] previous = np.zeros(n, dtype=ITYPE)
     cdef np.ndarray[int32_or_int64] match = (-1 * np.ones(n)).astype(inds.dtype)
-    cdef np.ndarray[ITYPE_t] row_match = -1 * np.ones(n, dtype=ITYPE)
+    cdef np.ndarray[ITYPE_t] row_match = (-1 * np.ones(n)).astype(ITYPE)
     cdef int queue_ptr, queue_col, ptr, i, j, queue_size
     cdef int col, next_num = 1
     cdef int32_or_int64 row, temp, eptr
