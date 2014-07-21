11ea5124eb48f6c095058e2e56f41c3591065f46
Pauli Virtanen <pav@iki.fi>
2014-07-22 00:01:23 +0300

    TST: special: enable some mpmath test for incomplete elliptic integrals E and K

--- scipy/special/tests/test_mpmath.py
+++ scipy/special/tests/test_mpmath.py
@@ -979,19 +979,29 @@ class TestSystematic(with_metaclass(_SystematicMeta, object)):
     def test_ellipe(self):
         assert_mpmath_equal(sc.ellipe,
                             mpmath.ellipe,
-                            [Arg()])
+                            [Arg(b=1.0)])
 
-    @knownfailure_overridable("insufficient accuracy from Cephes at large |phi|")
     def test_ellipeinc(self):
         assert_mpmath_equal(sc.ellipeinc,
                             mpmath.ellipe,
-                            [Arg(), Arg(b=1.0)])
+                            [Arg(-1e3, 1e3), Arg(b=1.0)])
 
     @knownfailure_overridable("insufficient accuracy from Cephes at large |phi|")
+    def test_ellipeinc_largephi(self):
+        assert_mpmath_equal(sc.ellipeinc,
+                            mpmath.ellipe,
+                            [Arg(), Arg()])
+
     def test_ellipf(self):
         assert_mpmath_equal(sc.ellipkinc,
                             mpmath.ellipf,
-                            [Arg(), Arg(b=1.0)])
+                            [Arg(-1e3, 1e3), Arg()])
+
+    @knownfailure_overridable("insufficient accuracy from Cephes at large |phi|")
+    def test_ellipf_largephi(self):
+        assert_mpmath_equal(sc.ellipkinc,
+                            mpmath.ellipf,
+                            [Arg(), Arg()])
 
     def test_ellipk(self):
         assert_mpmath_equal(sc.ellipk,
@@ -1001,6 +1011,21 @@ class TestSystematic(with_metaclass(_SystematicMeta, object)):
                             lambda m: mpmath.ellipk(1 - m),
                             [Arg(a=0.0)],
                             dps=400)
+
+    def test_ellipkinc(self):
+        def ellipkinc(phi, m):
+            return mpmath.ellippi(0, phi, m)
+        assert_mpmath_equal(sc.ellipkinc,
+                            ellipkinc,
+                            [Arg(-1e3, 1e3), Arg(b=1.0)])
+
+    @knownfailure_overridable("insufficient accuracy from Cephes at large |phi|")
+    def test_ellipkinc_largephi(self):
+        def ellipkinc(phi, m):
+            return mpmath.ellippi(0, phi, m)
+        assert_mpmath_equal(sc.ellipkinc,
+                            ellipkinc,
+                            [Arg(), Arg(b=1.0)])
 
     def test_ellipfun_sn(self):
         # Oscillating function --- limit range of first argument; the
