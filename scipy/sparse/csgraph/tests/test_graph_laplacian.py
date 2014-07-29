b690992b77d16d7144e3e494acb64626cc194080
alex <argriffi@ncsu.edu>
2014-07-29 19:59:06 -0400

    TST: add test coverage of laplacian ValueError

--- scipy/sparse/csgraph/tests/test_graph_laplacian.py
+++ scipy/sparse/csgraph/tests/test_graph_laplacian.py
@@ -5,7 +5,7 @@ from __future__ import division, print_function, absolute_import
 
 import numpy as np
 from numpy.testing import (run_module_suite, assert_allclose,
-        assert_array_almost_equal)
+        assert_array_almost_equal, assert_raises)
 from scipy import sparse
 
 from scipy.sparse import csgraph
@@ -46,6 +46,17 @@ def _check_symmetric_graph_laplacian(mat, normed):
 
     assert_array_almost_equal(laplacian,
         _explicit_laplacian(mat, normed=normed))
+
+
+def test_laplacian_value_error():
+    for t in int, float, complex:
+        for m in ([1, 1],
+                [[[1]]],
+                [[1, 2, 3], [4, 5, 6]],
+                [[1, 2], [3, 4], [5, 5]],
+                ):
+            A = np.array(m, dtype=t)
+            assert_raises(ValueError, csgraph.laplacian, A)
 
 
 def test_symmetric_graph_laplacian():
