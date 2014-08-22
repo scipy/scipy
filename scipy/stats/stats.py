394a428e10581333b75ad73021f233ee386206af
Warren Weckesser <warren.weckesser@gmail.com>
2014-08-22 18:59:08 -0400

    DOC: stats: Fix a bad indent in the histogram docstring.

--- scipy/stats/stats.py
+++ scipy/stats/stats.py
@@ -1635,7 +1635,7 @@ def histogram(a, numbins=10, defaultlimits=None, weights=None, printextras=False
         The lower and upper values for the range of the histogram.
         If no value is given, a range slightly larger then the range of the
         values in a is used. Specifically ``(a.min() - s, a.max() + s)``,
-            where ``s = (1/2)(a.max() - a.min()) / (numbins - 1)``.
+        where ``s = (1/2)(a.max() - a.min()) / (numbins - 1)``.
     weights : array_like, optional
         The weights for each value in `a`. Default is None, which gives each
         value a weight of 1.0
