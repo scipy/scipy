c9395e7d32224586ac8ca5cc1d9f691442689865
Gert-Ludwig Ingold <gert.ingold@physik.uni-augsburg.de>
2014-08-24 14:04:39 +0200

    test exposing problem in lqmn for 1<x<1.0001

--- scipy/special/tests/test_basic.py
+++ scipy/special/tests/test_basic.py
@@ -2557,6 +2557,17 @@ class TestLegendreFunctions(TestCase):
         assert_array_almost_equal(lqmnf[0][0],lqf[0],4)
         assert_array_almost_equal(lqmnf[1][0],lqf[1],4)
 
+    def test_lqmn_gt1(self):
+        """algorithm for real arguments changes at 1.0001
+           test against analytical result for m=2, n=1
+        """
+        x0 = 1.0001
+        delta = 0.00002
+        for x in (x0-delta, x0+delta):
+            lq = special.lqmn(2, 1, x)[0][-1, -1]
+            expected = 2/(x*x-1)
+            assert_almost_equal(lq, expected)
+
     def test_lqmn_shape(self):
         a, b = special.lqmn(4, 4, 1.1)
         assert_equal(a.shape, (5, 5))
