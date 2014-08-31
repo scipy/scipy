be9eeb60067a67913b206ce9def3dc91bfc3d890
Ralf Gommers <ralf.gommers@googlemail.com>
2014-08-31 10:55:07 +0200

    tmp - time_testsuite sysexit

--- tools/time_testsuite.py
+++ tools/time_testsuite.py
@@ -1,3 +1,4 @@
+import sys
 import time
 
 from scipy import (cluster, constants, fftpack, integrate, interpolate, io,
@@ -13,15 +14,24 @@ modules = [cluster, constants, fftpack, integrate, interpolate, io,
 def test_module(module, mode='fast'):
     """Run tests for a module, then return how much time they took."""
     t_start = time.time()
-    module.test(mode)
+    result = module.test(mode)
     t_stop = time.time()
-    return t_stop - t_start
+    return (result, t_stop - t_start)
 
 
 timings = dict()
+tests_passed = True
 for module in modules:
-    timings[module.__name__] = test_module(module, mode='full')
+    result, total_time = test_module(module, mode='full')
+    timings[module.__name__] = total_time
+    tests_passed = tests_passed and result.wasSuccessful()
 
 
 for key in reversed(sorted(timings, key=lambda key: timings[key])):
     print("%25s: %1.3f" % (key, timings[key]))
+
+
+if tests_passed:
+    sys.exit(0)
+else:
+    sys.exit(1)
