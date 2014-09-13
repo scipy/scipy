24bc5be56dd7305a439bfd0ae42e43118fac3492
Evgeni Burovski <evgeni@burovski.me>
2014-09-13 13:40:36 +0100

    TST: stats: guard test_moments with an env variable

--- scipy/lib/_util.py
+++ scipy/lib/_util.py
@@ -1,5 +1,9 @@
 import sys
 import warnings
+import os
+
+from nose import SkipTest
+from .decorator import decorator
 
 
 class DeprecatedImport(object):
@@ -34,3 +38,31 @@ class DeprecatedImport(object):
                       % (self._old_name, self._new_name),
                       DeprecationWarning)
         return getattr(self._mod, name)
+
+
+def setup_xslow():
+    """Convenience function for skipping xslow tests in nose generators.
+
+    Attaching a custom decorator to a nose test generator makes nose
+    unconditionally skip tests.
+    Use this one together with nose.with_setup::
+
+        from nose import with_setup
+
+        @with_setup(setup_xslow)
+        def test_gen():
+            yield check_a, a
+            ...
+
+        def check_a(a):
+            ...
+
+    """
+    try:
+        v = int(os.environ.get('SCIPY_XSLOW', '0'))
+        if not v:
+            raise ValueError()
+    except ValueError:
+        raise SkipTest("very slow test; set environment variable "
+                       "SCIPY_XSLOW=1 to run it")
+
