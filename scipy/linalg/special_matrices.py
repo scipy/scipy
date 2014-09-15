693ee1b0742199522abc98c6954eae453005606e
Warren Weckesser <warren.weckesser@gmail.com>
2014-09-15 15:06:58 -0400

    DOC: linalg: move the 'versionadded' info of 'pascal' to the notes.

--- scipy/linalg/special_matrices.py
+++ scipy/linalg/special_matrices.py
@@ -730,8 +730,6 @@ def pascal(n, kind='symmetric', exact=True):
     The Pascal matrix is a matrix containing the binomial coefficients as
     its elements.
 
-    .. versionadded:: 0.11.0
-
     Parameters
     ----------
     n : int
@@ -757,6 +755,8 @@ def pascal(n, kind='symmetric', exact=True):
     -----
     See http://en.wikipedia.org/wiki/Pascal_matrix for more information
     about Pascal matrices.
+
+    .. versionadded:: 0.11.0
 
     Examples
     --------
