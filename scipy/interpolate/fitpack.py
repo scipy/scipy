23bdb95fffea285291ee9a39997d175f855e3461
David Menéndez Hurtado <david.menendez.hurtado@scilifelab.se>
2014-09-25 13:09:45 +0200

    PEP8: putting back print_function for uniformity

--- scipy/interpolate/fitpack.py
+++ scipy/interpolate/fitpack.py
@@ -21,7 +21,7 @@ TODO: Make interfaces to the following fitpack functions:
     For univariate splines: cocosp, concon, fourco, insert
     For bivariate splines: profil, regrid, parsur, surev
 """
-from __future__ import division, absolute_import
+from __future__ import division, print_function, absolute_import
 
 
 __all__ = ['splrep', 'splprep', 'splev', 'splint', 'sproot', 'spalde',
