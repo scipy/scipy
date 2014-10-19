3ee2aad83c4d67758886398a5ccfd353f81a71f9
Lars Buitinck <larsmans@gmail.com>
2014-10-19 21:15:09 +0200

    ENH: sparse.linalg: refactor LinearOperator class hierarchy
    ... and document how to create a proper subclass of it: by
    implementing some "kernel" methods (protected methods for the
    C++/Java crowd) that receive properly validated input from
    public wrapper methods.
    
    Behavior of LinearOperator(matvec=some_callable) is preserved
    using __new__ magic to create a factory function that returns an
    instance of the proper subclass. No more monkey-patching!

--- scipy/sparse/linalg/_expm_multiply.py
+++ scipy/sparse/linalg/_expm_multiply.py
@@ -250,17 +250,17 @@ class MatrixPowerOperator(LinearOperator):
         self.ndim = A.ndim
         self.shape = A.shape
 
-    def matvec(self, x):
+    def _matvec(self, x):
         for i in range(self._p):
             x = self._A.dot(x)
         return x
 
-    def rmatvec(self, x):
+    def _rmatvec(self, x):
         for i in range(self._p):
             x = x.dot(self._A)
         return x
 
-    def matmat(self, X):
+    def _matmat(self, X):
         for i in range(self._p):
             X = self._A.dot(X)
         return X
