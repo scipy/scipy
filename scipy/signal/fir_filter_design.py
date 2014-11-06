642aa52b574ec23ba0381fa7e19add76efb2a698
Nils Werner <nils.werner@gmail.com>
2014-11-06 18:04:16 +0100

    remezord implementation

--- scipy/signal/fir_filter_design.py
+++ scipy/signal/fir_filter_design.py
@@ -8,7 +8,7 @@ from scipy.special import sinc
 from . import sigtools
 
 __all__ = ['kaiser_beta', 'kaiser_atten', 'kaiserord',
-           'firwin', 'firwin2', 'remez']
+           'firwin', 'firwin2', 'remez', 'remezord']
 
 
 # Some notes on function parameters:
@@ -583,3 +583,181 @@ def remez(numtaps, bands, desired, weight=None, Hz=1, type='bandpass',
     bands = np.asarray(bands).copy()
     return sigtools._remez(numtaps, bands, desired, weight, tnum, Hz,
                            maxiter, grid_density)
+
+
+def oddround(x):
+    """Return the nearest odd integer from x."""
+
+    return x-np.mod(x, 2)+1
+
+
+def oddceil(x):
+    """Return the smallest odd integer not less than x."""
+
+    return oddround(x+1)
+
+
+def remlplen_herrmann(fp, fs, dp, ds):
+    """
+    Determine the length of the low pass filter with passband frequency
+    fp, stopband frequency fs, passband ripple dp, and stopband ripple ds.
+    fp and fs must be normalized with respect to the sampling frequency.
+    Note that the filter order is one less than the filter length.
+
+    Uses approximation algorithm described by Herrmann et al.:
+
+    O. Herrmann, L.R. Raviner, and D.S.K. Chan, Practical Design Rules for
+    Optimum Finite Impulse Response Low-Pass Digital Filters, Bell Syst. Tech.
+    Jour., 52(6):769-799, Jul./Aug. 1973.
+    """
+
+    dF = fs-fp
+    a = [5.309e-3, 7.114e-2, -4.761e-1, -2.66e-3, -5.941e-1, -4.278e-1]
+    b = [11.01217, 0.51244]
+    Dinf = np.log10(ds)*(a[0]*np.log10(dp)**2+a[1]*np.log10(dp)+a[2]) + \
+        a[3]*np.log10(dp)**2+a[4]*np.log10(dp)+a[5]
+    f = b[0]+b[1]*(np.log10(dp)-np.log10(ds))
+    N1 = Dinf/dF-f*dF+1
+
+    return int(oddround(N1))
+
+
+def remlplen_kaiser(fp, fs, dp, ds):
+    """
+    Determine the length of the low pass filter with passband frequency
+    fp, stopband frequency fs, passband ripple dp, and stopband ripple ds.
+    fp and fs must be normalized with respect to the sampling frequency.
+    Note that the filter order is one less than the filter length.
+
+    Uses approximation algorithm described by Kaiser:
+
+    J.F. Kaiser, Nonrecursive Digital Filter Design Using I_0-sinh Window
+    function, Proc. IEEE Int. Symp. Circuits and Systems, 20-23, April 1974.
+    """
+
+    dF = fs-fp
+    N2 = (-20*np.log10(np.sqrt(dp*ds))-13.0)/(14.6*dF)+1.0
+
+    return int(oddceil(N2))
+
+
+def remlplen_ichige(fp, fs, dp, ds):
+    """
+    Determine the length of the low pass filter with passband frequency
+    fp, stopband frequency fs, passband ripple dp, and stopband ripple ds.
+    fp and fs must be normalized with respect to the sampling frequency.
+    Note that the filter order is one less than the filter length.
+
+    Uses approximation algorithm described by Ichige et al.:
+
+    K. Ichige, M. Iwaki, and R. Ishii, Accurate Estimation of Minimum
+    Filter Length for Optimum FIR Digital Filters, IEEE Transactions on
+    Circuits and Systems, 47(10):1008-1017, October 2000.
+    """
+
+    dF = fs-fp
+    v = lambda dF, dp: 2.325*((-np.log10(dp))**-0.445)*dF**(-1.39)
+    g = lambda fp, dF, d: (2.0/np.pi)*np.arctan(v(dF, dp)*(1.0/fp-1.0/(0.5-dF)))
+    h = lambda fp, dF, c: (2.0/np.pi)*np.arctan((c/dF)*(1.0/fp-1.0/(0.5-dF)))
+    Nc = ceil(1.0+(1.101/dF)*(-np.log10(2.0*dp))**1.1)
+    Nm = (0.52/dF)*np.log10(dp/ds)*(-np.log10(dp))**0.17
+    N3 = ceil(Nc*(g(fp, dF, dp)+g(0.5-dF-fp, dF, dp)+1.0)/3.0)
+    DN = ceil(Nm*(h(fp, dF, 1.1)-(h(0.5-dF-fp, dF, 0.29)-1.0)/2.0))
+    N4 = N3+DN
+
+    return int(N4)
+
+
+def remezord(freqs, amps, rips, Hz=1, alg='ichige'):
+    """Filter parameter selection for the Remez exchange algorithm.
+
+    Calculate the parameters required by the Remez exchange algorithm to
+    construct a finite impulse response (FIR) filter that approximately
+    meets the specified design.
+
+    Paramters:
+    ----------
+
+    freqs : ndarray
+        A monotonic sequence of band edges specified in Hertz. All
+        elements must be non-negative and less than 1/2 the
+        sampling frequency as given by the Hz parameter.
+    amps : ndarray
+        A sequence containing the amplitudes of the signal to be
+        filtered over the various bands.
+    rips : ndarray
+        A sequence specifying the maximum ripples of each band.
+    alg : string
+        Filter length approximation algorithm. May be
+        'herrmann', 'kaiser', or 'ichige'.
+
+    Returns
+    -------
+
+    numtaps, bands, desired, weight : mixed
+        See help for the remez function.
+
+    See
+    ---
+    remez : Calculate the minimax optimal filter using the
+        Remez exchange algorithm.
+
+    Notes
+    -----
+
+    .. versionadded:: 0.15.0
+
+    """
+
+    # Make sure the parameters are floating point numpy arrays:
+    freqs = np.asarray(freqs, 'd')
+    amps = np.asarray(amps, 'd')
+    rips = np.asarray(rips, 'd')
+
+    # Scale ripples with respect to band amplitudes:
+    rips /= (amps + (amps == 0.0))
+
+    # Normalize input frequencies with respect to sampling frequency:
+    freqs /= Hz
+
+    # Select filter length approximation algorithm:
+    if alg == 'herrmann':
+        remlplen = remlplen_herrmann
+    elif alg == 'kaiser':
+        remlplen = remlplen_kaiser
+    elif alg == 'ichige':
+        remlplen = remlplen_ichige
+    else:
+        raise ValueError('Unknown filter length approximation algorithm.')
+
+    # Validate inputs:
+    if np.any(freqs > 0.5):
+        raise ValueError('Frequency band edges must not exceed the Nyquist frequency.')
+    if np.any(freqs < 0.0):
+        raise ValueError('Frequency band edges must be nonnegative.')
+    if np.any(rips < 0.0):
+        raise ValueError('Ripples must be nonnegative.')
+    if len(amps) != len(rips):
+        raise ValueError('Number of amplitudes must equal number of ripples.')
+    if len(freqs) != 2*(len(amps)-1):
+        raise ValueError('Number of band edges must equal 2*((number of amplitudes)-1)')
+
+    # Find the longest filter length needed to implement any of the
+    # low-pass or high-pass filters with the specified edges:
+    f1 = freqs[0:-1:2]
+    f2 = freqs[1::2]
+    L = 0
+    for i in range(len(amps)-1):
+        L = max((L,
+                 remlplen(f1[i], f2[i], rips[i], rips[i+1]),
+                 remlplen(0.5-f2[i], 0.5-f1[i], rips[i+1], rips[i])))
+
+    # Cap the sequence of band edges with the limits of the digital frequency
+    # range:
+    bands = np.hstack((0.0, freqs, 0.5))
+
+    # The filter design weights correspond to the ratios between the maximum
+    # ripple and all of the other ripples:
+    weight = max(rips)/rips
+
+    return [L, bands, amps, weight]
