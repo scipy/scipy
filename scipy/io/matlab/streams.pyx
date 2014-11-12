af3b00af0298b564276e9fd1ed8a5cf8f67fad36
Ian Henriksen <insertinterestingnamehere@gmail.com>
2014-11-12 10:07:01 -0700

    BUG: Raise exceptions when an invalid file position is returned.
    (cherry picked from commit b6f7d848ec8f5c7dfdeb9d2b4c9e25bf3db9525a)

--- scipy/io/matlab/streams.pyx
+++ scipy/io/matlab/streams.pyx
@@ -201,7 +201,9 @@ cdef class ZlibInputStream(GenericStream):
         return (self._max_length == self._read_bytes) and \
                (self._buffer_size == self._buffer_position)
 
-    cpdef long int tell(self):
+    cpdef long int tell(self) except -1:
+        if self._total_position == -1:
+            raise IOError("Invalid file position.")
         return self._total_position
 
     cpdef int seek(self, long int offset, int whence=0) except -1:
@@ -304,8 +306,11 @@ cdef class FileStream(GenericStream):
             raise IOError('Failed seek')
         return ret
 
-    cpdef long int tell(self):
-        return ftell(self.file)
+    cpdef long int tell(self) except -1:
+        cdef long int position = ftell(self.file)
+        if position == -1:
+            raise IOError("Invalid file position.")
+        return position
 
     cdef int read_into(self, void *buf, size_t n) except -1:
         """ Read n bytes from stream into pre-allocated buffer `buf`
