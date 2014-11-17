621a06efbc7b46beaa31e6995d8123251701f270
Lars Buitinck <larsmans@gmail.com>
2014-11-17 21:04:40 +0100

    MAINT: optimize: clean up Zeros code
    * Use standard C header file <math.h>
    * Decouple implementation from interface in zeros.h
      (expanded out the ERROR macro for readability)
    * Don't use static variables, they make code non-reentrant
      and hinder compiler optimizations
    * Indentation, {}, split multi-statement lines

--- scipy/optimize/Zeros/bisect.c
+++ scipy/optimize/Zeros/bisect.c
@@ -1,9 +1,11 @@
 /* Written by Charles Harris charles.harris@sdl.usu.edu */
 
+#include <math.h>
 #include "zeros.h"
 
 double
-bisect(callback_type f, double xa, double xb, double xtol, double rtol, int iter, default_parameters *params)
+bisect(callback_type f, double xa, double xb, double xtol, double rtol,
+       int iter, default_parameters *params)
 {
     int i;
     double dm,xm,fm,fa,fb,tol;
@@ -13,12 +15,19 @@ bisect(callback_type f, double xa, double xb, double xtol, double rtol, int iter
     fa = (*f)(xa,params);
     fb = (*f)(xb,params);
     params->funcalls = 2;
-    if (fa*fb > 0) {ERROR(params,SIGNERR,0.0);}
-    if (fa == 0) return xa;
-    if (fb == 0) return xb;
+    if (fa*fb > 0) {
+        params->error_num = SIGNERR;
+        return 0.;
+    }
+    if (fa == 0) {
+        return xa;
+    }
+    if (fb == 0) {
+        return xb;
+    }
     dm = xb - xa;
     params->iterations = 0;
-    for(i=0; i<iter; i++) {
+    for (i=0; i<iter; i++) {
         params->iterations++;
         dm *= .5;
         xm = xa + dm;
@@ -27,8 +36,10 @@ bisect(callback_type f, double xa, double xb, double xtol, double rtol, int iter
         if (fm*fa >= 0) {
             xa = xm;
         }
-        if (fm == 0 || fabs(dm) < tol)
+        if (fm == 0 || fabs(dm) < tol) {
             return xm;
+        }
     }
-    ERROR(params,CONVERR,xa);
+    params->error_num = CONVERR;
+    return xa;
 }
