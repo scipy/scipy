54cb608f98beee472068b3d8cc5c85c883c8944c
Ralf Gommers <ralf.gommers@googlemail.com>
2014-11-26 21:42:03 +0100

    TST: add run_module_suite usage to tests for linalg Cython interface.

--- scipy/linalg/tests/test_blas_pointers.py
+++ scipy/linalg/tests/test_blas_pointers.py
@@ -1,2 +1,8 @@
+from numpy.testing import run_module_suite
+
 from scipy.linalg._test_blas_pointers import (test_dgemm_pointer,
                                              test_wfunc_pointers)
+
+
+if __name__ == "__main__":
+    run_module_suite()
--- scipy/linalg/tests/test_lapack_pointers.py
+++ scipy/linalg/tests/test_lapack_pointers.py
@@ -1,1 +1,7 @@
+from numpy.testing import run_module_suite
+
 from scipy.linalg._test_lapack_pointers import test_lamch
+
+
+if __name__ == "__main__":
+    run_module_suite()
