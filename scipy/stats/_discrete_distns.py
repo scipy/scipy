cbfd872df31c58ca6033816affdcc0d64e10595b
Aman Singh <ug201310004@iitj.ac.in>
2014-12-23 16:49:38 +0530

    Revert "Update _discrete_distns.py"
    This reverts commit 4e5b1e48fc3516c247015a3da41214b485204f2b.

--- scipy/stats/_discrete_distns.py
+++ scipy/stats/_discrete_distns.py
@@ -214,7 +214,6 @@ class geom_gen(rv_discrete):
         return np.power(1-p, k-1) * p
 
     def _logpmf(self, k, p):
-            
         return special.xlog1py(k - 1, -p) + log(p)
 
     def _cdf(self, x, p):
