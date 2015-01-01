6d31787ce1c34e295b80991520249b5e4ed241bd
Ralf Gommers <ralf.gommers@googlemail.com>
2015-01-01 16:04:59 +0100

    BLD: silence Numpy API deprecation build warnings for scipy.optimize

--- scipy/_build_utils/__init__.py
+++ scipy/_build_utils/__init__.py
@@ -1,1 +1,11 @@
 from ._fortran import *
+
+
+# Don't use deprecation Numpy C API.  Define this to a fixed version instead of
+# NPY_API_VERSION in order not to break compilation for released Scipy versions
+# when Numpy introduces a new deprecation.  Use in setup.py::
+#
+#   config.add_extension('_name', sources=['source_fname'], **numpy_nodepr_api)
+#
+numpy_nodepr_api = dict(define_macros=[("NPY_NO_DEPRECATED_API",
+                                        "NPY_1_9_API_VERSION")])
