5ec291861a0e249764356e4b1bc5039979860aee
alex <argriffi@ncsu.edu>
2015-01-03 13:48:01 -0500

    TST: move the empty svdvals test from the svd section to the svdvals section

--- scipy/linalg/tests/test_decomp.py
+++ scipy/linalg/tests/test_decomp.py
@@ -794,11 +794,6 @@ class TestSVD(TestCase):
     def setUp(self):
         seed(1234)
 
-    def test_empty(self):
-        for a in [[]], np.empty((2, 0)), np.ones((0, 3)):
-            s = svdvals(a)
-            assert_equal(s, np.empty(0))
-
     def test_simple(self):
         a = [[1,2,3],[1,20,3],[2,5,6]]
         for full_matrices in (True, False):
@@ -904,6 +899,11 @@ class TestSVD(TestCase):
 
 
 class TestSVDVals(TestCase):
+
+    def test_empty(self):
+        for a in [[]], np.empty((2, 0)), np.ones((0, 3)):
+            s = svdvals(a)
+            assert_equal(s, np.empty(0))
 
     def test_simple(self):
         a = [[1,2,3],[1,2,3],[2,5,6]]
