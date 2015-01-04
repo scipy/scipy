c7c6779eef599da8feb18635a896cd2ae0823da5
Ralf Gommers <ralf.gommers@googlemail.com>
2015-01-04 20:26:48 +0100

    DOC: add second example for stats.binned_statistic.  Thanks to @hamogu
    [ci skip]

--- scipy/stats/_binned_statistic.py
+++ scipy/stats/_binned_statistic.py
@@ -43,11 +43,11 @@ def binned_statistic(x, values, statistic='mean',
 
     bins : int or sequence of scalars, optional
         If `bins` is an int, it defines the number of equal-width bins in the
-        given range (10, by default).  If `bins` is a sequence, it defines the
+        given range (10 by default).  If `bins` is a sequence, it defines the
         bin edges, including the rightmost edge, allowing for non-uniform bin
-        widths.  Values in `x` that are smaller than lowest bin are assigned to
-        bin number 1, values beyond the highest bin are assigned to bin number
-        ``len(bins)``.
+        widths.  Values in `x` that are smaller than lowest bin edge are
+        assigned to bin number 0, values beyond the highest bin are assigned to
+        ``bins[-1]``.
     range : (float, float) or [(float, float)], optional
         The lower and upper range of the bins.  If not provided, range
         is simply ``(x.min(), x.max())``.  Values outside the range are
@@ -87,9 +87,29 @@ def binned_statistic(x, values, statistic='mean',
     ...                        bins=3)
     (array([ 1.,  2.,  4.]), array([ 1.,  2.,  3.,  4.]), array([1, 2, 1, 2, 3]))
 
-    Now use ``bin_edges`` and ``binnumber`` to make a plot of a distribution
-    that shows the mean and distribution around that mean per bin, on top of a
-    regular histogram and the probability distribution function:
+    As a second example, we now generate some random data of sailing boat speed
+    as a function of wind speed, and then determine how fast our boat is for
+    certain wind speeds:
+
+    >>> windspeed = 8 * np.random.rand(500)
+    >>> boatspeed = .3 * windspeed**.5 + .2 * np.random.rand(500)
+    >>> bin_means, bin_edges, binnumber = stats.binned_statistic(windspeed,
+    ...                 boatspeed, statistic='median', bins=[1,2,3,4,5,6,7])
+    >>> plt.figure()
+    >>> plt.plot(windspeed, boatspeed, 'b.', label='raw data')
+    >>> plt.hlines(bin_means, bin_edges[:-1], bin_edges[1:], colors='g', lw=5,
+    ...            label='binned statistic of data')
+    >>> plt.legend()
+
+    Now we can use ``binnumber`` to select all datapoints with a windspeed
+    below 1:
+
+    >>> low_boatspeed = boatspeed[binnumber == 0]
+
+    As a final example, we will use ``bin_edges`` and ``binnumber`` to make a
+    plot of a distribution that shows the mean and distribution around that
+    mean per bin, on top of a regular histogram and the probability
+    distribution function:
 
     >>> x = np.linspace(0, 5, num=500)
     >>> x_pdf = stats.maxwell.pdf(x)
@@ -100,6 +120,7 @@ def binned_statistic(x, values, statistic='mean',
     >>> bin_width = (bin_edges[1] - bin_edges[0])
     >>> bin_centers = bin_edges[1:] - bin_width/2
 
+    >>> plt.figure()
     >>> plt.hist(samples, bins=50, normed=True, histtype='stepfilled', alpha=0.2,
     ...          label='histogram of data')
     >>> plt.plot(x, x_pdf, 'r-', label='analytical pdf')
