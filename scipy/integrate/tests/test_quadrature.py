a543ccba5f5f0dd38f7d6339de2f072cdf439fb6
bewithaman <bewithaman@outlook.com>
2015-01-15 13:39:27 +0530

    Added test cases for simps function

--- scipy/integrate/tests/test_quadrature.py
+++ scipy/integrate/tests/test_quadrature.py
@@ -65,7 +65,7 @@ class TestQuadrature(TestCase):
         assert_almost_equal(val, 47.474999999999, decimal=6)
 
         #testing for extended method:
-        sample = [1, 3, 4, 4.5 ,10, 5.5, 4.6, 2, 3]
+        sample = [1, 3, 4, 4.5, 10, 5.5, 4.6, 2, 3]
         diff = 2.5
         val = simps(sample, dx=diff, method='extended')
         #test when equidistant sample are given.
