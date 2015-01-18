0aa9886adc8eda1515c193015f7f73354f89d1db
Sturla Molden <sturla@molden.no>
2015-01-18 21:12:38 +0100

    MAINT: remove NumPy deprecation define

--- scipy/spatial/ckdtree/src/ckdtree_query.cxx
+++ scipy/spatial/ckdtree/src/ckdtree_query.cxx
@@ -4,7 +4,13 @@
  * SciPy license
  */
 
-#define NPY_NO_DEPRECATED_API NPY_1_9_API_VERSION
+/*
+ * This would break SciPy with NumPy 1.6 so just accept the compiler
+ * warning for now.
+ * #define NPY_NO_DEPRECATED_API NPY_1_9_API_VERSION 
+ *
+ */
+ 
 #include <Python.h>
 #include "numpy/arrayobject.h"
 
