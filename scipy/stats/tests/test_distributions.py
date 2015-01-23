ea703b7af31428f2185cfd5e7bebbff500dffe36
maniteja123 <manitejanmt@gmail.com>
2015-01-23 03:16:21 -0500

    ENH: Improve rayleigh accuracy for large x.closes #4034 Included tests for rayleigh, lomax and truncexpon distributions

--- scipy/stats/tests/test_distributions.py
+++ scipy/stats/tests/test_distributions.py
@@ -2171,12 +2171,12 @@ def test_infinite_input():
 def test_lomax_accuracy():
     # regression test for gh-4033
     p = stats.lomax.ppf(stats.lomax.cdf(1e-100,1),1)
-    assert_almost_equal(p, 1e-100, decimal=15)
+    assert_allclose(p, 1e-100)
     
 def test_truncexpon_accuracy():
     # regression test for gh-4035
     p = stats.truncexpon.ppf(stats.truncexpon.cdf(1e-100,1),1)
-    assert_almost_equal(p, 1e-100, decimal=15)
+    assert_allclose(p, 1e-100)
 
 def test_rayleigh_accuracy():
     # regression test for gh-4034
