8a08d6bf735ec2a8b10d115c58dc726a1ab77ae3
e-q <eric.antonio.quintero@gmail.com>
2015-01-25 14:20:04 -0800

    BUG, STY: Various fixes for CSD

--- scipy/signal/spectral.py
+++ scipy/signal/spectral.py
@@ -267,138 +267,75 @@ def welch(x, fs=1.0, window='hanning', nperseg=256, noverlap=None, nfft=None,
     2.0077340678640727
 
     """
-    x = np.asarray(x)
-
-    if x.size == 0:
-        return np.empty(x.shape), np.empty(x.shape)
-
-    if axis != -1:
-        x = np.rollaxis(x, axis, len(x.shape))
-
-    if x.shape[-1] < nperseg:
-        warnings.warn('nperseg = %d, is greater than x.shape[%d] = %d, using '
-                      'nperseg = x.shape[%d]'
-                      % (nperseg, axis, x.shape[axis], axis))
-        nperseg = x.shape[-1]
-
-    if isinstance(window, string_types) or type(window) is tuple:
-        win = get_window(window, nperseg)
-    else:
-        win = np.asarray(window)
-        if len(win.shape) != 1:
-            raise ValueError('window must be 1-D')
-        if win.shape[0] > x.shape[-1]:
-            raise ValueError('window is longer than x.')
-        nperseg = win.shape[0]
-
-    # numpy 1.5.1 doesn't have result_type.
-    outdtype = (np.array([x[0]]) * np.array([1], 'f')).dtype.char.lower()
-    if win.dtype != outdtype:
-        win = win.astype(outdtype)
- 
-    if scaling == 'density':
-        scale = 1.0 / (fs * (win*win).sum())
-    elif scaling == 'spectrum':
-        scale = 1.0 / win.sum()**2
-    else:
-        raise ValueError('Unknown scaling: %r' % scaling)
-
-    if noverlap is None:
-        noverlap = nperseg // 2
-    elif noverlap >= nperseg:
-        raise ValueError('noverlap must be less than nperseg.')
-
-    if nfft is None:
-        nfft = nperseg
-    elif nfft < nperseg:
-        raise ValueError('nfft must be greater than or equal to nperseg.')
-
-    if not detrend:
-        detrend_func = lambda seg: seg
-    elif not hasattr(detrend, '__call__'):
-        detrend_func = lambda seg: signaltools.detrend(seg, type=detrend)
-    elif axis != -1:
-        # Wrap this function so that it receives a shape that it could
-        # reasonably expect to receive.
-        def detrend_func(seg):
-            seg = np.rollaxis(seg, -1, axis)
-            seg = detrend(seg)
-            return np.rollaxis(seg, axis, len(seg.shape))
-    else:
-        detrend_func = detrend
-
-    step = nperseg - noverlap
-    indices = np.arange(0, x.shape[-1]-nperseg+1, step)
-
-    if np.isrealobj(x) and return_onesided:
-        outshape = list(x.shape)
-        if nfft % 2 == 0:  # even
-            outshape[-1] = nfft // 2 + 1
-            Pxx = np.empty(outshape, outdtype)
-            for k, ind in enumerate(indices):
-                x_dt = detrend_func(x[..., ind:ind+nperseg])
-                xft = fftpack.rfft(x_dt*win, nfft)
-                # fftpack.rfft returns the positive frequency part of the fft
-                # as real values, packed r r i r i r i ...
-                # this indexing is to extract the matching real and imaginary
-                # parts, while also handling the pure real zero and nyquist
-                # frequencies.
-                if k == 0:
-                    Pxx[..., (0,-1)] = xft[..., (0,-1)]**2
-                    Pxx[..., 1:-1] = xft[..., 1:-1:2]**2 + xft[..., 2::2]**2
-                else:
-                    Pxx *= k/(k+1.0)
-                    Pxx[..., (0,-1)] += xft[..., (0,-1)]**2 / (k+1.0)
-                    Pxx[..., 1:-1] += (xft[..., 1:-1:2]**2 + xft[..., 2::2]**2) \
-                                    / (k+1.0)
-        else:  # odd
-            outshape[-1] = (nfft+1) // 2
-            Pxx = np.empty(outshape, outdtype)
-            for k, ind in enumerate(indices):
-                x_dt = detrend_func(x[..., ind:ind+nperseg])
-                xft = fftpack.rfft(x_dt*win, nfft)
-                if k == 0:
-                    Pxx[..., 0] = xft[..., 0]**2
-                    Pxx[..., 1:] = xft[..., 1::2]**2 + xft[..., 2::2]**2
-                else:
-                    Pxx *= k/(k+1.0)
-                    Pxx[..., 0] += xft[..., 0]**2 / (k+1)
-                    Pxx[..., 1:] += (xft[..., 1::2]**2 + xft[..., 2::2]**2) \
-                                  / (k+1.0)
-
-        Pxx[..., 1:-1] *= 2*scale
-        Pxx[..., (0,-1)] *= scale
-        f = np.arange(Pxx.shape[-1]) * (fs/nfft)
-    else:
-        for k, ind in enumerate(indices):
-            x_dt = detrend_func(x[..., ind:ind+nperseg])
-            xft = fftpack.fft(x_dt*win, nfft)
-            if k == 0:
-                Pxx = (xft * xft.conj()).real
-            else:
-                Pxx *= k/(k+1.0)
-                Pxx += (xft * xft.conj()).real / (k+1.0)
-        Pxx *= scale
-        f = fftpack.fftfreq(nfft, 1.0/fs)
-
-    if axis != -1:
-        Pxx = np.rollaxis(Pxx, -1, axis)
+    [f, Pxx] = csd(x, x, fs, window, nperseg, noverlap, nfft, detrend, 
+                   return_onesided, scaling, axis)
 
     return f, Pxx
 
 def coherence(x, y, fs=1.0, window='hanning', nperseg=256, noverlap=None,
-              nfft=None, detrend='constant', axis=-1, axisX=None, axisY=None):
+              nfft=None, detrend='constant', axis=-1):
+    """
+    Estimate the magnitude squared coherence estimate, Cxy, of discrete-time 
+    signals X and Y Welch's method. Cxy = abs(Pxy)**2/(Pxx*Pyy), where Pxx and 
+    Pyy are power spectral density estimates of X and Y, and Pxy is the cross
+    spectral density estimate of X and Y. 
 
-    if axisX is None: axisX = axis
-    if axisY is None: axisY = axis
+    Parameters
+    ----------
+    x : array_like
+        Time series of measurement values
+    y : array_like
+        Time series of measurement values
+    fs : float, optional
+        Sampling frequency of the `x` and `y` time series in units of Hz. 
+        Defaults to 1.0.
+    window : str or tuple or array_like, optional
+        Desired window to use. See `get_window` for a list of windows and
+        required parameters. If `window` is array_like it will be used
+        directly as the window and its length will be used for nperseg.
+        Defaults to 'hanning'.
+    nperseg : int, optional
+        Length of each segment.  Defaults to 256.
+    noverlap: int, optional
+        Number of points to overlap between segments. If None,
+        ``noverlap = nperseg / 2``.  Defaults to None.
+    nfft : int, optional
+        Length of the FFT used, if a zero padded FFT is desired.  If None,
+        the FFT length is `nperseg`. Defaults to None.
+    detrend : str or function or False, optional
+        Specifies how to detrend each segment. If `detrend` is a string,
+        it is passed as the ``type`` argument to `detrend`.  If it is a
+        function, it takes a segment and returns a detrended segment.
+        If `detrend` is False, no detrending is done.  Defaults to 'constant'.
+    axis : int, optional
+        Axis along which the CSD is computed for both inputs; the default is 
+        over the last axis (i.e. ``axis=-1``).
 
-    [ff,Pxx] = welch(x, fs=fs, window=window, nperseg=nperseg,
-                     noverlap=noverlap, nfft=nfft, detrend=detrend, axis=axisX)
-    [_, Pyy] = welch(y, fs=fs, window=window, nperseg=nperseg,
-                     noverlap=noverlap, nfft=nfft, detrend=detrend, axis=axisY)
-    [_, Pxy] = csd(x, y, fs=fs, window=window, nperseg=nperseg,
-                   noverlap=noverlap, nfft=nfft, detrend=detrend, axis=axis,
-                   axisX=axisX, axisY=axisY)
+    Returns
+    -------
+    f : ndarray
+        Array of sample frequencies.
+    Cxy : ndarray
+        Magnitude squared coherence of x and y.
+
+    See Also
+    --------
+    periodogram: Simple, optionally modified periodogram
+    lombscargle: Lomb-Scargle periodogram for unevenly sampled data
+    welch: Power spectral density by Welch's method.
+    csd: Cross spectral density by Welch's method. 
+
+    Notes
+    --------
+    An appropriate amount of overlap will depend on the choice of window
+    and on your requirements.  For the default 'hanning' window an
+    overlap of 50% is a reasonable trade off between accurately estimating
+    the signal power, while not over counting any of the data.  Narrower
+    windows may require a larger overlap.
+    """
+    [ff,Pxx] = welch(x, fs, window, nperseg, noverlap, nfft, detrend, axis)
+    [_, Pyy] = welch(y, fs, window, nperseg, noverlap, nfft, detrend, axis)
+    [_, Pxy] = csd(x, y, fs, window, nperseg, noverlap, nfft, detrend, axis)
 
     Cxy = np.abs(Pxy)**2/Pxx/Pyy
 
@@ -406,10 +343,9 @@ def coherence(x, y, fs=1.0, window='hanning', nperseg=256, noverlap=None,
 
 
 def csd(x, y, fs=1.0, window='hanning', nperseg=256, noverlap=None, nfft=None,
-        detrend='constant', return_onesided=True, scaling='density', axis=-1,
-        axisX=None, axisY=None):
+        detrend='constant', return_onesided=True, scaling='density', axis=-1):
     """
-    Estimate the cross spectral density using Welch's method. 
+    Estimate the cross power spectral density, Pxy, using Welch's method. 
 
     Parameters
     ----------
@@ -448,25 +384,45 @@ def csd(x, y, fs=1.0, window='hanning', nperseg=256, noverlap=None, nfft=None,
         computing the power spectrum ('spectrum') where Pxy has units of V**2
         if x and y are measured in V. Defaults to 'density'.
     axis : int, optional
-        Axis along which the periodogram is computed; the default is over
-        the last axis (i.e. ``axis=-1``).
-    axisX : int, optional
-        Axis along which the CSD is computed for the X data; defaults to the
-        'axis' keyword argument.
-    axisY : int, optional
-        Axis along which the CSD is computed for the Y data; defaults to the
-        'axis' keywrd argument.
+        Axis along which the CSD is computed for both inputs; the default is 
+        over the last axis (i.e. ``axis=-1``).
 
     Returns
     -------
     f : ndarray
         Array of sample frequencies.
     Pxy : ndarray
-        Power spectral density or power spectrum of x.
+        Cross spectral density or cross power spectrum of x,y.
 
+    See Also
+    --------
+    periodogram: Simple, optionally modified periodogram
+    lombscargle: Lomb-Scargle periodogram for unevenly sampled data
+    welch: Power spectral density by Welch's method. [Equivalent to csd(x,x)]
+    coherence: Magnitude squred coherence by Welch's method. 
+
+    Notes
+    --------
+    By convention, Pxy is computed with the conjugate FFT of X multiplied by 
+    the FFT of Y. 
+
+    An appropriate amount of overlap will depend on the choice of window
+    and on your requirements.  For the default 'hanning' window an
+    overlap of 50% is a reasonable trade off between accurately estimating
+    the signal power, while not over counting any of the data.  Narrower
+    windows may require a larger overlap.
+
+    If `noverlap` is 0, this method is equivalent to Bartlett's method [2]_.
+
+    References
+    ----------
+    .. [1] P. Welch, "The use of the fast Fourier transform for the
+           estimation of power spectra: A method based on time averaging
+           over short, modified periodograms", IEEE Trans. Audio
+           Electroacoust. vol. 15, pp. 70-73, 1967.
+    .. [2] M.S. Bartlett, "Periodogram Analysis and Continuous Spectra",
+           Biometrika, vol. 37, pp. 1-16, 1950.
     """
-    if axisX is None: axisX = axis
-    if axisY is None: axisY = axis
 
     if y is x: 
         psd = True
@@ -476,30 +432,32 @@ def csd(x, y, fs=1.0, window='hanning', nperseg=256, noverlap=None, nfft=None,
         x = np.asarray(x)
         y = np.asarray(y)
 
-    if x.size == 0 or y.size == 0:
+    if x.size == 0:
         return np.empty(x.shape), np.empty(x.shape)
+    if y.size == 0:
+        return np.empty(y.shape), np.empty(y.shape)
 
-    if axisX != -1:
-        x = np.rollaxis(x, axisX, len(x.shape))
-    if axisY != -1:
-        y = np.rollaxis(y, axisY, len(y.shape))
+    if axis != -1:
+        x = np.rollaxis(x, axis, len(x.shape))
+        y = np.rollaxis(y, axis, len(y.shape))
 
     if x.shape[-1] < nperseg:
         warnings.warn('nperseg = %d, is greater than x.shape[%d] = %d, using '
                       'nperseg = x.shape[%d]'
-                      % (nperseg, axisX, x.shape[axisX], axisX))
+                      % (nperseg, axis, x.shape[axis], axis))
         nperseg = x.shape[-1]
 
-    if psd is False:
+    # These checks only neccesary if x!=y
+    if not psd:
         if y.shape[-1] < nperseg:
             warnings.warn('nperseg = %d, is greater than y.shape[%d] = %d, '
                           'using nperseg = y.shape[%d]'
-                          % (nperseg, axisY, y.shape[axisY], axisY))
+                          % (nperseg, axis, y.shape[axis], axis))
             nperseg = y.shape[-1]
 
         # Check if we can broadcast the remaining axes together
         for a, b in zip(x.shape[-2::-1],y.shape[-2::-1]):
-            if a==1 or b==1 or a==b:
+            if a == 1 or b == 1 or a == b:
                 pass
             else:
                 raise ValueError('x and y cannot be broadcast together.')
@@ -581,8 +539,8 @@ def csd(x, y, fs=1.0, window='hanning', nperseg=256, noverlap=None, nfft=None,
         for k, ind in enumerate(indices):
             x_dt = detrend_func(x[..., ind:ind+nperseg])
             xft = fftpack.rfft(x_dt*win, nfft)
-            if psd is True:
-                yft=xft
+            if psd:
+                yft = xft
             else:
                 y_dt = detrend_func(y[..., ind:ind+nperseg])
                 yft = fftpack.rfft(y_dt*win, nfft)
@@ -593,29 +551,29 @@ def csd(x, y, fs=1.0, window='hanning', nperseg=256, noverlap=None, nfft=None,
             # parts, while also handling the pure real zero and nyquist
             # frequencies.
             if k == 0:
-                Pxy[...,0] = xft[0]*yft[0]
+                Pxy[...,0] = xft[...,0]*yft[...,0]
                 if nfft % 2 == 0:
-                    centralProduct = ((xft[1:-1:2] - 1j*xft[2:-1:2]) 
-                                      * (yft[1:-1:2] + 1j*yft[2:-1:2]))
-                    Pxy[...,-1] = xft[-1]*yft[-1]
+                    product = ((xft[...,1:-1:2] - 1j*xft[...,2:-1:2]) 
+                                      * (yft[...,1:-1:2] + 1j*yft[...,2:-1:2]))
+                    Pxy[...,-1] = xft[...,-1]*yft[...,-1]
                 else:
-                    centralProduct = ((xft[1::2] - 1j*xft[2::2]) 
-                                      * (yft[1::2] + 1j*yft[2::2]))
+                    product = ((xft[...,1::2] - 1j*xft[...,2::2]) 
+                                      * (yft[...,1::2] + 1j*yft[...,2::2]))
 
-                Pxy[...,1:1+len(centralProduct)] = centralProduct
+                Pxy[...,1:1+product.shape[-1]] = product
 
             else:
                 Pxy *= k/(k+1.0)
                 Pxy[...,0] += xft[0]*yft[0] / (k+1.0)
                 if nfft % 2 == 0:
-                    centralProduct = ((xft[1:-1:2] - 1j*xft[2:-1:2]) 
-                                      * (yft[1:-1:2] + 1j*yft[2:-1:2]))
-                    Pxy[...,-1] += xft[-1]*yft[-1] / (k+1.0)
+                    product = ((xft[...,1:-1:2] - 1j*xft[...,2:-1:2]) 
+                                      * (yft[...,1:-1:2] + 1j*yft[...,2:-1:2]))
+                    Pxy[...,-1] += xft[...,-1]*yft[...,-1] / (k+1.0)
                 else:
-                    centralProduct = ((xft[1::2] - 1j*xft[2::2]) 
-                                      * (yft[1::2] + 1j*yft[2::2]))
+                    product = ((xft[...,1::2] - 1j*xft[...,2::2]) 
+                                      * (yft[...,1::2] + 1j*yft[...,2::2]))
 
-                Pxy[...,1:1+len(centralProduct)] += centralProduct / (k+1.0)
+                Pxy[...,1:1+product.shape[-1]] += product / (k+1.0)
 
         Pxy[..., 1:-1] *= 2*scale
         Pxy[..., (0,-1)] *= scale
@@ -624,8 +582,8 @@ def csd(x, y, fs=1.0, window='hanning', nperseg=256, noverlap=None, nfft=None,
         for k, ind in enumerate(indices):
             x_dt = detrend_func(x[..., ind:ind+nperseg])
             xft = fftpack.fft(x_dt*win, nfft)
-            if psd is True:
-                yft=xft
+            if psd:
+                yft = xft
             else:
                 y_dt = detrend_func(y[..., ind:ind+nperseg])
                 yft = fftpack.fft(y_dt*win, nfft)
@@ -635,10 +593,12 @@ def csd(x, y, fs=1.0, window='hanning', nperseg=256, noverlap=None, nfft=None,
             else:
                 Pxy *= k/(k+1.0)
                 Pxy += (xft.conj() * yft) / (k+1.0)
+
         Pxy *= scale
+        Pxy = Pxy.astype(outdtype)
         f = fftpack.fftfreq(nfft, 1.0/fs)
 
-    if psd is True:
+    if psd:
         Pxy = Pxy.real
 
     if axis != -1:
