a93c747d0d098ecc56698596f4c206743c7e36ac
Raoul Bourquin <raoul.bourquin@sam.math.ethz.ch>
2015-03-03 15:11:44 +0100

    Initial implementation of h_roots_asy

--- scipy/special/orthogonal.py
+++ scipy/special/orthogonal.py
@@ -85,8 +85,11 @@ from __future__ import division, print_function, absolute_import
 
 # Scipy imports.
 import numpy as np
-from numpy import all, any, exp, inf, pi, sqrt
+from numpy import (any, exp, inf, pi, sqrt, floor, sin, cos, array, around,
+                   int, ceil, flipud, hstack, arccos, dot, column_stack,
+                   row_stack, ones_like, zeros_like, floating, arange)
 from scipy import linalg
+from scipy.special import airy
 
 # Local imports.
 from . import _ufuncs as cephes
@@ -531,6 +534,238 @@ def h_roots(n, mu=False):
     f = cephes.eval_hermite
     df = lambda n, x: 2.0 * n * cephes.eval_hermite(n-1, x)
     return _gen_roots_and_weights(m, mu0, an_func, bn_func, f, df, True, mu)
+
+
+def compute_tauk(n, k, steps=10):
+    """Helper function for lemma 3.1
+    """
+    a = n % 2 - 0.5
+    c = (4.0*floor(n/2.0) - 4.0*k + 3.0)*pi / (4.0*floor(n/2.0) + 2.0*a + 2.0)
+    f = lambda x: x - sin(x) - c
+    df = lambda x: 1.0 - cos(x)
+    xi = 0.5*pi
+    for i in xrange(steps):
+        xi = xi - f(xi)/df(xi)
+    return xi
+
+
+def initial_nodes_a(n, k):
+    """Lemma 3.1 (Tricomi initial guesses)
+    """
+    tauk = compute_tauk(n, k)
+    sigk = cos(0.5*tauk)**2
+    a = n % 2 - 0.5
+    nu = 4.0*floor(n/2.0) + 2.0*a + 2.0
+    # Initial approximation of Hermite roots (square)
+    xksq = nu*sigk - 1.0/(3.0*nu) * (5.0/(4.0*(1.0-sigk)**2) - 1.0/(1.0-sigk) - 0.25)
+    return xksq
+
+
+def compute_am(m):
+    """Compute approximations to the roots of the Airy Ai(x) function.
+    This formula is exact for m > 10.
+    """
+    sm = 3*pi*(4*m-1) / 8.0
+    coeffs = row_stack([1.0, 5.0/48.0, -5.0/36.0, 77125.0/82944.0,
+                        -108056875.0/6967296.0, 162375596875.0/334430208.0])
+    smp = column_stack([ones_like(sm), sm**(-2), sm**(-4), sm**(-6), sm**(-8), sm**(-10)])
+    am = -sm**(2.0/3.0) * dot(smp, coeffs).reshape((-1,))
+    return am
+
+
+def airy_root(m):
+    """Compute approximations to all roots of the Airy Ai(x) function.
+    """
+    airyroots = array([
+            -2.3381074104597670385,
+             -4.0879494441309706166,
+             -5.5205598280955510591,
+             -6.7867080900717589988,
+             -7.9441335871208531231,
+             -9.0226508533409803802,
+             -10.040174341558085931,
+             -11.008524303733262893,
+             -11.936015563236262517,
+             -12.828776752865757200])
+
+    am = zeros_like(m, dtype=floating)
+    I = m <= 10
+    am[I] = airyroots[m[I]-1]
+    J = m > 10
+    am[J] = compute_am(m[J])
+    return am
+
+
+def initial_nodes_b(n, k):
+    """Lemma 3.2 (Gatteschi initial guesses)
+    """
+    a = n % 2 - 0.5
+    nu = 4.0*floor(n/2.0) + 2.0*a + 2.0
+    # Airy roots by approximation
+    ak = airy_root(k)
+    # Initial approximation of Hermite roots (square)
+    xksq = (nu +
+            2.0**(2.0/3.0) * ak * nu**(1.0/3.0) +
+            1.0/5.0 * 2.0**(4.0/3.0) * ak**2 * nu**(-1.0/3.0) +
+            (9.0/140.0 - 12.0/175.0 * ak**3) * nu**(-1.0) +
+            (16.0/1575.0 * ak + 92.0/7875.0 * ak**4) * 2.0**(2.0/3.0) * nu**(-5.0/3.0) -
+            (15152.0/3031875.0 * ak**5 + 1088.0/121275.0 * ak**2) * 2.0**(1.0/3.0) * nu**(-7.0/3.0))
+    return xksq
+
+
+def initial_values(n):
+    """Compute initial approximetaion to the Hermite roots of order n.
+
+    Both initial guesses are used for the region where they are
+    accurate and then combined.
+    """
+    # Turnover point
+    # linear polynomial fit to error of 10, 25, 40, ..., 1000 point rules
+    fit = 0.49082003*n - 4.37859653
+    turnover = around(fit).astype(int)
+    # Compute all approximations
+    ia = arange(1, int(floor(n*0.5)+1))
+    ib = flipud(arange(1, int(1+n-ceil(n*0.5))))
+    xasq = initial_nodes_a(n, ia[:turnover+1])
+    xbsq = initial_nodes_b(n, ib[turnover+1:])
+    # Combine
+    iv = sqrt(hstack([xasq, xbsq]))
+    # Central node is always zero
+    if n % 2 == 1:
+        iv = hstack([0.0, iv])
+    return iv
+
+
+def pbcf(n, theta):
+    """Asymptotic series expansion of parabolic cylinder function.
+    """
+    mu = sqrt(2.0*n + 1.0)
+    st = sin(theta)
+    ct = cos(theta)
+    # http://dlmf.nist.gov/12.10#vii
+    mu = 2.0*n + 1.0
+    # http://dlmf.nist.gov/12.10#E23
+    eta = 0.5*theta - 0.5*st*ct
+    # http://dlmf.nist.gov/12.10#E39
+    zeta = -(3.0*eta/2.0) ** (2.0/3.0)
+    # http://dlmf.nist.gov/12.10#E40
+    phi = (-zeta / st**2) ** (0.25)
+    # Coefficients
+    # http://dlmf.nist.gov/12.10#E43
+    a0 =  1.0
+    a1 =  0.10416666666666666667
+    a2 =  0.083550347222222222222
+    a3 =  0.12822657455632716049
+    b0 =  1.0
+    b1 = -0.14583333333333333333
+    b2 = -0.098741319444444444444
+    b3 = -0.14331205391589506173
+    a = (a0, a1, a2, a3)
+    b = (b0, b1, b2, b3)
+    # Polynomials
+    # http://dlmf.nist.gov/12.10#E9
+    # http://dlmf.nist.gov/12.10#E10
+    u0 = 1.0
+    u1 = (    1.0*ct**3 -     6.0*ct) / 24.0
+    u2 = (   -9.0*ct**4 +   249.0*ct**2 +   145.0) / 1152.0
+    u3 = (-4042.0*ct**9 + 18189.0*ct**7 - 28287.0*ct**5 -151995.0*ct**3 - 259290.0*ct) / 414720.0
+    v0 = 1.0
+    v1 = (    1.0*ct**3 +     6.0*ct) / 24.0
+    v2 = (   15.0*ct**4 -   327.0*ct**2 - 143.0) / 1152.0
+    v3 = (-4042.0*ct**9 + 18189.0*ct**7 - 36387.0*ct**5 + 238425.0*ct**3 + 259290.0*ct) / 414720.0
+    u = (u0, u1, u2, u3)
+    v = (v0, v1, v2, v3)
+    # Airy Evaluation (Bi and Bip unused)
+    Ai, Aip, Bi, Bip = airy(mu**(4.0/6.0) * zeta)
+    # Prefactor for U
+    P = 2.0*sqrt(pi) * mu**(1.0/6.0) * phi
+    # Terms for U
+    # http://dlmf.nist.gov/12.10#E42
+    A0 =   b[0]*u[0]
+    A1 =  (b[2]*u[0] + phi**6*b[1]*u[1] + phi**12*b[0]*u[2]) / zeta**3
+    B0 =-(a[1]*u[0] + phi**6*a[0]*u[1]) / zeta**2
+    B1 = -(a[3]*u[0] + phi**6*a[2]*u[1] + phi**12*a[1]*u[2] + phi**18*a[0]*u[3]) / zeta**5
+    # U
+    # http://dlmf.nist.gov/12.10#E35
+    U = P * (Ai  * (A0 + A1/mu**2.0) +
+             Aip * (B0 + B1/mu**2.0) / mu**(8.0/6.0))
+    # Prefactor for derivative of U
+    Pd = sqrt(2.0*pi) * mu**(2.0/6.0) / phi
+    # Terms for derivative of U
+    # http://dlmf.nist.gov/12.10#E46
+    C0 = -(b[1]*v[0] + phi**6*b[0]*v[1]) / zeta
+    C1 = -(b[3]*v[0] + phi**6*b[2]*v[1] + phi**12*b[1]*v[2] + phi**18*b[0]*v[3]) / zeta**4
+    D0 =   a[0]*v[0]
+    D1 =  (a[2]*v[0] + phi**6*a[1]*v[1] + phi**12*a[0]*v[2]) / zeta**3
+    # Derivative of U
+    # http://dlmf.nist.gov/12.10#E36
+    Ud = Pd * (Ai  * (C0 + C1/mu**2.0) / mu**(4.0/6.0) +
+               Aip * (D0 + D1/mu**2.0) )
+    return U, Ud
+
+
+def newton(n, x_initial, maxit=5):
+    """Newton iteration for polishing the asymptotic approximation
+    to the zeros of the Hermite polynomials.
+    """
+    # Variable transformation
+    mu = sqrt(2.0*n + 1.0)
+    t = x_initial / mu
+    theta = arccos(t)
+    # Newton iteration
+    for i in xrange(maxit):
+        u, ud = pbcf(n, theta)
+        dtheta = u / (sqrt(2.0) * mu * sin(theta) * ud)
+        theta = theta + dtheta
+        if max(abs(dtheta)) < 1e-14:
+            break
+    # Undo variable transformation
+    x = mu * cos(theta)
+    # Central node is always zero
+    if n % 2 == 1:
+        x[0] = 0.0
+    # Compute weights
+    w = exp(-x**2) / (2.0*ud**2)
+    return x, w
+
+
+def h_roots_asy(n):
+    """Gauss-Hermite (physicst's) quadrature for large n
+
+    Computes the sample points and weights for Gauss-Hermite quadrature.
+    The sample points are the roots of the `n`th degree Hermite polynomial,
+    :math:`H_n(x)`.  These sample points and weights correctly integrate
+    polynomials of degree :math:`2*n - 1` or less over the interval
+    :math:`[-inf, inf]` with weight function :math:`f(x) = e^{-x^2}`.
+
+    Parameters
+    ----------
+    n : int
+        quadrature order
+
+    Returns
+    -------
+    x : ndarray
+        Sample points
+    w : ndarray
+        Weights
+
+    See Also
+    --------
+    h_roots
+    """
+    iv = initial_values(n)
+    nodes, weights = newton(n, iv)
+    # Combine with negative parts
+    if n % 2 == 0:
+        nodes = hstack([-flipud(nodes), nodes])
+        weights = hstack([flipud(weights), weights])
+    else:
+        nodes = hstack([-flipud(nodes[1:]), nodes])
+        weights = hstack([flipud(weights[1:]), weights])
+    # Scale weights
+    weights *= sqrt(pi) / sum(weights)
+    return nodes, weights
 
 
 def hermite(n, monic=False):
