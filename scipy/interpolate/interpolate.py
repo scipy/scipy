b32681c7411cbd048b4ba516cd29a0368187748c
Steven Byrnes <steven.byrnes@gmail.com>
2015-03-04 13:11:48 -0500

    DOC - add example to RegularGridInterpolator

--- scipy/interpolate/interpolate.py
+++ scipy/interpolate/interpolate.py
@@ -1464,6 +1464,36 @@ class RegularGridInterpolator(object):
 
     .. versionadded:: 0.14
 
+    Example
+    -------
+
+    Evaluate a simple example function on the points of a 3D grid:
+
+    >>> from scipy.interpolate import RegularGridInterpolator
+    >>> def f(x,y,z):
+    >>>     return 2 * x**3 + 3 * y**2 - z
+    >>> x = np.linspace(1, 4, 11)
+    >>> y = np.linspace(4, 7, 22)
+    >>> z = np.linspace(7, 9, 33)
+    >>> data = np.zeros((11, 22, 33))
+    >>> for i in range(11):
+    >>>     for j in range(22):
+    >>>         for k in range(33):
+    >>>             data[i,j,k] = f(x[i], y[j], z[k])
+
+    Define an interpolating function from this data:
+
+    >>> my_interpolating_function = RegularGridInterpolator((x,y,z), data)
+
+    Evaluate the interpolating function at the two points
+    (x,y,z) = (2.1, 6.2, 8.3) and (3.3, 5.2, 7.1):
+
+    >>> pts = np.array([[2.1, 6.2, 8.3], [3.3, 5.2, 7.1]])
+    >>> my_interpolating_function(pts)
+    array([ 125.80469388,  146.30069388])
+    
+    which is indeed a close approximation to [f(2.1,6.2,8.3), f(3.3,5.2,7.1)].
+
     See also
     --------
     NearestNDInterpolator : Nearest neighbour interpolation on unstructured
