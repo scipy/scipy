faffee172e0e7f946a1cd1e447fbc55898cd71f5
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2015-03-06 12:40:21 -0800

    ENH: Make better use of the numpy API
    A copy of the array is now created explicitly at the beginning of
    procrustes and with these a few operations were improved. In addition
    the readability of the check for zero-division was improved.
    
    Additionally the readability of the data validation was improved through a
    better use of the numpy API.

--- scipy/spatial/procrustes.py
+++ scipy/spatial/procrustes.py
@@ -97,27 +97,29 @@ def procrustes(data1, data2):
 
     """
 
-    data1 = np.asarray(data1, dtype=np.double)
-    data2 = np.asarray(data2, dtype=np.double)
+    mtx1 = np.array(data1, dtype=np.double, copy=True)
+    mtx2 = np.array(data2, dtype=np.double, copy=True)
 
-    if data1.ndim != 2 or data2.ndim != 2:
+    if mtx1.ndim != 2 or mtx2.ndim != 2:
         raise ValueError("Input matrices must be two-dimensional")
-    num_rows, num_cols = np.shape(data1)
-    if (num_rows, num_cols) != np.shape(data2):
+    if mtx1.shape != mtx2.shape:
         raise ValueError("Input matrices must be of same shape")
-    if num_rows == 0 or num_cols == 0:
+    if mtx1.size == 0:
         raise ValueError("Input matrices must be >0 rows and >0 cols")
 
     # translate all the data to the origin
-    mtx1 = data1-np.mean(data1, 0)
-    mtx2 = data2-np.mean(data2, 0)
+    mtx1 -= np.mean(mtx1, 0)
+    mtx2 -= np.mean(mtx2, 0)
 
-    if (not np.any(mtx1)) or (not np.any(mtx2)):
+    norm1 = np.linalg.norm(mtx1)
+    norm2 = np.linalg.norm(mtx2)
+
+    if norm1 == 0 or norm2 == 0:
         raise ValueError("Input matrices must contain >1 unique points")
 
     # change scaling of data (in rows) such that trace(mtx*mtx') = 1
-    mtx1 = mtx1/np.linalg.norm(mtx1)
-    mtx2 = mtx2/np.linalg.norm(mtx2)
+    mtx1 /= norm1
+    mtx2 /= norm2
 
     # transform mtx2 to minimize disparity
     R, s = orthogonal_procrustes(mtx1, mtx2)
