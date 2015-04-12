8ef942c3fc6b058465e44498ae509e8491c406a8
Paul Ortyl <ortylp@ortyl.org>
2015-04-12 22:02:30 +0200

    Add documentation for skipping unpack() and seek() at EOF in _skip_unknown_chunk()

--- scipy/io/wavfile.py
+++ scipy/io/wavfile.py
@@ -87,6 +87,10 @@ def _skip_unknown_chunk(fid):
         fmt = '<i'
 
     data = fid.read(4)
+    # call unpack() and seek() only if we have really read data from file
+    # otherwise empty read at the end of the file would trigger 
+    # unnecessary exception at unpack() call
+    # in case data equals somehow to 0, there is no need for seek() anyway
     if data:
         size = struct.unpack(fmt, data)[0]
         fid.seek(size, 1)
