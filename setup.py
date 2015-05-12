e153b5d374f888a4e22e7c2edf8b533ea6c0281a
Ralf Gommers <ralf.gommers@googlemail.com>
2015-05-12 23:06:21 +0200

    REL: set version to 0.16.0b1

--- setup.py
+++ setup.py
@@ -51,8 +51,8 @@ Operating System :: MacOS
 MAJOR = 0
 MINOR = 16
 MICRO = 0
-ISRELEASED = False
-VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
+ISRELEASED = True
+VERSION = '%d.%d.%db1' % (MAJOR, MINOR, MICRO)
 
 
 # Return the git revision as a string
