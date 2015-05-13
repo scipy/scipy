7e81a647f92c8fc3b9d3029e0a313b9e499e9fb4
Anne Archibald <peridot.faceted@gmail.com>
2015-05-13 08:25:03 +0200

    BUG: fix travis failure on numpy <1.7.0

--- scipy/misc/tests/test_common.py
+++ scipy/misc/tests/test_common.py
@@ -149,9 +149,9 @@ def test_logsumexp_shape():
     a = np.ones((1,2,3,4))
     b = np.ones_like(a)
 
-    r = logsumexp(a, axis=(1,3), b=b)
+    r = logsumexp(a, axis=2, b=b)
 
-    assert_equal(r.shape, (1,3))
+    assert_equal(r.shape, (1,2,4))
     if NumpyVersion(np.__version__) >= NumpyVersion('1.7.0'):
         r = logsumexp(a, axis=(1,3), b=b)
 
