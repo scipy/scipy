e665c3be661b8c597a28cefde31dd070ee9c1802
Maniteja Nandana <manitejanmt@gmail.com>
2015-05-31 23:13:27 +0530

    BENCH: Benchmarks for interpolate module Added benchmarks for univariate and bivariate splines

--- benchmarks/benchmarks/interpolate.py
+++ benchmarks/benchmarks/interpolate.py
@@ -132,11 +132,11 @@ class Rbf(Benchmark):
 
     def setup(self, n_samples, function):
         self.x = np.arange(n_samples)
-        self.y = np.exp(-self.x / 3.0)
+        self.y = np.sin(self.x)
         r_samples = n_samples / 2.
         self.X = np.arange(-r_samples, r_samples, 0.25)
         self.Y = np.arange(-r_samples, r_samples, 0.25)
-        self.z = np.sin(self.X**2+self.Y**2)
+        self.z = np.exp(-self.X**2-self.Y**2)
 
     def time_rbf_1d(self, n_samples, function):
         interpolate.Rbf(self.x, self.y, function=function)
@@ -145,3 +145,51 @@ class Rbf(Benchmark):
         interpolate.Rbf(self.X, self.Y, self.z, function=function)
 
 
+class UnivariateSpline(Benchmark):
+    param_names = ['n_samples', 'degree']
+    params = [
+        [10, 50, 100],
+        [3, 4, 5]
+    ]
+
+    def setup(self, n_samples, degree):
+        r_samples = n_samples / 2.
+        self.x = np.arange(-r_samples, r_samples, 0.25)
+        self.y = np.exp(-self.x**2) + 0.1 * np.random.randn(*self.x.shape)
+
+    def time_univariate_spline(self, n_samples, degree):
+        interpolate.UnivariateSpline(self.x, self.y, k=degree)
+
+
+class BivariateSpline(Benchmark):
+    """
+    Created on Thu Nov 26 22:00:20 2009
+    Author: josef-pktd and scipy mailinglist example
+    """
+    param_names = ['n_samples']
+    params = [
+        [10, 20, 30]
+    ]
+
+    def setup(self, n_samples):
+        x = np.arange(0, n_samples, 0.5)
+        y = np.arange(0, n_samples, 0.5)
+        x, y = np.meshgrid(x, y)
+        x = x.ravel()
+        y = y.ravel()
+        xmin = x.min()-1
+        xmax = x.max()+1
+        ymin = y.min()-1
+        ymax = y.max()+1
+        s = 1.1
+        self.yknots = np.linspace(ymin+s,ymax-s,10)
+        self.xknots = np.linspace(xmin+s,xmax-s,10)
+        self.z = np.sin(x) + 0.1*np.random.normal(size=x.shape)
+        self.x = x
+        self.y = y
+
+    def time_smooth_bivariate_spline(self, n_samples):
+        interpolate.SmoothBivariateSpline(self.x, self.y, self.z)
+
+    def time_lsq_bivariate_spline(self, n_samples):
+        interpolate.LSQBivariateSpline(self.x, self.y, self.z, self.xknots.flat, self.yknots.flat)
