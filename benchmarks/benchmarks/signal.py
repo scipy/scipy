c51ec7f5a4ae528a5defd12ca230ea12c2fef84c
Maniteja Nandana <manitejanmt@gmail.com>
2015-06-01 01:57:43 +0530

    BENCH: Benchmarks for signal module Benchmarks for convolve, correlate, fftconvolve, some LTI functions and spectral analysis tools

--- benchmarks/benchmarks/signal.py
+++ benchmarks/benchmarks/signal.py
@@ -5,8 +5,7 @@ from itertools import product
 import numpy as np
 
 try:
-    from scipy.signal import (convolve2d, correlate2d, lti, lsim, lsim2, welch,
-                              csd)
+    import scipy.signal as signal
 except ImportError:
     pass
 
@@ -23,16 +22,30 @@ class CalculateWindowedFFT(Benchmark):
         self.y = y
 
     def time_welch(self):
-        welch(self.x)
+        signal.welch(self.x)
     
     def time_csd(self):
-        csd(self.x, self.y)
+        signal.csd(self.x, self.y)
+
+    def time_periodogram(self):
+        signal.periodogram(self.x)
+
+    def time_spectrogram(self):
+        signal.spectrogram(self.x)
+
+    def time_coherence(self):
+        signal.coherence(self.x, self.y)
 
 
 class Convolve2D(Benchmark):
-    def setup(self):
-        np.random.seed(1234)
+    param_names = ['mode', 'boundary']    
+    params = [
+        ['full', 'valid', 'same'],
+        ['fill', 'wrap', 'symm']
+    ]
 
+    def setup(self, mode, boundary):
+        np.random.seed(1234)
         # sample a bunch of pairs of 2d arrays
         pairs = []
         for ma, na, mb, nb in product((1, 2, 8, 13, 30), repeat=4):
@@ -41,28 +54,88 @@ class Convolve2D(Benchmark):
             pairs.append((a, b))
         self.pairs = pairs
 
-    def time_convolutions(self):
-        fns = (convolve2d, correlate2d)
-        modes = ('full', 'valid', 'same')
-        boundaries = ('fill', 'wrap', 'symm')
-
-        # compute 2d convolutions and correlations for each 2d array pair
+    def time_convolve2d(self, mode, boundary):
         for a, b in self.pairs:
-            for fn, mode, boundary in product(fns, modes, boundaries):
-                if mode == 'valid':
-                    if b.shape[0] > a.shape[0] or b.shape[1] > a.shape[1]:
-                        continue
-                fn(a, b, mode=mode, boundary=boundary)
+            if mode == 'valid':
+                if b.shape[0] > a.shape[0] or b.shape[1] > a.shape[1]:
+                    continue
+            signal.convolve2d(a, b, mode=mode, boundary=boundary)
+
+    def time_correlate2d(self, mode, boundary):
+        for a, b in self.pairs:
+            if mode == 'valid':
+                if b.shape[0] > a.shape[0] or b.shape[1] > a.shape[1]:
+                    continue
+            signal.correlate2d(a, b, mode=mode, boundary=boundary)
+
+class FFTConvolve(Benchmark):
+    param_names = ['mode']    
+    params = [
+        ['full', 'valid', 'same']
+    ]
+
+    def setup(self, mode):
+        np.random.seed(1234)
+        # sample a bunch of pairs of 2d arrays
+        pairs = []
+        for ma, nb in product((1, 2, 8, 13, 30, 36, 50, 75), repeat=2):
+            a = np.random.randn(ma)
+            b = np.random.randn(nb)
+            pairs.append((a, b))
+        self.pairs = pairs
+
+    def time_convolve2d(self, mode):
+        for a, b in self.pairs:
+            if b.shape[0] > a.shape[0]:
+                continue
+            signal.fftconvolve(a, b, mode=mode)
+
+class Convolve(Benchmark):
+    param_names = ['mode']    
+    params = [
+        ['full', 'valid', 'same']
+    ]
+
+    def setup(self, mode):
+        np.random.seed(1234)
+        # sample a bunch of pairs of 2d arrays
+        pairs = []
+        for ma, nb in product((1, 2, 8, 13, 30, 36, 50, 75), repeat=2):
+            a = np.random.randn(ma)
+            b = np.random.randn(nb)
+            pairs.append((a, b))
+        self.pairs = pairs
+
+    def time_convolve(self, mode):
+        for a, b in self.pairs:
+            if b.shape[0] > a.shape[0]:
+                continue
+            signal.convolve(a, b, mode=mode)
+
+    def time_correlate(self, mode):
+        for a, b in self.pairs:
+            if b.shape[0] > a.shape[0]:
+                continue
+            signal.correlate(a, b, mode=mode)
 
 
 class LTI(Benchmark):
     def setup(self):
-        self.system = lti(1.0, [1, 0, 1])
+        self.system = signal.lti(1.0, [1, 0, 1])
         self.t = np.arange(0, 100, 0.5)
         self.u = np.sin(2 * self.t)
 
     def time_lsim(self):
-        lsim(self.system, self.u, self.t)
+        signal.lsim(self.system, self.u, self.t)
 
     def time_lsim2(self):
-        lsim2(self.system, self.u, self.t)
+        signal.lsim2(self.system, self.u, self.t)
+
+    def time_step(self):
+        signal.step(self.system, T=self.t)
+
+    def time_impulse(self):
+        signal.impulse(self.system, T=self.t)
+
+    def time_bode(self):
+        signal.bode(self.system)
