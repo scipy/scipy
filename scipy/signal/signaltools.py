45f5da2ffda7787f3c30fd9e58769893f974f166
endolith <endolith@gmail.com>
2015-07-03 18:21:08 -0400

    DOC: resample example
    Add an example of resample, showing how it is circular.

--- scipy/signal/signaltools.py
+++ scipy/signal/signaltools.py
@@ -1687,6 +1687,22 @@ def resample(x, num, t=None, axis=0, window=None):
     slow if the number of input samples is large and prime, see
     `scipy.fftpack.fft`.
 
+    Examples
+    --------
+    Note that the end of the resampled data rises to meet the first
+    sample of the next cycle:
+
+    >>> from scipy import signal
+    
+    >>> x = np.linspace(0, 10, 20, endpoint=False)
+    >>> y = np.cos(-x**2/6.0)
+    >>> f = signal.resample(y, 100)
+    >>> xnew = np.linspace(0, 10, 100, endpoint=False)
+    
+    >>> import matplotlib.pyplot as plt
+    >>> plt.plot(x, y, 'go-', xnew, f, '.-', 10, y[0], 'ro')
+    >>> plt.legend(['data', 'resampled'], loc='best')
+    >>> plt.show()
     """
     x = asarray(x)
     X = fft(x, axis=axis)
