8afcc9c560c70bde4ccde7614214b444d1ab445b
Abraham Escalante <aeklant@gmail.com>
2015-07-05 21:27:05 -0500

    TST: add tests for 'dist' and 'brack' in `stats.ppcc_max`

--- scipy/stats/morestats.py
+++ scipy/stats/morestats.py
@@ -511,7 +511,7 @@ def ppcc_max(x, brack=(0.0, 1.0), dist='tukeylambda'):
 
     See also
     --------
-    ppcc_plot
+    ppcc_plot, probplot, boxcox
 
     References
     ----------
@@ -526,15 +526,15 @@ def ppcc_max(x, brack=(0.0, 1.0), dist='tukeylambda'):
     with shape parameter -0.7:
 
     >>> from scipy import stats
-    >>> import matplotlib.pyplot as plt
-    >>> np.random.seed(1234567)
-    >>> x = stats.tukeylambda.rvs(-0.7, loc=2, scale=0.5, size=10000) + 1e4
+    >>> x = stats.tukeylambda.rvs(-0.7, loc=2, scale=0.5, size=10000,
+    ...                           random_state=1234567) + 1e4
 
     Now we explore this data with a PPCC plot as well as the related
     probability plot and Box-Cox normplot.  A red line is drawn where we
     expect the PPCC value to be maximal (at the shape parameter -0.7 used
     above):
 
+    >>> import matplotlib.pyplot as plt
     >>> fig = plt.figure(figsize=(8, 6))
     >>> ax = fig.add_subplot(111)
     >>> stats.ppcc_plot(x, -5, 5, plot=ax)
