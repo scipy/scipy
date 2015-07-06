7a089a96ec35197af1ea44d3a7c8f46c8c3314f1
Nikolay Mayorov <n59_ru@hotmail.com>
2015-07-06 03:47:32 +0300

    ENH: Initial version of bounded LSQ algorithms

---
+++ scipy/optimize/_lsq_trf.py
@@ -0,0 +1,358 @@
+"""Trust Region Reflective algorithm for least-squares optimization."""
+
+from __future__ import division
+
+import numpy as np
+from numpy.linalg import norm
+from scipy.linalg import svd
+
+from .optimize import OptimizeResult
+from ._lsq_bounds import (step_size_to_bound, make_strictly_feasible,
+                          find_active_constraints, scaling_vector)
+from ._lsq_trust_region import intersect_trust_region, solve_lsq_trust_region
+
+
+def minimize_quadratic(a, b, l, u):
+    """Minimize a 1-d quadratic function subject to bounds.
+
+    The free term is omitted, that is we consider y = a * t**2 + b * t.
+
+    Returns
+    -------
+    t : float
+        The minimum point.
+    y : float
+        The minimum value.
+    """
+    t = np.array([l, u])
+    if a != 0:
+        extremum = -0.5 * b / a
+        if l <= extremum <= u:
+            t = np.hstack((t, extremum))
+    y = a * t**2 + b * t
+    i = np.argmin(y)
+    return t[i], y[i]
+
+
+def build_1d_quadratic_function(J, diag, g, s, s0=None):
+    """Compute coefficients of a 1-d quadratic function for the line search
+    from the multidimensional quadratic function.
+
+    The function is given as follows:
+    ``f(t) = 0.5 * (s0 + t*s).T * (J.T*J + diag) * (s0 + t*s) +
+             g.T * (s0 + t*s)``.
+
+    Parameters
+    ----------
+    J : ndarray, shape (m, n)
+        Jacobian matrix.
+    diag : ndarray, shape (n,)
+        Addition diagonal term in a quadratic part.
+    g : ndarray, shape (n,)
+        Gradient, defines a linear term.
+    s : ndarray, shape (n,)
+        Direction of search.
+    s0 : None or ndarray with shape (n,), optional
+        Initial point. If None assumed to be 0.
+
+    Returns
+    ------
+    a : float
+        Coefficient for t**2.
+    b : float
+        Coefficient for t.
+
+    Notes
+    -----
+    The free term "c" is not returned as it is not usually required.
+    """
+    v = J.dot(s)
+    a = 0.5 * (np.dot(v, v) + np.dot(s * diag, s))
+    b = np.dot(g, s)
+    if s0 is not None:
+        u = J.dot(s0)
+        b += np.dot(u, v) + np.dot(s0 * diag, s)
+
+    return a, b
+
+
+def evaluate_quadratic_function(J, diag, g, steps):
+    """Compute values of a quadratic function arising in least-squares.
+
+    The function is 0.5 * s.T * (J.T * J + diag) * s + g.T * s.
+
+    Parameters
+    ----------
+    J : ndarray, shape (m, n)
+        Jacobian matrix.
+    diag : ndarray, shape (n,)
+        Additional diagonal term.
+    f : ndarray, shape (m,)
+        Vector of residuals.
+    steps : ndarray, shape (k, m)
+        Array containing k steps as rows.
+
+    Returns
+    -------
+    values : ndarray, shape (k,)
+        Array containing k values of the function.
+    """
+    Js = J.dot(steps.T)
+    return 0.5 * (np.sum(Js**2, axis=0) +
+                  np.sum(diag * steps**2, axis=1)) + np.dot(steps, g)
+
+
+def find_reflected_step(x, J_h, diag_h, g_h, p, p_h, d, Delta, l, u, theta):
+    """Find a single reflection step for Trust Region Reflective algorithm.
+
+    Also corrects the initial step p/p_h. This function must be called only
+    if x + p is not within the bounds.
+    """
+    # Use term "stride" for scalar step length.
+    p_stride, hits = step_size_to_bound(x, p, l, u)
+
+    # Compute the reflected direction.
+    r_h = np.copy(p_h)
+    r_h[hits.astype(bool)] *= -1
+    r = d * r_h
+
+    # Restrict the p step to exactly the bound.
+    p *= p_stride
+    p_h *= p_stride
+    x_on_bound = x + p
+
+    # Reflected direction will cross first either feasible region or trust
+    # region boundary.
+    _, to_tr = intersect_trust_region(p_h, r_h, Delta)
+    to_bound, _ = step_size_to_bound(x_on_bound, r, l, u)
+    to_bound *= theta  # Stay strictly interior.
+
+    r_stride_u = min(to_bound, to_tr)
+
+    # We want a reflected step be at the same theta distance from the bound,
+    # so we introduce a lower bound on the allowed stride.
+    # The formula below is correct as p_h and r_h has the same norm.
+
+    if r_stride_u > 0:
+        r_stride_l = (1 - theta) * p_stride / r_stride_u
+    else:
+        r_stride_l = -1
+
+    # Check if no reflection step is available.
+    if r_stride_l <= r_stride_u:
+        a, b = build_1d_quadratic_function(J_h, diag_h, g_h, r_h, s0=p_h)
+        r_stride, _ = minimize_quadratic(a, b, r_stride_l, r_stride_u)
+        r_h = p_h + r_h * r_stride
+    else:
+        r_h = None
+
+    # Now we want to correct p_h to make it strictly interior.
+    p_h *= theta
+
+    # If no reflection step just return p_h for convenience.
+    if r_h is None:
+        return p_h, p_h
+    else:
+        return p_h, r_h
+
+
+def find_gradient_step(x, J_h, diag_h, g_h, d, Delta, l, u, theta):
+    """Find a minimizer of a quadratic model along the scaled gradient."""
+    to_bound, _ = step_size_to_bound(x, -g_h * d, l, u)
+    to_bound *= theta
+
+    to_tr = Delta / norm(g_h)
+    g_stride = min(to_bound, to_tr)
+
+    a, b = build_1d_quadratic_function(J_h, diag_h, g_h, -g_h)
+    g_stride, _ = minimize_quadratic(a, b, 0.0, g_stride)
+
+    return -g_stride * g_h
+
+
+def trf(fun, jac, x0, lb, ub, ftol, xtol, gtol, max_nfev, scaling):
+    """Minimize the sum of squares of nonlinear functions subject to bounds on
+    independent variables by Trust Region Reflective algorithm.
+
+    Options
+    -------
+    ftol : float
+        The optimization process is stopped when ``dF < ftol * F`` and
+        dF_actual / dF_predicted > 0.25, where F is the objective function
+        value (the sum of squares), dF_actual is its change in the last
+        iteration, dF_predicted is predicted change from a local quadratic
+        model.
+    xtol : float, optional
+        The optimization process is stopped when
+        ``norm(dx) < xtol * max(EPS**0.5, norm(x))``, where dx is a step taken
+        in the last iteration and EPS is machine epsilon.
+    gtol : float, optional
+        The optimization process is stopped when
+        ``norm(g_scaled, ord=np.inf) < gtol``, where g_scaled is properly
+        scaled gradient to account for the presence of bounds.
+        The scaling imposed by `scaling` parameter is not considered.
+    max_nfev : None or int, optional
+        Maximum number of function evaluations before the termination.
+        If None (default), it is assigned to 100 * n.
+    """
+    EPS = np.finfo(float).eps
+
+    # We need strictly feasible guess to start with
+    x = make_strictly_feasible(x0, lb, ub, rstep=1e-10)
+
+    f = fun(x)
+    nfev = 1
+
+    J = jac(x, f)
+    njev = 1
+
+    if f.shape[0] != J.shape[0]:
+        raise RuntimeError("Inconsistent dimensions between the returns of "
+                           "`fun` and `jac` on the first iteration.")
+
+    g = J.T.dot(f)
+    m, n = J.shape
+
+    if scaling == 'jac':
+        J_norm = np.sum(J**2, axis=0)**0.5
+        J_norm[J_norm == 0] = 1
+        scale = 1 / J_norm
+    else:
+        scale = 1 / scaling
+
+    v, jv = scaling_vector(x, g, lb, ub)
+    Delta = norm(x0 / (scale * v**0.5))
+    if Delta == 0:
+        Delta = 1.0
+
+    J_augmented = np.empty((m + n, n))
+    f_augmented = np.zeros((m + n))
+
+    obj_value = np.dot(f, f)
+    alpha = 0.0  # "Levenberg-Marquardt" parameter
+
+    if max_nfev is None:
+        max_nfev = x0.size * 100
+
+    termination_status = None
+    while nfev < max_nfev:
+        if scaling == 'jac':
+            J_norm = np.sum(J**2, axis=0)**0.5
+            with np.errstate(divide='ignore'):
+                scale = np.minimum(scale, 1 / J_norm)
+
+        g = J.T.dot(f)
+
+        # Compute Coleman-Li scaling parameters and "hat" variables.
+        v, jv = scaling_vector(x, g, lb, ub)
+        d = v**0.5 * scale
+        g_h = d * g
+        diag_h = g * jv * scale**2
+
+        g_norm = norm(g * v, ord=np.inf)
+        if g_norm < gtol:
+            termination_status = 1
+
+        if termination_status is not None:
+            active_mask = find_active_constraints(x, lb, ub, rtol=xtol)
+            return OptimizeResult(
+                x=x, fun=f, jac=J, obj_value=obj_value, optimality=g_norm,
+                active_mask=active_mask, nfev=nfev, njev=njev,
+                status=termination_status, x_covariance=None)
+
+        # Jacobian in "hat" space.
+        J_h = J * d
+
+        # J_augmented is used to solve a trust-region subproblem with
+        # diagonal term diag_h.
+        J_augmented[:m] = J_h
+        J_augmented[m:] = np.diag(diag_h**0.5)
+        f_augmented[:m] = f
+
+        U, s, V = svd(J_augmented, full_matrices=False)
+        V = V.T
+        uf = U.T.dot(f_augmented)
+
+        # theta controls step back size from the bounds.
+        theta = max(0.995, 1 - g_norm)
+        actual_reduction = -1
+
+        # In the following: p - trust-region solution, r - reflected solution,
+        # c - minimizer along the scaled gradient, _h means the variable
+        # is computed in "hat" space.
+        while actual_reduction <= 0 and nfev < max_nfev:
+            p_h, alpha, n_iter = solve_lsq_trust_region(
+                n, m, uf, s, V, Delta, initial_alpha=alpha)
+            p = d * p_h
+
+            to_bound, _ = step_size_to_bound(x, p, lb, ub)
+            if to_bound >= 1:  # Trust region step is feasible.
+                # Still step back from the bounds
+                p_h *= min(theta * to_bound, 1)
+                steps_h = np.atleast_2d(p_h)
+            else:  # Otherwise consider a reflected and gradient steps.
+                p_h, r_h = find_reflected_step(x, J_h, diag_h, g_h, p, p_h,
+                                               d, Delta, lb, ub, theta)
+                c_h = find_gradient_step(x, J_h, diag_h, g_h,
+                                         d, Delta, lb, ub, theta)
+                steps_h = np.array([p_h, r_h, c_h])
+
+            qp_values = evaluate_quadratic_function(J_h, diag_h, g_h, steps_h)
+            min_index = np.argmin(qp_values)
+            step_h = steps_h[min_index]
+
+            # qp-values are negative, also need to double it
+            predicted_reduction = -2 * qp_values[min_index]
+
+            step = d * step_h
+            x_new = make_strictly_feasible(x + step, lb, ub)
+
+            f_new = fun(x_new)
+            nfev += 1
+
+            # Usual trust-region step quality estimation.
+            obj_value_new = np.dot(f_new, f_new)
+            actual_reduction = obj_value - obj_value_new
+            # Correction term is specific to the algorithm,
+            # vanishes in unbounded case.
+            correction = np.dot(step_h * diag_h, step_h)
+
+            if predicted_reduction > 0:
+                ratio = (actual_reduction - correction) / predicted_reduction
+            else:
+                ratio = 0
+
+            if ratio < 0.25:
+                Delta_new = 0.25 * norm(step_h)
+                alpha *= Delta / Delta_new
+                Delta = Delta_new
+            elif ratio > 0.75 and norm(step_h) > 0.95 * Delta:
+                Delta *= 2.0
+                alpha *= 0.5
+
+            ftol_satisfied = (abs(actual_reduction) < ftol * obj_value and
+                              ratio > 0.25)
+            xtol_satisfied = norm(step) < xtol * max(EPS**0.5, norm(x))
+
+            if ftol_satisfied and xtol_satisfied:
+                termination_status = 4
+            elif ftol_satisfied:
+                termination_status = 2
+            elif xtol_satisfied:
+                termination_status = 3
+            if termination_status is not None:
+                break
+
+        if actual_reduction > 0:
+            x = x_new
+            f = f_new
+            obj_value = obj_value_new
+
+            J = jac(x, f)
+            njev += 1
+
+    active_mask = find_active_constraints(x, lb, ub, rtol=xtol)
+    return OptimizeResult(
+        x=x, fun=f, jac=J, obj_value=obj_value, optimality=g_norm,
+        active_mask=active_mask, nfev=nfev, njev=njev, status=0,
+        x_covariance=None)
---
+++ scipy/optimize/tests/test_least_squares.py
@@ -0,0 +1,307 @@
+import numpy as np
+from numpy.testing import (run_module_suite, assert_, assert_allclose,
+                           TestCase, assert_raises, assert_equal,
+                           assert_almost_equal)
+
+from scipy.optimize import least_squares
+from scipy.optimize._minpack import error as MinpackError
+
+
+def fun_trivial(x, a=0):
+    return (x - a)**2 + 5.0
+
+def jac_trivial(x, a=0.0):
+    return 2 * (x - a)
+
+
+def fun_2d_trivial(x):
+    return np.array([x[0], x[1]])
+
+def jac_2d_trivial(x):
+    return np.identity(2)
+
+
+def fun_rosenbrock(x):
+    return np.array([10 * (x[1] - x[0]**2), (1 - x[0])])
+
+def jac_rosenbrock(x):
+    return np.array([
+        [-20 * x[0], 10],
+        [-1, 0]
+    ])
+
+
+def jac_rosenbrock_transposed(x):
+    return jac_rosenbrock(x).T
+
+
+def jac_rosenbrock_bad_dim(x):
+    return np.array([
+        [-20 * x[0], 10],
+        [-1, 0],
+        [0.0, 0.0]
+    ])
+
+
+# When x 1-d array, return is 2-d array..
+def fun_wrong_dimensions(x):
+    return np.array([x, x**2, x**3])
+
+
+def jac_wrong_dimensions(x, a=0.0):
+    return np.atleast_3d(jac_trivial(x, a=a))
+
+#
+# Parametrize basic smoke tests across methods
+#
+
+
+class BaseMixin(object):
+    def test_basic(self):
+        # Test that the basic calling sequence works.
+        res = least_squares(fun_trivial, 2., method=self.method)
+        assert_allclose(res.x, 0, atol=1e-4)
+        assert_allclose(res.fun, fun_trivial(res.x))
+
+    def test_args_kwargs(self):
+        # Test that args and kwargs are passed correctly to the functions.
+        # And that kwargs are not supported by 'lm'.
+        a = 3.0
+        for jac in ['2-point', '3-point', jac_trivial]:
+            res = least_squares(fun_trivial, 2.0, jac, args=(a,),
+                                method=self.method)
+            assert_allclose(res.x, a, rtol=1e-4)
+            assert_allclose(res.fun, fun_trivial(res.x, a))
+
+            assert_raises(TypeError, least_squares, fun_trivial, 2.0,
+                          args=(3, 4,), method=self.method)
+        
+            # Test that kwargs works for everything except 'lm.
+            if self.method == 'lm':
+                assert_raises(ValueError, least_squares, fun_trivial, 2.0,
+                              kwargs={'a': a}, method=self.method)
+            else:
+                res = least_squares(fun_trivial, 2.0, jac, kwargs={'a': a},
+                                    method=self.method)
+                assert_allclose(res.x, a, rtol=1e-4)
+                assert_allclose(res.fun, fun_trivial(res.x, a))
+                assert_raises(TypeError, least_squares, fun_trivial, 2.0,
+                              kwargs={'kaboom': 3}, method=self.method)
+
+    def test_jac_options(self):
+        for jac in ['2-point', '3-point', jac_trivial]:
+            res = least_squares(fun_trivial, 2.0, jac, method=self.method)
+            assert_allclose(res.x, 0, atol=1e-4)
+        assert_raises(ValueError, least_squares, fun_trivial, 2.0, jac='oops',
+                      method=self.method)
+
+    def test_nfev_options(self):
+        for max_nfev in [None, 20]:
+            res = least_squares(fun_trivial, 2.0, max_nfev=max_nfev,
+                                method=self.method)
+            assert_allclose(res.x, 0, atol=1e-4)
+
+    def test_scaling_options(self):
+        for scaling in [1.0, np.array([2.0]), 'jac']:
+            res = least_squares(fun_trivial, 2.0, scaling=scaling)
+            assert_allclose(res.x, 0)
+        assert_raises(ValueError, least_squares, fun_trivial,
+                      2.0, scaling='auto', method=self.method)
+        assert_raises(ValueError, least_squares, fun_trivial,
+                      2.0, scaling=-1.0, method=self.method)
+
+    def test_diff_step(self):
+        # res1 and res2 should be equivalent.
+        # res2 and res3 should be different.
+        res1 = least_squares(fun_trivial, 2.0, diff_step=1e-2,
+                             method=self.method)
+        res2 = least_squares(fun_trivial, 2.0, diff_step=-1e-2,
+                             method=self.method)
+        res3 = least_squares(fun_trivial, 2.0,
+                             diff_step=None, method=self.method)
+        assert_allclose(res1.x, 0, atol=1e-4)
+        assert_allclose(res2.x, 0, atol=1e-4)
+        assert_allclose(res3.x, 0, atol=1e-4)
+        assert_equal(res1.x, res2.x)
+        assert_equal(res1.nfev, res2.nfev)
+        assert_(res2.nfev != res3.nfev)
+
+    def test_incorrect_options_usage(self):
+        assert_raises(TypeError, least_squares, fun_trivial, 2.0,
+                      method=self.method, options={'no_such_option': 100})
+        assert_raises(TypeError, least_squares, fun_trivial, 2.0,
+                      method=self.method, options={'max_nfev': 100})
+
+    def test_full_result(self):
+        res = least_squares(fun_trivial, 2.0, method=self.method)
+        # Use assert_almost_equal to check shapes of arrays too.
+        assert_almost_equal(res.x, np.array([0]), decimal=1)
+        assert_almost_equal(res.obj_value, 25)
+        assert_almost_equal(res.fun, np.array([5]))
+        assert_almost_equal(res.jac, np.array([[0.0]]), decimal=2)
+        # 'lm' works weired on this problem
+        assert_almost_equal(res.optimality, 0, decimal=3)
+        assert_equal(res.active_mask, np.array([0]))
+        if self.method == 'lm':
+            assert_(res.nfev < 25)
+        else:
+            assert_(res.nfev < 10)
+        if self.method == 'lm':
+            assert_(res.njev is None)
+        else:
+            assert_(res.njev < 10)
+        assert_(res.status > 0)
+        assert_(res.success)
+
+    def test_rosenbrock(self):
+        x0 = [-2, 1]
+        x_opt = [1, 1]
+        for scaling in [1.0, np.array([1.0, 5.0]), 'jac']:
+            for jac in ['2-point', '3-point', jac_rosenbrock]:
+                res = least_squares(fun_rosenbrock, x0, jac, scaling=scaling,
+                                    method=self.method)
+                assert_allclose(res.x, x_opt)
+
+    def test_fun_wrong_dimensions(self):
+        if self.method == 'lm':
+            error = MinpackError
+        else:
+            error = RuntimeError
+        assert_raises(error, least_squares, fun_wrong_dimensions,
+                      2.0, method=self.method)
+
+    def test_jac_wrong_dimensions(self):
+        if self.method == 'lm':
+            error = MinpackError
+        else:
+            error = RuntimeError
+        assert_raises(error, least_squares, fun_trivial,
+                      2.0, jac_wrong_dimensions, method=self.method)
+
+    def test_fun_and_jac_inconsistent_dimensions(self):
+        x0 = [1, 2]
+        if self.method == 'lm':
+            error = TypeError
+        else:
+            error = RuntimeError
+        assert_raises(error, least_squares, fun_rosenbrock, x0,
+                      jac_rosenbrock_bad_dim, method=self.method)
+
+    def test_x0_multidimensional(self):
+        x0 = np.ones(4).reshape(2, 2)
+        assert_raises(ValueError, least_squares, fun_trivial, x0,
+                      method=self.method)
+
+
+class BoundsMixin(object):
+    def test_inconsistent(self):
+        assert_raises(ValueError, least_squares, fun_trivial, 2.0,
+                      bounds=(10.0, 0.0), method=self.method)
+
+    def test_infeasible(self):
+        assert_raises(ValueError, least_squares, fun_trivial, 2.0,
+                      bounds=(3., 4), method=self.method)
+                                                 
+    def test_wrong_number(self):
+        assert_raises(ValueError, least_squares, fun_trivial, 2.,
+                      bounds=(1., 2, 3), method=self.method)
+
+    def test_inconsistent_shape(self):
+        assert_raises(ValueError, least_squares, fun_trivial, 2.0,
+                      bounds=(1.0, [2.0, 3.0]), method=self.method)
+        # 1-D array can't be broadcasted
+        assert_raises(ValueError, least_squares, fun_rosenbrock, [1.0, 2.0],
+                      bounds=([0.0], [3.0, 4.0]), method=self.method)
+
+    def test_in_bounds(self):
+        for jac in ['2-point', '3-point', jac_trivial]:
+            res = least_squares(fun_trivial, 2.0, jac=jac,
+                                bounds=(-1.0, 3.0), method=self.method)
+            assert_allclose(res.x, 0.0, atol=1e-4)
+            assert_equal(res.active_mask, [0])
+            assert_(-1 <= res.x <= 3)
+            res = least_squares(fun_trivial, 2.0, jac=jac,
+                                bounds=(0.5, 3.0), method=self.method)
+            assert_allclose(res.x, 0.5, atol=1e-4)
+            assert_equal(res.active_mask, [-1])
+            assert_(0.5 <= res.x <= 3)
+
+    def test_bounds_shape(self):
+        for jac in ['2-point', '3-point', jac_2d_trivial]:
+            x0 = [1.0, 1.0]
+            res = least_squares(fun_2d_trivial, x0, jac=jac)
+            assert_allclose(res.x, [0.0, 0.0])
+            res = least_squares(fun_2d_trivial, x0, jac=jac,
+                                bounds=(0.5, [2.0, 2.0]), method=self.method)
+            assert_allclose(res.x, [0.5, 0.5])
+            res = least_squares(fun_2d_trivial, x0, jac=jac,
+                                bounds=([0.3, 0.2], 3.0), method=self.method)
+            assert_allclose(res.x, [0.3, 0.2])
+            res = least_squares(
+                fun_2d_trivial, x0, jac=jac, bounds=([-1, 0.5], [1.0, 3.0]),
+                method=self.method)
+            assert_allclose(res.x, [0.0, 0.5], atol=1e-5)
+
+    def test_rosenbrock_bounds(self):
+        x0_1 = np.array([-2.0, 1.0])
+        x0_2 = np.array([2.0, 2.0])
+        x0_3 = np.array([-2.0, 2.0])
+        x0_4 = np.array([0.0, 2.0])
+        x0_5 = np.array([-1.2, 1.0])
+        problems = [
+            (x0_1, ([-np.inf, -1.5], np.inf)),
+            (x0_2, ([-np.inf, 1.5], np.inf)),
+            (x0_3, ([-np.inf, 1.5], np.inf)),
+            (x0_4, ([-np.inf, 1.5], [1.0, np.inf])),
+            (x0_2, ([1.0, 1.5], [3.0, 3.0])),
+            (x0_5, ([-50.0, 0.0], [0.5, 100]))
+        ]
+        for x0, bounds in problems:
+            for scaling in [1.0, [1.0, 2.0], 'jac']:
+                for jac in ['2-point', '3-point', jac_rosenbrock]:
+                    res = least_squares(fun_rosenbrock, x0, jac, bounds,
+                                        method=self.method, scaling=scaling)
+                    assert_allclose(res.optimality, 0.0, atol=1e-5)
+
+
+class TestDogbox(BaseMixin, BoundsMixin, TestCase):
+    method = 'dogbox'
+
+
+class TestTRF(BaseMixin, BoundsMixin, TestCase):
+    method = 'trf'
+
+
+class TestLM(BaseMixin, TestCase):
+    method = 'lm'
+
+    def test_bounds_not_supported(self):
+        assert_raises(ValueError, least_squares, fun_trivial,
+                      2.0, bounds=(-3.0, 3.0), method='lm')
+
+    def test_repeated_options_errors(self):
+        assert_raises(TypeError, least_squares, fun_trivial,
+                      2.0, options={'diag': 1.0}, method='lm')
+        assert_raises(TypeError, least_squares, fun_trivial,
+                      2.0, options={'epsfcn': 1e-10}, method='lm')
+
+    def test_colderiv(self):
+        x0 = [-2, 1]
+        x_opt = [1, 1]
+        res = least_squares(fun_rosenbrock, x0, jac_rosenbrock_transposed,
+                            method='lm', options={'col_deriv': True})
+        assert_allclose(res.obj_value, 0)
+        assert_allclose(res.x, x_opt)
+
+#
+# One-off tests which do not need parameterization or are method-specific
+#
+def test_basic():
+    # test that 'method' arg is really optional
+    res = least_squares(fun_trivial, 2.0)
+    assert_allclose(res.x, 0, atol=1e-10)
+    assert_allclose(res.fun, fun_trivial(res.x))
+
+
+if __name__ == "__main__":
+    run_module_suite()
