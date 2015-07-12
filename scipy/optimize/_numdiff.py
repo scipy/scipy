be058a4e0f3904ff11734b18a906e8bce2d23c15
Nikolay Mayorov <n59_ru@hotmail.com>
2015-07-12 16:10:11 +0300

    ENH: Implemented sparse finite differencing

--- scipy/optimize/_numdiff.py
+++ scipy/optimize/_numdiff.py
@@ -1,8 +1,9 @@
 """Routines for numerical differentiation."""
+
 from __future__ import division
 
 import numpy as np
-
+from scipy.sparse import issparse, csc_matrix, csr_matrix, lil_matrix, find
 
 EPS = np.finfo(np.float64).eps
 
@@ -15,7 +16,7 @@ def _adjust_scheme_to_bounds(x0, h, num_steps, scheme, lb, ub):
     x0 : ndarray, shape (n,)
         Point at which we wish to estimate derivative.
     h : ndarray, shape (n,)
-        Desired finite difference step.
+        Desired finite difference steps.
     num_steps : int
         Number of `h` steps in one direction required to implement finite
         difference scheme. For example, 2 means that we need to evaluate
@@ -27,14 +28,13 @@ def _adjust_scheme_to_bounds(x0, h, num_steps, scheme, lb, ub):
     lb : ndarray, shape (n,)
         Lower bounds on independent variables.
     ub : ndarray, shape (n,)
-        Upper bounds on independent variables, None means no bounds.
+        Upper bounds on independent variables.
 
     Returns
     -------
     h_adjusted : ndarray, shape (n,)
-        Adjusted step sizes. Step sizes decrease only if sign flips or
-        switching to one-sided scheme still don't allow to take full
-        steps.
+        Adjusted step sizes. Step size decreases only if a sign flip or
+        switching to one-sided scheme doesn't allow to take a full step.
     use_one_sided : ndarray of bool, shape (n,)
         Whether to switch to one-sided scheme. Informative only for
         ``scheme='2-sided'``.
@@ -97,7 +97,7 @@ def _compute_absolute_step(rel_step, x0, method):
             raise ValueError("`method` must be '2-point' or '3-point'.")
 
     sign_x0 = (x0 >= 0).astype(float) * 2 - 1
-    return np.atleast_1d(rel_step * sign_x0 * np.maximum(1.0, np.abs(x0)))
+    return rel_step * sign_x0 * np.maximum(1.0, np.abs(x0))
 
 
 def _prepare_bounds(bounds, x0):
@@ -111,14 +111,91 @@ def _prepare_bounds(bounds, x0):
     return lb, ub
 
 
+def group_columns(A, order=None):
+    """Group columns of a 2-d matrix for sparse finite differencing [1]_.
+
+    Two columns are in the same group if in each row at least one of them
+    has zero. A greedy sequential algorithm is used to construct groups.
+
+    Parameters
+    ----------
+    A : array_like or sparse matrix, shape (m, n)
+        Matrix of which to group columns.
+    order : None or iterable of int, shape (n,)
+        Permutation array which defines the order of columns enumeration.
+        If None (default) the order will be ``range(n)``.
+
+    Returns
+    -------
+    groups : ndarray of int, shape (n,)
+        Contains values from 0 to n_groups-1, where n_groups is the number
+        of found groups. Each value ``groups[i]`` is an index of a group to
+        which i-th column assigned. The procedure was helpful only if
+        n_groups is significantly less than n.
+
+    References
+    ----------
+    .. [1] A. Curtis, M. J. D. Powell, and J. Reid, "On the estimation of
+           sparse Jacobian matrices", Journal of the Institute of Mathematics
+           and its Applications, 13 (1974), pp. 117-120.
+    """
+    # Transpose for convenience.
+    if issparse(A):
+        A = csr_matrix(A.T)
+        sparse = True
+    else:
+        A = np.atleast_2d(A).T
+        sparse = False
+
+    if A.ndim != 2:
+        raise ValueError("`A` must be 2-dimensional.")
+
+    n, m = A.shape
+
+    if order is None:
+        order = range(n)
+
+    groups = -np.ones(n, dtype=int)
+
+    current_group = 0
+    for i in order:
+        if groups[i] >= 0:
+            continue
+
+        groups[i] = current_group
+        non_grouped, = np.where(groups < 0)
+
+        if non_grouped.size == 0:
+            break
+
+        if sparse:
+            union = A[i].toarray().ravel()
+        else:
+            union = A[i].copy()
+
+        for j in non_grouped:
+            column_j = A[j]
+            if sparse:
+                column_j = column_j.toarray().ravel()
+
+            if np.dot(union, column_j) == 0:
+                groups[j] = current_group
+                union += column_j
+
+        current_group += 1
+
+    return groups
+
+
 def approx_derivative(fun, x0, method='3-point', rel_step=None, f0=None,
-                      args=(), kwargs={}, bounds=(-np.inf, np.inf)):
+                      args=(), kwargs={}, bounds=(-np.inf, np.inf),
+                      sparsity=None):
     """Compute finite difference approximation of the derivatives of a
     vector-valued function.
 
     If a function maps from R^n to R^m, its derivatives form m-by-n matrix
-    called Jacobian, where an element (i, j) is equal to the partial derivative
-    of f[i] with respect to x[j].
+    called Jacobian, where an element (i, j) is a partial derivative of f[i]
+    with respect to x[j].
 
     Parameters
     ----------
@@ -153,13 +230,28 @@ def approx_derivative(fun, x0, method='3-point', rel_step=None, f0=None,
         Each bound must match the size of `x0` or be a scalar, in the latter
         case the bound will be the same for all variables. Use it to limit the
         range of function evaluation.
+    sparsity : None or (structure, groups) tuple, optional
+        Defines sparsity structure of Jacobian.
+
+        * structure : array_like or sparse matrix of shape (m, n). A zero
+          element means that a corresponding element of Jacobian identically
+          equals to zero.
+        * groups : array_like of shape (n,). Precomputed columns grouping
+          for a given sparsity structure, function `group_columns` should be
+          used beforehand to compute it.
+
+        Note, that sparse differencing makes sense only for actually large
+        and sparse Jacobians. If None (default) standard dense differencing
+        will be used.
 
     Returns
     -------
-    J : ndarray of shape (n, m) or shape (n,)
+    J : ndarray or csr_matrix
         Finite difference approximation of derivatives in the form of Jacobian
-        matrix. If m = 1 then it is returned as a gradient in the form of
-        1-d array. See notes for motivation of this choice.
+        matrix. If `sparse` is None then ndarray with shape (m, n) is
+        returned. Although if m=1 it is returned as a gradient with
+        shape (n,). If `sparse` is not None, csr_matrix with shape (m, n) is
+        returned.
 
     See Also
     --------
@@ -168,31 +260,31 @@ def approx_derivative(fun, x0, method='3-point', rel_step=None, f0=None,
     Notes
     -----
     If `rel_step` is not provided, it assigned to ``EPS**(1/s)``, where EPS is
-    machine epsilon for float64 numbers, s=2 if `method` = '2-point' and s=3
-    if `method` = '3-point'. Such choice of relative step approximately
-    minimizes the sum of truncation and round-off errors, see [1]_.
+    machine epsilon for float64 numbers, s=2 for '2-point' method and s=3 for
+    '3-point' method. Such relative step approximately minimizes a sum of
+    truncation and round-off errors, see [1]_.
 
-    A finite difference scheme for `method` = '3-point' is selected
-    automatically. The well-known central difference scheme is used for points
-    sufficiently far from the bounds, and 3-point forward or backward scheme
-    is used for points near the boundary. Both schemes have the second order
-    accuracy in terms of Taylor expansion. Refer to [2]_ for the formula of
-    3-point forward and backward difference scheme.
+    A finite difference scheme for '3-point' method is selected automatically.
+    The well-known central difference scheme is used for points sufficiently
+    far from the boundary, and 3-point forward or backward scheme is used for
+    points near the boundary. Both schemes have the second-order accuracy in
+    terms of Taylor expansion. Refer to [2]_ for the formulas of 3-point
+    forward and backward difference schemes.
 
-    In case when m=1 Jacobian is returned with a shape (n,), on the other hand
-    when n=1 Jacobian is returned with a shape (m, 1). Our motivation is the
-    following: a) It handles a case of gradient computation (m=1) in
-    conventional way. a) It clearly separates these two different cases.
-    b) We can call np.atleast_2d on the return value and get a 2-d Jacobian
-    matrix with correct dimensions.
+    For dense differencing when m=1 Jacobian is returned with a shape (n,),
+    on the other hand when n=1 Jacobian is returned with a shape (m, 1).
+    Our motivation is the following: a) It handles a case of gradient
+    computation (m=1) in a conventional way. b) It clearly separates these two
+    different cases. b) In all cases np.atleast_2d can be called to get 2-d
+    Jacobian with correct dimensions.
 
     References
     ----------
-    .. [1] William H. Press et. al. "Numerical Recipes. The Art of Scientific
-           Computing. 3rd edition", sec. 5.7
+    .. [1] W. H. Press et. al. "Numerical Recipes. The Art of Scientific
+           Computing. 3rd edition", sec. 5.7.
 
-    .. [2] Bengt Fornberg, "Generation of Finite Difference Formulas on
-           Arbitrarily Spaced Grids", Mathematics of Computation 51, 1988
+    .. [2] B. Fornberg, "Generation of Finite Difference Formulas on
+           Arbitrarily Spaced Grids", Mathematics of Computation 51, 1988.
 
     Examples
     --------
@@ -258,28 +350,43 @@ def approx_derivative(fun, x0, method='3-point', rel_step=None, f0=None,
         h, use_one_sided = _adjust_scheme_to_bounds(
             x0, h, 1, '2-sided', lb, ub)
 
+    if sparsity is None:
+        return _dense_difference(fun_wrapped, x0, f0, h, use_one_sided, method)
+    else:
+        structure, groups = sparsity
+        if issparse(structure):
+            structure = csc_matrix(structure)
+        else:
+            structure = np.atleast_2d(structure)
+        groups = np.atleast_1d(groups)
+        return _sparse_difference(fun_wrapped, x0, f0, h, use_one_sided,
+                                  structure, groups, method)
+
+
+def _dense_difference(fun, x0, f0, h, use_one_sided, method):
     m = f0.size
     n = x0.size
     J_transposed = np.empty((n, m))
     h_vecs = np.diag(h)
+
     for i in range(h.size):
         if method == '2-point':
             x = x0 + h_vecs[i]
             dx = x[i] - x0[i]  # recompute dx as exactly representable number
-            df = fun_wrapped(x) - f0
+            df = fun(x) - f0
         elif method == '3-point' and use_one_sided[i]:
             x1 = x0 + h_vecs[i]
             x2 = x0 + 2 * h_vecs[i]
             dx = x2[i] - x0[i]
-            f1 = fun_wrapped(x1)
-            f2 = fun_wrapped(x2)
-            df = -3 * f0 + 4 * f1 - f2
+            f1 = fun(x1)
+            f2 = fun(x2)
+            df = -3.0 * f0 + 4 * f1 - f2
         elif method == '3-point' and not use_one_sided[i]:
             x1 = x0 - h_vecs[i]
             x2 = x0 + h_vecs[i]
             dx = x2[i] - x1[i]
-            f1 = fun_wrapped(x1)
-            f2 = fun_wrapped(x2)
+            f1 = fun(x1)
+            f2 = fun(x2)
             df = f2 - f1
         J_transposed[i] = df / dx
 
@@ -289,8 +396,62 @@ def approx_derivative(fun, x0, method='3-point', rel_step=None, f0=None,
     return J_transposed.T
 
 
+def _sparse_difference(fun, x0, f0, h, use_one_sided,
+                       structure, groups, method):
+    m = f0.size
+    n = x0.size
+    J = lil_matrix((m, n), dtype=float)
+    n_groups = np.max(groups) + 1
+    for group in range(n_groups):
+        e = np.equal(group, groups)
+        h_vec = h * e
+        if method == '2-point':
+            x = x0 + h_vec
+            dx = x - x0
+            df = fun(x) - f0
+            cols, = np.where(e)
+            i, j, v = find(structure[:, cols])
+            j = cols[j]
+        elif method == '3-point':
+            x1 = x0.copy()
+            x2 = x0.copy()
+
+            mask_1 = use_one_sided & e
+            x1[mask_1] += h_vec[mask_1]
+            x2[mask_1] += 2 * h_vec[mask_1]
+
+            mask_2 = ~use_one_sided & e
+            x1[mask_2] -= h_vec[mask_2]
+            x2[mask_2] += h_vec[mask_2]
+
+            dx = np.zeros(n)
+            dx[mask_1] = x2[mask_1] - x0[mask_1]
+            dx[mask_2] = x2[mask_2] - x1[mask_2]
+
+            f1 = fun(x1)
+            f2 = fun(x2)
+
+            cols, = np.where(e)
+            i, j, _ = find(structure[:, cols])
+            j = cols[j]
+
+            mask = use_one_sided[j]
+            df = np.empty(m)
+
+            rows = i[mask]
+            df[rows] = -3 * f0[rows] + 4 * f1[rows] - f2[rows]
+
+            rows = i[~mask]
+            df[rows] = f2[rows] - f1[rows]
+
+        J[i, j] = df[i] / dx[j]
+
+    return csr_matrix(J)
+
+
 def check_derivative(fun, jac, x0, args=(), kwargs={},
-                     bounds=(-np.inf, np.inf)):
+                     bounds=(-np.inf, np.inf), sparse_diff=False,
+                     sparsity=None):
     """Check correctness of a function computing derivatives (Jacobian or
     gradient) by comparison with finite difference approximation.
 
@@ -308,13 +469,30 @@ def check_derivative(fun, jac, x0, args=(), kwargs={},
         Point at which to estimate the derivatives. Float will be converted
         to 1-d array.
     args, kwargs : tuple and dict, optional
-        Additional arguments passed to `fun`. Both empty by default.
-        The calling signature is ``fun(x, *args, **kwargs)``.
+        Additional arguments passed to `fun` and `jac`. Both empty by default.
+        The calling signature is ``fun(x, *args, **kwargs)`` and the same
+        for `jac`.
     bounds : 2-tuple of array_like, optional
         Lower and upper bounds on independent variables. Defaults to no bounds.
         Each bound must match the size of `x0` or be a scalar, in the latter
         case the bound will be the same for all variables. Use it to limit the
         range of function evaluation.
+    sparse_diff : boolean, optional
+        If `jac` returns a sparse matrix this flag indicates whether to
+        use sparse differencing for Jacobian estimation.
+    sparsity : None or (structure, groups) tuple, optional
+        If `jac` returns a sparse matrix and ``sparse_diff=True``, provide a
+        tuple with following elements:
+
+        * structure : array_like or sparse matrix of shape (m, n). A zero
+          element means that a corresponding element of Jacobian identically
+          equals to zero.
+        * groups : array_like of shape (n,). Precomputed columns grouping
+          for a given sparsity structure, function `group_columns` should be
+          used beforehand to compute it.
+
+        If None `structure` will be taken from evaluated `jac` and `groups`
+        will be computed.
 
     Returns
     -------
@@ -349,8 +527,23 @@ def check_derivative(fun, jac, x0, args=(), kwargs={},
     >>> check_derivative(f, jac, x0, args=(1, 2))
     2.4492935982947064e-16
     """
-    jac_computed = jac(x0, *args, **kwargs)
-    jac_diff = approx_derivative(fun, x0, args=args, kwargs=kwargs,
-                                 bounds=bounds)
-    abs_err = np.abs(jac_computed - jac_diff)
-    return np.max(abs_err / np.maximum(1, np.abs(jac_diff)))
+    J_to_test = jac(x0, *args, **kwargs)
+    if issparse(J_to_test) and sparse_diff:
+        if sparsity is None:
+            groups = group_columns(J_to_test)
+            sparsity = (J_to_test, groups)
+        J_diff = approx_derivative(fun, x0, args=args, kwargs=kwargs,
+                                   bounds=bounds, sparsity=sparsity)
+        J_to_test = csr_matrix(J_to_test)
+        abs_err = J_to_test - J_diff
+        i, j, abs_err_data = find(abs_err)
+        jac_diff_data = np.asarray(J_diff[i, j]).ravel()
+        return np.max(np.abs(abs_err_data) /
+                      np.maximum(1, np.abs(jac_diff_data)))
+    else:
+        J_diff = approx_derivative(
+            fun, x0, args=args, kwargs=kwargs, bounds=bounds)
+        if issparse(J_to_test):
+            J_to_test = J_to_test.toarray()
+        abs_err = np.abs(J_to_test - J_diff)
+        return np.max(abs_err / np.maximum(1, np.abs(J_diff)))
