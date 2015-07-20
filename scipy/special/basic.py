cbb2c31dde9b85bacd54eb9e5af2ecb261c60129
Warren Weckesser <warren.weckesser@gmail.com>
2015-07-20 18:17:31 -0400

    DOC: special: Add an example showing the relation of diric to the Fourier transform.

--- scipy/special/basic.py
+++ scipy/special/basic.py
@@ -75,6 +75,52 @@ def diric(x, n):
     ...     plt.title('diric, n={}'.format(n))
     >>> plt.show()
 
+    The following example demonstrates that `diric` gives the magnitudes
+    (modulo the sign and scaling) of the Fourier coefficients of a
+    rectangular pulse.
+
+    Suppress output of values that are effectively 0:
+
+    >>> np.set_printoptions(suppress=True)
+
+    Create a signal `x` of length `m` with `k` ones:
+
+    >>> m = 8
+    >>> k = 3
+    >>> x = np.zeros(m)
+    >>> x[:k] = 1
+
+    Use the FFT to compute the Fourier transform of `x`, and
+    inspect the magnitudes of the coefficients:
+
+    >>> np.abs(np.fft.fft(x))
+    array([ 3.        ,  2.41421356,  1.        ,  0.41421356,  1.        ,
+            0.41421356,  1.        ,  2.41421356])
+
+    Now find the same values (up to sign) using `diric`.  We multiply
+    by `k` to account for the different scaling conventions of
+    `numpy.fft.fft` and `diric`:
+
+    >>> theta = np.linspace(0, 2*np.pi, m, endpoint=False)
+    >>> k * special.diric(theta, k)
+    array([ 3.        ,  2.41421356,  1.        , -0.41421356, -1.        ,
+           -0.41421356,  1.        ,  2.41421356])
+
+    We get the exact Fourier transform if we multiply by the phase
+    factors ``np.exp(-1j*theta)``:
+
+    >>> np.fft.fft(x)
+    array([ 3.00000000+0.j        ,  1.70710678-1.70710678j,
+            0.00000000-1.j        ,  0.29289322+0.29289322j,
+            1.00000000+0.j        ,  0.29289322-0.29289322j,
+            0.00000000+1.j        ,  1.70710678+1.70710678j])
+
+    >>> np.exp(-1j*theta) * k * special.diric(theta, k)
+    array([ 3.00000000+0.j        ,  1.70710678-1.70710678j,
+            0.00000000-1.j        ,  0.29289322+0.29289322j,
+            1.00000000+0.j        ,  0.29289322-0.29289322j,
+           -0.00000000+1.j        ,  1.70710678+1.70710678j])
+
     """
     x, n = asarray(x), asarray(n)
     n = asarray(n + (x-x))
