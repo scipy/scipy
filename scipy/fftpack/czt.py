600b90aad7e1ad92c452f04f1a4ad3a553cf9288
Endolith <endolith@gmail.com>
2015-07-22 21:09:15 -0400

    DOC: Various docstring cleanups
    Add more examples, move notes to Notes section, clarify starting point
    is in complex plane, add references, add links to other functions, etc.

--- scipy/fftpack/czt.py
+++ scipy/fftpack/czt.py
@@ -12,7 +12,7 @@ Transforms
 ----------
 
 CZT : callable (x, axis=-1) -> array
-   define a chirp-z transform that can be applied to different signals
+   define a chirp z-transform that can be applied to different signals
 ZoomFFT : callable (x, axis=-1) -> array
    define a Fourier transform on a range of frequencies
 ScaledFFT : callable (x, axis=-1) -> array
@@ -22,7 +22,7 @@ Functions
 ---------
 
 czt : array
-   compute the chirp-z transform for a signal
+   compute the chirp z-transform for a signal
 zoomfft : array
    compute the Fourier transform on a range of frequencies
 scaledfft : array
@@ -62,31 +62,49 @@ def _validate_sizes(n, m):
 
 def czt_points(m, w=None, a=1, scale=None):
     """
-    Returns the points at which the Z-transform is computed when doing a CZT
+    The points at which the Z-transform is computed when doing a `CZT`
     with the same arguments.
 
     Parameters
     ----------
     m : int, optional
-        The number of points desired.  The default is the length of
-        the input data.
+        The number of points desired.
     w : complex, optional
         The ratio between points in each step.
     a : complex, optional
         The starting point in the complex plane.  The default is 1.
     scale : float, optional
         Frequency scaling factor. This is a simpler and more precise way
-        to specify FFT-like transforms along the unit circle.  For
+        to specify FFT-like transforms along a circle.  For
         instance, when assigning scale=0.5, the resulting FT will span
         half of the frequency range that an FFT would produce, at half of
-        the frequency step size.  This is an alternative to `w`, and both
-        cannot be specified at the same time.
+        the frequency step size.
+        This is an alternative to `w`, and both cannot be specified at the
+        same time.
 
     Returns
     -------
     out : ndarray
         The points in the Z plane at which the CZT samples the Z-transform,
         as complex numbers.
+
+    Examples
+    --------
+    Plot the points of a 16-point FFT:
+
+    >>> import matplotlib.pyplot as plt
+    >>> points = czt_points(16, a=1+0j, scale=1)
+    >>> plt.plot(real(points), imag(points), 'o')
+    >>> plt.margins(0.1, 0.1); plt.axis('equal')
+    >>> plt.show()
+
+    and a 91-point logarithmic spiral:
+
+    >>> points = czt_points(91, 0.995*exp(-1j*pi*.05), 0.8*exp(1j*pi/6))
+    >>> plt.plot(real(points), imag(points), 'o')
+    >>> plt.margins(0.1, 0.1); plt.axis('equal')
+    >>> plt.show()
+
     """
     m = _validate_sizes(1, m)
 
@@ -110,49 +128,27 @@ def czt_points(m, w=None, a=1, scale=None):
 
 class CZT(object):
     """
-    Chirp-Z Transform.
+    Create a chirp Z-transform function.
 
     Transform to compute the frequency response around a spiral.
     Objects of this class are callables which can compute the
-    chirp-z transform on their inputs.  This object precalculates the constant
+    chirp Z-transform on their inputs.  This object precalculates the constant
     chirps used in the given transform.
-
-    If `w` does not lie on the unit circle, then the transform will be
-    around a spiral with exponentially increasing radius.  Regardless,
-    angle will increase linearly.
-
-    The chirp-z transform can be faster than an equivalent FFT with
-    zero padding.  Try it with your own array sizes to see.
-
-    Its most common application is computing large prime-length Fourier
-    transforms in O(N log N) time, rather than the O(N**2) time required by
-    the direct DFT calculation used in `fft`.
-
-    The chirp-z transform is considerably less precise than the
-    equivalent zero-padded FFT, with differences on the order of 1e-7
-    from the direct transform rather than 1e-15 as
-    seen with zero-padding.  Accuracy is better when using `scale` rather than
-    `w`, as any error in `w` is accumulated for long data lengths, drifting
-    away from the unit circle, but `scale` is limited to transforms that are
-    on the unit circle.
-
-    See zoomfft for a friendly interface to partial FFT calculations.
 
     Parameters
     ----------
     n : int
-        The size of the signal
+        The size of the signal.
     m : int, optional
-        The number of points desired.  The default is the length of
-        the input data.
+        The number of output points desired.  The default is `n`.
     w : complex, optional
         The ratio between points in each step.  This must be precise or the
         accumulated error will degrade the tail of the output sequence.
     a : complex, optional
-        The starting point in the complex plane.  The default is 1.
+        The starting point in the complex plane.  The default is 1+0j.
     scale : float, optional
-        Frequency scaling factor. This is a simpler and more precise way
-        to specify FFT-like transforms along the unit circle.  For
+        Frequency scaling factor. This is a simpler and more accurate way
+        to specify FFT-like transforms along a circle.  For
         instance, when assigning scale=0.5, the resulting FT will span
         half of the frequency range that an FFT would produce, at half of
         the frequency step size.  This is an alternative to `w`, and both
@@ -161,8 +157,65 @@ class CZT(object):
     Returns
     -------
     f : CZT
-        callable object ``f(x, axis=-1)`` for computing the chirp-z transform
+        callable object ``f(x, axis=-1)`` for computing the chirp z-transform
         on `x`
+
+    See Also
+    --------
+    ZoomFFT : for a friendly interface to partial FFT calculations
+
+    Notes
+    -----
+    If `w` does not lie on the unit circle, then the transform will be
+    around a spiral with exponentially-increasing radius.  Regardless,
+    angle will increase linearly.
+
+    For transforms that do lie on the unit circle, accuracy is better when
+    using `scale` rather than `w`, as any numerical error in `w` is
+    accumulated for long data lengths, drifting away from the unit circle.
+
+    The chirp z-transform can be faster than an equivalent FFT with
+    zero padding.  Try it with your own array sizes to see.
+
+    However, the chirp z-transform is considerably less precise than the
+    equivalent zero-padded FFT, with differences on the order of 1e-7 from the
+    direct transform rather than 1e-15 as seen with zero-padding.
+
+    Its most common application is computing large prime-length Fourier
+    transforms in O(N log N) time, rather than the O(N**2) time required by
+    the direct DFT calculation used in `fft`.
+
+    References
+    ----------
+    .. [1] Leo I. Bluestein, "A linear filtering approach to the computation
+           of the discrete Fourier transform," Northeast Electronics Research
+           and Engineering Meeting Record 10, 218-219 (1968).
+    .. [2] Rabiner, Schafer, and Rader, "The chirp z-transform algorithm and
+           its application," Bell Syst. Tech. J. 48, 1249-1292 (1969).
+
+    Examples
+    --------
+    Compute multiple prime-length FFTs:
+
+    >>> a = np.random.rand(7)
+    >>> b = np.random.rand(7)
+    >>> c = np.random.rand(7)
+    >>> czt_7 = CZT(n=7)
+    >>> A = czt_7(a)
+    >>> B = czt_7(b)
+    >>> C = czt_7(c)
+
+    Display the points at which the FFT is calculated:
+
+    >>> czt_7.points()
+    array([ 1.00000000+0.j,  0.62348980+0.78183148j,
+      -0.22252093+0.97492791j, -0.90096887+0.43388374j,
+      -0.90096887-0.43388374j, -0.22252093-0.97492791j,
+       0.62348980-0.78183148j])
+    >>> import matplotlib.pyplot as plt
+    >>> plt.plot(real(czt_7.points()), imag(czt_7.points()), 'o')
+    >>> plt.show()
+
     """
     def __init__(self, n, m=None, w=None, a=1, scale=None):
         m = _validate_sizes(n, m)
@@ -199,17 +252,17 @@ class CZT(object):
         Parameters
         ----------
         x : array
-          The signal to transform.
+            The signal to transform.
         axis : int, optional
-          Array dimension to operate over.  The default is the final
-          dimension.
+            Array dimension to operate over.  The default is the final
+            dimension.
 
         Returns
         -------
         out : ndarray
-          An array of the same dimensions as `x`, but with the length of the
-          transformed axis set to `m`.  Note that this is a view on a much
-          larger array.
+            An array of the same dimensions as `x`, but with the length of the
+            transformed axis set to `m`.  Note that this is a view on a much
+            larger array.
         """
         x = np.asarray(x)
         if x.shape[axis] != self.n:
@@ -233,45 +286,55 @@ class CZT(object):
 
 class ZoomFFT(CZT):
     """
-    Zoom FFT transform.
+    Create a zoom FFT transform function.
 
-    This is a specialization of the chirp Z transform(CZT) for a set of
-    equally spaced frequencies.
-
-    Defines a Fourier transform for a set of equally spaced frequencies.
+    This is a specialization of the chirp Z-transform (`CZT`) for a set of
+    equally-spaced frequencies around the unit circle.
 
     Parameters
     ----------
     n : int
-        size of the signal
+        The size of the signal.
     fn : array_like, optional
-        A scalar or length-2 sequence giving the start and end frequencies.
-        If a scalar, use 0 to f1.
+        A length-2 sequence [f1, f2] giving the frequency range, or a scalar,
+        for which the range [0, fn] is assumed.
     m : int, optional
-        size of the output
+        The number of output points desired.  The default is `n`.
     Fs : float, optional
-        sampling frequency (default=2)
+        The sampling frequency. (default=2)
 
     Returns
     -------
-    A ZoomFFT instance
-        A callable object f(x, axis=-1) for computing the zoom FFT on x.
+    f : ZoomFFT
+        A callable object ``f(x, axis=-1)`` for computing the zoom FFT on `x`.
 
     Notes
     -----
     Sampling frequency is 1/dt, the time step between samples in the
-    signal x.  The unit circle corresponds to frequencies from 0 up
+    signal `x`.  The unit circle corresponds to frequencies from 0 up
     to the sampling frequency.  The default sampling frequency of 2
     means that f1, f2 values up to the Nyquist frequency are in the
     range [0, 1). For f1, f2 values expressed in radians, a sampling
     frequency of 1/pi should be used.
 
+    Remember that a zoom FFT can only interpolate the points of the existing
+    FFT.  It cannot help to resolve two separate nearby frequencies.
+    Frequency resolution can only be increased by increasing acquisition
+    time.
+
+    Examples
+    --------
     To plot the transform results use something like the following:
 
-        t = transform(len(x), f1, f2, m)
-        f = linspace(f1, f2, m)
-        y = t(x)
-        plot(f, y)
+    >>> t = linspace(0, 1, 1021)
+    >>> x = cos(2*pi*15*t) + sin(2*pi*17*t)
+    >>> f1, f2 = 5, 27
+    >>> transform = ZoomFFT(len(x), [f1, f2], len(x), 1021)
+    >>> X = transform(x)
+    >>> f = linspace(f1, f2, len(x))
+    >>> import matplotlib.pyplot as plt
+    >>> plt.plot(f, 20*log10(abs(X)))
+    >>> plt.show()
 
     """
     def __init__(self, n, fn, m=None, Fs=2):
@@ -293,7 +356,7 @@ class ZoomFFT(CZT):
 
 class ScaledFFT(CZT):
     """
-        Scaled FFT transform.
+    Create a scaled FFT transform function.
 
     Similar to FFT, where the frequency range is scaled and divided
     into m-1 equal steps.  Like the FFT, frequencies are arranged from
@@ -305,31 +368,41 @@ class ScaledFFT(CZT):
 
         fftshift(zoomfft(x, [-scale, scale*(m-2.)/m], m=m))
 
-    For example:
-
-        m, n = 10, len(x)
-        sf = ScaledFFT(n, m=m, scale=0.25)
-        X = fftshift(fft(x))
-        W = linspace(-8, 8*(n-2.)/n, n)
-        SX = fftshift(sf(x))
-        SW = linspace(-2, 2*(m-2.)/m, m)
-        plot(X, W, SX, SW)
-
     Parameters
     ----------
     n : int
         Size of the signal
     m : int, optional
-        The size of the output.
-        Default: m=n
+        The size of the output. Default: m=n
     scale : float, optional
-        Frequency scaling factor.
-        Default: scale=1.0
+        Frequency scaling factor. Default: scale=1.0
 
     Returns
     -------
-    callable f(x, axis=-1)
-        function for computing the scaled FFT on x.
+    f : ScaledFFT
+        callable function ``f(x, axis=-1)`` for computing the scaled FFT on `x`.
+
+    Examples
+    --------
+    Show the relationship between scaled FFT and zoom FFT:
+
+    >>> scale, m, n = 0.25, 200, 100
+    >>> v = np.zeros(n)
+    >>> v[[1, 5, 21]] = 1
+    >>> shift = scipy.fftpack.fftshift
+    >>> x = np.linspace(-0.5, 0.5 - 1./n, n)
+    >>> xz = np.linspace(-scale * 0.5, scale * 0.5 * (m - 2.) / m, m)
+    >>> import matplotlib.pyplot as plt
+    >>> plt.plot(x, shift(abs(fft(v))), label='fft')
+    >>> plt.plot(x, shift(abs(scaledfft(v))), 'g.', label='x1 scaled fft')
+    >>> plt.plot(xz, abs(zoomfft(v, [-scale, scale * (m - 2.) / m], m=m)),
+    ...          'b+', label='zoomfft')
+    >>> plt.plot(xz, shift(abs(scaledfft(v, m=m, scale=scale))),
+    ...          'rx', label='x' + str(scale) + ' scaled fft')
+    >>> plt.yscale('log')
+    >>> plt.legend(loc='best')
+    >>> plt.show()
+
     """
     def __init__(self, n, m=None, scale=1.0):
         m = _validate_sizes(n, m)
@@ -349,7 +422,7 @@ def scaledfft(x, m=None, scale=1.0, axis=-1):
     """
     Limited frequency FFT.
 
-    See ScaledFFT doc for details
+    See `ScaledFFT` doc for details
 
     Parameters
     ----------
@@ -376,7 +449,7 @@ def scaledfft(x, m=None, scale=1.0, axis=-1):
 
 def czt(x, m=None, w=None, a=1, scale=None, axis=-1):
     """
-    Compute the frequency response around a spiral.
+    Compute the frequency response around a spiral in the Z plane.
 
     Parameters
     ----------
@@ -402,13 +475,27 @@ def czt(x, m=None, w=None, a=1, scale=None, axis=-1):
         larger array.  To save space, you may want to call it as
         ``y = ascontiguousarray(czt(x))``
 
+    See Also
+    --------
+    zoomfft : for a friendlier interface to partial FFT calculations
+
     Notes
     -----
-    See zoomfft for a friendlier interface to partial FFT calculations.
-
-    If the transform needs to be repeated, use CZT to construct a
+    If the transform needs to be repeated, use `CZT` to construct a
     specialized transform function which can be reused without
     recomputing constants.
+
+    Examples
+    --------
+    CZT can be used to calculate even-length rfft:
+
+    >>> from numpy.fft import rfft
+    >>> a = np.random.rand(1024)
+    >>> r = rfft(a)
+    >>> c = czt(a, m=513, scale=0.5*513/512)
+    >>> allclose(c, r)
+    True
+
     """
     x = np.asarray(x)
     transform = CZT(x.shape[axis], m=m, w=w, a=a, scale=scale)
@@ -417,17 +504,17 @@ def czt(x, m=None, w=None, a=1, scale=None, axis=-1):
 
 def zoomfft(x, fn, m=None, Fs=2, axis=-1):
     """
-    Compute the Fourier transform of x for frequencies in [f1, f2].
+    Compute the DFT of `x` only for frequencies in range `fn`.
 
     Parameters
     ----------
     x : array
         The input signal.
     fn : array_like, optional
-        A scalar or length-2 sequence giving the frequency range. If a scalar,
-        the range 0-fn is assumed.
+        A length-2 sequence [f1, f2] giving the frequency range, or a scalar,
+        for which the range [0, fn] is assumed.
     m : int, optional
-        The number of points to evaluate.  The default is the length of x.
+        The number of points to evaluate.  The default is the length of `x`.
     Fs : float, optional
         The sampling frequency.  With a sampling frequency of
         10kHz for example, the range f1 and f2 can be expressed in kHz.
@@ -452,7 +539,7 @@ def zoomfft(x, fn, m=None, Fs=2, axis=-1):
 
         plot(linspace(f1, f2, m), abs(zoomfft(x, [f1, f2], m)))
 
-    If the transform needs to be repeated, use ZoomFFT to construct
+    If the transform needs to be repeated, use `ZoomFFT` to construct
     a specialized transform function which can be reused without
     recomputing constants.
     """
