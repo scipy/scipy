28d0292a84e519b720e675a653678a749356d863
Yu Feng <yfeng1@waterfall.dyn.berkeley.edu>
2015-08-03 18:08:52 -0700

    MAINT : numpy lint-nize _wrap function declaration

--- scipy/spatial/ckdtree/src/ckdtree_methods.h
+++ scipy/spatial/ckdtree/src/ckdtree_methods.h
@@ -278,7 +278,8 @@ minkowski:
 
 #endif /* __GNUC__ */
 
-inline const npy_float64 _wrap(const npy_float64 x, const npy_float64 box) {
+inline const npy_float64 
+_wrap(const npy_float64 x, const npy_float64 box) {
     const npy_float64 r = std::floor(x / box);
     return x - r * box;
 }
