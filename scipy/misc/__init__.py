03cbd20ae702c9563cbd4e8917c2a12403e472ec
Pavel Ponomarev <pavel.v.ponomarev@gmail.com>
2015-08-11 17:40:05 +0300

    ENH: add a serial pool of workers.
    Serial pool of workers mimics functionality of a parallel pool with methods .map() and .poolsize().

--- scipy/misc/__init__.py
+++ scipy/misc/__init__.py
@@ -34,6 +34,7 @@ systems that don't have PIL installed.
    lena - Get classic image processing example image Lena
    logsumexp - Compute the log of the sum of exponentials of input elements
    pade - Pade approximation to function as the ratio of two polynomials
+   SPool - Serial pool of workers (to unify interfaces with pools of parallel workers)
    toimage - Takes a numpy array and returns a PIL image
    source - Print function source code
    who - Print the Numpy arrays in the given dictionary
@@ -43,8 +44,9 @@ systems that don't have PIL installed.
 from __future__ import division, print_function, absolute_import
 
 __all__ = ['who', 'source', 'info', 'doccer',
-           'comb', 'factorial', 'factorial2', 'factorialk']
+           'comb', 'factorial', 'factorial2', 'factorialk', 'SPool']
 
+from ._spool import SPool
 from . import doccer
 from .common import *
 from numpy import who, source, info as _info
