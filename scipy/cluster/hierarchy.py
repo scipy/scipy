afe42b1da151878a489b1786e57da307ebb30ebe
Ralf Gommers <ralf.gommers@gmail.com>
2015-08-16 17:35:29 +0200

    DOC: add example to cluster.hierarchy.dendrogram docstring.  Closes gh-5163.

--- scipy/cluster/hierarchy.py
+++ scipy/cluster/hierarchy.py
@@ -2064,6 +2064,34 @@ def dendrogram(Z, p=30, truncate_mode=None, color_threshold=None,
           ``i``-th leaf node corresponds to an original observation.
           Otherwise, it corresponds to a non-singleton cluster.
 
+    See Also
+    --------
+    linkage, set_link_color_palette
+
+    Examples
+    --------
+    >>> from scipy.cluster import hierarchy
+    >>> import matplotlib.pyplot as plt
+
+    A very basic example:
+
+    >>> ytdist = np.array([662., 877., 255., 412., 996., 295., 468., 268.,
+    ...                    400., 754., 564., 138., 219., 869., 669.])
+    >>> Z = hierarchy.linkage(ytdist, 'single')
+    >>> plt.figure()
+    >>> dn = hierarchy.dendrogram(Z)
+
+    Now plot in given axes, improve the color scheme and use both vertical and
+    horizontal orientations:
+
+    >>> hierarchy.set_link_color_palette(['m', 'c', 'y', 'k'])
+    >>> fig, axes = plt.subplots(1, 2, figsize=(8, 3))
+    >>> dn1 = hierarchy.dendrogram(Z, ax=axes[0], above_threshold_color='y',
+    ...                            orientation='top')
+    >>> dn2 = hierarchy.dendrogram(Z, ax=axes[1], above_threshold_color='#bcbddc',
+    ...                            orientation='right')
+    >>> plt.show()
+
     """
     # This feature was thought about but never implemented (still useful?):
     #
