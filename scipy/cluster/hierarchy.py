4e7d2410d7d8f03eb1f014d12418836b761fcd62
Ralf Gommers <ralf.gommers@gmail.com>
2015-08-16 16:48:48 +0200

    BUG: dendrogram: remove color_list kw from signature, it doesn't belong there.
    It wasn't in the parameters list and obviously wasn't meant to be there, given
    that (a) it is initialized as an empty list at the top of the function, and
    (b) the length of the input needed is hard to guess.
    
    Note that there is a function to set the color palette for dendrogram, namely
    `set_link_color_palette`.
    
    Closes gh-4926.

--- scipy/cluster/hierarchy.py
+++ scipy/cluster/hierarchy.py
@@ -1825,10 +1825,10 @@ def set_link_color_palette(palette):
 def dendrogram(Z, p=30, truncate_mode=None, color_threshold=None,
                get_leaves=True, orientation='top', labels=None,
                count_sort=False, distance_sort=False, show_leaf_counts=True,
-               no_plot=False, no_labels=False, color_list=None,
-               leaf_font_size=None, leaf_rotation=None, leaf_label_func=None,
-               no_leaves=False, show_contracted=False,
-               link_color_func=None, ax=None, above_threshold_color='b'):
+               no_plot=False, no_labels=False, leaf_font_size=None,
+               leaf_rotation=None, leaf_label_func=None, no_leaves=False,
+               show_contracted=False, link_color_func=None, ax=None,
+               above_threshold_color='b'):
     """
     Plots the hierarchical clustering as a dendrogram.
 
