456ad20604824c66fef3aca9cba3b09715287d08
endolith <endolith@gmail.com>
2015-08-22 00:58:47 -0400

    DOC: graphical example for KDTree.query_ball_point

--- scipy/spatial/kdtree.py
+++ scipy/spatial/kdtree.py
@@ -591,11 +591,22 @@ class KDTree(object):
         Examples
         --------
         >>> from scipy import spatial
-        >>> x, y = np.mgrid[0:4, 0:4]
+        >>> x, y = np.mgrid[0:5, 0:5]
         >>> points = zip(x.ravel(), y.ravel())
         >>> tree = spatial.KDTree(points)
         >>> tree.query_ball_point([2, 0], 1)
-        [4, 8, 9, 12]
+        [5, 10, 11, 15]
+
+        Query multiple points and plot the results:
+
+        >>> import matplotlib.pyplot as plt
+        >>> points = np.asarray(points)
+        >>> plt.plot(points[:,0], points[:,1], '.')
+        >>> for results in tree.query_ball_point(([2, 0], [3, 3]), 1):
+        >>>     nearby_points = points[results]
+        >>>     plt.plot(nearby_points[:,0], nearby_points[:,1], 'o')
+        >>> plt.margins(0.1, 0.1)
+        >>> plt.show()
 
         """
         x = np.asarray(x)
