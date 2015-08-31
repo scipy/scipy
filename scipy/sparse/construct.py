bde20a95ec12c86202253186d1df1e5ac89e67e0
Utkarsh Upadhyay <musically.ut@gmail.com>
2015-08-31 17:49:37 +0200

    Use 0 as default value of offests arg in diags.
    Also update the documentation to mention that the offsets argument
    accepts sole integers as arguments as well as sequences of integers.

--- scipy/sparse/construct.py
+++ scipy/sparse/construct.py
@@ -61,7 +61,7 @@ def spdiags(data, diags, m, n, format=None):
     return dia_matrix((data, diags), shape=(m,n)).asformat(format)
 
 
-def diags(diagonals, offsets=None, shape=None, format=None, dtype=None):
+def diags(diagonals, offsets=0, shape=None, format=None, dtype=None):
     """
     Construct a sparse matrix from diagonals.
 
@@ -70,7 +70,7 @@ def diags(diagonals, offsets=None, shape=None, format=None, dtype=None):
     diagonals : sequence of array_like
         Sequence of arrays containing the matrix diagonals,
         corresponding to `offsets`.
-    offsets : sequence of int, optional
+    offsets : sequence of int or an int, optional
         Diagonals to set:
           - k = 0  the main diagonal (default)
           - k > 0  the k-th upper diagonal
@@ -133,10 +133,6 @@ def diags(diagonals, offsets=None, shape=None, format=None, dtype=None):
            [ 0.,  0.,  0.,  3.],
            [ 0.,  0.,  0.,  0.]])
     """
-    # assume the primary diagonal was the intended diagonal
-    if offsets is None:
-        offsets = 0
-
     # if offsets is not a sequence, assume that there's only one diagonal
     try:
         iter(offsets)
