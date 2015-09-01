efd9ba4d0cbecbb0723b97552f85ad9b2167e22e
Endolith <endolith@gmail.com>
2015-08-31 23:04:11 -0400

    DOC: Fix indentation >>> vs ... that broke doctest

--- scipy/spatial/kdtree.py
+++ scipy/spatial/kdtree.py
@@ -603,8 +603,8 @@ class KDTree(object):
         >>> points = np.asarray(points)
         >>> plt.plot(points[:,0], points[:,1], '.')
         >>> for results in tree.query_ball_point(([2, 0], [3, 3]), 1):
-        >>>     nearby_points = points[results]
-        >>>     plt.plot(nearby_points[:,0], nearby_points[:,1], 'o')
+        ...     nearby_points = points[results]
+        ...     plt.plot(nearby_points[:,0], nearby_points[:,1], 'o')
         >>> plt.margins(0.1, 0.1)
         >>> plt.show()
 
