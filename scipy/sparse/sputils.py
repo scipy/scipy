63c112aa03e5e59e1558da07daa879215a269a2f
Lars Buitinck <larsmans@gmail.com>
2015-09-27 14:34:23 +0200

    MAINT: sparse: remove NumPy 1.5 workaround
    Fixes gh-5237.

--- scipy/sparse/sputils.py
+++ scipy/sparse/sputils.py
@@ -47,12 +47,7 @@ def upcast(*args):
     if t is not None:
         return t
 
-    if np.all([np.issubdtype(bool, arg) for arg in args]):
-        # numpy 1.5.x compat - it gives int8 for
-        # np.find_common_type([bool, bool)
-        upcast = bool
-    else:
-        upcast = np.find_common_type(args, [])
+    upcast = np.find_common_type(args, [])
 
     for t in supported_dtypes:
         if np.can_cast(upcast, t):
