567e7925ee78da17a27e900c20210ed90639c678
Endolith <endolith@gmail.com>
2015-10-22 23:33:13 -0400

    DOC: Use ascent for gradient image and enlarge
    "Ascent" has more smooth features than "face" so makes a better
    example of Scharr operator.  Also stack images vertically and enlarge
    for web docs.

--- scipy/signal/signaltools.py
+++ scipy/signal/signaltools.py
@@ -680,15 +680,15 @@ def convolve2d(in1, in2, mode='full', boundary='fill', fillvalue=0):
 
     >>> from scipy import signal
     >>> from scipy import misc
-    >>> face = misc.face(gray=True)
+    >>> ascent = misc.ascent()
     >>> scharr = np.array([[ -3-3j, 0-10j,  +3 -3j],
     ...                    [-10+0j, 0+ 0j, +10 +0j],
     ...                    [ -3+3j, 0+10j,  +3 +3j]]) # Gx + j*Gy
-    >>> grad = signal.convolve2d(face, scharr, boundary='symm', mode='same')
+    >>> grad = signal.convolve2d(ascent, scharr, boundary='symm', mode='same')
 
     >>> import matplotlib.pyplot as plt
-    >>> fig, (ax_orig, ax_mag, ax_ang) = plt.subplots(1, 3)
-    >>> ax_orig.imshow(face, cmap='gray')
+    >>> fig, (ax_orig, ax_mag, ax_ang) = plt.subplots(3, 1, figsize=(6, 15))
+    >>> ax_orig.imshow(ascent, cmap='gray')
     >>> ax_orig.set_title('Original')
     >>> ax_orig.set_axis_off()
     >>> ax_mag.imshow(np.absolute(grad), cmap='gray')
