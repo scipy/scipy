adde936a277f91e11ae42b65f5d98c7bc4326378
Pauli Virtanen <pav@iki.fi>
2015-11-15 18:29:02 +0200

    BUG: sparse: fix a bug in sparsetools input dtype resolution
    Ensure the determined common input dtype for the c++ code is such that
    all inputs can be safely cast into it. Also require that the common
    dtype can be safely cast to output arguments.
    
    Add tests. Remove some upcasts that this makes unnecessary.

--- scipy/sparse/tests/test_sparsetools.py
+++ scipy/sparse/tests/test_sparsetools.py
@@ -8,7 +8,8 @@ import threading
 
 from nose import SkipTest
 import numpy as np
-from numpy.testing import assert_raises, assert_equal, dec, run_module_suite, assert_
+from numpy.testing import (assert_raises, assert_equal, dec, run_module_suite, assert_,
+                           assert_allclose)
 from scipy.sparse import (_sparsetools, coo_matrix, csr_matrix, csc_matrix,
                           bsr_matrix, dia_matrix)
 from scipy.sparse.sputils import supported_dtypes
@@ -244,6 +245,42 @@ def test_csr_matmat_int64_overflow():
     b = a.T
 
     assert_raises(RuntimeError, a.dot, b)
+
+
+def test_upcast():
+    a0 = csr_matrix([[np.pi, np.pi*1j], [3, 4]], dtype=complex)
+    b0 = np.array([256+1j, 2**32], dtype=complex)
+
+    for a_dtype in supported_dtypes:
+        for b_dtype in supported_dtypes:
+            msg = "(%r, %r)" % (a_dtype, b_dtype)
+
+            if np.issubdtype(a_dtype, np.complexfloating):
+                a = a0.copy().astype(a_dtype)
+            else:
+                a = a0.real.copy().astype(a_dtype)
+
+            if np.issubdtype(b_dtype, np.complexfloating):
+                b = b0.copy().astype(b_dtype)
+            else:
+                b = b0.real.copy().astype(b_dtype)
+
+            if not (a_dtype == np.bool_ and b_dtype == np.bool_):
+                c = np.zeros((2,), dtype=np.bool_)
+                assert_raises(ValueError, _sparsetools.csr_matvec,
+                              2, 2, a.indptr, a.indices, a.data, b, c)
+
+            if ((np.issubdtype(a_dtype, np.complexfloating) and
+                 not np.issubdtype(b_dtype, np.complexfloating)) or
+                (not np.issubdtype(a_dtype, np.complexfloating) and
+                 np.issubdtype(b_dtype, np.complexfloating))):
+                c = np.zeros((2,), dtype=np.float64)
+                assert_raises(ValueError, _sparsetools.csr_matvec,
+                              2, 2, a.indptr, a.indices, a.data, b, c)
+
+            c = np.zeros((2,), dtype=np.result_type(a_dtype, b_dtype))
+            _sparsetools.csr_matvec(2, 2, a.indptr, a.indices, a.data, b, c)
+            assert_allclose(c, np.dot(a.toarray(), b), err_msg=msg)
 
 
 def check_free_memory(free_mb):
