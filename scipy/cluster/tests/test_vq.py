fix normalization of krandinit on rank def data
Also add a test to ensure the generated data has the proper covariance.

scipy/cluster/tests/test_vq.py
@@ -13,7 +13,7 @@ from numpy.testing import (assert_array_equal, assert_array_almost_equal,
     assert_)
 
 from scipy.cluster.vq import (kmeans, kmeans2, py_vq, py_vq2, vq, whiten,
-    ClusterError)
+    ClusterError, _krandinit)
 from scipy.cluster import _vq
 
 # Optional:
@@ -234,6 +234,17 @@ class TestKMean(TestCase):
                         message="One of the clusters is empty. Re-run")
             kmeans2(data, 3, minit='random')
             kmeans2(data[:, :1], 3, minit='random')  # special case (1-D)
+
+    def test_krandinit(self):
+        data = np.fromfile(DATAFILE1, sep=", ")
+        datas = [data.reshape((200, 2)), data.reshape((20, 20))[:10]]
+        k = int(1e6)
+        for data in datas:
+            np.random.seed(1234)
+            init = _krandinit(data, k)
+            orig_cov = np.cov(data, rowvar=0)
+            init_cov = np.cov(init, rowvar=0)
+            assert_allclose(orig_cov, init_cov, atol=1e-2)
 
     def test_kmeans2_empty(self):
         # Regression test for gh-1032.

scipy/cluster/vq.py
@@ -639,7 +639,7 @@ def _krandinit(data, k):
         mu = np.mean(data, axis=0)
         _, s, vh = np.linalg.svd(data - mu, full_matrices=False)
         x = np.random.randn(k, s.size)
-        sVh = s[:,None] * vh
+        sVh = s[:, None] * vh / np.sqrt(data.shape[0] - 1)
         x = np.dot(x, sVh) + mu
         return x
 