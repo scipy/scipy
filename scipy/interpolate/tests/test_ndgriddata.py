1d49ff0b06ac2e5358cdb7cdc2e1849530607b76
Evgeni Burovski <evgeni@burovski.me>
2015-11-18 13:18:42 +0000

    TST: tweak the test for NearestNDInterpolator

--- scipy/interpolate/tests/test_ndgriddata.py
+++ scipy/interpolate/tests/test_ndgriddata.py
@@ -175,10 +175,6 @@ def test_nearest_options():
     nndi_o = NearestNDInterpolator(x, y, tree_options=opts)
     assert_allclose(nndi(x), nndi_o(x), atol=1e-14)
 
-    with assert_raises(TypeError):
-        opts = {'invalid': 42}
-        NearestNDInterpolator(x, y, tree_options=opts)
-
 
 if __name__ == "__main__":
     run_module_suite()
