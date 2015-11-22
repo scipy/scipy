70e77cc16c876326d5be703216eb6d316043a4a8
Pauli Virtanen <pav@iki.fi>
2015-11-22 16:00:08 +0200

    Revert "BUG: optimize: do not use magic numbers in line search initialization"
    This reverts commit 81474eb34d99bba0b2908e35ba832ca735e40154.
    
    The reasoning the commit was based on is flawed --- for alpha_0 ~ 1, the
    resulting initial step in cg/bfgs is not |dx| ~ 1 because pk is not
    normalized.  The old_old_fval trick sets the initial line search step
    guess to alpha_0 ~ 5000/|g|^2, corresponding to initial step |dx| ~ 5000/|g|.

--- scipy/optimize/optimize.py
+++ scipy/optimize/optimize.py
@@ -853,7 +853,7 @@ def _minimize_bfgs(fun, x0, args=(), jac=None, callback=None,
     I = numpy.eye(N, dtype=int)
     Hk = I
     old_fval = f(x0)
-    old_old_fval = None
+    old_old_fval = old_fval + 5000
     xk = x0
     if retall:
         allvecs = [x0]
@@ -1163,7 +1163,7 @@ def _minimize_cg(fun, x0, args=(), jac=None, callback=None,
     k = 0
     xk = x0
     old_fval = f(xk)
-    old_old_fval = None
+    old_old_fval = old_fval + 5000
 
     if retall:
         allvecs = [xk]
