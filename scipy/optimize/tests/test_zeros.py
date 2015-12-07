b0a130941801abb48fa90e770b459898ed3b6005
Josh <person142@users.noreply.github.com>
2015-12-06 22:57:31 -0600

    TST: add test showing brent(q|h) might not achieve desired tolerance.

--- scipy/optimize/tests/test_zeros.py
+++ scipy/optimize/tests/test_zeros.py
@@ -78,6 +78,30 @@ def test_gh_5555():
                         err_msg='method %s' % method.__name__)
 
 
+def test_gh_5557():
+    # Show that without the changes in 5557 brentq and brenth might
+    # only achieve a tolerance of 2*(xtol + rtol*|res|).
+
+    # f linearly interpolates (0, -0.1), (0.5, -0.1), and (1,
+    # 0.4). The important parts are that |f(0)| < |f(1)| (so that
+    # brent takes 0 as the initial guess), |f(0)| < atol (so that
+    # brent accepts 0 as the root), and that the exact root of f lies
+    # more than atol away from 0 (so that brent doesn't achieve the
+    # desired tolerance).
+    def f(x):
+        if x < 0.5:
+            return -0.1
+        else:
+            return x - 0.6
+
+    atol = 0.51
+    rtol = 4*finfo(float).eps
+    methods = [cc.brentq, cc.brenth]
+    for method in methods:
+        res = method(f, 0, 1, xtol=atol, rtol=rtol)
+        assert_allclose(0.6, res, atol=atol, rtol=rtol)
+
+
 class TestRootResults:
     def test_repr(self):
         r = zeros.RootResults(root=1.0,
