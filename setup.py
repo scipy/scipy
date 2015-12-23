fe0b49ae3672d1a834abfb4c724187eeb5a3333e
Evgeni Burovski <evgeni@burovski.me>
2015-12-23 10:26:37 +0000

    REL: set version to 0.17.0rc1

--- setup.py
+++ setup.py
@@ -58,8 +58,8 @@ Operating System :: MacOS
 MAJOR = 0
 MINOR = 17
 MICRO = 0
-ISRELEASED = False
-VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
+ISRELEASED = True
+VERSION = '%d.%d.%drc1' % (MAJOR, MINOR, MICRO)
 
 
 # Return the git revision as a string
