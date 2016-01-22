4b17e2bcbed2ef65cd14923a1ed5d1ccb6d8f8aa
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2016-01-22 15:01:08 +0000

    REL: set version to 0.17.0

--- pavement.py
+++ pavement.py
@@ -119,7 +119,7 @@ RELEASE = 'doc/release/0.17.0-notes.rst'
 
 # Start/end of the log (from git)
 LOG_START = 'v0.16.0'
-LOG_END = 'v0.17.0rc2'
+LOG_END = 'v0.17.0'
 
 
 #-------------------------------------------------------
