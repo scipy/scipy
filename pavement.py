9417960f48e32238b033ecf29846c6afc0678ce5
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2016-01-23 23:39:13 +0000

    MAINT: drop python 3.3 from pavement.py
    As if building with numpy-vendor works at all.

--- pavement.py
+++ pavement.py
@@ -361,8 +361,6 @@ def release(options):
     bdist_superpack(options)
     options.python_version = '3.4'
     bdist_superpack(options)
-    options.python_version = '3.3'
-    bdist_superpack(options)
 
     # README (gpg signed) and Changelog
     write_release_and_log()
