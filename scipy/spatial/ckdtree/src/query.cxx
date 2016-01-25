b1e99b0e40523f03a664fcba4b6c0196f3aefda0
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2016-01-25 11:18:53 +0000

    MAINT: spatial: revert to the exlicit #define for numpy depr api

--- scipy/spatial/ckdtree/src/query.cxx
+++ scipy/spatial/ckdtree/src/query.cxx
@@ -1,5 +1,10 @@
-#define NPY_NO_DEPRECATED_API NPY_1_9_API_VERSION
-
+/*
+ * This would break SciPy with NumPy 1.6 so just accept the compiler
+ * warning for now.
+ * #define NPY_NO_DEPRECATED_API NPY_1_9_API_VERSION 
+ *
+ */
+ 
 #include <Python.h>
 #include "numpy/arrayobject.h"
 
