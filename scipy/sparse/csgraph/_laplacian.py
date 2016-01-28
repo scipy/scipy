94e2d5e4829b1cfc8c0f6bf6bc21ca24d097bc00
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2016-01-28 21:17:08 +1100

    Update sparse.csgraph.laplacian docstring
    The return type is incorrectly documented when the input is sparse,
    making it seem as though the function cannot work efficiently with
    sparse inputs, when in fact it can.

--- scipy/sparse/csgraph/_laplacian.py
+++ scipy/sparse/csgraph/_laplacian.py
@@ -34,8 +34,9 @@ def laplacian(csgraph, normed=False, return_diag=False, use_out_degree=False):
 
     Returns
     -------
-    lap : ndarray
-        The N x N laplacian matrix of graph.
+    lap : ndarray or coo_matrix
+        The N x N laplacian matrix of csgraph. It will be a numpy array (dense)
+        if the input was dense, sparse (COO format) otherwise.
     diag : ndarray, optional
         The length-N diagonal of the Laplacian matrix.
         For the normalized Laplacian, this is the array of square roots
