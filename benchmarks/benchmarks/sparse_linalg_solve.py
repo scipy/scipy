7570c00dbe6b58bb47fcbee16a36538069d39067
CJ Carey <perimosocordiae@gmail.com>
2016-02-04 15:27:49 -0500

    BENCH: fixing up sparse solver benches
    Reducing redundant solves, adding minres + spsolve

--- benchmarks/benchmarks/sparse_linalg_solve.py
+++ benchmarks/benchmarks/sparse_linalg_solve.py
@@ -8,7 +8,7 @@ from numpy.testing import assert_equal
 
 try:
     from scipy import linalg, sparse
-    from scipy.sparse.linalg import cg
+    from scipy.sparse.linalg import cg, minres, spsolve
 except ImportError:
     pass
 
@@ -27,38 +27,34 @@ def _create_sparse_poisson2d(n):
     P1d = _create_sparse_poisson1d(n)
     P2d = sparse.kronsum(P1d, P1d)
     assert_equal(P2d.shape, (n*n, n*n))
-    return P2d
+    return P2d.tocsr()
 
 
 class Bench(Benchmark):
     params = [
-        [4, 6, 10, 16, 25, 40, 64, 100, 160, 250, 400, 640, 1000, 1600],
-        ['sparse', 'dense']
+        [4, 6, 10, 16, 25, 40, 64, 100],
+        ['dense', 'spsolve', 'cg', 'minres']
     ]
     param_names = ['(n,n)', 'solver']
 
     def setup(self, n, solver):
-        dense_is_active = (n**2 < 600)
-        sparse_is_active = (n**2 < 20000)
-
-        if solver == 'dense' and not dense_is_active:
-            raise NotImplementedError()
-
-        if solver == 'sparse' and not sparse_is_active:
+        if solver == 'dense' and n >= 25:
             raise NotImplementedError()
 
         self.b = np.ones(n*n)
         self.P_sparse = _create_sparse_poisson2d(n)
-        self.P_dense = self.P_sparse.A
 
-    def time_cg(self, n, solver):
+        if solver == 'dense':
+            self.P_dense = self.P_sparse.A
+
+    def time_solve(self, n, solver):
         if solver == 'dense':
             linalg.solve(self.P_dense, self.b)
-        else:
+        elif solver == 'cg':
             cg(self.P_sparse, self.b)
-
-    def time_spsolve(self, n, solver):
-        if solver == 'dense':
-            linalg.solve(self.P_dense, self.b)
+        elif solver == 'minres':
+            minres(self.P_sparse, self.b)
+        elif solver == 'spsolve':
+            spsolve(self.P_sparse, self.b)
         else:
-            cg(self.P_sparse, self.b)
+            raise ValueError('Unknown solver: %r' % solver)
