pv/scipy-work

ENH: special: Add vectorized spherical Bessel functions.

scipy/special/generate_ufuncs.py
@@ -302,6 +302,10 @@ rel_entr -- rel_entr: dd->d                                -- _convex_analysis.p
 huber -- huber: dd->d                                      -- _convex_analysis.pxd
 pseudo_huber -- pseudo_huber: dd->d                        -- _convex_analysis.pxd
 exprel -- exprel: d->d                                     -- _exprel.pxd
+spherical_yn -- spherical_yn_real: ld->d, spherical_yn_complex: lD->D -- spherical_bessel.pxd
+spherical_jn -- spherical_jn_real: ld->d, spherical_jn_complex: lD->D -- spherical_bessel.pxd
+spherical_in -- spherical_in_real: ld->d, spherical_in_complex: lD->D -- spherical_bessel.pxd
+spherical_kn -- spherical_kn_real: ld->d, spherical_kn_complex: lD->D -- spherical_bessel.pxd
 """
 
 #---------------------------------------------------------------------------------

scipy/scipy

scipy/special/generate_ufuncs.py
@@ -303,6 +303,14 @@ rel_entr -- rel_entr: dd->d                                -- _convex_analysis.p
 huber -- huber: dd->d                                      -- _convex_analysis.pxd
 pseudo_huber -- pseudo_huber: dd->d                        -- _convex_analysis.pxd
 exprel -- exprel: d->d                                     -- _exprel.pxd
+_spherical_yn -- spherical_yn_real: ld->d, spherical_yn_complex: lD->D -- _spherical_bessel.pxd
+_spherical_jn -- spherical_jn_real: ld->d, spherical_jn_complex: lD->D -- _spherical_bessel.pxd
+_spherical_in -- spherical_in_real: ld->d, spherical_in_complex: lD->D -- _spherical_bessel.pxd
+_spherical_kn -- spherical_kn_real: ld->d, spherical_kn_complex: lD->D -- _spherical_bessel.pxd
+_spherical_yn_d -- spherical_yn_d_real: ld->d, spherical_yn_d_complex: lD->D -- _spherical_bessel.pxd
+_spherical_jn_d -- spherical_jn_d_real: ld->d, spherical_jn_d_complex: lD->D -- _spherical_bessel.pxd
+_spherical_in_d -- spherical_in_d_real: ld->d, spherical_in_d_complex: lD->D -- _spherical_bessel.pxd
+_spherical_kn_d -- spherical_kn_d_real: ld->d, spherical_kn_d_complex: lD->D -- _spherical_bessel.pxd
 """
 
 #---------------------------------------------------------------------------------