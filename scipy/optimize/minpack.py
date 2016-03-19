BUG, API: Add `jac` parameter to curve_fit and assure its correct scaling according to `sigma`
 Closes #5068

scipy/optimize/minpack.py
@@ -449,12 +449,24 @@ def leastsq(func, x0, args=(), Dfun=None, full_output=0,
         return (retval[0], info)
 
 
-def _general_function(params, xdata, ydata, function):
-    return function(xdata, *params) - ydata
+def _wrap_func(func, xdata, ydata, weights):
+    if weights is None:
+        def func_wrapped(params):
+            return func(xdata, *params) - ydata
+    else:
+        def func_wrapped(params):
+            return weights * (func(xdata, *params) - ydata)
+    return func_wrapped
 
 
-def _weighted_general_function(params, xdata, ydata, function, weights):
-    return weights * (function(xdata, *params) - ydata)
+def _wrap_jac(jac, xdata, weights):
+    if weights is None:
+        def jac_wrapped(params):
+            return jac(xdata, *params)
+    else:
+        def jac_wrapped(params):
+            return weights[:, np.newaxis] * jac(xdata, *params)
+    return jac_wrapped
 
 
 def _initialize_feasible(lb, ub):
@@ -476,7 +488,7 @@ def _initialize_feasible(lb, ub):
 
 def curve_fit(f, xdata, ydata, p0=None, sigma=None, absolute_sigma=False,
               check_finite=True, bounds=(-np.inf, np.inf), method=None,
-              **kwargs):
+              jac=None, **kwargs):
     """
     Use non-linear least squares to fit a function, f, to data.
 
@@ -534,6 +546,15 @@ def curve_fit(f, xdata, ydata, p0=None, sigma=None, absolute_sigma=False,
         case.
 
         .. versionadded:: 0.17
+    jac : callable, string or None, optional
+        Function with signature ``jac(x, ...)`` which computes the Jacobian
+        matrix of the model function with respect to parameters. It will be
+        scaled according to provided `sigma`. If None (default), the Jacobian
+        will be estimated numerically. You may also use string keywords to
+        select a finite difference scheme for 'trf' and 'dogbox' methods,
+        see `least_squares`.
+
+        .. versionadded:: 0.18
     kwargs
         Keyword arguments passed to `leastsq` for ``method='lm'`` or
         `least_squares` otherwise.
@@ -642,23 +663,23 @@ def curve_fit(f, xdata, ydata, p0=None, sigma=None, absolute_sigma=False,
         else:
             xdata = np.asarray(xdata)
 
-    args = (xdata, ydata, f)
-    if sigma is None:
-        func = _general_function
-    else:
-        func = _weighted_general_function
-        args += (1.0 / asarray(sigma),)
+    weights = 1.0 / asarray(sigma) if sigma is not None else None
+    func = _wrap_func(f, xdata, ydata, weights)
+    if callable(jac):
+        jac = _wrap_jac(jac, xdata, weights)
+    elif jac is None and method != 'lm':
+        jac = '2-point'
 
     if method == 'lm':
         # Remove full_output from kwargs, otherwise we're passing it in twice.
         return_full = kwargs.pop('full_output', False)
-        res = leastsq(func, p0, args=args, full_output=1, **kwargs)
+        res = leastsq(func, p0, Dfun=jac, full_output=1, **kwargs)
         popt, pcov, infodict, errmsg, ier = res
         cost = np.sum(infodict['fvec'] ** 2)
         if ier not in [1, 2, 3, 4]:
             raise RuntimeError("Optimal parameters not found: " + errmsg)
     else:
-        res = least_squares(func, p0, args=args, bounds=bounds, method=method,
+        res = least_squares(func, p0, jac=jac, bounds=bounds, method=method,
                             **kwargs)
 
         if not res.success: