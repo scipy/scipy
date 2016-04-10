12e2e20c090d04502111dfae2fc112bad5205a33
Ian Henriksen <insertinterestingnamehere@gmail.com>
2016-04-09 21:18:36 -0600

    MAINT: Added clanhf and slansf to the LAPACK ABI wrappers.

--- scipy/_build_utils/src/wrap_dummy_accelerate.f
+++ scipy/_build_utils/src/wrap_dummy_accelerate.f
@@ -101,6 +101,16 @@ c Fortran code to Accelerate.
       WCLANHE = CLANHE( NORM, UPLO, N, A, LDA, WORK )
       END FUNCTION
 
+      REAL FUNCTION WCLANHF( NORM, TRANSR, UPLO, N, A, WORK )
+      CHARACTER          NORM, TRANSR, UPLO
+      INTEGER            N
+      REAL               WORK( 0: * )
+      COMPLEX            A( 0: * )
+      EXTERNAL           CLANHF
+      REAL               CLANHF
+      WCLANHF = CLANHF( NORM, TRANSR, UPLO, N, A, WORK )
+      END FUNCTION
+
       REAL FUNCTION WCLANHP( NORM, UPLO, N, AP, WORK )
       CHARACTER          NORM, UPLO
       INTEGER            N
@@ -242,6 +252,15 @@ c Fortran code to Accelerate.
       EXTERNAL           SLANSB
       REAL               SLANSB
       WSLANSB = SLANSB( NORM, UPLO, N, K, AB, LDAB, WORK )
+      END FUNCTION
+
+      REAL FUNCTION WSLANSF( NORM, TRANSR, UPLO, N, A, WORK )
+      CHARACTER          NORM, TRANSR, UPLO
+      INTEGER            N
+      REAL               A( 0: * ), WORK( 0: * )
+      EXTERNAL           SLANSF
+      REAL               SLANSF
+      WSLANSF = SLANSF( NORM, TRANSR, UPLO, N, A, WORK)
       END FUNCTION
 
       REAL FUNCTION WSLANSP( NORM, UPLO, N, AP, WORK )
