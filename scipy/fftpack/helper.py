c69abae8a4c66ded0c3070a0f5dbb28a5de722f7
Endolith <endolith@gmail.com>
2016-05-01 13:28:07 -0400

    MAINT: Move _next_regular to fftpack.helper
    The optimal size is specific to FFTPACK (as opposed to say, FFTW), so
    the the function should be there, too.
    
    (Also PEP8 changes)

--- scipy/fftpack/helper.py
+++ scipy/fftpack/helper.py
@@ -1,9 +1,9 @@
 from __future__ import division, print_function, absolute_import
 
-__all__ = ['fftshift', 'ifftshift', 'fftfreq', 'rfftfreq']
-
 from numpy import arange
 from numpy.fft.helper import fftshift, ifftshift, fftfreq
+
+__all__ = ['fftshift', 'ifftshift', 'fftfreq', 'rfftfreq']
 
 
 def rfftfreq(n, d=1.0):
@@ -41,6 +41,57 @@ def rfftfreq(n, d=1.0):
 
     """
     if not isinstance(n, int) or n < 0:
-        raise ValueError("n = %s is not valid.  n must be a nonnegative integer." % n)
+        raise ValueError("n = %s is not valid. "
+                         "n must be a nonnegative integer." % n)
 
     return (arange(1, n + 1, dtype=int) // 2) / float(n * d)
+
+
+def _next_regular(target):
+    """
+    Find the next regular number greater than or equal to target.
+    Regular numbers are composites of the prime factors 2, 3, and 5.
+    Also known as 5-smooth numbers or Hamming numbers, these are the optimal
+    size for inputs to FFTPACK.
+
+    Target must be a positive integer.
+    """
+    if target <= 6:
+        return target
+
+    # Quickly check if it's already a power of 2
+    if not (target & (target-1)):
+        return target
+
+    match = float('inf')  # Anything found will be smaller
+    p5 = 1
+    while p5 < target:
+        p35 = p5
+        while p35 < target:
+            # Ceiling integer division, avoiding conversion to float
+            # (quotient = ceil(target / p35))
+            quotient = -(-target // p35)
+
+            # Quickly find next power of 2 >= quotient
+            try:
+                p2 = 2**((quotient - 1).bit_length())
+            except AttributeError:
+                # Fallback for Python <2.7
+                p2 = 2**(len(bin(quotient - 1)) - 2)
+
+            N = p2 * p35
+            if N == target:
+                return N
+            elif N < match:
+                match = N
+            p35 *= 3
+            if p35 == target:
+                return p35
+        if p35 < match:
+            match = p35
+        p5 *= 5
+        if p5 == target:
+            return p5
+    if p5 < match:
+        match = p5
+    return match
