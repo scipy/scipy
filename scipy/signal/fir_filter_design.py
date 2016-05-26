edbb6aa9a1e564885cf6d58bc321a6ab6873df1f
Eric Larson <larson.eric.d@gmail.com>
2016-05-25 23:37:15 -0300

    ENH: Allow linear desired

--- scipy/signal/fir_filter_design.py
+++ scipy/signal/fir_filter_design.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """Functions for FIR filter design."""
 from __future__ import division, print_function, absolute_import
 
@@ -5,6 +6,7 @@ from math import ceil, log
 import numpy as np
 from numpy.fft import irfft
 from scipy.special import sinc
+from scipy.linalg import toeplitz, hankel, pinv
 from . import sigtools
 
 __all__ = ['kaiser_beta', 'kaiser_atten', 'kaiserord',
@@ -203,6 +205,7 @@ def firwin(numtaps, cutoff, width=None, window='hamming', pass_zero=True,
     --------
     scipy.signal.firwin2
     scipy.signal.firls
+    scipy.signal.remez
 
     Examples
     --------
@@ -370,8 +373,9 @@ def firwin2(numtaps, freq, gain, nfreqs=None, window='hamming', nyq=1.0,
 
     See also
     --------
-    scipy.signal.firwin
-    scipy.signal.firls
+    firls
+    firwin
+    remez
 
     Notes
     -----
@@ -529,13 +533,13 @@ def remez(numtaps, bands, desired, weight=None, Hz=1, type='bandpass',
     type : {'bandpass', 'differentiator', 'hilbert'}, optional
         The type of filter:
 
-          'bandpass' : flat response in bands. This is the default.
+          * 'bandpass' : flat response in bands. This is the default.
 
-          'differentiator' : frequency proportional response in bands.
+          * 'differentiator' : frequency proportional response in bands.
 
-          'hilbert' : filter with odd symmetry, that is, type III
-                      (for even order) or type IV (for odd order)
-                      linear phase filters.
+          * 'hilbert' : filter with odd symmetry, that is, type III
+                        (for even order) or type IV (for odd order)
+                        linear phase filters.
 
     maxiter : int, optional
         Maximum number of iterations of the algorithm. Default is 25.
@@ -551,7 +555,10 @@ def remez(numtaps, bands, desired, weight=None, Hz=1, type='bandpass',
 
     See Also
     --------
-    freqz : Compute the frequency response of a digital filter.
+    freqz
+    firls
+    firwin
+    firwin2
 
     References
     ----------
@@ -602,28 +609,31 @@ def firls(numtaps, bands, desired, weight=None, nyq=1.):
     """
     FIR filter design using least-squares error minimization.
 
-    Calculate the filter coefficients for the finite impulse response
-    (FIR) filter which has the best approximation to the desired
-    frequency response described by `bands` and `desired` in the
-    least squares sense.
+    Calculate the filter coefficients for the linear-phase finite
+    impulse response (FIR) filter which has the best approximation
+    to the desired frequency response described by `bands` and
+    `desired` in the least squares sense (i.e., the integral of the
+    weighted mean-squared error within the specified bands is
+    minimized).
 
     Parameters
     ----------
     numtaps : int
         The number of taps in the FIR filter.  `numtaps` must be odd.
     bands : array_like
-        A monotonic sequence containing the band edges in Hz.
-        All elements must be non-negative and less than half the sampling
-        frequency as given by `Hz`.
+        A monotonic nondecreasing sequence containing the band edges in
+        Hz. All elements must be non-negative and less than or equal to
+        the Nyquist frequency given by `nyq`.
     desired : array_like
-        A sequence half the size of bands containing the desired gain
-        in each of the specified bands.
+        A sequence the same size as `bands` containing the desired gain
+        at the start and end point of each band.
     weight : array_like, optional
-        A relative weighting to give to each band region. The length of
-        `weight` has to be half the length of `bands`.
+        A relative weighting to give to each band region when solving
+        the least squares problem. `weight` has to be half the size of
+        `bands`.
     nyq : float, optional
-        Nyquist frequency.  Each frequency in `bands` must be between 0
-        and `nyq`.
+        Nyquist frequency. Each frequency in freq must be between 0 and
+        `nyq` (inclusive).
 
     Returns
     -------
@@ -637,74 +647,138 @@ def firls(numtaps, bands, desired, weight=None, nyq=1.):
 
     Examples
     --------
-    We want to construct a filter with a passband at 0.2-0.3 Hz, and
-    stop bands at 0-0.1 Hz and 0.4-0.5 Hz. Note that this means that the
-    behavior in the frequency ranges between those bands is unspecified and
-    may overshoot::
+    We want to construct a band-pass filter. Note that the behavior in the
+    frequency ranges between our stop bands and pass bands is unspecified,
+    and thus may overshoot depending on the parameters of our filter:
 
     >>> from scipy import signal
     >>> import matplotlib.pyplot as plt
-    >>> bpass = signal.firls(71, [0, 0.1, 0.2, 0.3, 0.4, 0.5], [0, 1, 0])
-    >>> freq, response = signal.freqz(bpass)
-    >>> ampl = np.abs(response)
-    >>> fig = plt.figure()
-    >>> ax1 = fig.add_subplot(111)
-    >>> ax1.semilogy(freq/(2*np.pi), ampl, 'b-')  # freq in Hz
+    >>> fig, axs = plt.subplots(2)
+    >>> nyq = 5.  # Hz
+    >>> desired = (0, 0, 1, 1, 0, 0)
+    >>> for bi, bands in enumerate(((0, 1, 2, 3, 4, 5), (0, 1, 2, 4, 4.45, 5))):
+    ...     fir_firls = signal.firls(73, bands, desired, nyq=nyq)
+    ...     fir_remez = signal.remez(73, bands, desired[::2], Hz=2 * nyq)
+    ...     fir_firwin2 = signal.firwin2(73, bands, desired, nyq=nyq)
+    ...     hs = list()
+    ...     ax = axs[bi]
+    ...     for fir in (fir_firls, fir_remez, fir_firwin2):
+    ...         freq, response = signal.freqz(fir)
+    ...         hs.append(ax.semilogy(nyq*freq/(np.pi), np.abs(response))[0])
+    ...     for band, gains in zip(zip(bands[::2], bands[1::2]), zip(desired[::2], desired[1::2])):
+    ...         ax.semilogy(band, np.maximum(gains, 1e-7), 'k--', linewidth=2)
+    ...     ax.legend(hs, ('firls', 'remez', 'firwin2'), loc='lower center')
+    ...
     >>> plt.show()
 
     Notes
     -----
-    This function constructs a Type I linear phase FIR filter, which contains
-    an odd number of `coeffs` satisfying for :math:`n < numtaps`:
+    This implementation follows the algorithm given in [1]_.
+    As noted there, least squares design has multiple advantages:
+
+        1. Optimal in a least-squares sense.
+        2. Simple, non-iterative method.
+        3. The general solution can obtained by solving a linear
+           system of equations.
+        4. Allows the use of a frequency dependent weighting function.
+
+    This function constructs a Type I linear phase FIR filter, which
+    contains an odd number of `coeffs` satisfying for :math:`n < numtaps`:
 
     .. math:: coeffs(n) = coeffs(numtaps - 1 - n)
 
-    The odd number of coefficients avoids a boundary condition at the
-    Nyquist frequency that would occur with an even number of coefficients.
+    The odd number of coefficients and filter symmetry avoid boundary
+    conditions that could otherwise occur at the Nyquist and 0 frequencies
+    (e.g., for Type II, III, or IV variants).
+
+    References
+    ----------
+    .. [1] http://cnx.org/contents/6x7LNQOp@7/Linear-Phase-Fir-Filter-Design
 
     .. versionadded:: 0.18
-    """
-    if numtaps % 2 == 0:
-        raise ValueError("numtaps must be odd.")
-    L = (numtaps-1) // 2
+    """  # noqa
+    numtaps = int(numtaps)
+    if numtaps % 2 == 0 or numtaps < 1:
+        raise ValueError("numtaps must be odd and >= 1")
+    M = (numtaps-1) // 2
 
-    # normalize bands and make it 2 columns
+    # normalize bands 0->1 and make it 2 columns
     nyq = float(nyq)
-    bands = np.asarray(bands).flatten() / (2 * nyq)
+    if nyq <= 0:
+        raise ValueError('nyq must be positive, got %s <= 0.' % nyq)
+    bands = np.asarray(bands).flatten() / nyq
     if len(bands) % 2 != 0:
         raise ValueError("bands must contain frequency pairs.")
     bands.shape = (-1, 2)
 
     # check remaining params
     desired = np.asarray(desired).flatten()
-    if len(bands) != len(desired):
-        raise ValueError("desired must have one entry per band, got %s "
-                         "entries for %s bands." % (len(desired), len(bands)))
-    if (np.diff(bands) < 0).any():
-        raise ValueError("bands must be monotonically nondecreasing")
+    if bands.size != desired.size:
+        raise ValueError("desired must have one entry per frequency, got %s "
+                         "gains for %s frequencies."
+                         % (desired.size, bands.size))
+    desired.shape = (-1, 2)
+    if (np.diff(bands) <= 0).any() or (np.diff(bands[:, 0]) < 0).any():
+        raise ValueError("bands must be monotonically nondecreasing and have "
+                         "width > 0.")
+    if (bands[:-1, 1] > bands[1:, 0]).any():
+        raise ValueError("bands must not overlap.")
     if (desired < 0).any():
-        raise ValueError("desired gains must be non-negative")
+        raise ValueError("desired must be non-negative.")
     if weight is None:
-        weight = np.ones_like(desired)
+        weight = np.ones(len(desired))
     weight = np.asarray(weight).flatten()
     if len(weight) != len(desired):
-        raise ValueError("weight must be the same size as desired")
+        raise ValueError("weight must be the same size as the number of "
+                         "band pairs (%s)." % (len(bands),))
     if (weight < 0).any():
-        raise ValueError("weight must be non-negative")
+        raise ValueError("weight must be non-negative.")
 
-    # set up the linear matrix equation to be solved, Ax = b
-    k = np.arange(L+1)[np.newaxis]
-    m = k.T
-    A = 0
-    b = 0
-    for i, (f0, f1) in enumerate(bands):
-        Ai = (f1 * (sinc(2*(m+k)*f1) + sinc(2*(m-k)*f1)) -
-              f0 * (sinc(2*(m+k)*f0) + sinc(2*(m-k)*f0)))
-        bi = desired[i] * (2*f1*sinc(2*m*f1) - 2*f0*sinc(2*m*f0))
-        A += Ai * weight[i]
-        b += bi * weight[i]
+    # Set up the linear matrix equation to be solved, Qa = b
 
-    # solve and return
-    x = np.linalg.solve(A, b).squeeze()
-    coeffs = np.hstack((x[:0:-1]/2, x[0], x[1:]/2))
+    # We can express Q(k,n) = 0.5 Q1(k,n) + 0.5 Q2(k,n)
+    # where Q1(k,n)=q(k−n) and Q2(k,n)=q(k+n), i.e. a Toeplitz plus Hankel.
+
+    # We omit the factor of 0.5 above, instead adding it during coefficient
+    # calculation.
+
+    # We also omit the 1/π from both Q and b equations, as they cancel
+    # during solving.
+
+    # We have that:
+    #     q(n) = 1/π ∫W(ω)cos(nω)dω (over 0->π)
+    # Using our nomalization ω=πf and with a constant weight W over each
+    # interval f1->f2 we get:
+    #     q(n) = W∫cos(πnf)df (0->1) = Wf sin(πnf)/πnf
+    # integrated over each f1->f2 pair (i.e., value at f2 - value at f1).
+    n = np.arange(numtaps)[:, np.newaxis, np.newaxis]
+    q = np.dot(np.diff(np.sinc(bands * n) * bands, axis=2)[:, :, 0], weight)
+
+    # Now we assemble our sum of Toeplitz and Hankel
+    Q1 = toeplitz(q[:M+1])
+    Q2 = hankel(q[:M+1], q[M:])
+    Q = Q1 + Q2
+
+    # Now for b(n) we have that:
+    #     b(n) = 1/π ∫ W(ω)D(ω)cos(nω)dω (over 0->π)
+    # Using our nomalization ω=πf and with a constant weight W over each
+    # interval and a linear term for D(ω) we get (over each f1->f2 interval):
+    #     b(n) = W ∫ (mf+c)cos(πnf)df
+    #          = f(mf+c)sin(πnf)/πnf + mf**2 cos(nπf)/(πnf)**2
+    # integrated over each f1->f2 pair (i.e., value at f2 - value at f1).
+    n = n[:M + 1]  # only need this many coefficients here
+    # Choose m and c such that we are at the start and end weights
+    m = (np.diff(desired, axis=1) / np.diff(bands, axis=1))
+    c = desired[:, [0]] - bands[:, [0]] * m
+    b = bands * (m*bands + c) * np.sinc(bands * n)
+    # Use L'Hospital's rule here for cos(nπf)/(πnf)**2 @ n=0
+    b[0] -= m * bands * bands / 2.
+    b[1:] += m * np.cos(n[1:] * np.pi * bands) / (np.pi * n[1:]) ** 2
+    b = np.dot(np.diff(b, axis=2)[:, :, 0], weight)
+
+    # Now we can solve the equation (use pinv because Q can be rank deficient)
+    a = np.dot(pinv(Q), b)
+
+    # make coefficients symmetric (linear phase)
+    coeffs = np.hstack((a[:0:-1], 2 * a[0], a[1:]))
     return coeffs
--- scipy/signal/tests/test_fir_filter_design.py
+++ scipy/signal/tests/test_fir_filter_design.py
@@ -394,26 +394,29 @@ class TestFirls(TestCase):
 
     def test_bad_args(self):
         # even numtaps
-        assert_raises(ValueError, firls, 10, [0.1, 0.2], [0])
+        assert_raises(ValueError, firls, 10, [0.1, 0.2], [0, 0])
         # odd bands
-        assert_raises(ValueError, firls, 11, [0.1, 0.2, 0.4], [0])
-        # len(bands) != 2*len(desired)
-        assert_raises(ValueError, firls, 11, [0.1, 0.2, 0.3, 0.4], [0])
+        assert_raises(ValueError, firls, 11, [0.1, 0.2, 0.4], [0, 0, 0])
+        # len(bands) != len(desired)
+        assert_raises(ValueError, firls, 11, [0.1, 0.2, 0.3, 0.4], [0, 0, 0])
         # non-monotonic bands
-        assert_raises(ValueError, firls, 11, [0.2, 0.1], [0])
+        assert_raises(ValueError, firls, 11, [0.2, 0.1], [0, 0])
+        assert_raises(ValueError, firls, 11, [0.1, 0.2, 0.3, 0.3], [0] * 4)
+        assert_raises(ValueError, firls, 11, [0.3, 0.4, 0.1, 0.2], [0] * 4)
+        assert_raises(ValueError, firls, 11, [0.1, 0.3, 0.2, 0.4], [0] * 4)
         # negative desired
-        assert_raises(ValueError, firls, 11, [0.1, 0.2], [-1])
-        # len(weight) != len(desired)
-        assert_raises(ValueError, firls, 11, [0.1, 0.2], [0], [1, 2])
+        assert_raises(ValueError, firls, 11, [0.1, 0.2], [-1, 1])
+        # len(weight) != len(pairs)
+        assert_raises(ValueError, firls, 11, [0.1, 0.2], [0, 0], [1, 2])
         # negative weight
-        assert_raises(ValueError, firls, 11, [0.1, 0.2], [0], [-1])
+        assert_raises(ValueError, firls, 11, [0.1, 0.2], [0, 0], [-1])
 
     def test_firls(self):
         N = 11  # number of taps in the filter
         a = 0.1  # width of the transition band
 
         # design a halfband symmetric low-pass filter
-        h = firls(11, [0, a, 0.5-a, 0.5], [1, 0], nyq=0.5)
+        h = firls(11, [0, a, 0.5-a, 0.5], [1, 1, 0, 0], nyq=0.5)
 
         # make sure the filter has correct # of taps
         assert_equal(len(h), N)
@@ -445,7 +448,7 @@ class TestFirls(TestCase):
 
     def test_compare(self):
         # compare to OCTAVE output
-        taps = firls(9, [0, 0.5, 0.55, 1], [1, 0], [1, 2])
+        taps = firls(9, [0, 0.5, 0.55, 1], [1, 1, 0, 0], [1, 2])
         # >> taps = firls(8, [0 0.5 0.55 1], [1 1 0 0], [1, 2]);
         known_taps = [-6.26930101730182e-04, -1.03354450635036e-01,
                       -9.81576747564301e-03, 3.17271686090449e-01,
@@ -455,13 +458,22 @@ class TestFirls(TestCase):
         assert_allclose(taps, known_taps)
 
         # compare to MATLAB output
-        taps = firls(11, [0, 0.5, 0.5, 1], [1, 0], [1, 2])
+        taps = firls(11, [0, 0.5, 0.5, 1], [1, 1, 0, 0], [1, 2])
         # >> taps = firls(10, [0 0.5 0.5 1], [1 1 0 0], [1, 2]);
         known_taps = [
             0.058545300496815, -0.014233383714318, -0.104688258464392,
             0.012403323025279, 0.317930861136062, 0.488047220029700,
             0.317930861136062, 0.012403323025279, -0.104688258464392,
             -0.014233383714318, 0.058545300496815]
+        assert_allclose(taps, known_taps)
+
+        # With linear changes:
+        taps = firls(7, (0, 1, 2, 3, 4, 5), [1, 0, 0, 1, 1, 0], nyq=10)
+        # >> taps = firls(6, [0, 0.1, 0.2, 0.3, 0.4, 0.5], [1, 0, 0, 1, 1, 0])
+        known_taps = [
+            1.156090832768218, -4.1385894727395849, 7.5288619164321826,
+            -8.5530572592947856, 7.5288619164321826, -4.1385894727395849,
+            1.156090832768218]
         assert_allclose(taps, known_taps)
 
 if __name__ == "__main__":
