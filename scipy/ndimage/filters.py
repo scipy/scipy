742158fe20c713fcbea41cdda8da235ee8b63e25
Martin Thoma <info@martin-thoma.de>
2016-06-09 12:14:12 +0200

    Add example for application of gaussian filter

--- scipy/ndimage/filters.py
+++ scipy/ndimage/filters.py
@@ -289,6 +289,23 @@ def gaussian_filter(input, sigma, order=0, output=None,
     because intermediate results may be stored with insufficient
     precision.
 
+    Examples
+    --------
+    >>> from scipy.ndimage import gaussian_filter
+    >>> a = np.arange(50, step=2).reshape((5,5))
+    >>> a
+    array([[ 0,  2,  4,  6,  8],
+           [10, 12, 14, 16, 18],
+           [20, 22, 24, 26, 28],
+           [30, 32, 34, 36, 38],
+           [40, 42, 44, 46, 48]])
+    >>> gaussian_filter(a, sigma=1)
+    array([[ 4,  6,  8,  9, 11],
+           [10, 12, 14, 15, 17],
+           [20, 22, 24, 25, 27],
+           [29, 31, 33, 34, 36],
+           [35, 37, 39, 40, 42]])
+
     """
     input = numpy.asarray(input)
     output, return_value = _ni_support._get_output(output, input)
