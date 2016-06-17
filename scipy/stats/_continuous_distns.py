BUG: stats: Fix broadcasting in the rvs() method of the distributions.
The class rv_generic was updated to handle broadcasting of the
arguments to the rvs() method.

The following distributions required additional custom changes to
support broadcasting in rvs():
    levy_stable, pearson3, rice, truncnorm,
    hypergeom, planck, randint.

The function `numpy.broadcast_to` is used in several places.  This
function was added to numpy in version 1.10, so a copy was added to
_lib/_numpy_compat.py to support older version of numpy.

Closes gh-2069.

scipy/stats/_continuous_distns.py
@@ -14,6 +14,7 @@ from scipy import integrate
 from scipy.special import (gammaln as gamln, gamma as gam, boxcox, boxcox1p,
                            inv_boxcox, inv_boxcox1p, erfc, chndtr, chndtrix,
                            i0, i1, ndtr as _norm_cdf, log_ndtr as _norm_logcdf)
+from scipy._lib._numpy_compat import broadcast_to
 
 from numpy import (where, arange, putmask, ravel, shape,
                    log, sqrt, exp, arctanh, tan, sin, arcsin, arctan,
@@ -2949,29 +2950,46 @@ class levy_stable_gen(rv_continuous):
     %(example)s
 
     """
+
     def _rvs(self, alpha, beta):
+
+        def alpha1func(alpha, beta, TH, aTH, bTH, cosTH, tanTH, W):
+            return (2/pi*(pi/2 + bTH)*tanTH -
+                    beta*log((pi/2*W*cosTH)/(pi/2 + bTH)))
+
+        def beta0func(alpha, beta, TH, aTH, bTH, cosTH, tanTH, W):
+            return (W/(cosTH/tan(aTH) + sin(TH)) *
+                    ((cos(aTH) + sin(aTH)*tanTH)/W)**(1.0/alpha))
+
+        def otherwise(alpha, beta, TH, aTH, bTH, cosTH, tanTH, W):
+            # alpha is not 1 and beta is not 0
+            val0 = beta*tan(pi*alpha/2)
+            th0 = arctan(val0)/alpha
+            val3 = W/(cosTH/tan(alpha*(th0 + TH)) + sin(TH))
+            res3 = val3*((cos(aTH) + sin(aTH)*tanTH - val0*(sin(aTH) -
+                         cos(aTH)*tanTH))/W)**(1.0/alpha)
+            return res3
+
+        def alphanot1func(alpha, beta, TH, aTH, bTH, cosTH, tanTH, W):
+            res = _lazywhere(beta == 0,
+                             (alpha, beta, TH, aTH, bTH, cosTH, tanTH, W),
+                             beta0func, f2=otherwise)
+            return res
+
         sz = self._size
+        alpha = broadcast_to(alpha, sz)
+        beta = broadcast_to(beta, sz)
         TH = uniform.rvs(loc=-pi/2.0, scale=pi, size=sz)
         W = expon.rvs(size=sz)
-        if alpha == 1:
-            return 2/pi*(pi/2+beta*TH)*tan(TH)-beta*log((pi/2*W*cos(TH))/(pi/2+beta*TH))
-
-        ialpha = 1.0/alpha
         aTH = alpha*TH
-        if beta == 0:
-            return W/(cos(TH)/tan(aTH)+sin(TH))*((cos(aTH)+sin(aTH)*tan(TH))/W)**ialpha
-
-        val0 = beta*tan(pi*alpha/2)
-        th0 = arctan(val0)/alpha
-        val3 = W/(cos(TH)/tan(alpha*(th0+TH))+sin(TH))
-        res3 = val3*((cos(aTH)+sin(aTH)*tan(TH)-val0*(sin(aTH)-cos(aTH)*tan(TH)))/W)**ialpha
-        return res3
+        bTH = beta*TH
+        cosTH = cos(TH)
+        tanTH = tan(TH)
+        res = _lazywhere(alpha == 1, (alpha, beta, TH, aTH, bTH, cosTH, tanTH, W),
+                         alpha1func, f2=alphanot1func)
+        return res
 
     def _argcheck(self, alpha, beta):
-        if beta == -1:
-            self.b = 0.0
-        elif beta == 1:
-            self.a = 0.0
         return (alpha > 0) & (alpha <= 2) & (beta <= 1) & (beta >= -1)
 
     def _pdf(self, x, alpha, beta):
@@ -4074,6 +4092,7 @@ class pearson3_gen(rv_continuous):
         ans, x, skew = np.broadcast_arrays([1.0], x, skew)
         ans = ans.copy()
 
+        # mask is True where skew is small enough to use the normal approx.
         mask = np.absolute(skew) < norm2pearson_transition
         invmask = ~mask
 
@@ -4133,13 +4152,18 @@ class pearson3_gen(rv_continuous):
         return ans
 
     def _rvs(self, skew):
+        skew = broadcast_to(skew, self._size)
         ans, x, transx, skew, mask, invmask, beta, alpha, zeta = (
             self._preprocess([0], skew))
-        if mask[0]:
-            return self._random_state.standard_normal(self._size)
-        ans = self._random_state.standard_gamma(alpha, self._size)/beta + zeta
-        if ans.size == 1:
-            return ans[0]
+
+        nsmall = mask.sum()
+        nbig = mask.size - nsmall
+        ans[mask] = self._random_state.standard_normal(nsmall)
+        ans[invmask] = (self._random_state.standard_gamma(alpha, nbig)/beta +
+                        zeta)
+
+        if self._size == ():
+            ans = ans[0]
         return ans
 
     def _ppf(self, q, skew):
@@ -4442,8 +4466,8 @@ class rice_gen(rv_continuous):
 
     def _rvs(self, b):
         # http://en.wikipedia.org/wiki/Rice_distribution
-        sz = self._size if self._size else 1
-        t = b/np.sqrt(2) + self._random_state.standard_normal(size=(2, sz))
+        t = b/np.sqrt(2) + self._random_state.standard_normal(size=(2,) +
+                                                              self._size)
         return np.sqrt((t*t).sum(axis=0))
 
     def _cdf(self, x, b):
@@ -4774,10 +4798,9 @@ class truncnorm_gen(rv_continuous):
         self._na = _norm_cdf(a)
         self._sb = _norm_sf(b)
         self._sa = _norm_sf(a)
-        if self.a > 0:
-            self._delta = -(self._sb - self._sa)
-        else:
-            self._delta = self._nb - self._na
+        self._delta = np.where(self.a > 0,
+                               -(self._sb - self._sa),
+                               self._nb - self._na)
         self._logdelta = log(self._delta)
         return (a != b)
 
@@ -4791,10 +4814,11 @@ class truncnorm_gen(rv_continuous):
         return (_norm_cdf(x) - self._na) / self._delta
 
     def _ppf(self, q, a, b):
-        if self.a > 0:
-            return _norm_isf(q*self._sb + self._sa*(1.0-q))
-        else:
-            return _norm_ppf(q*self._nb + self._na*(1.0-q))
+        # XXX Use _lazywhere...
+        ppf = np.where(self.a > 0,
+                       _norm_isf(q*self._sb + self._sa*(1.0-q)),
+                       _norm_ppf(q*self._nb + self._na*(1.0-q)))
+        return ppf
 
     def _stats(self, a, b):
         nA, nB = self._na, self._nb

scipy/stats/tests/common_tests.py
@@ -5,7 +5,7 @@ import pickle
 
 import numpy as np
 import numpy.testing as npt
-from numpy.testing import assert_allclose
+from numpy.testing import assert_allclose, assert_equal
 import numpy.ma.testutils as ma_npt
 
 from scipy._lib._util import getargspec_no_self as _getargspec
@@ -273,3 +273,14 @@ def check_pickling(distfn, args):
 
     # restore the random_state
     distfn.random_state = rndm
+
+
+def check_rvs_broadcast(distfunc, distname, allargs, shape, shape_only, otype):
+    np.random.seed(123)
+    sample = distfunc.rvs(*allargs)
+    assert_equal(sample.shape, shape, "%s: rvs failed to broadcast" % distname)
+    if not shape_only:
+        rvs = np.vectorize(lambda *allargs: distfunc.rvs(*allargs), otypes=otype)
+        np.random.seed(123)
+        expected = rvs(*allargs)
+        assert_allclose(sample, expected, rtol=1e-15)

scipy/stats/tests/test_continuous_basic.py
@@ -15,8 +15,7 @@ from common_tests import (check_normalization, check_moment, check_mean_expect,
                           check_edge_support, check_named_args,
                           check_random_state_property,
                           check_meth_dtype, check_ppf_dtype, check_cmplx_deriv,
-                          check_pickling)
-
+                          check_pickling, check_rvs_broadcast)
 from scipy.stats._distr_params import distcont
 
 """
@@ -215,6 +214,67 @@ def test_moments():
                    distname)
             yield check_loc_scale, distfn, arg, m, v, distname
             yield check_moment, distfn, arg, m, v, distname
+
+
+def test_rvs_broadcast():
+    for dist, shape_args in distcont:
+        # If shape_only is True, it means the _rvs method of the
+        # distribution uses more than one random number to generate a random
+        # variate.  That means the result of using rvs with broadcasting or
+        # with a nontrivial size will not necessarily be the same as using the
+        # numpy.vectorize'd version of rvs(), so we can only compare the shapes
+        # of the results, not the values.
+        # Whether or not a distribution is in the following list is an
+        # implementation detail of the distribution, not a requirement.  If
+        # the implementation the rvs() method of a distribution changes, this
+        # test might also have to be changed.
+        shape_only = dist in ['betaprime', 'dgamma', 'exponnorm',
+                              'nct', 'dweibull', 'rice', 'levy_stable',
+                              'skewnorm']
+
+        distfunc = getattr(stats, dist)
+        loc = np.zeros(2)
+        scale = np.ones((3, 1))
+        nargs = distfunc.numargs
+        allargs = []
+        bshape = [3, 2]
+        # Generate shape parameter arguments...
+        for k in range(nargs):
+            shp = (k + 4,) + (1,)*(k + 2)
+            allargs.append(shape_args[k]*np.ones(shp))
+            bshape.insert(0, k + 4)
+        allargs.extend([loc, scale])
+        # bshape holds the expected shape when loc, scale, and the shape
+        # parameters are all broadcast together.
+        yield check_rvs_broadcast, distfunc, dist, allargs, bshape, shape_only, 'd'
+
+
+def test_rvs_gh2069_regression():
+    # Regression tests for gh-2069.  In scipy 0.17 and earlier,
+    # these tests would fail.
+    #
+    # A typical example of the broken behavior:
+    # >>> norm.rvs(loc=np.zeros(5), scale=np.ones(5))
+    # array([-2.49613705, -2.49613705, -2.49613705, -2.49613705, -2.49613705])
+    np.random.seed(123)
+    vals = stats.norm.rvs(loc=np.zeros(5), scale=1)
+    d = np.diff(vals)
+    npt.assert_(np.all(d != 0), "All the values are equal, but they shouldn't be!")
+    vals = stats.norm.rvs(loc=0, scale=np.ones(5))
+    d = np.diff(vals)
+    npt.assert_(np.all(d != 0), "All the values are equal, but they shouldn't be!")
+    vals = stats.norm.rvs(loc=np.zeros(5), scale=np.ones(5))
+    d = np.diff(vals)
+    npt.assert_(np.all(d != 0), "All the values are equal, but they shouldn't be!")
+    vals = stats.norm.rvs(loc=np.array([[0], [0]]), scale=np.ones(5))
+    d = np.diff(vals.ravel())
+    npt.assert_(np.all(d != 0), "All the values are equal, but they shouldn't be!")
+
+    npt.assert_raises(ValueError, stats.norm.rvs, [[0, 0], [0, 0]],
+                  [[1, 1], [1, 1]], 1)
+    npt.assert_raises(ValueError, stats.gamma.rvs, [2, 3, 4, 5], 0, 1, (2, 2))
+    npt.assert_raises(ValueError, stats.gamma.rvs, [1, 1, 1, 1], [0, 0, 0, 0],
+                     [[1], [2]], (4,))
 
 
 def check_sample_meanvar_(distfn, arg, m, v, sm, sv, sn, msg):

scipy/stats/tests/test_distributions.py
@@ -42,7 +42,7 @@ dists = ['uniform', 'norm', 'lognorm', 'expon', 'beta',
          'genlogistic', 'logistic', 'gumbel_l', 'gumbel_r', 'gompertz',
          'hypsecant', 'laplace', 'reciprocal', 'trapz', 'triang', 'tukeylambda',
          'vonmises', 'vonmises_line', 'pearson3', 'gennorm', 'halfgennorm',
-         'rice', 'kappa4', 'kappa3']
+         'rice', 'kappa4', 'kappa3', 'truncnorm']
 
 
 def _assert_hasattr(a, b, msg=None):
@@ -79,7 +79,7 @@ def test_all_distributions():
             args = tuple(np.sort(np.random.random(nargs)))
         elif dist == 'triang':
             args = tuple(np.random.random(nargs))
-        elif dist == 'reciprocal':
+        elif dist == 'reciprocal' or dist == 'truncnorm':
             vals = np.random.random(nargs)
             vals[1] = vals[0] + 1.0
             args = tuple(vals)