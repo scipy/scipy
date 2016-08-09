fe0b7d181c563b130e6e31ed69f477c75271fc2d
Danilo Horta <danilo.horta@gmail.com>
2016-08-09 09:16:52 +0100

    pep8 blank lines, removing comments

--- scipy/optimize/optimize.py
+++ scipy/optimize/optimize.py
@@ -1759,26 +1759,6 @@ def _minimize_scalar_bounded(func, bounds, args=(),
 
     return result
 
-
-# class Brent:
-#     #need to rethink design of __init__
-#     def __init__(self, func, args=(), tol=1.48e-8, maxiter=500,
-#                  full_output=0):
-#         self.func = func
-#         self.args = args
-#         self.tol = tol
-#         self.maxiter = maxiter
-#         self._mintol = 1.0e-11
-#         self._cg = 0.3819660
-#         self.xmin = None
-#         self.fval = None
-#         self.iter = 0
-#         self.funcalls = 0
-#
-#     # need to rethink design of set_bracket (new options, etc)
-#     def set_bracket(self, brack=None):
-#         self.brack = brack
-
 def _get_bracket_info(func, args, brack):
     ### BEGIN core bracket_info code ###
     ### carefully DOCUMENT any CHANGES in core ##
@@ -1805,121 +1785,6 @@ def _get_bracket_info(func, args, brack):
     ### END core bracket_info code ###
 
     return xa, xb, xc, fa, fb, fc, funcalls
-
-    # def optimize(self):
-    #     # set up for optimization
-    #     func = self.func
-    #     xa, xb, xc, fa, fb, fc, funcalls = self.get_bracket_info()
-    #     _mintol = self._mintol
-    #     _cg = self._cg
-    #     #################################
-    #     #BEGIN CORE ALGORITHM
-    #     #################################
-    #     x = w = v = xb
-    #     fw = fv = fx = func(*((x,) + self.args))
-    #     if (xa < xc):
-    #         a = xa
-    #         b = xc
-    #     else:
-    #         a = xc
-    #         b = xa
-    #     deltax = 0.0
-    #     funcalls = 1
-    #     iter = 0
-    #     while (iter < self.maxiter):
-    #         tol1 = self.tol * numpy.abs(x) + _mintol
-    #         tol2 = 2.0 * tol1
-    #         xmid = 0.5 * (a + b)
-    #         # check for convergence
-    #         if numpy.abs(x - xmid) < (tol2 - 0.5 * (b - a)):
-    #             break
-    #         # XXX In the first iteration, rat is only bound in the true case
-    #         # of this conditional. This used to cause an UnboundLocalError
-    #         # (gh-4140). It should be set before the if (but to what?).
-    #         if (numpy.abs(deltax) <= tol1):
-    #             if (x >= xmid):
-    #                 deltax = a - x       # do a golden section step
-    #             else:
-    #                 deltax = b - x
-    #             rat = _cg * deltax
-    #         else:                              # do a parabolic step
-    #             tmp1 = (x - w) * (fx - fv)
-    #             tmp2 = (x - v) * (fx - fw)
-    #             p = (x - v) * tmp2 - (x - w) * tmp1
-    #             tmp2 = 2.0 * (tmp2 - tmp1)
-    #             if (tmp2 > 0.0):
-    #                 p = -p
-    #             tmp2 = numpy.abs(tmp2)
-    #             dx_temp = deltax
-    #             deltax = rat
-    #             # check parabolic fit
-    #             if ((p > tmp2 * (a - x)) and (p < tmp2 * (b - x)) and
-    #                     (numpy.abs(p) < numpy.abs(0.5 * tmp2 * dx_temp))):
-    #                 rat = p * 1.0 / tmp2        # if parabolic step is useful.
-    #                 u = x + rat
-    #                 if ((u - a) < tol2 or (b - u) < tol2):
-    #                     if xmid - x >= 0:
-    #                         rat = tol1
-    #                     else:
-    #                         rat = -tol1
-    #             else:
-    #                 if (x >= xmid):
-    #                     deltax = a - x  # if it's not do a golden section step
-    #                 else:
-    #                     deltax = b - x
-    #                 rat = _cg * deltax
-    #
-    #         if (numpy.abs(rat) < tol1):            # update by at least tol1
-    #             if rat >= 0:
-    #                 u = x + tol1
-    #             else:
-    #                 u = x - tol1
-    #         else:
-    #             u = x + rat
-    #         fu = func(*((u,) + self.args))      # calculate new output value
-    #         funcalls += 1
-    #
-    #         if (fu > fx):                 # if it's bigger than current
-    #             if (u < x):
-    #                 a = u
-    #             else:
-    #                 b = u
-    #             if (fu <= fw) or (w == x):
-    #                 v = w
-    #                 w = u
-    #                 fv = fw
-    #                 fw = fu
-    #             elif (fu <= fv) or (v == x) or (v == w):
-    #                 v = u
-    #                 fv = fu
-    #         else:
-    #             if (u >= x):
-    #                 a = x
-    #             else:
-    #                 b = x
-    #             v = w
-    #             w = x
-    #             x = u
-    #             fv = fw
-    #             fw = fx
-    #             fx = fu
-    #
-    #         iter += 1
-    #     #################################
-    #     #END CORE ALGORITHM
-    #     #################################
-    #
-    #     self.xmin = x
-    #     self.fval = fx
-    #     self.iter = iter
-    #     self.funcalls = funcalls
-    #
-    # def get_result(self, full_output=False):
-    #     if full_output:
-    #         return self.xmin, self.fval, self.iter, self.funcalls
-    #     else:
-    #         return self.xmin
-
 
 def brent(func, args=(), brack=None, tol=1.48e-8, full_output=0, maxiter=500):
     """
@@ -1999,7 +1864,6 @@ def _minimize_scalar_brent(func, brack=None, args=(),
     tol = xtol
     if tol < 0:
         raise ValueError('tolerance should be >= 0, got %r' % tol)
-
 
     a, _, b, _, _, _, nfev = _get_bracket_info(func, args, brack)
 
