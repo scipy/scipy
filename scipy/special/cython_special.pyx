0ad1259800c25997dfc8cc0a9fdccc0627cbf7fb
Josh Wilson <person142@users.noreply.github.com>
2016-10-30 11:05:42 -0500

    GEN: special: run generate_ufuncs.py

--- scipy/special/cython_special.pyx
+++ scipy/special/cython_special.pyx
@@ -979,37 +979,7 @@ Available Functions
         double zetac(double)
 
 """
-cimport numpy as np
-from numpy cimport (
-    npy_float, npy_double, npy_longdouble,
-    npy_cfloat, npy_cdouble, npy_clongdouble,
-    npy_int, npy_long,
-    NPY_FLOAT, NPY_DOUBLE, NPY_LONGDOUBLE,
-    NPY_CFLOAT, NPY_CDOUBLE, NPY_CLONGDOUBLE,
-    NPY_INT, NPY_LONG)
-
-cdef extern from "numpy/ufuncobject.h":
-    int PyUFunc_getfperr() nogil
-
-cdef public int wrap_PyUFunc_getfperr() nogil:
-    """
-    Call PyUFunc_getfperr in a context where PyUFunc_API array is initialized;
-    this avoids messing with the UNIQUE_SYMBOL #defines
-    """
-    return PyUFunc_getfperr()
-
-cdef extern from "numpy/npy_math.h":
-    double NPY_NAN
-
-cimport sf_error
-cimport _complexstuff
-cimport scipy.special._ufuncs_cxx
-from . import _ufuncs
-
-ctypedef long double long_double
-ctypedef float complex float_complex
-ctypedef double complex double_complex
-ctypedef long double complex long_double_complex
+include "_cython_special.pxi"
 
 cdef extern from "_ufuncs_defs.h":
     cdef npy_int _func_airy_wrap "airy_wrap"(npy_double, npy_double *, npy_double *, npy_double *, npy_double *)nogil
