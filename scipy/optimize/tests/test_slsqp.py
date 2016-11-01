38f5366e4e9e8da13778ccdf3953501c1cfcb9c0
Brandon Carter <b-carter@users.noreply.github.com>
2016-11-01 13:25:40 -0400

    ENH: Clearer error when fmin_slsqp obj doesn't return scalar (#6691)
    Closes gh-5433.

--- scipy/optimize/tests/test_slsqp.py
+++ scipy/optimize/tests/test_slsqp.py
@@ -4,7 +4,8 @@ Unit test for SLSQP optimization.
 from __future__ import division, print_function, absolute_import
 
 from numpy.testing import (assert_, assert_array_almost_equal, TestCase,
-                           assert_allclose, assert_equal, run_module_suite)
+                           assert_allclose, assert_equal, run_module_suite,
+                           assert_raises)
 import numpy as np
 
 from scipy._lib._testutils import knownfailure_overridable
@@ -304,6 +305,18 @@ class TestSLSQP(TestCase):
     def test_integer_bounds(self):
         # This should not raise an exception
         fmin_slsqp(lambda z: z**2 - 1, [0], bounds=[[0, 1]], iprint=0)
+
+    def test_obj_must_return_scalar(self):
+        # Regression test for Github Issue #5433
+        # If objective function does not return a scalar, raises ValueError
+        with assert_raises(ValueError):
+            fmin_slsqp(lambda x: [0, 1], [1, 2, 3])
+
+    def test_obj_returns_scalar_in_list(self):
+        # Test for Github Issue #5433 and PR #6691
+        # Objective function should be able to return length-1 Python list
+        #  containing the scalar
+        fmin_slsqp(lambda x: [0], [1, 2, 3])
 
     def test_callback(self):
         # Minimize, method='SLSQP': unbounded, approximated jacobian. Check for callback
