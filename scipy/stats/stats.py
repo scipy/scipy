4eb9679eef8917309b06dc85e01e2306618e99a1
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2016-11-11 02:19:39 +0100

    Temporarily testing to locate bug (2)

--- scipy/stats/stats.py
+++ scipy/stats/stats.py
@@ -3703,9 +3703,9 @@ def weightedtau(x, y, rank=True, weigher=None, additive=True):
         return WeightedTauResult(np.nan, np.nan)  # Return NaN if arrays are empty
 
     # Reduce to ranks unsupported types
-    if x.dtype not in (np.int32, np.int64, np.float32, np.float64):
+    if x.dtype not in (np.int64): # (np.int32, np.int64, np.float32, np.float64):
         x = _toranks(x)
-    if y.dtype not in (np.int32, np.int64, np.float32, np.float64):
+    if y.dtype not in (np.int64): # (np.int32, np.int64, np.float32, np.float64):
         y = _toranks(y)
     if x.dtype != y.dtype:
         if x.dtype != np.int64:
