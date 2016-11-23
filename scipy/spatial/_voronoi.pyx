c5f50f9bdf87d04f7cff987bc8ef55518854f91b
Tyler Reddy <tyler.je.reddy@gmail.com>
2016-11-23 13:09:57 +0000

    ENH: Fixed indentation / minor issues in _voronoi.pyx.

--- scipy/spatial/_voronoi.pyx
+++ scipy/spatial/_voronoi.pyx
@@ -5,56 +5,58 @@ cimport cython
 __all__ = ['sort_vertices_of_regions']
 
 cdef void remaining_filter(np.npy_intp shape, np.npy_intp[:] remaining,
-		np.npy_intp current_simplex):
-   cdef np.npy_intp i
-   for i in range(shape):
-     if remaining[i] == current_simplex:
-       remaining[i] = -2
-    
+                           np.npy_intp current_simplex):
+    cdef np.npy_intp i
+    for i in range(shape):
+        if remaining[i] == current_simplex:
+            remaining[i] = -2
+
+
 @cython.boundscheck(False)
 def sort_vertices_of_regions(int[:,::1] simplices, regions):
-  cdef np.npy_intp n, k, s, i
-  cdef np.npy_intp num_regions = len(regions)
-  cdef np.npy_intp current_simplex, current_vertex, remaining_count
-  cdef np.npy_intp cs_identified, remaining_size
-  cdef np.npy_intp[:] remaining
-  cdef np.npy_intp[:] sorted_vertices = np.zeros(max([len(region) for region in
-	  regions]), dtype = np.int64)
+    cdef np.npy_intp n, k, s, i
+    cdef np.npy_intp num_regions = len(regions)
+    cdef np.npy_intp current_simplex, current_vertex, remaining_count
+    cdef np.npy_intp cs_identified, remaining_size
+    cdef np.npy_intp[:] remaining
+    cdef np.npy_intp[:] sorted_vertices = np.zeros(max([len(region) for region
+                                                   in regions]),
+                                                   dtype=np.int64)
 
-  for n in range(num_regions):
-    remaining_count = 0
-    remaining = np.asarray(regions[n][:])
-    remaining_size = remaining.shape[0]
-    sorted_vertices[:] = -2
-    current_simplex = remaining[0]
-    for i in range(3):
-      k = simplices[current_simplex][i]
-      if k != n:
-        current_vertex = k
-        break
-    sorted_vertices[remaining_count] = current_simplex
-    remaining_count += 1
-    remaining_filter(remaining.shape[0], remaining, current_simplex)
-    while remaining_size > remaining_count:
-      cs_identified = 0
-      for i in range(remaining_size):
-        if remaining[i] == -2:
-          continue
-        s = remaining[i]
-        for j in range(3):
-          if current_vertex == simplices[s,j]:
-            current_simplex = remaining[i]
-            cs_identified += 1
-            break
-        if cs_identified > 0:
-          break
-      for i in range(3):
-        s = simplices[current_simplex, i]
-        if s != n and s != current_vertex:
-          current_vertex = s
-          break
-      sorted_vertices[remaining_count] = current_simplex
-      remaining_count += 1
-      remaining_filter(remaining.shape[0], remaining, current_simplex)
-    regions_arr = np.asarray(sorted_vertices)
-    regions[n] = regions_arr[regions_arr > -2].tolist()
+    for n in range(num_regions):
+        remaining_count = 0
+        remaining = np.asarray(regions[n][:])
+        remaining_size = remaining.shape[0]
+        sorted_vertices[:] = -2
+        current_simplex = remaining[0]
+        for i in range(3):
+            k = simplices[current_simplex][i]
+            if k != n:
+                current_vertex = k
+                break
+        sorted_vertices[remaining_count] = current_simplex
+        remaining_count += 1
+        remaining_filter(remaining.shape[0], remaining, current_simplex)
+        while remaining_size > remaining_count:
+            cs_identified = 0
+            for i in range(remaining_size):
+                if remaining[i] == -2:
+                    continue
+                s = remaining[i]
+                for j in range(3):
+                    if current_vertex == simplices[s, j]:
+                        current_simplex = remaining[i]
+                        cs_identified += 1
+                        break
+                if cs_identified > 0:
+                    break
+            for i in range(3):
+                s = simplices[current_simplex, i]
+                if s != n and s != current_vertex:
+                    current_vertex = s
+                    break
+            sorted_vertices[remaining_count] = current_simplex
+            remaining_count += 1
+            remaining_filter(remaining.shape[0], remaining, current_simplex)
+        regions_arr = np.asarray(sorted_vertices)
+        regions[n] = regions_arr[regions_arr > -2].tolist()
