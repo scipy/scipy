07699012e2edfb5d465857e38a1106c791e31ed0
Matt Ruffalo <matt.ruffalo@gmail.com>
2016-12-13 13:52:09 -0500

    io/matlab/mio.py: handle objects supporting PEP 519
    We don't need to specifically check for a `__fspath__` attribute here, since
    part of PEP 519 is enhancing `__builtins__.open` to support path-like objects.
    
    The logic changes are quite small: first, try to read from whatever is given to
    this function. If that succeeds, it's already a file-like object, and it's
    returned as-is. If it's not readable, try passing it to `open`. If that
    succeeds, return the new file-like object. If it fails with an IOError, check
    for object type, append '.mat' if appropriate, and try calling `open` again.
    
    If the user passes something nonsensical to `_open_file`, `open` will throw a
    `TypeError`, which is a perfectly sane thing to do:
    
    """
    Python 3.6.0rc1 (default, Dec  8 2016, 17:06:38)
    [GCC 5.4.0 20160609] on linux
    Type "help", "copyright", "credits" or "license" for more information.
    >>> open(object())
    Traceback (most recent call last):
      File "<stdin>", line 1, in <module>
    TypeError: expected str, bytes or os.PathLike object, not object
    """

--- scipy/io/matlab/mio.py
+++ scipy/io/matlab/mio.py
@@ -17,25 +17,30 @@ __all__ = ['mat_reader_factory', 'loadmat', 'savemat', 'whosmat']
 
 
 def _open_file(file_like, appendmat):
-    ''' Open `file_like` and return as file-like object '''
-    if isinstance(file_like, string_types):
-        try:
-            return open(file_like, 'rb')
-        except IOError as e:
+    """
+    Open `file_like` and return as file-like object. First, check if object is
+    already file-like; if so, return it as-is. Otherwise, try to pass it
+    to open().
+    """
+    try:
+        file_like.read(0)
+        return file_like
+    except AttributeError:
+        pass
+
+    try:
+        return open(file_like, 'rb')
+    except IOError as e:
+        # Probably "not found"
+        if isinstance(file_like, string_types):
             if appendmat and not file_like.endswith('.mat'):
                 file_like += '.mat'
                 try:
                     return open(file_like, 'rb')
                 except IOError:
-                    pass  # Rethrow the original exception.
-            raise
-    # not a string - maybe file-like object
-    try:
-        file_like.read(0)
-    except AttributeError:
-        raise IOError('Reader needs file name or open file-like object')
-    return file_like
-
+                    raise
+        else:
+            raise IOError('Reader needs file name or open file-like object')
 
 @docfiller
 def mat_reader_factory(file_name, appendmat=True, **kwargs):
