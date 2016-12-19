3f10eae137d9a531599e8f84ce0baa4865995e92
Jed Frey <jedediahfrey@users.noreply.github.com>
2016-12-19 15:43:18 -0500

    Updated documentation.
    TransferFunction's documentation referenced a sampling rate of 0.5s but the example used 0.1s. Changed the documentation to match the example code.

--- scipy/signal/ltisys.py
+++ scipy/signal/ltisys.py
@@ -713,7 +713,7 @@ class TransferFunction(LinearTimeInvariant):
     dt: None
     )
 
-    Contruct the transfer function with a sampling time of 0.5 seconds:
+    Contruct the transfer function with a sampling time of 0.1 seconds:
 
     .. math:: H(z) = \frac{z^2 + 3z + 3}{z^2 + 2z + 1}
 
