a2520cdfe35ec339f973ae65fab4824a9cf09703
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2016-12-24 12:34:06 +0300

    DOC: add python 3.6 trove classifiers

--- setup.py
+++ setup.py
@@ -48,6 +48,7 @@ Programming Language :: Python :: 3.2
 Programming Language :: Python :: 3.3
 Programming Language :: Python :: 3.4
 Programming Language :: Python :: 3.5
+Programming Language :: Python :: 3.6
 Topic :: Software Development
 Topic :: Scientific/Engineering
 Operating System :: Microsoft :: Windows
