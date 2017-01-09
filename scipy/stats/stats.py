BUG: added masking for paired value, updated tests

scipy/stats/stats.py
@@ -3310,14 +3310,12 @@ def spearmanr(a, b=None, axis=0, nan_policy='propagate'):
 
         b_contains_nan, nan_policy = _contains_nan(b, nan_policy)
 
-        if b_contains_nan and nan_policy == 'omit':
-            b = ma.masked_invalid(b)
-
         if a_contains_nan or b_contains_nan:
             if nan_policy == 'propagate':
                 return SpearmanrResult(np.nan, np.nan)
 
             if nan_policy == 'omit':
+                b = ma.masked_invalid(b)
                 return mstats_basic.spearmanr(a, b, axis)
 
         br = np.apply_along_axis(rankdata, axisout, b)