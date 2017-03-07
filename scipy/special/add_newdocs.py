c6f0e0d0bbdfa6f651618ebd054bfc30ce7110b1
Warren Weckesser <warren.weckesser@gmail.com>
2017-03-07 13:31:01 -0500

    DOC: special: Add an example to the airy docstring.

--- scipy/special/add_newdocs.py
+++ scipy/special/add_newdocs.py
@@ -213,6 +213,25 @@ add_newdoc("scipy.special", "airy",
     .. [2] Donald E. Amos, "AMOS, A Portable Package for Bessel Functions
            of a Complex Argument and Nonnegative Order",
            http://netlib.org/amos/.org/amos/
+
+    Examples
+    --------
+    Compute the Airy functions on the interval [-15, 5].
+
+    >>> from scipy import special
+    >>> x = np.linspace(-15, 5, 201)
+    >>> ai, aip, bi, bip = special.airy(x)
+
+    Plot Ai(x) and Bi(x).
+
+    >>> import matplotlib.pyplot as plt
+    >>> plt.plot(x, ai, 'r', label='Ai(x)')
+    >>> plt.plot(x, bi, 'b--', label='Bi(x)')
+    >>> plt.ylim(-0.5, 1.0)
+    >>> plt.grid()
+    >>> plt.legend(loc='upper left')
+    >>> plt.show()
+
     """)
 
 add_newdoc("scipy.special", "airye",
