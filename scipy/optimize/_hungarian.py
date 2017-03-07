bf1de190a6c4c07917e6a0dcd16b1bded30cd284
ashwinpathak20 <ashwinpathak20nov1996@gmail.com>
2017-03-07 09:19:48 +0530

    ENH: Translation of infs to a larger value in scipy.optimize

--- scipy/optimize/_hungarian.py
+++ scipy/optimize/_hungarian.py
@@ -8,6 +8,7 @@
 # License: 3-clause BSD
 
 import numpy as np
+import sys as sys
 
 
 def linear_sum_assignment(cost_matrix):
@@ -89,8 +90,11 @@ def linear_sum_assignment(cost_matrix):
         raise ValueError("expected a matrix containing numerical entries, got %s"
                          % (cost_matrix.dtype,))
 
-    if np.any(np.isinf(cost_matrix) | np.isnan(cost_matrix)):
+    if np.any(np.isnan(cost_matrix)):
         raise ValueError("matrix contains invalid numeric entries")
+
+    if np.any(np.isinf(cost_matrix)):
+        cost_matrix = sys.maxint
 
     if cost_matrix.dtype == np.dtype(np.bool):
         cost_matrix = cost_matrix.astype(np.int)
