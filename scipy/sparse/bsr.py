1937c62748d97ea0af7d5eb3a2d2437ae83c5017
Jonathan Tammo Siebert <siebertjonathan@aim.com>
2017-03-21 10:58:03 +0100

    DOC: Added docstring to isspmatrix_bsr

--- scipy/sparse/bsr.py
+++ scipy/sparse/bsr.py
@@ -665,4 +665,26 @@ class bsr_matrix(_cs_matrix, _minmax_mixin):
 
 
 def isspmatrix_bsr(x):
+    """Is x of a bsr_matrix type?
+
+    Parameters
+    ----------
+    x
+        object to check for being a bsr matrix
+
+    Returns
+    -------
+    bool
+        True if x is a bsr matrix, False otherwise
+
+    Examples
+    --------
+    >>> from scipy.sparse import bsr_matrix, isspmatrix_bsr
+    >>> isspmatrix_bsr(bsr_matrix([[5]]))
+    True
+
+    >>> from scipy.sparse import bsr_matrix, csr_matrix, isspmatrix_bsr
+    >>> isspmatrix_bsr(csr_matrix([[5]]))
+    False
+    """
     return isinstance(x, bsr_matrix)
