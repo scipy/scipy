5bc707b2837699cbf3e8114b444516be8b92e418
Matt Haberland <mdhaber@mit.edu>
2017-03-21 20:19:36 -0700

    BUG: optimize: Final (?) attempt at fixing 'encoding error' in CircleCi build
    Found and removed non-ascii characters in _linprog.py

--- scipy/optimize/_linprog.py
+++ scipy/optimize/_linprog.py
@@ -965,8 +965,8 @@ def linprog(c, A_ub=None, b_ub=None, A_eq=None, b_eq=None,
     .. [9] Bertsimas, Dimitris, and J. Tsitsiklis. "Introduction to linear
            programming." Athena Scientific 1 (1997): 997.
     .. [10] Andersen, Erling D., et al. Implementation of interior point  
-            methods for large scale linear programming. HEC/Université de 
-            Genève, 1996.
+            methods for large scale linear programming. HEC/Universite de 
+            Geneve, 1996.
 
     Examples
     --------
