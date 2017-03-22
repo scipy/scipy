0ef0d11912d08866aba04c2464a7d4aa19bf1ff8
Jonathan Tammo Siebert <siebertjonathan@aim.com>
2017-03-22 19:01:45 +0100

    DOC: Example for scipy.sparse.linalg.inv
    Added a small example to the docstring of scipy.sparse.linalg.inv

--- scipy/sparse/linalg/matfuncs.py
+++ scipy/sparse/linalg/matfuncs.py
@@ -51,6 +51,22 @@ def inv(A):
     to be non-sparse, it will likely be faster to convert `A` to dense and use
     scipy.linalg.inv.
 
+    Examples
+    --------
+    >>> from scipy.sparse import csc_matrix
+    >>> from scipy.sparse.linalg import inv
+    >>> A = csc_matrix([[1., 0.], [1., 2.]])
+    >>> Ainv = inv(A)
+    >>> Ainv
+    <2x2 sparse matrix of type '<type 'numpy.float64'>'
+        with 3 stored elements in Compressed Sparse Column format>
+    >>> A.dot(Ainv)
+    <2x2 sparse matrix of type '<type 'numpy.float64'>'
+        with 2 stored elements in Compressed Sparse Column format>
+    >>> A.dot(Ainv).todense()
+    matrix([[ 1.,  0.],
+            [ 0.,  1.]])
+
     .. versionadded:: 0.12.0
 
     """
