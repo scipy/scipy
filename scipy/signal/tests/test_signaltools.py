db4186d5dcb6ca9e62af145654f1c8950b14eb6e
Scott Sievert <scott@stsievert.com>
2017-03-27 20:48:24 -0500

    BUG: wrong condition for deciding to round fftconvolve output

--- scipy/signal/tests/test_signaltools.py
+++ scipy/signal/tests/test_signaltools.py
@@ -156,7 +156,8 @@ class TestConvolve(_TestConvolve):
         self.assertRaises(ValueError, convolve, *(a, b), **{'mode': 'valid'})
         self.assertRaises(ValueError, convolve, *(b, a), **{'mode': 'valid'})
 
-    def test_convolve_method(self, n=300):
+    def test_convolve_method(self, n=int(10e3)):
+        np.random.seed(42)
         types = sum([t for _, t in np.sctypes.items()], [])
         types = {np.dtype(t).name for t in types}
 
@@ -170,20 +171,28 @@ class TestConvolve(_TestConvolve):
         args = [(t1, t2, mode) for t1 in types for t2 in types
                                for mode in ['valid', 'full', 'same']]
 
+        array_types = {'i': np.random.choice([0, 1], size=n),
+                       'f': np.random.randn(n)}
+        array_types['b'] = array_types['u'] = array_types['i']
+        array_types['c'] = array_types['f'] + 0.5j * array_types['f']
+
         for t1, t2, mode in args:
-            x1 = np.ones(n).astype(t1)
-            x2 = np.ones(n//5).astype(t2)
+            x1 = array_types[np.dtype(t1).kind].astype(t1)
+            x2 = array_types[np.dtype(t2).kind].astype(t2)
 
             results = {key: convolve(x1, x2, method=key, mode=mode)
                        for key in ['fft', 'direct']}
 
             assert_equal(results['fft'].dtype, results['direct'].dtype)
 
+            if 'bool' in t1 and 'bool' in t2:
+                assert_equal(choose_conv_method(x1, x2), 'direct')
+                continue
+
             if any([t in {'complex64', 'float32'} for t in [t1, t2]]):
-                kwargs = {'rtol': 4.0e-3, 'atol': 1e-5}
-            elif t1 in {'float16', 'complex128'} and t2 in {'float16',
-                        'complex128'}:
-                kwargs = {'rtol': 1e-5, 'atol': 1e-3}
+                kwargs = {'rtol': 8.0e-3, 'atol': 1e-3}
+            elif 'float16' in [t1, t2]:
+                kwargs = {'rtol': 1e-2, 'atol': 1e-5}
             else:
                 kwargs = {'rtol': 1e-5, 'atol': 1e-5}
 
@@ -203,6 +212,7 @@ class TestConvolve(_TestConvolve):
                 assert_equal(fft, direct)
                 assert_equal(fft, 2**(2*n))
                 assert_equal(direct, 2**(2*n))
+
 
 class _TestConvolve2d(TestCase):
 
