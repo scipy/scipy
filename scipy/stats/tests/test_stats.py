0a73973dea0ff2e5946910aaa7a87fcbc2735bca
Shinya SUZUKI <minasitawakou@gmail.com>
2017-05-03 18:42:40 +0900

    TST: test_brunnermunzel: add input test with NaN

--- scipy/stats/tests/test_stats.py
+++ scipy/stats/tests/test_stats.py
@@ -4320,27 +4320,44 @@ class TestBrunnerMunzel(TestCase):
         assert_approx_equal(u2, -3.1374674823029505,
                             significant = self.significant)
         assert_approx_equal(p1, 0.0057862086661515377,
-                            significant = self.significant) 
+                            significant = self.significant)
 
     def test_brunnermunzel_alternative_error(self):
         alternative = "error"
+        distribution = "t"
+        nan_policy = "propagate"
         assert_(alternative not in ["two-sided", "greater", "less"])
         assert_raises(ValueError,
                       stats.brunnermunzel,
                       self.X,
                       self.Y,
-                      alternative)
+                      alternative,
+                      distribution,
+                      nan_policy)
+
+    def test_brunnermunzel_distribution_norm(self):
+        u1, p1 = stats.brunnermunzel(self.X, self.Y, distribution="normal")
+        u2, p2 = stats.brunnermunzel(self.Y, self.X, distribution="normal")
+        assert_approx_equal(p1, p2, significant = self.significant)
+        assert_approx_equal(u1, 3.1374674823029505,
+                            significant = self.significant)
+        assert_approx_equal(u2, -3.1374674823029505,
+                            significant = self.significant)
+        assert_approx_equal(p1, 0.0017041417600383024,
+                            significant = self.significant)
 
     def test_brunnermunzel_distribution_error(self):
         alternative = "two-sided"
         distribution = "error"
-        assert_(alternative not in ["t", "norm"])
+        nan_policy = "propagate"
+        assert_(alternative not in ["t", "normal"])
         assert_raises(ValueError,
                       stats.brunnermunzel,
                       self.X,
                       self.Y,
                       alternative,
-                      distribution)
+                      distribution,
+                      nan_policy)
 
     def test_brunnermunzel_empty_imput(self):
         u1, p1 = stats.brunnermunzel(self.X, [])
@@ -4354,13 +4371,49 @@ class TestBrunnerMunzel(TestCase):
         assert_equal(u3, np.nan)
         assert_equal(p3, np.nan)
 
-    def test_brunnermunzel_distribution_norm(self):
-        u1, p1 = stats.brunnermunzel(self.X, self.Y, distribution="norm")
-        u2, p2 = stats.brunnermunzel(self.Y, self.X, distribution="norm")
+    def test_brunnermunzel_nan_input_propagate(self):
+        X = [1,2,1,1,1,1,1,1,1,1,2,4,1,1, np.nan]
+        Y = [3,3,4,3,1,2,3,1,1,5,4]
+        u1, p1 = stats.brunnermunzel(X, Y, nan_policy="propagate")
+        u2, p2 = stats.brunnermunzel(Y, X, nan_policy="propagate")
+
+        assert_equal(u1, np.nan)
+        assert_equal(p1, np.nan)
+        assert_equal(u2, np.nan)
+        assert_equal(p2, np.nan)
+
+    def test_brunnermunzel_nan_input_raise(self):
+        X = [1,2,1,1,1,1,1,1,1,1,2,4,1,1, np.nan]
+        Y = [3,3,4,3,1,2,3,1,1,5,4]
+        alternative = "two-sided"
+        distribution = "t"
+        nan_policy = "raise"
+
+        assert_raises(ValueError,
+                      stats.brunnermunzel,
+                      X,
+                      Y,
+                      alternative,
+                      distribution,
+                      nan_policy)
+        assert_raises(ValueError,
+                      stats.brunnermunzel,
+                      Y,
+                      X,
+                      alternative,
+                      distribution,
+                      nan_policy)
+
+    def test_brunnermunzel_nan_input_omit(self):
+        X = [1,2,1,1,1,1,1,1,1,1,2,4,1,1, np.nan]
+        Y = [3,3,4,3,1,2,3,1,1,5,4]
+        u1, p1 = stats.brunnermunzel(X, Y, nan_policy="omit")
+        u2, p2 = stats.brunnermunzel(Y, X, nan_policy="omit")
+
         assert_approx_equal(p1, p2, significant = self.significant)
         assert_approx_equal(u1, 3.1374674823029505,
                             significant = self.significant)
         assert_approx_equal(u2, -3.1374674823029505,
                             significant = self.significant)
-        assert_approx_equal(p1, 0.0017041417600383024,
-                            significant = self.significant) 
+        assert_approx_equal(p1, 0.0057862086661515377,
+                            significant = self.significant)
