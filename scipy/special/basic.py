f55c2bc199fa560081b5277230aaca77a815eae3
endolith <endolith@gmail.com>
2017-05-13 12:10:26 -0400

    DOC:

--- scipy/special/basic.py
+++ scipy/special/basic.py
@@ -2421,8 +2421,8 @@ def zeta(x, q=None, out=None):
     ----------
     x : array_like of float
         Input data, must be real
-    q : array_like of float
-        Input data, must be real
+    q : array_like of float, optional
+        Input data, must be real.  Defaults to Riemann zeta.
     out : ndarray, optional
         Output array for the computed values.
 
