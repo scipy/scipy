e1d797cfd990ce490bcff0f983b74d57a27eed8c
Pauli Virtanen <pav@iki.fi>
2017-05-19 21:08:23 +0200

    BUG: sparse: make load_npz safe to use for uncontrolled input
    Set allow_pickle=False to block arbitrary code execution when loading
    files.

--- scipy/sparse/tests/test_matrix_io.py
+++ scipy/sparse/tests/test_matrix_io.py
@@ -1,8 +1,12 @@
+from __future__ import division, print_function, absolute_import
+
+import sys
 import os
 import numpy as np
 import tempfile
 
-from numpy.testing import assert_array_almost_equal, run_module_suite, assert_
+from numpy.testing import assert_array_almost_equal, run_module_suite, assert_, assert_raises, dec
+from scipy._lib._version import NumpyVersion
 
 from scipy.sparse import csc_matrix, csr_matrix, bsr_matrix, dia_matrix, coo_matrix, save_npz, load_npz
 
@@ -41,6 +45,24 @@ def test_save_and_load_one_entry():
     dense_matrix = np.zeros((4,6))
     dense_matrix[1,2] = 1
     _check_save_and_load(dense_matrix)
+
+
+@dec.skipif(NumpyVersion(np.__version__) < '1.10.0',
+            'disabling unpickling requires numpy >= 1.10.0')
+def test_malicious_load():
+    class Executor(object):
+        def __reduce__(self):
+            return (assert_, (False, 'unexpected code execution'))
+
+    fd, tmpfile = tempfile.mkstemp(suffix='.npz')
+    os.close(fd)
+    try:
+        np.savez(tmpfile, format=Executor())
+
+        # Should raise a ValueError, not execute code
+        assert_raises(ValueError, load_npz, tmpfile)
+    finally:
+        os.remove(tmpfile)
 
 
 if __name__ == "__main__":
