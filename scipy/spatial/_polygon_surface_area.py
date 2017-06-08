18a5cddb93d981338135f58be067b806e5044076
Tyler Reddy <tyler.je.reddy@gmail.com>
2017-06-07 22:30:47 -0600

    ENH: poly_area() now correctly handles surface area calculations irrespective of CW or CCW polygon vertex sort order.

--- scipy/spatial/_polygon_surface_area.py
+++ scipy/spatial/_polygon_surface_area.py
@@ -15,13 +15,6 @@ import numpy as np
 #
 # Distributed under the same BSD license as Scipy.
 
-def _wrap_vertices(vertices, num_vertices):
-    # wrap vertices so that last point is also first point
-    new_vertices = np.zeros((num_vertices + 1, 3))
-    new_vertices[:-1,...] = vertices
-    new_vertices[-1,...] = vertices[0,...]
-    return new_vertices
-
 def poly_area(vertices, radius=None):
     # calculate the surface area of a planar or spherical polygon
     # crude pure Python implementation for handling a single
@@ -32,19 +25,22 @@ def poly_area(vertices, radius=None):
     # TODO: check for duplicate vertices in input (and corresponding
     # unit test?)
     num_vertices = vertices.shape[0]
-    new_vertices = _wrap_vertices(vertices, num_vertices)
     area_sum = 0
 
     if radius is not None: # spherical polygons
         if radius <= 0.0:
             raise ValueError('radius must be > 0.0')
 
-        lambda_vals = np.arctan2(new_vertices[...,1], new_vertices[...,0]) # longitudes
-        phi_vals = np.arcsin(new_vertices[...,2] / radius) # latitudes
+        lambda_vals = np.arctan2(vertices[...,1], vertices[...,0]) # longitudes
+        phi_vals = np.arcsin(vertices[...,2] / radius) # latitudes
 
         for i in xrange(0, num_vertices):
-            delta_lambda = (lambda_vals[i + 1] -
-                            lambda_vals[i - 1])
+            forward_index = i + 1
+            backward_index = i - 1
+            if forward_index > (num_vertices - 1):
+                forward_index = 0
+            delta_lambda = (lambda_vals[forward_index] -
+                            lambda_vals[backward_index])
             area_sum += delta_lambda * np.sin(phi_vals[i])
         
         # the paper divides by 2 here, but my testing
@@ -52,9 +48,13 @@ def poly_area(vertices, radius=None):
         area = (radius ** 2) * area_sum
     else: # planar polygon
         for i in xrange(0, num_vertices):
-            delta_x = (new_vertices[i + 1][0] -
-                       new_vertices[i - 1][0])
-            area_sum += delta_x * new_vertices[i][1] 
+            forward_index = i + 1
+            backward_index = i - 1
+            if forward_index > (num_vertices - 1):
+                forward_index = 0
+            delta_x = (vertices[forward_index][0] -
+                       vertices[backward_index][0])
+            area_sum += delta_x * vertices[i][1]
         area = -0.5 * area_sum
     
     return abs(area)
