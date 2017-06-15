188235ae5cefb5a56d85930c8e493f8a27ddbee6
Paul van Mulbregt <pvanmulbregt@users.noreply.github.com>
2017-06-15 12:24:42 -0400

    Added some testing of scipy.special.kolmogorov/kolmogi.

--- scipy/special/tests/test_kolmogorov.py
+++ scipy/special/tests/test_kolmogorov.py
@@ -5,10 +5,9 @@ import itertools
 import numpy as np
 from numpy.testing import (TestCase, dec, assert_, run_module_suite)
 from scipy.special._testutils import FuncData
-from scipy.special import smirnov, smirnovi
+from scipy.special import smirnov, smirnovi, kolmogorov, kolmogi
 
 _rtol = 1e-10
-
 
 class TestSmirnov(TestCase):
     def test_nan(self):
@@ -172,6 +171,73 @@ class TestSmirnovi(TestCase):
 
         dataset = np.asarray(dataset)
         FuncData(smirnovi, dataset, (0, 1), 2, rtol=_rtol).check()
+
+
+class TestKolmogorov(TestCase):
+    def test_nan(self):
+        assert_(np.isnan(kolmogorov(np.nan)))
+
+    def test_basic(self):
+        dataset = [(0, 1.0),
+                   (0.5, 0.96394524366487511),
+                   (1, 0.26999967167735456),
+                   (2, 0.00067092525577969533)]
+
+        dataset = np.asarray(dataset)
+        FuncData(kolmogorov, dataset, (0,), 1, rtol=_rtol).check()
+
+    def test_smallx(self):
+        epsilon = 0.1 ** np.arange(1, 14)
+        x = np.array([0.571173265106, 0.441027698518, 0.374219690278, 0.331392659217,
+                      0.300820537459, 0.277539353999, 0.259023494805, 0.243829561254,
+                      0.231063086389, 0.220135543236, 0.210641372041, 0.202290283658,
+                      0.19487060742])
+
+        dataset = np.column_stack([x, 1-epsilon])
+        FuncData(kolmogorov, dataset, (0,), 1, rtol=_rtol).check()
+
+    @dec.knownfailureif(True, "test fails; kolmogi() is not accurate for small p")
+    def test_round_trip(self):
+        def _ki_k(_x):
+            return kolmogi(kolmogorov(_x))
+
+        x = np.linspace(0.0, 2.0, 21, endpoint=True)
+        dataset = np.column_stack([x, x])
+        FuncData(_ki_k, dataset, (0,), 1, rtol=_rtol).check()
+
+
+class TestKolmogi(TestCase):
+    def test_nan(self):
+        assert_(np.isnan(kolmogi(np.nan)))
+
+    @dec.knownfailureif(True, "test fails; kolmogi() is not accurate for small p")
+    def test_basic(self):
+        dataset = [(1.0, 0),
+                   (0.96394524366487511, 0.5),
+                   (0.26999967167735456, 1),
+                   (0.00067092525577969533, 2)]
+
+        dataset = np.asarray(dataset)
+        FuncData(kolmogi, dataset, (0,), 1, rtol=_rtol).check()
+
+    @dec.knownfailureif(True, "test fails; kolmogi() is not accurate for small p")
+    def test_smallp(self):
+        epsilon = 0.1 ** np.arange(1, 14)
+        x = np.array([0.571173265106, 0.441027698518, 0.374219690278, 0.331392659217,
+                      0.300820537459, 0.277539353999, 0.259023494805, 0.243829561254,
+                      0.231063086389, 0.220135543236, 0.210641372041, 0.202290283658,
+                      0.19487060742])
+
+        dataset = np.column_stack([1-epsilon, x])
+        FuncData(kolmogi, dataset, (0,), 1, rtol=_rtol).check()
+
+    def test_round_trip(self):
+        def _k_ki(_p):
+            return kolmogorov(kolmogi(_p))
+
+        p = np.linspace(0.1, 1.0, 10, endpoint=True)
+        dataset = np.column_stack([p, p])
+        FuncData(_k_ki, dataset, (0,), 1, rtol=_rtol).check()
 
 
 if __name__ == "__main__":
