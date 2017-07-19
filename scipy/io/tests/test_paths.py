4739227dc96b8a382f37cacdf8004c695737e131
Pauli Virtanen <pav@iki.fi>
2017-07-19 21:32:57 +0200

    TST: move test_paths under io/ + add .sav test

--- scipy/io/tests/test_paths.py
+++ scipy/io/tests/test_paths.py
@@ -1,8 +1,8 @@
 """
 Ensure that we can use pathlib.Path objects in all relevant IO functions.
 """
+import os
 import sys
-import unittest
 
 try:
     from pathlib import Path
@@ -12,7 +12,7 @@ except ImportError:
     pass
 
 import numpy as np
-from numpy.testing import assert_, assert_raises
+from numpy.testing import assert_, assert_raises, dec
 
 import scipy.io
 from scipy._lib._tmpdirs import tempdir
@@ -23,17 +23,21 @@ import scipy.sparse
 if sys.version_info < (3, 3):
     FileNotFoundError = IOError
 
-@unittest.skipIf(sys.version_info < (3, 6),
-                 'Passing path-like objects to IO functions requires Python >= 3.6')
-class TestPaths(unittest.TestCase):
+
+skip_dec = dec.skipif(sys.version_info < (3, 6),
+                      'Passing path-like objects to IO functions requires Python >= 3.6')
+
+class TestPaths(object):
     data = np.arange(5)
 
+    @skip_dec
     def test_savemat(self):
         with tempdir() as temp_dir:
             path = Path(temp_dir) / 'data.mat'
             scipy.io.savemat(path, {'data': self.data})
             assert_(path.is_file())
 
+    @skip_dec
     def test_loadmat(self):
         # Save data with string path, load with pathlib.Path
         with tempdir() as temp_dir:
@@ -43,6 +47,7 @@ class TestPaths(unittest.TestCase):
             mat_contents = scipy.io.loadmat(path)
             assert_((mat_contents['data'] == self.data).all())
 
+    @skip_dec
     def test_whosmat(self):
         # Save data with string path, load with pathlib.Path
         with tempdir() as temp_dir:
@@ -52,16 +57,13 @@ class TestPaths(unittest.TestCase):
             contents = scipy.io.whosmat(path)
             assert_(contents[0] == ('data', (1, 5), 'int64'))
 
+    @skip_dec
     def test_readsav(self):
-        # I have no idea how to create a valid IDL .sav file, so let's
-        # just try to open something that doesn't exist, and ensure that we get
-        # a FileNotFoundError (as opposed to a TypeError, if `open` can't
-        # understand the argument, or another type of exception if `readsav` is
-        # unnecessarily strict in which type it accepts.
-        with self.assertRaises(FileNotFoundError):
-            path = Path('nonexistent.sav')
-            scipy.io.readsav(path)
+        filename = os.path.join(os.path.dirname(__file__), 'data', 'scalar_string.sav')
+        path = Path(filename)
+        scipy.io.readsav(path)
 
+    @skip_dec
     def test_hb_read(self):
         # Save data with string path, load with pathlib.Path
         with tempdir() as temp_dir:
@@ -72,6 +74,7 @@ class TestPaths(unittest.TestCase):
             data_new = scipy.io.harwell_boeing.hb_read(path)
             assert_((data_new != data).nnz == 0)
 
+    @skip_dec
     def test_hb_write(self):
         with tempdir() as temp_dir:
             data = scipy.sparse.csr_matrix(scipy.sparse.eye(3))
