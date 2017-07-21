3a80f2042a2ff17b733a0fc5b9c37dc3b2388483
xoviat <xoviat@users.noreply.github.com>
2017-07-21 01:59:30 -0500

    distributor_init: cleanup

--- scipy/_distributor_init.py
+++ scipy/_distributor_init.py
@@ -8,12 +8,3 @@ For example, this is a good place to put any checks for hardware requirements.
 The scipy standard source distribution will not put code in this file, so you
 can safely replace this file with your own version.
 """
-
-import os
-import sys
-
-# Patch the DLL search path on windows so that extensions can load any DLLs that
-# they depend on (specifically gfortran DLLs) on windows.
-
-if sys.platform == 'win32':
-    os.environ["PATH"] += os.pathsep + os.path.join(os.path.dirname(__file__), '_lib')
