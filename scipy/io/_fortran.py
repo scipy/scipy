6e2071599f2e760bc77228301616c894fbff24a2
Pauli Virtanen <pav@iki.fi>
2017-08-19 16:01:09 +0200

    DOC: io: fix example in FortranFile docstring

--- scipy/io/_fortran.py
+++ scipy/io/_fortran.py
@@ -57,16 +57,15 @@ class FortranFile(object):
     >>> from scipy.io import FortranFile
     >>> f = FortranFile('test.unf', 'w')
     >>> f.write_record(np.array([1,2,3,4,5], dtype=np.int32))
-    >>> f.write_record(np.linspace(0,1,20).reshape((5,-1)).T)
+    >>> f.write_record(np.linspace(0,1,20).reshape((5,4)).T)
     >>> f.close()
 
     To read this file:
 
-    >>> from scipy.io import FortranFile
     >>> f = FortranFile('test.unf', 'r')
     >>> print(f.read_ints(np.int32))
     [1 2 3 4 5]
-    >>> print(f.read_reals(float).reshape((5,-1)).T)
+    >>> print(f.read_reals(float).reshape((5,4), order="F"))
     [[ 0.          0.05263158  0.10526316  0.15789474]
      [ 0.21052632  0.26315789  0.31578947  0.36842105]
      [ 0.42105263  0.47368421  0.52631579  0.57894737]
