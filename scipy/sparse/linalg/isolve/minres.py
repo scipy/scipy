c36b6593a25f4628a398b3cc4304dd75b7a622a0
Andras Deak <deak.andris@gmail.com>
2017-08-20 16:17:56 +0200

    DOC: Replace rotten links in the docstring of minres

--- scipy/sparse/linalg/isolve/minres.py
+++ scipy/sparse/linalg/isolve/minres.py
@@ -31,10 +31,10 @@ References
 Solution of sparse indefinite systems of linear equations,
     C. C. Paige and M. A. Saunders (1975),
     SIAM J. Numer. Anal. 12(4), pp. 617-629.
-    http://www.stanford.edu/group/SOL/software/minres.html
+    https://web.stanford.edu/group/SOL/software/minres/
 
 This file is a translation of the following MATLAB implementation:
-    http://www.stanford.edu/group/SOL/software/minres/matlab/
+    https://web.stanford.edu/group/SOL/software/minres/minres-matlab.zip
 """
 
 
