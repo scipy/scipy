ba7059ef15213cd084cd3d08e208e815b8a03e78
Antonio H Ribeiro <antonior92@gmail.com>
2017-08-22 15:21:33 -0300

    ENH: optimize: barrier subproblem sparse jacobians computation
    Beter way of assembling barrier subproblem sparse jacobians.
    The new way is significanlty faster for large matrices.

--- scipy/optimize/_large_scale_constrained/tr_interior_point.py
+++ scipy/optimize/_large_scale_constrained/tr_interior_point.py
@@ -159,22 +159,56 @@ class BarrierSubproblem:
         if self.n_ineq == 0:
             return J_eq
         else:
-            if spc.issparse(J_eq) or spc.issparse(J_eq):
-                # TODO: improve efficiency of this operation
-                S = spc.diags((s,), (0,))
-                return spc.bmat([[J_eq, None],
-                                 [J_ineq, S]], "csr")
+            if spc.issparse(J_eq) or spc.issparse(J_ineq):
+                # It is expected that J_eq and J_ineq
+                # are already `csr_matrix` because of
+                # the way ``BoxConstraint``, ``NonlinearConstraint``
+                # and ``LinearConstraint`` are defined.
+                J_eq = spc.csr_matrix(J_eq)
+                J_ineq = spc.csr_matrix(J_ineq)
+                return self._assemble_sparse_jacobian(J_eq, J_ineq, s)
             else:
                 S = np.diag(s)
                 zeros = np.zeros((self.n_eq, self.n_ineq))
-                # Convert to dense matrix
+                # Convert to matrix
                 if spc.issparse(J_ineq):
-                    J_ineq = J_ineq.todense()
+                    J_ineq = J_ineq.toarray()
                 if spc.issparse(J_eq):
-                    J_eq = J_eq.todense()
+                    J_eq = J_eq.toarray()
                 # Concatenate matrices
                 return np.asarray(np.bmat([[J_eq, zeros],
                                            [J_ineq, S]]))
+
+    def _assemble_sparse_jacobian(self, J_eq, J_ineq, s):
+        """Assemble sparse jacobian given its components.
+
+        Given ``J_eq``, ``J_ineq`` and ``s`` returns:
+            jacobian = [ J_eq,     0     ]
+                       [ J_ineq, diag(s) ]
+
+        It is equivalent to:
+            spc.bmat([[ J_eq,   None    ],
+                      [ J_ineq, diag(s) ]], "csr")
+        but significantly more efficient for this
+        given structure.
+        """
+        n_vars, n_ineq, n_eq = self.n_vars, self.n_ineq, self.n_eq
+        J_aux = spc.vstack([J_eq, J_ineq], "csr")
+        indptr, indices, data = J_aux.indptr, J_aux.indices, J_aux.data
+        new_indptr = indptr + np.hstack((np.zeros(n_eq, dtype=int),
+                                         np.arange(n_ineq+1, dtype=int)))
+        size = indices.size+n_ineq
+        new_indices = np.empty(size)
+        new_data = np.empty(size)
+        mask = np.full(size, False, bool)
+        mask[new_indptr[-n_ineq:]-1] = True
+        new_indices[mask] = n_vars+np.arange(n_ineq)
+        new_indices[~mask] = indices
+        new_data[mask] = s
+        new_data[~mask] = data
+        J = spc.csr_matrix((new_data, new_indices, new_indptr),
+                           (n_eq + n_ineq, n_vars + n_ineq))
+        return J
 
     def lagrangian_hessian_x(self, z, v):
         """Returns Lagrangian Hessian (in relation to `x`) -> Hx"""
