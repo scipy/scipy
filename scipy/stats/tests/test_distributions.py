16bf9a9e2bc1699213e3e918e9f050a413eb121d
Zhiqing Xiao <ZhiqingXiao@users.noreply.github.com>
2017-08-25 20:18:02 +0800

    TST: change the test data
    TST: change the test data
    (follow up of ENH: Gamma/Gompertz distribution #7791)
    
    (+) Change the inputs and outputs of tests to what can be calculated by pen.
    (+) Bug fix for my previous commit (forget to add the package name "stats" in front of the object "gamma_gompertz".

--- scipy/stats/tests/test_distributions.py
+++ scipy/stats/tests/test_distributions.py
@@ -1380,27 +1380,32 @@ class TestGumbelL(object):
         
 class TestGammaGompertz(object):
     def test_pdf(self):
-        vals = gamma_gompertz.pdf(np.arange(0., 5., 1.), 1., 1.)
-        expected = [1., 0.367879441, 0.135335283, 0.0497870684, 0.0183156389]
+        vals = stats.gamma_gompertz.pdf(np.log(np.arange(1., 6., 1.)), 1., 2.)
+        expected = [0.5, 4./9., 0.375, 8./25., 5./18.]
         assert_almost_equal(vals, expected)
         
     def test_cdf(self):
-        vals = gamma_gompertz.cdf(np.arange(0., 5., 1.), 1., 1.)
-        expected = [0., 0.63212056, 0.86466472, 0.95021293, 0.98168436]
+        vals = stats.gamma_gompertz.cdf(np.log(np.arange(1., 6., 1.)), 1., 2.)
+        expected = [0., 1./3., 0.5, 0.6, 2./3.]
         assert_almost_equal(vals, expected)
         
     def test_ppf(self):
-        vals = gamma_gompertz.ppf(np.arange(0., 1., 0.2), 1., 1.)
-        expected = [0., 0.22314355, 0.51082562, 0.91629073, 1.60943791]
+        vals = stats.gamma_gompertz.ppf([0., 1./3., 0.5, 0.6, 2./3.], 1., 2.)
+        expected = np.log(np.arange(1., 6., 1.))
+        assert_almost_equal(vals, expected)
         
     def test_expon(self):
         # test against expon (sepcial case for beta=1)
-        points = [1, 2, 3]
-        pdf1 = gamma_gompertz.pdf(points, 1., 1.)
+        points = [1., 2., 3.]
+        pdf1 = stats.gamma_gompertz.pdf(points, 1., 1.)
         pdf2 = stats.expon.pdf(points)
         assert_almost_equal(pdf1, pdf2)
 
+    def test_cdf_ppf_accuracy():
+        p = stats.gamma_gompertz.ppf(gamma_gompertz.cdf(1e-8, 1., 1.), 1., 1.)
+        assert_allclose(p, 1e-8)
         
+
 class TestArrayArgument(object):  # test for ticket:992
     def setup_method(self):
         np.random.seed(1234)
@@ -2999,11 +3004,6 @@ def test_gompertz_accuracy():
     p = stats.gompertz.ppf(stats.gompertz.cdf(1e-100, 1), 1)
     assert_allclose(p, 1e-100)
 
-    
-def test_gamma_gompertz_accuracy():
-    p = gamma_gompertz.ppf(gamma_gompertz.cdf(1e-8, 1., 1.), 1., 1.)
-    assert_allclose(p, 1e-8)
-    
 
 def test_truncexpon_accuracy():
     # regression test for gh-4035
