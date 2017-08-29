c0dadbce83b078a93c6032f3f028b82ad116c3b2
CJ Carey <perimosocordiae@gmail.com>
2017-08-29 09:09:02 -0400

    Spelling corrections

--- scipy/sparse/coo.py
+++ scipy/sparse/coo.py
@@ -87,7 +87,7 @@ class coo_matrix(_data_matrix, _minmax_mixin):
 
     Examples
     --------
-    Constructing a empty matrix
+    Constructing an empty matrix
 
     >>> from scipy.sparse import coo_matrix
     >>> coo_matrix((3, 4), dtype=np.int8).toarray()
@@ -106,13 +106,13 @@ class coo_matrix(_data_matrix, _minmax_mixin):
            [0, 0, 0, 0],
            [0, 0, 0, 5]])
 
-    Construcing a matrix with duplicates indicies
+    Constructing a matrix with duplicate indices
 
     >>> row  = np.array([0, 0, 1, 3, 1, 0, 0])
     >>> col  = np.array([0, 2, 1, 3, 1, 0, 0])
     >>> data = np.array([1, 1, 1, 1, 1, 1, 1])
     >>> coo = coo_matrix((data, (row, col)), shape=(4, 4))
-    >>> # duplicate indicies are maintained until implicitly or explicitly summed
+    >>> # Duplicate indices are maintained until implicitly or explicitly summed
     >>> np.max(coo.data)
     1
     >>> coo.to_array()
