4b92bc9fffc5153e4df1b3dd0a3d7e71af3f01ab
Ilhan Polat <ilhanpolat@gmail.com>
2017-09-09 03:07:59 +0200

    ENH: Added BLAS wrappers for sbmv/hbmv and tests

--- scipy/linalg/tests/test_fblas.py
+++ scipy/linalg/tests/test_fblas.py
@@ -10,7 +10,7 @@ from __future__ import division, print_function, absolute_import
 
 from numpy import float32, float64, complex64, complex128, arange, array, \
                   zeros, shape, transpose, newaxis, common_type, conjugate, \
-                  tril_indices, ones, mod
+                  tril_indices, ones, mod, diag
 
 from numpy.random import rand, seed
 from scipy.linalg import _fblas as fblas
@@ -544,6 +544,36 @@ except AttributeError:
 class TestZgemv(BaseGemv):
     blas_func = fblas.zgemv
     dtype = complex128
+
+
+def test_sbmv_hbmv():
+    seed(1234)
+    for ind, dtype in enumerate(DTYPES):
+        n = 6
+        k = 2
+        A = zeros((n, n), dtype=dtype)
+        Ab = zeros((k+1, n), dtype=dtype)
+
+        # Form the array and its packed banded storage
+        A[arange(n), arange(n)] = rand(n)
+        for ind2 in range(1, k+1):
+            temp = rand(n-ind2)
+            A[arange(n-ind2), arange(ind2, n)] = temp
+            Ab[-1-ind2, ind2:] = temp
+        A = A.astype(dtype)
+        A = A + A.T if ind < 2 else A + A.conj().T
+        Ab[-1, :] = diag(A)
+        x = rand(n).astype(dtype)
+        y = rand(n).astype(dtype)
+        alpha, beta = dtype(1.25), dtype(3)
+
+        if ind > 1:
+            func, = get_blas_funcs(('hbmv',), dtype=dtype)
+        else:
+            func, = get_blas_funcs(('sbmv',), dtype=dtype)
+        y1 = func(k=k, alpha=alpha, a=Ab, x=x, y=y, beta=beta)
+        y2 = alpha * A.dot(x) + beta * y
+        assert_array_almost_equal(y1, y2)
 
 
 def test_spmv_hpmv():
