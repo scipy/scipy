8bfaacf3dfb9655f943a5ac9b8394d08d5eeffd7
Ralf Gommers <ralf.gommers@gmail.com>
2017-09-13 23:08:33 +1200

    MAINT: some doc and style fixes for linalg.clarkson_woodruff_transform

--- scipy/linalg/_sketches.py
+++ scipy/linalg/_sketches.py
@@ -14,19 +14,24 @@ __all__ = ['clarkson_woodruff_transform']
 
 def cwt_matrix(n_rows, n_columns, seed=None):
     r""""
-    Generate a matrix S for the Clarkson-Woodruff sketch. Given the desired
-    size of matrix, the method returns a matrix S of size (n_rows, n_columns)
-    where each column has all the entries set to 0 less one position which has
-    been randomly set to +1 o -1 with equal probability.
+    Generate a matrix S for the Clarkson-Woodruff sketch.
+
+    Given the desired size of matrix, the method returns a matrix S of size
+    (n_rows, n_columns) where each column has all the entries set to 0 less one
+    position which has been randomly set to +1 or -1 with equal probability.
 
     Parameters
     ----------
     n_rows: int
-        number of rows of S
+        Number of rows of S
     n_columns: int
-        number of columns of S
-    seed: int
-        Seed the generator
+        Number of columns of S
+    seed : None or int or `numpy.random.RandomState` instance, optional
+        This parameter defines the ``RandomState`` object to use for drawing
+        random variates.
+        If None (or ``np.random``), the global ``np.random`` state is used.
+        If integer, it is used to seed the local ``RandomState`` instance.
+        Default is None.
 
     Returns
     -------
@@ -47,42 +52,50 @@ def cwt_matrix(n_rows, n_columns, seed=None):
 
     return S
 
+
 def clarkson_woodruff_transform(input_matrix, sketch_size, seed=None):
     r""""
-    Given an input_matrix  of size (n, d), compute a matrix A' of size
-    (sketch_size, d) which holds:
-    
+    Find low-rank matrix approximation via the Clarkson-Woodruff Transform.
+
+    Given an input_matrix ``A`` of size ``(n, d)``, compute a matrix ``A'`` of
+    size (sketch_size, d) which holds:
+
     .. math:: ||Ax|| = (1 \pm \epsilon)||A'x||
 
     with high probability.
 
     The error is related to the number of rows of the sketch and it is bounded
-    
+
     .. math:: poly(r(\epsilon^{-1}))
-    
+
     Parameters
     ----------
-    input_matrix: (n, d) array_like
-        Input matrix
+    input_matrix: array_like
+        Input matrix, of shape ``(n, d)``.
     sketch_size: int
-        number of rows for the sketch
-    seed: int
-        Seed the generator
+        Number of rows for the sketch.
+    seed : None or int or `numpy.random.RandomState` instance, optional
+        This parameter defines the ``RandomState`` object to use for drawing
+        random variates.
+        If None (or ``np.random``), the global ``np.random`` state is used.
+        If integer, it is used to seed the local ``RandomState`` instance.
+        Default is None.
 
     Returns
     -------
-    A' : (sketch_size, d) array_like
-        Sketch of A
+    A' : array_like
+        Sketch of the input matrix ``A``, of size ``(sketch_size, d)``.
 
     Notes
     -----
-    This is an implementation of the Clarkson-Woodruff Transform (CountSketch)
-    A' can be computed in O(nnz(A)) but we don't take advantage of sparse
-    matrices in this implementation
+    This is an implementation of the Clarkson-Woodruff Transform (CountSketch).
+    ``A'`` can be computed in principle in ``O(nnz(A))`` (with ``nnz`` meaning
+    the number of nonzero entries), however we don't take advantage of sparse
+    matrices in this implementation.
 
     Examples
     --------
-    Given a big dense matrix `A`:
+    Given a big dense matrix ``A``:
 
     >>> from scipy import linalg
     >>> n_rows, n_columns, sketch_n_rows = (2000, 100, 100)
@@ -93,15 +106,16 @@ def clarkson_woodruff_transform(input_matrix, sketch_size, seed=None):
     >>> sketch.shape
     (100, 100)
     >>> normA = linalg.norm(A)
-    >>> normSketch = linalg.norm(sketch)
-    >>> # with high probability
-    >>> # abs(normA-normSketch) < threshold
+    >>> norm_sketch = linalg.norm(sketch)
+
+    Now with high probability, the condition ``abs(normA-normSketch) <
+    threshold`` holds.
 
     References
     ----------
     .. [1] Kenneth L. Clarkson and David P. Woodruff. Low rank approximation and
            regression in input sparsity time. In STOC, 2013.
-    """
 
+    """
     S = cwt_matrix(sketch_size, input_matrix.shape[0], seed)
     return np.dot(S, input_matrix)
--- scipy/linalg/tests/test_sketches.py
+++ scipy/linalg/tests/test_sketches.py
@@ -4,19 +4,17 @@ from __future__ import division, print_function, absolute_import
 import numpy as np
 from scipy.linalg import clarkson_woodruff_transform
 
-from numpy.testing import (assert_equal, assert_almost_equal,
-                           assert_array_almost_equal, assert_array_equal,
-                           assert_raises, assert_, assert_allclose)
-
-import pytest
+from numpy.testing import assert_
 
 
 def make_random_dense_gaussian_matrix(n_rows, n_columns, mu=0, sigma=0.01):
     """
     Make some random data with Gaussian distributed values
     """
+    np.random.seed(142352345)
     res = np.random.normal(mu, sigma, n_rows*n_columns)
     return np.reshape(res, (n_rows, n_columns))
+
 
 class TestClarksonWoodruffTransform(object):
     """
@@ -33,16 +31,14 @@ class TestClarksonWoodruffTransform(object):
     threshold = 0.1
 
     dense_big_matrix = make_random_dense_gaussian_matrix(n_matrix_rows,
-        n_matrix_columns)
-    
-    def test_sketch_dimensions(self):
-        sketch = clarkson_woodruff_transform(
-            self.dense_big_matrix,
-            self.n_sketch_rows)
+                                                         n_matrix_columns)
 
-        assert_(sketch.shape == (
-            self.n_sketch_rows,
-            self.dense_big_matrix.shape[1]))
+    def test_sketch_dimensions(self):
+        sketch = clarkson_woodruff_transform(self.dense_big_matrix,
+                                             self.n_sketch_rows)
+
+        assert_(sketch.shape == (self.n_sketch_rows,
+                                 self.dense_big_matrix.shape[1]))
 
     def test_sketch_rows_norm(self):
         # Given the probabilistic nature of the sketches
@@ -54,13 +50,12 @@ class TestClarksonWoodruffTransform(object):
                  1302443994, 1521083269, 1501189312, 1126232505, 1533465685]
 
         for seed_ in seeds:
-            sketch = clarkson_woodruff_transform(
-                self.dense_big_matrix,
-                self.n_sketch_rows,
-                seed_)
+            sketch = clarkson_woodruff_transform(self.dense_big_matrix,
+                                                 self.n_sketch_rows, seed_)
 
-            #we could use other norms (like L2)
+            # We could use other norms (like L2)
             err = np.linalg.norm(self.dense_big_matrix) - np.linalg.norm(sketch)
             if err > self.threshold:
                 n_errors += 1
+
         assert_(n_errors == 0)
