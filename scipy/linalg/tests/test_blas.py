9bcaf3f6c5b9e936d6ed95bd6e189f40e89f8898
Ilhan Polat <ilhanpolat@gmail.com>
2017-10-06 13:32:12 +0200

    TST: Relax test_trsm precision to 5 decimals
    And switch to triangular solver.
    
    (backport of gh-7986)

--- scipy/linalg/tests/test_blas.py
+++ scipy/linalg/tests/test_blas.py
@@ -23,7 +23,8 @@ from numpy import float32, float64, complex64, complex128, arange, triu, \
                   nonzero
 
 from numpy.random import rand, seed
-from scipy.linalg import _fblas as fblas, get_blas_funcs, toeplitz, solve
+from scipy.linalg import _fblas as fblas, get_blas_funcs, toeplitz, solve, \
+                                          solve_triangular
 
 try:
     from scipy.linalg import _cblas as cblas
@@ -1030,6 +1031,7 @@ class TestTRMM(object):
 def test_trsm():
     seed(1234)
     for ind, dtype in enumerate(DTYPES):
+        tol = np.finfo(dtype).eps*1000
         func, = get_blas_funcs(('trsm',), dtype=dtype)
 
         # Test protection against size mismatches
@@ -1052,26 +1054,26 @@ def test_trsm():
         x1 = func(alpha=alpha, a=A, b=B1)
         assert_equal(B1.shape, x1.shape)
         x2 = solve(Au, alpha*B1)
-        assert_array_almost_equal(x1, x2)
+        assert_allclose(x1, x2, atol=tol)
 
         x1 = func(alpha=alpha, a=A, b=B1, trans_a=1)
         x2 = solve(Au.T, alpha*B1)
-        assert_array_almost_equal(x1, x2)
+        assert_allclose(x1, x2, atol=tol)
 
         x1 = func(alpha=alpha, a=A, b=B1, trans_a=2)
         x2 = solve(Au.conj().T, alpha*B1)
-        assert_array_almost_equal(x1, x2)
+        assert_allclose(x1, x2, atol=tol)
 
         x1 = func(alpha=alpha, a=A, b=B1, diag=1)
         Au[arange(m), arange(m)] = dtype(1)
         x2 = solve(Au, alpha*B1)
-        assert_array_almost_equal(x1, x2)
+        assert_allclose(x1, x2, atol=tol)
 
         x1 = func(alpha=alpha, a=A, b=B2, diag=1, side=1)
         x2 = solve(Au.conj().T, alpha*B2.conj().T)
-        assert_array_almost_equal(x1, x2.conj().T)
+        assert_allclose(x1, x2.conj().T, atol=tol)
 
         x1 = func(alpha=alpha, a=A, b=B2, diag=1, side=1, lower=1)
         Al[arange(m), arange(m)] = dtype(1)
         x2 = solve(Al.conj().T, alpha*B2.conj().T)
-        assert_array_almost_equal(x1, x2.conj().T)
+        assert_allclose(x1, x2.conj().T, atol=tol)
