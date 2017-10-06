2c002457b97f7fc72a6397398043a1aa7f5c8598
xoviat <xoviat@users.noreply.github.com>
2017-10-05 22:40:55 -0500

    TST: special: reduce mpmath test points
    Reduce hyp2f1 test points to ~300.

--- scipy/special/tests/test_mpmath.py
+++ scipy/special/tests/test_mpmath.py
@@ -108,8 +108,11 @@ def test_hyp0f1_gh_1609():
 @pytest.mark.xfail(reason="hyp2f1 produces wrong/nonstandard values (gh-7961)")
 @check_version(mpmath, '1.0.0')
 def test_hyp2f1_strange_points():
-    numbers = [2, -2, -1, 0.7, 100, -50, 1e30, -1000]    
-    pts = itertools.product(numbers, repeat=4)
+    pts = [
+        (2, -1, -1, 0.7),  # expected: 2.4
+        (2, -2, -2, 0.7),  # expected: 3.87
+    ]
+    pts += list(itertools.product([2, -0.7, -1000, 1e30], repeat=4))
     kw = dict(eliminate=True)
     dataset = [p + (float(mpmath.hyp2f1(*p, **kw)),) for p in pts]
     dataset = np.array(dataset, dtype=np.float_)
