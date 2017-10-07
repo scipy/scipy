0109bbf03efc543f8a5584d91f29d36698642b63
xoviat <xoviat@users.noreply.github.com>
2017-10-07 15:03:11 -0500

    TST: special: mpmath: filter points to requested crieteria

--- scipy/special/tests/test_mpmath.py
+++ scipy/special/tests/test_mpmath.py
@@ -113,6 +113,10 @@ def test_hyp2f1_strange_points():
         (2, -2, -2, 0.7),  # expected: 3.87
     ]
     pts += list(itertools.product([2, 1, -0.7, -1000], repeat=4))
+    pts = [
+        (a, b, c, x) for a, b, c, x in pts 
+        if b == c and round(b) == b and b < 0
+    ]
     kw = dict(eliminate=True)
     dataset = [p + (float(mpmath.hyp2f1(*p, **kw)),) for p in pts]
     dataset = np.array(dataset, dtype=np.float_)
