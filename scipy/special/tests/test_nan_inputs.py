ede5590b12dbeecd244fdafa4443b6c3a9d9ef40
Josh Wilson <person142@users.noreply.github.com>
2017-10-07 20:39:42 -0500

    BUG: special: fix ufunc results for `nan` arguments
    Closes gh-7988.

--- scipy/special/tests/test_nan_inputs.py
+++ scipy/special/tests/test_nan_inputs.py
@@ -11,31 +11,7 @@ import scipy.special as sc
 from scipy._lib._numpy_compat import suppress_warnings
 
 
-KNOWNFAILURES = {
-    sc.bdtrc: 'get 1.0',
-    sc.chdtri: 'code hangs',
-    sc.entr: 'get -inf',
-    sc.expn: 'get inf',
-    sc.gammainccinv: 'code hangs',
-    sc.gammaincinv: 'code hangs',
-    sc.gammasgn: 'get 1.0',
-    sc.hankel1: 'segfault',
-    sc.hankel1e: 'segfault',
-    sc.hankel2: 'segfault',
-    sc.hankel2e: 'segfault',
-    sc.ive: 'segfault',
-    sc.jv: 'segfault',
-    sc.jve: 'segfault',
-    sc.kl_div: 'get inf',
-    sc.kv: 'segfault',
-    sc.kve: 'segfault',
-    sc.pbdv: 'hangs on Win32',
-    sc.pbvv: 'crashes on Win32',
-    sc.rel_entr: 'get inf',
-    sc.yv: 'segfault',
-    sc.yve: 'segfault'
-}
-
+KNOWNFAILURES = {}
 
 POSTPROCESSING = {
     sc.hyp2f0: lambda x, y: x  # Second argument is an error estimate
