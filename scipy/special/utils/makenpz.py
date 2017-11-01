6c6ded8f552cc4c7ee103e7bf3b2be36041aa67c
Pauli Virtanen <pav@iki.fi>
2017-11-01 19:12:10 +0100

    MAINT: special: generate test data npz files in setup.py and remove from VC

--- scipy/special/utils/makenpz.py
+++ scipy/special/utils/makenpz.py
@@ -9,35 +9,58 @@ from __future__ import division, print_function, absolute_import
 
 import os
 import numpy as np
-from optparse import OptionParser
+import argparse
+
+from distutils.util import newer
 
 
 def main():
-    p = OptionParser()
-    options, args = p.parse_args()
+    p = argparse.ArgumentParser(usage=__doc__.strip())
+    p.add_argument('--use-timestamp', action='store_true', default=False,
+                   help="don't rewrite npz file if it is newer than sources")
+    p.add_argument('dirname')
+    args = p.parse_args()
 
-    if len(args) != 1:
-        p.error("no valid directory given")
-
-    inp = args[0]
+    inp = os.path.normpath(args.dirname)
     outp = inp + ".npz"
 
+    # Skip rebuilding if no sources
+    if os.path.isfile(outp) and not os.path.isdir(inp):
+        print("[makenpz] {} not rebuilt".format(outp))
+        return
+
+    # Find source files
     files = []
     for dirpath, dirnames, filenames in os.walk(inp):
         for fn in filenames:
             if fn.endswith('.txt'):
-                files.append(
-                    (dirpath[len(inp)+1:] + '/' + fn[:-4],
-                     os.path.join(dirpath, fn)))
+                key = dirpath[len(inp)+1:] + '-' + fn[:-4]
+                key = key.strip('-')
+                files.append((key, os.path.join(dirpath, fn)))
+
+    # Check if changes required
+    if args.use_timestamp and os.path.isfile(outp):
+        try:
+            old_data = np.load(outp)
+            try:
+                changed = set(old_data.keys()) != set(key for key, _ in files)
+            finally:
+                old_data.close()
+        except (IOError, OSError):
+            # corrupted file
+            changed = True
+
+        changed = changed or any(newer(fn, outp) for key, fn in files)
+        changed = changed or newer(__file__, outp)
+        if not changed:
+            print("[makenpz] {} is already up to date".format(outp))
+            return
 
     data = {}
     for key, fn in files:
-        key = key.replace('/', '-').strip('-')
-        try:
-            data[key] = np.loadtxt(fn)
-        except ValueError:
-            print("Failed to load", fn)
+        data[key] = np.loadtxt(fn)
 
+    print("[makenpz] generating {}".format(outp))
     savez_compress(outp, **data)
 
 
