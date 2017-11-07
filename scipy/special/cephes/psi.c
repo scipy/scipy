370bb4f4777b9660fca970a3f8ed8014981daca5
Josh Wilson <person142@users.noreply.github.com>
2017-11-06 18:47:05 -0600

    ENH: special: use rational approximation for `digamma` on `[1, 2]`
    This speeds up the function evaluation on that interval. Closes
    gh-8127.

--- scipy/special/cephes/psi.c
+++ scipy/special/cephes/psi.c
@@ -49,10 +49,19 @@
  *     message         condition      value returned
  * psi singularity    x integer <=0      NPY_INFINITY
  */
-
+
 /*
  * Cephes Math Library Release 2.8:  June, 2000
  * Copyright 1984, 1987, 1992, 2000 by Stephen L. Moshier
+ */
+
+/*
+ * Code for the rational approximation on [1, 2] is:
+ *
+ * (C) Copyright John Maddock 2006.
+ * Use, modification and distribution are subject to the
+ * Boost Software License, Version 1.0. (See accompanying file
+ * LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
  */
 
 #include "mconf.h"
@@ -66,6 +75,57 @@ static double A[] = {
     -8.33333333333333333333E-3,
     8.33333333333333333333E-2
 };
+
+
+double digamma_imp_1_2(double x)
+{
+    /*
+     * Rational approximation on [1, 2] taken from Boost.
+     *
+     * Now for the approximation, we use the form:
+     *
+     * digamma(x) = (x - root) * (Y + R(x-1))
+     *
+     * Where root is the location of the positive root of digamma,
+     * Y is a constant, and R is optimised for low absolute error
+     * compared to Y.
+     *
+     * Maximum Deviation Found:               1.466e-18
+     * At double precision, max error found:  2.452e-17
+     */
+    double r, g;
+
+    static const float Y = 0.99558162689208984f;
+
+    static const double root1 = 1569415565.0 / 1073741824.0;
+    static const double root2 = (381566830.0 / 1073741824.0) / 1073741824.0;
+    static const double root3 = 0.9016312093258695918615325266959189453125e-19;
+
+   static double P[] = {
+       -0.0020713321167745952,
+       -0.045251321448739056,
+       -0.28919126444774784,
+       -0.65031853770896507,
+       -0.32555031186804491,
+       0.25479851061131551
+   };
+   static double Q[] = {
+       -0.55789841321675513e-6,
+       0.0021284987017821144,
+       0.054151797245674225,
+       0.43593529692665969,
+       1.4606242909763515,
+       2.0767117023730469,
+       1.0
+   };
+   g = x - root1;
+   g -= root2;
+   g -= root3;
+   r = polevl(x - 1.0, P, 5) / polevl(x - 1.0, Q, 6);
+
+   return g * Y + g * r;
+}
+
 
 double psi(x)
 double x;
@@ -110,6 +170,11 @@ double x;
 	    y += 1.0 / w;
 	}
 	y -= NPY_EULER;
+	goto done;
+    }
+
+    if ((1.0 <= x) && (x <= 2.0)) {
+	y = digamma_imp_1_2(x);
 	goto done;
     }
 
