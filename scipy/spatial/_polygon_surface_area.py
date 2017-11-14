849e4035daa8ab3e1e2f46ccd2dcf6c0ccfa0fd8
Tyler Reddy <tyler.je.reddy@gmail.com>
2017-11-14 15:02:00 -0700

    Improved support for input spheres with non-unit radii in _polygon_surface_area.py; temporary disable of cython spherical area calc switch.

--- scipy/spatial/_polygon_surface_area.py
+++ scipy/spatial/_polygon_surface_area.py
@@ -19,6 +19,33 @@ from six.moves import xrange
 #
 # Distributed under the same BSD license as Scipy.
 
+def convert_cartesian_array_to_spherical_array(coord_array,angle_measure='radians'):
+    '''Take shape (N,3) cartesian coord_array and return an array of the same shape in spherical polar form (r, theta, phi). Based on StackOverflow response: http://stackoverflow.com/a/4116899
+    use radians for the angles by default, degrees if angle_measure == 'degrees' '''
+    spherical_coord_array = np.zeros(coord_array.shape)
+    xy = coord_array[...,0]**2 + coord_array[...,1]**2
+    spherical_coord_array[...,0] = np.sqrt(xy + coord_array[...,2]**2)
+    spherical_coord_array[...,1] = np.arctan2(coord_array[...,1], coord_array[...,0])
+    spherical_coord_array[...,2] = np.arccos(coord_array[...,2] / spherical_coord_array[...,0])
+    if angle_measure == 'degrees':
+        spherical_coord_array[...,1] = np.degrees(spherical_coord_array[...,1])
+        spherical_coord_array[...,2] = np.degrees(spherical_coord_array[...,2])
+    return spherical_coord_array
+
+def convert_spherical_array_to_cartesian_array(spherical_coord_array,angle_measure='radians'):
+    '''Take shape (N,3) spherical_coord_array (r,theta,phi) and return an array of the same shape in cartesian coordinate form (x,y,z). Based on the equations provided at: http://en.wikipedia.org/wiki/List_of_common_coordinate_transformations#From_spherical_coordinates
+    use radians for the angles by default, degrees if angle_measure == 'degrees' '''
+    cartesian_coord_array = np.zeros(spherical_coord_array.shape)
+    #convert to radians if degrees are used in input (prior to Cartesian conversion process)
+    if angle_measure == 'degrees':
+        spherical_coord_array[...,1] = np.deg2rad(spherical_coord_array[...,1])
+        spherical_coord_array[...,2] = np.deg2rad(spherical_coord_array[...,2])
+    #now the conversion to Cartesian coords
+    cartesian_coord_array[...,0] = spherical_coord_array[...,0] * np.cos(spherical_coord_array[...,1]) * np.sin(spherical_coord_array[...,2])
+    cartesian_coord_array[...,1] = spherical_coord_array[...,0] * np.sin(spherical_coord_array[...,1]) * np.sin(spherical_coord_array[...,2])
+    cartesian_coord_array[...,2] = spherical_coord_array[...,0] * np.cos(spherical_coord_array[...,2])
+    return cartesian_coord_array
+
 def _vertex_index_strider(index, num_vertices):
     # handle the wrapping / iteration over
     # polygon vertices in either CW or CCW
@@ -64,7 +91,7 @@ def _spherical_polygon_area(vertices, radius, discretizations):
         new_pts = _slerp(vertices[i], vertices[i-1], discretizations)
 
         lambda_range = np.arctan2(new_pts[...,1], new_pts[...,0])
-        phi_range = np.arcsin((new_pts[...,2] / radius))
+        phi_range = np.arcsin((new_pts[...,2]))
         area_element = 0
         for j in xrange(discretizations - 1):
             delta_lambda = (lambda_range[j+1] -
@@ -72,8 +99,6 @@ def _spherical_polygon_area(vertices, radius, discretizations):
             second_term = 2 + np.sin(phi_range[j]) + np.sin(phi_range[j+1])
             area_element += (delta_lambda * second_term * (radius ** 2) * 0.5)
         area_sum += area_element
-    # the paper divides by 2 here, but my testing
-    # suggests we should not do that!
     area = area_sum
     return area
 
@@ -93,14 +118,23 @@ def poly_area(vertices, radius=None, threshold=1e-21,
                                             threshold=threshold)
         raise ValueError(err_str)
 
+
     if radius is not None: # spherical polygons
         if radius <= threshold:
             err_str = 'radius must be > {threshold}'.format(threshold=threshold)
             raise ValueError(err_str)
+
+        # normalize vertices to unit sphere
+        vertices = convert_cartesian_array_to_spherical_array(vertices)
+        vertices[...,0] = 1.
+        vertices = convert_spherical_array_to_cartesian_array(vertices)
+
         if cython is None:
             area = _spherical_polygon_area(vertices, radius, discretizations)
         else: # cython code for spherical polygon SA
-            area = _surface_area.spherical_polygon_area(vertices, radius)
+            area = _spherical_polygon_area(vertices, radius, discretizations)
+            #area = _surface_area.spherical_polygon_area(vertices, radius,
+                                                        #discretizations)
     else: # planar polygon
         if cython is None:
             area = _planar_polygon_area(vertices)
