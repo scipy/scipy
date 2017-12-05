Added Zolotarev CDF method. Fix return data masking bug. Disable FFT by default for CDF. Increase threshold to 100 for PDF FFT. Take real part in quad to remove complexwarning.

scipy/stats/_continuous_distns.py
@@ -3385,13 +3385,18 @@ class levy_stable_gen(rv_continuous):
     For evaluation of pdf we use either Zolotarev S_0 parameterization with integration, 
     direct integration of standard parameterization of characteristic function or FFT of 
     characteristic function. FFT is used if number of points is greater than 
-    levy_stable.pdf_fft_min_points_threshold (defaults to 5) otherwise we use one of the 
-    other methods.  The default method os Zolotarev's but can be changed by setting 
-    levy_stable.pdf_default_method to any string other than 'zolotarev'. To increase accuracy 
-    of FFT calculation one can specify levy_stable.pdf_fft_grid_spacing (defaults to 0.01) 
-    and pdf_fft_n_points_two_power (defaults to a value that covers the input range * 4). 
-    Setting pdf_fft_n_points_two_power to 16 should be sufficiently accurate in most cases 
-    at the expense of CPU time.
+    levy_stable.pdf_fft_min_points_threshold (defaults to 100) otherwise we use one of the 
+    other methods. Setting the threshold to None will disable FFT. The default method is 
+    Zolotarev's but can be changed by setting levy_stable.pdf_default_method to any string 
+    other than 'zolotarev'. To increase accuracy of FFT calculation one can specify 
+    levy_stable.pdf_fft_grid_spacing (defaults to 0.01) and pdf_fft_n_points_two_power 
+    (defaults to a value that covers the input range * 4). Setting pdf_fft_n_points_two_power 
+    to 16 should be sufficiently accurate in most cases at the expense of CPU time.
+    
+    For evaluation of cdf we use Zolatarev S_0 parameterization with integration or integral of
+    the pdf FFT interpolated spline. The settings affecting FFT calculation are the same as
+    for pdf calculation. Setting the threshold to None (default) will disable FFT. For cdf 
+    calculations the Zolatarev method is superior in accuracy, so FFT is disabled by default.
     
     Fitting uses quantile estimation method in [MC].
 
@@ -3472,7 +3477,7 @@ class levy_stable_gen(rv_continuous):
     @staticmethod
     def _pdf_single_value_cf_integrate(x, alpha, beta):
         cf = lambda t: levy_stable_gen._cf(t, alpha, beta)
-        return integrate.quad(lambda t: np.exp(-1j*t*x)*cf(t), -np.inf, np.inf, limit=1000)[0]/np.pi/2
+        return integrate.quad(lambda t: np.real(np.exp(-1j*t*x)*cf(t)), -np.inf, np.inf, limit=1000)[0]/np.pi/2
     
     @staticmethod
     def _pdf_single_value_zolotarev(x, alpha, beta):
@@ -3500,20 +3505,64 @@ class levy_stable_gen(rv_continuous):
             else:
                 return levy_stable_gen._pdf_single_value_zolotarev(-x, alpha, -beta)
         else:
-            # since scale zero, no need to reposition x for S_0 parameterization
+            # since location zero, no need to reposition x for S_0 parameterization
             xi = np.pi/2
-            if beta != 0:
-                np.seterr(all="ignore")
-                
+            if beta != 0:                
                 def V(theta):
                     expr_1 = np.pi/2+beta*theta
                     return 2. * expr_1 * np.exp(expr_1*np.tan(theta)/beta) / np.cos(theta) / np.pi 
             
-                expr_1 = np.exp(-np.pi*x/beta/2.)
-                int_1 = integrate.fixed_quad(lambda theta: V(theta)*np.exp(-expr_1 * V(theta)), -np.pi/2, np.pi/2)[0]
-                return expr_1 * int_1 / np.abs(beta) / 2.
+                with np.errstate(all="ignore"):
+                    expr_1 = np.exp(-np.pi*x/beta/2.)
+                    int_1 = integrate.fixed_quad(lambda theta: V(theta)*np.exp(-expr_1 * V(theta)), -np.pi/2, np.pi/2)[0]
+                    return expr_1 * int_1 / np.abs(beta) / 2.
             else:
                 return 1/(1+x**2)/np.pi
+
+    @staticmethod
+    def _cdf_single_value_zolotarev(x, alpha, beta):
+        """Calculate cdf using Zolotarev's methods as detailed in [BS].
+        """
+        zeta = -beta*np.tan(np.pi*alpha/2.)
+        if alpha != 1:
+            x0 = x + zeta  # convert to S_0 parameterization
+            xi = np.arctan(-zeta)/alpha
+            
+            def V(theta):
+                return np.cos(alpha*xi)**(1/(alpha-1)) * \
+                                (np.cos(theta)/np.sin(alpha*(xi+theta)))**(alpha/(alpha-1)) * \
+                                (np.cos(alpha*xi+(alpha-1)*theta)/np.cos(theta))
+            if x0 > zeta:
+                c_1 = 1 if alpha > 1 else .5 - xi/np.pi
+                
+                def f(theta):
+                    return np.exp(-V(theta)*(x0-zeta)**(alpha/(alpha-1)))
+
+                with np.errstate(all="ignore"):
+                    intg = integrate.quad(f, -xi, np.pi/2)[0]
+                    return c_1 + np.sign(1-alpha) * intg / np.pi
+            elif x0 == zeta:
+                return .5 - xi/np.pi
+            else:
+                return 1 - levy_stable_gen._cdf_single_value_zolotarev(-x, alpha, -beta)
+                
+        else:
+            # since location zero, no need to reposition x for S_0 parameterization
+            xi = np.pi/2
+            if beta > 0:
+                
+                def V(theta):
+                    expr_1 = np.pi/2+beta*theta
+                    return 2. * expr_1 * np.exp(expr_1*np.tan(theta)/beta) / np.cos(theta) / np.pi 
+                
+                with np.errstate(all="ignore"):
+                    expr_1 = np.exp(-np.pi*x/beta/2.)
+                    int_1 = integrate.fixed_quad(lambda theta: np.exp(-expr_1 * V(theta)), -np.pi/2, np.pi/2)[0]
+                    return int_1 / np.pi
+            elif beta == 1:
+                return .5 + np.arctan(x)/np.pi
+            else:
+                return 1 - levy_stable_gen._cdf_single_value_zolotarev(x, alpha, -beta)
         
     def _pdf(self, x, alpha, beta):
 
@@ -3530,7 +3579,7 @@ class levy_stable_gen(rv_continuous):
         else:
             pdf_single_value_method = levy_stable_gen._pdf_single_value_cf_integrate
         
-        fft_min_points_threshold = getattr(self, 'pdf_fft_min_points_threshold', 5)
+        fft_min_points_threshold = getattr(self, 'pdf_fft_min_points_threshold', 100)
         fft_grid_spacing = getattr(self, 'pdf_fft_grid_spacing', 0.01)
         fft_n_points_two_power = getattr(self, 'pdf_fft_n_points_two_power', None)
         
@@ -3539,9 +3588,9 @@ class levy_stable_gen(rv_continuous):
         for pair in uniq_param_pairs:
             data_mask = np.all(data_in[:,1:] == pair, axis=-1)
             data_subset = data_in[data_mask]
-            if len(data_subset) < fft_min_points_threshold:
-                data_out[data_mask] = [pdf_single_value_method(_x, _alpha, _beta) 
-                            for _x, _alpha, _beta in data_subset]
+            if fft_min_points_threshold is None or len(data_subset) < fft_min_points_threshold:
+                data_out[data_mask] = np.array([pdf_single_value_method(_x, _alpha, _beta) 
+                            for _x, _alpha, _beta in data_subset]).reshape(len(data_subset), 1)
             else:
                 _alpha, _beta = pair
                 _x = data_subset[:,(0,)]
@@ -3565,6 +3614,7 @@ class levy_stable_gen(rv_continuous):
         data_in = np.dstack((x, alpha, beta))[0]
         data_out = np.empty(shape=(len(data_in),1))
         
+        fft_min_points_threshold = getattr(self, 'pdf_fft_min_points_threshold', None)
         fft_grid_spacing = getattr(self, 'pdf_fft_grid_spacing', 0.01)
         fft_n_points_two_power = getattr(self, 'pdf_fft_n_points_two_power', None)
         
@@ -3573,17 +3623,20 @@ class levy_stable_gen(rv_continuous):
         for pair in uniq_param_pairs:
             data_mask = np.all(data_in[:,1:] == pair, axis=-1)
             data_subset = data_in[data_mask]
-
-            _alpha, _beta = pair
-            _x = data_subset[:,(0,)]
-            
-            # need enough points to "cover" _x for interpolation
-            h = fft_grid_spacing
-            q = 16 if fft_n_points_two_power is None else int(fft_n_points_two_power)
-            
-            density_x, density = levy_stable_gen._pdf_from_cf_with_fft(lambda t: levy_stable_gen._cf(t, _alpha, _beta), h=h, q=q)
-            f = interpolate.InterpolatedUnivariateSpline(density_x, np.real(density))
-            data_out[data_mask] = np.array([f.integral(self.a, x_1) for x_1 in _x]).reshape(data_out[data_mask].shape)
+            if fft_min_points_threshold is None or len(data_subset) < fft_min_points_threshold:
+                data_out[data_mask] = np.array([levy_stable._cdf_single_value_zolotarev(_x, _alpha, _beta) 
+                            for _x, _alpha, _beta in data_subset]).reshape(len(data_subset), 1)
+            else:
+                _alpha, _beta = pair
+                _x = data_subset[:,(0,)]
+                
+                # need enough points to "cover" _x for interpolation
+                h = fft_grid_spacing
+                q = 16 if fft_n_points_two_power is None else int(fft_n_points_two_power)
+                
+                density_x, density = levy_stable_gen._pdf_from_cf_with_fft(lambda t: levy_stable_gen._cf(t, _alpha, _beta), h=h, q=q)
+                f = interpolate.InterpolatedUnivariateSpline(density_x, np.real(density))
+                data_out[data_mask] = np.array([f.integral(self.a, x_1) for x_1 in _x]).reshape(data_out[data_mask].shape)
                 
         return data_out.T[0]
     

scipy/stats/tests/test_distributions.py
@@ -1391,11 +1391,14 @@ class TestLevyStable(object):
         
         # test single data points (uses cf def integral)
         stats.levy_stable.pdf_default_method = 'quadrature'
-        for (x, density, alpha, beta) in data:
-            with suppress_warnings() as sup:
-                sup.filter(numpy.ComplexWarning, "Casting complex values to real discards the imaginary part")
-                pdf = stats.levy_stable.pdf(x, alpha, beta, scale=1, loc=0)
-                assert_almost_equal(pdf, density)
+        stats.levy_stable.pdf_fft_min_points_threshold = None
+        xs = data[:,(0,)]
+        density = data[:,(1,)]
+        alphas = data[:,(2,)]
+        betas = data[:,(3,)]
+        pdf = stats.levy_stable.pdf(xs, alphas, betas, scale=1, loc=0)
+        assert_almost_equal(pdf, density, 4)    
+
 
     def test_pdf_zolatarev(self):
         # test values against Nolan's stable.exe output
@@ -1403,10 +1406,14 @@ class TestLevyStable(object):
                                                  'data/stable-pdf-sample-data.npy')))
         
         # test single data points (uses zolotarev parameterization) 
+        stats.levy_stable.pdf_fft_min_points_threshold = None
         stats.levy_stable.pdf_default_method = 'zolotarev'
-        for (x, density, alpha, beta) in data:
-            pdf = stats.levy_stable.pdf(x, alpha, beta, scale=1, loc=0)
-            assert_almost_equal(pdf, density)
+        xs = data[:,(0,)]
+        density = data[:,(1,)]
+        alphas = data[:,(2,)]
+        betas = data[:,(3,)]
+        pdf = stats.levy_stable.pdf(xs, alphas, betas, scale=1, loc=0)
+        assert_almost_equal(pdf, density, 4)    
 
     def test_pdf_fft(self):
         # test values against Nolan's stable.exe output
@@ -1414,6 +1421,7 @@ class TestLevyStable(object):
                                                  'data/stable-pdf-sample-data.npy')))
             
         # test bulk data (uses fft)
+        stats.levy_stable.pdf_fft_min_points_threshold = 0
         xs = data[:,(0,)]
         density = data[:,(1,)]
         alphas = data[:,(2,)]
@@ -1421,18 +1429,33 @@ class TestLevyStable(object):
         pdf = stats.levy_stable.pdf(xs, alphas, betas, scale=1, loc=0)
         assert_almost_equal(pdf, density, 4)
 
-    def test_cdf(self):
+    def test_cdf_fft(self):
         # test values against Nolan's stable.exe output
         data = np.load(os.path.abspath(os.path.join(os.path.dirname(__file__),
                                                  'data/stable-cdf-sample-data.npy')))
             
         # test bulk data (uses fft)
+        stats.levy_stable.pdf_fft_min_points_threshold = 0
         xs = data[:,(0,)]
         cdf_test = data[:,(1,)]
         alphas = data[:,(2,)]
         betas = data[:,(3,)]
         cdf = stats.levy_stable.cdf(xs, alphas, betas, scale=1, loc=0)
         assert_almost_equal(cdf, cdf_test, 2)
+        
+    def test_cdf_zolotarev(self):
+        # test values against Nolan's stable.exe output
+        data = np.load(os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                                 'data/stable-cdf-sample-data.npy')))
+            
+        # test bulk data (uses zolotarev)
+        stats.levy_stable.pdf_fft_min_points_threshold = None
+        xs = data[:,(0,)]
+        cdf_test = data[:,(1,)]
+        alphas = data[:,(2,)]
+        betas = data[:,(3,)]
+        cdf = stats.levy_stable.cdf(xs, alphas, betas, scale=1, loc=0)
+        assert_almost_equal(cdf, cdf_test)
     
     def test_fit(self):
         # contruct data to have percentiles that match