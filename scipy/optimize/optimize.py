a03fd3f015503802bf4246877115cc07c9a4afb5
chrisb83 <33071866+chrisb83@users.noreply.github.com>
2018-01-11 20:43:58 +0100

    BUG fixed printing of convergence message in minimize_scalar in scipy/optimize (#8273)
    followed suggestion by dlax in #8273 and converted disp from bool to int
    also corrected docstring of _minimize_scalar_bounded (disp is int instead of bool)

--- scipy/optimize/optimize.py
+++ scipy/optimize/optimize.py
@@ -1728,8 +1728,12 @@ def _minimize_scalar_bounded(func, bounds, args=(),
     -------
     maxiter : int
         Maximum number of iterations to perform.
-    disp : bool
-        Set to True to print convergence messages.
+    disp: int, optional
+        If non-zero, print messages.
+            0 : no message printing.
+            1 : non-convergence notification messages only.
+            2 : print a message on convergence too.
+            3 : print iteration results.
     xatol : float
         Absolute error in solution `xopt` acceptable for convergence.
 
