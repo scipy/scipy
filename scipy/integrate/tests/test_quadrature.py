a4c40dcb8d13aba89ca154c4919cc9118c77ac01
Andrew Nelson <andyfaff@gmail.com>
2018-01-18 15:46:13 +1100

    TST: add ndim tests for integrate.simps

--- scipy/integrate/tests/test_quadrature.py
+++ scipy/integrate/tests/test_quadrature.py
@@ -170,6 +170,43 @@ class TestQuadrature(object):
         b[:, 2, :] = a[:, 0, :]
         assert_equal(simps(b, b, axis=1), simps(a, a, axis=1))
 
+    def test_ndim(self):
+        x = np.linspace(0, 1, 3)
+        y = np.linspace(0, 2, 8)
+        z = np.linspace(0, 3, 13)
+
+        wx = np.ones_like(x) * (x[1] - x[0])
+        wx[0] /= 2
+        wx[-1] /= 2
+        wy = np.ones_like(y) * (y[1] - y[0])
+        wy[0] /= 2
+        wy[-1] /= 2
+        wz = np.ones_like(z) * (z[1] - z[0])
+        wz[0] /= 2
+        wz[-1] /= 2
+
+        q = x[:, None, None] + y[None,:, None] + z[None, None,:]
+
+        qx = (q * wx[:, None, None]).sum(axis=0)
+        qy = (q * wy[None, :, None]).sum(axis=1)
+        qz = (q * wz[None, None, :]).sum(axis=2)
+
+        # n-d `x`
+        r = simps(q, x=x[:, None, None], axis=0)
+        assert_almost_equal(r, qx)
+        r = simps(q, x=y[None,:, None], axis=1)
+        assert_almost_equal(r, qy)
+        r = simps(q, x=z[None, None,:], axis=2)
+        assert_almost_equal(r, qz)
+
+        # 1-d `x`
+        r = simps(q, x=x, axis=0)
+        assert_almost_equal(r, qx)
+        r = simps(q, x=y, axis=1)
+        assert_almost_equal(r, qy)
+        r = simps(q, x=z, axis=2)
+        assert_almost_equal(r, qz)
+
 
 class TestCumtrapz(object):
     def test_1d(self):
