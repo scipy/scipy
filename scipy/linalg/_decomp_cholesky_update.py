d20f92fce9f1956bfa65365feeec39621a071932
harke <22569641+ashimb9@users.noreply.github.com>
2018-01-24 03:28:42 -0600

    ENH: Addressed review comments and added tests

--- scipy/linalg/_decomp_cholesky_update.py
+++ scipy/linalg/_decomp_cholesky_update.py
@@ -1,13 +1,12 @@
 from __future__ import division, print_function, absolute_import
-import numpy
-from numpy import empty_like, sqrt, spacing, tril, triu
-from scipy.linalg import LinAlgError
+from numpy import empty_like, sqrt, spacing
+from .misc import LinAlgError, _datacopied
+from .decomp import _asarray_validated
 
 __all__ = ['cholesky_update']
 
-
-def cholesky_update(R, z, mod_type="+", lower=False, overwrite_R=False,
-               overwrite_z=False, check_finite=True):
+def cholesky_update(R, z, downdate=False, lower=False, overwrite_R=False,
+               overwrite_z=False, eps=None, check_finite=True):
     """
     Performs a rank-1 update or downdate of Cholesky factors.
 
@@ -17,14 +16,14 @@ def cholesky_update(R, z, mod_type="+", lower=False, overwrite_R=False,
 
     Parameters
     ----------
-    R : (N, N) ndarray
-        The 2D square array from which the triangular part will be used to
+    R : (N, N) array_like
+        The 2D input data from which the triangular part will be used to
         read the Cholesky factor.The remaining parts are ignored.
-    z : (N,) ndarray
-        A 1D modifier (update or downdate) array
-    mod_type: string, optional
-        The type of modification desired. A "+" indicates an update while a
-        "-" indicates a downdate. Default is "+" (update).
+    z : (N,) array_like
+        A 1D update/downdate vector
+    downdate: bool, optional
+        The type of rank-1 modification desired. False indicates an update
+        while True indicates a downdate. Default is False.
     lower: bool, optional
         Whether input array is upper or lower triangular Cholesky
         factorization. Default is upper-triangular.
@@ -35,6 +34,12 @@ def cholesky_update(R, z, mod_type="+", lower=False, overwrite_R=False,
     overwrite_z : bool, optional
         If set to True the entries of the update array will be modified during
         the computations instead of creating a new array. Default is False.
+    eps : float, optional
+        This determines the tolerance below which we consider ``alpha`` values
+        to be effectively zero. Nominally this value should be zero, but
+        numerical issues cause a tolerance about zero to be necessary.
+        If the default value is unchanged then it is set to
+        ``R.shape[1] * numpy.spacing(1.).`` during runtime. Default is None.
     check_finite : bool, optional
         Whether to check that the input matrix contains only finite numbers.
         Disabling may give a performance gain, but may result in problems
@@ -48,57 +53,55 @@ def cholesky_update(R, z, mod_type="+", lower=False, overwrite_R=False,
 
     References
     ----------
-    .. [1] DOI:10.1007/BF01933218
-    """
+    .. [1] : C.T. Pan, "A modification to the LINPACK downdating algorithm",
+       BIT Numerical Mathematics, Vol.30(4), 1990, DOI:10.1007/BF02165404
 
-    # Check for invalid mod_type argument
-    if mod_type not in ['+', '-']:
-        raise ValueError("'mod_type' argument should be one of ['+','-']")
-    # Copy or overwrite?
-    D = R if overwrite_R else R.copy()
-    z = z if overwrite_z else z.copy()
-    # Check finite, else force array
-    if check_finite:
-        R = numpy.asarray_chkfinite(R)
-        z = numpy.asarray_chkfinite(z)
-    else:
-        R = numpy.asarray(R)
-        z = numpy.asarray(z)
-    # Check for appropriate array dimensions
-    if len(R.shape) != 2:
+    """
+    # Input validation
+    R1 = _asarray_validated(R, check_finite=check_finite)
+    z1 = _asarray_validated(z, check_finite=check_finite)
+    m, n = R1.shape
+
+    overwrite_R = overwrite_R or _datacopied(R1, R)
+    overwrite_z = overwrite_z or _datacopied(z1, z)
+
+    # Dimension check
+    if len(R1.shape) != 2:
         raise ValueError("Expected 2D array to be updated.")
-    if len(z.squeeze().shape) != 1:
+    if len(z1.squeeze().shape) != 1:
         raise ValueError("Expected 1D update vector")
-    # Ensure square array
-    m, n = R.shape
+
+    # Square matrix check
     if m != n:
-        raise ValueError("A square array is expected.")
-    # Ensure appropriately triangular array
-    is_proper_tri = {True: numpy.allclose(tril(R), R),
-                     False: numpy.allclose(triu(R), R)}[lower]
-    if not is_proper_tri:
-        raise ValueError(
-            "Expected {} triangular array.".format("lower" if lower else
-                                                   "upper"))
-    # Cleanup
-    del m, is_proper_tri
-    eps = n * spacing(1.)  # For complex this needs modification
+        raise ValueError('Input needs to be a square matrix.')
+
+    # Compatible dimensions check
+    if m != len(z1):
+            raise ValueError('Input z has to have same length as the number '
+                             'of rows as input R')
+    # Copy or not
+    R = R1 if overwrite_R else R1.copy()
+    z = z1 if overwrite_z else z1.copy()
+
+    # Initializations
+    if eps is None:
+        eps = n * spacing(1.)  # For complex this needs modification
     alpha, beta = empty_like(z), empty_like(z)
     alpha[-1], beta[-1] = 1., 1.
+    sign = -1 if downdate else 1
+    R = R.T if lower else R
 
     for r in range(n):
         a = z[r] / R[r, r]
-        alpha[r] = alpha[r - 1] + a ** 2 if mod_type == "+" else \
-            alpha[r - 1] - a ** 2
+        alpha[r] = alpha[r - 1] + sign * a ** 2
         # Numerically zero or negative
         if alpha[r] < eps:
-            # Made up err msg.
+            # Error msg.
             raise LinAlgError('The Cholesky factor becomes nonpositive'
                               'with this downdate at the step {}'.format(r))
         beta[r] = sqrt(alpha[r])
         z[r + 1:] -= a * R[r, r + 1:]
-        D[r, r:] *= beta[r] / beta[r - 1]
-        D[r, r + 1:] += a / (beta[r] * beta[r - 1]) * z[r + 1:] if \
-            mod_type == "+" else -a / (beta[r] * beta[r - 1]) * z[r + 1:]
+        R[r, r:] *= beta[r] / beta[r - 1]
+        R[r, r + 1:] += sign * a / (beta[r] * beta[r - 1]) * z[r + 1:]
 
-    return D
+    return R if not lower else R.T
--- scipy/linalg/tests/test_decomp_cholesky_update.py
+++ scipy/linalg/tests/test_decomp_cholesky_update.py
@@ -1,15 +1,16 @@
 from __future__ import division, print_function, absolute_import
 import numpy as np
-from numpy.testing import assert_, assert_allclose, assert_equal
+from numpy.testing import assert_allclose, assert_raises, assert_array_equal
 from scipy import linalg
 from scipy.linalg._decomp_cholesky_update import *
-from scipy.linalg.tests.test_decomp_update import assert_upper_tri
+from numpy.random import RandomState
 
-def gen_input(n=1000):
-    A = np.random.rand(n, n)
+def gen_input(n=1000, seed=72479):
+    rand_state = RandomState(seed)
+    A = rand_state.rand(n, n)
     A = A + A.T + n * np.eye(n)
     R = linalg.cholesky(A)
-    z = np.random.rand(n)
+    z = rand_state.rand(n)
     # Uncomment the following to make it occasionally trip up
     #z *= 0.95 * np.sqrt(n)
     return R, z
@@ -28,7 +29,95 @@ class CholeskyUpdateTest(object):
         # Test rank-1 downdate
         n = 1000
         R, z = gen_input(n)
-        D = cholesky_update(R, z, mod_type="-")
+        D = cholesky_update(R, z, downdate=True)
         tol = n * np.spacing(100.)
         assert_allclose(D.T.dot(D) - (R.T.dot(R) - np.outer(z, z)),
                         np.zeros([n, n]), atol=tol)
+
+    def test_list_input(self):
+        # Test list as input
+        n = 1000
+        R, z = gen_input(n)
+        D = cholesky_update(list(R), list(z))
+        tol = n * np.spacing(100.)
+        assert_allclose(D.T.dot(D) - (R.T.dot(R) - np.outer(z, z)),
+                        np.zeros([n, n]), atol=tol)
+
+    def test_finite(self):
+        # Test with nan and inf in the input
+        n = 10
+        R, z = gen_input(n)
+
+        # Test exception if nan present in array
+        R[0, 0] = np.nan
+        assert_raises(cholesky_update, R, z)
+
+        # Test exception if inf present in array
+        R[0, 0] = np.inf
+        assert_raises(cholesky_update, R, z)
+
+        # Test exception if nan present in update vector
+        R, z = gen_input(n)
+
+        z[0] = np.nan
+        assert_raises(cholesky_update, R, z)
+
+        # Test exception if inf present in update vector
+        z[0] = np.inf
+        assert_raises(cholesky_update, R, z)
+
+    def test_dimensions(self):
+        # Tests related to dimension of both array and vector
+        n = 10
+        R, z = gen_input(n)
+
+        # Array: Too many dims
+        assert_raises(cholesky_update, np.expand_dims(R, 0), z)
+
+        # Array: Too few dims
+        assert_raises(cholesky_update, z, z)
+
+        # Vector: Too many dims
+        assert_raises(cholesky_update, R, R)
+
+        # Rectangular array
+        assert_raises(cholesky_update, R[:, :-1], z)
+
+        # Incompatible dims. between R and z
+        assert_raises(cholesky_update, R[:-1, :-1], z)
+
+    def test_param_overwrite(self):
+        # Test param overwrite
+        n = 100
+
+        # Test with overwrite_R=False and overwrite_z=False
+        R, z = gen_input(n)
+        R_copy = R.copy()
+        z_copy = z.copy()
+        cholesky_update(R, z)
+        assert_array_equal(R, R_copy)
+        assert_array_equal(z, z_copy)
+
+        # Test with overwrite_R=True and overwrite_z=True
+        R, z = gen_input(n)
+        R_copy = R.copy()
+        z_copy = z.copy()
+        cholesky_update(R, z, overwrite_R=True, overwrite_z=True)
+        assert_raises(AssertionError, assert_array_equal, R, R_copy)
+        assert_raises(AssertionError, assert_array_equal, z, z_copy)
+
+    def test_param_lower(self):
+        # Test the 'lower' parameter
+        n = 100
+        R, z = gen_input(n)
+        L = R.T
+        assert_allclose(cholesky_update(R, z),
+                        cholesky_update(L, z, lower=True).T)
+
+    def test_param_eps(self):
+        # Test the 'eps' parameter
+        n = 100
+        R, z = gen_input(n)
+        eps_val = n * np.spacing(1.)
+        assert_allclose(cholesky_update(R, z),
+                        cholesky_update(R, z, eps=eps_val))
