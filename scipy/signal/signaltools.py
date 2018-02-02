f797c545720697e3a7f1cdb1894d2790a4f85625
deepakgouda <deepakgouda1729@gmail.com>
2018-02-02 20:50:57 +0530

    BUG: Issue #4464 Fixed residuez which failed for Complex Numbers

--- scipy/signal/signaltools.py
+++ scipy/signal/signaltools.py
@@ -14,7 +14,7 @@ from scipy._lib.six import callable
 from scipy._lib._version import NumpyVersion
 from scipy import fftpack, linalg
 from numpy import (allclose, angle, arange, argsort, array, asarray,
-                   atleast_1d, atleast_2d, cast, dot, exp, expand_dims,
+                   atleast_1d, atleast_2d, cast, dot, exp, expand_dims,iscomplex, 
                    iscomplexobj, mean, ndarray, newaxis, ones, pi,
                    poly, polyadd, polyder, polydiv, polymul, polysub, polyval,
                    product, r_, ravel, real_if_close, reshape,
@@ -1978,16 +1978,19 @@ def residuez(b, a, tol=1e-3, rtype='avg'):
     b = brev[::-1]
     p = roots(a)
     r = p * 0.0
-    pout, mult = unique_roots(p, tol=tol, rtype=rtype)
-    p = []
-    for n in range(len(pout)):
-        p.extend([pout[n]] * mult[n])
+    for i in range(len(p)):
+        round_p = p[i]*0.0
+        sig_digits=int(math.ceil(-math.log(tol,10)))
         if iscomplex(p[i]):
             round_p = round_p+round(p[i].real,sig_digits)
             round_p = round_p+round(p[i].imag,sig_digits)*1j
         else:
             round_p = round(p[i],sig_digits)
         p[i] = round_p
+    pout, mult = unique_roots(p, tol=tol, rtype=rtype)
+    p = []
+    for n in range(len(pout)):
+        p.extend([pout[n]] * mult[n])
     p = asarray(p)
     # Compute the residue from the general formula (for discrete-time)
     #  the polynomial is in z**(-1) and the multiplication is by terms
