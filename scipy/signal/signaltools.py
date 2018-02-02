211e4232cf34305c81e6233da00cda623745fd13
deepakgouda <deepakgouda1729@gmail.com>
2018-02-03 00:05:59 +0530

    BUG: Issue #4464 Fixed residuez which failed for Complex Numbers

--- scipy/signal/signaltools.py
+++ scipy/signal/signaltools.py
@@ -1980,7 +1980,7 @@ def residuez(b, a, tol=1e-3, rtype='avg'):
     r = p * 0.0
     for i in range(len(p)):
         round_p = p[i]*0.0
-        sig_digits=int(math.ceil(-math.log(tol,10)))
+        sig_digits = int(math.ceil(-math.log(tol,10)))
         if iscomplex(p[i]):
             round_p = round_p+round(p[i].real,sig_digits)
             round_p = round_p+round(p[i].imag,sig_digits)*1j
