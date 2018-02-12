69cc87b19dba68375d9c48094fd90350dd856e90
Josh Wilson <person142@users.noreply.github.com>
2018-02-11 19:22:50 -0600

    MAINT: special: remove unused `sincos` implementation (#8405)

--- scipy/special/cephes.h
+++ scipy/special/cephes.h
@@ -160,9 +160,6 @@ extern double simpsn ( double f[], double delta );
 extern int simq ( double A[], double B[], double X[], int n, int flag, int IPS[] );
 */
 extern double radian ( double d, double m, double s );
-/*
-extern int sincos ( double x, double *s, double *c, int flg );
-*/
 extern double sindg ( double x );
 extern double cosdg ( double x );
 /*
