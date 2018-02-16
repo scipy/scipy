836ebb94a6934c27528625fa627fa722bade2ce0
Joscha Reimer <jor@informatik.uni-kiel.de>
2018-02-16 15:27:27 +0100

    ENH: avoid extra copy

--- scipy/sparse/base.py
+++ scipy/sparse/base.py
@@ -720,7 +720,7 @@ class spmatrix(object):
 
         """
         if np.issubdtype(self.dtype, np.complexfloating):
-            return self.tocsr().conj()
+            return self.tocsr(copy=copy).conj()
         elif copy:
             return self.copy()
         else:
