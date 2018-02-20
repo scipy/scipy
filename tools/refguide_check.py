8d5b2042d8153d58778dd776c006acca2eb65450
endolith <endolith@gmail.com>
2018-02-19 20:36:20 -0500

    TST: Generalize doctest stopwords .axis( .plot(
    Lines like 
        ax1.axis(...)
    return a list, but we don't care for doctest purposes.

--- tools/refguide_check.py
+++ tools/refguide_check.py
@@ -516,7 +516,7 @@ class Checker(doctest.OutputChecker):
     vanilla = doctest.OutputChecker()
     rndm_markers = {'# random', '# Random', '#random', '#Random', "# may vary"}
     stopwords = {'plt.', '.hist', '.show', '.ylim', '.subplot(',
-                 'set_title', 'imshow', 'plt.show', 'ax.axis', 'plt.plot(',
+                 'set_title', 'imshow', 'plt.show', '.axis(', '.plot(',
                  '.bar(', '.title', '.ylabel', '.xlabel', 'set_ylim', 'set_xlim',
                  '# reformatted', '.set_xlabel(', '.set_ylabel(', '.set_zlabel(',
                  '.set(xlim=', '.set(ylim=', '.set(xlabel=', '.set(ylabel='}
