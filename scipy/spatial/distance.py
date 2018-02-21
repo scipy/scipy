f7924dded396910d7514ef66b653f7d22d5b68ec
CJ Carey <perimosocordiae@gmail.com>
2018-02-21 11:15:00 -0500

    pep8 line wrapping

--- scipy/spatial/distance.py
+++ scipy/spatial/distance.py
@@ -1861,7 +1861,8 @@ def pdist(X, metric='euclidean', *args, **kwargs):
     # between all pairs of vectors in X using the distance metric 'abc' but
     # with a more succinct, verifiable, but less efficient implementation.
 
-    X = _asarray_validated(X, sparse_ok=False, objects_ok=True, mask_ok=True, check_finite=False)
+    X = _asarray_validated(X, sparse_ok=False, objects_ok=True, mask_ok=True,
+                           check_finite=False)
     kwargs = _args_to_kwargs_xdist(args, kwargs, metric, "pdist")
 
     X = np.asarray(X, order='c')
