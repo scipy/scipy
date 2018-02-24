7080f7a10981c542f73de8b48725b0a81ba89e20
Rafał Byczek <rafal.algo@gmail.com>
2018-02-24 16:42:59 +0100

    fix

--- scipy/ndimage/measurements.py
+++ scipy/ndimage/measurements.py
@@ -1095,9 +1095,14 @@ def minimum_position(input, labels=None, index=None):
 
     Examples
     --------
-    >>> a = np.array([[10, 20, 30], [40, 80, 100], [1, 100, 200]])
-    >>> b = np.array([[1, 2, 0, 1], [5, 3, 0, 4], [0, 0, 0, 7], [9, 3, 0, 0]])
-    
+    >>> a = np.array([[10, 20, 30],
+    ...               [40, 80, 100],
+    ...               [1, 100, 200]])
+    >>> b = np.array([[1, 2, 0, 1],
+    ...               [5, 3, 0, 4],
+    ...               [0, 0, 0, 7],
+    ...               [9, 3, 0, 0]])
+
     >>> from scipy import ndimage
     
     >>> ndimage.minimum_position(a)
@@ -1106,6 +1111,7 @@ def minimum_position(input, labels=None, index=None):
     (0, 2)
 
     Features to process can be specified using `labels` and `index`:
+
     >>> label, pos = ndimage.label(a)
     >>> ndimage.minimum_position(a, label, index=np.arange(1, pos+1))
     [(2, 0)]
