db5f65f1b5d841461621596824e3b55b9e4c541d
Rafał Byczek <rafal.algo@gmail.com>
2018-02-24 19:01:33 +0100

    add test

--- scipy/stats/tests/test_stats.py
+++ scipy/stats/tests/test_stats.py
@@ -147,6 +147,11 @@ class TestCorrPearsonr(object):
         other variables.  The same should go for SPEARMAN correlations, if
         your program has them.
     """
+    def test_divide_by_zero(self):
+        y = stats.pearsonr([0.89038028146423564557, 1.10961971853576435443], [0.66995440445715814803, 1.33004559554284207401])
+        r = y[0]
+        assert_approx_equal(r, 0.9999999999999998);    
+    
     def test_pXX(self):
         y = stats.pearsonr(X,X)
         r = y[0]
