665fe7da53e2b88ca846034c6d799b6a944072cf
ziejcow <jan.gwinner@gmail.com>
2018-03-07 14:34:46 +0100

    DOC: Docstring example for signal.cmplx_sort (#8481)

--- scipy/signal/signaltools.py
+++ scipy/signal/signaltools.py
@@ -1677,6 +1677,16 @@ def cmplx_sort(p):
     indx : ndarray
         Array of indices needed to sort the input `p`.
 
+    Examples
+    --------
+    >>> from scipy import signal
+    >>> vals = [1, 4, 1+1.j, 3]
+    >>> p_sorted, indx = signal.cmplx_sort(vals)
+    >>> p_sorted
+    array([1.+0.j, 1.+1.j, 3.+0.j, 4.+0.j])
+    >>> indx
+    array([0, 2, 3, 1])
+
     """
     p = asarray(p)
     if iscomplexobj(p):
