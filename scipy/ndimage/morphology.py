15e98d4f8778bd730f1ed04d82ead65fb51e370f
Aditya Bharti <adibhar97@gmail.com>
2018-03-13 10:12:40 +0530

    Fixed import error

--- scipy/ndimage/morphology.py
+++ scipy/ndimage/morphology.py
@@ -29,6 +29,7 @@
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 from __future__ import division, print_function, absolute_import
+import warnings
 
 import numpy
 from . import _ni_support
