3c7b664708f612ab2094c96f92b18d3f7fc724b2
sgubian <sylvain.gubian@pmi.com>
2018-03-23 22:49:38 +0100

    Fix benchmark optimize wrong lines

--- benchmarks/benchmarks/optimize.py
+++ benchmarks/benchmarks/optimize.py
@@ -509,26 +509,10 @@ class BenchGlobal(Benchmark):
 
     def setup_cache(self):
         if not self.enabled:
-            return {}
-        numtrials = slow
-        results = {}
-        solvers = ['DE', 'basinh.', 'SA']
             return
 
-        for name, klass in sorted(self._functions.items()):
-            try:
-                f = klass()
-                b = _BenchOptimizers.from_funcobj(name, f)
-                with np.errstate(all='ignore'):
-                    b.bench_run_global(methods=solvers, numtrials=numtrials)
-
-                results[name] = b.average_results()
-            except:
-                results[name] = "\n".join(traceback.format_exc())
-                continue
-
-            dump_fn = os.path.join(os.path.dirname(__file__), '..', 'global-bench-results.json')
-            with open(dump_fn, 'w') as f:
-                json.dump(results, f, indent=2, sort_keys=True)
+        # create the logfile to start with
+        with open(dump_fn, 'w') as f:
+            json.dump({}, f, indent=2)
 
         return results
