30ecf928c018b3aab320e5c41f9a0b28951557d7
Paul van Mulbregt <pvanmulbregt@users.noreply.github.com>
2018-04-10 19:54:29 -0400

    DOC: Fix the svd null_space example

--- scipy/linalg/decomp_svd.py
+++ scipy/linalg/decomp_svd.py
@@ -360,7 +360,8 @@ def null_space(A, rcond=None):
 
     >>> from scipy.linalg import null_space
     >>> A = np.array([[1, 1], [1, 1]])
-    >>> null_space(A) * np.sign(A[0,0])  # Remove the sign ambiguity of the vector
+    >>> ns = null_space(A)
+    >>> ns * np.sign(ns[0,0])  # Remove the sign ambiguity of the vector
     array([[ 0.70710678],
            [-0.70710678]])
 
