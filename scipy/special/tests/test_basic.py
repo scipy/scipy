BUG: Improve numerical precision/convergence failures of smirnov/kolmogorov.
Addresses #7426, #7455, #7456, #7492.
scipy.special.cephes:
A rewrite of the algorithms in the Kolomgorov-Smirnov statistics package.
Kolmogorov: Improve efficiency of kolmogorov().
Fixed NANs, false convergence, unbounded computation in kolmogi().
Added dedicated functions (kolmogc/kolmogci/ kolmogp) to compute
the CDF, PPF and PDF.

Smirnov: Improve accuracy of smirnov().  Computation suffered from underflow
and/or denormalization, resulting in probabilities occasionally off by orders
of magnitude.  The SF/CDF is now usually with 1 ULP, the PDF within a few ULP.
Fixed NANs, convergence failures, false convergence in smirnovi().
Added dedicated functions (smirnovc/smirnovci/smirnovp) to compute
the CDF, PPF and PDF.
Added additional testing of K-S, removed xfail on previous tests.

scipy.stats: ksone and kstwobign classes.
Added dedicated _sf, _pdf, _isf methods to call new K-S functionality.
Updated _cdf and _ppf methods to make appropriate calls.

The accuracy improvements to smirnov() required increased precision
in some parts of the computation. Uses the recent port the dd_real part of
the qd package.

Allow FuncData testing to specify a list/array of dtypes to apply.
FuncData.check(dtypes=[int, float, float]) will treat the first 3 columns
as type int, float, float respectively, rather than blanketly
treating all as the underlying type of the data.  Needed for testing
some C functions that take an int as a parameter.

scipy/special/tests/test_basic.py
@@ -570,7 +570,17 @@ class TestCephes(object):
         assert_(np.isnan(cephes.kolmogi(np.nan)))
 
     def test_kolmogorov(self):
-        assert_equal(cephes.kolmogorov(0),1.0)
+        assert_equal(cephes.kolmogorov(0), 1.0)
+
+    def test_kolmogp(self):
+        assert_equal(cephes.kolmogp(0), -0.0)
+
+    def test_kolmogc(self):
+        assert_equal(cephes.kolmogc(0), 0.0)
+
+    def test_kolmogci(self):
+        assert_equal(cephes.kolmogci(0), 0.0)
+        assert_(np.isnan(cephes.kolmogi(np.nan)))
 
     def _check_kv(self):
         cephes.kv(1,1)
@@ -910,10 +920,29 @@ class TestCephes(object):
         assert_equal(cephes.smirnov(1,.1),0.9)
         assert_(np.isnan(cephes.smirnov(1,np.nan)))
 
+    def test_smirnovp(self):
+        assert_equal(cephes.smirnovp(1, .1), -1)
+        assert_equal(cephes.smirnovp(2, 0.75), -2*(0.25)**(2-1))
+        assert_equal(cephes.smirnovp(3, 0.75), -3*(0.25)**(3-1))
+        assert_(np.isnan(cephes.smirnovp(1, np.nan)))
+
+    def test_smirnovc(self):
+        assert_equal(cephes.smirnovc(1,.1),0.1)
+        assert_(np.isnan(cephes.smirnovc(1,np.nan)))
+        x10 = np.linspace(0, 1, 11, endpoint=True)
+        assert_almost_equal(cephes.smirnovc(3, x10), 1-cephes.smirnov(3, x10))
+        x4 = np.linspace(0, 1, 5, endpoint=True)
+        assert_almost_equal(cephes.smirnovc(4, x4), 1-cephes.smirnov(4, x4))
+
     def test_smirnovi(self):
         assert_almost_equal(cephes.smirnov(1,cephes.smirnovi(1,0.4)),0.4)
         assert_almost_equal(cephes.smirnov(1,cephes.smirnovi(1,0.6)),0.6)
         assert_(np.isnan(cephes.smirnovi(1,np.nan)))
+
+    def test_smirnovci(self):
+        assert_almost_equal(cephes.smirnovc(1,cephes.smirnovci(1,0.4)),0.4)
+        assert_almost_equal(cephes.smirnovc(1,cephes.smirnovci(1,0.6)),0.6)
+        assert_(np.isnan(cephes.smirnovci(1,np.nan)))
 
     def test_spence(self):
         assert_equal(cephes.spence(1),0.0)