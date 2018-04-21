9f6f3f18e78b45e1ad1ef76b18b8e5a87ffe5516
Joaquin Derrac <carrdelling@gmail.com>
2018-04-21 16:30:08 +0100

    Added documentation for `complete` ustering method

--- scipy/cluster/hierarchy.py
+++ scipy/cluster/hierarchy.py
@@ -312,7 +312,7 @@ def single(y):
     explanation of its contents.
 
     We can use :func:`scipy.cluster.hierarchy.fcluster` to 
-    see to which cluster would each initial point belong 
+    see to which cluster each initial point would belong 
     given a distance threshold:   
 
     >>> fcluster(Z, 0.9, criterion='distance')
@@ -350,6 +350,65 @@ def complete(y):
     --------
     linkage: for advanced creation of hierarchical clusterings.
     scipy.spatial.distance.pdist : pairwise distance metrics
+
+    Examples
+    --------
+    >>> from scipy.cluster.hierarchy import complete, fcluster
+    >>> from scipy.spatial.distance import pdist
+
+    First we need a toy dataset to play with:
+
+    x x    x x
+    x        x
+
+    x        x
+    x x    x x
+
+    >>> X = [[0, 0], [0, 1], [1, 0],
+             [0, 4], [0, 3], [1, 4],
+             [4, 0], [3, 0], [4, 1], 
+             [4, 4], [3, 4], [4, 3]]
+
+    Then we get a condensed distance matrix from this dataset:
+    
+    >>> y = pdist(X)
+
+    Finally, we can perform the clustering
+
+    >>> Z = complete(y)
+
+    >>> Z
+    array([[ 0.        ,  1.        ,  1.        ,  2.        ],
+           [ 3.        ,  4.        ,  1.        ,  2.        ],
+           [ 6.        ,  7.        ,  1.        ,  2.        ],
+           [ 9.        , 10.        ,  1.        ,  2.        ],
+           [ 2.        , 12.        ,  1.41421356,  3.        ],
+           [ 5.        , 13.        ,  1.41421356,  3.        ],
+           [ 8.        , 14.        ,  1.41421356,  3.        ],
+           [11.        , 15.        ,  1.41421356,  3.        ],
+           [16.        , 17.        ,  4.12310563,  6.        ],
+           [18.        , 19.        ,  4.12310563,  6.        ],
+           [20.        , 21.        ,  5.65685425, 12.        ]])
+
+    The linkage matrix Z represents a dendrogram - see 
+    :func:`scipy.cluster.hierarchy.linkage` for a detailed 
+    explanation of its contents.
+
+    We can use :func:`scipy.cluster.hierarchy.fcluster` to 
+    see to which cluster each initial point would belong 
+    given a distance threshold:   
+
+    >>> fcluster(Z, 0.9, criterion='distance')
+    array([ 1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12], dtype=int32)
+    >>> fcluster(Z, 1.5, criterion='distance')
+    array([1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4], dtype=int32)
+    >>> fcluster(Z, 4.5, criterion='distance')
+    array([1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2], dtype=int32)
+    >>> fcluster(Z, 6, criterion='distance')
+    array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], dtype=int32)
+
+    Also :func:`scipy.cluster.hierarchy.dendrogram` can be used 
+    to generate a plot of the dendrogram.
 
     """
     return linkage(y, method='complete', metric='euclidean')
