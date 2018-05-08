95296e4017ec32a516bf4932569ec5965f3917a2
Aditya Bharti <adibhar97@gmail.com>
2018-05-08 22:01:29 +0530

    Initial commit for Rotation class

---
+++ scipy/spatial/transform/rotation.py
@@ -0,0 +1,12 @@
+from __future__ import division, print_function, absolute_import
+
+import numpy as np
+
+class Rotation(object):
+    """Rotation in 3 dimensions.
+
+    This class will include initializers from different representations, converters
+    and some useful algorithms such as quaternion slerp and rotation estimation.
+    """
+    def __init__(self):
+        pass
