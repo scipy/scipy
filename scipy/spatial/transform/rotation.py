e30df19814635d806d65612ef277a32316095c1b
Aditya Bharti <adibhar97@gmail.com>
2018-05-08 23:10:51 +0530

    Modified docstring

--- scipy/spatial/transform/rotation.py
+++ scipy/spatial/transform/rotation.py
@@ -3,12 +3,10 @@ from __future__ import division, print_function, absolute_import
 import numpy as np
 
 class Rotation(object):
-    """
-    This class implements implements rotations in 3 dimensions using quaternions.
+    """Rotation in 3 dimensions.
 
-    While there are different representations of rotation, quaternions have been chosen for
-    internal representation. This class will include initializers from different representations,
-    converters and some useful algorithms such as quaternion slerp and rotation estimation.
+    This class will include initializers from different representations, converters
+    and some useful algorithms such as quaternion slerp and rotation estimation.
     """
     def __init__(self):
         pass
