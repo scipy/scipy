3808a9767328cddb08c6f78311b73009c93d7c3e
Aditya Bharti <adibhar97@gmail.com>
2018-05-15 22:53:45 +0530

    Reduced number of operations when normalizing. Added tests.

--- scipy/spatial/transform/rotation.py
+++ scipy/spatial/transform/rotation.py
@@ -16,59 +16,60 @@ class Rotation(object):
     Methods
     -------
     from_quaternion
-
     """
     def __init__(self, quat, normalized=False):
         self._single = False
         # Try to convert to numpy array
-        quat = np.asarray(quat)
+        quat = np.asarray(quat, dtype=float)
+
+        if quat.ndim not in [1, 2]:
+            raise ValueError("Input quat should be of shape (4,) or (N x 4).")
 
         # If a single quaternion is given, convert it to a 2D 1 x 4 matrix but
         # set self._single to True so that we can return appropriate objects
         # in the `to_...` methods
-        if quat.shape == (4,):
+        if quat.ndim == 1:
             quat = quat[None, :]
             self._single = True
 
         # Each row should have 4 numbers
         if quat.shape[1] != 4:
-            raise ValueError("A quaternion should have 4 numbers in (x,y,z,w) format")
+            raise ValueError("A quaternion should have 4 numbers.")
 
-        normalized_quat = quat
+        self._quat = quat
+
         if not normalized:
             # L2 norm of each row
             norms = scipy.linalg.norm(quat, axis=1)
 
-            # Warn user for zero (eps?) norm and set to identity quaternion, which
-            # is (0,0,0,1) in (x,y,z,w) format
-            bool_indices = norms == 0
-            if np.any(bool_indices == True):
-                warnings.warn("Found zero norm quaternions in input, replacing with identity quaternions")
-                quat[bool_indices] = np.array([0, 0, 0, 1])
-                norms[bool_indices] = 1.0
-
-            # Normalize each quaternion
-            # In case quat is [4 x 4], ensure norm is broadcasted along each column
-            normalised_quat = quat / norms[:, None]
-        self._quat = normalised_quat
+            # Warn user for zero (eps?) norm and set to identity quaternion,
+            # which (0,0,0,1) in (x,y,z,w) format
+            zero_norms = norms == 0
+            if zero_norms.any():
+                warnings.warn("Found zero norm quaternions in input, replacing with identity quaternions.")
+                self._quat[zero_norms] = np.array([0, 0, 0, 1])
+            # Normalize each quaternion, ensuring norm is broadcasted along
+            # each column.
+            self._quat[~zero_norms] /= norms[~zero_norms][:, None]
 
     @classmethod
     def from_quaternion(cls, quat, normalized=False):
-        """Initialize Rotation from possibly unnormalized quaternions.
+        """Initialize Rotation from quaternions.
 
-        This classmethod normalizes the input quaternions and returns a
-        `Rotation` object.
+        This classmethod returns a `Rotation` object from the input quaternions.
+        If `normalized` is `True`, then the quaternions are assumed to have
+        unit norm, else the quaternions are normalized before the object is
+        created.
 
         Parameters
         ----------
         quat : array_like, shape (N, 4) or (4,)
-               Each row is a (possibly non-unit norm) quaternion in scalar-last
-               (x, y, z, w) format.
+            Each row is a (possibly non-unit norm) quaternion in scalar-last
+            (x, y, z, w) format.
 
-        normalized : boolean, optional, default = False
-                     If this flag is `True`, then it is assumed that the input
-                     quaternions all have unit norm and are not normalized again.
-
+        normalized : boolean, optional
+            If this flag is `True`, then it is assumed that the input quaternions
+            all have unit norm and are not normalized again. Default is False.
         """
 
         return cls(quat, normalized)
--- scipy/spatial/transform/tests/test_rotation.py
+++ scipy/spatial/transform/tests/test_rotation.py
@@ -1,5 +1,7 @@
 from __future__ import division, print_function, absolute_import
 
+import pytest
+
 import numpy as np
 from numpy.testing import assert_array_almost_equal
 from scipy.spatial.transform import Rotation
@@ -11,11 +13,13 @@ def test_generic_quat_matrix():
     expected_quat = x / np.array([[5], [13]])
     assert_array_almost_equal(r._quat, expected_quat)
 
+
 def test_from_single_quaternion():
     x = np.array([3,4,0,0])
     r = Rotation.from_quaternion(x)
     expected_quat = x[None, :] / 5
     assert_array_almost_equal(r._quat, expected_quat)
+
 
 def test_from_square_quat_matrix():
     # Ensure proper norm array broadcasting
@@ -28,3 +32,32 @@ def test_from_square_quat_matrix():
     r = Rotation.from_quaternion(x)
     expected_quat = x / np.array([[5], [13], [1], [1]])
     assert_array_almost_equal(r._quat, expected_quat)
+
+
+def test_malformed_1d_from_quaternion():
+    with pytest.raises(ValueError):
+        Rotation.from_quaternion(np.array([1,2,3]))
+
+
+def test_malformed_2d_from_quaternion():
+    with pytest.raises(ValueError):
+        Rotation.from_quaternion(np.array([
+            [1,2,3,4,5],
+            [4,5,6,7,8]
+            ]))
+
+
+def test_zero_norms_from_quaternion():
+    x = np.array([
+            [3, 4, 0, 0],
+            [0, 0, 0, 0],
+            [5, 0, 12, 0]
+            ])
+    with pytest.raises(UserWarning):
+        r = Rotation.from_quaternion(x)
+        expected_quat = np.array([
+            x[0] / 5,
+            [0, 0, 0, 1],
+            x[2] / 13
+            ])
+        assert_array_almost_equal(r, expected_quat)
