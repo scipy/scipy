3d2310dfc3fa3c729b429989edec7c87e200e91d
Aditya Bharti <adibhar97@gmail.com>
2018-05-16 02:24:25 +0530

    STY: PEP8 compliance for `rotation.py` and `setup.py`

--- scipy/spatial/transform/rotation.py
+++ scipy/spatial/transform/rotation.py
@@ -4,14 +4,16 @@ import numpy as np
 import scipy.linalg
 import warnings
 
+
 class Rotation(object):
     """Rotation in 3 dimensions.
 
-    This class will include initializers from different representations, converters
-    and some useful algorithms such as quaternion slerp and rotation estimation.
+    This class will include initializers from different representations,
+    converters and some useful algorithms such as quaternion slerp and
+    rotation estimation.
 
-    For initializing Rotations usage of `from_...` methods such as `from_quaternion`
-    is recommended instead of using `__init__`.
+    For initializing Rotations usage of `from_...` methods such as
+    `from_quaternion` is recommended instead of using `__init__`.
 
     Methods
     -------
@@ -43,7 +45,8 @@ class Rotation(object):
             # which (0,0,0,1) in (x,y,z,w) format
             zero_norms = norms == 0
             if zero_norms.any():
-                warnings.warn("Found zero norm quaternions in input, replacing with identity quaternions.")
+                warnings.warn("""Found zero norm quaternions in input,
+                        replacing with identity quaternions.""")
                 self._quat[zero_norms] = np.array([0, 0, 0, 1])
             # Normalize each quaternion, ensuring norm is broadcasted along
             # each column.
@@ -53,7 +56,7 @@ class Rotation(object):
     def from_quaternion(cls, quat, normalized=False):
         """Initialize Rotation from quaternions.
 
-        This classmethod returns a `Rotation` object from the input quaternions.
+        This classmethod returns a `Rotation` object from the input quaternions
         If `normalized` is `True`, then the quaternions are assumed to have
         unit norm, else the quaternions are normalized before the object is
         created.
@@ -64,8 +67,9 @@ class Rotation(object):
             Each row is a (possibly non-unit norm) quaternion in scalar-last
             (x, y, z, w) format.
         normalized : boolean, optional
-            If this flag is `True`, then it is assumed that the input quaternions
-            all have unit norm and are not normalized again. Default is False.
+            If this flag is `True`, then it is assumed that the input
+            quaternions all have unit norm and are not normalized again.
+            Default is False.
         """
 
         return cls(quat, normalized)
