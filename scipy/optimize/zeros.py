1532ee9a054a4e08de7f0f675e03ad5bc3144753
Mark Mikofski <bwana.marko@yahoo.com>
2018-06-06 12:30:11 -0700

    TST: ENH: trying to halley to converge
    * use xtol and rtol from zeros

--- scipy/optimize/zeros.py
+++ scipy/optimize/zeros.py
@@ -195,7 +195,7 @@ def newton(func, x0, fprime=None, args=(), tol=1.48e-8, maxiter=50,
             fval = func(p0, *args)
             funcalls += 1
             # If a root has been found within some tolerance, then terminate
-            if fval == 0 or abs(fval) < finfo(float).eps:
+            if fval == 0 or abs(fval) < _xtol + _rtol * abs(fval):
                 return _results_select(full_output, (p0, funcalls, itr, _ECONVERGED))
             fder = fprime(p0, *args)
             funcalls += 1
