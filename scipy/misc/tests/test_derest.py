26a773f402b3ddc7caa778e6bc31779e45aeb614
Siva Kannan <siva.kannan@tut.fi>
2018-07-06 11:10:24 +0300

    MAINT: Fix error in test_derest
    The test code for derest had incorrect scope for variables.
    Fix error by making the variables local to each test function.

--- scipy/misc/tests/test_derest.py
+++ scipy/misc/tests/test_derest.py
@@ -6,25 +6,37 @@ import os
 
 class TestDerest(object):
 
-    file_path = os.path.join(os.path.dirname(__file__), "data/derestData.npz")
-    data = np.load(file_path)
-
-    x = data['x']
-    outtest = data['out']
-    tBasic = data['tBasic']
-    tDenseSmall = data['tDenseSmall']
-    tDenseLarge = data['tDenseLarge']
-    denseSmalltest = data['denseSmall']
-    denseLargetest = data['denseLarge']
-
     def test_basic(self):
-        out = derest.derest(tBasic, x)
+        file_path = os.path.join(os.path.dirname(__file__), "data/derestData.npz")
+        data = np.load(file_path)
+
+        x = data['x']
+        outtest = data['out']
+        tbasic = data['tBasic']
+
+        out = derest.derest(tbasic, x)
         assert_allclose(out, outtest)
 
     def test_dense_small(self):
-        __, out = derest(tBasic, x, tDenseSmall)
-        assert_allclose(out, denseSmalltest)
+        file_path = os.path.join(os.path.dirname(__file__), "data/derestData.npz")
+        data = np.load(file_path)
+
+        x = data['x']
+        tbasic = data['tBasic']
+        tdensesmall = data['tDenseSmall']
+        densesmalltest = data['denseSmall']
+
+        __, out = derest(tbasic, x, tdensesmall)
+        assert_allclose(out, densesmalltest)
 
     def test_dense_large(self):
-        __, out = derest(tBasic, x, tDenseLarge)
-        assert_allclose(out, denseLargetest)
+        file_path = os.path.join(os.path.dirname(__file__), "data/derestData.npz")
+        data = np.load(file_path)
+
+        x = data['x']
+        tbasic = data['tBasic']
+        tdenselarge = data['tDenseLarge']
+        denselargetest = data['denseLarge']
+
+        __, out = derest(tbasic, x, tdenselarge)
+        assert_allclose(out, denselargetest)
