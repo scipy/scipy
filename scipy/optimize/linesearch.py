f0a15f6e3b4e002b47da90117985c0ff43096f3c
Wate <31439027+Watesoyan@users.noreply.github.com>
2018-07-12 09:55:16 +0800

    Update linesearch.py

--- scipy/optimize/linesearch.py
+++ scipy/optimize/linesearch.py
@@ -480,11 +480,13 @@ def _cubicmin(a, fa, fpa, b, fb, c, fc):
             db = b - a
             dc = c - a
             denom = (db * dc) ** 2 * (db - dc)
-            [A, B] = np.dot(
-                             np.array([[dc ** 2, -db ** 2], 
-                                       [-dc ** 3, db ** 3]], dtype=np.float64), 
-                             np.array([fb - fa - C * db, fc - fa - C * dc], dtype=np.float64)
-                           )
+            d1 = np.empty((2, 2))
+            d1[0, 0] = dc ** 2
+            d1[0, 1] = -db ** 2
+            d1[1, 0] = -dc ** 3
+            d1[1, 1] = db ** 3
+            [A, B] = np.dot(d1, np.asarray([fb - fa - C * db,
+                                            fc - fa - C * dc]).flatten())
             A /= denom
             B /= denom
             radical = B * B - 3 * A * C
