e7c9e4e30976db6938c3d97a89c36cc33488cb12
Mark Mikofski <bwana.marko@yahoo.com>
2018-08-11 01:25:17 -0700

    TST: must use callback_type signature in zeros_struct test callbacks
    * use void* instead of test_params* and then cast args later
    * something is up with rsh, whaaaattt? weird
    *

--- scipy/optimize/cython_optimize/tests/zeros_struct_examples.pyx
+++ scipy/optimize/cython_optimize/tests/zeros_struct_examples.pyx
@@ -22,31 +22,31 @@ IL = [sin(il) + 6.0 for il in range(NUM_OF_IRRAD)]
 
 
 # governing equations
-cdef double f_solarcell(double i, test_params *args):
+cdef double f_solarcell(double i, void *args):
     cdef test_params *myargs
     cdef double v, il, io, rs, rsh, vt
     # unpack structure
-    myargs = args
+    myargs = <test_params *> args
     v = myargs.voltage
     il = myargs.light_current
     io = myargs.dark_current
     rs = myargs.series_resistance
-    rsh = myargs.shunt_resistsance
+    #rsh = myargs.shunt_resistsance
     vt = myargs.thermal_voltage
     vd = v + i * rs
     return il - io * (exp(vd / vt) - 1.0) - vd / rsh - i
 
 
-cdef double fprime(double i, test_params *args):
+cdef double fprime(double i, void *args):
     cdef test_params *myargs
     cdef double v, il, io, rs, rsh, vt
     # unpack structure
-    myargs = args
+    myargs = <test_params *> args
     v = myargs.voltage
     il = myargs.light_current
     io = myargs.dark_current
     rs = myargs.series_resistance
-    rsh = myargs.shunt_resistsance
+    #rsh = myargs.shunt_resistsance
     vt = myargs.thermal_voltage
     return -io * exp((v + i * rs) / vt) * rs / vt - rs / rsh - 1
 
@@ -59,13 +59,13 @@ cdef double solarcell_newton(tuple args):
     myargs.light_current = args[1]
     myargs.dark_current = args[2]
     myargs.series_resistance = args[3]
-    myargs.shunt_resistsance = args[4]
+    #myargs.shunt_resistsance = args[4]
     myargs.thermal_voltage = args[5]
     return zeros_struct.newton(f_solarcell, 6.0, fprime, myargs)
 
 
 # cython
-def test_cython_newton(v=5.25, il=IL, args=(1e-09, 0.004, 10, 0.27456)):
+def test_cython_newton(v=5.25, il=IL, args=(1e-09, 0.004, 10.0, 0.27456)):
     return map(solarcell_newton,
                ((v, il_,) + args for il_ in il))
 
@@ -78,12 +78,12 @@ cdef double solarcell_bisect(tuple args):
     myargs.light_current = args[1]
     myargs.dark_current = args[2]
     myargs.series_resistance = args[3]
-    myargs.shunt_resistsance = args[4]
+    #myargs.shunt_resistsance = args[4]
     myargs.thermal_voltage = args[5]
-    return zeros_struct.bisect(f_solarcell, 7, 0, myargs, 0.001, 0.001, 10)
+    return zeros_struct.bisect(f_solarcell, 7.0, 0.0, myargs, 0.001, 0.001, 10)
 
 
 # cython
-def test_cython_bisect(v=5.25, il=IL, args=(1e-09, 0.004, 10, 0.27456)):
+def test_cython_bisect(v=5.25, il=IL, args=(1e-09, 0.004, 10.0, 0.27456)):
     return map(solarcell_bisect,
                ((v, il_,) + args for il_ in il))
