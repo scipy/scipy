629cd2cff9c2f1297ed24a8fa30c458434497135
Theodore Hu <theodore.n.hu@gmail.com>
2018-08-17 20:55:27 -0700

    TST: Add ifixx test

--- scipy/odr/tests/test_odr.py
+++ scipy/odr/tests/test_odr.py
@@ -344,3 +344,15 @@ class TestODR(object):
         result = job.run()
         assert_equal(result.info, 2)
 
+    def test_ifixx(self):
+        x1 = [-2.01, -0.99, -0.001, 1.02, 1.98]
+        x2 = [3.98, 1.01, 0.001, 0.998, 4.01]
+        fix = np.vstack((np.zeros_like(x1, dtype=int), np.ones_like(x2, dtype=int)))
+        data = Data(np.vstack((x1, x2)), y=1, fix=fix)
+        model = Model(lambda beta, x: x[1, :] - beta[0] * x[0, :]**2., implicit=True)
+
+        odr1 = ODR(data, model, beta0=np.array([1.]))
+        sol1 = odr1.run()
+        odr2 = ODR(data, model, beta0=np.array([1.]), ifixx=fix)
+        sol2 = odr2.run()
+        assert_equal(sol1.beta, sol2.beta)
