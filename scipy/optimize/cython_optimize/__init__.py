66ac65f3afaaf64b1d89df208648c58d46510d74
Mark Mikofski <bwana.marko@yahoo.com>
2018-08-23 23:53:01 -0700

    DOC: add documentation to cython_optimize

--- scipy/optimize/cython_optimize/__init__.py
+++ scipy/optimize/cython_optimize/__init__.py
@@ -1,104 +1,143 @@
 """
-Cython Optimize
-===============
-This package contains an experimental Cython API for ``scipy.optimize``.
-This is a WIP. Only ``zeros.newton`` and ``zeros.bisect`` are currently
-supported.
+Cython Optimize Zeros API
+=========================
+This package contains a Cython API for the root finding :ref:`scalar-functions`
+in :py:mod:`scipy.optimize` to obtain performant and efficient pure C
+implementations. Faster execution is not guaranteed, but you might get 5-10X
+improvement versus pure Python. The Cython Optimize Zeros API  could be used
+for native C looping or with Cython `prange` for efficient execution of large
+arrays of inputs with the same callbacks.
 
-Proposal
+Callback Signatures
+-------------------
+The Cython Optimize Zeros API provides three callback signagures for the root
+finders, two which are safe without the global interpreter lock (GIL) and one
+that uses Python tuples.
+
+- ``double (*callback_type)(double, void*)``
+- ``double (*callback_type_array)(int, double*)``
+- ``double (*callback_type_tuple)(double, tuple)``
+
+Available Functions
+-------------------
+There are seven available functions which are all implemented in pure C, and
+exposed in modules matching the expected callback signature.
+
+* ``scipy.optimize.cython_optimize.zeros_struct`` exposes root finders expecting
+  extra arguments in a native C ``struct``. See examples for usage.
+
+    - :py:func:`~scipy.optimize.cython_optimize.zeros_struct.newton`::
+
+            cdef double newton(callback_type func, double x0, callback_type fprime, void* args, double tol, int maxiter)
+
+    - :py:func:`~scipy.optimize.cython_optimize.zeros_struct.secant`::
+
+            double secant(callback_type func, double x0, void* args, double tol, int maxiter)
+
+    - :py:func:`~scipy.optimize.cython_optimize.zeros_struct.halley`::
+
+            double halley(callback_type func, double x0, callback_type fprime, void* args, double tol, int maxiter, callback_type fprime2)
+
+    - :py:func:`~scipy.optimize.cython_optimize.zeros_struct.bisect`::
+
+            double bisect(callback_type f, double xa, double xb, void* args, double xtol, double rtol, int iter)
+
+    - :py:func:`~scipy.optimize.cython_optimize.zeros_struct.ridder`::
+
+            double ridder(callback_type f, double xa, double xb, void* args, double xtol, double rtol, int iter)
+
+    - :py:func:`~scipy.optimize.cython_optimize.zeros_struct.brenth`::
+
+            double brenth(callback_type f, double xa, double xb, void* args, double xtol, double rtol, int iter)
+
+    - :py:func:`~scipy.optimize.cython_optimize.zeros_struct.brentq`::
+
+            double brentq(callback_type f, double xa, double xb, void* args, double xtol, double rtol, int iter)
+
+* ``scipy.optimize.cython_optimize.zeros_array`` exposes root finders expecting
+  extra arguments in a native C array of doubles. See examples for usage.
+
+    - :py:func:`~scipy.optimize.cython_optimize.zeros_array.newton`::
+
+            double newton(callback_type_array func, double x0, callback_type_array fprime, int n, double* args, double tol, int maxiter)
+
+    - :py:func:`~scipy.optimize.cython_optimize.zeros_array.secant`::
+
+            double secant(callback_type_array func, double x0, int n, double* args, double tol, int maxiter)
+
+    - :py:func:`~scipy.optimize.cython_optimize.zeros_array.halley`::
+
+            double halley(callback_type_array func, double x0, callback_type_array fprime, int n, double* args, double tol, int maxiter, callback_type_array fprime2)
+
+    - :py:func:`~scipy.optimize.cython_optimize.zeros_array.bisect`::
+
+            double bisect(callback_type_array f, double xa, double xb, int n, double* args, double xtol, double rtol, int iter)
+
+    - :py:func:`~scipy.optimize.cython_optimize.zeros_array.ridder`::
+
+            double ridder(callback_type_array f, double xa, double xb, int n, double* args, double xtol, double rtol, int iter)
+
+    - :py:func:`~scipy.optimize.cython_optimize.zeros_array.brenth`::
+
+            double brenth(callback_type_array f, double xa, double xb, int n, double* args, double xtol, double rtol, int iter)
+
+    - :py:func:`~scipy.optimize.cython_optimize.zeros_array.brentq`::
+
+            double brentq(callback_type_array f, double xa, double xb, int n, double* args, double xtol, double rtol, int iter)
+
+* ``scipy.optimize.cython_optimize.zeros`` exposes root finders expecting extra
+  arguments in a Python tuple.
+
+    - :py:func:`~scipy.optimize.cython_optimize.zeros.newton`::
+
+            double newton(callback_type_tuple func, double x0, callback_type_tuple fprime, tuple args, double tol, int maxiter)
+
+    - :py:func:`~scipy.optimize.cython_optimize.zeros.secant`::
+
+            double secant(callback_type_tuple func, double x0, tuple args, double tol, int maxiter)
+
+    - :py:func:`~scipy.optimize.cython_optimize.zeros.halley`::
+
+            double halley(callback_type_tuple func, double x0, callback_type_tuple fprime, tuple args, double tol, int maxiter, callback_type_tuple fprime2)
+
+    - :py:func:`~scipy.optimize.cython_optimize.zeros.bisect`::
+
+            double bisect(callback_type_tuple f, double xa, double xb, tuple args, double xtol, double rtol, int iter)
+
+    - :py:func:`~scipy.optimize.cython_optimize.zeros.ridder`::
+
+            double ridder(callback_type_tuple f, double xa, double xb, tuple args, double xtol, double rtol, int iter)
+
+    - :py:func:`~scipy.optimize.cython_optimize.zeros.brenth`::
+
+            double brenth(callback_type_tuple f, double xa, double xb, tuple args, double xtol, double rtol, int iter)
+
+    - :py:func:`~scipy.optimize.cython_optimize.zeros.brentq`::
+
+            double brentq(callback_type_tuple f, double xa, double xb, tuple args, double xtol, double rtol, int iter)
+
+Examples
 --------
-The Cython API will not use any Python objects so that it can be used GIL free
-if desired. The callback will use the basic
-``double (*callback_type)(double,void*)`` type definition.
+Usage of the Cython Optimize Zeros API requires the use of Cython to write
+callbacks that are compiled into native C. There are several usage examples
+with different combinations of callbacks and root-finders in the
+``cython_optimize/Examples`` folder. There are five steps:
 
-Background
-----------
-Currently there are 5 methods in ``zeros``:
-
-    * `newton`
-    * `bisect`
-    * `ridder`
-    * `brentq`
-    * `brenth`
-
-Only ``newton`` is pure Python, and is contained in the ``zeros.py`` module
-itself. The other four are written in native C with no Python bindings. This is
-advantageous because they can be used GIL free if the Cython API is also Python
-free.
-
-``zeros.c``
-~~~~~~~~~~~
-The four native C functions are currently exposed using the Python C-API in
-``zeros.c``, which is exported as the private Python module ``_zeros`` and
-imported into ``zeros.py``. The Python methods in ``zeros.py`` are mapped to
-private methods in ``zeros.c`` called ``_bisect``, ``_ridder``, ``_brentq``, and
-``_brenth``. These methods all execute the function ``call_solver``, which is
-_not_ exported. As the first argument they all pass a ``solver_type``, which is
-a reference to the native C function. The other argument is a tuple of all of
-the arguments from the original caller in ``zeros.py``.
-
-``call_solver``
-~~~~~~~~~~~~~~~
-This hidden method performs the redundant steps of parsing the arguments tuple,
-error checking, allocating a new tuple to include the independent variable, and
-incrementing the reference count of each stolen item from the original tuple of
-additional callback function parameters.
-
-The parsed argument tuple is the same for all four methods:
-
-* ``f`` - Python callback function that takes a tuple
-* ``a`` - lower limit of the independent variable
-* ``b`` - upper limit of the independent variable
-* ``xtol`` - absolute tolerance
-* ``rtol`` - relative tolerance
-* ``iter`` - maximum iterations
-* ``xargs`` - Python tuple of extra callback arguments
-* ``fulloutput`` - flag to use ``RootResults`` object which returns iterations,
-function calls, and converged flag
-* ``disp`` - raise ``RuntimeError`` if didn't converge
-
-``scipy_zeros_parameters``
-~~~~~~~~~~~~~~~~~~~~~~~~~~
-The Python module ``zeros.c`` mimics the ``default_parameters`` structure found
-in the ``zeros.h`` header, but adds on 3 python objects, and calls the new
-structure, ``scipy_zeros_parameters``:
-
-* Python callback function passed to the ``zeros.py`` method
-* Python tuple containing the extra callback arguments.
-* a native C jump buffer called ``env`` used to quit execution and return
-``NULL`` if the Python callback returns ``NULL``.
-
-The arguments are copied to a new temporary Python tuple one item larger for the
-independent variable, and the copied references are incremented, so that when
-the temporary tuple is decremeneted, the arguments are still referenced. Then
-the ``scipy_zeros_parameters`` structure fields are set to the Python callback
-``f`` and the temporary ``fargs`` tuple. Then if ``setjmp`` returns 0, _ie_: it
-was called directly and not the result of ``longjmp``, the native C solver
-function is called, with a native C callback function called
-``scipy_zeros_functions_func`` as the first argument.
-
-``scipy_zeros_functions_func``
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-This is a function wrapper. It mimics ``callback_type`` from the ``zeros.h``
-header, which takes a double and a pointer to a ``default_parameters``
-structure. When the wrapper executes, it gets the Python callback function and
-the the arguments tuple from the structure, sets the first item in the tuple to
-the double, passes it the callback, and executes it. If the callback fails, then
-it executes a long jump to the set jump point, which terminates ``call_solver``,
-decrements the ``fargs`` temporary tuple, and returns ``NULL``. Otherwise, the
-wrapper function copies the return value to a native double, decrements the
-Python return value object, and returns the native double value to the solver.
-
-The solver continues iterating and ultimately returns the zero to the Python
-module, which either copies it to the ``RootResults`` object, or copies it to a Python
-float, and returns.
+1. Select a callback signature, *eg* ``scipy.optimize.cython_optimize.zeros_struct``
+2. Select the root finder, *eg* :py:func:`~scipy.optimize.cython_optimize.zeros_struct.newton`
+3. Create a Cython ``.pyx`` file that imports the zeros module with the
+  selected callback signature, create the callback(s) using the selected
+  signature, and call the selected root-finder passing the callback(s) and any
+  extra arguments, and other parmetres paramters
+4. If you want to call your function from Python, create a wrapper
+5. Create a Cython ``.pxd`` file if you need to export any Cython functions
 """
 
 from __future__ import division, print_function, absolute_import
 
-__all__ = ['zeros', 'zeros_struct']
+__all__ = ['zeros', 'zeros_struct', 'zeros_array']
 
-from . import zeros, zeros_struct
+from . import zeros, zeros_struct, zeros_array
 
 from scipy._lib._testutils import PytestTester
 test = PytestTester(__name__)
