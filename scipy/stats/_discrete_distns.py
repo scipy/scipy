8b44f3c929a1713111424075ceac8c1374e1d53f
Lucas Roberts <rlucas7@vt.edu>
2018-10-21 15:42:04 -0400

    make PEP8 compliant line continuations

--- scipy/stats/_discrete_distns.py
+++ scipy/stats/_discrete_distns.py
@@ -956,8 +956,8 @@ class yulesimon_gen(rv_discrete):
                 np.inf)
         g1 = np.where(alpha <= 2, np.nan, g1)
         g2 = np.where(alpha > 4,
-                (alpha + 3) + (alpha**3 - 49 * alpha - 22) / (alpha * (alpha - 4) * (alpha - 3)),
-                np.inf)
+                (alpha + 3) + (alpha**3 - 49 * alpha - 22) / (alpha * \
+                        (alpha - 4) * (alpha - 3)), np.inf)
         g2 = np.where(alpha <= 2, np.nan, g2)
         return mu, mu2, g1, g2
 
