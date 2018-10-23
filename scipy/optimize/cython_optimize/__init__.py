9a907cabebfb7343a84d03353449c294e4c1a289
Mark Mikofski <bwana.marko@yahoo.com>
2018-10-23 01:19:23 -0700

    simplify docs

--- scipy/optimize/cython_optimize/__init__.py
+++ scipy/optimize/cython_optimize/__init__.py
@@ -1,24 +1,30 @@
 """
 Cython Optimize Zeros API
 =========================
-This package contains a Cython API for some of the root finding scalar
-functions in :py:mod:`scipy.optimize` to obtain efficient pure C
-implementations, although faster execution is not guaranteed. For example the
-Cython Optimize Zeros API could be used for native C looping or with Cython
-``prange`` for efficient execution of large arrays of inputs with the same
-callbacks. The Cython Optimize Zeros API can be imported into Cython code using
-the following::
+The following four functions in ``scipy.optimize`` can be accessed directly
+from Cython:
+
+- ``scipy.optimize.bisect``
+- ``scipy.optimize.ridder``
+- ``scipy.optimize.brenth``
+- ``scipy.optimize.brentq``
+
+The Cython Optimize Zeros API could be used to "vectorize" a root finder given
+a single callback function and an array of input conditions using a loop in C
+or with Cython ``prange``. Import the module into Cython as follows::
 
     from scipy.optimize cimport cython_optimize
 
 Callback Signatures
 -------------------
-The Cython Optimize Zeros API provides three callback signatures for the root
-finders, two which are safe without the global interpreter lock (GIL) and one
-that uses Python tuples. The functions are grouped by callback signatures into
+Three different callback signatures can be used with the root finders. The functions are grouped by callback signatures into
 Cython modules that can be imported using ``cimport``.
 
-.. py:module:: scipy.optimize.cython_optimize.zeros_struct
+    double (*callback_type)(double, void*)
+    double (*callback_type_array)(int, double*)
+    double (*callback_type_tuple)(double, tuple)
+
+Import the module with the desired callback type.
 
 ``cython_optimize.zeros_struct``
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -26,13 +32,9 @@ The root-finders in this module use a callback that takes a double with the
 scalar independent variable as the 1st argument and a user defined ``struct``
 with any extra parameters as the 2nd. ::
 
-    double (*callback_type)(double, void*)
-
-This module can be imported using the following::
+Import the module into Cython as follows::
 
     from scipy.optimize.cython_optimize cimport zeros_struct
-
-.. py:module:: scipy.optimize.cython_optimize.zeros_array
 
 ``cython_optimize.zeros_array``
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -45,13 +47,9 @@ your callback. Therefore, in your callback, the independent variable must be
 the first element in the array, followed by the extra parameters.  Also the
 maximum number of extra parameters is hard-coded as ``MAXARGS = 10``. ::
 
-    double (*callback_type_array)(int, double*)
-
-This module can be imported using the following::
+Import the module into Cython as follows::
 
     from scipy.optimize.cython_optimize cimport zeros_array
-
-.. py:module:: scipy.optimize.cython_optimize.zeros_tuple
 
 ``cython_optimize.zeros_tuple``
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -60,294 +58,21 @@ scalar independent variable as the 1st argument and a Python tuple with any
 extra parameters as the 2nd. Therefore this signature is *not* safe to use with
 ``nogil``. ::
 
-    double (*callback_type_tuple)(double, tuple)
-
-This module can be imported using the following::
+Import the module into Cython as follows::
 
     from scipy.optimize.cython_optimize cimport zeros_tuple
 
-Available Functions
--------------------
-There are four available functions which are all implemented in pure C and
-exposed in modules matching the expected callback signature. These root-finding
-functions correspond to the functions available in ``scipy.optimize.zeros``.
-
-- :py:func:`~scipy.optimize.bisect`
-- :py:func:`~scipy.optimize.ridder`
-- :py:func:`~scipy.optimize.brenth`
-- :py:func:`~scipy.optimize.brentq`
-
-:py:mod:`~scipy.optimize.cython_optimize.zeros_struct`
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-This module exposes root finders expecting extra arguments in a native C
-``struct``. See examples for usage.
-
-- ``cython_optimize.zeros_struct.bisect``
-
-  .. py:function:: scipy.optimize.cython_optimize.zeros_struct.bisect(f, xa, xb, args, xtol, rtol, iter, full_output)
-
-     Similiar to :py:func:`~scipy.optimize.bisect` with a callback, ``f``, that
-     takes a double and a native C ``struct`` ::
-
-         double bisect(callback_type f, double xa, double xb, void* args,
-                       double xtol, double rtol, int iter, scipy_zeros_parameters *full_output)
-
-     :param callback_type f: the callback function
-     :param double xa: lower bound
-     :param double xb: upper bound
-     :param void* args: a ``struct`` of extra parameters
-     :param double xtol: absolute tolerance
-     :param double rtol: relative tolerance
-     :param int iter: maximum number of iterations
-     :param scipy_zeros_parameters* full_output: optional output ``struct``, use ``NULL`` if not desired
-     :return: double
-
-- ``cython_optimize.zeros_struct.ridder``
-
-  .. py:function:: scipy.optimize.cython_optimize.zeros_struct.ridder(f, xa, xb, args, xtol, rtol, iter, full_output)
-
-     Similar to :py:func:`~scipy.optimize.ridder` with a callback, ``f``, that
-     takes a double and a native C ``struct`` ::
-
-         double ridder(callback_type f, double xa, double xb, void* args,
-                       double xtol, double rtol, int iter, scipy_zeros_parameters *full_output)
-
-     :param callback_type f: the callback function
-     :param double xa: lower bound
-     :param double xb: upper bound
-     :param void* args: a ``struct`` of extra parameters
-     :param double xtol: absolute tolerance
-     :param double rtol: relative tolerance
-     :param int iter: maximum number of iterations
-     :param scipy_zeros_parameters* full_output: optional output ``struct``, use ``NULL`` if not desired
-     :return: double
-
-- ``cython_optimize.zeros_struct.brenth``
-
-  .. py:function:: scipy.optimize.cython_optimize.zeros_struct.brenth(f, xa, xb, args, xtol, rtol, iter, full_output)
-
-     Similar to :py:func:`~scipy.optimize.brenth` with a callback, ``f``, that
-     takes a double and a native C ``struct`` ::
-
-         double brenth(callback_type f, double xa, double xb, void* args,
-                       double xtol, double rtol, int iter, scipy_zeros_parameters *full_output)
-
-     :param callback_type f: the callback function
-     :param double xa: lower bound
-     :param double xb: upper bound
-     :param void* args: a ``struct`` of extra parameters
-     :param double xtol: absolute tolerance
-     :param double rtol: relative tolerance
-     :param int iter: maximum number of iterations
-     :param scipy_zeros_parameters* full_output: optional output ``struct``, use ``NULL`` if not desired
-     :return: double
-
-- ``cython_optimize.zeros_struct.brentq``
-
-  .. py:function:: scipy.optimize.cython_optimize.zeros_struct.brentq(f, xa, xb, args, xtol, rtol, iter, full_output)
-
-     Similar to :py:func:`~scipy.optimize.brentq` with a callback, ``f``, that
-     takes a double and a native C ``struct`` ::
-
-         double brentq(callback_type f, double xa, double xb, void* args,
-                       double xtol, double rtol, int iter, scipy_zeros_parameters *full_output)
-
-     :param callback_type f: the callback function
-     :param double xa: lower bound
-     :param double xb: upper bound
-     :param void* args: a ``struct`` of extra parameters
-     :param double xtol: absolute tolerance
-     :param double rtol: relative tolerance
-     :param int iter: maximum number of iterations
-     :param scipy_zeros_parameters* full_output: optional output ``struct``, use ``NULL`` if not desired
-     :return: double
-
-:py:mod:`~scipy.optimize.cython_optimize.zeros_array`
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-Exposes root finders expecting extra arguments in a native C array of doubles.
-See examples for usage.
-
-- ``cython_optimize.zeros_array.bisect``
-
-  .. py:function:: scipy.optimize.cython_optimize.zeros_array.bisect(f, xa, xb, n, args, xtol, rtol, iter, full_output)
-
-     Similiar to :py:func:`~scipy.optimize.bisect` with a callback, ``f``, that
-     takes an integer and an array of doubles ::
-
-        double bisect(callback_type_array f, double xa, double xb, int n, double* args,
-                      double xtol, double rtol, int iter, scipy_zeros_parameters *full_output)
-
-     :param callback_type_array f: the callback function
-     :param double xa: lower bound
-     :param double xb: upper bound
-     :param int n: the number of extra parameters
-     :param double* args: an array of extra parameters, max size of 10
-     :param double xtol: absolute tolerance
-     :param double rtol: relative tolerance
-     :param int iter: maximum number of iterations
-     :param scipy_zeros_parameters full_output: optional output ``struct``, use ``NULL`` if not desired
-     :return: double
-
-- ``cython_optimize.zeros_array.ridder``
-
-  .. py:function:: scipy.optimize.cython_optimize.zeros_array.ridder(f, xa, xb, n, args, xtol, rtol, iter, full_output)
-
-     Similiar to :py:func:`~scipy.optimize.ridder` with a callback, ``f``, that
-     takes an integer and an array of doubles ::
-
-        double ridder(callback_type_array f, double xa, double xb, int n, double* args,
-                      double xtol, double rtol, int iter, scipy_zeros_parameters *full_output)
-
-     :param callback_type_array f: the callback function
-     :param double xa: lower bound
-     :param double xb: upper bound
-     :param int n: the number of extra parameters
-     :param double* args: an array of extra parameters, max size of 10
-     :param double xtol: absolute tolerance
-     :param double rtol: relative tolerance
-     :param int iter: maximum number of iterations
-     :param scipy_zeros_parameters* full_output: optional output ``struct``, use ``NULL`` if not desired
-     :return: double
-
-- ``cython_optimize.zeros_array.brenth``
-
-  .. py:function:: scipy.optimize.cython_optimize.zeros_array.brenth(f, xa, xb, n, args, xtol, rtol, iter, full_output)
-
-     Similiar to :py:func:`~scipy.optimize.brenth` with a callback, ``f``, that
-     takes an integer and an array of doubles ::
-
-        double brenth(callback_type_array f, double xa, double xb, int n, double* args,
-                      double xtol, double rtol, int iter, scipy_zeros_parameters *full_output)
-
-     :param callback_type_array f: the callback function
-     :param double xa: lower bound
-     :param double xb: upper bound
-     :param int n: the number of extra parameters
-     :param double* args: an array of extra parameters, max size of 10
-     :param double xtol: absolute tolerance
-     :param double rtol: relative tolerance
-     :param int iter: maximum number of iterations
-     :param scipy_zeros_parameters* full_output: optional output ``struct``, use ``NULL`` if not desired
-     :return: double
-
-- ``cython_optimize.zeros_array.brentq``
-
-  .. py:function:: scipy.optimize.cython_optimize.zeros_array.brentq(f, xa, xb, n, args, xtol, rtol, iter, full_output)
-
-     Similiar to :py:func:`~scipy.optimize.brentq` with a callback, ``f``, that
-     takes an integer and an array of doubles ::
-
-        double brentq(callback_type_array f, double xa, double xb, int n, double* args,
-                      double xtol, double rtol, int iter, scipy_zeros_parameters *full_output)
-
-     :param callback_type_array f: the callback function
-     :param double xa: lower bound
-     :param double xb: upper bound
-     :param int n: the number of extra parameters
-     :param double* args: an array of extra parameters, max size of 10
-     :param double xtol: absolute tolerance
-     :param double rtol: relative tolerance
-     :param int iter: maximum number of iterations
-     :param scipy_zeros_parameters* full_output: optional output ``struct``, use ``NULL`` if not desired
-     :return: double
-
-:py:mod:`~scipy.optimize.cython_optimize.zeros_tuple`
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-Exposes root finders expecting extra arguments in a Python tuple.
-
-- ``cython_optimize.zeros_tuple.bisect``
-
-  .. py:function:: scipy.optimize.cython_optimize.zeros_tuple.bisect(f, xa, xb, args, xtol, rtol, iter, full_output)
-
-     Similiar to :py:func:`~scipy.optimize.bisect` with a callback, ``f``, that
-     takes a double and a Python tuple ::
-
-        double bisect(callback_type_tuple f, double xa, double xb, tuple args,
-                      double xtol, double rtol, int iter, scipy_zeros_parameters *full_output)
-
-     :param callback_type_tuple f: the callback function
-     :param double xa: lower bound
-     :param double xb: upper bound
-     :param tuple args: extra parameters
-     :param double xtol: absolute tolerance
-     :param double rtol: relative tolerance
-     :param int iter: maximum number of iterations
-     :param scipy_zeros_parameters* full_output: optional output ``struct``, use ``NULL`` if not desired
-     :return: double
-
-- ``cython_optimize.zeros_tuple.ridder``
-
-  .. py:function:: scipy.optimize.cython_optimize.zeros_tuple.ridder(f, xa, xb, args, xtol, rtol, iter, full_output)
-
-     Similiar to :py:func:`~scipy.optimize.ridder` with a callback, ``f``, that
-     takes a double and a Python tuple ::
-
-        double ridder(callback_type_tuple f, double xa, double xb, tuple args,
-                      double xtol, double rtol, int iter, scipy_zeros_parameters *full_output)
-
-     :param callback_type_tuple f: the callback function
-     :param double xa: lower bound
-     :param double xb: upper bound
-     :param tuple args: extra parameters
-     :param double xtol: absolute tolerance
-     :param double rtol: relative tolerance
-     :param int iter: maximum number of iterations
-     :param scipy_zeros_parameters* full_output: optional output ``struct``, use ``NULL`` if not desired
-     :return: double
-
-- ``cython_optimize.zeros_tuple.brenth``
-
-  .. py:function:: scipy.optimize.cython_optimize.zeros_tuple.brenth(f, xa, xb, args, xtol, rtol, iter, full_output)
-
-     Similiar to :py:func:`~scipy.optimize.brenth` with a callback, ``f``, that
-     takes a double and a Python tuple ::
-
-        double brenth(callback_type_tuple f, double xa, double xb, tuple args,
-                      double xtol, double rtol, int iter, scipy_zeros_parameters *full_output)
-
-     :param callback_type_tuple f: the callback function
-     :param double xa: lower bound
-     :param double xb: upper bound
-     :param tuple args: extra parameters
-     :param double xtol: absolute tolerance
-     :param double rtol: relative tolerance
-     :param int iter: maximum number of iterations
-     :param scipy_zeros_parameters* full_output: optional output ``struct``, use ``NULL`` if not desired
-     :return: double
-
-- ``cython_optimize.zeros_tuple.brentq``
-
-  .. py:function:: scipy.optimize.cython_optimize.zeros_tuple.brentq(f, xa, xb, args, xtol, rtol, iter, full_output)
-
-     Similiar to :py:func:`~scipy.optimize.brentq` with a callback, ``f``, that
-     takes a double and a Python tuple ::
-
-        double brentq(callback_type_tuple f, double xa, double xb, tuple args,
-                      double xtol, double rtol, int iter, scipy_zeros_parameters *full_output)
-
-     :param callback_type_tuple f: the callback function
-     :param double xa: lower bound
-     :param double xb: upper bound
-     :param tuple args: extra parameters
-     :param double xtol: absolute tolerance
-     :param double rtol: relative tolerance
-     :param int iter: maximum number of iterations
-     :param scipy_zeros_parameters* full_output: optional output ``struct``, use ``NULL`` if not desired
-     :return: double
-
 Examples
 --------
-Usage of :py:mod:`~scipy.optimize.cython_optimize` requires the use of Cython
-to write callbacks that are compiled into native C. There are several usage examples that solve the single-diode model of
-a solar cell in the ``cython_optimize/Examples`` folder. These examples are
-also used to test the different combinations of callbacks and root-finders.
+Usage of ``scipy.optimize.cython_optimize`` requires the use of Cython to write
+callbacks that are compiled into C.
 
-These are the basic steps to use :py:mod:`~scipy.optimize.cython_optimize`:
+These are the basic steps to use ``scipy.optimize.cython_optimize``:
 
 1. Select a callback signature, for example:
-   :py:mod:`~scipy.optimize.cython_optimize.zeros_struct`
+   ``scipy.optimize.cython_optimize.zeros_struct``
 2. Select the root finder, for example:
-   :py:func:`~scipy.optimize.cython_optimize.zeros_struct.brentq`
+   ``scipy.optimize.cython_optimize.zeros_struct.brentq``
 3. Create a Cython ``.pyx`` file that imports the zeros module with the
    selected callback signature, create the callback(s) using the selected
    signature, and call the selected root-finder passing the callback(s), any
@@ -387,9 +112,9 @@ These are the basic steps to use :py:mod:`~scipy.optimize.cython_optimize`:
 
 Full Output
 -----------
-The  functions in :py:mod:`~scipy.optimize.cython_optimize` can also copy the
-full output from the solver to a C ``struct`` that is passed as its last
-argument. If you don't want the full output just pass ``NULL``. The full output
+The  functions in ``scipy.optimize.cython_optimize`` can also copy the full
+output from the solver to a C ``struct`` that is passed as its last argument.
+If you don't want the full output just pass ``NULL``. The full output
 ``struct`` should contain the following:
 
 - ``funcalls``: number of function calls
