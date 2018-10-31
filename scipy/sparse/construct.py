39b885d4d7d0647527db0ebb8e9a718687ad84a5
ludcila <cheunglucia@gmail.com>
2018-10-31 13:56:25 -0500

    Improve code

--- scipy/sparse/construct.py
+++ scipy/sparse/construct.py
@@ -625,7 +625,7 @@ def bmat(blocks, format=None, dtype=None):
     return coo_matrix((data, (row, col)), shape=shape).asformat(format)
 
 
-def block_diag(mats, format='coo', dtype=None):
+def block_diag(mats, format=None, dtype=None):
     """
     Build a block diagonal sparse matrix from provided matrices.
 
@@ -667,7 +667,6 @@ def block_diag(mats, format='coo', dtype=None):
            [0, 0, 0, 7]])
 
     """
-    dtype = np.dtype(dtype)
     row = []
     col = []
     data = []
@@ -677,14 +676,13 @@ def block_diag(mats, format='coo', dtype=None):
         if issparse(a):
             a = a.tocsr()
         else:
-            a = coo_matrix(a).tocsr()
+            a = csr_matrix(a)
         nrows, ncols = a.shape
         for r in range(nrows):
             for c in range(ncols):
-                if a[r, c] is not None:
-                    row.append(r + r_idx)
-                    col.append(c + c_idx)
-                    data.append(a[r, c])
+                row.append(r + r_idx)
+                col.append(c + c_idx)
+                data.append(a[r, c])
         r_idx = r_idx + nrows
         c_idx = c_idx + ncols
     return coo_matrix((data, (row, col)), dtype=dtype).asformat(format)
