55ccee79650128efed07da2e162e97cf36866378
chrisb83 <christoph.baumgarten@gmail.com>
2018-11-03 22:45:46 +0100

    minor docstring changes

--- scipy/stats/_rvs_sampling.py
+++ scipy/stats/_rvs_sampling.py
@@ -15,12 +15,12 @@ def rvs_ratio_uniforms(pdf, umax, vmin, vmax, size=1, c=0, random_state=None):
     pdf : callable
         A function with signature `pdf(x)` that is the probability
         density function of the distribution.
+    umax : float
+        The upper bound of the bounding rectangle in the y-direction.
     vmin : float
         The lower bound of the bounding rectangle in the x-direction.
     vmax : float
         The upper bound of the bounding rectangle in the x-direction.
-    umax : float
-        The upper bound of the bounding rectangle in the y-direction.
     size : int or tuple of ints, optional
         Defining number of random variates (default is 1).
     c : float, optional.
@@ -38,41 +38,41 @@ def rvs_ratio_uniforms(pdf, umax, vmin, vmax, size=1, c=0, random_state=None):
 
     Notes
     -----
-    Given a univariate probability density function (pdf) f and a constant c,
-    define the set ``A = {(u, v) : 0 < u <= sqrt(f(v/u + c))}``.
-    If ``(U, V)`` is a random vector uniformly distributed over ``A``,
-    then ``V/U + c`` follows a distribution with pdf ``f``.
+    Given a univariate probability density function `pdf` and a constant `c`,
+    define the set ``A = {(u, v) : 0 < u <= sqrt(pdf(v/u + c))}``.
+    If `(U, V)` is a random vector uniformly distributed over `A`,
+    then `V/U + c` follows a distribution according to `pdf`.
 
     The above result (see [1]_, [2]_) can be used to sample random variables
     using only the pdf, i.e. no inversion of the cdf is required. Typical
-    choices of c are zero or the mode of f. The set ``A`` is a subset of the
-    rectangle ``R = [0, umax] x [vmin, vmax]`` where
+    choices of `c` are zero or the mode of `pdf`. The set `A` is a subset of
+    the rectangle ``R = [0, umax] x [vmin, vmax]`` where
 
-    - ``umax = sup sqrt(f(x))``
-    - ``vmin = inf (x - c) sqrt(f(x))``
-    - ``vmax = sup (x - c) sqrt(f(x))``
+    - ``umax = sup sqrt(pdf(x))``
+    - ``vmin = inf (x - c) sqrt(pdf(x))``
+    - ``vmax = sup (x - c) sqrt(pdf(x))``
 
-    In particular, these values are finite if `f` is bounded and
-    ``x**2 * f(x)`` is bounded (i.e. subquadratic tails).
-    One can generate ``(U, V)`` uniformly on ``R`` and return
-    ``V/U + c`` if ``(U, V)`` are also in ``A`` which can be directly
+    In particular, these values are finite if `pdf` is bounded and
+    ``x**2 * pdf(x)`` is bounded (i.e. subquadratic tails).
+    One can generate `(U, V)` uniformly on `R` and return
+    `V/U + c` if `(U, V)` are also in `A` which can be directly
     verified.
 
-    Intuitively, the method works well if ``A`` fills up most of the
-    enclosing rectangle such that the probability is high that ``(U, V)``
-    lies in ``A`` whenever it lies in ``R`` as the number of required
+    Intuitively, the method works well if `A` fills up most of the
+    enclosing rectangle such that the probability is high that `(U, V)`
+    lies in `A` whenever it lies in `R` as the number of required
     iterations becomes too large otherwise. To be more precise, note that
-    the expected number of iterations to draw ``(U, V)`` uniformly
-    distributed on ``R`` such that ``(U, V)`` is also in ``A`` is given by
+    the expected number of iterations to draw `(U, V)` uniformly
+    distributed on `R` such that `(U, V)` is also in `A` is given by
     the ratio ``area(R) / area(A) = 2 * umax * (vmax - vmin)``, using the fact
-    that the area of ``A`` is equal to 1/2 (Theorem 7.1 in [1]_). A warning
+    that the area of `A` is equal to 1/2 (Theorem 7.1 in [1]_). A warning
     is displayed if this ratio is larger than 20. Moreover, if the sampling
-    fails to generate a single random variate after 50'000 iterations (i.e.
-    not a single draw is in ``A``), an exception is raised.
+    fails to generate a single random variate after 50000 iterations (i.e.
+    not a single draw is in `A`), an exception is raised.
 
     If the bounding rectangle is not correctly specified (i.e. if it does not
-    contain ``A``), the algorithm samples from a distribution different from
-    the one given by the pdf. It is therefore recommended to perform a
+    contain `A`), the algorithm samples from a distribution different from
+    the one given by `pdf`. It is therefore recommended to perform a
     test such as `stats.kstest` as a check.
 
     References
@@ -101,7 +101,7 @@ def rvs_ratio_uniforms(pdf, umax, vmin, vmax, size=1, c=0, random_state=None):
     >>> rvs = stats.rvs_ratio_uniforms(f, umax, vmin, vmax, size=2500)
 
     The K-S test confirms that the random variates are indeed normally
-    distributed (p-value does not reject normality):
+    distributed (normality is not rejected at 5% significance level):
 
     >>> stats.kstest(rvs, 'norm')[1]
     0.3420173467307603
