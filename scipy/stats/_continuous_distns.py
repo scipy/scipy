b30ff1d72bf0c11374d187182da4d7d7ac1baa7b
Lucas Roberts <rlucas7@vt.edu>
2018-12-14 21:59:32 -0500

    WIP: fix docstring and use log1p for @chrisb83 comments

--- scipy/stats/_continuous_distns.py
+++ scipy/stats/_continuous_distns.py
@@ -763,14 +763,6 @@ class burr_gen(rv_continuous):
 
     `burr` takes :math:`c` and :math:`d` as shape parameters.
 
-    The kth moment of `burr` is:
-
-    .. math::
-
-        E(X^k) = \frac{B(d + k/c, 1 - k/c)}{B(d,1)}
-
-    where :math:`c` and :math:`d` are shape parameters and `c > k`.
-
     This is the PDF corresponding to the third CDF given in Burr's list;
     specifically, it is equation (11) in Burr's paper [1]_. The distribution
     is also commonly referred to as the Dagum distribution [2]_. If the
@@ -798,7 +790,7 @@ class burr_gen(rv_continuous):
 
     def _logpdf(self, x, c, d):
         return np.log(c) + np.log(d) + (-c - 1.0) * np.log(x) \
-                + (-d - 1.0) * np.log(1 + x**(-c))
+                + (-d - 1.0) * np.log1p(x**(-c))
 
     def _cdf(self, x, c, d):
         return (1 + x**(-c))**(-d)
