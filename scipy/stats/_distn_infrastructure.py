36abad46d04cb7d67ea692f33eb9151227efd29e
Dowon <akahard2dj@naver.com>
2018-12-18 11:08:49 +0900

    array value checking method changes

--- scipy/stats/_distn_infrastructure.py
+++ scipy/stats/_distn_infrastructure.py
@@ -3357,7 +3357,7 @@ class rv_sample(rv_discrete):
 
         if np.shape(xk) != np.shape(pk):
             raise ValueError("xk and pk must have the same shape.")
-        if not all([x >= 0.0 for x in pk]):
+        if np.less(pk, 0.0).any():
             raise ValueError("pk cannot be negative values.")
         if not np.allclose(np.sum(pk), 1):
             raise ValueError("The sum of provided pk is not 1.")
