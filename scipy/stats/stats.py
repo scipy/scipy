0464ae0cbdc8d5f3369d828ae344f51a1848b481
Stefan Peterson <sjpet@users.noreply.github.com>
2018-12-18 07:56:55 +0100

    DOC: specify which definition of skewness is used (#9505)
    DOC: specify which definition of skewness is used

--- scipy/stats/stats.py
+++ scipy/stats/stats.py
@@ -1027,7 +1027,7 @@ def variation(a, axis=0, nan_policy='propagate'):
 
 def skew(a, axis=0, bias=True, nan_policy='propagate'):
     """
-    Compute the skewness of a data set.
+    Compute the sample skewness of a data set.
 
     For normally distributed data, the skewness should be about 0. For
     unimodal continuous distributions, a skewness value > 0 means that
@@ -1054,6 +1054,25 @@ def skew(a, axis=0, bias=True, nan_policy='propagate'):
     skewness : ndarray
         The skewness of values along an axis, returning 0 where all values are
         equal.
+
+    Notes
+    -----
+    The sample skewness is computed as the Fisher-Pearson coefficicient
+    of skewness, i.e.
+    
+    .. math:: g_1=\\frac{m_3}{m_2^{3/2}}
+
+    where
+
+    .. math:: m_i=\\frac{1}{N}\\sum_{n=1}^N(x[n]-\\bar{x})^i
+
+    is the biased sample :math:`i\\texttt{th}` central moment, and :math:`\\bar{x}` is
+    the sample mean.  If ``bias`` is False, the calculations are
+    corrected for bias and the value computed is the adjusted
+    Fisher-Pearson standardized moment coefficient, i.e. 
+    
+    .. math:: G_1=\\frac{k_3}{k_2^{3/2}}=
+                  \\frac{\\sqrt{N(N-1)}}{N-2}\\frac{m_3}{m_2^{3/2}}.
 
     References
     ----------
