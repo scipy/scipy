e57dbc0f58d95853d1ad677dbf11509469b130db
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2019-01-02 16:42:50 +0300

    DOC: fix import in stats.contingency refguide-check

--- scipy/stats/contingency.py
+++ scipy/stats/contingency.py
@@ -34,6 +34,7 @@ def margins(a):
     >>> a
     array([[ 0,  1,  2,  3,  4,  5],
            [ 6,  7,  8,  9, 10, 11]])
+    >>> from scipy.stats.contingency import margins
     >>> m0, m1 = margins(a)
     >>> m0
     array([[15],
