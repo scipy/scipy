271a2003aa1d584ef69a495e3c4d2aac3622909a
Mark Mikofski <bwana.marko@yahoo.com>
2019-01-06 15:52:11 -0800

    TST: test newton fails for mismatch between f and fpp
    * if passed an array longer than size of one
    
    Signed-off-by: Mark Mikofski <bwana.marko@yahoo.com>

--- scipy/optimize/tests/test_zeros.py
+++ scipy/optimize/tests/test_zeros.py
@@ -638,3 +638,18 @@ def test_gh_9608_preserve_array_shape():
     x0 = np.array([-2], dtype=np.float32)
     rt, r = newton(f, x0, fprime=fp, fprime2=fpp, full_output=True)
     assert(r.converged)
+
+    x0_array = np.array([-2, -3], dtype=np.float32)
+    # This next invocation should fail
+    with pytest.raises(IndexError):
+        result = zeros.newton(
+            f, x0_array, fprime=fp, fprime2=fpp, full_output=True
+        )
+
+    def fpp_array(x):
+        return 2*np.ones(np.shape(x), dtype=np.float32)
+
+    result = zeros.newton(
+        f, x0_array, fprime=fp, fprime2=fpp_array, full_output=True
+    )
+    assert result.converged.all()
