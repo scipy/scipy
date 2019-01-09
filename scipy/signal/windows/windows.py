65adf872da2e47b75029de3ca8653aa7b3bd5c0e
Eric Larson <larson.eric.d@gmail.com>
2019-01-08 21:38:59 -0500

    FIX: Be more picky

--- scipy/signal/windows/windows.py
+++ scipy/signal/windows/windows.py
@@ -430,7 +430,7 @@ def blackman(M, sym=True):
     >>> A = fft(window, 2048) / (len(window)/2.0)
     >>> freq = np.linspace(-0.5, 0.5, len(A))
     >>> response = np.abs(fftshift(A / abs(A).max()))
-    >>> response = 20 * np.log10(np.maximum(rensponse, 1e-10))
+    >>> response = 20 * np.log10(np.maximum(response, 1e-10))
     >>> plt.plot(freq, response)
     >>> plt.axis([-0.5, 0.5, -120, 0])
     >>> plt.title("Frequency response of the Blackman window")
