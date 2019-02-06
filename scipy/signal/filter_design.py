6ca541da3e45115754feac8c0c26dcf3e0f40208
Franz Forstmayr <franz.forstmayr@rosenberger.com>
2019-02-06 21:32:08 +0100

    Added example for bilinear_zpk

--- scipy/signal/filter_design.py
+++ scipy/signal/filter_design.py
@@ -2341,6 +2341,24 @@ def bilinear_zpk(z, p, k, fs):
     -----
     .. versionadded:: 1.1.0
 
+    Examples
+    --------
+    >>> from scipy import signal
+    >>> import matplotlib.pyplot as plt
+    >>> import numpy as np
+
+    >>> fs = 100
+    >>> bf = 2 * np.pi * np.array([7, 13])
+    >>> filts = signal.lti(*signal.butter(4, bf, btype='bandpass', analog=True, output='zpk'))
+    >>> filtz = signal.lti(*signal.bilinear_zpk(filts.zeros, filts.poles, filts.gain, fs))
+    >>> wz, hz = signal.freqz_zpk(filtz.zeros, filtz.poles, filtz.gain)
+    >>> ws, hs = signal.freqs_zpk(filts.zeros, filts.poles, filts.gain, worN=fs*wz)
+    >>> plt.semilogx(wz*fs/(2*np.pi), np.abs(hz), label=r'$|H(j \omega)|$ of analog bandpass')
+    >>> plt.semilogx(wz*fs/(2*np.pi), np.abs(hs), label=r'$|H_z(j \omega)|$ of digital bandpass')
+    >>> plt.legend()
+    >>> plt.xlabel('Frequency [Hz]')
+    >>> plt.ylabel('Magnitude [dB]')
+    >>> plt.grid()
     """
     z = atleast_1d(z)
     p = atleast_1d(p)
