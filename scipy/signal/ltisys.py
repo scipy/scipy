4389087ff11e5256c8c96388bf1ed5bad2e8702a
hbina4326 <hanif.ariffin.4326@gmail.com>
2019-02-19 17:17:33 -0500

    fixed the example code given in scipy.signal.lsim by importing the required library and call to function that will actually show the graph

--- scipy/signal/ltisys.py
+++ scipy/signal/ltisys.py
@@ -1917,11 +1917,13 @@ def lsim(system, U, T, X0=None, interp=True):
 
     >>> from scipy import signal
     >>> system = signal.lti([[0., 1.], [0., 0.]], [[0.], [1.]], [[1., 0.]], 0.)
+    >>> import numpy as np
     >>> t = np.linspace(0, 5)
     >>> u = np.ones_like(t)
     >>> tout, y, x = signal.lsim(system, u, t)
     >>> import matplotlib.pyplot as plt
     >>> plt.plot(t, y)
+    >>> plt.show()
     """
     if isinstance(system, lti):
         sys = system._as_ss()
