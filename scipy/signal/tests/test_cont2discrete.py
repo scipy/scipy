2e55c6057e864e9d42f88bbf00a8f4349e28edaf
Vladimir Korolev <balodja@gmail.com>
2019-03-02 01:35:59 +0300

    Add some non-trivial tests of impulse-zoh-foh discretizations to signal.cont2discrete.

--- scipy/signal/tests/test_cont2discrete.py
+++ scipy/signal/tests/test_cont2discrete.py
@@ -5,12 +5,16 @@ from numpy.testing import \
                           assert_array_almost_equal, assert_almost_equal, \
                           assert_allclose, assert_equal
 
-import warnings
+import pytest
 from scipy.signal import cont2discrete as c2d
 from scipy.signal import dlsim, ss2tf, ss2zpk, lsim2, lti
+from scipy.signal import tf2ss, impulse2, dimpulse, step2, dstep
 
 # Author: Jeffrey Armstrong <jeff@approximatrix.com>
 # March 29, 2011
+
+# You can run these tests isolated as follows:
+# $ python runtests.py --tests scipy/signal/tests/test_cont2discrete.py
 
 
 class TestC2D(object):
@@ -414,3 +418,40 @@ class TestC2dLti(object):
         # Somehow a lot of numerical errors
         assert_allclose(sys.den, den_res, atol=0.02)
         assert_allclose(sys.num, num_res, atol=0.02)
+
+
+class TestC2dInvariants:
+    # Some test cases for checking the invariances.
+    # Array of triplets: (system, sample time, number of samples)
+    cases = [
+        (tf2ss([1, 1], [1, 1.5, 1]), 0.25, 10),
+        (tf2ss([1, 2], [1, 1.5, 3, 1]), 0.5, 10),
+        (tf2ss(0.1, [1, 1, 2, 1]), 0.5, 10),
+    ]
+
+    # Some options for lsim2 and derived routines
+    tolerances = {'rtol': 1e-9, 'atol': 1e-11}
+
+    # Check that systems discretized with the impulse-invariant method really hold the invariant
+    @pytest.mark.parametrize("sys,sample_time,samples_number", cases)
+    def test_impulse_invariant(self, sys, sample_time, samples_number):
+        time = np.arange(samples_number) * sample_time
+        _, yout_cont = impulse2(sys, T=time, **self.tolerances)
+        _, yout_disc = dimpulse(c2d(sys, sample_time, method='impulse'), n=len(time))
+        assert_allclose(sample_time * yout_cont.ravel(), yout_disc[0].ravel())
+
+    # Step invariant should hold for ZOH discretized systems
+    @pytest.mark.parametrize("sys,sample_time,samples_number", cases)
+    def test_step_invariant(self, sys, sample_time, samples_number):
+        time = np.arange(samples_number) * sample_time
+        _, yout_cont = step2(sys, T=time, **self.tolerances)
+        _, yout_disc = dstep(c2d(sys, sample_time, method='zoh'), n=len(time))
+        assert_allclose(yout_cont.ravel(), yout_disc[0].ravel())
+
+    # Linear invariant should hold for FOH discretized systems
+    @pytest.mark.parametrize("sys,sample_time,samples_number", cases)
+    def test_linear_invariant(self, sys, sample_time, samples_number):
+        time = np.arange(samples_number) * sample_time
+        _, yout_cont, _ = lsim2(sys, T=time, U=time, **self.tolerances)
+        _, yout_disc, _ = dlsim(c2d(sys, sample_time, method='foh'), u=time)
+        assert_allclose(yout_cont.ravel(), yout_disc.ravel())
