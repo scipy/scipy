7938df6dc5b36cb12a0f34e24ef01d0fd75fc986
Tyler Reddy <tyler.je.reddy@gmail.com>
2019-03-13 09:44:04 -0700

    MAINT: address reviewer comments in PR 9664.

--- benchmarks/benchmarks/sparse_csgraph_djisktra.py
+++ benchmarks/benchmarks/sparse_csgraph_djisktra.py
@@ -21,17 +21,17 @@ class Dijkstra(Benchmark):
     def setup(self, n, min_only):
         np.random.seed(1234)
         # make a random connectivity matrix
-        data = scipy.sparse.rand(n, n, density=0.2, format='csc', random_state=42, dtype=np.bool)
+        data = scipy.sparse.rand(n, n, density=0.2, format='csc',
+                                 random_state=42, dtype=np.bool)
         data.setdiag(np.zeros(n, dtype=np.bool))
         self.data = data
         # choose some random vertices
         v = np.arange(n)
         np.random.shuffle(v)
         self.indices = v[:int(n*.1)]
-        self.min_only = min_only
 
     def time_dijkstra_multi(self, n, min_only):
-        return dijkstra(self.data,
-                        directed=False,
-                        indices=self.indices,
-                        min_only=self.min_only)
+        dijkstra(self.data,
+                 directed=False,
+                 indices=self.indices,
+                 min_only=min_only)
