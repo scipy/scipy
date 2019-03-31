a33b260281d83c62ae7c0cf0f2d88b2b68ae788b
Mark Mikofski <bwana.marko@yahoo.com>
2019-03-31 16:16:45 -0700

    TEST: correct values for EXPECTED, improve test description

--- scipy/optimize/tests/test_cython_optimize.py
+++ scipy/optimize/tests/test_cython_optimize.py
@@ -17,23 +17,26 @@ from __future__ import division, print_function, absolute_import
 import numpy.testing as npt
 from scipy.optimize.cython_optimize import _zeros
 
-# constants
+# CONSTANTS
+# Solve x**3 - A0 = 0  for A0 = [2.0, 2.1, ..., 2.9].
+# The ARGS have 3 elements just to show how this could be done for any cubic
+# polynomial.
 A0 = tuple(-2.0 - x/10.0 for x in range(10))  # constant term
 ARGS = (0.0, 0.0, 1.0)  # 1st, 2nd, and 3rd order terms
 XLO, XHI = 0.0, 2.0  # first and second bounds of zeros functions
 # absolute and relative tolerances and max iterations for zeros functions
 XTOL, RTOL, MITR = 0.001, 0.001, 10
-EXPECTED = [
-    1.259872799904563,
-    1.28042866862737,
-    1.3003083443276644,
-    1.3195775152132223,
-    1.3382912269178606,
-    1.3576742112124918,
-    1.375418396530433,
-    1.3927268676453197,
-    1.4096289546536032,
-    1.4261502005552444]
+EXPECTED = [(-a0) ** (1.0/3.0) for a0 in A0]
+# = [1.2599210498948732,
+#    1.2805791649874942,
+#    1.300591446851387,
+#    1.3200061217959123,
+#    1.338865900164339,
+#    1.3572088082974532,
+#    1.375068867074141,
+#    1.3924766500838337,
+#    1.4094597464129783,
+#    1.4260431471424087]
 
 
 # test bisect
@@ -84,7 +87,7 @@ def test_brentq():
 def test_brentq_full_output():
     output = _zeros.full_output_example(
         (A0[0],) + ARGS, XLO, XHI, XTOL, RTOL, MITR)
-    npt.assert_allclose(EXPECTED[0], output['root'])
+    npt.assert_allclose(EXPECTED[0], output['root'], rtol=RTOL, atol=XTOL)
     npt.assert_equal(6, output['iterations'])
     npt.assert_equal(7, output['funcalls'])
     npt.assert_equal(0, output['error_num'])
