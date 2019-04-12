b4bf4ed0c8de9cfba2b59445434b50fa0becbf0c
Jarrod Millman <jarrod.millman@gmail.com>
2019-04-11 18:04:47 -0700

    Add project_urls to setup
    These are used by PyPI and other services.

--- setup.py
+++ setup.py
@@ -447,6 +447,11 @@ def setup_package():
         long_description="\n".join(DOCLINES[2:]),
         url="https://www.scipy.org",
         download_url="https://github.com/scipy/scipy/releases",
+        project_urls={
+            "Bug Tracker": "https://github.com/scipy/scipy/issues",
+            "Documentation": "https://docs.scipy.org/doc/scipy/reference/",
+            "Source Code": "https://github.com/scipy/scipy",
+        },
         license='BSD',
         cmdclass=cmdclass,
         classifiers=[_f for _f in CLASSIFIERS.split('\n') if _f],
