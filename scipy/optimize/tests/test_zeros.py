bbd0a8793dcf8b61e7248c10c77ee10bcb50a4a0
Mark Mikofski <bwana.marko@yahoo.com>
2019-04-19 00:05:02 -0700

    TST:MAINT: use pytest parametrize

--- scipy/optimize/tests/test_zeros.py
+++ scipy/optimize/tests/test_zeros.py
@@ -663,22 +663,22 @@ def test_gh_9608_preserve_array_shape():
     assert result.converged.all()
 
 
-def test_gh9254_flag_if_maxiter_exceeded():
+@pytest.mark.parametrize(
+    "maximum_iterations,flag_expected",
+    [(10, zeros.CONVERR), (100, zeros.CONVERGED)])
+def test_gh9254_flag_if_maxiter_exceeded(maximum_iterations, flag_expected):
     """
     Test that if the maximum iterations is exceeded that the flag is not
     converged.
     """
-    maximum_iterations = 10
-    r = zeros.brentq(
+    result = zeros.brentq(
         lambda x: ((1.2*x - 2.3)*x + 3.4)*x - 4.5,
         -30, 30, (), 1e-6, 1e-6, maximum_iterations,
         full_output=True, disp=False)
-    assert r[1].flag == zeros.CONVERR
-    assert r[1].iterations == maximum_iterations
-    more_maximum_iterations = 100
-    r = zeros.brentq(
-        lambda x: ((1.2*x - 2.3)*x + 3.4)*x - 4.5,
-        -30, 30, (), 1e-6, 1e-6, more_maximum_iterations,
-        full_output=True, disp=False)
-    assert r[1].flag == zeros.CONVERGED
-    assert r[1].iterations < more_maximum_iterations
+    assert result[1].flag == flag_expected
+    if flag_expected == zeros.CONVERR:
+        # didn't converge because exceeded maximum iterations
+        assert result[1].iterations == maximum_iterations
+    elif flag_expected == zeros.CONVERGED:
+        # converged before maximum iterations
+        assert result[1].iterations < maximum_iterations
