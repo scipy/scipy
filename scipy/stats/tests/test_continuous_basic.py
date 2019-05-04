ab35ab511988634c0c6fe16c7bb5080f8c1f7b99
chrisb83 <christoph.baumgarten@gmail.com>
2019-05-04 14:45:09 +0200

    add distribution to shape_only tests

--- scipy/stats/tests/test_continuous_basic.py
+++ scipy/stats/tests/test_continuous_basic.py
@@ -240,7 +240,8 @@ def test_rvs_broadcast(dist, shape_args):
     # the implementation the rvs() method of a distribution changes, this
     # test might also have to be changed.
     shape_only = dist in ['betaprime', 'dgamma', 'exponnorm', 'norminvgauss',
-                          'nct', 'dweibull', 'rice', 'levy_stable', 'skewnorm']
+                          'nct', 'dweibull', 'rice', 'levy_stable', 'skewnorm',
+                          'semicircular']
 
     distfunc = getattr(stats, dist)
     loc = np.zeros(2)
