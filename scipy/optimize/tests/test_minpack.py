b33476a61e943d3b7de2b04135b43d6878ec9d5e
Antony Lee <anntzer.lee@gmail.com>
2019-05-10 18:55:36 +0200

    BUG: Correctly handle broadcasted ydata in curve_fit pcov computation.
    When pcov is passed a scalar y that's intended to be broadcasted,
    the fitted value is correctly computed, but the computation of the
    covariance matrix fails because y.size is not the output dimension.
    
    Fix this by using the correct output dimension.

--- scipy/optimize/tests/test_minpack.py
+++ scipy/optimize/tests/test_minpack.py
@@ -771,6 +771,21 @@ class TestCurveFit(object):
 
         assert_allclose(popt_32, popt_64, atol=2e-5)
 
+    def test_broadcast_y(self):
+        xdata = np.arange(10)
+        target = 4.7 * xdata ** 2 + 3.5 * xdata + np.random.rand(len(xdata))
+        fit_func = lambda x, a, b: a*x**2 + b*x - target
+        for method in ['lm', 'trf', 'dogbox']:
+            popt0, pcov0 = curve_fit(fit_func,
+                                     xdata=xdata,
+                                     ydata=np.zeros_like(xdata),
+                                     method=method)
+            popt1, pcov1 = curve_fit(fit_func,
+                                     xdata=xdata,
+                                     ydata=0,
+                                     method=method)
+            assert_array_equal(pcov0, pcov1)
+
 
 class TestFixedPoint(object):
 
