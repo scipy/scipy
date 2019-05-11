5a500ed8d11941028bf8b871ccac44a45cefec8f
Nikolay Mayorov <nikolay.mayorov@zoho.com>
2019-05-12 01:07:20 +0300

    ENH: Add DOP853 constant definitions and test

--- scipy/integrate/_ivp/tests/test_rk_coefficients.py
+++ scipy/integrate/_ivp/tests/test_rk_coefficients.py
@@ -2,9 +2,17 @@ import pytest
 from numpy.testing import assert_allclose
 import numpy as np
 from scipy.integrate import RK23, RK45
+from scipy.integrate._ivp import dop853_coefficients
 
 
 @pytest.mark.parametrize("solver", [RK23, RK45])
 def test_coefficient_properties(solver):
     assert_allclose(np.sum(solver.B), 1, rtol=1e-15)
     assert_allclose(np.sum(solver.A, axis=1), solver.C, rtol=1e-15)
+
+
+def test_coefficient_properties_dop853():
+    assert_allclose(np.sum(dop853_coefficients.B), 1, rtol=1e-15)
+    assert_allclose(np.sum(dop853_coefficients.A, axis=1),
+                    dop853_coefficients.C,
+                    rtol=1e-14)
