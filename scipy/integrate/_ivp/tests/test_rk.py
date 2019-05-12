b86d23b0302bb4d0efa2aa203883a78d3dcbf26e
Nikolay Mayorov <nikolay.mayorov@zoho.com>
2019-05-12 12:51:32 +0300

    TST: Test of error estimation of Runge-Kutta methods

--- scipy/integrate/_ivp/tests/test_rk.py
+++ scipy/integrate/_ivp/tests/test_rk.py
@@ -1,5 +1,5 @@
 import pytest
-from numpy.testing import assert_allclose
+from numpy.testing import assert_allclose, assert_
 import numpy as np
 from scipy.integrate import RK23, RK45, DOP853
 from scipy.integrate._ivp import dop853_coefficients
@@ -16,3 +16,13 @@ def test_coefficient_properties_dop853():
     assert_allclose(np.sum(dop853_coefficients.A, axis=1),
                     dop853_coefficients.C,
                     rtol=1e-14)
+
+
+@pytest.mark.parametrize("solver_class", [RK23, RK45, DOP853])
+def test_error_estimation(solver_class):
+    step = 0.2
+    solver = solver_class(lambda t, y: y, 0, [1], 1, first_step=step)
+    solver.step()
+    error_estimate = solver._estimate_errors(solver.K, step)
+    error = solver.y - np.exp([step])
+    assert_(np.abs(error) < np.abs(error_estimate))
