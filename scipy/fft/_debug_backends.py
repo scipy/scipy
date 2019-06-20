1450a07f77ebce88463df7c089991bd947d086ce
Peter Bell <peterbell10@live.co.uk>
2019-06-20 19:01:21 +0100

    ENH: Add pyfftw backend for testing

--- scipy/fft/_debug_backends.py
+++ scipy/fft/_debug_backends.py
@@ -1,4 +1,8 @@
 import numpy as np
+try:
+    import pyfftw.interfaces.numpy_fft as pyfftw_fft
+except ImportError:
+    pass
 
 class NumPyBackend:
     """Backend that uses numpy.fft"""
@@ -6,7 +10,7 @@ class NumPyBackend:
 
     @staticmethod
     def __ua_convert__(value, type, coerce):
-        if type is np.array:
+        if type is np.ndarray:
             if not coerce:
                 return value
             return np.asarray(value)
@@ -17,6 +21,22 @@ class NumPyBackend:
         kwargs.pop("overwrite_x", None)
 
         fn = getattr(np.fft, method.__name__)
+        return (NotImplemented if fn is None
+                else fn(*args, **kwargs))
+
+
+class PyfftwBackend:
+    __ua_domain__ = 'scipy.fft'
+
+    @staticmethod
+    def __ua_convert__(value, type, coerce):
+        return (value if type is np.ndarray else NotImplemented)
+
+    @staticmethod
+    def __ua_function__(method, args, kwargs):
+        kwargs.pop('overwrite_x', None)
+
+        fn = getattr(pyfftw_fft, method.__name__)
         return (NotImplemented if fn is None
                 else fn(*args, **kwargs))
 
