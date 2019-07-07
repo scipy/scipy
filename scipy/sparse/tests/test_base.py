0d43695407c946fd5d34f06fb8caa6c79b1bd818
makbigc <makszechun@gmail.com>
2019-07-07 22:43:49 +0800

    fix PEP 8 issue

--- scipy/sparse/tests/test_base.py
+++ scipy/sparse/tests/test_base.py
@@ -660,7 +660,7 @@ class _TestCommon(object):
 
     def test_round(self):
         decimal = 1
-        A = matrix([[-1.35, 0.56],[17.25, -5.98]],'d')
+        A = matrix([[-1.35, 0.56], [17.25, -5.98]], 'd')
         assert_equal(np.around(A, decimals=decimal),
                      round(self.spmatrix(A), ndigits=decimal).todense())
 
