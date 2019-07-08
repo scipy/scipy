cc7ff49c1d367a1d9915521aec70a30e776dd126
Peter Bell <peterbell10@live.co.uk>
2019-07-08 19:00:08 +0100

    TST: Add tests for hermitian transforms

--- scipy/fft/tests/test_numpy.py
+++ scipy/fft/tests/test_numpy.py
@@ -135,8 +135,33 @@ class TestFFT1D(object):
             x_herm, fft.ihfft(fft.hfft(x_herm, norm="ortho"),
                                  norm="ortho"))
 
+    def test_hfft2(self):
+        x = random((30, 20))
+        assert_array_almost_equal(x, fft.hfft2(fft.ihfft2(x)))
+        assert_array_almost_equal(
+            x, fft.hfft2(fft.ihfft2(x, norm="ortho"), norm="ortho"))
+
+    def test_ihfft2(self):
+        x = random((30, 20))
+        assert_array_almost_equal(fft.ifft2(x)[:, :11], fft.ihfft2(x))
+        assert_array_almost_equal(fft.ihfft2(x) * np.sqrt(30 * 20),
+                                  fft.ihfft2(x, norm="ortho"))
+
+    def test_hfftn(self):
+        x = random((30, 20, 10))
+        assert_array_almost_equal(x, fft.hfftn(fft.ihfftn(x)))
+        assert_array_almost_equal(
+            x, fft.hfftn(fft.ihfftn(x, norm="ortho"), norm="ortho"))
+
+    def test_ihfftn(self):
+        x = random((30, 20, 10))
+        assert_array_almost_equal(fft.ifftn(x)[:, :, :6], fft.ihfftn(x))
+        assert_array_almost_equal(fft.ihfftn(x) * np.sqrt(30 * 20 * 10),
+                                  fft.ihfftn(x, norm="ortho"))
+
     @pytest.mark.parametrize("op", [fft.fftn, fft.ifftn,
-                                    fft.rfftn, fft.irfftn])
+                                    fft.rfftn, fft.irfftn,
+                                    fft.hfftn, fft.ihfftn])
     def test_axes(self, op):
         x = random((30, 20, 10))
         axes = [(0, 1, 2), (0, 2, 1), (1, 0, 2), (1, 2, 0), (2, 0, 1), (2, 1, 0)]
@@ -154,7 +179,7 @@ class TestFFT1D(object):
                       (fft.rfft, fft.irfft),
                       # hfft: order so the first function takes x.size samples
                       #       (necessary for comparison to x_norm above)
-                      #(fft.ihfft, fft.hfft),
+                      (fft.ihfft, fft.hfft),
                       ]
         for forw, back in func_pairs:
             for n in [x.size, 2*x.size]:
@@ -171,6 +196,7 @@ class TestFFT1D(object):
         x = random(30).astype(dtype)
         assert_array_almost_equal(fft.ifft(fft.fft(x)), x)
         assert_array_almost_equal(fft.irfft(fft.rfft(x)), x)
+        assert_array_almost_equal(fft.hfft(fft.ihfft(x), len(x)), x)
 
 
 @pytest.mark.parametrize(
@@ -248,6 +274,14 @@ class TestFFTThreadSafe(object):
     def test_irfft(self):
         a = np.ones(self.input_shape) * 1+0j
         self._test_mtsame(fft.irfft, a)
+
+    def test_hfft(self):
+        a = np.ones(self.input_shape, np.complex64)
+        self._test_mtsame(fft.hfft, a)
+
+    def test_ihfft(self):
+        a = np.ones(self.input_shape)
+        self._test_mtsame(fft.ihfft, a)
 
 
 class TestIRFFTN(object):
