31251bad6904211fc4be8646c0fb888065231de7
Peter Bell <peterbell10@live.co.uk>
2019-07-09 12:56:12 +0100

    MAINT: Update vendored uarray version
    The also moves the compile flag detection code from fft._pocketfft into
    _build_utils as it is now needed to build uarray as well.

--- scipy/_lib/uarray/_backend.py
+++ scipy/_lib/uarray/_backend.py
@@ -11,27 +11,66 @@ from typing import (
     List,
 )
 import inspect
-from contextvars import ContextVar
 import functools
-import contextlib
+from . import _uarray  # type: ignore
+import copyreg
+import atexit
+import pickle
 
 ArgumentExtractorType = Callable[..., Tuple["Dispatchable", ...]]
 ArgumentReplacerType = Callable[[Tuple, Dict, Tuple], Tuple[Tuple, Dict]]
 
+from ._uarray import (
+    BackendNotImplementedError,
+    _Function,
+    _SkipBackendContext,
+    _SetBackendContext,
+)
 
-class BackendNotImplementedError(NotImplementedError):
-    """
-    An exception that is thrown when no compatible backend is found for a method.
-    """
+
+def unpickle_function(mod_name, qname):
+    import importlib
+
+    try:
+        module = importlib.import_module(mod_name)
+        func = getattr(module, qname)
+        return func
+    except (ImportError, AttributeError) as e:
+        from pickle import UnpicklingError
+
+        raise UnpicklingError from e
+
+
+def pickle_function(func):
+    mod_name = getattr(func, "__module__", None)
+    qname = getattr(func, "__qualname__", None)
+
+    try:
+        test = unpickle_function(mod_name, qname)
+    except pickle.UnpicklingError:
+        test = None
+
+    if test is not func:
+        raise pickle.PicklingError(
+            "Can't pickle {}: it's not the same object as {}".format(func, test)
+        )
+
+    return unpickle_function, (mod_name, qname)
+
+
+copyreg.pickle(_Function, pickle_function)
+atexit.register(_uarray.clear_all_globals)
 
 
 def create_multimethod(*args, **kwargs):
     """
     Creates a decorator for generating multimethods.
+
     This function creates a decorator that can be used with an argument
     extractor in order to generate a multimethod. Other than for the
     argument extractor, all arguments are passed on to
     :obj:`generate_multimethod`.
+
     See Also
     --------
     generate_multimethod
@@ -52,6 +91,7 @@ def generate_multimethod(
 ):
     """
     Generates a multimethod.
+
     Parameters
     ----------
     argument_extractor : ArgumentExtractorType
@@ -66,21 +106,29 @@ def generate_multimethod(
     default: Optional[Callable], optional
         The default implementation of this multimethod, where ``None`` (the default) specifies
         there is no default implementation.
+
     Examples
     --------
     In this example, ``a`` is to be dispatched over, so we return it, while marking it as an ``int``.
     The trailing comma is needed because the args have to be returned as an iterable.
+
     >>> def override_me(a, b):
     ...   return Dispatchable(a, int),
+
     Next, we define the argument replacer that replaces the dispatchables inside args/kwargs with the
     supplied ones.
+
     >>> def override_replacer(args, kwargs, dispatchables):
     ...     return (dispatchables[0], args[1]), {}
+
     Next, we define the multimethod.
+
     >>> overridden_me = generate_multimethod(
     ...     override_me, override_replacer, "ua_examples"
     ... )
+
     Notice that there's no default implementation, unless you supply one.
+
     >>> overridden_me(1, "a")
     Traceback (most recent call last):
         ...
@@ -90,217 +138,59 @@ def generate_multimethod(
     ... )
     >>> overridden_me2(1, "a")
     (1, 'a')
+
     See Also
     --------
     uarray
         See the module documentation for how to override the method by creating backends.
     """
     kw_defaults, arg_defaults, opts = get_defaults(argument_extractor)
+    ua_func = _Function(
+        argument_extractor,
+        argument_replacer,
+        domain,
+        arg_defaults,
+        kw_defaults,
+        default,
+    )
 
-    @functools.wraps(argument_extractor)
-    def inner(*args, **kwargs):
-        dispatchable_args = argument_extractor(*args, **kwargs)
-        errors = []
-
-        args = canonicalize_args(args, kwargs)
-        result = NotImplemented
-
-        for options in _backend_order(domain):
-            res = (
-                replace_dispatchables(
-                    options.backend,
-                    args,
-                    kwargs,
-                    dispatchable_args,
-                    coerce=options.coerce,
-                )
-                if hasattr(options.backend, "__ua_convert__")
-                else (args, kwargs)
-            )
-
-            if res is NotImplemented:
-                continue
-
-            a, kw = res
-
-            for k, v in kw_defaults.items():
-                if k in kw and kw[k] is v:
-                    del kw[k]
-
-            result = options.backend.__ua_function__(inner, a, kw)
-
-            if result is NotImplemented:
-                result = try_default(a, kw, options, errors)
-
-            if result is not NotImplemented:
-                break
-        else:
-            result = try_default(args, kwargs, None, errors)
-
-        if result is NotImplemented:
-            raise BackendNotImplementedError(
-                "No selected backends had an implementation for this function.", errors
-            )
-
-        return result
-
-    def try_default(args, kwargs, options, errors):
-        if default is not None:
-            try:
-                if options is not None:
-                    with set_backend(options.backend, only=True, coerce=options.coerce):
-                        return default(*args, **kwargs)
-                else:
-                    return default(*args, **kwargs)
-            except BackendNotImplementedError as e:
-                errors.append(e)
-
-        return NotImplemented
-
-    def replace_dispatchables(
-        backend, args, kwargs, dispatchable_args, coerce: Optional[bool] = False
-    ):
-        replaced_args: Iterable = backend.__ua_convert__(dispatchable_args, coerce)
-
-        if replaced_args is NotImplemented:
-            return NotImplemented
-
-        return argument_replacer(args, kwargs, tuple(replaced_args))
-
-    def canonicalize_args(args, kwargs):
-        if len(args) > len(arg_defaults):
-            return args
-
-        match = 0
-        for a, d in zip(args[::-1], arg_defaults[len(args) - 1 :: -1]):
-            if a is d:
-                match += 1
-            else:
-                break
-
-        args = args[:-match] if match > 0 else args
-        return args
-
-    inner._coerce_args = replace_dispatchables  # type: ignore
-
-    return inner
+    return functools.update_wrapper(ua_func, argument_extractor)
 
 
-class _BackendOptions:
-    def __init__(self, backend, coerce: bool = False, only: bool = False):
-        """
-        The backend plus any additonal options associated with it.
-        Parameters
-        ----------
-        backend : Backend
-            The associated backend.
-        coerce: bool, optional
-            Whether or not the backend is being coerced. Implies ``only``.
-        only: bool, optional
-            Whether or not this is the only backend to try.
-        """
-        self.backend = backend
-        self.coerce = coerce
-        self.only = only or coerce
-
-
-_backends: Dict[str, ContextVar] = {}
-
-
-def _backend_order(domain: str) -> Iterable[_BackendOptions]:
-    skip = _get_skipped_backends(domain).get()
-    pref = _get_preferred_backends(domain).get()
-
-    for options in pref:
-        if options.backend not in skip:
-            yield options
-
-            if options.only:
-                return
-
-    if domain in _backends and _backends[domain] not in skip:
-        yield _BackendOptions(_backends[domain])
-
-    if domain in _registered_backend:
-        for backend in _registered_backend[domain]:
-            if backend not in skip:
-                yield _BackendOptions(backend)
-
-
-def _get_preferred_backends(domain: str) -> ContextVar[Tuple[_BackendOptions, ...]]:
-    if domain not in _preferred_backend:
-        _preferred_backend[domain] = ContextVar(
-            f"_preferred_backend[{domain}]", default=()
-        )
-    return _preferred_backend[domain]
-
-
-def _get_registered_backends(domain: str) -> Set[_BackendOptions]:
-    if domain not in _registered_backend:
-        _registered_backend[domain] = set()
-    return _registered_backend[domain]
-
-
-def _get_skipped_backends(domain: str) -> ContextVar[Set]:
-    if domain not in _skipped_backend:
-        _skipped_backend[domain] = ContextVar(
-            f"_skipped_backend[{domain}]", default=set()
-        )
-    return _skipped_backend[domain]
-
-
-_preferred_backend: Dict[str, ContextVar[Tuple[_BackendOptions, ...]]] = {}
-_registered_backend: Dict[str, Set[_BackendOptions]] = {}
-_skipped_backend: Dict[str, ContextVar[Set]] = {}
-
-
-@contextlib.contextmanager
 def set_backend(backend, *args, **kwargs):
     """
     A context manager that sets the preferred backend. Uses :obj:`BackendOptions` to create
     the options, and sets the backend with the preferred options.
+
     Parameters
     ----------
     backend
         The backend to set.
+
     See Also
     --------
     BackendOptions: The backend plus options.
     skip_backend: A context manager that allows skipping of backends.
     """
-    options = _BackendOptions(backend, *args, **kwargs)
-    pref = _get_preferred_backends(backend.__ua_domain__)
-    token = pref.set((options,) + pref.get())
-
-    try:
-        yield
-    finally:
-        pref.reset(token)
+    return _SetBackendContext(backend, *args, **kwargs)
 
 
-@contextlib.contextmanager
 def skip_backend(backend):
     """
     A context manager that allows one to skip a given backend from processing
     entirely. This allows one to use another backend's code in a library that
     is also a consumer of the same backend.
+
     Parameters
     ----------
     backend
         The backend to skip.
+
     See Also
     --------
     set_backend: A context manager that allows setting of backends.
     """
-    skip = _get_skipped_backends(backend.__ua_domain__)
-    new = set(skip.get())
-    new.add(backend)
-    token = skip.set(new)
-
-    try:
-        yield
-    finally:
-        skip.reset(token)
+    return _SkipBackendContext(backend)
 
 
 def get_defaults(f):
@@ -318,7 +208,7 @@ def get_defaults(f):
             arg_defaults.append(v.default)
         opts.add(k)
 
-    return kw_defaults, arg_defaults, opts
+    return kw_defaults, tuple(arg_defaults), opts
 
 
 def set_global_backend(backend):
@@ -327,18 +217,21 @@ def set_global_backend(backend):
     will be tried in the list of backends automatically, unless the
     ``only`` flag is set on a backend. This will be the first tried
     backend outside the :obj:`set_backend` context manager.
+
     Note that this method is not thread-safe.
+
     .. warning::
         We caution library authors against using this function in
         their code. We do *not* support this use-case. This function
         is meant to be used only by users themselves, or by a reference
         implementation, if one exists.
+
     Parameters
     ----------
     backend
         The backend to register.
     """
-    _backends[backend.__ua_domain__] = backend
+    _uarray.set_global_backend(backend)
 
 
 def register_backend(backend):
@@ -346,33 +239,41 @@ def register_backend(backend):
     This utility method sets registers backend for permanent use. It
     will be tried in the list of backends automatically, unless the
     ``only`` flag is set on a backend.
+
     Note that this method is not thread-safe.
+
     Parameters
     ----------
     backend
         The backend to register.
     """
-    _get_registered_backends(backend.__ua_domain__).add(backend)
+    _uarray.register_backend(backend)
 
 
 class Dispatchable:
     """
     A utility class which marks an argument with a specific dispatch type.
+
+
     Attributes
     ----------
     value
         The value of the Dispatchable.
+
     type
         The type of the Dispatchable.
+
     Examples
     --------
     >>> x = Dispatchable(1, str)
     >>> x
     <Dispatchable: type=<class 'str'>, value=1>
+
     See Also
     --------
     all_of_type
         Marks all unmarked parameters of a function.
+
     mark_as
         Allows one to create a utility function to mark as a given type.
     """
@@ -387,7 +288,9 @@ class Dispatchable:
 
     def __str__(self):
         return (
-            f"<{type(self).__name__}: type={repr(self.type)}, value={repr(self.value)}>"
+            "<{0}: type={1!r}, value={2!r}>".format(
+                type(self).__name__, self.type, self.value
+            )
         )
 
     __repr__ = __str__
@@ -396,6 +299,7 @@ class Dispatchable:
 def mark_as(dispatch_type):
     """
     Creates a utility function to mark something as a specific type.
+
     Examples
     --------
     >>> mark_int = mark_as(int)
@@ -408,6 +312,7 @@ def mark_as(dispatch_type):
 def all_of_type(arg_type):
     """
     Marks all unmarked arguments as a given type.
+
     Examples
     --------
     >>> @all_of_type(str)
@@ -438,6 +343,7 @@ def wrap_single_convertor(convert_single):
     Wraps a ``__ua_convert__`` defined for a single element to all elements.
     If any of them return ``NotImplemented``, the operation is assumed to be
     undefined.
+
     Accepts a signature of (value, type, coerce).
     """
 
--- scipy/_lib/uarray/setup.py
+++ scipy/_lib/uarray/setup.py
@@ -1,8 +1,27 @@
+
+def pre_build_hook(build_ext, ext):
+    from scipy._build_utils.compiler_helper import (get_cxx_std_flag,
+                                                    try_add_flag)
+    cc = build_ext._cxx_compiler
+    args = ext.extra_compile_args
+
+    std_flag = get_cxx_std_flag(cc)
+    if std_flag is not None:
+        args.append(std_flag)
+
+    try_add_flag(args, cc, '-fvisibility=hidden')
+
+
 def configuration(parent_package='', top_path=None):
     from numpy.distutils.misc_util import Configuration
 
     config = Configuration('uarray', parent_package, top_path)
     config.add_data_files('LICENSE')
+    ext = config.add_extension('_uarray',
+                               sources=['_uarray_dispatch.cxx'],
+                               language='c++')
+    ext._pre_build_hook = pre_build_hook
+    config.add_data_dir('tests')
     return config
 
 
