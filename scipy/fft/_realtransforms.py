3481659fa9deafe6664ac08810483f63ed1f8a86
Peter Bell <peterbell10@live.co.uk>
2019-07-10 14:00:47 +0100

    FIX: wrapping of real transform functions

--- scipy/fft/_realtransforms.py
+++ scipy/fft/_realtransforms.py
@@ -2,36 +2,62 @@ import scipy.fftpack as _fftpack
 from ._basic import _dispatch
 from scipy._lib.uarray import Dispatchable
 import numpy as np
+import functools
 
 __all__ = ['dct', 'idct', 'dst', 'idst', 'dctn', 'idctn', 'dstn', 'idstn']
 
-def _doc_wrap(transform_func, new_func):
-    doc = transform_func.__doc__ or ''
-    new_func.__doc__ = doc.replace('fftpack', 'fft')
-    new_func.__name__ = transform_func.__name__
-    return new_func
+def _inherit_doc(fftpack_fun):
+    def inner(fft_fun):
+        doc = fftpack_fun.__doc__ or ''
+        fft_fun.__doc__ = doc.replace('fftpack', 'fft')
+        return fft_fun
+
+    return inner
 
 
-def _doc_wrap_1d(transform_func):
-    @_dispatch
-    def inner(x, type=2, n=None, axis=-1, norm=None, overwrite_x=False):
-        return Dispatchable(x, np.ndarray),
-    return _doc_wrap(transform_func, inner)
+@_dispatch
+@_inherit_doc(_fftpack.dctn)
+def dctn(x, type=2, shape=None, axes=None, norm=None, overwrite_x=False):
+    return (Dispatchable(x, np.ndarray),)
 
 
-def _doc_wrap_nd(transform_func):
-    @_dispatch
-    def inner(x, type=2, shape=None, axes=None, norm=None, overwrite_x=False):
-        return Dispatchable(x, np.ndarray),
-    return _doc_wrap(transform_func, inner)
+@_dispatch
+@_inherit_doc(_fftpack.idctn)
+def idctn(x, type=2, shape=None, axes=None, norm=None, overwrite_x=False):
+    return (Dispatchable(x, np.ndarray),)
 
 
-dctn = _doc_wrap_nd(_fftpack.dctn)
-idctn = _doc_wrap_nd(_fftpack.idctn)
-dstn = _doc_wrap_nd(_fftpack.dstn)
-idstn = _doc_wrap_nd(_fftpack.idstn)
+@_dispatch
+@_inherit_doc(_fftpack.dstn)
+def dstn(x, type=2, shape=None, axes=None, norm=None, overwrite_x=False):
+    return (Dispatchable(x, np.ndarray),)
 
-dct = _doc_wrap_1d(_fftpack.dct)
-idct = _doc_wrap_1d(_fftpack.idct)
-dst = _doc_wrap_1d(_fftpack.dst)
-idst = _doc_wrap_1d(_fftpack.idst)
+
+@_dispatch
+@_inherit_doc(_fftpack.idstn)
+def idstn(x, type=2, shape=None, axes=None, norm=None, overwrite_x=False):
+    return (Dispatchable(x, np.ndarray),)
+
+
+@_dispatch
+@_inherit_doc(_fftpack.dct)
+def dct(x, type=2, n=None, axis=-1, norm=None, overwrite_x=False):
+    return (Dispatchable(x, np.ndarray),)
+
+
+@_dispatch
+@_inherit_doc(_fftpack.idct)
+def idct(x, type=2, n=None, axis=-1, norm=None, overwrite_x=False):
+    return (Dispatchable(x, np.ndarray),)
+
+
+@_dispatch
+@_inherit_doc(_fftpack.dst)
+def dst(x, type=2, n=None, axis=-1, norm=None, overwrite_x=False):
+    return (Dispatchable(x, np.ndarray),)
+
+
+@_dispatch
+@_inherit_doc(_fftpack.idst)
+def idst(x, type=2, n=None, axis=-1, norm=None, overwrite_x=False):
+    return (Dispatchable(x, np.ndarray),)
