f83a9f58387fd39d1d79314f215d190e7382a742
Peter Bell <peterbell10@live.co.uk>
2019-07-11 19:31:42 +0100

    MAINT: Refactor _inherit_doc into doc_replacer and move to doccer

--- scipy/fft/_realtransforms.py
+++ scipy/fft/_realtransforms.py
@@ -1,63 +1,56 @@
 import scipy.fftpack as _fftpack
 from ._basic import _dispatch
 from scipy._lib.uarray import Dispatchable
+from scipy._lib.doccer import doc_replace
 import numpy as np
 import functools
 
 __all__ = ['dct', 'idct', 'dst', 'idst', 'dctn', 'idctn', 'dstn', 'idstn']
 
-def _inherit_doc(fftpack_fun):
-    def inner(fft_fun):
-        doc = fftpack_fun.__doc__ or ''
-        fft_fun.__doc__ = doc.replace('fftpack', 'fft')
-        return fft_fun
-
-    return inner
-
 
 @_dispatch
-@_inherit_doc(_fftpack.dctn)
+@doc_replace(_fftpack.dctn, 'fftpack', 'fft')
 def dctn(x, type=2, shape=None, axes=None, norm=None, overwrite_x=False):
     return (Dispatchable(x, np.ndarray),)
 
 
 @_dispatch
-@_inherit_doc(_fftpack.idctn)
+@doc_replace(_fftpack.idctn, 'fftpack', 'fft')
 def idctn(x, type=2, shape=None, axes=None, norm=None, overwrite_x=False):
     return (Dispatchable(x, np.ndarray),)
 
 
 @_dispatch
-@_inherit_doc(_fftpack.dstn)
+@doc_replace(_fftpack.dstn, 'fftpack', 'fft')
 def dstn(x, type=2, shape=None, axes=None, norm=None, overwrite_x=False):
     return (Dispatchable(x, np.ndarray),)
 
 
 @_dispatch
-@_inherit_doc(_fftpack.idstn)
+@doc_replace(_fftpack.idstn, 'fftpack', 'fft')
 def idstn(x, type=2, shape=None, axes=None, norm=None, overwrite_x=False):
     return (Dispatchable(x, np.ndarray),)
 
 
 @_dispatch
-@_inherit_doc(_fftpack.dct)
+@doc_replace(_fftpack.dct, 'fftpack', 'fft')
 def dct(x, type=2, n=None, axis=-1, norm=None, overwrite_x=False):
     return (Dispatchable(x, np.ndarray),)
 
 
 @_dispatch
-@_inherit_doc(_fftpack.idct)
+@doc_replace(_fftpack.idct, 'fftpack', 'fft')
 def idct(x, type=2, n=None, axis=-1, norm=None, overwrite_x=False):
     return (Dispatchable(x, np.ndarray),)
 
 
 @_dispatch
-@_inherit_doc(_fftpack.dst)
+@doc_replace(_fftpack.dst, 'fftpack', 'fft')
 def dst(x, type=2, n=None, axis=-1, norm=None, overwrite_x=False):
     return (Dispatchable(x, np.ndarray),)
 
 
 @_dispatch
-@_inherit_doc(_fftpack.idst)
+@doc_replace(_fftpack.idst, 'fftpack', 'fft')
 def idst(x, type=2, n=None, axis=-1, norm=None, overwrite_x=False):
     return (Dispatchable(x, np.ndarray),)
