5c7fadcf957048369fbeb697b01361eff81e8b0f
Sambit Panda <spanda3@jhu.edu>
2019-07-27 10:42:13 -0400

    ENH: Fix build errors with multiscale_graphcorr tutorial

--- doc/source/tutorial/stats/plots/mgc_plot1.py
+++ doc/source/tutorial/stats/plots/mgc_plot1.py
@@ -5,7 +5,7 @@ from scipy.stats import multiscale_graphcorr
 
 def sim_plot(x, y, sim_name):
     """Plot simulations."""
-    fig = plt.figure(figsize=(8,8))
+    fig = plt.figure(figsize=(8, 8))
     fig.suptitle(sim_name + " Simulation", fontsize=17)
     plt.scatter(x, y)
     plt.show()
@@ -16,6 +16,8 @@ x = np.array([0.09762701, 0.43037873, 0.20552675, 0.08976637,
               0.92732552, -0.23311696, 0.58345008, 0.05778984,
               0.13608912, 0.85119328, -0.85792788, -0.8257414,
               -0.95956321, 0.66523969, 0.5563135, 0.7400243])
+
+
 y = np.array([-0.75550809, 1.40576643, -0.04929934, -0.12927078,
               -0.77908808, 0.6805334, -0.9317745, 0.67717586,
               0.47959224, -0.03966571, 0.32804751, -0.53252625,
