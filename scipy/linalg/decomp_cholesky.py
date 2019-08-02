43a7e0ca18afd74951cddbfdf9fc27b0d8a72e02
tuxcell <josephgama@yahoo.com>
2019-08-02 19:49:24 +0200

    remove helper functions

--- scipy/linalg/decomp_cholesky.py
+++ scipy/linalg/decomp_cholesky.py
@@ -13,7 +13,7 @@ __all__ = ['cholesky', 'cho_factor', 'cho_solve', 'cholesky_banded',
 
 
 def _cholesky(a, lower=False, overwrite_a=False, clean=True,
-              check_finite=True, full_pivot=False, pivot_tol=-1):
+              check_finite=True):
     """Common code for cholesky() and cho_factor()."""
 
     a1 = asarray_chkfinite(a) if check_finite else asarray(a)
@@ -32,41 +32,19 @@ def _cholesky(a, lower=False, overwrite_a=False, clean=True,
     if a1.size == 0:
         return a1.copy(), lower
 
-    if not is_matrix_hermitian():
-        raise LinAlgError("Expected symmetric or hermitian matrix")
-
     overwrite_a = overwrite_a or _datacopied(a1, a)
+    potrf, = get_lapack_funcs(('potrf',), (a1,))
+    c, info = potrf(a1, lower=lower, overwrite_a=overwrite_a, clean=clean)
+    if info > 0:
+        raise LinAlgError("%d-th leading minor of the array is not positive "
+                          "definite" % info)
+    if info < 0:
+        raise ValueError('LAPACK reported an illegal value in {}-th argument'
+                         'on entry to "POTRF".'.format(-info))
+    return c, lower
 
-    # if the pivot flag is false, return the result
-    if not full_pivot:
-        potrf, = get_lapack_funcs(('potrf',), (a1,))
-        c, info = potrf(a1, lower=lower, overwrite_a=overwrite_a, clean=clean)
-        if info > 0:
-            raise LinAlgError("%d-th leading minor of the array is not positive "
-                              "definite" % info)
-        if info < 0:
-            raise ValueError('LAPACK reported an illegal value in {}-th argument'
-                             'on entry to "POTRF".'.format(-info))
-        return c, lower
-    else: # if the pivot flag is true, return the result plus rank and pivot
 
-        pstrf, = get_lapack_funcs(('pstrf',), (a1,))
-        c, pivot, rank, info=pstrf(a1, lower=False, overwrite_a=False, clean=True, tol=pivot_tol)
-
-        if info > 0:
-            if rank == 0:
-                raise LinAlgError("%d-th leading minor of the array is not positive "
-                              "semidefinite" % info)
-            else:
-                raise LinAlgError("The array is rank deficient with "
-                              "computed rank %d" % info)
-
-        if info < 0:
-            raise ValueError('LAPACK reported an illegal value in {}-th argument'
-                         'on entry to "PSTRF".'.format(-info))
-        return c, lower, rank, pivot
-
-def cholesky(a, lower=False, overwrite_a=False, check_finite=True, full_pivot=False, pivot_tol=-1):
+def cholesky(a, lower=False, overwrite_a=False, check_finite=True):
     """
     Compute the Cholesky decomposition of a matrix.
 
@@ -86,10 +64,6 @@ def cholesky(a, lower=False, overwrite_a=False, check_finite=True, full_pivot=Fa
         Whether to check that the input matrix contains only finite numbers.
         Disabling may give a performance gain, but may result in problems
         (crashes, non-termination) if the inputs do contain infinities or NaNs.
-    full_pivot : bool, optional
-        Whether to use full pivot or not
-    pivot_tol : float, optional
-        Tolerance for the pivot, if < 0 then tolerance = N*U*MAX( A(M,M) )
 
     Returns
     -------
@@ -113,12 +87,9 @@ def cholesky(a, lower=False, overwrite_a=False, check_finite=True, full_pivot=Fa
            [ 0.+2.j,  5.+0.j]])
 
     """
-    if not full_pivot:
-        c,lower = _cholesky(a, lower=lower, overwrite_a=overwrite_a, clean=True, check_finite=check_finite)
-        return c
-    else:
-        c,lower, rank_bn, piv = _cholesky(a, lower=lower, overwrite_a=overwrite_a, clean=True, check_finite=check_finite, full_pivot=full_pivot, pivot_tol=pivot_tol)
-        return c, rank_bn, piv
+    c, lower = _cholesky(a, lower=lower, overwrite_a=overwrite_a, clean=True,
+                         check_finite=check_finite)
+    return c
 
 
 def cho_factor(a, lower=False, overwrite_a=False, check_finite=True):
