f1dcc7b2c0b04d494eb052fc045bb1b3987d056a
Gregory Lee <grlee77@gmail.com>
2019-08-04 05:40:29 -0400

    TST: add fftn/etc test cases using the s argument

--- scipy/fft/tests/test_numpy.py
+++ scipy/fft/tests/test_numpy.py
@@ -171,6 +171,24 @@ class TestFFT1D(object):
             tr_op = np.transpose(op(x, axes=a), a)
             assert_array_almost_equal(op_tr, tr_op)
 
+    @pytest.mark.parametrize("op", [fft.fft2, fft.ifft2,
+                                    fft.rfft2, fft.irfft2,
+                                    fft.hfft2, fft.ihfft2,
+                                    fft.fftn, fft.ifftn,
+                                    fft.rfftn, fft.irfftn,
+                                    fft.hfftn, fft.ihfftn])
+    def test_axes_subset_with_shape(self, op):
+        x = random((16, 8, 4))
+        axes = [(0, 1, 2), (0, 2, 1), (1, 2, 0)]
+        for a in axes:
+            # different shape on the first two axes
+            shape = tuple([2*x.shape[ax] if ax in a[:2] else x.shape[ax]
+                           for ax in range(x.ndim)])
+            # transform only the first two axes
+            op_tr = op(np.transpose(x, a), s=shape[:2], axes=(0, 1))
+            tr_op = np.transpose(op(x, s=shape[:2], axes=a[:2]), a)
+            assert_array_almost_equal(op_tr, tr_op)
+
     def test_all_1d_norm_preserving(self):
         # verify that round-trip transforms are norm-preserving
         x = random(30)
