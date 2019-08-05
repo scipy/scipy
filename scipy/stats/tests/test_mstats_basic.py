a9258eaa38f0a3b01be77469fcc68fa2c619178e
makbigc <makszechun@gmail.com>
2019-08-06 00:53:20 +0800

    remove the redundant line

--- scipy/stats/tests/test_mstats_basic.py
+++ scipy/stats/tests/test_mstats_basic.py
@@ -415,7 +415,6 @@ class TestTrimming(object):
         result = mstats.trimr(x, limits=(0.15, 0.14), inclusive=(False, False))
         expected = ma.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
                             mask=[1, 1, 0, 0, 0, 0, 0, 0, 0, 1])
-        pytest.set_trace()
         assert_equal(result, expected)
         assert_equal(result.mask, expected.mask)
 
