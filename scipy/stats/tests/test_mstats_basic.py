cea54dc1f7d44de843ae51eb314de70638ff8ad6
makbigc <makszechun@gmail.com>
2019-08-05 23:58:13 +0800

    Assert the equality of mask too

--- scipy/stats/tests/test_mstats_basic.py
+++ scipy/stats/tests/test_mstats_basic.py
@@ -413,8 +413,11 @@ class TestTrimming(object):
     def test_trimr(self):
         x = ma.arange(10)
         result = mstats.trimr(x, limits=(0.15, 0.14), inclusive=(False, False))
-        expected = [None, None, 2, 3, 4, 5, 6, 7, 8, None]
+        expected = ma.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+                            mask=[1, 1, 0, 0, 0, 0, 0, 0, 0, 1])
+        pytest.set_trace()
         assert_equal(result, expected)
+        assert_equal(result.mask, expected.mask)
 
     def test_trimmedmean(self):
         data = ma.array([77, 87, 88,114,151,210,219,246,253,262,
