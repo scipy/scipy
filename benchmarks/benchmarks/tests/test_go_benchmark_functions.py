4197be47a76a1b2f91fdaa6962dc7f05c537bf2b
Andrew Nelson <andyfaff@gmail.com>
2019-08-11 21:26:05 +1000

    BENCH: address gh8592 go_bench

--- benchmarks/benchmarks/tests/test_go_benchmark_functions.py
+++ benchmarks/benchmarks/tests/test_go_benchmark_functions.py
@@ -1,27 +1,25 @@
 """
 Unit tests for the global optimization benchmark functions
 """
-from __future__ import division, print_function, absolute_import
 import numpy as np
-from . import go_benchmark_functions as gbf
+from .. import go_benchmark_functions as gbf
 import inspect
-from numpy.testing import TestCase, run_module_suite, assert_
 
 
-class TestGoBenchmarkFunctions(TestCase):
+class TestGoBenchmarkFunctions(object):
 
-    def setUp(self):
+    def setup_method(self):
         bench_members = inspect.getmembers(gbf, inspect.isclass)
-        self.benchmark_functions = [item for item in bench_members if
-                                    issubclass(item[1], gbf.Benchmark)]
+        self.benchmark_functions = {it[0]:it[1] for it in bench_members if
+                                    issubclass(it[1], gbf.Benchmark)}
 
-    def tearDown(self):
+    def teardown_method(self):
         pass
 
     def test_optimum_solution(self):
         # Check that the function returns the global minimum if given
         # the optimal solution
-        for name, klass in self.benchmark_functions:
+        for name, klass in self.benchmark_functions.items():
             # LennardJones is filtered here because there are many global
             # optimima that give the same minimum energy
             if (name in ['Benchmark', 'LennardJones'] or
@@ -29,12 +27,20 @@ class TestGoBenchmarkFunctions(TestCase):
                 continue
 
             f = klass()
+
+            if name in ['Damavandi', 'Csendes']:
+                with np.errstate(divide='ignore', invalid='ignore'):
+                    print(name, f.fun(np.asarray(f.global_optimum[0])),
+                          f.fglob)
+                    assert np.isnan(f.fun(np.asarray(f.global_optimum[0])))
+                    continue
+
             print(name, f.fun(np.asarray(f.global_optimum[0])), f.fglob)
-            assert_(f.success(f.global_optimum[0]))
+            assert f.success(f.global_optimum[0])
 
     def test_solution_exists(self):
         # Every benchmark function should have a minimum energy
-        for name, klass in self.benchmark_functions:
+        for name, klass in self.benchmark_functions.items():
             if name == 'Benchmark':
                 continue
 
@@ -45,7 +51,7 @@ class TestGoBenchmarkFunctions(TestCase):
     def test_bounds_access_subscriptable(self):
         # In Python 2 zip returns a list which is subscriptable
         # In Python 3 zip returns a zip object, which is not subscriptable
-        for name, klass in self.benchmark_functions:
+        for name, klass in self.benchmark_functions.items():
             if (name == 'Benchmark' or name.startswith('Problem')):
                 continue
 
@@ -61,14 +67,10 @@ class TestGoBenchmarkFunctions(TestCase):
         # if we change the size of the problem then the initial vector has to
         # resize
         x0 = L.initial_vector()
-        assert_(len(x0) == 10)
+        assert len(x0) == 10
 
         # the bounds should be the correct length now.
         bounds = L.bounds
-        assert_(len(bounds) == 10)
+        assert len(bounds) == 10
 
-        assert_(L.N == 10)
-
-
-if __name__ == '__main__':
-    run_module_suite()
+        assert L.N == 10
