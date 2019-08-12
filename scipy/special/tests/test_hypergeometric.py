ead61659b4c2cb71494fdaf49f26f871f3ef69a5
Josh Wilson <person142@users.noreply.github.com>
2019-08-11 17:47:25 -0700

    TST: special: parametrize `hyp1f1` tests for better error reporting

--- scipy/special/tests/test_hypergeometric.py
+++ scipy/special/tests/test_hypergeometric.py
@@ -1,3 +1,4 @@
+import pytest
 import numpy as np
 from numpy.testing import assert_allclose
 from numpy.testing import assert_equal
@@ -21,25 +22,28 @@ class TestHyperu:
 
 class TestHyp1f1:
 
-    def test_special_cases(self):
-        assert all(np.isnan([
-            sc.hyp1f1(np.nan, 1, 1),
-            sc.hyp1f1(1, np.nan, 1),
-            sc.hyp1f1(1, 1, np.nan)
-        ]))
+    @pytest.mark.parametrize('a, b, x', [
+        (np.nan, 1, 1),
+        (1, np.nan, 1),
+        (1, 1, np.nan)
+    ])
+    def test_nan_inputs(self, a, b, x):
+        assert np.isnan(sc.hyp1f1(a, b, x))
+
+    def test_poles(self):
         assert_equal(sc.hyp1f1(1, [0, -1, -2, -3, -4], 0.5), np.infty)
 
+    @pytest.mark.parametrize('a, b, x, result', [
+        (-1, 1, 0.5, 0.5),
+        (1, 1, 0.5, 1.6487212707001281468),
+        (2, 1, 0.5, 2.4730819060501922203),
+        (1, 2, 0.5, 1.2974425414002562937),
+        (-10, 1, 0.5, -0.38937441413785204475)
+    ])
+    def test_special_cases(self, a, b, x, result):
         # Hit all the special case branches at the beginning of the
         # function. Desired answers computed using Mpmath.
-        points = (
-            ((-1, 1, 0.5), 0.5),
-            ((1, 1, 0.5), 1.6487212707001281468),
-            ((2, 1, 0.5), 2.4730819060501922203),
-            ((1, 2, 0.5), 1.2974425414002562937),
-            ((-10, 1, 0.5), -0.38937441413785204475)
-        )
-        for point, result in points:
-            assert_allclose(sc.hyp1f1(*point), result, atol=0, rtol=1e-15)
+        assert_allclose(sc.hyp1f1(a, b, x), result, atol=0, rtol=1e-15)
 
     def test_gh_3492(self):
         desired = 0.99973683897677527773  # Computed using Mpmath
