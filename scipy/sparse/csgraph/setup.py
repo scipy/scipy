6bd9cb57d1d6436830ad29279742edec7e44466e
Søren Fuglede Jørgensen <s@fuglede.dk>
2019-08-25 23:06:02 +0200

    MAINT: Move _max_flow to _flow to simplify a few natural generalizations
    The logic behind this move is that future iterations on the
    functionality provided by the module might well go beyond just computing
    maximum flow; low-hanging fruits would include minimum cost flow and
    minimum cost maximum flow, both of which would have a natural home in
    the same module as the existing functionality, but both of which would
    renders its name rather awkward.

--- scipy/sparse/csgraph/setup.py
+++ scipy/sparse/csgraph/setup.py
@@ -21,8 +21,8 @@ def configuration(parent_package='', top_path=None):
          sources=['_min_spanning_tree.c'],
          include_dirs=[numpy.get_include()])
     
-    config.add_extension('_max_flow',
-         sources=['_max_flow.c'],
+    config.add_extension('_flow',
+         sources=['_flow.c'],
          include_dirs=[numpy.get_include()])
     
     config.add_extension('_reordering',
