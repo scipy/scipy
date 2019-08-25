5c584f68f8da432141aed250a0fc9780e5817d2e
michaelmarien <marien.mich@gmail.com>
2019-08-25 17:15:04 +0200

    ENH: minor change to message output

--- scipy/sparse/linalg/isolve/minres.py
+++ scipy/sparse/linalg/isolve/minres.py
@@ -82,7 +82,7 @@ def minres(A, b, x0=None, shift=0.0, tol=1e-5, maxiter=None,
         maxiter = 5 * n
 
     msg = [' beta2 = 0.  If M = I, b and x are eigenvectors    ',   # -1
-            ' beta1 = 0.  The exact solution is  x = 0          ',   # 0
+            ' beta1 = 0.  The exact solution is x0          ',   # 0
             ' A solution to Ax = b was found, given rtol        ',   # 1
             ' A least-squares solution was found, given rtol    ',   # 2
             ' Reasonable accuracy achieved, given eps           ',   # 3
