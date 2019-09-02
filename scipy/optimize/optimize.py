07d7e11127e0b094d29a037c19dc7384b40678e0
Andrew Nelson <andyfaff@gmail.com>
2019-09-02 20:15:51 +1000

    MAINT: approx_hess_p, swap eval order. ScalarFunction cache in NewtonCG may enable one less fprime calculation.

--- scipy/optimize/optimize.py
+++ scipy/optimize/optimize.py
@@ -879,8 +879,9 @@ def check_grad(func, grad, x0, *args, **kwargs):
 
 
 def approx_fhess_p(x0, p, fprime, epsilon, *args):
-    f2 = fprime(*((x0 + epsilon*p,) + args))
+    # calculate fprime(x0) first, as this may be cached by ScalarFunction
     f1 = fprime(*((x0,) + args))
+    f2 = fprime(*((x0 + epsilon*p,) + args))
     return (f2 - f1) / epsilon
 
 
