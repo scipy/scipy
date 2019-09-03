1a69379b405356a26b3c56b1a3b6e6d9d49aedcb
Matt Haberland <mhaberla@calpoly.edu>
2019-09-03 05:20:41 -0700

    BENCH: optimize: added more Netlib benchmarks; accuracy checks now in teardown

--- benchmarks/benchmarks/optimize_linprog.py
+++ benchmarks/benchmarks/optimize_linprog.py
@@ -16,8 +16,22 @@ except ImportError:
 
 from .common import Benchmark
 
-methods = ["revised simplex", "interior-point"]
-problems = ["AFIRO", "BLEND"]
+methods = [("interior-point", {"sparse": True}),
+           ("interior-point", {"sparse": False}),
+           ("revised simplex", {})]
+problems = ['25FV47', '80BAU3B', 'ADLITTLE', 'AFIRO', 'AGG', 'AGG2', 'AGG3',
+            'BANDM', 'BEACONFD', 'BLEND', 'BNL1', 'BNL2', 'BORE3D', 'BRANDY',
+            'CAPRI', 'CYCLE', 'CZPROB', 'D6CUBE', 'DEGEN2', 'DEGEN3', 'E226',
+            'ETAMACRO', 'FFFFF800', 'FINNIS', 'FIT1D', 'FIT1P', 'GANGES',
+            'GFRD-PNC', 'GROW15', 'GROW22', 'GROW7', 'ISRAEL', 'KB2', 'LOTFI',
+            'MAROS', 'MODSZK1', 'PEROLD', 'PILOT', 'PILOT-WE', 'PILOT4',
+            'PILOTNOV', 'QAP8', 'RECIPE', 'SC105', 'SC205', 'SC50A', 'SC50B',
+            'SCAGR25', 'SCAGR7', 'SCFXM1', 'SCFXM2', 'SCFXM3', 'SCORPION',
+            'SCRS8', 'SCSD1', 'SCSD6', 'SCSD8', 'SCTAP1', 'SCTAP2', 'SCTAP3',
+            'SHARE1B', 'SHARE2B', 'SHELL', 'SHIP04L', 'SHIP04S', 'SHIP08L',
+            'SHIP08S', 'SHIP12L', 'SHIP12S', 'SIERRA', 'STAIR', 'STANDATA',
+            'STANDMPS', 'STOCFOR1', 'STOCFOR2', 'TRUSS', 'TUFF', 'VTP-BASE',
+            'WOOD1P', 'WOODW']
 
 
 def klee_minty(D):
@@ -46,12 +60,16 @@ class MagicSquare(Benchmark):
         self.A_eq, self.b_eq, self.c, numbers = magic_square(dims)
 
     def time_magic_square(self, meth, prob):
-        dims, obj = prob
+        method, options = meth
         with suppress_warnings() as sup:
             sup.filter(OptimizeWarning, "A_eq does not appear")
             res = linprog(c=self.c, A_eq=self.A_eq, b_eq=self.b_eq,
-                          bounds=(0, 1), method=meth)
-            np.testing.assert_allclose(obj, res.fun, rtol=1e-6, atol=1e-3)
+                          bounds=(0, 1), method=method, options=options)
+            self.fun = res.fun
+
+    def teardown(self, meth, prob):
+        dims, obj = prob
+        np.testing.assert_allclose(obj, self.fun, rtol=1e-6, atol=1e-3)
 
 
 class KleeMinty(Benchmark):
@@ -64,12 +82,17 @@ class KleeMinty(Benchmark):
 
     def setup(self, meth, dims):
         self.c, self.A_ub, self.b_ub, self.xf, self.obj = klee_minty(dims)
-        self.meth = meth
 
     def time_klee_minty(self, meth, dims):
-        res = linprog(c=self.c, A_ub=self.A_ub, b_ub=self.b_ub, method=self.meth)
-        np.testing.assert_allclose(self.obj, res.fun, rtol=1e-6, atol=1e-3)
-        np.testing.assert_allclose(self.xf, res.x, rtol=1e-6, atol=1e-3)
+        method, options = meth
+        res = linprog(c=self.c, A_ub=self.A_ub, b_ub=self.b_ub,
+                      method=method, options=options)
+        self.fun = res.fun
+        self.x = res.x
+
+    def teardown(self, meth, prob):
+        np.testing.assert_allclose(self.obj, self.fun, rtol=1e-6, atol=1e-3)
+        np.testing.assert_allclose(self.xf, self.x, rtol=1e-6, atol=1e-3)
 
 
 class LpGen(Benchmark):
@@ -82,12 +105,13 @@ class LpGen(Benchmark):
 
     def setup(self, meth, m, n):
         self.A, self.b, self.c = lpgen_2d(m, n)
-        self.meth = meth
 
     def time_lpgen(self, meth, m, n):
+        method, options = meth
         with suppress_warnings() as sup:
             sup.filter(RuntimeWarning, "scipy.linalg.solve\nIll-conditioned")
-            linprog(c=self.c, A_ub=self.A, b_ub=self.b, method=self.meth)
+            linprog(c=self.c, A_ub=self.A, b_ub=self.b,
+                    method=method, options=options)
 
 
 class Netlib(Benchmark):
@@ -99,21 +123,27 @@ class Netlib(Benchmark):
 
     def setup(self, meth, prob):
         dir_path = os.path.dirname(os.path.realpath(__file__))
-        data = np.load(dir_path + "/linprog_benchmark_files/" + prob + ".npz")
+        data = np.load(dir_path + "/linprog_benchmark_files/" + prob + ".npz",
+                       allow_pickle=True)
         self.c = data["c"]
         self.A_eq = data["A_eq"]
         self.A_ub = data["A_ub"]
         self.b_ub = data["b_ub"]
         self.b_eq = data["b_eq"]
-        self.bounds = (0, None)
+        self.bounds = np.squeeze(data["bounds"])
         self.obj = float(data["obj"].flatten()[0])
 
     def time_netlib(self, meth, prob):
+        method, options = meth
         res = linprog(c=self.c,
                       A_ub=self.A_ub,
                       b_ub=self.b_ub,
                       A_eq=self.A_eq,
                       b_eq=self.b_eq,
                       bounds=self.bounds,
-                      method=meth)
-        np.testing.assert_allclose(self.obj, res.fun)
+                      method=method,
+                      options=options)
+        self.fun = res.fun
+
+    def teardown(self, meth, prob):
+        np.testing.assert_allclose(self.obj, self.fun, rtol=1e-4, atol=1e-4)
