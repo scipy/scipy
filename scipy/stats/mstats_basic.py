5daff5ce54f6943a48cc10d73b028f790e1525cc
Miguel de Val-Borro <miguel.deval@gmail.com>
2019-09-27 01:15:36 -0400

    MAINT: Update R project URL

--- scipy/stats/mstats_basic.py
+++ scipy/stats/mstats_basic.py
@@ -2574,7 +2574,7 @@ def mquantiles(a, prob=list([.25,.5,.75]), alphap=.4, betap=.4, axis=None,
 
     References
     ----------
-    .. [1] *R* statistical software: http://www.r-project.org/
+    .. [1] *R* statistical software: https://www.r-project.org/
     .. [2] *R* ``quantile`` function:
             http://stat.ethz.ch/R-manual/R-devel/library/stats/html/quantile.html
 
