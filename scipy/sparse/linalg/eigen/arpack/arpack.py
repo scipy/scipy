1929293372a1c3eace8e596f2f5025981cb9dcdf
lobpcg <42650045+lobpcg@users.noreply.github.com>
2019-09-29 00:49:23 -0400

    pylint spacing

--- scipy/sparse/linalg/eigen/arpack/arpack.py
+++ scipy/sparse/linalg/eigen/arpack/arpack.py
@@ -1842,11 +1842,11 @@ def svds(A, k=6, ncv=None, tol=0, which='LM', v0=None,
             X = np.random.RandomState(52).randn(min(A.shape), k)
 
         eigvals, eigvec = lobpcg(XH_X, X, tol=tol ** 2, maxiter=maxiter,
-                          largest=largest)
+                                 largest=largest)
 
     elif solver == 'arpack' or solver == None:
         eigvals, eigvec = eigsh(XH_X, k=k, tol=tol ** 2, maxiter=maxiter,
-                                      ncv=ncv, which=which, v0=v0)
+                                ncv=ncv, which=which, v0=v0)
 
     else:
         raise ValueError("solver must be either 'arpack', or 'lobpcg'.")
