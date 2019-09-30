f042bc02b73ed36782f6dc132d315f5067417d3c
lobpcg <42650045+lobpcg@users.noreply.github.com>
2019-09-30 01:33:43 -0400

    typo fixed

--- scipy/sparse/linalg/eigen/lobpcg/tests/test_lobpcg.py
+++ scipy/sparse/linalg/eigen/lobpcg/tests/test_lobpcg.py
@@ -313,7 +313,7 @@ def test_maxit_None():
     X = np.random.randn(n, m)
     X=X.astype(np.float32)
     _, _, l_h = lobpcg(A, X, tol=1e-8, maxiter=20, retLambdaHistory=True)
-    assert_allclose((np.shape(l_h)[0], 20+2, atol=0)
+    assert_allclose((np.shape(l_h)[0], 20+2, atol=0))
 
 
 @pytest.mark.slow
