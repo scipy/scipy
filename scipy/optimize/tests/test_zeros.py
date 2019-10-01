f46f55a0710efd0bb332dec12e78e716e3151c57
Joseph Weston <joseph@weston.cloud>
2019-10-01 13:17:04 +0200

    TST: add tests that fail when root finders are passed lru-cached functions

--- scipy/optimize/tests/test_zeros.py
+++ scipy/optimize/tests/test_zeros.py
@@ -2,6 +2,7 @@ from __future__ import division, print_function, absolute_import
 import pytest
 
 from math import sqrt, exp, sin, cos
+from functools import lru_cache
 
 from numpy.testing import (assert_warns, assert_,
                            assert_allclose,
@@ -54,6 +55,12 @@ def f2_2(x):
     return exp(x) + cos(x)
 
 
+# lru cached function
+@lru_cache()
+def f_lrucached(x):
+    return x
+
+
 class TestBasic(object):
 
     def run_check_by_name(self, name, smoothness=0, **kwargs):
@@ -82,6 +89,15 @@ class TestBasic(object):
             assert_(r.converged)
             assert_allclose(zero, 1.0, atol=xtol, rtol=rtol,
                             err_msg='method %s, function %s' % (name, fname))
+
+    def run_check_lru_cached(self, method, name):
+        # check that https://github.com/scipy/scipy/issues/10846 is fixed
+        a = -1
+        b = 1
+        zero, r = method(f_lrucached, a, b, full_output=True)
+        assert_(r.converged)
+        assert_allclose(zero, 0,
+                        err_msg='method %s, function %s' % (name, 'f_lrucached'))
 
     def _run_one_test(self, tc, method, sig_args_keys=None,
                       sig_kwargs_keys=None, **kwargs):
@@ -172,16 +188,19 @@ class TestBasic(object):
 
     def test_bisect(self):
         self.run_check(zeros.bisect, 'bisect')
+        self.run_check_lru_cached(zeros.bisect, 'bisect')
         self.run_check_by_name('bisect')
         self.run_collection('aps', zeros.bisect, 'bisect', smoothness=1)
 
     def test_ridder(self):
         self.run_check(zeros.ridder, 'ridder')
+        self.run_check_lru_cached(zeros.ridder, 'ridder')
         self.run_check_by_name('ridder')
         self.run_collection('aps', zeros.ridder, 'ridder', smoothness=1)
 
     def test_brentq(self):
         self.run_check(zeros.brentq, 'brentq')
+        self.run_check_lru_cached(zeros.brentq, 'brentq')
         self.run_check_by_name('brentq')
         # Brentq/h needs a lower tolerance to be specified
         self.run_collection('aps', zeros.brentq, 'brentq', smoothness=1,
@@ -189,12 +208,14 @@ class TestBasic(object):
 
     def test_brenth(self):
         self.run_check(zeros.brenth, 'brenth')
+        self.run_check_lru_cached(zeros.brenth, 'brenth')
         self.run_check_by_name('brenth')
         self.run_collection('aps', zeros.brenth, 'brenth', smoothness=1,
                             xtol=1e-14, rtol=1e-14)
 
     def test_toms748(self):
         self.run_check(zeros.toms748, 'toms748')
+        self.run_check_lru_cached(zeros.toms748, 'toms748')
         self.run_check_by_name('toms748')
         self.run_collection('aps', zeros.toms748, 'toms748', smoothness=1)
 
