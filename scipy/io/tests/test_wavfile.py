1e75c3d9631ed97c7af140971744724036b59b0e
Stephen P. Cook <s.cook@seiche.com>
2019-10-14 14:51:27 +0100

    Use context manager version of pytest.raises

--- scipy/io/tests/test_wavfile.py
+++ scipy/io/tests/test_wavfile.py
@@ -95,7 +95,8 @@ def test_read_early_eof_with_data():
 def test_read_early_eof():
     for mmap in [False, True]:
         with open(datafile('test-44100Hz-le-1ch-4bytes-early-eof-no-data.wav'), 'rb') as fp:
-            assert_raises(ValueError, wavfile.read, fp, mmap=mmap)
+            with assert_raises(ValueError, match="Unexpected end of file."):
+                wavfile.read(fp, mmap=mmap)
 
 
 def test_read_incomplete_chunk():
