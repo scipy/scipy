68ac5178ddf55c6fb5d243c5b4d2357e15f756d3
Andrew Nelson <andyfaff@gmail.com>
2019-10-19 11:20:04 +1100

    PEP8

--- scipy/optimize/_trustregion_constr/tests/test_report.py
+++ scipy/optimize/_trustregion_constr/tests/test_report.py
@@ -9,8 +9,8 @@ from scipy.optimize import minimize, Bounds
 def test_gh10880():
     # checks that verbose reporting works with trust-constr
     bnds = Bounds(1, 2)
-    opts = {'maxiter' : 1000, 'verbose' : 2}
+    opts = {'maxiter': 1000, 'verbose': 2}
     res = minimize(lambda x: x**2, x0=2., method='trust-constr', bounds=bnds, options=opts)
 
-    opts = {'maxiter' : 1000, 'verbose' : 3}
+    opts = {'maxiter': 1000, 'verbose': 3}
     res = minimize(lambda x: x**2, x0=2., method='trust-constr', bounds=bnds, options=opts)
