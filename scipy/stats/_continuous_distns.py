aeaa300188bee16ac6b8e22dc44c11cddc52b341
Scott Sievert <dev@stsievert.com>
2019-10-26 19:20:50 -0500

    doctest: +SKIP

--- scipy/stats/_continuous_distns.py
+++ scipy/stats/_continuous_distns.py
@@ -6113,7 +6113,7 @@ class reciprocal_gen(rv_continuous):
     >>> ax.set_ylabel("Frequency")
     >>> ax.set_xlabel("Value of random variable")
     >>> ax.xaxis.set_major_locator(plt.FixedLocator([0, 1, 2]))
-    >>> ax.set_xticklabels(["$10^{}$".format(i) for i in [0, 1, 2]])
+    >>> ax.set_xticklabels(["$10^{}$".format(i) for i in [0, 1, 2]])  # doctest: +SKIP
     >>> plt.show()
 
     %(example)s
