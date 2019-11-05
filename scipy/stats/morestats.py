a4484039baccd75abdcbe01e328c1f50e9a4cc5d
brandondavid <brandon.david@zoho.com>
2019-11-05 05:38:53 -0800

    Resolve numpydoc PR09 error

--- scipy/stats/morestats.py
+++ scipy/stats/morestats.py
@@ -2369,11 +2369,11 @@ def binom_test(x, n=None, p=0.5, alternative='two-sided'):
         The number of successes, or if x has length 2, it is the
         number of successes and the number of failures.
     n : int
-        the number of trials.  This is ignored if x gives both the
-        number of successes and failures
+        The number of trials.  This is ignored if x gives both the
+        number of successes and failures.
     p : float, optional
-        The hypothesized probability of success.  0 <= p <= 1. The
-        default value is p = 0.5
+        The hypothesized probability of success.  `0 <= p <= 1`. The
+        default value is `p = 0.5`.
     alternative : {'two-sided', 'greater', 'less'}, optional
         Indicates the alternative hypothesis. The default value is
         'two-sided'.
