752018399c73fdfa5ee88c3e55e6e62df358e88e
mkg33 <maja.k.gwozdz@gmail.com>
2019-12-01 19:01:28 +0100

    Further revisions and mistake correction

--- scipy/spatial/__init__.py
+++ scipy/spatial/__init__.py
@@ -5,7 +5,7 @@ Spatial algorithms and data structures (:mod:`scipy.spatial`)
 
 .. currentmodule:: scipy.spatial
 
-Spatial Transformations
+Spatial transformations
 =======================
 
 These are contained in the `scipy.spatial.transform` submodule.
@@ -16,13 +16,13 @@ Nearest-neighbor queries
    :toctree: generated/
 
    KDTree      -- class for efficient nearest-neighbor queries
-   cKDTree     -- class for efficient nearest-neighbor queries (faster impl.)
+   cKDTree     -- class for efficient nearest-neighbor queries (faster implementation)
    Rectangle
 
 Distance metrics are contained in the :mod:`scipy.spatial.distance` submodule.
 
 Delaunay triangulation, convex hulls, and Voronoi diagrams
-=========================================================
+==========================================================
 
 .. autosummary::
    :toctree: generated/
@@ -41,29 +41,29 @@ Plotting helpers
 
    delaunay_plot_2d     -- plot 2-D triangulation
    convex_hull_plot_2d  -- plot 2-D convex hull
-   voronoi_plot_2d      -- plot 2-D voronoi diagram
+   voronoi_plot_2d      -- plot 2-D Voronoi diagram
 
 .. seealso:: :ref:`Tutorial <qhulltutorial>`
 
 
 Simplex representation
 ======================
-The simplices (triangles, tetrahedra, ...) appearing in the Delaunay
-tessellation (N-dim simplices), convex hull facets, and Voronoi ridges
-(N-1 dim simplices) are represented in the following scheme::
+The simplices (triangles, tetrahedra, etc.) appearing in the Delaunay
+tessellation (N-D simplices), convex hull facets, and Voronoi ridges
+(N-1-D simplices) are represented in the following scheme::
 
     tess = Delaunay(points)
     hull = ConvexHull(points)
     voro = Voronoi(points)
 
-    # coordinates of the j-th vertex of the i-th simplex
+    # coordinates of the jth vertex of the ith simplex
     tess.points[tess.simplices[i, j], :]        # tessellation element
     hull.points[hull.simplices[i, j], :]        # convex hull facet
     voro.vertices[voro.ridge_vertices[i, j], :] # ridge between Voronoi cells
 
 For Delaunay triangulations and convex hulls, the neighborhood
 structure of the simplices satisfies the condition:
-``tess.neighbors[i,j]`` is the neighboring simplex of the ``i``-th
+``tess.neighbors[i,j]`` is the neighboring simplex of the ith
 simplex, opposite to the ``j``-vertex. It is -1 in case of no neighbor.
 
 Convex hull facets also define a hyperplane equation::
@@ -71,7 +71,7 @@ Convex hull facets also define a hyperplane equation::
     (hull.equations[i,:-1] * coord).sum() + hull.equations[i,-1] == 0
 
 Similar hyperplane equations for the Delaunay triangulation correspond
-to the convex hull facets on the corresponding N+1 dimensional
+to the convex hull facets on the corresponding N+1-D
 paraboloid.
 
 The Delaunay triangulation objects offer a method for locating the
