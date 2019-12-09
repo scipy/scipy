ef0703d99ad93fee8897652f56083be9fb15b99e
Pauli Virtanen <pav@iki.fi>
2019-12-09 22:39:51 +0200

    ENH: linalg: build _fblas_64 library when using 64-bit BLAS

--- scipy/linalg/setup.py
+++ scipy/linalg/setup.py
@@ -1,14 +1,70 @@
 from __future__ import division, print_function, absolute_import
 
 import os
+import re
+import glob
 from os.path import join
+
+
+def patch_int64_pyf(filename, basename_suffix='_64'):
+    """
+    Patch .pyf files to ILP64 ABI.
+
+    Does the replacements::
+
+    - ``npy_int -> npy_int64``
+    - Appends `basename_suffix` to module name and file basename.
+    - Appends `basename_suffix` to include directives.
+
+    """
+    from scipy._build_utils import write_file_content
+
+    with open(filename, 'r') as f:
+        text = f.read()
+
+    text = re.sub(r"npy_int(?!\w)", "npy_int64", text, flags=re.I)
+    text = re.sub(r"python module ([a-z0-9_]+)",
+                  r"python module \1{}".format(basename_suffix),
+                  text, flags=re.I)
+    text = re.sub(r"include\s+'([a-z0-9_]+)(\.[a-z0-9.]+)'",
+                  r"include '\1{}\2'".format(basename_suffix),
+                  text, flags=re.I)
+
+    basename = os.path.basename(filename)
+    if basename.endswith('.pyf.src'):
+        base, ext = basename[:-8], basename[-8:]
+    else:
+        base, ext = os.path.splitext(basename)
+
+    ext_dir = os.path.relpath(os.path.dirname(filename), os.getcwd())
+    dst = os.path.join('build', 'build_src.common', ext_dir,
+                       base + basename_suffix + ext)
+
+    write_file_content(dst, text)
+
+    return os.path.relpath(dst, os.path.dirname(filename))
+
+
+def patch_int64_pyf_glob(filenames, basename_suffix='_64'):
+    new_filenames = []
+    for fn_glob in filenames:
+        fn_glob = os.path.join(os.path.dirname(__file__), fn_glob)
+        for fn in glob.glob(fn_glob):
+            if not (fn.endswith('.pyf') or fn.endswith('.pyf.src')):
+                new_filenames.append(fn)
+                continue
+
+            new_filenames.append(patch_int64_pyf(fn, basename_suffix))
+
+    return new_filenames
 
 
 def configuration(parent_package='', top_path=None):
     from distutils.sysconfig import get_python_inc
     from scipy._build_utils.system_info import get_info, NotFoundError, numpy_info
     from numpy.distutils.misc_util import Configuration, get_numpy_include_dirs
-    from scipy._build_utils import get_g77_abi_wrappers
+    from scipy._build_utils import (get_g77_abi_wrappers, uses_blas64,
+                                    blas_ilp64_pre_build_hook, get_f2py_int64_options)
 
     config = Configuration('linalg', parent_package, top_path)
 
@@ -19,6 +75,9 @@ def configuration(parent_package='', top_path=None):
     if atlas_version:
         print(('ATLAS version: %s' % atlas_version))
 
+    if uses_blas64():
+        lapack_ilp64_opt = get_info('lapack_ilp64_opt', 2)
+
     # fblas:
     sources = ['fblas.pyf.src']
     sources += get_g77_abi_wrappers(lapack_opt)
@@ -28,6 +87,14 @@ def configuration(parent_package='', top_path=None):
                          depends=['fblas_l?.pyf.src'],
                          extra_info=lapack_opt
                          )
+
+    if uses_blas64():
+        ext = config.add_extension('_fblas_64',
+                                   sources=patch_int64_pyf_glob(sources),
+                                   depends=patch_int64_pyf_glob(['fblas_l?.pyf.src']),
+                                   extra_info=lapack_ilp64_opt,
+                                   f2py_options=get_f2py_int64_options())
+        ext._pre_build_hook = blas_ilp64_pre_build_hook(lapack_ilp64_opt)
 
     # flapack:
     sources = ['flapack.pyf.src']
