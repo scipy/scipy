fd913211e34af0e4ea620ae04e46b37cb4bf98ec
Lucas Roberts <rlucas7@vt.edu>
2019-12-15 22:42:16 -0500

    DOC: fix typo in doc for SpearmanRConstantInput warning

--- scipy/stats/__init__.py
+++ scipy/stats/__init__.py
@@ -374,7 +374,7 @@ Warnings used in :mod:`scipy.stats`
 
    PearsonRConstantInputWarning
    PearsonRNearConstantInputWarning
-   SpearmanRRConstantInputWarning
+   SpearmanRConstantInputWarning
 
 For many more stat related functions install the software R and the
 interface package rpy.
