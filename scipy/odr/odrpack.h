d0b12a63de78eef1e12261ffbd16e2ae86b0da05
Pauli Virtanen <pav@iki.fi>
2019-12-23 13:18:43 +0200

    ENH: odr: support ILP64

--- scipy/odr/odrpack.h
+++ scipy/odr/odrpack.h
@@ -4,6 +4,38 @@
 
 #include "numpy/npy_3kcompat.h"
 
+#ifdef HAVE_BLAS_ILP64
+
+#define INT_TYPE npy_int64
+#define INT_TYPE_NPY NPY_INT64
+
+#if NPY_BITSOF_SHORT == 64
+#define INT_TYPE_PYFMT   "h"
+#define INT_TYPE_FMT "%h"
+#elif NPY_BITSOF_INT == 64
+#define INT_TYPE_PYFMT   "i"
+#define INT_TYPE_FMT "%d"
+#elif NPY_BITSOF_LONG == 64
+#define INT_TYPE_PYFMT   "l"
+#define INT_TYPE_FMT "%ld"
+#elif NPY_BITSOF_LONGLONG == 64
+#define INT_TYPE_PYFMT   "L"
+#define INT_TYPE_FMT "%lld"
+#else
+#error No compatible 64-bit integer size. \
+       Please contact NumPy maintainers and give detailed information about your \
+       compiler and platform, or set NPY_USE_BLAS64_=0
+#endif
+
+#else
+
+#define INT_TYPE int
+#define INT_TYPE_NPY NPY_INT
+#define INT_TYPE_FMT NPY_INT_FMT
+#define INT_TYPE_PYFMT   "i"
+
+#endif
+
 #if defined(NO_APPEND_FORTRAN)
 #if defined(UPPERCASE_FORTRAN)
 #define F_FUNC(f,F) F
@@ -40,14 +72,14 @@ static ODR_info odr_global;
 static PyObject *odr_error=NULL;
 static PyObject *odr_stop=NULL;
 
-void fcn_callback(int *n, int *m, int *np, int *nq, int *ldn, int *ldm,
-		  int *ldnp, double *beta, double *xplusd, int *ifixb,
-		  int *ifixx, int *ldfix, int *ideval, double *f,
-		  double *fjacb, double *fjacd, int *istop);
+void fcn_callback(INT_TYPE *n, INT_TYPE *m, INT_TYPE *np, INT_TYPE *nq, INT_TYPE *ldn, INT_TYPE *ldm,
+		  INT_TYPE *ldnp, double *beta, double *xplusd, INT_TYPE *ifixb,
+		  INT_TYPE *ifixx, INT_TYPE *ldfix, INT_TYPE *ideval, double *f,
+		  double *fjacb, double *fjacd, INT_TYPE *istop);
 
-PyObject *gen_output(int n, int m, int np, int nq, int ldwe, int ld2we,
+PyObject *gen_output(INT_TYPE n, INT_TYPE m, INT_TYPE np, INT_TYPE nq, INT_TYPE ldwe, INT_TYPE ld2we,
 		     PyArrayObject *beta, PyArrayObject *work, PyArrayObject *iwork,
-		     int isodr, int info, int full_output);
+		     INT_TYPE isodr, INT_TYPE info, int full_output);
 
 PyObject *odr(PyObject *self, PyObject *args, PyObject *kwds);
 
