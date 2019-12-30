7573a000a3e118db5c978d6b13f7c13b4902cce1
Pauli Virtanen <pav@iki.fi>
2019-12-30 15:44:04 +0200

    BLD: sparse.linalg/superlu: support ILP64 BLAS in SuperLU

--- scipy/sparse/linalg/dsolve/setup.py
+++ scipy/sparse/linalg/dsolve/setup.py
@@ -9,12 +9,11 @@ import glob
 def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
     from scipy._build_utils.system_info import get_info
-    from scipy._build_utils import numpy_nodepr_api
+    from scipy._build_utils import numpy_nodepr_api, combine_dict, uses_blas64
 
     config = Configuration('dsolve',parent_package,top_path)
     config.add_data_dir('tests')
 
-    lapack_opt = get_info('lapack_opt',notfound_action=2)
     if sys.platform == 'win32':
         superlu_defs = [('NO_TIMER',1)]
     else:
@@ -26,23 +25,35 @@ def configuration(parent_package='',top_path=None):
     sources = sorted(glob.glob(join(superlu_src, '*.c')))
     headers = list(glob.glob(join(superlu_src, '*.h')))
 
+    if uses_blas64():
+        lapack_opt = get_info('lapack_ilp64_opt',notfound_action=2)
+        lapack_opt = combine_dict(lapack_opt,
+                                  define_macros=[('BLASLAPACK6432_SUFFIX', 'superlu_blas_')])
+        sources.append(join(dirname(__file__), '..', '..', '..', '_build_utils',
+                            'src', 'blaslapack6432.c'))
+    else:
+        lapack_opt = get_info('lapack_opt',notfound_action=2)
+
+    cfg = combine_dict(lapack_opt, numpy_nodepr_api,
+                       define_macros=superlu_defs,
+                       include_dirs=[superlu_src,
+                                     join(dirname(__file__), 'SuperLU')])
+
     config.add_library('superlu_src',
                        sources=sources,
-                       macros=superlu_defs,
-                       include_dirs=[superlu_src],
-                       )
+                       macros=cfg['define_macros'],
+                       include_dirs=cfg['include_dirs'])
 
     # Extension
     ext_sources = ['_superlumodule.c',
                    '_superlu_utils.c',
                    '_superluobject.c']
 
+    cfg = combine_dict(cfg, libraries=['superlu_src'])
     config.add_extension('_superlu',
                          sources=ext_sources,
-                         libraries=['superlu_src'],
                          depends=(sources + headers),
-                         extra_info=lapack_opt,
-                         **numpy_nodepr_api
+                         **cfg
                          )
 
     # Add license files
