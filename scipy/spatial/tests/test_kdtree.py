49849676c8bf69b641952297ddc903db334c8123
Tyler Reddy <tyler.je.reddy@gmail.com>
2020-01-12 20:45:14 -0700

    Revert "Fix 5040"
    This reverts commit 9e08c44fa23c1e772b2290bc56da4d687bf64d63.

--- scipy/spatial/tests/test_kdtree.py
+++ scipy/spatial/tests/test_kdtree.py
@@ -1335,62 +1335,6 @@ def test_short_knn():
             [0., 0.01, np.inf, np.inf],
             [0., np.inf, np.inf, np.inf]])
 
-def test_query_ball_point_vector_r():
-
-    np.random.seed(1234)
-    data = np.random.normal(size=(100, 3))
-    query = np.random.normal(size=(100, 3))
-    tree = cKDTree(data)
-    d = np.random.uniform(0, 0.3, size=len(query))
-
-    rvector = tree.query_ball_point(query, d)
-    rscalar = [tree.query_ball_point(qi, di) for qi, di in zip(query, d)]
-    for a, b in zip(rvector, rscalar):
-        assert_array_equal(sorted(a), sorted(b))
-
-def test_query_ball_point_length():
-
-    np.random.seed(1234)
-    data = np.random.normal(size=(100, 3))
-    query = np.random.normal(size=(100, 3))
-    tree = cKDTree(data)
-    d = 0.3
-
-    length = tree.query_ball_point(query, d, return_length=True)
-    length2 = [len(ind) for ind in tree.query_ball_point(query, d, return_length=False)]
-    length3 = [len(tree.query_ball_point(qi, d)) for qi in query]
-    length4 = [tree.query_ball_point(qi, d, return_length=True) for qi in query]
-    assert_array_equal(length, length2)
-    assert_array_equal(length, length3)
-    assert_array_equal(length, length4)
-
-@pytest.mark.parametrize("balanced_tree, compact_nodes",
-    [(True, False),
-     (True, True),
-     (False, False),
-     (False, True)])
-def test_cdktree_empty_input(balanced_tree, compact_nodes):
-    # https://github.com/scipy/scipy/issues/5040
-    np.random.seed(1234)
-    empty_v3 = np.empty(shape=(0, 3))
-    query_v3 = np.ones(shape=(1, 3))
-    query_v2 = np.ones(shape=(2, 3))
-
-    tree = cKDTree(empty_v3, balanced_tree=balanced_tree, compact_nodes=compact_nodes)
-    length = tree.query_ball_point(query_v3, 0.3, return_length=True)
-    assert length == 0
-
-    dd, ii = tree.query(query_v2, 2)
-    assert ii.shape == (2, 2)
-    assert dd.shape == (2, 2)
-    assert np.isinf(dd).all()
-
-    N = tree.count_neighbors(tree, [0, 1])
-    assert_array_equal(N, [0, 0])
-
-    M = tree.sparse_distance_matrix(tree, 0.3)
-    assert M.shape == (0, 0)
-
 class Test_sorted_query_ball_point(object):
 
     def setup_method(self):
