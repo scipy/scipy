8c5ee76509e9c12e62896212c94251690116af07
Gregory Gundersen <ggundersen@gmail.com>
2020-01-24 17:40:11 -0500

    Multivariate t-distribution and associated unit tests.

--- scipy/stats/_multivariate.py
+++ scipy/stats/_multivariate.py
@@ -1,6 +1,8 @@
 #
 # Author: Joris Vankerschaver 2013
 #
+# w/ additions by Gregory Gundersen, January 2020
+#
 from __future__ import division, print_function, absolute_import
 
 import math
@@ -26,7 +28,8 @@ __all__ = ['multivariate_normal',
            'special_ortho_group',
            'ortho_group',
            'random_correlation',
-           'unitary_group']
+           'unitary_group',
+           'multivariate_t']
 
 _LOG_2PI = np.log(2 * np.pi)
 _LOG_2 = np.log(2)
@@ -3825,3 +3828,231 @@ class unitary_group_gen(multi_rv_generic):
 
 
 unitary_group = unitary_group_gen()
+
+
+class multivariate_t_gen(multi_rv_generic):
+
+    def __init__(self, seed=None):
+        """Initialize a multivariate t-distributed random variable.
+
+        Parameters
+        ----------
+        seed : Random state.
+        """
+        self._random_state = check_random_state(seed)
+
+    def __call__(self, mean=None, shape=1, df=1, seed=None):
+        """Create a frozen multivariate t-distribution. See
+        `multivariate_t_frozen` for parameters.
+        """
+        return multivariate_t_frozen(mean=mean, shape=shape, df=df, seed=seed)
+
+    def pdf(self, x, mean=None, shape=1, df=1):
+        """Multivariate t-distribution probability density function.
+
+        Parameters
+        ----------
+        x : array_like
+            Points at which to evaluate the log of the probability density
+            function.
+        mean : array_like, optional
+            Mean of the distribution (default zero).
+        shape : array_like, optional
+            Positive definite shape matrix. This is not the distribution's
+            covariance matrix (default one).
+        df : Degrees of freedom.
+
+        Returns
+        -------
+        pdf : Probability density function evaluated at `x`.
+        """
+        dim, mean, shape, df = self._process_parameters(mean, shape, df)
+        x = self._process_quantiles(x, dim)
+        shape_info = _PSD(shape)
+        logpdf = self._logpdf(x, mean, shape_info.U, shape_info.log_pdet, df,
+                              dim)
+        return np.exp(logpdf)
+
+    def logpdf(self, x, mean=None, shape=1, df=1):
+        """Log of the multivariate t-distribution probability density function.
+
+        Parameters
+        ----------
+        x : array_like
+            Points at which to evaluate the log of the probability density
+            function.
+        mean : array_like, optional
+            Mean of the distribution (default zero).
+        shape : array_like, optional
+            Positive definite shape matrix. This is not the distribution's
+            covariance matrix (default one).
+        df : Degrees of freedom.
+
+        Returns
+        -------
+        logpdf : Log of the probability density function evaluated at `x`.
+        """
+        dim, mean, shape, df = self._process_parameters(mean, shape, df)
+        x = self._process_quantiles(x, dim)
+        shape_info = _PSD(shape)
+        return self._logpdf(x, mean, shape_info.U, shape_info.log_pdet, df, dim)
+
+    def _logpdf(self, x, mean, U, log_pdet, df, dim):
+        """Utility method `pdf`, `logpdf` for parameters.
+        """
+        dev  = x - mean
+        maha = np.square(np.dot(dev, U)).sum(axis=-1)
+
+        t = 0.5 * (df + dim)
+        A = gammaln(t)
+        B = gammaln(0.5 * df)
+        C = dim/2. * np.log(df * np.pi)
+        D = 0.5 * log_pdet
+        E = -t * np.log(1 + (1./df) * maha)
+
+        return A - B - C - D + E
+
+    def rvs(self, mean=None, shape=1, df=1, size=1, random_state=None):
+        """Draw random samples from a multivariate t-distribution.
+
+        Parameters
+        ----------
+        size : integer, optional
+            Number of samples to draw (default 1).
+        random_state : np.random.RandomState instance
+            RandomState used for drawing the random variates.
+
+        Returns
+        -------
+        rvs : ndarray or scalar
+            Random variates of size (`size`, `P`), where `P` is the
+            dimension of the random variable.
+        """
+        # For implementation details, see equation (3):
+        #
+        #    Hofert, "On Sampling from the Multivariatet Distribution", 2013
+        #
+        dim, mean, shape, df = self._process_parameters(mean, shape, df)
+        if random_state is not None:
+            rng = check_random_state(random_state)
+        else:
+            rng = self._random_state
+
+        if df == np.inf:
+            x = np.ones(size)
+        else:
+            x = rng.chisquare(df, size=size) / df
+
+        z = rng.multivariate_normal(np.zeros(dim), shape, size=size)
+        samples = mean + z / np.sqrt(x)[:, None]
+        return samples
+
+    def _process_quantiles(self, x, dim):
+        """Adjust quantiles array so that last axis labels the components of
+        each data point.
+        """
+        x = np.asarray(x, dtype=float)
+        if x.ndim == 0:
+            x = x[np.newaxis]
+        elif x.ndim == 1:
+            if dim == 1:
+                x = x[:, np.newaxis]
+            else:
+                x = x[np.newaxis, :]
+        return x
+
+    def _process_parameters(self, mean, shape, df):
+        """Infer dimensionality from mean array and shape matrix, handle
+        defaults, and ensure compatible dimensions.
+        """
+        if mean is None and shape is None:
+            shape = np.asarray(1, dtype=float)
+            dim = 1
+        elif mean is None:
+            shape = np.asarray(shape, dtype=float)
+            if shape.ndim < 2:
+                dim = 1
+            else:
+                dim = shape.shape[0]
+            mean = np.zeros(dim)
+        else:
+            shape = np.asarray(shape, dtype=float)
+            mean = np.asarray(mean, dtype=float)
+            dim = mean.size
+
+        if dim == 1:
+            mean.shape = (1,)
+            shape.shape = (1, 1)
+
+        if mean.ndim != 1 or mean.shape[0] != dim:
+            raise ValueError("Array 'mean' must be a vector of length %d." %
+                             dim)
+        if shape.ndim == 0:
+            shape = shape * np.eye(dim)
+        elif shape.ndim == 1:
+            shape = np.diag(shape)
+        elif shape.ndim == 2 and shape.shape != (dim, dim):
+            rows, cols = shape.shape
+            if rows != cols:
+                msg = ("Array 'cov' must be square if it is two dimensional,"
+                       " but cov.shape = %s." % str(shape.shape))
+            else:
+                msg = ("Dimension mismatch: array 'cov' is of shape %s,"
+                       " but 'mean' is a vector of length %d.")
+                msg = msg % (str(shape.shape), len(mean))
+            raise ValueError(msg)
+        elif shape.ndim > 2:
+            raise ValueError("Array 'cov' must be at most two-dimensional,"
+                             " but cov.ndim = %d" % shape.ndim)
+
+        # Process degrees of freedom.
+        if df is None:
+            df = 1
+        if not isinstance(df, int) and not np.isinf(df):
+            raise ValueError("'df' must be an integer or 'np.inf' but is of "
+                             "type %s" % type(df))
+
+        return dim, mean, shape, df
+
+
+class multivariate_t_frozen(multi_rv_frozen):
+
+    def __init__(self, mean=None, shape=1, df=1, seed=None):
+        """
+        Create a frozen multivariate normal distribution.
+
+        Parameters
+        ----------
+        x : array_like
+            Points at which to evaluate the log of the probability density
+            function.
+        mean : array_like, optional
+            Mean of the distribution (default zero).
+        shape : array_like, optional
+            Positive definite shape matrix. This is not the distribution's
+            covariance matrix (default one).
+        df : Degrees of freedom.
+        """
+        self._dist = multivariate_t_gen(seed)
+        dim, mean, shape, df = self._dist._process_parameters(mean, shape, df)
+        self.dim, self.mean, self.shape, self.df = dim, mean, shape, df
+        self.shape_info = _PSD(shape)
+
+    def logpdf(self, x):
+        x = self._dist._process_quantiles(x, self.dim)
+        U = self.shape_info.U
+        log_pdet = self.shape_info.log_pdet
+        return self._dist._logpdf(x, self.mean, U, log_pdet, self.df, self.dim)
+
+    def pdf(self, x):
+        return np.exp(self.logpdf(x))
+
+    def rvs(self, size=1, random_state=None):
+        return self._dist.rvs(mean=self.mean,
+                              shape=self.shape,
+                              df=self.df,
+                              size=size,
+                              random_state=random_state)
+
+
+multivariate_t = multivariate_t_gen()
--- scipy/stats/tests/test_multivariate.py
+++ scipy/stats/tests/test_multivariate.py
@@ -29,6 +29,9 @@ from scipy.stats import wishart, multinomial, invwishart, chi2, invgamma
 from scipy.stats import norm, uniform
 from scipy.stats import ks_2samp, kstest
 from scipy.stats import binom
+from scipy.stats import multivariate_t
+from scipy.stats import cauchy
+from scipy.stats import normaltest
 
 from scipy.integrate import romb
 from scipy.special import multigammaln
@@ -1636,6 +1639,131 @@ class TestUnitaryGroup(object):
         x = np.arctan2(eigs.imag, eigs.real)
         res = kstest(x.ravel(), uniform(-np.pi, 2*np.pi).cdf)
         assert_(res.pvalue > 0.05)
+
+
+# These tests were created by running vpa(mvtpdf(...)) in MATLAB. The
+# function takes no `mu` parameter. The tests were run as
+#
+# >> ans = vpa(mvtpdf(x - mu, shape, df));
+#
+MULTIVARIATE_T_TESTS = [{
+    'x': [
+        [1, 2],
+        [4, 1],
+        [2, 1],
+        [2, 4],
+        [1, 4],
+        [4, 1],
+        [3, 2],
+        [3, 3],
+        [4, 4],
+        [5, 1],
+    ],
+    'mu': [0, 0],
+    'shape': [[1, 0], [0, 1]],
+    'df': 4,
+    'ans': [
+        0.013972450422333741737457302178882,
+        0.0010998721906793330026219646100571,
+        0.013972450422333741737457302178882,
+        0.00073682844024025606101402363634634,
+        0.0010998721906793330026219646100571,
+        0.0010998721906793330026219646100571,
+        0.0020732579600816823488240725481546,
+        0.00095660371505271429414668515889275,
+        0.00021831953784896498569831346792114,
+        0.00037725616140301147447000396084604
+    ]
+}, {
+    'x': [
+        [0.9718, 0.1298, 0.8134],
+        [0.4922, 0.5522, 0.7185],
+        [0.3010, 0.1491, 0.5008],
+        [0.5971, 0.2585, 0.8940],
+        [0.5434, 0.5287, 0.9507],
+    ],
+    'mu': [-1, 1, 50],
+    'shape': [
+        [1.0000, 0.5000, 0.2500],
+        [0.5000, 1.0000, -0.1000],
+        [0.2500, -0.1000, 1.0000],
+    ],
+    'df': 8,
+    'ans': [
+        0.00000000000000069609279697467772867405511133763,
+        0.00000000000000073700739052207366474839369535934,
+        0.00000000000000069522909962669171512174435447027,
+        0.00000000000000074212293557998314091880208889767,
+        0.00000000000000077039675154022118593323030449058,
+    ]
+}]
+
+
+class TestMultivariateT:
+
+    def test_pdf_correctness(self):
+        for t in MULTIVARIATE_T_TESTS:
+            val = multivariate_t.pdf(t['x'], t['mu'], t['shape'], t['df'])
+            assert_array_almost_equal(val, t['ans'])
+
+    def test_logpdf_correct(self):
+        for t in MULTIVARIATE_T_TESTS:
+            dist = multivariate_t(t['mu'], t['shape'], t['df'])
+            # We already test this elsewhere, but let's explicitly confirm the
+            # PDF is correct since this test requires checking the log of that
+            # value.
+            val1 = dist.pdf(t['x'])
+            assert_array_almost_equal(val1, t['ans'])
+            val2 = dist.logpdf(t['x'])
+            assert_array_almost_equal(np.log(val1), val2)
+
+    # https://github.com/scipy/scipy/issues/10042#issuecomment-576795195
+    def test_mvt_with_df_one_is_cauchy(self):
+        x = [9, 7, 4, 1, -3, 9, 0, -3, -1, 3]
+        val = multivariate_t.pdf(x, df=1)
+        ans = cauchy.pdf(x)
+        assert_array_almost_equal(val, ans)
+
+    def test_mvt_with_high_df_is_approx_normal(self):
+        # `normaltest` returns the chi-squared statistic and the associated
+        # p-value. The null hypothesis is that `x` came from a normal
+        # distribution, so a low p-value represents rejecting the null, i.e.
+        # that it is unlikely that `x` came a normal distribution.
+        P_VAL_MIN = 0.1
+
+        dist = multivariate_t(0, 1, df=100000, seed=1)
+        samples = dist.rvs(size=100000)
+        _, p = normaltest(samples)
+        assert (p > P_VAL_MIN)
+
+        dist = multivariate_t([-2, 3], [[10, -1], [-1, 10]], df=100000,
+                              seed=42)
+        samples = dist.rvs(size=100000)
+        _, p = normaltest(samples)
+        assert ((p > P_VAL_MIN).all())
+
+        dist = multivariate_t(0, 1, df=np.inf, seed=7)
+        samples = dist.rvs(size=100000)
+        _, p = normaltest(samples)
+        assert (p > P_VAL_MIN)
+
+    def test_default_arguments(self):
+        dist = multivariate_t()
+        assert_equal(dist.mean, [0])
+        assert_equal(dist.shape, [[1]])
+        assert (dist.df == 1)
+
+    def test_reproducibility(self):
+        rng = np.random.RandomState(4)
+        mean = rng.random(3)
+        tmp = rng.random((20, 3))
+        shape = np.matmul(tmp.T, tmp)
+        dist1 = multivariate_t(mean, shape, df=3, seed=2)
+        dist2 = multivariate_t(mean, shape, df=3, seed=2)
+        samples1 = dist1.rvs(size=10)
+        samples2 = dist2.rvs(size=10)
+        assert_equal(samples1, samples2)
+
 
 def check_pickling(distfn, args):
     # check that a distribution instance pickles and unpickles
