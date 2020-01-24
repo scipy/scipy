d806cd1936d59159bfa1890865acab2f7c7d2a39
Josh Wilson <person142@users.noreply.github.com>
2020-01-23 22:32:08 -0800

    ENH: Add tool to lint diffs
    As discussed on the mailing list, we can move towards better code
    style without causing disruption by adding extra lint checks for
    diffs. This adds a script that lints the diff between the tip of the
    current branch and where it split off of master.

---
+++ tools/lint_diff.py
@@ -0,0 +1,84 @@
+import os
+import sys
+import subprocess
+
+CONFIG = os.path.join(
+    os.path.abspath(os.path.dirname(__file__)),
+    'lint_diff.ini',
+)
+
+
+def rev_list(branch, num_commits):
+    """List commits in reverse chronological order.
+
+    Only the first `num_commits` are shown.
+
+    """
+    res = subprocess.run(
+        [
+            'git',
+            'rev-list',
+            '--max-count',
+            f'{num_commits}',
+            '--first-parent',
+            branch
+        ],
+        capture_output=True,
+        text=True,
+    )
+    res.check_returncode()
+    return res.stdout.rstrip('\n').split('\n')
+
+
+def find_branch_point():
+    """Find when the current branch split off master.
+
+    It is based off of this Stackoverflow post:
+
+    https://stackoverflow.com/questions/1527234/finding-a-branch-point-with-git#4991675
+
+    """
+    branch_commits = rev_list('HEAD', 100)
+    master_commits = set(rev_list('master', 100))
+    for branch_commit in branch_commits:
+        if branch_commit in master_commits:
+            return branch_commit
+
+    # If a branch split off over 100 commits ago we will fail to find
+    # the ancestor.
+    raise RuntimeError('Failed to find a common ancestor')
+
+
+def find_diff(sha):
+    """Find the diff since the given sha."""
+    res = subprocess.run(
+        ['git', 'diff', '--unified=0', sha],
+        capture_output=True,
+        text=True,
+    )
+    res.check_returncode()
+    return res.stdout
+
+
+def run_pycodestyle(diff):
+    """Run pycodestyle on the given diff."""
+    res = subprocess.run(
+        ['pycodestyle', '--diff', '--config', CONFIG],
+        input=diff,
+        capture_output=True,
+        text=True,
+    )
+    return res.returncode, res.stdout
+
+
+def main():
+    branch_point = find_branch_point()
+    diff = find_diff(branch_point)
+    rc, errors = run_pycodestyle(diff)
+    if errors:
+        print(errors)
+    sys.exit(rc)
+
+
+if __name__ == '__main__':
+    main()
