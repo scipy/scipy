492b00f2b752ae5100f3a19354e432ae9131e18d
Stefan Endres <Stefan.C.Endres@gmail.com>
2020-01-29 15:20:54 +0100

    Fix documentation issue
    The inequality in the docstring should be reversed, fixes #11428 .

--- scipy/optimize/_shgo.py
+++ scipy/optimize/_shgo.py
@@ -45,7 +45,7 @@ def shgo(func, bounds, args=(), constraints=None, n=100, iters=1, callback=None,
         Constraints definition.
         Function(s) ``R**n`` in the form::
 
-            g(x) <= 0 applied as g : R^n -> R^m
+            g(x) >= 0 applied as g : R^n -> R^m
             h(x) == 0 applied as h : R^n -> R^p
 
         Each constraint is defined in a dictionary with fields:
