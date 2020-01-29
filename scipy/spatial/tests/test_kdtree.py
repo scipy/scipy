ca54f2f695396385f60ab9f0fe4af12ec7168431
Seth Troisi <sethtroisi@google.com>
2020-01-29 03:19:49 -0800

    MAINT: Remove Python 2 workarounds

--- scipy/spatial/tests/test_kdtree.py
+++ scipy/spatial/tests/test_kdtree.py
@@ -1160,7 +1160,7 @@ def test_ckdtree_memuse():
     try:
         import resource
     except ImportError:
-        # resource is not available on Windows with Python 2.6
+        # resource is not available on Windows
         return
     # Make some data
     dx, dy = 0.05, 0.05
