25982656a694a203a7ba6bf65d200f71e04b4b3b
Søren Fuglede Jørgensen <s@fuglede.dk>
2020-02-02 19:50:40 +0100

    BUG: fix augmentation being broken in maximum_bipartite_matching
    This fixes an issue that would worst case cause infeasible
    solutions to be produced by maximum_bipartite_matching as the
    value of the `x`-array would end up containing edges that do
    not belong to the input graph.
    
    The root cause of the issue was a piece broken logic in the DFS
    part of the Hopcroft--Karp implementation concerned with keeping
    track of the augmenting path. We fix the issue by replacing
    this logic with a more standard approach of marking the parents
    of each vertex on the path (or rather, its grandparent, as we
    still only deal explicitly with one partition at a time).
    
    A regression test has been added to showcase an example of an
    input that would previously lead to an infeasible solution.

--- scipy/sparse/csgraph/tests/test_matching.py
+++ scipy/sparse/csgraph/tests/test_matching.py
@@ -84,6 +84,28 @@ def test_explicit_zeros_count_as_edges():
     assert_array_equal(expected_matching, y)
 
 
+def test_feasibility_of_result():
+    # This is a regression test for GitHub issue #11458
+    data = np.ones(50, dtype=int)
+    indices = [11, 12, 19, 22, 23, 5, 22, 3, 8, 10, 5, 6, 11, 12, 13, 5, 13,
+               14, 20, 22, 3, 15, 3, 13, 14, 11, 12, 19, 22, 23, 5, 22, 3, 8,
+               10, 5, 6, 11, 12, 13, 5, 13, 14, 20, 22, 3, 15, 3, 13, 14]
+    indptr = [0, 5, 7, 10, 10, 15, 20, 22, 22, 23, 25, 30, 32, 35, 35, 40, 45,
+              47, 47, 48, 50]
+    graph = csr_matrix((data, indices, indptr), shape=(20, 25))
+    x = maximum_bipartite_matching(graph, perm_type='row')
+    y = maximum_bipartite_matching(graph, perm_type='column')
+    assert (x != -1).sum() == 13
+    assert (y != -1).sum() == 13
+    # Ensure that each element of the matching is in fact an edge in the graph.
+    for u, v in zip(range(graph.shape[0]), y):
+        if v != -1:
+            assert graph[u, v]
+    for u, v in zip(x, range(graph.shape[1])):
+        if u != -1:
+            assert graph[u, v]
+
+
 def test_large_random_graph_with_one_edge_incident_to_each_vertex():
     np.random.seed(42)
     A = diags(np.ones(25), offsets=0, format='csr')
