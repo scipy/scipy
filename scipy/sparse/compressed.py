5d87a9608a71b604ce6e49eb89699acf75642b4d
Santiago M. Mola <santi@mola.io>
2020-02-06 14:49:32 +0100

    ENH: avoid unnecessary array copies in matrix product
    csr/csc matrices were unnecessarily creating indices copies with int32
    to int64 in some cases.
    
    *_matmat_pass{1,2} are renamed to *_matmat_maxnnz and *_matmat. The
    first only computes the nnz for the result matrix, which is enough to
    determine the index dtype of the result.
    
    bsr_matmat (previously bsr_matmat_pass2) now assigns indices to indptr.

--- scipy/sparse/compressed.py
+++ scipy/sparse/compressed.py
@@ -503,25 +503,23 @@ class _cs_matrix(_data_matrix, _minmax_mixin, IndexMixin):
         idx_dtype = get_index_dtype((self.indptr, self.indices,
                                      other.indptr, other.indices),
                                     maxval=M*N)
-        indptr = np.empty(major_axis + 1, dtype=idx_dtype)
 
-        fn = getattr(_sparsetools, self.format + '_matmat_pass1')
-        fn(M, N,
-           np.asarray(self.indptr, dtype=idx_dtype),
-           np.asarray(self.indices, dtype=idx_dtype),
-           np.asarray(other.indptr, dtype=idx_dtype),
-           np.asarray(other.indices, dtype=idx_dtype),
-           indptr)
+        fn = getattr(_sparsetools, self.format + '_matmat_maxnnz')
+        nnz = fn(M, N,
+                 np.asarray(self.indptr, dtype=idx_dtype),
+                 np.asarray(self.indices, dtype=idx_dtype),
+                 np.asarray(other.indptr, dtype=idx_dtype),
+                 np.asarray(other.indices, dtype=idx_dtype))
 
-        nnz = indptr[-1]
         idx_dtype = get_index_dtype((self.indptr, self.indices,
                                      other.indptr, other.indices),
                                     maxval=nnz)
-        indptr = np.asarray(indptr, dtype=idx_dtype)
+
+        indptr = np.empty(major_axis + 1, dtype=idx_dtype)
         indices = np.empty(nnz, dtype=idx_dtype)
         data = np.empty(nnz, dtype=upcast(self.dtype, other.dtype))
 
-        fn = getattr(_sparsetools, self.format + '_matmat_pass2')
+        fn = getattr(_sparsetools, self.format + '_matmat')
         fn(M, N, np.asarray(self.indptr, dtype=idx_dtype),
            np.asarray(self.indices, dtype=idx_dtype),
            self.data,
