e109fe60b4a6ec4252fb03707e6a6d81a7f03567
pdmurray <peynmurray@gmail.com>
2020-02-08 22:18:52 -0800

    BUG: make special.eval_hermite return nan if second argument is nan

--- scipy/special/tests/test_orthogonal_eval.py
+++ scipy/special/tests/test_orthogonal_eval.py
@@ -2,8 +2,9 @@ from __future__ import division, print_function, absolute_import
 
 import numpy as np
 from numpy.testing import assert_, assert_allclose
-import scipy.special.orthogonal as orth
+import pytest
 
+import scipy.special.orthogonal as orth
 from scipy.special._testutils import FuncData
 
 
@@ -252,3 +253,11 @@ def test_hermite_domain():
     # Regression test for gh-11091.
     assert np.isnan(orth.eval_hermite(-1, 1.0))
     assert np.isnan(orth.eval_hermitenorm(-1, 1.0))
+
+
+@pytest.mark.parametrize("n", [0, 1, 2])
+@pytest.mark.parametrize("x", [0, 1, np.nan])
+def test_hermite_nan(n, x):
+    # Regression test for gh-11369.
+    assert np.isnan(orth.eval_hermite(n, x)) == np.any(np.isnan([n, x]))
+    assert np.isnan(orth.eval_hermitenorm(n, x)) == np.any(np.isnan([n, x]))
