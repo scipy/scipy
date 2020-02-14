5ed220bb434a0f614b93d31adabd920036576f89
grapheo12 <donofsuri@gmail.com>
2020-02-14 09:52:54 +0530

    DOC: Changed example image so that it passes the checks

--- scipy/signal/signaltools.py
+++ scipy/signal/signaltools.py
@@ -1438,8 +1438,9 @@ def wiener(im, mysize=None, noise=None):
     >>> from scipy.misc import face
     >>> from scipy.signal.signaltools import wiener
     >>> import matplotlib.pyplot as plt
-    >>> img = face(gray=True)
-    >>> filtered_img = wiener(img, (5, 5), 1.5)  #Filter the image
+    >>> import numpy as np
+    >>> img = np.random.random((40, 40))    #Create a random image
+    >>> filtered_img = wiener(img, (5, 5))  #Filter the image
     >>> f, (plot1, plot2) = plt.subplots(1, 2)
     >>> plot1.imshow(img)
     >>> plot2.imshow(filtered_img)
