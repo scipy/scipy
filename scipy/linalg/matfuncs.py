33c321fcf2332fa00d0052298bf3503894e9411e
Ralf Gommers <ralf.gommers@gmail.com>
2020-02-16 11:09:59 -0800

    DOC: fix reST syntax in `linalg.khatri_rao`

--- scipy/linalg/matfuncs.py
+++ scipy/linalg/matfuncs.py
@@ -697,7 +697,7 @@ def khatri_rao(a, b):
 
         (A_{ij}  \bigotimes B_{ij})_{ij}
 
-    which is the Kronecker product of every column of A and B, e.g.,
+    which is the Kronecker product of every column of A and B, e.g.::
 
         c = np.vstack([np.kron(a[:, k], b[:, k]) for k in range(b.shape[1])]).T
 
