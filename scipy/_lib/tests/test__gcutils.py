19eb5cd829f1a54ef883a38f26bed10776a13a81
Paul van Mulbregt <pvanmulbregt@users.noreply.github.com>
2020-02-17 15:45:42 -0500

    MAINT: Fixed several unused imports and unused assignments.

--- scipy/_lib/tests/test__gcutils.py
+++ scipy/_lib/tests/test__gcutils.py
@@ -73,6 +73,10 @@ def test_assert_deallocated_nodel():
         pass
     with pytest.raises(ReferenceError):
         # Need to delete after using if in with-block context
+        # Note: assert_deallocated(C) needs to be assigned for the test
+        # to function correctly.  It is assigned to c, but c itself is
+        # not referenced in the body of the with, it is only there for
+        # the refcount.
         with assert_deallocated(C) as c:
             pass
 
