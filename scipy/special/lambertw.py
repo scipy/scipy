5f3b3a68ae55032fc4ae461d370df36afe83ab5c
James Wright <james@jameswright.xyz>
2020-03-08 16:36:43 -0600

    DOC: Update paper URL in lambertw documentation

--- scipy/special/lambertw.py
+++ scipy/special/lambertw.py
@@ -67,7 +67,7 @@ def lambertw(z, k=0, tol=1e-8):
     .. [1] https://en.wikipedia.org/wiki/Lambert_W_function
     .. [2] Corless et al, "On the Lambert W function", Adv. Comp. Math. 5
        (1996) 329-359.
-       http://www.apmaths.uwo.ca/~djeffrey/Offprints/W-adv-cm.pdf
+       https://cs.uwaterloo.ca/research/tr/1993/03/W.pdf
 
     Examples
     --------
