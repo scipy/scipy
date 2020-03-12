f87e603b1e887b23cbcc8ce2767ab3b4f819079e
ryanlu41 <ryanlu41@gmail.com>
2020-03-12 14:45:02 -0400

    formatting changes

--- benchmarks/benchmarks/ndimage.py
+++ benchmarks/benchmarks/ndimage.py
@@ -1,6 +1,6 @@
 from __future__ import division, absolute_import, print_function
 
-import numpy as np 
+import numpy as np
 import timeit
 
 try:
@@ -10,6 +10,7 @@ except ImportError:
 
 from .common import Benchmark
 
+
 class GaborFilter(Benchmark):
     param_names = ['truncate']
     params = [1, 2, 3]
@@ -18,9 +19,9 @@ class GaborFilter(Benchmark):
         np.random.seed(123456)
         self.image = np.random.randint(0, 256, size=[25, 25])
         self.volume = np.random.randint(0, 256, size=[25, 25, 25])
-    
+
     def time_gabor_2d(self, truncate):
         gabor_filter(self.image, 2.5, 0, 0.1, 0, truncate=truncate)
-    
+
     def time_gabor_3d(self, truncate):
         gabor_filter(self.volume, 5, 0, 0.1, 0, truncate=truncate)
