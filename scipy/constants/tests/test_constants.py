779c1f99438b888e70adefaf91cacdf8640822ea
jakobjakobson13 <43045863+jakobjakobson13@users.noreply.github.com>
2020-03-20 21:20:08 +0100

    change speed_of_light to c

--- scipy/constants/tests/test_constants.py
+++ scipy/constants/tests/test_constants.py
@@ -27,9 +27,9 @@ def test_convert_temperature():
 
 
 def test_lambda_to_nu():
-    assert_equal(sc.lambda2nu(sc.speed_of_light), 1)
+    assert_equal(sc.lambda2nu(sc.c), 1)
 
 
 def test_nu_to_lambda():
-    assert_equal(sc.nu2lambda(1), sc.speed_of_light)
+    assert_equal(sc.nu2lambda(1), sc.c)
 
