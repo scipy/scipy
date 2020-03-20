8d6c7a10ad423f5e5407904b8ca27d4554d97cc0
Josh Wilson <person142@users.noreply.github.com>
2020-03-20 08:05:05 -0700

    MAINT: linalg: remove outdated `linalg_version`

--- scipy/linalg/setup.py
+++ scipy/linalg/setup.py
@@ -118,7 +118,5 @@ def configuration(parent_package='', top_path=None):
 
 if __name__ == '__main__':
     from numpy.distutils.core import setup
-    from scipy.linalg.linalg_version import linalg_version
 
-    setup(version=linalg_version,
-          **configuration(top_path='').todict())
+    setup(**configuration(top_path='').todict())
