f738ddd7eeadd060d7918f08c01c45a8800ca6dd
Atsushi Sakai <asakai.amsl+github@gmail.com>
2020-03-24 20:20:07 +0900

    fixed docstring based on review.

--- scipy/optimize/nonlin.py
+++ scipy/optimize/nonlin.py
@@ -1374,7 +1374,7 @@ class KrylovJacobian(Jacobian):
 
         The default is `scipy.sparse.linalg.lgmres`.
     inner_maxiter : int, optional
-        Parameter to pass to the \"inner\" Krylov solver: maximum number of
+        Parameter to pass to the "inner" Krylov solver: maximum number of
         iterations. Iteration will stop after maxiter steps even if the
         specified tolerance has not been achieved.
     inner_M : LinearOperator or InverseJacobian
@@ -1394,7 +1394,7 @@ class KrylovJacobian(Jacobian):
         Size of the subspace kept across LGMRES nonlinear iterations.
         See `scipy.sparse.linalg.lgmres` for details.
     inner_kwargs : kwargs
-        Keyword parameters for the \"inner\" Krylov solver
+        Keyword parameters for the "inner" Krylov solver
         (defined with `method`). Parameter names must start with
         the `inner_` prefix which will be stripped before passing on
         the inner method. See, e.g., `scipy.sparse.linalg.gmres` for details.
