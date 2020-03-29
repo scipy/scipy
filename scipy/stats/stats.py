BUG: refix wrong shape from median_absolute_deviation for arrays with size 0 and an axis

scipy/stats/stats.py
@@ -2829,12 +2829,16 @@ def median_absolute_deviation(x, axis=0, center=np.median, scale=1.4826,
 
     # Consistent with `np.var` and `np.std`.
     if not x.size:
-        nan_shape = [item for i, item in enumerate(x.shape) if i != axis]
-        nan_array = np.full(nan_shape, np.nan)
-        if not nan_array.size:
+        if x.shape == (0,): # empty array
             return np.nan
-        else:
-            return nan_array
+        nan_shape = list(x.shape)
+        try:
+            del nan_shape[axis]
+        except IndexError:
+            raise ValueError("axis number {} is invalid".format(axis))
+
+        nan_array = np.full(nan_shape, np.nan)
+        return nan_array
 
     contains_nan, nan_policy = _contains_nan(x, nan_policy)
 

scipy/stats/tests/test_stats.py
@@ -1887,7 +1887,7 @@ class TestVariability(object):
     def test_mad_nan_shape1(self):
         z = np.ones((3, 0))
         mad_axis0 = stats.median_absolute_deviation(z, axis=0)
-        assert_equal(mad_axis0, np.nan)
+        assert_equal(mad_axis0.shape, (0,))
         mad_axis1 = stats.median_absolute_deviation(z, axis=1)
         assert_equal(mad_axis1, np.array([np.nan, np.nan, np.nan]))
         assert_equal(mad_axis1.shape, (3,))
@@ -1895,14 +1895,14 @@ class TestVariability(object):
     def test_mad_nan_shape2(self):
         z = np.ones((3, 0, 2))
         mad_axis0 = stats.median_absolute_deviation(z, axis=0)
-        assert_equal(mad_axis0, np.nan)
+        assert_equal(mad_axis0.shape, (0, 2))
         mad_axis1 = stats.median_absolute_deviation(z, axis=1)
         assert_equal(mad_axis1, np.array([[np.nan, np.nan],
                                           [np.nan, np.nan],
                                           [np.nan, np.nan]]))
         assert_equal(mad_axis1.shape, (3, 2))
         mad_axis2 = stats.median_absolute_deviation(z, axis=2)
-        assert_equal(mad_axis2, np.nan)
+        assert_equal(mad_axis2.shape, (3, 0))
 
     def test_mad_nan_propagate(self):
         dat = np.array([2.20, 2.20, 2.4, 2.4, 2.5, 2.7, 2.8, 2.9, 3.03,