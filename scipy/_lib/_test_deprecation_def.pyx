dfd9204c5c4aece4e36da5a2038739a163062938
Pauli Virtanen <pav@iki.fi>
2020-04-06 20:32:49 +0300

    MAINT: _lib: add helper function for deprecating Cython API functions

---
+++ scipy/_lib/_test_deprecation_def.pyx
@@ -0,0 +1,11 @@
+cdef public int foo():
+    return 1
+
+cdef public int foo_deprecated():
+    return 1
+
+from scipy._lib.deprecation import deprecate_cython_api
+import scipy._lib._test_deprecation_def as mod
+deprecate_cython_api(mod, "foo_deprecated", new_name="foo",
+                     message="Deprecated in Scipy 42.0.0")
+del deprecate_cython_api, mod
