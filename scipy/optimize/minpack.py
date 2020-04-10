aa0b4092cd314b3974b0ce0fac9ef1fde120506c
Atsushi Sakai <asakai.amsl+github@gmail.com>
2020-04-10 20:53:14 +0900

    Doc: add Examples in scipy.optimize.fsolve docstring

--- scipy/optimize/minpack.py
+++ scipy/optimize/minpack.py
@@ -133,6 +133,18 @@ def fsolve(func, x0, args=(), fprime=None, full_output=0,
     -----
     ``fsolve`` is a wrapper around MINPACK's hybrd and hybrj algorithms.
 
+    Examples
+    --------
+    >>> from scipy.optimize import fsolve
+    >>> def func(x):
+    ...     return [x[0]*np.cos(x[1]) - 4,
+    ...             x[1]*x[0] - x[1] - 5]
+    >>> root = fsolve(func, [1, 1])
+    >>> root
+    array([6.50409711, 0.90841421])
+    >>> np.isclose(func(root), [0.0, 0.0]) # f(root) should be almost 0.0.
+    array([ True,  True])
+
     """
     options = {'col_deriv': col_deriv,
                'xtol': xtol,
