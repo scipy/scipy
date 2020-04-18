87385cf0b3264dcbf8bb1e1921f7c2b913ed01dc
Atsushi Sakai <asakai.amsl+github@gmail.com>
2020-04-18 10:43:49 +0900

    update docstring base on review.

--- scipy/interpolate/polyint.py
+++ scipy/interpolate/polyint.py
@@ -445,14 +445,16 @@ def approximate_taylor_polynomial(f,x,degree,scale,order=None):
     various degrees:
 
     >>> import matplotlib.pyplot as plt
-    >>> from scipy import interpolate
+    >>> from scipy.interpolate import approximate_taylor_polynomial
     >>> x = np.linspace(-10.0, 10.0, num=100)
     >>> plt.plot(x, np.sin(x), label="sin curve")
     >>> for degree in np.arange(1, 15, step=2):
-    >>>     sin_taylor = interpolate.approximate_taylor_polynomial(np.sin, 0,
-    ...                                                             degree, 1)
-    >>>     plt.plot(x, sin_taylor(x), label=f"degree={degree}")
-    >>> plt.legend()
+    ...     sin_taylor = approximate_taylor_polynomial(np.sin, 0, degree, 1,
+    ...                                                 order=degree + 2)
+    ...     plt.plot(x, sin_taylor(x), label=f"degree={degree}")
+    >>> plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left',
+    ...             borderaxespad=0.0, shadow=True)
+    >>> plt.tight_layout()
     >>> plt.axis([-10, 10, -10, 10])
     >>> plt.show()
 
