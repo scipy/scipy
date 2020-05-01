f587e088467159488117f67206e9b63628460074
Atsushi Sakai <asakai.amsl+github@gmail.com>
2020-05-01 09:26:48 +0900

    DOC: add Examples for stats.mstats.argstoarray() docstring.

--- scipy/stats/mstats_basic.py
+++ scipy/stats/mstats_basic.py
@@ -137,6 +137,22 @@ def argstoarray(*args):
     `numpy.ma.row_stack` has identical behavior, but is called with a sequence
     of sequences.
 
+    Examples
+    --------
+    A 2D masked array constructed from a group of sequences is returned.
+
+    >>> from scipy.stats.mstats import argstoarray
+    >>> argstoarray([1, 2, 3], [4, 5, 6])
+    [[1.0 2.0 3.0]
+     [4.0 5.0 6.0]]
+
+    The returned masked array filled with missing values when the lengths of
+    sequences are different.
+
+    >>> argstoarray([1, 3], [4, 5, 6])
+    [[1.0 3.0 --]
+     [4.0 5.0 6.0]]
+
     """
     if len(args) == 1 and not isinstance(args[0], ndarray):
         output = ma.asarray(args[0])
