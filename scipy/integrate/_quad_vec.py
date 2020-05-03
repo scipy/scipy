a67177d1b126ac34a5f298721b82cf55b8dfe491
Atsushi Sakai <asakai.amsl+github@gmail.com>
2020-05-03 23:07:57 +0900

    DOC: integrate: add examples for quad_vec() and quad_explain() (#12004)

--- scipy/integrate/_quad_vec.py
+++ scipy/integrate/_quad_vec.py
@@ -103,7 +103,7 @@ class _Bunch(object):
 
 def quad_vec(f, a, b, epsabs=1e-200, epsrel=1e-8, norm='2', cache_size=100e6, limit=10000,
              workers=1, points=None, quadrature=None, full_output=False):
-    """Adaptive integration of a vector-valued function.
+    r"""Adaptive integration of a vector-valued function.
 
     Parameters
     ----------
@@ -195,6 +195,21 @@ def quad_vec(f, a, b, epsabs=1e-200, epsrel=1e-8, norm='2', cache_size=100e6, li
     References
     ----------
     [1] R. Piessens, E. de Doncker, QUADPACK (1983).
+
+    Examples
+    --------
+    We can compute integrations of a vector-valued function:
+
+    >>> from scipy.integrate import quad_vec
+    >>> import matplotlib.pyplot as plt
+    >>> alpha = np.linspace(0.0, 2.0, num=30)
+    >>> f = lambda x: x**alpha
+    >>> x0, x1 = 0, 2
+    >>> y, err = quad_vec(f, x0, x1)
+    >>> plt.plot(alpha, y)
+    >>> plt.xlabel(r"$\alpha$")
+    >>> plt.ylabel(r"$\int_{0}^{2} x^\alpha dx$")
+    >>> plt.show()
 
     """
     a = float(a)
