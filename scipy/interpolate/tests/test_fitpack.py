8699e97b80e803b2ede30a656f25ec0617af51ab
Pauli Virtanen <pav@iki.fi>
2020-05-17 15:38:30 +0300

    TST: interpolate: add fitpack test that passes only for ilp64

--- scipy/interpolate/tests/test_fitpack.py
+++ scipy/interpolate/tests/test_fitpack.py
@@ -6,6 +6,7 @@ from numpy.testing import (assert_equal, assert_allclose, assert_,
                            assert_almost_equal, assert_array_almost_equal)
 from pytest import raises as assert_raises
 import pytest
+from scipy._lib._testutils import check_free_memory
 
 from numpy import array, asarray, pi, sin, cos, arange, dot, ravel, sqrt, round
 from scipy import interpolate
@@ -404,6 +405,16 @@ class TestBisplrep(object):
         # code to crash when compiled with -O3
         bisplrep(data[:,0], data[:,1], data[:,2], kx=3, ky=3, s=0,
                  full_output=True)
+
+    @pytest.mark.skipif(dfitpack_int != np.int64, reason="needs ilp64 fitpack")
+    def test_ilp64_bisplrep(self):
+        check_free_memory(28000)  # VM size, doesn't actually use the pages
+        x = np.linspace(0, 1, 400)
+        y = np.linspace(0, 1, 400)
+        x, y = np.meshgrid(x, y)
+        z = np.zeros_like(x)
+        tck = bisplrep(x, y, z, kx=3, ky=3, s=0)
+        assert_allclose(bisplev(0.5, 0.5, tck), 0.0)
 
 
 def test_dblint():
