2f32958cc3eea06a59d9ae87284acf51a233c22e
Warren Weckesser <warren.weckesser@gmail.com>
2020-05-28 19:28:16 -0400

    BUG: stats: Fix handling of edge cases in median_abs_deviation. (#12217)
    * BUG: stats: Fix handling of edge cases in median_abs_deviation.
    
    The edge cases that are fixed:
    * Size 0 arrays with an axis argument are now handled correctly;
      the return value is an appropriately shaped array of nan.
    * The return value when the input contains nan and axis is used
      with nan_policy 'propagate' is fixed. The function used to return a
      scalar nan value. The function now returns the array with the result of the
      MAD calculation applied along the specified axis.
    * inf is not ignored when nan_policy='omit'.
    Additional enhancements: 
    * Explicitly check that `center` is actually callable.
    * Expand the comments in the helper function _mad_1d.

--- scipy/stats/tests/test_stats.py
+++ scipy/stats/tests/test_stats.py
@@ -1882,11 +1882,11 @@ class TestVariability(object):
 class TestMedianAbsDeviation(object):
     def setup_class(self):
         self.dat_nan = np.array([2.20, 2.20, 2.4, 2.4, 2.5, 2.7, 2.8, 2.9,
-                3.03, 3.03, 3.10, 3.37, 3.4, 3.4, 3.4, 3.5, 3.6, 3.7, 3.7,
-                3.7, 3.7, 3.77, 5.28, np.nan])
+                                 3.03, 3.03, 3.10, 3.37, 3.4, 3.4, 3.4, 3.5,
+                                 3.6, 3.7, 3.7, 3.7, 3.7, 3.77, 5.28, np.nan])
         self.dat = np.array([2.20, 2.20, 2.4, 2.4, 2.5, 2.7, 2.8, 2.9, 3.03,
-                3.03, 3.10, 3.37, 3.4, 3.4, 3.4, 3.5, 3.6, 3.7, 3.7,
-                3.7, 3.7, 3.77, 5.28, 28.95])
+                             3.03, 3.10, 3.37, 3.4, 3.4, 3.4, 3.5, 3.6, 3.7,
+                             3.7, 3.7, 3.7, 3.77, 5.28, 28.95])
 
     def test_median_abs_deviation(self):
         assert_almost_equal(stats.median_abs_deviation(self.dat, axis=None),
@@ -1899,6 +1899,47 @@ class TestMedianAbsDeviation(object):
     def test_mad_nan_omit(self):
         mad = stats.median_abs_deviation(self.dat_nan, nan_policy='omit')
         assert_almost_equal(mad, 0.34)
+
+    def test_axis_and_nan(self):
+        x = np.array([[1.0, 2.0, 3.0, 4.0, np.nan],
+                      [1.0, 4.0, 5.0, 8.0, 9.0]])
+        mad = stats.median_abs_deviation(x, axis=1)
+        assert_equal(mad, np.array([np.nan, 3.0]))
+
+    def test_nan_policy_omit_with_inf(sef):
+        z = np.array([1, 3, 4, 6, 99, np.nan, np.inf])
+        mad = stats.median_abs_deviation(z, nan_policy='omit')
+        assert_equal(mad, 3.0)
+
+    @pytest.mark.parametrize('axis', [0, 1, 2, None])
+    def test_size_zero_with_axis(self, axis):
+        x = np.zeros((3, 0, 4))
+        mad = stats.median_abs_deviation(x, axis=axis)
+        assert_equal(mad, np.full_like(x.sum(axis=axis), fill_value=np.nan))
+
+    @pytest.mark.parametrize('nan_policy, expected',
+                             [('omit', np.array([np.nan, 1.5, 1.5])),
+                              ('propagate', np.array([np.nan, np.nan, 1.5]))])
+    def test_nan_policy_with_axis(self, nan_policy, expected):
+        x = np.array([[np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],
+                      [1, 5, 3, 6, np.nan, np.nan],
+                      [5, 6, 7, 9, 9, 10]])
+        mad = stats.median_abs_deviation(x, nan_policy=nan_policy, axis=1)
+        assert_equal(mad, expected)
+
+    @pytest.mark.parametrize('axis, expected',
+                             [(1, [2.5, 2.0, 12.0]), (None, 4.5)])
+    def test_center_mean_with_nan(self, axis, expected):
+        x = np.array([[1, 2, 4, 9, np.nan],
+                      [0, 1, 1, 1, 12],
+                      [-10, -10, -10, 20, 20]])
+        mad = stats.median_abs_deviation(x, center=np.mean, nan_policy='omit',
+                                         axis=axis)
+        assert_allclose(mad, expected, rtol=1e-15, atol=1e-15)
+
+    def test_center_not_callable(self):
+        with pytest.raises(TypeError, match='callable'):
+            stats.median_abs_deviation([1, 2, 3, 5], center=99)
 
 
 class TestMedianAbsoluteDeviation(object):
