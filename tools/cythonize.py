7962367b7f2f191d3f780341049b0e5885762e94
Paul van Mulbregt <pvanmulbregt@users.noreply.github.com>
2020-06-01 22:03:59 -0400

    Added raw qualifier to 3 regex strings in tools/cythonize.py.

--- tools/cythonize.py
+++ tools/cythonize.py
@@ -181,9 +181,9 @@ def get_cython_dependencies(fullfrompath):
     fullfromdir = os.path.dirname(fullfrompath)
     deps = set()
     with open(fullfrompath, 'r') as f:
-        pxipattern = re.compile('include "([a-zA-Z0-9_]+\.pxi)"')
-        pxdpattern1 = re.compile('from \. cimport ([a-zA-Z0-9_]+)')
-        pxdpattern2 = re.compile('from \.([a-zA-Z0-9_]+) cimport')
+        pxipattern = re.compile(r'include "([a-zA-Z0-9_]+\.pxi)"')
+        pxdpattern1 = re.compile(r'from \. cimport ([a-zA-Z0-9_]+)')
+        pxdpattern2 = re.compile(r'from \.([a-zA-Z0-9_]+) cimport')
 
         for line in f:
             m = pxipattern.match(line)
