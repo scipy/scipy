4753135cdaf4d489b8bcb723a99b63ba74a82a1e
Zach Colbert <colbertz@oregonstate.edu>
2020-06-10 13:23:15 -0700

    DOC: add examples for eval_legendre()

--- scipy/special/add_newdocs.py
+++ scipy/special/add_newdocs.py
@@ -2951,6 +2951,43 @@ add_newdoc("eval_legendre",
         Handbook of Mathematical Functions with Formulas,
         Graphs, and Mathematical Tables. New York: Dover, 1972.
 
+    Examples
+    --------
+    >>> from scipy.special import eval_legendre
+
+    Evaluate the zero-order Legendre polynomial at x = 0
+
+    >>> eval_legendre(0, 0)
+    1.0
+
+    Evaluate the first-order Legendre polynomial between -1 and 1
+
+    >>> import numpy as np
+    >>> X = np.linspace(-1, 1, 5)  # Domain of Legendre polynomials
+    >>> eval_legendre(1, X)
+    array([-1. , -0.5,  0. ,  0.5,  1. ])
+
+    Evaluate Legendre polynomials of order 0 through 5 at x = 0
+
+    >>> N = range(0, 5)
+    >>> eval_legendre(N, 0)
+    array([ 1.   ,  0.   , -0.5  ,  0.   ,  0.375])
+
+    Plot Legendre polynomials of order 0 through 5
+
+    >>> X = np.linspace(-1, 1)  # Plot over -1 to 1 domain
+
+    >>> import matplotlib.pyplot as plt
+    >>> for n in range(0, 5):       # Plot each polynomial of order n
+    ...     y = eval_legendre(n, X)
+    ...     plt.plot(X, y, label=r'$P_{}(x)$'.format(n))
+
+    >>> plt.title("Legendre Polynomials")
+    >>> plt.xlabel("x")
+    >>> plt.ylabel(r'$P_n(x)$')
+    >>> plt.legend(loc='lower right')
+    >>> plt.show()
+
     """)
 
 add_newdoc("eval_sh_legendre",
