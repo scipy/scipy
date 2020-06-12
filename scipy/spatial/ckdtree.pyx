c27ba6b1d4121b57c42cb147160058575ce13cf2
Atsushi Sakai <asakai.amsl+github@gmail.com>
2020-06-12 21:57:38 +0900

    fix docstring

--- scipy/spatial/ckdtree.pyx
+++ scipy/spatial/ckdtree.pyx
@@ -1139,12 +1139,12 @@ cdef class cKDTree:
 
         >>> import matplotlib.pyplot as plt
         >>> import numpy as np
-        >>> from scipy.spatial import KDTree
+        >>> from scipy.spatial import cKDTree
         >>> np.random.seed(21701)
         >>> points = np.random.random((20, 2))
         >>> plt.figure(figsize=(6, 6))
         >>> plt.plot(points[:, 0], points[:, 1], "xk")
-        >>> kd_tree = KDTree(points)
+        >>> kd_tree = cKDTree(points)
         >>> pairs = kd_tree.query_pairs(r=0.2)
         >>> for (i, j) in pairs:
         ...     plt.plot([points[i, 0], points[j, 0]],
