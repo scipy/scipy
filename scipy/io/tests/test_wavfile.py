7385678e84272c29718b6c062fb34adb8bf492ca
endolith <endolith@gmail.com>
2020-06-19 08:08:05 -0400

    TST: Add 36-, 45-, 53-bit example tests
    To test 5-, 6-, and 7-byte ranges.
    
    These probably don't exist, but testing for completeness

--- scipy/io/tests/test_wavfile.py
+++ scipy/io/tests/test_wavfile.py
@@ -155,6 +155,84 @@ def test_24_bit_odd_size_with_pad():
     del data
 
 
+def test_36_bit_odd_size():
+    # 36-bit, 5 B container, 3 channels, 5 samples, 75 B data chunk + pad
+    filename = 'test-8000Hz-le-3ch-5S-36bit.wav'
+    rate, data = wavfile.read(datafile(filename), mmap=False)
+
+    assert_equal(rate, 8000)
+    assert_(np.issubdtype(data.dtype, np.int64))
+    assert_equal(data.shape, (5, 3))
+
+    # 28 LSBits should be 0
+    assert_equal(data & 0xfffffff, 0)
+
+    # Hand-made max/min samples under different conventions:
+    #            Fixed-point 2**(N-1)    Full-scale 2**(N-1)-1       LSB
+    correct = [[-0x8000_0000_0000_0000, -0x7fff_ffff_f000_0000, -0x2000_0000],
+               [-0x4000_0000_0000_0000, -0x3fff_ffff_f000_0000, -0x1000_0000],
+               [+0x0000_0000_0000_0000, +0x0000_0000_0000_0000, +0x0000_0000],
+               [+0x4000_0000_0000_0000, +0x3fff_ffff_f000_0000, +0x1000_0000],
+               [+0x7fff_ffff_f000_0000, +0x7fff_ffff_f000_0000, +0x2000_0000]]
+    #              ^ clipped
+
+    assert_equal(data, correct)
+
+    del data
+
+
+def test_45_bit_even_size():
+    # 45-bit, 6 B container, 3 channels, 5 samples, 90 B data chunk
+    filename = 'test-8000Hz-le-3ch-5S-45bit.wav'
+    rate, data = wavfile.read(datafile(filename), mmap=False)
+
+    assert_equal(rate, 8000)
+    assert_(np.issubdtype(data.dtype, np.int64))
+    assert_equal(data.shape, (5, 3))
+
+    # 19 LSBits should be 0
+    assert_equal(data & 0x7ffff, 0)
+
+    # Hand-made max/min samples under different conventions:
+    #            Fixed-point 2**(N-1)    Full-scale 2**(N-1)-1      LSB
+    correct = [[-0x8000_0000_0000_0000, -0x7fff_ffff_fff8_0000, -0x10_0000],
+               [-0x4000_0000_0000_0000, -0x3fff_ffff_fff8_0000, -0x08_0000],
+               [+0x0000_0000_0000_0000, +0x0000_0000_0000_0000, +0x00_0000],
+               [+0x4000_0000_0000_0000, +0x3fff_ffff_fff8_0000, +0x08_0000],
+               [+0x7fff_ffff_fff8_0000, +0x7fff_ffff_fff8_0000, +0x10_0000]]
+    #              ^ clipped
+
+    assert_equal(data, correct)
+
+    del data
+
+
+def test_53_bit_odd_size():
+    # 53-bit, 7 B container, 3 channels, 5 samples, 105 B data chunk + pad
+    filename = 'test-8000Hz-le-3ch-5S-53bit.wav'
+    rate, data = wavfile.read(datafile(filename), mmap=False)
+
+    assert_equal(rate, 8000)
+    assert_(np.issubdtype(data.dtype, np.int64))
+    assert_equal(data.shape, (5, 3))
+
+    # 11 LSBits should be 0
+    assert_equal(data & 0x7ff, 0)
+
+    # Hand-made max/min samples under different conventions:
+    #            Fixed-point 2**(N-1)    Full-scale 2**(N-1)-1    LSB
+    correct = [[-0x8000_0000_0000_0000, -0x7fff_ffff_ffff_f800, -0x1000],
+               [-0x4000_0000_0000_0000, -0x3fff_ffff_ffff_f800, -0x0800],
+               [+0x0000_0000_0000_0000, +0x0000_0000_0000_0000, +0x0000],
+               [+0x4000_0000_0000_0000, +0x3fff_ffff_ffff_f800, +0x0800],
+               [+0x7fff_ffff_ffff_f800, +0x7fff_ffff_ffff_f800, +0x1000]]
+    #              ^ clipped
+
+    assert_equal(data, correct)
+
+    del data
+
+
 def test_64_bit_even_size():
     # 64-bit, 8 B container, 3 channels, 5 samples, 120 B data chunk
     for mmap in [False, True]:
