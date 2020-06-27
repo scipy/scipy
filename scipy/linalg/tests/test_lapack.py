d4e807550b08186ddce0e4be90bce760635a8c65
Ilhan Polat <ilhanpolat@gmail.com>
2020-06-27 14:06:27 +0200

    ENH:TST:linalg: Added wrappers for ppcon/pptri

--- scipy/linalg/tests/test_lapack.py
+++ scipy/linalg/tests/test_lapack.py
@@ -2926,27 +2926,48 @@ def test_ptsvx_NAG(d, e, b, x):
     assert_array_almost_equal(x, x_ptsvx)
 
 
+@pytest.mark.parametrize('lower', [False, True])
 @pytest.mark.parametrize('dtype', DTYPES)
-def test_pptrs_pptrf_ppsv(dtype):
+def test_pptrs_pptri_pptrf_ppsv_ppcon(dtype, lower):
     seed(1234)
+    atol = np.finfo(dtype).eps*100
     # Manual conversion to/from packed format is feasible here.
     n, nrhs = 10, 4
     a = generate_random_dtype_array([n, n], dtype=dtype)
     b = generate_random_dtype_array([n, nrhs], dtype=dtype)
+
     a = a.conj().T + a + np.eye(n, dtype=dtype) * dtype(5.)
-    inds = ([x for y in range(1, n+1) for x in range(y)],
-            [y-1 for y in range(1, n+1) for x in range(y)])
+    if lower:
+        inds = ([x for y in range(n) for x in range(y, n)],
+                [y for y in range(n) for x in range(y, n)])
+    else:
+        inds = ([x for y in range(1, n+1) for x in range(y)],
+                [y-1 for y in range(1, n+1) for x in range(y)])
     ap = a[inds]
-    ppsv, pptrf, pptrs = get_lapack_funcs(('ppsv', 'pptrf', 'pptrs'),
-                                          dtype=dtype)
+    ppsv, pptrf, pptrs, pptri, ppcon = get_lapack_funcs(('ppsv', 'pptrf',
+                                                         'pptrs', 'pptri',
+                                                         'ppcon'), dtype=dtype)
 
-    ul, info = pptrf(n, ap)
-    aul = cholesky(a)[inds]
-    assert_allclose(ul, aul, rtol=np.finfo(dtype).eps*50)
+    ul, info = pptrf(n, ap, lower=lower)
+    assert_equal(info, 0)
+    aul = cholesky(a, lower=lower)[inds]
+    assert_allclose(ul, aul, rtol=0, atol=atol)
 
-    x, info = pptrs(n, ul, b)
+    uli, info = pptri(n, ul, lower=lower)
+    assert_equal(info, 0)
+    auli = inv(a)[inds]
+    assert_allclose(uli, auli, rtol=0, atol=atol)
+
+    x, info = pptrs(n, ul, b, lower=lower)
+    assert_equal(info, 0)
     bx = solve(a, b)
-    assert_allclose(x, bx, rtol=np.finfo(dtype).eps*50)
+    assert_allclose(x, bx, rtol=0, atol=atol)
 
-    xv, info = ppsv(n, ap, b)
-    assert_allclose(xv, bx, rtol=np.finfo(dtype).eps*50)
+    xv, info = ppsv(n, ap, b, lower=lower)
+    assert_equal(info, 0)
+    assert_allclose(xv, bx, rtol=0, atol=atol)
+
+    anorm = np.linalg.norm(a, 1)
+    rcond, info = ppcon(n, ap, anorm=anorm, lower=lower)
+    assert_equal(info, 0)
+    assert_(abs(1/rcond - np.linalg.cond(a, p=1))*rcond < 1)
