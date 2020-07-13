f3b962d6f37a049e9163ea1518f29f9189570405
Matt Haberland <mhaberla@calpoly.edu>
2020-07-12 22:09:30 -0700

    DOC: stats: refine pagel documentation

--- scipy/stats/_pagel.py
+++ scipy/stats/_pagel.py
@@ -8,7 +8,7 @@ Page_L_Result = namedtuple('Page_L_Result',
                            ('statistic', 'pvalue', 'method'))
 
 
-def pagel(data, ranked=True, hypothesis=None, method='auto', n_s=2000,
+def pagel(data, ranked=True, predicted_ranks=None, method='auto', n_s=2000,
           ties=None):
     r"""
     Compute Page's L, a measure of trend in observations between treatments.
@@ -39,17 +39,18 @@ def pagel(data, ranked=True, hypothesis=None, method='auto', n_s=2000,
         column :math:`j` is the observation corresponding with subject
         :math:`i` and treatment :math:`j`. By default, the data
         are assumed to be ranked, and the columns are assumed to
-        be arranged in order of decreasing hypothesized mean.
+        be arranged in order of decreasing predicted mean.
 
     ranked : boolean, default = ``True``
         By default, the data are assumed to be ranked. If ``False``,
         the ``data`` will be ranked with `scipy.stats.rankdata` along
         ``axis=1``.
 
-    hypothesis : array-like, optional
-        The hypothesized ordering of the column means; if specified the columns
-        of ``data`` will be rearranged like ``data[:, hypothesis]`` so that
-        the columns are in order of decreasing hypothesized rank.
+    predicted_ranks : array-like, optional
+        The predicted ranks of the column means. If not specified,
+        the columns are assumed to be arranged in order of decreasing
+        predicted mean, so the `predicted_ranks` are
+        :math:`[n, n-1, \dots, 2, 1]`.
 
     method : {'auto', 'asymptotic', 'exact', 'mc'}, optional
         Selects the method used to calculate the *p*-value. The following
@@ -97,41 +98,53 @@ def pagel(data, ranked=True, hypothesis=None, method='auto', n_s=2000,
     "groupings by ability or some other control variable, or judges doing
     the ranking, or random replications of some other sort."
 
-    The procedure for performing the asymptotic :math:`L` test, adapted from
+    The procedure for calculating the :math:`L` statistic, adapted from
     [1]_, is:
 
-        1. Predetermine with careful logic the appropriate hypotheses
+        1. "Predetermine with careful logic the appropriate hypotheses
            concerning the predicted ording of the experimental results.
            If no reasonable basis for ordering any treatments is known, the
-           :math:`L` test is not appropriate.
-        2. As in other experiments, determine at what level of confidence
+           :math:`L` test is not appropriate."
+        2. "As in other experiments, determine at what level of confidence
            you will reject the null hypothesis that there is no agreement of
-           experimental results with the monotonic hypothesis.
-        3. Cast the experimental material into a two-way table of :math:`n`
+           experimental results with the monotonic hypothesis."
+        3. "Cast the experimental material into a two-way table of :math:`n`
            columns (treatments, objects ranked, conditions) and :math:`m`
-           rows (subjects, replication groups, levels of control variables).
-        4. When experimental observations are recorded, rank them across each
-           row, e.g. ``data = scipy.stats.rankdata(data, axis=1)``.
-        5. Add the ranks in each column, e.g. ``a = np.sum(data, axis=0)``.
-        6. Multiply each sum of ranks by the predicted rank for that same
-           column, e.g. ``b = (hypothesis+1) * a``.
-        7. Sum all such products, e.g. ``c = b.sum()``.
-        8. For an asymptotic test, use the statistic
+           rows (subjects, replication groups, levels of control variables)."
+        4. "When experimental observations are recorded, rank them across each
+           row", e.g. ``ranks = scipy.stats.rankdata(data, axis=1)``.
+        5. "Add the ranks in each column", e.g.
+           ``colsums = np.sum(ranks, axis=0)``.
+        6. "Multiply each sum of ranks by the predicted rank for that same
+           column", e.g. ``products = predicted_ranks * colsums``.
+        7. "Sum all such products", e.g. ``L = products.sum()``.
 
-        .. math::
+    [1]_ continues by suggesting use of the standardized statistic
 
-           \chi_L^2 = \frac{\left[12L-3mn(n+1)^2\right]^2}{mn^2(n^2-1)(n+1)}
+    .. math::
 
-           which is distributed approximately as chi-square with 1 degree of
-           freedom. The ordinary use of :math:`\chi^2` tables would be
-           equivalent to a two-sided test of agreement. If a one-sided test
-           is desired, *as will almost always be the case*, the probability
-           discovered in the chi-square table should be *halved*.
+        \chi_L^2 = \frac{\left[12L-3mn(n+1)^2\right]^2}{mn^2(n^2-1)(n+1)}
 
-    Here, the *p*-value always corresponds with a one-sided test. The
-    asymptotic calculation is not adjusted for the possibility of ties,
-    which, according to [2]_, does not violate the significance level of the
-    result.
+    "which is distributed approximately as chi-square with 1 degree of
+    freedom. The ordinary use of :math:`\chi^2` tables would be
+    equivalent to a two-sided test of agreement. If a one-sided test
+    is desired, *as will almost always be the case*, the probability
+    discovered in the chi-square table should be *halved*."
+
+    However, this standardized statistic does not distinguish between the
+    observed values being well correlated with the predicted ranks and being
+    _anti_-correlated with the predicted ranks. Instead, we follow [2]_
+    and calculate the standardized statistic
+
+    .. math::
+
+        \Lambda = \frac{L - E_0}{\sqrt{V_0}},
+
+    where :math:`E_0 = \frac{1}{4} mn(n+1)^2` and
+    :math:`V_0 = \frac{1}{144} mn^2(n+1)(n^2-1)`, "which is asymptotically
+    normal under the null hypothesis". The asymptotic calculation is not
+    adjusted for the possibility of ties, which, according to [2]_, does not
+    violate the significance level of the result.
 
     The *p*-value for ``method='exact'`` is generated by comparing the observed
     value of :math:`L` against the :math:`L` values generated for all
@@ -199,7 +212,7 @@ def pagel(data, ranked=True, hypothesis=None, method='auto', n_s=2000,
     ratings and thus the highest ranks, the column corresponding with
     seminar rankings should be first. In fact, according to our alternative
     hypothesis, the order of the columns should be reversed. We pass the
-    properly rearranged table of ranks into `pagel`.
+    rearranged table of ranks into `pagel`.
 
     >>> from scipy.stats import pagel
     >>> arranged_ranks = ranks[:, ::-1]
@@ -209,20 +222,35 @@ def pagel(data, ranked=True, hypothesis=None, method='auto', n_s=2000,
 
     The value of the :math:`L` statistic, 133.5, is as expected:
 
-    >>> print(res.statistic == ([3, 2, 1] * arranged_ranks).sum())
+    >>> import numpy as np
+    >>> m, n = arranged_ranks.shape
+    >>> predicted_ranks = np.arange(n, 0, -1)
+    >>> L = (predicted_ranks * np.sum(arranged_ranks, axis=0)).sum()
+    >>> print(res.statistic == L)
     True
 
-    The *p*-value of 0.001269 indicates that there is a 0.1269% chance that
+    The *p*-value is the survival function of the normal distribution evaluated
+    at the standardized test statistic:
+
+    >>> from scipy.stats import norm
+    >>> E0 = (m*n*(n+1)**2)/4
+    >>> V0 = (m*n**2*(n+1)*(n**2-1))/144
+    >>> Lambda = (L-E0)/np.sqrt(V0)
+    >>> p = norm.sf(Lambda)
+    >>> print(res.pvalue == p)
+    True
+
+    This value indicates that there is a 0.1269% chance that
     the :math:`L` statistic would reach such an extreme value under the null
     hypothesis. Because 0.1269% is less than 1%, we have evidence to reject
     the null hypothesis in favor of our alternative at a 99% confidence level.
 
-    Note that the calculation above can be simplified using the ``ranked``
-    and ``hypothesis`` parameters.
+    Note that the we can also pass in the data as originally tabulated if we
+    also provide ``ranked`` and ``predicted_ranks`` arguments:
 
-    >>> res = pagel(table,                # data as originally tabulated
-                    ranked=False,         # original data is not ranked
-                    hypothesis=[2, 1, 0]  # hypothesized order of means
+    >>> res = pagel(table,                      # data as originally tabulated
+                    ranked=False,               # originally, data was not ranked
+                    predicted_ranks=[1, 2, 3]   # originally, data was in order of increasing rank
                     )
     >>> print(res)
     Page_L_Result(statistic=133.5, pvalue=0.0012693433690751756, method='asymptotic')
@@ -230,19 +258,20 @@ def pagel(data, ranked=True, hypothesis=None, method='auto', n_s=2000,
     """
 
     data = np.array(data, copy=False)
+    m, n = data.shape
 
     if not ranked:
         data = scipy.stats.rankdata(data, axis=1)
 
-    if hypothesis is not None:
-        data = data[:, hypothesis]
+    if predicted_ranks is None:
+        predicted_ranks = np.arange(n, 0, -1)
 
-    m, n = data.shape
-
-    ranks = np.arange(n, 0, -1)
+    # Calculate the L statistic
+    colsums = np.sum(data, axis=0)
+    products = predicted_ranks * colsums
+    L = products.sum()
 
     method = "asymptotic"
-    L = (ranks * data).sum()
 
     # Using [1] as a reference, the asymptotic p-value would be calculated as:
     # chi_L = (12*L - 3*m*n*(n+1)**2)**2/(m*n**2*(n**2-1)*(n+1))
@@ -252,6 +281,7 @@ def pagel(data, ranked=True, hypothesis=None, method='auto', n_s=2000,
     # Use [2] page 151
     E0 = (m*n*(n+1)**2)/4
     V0 = (m*n**2*(n+1)*(n**2-1))/144
-    p = norm.sf((L-E0)/np.sqrt(V0))
+    Lambda = (L-E0)/np.sqrt(V0)
+    p = norm.sf(Lambda)
 
     return Page_L_Result(L, p, method)
