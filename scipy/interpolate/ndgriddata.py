2c2e239d2b24c9b4d0c45bc33dab07d630735bd4
Atsushi Sakai <asakai.amsl+github@gmail.com>
2020-07-20 20:06:11 +0900

    add Examples add see also for LinearNDInterpolator, NearestNDInterpolator, and CloughTocher2DInterpolator.

--- scipy/interpolate/ndgriddata.py
+++ scipy/interpolate/ndgriddata.py
@@ -51,6 +51,36 @@ class NearestNDInterpolator(NDInterpolatorBase):
     -----
     Uses ``scipy.spatial.cKDTree``
 
+    Examples
+    --------
+    We can interpolate values on a 2D plane:
+
+    >>> from scipy.interpolate import NearestNDInterpolator
+    >>> import matplotlib.pyplot as plt
+    >>> np.random.seed(0)
+    >>> x = np.random.random(10) - 0.5
+    >>> y = np.random.random(10) - 0.5
+    >>> z = np.hypot(x, y)
+    >>> X = np.linspace(min(x), max(x))
+    >>> Y = np.linspace(min(y), max(y))
+    >>> X, Y = np.meshgrid(X, Y)  # 2D grid for interpolation
+    >>> interp = NearestNDInterpolator(list(zip(x, y)), z)
+    >>> Z = interp(X, Y)
+    >>> plt.pcolormesh(X, Y, Z)
+    >>> plt.plot(x, y, "ok", label="input point")
+    >>> plt.legend()
+    >>> plt.colorbar()
+    >>> plt.show()
+
+    See also
+    --------
+    griddata :
+        Interpolate unstructured D-D data.
+    LinearNDInterpolator :
+        Piecewise linear interpolant in N dimensions.
+    CloughTocher2DInterpolator :
+        Piecewise cubic, C1 smooth, curvature-minimizing interpolant in 2D.
+
     """
 
     def __init__(self, x, y, rescale=False, tree_options=None):
