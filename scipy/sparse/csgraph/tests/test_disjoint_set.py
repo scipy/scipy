7845101e35eba3d2c6b24d15883b496e51d75bff
pmla <peter.mahler.larsen@gmail.com>
2020-07-23 12:40:04 +0200

    better testing

--- scipy/sparse/csgraph/tests/test_disjoint_set.py
+++ scipy/sparse/csgraph/tests/test_disjoint_set.py
@@ -30,23 +30,28 @@ def test_self_unions():
 
 
 def test_equal_size_ordering():
-    n = 10
+    n = 20
     dis = DisjointSet(n)
+    rng = np.random.RandomState(seed=0)
 
     for i in range(0, n, 2):
-        assert dis.union(i, i + 1)
+        indices = [i, i + 1]
+        rng.shuffle(indices)
+        assert dis.union(indices[0], indices[1])
         assert dis.find(i) == i
         assert dis.find(i + 1) == i
 
 
 def test_binary_tree():
-    kmax = 4
+    kmax = 10
     n = 2**kmax
     dis = DisjointSet(n)
+    rng = np.random.RandomState(seed=0)
 
     for k in 2**np.arange(kmax):
         for i in range(0, n, 2 * k):
-            assert dis.union(i, i + k)
+            r1, r2 =  + rng.randint(0, k, size=2)
+            assert dis.union(i + r1, i + k + r2)
 
         roots = [dis.find(i) for i in range(n)]
         expected = np.arange(n) - np.arange(n) % (2 * k)
