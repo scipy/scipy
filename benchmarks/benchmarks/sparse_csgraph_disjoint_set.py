363903601a4ee9b9053febcc4c80c03789cf6cc0
pmla <peter.mahler.larsen@gmail.com>
2020-08-04 19:28:17 +0200

    more benchmark cases

--- benchmarks/benchmarks/sparse_csgraph_disjoint_set.py
+++ benchmarks/benchmarks/sparse_csgraph_disjoint_set.py
@@ -19,26 +19,41 @@ class Bench(Benchmark):
         self.nodes = np.unique(self.edges)
         self.disjoint_set = DisjointSet(self.nodes)
 
-        self.premerged = DisjointSet(self.nodes)
+        self.pre_merged = DisjointSet(self.nodes)
         for a, b in self.edges:
-            self.premerged.merge(a, b)
+            self.pre_merged.merge(a, b)
+
+        self.pre_merged_found = DisjointSet(self.nodes)
+        for a, b in self.edges:
+            self.pre_merged_found.merge(a, b)
+        for x in self.nodes:
+            self.pre_merged_found[x]
 
     def time_merge(self, n):
         dis = self.disjoint_set
         for a, b in self.edges:
             dis.merge(a, b)
 
+    def time_merge_already_merged(self, n):
+        dis = self.pre_merged
+        for a, b in self.edges:
+            dis.merge(a, b)
+
     def time_find(self, n):
-        dis = self.premerged
+        dis = self.pre_merged
+        return [dis[i] for i in self.nodes]
+
+    def time_find_already_found(self, n):
+        dis = self.pre_merged_found
         return [dis[i] for i in self.nodes]
 
     def time_contains(self, n):
         # Test for presence
-        assert self.nodes[0] in self.premerged
-        assert self.nodes[n // 2] in self.premerged
-        assert self.nodes[-1] in self.premerged
+        assert self.nodes[0] in self.pre_merged
+        assert self.nodes[n // 2] in self.pre_merged
+        assert self.nodes[-1] in self.pre_merged
 
         # Test for absense
-        assert None not in self.premerged
-        assert "dummy" not in self.premerged
-        assert (1, 2, 3) not in self.premerged
+        assert None not in self.pre_merged
+        assert "dummy" not in self.pre_merged
+        assert (1, 2, 3) not in self.pre_merged
