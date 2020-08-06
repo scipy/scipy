BUG: update median averaging in signal.csd
Separately median-average real and imaginary components of complex
CSDs. This avoids unexpected behavior when np.median is passed a
complex array.

This addresses #12669

scipy/signal/spectral.py
@@ -585,7 +585,13 @@ def csd(x, y, fs=1.0, window='hann', nperseg=None, noverlap=None, nfft=None,
     if len(Pxy.shape) >= 2 and Pxy.size > 0:
         if Pxy.shape[-1] > 1:
             if average == 'median':
-                Pxy = np.median(Pxy, axis=-1) / _median_bias(Pxy.shape[-1])
+                # np.median must be passed real arrays for the desired result
+                if np.iscomplexobj(Pxy):
+                    Pxy = (np.median(np.real(Pxy), axis=-1)
+                           + 1j * np.median(np.imag(Pxy), axis=-1))
+                    Pxy /= _median_bias(Pxy.shape[-1])
+                else:
+                    Pxy = np.median(Pxy, axis=-1) / _median_bias(Pxy.shape[-1])
             elif average == 'mean':
                 Pxy = Pxy.mean(axis=-1)
             else: