d76c998290fce7750c83874287852b17208c9649
Yukti Khosla <44090430+Yukti-09@users.noreply.github.com>
2020-08-15 18:10:01 +0530

    Update 2DTransfrom.py

--- scipy/spatial/transform/2DTransfrom.py
+++ scipy/spatial/transform/2DTransfrom.py
@@ -6,36 +6,36 @@ def scaling(scaling_factor):
 	return(scaling_factor*(np.identity(2))) #This returns a scaling matrix
 
 def rotation(angle):
-	arr = np.empty([2, 2])
 	c = np.cos(angle)
 	s = np.sin(angle)
-	arr[0][0] = c
-	arr[0][1] = -s
-	arr[1][0] = s
-	arr[1][1] = c
+	arr = np.empty(c.shape + (2, 2), dtype=c.dtype)
+	arr[..., 0, 0] = c
+	arr[..., 0, 1] = -s
+	arr[..., 1, 0] = s
+	arr[..., 1, 1] = c
 
 	return arr #This returns a rotation matrix
 
 	
 def projection(angle):
-	arr = np.empty([2, 2])
 	c = np.cos(angle)
 	s = np.sin(angle)
-	arr[0][0] = c*c
-	arr[0][1] = c*s
-	arr[1][0] = c*s
-	arr[1][1] = s*s
+	arr = np.empty(c.shape + (2, 2), dtype=c.dtype)
+	arr[..., 0, 0] = c*c
+	arr[..., 0, 1] = c*s
+	arr[..., 1, 0] = c*s
+	arr[..., 1, 1] = s*s
 
 	return arr #This returns a rotation matrix
 
 
 def reflection(angle):
-	arr = np.empty([2, 2])
+	arr = np.empty(c.shape + (2, 2), dtype=c.dtype)
 	c = np.cos(angle)
 	s = np.sin(angle)
-	arr[0][0] = (2*c) -1
-	arr[0][1] = 2*s*c
-	arr[1][0] = 2*s*c
-	arr[1][1] = (2*s) -1
+	arr[..., 0, 0] = (2*c) -1
+	arr[..., 0, 1] = 2*c*s
+	arr[..., 1, 0] = 2*c*s
+	arr[..., 1, 1] = (2*s)-1
 
 	return arr #This returns a reflection matrix
