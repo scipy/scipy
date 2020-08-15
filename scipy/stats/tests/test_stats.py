1a10291e02a88853c8a378148b4c876d250a323c
Will Tirone <will.tirone1@gmail.com>
2020-08-15 10:11:16 -0400

    BUG: per #12654 adjusted mudholkar_george method to combine p values

--- scipy/stats/tests/test_stats.py
+++ scipy/stats/tests/test_stats.py
@@ -5027,10 +5027,10 @@ class TestCombinePvalues(object):
     def test_tippett(self):
         Z, p = stats.combine_pvalues([.01, .2, .3], method='tippett')
         assert_approx_equal(p, 0.970299, significant=4)
-
+        
     def test_mudholkar_george(self):
-        Z, p = stats.combine_pvalues([.01, .2, .3], method='mudholkar_george')
-        assert_approx_equal(p, 3.7191571041915e-07, significant=4)
+        Z, p = stats.combine_pvalues([.1, .1, .1], method='mudholkar_george')
+        assert_approx_equal(p, 0.019462, significant=4)
 
     def test_mudholkar_george_equal_fisher_minus_pearson(self):
         Z, p = stats.combine_pvalues([.01, .2, .3], method='mudholkar_george')
