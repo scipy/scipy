675a3460c134b753e4b3e959bfd72a6442968d4a
Matt Haberland <mdhaber@mit.edu>
2020-08-28 08:50:18 -0700

    FIXUP: optimize: correct reference numbers

--- scipy/optimize/_qap.py
+++ scipy/optimize/_qap.py
@@ -490,7 +490,7 @@ def _quadratic_assignment_faq(A, B,
         _check_init_input(init_J, n_unseed)
         J = init_J
 
-# [2] Algorithm 1 Step 1 - choose initialization
+# [1] Algorithm 1 Step 1 - choose initialization
     if init_weight != 1:
         # generate a nxn matrix where each entry is a random number [0, 1]
         # would use rand, but Generators don't have it
@@ -504,11 +504,11 @@ def _quadratic_assignment_faq(A, B,
         P = J
     const_sum = A21 @ B21.T + A12.T @ B12
 
-    # [2] Algorithm 1 Step 2 - loop while stopping criteria not met
+    # [1] Algorithm 1 Step 2 - loop while stopping criteria not met
     for n_iter in range(maxiter):
-        # [2] Algorithm 1 Step 3 - compute the gradient of f(P) = -tr(APB^tP^t)
+        # [1] Algorithm 1 Step 3 - compute the gradient of f(P) = -tr(APB^tP^t)
         grad_fp = (const_sum + A22 @ P @ B22.T + A22.T @ P @ B22)
-        # [2] Algorithm 1 Step 4 - get direction Q by solving Eq. 8
+        # [1] Algorithm 1 Step 4 - get direction Q by solving Eq. 8
         rows, cols = linear_sum_assignment(obj_func_scalar * grad_fp)
         Q = np.zeros((n_unseed, n_unseed))
         Q[rows, cols] = 1  # initialize search direction matrix Q
@@ -527,16 +527,16 @@ def _quadratic_assignment_faq(A, B,
                 + ((xP1xQ.T @ A22) * (B22 @ xP1xQ.T)).sum()
             )
 
-        # [2] Algorithm 1 Step 5 - compute the step size
+        # [1] Algorithm 1 Step 5 - compute the step size
         alpha = minimize_scalar(f, bounds=(0, 1), method="bounded").x
-        # [2] Algorithm 1 Step 6 - Update P
+        # [1] Algorithm 1 Step 6 - Update P
         P_i1 = alpha * P + (1 - alpha) * Q
         if np.linalg.norm(P - P_i1) < tol:
             break
         P = P_i1
-    # [2] Algorithm 1 Step 7 - end main loop
+    # [1] Algorithm 1 Step 7 - end main loop
 
-    # [2] Algorithm 1 Step 8 - project onto the set of permutation matrices
+    # [1] Algorithm 1 Step 8 - project onto the set of permutation matrices
     _, col = linear_sum_assignment(-P)
     perm = np.concatenate((np.arange(n_seeds), col + n_seeds))
 
