BUG: stats: Fix formula in the `stats` method of the ARGUS distribution.
Replace `np.exp(-chi2/4) * sc.iv(1, chi2/4)` with `sc.ive(1, chi2/4)`.
This is exactly what `ive` is for.

Before:

    >>> argus.mean(60)
    [...]/_continuous_distns.py:8542: RuntimeWarning: invalid value encountered in multiply
      m = np.sqrt(np.pi/8) * chi * np.exp(-chi2/4) * sc.iv(1, chi2/4) / phi
    nan

After:

    >>> argus.mean(60)
    0.99958318851653

scipy/stats/_continuous_distns.py
@@ -8395,7 +8395,7 @@ class argus_gen(rv_continuous):
     def _stats(self, chi):
         chi2 = chi**2
         phi = _argus_phi(chi)
-        m = np.sqrt(np.pi/8) * chi * np.exp(-chi2/4) * sc.iv(1, chi2/4) / phi
+        m = np.sqrt(np.pi/8) * chi * sc.ive(1, chi2/4) / phi
         v = (1 - 3 / chi2 + chi * _norm_pdf(chi) / phi) - m**2
         return m, v, None, None
 