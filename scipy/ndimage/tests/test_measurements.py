fa4228a56a0d19c5ff9ea4157c84029fcd3d952a
Gregory Lee <grlee77@gmail.com>
2020-09-12 12:33:00 -0400

    pep8 fix

--- scipy/ndimage/tests/test_measurements.py
+++ scipy/ndimage/tests/test_measurements.py
@@ -701,14 +701,14 @@ def test_median03():
 def test_median_gh12836_bool():
     # test boolean addition fix on example from gh-12836
     a = np.asarray([1, 1], dtype=np.bool)
-    output = ndimage.median(a, labels=np.ones((2,)), index=[1,])
+    output = ndimage.median(a, labels=np.ones((2,)), index=[1])
     assert_array_almost_equal(output, [1.0])
 
 
 def test_median_no_int_overflow():
     # test integer overflow fix on example from gh-12836
     a = np.asarray([65, 70], dtype=np.int8)
-    output = ndimage.median(a, labels=np.ones((2,)), index=[1,])
+    output = ndimage.median(a, labels=np.ones((2,)), index=[1])
     assert_array_almost_equal(output, [67.5])
 
 
