9ff37c9d1a002ee699a35ba4d796bca8ce4be752
Trevor Andersen <trevorandersen@users.noreply.github.com>
2020-09-15 22:32:57 +1000

    update wavefile.py to fix write example in docs

--- scipy/io/wavfile.py
+++ scipy/io/wavfile.py
@@ -742,10 +742,11 @@ def write(filename, rate, data):
     Write to 16-bit PCM, Mono.
 
     >>> from scipy.io.wavfile import write
+    >>> import numpy as np
     >>> samplerate = 44100; fs = 100
     >>> t = np.linspace(0., 1., samplerate)
     >>> amplitude = np.iinfo(np.int16).max
-    >>> data = amplitude * np.sin(2. * np.pi * fs * t)
+    >>> data = np.int16(amplitude * np.sin(2. * np.pi * fs * t))
     >>> write("example.wav", samplerate, data.astype(np.int16))
 
     """
