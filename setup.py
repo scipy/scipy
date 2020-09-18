6b285592e503fdcfb385085ed9e87cc68b0ea8cd
Ralf Gommers <ralf.gommers@gmail.com>
2020-09-18 16:10:02 +0100

    BLD: also pin pybind11

--- setup.py
+++ setup.py
@@ -521,7 +521,7 @@ def setup_package():
                           else [])
 
     install_requires = build_requires
-    setup_requires = build_requires + ['pybind11>=2.4.3']
+    setup_requires = build_requires + ['pybind11>=2.4.3,<=2.5.0']
 
     metadata = dict(
         name='scipy',
