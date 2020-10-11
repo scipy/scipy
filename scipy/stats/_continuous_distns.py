MAINT: fix numerical precision in nct.stats
  * for values of df > 344 the stats method
    of nct would raise runtime warnings and
    become nan unnecessarily.

MAINT: add fix for df < 2 cases to nan

Update scipy/stats/tests/test_distributions.py

Co-authored-by: Warren Weckesser <warren.weckesser@gmail.com>

Update scipy/stats/tests/test_distributions.py

Co-authored-by: Warren Weckesser <warren.weckesser@gmail.com>

bump rtol 1e-13 to 1e-10 after mpmath

scipy/stats/_continuous_distns.py
@@ -5912,12 +5912,12 @@ class nct_gen(rv_continuous):
         #
         mu, mu2, g1, g2 = None, None, None, None
 
-        gfac = sc.gamma(df/2.-0.5) / sc.gamma(df/2.)
+        gfac = np.exp(sc.betaln(df/2-0.5, 0.5) - sc.gammaln(0.5))
         c11 = np.sqrt(df/2.) * gfac
-        c20 = df / (df-2.)
+        c20 = np.where(df > 2., df / (df-2.), np.nan)
         c22 = c20 - c11*c11
-        mu = np.where(df > 1, nc*c11, np.inf)
-        mu2 = np.where(df > 2, c22*nc*nc + c20, np.inf)
+        mu = np.where(df > 1, nc*c11, np.nan)
+        mu2 = np.where(df > 2, c22*nc*nc + c20, np.nan)
         if 's' in moments:
             c33t = df * (7.-2.*df) / (df-2.) / (df-3.) + 2.*c11*c11
             c31t = 3.*df / (df-2.) / (df-3.)