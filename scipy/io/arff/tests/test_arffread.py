b827a81821d459b12224b2e24db5d365c309abeb
Jonas Jonker <jonasjonker@users.noreply.github.com>
2020-11-01 12:07:25 +0100

    BUG: added line.strip() to split_data_line()  (#12964)
    added test data with trailing and leading spaces in  class @ATTRIBUTE  declaration

--- scipy/io/arff/tests/test_arffread.py
+++ scipy/io/arff/tests/test_arffread.py
@@ -57,6 +57,10 @@ class TestData(object):
     def test3(self):
         # Parsing trivial file with nominal attribute of 1 character.
         self._test(test6)
+        
+    def test4(self):
+        # Parsing trivial file with trailing spaces in attribute declaration.
+        self._test(test11)
 
     def _test(self, test_file):
         data, meta = loadarff(test_file)
