384652a535400770c8b4010339928ca3874b8304
endolith <endolith@gmail.com>
2020-11-17 07:53:11 -0500

    DOC: Add descriptions to scipy.fft.__doc__
    Addresses https://github.com/scipy/scipy/pull/4607#discussion_r523772549

--- scipy/fft/__init__.py
+++ scipy/fft/__init__.py
@@ -60,11 +60,11 @@ Chirp Z-transform and Zoom FFT
 .. autosummary::
    :toctree: generated/
 
-   czt
-   zoomfft
-   CZT
-   ZoomFFT
-   czt_points
+   czt - Chirp z-transform convenience function
+   zoomfft - Zoom FFT convenience function
+   CZT - Chirp z-transform function generator
+   ZoomFFT - Zoom FFT function generator
+   czt_points - Output the z-plane points sampled by a chirp z-transform
 
 The functions are simpler to use, but less efficient, because they internally
 generate the same chirp signals with every call.  When using the same
