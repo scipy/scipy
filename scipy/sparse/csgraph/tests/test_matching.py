92e41f4247fa4fad7cfc31fbf2695f80da7bf126
Søren Fuglede Jørgensen <s@fuglede.dk>
2020-11-19 20:42:26 +0100

    MAINT: use parentheses for line continuations

--- scipy/sparse/csgraph/tests/test_matching.py
+++ scipy/sparse/csgraph/tests/test_matching.py
@@ -3,8 +3,9 @@ from numpy.testing import assert_array_equal, assert_equal
 import pytest
 
 from scipy.sparse import csr_matrix, coo_matrix, diags
-from scipy.sparse.csgraph import maximum_bipartite_matching,\
-    min_weight_full_bipartite_matching
+from scipy.sparse.csgraph import (
+    maximum_bipartite_matching, min_weight_full_bipartite_matching
+)
 
 
 def test_maximum_bipartite_matching_raises_on_dense_input():
