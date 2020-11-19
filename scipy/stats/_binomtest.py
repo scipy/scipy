b3909465b395d8175a5332ce0962fb16015aeddf
Warren Weckesser <warren.weckesser@gmail.com>
2020-11-19 02:04:38 -0500

    DOC: stats: Tweak docstrings for binomtest and its return value; add refs.

--- scipy/stats/_binomtest.py
+++ scipy/stats/_binomtest.py
@@ -41,25 +41,38 @@ class BinomTestResult:
             Selects the method used to compute the confidence interval
             for the estimate of the proportion:
 
-                ``'exact'`` :
-                    Use the Clopper-Pearson exact method
-                ``'wilson'`` :
-                    Wilson's method, without continuity correction
-                ``'wilsoncc'`` :
-                    Wilson's method, with continuity correction
+            'exact' :
+                Use the Clopper-Pearson exact method [1]_.
+            'wilson' :
+                Wilson's method, without continuity correction ([2]_, [3]_).
+            'wilsoncc' :
+                Wilson's method, with continuity correction ([2]_, [3]_).
 
             Default is ``'exact'``.
 
         Returns
         -------
-        ci : A namedtuple containing the lower and upper limits
-            of the confidence interval.
+        ci : namedtuple with fields ``low`` and ``high``
+            Contains the lower and upper bounds of the confidence interval.
+
+        References
+        ----------
+        .. [1] C. J. Clopper and E. S. Pearson, The use of confidence or
+               fiducial limits illustrated in the case of the binomial,
+               Biometrika, Vol. 26, No. 4, pp 404-413 (Dec. 1934).
+        .. [2] E. B. Wilson, Probable inference, the law of succession, and
+               statistical inference, J. Amer. Stat. Assoc., 22, pp 209-212
+               (1927).
+        .. [3] Robert G. Newcombe, Two-sided confidence intervals for the
+               single proportion: comparison of seven methods, Statistics
+               in Medicine, 17, pp 857-872 (1998).
         """
         if method not in ('exact', 'wilson', 'wilsoncc'):
             raise ValueError("method must be one of 'exact', 'wilson' or "
                              "'wilsoncc'.")
         if not (0 <= confidence_level <= 1):
-            raise ValueError('confidence_level must be in the interval [0, 1].')
+            raise ValueError('confidence_level must be in the interval '
+                             '[0, 1].')
         if method == 'exact':
             low, high = _binom_exact_conf_int(self.k, self.n,
                                               confidence_level,
@@ -160,8 +173,11 @@ def binomtest(k, *, n=None, p=0.5, alternative='two-sided'):
     """
     Perform a test that the probability of success is p.
 
-    This is a test of the null hypothesis that the probability of success
-    in a Bernoulli experiment is `p`.
+    The binomial test [1]_ is a test of the null hypothesis that the
+    probability of success in a Bernoulli experiment is `p`.
+
+    Details of the test can be found in many texts on statistics, such
+    as section 24.5 of [2]_.
 
     Parameters
     ----------
@@ -181,20 +197,27 @@ def binomtest(k, *, n=None, p=0.5, alternative='two-sided'):
     result : BinomTestResult instance
         The return value is an object with the following attributes:
 
-        * `k` : int
-        * `n` : int
-        * `alternative` : str
-        * `pvalue` : float
-        * `proportion_estimate` : float
+        k : int
+            Copied from the input.
+        n : int
+            Copied from the input.
+        alternative : str
+            Copied from the input.
+        pvalue : float
+            The p-value of the hypothesis test.
+        proportion_estimate : float
+            The estimate of the proportion of successes.
 
         The object has the following methods:
 
-        * `proportion_ci(confidence_level=0.95, method='exact')`
+        proportion_ci(confidence_level=0.95, method='exact') :
             Compute the confidence interval for ``proportion_estimate``.
 
     References
     ----------
-    .. [1] https://en.wikipedia.org/wiki/Binomial_test
+    .. [1] Binomial test, https://en.wikipedia.org/wiki/Binomial_test
+    .. [2] Jerrold H. Zar, Biostatistical Analysis (fifth edition),
+           Prentice Hall, Upper Saddle River, New Jersey USA (2010)
 
     Examples
     --------
