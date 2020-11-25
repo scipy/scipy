f8663682e49e5a13eae758515ae99336be91ed94
Andrew Nelson <andyfaff@gmail.com>
2020-11-25 17:29:49 +1100

    MAINT: correct refguide check

--- scipy/optimize/_basinhopping.py
+++ scipy/optimize/_basinhopping.py
@@ -356,7 +356,8 @@ def basinhopping(func, x0, niter=100, T=1.0, stepsize=0.5,
     x0 : array_like
         Initial guess.
     niter : integer, optional
-        The number of basin-hopping iterations
+        The number of basin-hopping iterations. There will be a total of
+        ``niter + 1`` runs of the local minimizer.
     T : float, optional
         The "temperature" parameter for the accept or reject criterion. Higher
         "temperatures" mean that larger jumps in function value will be
@@ -601,6 +602,7 @@ def basinhopping(func, x0, niter=100, T=1.0, stepsize=0.5,
     >>> np.random.seed(1)
     >>> ret = basinhopping(func2d, x0, minimizer_kwargs=minimizer_kwargs,
     ...                    niter=10, callback=print_fun)
+    at minimum 0.4159 accepted 1
     at minimum 0.4159 accepted 1
     at minimum -0.9073 accepted 1
     at minimum -0.1021 accepted 1
