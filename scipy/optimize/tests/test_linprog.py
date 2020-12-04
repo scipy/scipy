da16151badd22b341b79071c332ebb761531f31d
mckib2 <nicholas.bgp@gmail.com>
2020-12-03 19:56:09 -0700

    BUG: skip 0 iter test for infeasible problem due to HiGHS presolve

--- scipy/optimize/tests/test_linprog.py
+++ scipy/optimize/tests/test_linprog.py
@@ -736,7 +736,9 @@ class LinprogCommonTests(object):
         _assert_infeasible(res)
 
         # Infeasibility detected in presolve
-        if self.options.get('presolve', True):
+        # TODO: HiGHS presolve for simplex/ipx currently not finding this case;
+        #       see https://github.com/ERGO-Code/HiGHS/issues/425
+        if self.options.get('presolve', True) and 'highs' not in self.method:
             assert_equal(res.nit, 0)
 
     def test_singleton_row_eq_2(self):
