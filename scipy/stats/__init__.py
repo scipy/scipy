0f74d8b62282372d651b481d4a1a85de8b8dcdaf
ace-e4s <ali.cetin@4subsea.com>
2020-12-08 21:11:10 +0100

    update init

--- scipy/stats/__init__.py
+++ scipy/stats/__init__.py
@@ -117,7 +117,7 @@ Continuous distributions
    triang            -- Triangular
    truncexpon        -- Truncated Exponential
    truncnorm         -- Truncated Normal
-   truncweibull_min  -- Left truncated minimum Weibull distribution
+   truncweibull_min  -- Truncated minimum Weibull distribution
    tukeylambda       -- Tukey-Lambda
    uniform           -- Uniform
    vonmises          -- Von-Mises (Circular)
