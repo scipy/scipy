8ffe18c4bab56501baa500a4a3487043eeacaad3
Matt Haberland <mhaberla@calpoly.edu>
2020-12-29 12:03:22 -0800

    ENH: stats: added input validation and basic documentation to mannwhitneyu2

--- scipy/stats/_mannwhitneyu.py
+++ scipy/stats/_mannwhitneyu.py
@@ -5,7 +5,6 @@ from scipy import special
 from scipy import stats
 
 
-
 def _broadcast_concatenate(x, y, axis):
     '''Broadcast then concatenate arrays, leaving concatenation axis last'''
     x = x.swapaxes(axis, -1)
@@ -95,31 +94,118 @@ _mwu_state = _MWU()
 def _get_mwu_z(U, n1, n2, ranks, axis=0, continuity=True):
     '''Standardized MWU statistic'''
     # Follows mannwhitneyu2 [2]
-    n = n1 + n2
-    c = -0.5 * continuity
     m_u = n1 * n2 / 2
+    n = n1 + n2
 
     # Tie correction. scipy.stats.tiecorrect is not vectorized; this is.
+    # element i of t is the number of subjects sharing rank i
     _, t = np.unique(ranks, return_counts=True, axis=-1)
-    s = np.sqrt(n1*n2/12 * (n1 + n2 + 1 - (t**3 - t).sum(axis=-1)/(n*(n-1))))
+    s = np.sqrt(n1*n2/12 * ((n + 1) - (t**3 - t).sum(axis=-1)/(n*(n-1))))
 
-    z = (U + c - m_u) / s
+    c = -0.5 * continuity
+    z = (U - m_u + c) / s
     return z
 
 
+def _mwu_input_validation(x, y, use_continuity, alternative, axis, method):
+    ''' Input validation and standardization for mannwhitneyu2 '''
+    # Would use np.asarray_chkfinite, but infs are OK
+    x, y = np.asarray(x), np.asarray(y)
+    if np.isnan(x).any() or np.isnan(y).any():
+        raise ValueError('`x` and `y` must not contain NaNs.')
+    if np.size(x) == 0 or np.size(y) == 0:
+        raise ValueError('`x` and `y` must be of nonzero size.')
+
+    bools = {True, False}
+    if use_continuity not in bools:
+        raise ValueError(f'`use_continuity` must be one of {bools}.')
+
+    alternatives = {"two-sided", "less", "greater"}
+    alternative = alternative.lower()
+    if alternative not in alternatives:
+        raise ValueError(f'`alternative` must be one of {alternatives}.')
+
+    axis_int = int(axis)
+    if axis != axis_int or axis_int < 0:
+        raise ValueError('`index` must be a positive integer.')
+
+    methods = {"asymptotic", "exact"}
+    method = method.lower()
+    if method not in methods:
+        raise ValueError(f'`method` must be one of {methods}.')
+
+    return x, y, use_continuity, alternative, axis_int, method
+
 mwu_result = make_dataclass("MannWhitneyUResult", ("statistic", "pvalue"))
 # Using `nametuple` for now to pass existing mannwhitneyu tests without
-# modificatino
+# modification
 MannwhitneyuResult = namedtuple('MannwhitneyuResult', ('statistic', 'pvalue'))
 
 
-def mannwhitneyu2(x, y, continuity=True, alternative=None, axis=0,
-                  exact=False):
-    '''Mann-Whitney U Test
+def mannwhitneyu2(x, y, use_continuity=True, alternative="two-sided",
+                  axis=0, method="asymptotic"):
+    '''Perform the Mann-Whitney U rank test on two independent samples.
 
-    Yet another implementation. Currently allows n-d x and y for asymptotic
-    test only, but exact test can be vectorized, too. Calculation of
-    exact distribution is memoized.
+    The Mann-Whitney U test is a nonparametric test of the null hypothesis
+    that the distribution underlying sample `x` is neither stochastically
+    greater nor stochastically less than the distribution underlying sample
+    `y`.
+
+    Parameters
+    ----------
+    x, y : array-like
+        N-d arrays of samples. The arrays must be broadcastable except along
+        the dimension given by `axis`.
+    use_continuity : bool, optional
+            Whether a continuity correction (1/2) should be applied.
+            Default is True when `method` is `asymptotic`; has no effect
+            otherwise.
+    alternative : {'two-sided', 'less', 'greater'}, optional
+        Defines the alternative hypothesis.
+        The following options are available:
+
+          * 'two-sided' (default)
+          * 'less': one-sided
+          * 'greater': one-sided
+
+    axis : int, optional
+        Axis along which to perform the test. Default is 0.
+    method : {'asymptotic', 'exact'}, optional
+        Selects the method used to calculate the *p*-value. The following
+        options are available.
+        * 'asymptotic': (default) compares the standardized test statistic
+          against the normal distribution, correcting for ties.
+        * 'exact': computes the exact *p*-value by comparing the observed
+          :math:`L` statistic against those realized by all possible
+          permutations of ranks (under the null hypothesis that each
+          permutation is equally likely)
+
+    Returns
+    -------
+    res : MannwhitneyuResult
+        An object containing attributes:
+
+        statistic : float
+            The Mann-Whitney U statistic corresponding with sample `x`.
+        pvalue : float
+            The associated *p*-value for the chosen `alternative`.
+
+    Notes
+    -----
+    `method` 'exact' is recommended when there are no ties and either sample
+    size is less than 8 _[1]. The implementation follows the original
+    recurrence relation proposed in _[1] as described in _[3].
+
+    If ``U1`` is the statistic corresponding with sample `x`, then the
+    statistic corresponding with sample `y` is
+    ``x.shape[axis] * y.shape[axis] - U1``.
+
+    `mannwhitneyu2` is for independent samples. For related samples,
+    consider `wilcoxon`.
+
+    See Also
+    --------
+    `wilcoxon`
 
     References
     ----------
@@ -132,28 +218,30 @@ def mannwhitneyu2(x, y, continuity=True, alternative=None, axis=0,
            Wilcoxon-Mann-Whitney test", Journal of Statistical Planning and
            Inference, Vol. 79, pp. 349-364, 1999.
     '''
-    x, y = np.asarray(x), np.asarray(y)
+
+    x, y, use_continuity, alternative, axis_int, method = (
+        _mwu_input_validation(x, y, use_continuity, alternative, axis, method))
+
     x, y, xy = _broadcast_concatenate(x, y, axis)
 
     # Follows [2]
     n1, n2 = x.shape[-1], y.shape[-1]
-    ranks = stats.rankdata(xy, axis=-1)
-    R1 = ranks[..., :n1].sum(axis=-1)
-    R2 = ranks[..., n1:].sum(axis=-1)
-    U1 = R1 - n1*(n1+1)/2
-    U2 = R2 - n2*(n2+1)/2
+    ranks = stats.rankdata(xy, axis=-1)  # method 2, step 1
+    R1 = ranks[..., :n1].sum(axis=-1)    # method 2, step 2
+    U1 = R1 - n1*(n1+1)/2                # method 2, step 3
+    U2 = n1 * n2 - U1                    # as U1 + U2 = n1 * n2
 
     if alternative == "greater":
         U, f = U1, 1  # U is the statistic to use for p-value, f is a factor
     elif alternative == "less":
-        U, f = U2, 1
+        U, f = U2, 1  # Due to symmetry, use SF of U2 rather than CDF of U1
     else:
-        U, f = np.maximum(U1, U2), 2  # multiply by two for two-sided test
+        U, f = np.maximum(U1, U2), 2  # multiply SF by two for two-sided test
 
-    if exact:
+    if method == "exact":
         p = _mwu_state.sf(U.astype(int), n1, n2)
-    else:
-        z = _get_mwu_z(U, n1, n2, ranks, continuity=continuity)
+    elif method == "asymptotic":
+        z = _get_mwu_z(U, n1, n2, ranks, continuity=use_continuity)
         p = stats.norm.sf(z)
     p *= f
 
