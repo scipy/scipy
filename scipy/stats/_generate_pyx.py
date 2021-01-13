a56f5aa3369fabc80da0bbe39118d552b6b00e52
mckib2 <nicholas.bgp@gmail.com>
2021-01-12 22:18:11 -0700

    FIX: resolve Generator objects if numpy.random has them; check numpy version using pep440 tools

--- scipy/stats/_generate_pyx.py
+++ scipy/stats/_generate_pyx.py
@@ -1,9 +1,11 @@
 import pathlib
-import numpy as np
 
 def make_biasedurn():
     # numpy's random C API changes between 17 and 18
-    NPY_OLD = int(np.__version__.split('.')[1]) < 18
+    from numpy import __version__ as __numpy_version__
+    from scipy._lib import _pep440
+    NPY_OLD = _pep440.parse(__numpy_version__) < _pep440.Version('1.18')
+    del _pep440
     biasedurn_base = (pathlib.Path(__file__).parent / 'biasedurn').absolute()
     with open(biasedurn_base.with_suffix('.pyx.templ'), 'r') as src:
         contents = src.read()
