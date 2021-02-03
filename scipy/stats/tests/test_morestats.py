e24e32ee8af5ce974497de4f7f0d340911b819f6
ryu577 <rohitpandey576@gmail.com>
2021-02-03 15:44:34 -0800

    Added test for monotonic PMF

--- scipy/stats/tests/test_morestats.py
+++ scipy/stats/tests/test_morestats.py
@@ -715,6 +715,8 @@ class TestBinomTest:
         assert_allclose(res.pvalue, 0.19667729017182273, rtol=1e-8)
         res = stats.binomtest(3, 10, .5)
         assert_allclose(res.pvalue, 0.3437499999999999, rtol=1e-8)
+        res = stats.binomtest(2, 2, .4)
+        assert_allclose(res.pvalue, 0.16000000000000003, rtol=1e-8)
 
     # Expected results here are from R 3.6.2 binom.test
     @pytest.mark.parametrize('alternative, pval, ci_low, ci_high',
