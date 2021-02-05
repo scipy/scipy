bbb09a77bc0948351f8ffd6320faa0518c16a21d
Arthur Volant <arthurvolant@gmail.com>
2021-02-05 18:34:21 +0100

    DOC: Update _hypotests.py
    Update documentation over `num_it`
    param

--- scipy/stats/_hypotests.py
+++ scipy/stats/_hypotests.py
@@ -697,7 +697,10 @@ def barnard_exact(table, alternative="two-sided", pooled=True, num_it=3):
 
     num_it : int, optional
         Number of iterations of the grid search. Default is 3. Must be
-        non-negative.
+        non-negative. In most cases, 3 iterations is perfectly enough to
+        reach a precision a goood precision. Above a certain number (around
+        6 iterations), the result will not change anymore. Note that every
+        iteration added comes with a performance cost.
 
     Returns
     -------
