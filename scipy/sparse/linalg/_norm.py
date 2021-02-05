a8e9d29799f3c07da89dd82f53afc2b5c5608703
Joshua Stevenson <joshuas153@gmail.com>
2021-02-05 16:04:20 +1100

    ENH: add faster implementation of Frobenius norm for sparse matrices which support indexing

--- scipy/sparse/linalg/_norm.py
+++ scipy/sparse/linalg/_norm.py
@@ -11,9 +11,15 @@ __all__ = ['norm']
 
 def _sparse_frobenius_norm(x):
     if np.issubdtype(x.dtype, np.complexfloating):
-        sqnorm = abs(x).power(2).sum()
+        try:
+            sqnorm = sum(abs(x[i,j])**2 for i, j in zip(*x.nonzero()))**(1/2)
+        except TypeError:
+            sqnorm = abs(x).power(2).sum()
     else:
-        sqnorm = x.power(2).sum()
+        try:
+            sqnorm = sum(x[i,j]**2 for i, j in zip(*x.nonzero()))**(1/2)
+        except TypeError:
+            sqnorm = x.power(2).sum()
     return sqrt(sqnorm)
 
 
