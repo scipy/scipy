b57679a178c726eb98b176b44389cdf117f8cff3
Blair Bonnett <blair.bonnett@gmail.com>
2021-02-10 11:38:09 +0100

    BENCH: add signal_filtering.MedFilt2D benchmark.
    Contains time and peakmem benchmarks for single- and multi-threaded
    calculations using scipy.signal.medfilt2d.

--- benchmarks/benchmarks/signal_filtering.py
+++ benchmarks/benchmarks/signal_filtering.py
@@ -1,11 +1,13 @@
 import numpy as np
+from numpy.testing import assert_allclose
 import timeit
-from concurrent.futures import ThreadPoolExecutor, wait
+from concurrent.futures import ThreadPoolExecutor, as_completed, wait
 
 from .common import Benchmark, safe_import
 
 with safe_import():
-    from scipy.signal import lfilter, firwin, decimate, butter, sosfilt
+    from scipy.signal import (lfilter, firwin, decimate, butter, sosfilt,
+                              medfilt2d)
 
 
 class Decimate(Benchmark):
@@ -82,3 +84,54 @@ class Sosfilt(Benchmark):
 
     def time_sosfilt_basic(self, n_samples, order):
         sosfilt(self.sos, self.y)
+
+
+class MedFilt2D(Benchmark):
+    def setup_cache(self):
+        np.random.seed(8176)
+        data = np.random.randn(2000, 4000)
+        expected = medfilt2d(data, 5)
+        np.save("medfilt2d_data.npy", data)
+        np.save("medfilt2d_expected.npy", expected)
+
+    def setup(self):
+        self.data = np.load("medfilt2d_data.npy")
+        self.expected = np.load("medfilt2d_expected.npy")
+
+    def teardown(self):
+        assert_allclose(self.output, self.expected)
+
+    def _multithreaded(self):
+        # Lets take four overlapping chunks over the last axis. First pair of
+        # values is range to select from input, second pair range of the result
+        # to use, last pair the range of the output array to store it in.
+        chunks = (
+            (0, 1002, None, -2, 0, 1000),
+            (998, 2002, 2, -2, 1000, 2000),
+            (1998, 3002, 2, -2, 2000, 3000),
+            (2998, 4000, 2, None, 3000, 4000)
+        )
+
+        self.output = np.empty(self.data.shape, dtype=self.data.dtype)
+
+        def apply(chunk):
+            return medfilt2d(self.data[:, chunk[0]:chunk[1]], 5)
+
+        with ThreadPoolExecutor(max_workers=4) as executor:
+            futures = {executor.submit(apply, chk): chk for chk in chunks}
+            for future in as_completed(futures):
+                chk = futures[future]
+                result = future.result()
+                self.output[:, chk[4]:chk[5]] = result[:, chk[2]:chk[3]]
+
+    def time_singlethreaded(self):
+        self.output = medfilt2d(self.data, 5)
+
+    def time_multithreaded(self):
+        self._multithreaded()
+
+    def peakmem_singlethreaded(self):
+        self.output = medfilt2d(self.data, 5)
+
+    def peakmem_multithreaded(self):
+        self._multithreaded()
