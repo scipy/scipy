8c58fc075d7a1ae0cf7657962414ae2b772b610a
Arthur Volant <arthurvolant@gmail.com>
2021-02-28 18:45:13 +0100

    DOC: Update doc

--- scipy/stats/_hypotests.py
+++ scipy/stats/_hypotests.py
@@ -756,6 +756,9 @@ def barnard_exact(table, alternative="two-sided", pooled=True, n=32):
                 \frac{1}{c_2})
             }
         }
+    with :math:`\hat{p_1}, \hat{p_2}` and :math:`\hat{p}` the estimator of
+    :math:`p_1, p_2` and :math:`p`, the latter being the combined probability,
+    given the assumption that :math:`p_1 = p_2`.
 
     If this assumption is invalid (``pooled = False``), the statistic is:
 
@@ -776,14 +779,16 @@ def barnard_exact(table, alternative="two-sided", pooled=True, n=32):
 
         pvalue =
         \sum
-            \binom{total\_col\_1}{x_{11}}
-            \binom{total\_col\_2}{x_{12}}
+            \binom{c_1}{x_{11}}
+            \binom{c_2}{x_{12}}
             \pi^{x_{11} + x_{12}}
-            (1 - \pi)^{total - x_{11} - x_{12}}
+            (1 - \pi)^{n - x_{11} - x_{12}}
 
     where :math:`T(X) \leq T(X_0)`, if `alternative` = "less"
     :math:`T(X) \geq T(X_0)`, if `alternative` = "greater"
     :math:`T(X) \geq |T(X_0)|`, if `alternative` = "two-sided".
+    Above, :math:`c_1, c_2` are the sum of the columns 1 and 2,
+    and :math:`n` the total (sum of the 4 sample's element).
 
     The returned p-value is the maximum p-value taken over the nuisance
     parameter :math:`\pi`, where :math:`0 \leq \pi \leq 1`.
