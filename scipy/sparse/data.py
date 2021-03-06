FIX: sparse: cast C integral type to Python int to handle possible overflow

scipy/sparse/data.py
@@ -271,7 +271,9 @@ class _minmax_mixin(object):
                 m = mat.data[am]
 
                 if compare(m, zero):
-                    return mat.row[am] * mat.shape[1] + mat.col[am]
+                    # cast at least 1 to Python int to avoid overflow
+                    # and RuntimeError
+                    return int(mat.row[am]) * mat.shape[1] + mat.col[am]
                 else:
                     size = np.prod(mat.shape)
                     if size == mat.nnz:

scipy/sparse/tests/test_csc.py
@@ -1,6 +1,6 @@
 import numpy as np
 from numpy.testing import assert_array_almost_equal, assert_
-from scipy.sparse import csr_matrix, csc_matrix
+from scipy.sparse import csr_matrix, csc_matrix, lil_matrix
 
 import pytest
 
@@ -69,3 +69,15 @@ def test_csc_empty_slices(matrix_input, axis, expected_shape):
 
     assert actual_shape_1 == expected_shape
     assert actual_shape_1 == actual_shape_2
+
+
+def test_argmax_overflow():
+    '''
+    See gh-13646: Windows integer overflow for large sparse matrices.
+    '''
+    dim = (100000, 100000)
+    A = lil_matrix(dim)
+    A[-2, -2] = 42
+    A = csc_matrix(A)
+    idx = np.unravel_index(A.argmax(), dim)
+    assert A[idx] == A[-2, -2]