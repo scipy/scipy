6de562642bebdeacc3527a619e9ba966e2c104e8
Arthur Volant <arthurvolant@gmail.com>
2021-03-14 23:55:29 +0100

    MAINT: remove update_discrepancy in cython file

--- scipy/stats/_qmc_cy.pyx
+++ scipy/stats/_qmc_cy.pyx
@@ -241,63 +241,6 @@ def _cy_wrapper_update_discrepancy(double[::1] x_new_view,
     return c_update_discrepancy(x_new_view, sample_view, initial_disc)
 
 
-def _update_discrepancy_cy(x_new, sample, double initial_disc):
-    """Update the centered discrepancy with a new sample.
-
-    Parameters
-    ----------
-    x_new : array_like (1, d)
-        The new sample to add in `sample`.
-    sample : array_like (n, d)
-        The initial sample.
-    initial_disc : float
-        Centered discrepancy of the `sample`.
-
-    Returns
-    -------
-    discrepancy : float
-        Centered discrepancy of the sample composed of `x_new` and `sample`.
-
-    Examples
-    --------
-    We can also compute iteratively the discrepancy by using
-    ``iterative=True``.
-
-    >>> from scipy.stats import qmc
-    >>> space = np.array([[1, 3], [2, 6], [3, 2], [4, 5], [5, 1], [6, 4]])
-    >>> l_bounds = [0.5, 0.5]
-    >>> u_bounds = [6.5, 6.5]
-    >>> space = qmc.scale(space, l_bounds, u_bounds, reverse=True)
-    >>> disc_init = qmc.discrepancy(space[:-1], iterative=True)
-    >>> disc_init
-    0.04769081147119336
-    >>> qmc.update_discrepancy(space[-1], space[:-1], disc_init)
-    0.008142039609053513
-
-    """
-    sample = np.asarray(sample)
-    x_new = np.asarray(x_new)
-
-    # Checking that sample is within the hypercube and 2D
-    if not sample.ndim == 2:
-        raise ValueError('Sample is not a 2D array')
-
-    if not (np.all(sample >= 0) and np.all(sample <= 1)):
-        raise ValueError('Sample is not in unit hypercube')
-
-    # Checking that x_new is within the hypercube and 1D
-    if not x_new.ndim == 1:
-        raise ValueError('x_new is not a 1D array')
-
-    if not (np.all(x_new >= 0) and np.all(x_new <= 1)):
-        raise ValueError('x_new is not in unit hypercube')
-
-    if x_new.shape[0] != sample.shape[1]:
-        raise ValueError('x_new and Sample must be broadcastable')
-
-    return c_update_discrepancy(x_new, sample, initial_disc)
-
-
 cdef double c_update_discrepancy(double[::1] x_new_view,
                                  double[:, ::1] sample_view,
                                  double initial_disc):
