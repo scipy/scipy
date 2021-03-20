cc58a73614e9b156e90466fd22e976f9ea9557ed
Matt Haberland <mhaberla@calpoly.edu>
2021-03-20 01:15:45 -0700

    Update scipy/stats/tests/test_mstats_basic.py

--- scipy/stats/tests/test_mstats_basic.py
+++ scipy/stats/tests/test_mstats_basic.py
@@ -229,6 +229,7 @@ class TestCorr(object):
         y = [22.6, 8.3, 44.4, 11.9, 24.6, 0.6, 5.7, 41.6,
               0.0, 0.6, 6.7, 3.8, 1.0, 1.2, 1.4, np.nan]
         (x, y) = (ma.fix_invalid(x), ma.fix_invalid(y))
+        assert_almost_equal(mstats.spearmanr(x,y)[0], 0.6887299)   
         # check against R
         # options(digits=16)
         # cor.test(c(2.0, 47.4, 42.0, 10.8, 60.1, 1.7, 64.0, 63.1, 
