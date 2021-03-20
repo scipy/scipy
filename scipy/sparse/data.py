FIX: sparse: csc_matrix.argmax() integer overflow (#13649)
cast C integral type to Python int to handle possible overflow

scipy/sparse/data.py
@@ -271,7 +271,9 @@ class _minmax_mixin(object):
                 m = mat.data[am]
 
                 if compare(m, zero):
-                    return mat.row[am] * mat.shape[1] + mat.col[am]
+                    # cast to Python int to avoid overflow
+                    # and RuntimeError
+                    return int(mat.row[am])*mat.shape[1] + int(mat.col[am])
                 else:
                     size = np.prod(mat.shape)
                     if size == mat.nnz: