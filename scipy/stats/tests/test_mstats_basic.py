49f51b0e3c8ad9b811ff8e15ac335896cf5b8128
Matt Haberland <mhaberla@calpoly.edu>
2021-03-25 11:48:12 -0700

    Update scipy/stats/tests/test_mstats_basic.py

--- scipy/stats/tests/test_mstats_basic.py
+++ scipy/stats/tests/test_mstats_basic.py
@@ -229,7 +229,7 @@ class TestCorr(object):
         y = [22.6, 8.3, 44.4, 11.9, 24.6, 0.6, 5.7, 41.6,
               0.0, 0.6, 6.7, 3.8, 1.0, 1.2, 1.4, np.nan]
         (x, y) = (ma.fix_invalid(x), ma.fix_invalid(y))
-
+        assert_almost_equal(mstats.spearmanr(x,y)[0], 0.6887299)
         # Next test is to make sure calculation uses sufficient precision.
         # The denominator's value is ~n^3 and used to be represented as an
         # int. 2000**3 > 2**32 so these arrays would cause overflow on
