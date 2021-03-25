057c2e3e8d85c18bf5969bceba7bd5a519d4cf16
Matt Haberland <mdhaber@mit.edu>
2021-03-25 10:49:59 -0700

    Update scipy/stats/morestats.py

--- scipy/stats/morestats.py
+++ scipy/stats/morestats.py
@@ -1033,7 +1033,7 @@ def boxcox(x, lmbda=None, alpha=None, optimizer=None):
 
     >>> fig = plt.figure()
     >>> ax1 = fig.add_subplot(211)
-    >>> x = stats.loggamma.rvs(5, size=500, random_state=0) + 5
+    >>> x = stats.loggamma.rvs(5, size=500) + 5
     >>> prob = stats.probplot(x, dist=stats.norm, plot=ax1)
     >>> ax1.set_xlabel('')
     >>> ax1.set_title('Probplot against normal distribution')
