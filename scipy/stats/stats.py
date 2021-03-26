4113937c3df5fb4a1daac5d2d143d5f86367dd44
Sam Wallan <44255917+swallan@users.noreply.github.com>
2021-03-25 21:45:24 -0700

    PEP: utf-8 fixes

--- scipy/stats/stats.py
+++ scipy/stats/stats.py
@@ -6048,9 +6048,9 @@ def ttest_ind(a, b, axis=0, equal_var=True, nan_policy='propagate',
     .. [4] Karen K. Yuen (1974), The two-sample trimmed t for unequal
            population variances, Biometrika Volume 61, Number 1, 165-170,
            DOI: https://doi.org/10.1093/biomet/61.1.165
-    .. [5] Yuen, Karen K., and W. J. Dixon. “The Approximate Behaviour and
-           Performance of the Two-Sample Trimmed t.” Biometrika, vol. 60,
-           no. 2, 1973, pp. 369–374. JSTOR, www.jstor.org/stable/2334550.
+    .. [5] Yuen, Karen K., and W. J. Dixon. "The Approximate Behaviour and
+           Performance of the Two-Sample Trimmed t." Biometrika, vol. 60,
+           no. 2, 1973, pp. 369-374. JSTOR, www.jstor.org/stable/2334550.
 
     Examples
     --------
