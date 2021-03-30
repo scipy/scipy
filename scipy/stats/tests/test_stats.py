14babc502b3c36648d2c30bef7a6dd117353afef
Sam Wallan <44255917+swallan@users.noreply.github.com>
2021-03-29 17:04:45 -0700

    TST: prametrize r comparisons, more digits, allclose

--- scipy/stats/tests/test_stats.py
+++ scipy/stats/tests/test_stats.py
@@ -4086,53 +4086,27 @@ class Test_ttest_ind_common:
 
 
 class Test_ttest_trim:
+    params = [
+        [[1, 2, 3], [1.1, 2.9, 4.2], 0.53619490753126731, -0.6864951273557258,
+         .2],
+        [[56, 128.6, 12, 123.8, 64.34, 78, 763.3], [1.1, 2.9, 4.2],
+         0.00998909252078421, 4.591598691181999, .2],
+        [[56, 128.6, 12, 123.8, 64.34, 78, 763.3], [1.1, 2.9, 4.2],
+         0.10512380092302633, 2.832256715395378, .32],
+        [[2.7, 2.7, 1.1, 3.0, 1.9, 3.0, 3.8, 3.8, 0.3, 1.9, 1.9],
+         [6.5, 5.4, 8.1, 3.5, 0.5, 3.8, 6.8, 4.9, 9.5, 6.2, 4.1],
+         0.002878909511344, -4.2461168970325, .2],
+        [[-0.84504783, 0.13366078, 3.53601757, -0.62908581, 0.54119466,
+          -1.16511574, -0.08836614, 1.18495416, 2.48028757, -1.58925028,
+          -1.6706357, 0.3090472, -2.12258305, 0.3697304, -1.0415207,
+          -0.57783497, -0.90997008, 1.09850192, 0.41270579, -1.4927376],
+         [1.2725522, 1.1657899, 2.7509041, 1.2389013, -0.9490494, -1.0752459,
+          1.1038576, 2.9912821, 3.5349111, 0.4171922, 1.0168959, -0.7625041,
+          -0.4300008, 3.0431921, 1.6035947, 0.5285634, -0.7649405, 1.5575896,
+          1.3670797, 1.1726023], 0.005293305834235, -3.0983317739483, .2]]
 
-    def test_ttest_compare_r1(self):
-        '''
-        > library(PairedData)
-        > a <- c(1, 2, 3)
-        > b <- c(1.1, 2.9, 4.2)
-        > yuen.t.test(a, b, tr=.2)
-        '''
-        a = (1, 2, 3)
-        b = (1.1, 2.9, 4.2)
-        pr = 0.53619490753126731
-        tr = -0.68649512735572582
-        t, p = stats.ttest_ind(a, b, trim=.20, equal_var=False)
-        assert_almost_equal(t, tr)
-        assert_almost_equal(p, pr)
-
-    def test_ttest_compare_r2(self):
-        '''
-        > library(PairedData)
-        > a <- c(56, 128.6, 12, 123.8, 64.34, 78, 763.3)
-        > b <- c(1.1, 2.9, 4.2)
-        > yuen.t.test(a, b, tr=.2)
-        '''
-        a = (56, 128.6, 12, 123.8, 64.34, 78, 763.3)
-        b = (1.1, 2.9, 4.2)
-        pr = 0.00998909252078421
-        tr = 4.591598691181999
-        t, p = stats.ttest_ind(a, b, trim=.20, equal_var=False)
-        assert_almost_equal(t, tr)
-        assert_almost_equal(p, pr)
-
-    def test_ttest_compare_r3(self):
-        '''
-        > library(PairedData)
-        > a <- c(56, 128.6, 12, 123.8, 64.34, 78, 763.3)
-        > b <- c(1.1, 2.9, 4.2)
-        >  yuen.t.test(a, b, tr=.32)
-        '''
-        a = (56, 128.6, 12, 123.8, 64.34, 78, 763.3)
-        b = (1.1, 2.9, 4.2)
-        pr = 0.10512380092302633
-        tr = 2.832256715395378
-        t, p = stats.ttest_ind(a, b, trim=.32, equal_var=False)
-        assert_almost_equal(t, tr)
-        assert_almost_equal(p, pr)
-
-    def test_compare_r_1(self):
+    @pytest.mark.parametrize("a,b,pr,tr,trim", params)
+    def test_ttest_compare_r(self, a, b, pr, tr, trim):
         '''
         Using PairedData's yuen.t.test method. Something to note is that there
         are at least 3 R packages that come with a trimmed t-test method, and
@@ -4140,70 +4114,32 @@ class Test_ttest_trim:
         method's results match this method, SAS, and one of the other R
         methods. A notable discrepancy was the DescTools implementation of the
         function, which only sometimes agreed with SAS, WRS2, PairedData and
-        this implementation. For this reason, all comparisons in R are made
+        this implementation. For this reason, most comparisons in R are made
         against PairedData's method.
 
-        > a <- c(2.7,2.7,1.1,3.0,1.9,3.0,3.8,3.8,0.3,1.9,1.9)
-        > b <- c(6.5,5.4,8.1,3.5,0.5,3.8,6.8,4.9,9.5,6.2,4.1)
-        > yuen.t.test(a, b)
+        Rather than providing the input and output for all evaluations, here is
+        a representative example:
+        > library(PairedData)
+        > a <- c(1, 2, 3)
+        > b <- c(1.1, 2.9, 4.2)
+        > options(digits=16)
+        > yuen.t.test(a, b, tr=.2)
 
             Two-sample Yuen test, trim=0.2
 
         data:  x and y
-        t = -4.2461, df = 7.92, p-value = 0.002879
-        alternative hypothesis: true difference in trimmed means is not equal \
+        t = -0.68649512735573, df = 3.4104431643464, p-value = 0.5361949075313
+        alternative hypothesis: true difference in trimmed means is not equal
         to 0
         95 percent confidence interval:
-         -4.543893 -1.341821
+         -3.912777195645217  2.446110528978550
         sample estimates:
         trimmed mean of x trimmed mean of y
-                 2.442857          5.385714
+        2.000000000000000 2.73333333333333
         '''
-        a = [2.7, 2.7, 1.1, 3.0, 1.9, 3.0, 3.8, 3.8, 0.3, 1.9, 1.9]
-        b = [6.5, 5.4, 8.1, 3.5, 0.5, 3.8, 6.8, 4.9, 9.5, 6.2, 4.1]
-        res = stats.ttest_ind(a, b, trim=.20, equal_var=False)
-        assert_allclose(res.pvalue, 0.00287891, atol=1e-7)
-        assert_allclose(res.statistic, -4.246117, atol=1e-6)
-
-    def test_compare_r_2(self):
-        '''
-        see: https://rdrr.io/cran/PairedData/man/yuen.t.test.html
-        library(PairedData)
-
-        > a <-c(-0.84504783, 0.13366078, 3.53601757, -0.62908581, 0.54119466,
-        +       -1.16511574, -0.08836614, 1.18495416, 2.48028757, -1.58925028,
-        +       -1.6706357, 0.3090472, -2.12258305, 0.3697304, -1.0415207,
-        +       -0.57783497, -0.90997008, 1.09850192, 0.41270579, -1.4927376)
-        > b <-c(1.2725522, 1.1657899, 2.7509041, 1.2389013, -0.9490494,
-        +       -1.0752459, 1.1038576, 2.9912821, 3.5349111, 0.4171922,
-        +       1.0168959, -0.7625041, -0.4300008, 3.0431921, 1.6035947,
-        +       0.5285634, -0.7649405, 1.5575896, 1.3670797, 1.1726023)
-        > yuen.t.test(a, b)
-
-            Two-sample Yuen test, trim=0.2
-
-        data:  a and b
-        t = -3.0983, df = 21.749, p-value = 0.005293
-        alternative hypothesis: true difference in trimmed means is not equal \
-        to 0
-        95 percent confidence interval:
-         -2.157551 -0.426652
-        sample estimates:
-        trimmed mean of x trimmed mean of y
-               -0.2908835         1.0012182
-
-        '''
-        a = [-0.84504783, 0.13366078, 3.53601757, -0.62908581, 0.54119466,
-             -1.16511574, -0.08836614, 1.18495416, 2.48028757, -1.58925028,
-             -1.6706357, 0.3090472, -2.12258305, 0.3697304, -1.0415207,
-             -0.57783497, -0.90997008, 1.09850192, 0.41270579, -1.4927376]
-        b = [1.2725522, 1.1657899, 2.7509041, 1.2389013, -0.9490494,
-             -1.0752459, 1.1038576, 2.9912821, 3.5349111, 0.4171922,
-             1.0168959, -0.7625041, -0.4300008, 3.0431921, 1.6035947,
-             0.5285634, -0.7649405, 1.5575896, 1.3670797, 1.1726023]
-        res = stats.ttest_ind(a, b, trim=.20, equal_var=False)
-        assert_allclose(res.pvalue, 0.005293306, atol=1e-7)
-        assert_allclose(res.statistic, -3.098332, atol=1e-6)
+        t, p = stats.ttest_ind(a, b, trim=trim, equal_var=False)
+        assert_allclose(t, tr, atol=1e-15)
+        assert_allclose(p, pr, atol=1e-15)
 
     def test_compare_SAS(self):
         # Source of the data used in this test:
@@ -4215,34 +4151,36 @@ class Test_ttest_trim:
         # the nearest whole number. However, consistent with
         # `scipy.stats.trimmed_mean`, this test truncates to the lower
         # whole number. In this example, the paper notes that 1 value is
-        # trimmed off of each side. 10% replicates this amount of trimming.
-        res = stats.ttest_ind(a, b, trim=.09, equal_var=False)
-        assert_allclose(res.pvalue, 0.514522, atol=1e-6)
-        assert_allclose(res.statistic, 0.669169, atol=1e-6)
+        # trimmed off of each side. 9% replicates this amount of trimming.
+        t, p = stats.ttest_ind(a, b, trim=.09, equal_var=False)
+        assert_allclose(p, 0.514522, atol=1e-6)
+        assert_allclose(t, 0.669169, atol=1e-6)
 
     def test_equal_var(self):
         '''
+        The PairedData library only supports unequal varainces. To compare
+        samples with equal variances, the multicon library is used.
         > library(multicon)
         > a <- c(2.7, 2.7, 1.1, 3.0, 1.9, 3.0, 3.8, 3.8, 0.3, 1.9, 1.9)
         > b <- c(6.5, 5.4, 8.1, 3.5, 0.5, 3.8, 6.8, 4.9, 9.5, 6.2, 4.1)
         > dv = c(a,b)
-        > iv = c(rep(a, length(a)), rep(b, length(b)))
+        > iv = c(rep('a', length(a)), rep('b', length(b)))
         > yuenContrast(dv~ iv, EQVAR = TRUE)
         $Ms
-           N        M wgt
-        a 11 2.442857   1
-        b 11 5.385714  -1
+           N                 M wgt
+        a 11 2.442857142857143   1
+        b 11 5.385714285714286  -1
 
         $test
-                     stat df     crit           p
-        results -4.246117 12 2.178813 0.001135088
+                              stat df              crit                   p
+        results -4.246116897032513 12 2.178812829667228 0.00113508833897713
         '''
         a = [2.7, 2.7, 1.1, 3.0, 1.9, 3.0, 3.8, 3.8, 0.3, 1.9, 1.9]
         b = [6.5, 5.4, 8.1, 3.5, 0.5, 3.8, 6.8, 4.9, 9.5, 6.2, 4.1]
         # `equal_var=True` is default
-        res = stats.ttest_ind(a, b, trim=.2)
-        assert_allclose(res.pvalue, 0.001135088, atol=1e-9)
-        assert_allclose(res.statistic, -4.246117, atol=1e-7)
+        t, p = stats.ttest_ind(a, b, trim=.2)
+        assert_allclose(p, 0.00113508833897713, atol=1e-10)
+        assert_allclose(t, -4.246116897032513, atol=1e-10)
 
     def test_axis_simple(self):
         np.random.seed(123)
@@ -4269,50 +4207,23 @@ class Test_ttest_trim:
         for (i, v) in zip(res_individual, res_vec_list):
             assert_array_equal(i, v)
 
-    def test_alternatives(self):
+    @pytest.mark.parametrize('alt,pr,tr',
+                             [['greater', 0.9985605452443, -4.2461168970325],
+                              ['less', 0.001439454755672, -4.2461168970325]])
+    def test_alternatives(self, alt, pr, tr):
         '''
         > library(PairedData)
         > a <- c(2.7,2.7,1.1,3.0,1.9,3.0,3.8,3.8,0.3,1.9,1.9)
         > b <- c(6.5,5.4,8.1,3.5,0.5,3.8,6.8,4.9,9.5,6.2,4.1)
+        > options(digits=16)
         > yuen.t.test(a, b, alternative = 'greater')
-
-            Two-sample Yuen test, trim=0.2
-
-        data:  x and y
-        t = -4.2461, df = 7.92, p-value = 0.9986
-        alternative hypothesis: true difference
-        in trimmed means is greater than 0
-        95 percent confidence interval:
-         -4.233343       Inf
-        sample estimates:
-        trimmed mean of x trimmed mean of y
-                 2.442857          5.385714
-         > yuen.t.test(a, b, alternative = 'less')
-
-            Two-sample Yuen test, trim=0.2
-
-        data:  x and y
-        t = -4.2461, df = 7.92, p-value = 0.001439
-        alternative hypothesis: true difference
-        in trimmed means is less than 0
-        95 percent confidence interval:
-              -Inf -1.652371
-        sample estimates:
-        trimmed mean of x trimmed mean of y
-                 2.442857          5.385714
         '''
         a = [2.7, 2.7, 1.1, 3.0, 1.9, 3.0, 3.8, 3.8, 0.3, 1.9, 1.9]
         b = [6.5, 5.4, 8.1, 3.5, 0.5, 3.8, 6.8, 4.9, 9.5, 6.2, 4.1]
 
-        res_greater = stats.ttest_ind(a, b, trim=.2, equal_var=False,
-                                      alternative='greater')
-        assert_allclose(res_greater.pvalue, 0.9986, atol=1e-4)
-        assert_allclose(res_greater.statistic, -4.2461, atol=1e-4)
-
-        res_less = stats.ttest_ind(a, b, trim=.2, equal_var=False,
-                                   alternative='less')
-        assert_allclose(res_less.pvalue, 0.001439, atol=1e-6)
-        assert_allclose(res_less.statistic, -4.2461, atol=1e-4)
+        t, p = stats.ttest_ind(a, b, trim=.2, equal_var=False, alternative=alt)
+        assert_allclose(p, pr, atol=1e-10)
+        assert_allclose(t, tr, atol=1e-10)
 
     def test_errors_unsupported(self):
         # confirm that attempting to trim with NaNs or permutations raises an
