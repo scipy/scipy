e51705c37f17a4da72d824ada08a6d4b67d1c73d
Atsushi Sakai <asakai.amsl+github@gmail.com>
2021-04-03 22:23:27 +0900

    add x_opt input and test using it.

--- scipy/optimize/tests/test_minimize_constrained.py
+++ scipy/optimize/tests/test_minimize_constrained.py
@@ -680,13 +680,13 @@ def test_bug_11886():
 
 class TestBoundedNelderMead:
 
-    @pytest.mark.parametrize('bounds',
-                             [Bounds(-np.inf, np.inf),
-                              Bounds(-np.inf, -0.8),
-                              Bounds(3.0, np.inf),
-                              Bounds([3.0, 1.0], [4.0, 5.0]),
+    @pytest.mark.parametrize('bounds, x_opt',
+                             [(Bounds(-np.inf, np.inf), Rosenbrock().x_opt),
+                              (Bounds(-np.inf, -0.8), [-0.8, -0.8]),
+                              (Bounds(3.0, np.inf), [3.0, 9.0]),
+                              (Bounds([3.0, 1.0], [4.0, 5.0]), [3., 5.]),
                               ])
-    def test_rosen_brock_with_bounds(self, bounds):
+    def test_rosen_brock_with_bounds(self, bounds, x_opt):
         prob = Rosenbrock()
         with suppress_warnings() as sup:
             sup.filter(UserWarning, "Initial guess is not within "
@@ -697,6 +697,7 @@ class TestBoundedNelderMead:
             assert np.less_equal(bounds.lb, result.x).all()
             assert np.less_equal(result.x, bounds.ub).all()
             assert np.allclose(prob.fun(result.x), result.fun)
+            assert np.allclose(result.x, x_opt, atol=1.e-3)
 
     def test_equal_all_bounds(self):
         prob = Rosenbrock()
