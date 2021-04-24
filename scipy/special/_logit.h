10def6c4cc470d5dc5d2bc78c79d7a7a1973c7bc
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2021-04-24 21:08:37 +0300

    MAINT: special: get rid of _logit.c.src
    Use C++ templates instead. Note the need to manually instantiate
    kernels because otherwise it fails to compile with:
    
    scipy/special/_ufuncs_cxx.cxx:4096:66: error: overloaded function with no contextual type information
     4096 |   __pyx_v_5scipy_7special_11_ufuncs_cxx__export_logit = ((void *)logit);

--- scipy/special/_logit.h
+++ scipy/special/_logit.h
@@ -3,12 +3,27 @@
 
 #include "numpy/npy_math.h"
 
-npy_float logitf(npy_float x);
-npy_double logit(npy_double x);
-npy_longdouble logitl(npy_longdouble x);
 
-npy_float expitf(npy_float x);
-npy_double expit(npy_double x);
-npy_longdouble expitl(npy_longdouble x);
+template <typename T>
+inline T _logit(T x) {
+    x /= 1 - x;
+    return npy_log(x);
+};
+
+
+template<typename T>
+inline T _expit(T x) {
+    return 1 / (1 + npy_exp(-x));
+};
+
+
+npy_float logitf(npy_float x)  {return _logit(x);};
+npy_double logit(npy_double x) {return _logit(x);};
+npy_longdouble logitl(npy_longdouble x) {return _logit(x);};
+
+npy_float expitf(npy_float x) {return _expit(x);};
+npy_double expit(npy_double x) {return _expit(x);};
+npy_longdouble expitl(npy_longdouble x) {return _expit(x);};
+
 
 #endif
