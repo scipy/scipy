5ca45b50718925725a63957d85739336a32d780d
Trever Hines <treverhines@gmail.com>
2021-05-01 15:59:42 -0400

    TST: Added test for LinAlgError when observations are collinear

--- scipy/interpolate/tests/test_rbfinterp.py
+++ scipy/interpolate/tests/test_rbfinterp.py
@@ -1,6 +1,7 @@
 import pickle
 import pytest
 import numpy as np
+from numpy.linalg import LinAlgError
 from numpy.testing import assert_allclose, assert_array_equal
 from scipy.stats.qmc import Halton
 from scipy.spatial import cKDTree
@@ -339,6 +340,15 @@ class _TestRBFInterpolator:
             match = f'`degree` should not be below {deg}'
             with pytest.warns(Warning, match=match):
                 self.build(y, d, epsilon=1.0, kernel=kernel, degree=deg-1)
+
+    def test_rank_error(self):
+        # An error should be raised when `kernel` is "thin_plate_spline" and
+        # observations are 2-D and collinear
+        y = np.array([[2.0, 0.0], [1.0, 0.0], [0.0, 0.0]])
+        d = np.array([0.0, 0.0, 0.0])
+        match = 'does not have full column rank'
+        with pytest.raises(LinAlgError, match=match):
+            self.build(y, d, kernel='thin_plate_spline')(y)
 
     def test_single_point(self):
         # make sure interpolation still works with only one point (in 1, 2, and
