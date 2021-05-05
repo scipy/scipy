196e3225168f40502b8fb75830019bfa3895454b
Peter Bell <peterbell10@live.co.uk>
2021-05-05 19:45:23 +0100

    ENH: High performance reduction machinery

--- scipy/spatial/src/distance_metrics.h
+++ scipy/spatial/src/distance_metrics.h
@@ -3,75 +3,157 @@
 #include <cmath>
 #include "views.h"
 
+struct Identity {
+    template <typename T>
+    T operator() (T && val) const {
+        return std::forward<T>(val);
+    }
+};
+
+template <int ilp_factor=4, typename T,
+          typename TransformFunc,
+          typename ProjectFunc = Identity,
+          typename ReduceFunc = std::plus<>>
+void transform_reduce_2d_(
+    StridedView2D<T> out, StridedView2D<const T> x, StridedView2D<const T> y,
+    const TransformFunc& map, const ProjectFunc& project = Identity{},
+    const ReduceFunc& reduce = std::plus<>{}) {
+    intptr_t xs = x.strides[1], ys = y.strides[1];
+
+    intptr_t i = 0;
+    for (; i + (ilp_factor - 1) < x.shape[0]; i += ilp_factor) {
+        const T* x_rows[ilp_factor];
+        const T* y_rows[ilp_factor];
+        for (int k = 0; k < ilp_factor; ++k) {
+            x_rows[k] = &x(i + k, 0);
+            y_rows[k] = &y(i + k, 0);
+        }
+
+        T dist[ilp_factor] = {0};
+        for (intptr_t j = 0; j < x.shape[1]; ++j) {
+            for (int k = 0; k < ilp_factor; ++k) {
+                auto val = map(x_rows[k][j * xs], y_rows[k][j * ys]);
+                dist[k] = reduce(dist[k], val);
+            }
+        }
+
+        for (int k = 0; k < ilp_factor; ++k) {
+            out(i + k, 0) = project(dist[k]);
+        }
+    }
+    for (; i < x.shape[0]; ++i) {
+        const T* x_row = &x(i, 0);
+        const T* y_row = &y(i, 0);
+        T dist = 0;
+        for (intptr_t j = 0; j < x.shape[1]; ++j) {
+            auto val = map(x_row[j * xs], y_row[j * ys]);
+            dist = reduce(dist, val);
+        }
+        out(i, 0) = project(dist);
+    }
+}
+
+template <int ilp_factor=2, typename T,
+          typename TransformFunc,
+          typename ProjectFunc=Identity,
+          typename ReduceFunc = std::plus<>>
+void transform_reduce_2d_(
+    StridedView2D<T> out, StridedView2D<const T> x, StridedView2D<const T> y,
+    StridedView2D<const T> w, const TransformFunc& map,
+    const ProjectFunc& project = Identity{},
+    const ReduceFunc& reduce = std::plus<>{}) {
+    intptr_t i = 0;
+    intptr_t xs = x.strides[1], ys = y.strides[1], ws = w.strides[1];
+
+    for (; i + (ilp_factor - 1) < x.shape[0]; i += ilp_factor) {
+        const T* x_rows[ilp_factor];
+        const T* y_rows[ilp_factor];
+        const T* w_rows[ilp_factor];
+        for (int k = 0; k < ilp_factor; ++k) {
+            x_rows[k] = &x(i + k, 0);
+            y_rows[k] = &y(i + k, 0);
+            w_rows[k] = &w(i + k, 0);
+        }
+
+        T dist[ilp_factor] = {0};
+        for (intptr_t j = 0; j < x.shape[1]; ++j) {
+            for (int k = 0; k < ilp_factor; ++k) {
+                auto val = map(x_rows[k][j * xs], y_rows[k][j * ys], w_rows[k][j * ws]);
+                dist[k] = reduce(dist[k], val);
+            }
+        }
+
+        for (int k = 0; k < ilp_factor; ++k) {
+            out(i + k, 0) = project(dist[k]);
+        }
+    }
+    for (; i < x.shape[0]; ++i) {
+        const T* x_row = &x(i, 0);
+        const T* y_row = &y(i, 0);
+        const T* w_row = &w(i, 0);
+        T dist = 0;
+        for (intptr_t j = 0; j < x.shape[1]; ++j) {
+            auto val = map(x_row[j * xs], y_row[j * ys], w_row[j * ws]);
+            dist = reduce(dist, val);
+        }
+        out(i, 0) = project(dist);
+    }
+}
+
 struct MinkowskiDistance {
     double p_;
 
     template <typename T>
     void operator()(StridedView2D<T> out, StridedView2D<const T> x, StridedView2D<const T> y) const {
-        T p = p_;
-        for (intptr_t i = 0; i < x.shape[0]; ++i) {
-            T p_dist = 0;
-            for (intptr_t j = 0; j < x.shape[1]; ++j) {
-                auto diff = std::abs(x(i, j) - y(i, j));
-                p_dist += std::pow(diff, p);
-            }
-            out(i, 0) = std::pow(p_dist, 1 / p);
-        }
+        const T p = static_cast<T>(p_);
+        const T invp = static_cast<T>(1.0 / p_);
+        transform_reduce_2d_(out, x, y, [p](T x, T y) {
+            auto diff = std::abs(x - y);
+            return std::pow(diff, p);
+        },
+        [invp](T x) { return std::pow(x, invp); });
     }
 
     template <typename T>
     void operator()(StridedView2D<T> out, StridedView2D<const T> x, StridedView2D<const T> y, StridedView2D<const T> w) const {
-        T p = p_;
-        for (intptr_t i = 0; i < x.shape[0]; ++i) {
-            T p_dist = 0;
-            for (intptr_t j = 0; j < x.shape[1]; ++j) {
-                auto diff = std::abs(x(i, j) - y(i, j));
-                p_dist += w(i, j) * std::pow(diff, p);
-            }
-            out(i, 0) = std::pow(p_dist, 1 / p);
-        }
+        const T p = static_cast<T>(p_);
+        const T invp = static_cast<T>(1.0 / p_);
+        transform_reduce_2d_(out, x, y, w, [p](T x, T y, T w) {
+            auto diff = std::abs(x - y);
+            return w * std::pow(diff, p);
+        },
+        [invp](T x) { return std::pow(x, invp); });
     }
 };
 
 struct EuclideanDistance {
     template <typename T>
     void operator()(StridedView2D<T> out, StridedView2D<const T> x, StridedView2D<const T> y) const {
-        for (intptr_t i = 0; i < x.shape[0]; ++i) {
-            T sqdist = 0;
-            for (intptr_t j = 0; j < x.shape[1]; ++j) {
-                auto diff = std::abs(x(i, j) - y(i, j));
-                sqdist += diff * diff;
-            }
-            out(i, 0) = std::sqrt(sqdist);
-        }
+        transform_reduce_2d_(out, x, y, [](T x, T y) {
+            auto diff = std::abs(x - y);
+            return diff * diff;
+        },
+        [](T x) { return std::sqrt(x); });
     }
 
     template <typename T>
     void operator()(StridedView2D<T> out, StridedView2D<const T> x, StridedView2D<const T> y, StridedView2D<const T> w) const {
-        for (intptr_t i = 0; i < x.shape[0]; ++i) {
-            T sqdist = 0;
-            for (intptr_t j = 0; j < x.shape[1]; ++j) {
-                auto diff = std::abs(x(i, j) - y(i, j));
-                sqdist += w(i, j) * (diff * diff);
-            }
-            out(i, 0) = std::sqrt(sqdist);
-        }
+        transform_reduce_2d_(out, x, y, w, [](T x, T y, T w) {
+            auto diff = std::abs(x - y);
+            return w * (diff * diff);
+        },
+        [](T x) { return std::sqrt(x); });
     }
 };
 
 struct ChebyshevDistance {
     template <typename T>
     void operator()(StridedView2D<T> out, StridedView2D<const T> x, StridedView2D<const T> y) const {
-        for (intptr_t i = 0; i < x.shape[0]; ++i) {
-            T dist = 0;
-            for (intptr_t j = 0; j < x.shape[1]; ++j) {
-                auto diff = std::abs(x(i, j) - y(i, j));
-                if (diff > dist) {
-                    dist = diff;
-                }
-            }
-            out(i, 0) = dist;
-        }
+        transform_reduce_2d_(out, x, y, [](T x, T y) {
+            return std::abs(x - y);
+        },
+        Identity{},
+        [](T x, T y) { return std::max(x, y); });
     }
 
     template <typename T>
@@ -92,37 +174,15 @@ struct ChebyshevDistance {
 struct CityBlockDistance {
     template <typename T>
     void operator()(StridedView2D<T> out, StridedView2D<const T> x, StridedView2D<const T> y) const {
-        if (x.strides[1] == 1 && y.strides[1] == 1) {
-            for (intptr_t i = 0; i < x.shape[0]; ++i) {
-                const T* x_data = x.data + i * x.strides[0];
-                const T* y_data = y.data + i * y.strides[0];
-
-                T dist = 0;
-                for (intptr_t j = 0; j < x.shape[1]; ++j) {
-                    dist += std::abs(x_data[j] - y_data[j]);
-                }
-                out(i, 0) = dist;
-            }
-        } else {
-            for (intptr_t i = 0; i < x.shape[0]; ++i) {
-                T dist = 0;
-                for (intptr_t j = 0; j < x.shape[1]; ++j) {
-                    dist += std::abs(x(i, j) - y(i, j));
-                }
-                out(i, 0) = dist;
-            }
-        }
+        transform_reduce_2d_(out, x, y, [](T x, T y) {
+            return std::abs(x - y);
+        });
     }
 
     template <typename T>
     void operator()(StridedView2D<T> out, StridedView2D<const T> x, StridedView2D<const T> y, StridedView2D<const T> w) const {
-        for (intptr_t i = 0; i < x.shape[0]; ++i) {
-            T dist = 0;
-            for (intptr_t j = 0; j < x.shape[1]; ++j) {
-                auto diff = std::abs(x(i, j) - y(i, j));
-                dist += w(i, j) * diff;
-            }
-            out(i, 0) = dist;
-        }
+        transform_reduce_2d_(out, x, y, w, [](T x, T y, T w) {
+            return w * std::abs(x - y);
+        });
     }
 };
