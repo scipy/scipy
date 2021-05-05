8e93d1eca225928d30e372f9875a9e9d5e89601f
Peter Bell <peterbell10@live.co.uk>
2021-05-05 21:19:38 +0100

    MAINT: Fix old gcc build and force loop unrolling

--- scipy/spatial/src/distance_metrics.h
+++ scipy/spatial/src/distance_metrics.h
@@ -3,42 +3,103 @@
 #include <cmath>
 #include "views.h"
 
+#ifdef __GNUC__
+#define ALWAYS_INLINE inline __attribute__((always_inline))
+#define INLINE_LAMBDA __attribute__((always_inline))
+#elif defined(MSC_VER)
+#define ALWAYS_INLINE __forceinline
+#define INLINE_LAMBDA
+#else
+#define ALWAYS_INLINE inline
+#define INLINE_LAMBDA
+#endif
+
 struct Identity {
     template <typename T>
-    T operator() (T && val) const {
+    ALWAYS_INLINE T operator() (T && val) const {
         return std::forward<T>(val);
+    }
+};
+
+struct Plus {
+    template <typename T>
+    ALWAYS_INLINE T operator()(T a, T b) const {
+        return a + b;
+    }
+};
+
+// Helper to force a fixed bound loop to be completely unrolled
+template <int unroll>
+struct ForceUnroll{
+    template <typename Func>
+    ALWAYS_INLINE void operator()(const Func& f) const {
+        ForceUnroll<unroll - 1>{}(f);
+        f(unroll - 1);
+    }
+};
+
+template <>
+struct ForceUnroll<1> {
+    template <typename Func>
+    ALWAYS_INLINE void operator()(const Func& f) const {
+        f(0);
     }
 };
 
 template <int ilp_factor=4, typename T,
           typename TransformFunc,
           typename ProjectFunc = Identity,
-          typename ReduceFunc = std::plus<>>
+          typename ReduceFunc = Plus>
 void transform_reduce_2d_(
     StridedView2D<T> out, StridedView2D<const T> x, StridedView2D<const T> y,
     const TransformFunc& map, const ProjectFunc& project = Identity{},
-    const ReduceFunc& reduce = std::plus<>{}) {
+    const ReduceFunc& reduce = Plus{}) {
     intptr_t xs = x.strides[1], ys = y.strides[1];
 
     intptr_t i = 0;
-    for (; i + (ilp_factor - 1) < x.shape[0]; i += ilp_factor) {
-        const T* x_rows[ilp_factor];
-        const T* y_rows[ilp_factor];
-        for (int k = 0; k < ilp_factor; ++k) {
-            x_rows[k] = &x(i + k, 0);
-            y_rows[k] = &y(i + k, 0);
-        }
+    if (xs == 1 && ys == 1) {
+        for (; i + (ilp_factor - 1) < x.shape[0]; i += ilp_factor) {
+            const T* x_rows[ilp_factor];
+            const T* y_rows[ilp_factor];
+            ForceUnroll<ilp_factor>{}([&](int k) {
+                x_rows[k] = &x(i + k, 0);
+                y_rows[k] = &y(i + k, 0);
+            });
 
-        T dist[ilp_factor] = {0};
-        for (intptr_t j = 0; j < x.shape[1]; ++j) {
-            for (int k = 0; k < ilp_factor; ++k) {
-                auto val = map(x_rows[k][j * xs], y_rows[k][j * ys]);
-                dist[k] = reduce(dist[k], val);
+            T dist[ilp_factor] = {0};
+            for (intptr_t j = 0; j < x.shape[1]; ++j) {
+                ForceUnroll<ilp_factor>{}([&](int k) {
+                    auto val = map(x_rows[k][j], y_rows[k][j]);
+                    dist[k] = reduce(dist[k], val);
+                });
             }
-        }
 
-        for (int k = 0; k < ilp_factor; ++k) {
-            out(i + k, 0) = project(dist[k]);
+            ForceUnroll<ilp_factor>{}([&](int k) {
+                out(i + k, 0) = project(dist[k]);
+            });
+        }
+    } else {
+        for (; i + (ilp_factor - 1) < x.shape[0]; i += ilp_factor) {
+            const T* x_rows[ilp_factor];
+            const T* y_rows[ilp_factor];
+            ForceUnroll<ilp_factor>{}([&](int k) {
+                x_rows[k] = &x(i + k, 0);
+                y_rows[k] = &y(i + k, 0);
+            });
+
+            T dist[ilp_factor] = {0};
+            for (intptr_t j = 0; j < x.shape[1]; ++j) {
+                auto x_offset = j * xs;
+                auto y_offset = j * ys;
+                ForceUnroll<ilp_factor>{}([&](int k) {
+                    auto val = map(x_rows[k][x_offset], y_rows[k][y_offset]);
+                    dist[k] = reduce(dist[k], val);
+                });
+            }
+
+            ForceUnroll<ilp_factor>{}([&](int k) {
+                out(i + k, 0) = project(dist[k]);
+            });
         }
     }
     for (; i < x.shape[0]; ++i) {
@@ -55,13 +116,13 @@ void transform_reduce_2d_(
 
 template <int ilp_factor=2, typename T,
           typename TransformFunc,
-          typename ProjectFunc=Identity,
-          typename ReduceFunc = std::plus<>>
+          typename ProjectFunc = Identity,
+          typename ReduceFunc = Plus>
 void transform_reduce_2d_(
     StridedView2D<T> out, StridedView2D<const T> x, StridedView2D<const T> y,
     StridedView2D<const T> w, const TransformFunc& map,
     const ProjectFunc& project = Identity{},
-    const ReduceFunc& reduce = std::plus<>{}) {
+    const ReduceFunc& reduce = Plus{}) {
     intptr_t i = 0;
     intptr_t xs = x.strides[1], ys = y.strides[1], ws = w.strides[1];
 
@@ -69,23 +130,23 @@ void transform_reduce_2d_(
         const T* x_rows[ilp_factor];
         const T* y_rows[ilp_factor];
         const T* w_rows[ilp_factor];
-        for (int k = 0; k < ilp_factor; ++k) {
+        ForceUnroll<ilp_factor>{}([&](int k) {
             x_rows[k] = &x(i + k, 0);
             y_rows[k] = &y(i + k, 0);
             w_rows[k] = &w(i + k, 0);
-        }
+        });
 
         T dist[ilp_factor] = {0};
         for (intptr_t j = 0; j < x.shape[1]; ++j) {
-            for (int k = 0; k < ilp_factor; ++k) {
+            ForceUnroll<ilp_factor>{}([&](int k) {
                 auto val = map(x_rows[k][j * xs], y_rows[k][j * ys], w_rows[k][j * ws]);
                 dist[k] = reduce(dist[k], val);
-            }
+            });
         }
 
-        for (int k = 0; k < ilp_factor; ++k) {
+        ForceUnroll<ilp_factor>{}([&](int k) {
             out(i + k, 0) = project(dist[k]);
-        }
+        });
     }
     for (; i < x.shape[0]; ++i) {
         const T* x_row = &x(i, 0);
@@ -107,7 +168,7 @@ struct MinkowskiDistance {
     void operator()(StridedView2D<T> out, StridedView2D<const T> x, StridedView2D<const T> y) const {
         const T p = static_cast<T>(p_);
         const T invp = static_cast<T>(1.0 / p_);
-        transform_reduce_2d_(out, x, y, [p](T x, T y) {
+        transform_reduce_2d_(out, x, y, [p](T x, T y) INLINE_LAMBDA {
             auto diff = std::abs(x - y);
             return std::pow(diff, p);
         },
@@ -118,7 +179,7 @@ struct MinkowskiDistance {
     void operator()(StridedView2D<T> out, StridedView2D<const T> x, StridedView2D<const T> y, StridedView2D<const T> w) const {
         const T p = static_cast<T>(p_);
         const T invp = static_cast<T>(1.0 / p_);
-        transform_reduce_2d_(out, x, y, w, [p](T x, T y, T w) {
+        transform_reduce_2d_(out, x, y, w, [p](T x, T y, T w) INLINE_LAMBDA {
             auto diff = std::abs(x - y);
             return w * std::pow(diff, p);
         },
@@ -129,7 +190,7 @@ struct MinkowskiDistance {
 struct EuclideanDistance {
     template <typename T>
     void operator()(StridedView2D<T> out, StridedView2D<const T> x, StridedView2D<const T> y) const {
-        transform_reduce_2d_(out, x, y, [](T x, T y) {
+        transform_reduce_2d_(out, x, y, [](T x, T y) INLINE_LAMBDA {
             auto diff = std::abs(x - y);
             return diff * diff;
         },
@@ -138,7 +199,7 @@ struct EuclideanDistance {
 
     template <typename T>
     void operator()(StridedView2D<T> out, StridedView2D<const T> x, StridedView2D<const T> y, StridedView2D<const T> w) const {
-        transform_reduce_2d_(out, x, y, w, [](T x, T y, T w) {
+        transform_reduce_2d_(out, x, y, w, [](T x, T y, T w) INLINE_LAMBDA {
             auto diff = std::abs(x - y);
             return w * (diff * diff);
         },
@@ -149,7 +210,7 @@ struct EuclideanDistance {
 struct ChebyshevDistance {
     template <typename T>
     void operator()(StridedView2D<T> out, StridedView2D<const T> x, StridedView2D<const T> y) const {
-        transform_reduce_2d_(out, x, y, [](T x, T y) {
+        transform_reduce_2d_(out, x, y, [](T x, T y) INLINE_LAMBDA {
             return std::abs(x - y);
         },
         Identity{},
@@ -174,14 +235,14 @@ struct ChebyshevDistance {
 struct CityBlockDistance {
     template <typename T>
     void operator()(StridedView2D<T> out, StridedView2D<const T> x, StridedView2D<const T> y) const {
-        transform_reduce_2d_(out, x, y, [](T x, T y) {
+        transform_reduce_2d_<2>(out, x, y, [](T x, T y) INLINE_LAMBDA {
             return std::abs(x - y);
         });
     }
 
     template <typename T>
     void operator()(StridedView2D<T> out, StridedView2D<const T> x, StridedView2D<const T> y, StridedView2D<const T> w) const {
-        transform_reduce_2d_(out, x, y, w, [](T x, T y, T w) {
+        transform_reduce_2d_(out, x, y, w, [](T x, T y, T w) INLINE_LAMBDA {
             return w * std::abs(x - y);
         });
     }
