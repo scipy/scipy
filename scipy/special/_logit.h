e571af738d79a970fb94597fd77790c90ba60052
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2021-05-08 23:30:42 +0300

    MAINT: special: get rid of _logit.c.src (#13931)
    Ref gh-13930
    
    Get rid of `numpy.distutils` templating in `_logit.c.src` and use C++ templates instead.
    
    Note the need to manually instantiate templates because otherwise it fails to compile with:
    ```
    scipy/special/_ufuncs_cxx.cxx:4096:66: error: overloaded function with no contextual type information
     4096 |   __pyx_v_5scipy_7special_11_ufuncs_cxx__export_logit = ((void *)logit);
    ```
    
    Apparently `_generate_pyx.py` supports Cython fused types, so it can probably grow the template support, too. But that's for some other day certainly.

--- scipy/special/_logit.h
+++ scipy/special/_logit.h
@@ -1,14 +1,28 @@
 #ifndef _LOGIT_H_
 #define _LOGIT_H_
 
-#include "numpy/npy_math.h"
+#include<cmath>
 
-npy_float logitf(npy_float x);
-npy_double logit(npy_double x);
-npy_longdouble logitl(npy_longdouble x);
+template <typename T>
+inline T _logit(T x) {
+    x /= 1 - x;
+    return std::log(x);
+};
 
-npy_float expitf(npy_float x);
-npy_double expit(npy_double x);
-npy_longdouble expitl(npy_longdouble x);
+
+template<typename T>
+inline T _expit(T x) {
+    return 1 / (1 + std::exp(-x));
+};
+
+
+npy_float logitf(npy_float x)  {return _logit(x);};
+npy_double logit(npy_double x) {return _logit(x);};
+npy_longdouble logitl(npy_longdouble x) {return _logit(x);};
+
+npy_float expitf(npy_float x) {return _expit(x);};
+npy_double expit(npy_double x) {return _expit(x);};
+npy_longdouble expitl(npy_longdouble x) {return _expit(x);};
+
 
 #endif
