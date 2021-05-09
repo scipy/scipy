6ad13287d43613e45c82dfc64f2cca67866eceb9
steppi <albert_steppi@hms.harvard.edu>
2021-05-09 15:15:22 -0400

    Change tests to use assert_func_equal

--- scipy/special/tests/test_ndtri_exp.py
+++ scipy/special/tests/test_ndtri_exp.py
@@ -1,15 +1,20 @@
 import sys
 import pytest
 import numpy as np
-from numpy.testing import assert_allclose, assert_equal
+from numpy.testing import assert_equal
+from scipy.special import log_ndtr, ndtri_exp
+from scipy.special._testutils import assert_func_equal
 
-import scipy.special as sc
+
+def log_ndtr_ndtri_exp(y):
+    return log_ndtr(ndtri_exp(y))
 
 
-def uniform_random_points(left, right, num_points=1000, seed=None):
-    random_state = np.random.RandomState(seed)
-    points = random_state.random_sample(num_points)
-    return (right - left) * points + left
+@pytest.fixture(scope='class')
+def uniform_random_points():
+    random_state = np.random.RandomState(1729)
+    points = random_state.random_sample(1000)
+    return points
 
 
 class TestNdtriExp(object):
@@ -24,34 +29,33 @@ class TestNdtriExp(object):
     """
     @pytest.mark.parametrize('test_input',
                              [-1e1, -1e2, -1e10, -1e20, -sys.float_info.max])
-    def test_very_small_arg(self, test_input):
+    def test_very_small_arg(self, test_input, uniform_random_points):
         scale = test_input
-        points = scale * uniform_random_points(0.5, 1, num_points=10**2,
-                                               seed=561)
-        assert_allclose(sc.log_ndtr(sc.ndtri_exp(points)), points,
+        points = scale * (0.5 * uniform_random_points + 0.5)
+        assert_func_equal(log_ndtr_ndtri_exp, lambda y: y, points,
                         rtol=1e-14)
 
-    @pytest.mark.parametrize('test_input,expected_rtol',
-                             [((-10, -2, 1105), 1e-14),
-                              ((-2, -0.14542, 1729), 1e-12),
-                              ((-0.14542, -1e-6, 2465), 1e-10),
-                              ((-1e-6, 0, 2821), 1e-6)])
-    def test_in_interval(self, test_input, expected_rtol):
-        left, right, seed = test_input
-        points = uniform_random_points(left, right, num_points=10**3,
-                                       seed=seed)
-        assert_allclose(sc.log_ndtr(sc.ndtri_exp(points)), points,
+    @pytest.mark.parametrize('interval,expected_rtol',
+                             [((-10, -2), 1e-14),
+                              ((-2, -0.14542), 1e-12),
+                              ((-0.14542, -1e-6), 1e-10),
+                              ((-1e-6, 0), 1e-6)])
+    def test_in_interval(self, interval, expected_rtol,
+                         uniform_random_points):
+        left, right = interval
+        points = (right - left) * uniform_random_points + left
+        assert_func_equal(log_ndtr_ndtri_exp, lambda y: y, points,
                         rtol=expected_rtol)
 
     def test_extreme(self):
-        assert_allclose([sc.log_ndtr(sc.ndtri_exp(-sys.float_info.max)),
-                         sc.log_ndtr(sc.ndtri_exp(-sys.float_info.min))],
-                        [-sys.float_info.max, -sys.float_info.min],
-                        rtol=1e-12)
+        assert_func_equal(log_ndtr_ndtri_exp,
+                          lambda y: y,
+                          [-sys.float_info.max, -sys.float_info.min],
+                          rtol=1e-12)
 
     def test_asymptotes(self):
-        assert_equal(sc.ndtri_exp([-np.inf, 0.0]),
+        assert_equal(ndtri_exp([-np.inf, 0.0]),
                      [-np.inf,  np.inf])
 
     def test_outside_domain(self):
-        assert np.isnan(sc.ndtri_exp(1.0))
+        assert np.isnan(ndtri_exp(1.0))
