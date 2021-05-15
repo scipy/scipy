611a4f5feffafb4aebd1f47bd8fce456980227af
lokijuhy <laura.kinkead@uzh.ch>
2021-05-15 16:12:12 +0200

    MAINT: change neldermead maxfev print to warning

--- scipy/optimize/tests/test_optimize.py
+++ scipy/optimize/tests/test_optimize.py
@@ -291,6 +291,13 @@ class CheckOptimizeParameterized(CheckOptimize):
             assert self.gradcalls == 0
 
     def test_warn_neldermead(self):
+        with pytest.warns(UserWarning, match=r'Maximum number of function'):
+            func = lambda x: np.sum(x ** 2)
+            p0 = [0.15746215, 0.48087031, 0.44519198, 0.4223638, 0.61505159,
+                  0.32308456, 0.9692297, 0.4471682, 0.77411992, 0.80441652,
+                  0.35994957, 0.75487856, 0.99973421, 0.65063887, 0.09626474]
+            optimize.optimize._minimize_neldermead(func, p0, disp=True)
+
         with pytest.warns(UserWarning, match=r'Maximum number of iterations'):
             x0 = np.zeros(10)
             func = optimize.rosen
