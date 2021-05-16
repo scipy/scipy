7cc7d866681a5557d1b55d86e8e02ecd48953f69
akpandeya <akpandeya1@gmail.com>
2021-05-16 14:16:29 +0200

    MAINT: Closes #14038

--- scipy/stats/_sobol.pyi
+++ scipy/stats/_sobol.pyi
@@ -3,8 +3,56 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     import numpy.typing as npt
 
-def _test_find_index(
-    p_cumulative : npt.ArrayLike, 
-    size : int, 
-    value : float,
-    ) -> int:...
+def initialize_v(
+    v : npt.ArrayLike, 
+    dim : int
+) -> None: ...
+
+def _cscramble (
+    dim : int,
+    ltm : npt.ArrayLike,
+    sv: npt.ArrayLike
+) -> None: ...
+
+def _fill_p_cumulative(
+    p: npt.ArrayLike,
+    p_cumulative: npt.ArrayLike
+) -> None: ...
+
+def _categorize(
+    draws: npt.ArrayLike,
+    p_cumulative: npt.ArrayLike,
+    result: npt.ArrayLike
+) -> None: ...
+
+def _draw(
+    n : int,
+    num_gen: int,
+    dim: int,
+    sv: npt.ArrayLike,
+    quasi: npt.ArrayLike,
+    result: npt.ArrayLike
+    ) -> None: ...
+
+def _fast_forward(
+    n: int,
+    num_gen: int,
+    dim: int,
+    sv: npt.ArrayLike,
+    quasi: npt.ArrayLike
+    ) -> None: ...
+
+def _categorize(
+    draws: npt.ArrayLike,
+    p_cumulative: npt.ArrayLike,
+    result: npt.ArrayLike
+    ) -> None: ...
+
+def initialize_direction_numbers() -> None: ...
+
+_MAXDIM: int = ...
+_MAXBIT: int = ...
+ 
+
+
+
