95e5c85960c496dbea0c2d0cfb90debad252463f
Peter Bell <peterbell10@live.co.uk>
2021-05-17 23:25:51 +0100

    BLD: Avoid importing scipy.stats during cythonize stage

--- scipy/stats/_generate_pyx.py
+++ scipy/stats/_generate_pyx.py
@@ -1,5 +1,7 @@
 import pathlib
 from shutil import copyfile
+import subprocess
+import sys
 
 
 def isNPY_OLD():
@@ -22,37 +24,9 @@ def make_biasedurn():
 
 
 def make_boost():
-    # create target directory
-    (pathlib.Path(__file__).parent / '_boost/src').mkdir(exist_ok=True,
-                                                         parents=True)
-    src_dir = pathlib.Path(__file__).parent / '_boost/src'
-
-    # copy contents of include into directory to satisfy Cython
-    # PXD include conditions
-    inc_dir = pathlib.Path(__file__).parent / '_boost/include'
-    src = 'templated_pyufunc.pxd'
-    copyfile(inc_dir / src, src_dir / src)
-
-    # generate the PXD and PYX wrappers
-    from _boost.include.gen_func_defs_pxd import (  # type: ignore
-        _gen_func_defs_pxd)
-    from _boost.include.code_gen import _ufunc_gen  # type: ignore
-    from _boost._info import (  # type: ignore
-        _x_funcs, _no_x_funcs, _klass_mapper)
-    _gen_func_defs_pxd(
-        f'{src_dir}/func_defs.pxd',
-        x_funcs=_x_funcs,
-        no_x_funcs=_no_x_funcs)
-    for b, s in _klass_mapper.items():
-        _ufunc_gen(
-            scipy_dist=s.scipy_name,
-            types=['NPY_FLOAT', 'NPY_DOUBLE', 'NPY_LONGDOUBLE'],
-            ctor_args=s.ctor_args,
-            filename=f'{src_dir}/{s.scipy_name}_ufunc.pyx',
-            boost_dist=f'{b}_distribution',
-            x_funcs=_x_funcs,
-            no_x_funcs=_no_x_funcs,
-        )
+    # Call code generator inside _boost directory
+    code_gen = pathlib.Path(__file__).parent / '_boost/include/code_gen.py'
+    subprocess.run([sys.executable, str(code_gen)], check=True)
 
 
 if __name__ == '__main__':
