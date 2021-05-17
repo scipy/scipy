94528e302242285239a5b43f0cbf31c24c6090e5
paugier <pierre.augier@univ-grenoble-alpes.fr>
2021-05-17 17:52:04 +0200

    Improve the code snippet in signal.hilbert docstring.

--- scipy/signal/signaltools.py
+++ scipy/signal/signaltools.py
@@ -2256,16 +2256,15 @@ def hilbert(x, N=None, axis=-1):
     >>> instantaneous_frequency = (np.diff(instantaneous_phase) /
     ...                            (2.0*np.pi) * fs)
 
-    >>> fig = plt.figure()
-    >>> ax0 = fig.add_subplot(211)
+    >>> fig, (ax0, ax1) = plt.subplots(nrows=2)
     >>> ax0.plot(t, signal, label='signal')
     >>> ax0.plot(t, amplitude_envelope, label='envelope')
     >>> ax0.set_xlabel("time in seconds")
     >>> ax0.legend()
-    >>> ax1 = fig.add_subplot(212)
     >>> ax1.plot(t[1:], instantaneous_frequency)
     >>> ax1.set_xlabel("time in seconds")
     >>> ax1.set_ylim(0.0, 120.0)
+    >>> fig.tight_layout()
 
     References
     ----------
