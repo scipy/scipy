3a16cf955636905dca2a6e9f9495ee113b905bfb
Atsushi Sakai <asakai.amsl+github@gmail.com>
2021-05-19 21:25:05 +0900

    DOC: signal: improved docstring code based on review

--- scipy/signal/lti_conversion.py
+++ scipy/signal/lti_conversion.py
@@ -391,16 +391,16 @@ def cont2discrete(system, dt, method="zoh", alpha=None):
     >>> import matplotlib.pyplot as plt
     >>> from scipy.signal import cont2discrete, lti, dlti, dstep
 
-    Define a continious state-space system.
+    Define a continuous state-space system.
 
-    >>> A = np.array([[0, 1],[-10, -3]])
-    >>> B = np.array([[0],[10]])
-    >>> C = np.array([[1, 0]])
-    >>> D = np.array([[0]])
+    >>> A = np.array([[0, 1],[-10., -3]])
+    >>> B = np.array([[0],[10.]])
+    >>> C = np.array([[1., 0]])
+    >>> D = np.array([[0.]])
     >>> l_system = lti(A, B, C, D)
     >>> t, x = l_system.step(T=np.linspace(0, 5, 100))
-    >>> plt.plot(t, x, label='Continuous')
-    >>> plt.hlines(1, t[0], t[-1], linestyles='dotted')
+    >>> fig, ax = plt.subplots()
+    >>> ax.plot(t, x, label='Continuous', linewidth=3)
 
     Transform it to a discrete state-space system using several methods.
 
@@ -408,10 +408,10 @@ def cont2discrete(system, dt, method="zoh", alpha=None):
     >>> for method in ['zoh', 'bilinear', 'euler', 'backward_diff', 'foh', 'impulse']:
     ...    d_system = cont2discrete((A, B, C, D), dt, method=method)
     ...    s, x_d = dstep(d_system)
-    ...    plt.step(s, np.squeeze(x_d), label=method, where='post')
-    >>> plt.axis([t[0], t[-1], x[0], 1.4])
-    >>> plt.legend(loc='best')
-    >>> plt.tight_layout()
+    ...    ax.step(s, np.squeeze(x_d), label=method, where='post')
+    >>> ax.axis([t[0], t[-1], x[0], 1.4])
+    >>> ax.legend(loc='best')
+    >>> fig.tight_layout()
     >>> plt.show()
 
     References
