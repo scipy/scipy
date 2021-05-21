a54368fb81dfbdd9b27966ccb04ae16e3879feed
Anselm Hahn <Anselm.Hahn@gmail.com>
2021-05-21 08:23:09 +0200

    Update LSQUnivariateSpline(UnivariateSpline)
    Update the parameter of `LSQUnivariateSpline(UnivariateSpline)`

--- scipy/interpolate/fitpack2.py
+++ scipy/interpolate/fitpack2.py
@@ -696,23 +696,25 @@ class LSQUnivariateSpline(UnivariateSpline):
     Parameters
     ----------
     x : (N,) array_like
-        Input dimension of data points -- must be increasing
+        1-D array of independent input data. Must be increasing;
+        must be strictly increasing.
     y : (N,) array_like
-        Input dimension of data points
+        1-D array of dependent input data, of the same length as `x`.
     t : (M,) array_like
-        interior knots of the spline.  Must be in ascending order and::
+        1-D array of interior knots of the spline.
+        Must be in ascending order and::
 
             bbox[0] < t[0] < ... < t[-1] < bbox[-1]
 
     w : (N,) array_like, optional
-        weights for spline fitting. Must be positive. If None (default),
-        weights are all equal.
+        Weights for spline fitting. Must be positive. If `w` is None,
+        weights are all equal. Default is None.
     bbox : (2,) array_like, optional
         2-sequence specifying the boundary of the approximation interval. If
-        None (default), ``bbox = [x[0], x[-1]]``.
+        `bbox` is None, ``bbox=[x[0], x[-1]]``. Default is None
     k : int, optional
-        Degree of the smoothing spline.  Must be 1 <= `k` <= 5.
-        Default is `k` = 3, a cubic spline.
+        Degree of the smoothing spline. Must be 1 <= `k` <= 5.
+        ``k = 3`` is a cubic spline. Default is 3.
     ext : int or str, optional
         Controls the extrapolation mode for elements
         not in the interval defined by the knot sequence.
@@ -722,7 +724,7 @@ class LSQUnivariateSpline(UnivariateSpline):
         * if ext=2 or 'raise', raise a ValueError
         * if ext=3 of 'const', return the boundary value.
 
-        The default value is 0.
+        Default is 0.
 
     check_finite : bool, optional
         Whether to check that the input arrays contain only finite numbers.
@@ -734,7 +736,7 @@ class LSQUnivariateSpline(UnivariateSpline):
     Raises
     ------
     ValueError
-        If the interior knots do not satisfy the Schoenberg-Whitney conditions
+        If the interior knots do not satisfy the Schoenberg-Whitney conditions.
 
     See Also
     --------
