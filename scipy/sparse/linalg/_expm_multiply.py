39e8dcb1ee9271eeecbc3e24064e49c6b4db8db5
Weh Andreas <andreas.weh@student.uni-augsburg.de>
2021-06-15 12:33:56 +0200

    Move trace estimation into expm_multiply module

--- scipy/sparse/linalg/_expm_multiply.py
+++ scipy/sparse/linalg/_expm_multiply.py
@@ -8,8 +8,8 @@ import scipy.sparse.linalg
 from scipy.sparse.linalg import aslinearoperator
 from scipy.sparse.sputils import is_pydata_spmatrix
 from scipy.sparse.linalg.interface import IdentityOperator
-from scipy.sparse.linalg._traceest import traceest
 from scipy.sparse.linalg._onenormest import onenormest
+from scipy.linalg.decomp_qr import qr
 
 __all__ = ['expm_multiply']
 
@@ -42,6 +42,52 @@ def _trace(A):
         return A.to_scipy_sparse().diagonal().sum()
     else:
         return np.trace(A)
+
+
+def traceest(A, m3, seed=None):
+    """Estimate `np.trace(A)` using `3*m3` matrix-vector products.
+
+    The result is not deterministic.
+
+    Parameters
+    ----------
+    A : LinearOperator
+        Linear operator whose trace will be estimated. Has to be square.
+    m3 : int
+        Number of matrix-vector products divided by 3 used to estimate the trace.
+    seed : optional
+        Seed for `numpy.random.default_rng`.
+        Can be provided to obtain deterministic results.
+
+    Returns
+    -------
+    trace : LinearOperator.dtype
+        Estimate of the trace
+
+    Notes
+    -----
+    This is the Hutch++ algorithm given in [1]_.
+
+    References
+    ----------
+    .. [1] Meyer, Raphael A., Cameron Musco, Christopher Musco, and David P.
+       Woodruff. "Hutch++: Optimal Stochastic Trace Estimation." In Symposium
+       on Simplicity in Algorithms (SOSA), pp. 142-155. Society for Industrial
+       and Applied Mathematics, 2021
+       https://doi.org/10.1137/1.9781611976496.16
+
+    """
+    rng = np.random.default_rng(seed)
+    if len(A.shape) != 2 or A.shape[-1] != A.shape[-2]:
+        raise ValueError("Expected A to be like a square matrix.")
+    n = A.shape[-1]
+    S = rng.choice([-1.0, +1.0], [n, m3])
+    Q, _ = qr(A.matmat(S), overwrite_a=True, mode='economic')
+    trQAQ = np.trace(Q.conj().T @ A.matmat(Q))
+    G = rng.choice([-1, +1], [n, m3])
+    right = G - Q@(Q.conj().T @ G)
+    trGAG = np.trace(right.conj().T @ A.matmat(right))
+    return trQAQ + trGAG/m3
 
 
 def _ident_like(A):
