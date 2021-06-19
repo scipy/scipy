44e31b1eb202ef91caad059952ba268d2610a60b
Tyler Reddy <tyler.je.reddy@gmail.com>
2021-06-19 15:17:54 -0600

    REL: set version to 1.7.0 'final'

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ MINOR = 7
 MICRO = 0
 ISRELEASED = True
 IS_RELEASE_BRANCH = True
-VERSION = '%d.%d.%drc2' % (MAJOR, MINOR, MICRO)
+VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
 
 # Return the git revision as a string
