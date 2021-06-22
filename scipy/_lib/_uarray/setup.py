ac5246d976cd057f756e63512c14395a552feb48
Anirudh Dagar <anirudhdagar6@gmail.com>
2021-06-22 15:16:20 +0530

    MAINT: Move the compare version to 0.8

--- scipy/_lib/_uarray/setup.py
+++ scipy/_lib/_uarray/setup.py
@@ -20,7 +20,7 @@ def configuration(parent_package='', top_path=None):
     config.add_data_files('LICENSE')
     ext = config.add_extension('_uarray',
                                sources=['_uarray_dispatch.cxx', 'vectorcall.cxx'],
-                               depends=['small_dynamic_array.h', 'vectorcall.h']
+                               depends=['small_dynamic_array.h', 'vectorcall.h'],
                                language='c++')
     ext._pre_build_hook = pre_build_hook
     return config
