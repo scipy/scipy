cc05aecf5d47e7073851105fb22da465ac718c5e
Georgii Bocharov <georgeme007@gmail.com>
2021-07-04 10:18:02 -0400

    added regression tests for pickling of fit method

--- scipy/stats/tests/common_tests.py
+++ scipy/stats/tests/common_tests.py
@@ -302,6 +302,16 @@ def check_pickling(distfn, args):
     r1 = unpickled.rvs(size=8)
     npt.assert_equal(r0, r1)
 
+    # check pickling/unpickling of .fit method
+    fit_function = distfn.fit
+    pickled_fit_function = pickle.dumps(fit_function)
+    unpickled_fit_function = pickle.loads(pickled_fit_function)
+    
+    data = distfn.rvs(*args, size=8)
+    p0 = fit_function(data, *args)
+    p1 = unpickled_fit_function(data, *args)
+    npt.assert_equal(p0, p1)
+    
     # restore the random_state
     distfn.random_state = rndm
 
