4efc80ff9569c0b704770411712e59405255de85
Jebby993 <61740967+Jebby993@users.noreply.github.com>
2021-07-14 18:05:35 +0200

    Update interpolate.py

--- scipy/interpolate/interpolate.py
+++ scipy/interpolate/interpolate.py
@@ -71,7 +71,8 @@ def lagrange(x, w):
     >>> x_new = np.arange(0, 2.1, 0.1)
     >>> plt.scatter(x, y, label='data')
     >>> plt.plot(x_new, Polynomial(poly.coef[::-1])(x_new), label='Polynomial')
-    >>> plt.plot(x_new, 3*x_new**2 - 2*x_new + 0*x_new, label=r"$3 x^2 - 2 x$", linestyle='-.')
+    >>> plt.plot(x_new, 3*x_new**2 - 2*x_new + 0*x_new,
+				  label=r"$3 x^2 - 2 x$", linestyle='-.')
     >>> plt.legend()
     >>> plt.show()
 
