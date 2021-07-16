cb0700e8f0b46990def8a7b3f1ea62311258562e
Jebby993 <61740967+Jebby993@users.noreply.github.com>
2021-07-16 09:34:33 +0200

    DOC: interpolate: Fix the docstring example of "lagrange" (#14413)
    Reviewed at gh-14413

--- scipy/interpolate/interpolate.py
+++ scipy/interpolate/interpolate.py
@@ -64,8 +64,17 @@ def lagrange(x, w):
         \end{aligned}
 
     >>> from numpy.polynomial.polynomial import Polynomial
-    >>> Polynomial(poly).coef
-    array([ 3., -2.,  0.])
+    >>> Polynomial(poly.coef[::-1]).coef
+    array([ 0., -2.,  3.])
+
+    >>> import matplotlib.pyplot as plt
+    >>> x_new = np.arange(0, 2.1, 0.1)
+    >>> plt.scatter(x, y, label='data')
+    >>> plt.plot(x_new, Polynomial(poly.coef[::-1])(x_new), label='Polynomial')
+    >>> plt.plot(x_new, 3*x_new**2 - 2*x_new + 0*x_new,
+    ...          label=r"$3 x^2 - 2 x$", linestyle='-.')
+    >>> plt.legend()
+    >>> plt.show()
 
     """
 
