fca372842e1327385c6552bce849691572b541d9
Anirudh Dagar <anirudhdagar6@gmail.com>
2021-07-18 15:20:37 +0530

    ENH: Make args keyword only

--- scipy/integrate/_quad_vec.py
+++ scipy/integrate/_quad_vec.py
@@ -103,7 +103,7 @@ class _Bunch:
 
 def quad_vec(f, a, b, epsabs=1e-200, epsrel=1e-8, norm='2', cache_size=100e6, limit=10000,
              workers=1, points=None, quadrature=None, full_output=False,
-             args=()):
+             *, args=()):
     r"""Adaptive integration of a vector-valued function.
 
     Parameters
