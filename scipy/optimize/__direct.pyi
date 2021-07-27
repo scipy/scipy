7bfb8f413a1b5cb6c4a6569929fa99befcac5e4f
czgdp1807 <gdp.1807@gmail.com>
2021-07-27 14:07:34 +0530

    Fixed __direct.pyi syntax

--- scipy/optimize/__direct.pyi
+++ scipy/optimize/__direct.pyi
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING, Callable
+from typing import Any, TYPE_CHECKING, Callable, Tuple
 from .optimize import OptimizeResult
 from ._constraints import Bounds
 import numpy as np
@@ -8,7 +8,7 @@ if TYPE_CHECKING:
     import numpy.typing as npt
 
 def _minimize_direct(
-        func: Callable[...],
+        func: Callable[[npt.ArrayLike, Tuple[Any]], float],
         bounds: Bounds,
         args: tuple,
         disp: bool,
