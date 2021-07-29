3ea437f23729119887673994f829d95ed06af996
Anirudh Dagar <anirudhdagar6@gmail.com>
2021-07-29 09:27:02 +0530

    DEP: Specify version 2.0.0 for ndimage deprecation expiry

--- scipy/ndimage/interpolation.py
+++ scipy/ndimage/interpolation.py
@@ -1,6 +1,6 @@
-# This file is not meant for public use and will be removed in the future
-# versions of SciPy. Use the `scipy.ndimage` namespace for importing the
-# functions included below.
+# This file is not meant for public use and will be removed in SciPy v2.0.0.
+# Use the `scipy.ndimage` namespace for importing the functions
+# included below.
 
 import warnings
 from . import _interpolation
