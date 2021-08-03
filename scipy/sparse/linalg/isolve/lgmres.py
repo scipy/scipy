d6a03cb5ce9f2a02f51362c986f858d14f144313
Gang Zhao <zhaog6@lsec.cc.ac.cn>
2021-08-03 16:15:32 +0800

    ENH: sparse.linalg: The solution is zero when R.H.S. is zero

--- scipy/sparse/linalg/isolve/lgmres.py
+++ scipy/sparse/linalg/isolve/lgmres.py
@@ -143,7 +143,11 @@ def lgmres(A, b, x0=None, tol=1e-5, maxiter=1000, M=None, callback=None,
     axpy, dot, scal = None, None, None
     nrm2 = get_blas_funcs('nrm2', [b])
 
+    m, n = A.shape
     b_norm = nrm2(b)
+    if b_norm == 0:
+        x = b
+        return (postprocess(x), 0)
     ptol_max_factor = 1.0
 
     for k_outer in range(maxiter):
