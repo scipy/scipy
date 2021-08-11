101ace9dee53097203404cdc77195f2acc3bca5f
Matt Haberland <mhaberla@calpoly.edu>
2021-08-10 23:23:53 -0700

    ENH: stats: add Monte Carlo hypothesis test

--- scipy/stats/__init__.py
+++ scipy/stats/__init__.py
@@ -232,7 +232,6 @@ Summary statistics
    differential_entropy
    median_absolute_deviation
    median_abs_deviation
-   bootstrap
 
 Frequency statistics
 ====================
@@ -329,6 +328,15 @@ Quasi-Monte Carlo
 
    stats.qmc
 
+Resampling Methods
+==================
+
+.. autosummary::
+   :toctree: generated/
+
+   bootstrap
+   permutation_test
+   monte_carlo_test
 
 Masked statistics functions
 ===========================
@@ -449,7 +457,7 @@ from . import qmc
 from ._multivariate import *
 from . import contingency
 from .contingency import chi2_contingency
-from ._bootstrap import bootstrap
+from ._bootstrap import bootstrap, monte_carlo_test
 from ._entropy import *
 from ._hypotests import *
 from ._rvs_sampling import rvs_ratio_uniforms, NumericalInverseHermite
