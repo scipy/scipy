c2fa38c5af66ccd4da8747bad678909fd174e657
egorz734 <egorz734@mail.ru>
2021-08-12 09:54:30 +0300

    ENH: new boundary checks and tests

--- scipy/interpolate/tests/test_bsplines.py
+++ scipy/interpolate/tests/test_bsplines.py
@@ -492,9 +492,10 @@ class TestBSpline:
 
     def test_design_matrix_t_shapes(self):
         # test for minimal possible `t` shape
-        t = [0., 1., 1., 1., 2.]
-        des_matr = BSpline.design_matrix(1., t, 3).toarray()
-        assert_allclose(des_matr, [[1.]], atol=1e-14)
+        t = [1., 1., 1., 2., 3., 4., 4., 4.]
+        des_matr = BSpline.design_matrix(2., t, 3).toarray()
+        assert_allclose(des_matr, [[0.25, 0.58333333, 0.16666667, 0.]],
+                                                            atol=1e-9)
 
     def test_design_matrix_asserts(self):
         np.random.seed(1234)
