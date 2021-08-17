43a7bd65a10d1ee401f764c6bf73a00d3c4db938
czgdp1807 <gdp.1807@gmail.com>
2021-08-17 22:06:45 +0530

    Removed unused subroutine msmerge

--- scipy/linalg/src/id_dist/src/prini.f
+++ scipy/linalg/src/id_dist/src/prini.f
@@ -123,32 +123,7 @@ C
 CCCC    I=MRUN(J)
         RETURN
         END
-c 
-c 
-c 
-c 
-c 
-        subroutine msgmerge(a,b,c)
-        save
-        character *1 a(1),b(1),c(1),ast
-        data ast/'*'/
-c 
-        do 1200 i=1,1000
-c 
-        if(a(i) .eq. ast) goto 1400
-        c(i)=a(i)
-        iadd=i
- 1200 continue
-c 
- 1400 continue
-c 
-        do 1800 i=1,1000
-c 
-        c(iadd+i)=b(i)
-        if(b(i) .eq. ast) return
- 1800 continue
-        return
-        end
+
 c 
 c 
 c 
