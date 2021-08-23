5b89b3c4b99c197ed243e07d403e1bb9ae513682
Eric Schanet <eric.schanet@gmail.com>
2021-08-23 06:43:50 +0200

    DOC: add example usage of options param in scipy.optimize.linprog (#14628)
    * DOC: add example usage of options in scipy.optimize.linprog
    
    Co-authored-by: Matt Haberland <mhaberla@calpoly.edu>

--- scipy/optimize/_linprog.py
+++ scipy/optimize/_linprog.py
@@ -575,6 +575,21 @@ def linprog(c, A_ub=None, b_ub=None, A_eq=None, b_eq=None,
      success: True
            x: array([10., -3.]) # may vary
 
+    You can use the ``options`` parameter, e.g.,
+    to restrict the maximum number of iterations.
+
+    >>> res = linprog(c, A_ub=A, b_ub=b, bounds=[x0_bounds, x1_bounds],
+    ...               options={'maxiter': 4})
+    >>> print(res)
+        con: array([], dtype=float64)
+        fun: -21.35207150630407
+    message: 'The iteration limit was reached before the algorithm converged.'
+        nit: 4
+      slack: array([37.19406046,  0.5727398 ])
+     status: 1
+    success: False
+          x: array([ 9.4021973 , -2.98746855])
+
     """
 
     meth = method.lower()
