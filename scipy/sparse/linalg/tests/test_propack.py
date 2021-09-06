98249f98a995443f5e6eed2c730b26e40a2b0997
Matt Haberland <mhaberla@calpoly.edu>
2021-09-06 10:25:56 -0700

    TST: sparse.linalg: match error message in assert_raises

--- scipy/sparse/linalg/tests/test_propack.py
+++ scipy/sparse/linalg/tests/test_propack.py
@@ -3,8 +3,8 @@ import pytest
 import sys
 
 import numpy as np
-from numpy.testing import assert_allclose, assert_raises
-
+from numpy.testing import assert_allclose
+from pytest import raises as assert_raises
 from scipy.sparse.linalg._svdp import _svdp
 from scipy.sparse import csr_matrix, csc_matrix, coo_matrix
 
@@ -96,11 +96,13 @@ def test_svdp(ctor, precision, irl, which):
     dtype = _dtype_map[precision]
     n, m, k = 10, 20, 3
     if which == 'SM' and not irl:
-        with assert_raises(ValueError):
+        message = "`which`='SM' requires irl_mode=True"
+        with assert_raises(ValueError, match=message):
             check_svdp(n, m, ctor, dtype, k, irl, which)
     else:
         if is_32bit() and is_complex_type(dtype):
-            with assert_raises(TypeError):
+            message = 'PROPACK complex-valued SVD methods not available '
+            with assert_raises(TypeError, match=message):
                 check_svdp(n, m, ctor, dtype, k, irl, which)
         else:
             check_svdp(n, m, ctor, dtype, k, irl, which)
