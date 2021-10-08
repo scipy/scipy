6d2d761d1b289fd55969d2e2b9120ed97b3837df
Dima Pasechnik <dima@pasechnik.info>
2021-10-08 14:20:41 +0100

    also need type 'F' bump for Hermitean on Python 3.8-dbg

--- scipy/sparse/linalg/eigen/arpack/tests/test_arpack.py
+++ scipy/sparse/linalg/eigen/arpack/tests/test_arpack.py
@@ -73,7 +73,7 @@ def _get_test_tolerance(type_char, mattype=None, D_type=None, which=None):
         tol = 30 * np.finfo(np.float32).eps
         rtol *= 5
 
-    if type_char == 'D'  and which in ('LM','SM','LA') and \
+    if type_char in ('D','F')  and which in ('LM','SM','LA') and \
           D_type.name == "gen-hermitian-Mc":
         # missing case 2, and more, from PR 14798
         tol = 30 * np.finfo(np.float32).eps
