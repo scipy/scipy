8f4f24fe8fcfb77ecac742297a725fd4e8457dc6
Pantelis Antonoudiou <pantelis.antonoudiou@gmail.com>
2021-11-01 14:29:34 -0400

    DOC: adding example to decimate function (#14947)
    * adding example to decimate function
    
    * remove whitespace and group imports

--- scipy/signal/signaltools.py
+++ scipy/signal/signaltools.py
@@ -4395,6 +4395,42 @@ def decimate(x, q, n=None, ftype='iir', axis=-1, zero_phase=True):
     The ``zero_phase`` keyword was added in 0.18.0.
     The possibility to use instances of ``dlti`` as ``ftype`` was added in
     0.18.0.
+
+    Examples
+    --------
+
+    >>> from scipy import signal
+    >>> import matplotlib.pyplot as plt
+
+    Define wave parameters.
+
+    >>> wave_duration = 3
+    >>> sample_rate = 100
+    >>> freq = 2
+    >>> q = 5
+
+    Calculate number of samples.
+
+    >>> samples = wave_duration*sample_rate
+    >>> samples_decimated = int(samples/q)
+
+    Create cosine wave.
+
+    >>> x = np.linspace(0, wave_duration, samples, endpoint=False)
+    >>> y = np.cos(x*np.pi*freq*2)
+
+    Decimate cosine wave.
+
+    >>> ydem = signal.decimate(y, q)
+    >>> xnew = np.linspace(0, wave_duration, samples_decimated, endpoint=False)
+
+    Plot original and decimated waves.
+
+    >>> plt.plot(x, y, '.-', xnew, ydem, 'o-')
+    >>> plt.xlabel('Time, Seconds')
+    >>> plt.legend(['data', 'decimated'], loc='best')
+    >>> plt.show()
+
     """
 
     x = np.asarray(x)
