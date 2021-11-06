1c2953f4bf8c5ccfd3837c94a0a96db01be1aa86
Ralf Gommers <ralf.gommers@gmail.com>
2021-11-06 18:34:27 +0100

    REL: set version to 1.7.3.dev0
    [ci skip]

--- setup.py
+++ setup.py
@@ -57,8 +57,8 @@ Operating System :: MacOS
 
 MAJOR = 1
 MINOR = 7
-MICRO = 2
-ISRELEASED = True
+MICRO = 3
+ISRELEASED = False
 IS_RELEASE_BRANCH = True
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
