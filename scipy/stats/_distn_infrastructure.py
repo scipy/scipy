BUG: stats: fix broadcasting behavior of argsreduce (#14955)
* BUG: stats: fix broadcasting behavior of argsreduce
* TST: stats: add one more element in the array

Co-authored-by: Pamphile ROY <roy.pamphile@gmail.com>

scipy/stats/_distn_infrastructure.py
@@ -567,8 +567,9 @@ def argsreduce(cond, *args):
         newargs = [newargs, ]
 
     if np.all(cond):
-        # Nothing to do
-        return newargs
+        # broadcast arrays with cond
+        *newargs, cond = np.broadcast_arrays(*newargs, cond)
+        return [arg.ravel() for arg in newargs]
 
     s = cond.shape
     # np.extract returns flattened arrays, which are not broadcastable together