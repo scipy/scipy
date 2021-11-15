ff93f2ecd76195fa01b12199daa03120896445ec
Jan-Hendrik Müller <44469195+kolibril13@users.noreply.github.com>
2021-11-15 09:04:36 +0100

    code format, increased resolution, other angle

--- doc/source/tutorial/examples/gaussian_filter_plot1.py
+++ doc/source/tutorial/examples/gaussian_filter_plot1.py
@@ -1,59 +1,49 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import matplotlib as mpl
 from scipy.ndimage import gaussian_filter
 
 grids = 2
 boxs = 5
 x, y, z = np.indices((boxs * grids, boxs * grids, boxs * grids))
 
-def cube(pos=(0, 0, 0)):
-    o1 = pos[0]
-    o2 = pos[1]
-    o3 = pos[2]
+
+def make_cube(pos=(0, 0, 0)):  # make a boolean mask
     cube = (
-        (o1 <= x)
-        & (x < o1 + boxs)
-        & (o2 <= y)
-        & (y < o2 + boxs)
-        & (o3 <= z)
-        & (z < o3 + boxs)
+        (pos[0] <= x)
+        & (x < pos[0] + boxs)
+        & (pos[1] <= y)
+        & (y < pos[1] + boxs)
+        & (pos[2] <= z)
+        & (z < pos[2] + boxs)
     )
     return cube
 
 
 voxelarray = np.zeros((boxs * grids, boxs * grids, boxs * grids))
 
-voxelarray[cube((0, 0, 0))] = 1 * 255 / 8
-voxelarray[cube((boxs, 0, 0))] = 2 * 255 / 8
-voxelarray[cube((0, boxs, 0))] = 3 * 255 / 8
-voxelarray[cube((boxs, boxs, 0))] = 4 * 255 / 8
-voxelarray[cube((0, 0, boxs))] = 5 * 255 / 8
-voxelarray[cube((boxs, 0, boxs))] = 6 * 255 / 8
-voxelarray[cube((0, boxs, boxs))] = 7 * 255 / 8
-voxelarray[cube((boxs, boxs, boxs))] = 8 * 255 / 8
-voxelarray = np.uint8(voxelarray)
+i = 1
+for xi in range(0, 2):
+    for yi in range(0, 2):
+        for zi in range(0, 2):
+            cube = make_cube((xi * boxs, yi * boxs, zi * boxs))
+            voxelarray[cube] = i
+            i += 1
+
+voxelarray = np.uint8(voxelarray * 255 / 8)
 
 cmap = plt.get_cmap("YlGnBu")
-cols256 = []
-for i in range(cmap.N):
-    rgb = cmap(i)[:3]
-    cols256.append(mpl.colors.rgb2hex(rgb))
 
-def get_color(x):
-    return cols256[x]
-
-get_color = np.vectorize(get_color)
-
-colors = get_color(voxelarray)
 
 def plot_voxels(varray, fig, subp, title):
-    colors = get_color(varray)
+    colors = cmap(varray)
     ax = fig.add_subplot(*subp, projection="3d")
+    ax.view_init(30, 200)
     plt.axis("off")
     ax.voxels(varray, facecolors=colors, edgecolor="#000000", linewidth=0.1)
     ax.set_title(title)
 
+
+plt.rcParams["figure.dpi"] = 250
 fig = plt.figure(figsize=plt.figaspect(0.5))
 plot_voxels(voxelarray, fig, subp=(1, 3, 1), title="Original")
 voxelarray2 = gaussian_filter(voxelarray, sigma=1)
