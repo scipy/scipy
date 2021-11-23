3e5b7ca97df4c5ebb78de8f1054a86f435c8f1b5
Pamphile ROY <roy.pamphile@gmail.com>
2021-11-23 09:57:56 +0100

    BUG: out of bounds indexing in stats.qmc.update_discrepancy.
    Co-authored-by: 99991 <99991@users.noreply.github.com>

--- scipy/stats/tests/test_qmc.py
+++ scipy/stats/tests/test_qmc.py
@@ -180,14 +180,24 @@ class TestUtils:
             qmc.discrepancy(sample, workers=-2)
 
     def test_update_discrepancy(self):
+        # From Fang et al. Design and modeling for computer experiments, 2006
         space_1 = np.array([[1, 3], [2, 6], [3, 2], [4, 5], [5, 1], [6, 4]])
         space_1 = (2.0 * space_1 - 1.0) / (2.0 * 6.0)
 
         disc_init = qmc.discrepancy(space_1[:-1], iterative=True)
-        disc_iter = update_discrepancy(space_1[-1], space_1[:-1],
-                                       disc_init)
+        disc_iter = update_discrepancy(space_1[-1], space_1[:-1], disc_init)
 
         assert_allclose(disc_iter, 0.0081, atol=1e-4)
+
+        # n<d
+        rng = np.random.default_rng(241557431858162136881731220526394276199)
+        space_1 = rng.random((4, 10))
+
+        disc_ref = qmc.discrepancy(space_1)
+        disc_init = qmc.discrepancy(space_1[:-1], iterative=True)
+        disc_iter = update_discrepancy(space_1[-1], space_1[:-1], disc_init)
+
+        assert_allclose(disc_iter, disc_ref, atol=1e-4)
 
         # errors
         with pytest.raises(ValueError, match=r"Sample is not in unit "
