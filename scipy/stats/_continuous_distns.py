BUG: stats: Fix numerical issues of recipinvgauss
The distribution `recipinvgauss` had issues:

* The `pdf` and `logpdf` methods returned nan at x=0.
* The `cdf` method might return negative values for small x
  (e.g. `recipinvgauss.cdf(0.05, 0.5)` returned ≈ -1.31e-18,
  but the correct value is ≈ 6.59e-20).
* The `sf` method lost precision for large x, and for
  sufficiently large x, would return 0 when the true value was
  well within the limits of what can be represented with 64 bit
  floating point (e.g. `recipinvgauss.sf(80, 0.5)` would return
  0, but the correct value is ≈ 2.6998e-18).

scipy/stats/_continuous_distns.py
@@ -7287,16 +7287,25 @@ class recipinvgauss_gen(rv_continuous):
     def _pdf(self, x, mu):
         # recipinvgauss.pdf(x, mu) =
         #                     1/sqrt(2*pi*x) * exp(-(1-mu*x)**2/(2*x*mu**2))
-        return 1.0/np.sqrt(2*np.pi*x)*np.exp(-(1-mu*x)**2.0 / (2*x*mu**2.0))
+        return np.exp(self._logpdf(x, mu))
 
     def _logpdf(self, x, mu):
-        return -(1-mu*x)**2.0 / (2*x*mu**2.0) - 0.5*np.log(2*np.pi*x)
+        return _lazywhere(x > 0, (x, mu),
+                          lambda x, mu: (-(1 - mu*x)**2.0 / (2*x*mu**2.0)
+                                         - 0.5*np.log(2*np.pi*x)),
+                          fillvalue=-np.inf)
 
     def _cdf(self, x, mu):
         trm1 = 1.0/mu - x
         trm2 = 1.0/mu + x
         isqx = 1.0/np.sqrt(x)
-        return 1.0-_norm_cdf(isqx*trm1)-np.exp(2.0/mu)*_norm_cdf(-isqx*trm2)
+        return _norm_cdf(-isqx*trm1) - np.exp(2.0/mu)*_norm_cdf(-isqx*trm2)
+
+    def _sf(self, x, mu):
+        trm1 = 1.0/mu - x
+        trm2 = 1.0/mu + x
+        isqx = 1.0/np.sqrt(x)
+        return _norm_cdf(isqx*trm1) + np.exp(2.0/mu)*_norm_cdf(-isqx*trm2)
 
     def _rvs(self, mu, size=None, random_state=None):
         return 1.0/random_state.wald(mu, 1.0, size=size)
@@ -9053,6 +9062,7 @@ def _argus_phi(chi):
     for small chi.
     """
     return sc.gammainc(1.5, chi**2/2) / 2
+
 
 class argus_gen(rv_continuous):
     r"""