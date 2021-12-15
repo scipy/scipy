e5aef4c37b79e3c53b1fb3e9eb1f5746e2a66f94
warren <warren.weckesser@gmail.com>
2021-12-15 10:06:21 -0500

    BUG: sparse.linalg: Fix deprecation warnings.
    Attempting to use `scipy.sparse.linalg.eigen.arpack` or
    `scipy.sparse.linalg.dsolve.linsolve` resulted in an incorrect
    warning message.
    
    Closes gh-15206.

--- scipy/sparse/linalg/dsolve.py
+++ scipy/sparse/linalg/dsolve.py
@@ -25,8 +25,14 @@ def __getattr__(name):
             "scipy.sparse.linalg.dsolve is deprecated and has no attribute "
             f"{name}. Try looking in scipy.sparse.linalg instead.")
 
-    warnings.warn(f"Please use `{name}` from the `scipy.sparse.linalg` namespace, "
-                  "the `scipy.sparse.linalg.dsolve` namespace is deprecated.",
-                  category=DeprecationWarning, stacklevel=2)
+    if name in dsolve_modules:
+        msg = (f'The module `scipy.sparse.linalg.dsolve.{name}` is '
+               'deprecated. All public names must be imported directly from '
+               'the `scipy.sparse.linalg` namespace.')
+    else:
+        msg = (f"Please use `{name}` from the `scipy.sparse.linalg` namespace,"
+               " the `scipy.sparse.linalg.eigen` namespace is deprecated.")
+
+    warnings.warn(msg, category=DeprecationWarning, stacklevel=2)
 
     return getattr(_dsolve, name)
