1e793abe4cd7281ec99b501ad6e0ce052285f162
Jarrod Millman <jarrod.millman@gmail.com>
2021-12-17 10:00:39 -0800

    BLD: update setup.py for Python 3.10 (#15236)
    * BLD: update pyproject/setup for Python 3.10
    
    * Revert unnecessary changes

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ Programming Language :: Python
 Programming Language :: Python :: 3
 Programming Language :: Python :: 3.8
 Programming Language :: Python :: 3.9
+Programming Language :: Python :: 3.10
 Topic :: Software Development :: Libraries
 Topic :: Scientific/Engineering
 Operating System :: Microsoft :: Windows
