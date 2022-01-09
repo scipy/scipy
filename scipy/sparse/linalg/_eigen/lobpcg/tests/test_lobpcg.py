41965a0db6987f16bba2b9f2a8f460376b743bf7
Andrew Knyazev <andrew.knyazev@ucdenver.edu>
2022-01-08 21:55:10 -0500

    Update test_lobpcg.py
    Authors added

--- scipy/sparse/linalg/_eigen/lobpcg/tests/test_lobpcg.py
+++ scipy/sparse/linalg/_eigen/lobpcg/tests/test_lobpcg.py
@@ -1,6 +1,8 @@
 """ Test functions for the sparse.linalg._eigen.lobpcg module
 """
 # Author: Andrew Knyazev <Andrew.Knyazev@ucdenver.edu>
+#         Robert Cimrman
+#         Nils Wagner
 
 import itertools
 import platform
