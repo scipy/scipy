cca9f5ea48dad29ce290d4e7a8710be501e6869c
Matthew Brett <matthew.brett@gmail.com>
2022-01-12 22:10:52 +0000

    Separate cython compile from c compile.

--- scipy/_build_utils/cythoner.py
+++ scipy/_build_utils/cythoner.py
@@ -4,16 +4,11 @@ import os.path as op
 import sys
 import subprocess as sbp
 
-from Cython.Compiler.Main import Context
-
 _, in_fname, out_fname = sys.argv
 in_fname, out_fname = (op.abspath(p) for p in (in_fname, out_fname))
 
-module_name = Context([], []).extract_module_name(in_fname, object())
-
 res = sbp.run(
-    ['cython', '-3',
-    '--module-name', module_name,
+    ['cython', '-3', '--fast-fail',
     '--output-file', out_fname,
     '--include-dir', os.getcwd(),
      in_fname],
