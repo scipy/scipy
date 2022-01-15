93eebe8514680f5e26d56652756198e38b941b75
pmla <peter.mahler.larsen@gmail.com>
2022-01-15 13:12:42 +0100

    use `assert_allclose`

--- scipy/spatial/transform/tests/test_rotation.py
+++ scipy/spatial/transform/tests/test_rotation.py
@@ -128,7 +128,7 @@ def test_quaternion_ordering_invalid_input():
 def test_quaternion_ordering_scalar_first(num):
     x = Rotation.random(num=num, random_state=0).as_quat(order='wxyz')
     rot = Rotation.from_quat(x, order='wxyz')
-    assert_array_almost_equal(x, rot.as_quat(order='wxyz'))
+    assert_allclose(x, rot.as_quat(order='wxyz'))
 
 
 def test_as_matrix_from_generic_input():
