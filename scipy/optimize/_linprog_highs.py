9146c809885d8cea2f1db0e33697d76c3c8c8dcd
Matt Haberland <mhaberla@calpoly.edu>
2022-01-26 04:14:11 -0800

    TST: optimize: fix HiGHS status/message tests; add tests

--- scipy/optimize/_linprog_highs.py
+++ scipy/optimize/_linprog_highs.py
@@ -57,9 +57,10 @@ from scipy.sparse import csc_matrix, vstack, issparse
 
 
 def _highs_to_scipy_status_message(highs_status, highs_message):
+    """Converts HiGHS status number/message to SciPy status number/message"""
 
     scipy_statuses_messages = {
-        None: (4, "HiGHS did not provide a status code."),
+        None: (4, "HiGHS did not provide a status code. "),
         MODEL_STATUS_NOTSET: (4, ""),
         MODEL_STATUS_LOAD_ERROR: (4, ""),
         MODEL_STATUS_MODEL_ERROR: (2, ""),
@@ -69,37 +70,18 @@ def _highs_to_scipy_status_message(highs_status, highs_message):
         MODEL_STATUS_MODEL_EMPTY: (4, ""),
         MODEL_STATUS_RDOVUB: (4, ""),
         MODEL_STATUS_REACHED_OBJECTIVE_TARGET: (4, ""),
-        MODEL_STATUS_OPTIMAL: (
-            0,
-            "Optimization terminated successfully. ",
-        ),
-        MODEL_STATUS_REACHED_TIME_LIMIT: (
-            1,
-            "Time limit reached. ",
-        ),
-        MODEL_STATUS_REACHED_ITERATION_LIMIT: (
-            1,
-            "Iteration limit reached. ",
-        ),
-        MODEL_STATUS_INFEASIBLE: (
-            2,
-            "The problem is infeasible. ",
-        ),
-        MODEL_STATUS_UNBOUNDED: (
-            3,
-            "The problem is unbounded. ",
-        ),
-        MODEL_STATUS_UNBOUNDED_OR_INFEASIBLE: (
-            4,
-            "The problem is unbounded or infeasible. ",
-        ),
-    }
-    unrecognized = "The HiGHS status code was not recognized. "
+        MODEL_STATUS_OPTIMAL: (0, "Optimization terminated successfully. "),
+        MODEL_STATUS_REACHED_TIME_LIMIT: (1, "Time limit reached. "),
+        MODEL_STATUS_REACHED_ITERATION_LIMIT: (1, "Iteration limit reached. "),
+        MODEL_STATUS_INFEASIBLE: (2, "The problem is infeasible. "),
+        MODEL_STATUS_UNBOUNDED: (3, "The problem is unbounded. "),
+        MODEL_STATUS_UNBOUNDED_OR_INFEASIBLE: (4,
+            "The problem is unbounded or infeasible. ")}
+    unrecognized = (4, "The HiGHS status code was not recognized. ")
     scipy_status, scipy_message = (
-        scipy_statuses_messages.get(highs_status, unrecognized)
-        )
+        scipy_statuses_messages.get(highs_status, unrecognized))
     scipy_message = (f"{scipy_message}"
-                     "(HiGHS Status {highs_status}: {highs_message})")
+                     f"(HiGHS Status {highs_status}: {highs_message})")
     return scipy_status, scipy_message
 
 
--- scipy/optimize/tests/test_linprog.py
+++ scipy/optimize/tests/test_linprog.py
@@ -289,11 +289,11 @@ def test_choose_solver():
     _assert_success(res, desired_fun=-18.0, desired_x=[2, 6])
 
 
-def test_highs_status_message(self):
+def test_highs_status_message():
     res = linprog(1, method='highs')
-    msg = "Optimization terminated successfully. (HiGHS Status 7:..."
+    msg = "Optimization terminated successfully. (HiGHS Status 7:"
     assert res.status == 0
-    assert res.message == msg
+    assert res.message.startswith(msg)
 
     A, b, c, numbers, M = magic_square(6)
     bounds = [(0, 1)] * len(c)
@@ -301,26 +301,37 @@ def test_highs_status_message(self):
     options = {"time_limit": 0.1}
     res = linprog(c=c, A_eq=A, b_eq=b, bounds=bounds, method='highs',
                   options=options, integrality=integrality)
-    msg = "Time limit reached.  (HiGHS Status 7:..."
+    msg = "Time limit reached. (HiGHS Status 13:"
     assert res.status == 1
-    assert res.message == msg
+    assert res.message.startswith(msg)
 
     options = {"maxiter": 10}
-    res = linprog(c=c, A_eq=A, b_eq=b, bounds=bounds, method='highs',
-                  options=options, integrality=integrality)
-    msg = "Iteration limit reached.  (HiGHS Status 7:..."
+    res = linprog(c=c, A_eq=A, b_eq=b, bounds=bounds, method='highs-ds',
+                  options=options)
+    msg = "Iteration limit reached. (HiGHS Status 14:"
     assert res.status == 1
-    assert res.message == msg
+    assert res.message.startswith(msg)
 
     res = linprog(1, bounds=(1, -1), method='highs')
-    msg = "The problem is infeasible. (HiGHS Status 7:..."
+    msg = "The problem is infeasible. (HiGHS Status 8:"
     assert res.status == 2
-    assert res.message == msg
+    assert res.message.startswith(msg)
 
     res = linprog(-1, method='highs')
-    msg = "The problem is unbounded. (HiGHS Status 7:..."
+    msg = "The problem is unbounded. (HiGHS Status 10:"
     assert res.status == 3
-    assert res.message == msg
+    assert res.message.startswith(msg)
+
+    from scipy.optimize._linprog_highs import _highs_to_scipy_status_message
+    status, message = _highs_to_scipy_status_message(58, "Hello!")
+    msg = "The HiGHS status code was not recognized. (HiGHS Status 58:"
+    assert status == 4
+    assert message.startswith(msg)
+
+    status, message = _highs_to_scipy_status_message(None, None)
+    msg = "HiGHS did not provide a status code. (HiGHS Status None: None)"
+    assert status == 4
+    assert message.startswith(msg)
 
 
 A_ub = None
