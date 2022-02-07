d02b467a208399b0ae7b4232fc5e92e4b017f01e
Samuel Wallan <44255917+swallan@users.noreply.github.com>
2022-02-07 11:31:20 -0800

    TST: check for p>=0 to accommodate linux32bit

--- scipy/stats/tests/test_distributions.py
+++ scipy/stats/tests/test_distributions.py
@@ -5236,11 +5236,12 @@ class TestStudentizedRange:
         assert_(stats.studentized_range._argcheck(k, df))
 
     def test_clipping(self):
-        # The result of this computation was -9.9253938401489e-14, but p-values
-        # should not be negative. The result from mpmath is 0.
+        # The result of this computation was -9.9253938401489e-14 on some
+        # systems, but p-values should not be negative. The result from mpmath
+        # is 0.
         q, k, v = 34.6413996195345746, 3, 339
         p = stats.studentized_range.sf(q, k, v)
-        assert_equal(p, 0)
+        assert_equal(p >= 0, True)
 
 
 def test_540_567():
