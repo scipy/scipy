e943df8ca625405f18669c2c3f37e8cff5ec2524
Tirth Patel <tirthasheshpatel@gmail.com>
2022-02-08 19:39:29 +0530

    TST: stats: add tests for 1D cases, zero-sized output case, etc

--- scipy/stats/tests/test_axis_nan_policy.py
+++ scipy/stats/tests/test_axis_nan_policy.py
@@ -347,39 +347,65 @@ def test_axis_nan_policy_axis_is_None(hypotest, args, kwds, n_samples,
                 assert_equal(res1dc, res1da)
 
 
-@pytest.mark.parametrize(("hypotest", "args", "kwds", "n_samples", "n_outputs",
-                          "paired", "unpacker"), axis_nan_policy_cases)
+# Test keepdims for:
+#     - single-output and multi-output functions (gmean and mannwhitneyu)
+#     - Axis negative, positive, None, and tuple
+#     - 1D with no NaNs
+#     - 1D with NaN propagation
+#     - Zero-sized output
 @pytest.mark.parametrize("nan_policy", ("omit", "propagate"))
-@pytest.mark.parametrize("axis", (None, 0, -1, (0, 2), (1, -1), (3, 1, 2, 0)))
-def test_keepdims(hypotest, args, kwds, n_samples, n_outputs, paired, unpacker,
-                  axis, nan_policy):
+@pytest.mark.parametrize(
+    ("hypotest", "args", "kwds", "n_samples", "unpacker"),
+    ((stats.gmean, tuple(), dict(), 1, lambda x: (x,)),
+     (stats.mannwhitneyu, tuple(), {'method': 'asymptotic'}, 2, None))
+)
+@pytest.mark.parametrize(
+    ("sample_shape", "axis_cases"),
+    (((2, 3, 3, 4), (None, 0, -1, (0, 2), (1, -1), (3, 1, 2, 0))),
+     ((10, ), (0, -1)),
+     ((20, 0), (0, 1)))
+)
+def test_keepdims(hypotest, args, kwds, n_samples, unpacker,
+                  sample_shape, axis_cases, nan_policy):
     # test if keepdims parameter works correctly
     if not unpacker:
         def unpacker(res):
             return res
     rng = np.random.default_rng(0)
-    data = [rng.random((2, 3, 3, 4)) for _ in range(n_samples)]
-    nan_mask = [rng.random((2, 3, 3, 4)) < 0.2 for _ in range(n_samples)]
-    for sample, mask in zip(data, nan_mask):
+    data = [rng.random(sample_shape) for _ in range(n_samples)]
+    nan_data = [sample.copy() for sample in data]
+    nan_mask = [rng.random(sample_shape) < 0.2 for _ in range(n_samples)]
+    for sample, mask in zip(nan_data, nan_mask):
         sample[mask] = np.nan
-    expected_shape = list(data[0].shape)
-    if axis is None:
-        expected_shape = [1, 1, 1, 1]
-    else:
-        if isinstance(axis, int):
-            expected_shape[axis] = 1
+    for axis in axis_cases:
+        expected_shape = list(sample_shape)
+        if axis is None:
+            expected_shape = np.ones(len(sample_shape))
         else:
-            for ax in axis:
-                expected_shape[ax] = 1
-    expected_shape = tuple(expected_shape)
-    res = unpacker(hypotest(*data, axis=axis, keepdims=True,
-                            nan_policy=nan_policy, *args, **kwds))
-    res_base = unpacker(hypotest(*data, axis=axis, keepdims=False,
-                                 nan_policy=nan_policy, *args, **kwds))
-    for r, r_base in zip(res, res_base):
-        assert r.shape == expected_shape
-        r = np.squeeze(r, axis=axis)
-        assert_equal(r, r_base)
+            if isinstance(axis, int):
+                expected_shape[axis] = 1
+            else:
+                for ax in axis:
+                    expected_shape[ax] = 1
+        expected_shape = tuple(expected_shape)
+        res = unpacker(hypotest(*data, *args, axis=axis, keepdims=True,
+                                **kwds))
+        res_base = unpacker(hypotest(*data, *args, axis=axis, keepdims=False,
+                                     **kwds))
+        nan_res = unpacker(hypotest(*nan_data, *args, axis=axis,
+                                    keepdims=True, nan_policy=nan_policy,
+                                    **kwds))
+        nan_res_base = unpacker(hypotest(*nan_data, *args, axis=axis,
+                                         keepdims=False,
+                                         nan_policy=nan_policy, **kwds))
+        for r, r_base, rn, rn_base in zip(res, res_base, nan_res,
+                                          nan_res_base):
+            assert r.shape == expected_shape
+            r = np.squeeze(r, axis=axis)
+            assert_equal(r, r_base)
+            assert rn.shape == expected_shape
+            rn = np.squeeze(rn, axis=axis)
+            assert_equal(rn, rn_base)
 
 
 @pytest.mark.parametrize(("axis"), (0, 1, 2))
