943fe1826bcea027a4c836fc7c47cdf95b91e0d9
Anirudh Dagar <anirudhdagar6@gmail.com>
2022-02-17 01:44:35 +0100

    STY: Fix linting errors

--- scipy/datasets/__init__.py
+++ scipy/datasets/__init__.py
@@ -2,12 +2,16 @@
 ================================
 Datasets (:mod:`scipy.datasets`)
 ================================
+
 .. currentmodule:: scipy.datasets
+
 .. autosummary::
    :toctree: generated/
+
    ascent - Get example image for processing
    face - Get example image for processing
    electrocardiogram - Load an example of a one-dimensional signal.
+
 """
 
 from ._fetchers import face, ascent, electrocardiogram
