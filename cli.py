cdab762ca21f6241225dc0eb4f34468474604e50
sayantikabanik <sayantikabanik122@gmail.com>
2022-03-01 15:00:08 +0530

    help text added

--- cli.py
+++ cli.py
@@ -1,8 +1,9 @@
 """
-Info:
+Info: Run tests, builds and other tasks using, typer and system package
 -------
     Basic example combining typer and system package
     cmd options:
+        $ python cli.py --help
         $ python cli.py bench <flag: -t/-s>
         $ python cli.py test <flag: -t/-s> <module>
         $ python cli.py build
@@ -16,17 +17,26 @@ app = typer.Typer()
 
 @app.command()
 def build():
+    """
+    Run Scipy build
+    """
     os.system("python dev.py --build-only")
 
 
 @app.command(context_settings={"allow_extra_args": True, "ignore_unknown_options": True})
 def bench(ctx: typer.Context):
+    """
+    Run benchmark options
+    """
     for extra_arg in ctx.args:
         os.system(f"python dev.py --bench {extra_arg} integrate.SolveBVP")
 
 
 @app.command(context_settings={"allow_extra_args": True, "ignore_unknown_options": True})
 def test(ctx: typer.Context):
+    """
+    Run test for a given module
+    """
     _arg = ""
     for extra_arg in ctx.args:
         _arg += extra_arg + " "
