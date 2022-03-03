0b25749b7ffd8a9175155ed43fdb333967d3e845
Jake Bowhay <jb9.bowhay@gmail.com>
2022-03-03 21:07:05 +0000

    TST: parameterise tests

--- scipy/special/tests/test_basic.py
+++ scipy/special/tests/test_basic.py
@@ -1319,30 +1319,32 @@ class TestCombinatorics:
         expected = 100891344545564193334812497256
         assert_equal(special.comb(100, 50, exact=True), expected)
 
-        with pytest.warns(
-            DeprecationWarning,
-            match=r"Non-integer arguments are currently being cast to",
-        ):
-            assert_equal(
-                special.comb(4.5, 3, exact=True, legacy=True),
-                special.comb(4, 3, exact=True),
-            )
-        with pytest.warns(
-            DeprecationWarning,
-            match=r"Non-integer arguments are currently being cast to",
-        ):
-            assert_equal(
-                special.comb(4, 3.5, exact=True, legacy=True),
-                special.comb(4, 3, exact=True),
-            )
-        assert_equal(
-            special.comb(4.5, 3, exact=True, legacy=False),
-            special.comb(4.5, 3),
-        )
-        assert_equal(
-            special.comb(4, 3.5, exact=True, legacy=False),
-            special.comb(4, 3.5),
-        )
+    @pytest.mark.parametrize("repetition", [True, False])
+    @pytest.mark.parametrize("legacy", [True, False])
+    @pytest.mark.parametrize("k", [3.5, 3])
+    @pytest.mark.parametrize("N", [4.5, 4])
+    def test_comb_legacy(self, N, k, legacy, repetition):
+        # test is only relevant for exact=True
+        if legacy and (N != int(N) or k != int(k)):
+            with pytest.warns(
+                DeprecationWarning,
+                match=r"Non-integer arguments are currently being cast to",
+            ):
+                result = special.comb(N, k, exact=True, legacy=legacy,
+                                      repetition=repetition)
+        else:
+            result = special.comb(N, k, exact=True, legacy=legacy,
+                                  repetition=repetition)
+        if legacy:
+            # for exact=True and legacy=True, cast input arguments, else don't
+            if repetition:
+                N, k = int(N + k - 1), int(k)
+                repetition = False
+            else:
+                N, k = int(N), int(k)
+        # expected result is the same as with exact=False
+        expected = special.comb(N, k, legacy=legacy, repetition=repetition)
+        assert_equal(result, expected)
 
     def test_comb_with_np_int64(self):
         n = 70
