feabe01f4b00a33b2cb6763a7ea076ec2351b156
Tirth Patel <tirthasheshpatel@gmail.com>
2022-03-04 14:02:40 +0530

    MAINT: remove np.errstate context altogether
    [skip actions] [skip circle]

--- scipy/signal/windows/_windows.py
+++ scipy/signal/windows/_windows.py
@@ -336,9 +336,7 @@ def bohman(M, sym=True):
     >>> plt.figure()
     >>> A = fft(window, 2048) / (len(window)/2.0)
     >>> freq = np.linspace(-0.5, 0.5, len(A))
-    >>> with np.errstate(divide='ignore'):
-    ...     response = 20 * np.log10(np.abs(fftshift(A / abs(A).max())))
-    ...
+    >>> response = 20 * np.log10(np.abs(fftshift(A / abs(A).max())))
     >>> plt.plot(freq, response)
     >>> plt.axis([-0.5, 0.5, -120, 0])
     >>> plt.title("Frequency response of the Bohman window")
@@ -1601,9 +1599,7 @@ def cosine(M, sym=True):
     >>> plt.figure()
     >>> A = fft(window, 2048) / (len(window)/2.0)
     >>> freq = np.linspace(-0.5, 0.5, len(A))
-    >>> with np.errstate(divide='ignore'):
-    ...     response = 20 * np.log10(np.abs(fftshift(A / abs(A).max())))
-    ...
+    >>> response = 20 * np.log10(np.abs(fftshift(A / abs(A).max())))
     >>> plt.plot(freq, response)
     >>> plt.axis([-0.5, 0.5, -120, 0])
     >>> plt.title("Frequency response of the cosine window")
