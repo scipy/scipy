BENCH: speed factorial benchmarks up and fix overflow error

benchmarks/benchmarks/special.py
@@ -77,9 +77,9 @@ class Expn(Benchmark):
 
 class Factorial(Benchmark):
     def setup(self, *args):
-        self.positive_ints = np.arange(100, 1001, step=10, dtype=int)
+        self.positive_ints = np.arange(10, 111, step=20, dtype=int)
         self.negative_ints = -1 * self.positive_ints
-        self.positive_floats = np.linspace(100.2, 10000.8)
+        self.positive_floats = np.linspace(100.2, 1000.8, num=10)
         self.negative_floats = -1 * self.positive_floats
 
     @with_attributes(params=[(100, 1000, 10000)],
@@ -110,7 +110,7 @@ class Factorial(Benchmark):
     def time_factorial_exact_false_array_negative_float(self):
         factorial(self.negative_floats, exact=False)
 
-    @with_attributes(params=[(100, 1000, 5000)],
+    @with_attributes(params=[(100, 200, 400)],
                      param_names=['n'])
     def time_factorial_exact_true_scalar_positive_int(self, n):
         factorial(n, exact=True)
@@ -133,10 +133,10 @@ class Factorial(Benchmark):
 
 class Factorial2(Benchmark):
     def setup(self, *args):
-        self.positive_ints = np.arange(100, 1001, step=10, dtype=int)
+        self.positive_ints = np.arange(100, 201, step=20, dtype=int)
         self.negative_ints = -1 * self.positive_ints
 
-    @with_attributes(params=[(100, 1000, 2000)],
+    @with_attributes(params=[(100, 200, 400)],
                      param_names=['n'])
     def time_factorial2_exact_false_scalar_positive_int(self, n):
         factorial2(n, exact=False)
@@ -150,7 +150,7 @@ class Factorial2(Benchmark):
     def time_factorial2_exact_false_array_negative_int(self):
         factorial2(self.negative_ints, exact=False)
 
-    @with_attributes(params=[(100, 1000, 2000)],
+    @with_attributes(params=[(100, 200, 400)],
                      param_names=['n'])
     def time_factorial2_exact_true_scalar_positive_int(self, n):
         factorial2(n, exact=True)
@@ -160,7 +160,7 @@ class Factorial2(Benchmark):
 
 
 class FactorialK(Benchmark):
-    @with_attributes(params=[(100, 1000), range(1, 10)],
+    @with_attributes(params=[(100, 500), range(1, 10)],
                      param_names=['n', 'k'])
     def time_factorialk_exact_true_scalar_positive_int(self, n, k):
         factorialk(n, k, exact=True)