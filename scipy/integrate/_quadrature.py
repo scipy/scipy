97174cc3de5a108658d74284a8ae9756aecd15d6
k-styles <kartikhanand@gmail.com>
2022-03-20 20:10:05 +0530

    handle integrate.simpson base case when span is zero

--- scipy/integrate/_quadrature.py
+++ scipy/integrate/_quadrature.py
@@ -411,9 +411,9 @@ def _basic_simpson(y, start, stop, x, dx, axis):
         h1 = h[sl1]
         hsum = h0 + h1
         hprod = h0 * h1
-        h0divh1 = h0 / h1
-        tmp = hsum/6.0 * (y[slice0] * (2 - 1.0/h0divh1) +
-                          y[slice1] * (hsum * hsum / hprod) +
+        h0divh1 = np.divide(h0, h1, out=np.zeros_like(h0), where=h1!=0, casting="unsafe")
+        tmp = hsum/6.0 * (y[slice0] * ( 2 - np.divide(1.0, h0divh1, out=np.zeros_like(h0divh1), where=h0divh1!=0, casting="unsafe") ) +
+                          y[slice1] * ( hsum * np.divide(hsum, hprod, out=np.zeros_like(hsum), where=hprod!=0, casting="unsafe") ) +
                           y[slice2] * (2 - h0divh1))
         result = np.sum(tmp, axis=axis)
     return result
