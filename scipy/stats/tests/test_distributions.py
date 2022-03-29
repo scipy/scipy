b9f96bc12d0ab178a022f7c53da2125b67e8300c
Warren Weckesser <warren.weckesser@gmail.com>
2022-03-29 13:59:46 -0400

    ENH: stats: Implement _sf and _isf for norminvgauss. (#13338)

--- scipy/stats/tests/test_distributions.py
+++ scipy/stats/tests/test_distributions.py
@@ -612,6 +612,68 @@ class TestNormInvGauss:
         vals_pdf = stats.norminvgauss.pdf(x_test, a=1, b=0.5)
         assert_allclose(vals_pdf, r_pdf, atol=1e-9)
 
+    @pytest.mark.parametrize('x, a, b, sf, rtol',
+                             [(-1, 1, 0, 0.8759652211005315, 1e-13),
+                              (25, 1, 0, 1.1318690184042579e-13, 1e-4),
+                              (1, 5, -1.5, 0.002066711134653577, 1e-12),
+                              (10, 5, -1.5, 2.308435233930669e-29, 1e-9)])
+    def test_sf_isf_mpmath(self, x, a, b, sf, rtol):
+        # The data in this test is based on this code that uses mpmath:
+        #
+        # -----
+        # import mpmath
+        #
+        # mpmath.mp.dps = 50
+        #
+        # def pdf(x, a, b):
+        #     x = mpmath.mpf(x)
+        #     a = mpmath.mpf(a)
+        #     b = mpmath.mpf(b)
+        #     g = mpmath.sqrt(a**2 - b**2)
+        #     t = mpmath.sqrt(1 + x**2)
+        #     return (a * mpmath.besselk(1, a*t) * mpmath.exp(g + b*x)
+        #             / (mpmath.pi * t))
+        #
+        # def sf(x, a, b):
+        #     return mpmath.quad(lambda x: pdf(x, a, b), [x, mpmath.inf])
+        #
+        # -----
+        #
+        # In particular,
+        #
+        # >>> float(sf(-1, 1, 0))
+        # 0.8759652211005315
+        # >>> float(sf(25, 1, 0))
+        # 1.1318690184042579e-13
+        # >>> float(sf(1, 5, -1.5))
+        # 0.002066711134653577
+        # >>> float(sf(10, 5, -1.5))
+        # 2.308435233930669e-29
+
+        s = stats.norminvgauss.sf(x, a, b)
+        assert_allclose(s, sf, rtol=rtol)
+        i = stats.norminvgauss.isf(sf, a, b)
+        assert_allclose(i, x, rtol=rtol)
+
+    def test_sf_isf_mpmath_vectorized(self):
+        x = [-1, 25]
+        a = [1, 1]
+        b = 0
+        sf = [0.8759652211005315, 1.1318690184042579e-13]  # see previous test
+        s = stats.norminvgauss.sf(x, a, b)
+        assert_allclose(s, sf, rtol=1e-13, atol=1e-16)
+        i = stats.norminvgauss.isf(sf, a, b)
+        # Not perfect, but better than it was. See gh-13338.
+        assert_allclose(i, x, rtol=1e-6)
+
+    def test_gh8718(self):
+        # Add test that gh-13338 resolved gh-8718
+        dst = stats.norminvgauss(1, 0)
+        x = np.arange(0, 20, 2)
+        sf = dst.sf(x)
+        isf = dst.isf(sf)
+        assert_allclose(isf, x)
+
     def test_stats(self):
         a, b = 1, 0.5
         gamma = np.sqrt(a**2 - b**2)
