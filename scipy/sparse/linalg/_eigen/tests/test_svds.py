806d8d2c5f6f24e10749f7c23365c0d0a267c448
Andrew Knyazev <andrew.knyazev@ucdenver.edu>
2022-03-30 13:50:29 -0400

    remove test for unsupported dtypes in ARPACK

--- scipy/sparse/linalg/_eigen/tests/test_svds.py
+++ scipy/sparse/linalg/_eigen/tests/test_svds.py
@@ -679,8 +679,8 @@ class SVDSCommonTests:
         assert_array_equal(s, 0)
 
     @pytest.mark.parametrize("shape", ((20, 20), (20, 21), (21, 20)))
-    @pytest.mark.parametrize("dtype", DTYPES)
-    # @pytest.mark.parametrize("dtype", (float, complex, np.float32))
+    # ARPACK supports only dtype float, complex, or np.float32
+    @pytest.mark.parametrize("dtype", (float, complex, np.float32))
     def test_small_sigma(self, shape, dtype):
         # https://github.com/scipy/scipy/pull/11829
         if dtype == complex and self.solver == 'propack':
