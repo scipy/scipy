50ad9e100e07c0268dd92f96cf3baa464fe251f9
Andrew Knyazev <andrew.knyazev@ucdenver.edu>
2022-03-30 23:22:08 -0400

    qr mistake fixed

--- scipy/sparse/linalg/_eigen/_svds.py
+++ scipy/sparse/linalg/_eigen/_svds.py
@@ -326,7 +326,8 @@ def svds(A, k=6, ncv=None, tol=0, which='LM', v0=None,
         _, eigvec = eigsh(XH_X, k=k, tol=tol ** 2, maxiter=maxiter,
                                 ncv=ncv, which=which, v0=v0)
 
-    u = X_matmat(np.linalg.qr(eigvec))
+    q, _ = np.linalg.qr(eigvec)
+    u = X_matmat(q)
     if not return_singular_vectors:
         s = svd(u, compute_uv=False)
         return s[::-1]
