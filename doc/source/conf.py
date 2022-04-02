27195e4870cd0155b19545df3d2479e2d437efd0
Pamphile Roy <roy.pamphile@gmail.com>
2022-04-02 22:44:20 +0200

    DOC: fix missing comma in conf

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -190,7 +190,7 @@ html_theme_options = {
   "github_url": "https://github.com/scipy/scipy",
   "navbar_end": ["version-switcher", "navbar-icon-links"],
   "switcher": {
-      "json_url": "https://scipy.github.io/devdocs/_static/version_switcher.json"
+      "json_url": "https://scipy.github.io/devdocs/_static/version_switcher.json",
       "version_match": version,
   }
 }
