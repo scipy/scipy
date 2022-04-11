f211d187f10f6e32943b3a87445edde19bae8cc2
Eli Rykoff <erykoff@stanford.edu>
2022-04-11 12:52:19 -0700

    Add MemoizeJac to public exports.

--- scipy/optimize/_optimize.py
+++ scipy/optimize/_optimize.py
@@ -21,7 +21,7 @@ __all__ = ['fmin', 'fmin_powell', 'fmin_bfgs', 'fmin_ncg', 'fmin_cg',
            'fminbound', 'brent', 'golden', 'bracket', 'rosen', 'rosen_der',
            'rosen_hess', 'rosen_hess_prod', 'brute', 'approx_fprime',
            'line_search', 'check_grad', 'OptimizeResult', 'show_options',
-           'OptimizeWarning']
+           'OptimizeWarning', 'MemoizeJac']
 
 __docformat__ = "restructuredtext en"
 
