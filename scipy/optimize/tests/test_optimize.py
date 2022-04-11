72427c944aa649705f40b67d9631663190f2fdf2
Eli Rykoff <erykoff@stanford.edu>
2022-04-11 12:53:22 -0700

    Import from public namespaces where possible.

--- scipy/optimize/tests/test_optimize.py
+++ scipy/optimize/tests/test_optimize.py
@@ -20,14 +20,17 @@ import pytest
 from pytest import raises as assert_raises
 
 from scipy import optimize
-from scipy.optimize._minimize import Bounds, NonlinearConstraint
+from scipy.optimize import (Bounds,
+                            NonlinearConstraint,
+                            ScalarFunction,
+                            MemoizeJac,
+                            show_options)
 from scipy.optimize._minimize import MINIMIZE_METHODS, MINIMIZE_SCALAR_METHODS
 from scipy.optimize._linprog import LINPROG_METHODS
 from scipy.optimize._root import ROOT_METHODS
 from scipy.optimize._root_scalar import ROOT_SCALAR_METHODS
 from scipy.optimize._qap import QUADRATIC_ASSIGNMENT_METHODS
-from scipy.optimize._differentiable_functions import ScalarFunction, FD_METHODS
-from scipy.optimize._optimize import MemoizeJac, show_options
+from scipy.optimize._differentiable_functions import FD_METHODS
 
 
 def test_check_grad():
