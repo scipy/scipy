6a5d2e7622c5f590b4a12df42826999b8f84ec29
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2022-04-20 20:56:04 +0300

    DOC: interpolate: add a second docstring example for RGI

--- scipy/interpolate/_interpolate.py
+++ scipy/interpolate/_interpolate.py
@@ -2432,6 +2432,38 @@ class RegularGridInterpolator:
     (125.54200000000002, 145.894)
 
 
+    As a second example, we interpolate and extrapolate a 2D data:
+
+    >>> x, y = np.array([-2, 0, 4]), np.array([-2, 0, 2, 5])
+    >>> def ff(x, y):
+    ...     return x**2 + y**2
+
+    >>> xg, yg = np.meshgrid(x, y, indexing='ij')
+    >>> data = ff(xg, yg)
+    >>> interp = RegularGridInterpolator((x, y), data,
+    ...                                  bounds_error=False, fill_value=None)
+
+    >>> import matplotlib.pyplot as plt
+    >>> fig = plt.figure()
+    >>> ax = fig.add_subplot(projection='3d')
+    >>> ax.scatter(xg.ravel(), yg.ravel(), data.ravel(), s=60, c='k', label='data')
+
+    Evaluate and plot the interpolator on a finer grid
+
+    >>> xx = np.linspace(-4, 9, 31)
+    >>> yy = np.linspace(-4, 9, 31)
+    >>> X, Y = np.meshgrid(xx, yy, indexing='ij')
+
+    >>> # interpolator
+    >>> ax.plot_wireframe(X, Y, interp((X, Y)), rstride=3, cstride=3,
+    ...                   alpha=0.4, color='m', label='linear interp')
+
+    >>> # ground truth
+    >>> ax.plot_wireframe(X, Y, ff(X, Y), rstride=3, cstride=3,
+    ...                   alpha=0.4, label='ground truth')
+    >>> plt.legend()
+
+
     See also
     --------
     NearestNDInterpolator : Nearest neighbor interpolation on *unstructured*
