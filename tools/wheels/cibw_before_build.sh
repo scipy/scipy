199ee2588a25d3c25cb41ca2be48de8ea445c057
Tyler Reddy <tyler.je.reddy@gmail.com>
2022-04-26 22:00:40 -0600

    MAINT: PR 15925 revisions
    * copy over some NumPy cibuildwheel testing
    infra
    
    [skip actions] [skip circleci] [skip ci] [skip azp]

--- tools/wheels/cibw_before_build.sh
+++ tools/wheels/cibw_before_build.sh
@@ -3,6 +3,15 @@ set -xe
 PROJECT_DIR="$1"
 PLATFORM=$(PYTHONPATH=tools python -c "import openblas_support; print(openblas_support.get_plat())")
 
+# Update license
+if [[ $RUNNER_OS == "Linux" || $(echo $PLATFORM | grep -E -c "linux") ]] ; then
+    cat $PROJECT_DIR/tools/wheels/LICENSE_linux.txt >> $PROJECT_DIR/LICENSE.txt
+elif [[ $RUNNER_OS == "macOS" ]]; then
+    cat $PROJECT_DIR/tools/wheels/LICENSE_osx.txt >> $PROJECT_DIR/LICENSE.txt
+elif [[ $RUNNER_OS == "Windows" ]]; then
+    cat $PROJECT_DIR/tools/wheels/LICENSE_win32.txt >> $PROJECT_DIR/LICENSE.txt
+fi
+
 # Install Openblas
 basedir=$(python tools/openblas_support.py)
 cp -r $basedir/lib/* /usr/local/lib
