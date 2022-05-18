b2d248fb730c27bbd2daf5b636c50ee4da0b1de9
Sam Wallan <44255917+swallan@users.noreply.github.com>
2022-05-18 14:51:48 -0700

    ENH: branch in mood based on ties or not - remove pythran, but the method for ties is pythran ready

--- scipy/stats/_morestats.py
+++ scipy/stats/_morestats.py
@@ -2884,10 +2884,77 @@ def fligner(*samples, center='median', proportiontocut=0.05):
     pval = distributions.chi2.sf(Xsq, k - 1)  # 1 - cdf
     return FlignerResult(Xsq, pval)
 
-from ._mood_inner_lc import _mood_inner_lc
+@_axis_nan_policy_factory(lambda x1: (x1,), n_samples=2, n_outputs=1)
+def _mood_inner_lc(y, x) -> float:
+    n = x.shape[0]
+    m = y.shape[0]
+    N = n + m
+    xy = np.concatenate((x, y))
 
-@_axis_nan_policy_factory(lambda x1, x2: (x1, x2), n_samples=2)
-def mood(x, y, alternative="two-sided"):
+    # obtain the unique values and the counts of each.
+    # "a_i, + b_i, = t_i, for j = 1, ... k", where `k` is the number of unique
+    # classes, and "[t]he number of values associated with the x's and y's in
+    # the jth class will be denoted by a_i, and b_i respectively."
+    # (Mielke, 312)
+    uniques, t = np.unique(sorted(xy), return_counts=1)
+    k = len(uniques)
+    js = list(np.arange(1, k + 1))
+
+    # the `b` array mentioned in the paper is not used, outside of the
+    # calculation of `t`, so we do not need to calculate it separately. Here
+    # we calculate `a`. In plain language, `a[i]` is the number of values in
+    # `x` that equal `uniques[i]`.
+    _, xyx_counts = np.unique(sorted(np.concatenate((xy, x))),
+                              return_counts=1)
+    a = xyx_counts - t
+    # "Define .. a_0 = b_0 = t_0 = S_0 = 0" (Mielke 312) so we shift  `a`
+    # and `t` arrays over 1 to allow a first element of 0 to accommodate this
+    # indexing.
+    t = np.concatenate(([0], t))
+    a = np.concatenate(([0], a))
+
+    # S is built from `t`, so it does not need a preceding zero added on.
+    S = np.cumsum(t)
+    # define a copy of `S` with a prepending zero for later use to avoid
+    # indexing
+    S_i_m1 = np.concatenate(([0], S[:-1]))
+
+    # Psi, as defined by the 6th unnumbered equation on page 312 (Mielke).
+    # Note that in the paper there is an error where the denominator `2` is
+    # squared when it should be the entire equation.
+    psi = lambda I: (I - (N + 1)/2)**2
+
+    # define summation range for use in calculation of phi, as seen in sum
+    # in the unnumbered equation on the bottom of page 312 (Mielke).
+    s_lower = [S[jsi - 1] + 1 for jsi in js]
+    s_upper = [S[jsi] + 1 for jsi in js]
+    phi_I = [range(s_lower[idx], s_upper[idx]) for idx in range(k)]
+
+    # for every range in the above array, determine the sum of psi(I) for
+    # every element in the range. Divide all the sums by `t`. Following the
+    # last unnumbered equation on page 312.
+    phis = []
+    for i in range(len(phi_I)):
+        psi_temp = [psi(phi_I_i) for phi_I_i in phi_I[i]]
+        phis.append(sum(psi_temp) / t[(i + 1)])
+
+    # `T` is equal to a[j] * phi[j], per the first unnumbered equation on
+    # page 312. `phis` is already in the order based on `js`, so we index
+    # into `a` with `js` as well.
+    T = sum(phis * a[js])
+
+    # The approximate statistic
+    E_0_T = n * (N * N - 1) / 12
+
+    varM = (m * n * (N + 1.0) * (N ** 2 - 4) / 180 -
+            m * n / (180 * N * (N - 1)) * np.sum(
+                t * (t**2 - 1) * (t**2 - 4 + (15 * (N - S - S_i_m1) ** 2))
+            ))
+
+    return ((T - E_0_T) / np.sqrt(varM),)
+
+
+def mood(x, y, axis=0, alternative="two-sided"):
     """Perform Mood's test for equal scale parameters.
 
     Mood's two-sample test for scale parameters is a non-parametric
@@ -2898,6 +2965,11 @@ def mood(x, y, alternative="two-sided"):
     ----------
     x, y : array_like
         Arrays of sample data.
+    axis : int, optional
+        The axis along which the samples are tested.  `x` and `y` can be of
+        different length along `axis`.
+        If `axis` is None, `x` and `y` are flattened and the test is done on
+        all values in the flattened arrays.
     alternative : {'two-sided', 'less', 'greater'}, optional
         Defines the alternative hypothesis. Default is 'two-sided'.
         The following options are available:
@@ -2963,15 +3035,62 @@ def mood(x, y, alternative="two-sided"):
     (array([-5.76174136, -6.12650783]), array([8.32505043e-09, 8.98287869e-10]))
 
     """
-    # Specify an order for passing arrays into Pythran
-    x = np.asarray(x, dtype=float, order="C")
-    y = np.asarray(y, dtype=float, order="C")
+    x = np.asarray(x, dtype=float)
+    y = np.asarray(y, dtype=float)
 
-    if x.shape[0] + y.shape[0] < 3:
+    if axis is None:
+        x = x.flatten()
+        y = y.flatten()
+        axis = 0
+
+    if axis < 0:
+        axis = x.ndim + axis
+
+    # Determine shape of the result arrays
+    res_shape = tuple([x.shape[ax] for ax in range(len(x.shape)) if ax != axis])
+    if not (res_shape == tuple([y.shape[ax] for ax in range(len(y.shape)) if
+                                ax != axis])):
+        raise ValueError("Dimensions of x and y on all axes except `axis` "
+                         "should match")
+
+    n = x.shape[axis]
+    m = y.shape[axis]
+    N = m + n
+    if N < 3:
         raise ValueError("Not enough observations.")
 
-    z = np.asarray(_mood_inner_lc(y, x))
+
+    xy = np.concatenate((x, y), axis=axis)
+    # determine if any of the samples contain ties
+    if 0 in np.diff(np.sort(xy)):
+        z = np.asarray(_mood_inner_lc(y, x, axis=axis))
+    else:
+        if axis != 0:
+            xy = np.moveaxis(xy, axis, 0)
+
+        xy = xy.reshape(xy.shape[0], -1)
+        # Generalized to the n-dimensional case by adding the axis argument,
+        # and using for loops, since rankdata is not vectorized.  For improving
+        # performance consider vectorizing rankdata function.
+        all_ranks = np.empty_like(xy)
+        for j in range(xy.shape[1]):
+            all_ranks[:, j] = _stats_py.rankdata(xy[:, j])
+
+        Ri = all_ranks[:n]
+        M = np.sum((Ri - (N + 1.0) / 2) ** 2, axis=0)
+        # Approx stat.
+        mnM = n * (N * N - 1.0) / 12
+        varM = m * n * (N + 1.0) * (N + 2) * (N - 2) / 180
+        z = (M - mnM) / sqrt(varM)
     z, pval = _normtest_finish(z, alternative)
+
+    if res_shape == ():
+        # Return scalars, not 0-D arrays
+        z = z[0]
+        pval = pval[0]
+    else:
+        z.shape = res_shape
+        pval.shape = res_shape
     return z, pval
 
 
