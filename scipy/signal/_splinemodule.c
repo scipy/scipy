733e88c94c041e5d58bd8692295257963be923b9
Ralf Gommers <ralf.gommers@gmail.com>
2022-05-28 15:17:17 +0200

    MAINT: move `import_array` before module creation in module inits
    This seems to be the right order, fixes an issue where the reference
    to the newly created module isn't decreased if `import_array` fails.
    
    Note that `import_array` already does its own error checking, and
    returns NULL if NumPy initialization fails, so no checks are needed.
    This makes the code a bit more concise.
    
    Closes gh-16231

--- scipy/signal/_splinemodule.c
+++ scipy/signal/_splinemodule.c
@@ -554,13 +554,6 @@ static struct PyModuleDef moduledef = {
 PyMODINIT_FUNC
 PyInit__spline(void)
 {
-    PyObject *module;
-    module = PyModule_Create(&moduledef);
-    if (module == NULL) {
-        return NULL;
-    }
-
     import_array();
-
-    return module;
+    return PyModule_Create(&moduledef);
 }
