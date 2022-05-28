BUG: linalg: Add precision preservation for linalg.sqrtm

scipy/linalg/_matfuncs_sqrtm.py
@@ -132,7 +132,11 @@ def sqrtm(A, disp=True, blocksize=64):
     Returns
     -------
     sqrtm : (N, N) ndarray
-        Value of the sqrt function at `A`
+        Value of the sqrt function at `A`. The dtype is float or complex.
+        The precision (data size) is determined based on the precision of
+        input `A`. When the dtype is float, the precision is same as `A`.
+        When the dtype is complex, the precition is double as `A`. The
+        precision might be cliped by each dtype precision range.
 
     errest : float
         (if disp == False)
@@ -159,6 +163,7 @@ def sqrtm(A, disp=True, blocksize=64):
            [ 1.,  4.]])
 
     """
+    byte_size = np.asarray(A).dtype.itemsize
     A = _asarray_validated(A, check_finite=True, as_inexact=True)
     if len(A.shape) != 2:
         raise ValueError("Non-matrix input to matrix function.")
@@ -176,6 +181,12 @@ def sqrtm(A, disp=True, blocksize=64):
         R = _sqrtm_triu(T, blocksize=blocksize)
         ZH = np.conjugate(Z).T
         X = Z.dot(R).dot(ZH)
+        if not np.iscomplexobj(X):
+            # float byte size range: f2 ~ f16
+            X = X.astype(f"f{np.clip(byte_size, 2, 16)}", copy=False)
+        else:
+            # complex byte size range: c8 ~ c32
+            X = X.astype(f"c{np.clip(byte_size*2, 8, 32)}", copy=False)
     except SqrtmError:
         failflag = True
         X = np.empty_like(A)