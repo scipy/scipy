c16c5f6c7c8810155ec9fa3333c6f2ca2a7ea9c3
Matt Haberland <mhaberla@calpoly.edu>
2022-06-18 00:13:36 -0500

    MAINT: stats: revert to mode not consuming axis

--- scipy/stats/_stats_py.py
+++ scipy/stats/_stats_py.py
@@ -616,12 +616,12 @@ def mode(a, axis=0, nan_policy='propagate'):
     ...               [4, 7, 5, 9]])
     >>> from scipy import stats
     >>> stats.mode(a)
-    ModeResult(mode=array([3, 1, 0, 0]), count=array([1, 1, 1, 1]))
+    ModeResult(mode=array([[3, 1, 0, 0]]), count=array([[1, 1, 1, 1]]))
 
     To get mode of whole array, specify ``axis=None``:
 
     >>> stats.mode(a, axis=None)
-    ModeResult(mode=3, count=3)
+    ModeResult(mode=[3], count=[3])
 
     """  # noqa: E501
     a, axis = _chk_asarray(a, axis)
@@ -662,8 +662,9 @@ def mode(a, axis=0, nan_policy='propagate'):
     counts = np.empty(a_view.shape[:-1], dtype=np.int_)
     for ind in inds:
         modes[ind], counts[ind] = _mode1D(a_view[ind])
-
-    return ModeResult(modes[()], counts[()])
+    newshape = list(a.shape)
+    newshape[axis] = 1
+    return ModeResult(modes.reshape(newshape), counts.reshape(newshape))
 
 
 def _mask_to_limits(a, limits, inclusive):
