334e0730f985fc459a03461ed9a4a7261942ee39
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2022-06-25 20:29:51 +0300

    DOC: avoid deprecated imports from optimize.nonlin

--- scipy/optimize/_nonlin.py
+++ scipy/optimize/_nonlin.py
@@ -1338,8 +1338,8 @@ class KrylovJacobian(Jacobian):
         Note that you can use also inverse Jacobians as (adaptive)
         preconditioners. For example,
 
-        >>> from scipy.optimize.nonlin import BroydenFirst, KrylovJacobian
-        >>> from scipy.optimize.nonlin import InverseJacobian
+        >>> from scipy.optimize import BroydenFirst, KrylovJacobian
+        >>> from scipy.optimize import InverseJacobian
         >>> jac = BroydenFirst()
         >>> kjac = KrylovJacobian(inner_M=InverseJacobian(jac))
 
