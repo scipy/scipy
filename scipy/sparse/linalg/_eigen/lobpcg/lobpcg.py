4c5735a99b8488ee8fccd6d0e3002e9fe8a7f8a2
Andrew Knyazev <andrew.knyazev@ucdenver.edu>
2022-06-30 10:07:00 -0400

    docstring update

--- scipy/sparse/linalg/_eigen/lobpcg/lobpcg.py
+++ scipy/sparse/linalg/_eigen/lobpcg/lobpcg.py
@@ -148,35 +148,42 @@ def lobpcg(
 
     Parameters
     ----------
-    A : {sparse matrix, dense matrix, LinearOperator}
+    A : {sparse matrix, dense matrix, LinearOperator, callable object}
         The symmetric linear operator of the problem, usually a
         sparse matrix.  Often called the "stiffness matrix".
     X : ndarray, float32 or float64
         Initial approximation to the ``k`` eigenvectors (non-sparse). If `A`
         has ``shape=(n,n)`` then `X` should have shape ``shape=(n,k)``.
-    B : {dense matrix, sparse matrix, LinearOperator}, optional
+    B : {dense matrix, sparse matrix, LinearOperator, callable object}
+        Optional.
         The right hand side operator in a generalized eigenproblem.
         By default, ``B = Identity``.  Often called the "mass matrix".
-    M : {dense matrix, sparse matrix, LinearOperator}, optional
+    M : {dense matrix, sparse matrix, LinearOperator, callable object}
+        Optional.
         Preconditioner to `A`; by default ``M = Identity``.
         `M` should approximate the inverse of `A`.
-    Y : ndarray, float32 or float64, optional
+    Y : ndarray, float32 or float64, optional.
         An n-by-sizeY matrix of constraints (non-sparse), sizeY < n.
         The iterations will be performed in the B-orthogonal complement
         of the column-space of Y. Y must be full rank.
-    tol : scalar, optional
+    tol : scalar, optional.
         Solver tolerance (stopping criterion).
         The default is ``tol=n*sqrt(eps)``.
-    maxiter : int, optional
-        Maximum number of iterations.  The default is ``maxiter = 20``.
-    largest : bool, optional
+    maxiter : int, optional.
+        Maximum number of iterations.  The default is ``maxiter=20``.
+    largest : bool, optional.
         When True, solve for the largest eigenvalues, otherwise the smallest.
     verbosityLevel : int, optional
         Controls solver output.  The default is ``verbosityLevel=0``.
-    retLambdaHistory : bool, optional
+    retLambdaHistory : bool, optional.
         Whether to return eigenvalue history.  Default is False.
-    retResidualNormsHistory : bool, optional
+    retResidualNormsHistory : bool, optional.
         Whether to return history of residual norms.  Default is False.
+    restartControl : int, optional. 
+        Iterations restrart if the resuduals jump up 2**restartControl times
+        compared to the smallest ones recorded in retResidualNormsHistory.
+        The default is ``restartControl=20``, making the restarts rare for
+        backward compatibility.
 
     Returns
     -------
