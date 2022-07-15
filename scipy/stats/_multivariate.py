f4e5ee5afec6ab8e34516daa3dbe3d39d40ce30d
Giacomo Petrillo <info@giacomopetrillo.com>
2022-07-15 12:40:33 +0200

    complete matt's proposed changes

--- scipy/stats/_multivariate.py
+++ scipy/stats/_multivariate.py
@@ -4,6 +4,7 @@
 import math
 import numpy as np
 from numpy import asarray_chkfinite, asarray
+from numpy.lib import NumpyVersion
 import scipy.linalg
 from scipy._lib import doccer
 from scipy.special import gammaln, psi, multigammaln, xlogy, entr, betaln
@@ -4002,7 +4003,7 @@ class unitary_group_gen(multi_rv_generic):
         random_state = self._get_random_state(random_state)
 
         size = int(size)
-        if size > 1 and not _has_vectorized_qr():
+        if size > 1 and NumpyVersion(np.__version__) < '1.22.0':
             return np.array([self.rvs(dim, size=1, random_state=random_state)
                              for i in range(size)])
 
@@ -4012,8 +4013,12 @@ class unitary_group_gen(multi_rv_generic):
         z = 1/math.sqrt(2)*(random_state.normal(size=size + (dim, dim)) +
                             1j*random_state.normal(size=size + (dim, dim)))
         q, r = np.linalg.qr(z)
-        d = r.diagonal(0, -2, -1)
-        q *= (d/abs(d))[..., None, :]
+        # The last two dimensions are the rows and columns of R matrices.
+        # Extract the diagonals. Note that this eliminates a dimension.
+        d = r.diagonal(offset=0, axis1=-2, axis2=-1)
+        # Add back a dimension for proper broadcasting: we're dividing
+        # each row of each R matrix by the diagonal of the R matrix.
+        q *= (d/abs(d))[..., np.newaxis, :]  # to broadcast properly
         return q
 
 
