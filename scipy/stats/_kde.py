294ce5669b7f1518668eacd6abcbe1919e68265f
cmdupuis3 <cd3207@columbia.edu>
2022-07-16 16:44:25 +0000

    DOC: import numpy in _kde.py examples [skip ci]

--- scipy/stats/_kde.py
+++ scipy/stats/_kde.py
@@ -155,6 +155,7 @@ class gaussian_kde:
     --------
     Generate some random two-dimensional data:
 
+    >>> import numpy as np
     >>> from scipy import stats
     >>> def measure(n):
     ...     "Measurement model, return two coupled measurements."
@@ -514,6 +515,7 @@ class gaussian_kde:
 
         Examples
         --------
+        >>> import numpy as np
         >>> import scipy.stats as stats
         >>> x1 = np.array([-7, -5, 1, 4, 5.])
         >>> kde = stats.gaussian_kde(x1)
