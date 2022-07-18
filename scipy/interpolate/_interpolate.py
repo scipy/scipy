1e5d6c04fddfa9dcbfe6ab3a6aafe0827f0173cc
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2022-07-18 09:53:19 +0300

    Update scipy/interpolate/_interpolate.py

--- scipy/interpolate/_interpolate.py
+++ scipy/interpolate/_interpolate.py
@@ -181,7 +181,7 @@ class interp2d:
     used directly.
 
     The coordinates of the data points to interpolate `xnew` and `ynew`
-    have to be sorted by ascending order. 
+    have to be sorted by ascending order.
     
     `interp2d` is legacy and is not
     recommended for use in new code. New code should use
