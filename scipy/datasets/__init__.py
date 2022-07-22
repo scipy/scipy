e8c8ac52554568f9c091c32e2a3e5b8005000409
Anirudh Dagar <anirudhdagar6@gmail.com>
2022-07-22 03:57:20 +0200

    DOC: Explain dataset repos usage & manual cache loading

--- scipy/datasets/__init__.py
+++ scipy/datasets/__init__.py
@@ -12,6 +12,47 @@ Datasets (:mod:`scipy.datasets`)
    face - Get example image for processing
    electrocardiogram - Load an example of a one-dimensional signal.
 
+
+Usage of Datasets
+^^^^^^^^^^^^^^^^^
+
+SciPy dataset files are stored within individual github repositories under
+the SciPy Org, following a naming convention as ``'dataset-<name>'``,
+for example `scipy.datasets.face` files live at
+https://github.com/scipy/dataset-face.
+The `scipy.datasets` submodule utilizes and depends on
+`Pooch <https://www.fatiando.org/pooch/latest/>`_, a python package built to
+simplify fetching data files. Pooch uses these repos to retrieve
+the respective dataset files when calling the dataset function.
+
+A registry of all the datasets, essentially a mapping of filenames
+with their SHA256 hash and repo urls are maintained,
+which pooch uses to handle and verify the downloads
+on function call. After downloading the dataset once, the files
+are saved in the system cache directory under ``'scipy-data'``.
+
+Dataset cache locations may vary on different platforms.
+
+For Mac::
+
+    '~/Library/Caches/scipy-data'
+
+For Unix::
+
+    '~/.cache/scipy-data' or the value of the XDG_CACHE_HOME environment
+    variable, if defined.
+
+For Windows::
+
+    'C:\\Users\\<user>\\AppData\\Local\\<AppAuthor>\\scipy-data\\Cache'
+
+
+In environments with constrained network connectivity for various security
+reasons or on systems without continuous internet connections, one may
+manually load the cache of the datasets by placing the contents of the dataset
+repo in the above mentioned cache directory to avoid fetching dataset errors
+without the internet connectivity.
+
 """
 import warnings
 
