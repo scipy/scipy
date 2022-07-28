4e9ed0298a492ab0fb1f8bcd25ee487f50a34f1e
Tyler Reddy <tyler.je.reddy@gmail.com>
2022-07-28 11:24:13 -0600

    REL: 1.9.0 final unreleased.

--- tools/version_utils.py
+++ tools/version_utils.py
@@ -6,9 +6,9 @@ import argparse
 MAJOR = 1
 MINOR = 9
 MICRO = 0
-ISRELEASED = True
+ISRELEASED = False
 IS_RELEASE_BRANCH = True
-VERSION = '%d.%d.%drc3' % (MAJOR, MINOR, MICRO)
+VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
 
 def get_version_info(source_root):
