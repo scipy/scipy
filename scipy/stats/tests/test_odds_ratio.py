72462d6cb618b7fff84b753950b407678bfe2feb
Matt Haberland <mhaberla@calpoly.edu>
2022-07-30 21:41:28 -0700

    MAINT: stats.odds_ratio: correctly import fisher_exact_results_from_r from new location

--- scipy/stats/tests/test_odds_ratio.py
+++ scipy/stats/tests/test_odds_ratio.py
@@ -3,7 +3,7 @@ import numpy as np
 from numpy.testing import assert_equal, assert_allclose
 from .._discrete_distns import nchypergeom_fisher, hypergeom
 from scipy.stats._odds_ratio import odds_ratio
-from .fisher_exact_results_from_r import data
+from .data.fisher_exact_results_from_r import data
 
 
 class TestOddsRatio:
