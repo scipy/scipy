db2c66b4f842726ad8e42746d7822e83065e4946
Jaiden di Lanzo <jaiden.dilanzo@bigpond.com>
2022-08-01 08:19:46 +0800

    DOC: Integrate: Add improper integral example for `dblquad`.

--- scipy/integrate/_quadpack_py.py
+++ scipy/integrate/_quadpack_py.py
@@ -700,6 +700,17 @@ def dblquad(func, a, b, gfun, hfun, args=(), epsabs=1.49e-8, epsrel=1.49e-8):
     >>> integrate.dblquad(f, 0, 1, lambda x: x, lambda x: 2-x, args=(3,))
         (0.9999999999999999, 1.6653345369377348e-14)
 
+    Compute the two-dimensional Gaussian Integral, which is the integral of the
+    Gaussian function :math:`f(x,y) = e^{-(x^{2} + y^{2})}`, over
+    :math:`(-\\infty,\\infty)`. That is, compute the integral
+    :math:`\\iint^{\\infty}_{-\\infty} e^{-(x^{2} + y^{2})} \\,dy\\,dx`:
+
+    >>> import numpy as np
+    >>> from scipy import integrate
+    >>> f = lambda x, y: np.exp(-(x ** 2 + y ** 2))
+    >>> integrate.dblquad(f, -np.inf, np.inf, -np.inf, np.inf)
+        (3.141592653589777, 2.5173086737433208e-08)
+
     """
 
     def temp_ranges(*args):
