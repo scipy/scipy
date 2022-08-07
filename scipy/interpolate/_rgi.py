b284750ebb804ab49c0e756f57751a46333533a4
Jake Bowhay <jb9.bowhay@gmail.com>
2022-08-07 20:54:50 +0100

    DOC: update __call__documentation

--- scipy/interpolate/_rgi.py
+++ scipy/interpolate/_rgi.py
@@ -268,9 +268,15 @@ class RegularGridInterpolator:
         xi : ndarray of shape (..., ndim)
             The coordinates to evaluate the interpolator at.
 
-        method : str
-            The method of interpolation to perform. Supported are "linear" and
-            "nearest".
+        method : str, optional
+            The method of interpolation to perform. Supported are "linear",
+            "nearest", "slinear", "cubic", "quintic" and "pchip". Default is
+            the method chosen when the interpolator was created.
+
+        Returns
+        -------
+        values_x : ndarray, shape xi.shape[:-1] + values.shape[ndim:]
+            Interpolated values at `xi`.
 
         Examples
         --------
