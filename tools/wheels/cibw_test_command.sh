c22b0ff0fbd17975d2a07fc976c0765ab1de6e34
Andrew Nelson <andyfaff@gmail.com>
2022-08-08 12:23:48 +1000

    TST correctly

--- tools/wheels/cibw_test_command.sh
+++ tools/wheels/cibw_test_command.sh
@@ -3,8 +3,10 @@ set -xe
 PROJECT_DIR="$1"
 
 # python $PROJECT_DIR/tools/wheels/check_license.py
-if [[ $UNAME == "Linux" || $UNAME == "Darwin" ]] ; then
+if [[ $(uname) == "Linux" || $(uname) == "Darwin" ]] ; then
     python $PROJECT_DIR/tools/openblas_support.py --check_version
 fi
+echo $?
 
-python -c "import scipy; scipy.test()"
+python -c "import sys; import scipy; sys.exit(not scipy.test())"
+echo $?
