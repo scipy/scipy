bccd95d035b400470edc3f27311503e1a17d1f20
Andrew Nelson <andyfaff@gmail.com>
2022-08-08 13:08:34 +1000

    start win32

--- tools/wheels/cibw_before_build.sh
+++ tools/wheels/cibw_before_build.sh
@@ -26,20 +26,28 @@ if [[ $RUNNER_OS == "Linux" || $RUNNER_OS == "macOS" ]] ; then
     fi
 elif [[ $RUNNER_OS == "Windows" ]]; then
     PYTHONPATH=tools python -c "import openblas_support; openblas_support.make_init('scipy')"
-    #    target=$(python tools/openblas_support.py)
-    #    # Download openBLAS and put it into c/opt/64/lib
-    #    target=$(python -c "import tools.openblas_support as obs; plat=obs.get_plat(); ilp64=obs.get_ilp64(); target=f'openblas_{plat}.zip'; obs.download_openblas(target, plat, ilp64);print(target)")
-    #    unzip $target -d /c/opt
-    #    pushd /c/opt/64/lib
-    #    mv /c/opt/64/lib/libopenblas_v0.3.18-gcc_8_1_0.a  libopenblas.a
-    #    rm *.lib
+    mkdir -p /c/opt/openblas/if_32/32/lib/pkgconfig
+    mkdir -p /c/opt/openblas/if_32/64/lib/pkgconfig
 
-    # the following is used in CI, the previous lines have been used in making wheels. Figure out which one
-    # to use.
-    curl -L https://github.com/scipy/scipy-ci-artifacts/raw/main/openblas_32_if.zip -o openblas_32_if.zip
-    mkdir -p /c/opt
-    unzip openblas_32_if.zip -d /c
-    rm /c/opt/openblas/if_32/64/lib/*.dll.a
+    # delvewheel is the equivalent of delocate/auditwheel for windows.
+    python -m pip install delvewheel
+    mkdir -p /c/opt/openblas/openblas_dll
+    which strip
+
+    # The 32/64 bit Fortran wheels are currently coming from different locations.
+    if [[ $PLATFORM == 'win-32' ]]; then
+      # 32-bit openBLAS
+      # Download 32 bit openBLAS and put it into c/opt/32/lib
+      target=$(python -c "import tools.openblas_support as obs; plat=obs.get_plat(); ilp64=obs.get_ilp64(); target=f'openblas_{plat}.zip'; obs.download_openblas(target, plat, ilp64);print(target)")
+      unzip $target -d /c/opt/openblas/if_32/
+      cp /c/opt/openblas/if_32/32/bin/*.dll /c/opt/openblas/openblas_dll
+      # rm /c/opt/openblas/if_32/32/lib/*.dll.a
+    else
+      # 64-bit openBLAS
+      curl -L https://github.com/scipy/scipy-ci-artifacts/raw/main/openblas_32_if.zip -o openblas_32_if.zip
+      unzip openblas_32_if.zip -d /c
+      cp /c/opt/openblas/if_32/64/bin/*.dll /c/opt/openblas/openblas_dll
+    fi
 fi
 
 # Install GFortran
