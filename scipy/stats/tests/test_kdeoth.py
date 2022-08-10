9674d08618a4a48414ba168e987f52935082f6d7
Matt Haberland <mhaberla@calpoly.edu>
2022-08-10 16:05:43 -0700

    MAINT: stats.gaussian_kde: remote pdf_marginal/logpdf_marginal

--- scipy/stats/tests/test_kdeoth.py
+++ scipy/stats/tests/test_kdeoth.py
@@ -392,65 +392,12 @@ def test_pdf_logpdf_weighted():
     assert_almost_equal(pdf, pdf2, decimal=12)
 
 
-def test_pdf_logpdf_marginal():
-    np.random.seed(1)
-    n_basesample = 50
-    n_sample = 25
-    n_dim = 10
-    xn = np.random.randn(n_dim, n_basesample)
-    xs = np.random.randn(n_dim, n_sample)
-
-    # Default
-    gkde = stats.gaussian_kde(xn)
-    pdf = gkde.pdf_marginal(xs[:1], np.arange(1))
-    pdf2 = np.exp(gkde.logpdf_marginal(xs[:1], np.arange(1)))
-    assert_almost_equal(pdf, pdf2, decimal=12)
-
-    pdf = gkde.pdf_marginal(xs, np.arange(n_dim))
-    pdf2 = gkde.pdf(xs)
-    assert_almost_equal(pdf, pdf2, decimal=12)
-
-
-def test_pdf_logpdf_marginal_weighted():
-    np.random.seed(1)
-    n_basesample = 50
-    n_sample = 25
-    n_dim = 10
-    xn = np.random.randn(n_dim, n_basesample)
-    xs = np.random.randn(n_dim, n_sample)
-    wn = np.random.rand(n_basesample)
-
-    gkde = stats.gaussian_kde(xn, weights=wn)
-    logpdf = np.log(gkde.pdf_marginal(xs[:5], np.arange(5)))
-    logpdf2 = gkde.logpdf_marginal(xs[:5], np.arange(5))
-    assert_almost_equal(logpdf, logpdf2, decimal=12)
-
-
-def test_old_pr_vs_new():
-    np.random.seed(1)
-    n_basesample = 50
-    n_sample = 25
-    n_dim = 10
-    xn = np.random.randn(n_dim, n_basesample)
-    xs = np.random.randn(n_dim, n_sample)
-
-    # Old PR
-    gkde = stats.gaussian_kde(xn)
-    pdf = gkde.pdf_marginal(xs[:1], np.arange(1))
-
-    # New PR
-    marginal = gkde.marginalize(axis=0)
-    pdf2 = marginal.pdf(xs[:1])
-
-    assert_allclose(pdf, pdf2)
-
-
 def test_marginal_1_axis():
     rng = np.random.default_rng(6111799263660870475)
     n_data = 50
     n_dim = 10
     dataset = rng.normal(size=(n_dim, n_data))
-    points = np.random.randn(n_dim, 3)  # evaluation point
+    points = np.random.randn(n_dim, 3)
 
     axis = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])  # dimensions to keep
 
@@ -478,7 +425,7 @@ def test_marginal_2_axis():
     n_data = 30
     n_dim = 4
     dataset = rng.normal(size=(n_dim, n_data))
-    points = np.random.randn(n_dim, 3)  # evaluation point
+    points = np.random.randn(n_dim, 3)
 
     axis = np.array([1, 3])  # dimensions to keep
 
