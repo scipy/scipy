517468d0170beb1efeb535e71866d4511c7332c4
Ralf Gommers <ralf.gommers@gmail.com>
2022-08-12 08:06:25 +0200

    DOC: some tweaks and style cleanups to the `scipy.datasets` docstring
    [ci skip]

--- scipy/datasets/__init__.py
+++ scipy/datasets/__init__.py
@@ -8,53 +8,48 @@ Datasets (:mod:`scipy.datasets`)
 .. autosummary::
    :toctree: generated/
 
-   ascent - Get example image for processing
-   face - Get example image for processing
-   electrocardiogram - Load an example of a one-dimensional signal.
+   ascent
+   face
+   electrocardiogram
 
 
 Usage of Datasets
-^^^^^^^^^^^^^^^^^
+=================
 
-SciPy dataset methods can be simply called as follows:
-``'<dataset-name>()'`` This downloads the dataset files over
-the network once, and saves the cache, before returning
-a `numpy.ndarray` object representing the dataset.
+SciPy dataset methods can be simply called as follows: ``'<dataset-name>()'``
+This downloads the dataset files over the network once, and saves the cache,
+before returning a `numpy.ndarray` object representing the dataset.
 
-Note that the return data structure and data type might be different
-for different dataset methods. For a more detailed
-example on usage, please look into the
-particular dataset method documentation above.
+Note that the return data structure and data type might be different for
+different dataset methods. For a more detailed example on usage, please look
+into the particular dataset method documentation above.
 
 
-How dataset retrieval and storage works?
-^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+How dataset retrieval and storage works
+=======================================
 
-SciPy dataset files are stored within individual github repositories under
-the SciPy GitHub organization, following a naming convention as
+SciPy dataset files are stored within individual github repositories under the
+SciPy GitHub organization, following a naming convention as
 ``'dataset-<name>'``, for example `scipy.datasets.face` files live at
-https://github.com/scipy/dataset-face.
-The `scipy.datasets` submodule utilizes and depends on
-`Pooch <https://www.fatiando.org/pooch/latest/>`_, a Python package built to
-simplify fetching data files. Pooch uses these repos to retrieve
-the respective dataset files when calling the dataset function.
+https://github.com/scipy/dataset-face.  The `scipy.datasets` submodule utilizes
+and depends on `Pooch <https://www.fatiando.org/pooch/latest/>`_, a Python
+package built to simplify fetching data files. Pooch uses these repos to
+retrieve the respective dataset files when calling the dataset function.
 
-A registry of all the datasets, essentially a mapping of filenames
-with their SHA256 hash and repo urls are maintained,
-which Pooch uses to handle and verify the downloads
-on function call. After downloading the dataset once, the files
+A registry of all the datasets, essentially a mapping of filenames with their
+SHA256 hash and repo urls are maintained, which Pooch uses to handle and verify
+the downloads on function call. After downloading the dataset once, the files
 are saved in the system cache directory under ``'scipy-data'``.
 
 Dataset cache locations may vary on different platforms.
 
-For Mac::
+For macOS::
 
     '~/Library/Caches/scipy-data'
 
-For Unix::
+For Linux and other Unix-like platforms::
 
-    '~/.cache/scipy-data' or the value of the XDG_CACHE_HOME environment
-    variable, if defined.
+    '~/.cache/scipy-data'  # or the value of the XDG_CACHE_HOME env var, if defined
 
 For Windows::
 
@@ -62,10 +57,10 @@ For Windows::
 
 
 In environments with constrained network connectivity for various security
-reasons or on systems without continuous internet connections, one may
-manually load the cache of the datasets by placing the contents of the dataset
-repo in the above mentioned cache directory to avoid fetching dataset errors
-without the internet connectivity.
+reasons or on systems without continuous internet connections, one may manually
+load the cache of the datasets by placing the contents of the dataset repo in
+the above mentioned cache directory to avoid fetching dataset errors without
+the internet connectivity.
 
 """
 import warnings
