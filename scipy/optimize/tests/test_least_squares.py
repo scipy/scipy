108c80e71218533db397e6cd2a92a907bf4679a4
Franz Forstmayr <forstmayr.franz@gmail.com>
2022-08-22 21:53:55 +0200

    TST: Test several bound shapes with and withot jac together with bounds instances

--- scipy/optimize/tests/test_least_squares.py
+++ scipy/optimize/tests/test_least_squares.py
@@ -415,20 +415,27 @@ class BoundsMixin:
             assert_(0.5 <= res.x <= 3)
 
     def test_bounds_shape(self):
+        bounds_direkt = lambda x: x
+        bounds_instances = lambda x: Bounds(x[0], x[1])
+
         for jac in ['2-point', '3-point', 'cs', jac_2d_trivial]:
-            x0 = [1.0, 1.0]
-            res = least_squares(fun_2d_trivial, x0, jac=jac)
-            assert_allclose(res.x, [0.0, 0.0])
-            res = least_squares(fun_2d_trivial, x0, jac=jac,
-                                bounds=(0.5, [2.0, 2.0]), method=self.method)
-            assert_allclose(res.x, [0.5, 0.5])
-            res = least_squares(fun_2d_trivial, x0, jac=jac,
-                                bounds=([0.3, 0.2], 3.0), method=self.method)
-            assert_allclose(res.x, [0.3, 0.2])
-            res = least_squares(
-                fun_2d_trivial, x0, jac=jac, bounds=([-1, 0.5], [1.0, 3.0]),
-                method=self.method)
-            assert_allclose(res.x, [0.0, 0.5], atol=1e-5)
+            for bounds_func in [bounds_direkt, bounds_instances]:
+                x0 = [1.0, 1.0]
+                res = least_squares(fun_2d_trivial, x0, jac=jac)
+                assert_allclose(res.x, [0.0, 0.0])
+                res = least_squares(fun_2d_trivial, x0, jac=jac,
+                                    bounds=bounds_func((0.5, [2.0, 2.0])),
+                                    method=self.method)
+                assert_allclose(res.x, [0.5, 0.5])
+                res = least_squares(fun_2d_trivial, x0, jac=jac,
+                                    bounds=bounds_func(([0.3, 0.2], 3.0)),
+                                    method=self.method)
+                assert_allclose(res.x, [0.3, 0.2])
+                res = least_squares(
+                    fun_2d_trivial, x0, jac=jac, 
+                    bounds=bounds_func(([-1, 0.5], [1.0, 3.0])),
+                    method=self.method)
+                assert_allclose(res.x, [0.0, 0.5], atol=1e-5)
 
     def test_bounds_instances(self):
         res = least_squares(fun_trivial, 0.5, bounds=Bounds())
