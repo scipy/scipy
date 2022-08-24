841abee07feae5d775db0ba19da2177df50e7906
Matt Haberland <mhaberla@calpoly.edu>
2022-08-23 23:06:43 -0700

    ENH: optimize.OptimizeResult: improve pretty-printing

--- scipy/optimize/_optimize.py
+++ scipy/optimize/_optimize.py
@@ -81,6 +81,56 @@ class MemoizeJac:
         return self.jac
 
 
+def _indenter(s, n=0):
+    """
+    Ensures that lines after the first are indented by the specified amount
+    """
+    split = s.split("\n")
+    indent = " "*n
+    return ("\n" + indent).join(split)
+
+
+def _float_formatter_10(x):
+    """
+    Returns a string representation of a float with exactly ten characters
+    """
+    if np.isposinf(x):
+        return "       inf"
+    elif np.isneginf(x):
+        return "      -inf"
+    elif np.isnan(x):
+        return "       nan"
+    return np.format_float_scientific(x, precision=3, pad_left=2, unique=False)
+
+
+def _dict_formatter(d, n=0, mplus=1):
+    """
+    Pretty printer for dictionaries
+
+    `n` keeps track of the starting indentation;
+    lines are indented by this much after a line break.
+    `mplus` is additional left padding applied to keys
+    """
+    if isinstance(d, dict):
+        m = max(map(len, list(d.keys()))) + mplus # width to print keys
+        s = '\n'.join([k.rjust(m) + ': ' +  # right justified, width m
+                       _indenter(_dict_formatter(v, m+n+2, 0), m+2)
+                       for k, v in sorted(d.items())])  # +2 is for ': '
+    else:
+        # By default, NumPy arrays print with linewidth is 76; `n` is
+        # the indent at which they begin printing, so it is subtracted
+        # from the default to avoid exceeding 76 characters total.
+        # `edgeitems` is the number of elements to include before and after
+        # ellipses when arrays are not shown in full.
+        # `threshold` is the maximum number of elements for which an
+        # array is shown in full.
+        # These values tend to work well for use with OptimizeResult.
+        with np.printoptions(linewidth=76-n, edgeitems=2, threshold=12,
+                             formatter={'float_kind': _float_formatter_10}):
+            s = str(d)
+    return s
+
+
 class OptimizeResult(dict):
     """ Represents the optimization result.
 
@@ -130,9 +180,7 @@ class OptimizeResult(dict):
 
     def __repr__(self):
         if self.keys():
-            m = max(map(len, list(self.keys()))) + 1
-            return '\n'.join([k.rjust(m) + ': ' + repr(v)
-                              for k, v in sorted(self.items())])
+            return _dict_formatter(self)
         else:
             return self.__class__.__name__ + "()"
 
