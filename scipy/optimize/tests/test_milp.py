f5c10d8e0b424a47368ebdccd3297978fbd2f0bc
Nicholas McKibben <nicholas.bgp@gmail.com>
2022-08-24 00:00:57 -0700

    TST: skip time/node limit test for 32-bit builds

--- scipy/optimize/tests/test_milp.py
+++ scipy/optimize/tests/test_milp.py
@@ -277,6 +277,7 @@ _msg_time = "Time limit reached. (HiGHS Status 13:"
 _msg_iter = "Iteration limit reached. (HiGHS Status 14:"
 
 
+@pytest.mark.skipif(np.intp(0).itemsize < 8, reason="Unhandled 32-bit GCC FP bug")
 @pytest.mark.slow
 @pytest.mark.parametrize(["options", "msg"], [({"time_limit": 1}, _msg_time),
                                               ({"node_limit": 10}, _msg_iter)])
