22712296f9ec5d1d606b31c1a2a8de1fd05e7651
Franz Forstmayr <forstmayr.franz@gmail.com>
2022-08-30 23:09:53 +0200

    DOC: Add lsim2, step2 and impulse2 to pre plot code to ignore DeprecationWarning in sphinx build

--- scipy/signal/_ltisys.py
+++ scipy/signal/_ltisys.py
@@ -1854,9 +1854,7 @@ def lsim2(system, U=None, T=None, X0=None, **kwargs):
 
     Simulate the filter with `lsim2`.
 
-    >>> with warnings.catch_warnings():
-    ...     warnings.simplefilter(action='ignore', category=DeprecationWarning)
-    ...     tout, yout, xout = lsim2((b, a), U=u, T=t)
+    >>> tout, yout, xout = lsim2((b, a), U=u, T=t)
 
     Plot the result.
 
@@ -1887,9 +1885,8 @@ def lsim2(system, U=None, T=None, X0=None, **kwargs):
     Compute the simulation, and then plot `y`.  As expected, the plot shows
     the curve ``y = 0.5*t**2``.
 
-    >>> with warnings.catch_warnings():
-    ...     warnings.simplefilter(action='ignore', category=DeprecationWarning)
-    ...     tout, y, x = lsim2(system, u, t)
+    
+    >>> tout, y, x = lsim2(system, u, t)
     >>> plt.plot(t, y)
     >>> plt.grid(alpha=0.3)
     >>> plt.xlabel('t')
@@ -2358,9 +2355,8 @@ def impulse2(system, X0=None, T=None, N=None, **kwargs):
     >>> import warnings
 
     >>> system = ([1.0], [1.0, 2.0, 1.0])
-    >>> with warnings.catch_warnings():
-    ...     warnings.simplefilter(action='ignore', category=DeprecationWarning)
-    ...     t, y = signal.impulse2(system)
+
+    >>> t, y = signal.impulse2(system)
     >>> import matplotlib.pyplot as plt
     >>> plt.plot(t, y)
 
@@ -2532,9 +2528,7 @@ def step2(system, X0=None, T=None, N=None, **kwargs):
     >>> import warnings
 
     >>> lti = signal.lti([1.0], [1.0, 1.0])
-    >>> with warnings.catch_warnings():
-    ...     warnings.simplefilter(action='ignore', category=DeprecationWarning)
-    ...     t, y = signal.step2(lti)
+    >>> t, y = signal.step2(lti)
 
     >>> plt.plot(t, y)
     >>> plt.xlabel('Time [s]')
