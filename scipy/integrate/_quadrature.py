ENH: integrate.qmc_quad: fix sign of integral

scipy/integrate/_quadrature.py
@@ -1054,7 +1054,8 @@ def _qmc_quad_iv(func, ranges, n_points, n_estimates, qrng, log, args):
         message = "`func` must be callable."
         raise TypeError(message)
 
-    ranges = np.atleast_2d(ranges)
+    # Ranges will be modified, so copy. Oh well if it's copied twice.
+    ranges = np.atleast_2d(ranges).copy()
     dim = ranges.shape[0]
 
     if args is None:
@@ -1243,6 +1244,14 @@ def qmc_quad(func, ranges, *, n_points=1024, n_estimates=8, qrng=None,
     func, ranges, n_points, n_estimates, qrng, rng, log, args, stats = args
 
     lb, ub = ranges.T
+
+    # The sign of the integral depends on the order of the limits. Fix this by
+    # ensuring that lower bounds are indeed lower and setting sign of resulting
+    # integral manually
+    i_swap = ub < lb
+    sign = (-1)**(i_swap.sum(axis=-1))  # odd # of swaps -> negative
+    lb[i_swap], ub[i_swap] = ub[i_swap], lb[i_swap]
+
     A = np.prod(ub - lb)
     dA = A / n_points
 
@@ -1266,5 +1275,6 @@ def qmc_quad(func, ranges, *, n_points=1024, n_estimates=8, qrng=None,
         qrng = type(qrng)(seed=rng, **qrng._init)
 
     integral = np.mean(estimates)
+    integral = integral + np.pi*1j if (log and sign < 0) else integral*sign
     standard_error = stats.sem(estimates)
     return QMCQuadResult(integral, standard_error)

scipy/integrate/tests/test_quadrature.py
@@ -332,9 +332,7 @@ class TestQMCQuad():
         with pytest.raises(TypeError, match=message):
             qmc_quad(lambda x, y: 1, [[0, 1], [0, 1]], log=10)
 
-    @pytest.mark.parametrize("n_points", [2**8, 2**12])
-    @pytest.mark.parametrize("n_estimates", [8, 16])
-    def test_basic(self, n_points, n_estimates):
+    def basic_test(self, n_points=2**8, n_estimates=8, signs=np.ones(2)):
 
         ndim = 2
         mean = np.zeros(ndim)
@@ -347,20 +345,31 @@ class TestQMCQuad():
         rng = np.random.default_rng(2879434385674690281)
         qrng = stats.qmc.Sobol(ndim, seed=rng)
         lb = np.zeros(ndim)
-        ub = np.ones(ndim)
+        ub = np.ones(ndim) * signs
         ranges = np.asarray([lb, ub]).T
         res = qmc_quad(func, ranges, n_points=n_points,
                        n_estimates=n_estimates, args=(mean, cov), qrng=qrng)
         ref = stats.multivariate_normal.cdf(ub, mean, cov, lower_limit=lb)
         atol = sc.stdtrit(n_estimates-1, 0.995) * res.standard_error  # 99% CI
         assert_allclose(res.integral, ref, atol=atol)
+        assert np.prod(signs)*res.integral > 0
 
         rng = np.random.default_rng(2879434385674690281)
         qrng = stats.qmc.Sobol(ndim, seed=rng)
         logres = qmc_quad(lambda *args: np.log(func(*args)), ranges,
                           n_points=n_points, n_estimates=n_estimates,
-                          args=(mean, cov), qrng=qrng)
-        assert_allclose(np.exp(logres.integral), res.integral, rtol=5e-2)
+                          args=(mean, cov), log=True, qrng=qrng)
+        assert_allclose(np.exp(logres.integral), res.integral)
+        assert np.imag(logres.integral) == (np.pi if np.prod(signs) < 0 else 0)
+
+    @pytest.mark.parametrize("n_points", [2**8, 2**12])
+    @pytest.mark.parametrize("n_estimates", [8, 16])
+    def test_basic(self, n_points, n_estimates):
+        self.basic_test(n_points, n_estimates)
+
+    @pytest.mark.parametrize("signs", [[1, 1], [-1, -1], [-1, 1], [1, -1]])
+    def test_sign(self, signs):
+        self.basic_test(signs=signs)
 
     def test_flexible_input(self):
         # check that qrng is not required