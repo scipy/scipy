6faf5549ae8433b8b735bfaa68cb1bfe803fec9c
Matt Haberland <mhaberla@calpoly.edu>
2022-09-01 14:11:34 -0700

    STY: io/matlab/_mio.py PEP8 correction to fix CI

--- scipy/io/matlab/_mio.py
+++ scipy/io/matlab/_mio.py
@@ -77,7 +77,7 @@ def mat_reader_factory(file_name, appendmat=True, **kwargs):
     elif mjv == 1:
         return MatFile5Reader(byte_stream, **kwargs), file_opened
     elif mjv == 2:
-        raise NotImplementedError('Please use HDF reader for matlab v7.3 files,'\
+        raise NotImplementedError('Please use HDF reader for matlab v7.3 files,'
                                   ' e.g. h5py')
     else:
         raise TypeError('Did not recognize version %s' % mjv)
