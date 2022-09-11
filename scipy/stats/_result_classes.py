48b33c1be1c973b06e201194dd0716be63ba9a19
Matt Haberland <mhaberla@calpoly.edu>
2022-09-10 19:32:40 -0700

    ENH: stats.ttest_rel: add confidence_interval to result (#16902)
    * ENH: stats.ttest_rel: add confidence_interval to result

--- scipy/stats/_result_classes.py
+++ scipy/stats/_result_classes.py
@@ -17,18 +17,18 @@ Result classes
    PearsonRResult
    FitResult
    OddsRatioResult
-   Ttest_1sampResult
+   TtestResult
 
 """
 
 __all__ = ['BinomTestResult', 'RelativeRiskResult', 'TukeyHSDResult',
            'PearsonRResult', 'FitResult', 'OddsRatioResult',
-           'Ttest_1sampResult']
+           'TtestResult']
 
 
 from ._binomtest import BinomTestResult
 from ._odds_ratio import OddsRatioResult
 from ._relative_risk import RelativeRiskResult
 from ._hypotests import TukeyHSDResult
-from ._stats_py import PearsonRResult, Ttest_1sampResult
+from ._stats_py import PearsonRResult, TtestResult
 from ._fit import FitResult
