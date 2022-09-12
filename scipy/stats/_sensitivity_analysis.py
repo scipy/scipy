1cdf207ee1a8a20828336206bb5882eaf391d6e3
Pamphile Roy <roy.pamphile@gmail.com>
2022-09-12 18:51:21 +0200

    Add metamodel function

--- scipy/stats/_sensitivity_analysis.py
+++ scipy/stats/_sensitivity_analysis.py
@@ -145,3 +145,111 @@ def discrepancy_indices(*, func, n, d, dists=None, l_bounds=None, u_bounds=None,
 #     l_bounds=[-np.pi, -np.pi, -np.pi], u_bounds=[np.pi, np.pi, np.pi]
 # )
 # print(indices)
+
+def discontinuous(x):
+    res = np.zeros_like(x)
+    res[np.where(x > 0.5)] = 1
+    return res
+
+
+functions = [
+    ("Linear", lambda x: x),
+    ("Quadratic", lambda x: x**2),
+    ("Cubic", lambda x: x**3),
+    ("Exponential", lambda x: (np.exp(x) - 1) / (np.exp(1) - 1)),
+    ("Periodic", lambda x: np.sin(2 * np.pi * x) / 2 + 0.5),
+    ("Discontinuous", discontinuous),
+    ("No effect", lambda x: x * 0),
+    ("Non monotonic", lambda x: 4 * (x - 0.5) ** 2),
+    ("Inverse", lambda x: (10 - 1 / 1.1) ** (-1) * (x + 0.1) ** (- 1) - 0.1),
+    ("Trigonometric", lambda x: np.cos(x)),
+    ("Piecewise large", lambda x: ((-1) ** (4 * x).astype(int) * (0.125 - np.mod(x, 0.25)) + 0.125)),
+    ("Piecewise small", lambda x: ((-1) ** (32 * x).astype(int) * (0.03125 - 2 * np.mod(x, 0.03125)) + 0.03125) / 2),
+    ("Oscillation", lambda x: x ** 2 - 0.2 * np.cos(7 * np.pi * x)),
+]
+
+distributions = [
+    ("uniform", stats.uniform()),
+    ("normal", stats.norm(loc=0.5, scale=0.15)),
+    ("beta", stats.beta(a=8, b=2)),
+    ("beta2", stats.beta(a=2, b=8)),
+    ("beta3", stats.beta(a=2, b=0.8)),
+    ("beta4", stats.beta(a=0.8, b=2)),
+    ("logitnormal", stats.logistic(loc=0, scale=3.16)),
+]
+
+
+def mixture_phi(n, rng):
+    dists = [
+        stats.norm(loc=0, scale=np.sqrt(0.5)),
+        stats.norm(loc=0, scale=np.sqrt(5)),
+    ]
+    coefficients = np.array([0.7, 0.3])
+
+    n_dists = len(dists)
+    data = np.zeros((n, n_dists))
+    for i, dist in enumerate(dists):
+        data[:, i] = dist.rvs(n, random_state=rng)
+    random_idx = rng.choice(np.arange(n_dists), size=(n,), p=coefficients)
+    sample = data[np.arange(n), random_idx]
+
+    return sample
+
+
+
+
+
+class MetaModel:
+
+    def __init__(self, d, seed=None):
+
+        rng = np.random.default_rng(seed)
+
+        n_funcs = len(functions)
+
+        self.d = d
+
+        # first order
+        u = rng.integers(low=0, high=n_funcs, size=d)
+        self.u = np.array(functions)[u]
+        # phi[0] * u * u
+
+        d2 = int(0.5 * d)
+        self.V = rng.integers(low=0, high=d, size=(d2, 2))
+        # phi[1] * u[V[0]] * u[V[1]]
+
+        d3 = int(0.2 * d)
+        self.W = rng.integers(low=0, high=d, size=(d3, 3))
+        # phi[2] * u[W[0]] * u[W[1]] * u[W[2]]
+
+        phi = mixture_phi(d + d2 + d3, rng)
+        self.phi = [phi[:d], phi[d:d+d2], phi[d+d2:]]
+
+    def __call__(self, x):
+
+        first_order = np.zeros((len(x),))
+        for d_ in range(self.d):
+            first_order += self.phi[0][d_] * self.u[d_][1](x[:, d_])
+
+        second_order = np.zeros((len(x),))
+        for i, v_ in enumerate(self.V):
+            second_order += self.phi[1][i] \
+                            * self.u[v_[0]][1](x[:, v_[0]]) \
+                            * self.u[v_[1]][1](x[:, v_[1]])
+
+        third_order = np.zeros((len(x),))
+        for i, w_ in enumerate(self.W):
+            third_order += self.phi[2][i] \
+                           * self.u[w_[0]][1](x[:, w_[0]]) \
+                           * self.u[w_[1]][1](x[:, w_[1]]) \
+                           * self.u[w_[2]][1](x[:, w_[2]])
+
+        return (first_order + second_order + third_order).reshape(-1, 1)
+
+
+rng = np.random.default_rng()
+metamodel = MetaModel(d=4, seed=rng)
+sample = rng.random((100, 4))
+metamodel(sample)
+
+pass
