58a4477f35766854eb7182170163fdd7106b8002
Jake Bowhay <jb9.bowhay@gmail.com>
2022-09-20 23:48:17 +0100

    STY: fix lint

--- scipy/_lib/tests/test__finite_differences.py
+++ scipy/_lib/tests/test__finite_differences.py
@@ -6,15 +6,16 @@ from numpy.testing import assert_almost_equal
 
 from scipy._lib._finite_differences import _derivative
 
+
 class TestDerivative:
     def test_not_enough_points(self):
         with raises(ValueError, match="derivative order \\+ 1."):
             _derivative(lambda x: x, 0, order=5, n=3)
-    
+
     def test_odd_n(self):
         with raises(ValueError, match="must be odd."):
-            _derivative(lambda x: x,0, n=2)
-    
+            _derivative(lambda x: x, 0, n=2)
+
     @pytest.mark.parametrize(
         ["order", "n", "expected"],
         [(1, 3, 0.9800665778256222),
@@ -30,4 +31,5 @@ class TestDerivative:
          (3, 5, -0.9992007221626407)]
     )
     def test_computation(self, order, n, expected):
-        assert_almost_equal(_derivative(sin, 0.2, dx=1e-5, order=order, n=n), expected)
+        assert_almost_equal(_derivative(sin, 0.2, dx=1e-5, order=order, n=n),
+                            expected)
