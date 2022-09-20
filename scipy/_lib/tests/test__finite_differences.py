26b8e6189a06777b97f61117ef70dbaaef9648d6
Jake Bowhay <jb9.bowhay@gmail.com>
2022-09-20 22:35:48 +0100

    TST: add test coverage to compuation of derivates

--- scipy/_lib/tests/test__finite_differences.py
+++ scipy/_lib/tests/test__finite_differences.py
@@ -1,4 +1,8 @@
+from math import sin
+
+import pytest
 from pytest import raises
+from numpy.testing import assert_almost_equal
 
 from scipy._lib._finite_differences import _derivative
 
@@ -10,3 +14,20 @@ class TestDerivative:
     def test_odd_n(self):
         with raises(ValueError, match="must be odd."):
             _derivative(lambda x: x,0, n=2)
+    
+    @pytest.mark.parametrize(
+        ["order", "n", "expected"],
+        [(1, 3, 0.9800665778256222),
+         (1, 5, 0.9800665778415817),
+         (1, 7, 0.9800665778415817),
+         (1, 9, 0.9800665778425575),
+         (1, 11, 0.9800665778419693),
+         (2, 3, -0.19866913669730477),
+         (2, 5, -0.19866948364199996),
+         (2, 7, -0.19866908899240918),
+         (2, 9, -0.19866885805734644),
+         (2, 11, -0.19866834834680008),
+         (3, 5, -1.0130785099704551)]
+    )
+    def test_computation(self, order, n, expected):
+        assert_almost_equal(_derivative(sin, 0.2, dx=1e-5, order=order, n=n), expected)
