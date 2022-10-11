6d51fe63894c98d91dfa1f07b88dccc501709464
Neil Flood <neilflood@fastmail.fm>
2022-10-11 19:59:05 +1000

    Ensure a fresh array for each shape test

--- scipy/ndimage/tests/test_measurements.py
+++ scipy/ndimage/tests/test_measurements.py
@@ -483,10 +483,9 @@ def test_value_indices02():
 
 def test_value_indices03():
     "Test different input array shapes, from 1-D to 4-D"
-    baseArray = np.arange(36)//3
-    trueKeys = np.unique(baseArray)
-    for shape in [(18, 2), (3, 3, 4), (3, 3, 2, 2), (36,)]:
-        a = baseArray.reshape(shape)
+    for shape in [(36,), (18, 2), (3, 3, 4), (3, 3, 2, 2)]:
+        a = np.array((12*[1]+12*[2]+12*[3]), dtype=np.int32).reshape(shape)
+        trueKeys = np.unique(a)
         vi = ndimage.value_indices(a)
         assert_equal(list(vi.keys()), trueKeys)
         for k in trueKeys:
