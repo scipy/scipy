a52115bf7fc22be2b941b5a59622df88a3f68844
Hood Chatham <roberthoodchatham@gmail.com>
2022-11-13 14:26:58 +0300

    MAINT: stats: rename stats/_page_trend_test.py to _page_trend_test_.py
    This is pyodide process specific AFAICS, and seems harmless for SciPy itself.

--- scipy/stats/__init__.py
+++ scipy/stats/__init__.py
@@ -499,7 +499,7 @@ from ._resampling import bootstrap, monte_carlo_test, permutation_test
 from ._entropy import *
 from ._hypotests import *
 from ._rvs_sampling import rvs_ratio_uniforms
-from ._page_trend_test import page_trend_test
+from ._page_trend_test_ import page_trend_test
 from ._mannwhitneyu import mannwhitneyu
 from ._fit import fit
 from ._covariance import Covariance
