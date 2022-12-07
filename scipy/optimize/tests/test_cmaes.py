2751c5af663ab022253e71e74b30f9ee99fc90ca
PoloDSBA <paul.bedier@essec.edu>
2022-12-07 21:49:00 +0100

    Update for fmin

--- scipy/optimize/tests/test_cmaes.py
+++ scipy/optimize/tests/test_cmaes.py
@@ -1,8 +1,7 @@
 import numpy as np
-#from scipy.optimize_optimize import _minimize_cmaes, rosen
 from scipy.optimize._optimize import _minimize_cmaes, rosen
-dim = 40
-mean = 3 * np.ones(40)
-sigma = 2
+
+mean = np.ones(5)
+sigma = 1
 
 _minimize_cmaes(func=rosen, mean=mean, sigma=sigma)
