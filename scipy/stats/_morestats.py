4d7b336a5572c2fd01a0fe1dbafe5c921fa30a68
Matt Haberland <mhaberla@calpoly.edu>
2022-12-07 22:00:59 -0800

    DOC: stats.false_discovery_control: doc improvements
    [skip azp] [skip actions] [skip cirrus]

--- scipy/stats/_morestats.py
+++ scipy/stats/_morestats.py
@@ -4187,15 +4187,17 @@ def directional_stats(samples, *, axis=0, normalize=True):
 
 
 def false_discovery_control(ps, *, axis=0, method='bh'):
-    """Adjust p-values to control the false discovery rate (FDR).
+    """Adjust p-values to control the false discovery rate.
 
+    The false discovery rate (FDR) is the expected proportion of rejected null
+    hypotheses that are actually true.
     If the null hypothesis is rejected when the *adjusted* p-value falls below
     a specified level, the false discovery rate is controlled at that level.
 
     Parameters
     ----------
     ps : 1D array_like
-        The p-values to adjust. Elements must be numbers between 0 and 1.
+        The p-values to adjust. Elements must be real numbers between 0 and 1.
     axis : int
         The axis along which to perform the adjustment. The adjustment is
         performed independently along each axis-slice. If `axis` is None, `ps`
@@ -4220,10 +4222,9 @@ def false_discovery_control(ps, *, axis=0, method='bh'):
 
     Notes
     -----
-    The false discovery rate is the fraction of rejected null hypotheses
-    that were actually true. In multiple hypothesis testing, false discovery
-    control procedures tend to offer higher power than familywise error rate
-    control procedures (e.g. Bonferroni correction [1]_).
+    In multiple hypothesis testing, false discovery control procedures tend to
+    offer higher power than familywise error rate control procedures (e.g.
+    Bonferroni correction [1]_).
 
     If the p-values correspond with independent tests (or tests with
     "positive regression dependencies" [2]_), rejecting null hypotheses
@@ -4234,7 +4235,8 @@ def false_discovery_control(ps, *, axis=0, method='bh'):
     true even for dependent tests when the p-values are adjusted accorded to
     the more conservative Benjaminini-Yekutieli procedure.
 
-    The behavior of this function is similar to the R function ``p.adjust``.
+    The adjusted p-values produced by this function are comparable to those
+    produced by the R function ``p.adjust``.
 
     References
     ----------
@@ -4272,7 +4274,7 @@ def false_discovery_control(ps, *, axis=0, method='bh'):
     because it would not be appropriate to conclude that the front-loaded
     treatment is better if it is merely equivalent to the previous treatment.
 
-    The p-values from [4]_ were
+    The p-values corresponding with the 15 hypotheses in this family were
 
     >>> ps = [0.0001, 0.0004, 0.0019, 0.0095, 0.0201, 0.0278, 0.0298, 0.0344,
     ...       0.0459, 0.3240, 0.4262, 0.5719, 0.6528, 0.7590, 1.000]
@@ -4280,9 +4282,34 @@ def false_discovery_control(ps, *, axis=0, method='bh'):
     If the chosen significance level is 0.05, we may be tempted to reject the
     null hypotheses for the tests corresponding with the first nine p-values,
     as the first nine p-values fall below the chosen significance level.
+    However, this would ignore the problem of "multiplicity": if we fail to
+    correct for the fact that multiple comparisons are being performed, we
+    are more likely to incorrectly reject true null hypotheses.
 
-    If we wish to control the false discovery rate at 0.05, however, we could
-    apply the Benjamini-Hochberg p-value adjustment:
+    One approach to the multiplicity problem is to control the family-wise
+    error rate (FWER), that is, the rate at which the null hypothesis is
+    rejected when it is actually true. A common procedure of this kind is the
+    Bonferroni correction [1]_.  We begin by multiplying the p-values by the
+    number of hypotheses tested.
+
+    >>> import numpy as np
+    >>> np.array(ps) * len(ps)
+    array([1.5000e-03, 6.0000e-03, 2.8500e-02, 1.4250e-01, 3.0150e-01,
+           4.1700e-01, 4.4700e-01, 5.1600e-01, 6.8850e-01, 4.8600e+00,
+           6.3930e+00, 8.5785e+00, 9.7920e+00, 1.1385e+01, 1.5000e+01])
+
+    To control the FWER at 5%, we reject only the hypotheses corresponding
+    with adjusted p-values less than 0.05. In this case, only the hypotheses
+    corresponding with the first three p-values can be rejected. According to
+    [1], these three hypotheses concerned "allergic reaction" and "two
+    different aspects of bleeding."
+
+    An alternative approach is to control the false discovery rate: the
+    expected fraction of rejected null hypotheses that are actually true. The
+    advantage of this approach is that it typically affords greater power: an
+    increased rate of rejecting the null hypothesis when it is indeed false. To
+    control the false discovery rate at 5%, we apply the Benjamini-Hochberg
+    p-value adjustment.
 
     >>> from scipy import stats
     >>> stats.false_discovery_control(ps)
@@ -4290,8 +4317,11 @@ def false_discovery_control(ps, *, axis=0, method='bh'):
            0.06385714, 0.06385714, 0.0645    , 0.0765    , 0.486     ,
            0.58118182, 0.714875  , 0.75323077, 0.81321429, 1.        ])
 
-    Only the first four adjusted p-values fall below 0.05, so we would reject
-    only the null hypotheses corresponding with these p-values.
+    Now, the first *four* adjusted p-values fall below 0.05, so we would reject
+    the null hypotheses corresponding with these *four* p-values. Rejection
+    of the fourth null hypothesis was particularly important to the original
+    study as it led to the conclusion that the new treatment had a
+    "substantially lower in-hospital mortality rate."
 
     """
     # Input Validation and Special Cases
@@ -4323,8 +4353,9 @@ def false_discovery_control(ps, *, axis=0, method='bh'):
     m = ps.shape[-1]
 
     # Main Algorithm
-    # Equivalent to the ideas of [1] and [2], except that this adjust the
-    # p-values as in [3] and R's p.adjust.
+    # Equivalent to the ideas of [1] and [2], except that this adjusts the
+    # p-values as described in [3]. The results are similar to those produced
+    # by R's p.adjust.
 
     # "Let [ps] be the ordered observed p-values..."
     order = np.argsort(ps, axis=-1)
@@ -4339,7 +4370,9 @@ def false_discovery_control(ps, *, axis=0, method='bh'):
         ps *= np.sum(1 / i)
 
     # accounts for rejecting all null hypotheses i for i < k, where k is
-    # defined in Eq. 1 of either [1] or [2]. See [3].
+    # defined in Eq. 1 of either [1] or [2]. See [3]. Starting with the index j
+    # of the second to last element, we replace element j with element j+1 if
+    # the latter is smaller.
     np.minimum.accumulate(ps[..., ::-1], out=ps[..., ::-1], axis=-1)
 
     # Restore original order of axes and data
