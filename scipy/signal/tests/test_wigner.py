1d169b31b8ef6f73a2af297d96c617e53a9472eb
Pamphile Roy <roy.pamphile@gmail.com>
2022-12-09 16:13:49 +0100

    MAINT: remove submodule wigner

--- scipy/signal/tests/test_wigner.py
+++ scipy/signal/tests/test_wigner.py
@@ -2,12 +2,12 @@ import numpy as np
 from numpy.testing import assert_allclose
 from scipy.signal import gausspulse
 
-from scipy.signal import wigner
+from scipy.signal import wigner_ville
 
 
 class TestWigner:
     def test_wigner_ville_shapes(self):
-        t, f, wv = wigner.wigner_ville(np.sin(np.linspace(0, 100, 1024)),
+        t, f, wv = wigner_ville(np.sin(np.linspace(0, 100, 1024)),
                                        resolution=4, window_size=128)
         assert len(t) == 256
         assert len(f) == 128
@@ -19,7 +19,7 @@ class TestWigner:
         tc = 512 / fs
         t = np.arange(0, 1024) / fs
         x_re, x_im = gausspulse(t - tc, fc, bw=0.2, retquad=True)
-        t, f, wv = wigner.wigner_ville(x_re + 1j * x_im, fs)
+        t, f, wv = wigner_ville(x_re + 1j * x_im, fs)
         max_idx = np.unravel_index(np.argmax(wv), wv.shape)
         assert_allclose(fc, f[max_idx[0]], atol=f[1] - f[0])
         assert_allclose(tc, t[max_idx[1]], atol=t[1] - t[0])
