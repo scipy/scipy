c63aa8523327bcb9c7ea28b28aa9de3f180454ec
jakobjakobson13 <43045863+jakobjakobson13@users.noreply.github.com>
2022-12-10 21:06:57 +0100

    Update test_codata.py

--- scipy/constants/tests/test_codata.py
+++ scipy/constants/tests/test_codata.py
@@ -1,4 +1,4 @@
-from scipy.constants import find, value, ConstantWarning, c, speed_of_light
+from scipy.constants import find, value, ConstantWarning
 from numpy.testing import (assert_equal, assert_, assert_almost_equal,
                            suppress_warnings)
 import scipy.constants._codata as _cd
@@ -25,7 +25,8 @@ def test_find():
 
 
 def test_basic_lookup():
-    assert_equal('%d %s' % (_cd.c, _cd.unit('speed of light in vacuum')),
+    assert_equal('%d %s' % (_cd.value('speed of light in vacuum'),
+                            _cd.unit('speed of light in vacuum')),
                  '299792458 m s^-1')
 
 
