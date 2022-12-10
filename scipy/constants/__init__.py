8e9a424077214d8a8f126062fd955e723070a37f
jakobjakobson13 <43045863+jakobjakobson13@users.noreply.github.com>
2022-12-10 19:39:30 +0100

    delete ._constants import

--- scipy/constants/__init__.py
+++ scipy/constants/__init__.py
@@ -40,7 +40,6 @@ References
 """
 # Modules contributed by BasSw (wegwerp@gmail.com)
 from ._codata import *
-from ._constants import *
 from ._codata import _obsolete_constants, physical_constants
 
 # Deprecated namespaces, to be removed in v2.0.0
