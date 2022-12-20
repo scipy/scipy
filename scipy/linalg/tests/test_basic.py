5055c7eb2f13d05ab03e304d1f0ee83933d64009
Tyler Reddy <tyler.je.reddy@gmail.com>
2022-12-20 18:08:46 +0000

    MAINT, TST: alpine/musl segfault shim
    * gh-17630 describes a segfault observed in Cirrus CI (alpine)
    in `test_random_complex_exact` with NumPy `1.24.0`; it
    was not reproducible locally in an `alpine` docker container
    with latest SciPy + NumPy `1.24.0` built from tarball via
    `pip`
    
    * this branch aims to avoid the issue with a very-scoped
    test skip--only when `glibc` was not used to buid the
    Python interpreter, and only when NumPy is at `1.24.0`
    or greater
    
    [skip circle]

--- scipy/linalg/tests/test_basic.py
+++ scipy/linalg/tests/test_basic.py
@@ -1,3 +1,4 @@
+import platform
 import itertools
 import warnings
 
@@ -13,6 +14,7 @@ from numpy.testing import (assert_equal, assert_almost_equal, assert_,
 import pytest
 from pytest import raises as assert_raises
 
+from scipy._lib import _pep440
 from scipy.linalg import (solve, inv, det, lstsq, pinv, pinvh, norm,
                           solve_banded, solveh_banded, solve_triangular,
                           solve_circulant, circulant, LinAlgError, block_diag,
@@ -1132,6 +1134,11 @@ class TestLstsq:
                                           err_msg="driver: %s" % lapack_driver)
 
     def test_random_complex_exact(self):
+        if platform.system() != "Windows":
+            if _pep440.parse(np.__version__) >= _pep440.Version("1.24.0"):
+                libc_flavor = platform.libc_ver()[0]
+                if libc_flavor != "glibc":
+                    pytest.skip("segfault observed on alpine per gh-17630")
         for dtype in COMPLEX_DTYPES:
             for n in (20, 200):
                 for lapack_driver in TestLstsq.lapack_drivers:
