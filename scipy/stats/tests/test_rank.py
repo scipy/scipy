b6bcd36bf1a347ff9123c192a11ccfdda2aba9e7
Oriol (ZBook) <oriol.abril.pla@gmail.com>
2022-12-22 15:16:53 +0100

    TST: add test for propagate+axis=None

--- scipy/stats/tests/test_rank.py
+++ scipy/stats/tests/test_rank.py
@@ -224,13 +224,16 @@ class TestRankData:
 
         assert_array_equal(res, res0)
 
-    def test_nan_policy_omit_2d_flatten(self):
-        # 2 2d-array test
+    def test_nan_policy_2d_axis_none(self):
+        # 2 2d-array test with axis=None
         data = [[0, np.nan, 3],
                 [4, 2, np.nan],
                 [1, 2, 2]]
         assert_array_equal(rankdata(data, axis=None, nan_policy='omit'),
                            [1., np.nan, 6., 7., 4., np.nan, 2., 4., 4.])
+        assert_array_equal(rankdata(data, axis=None, nan_policy='propagate'),
+                           [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan,
+                            np.nan, np.nan, np.nan])
 
     def test_nan_policy_raise(self):
         # 1 1d-array test
