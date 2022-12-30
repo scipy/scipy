15280d3ebb1a39fcf161863aaa8ce146b2f99133
Pamphile Roy <roy.pamphile@gmail.com>
2022-12-30 13:12:18 +0100

    DOC/STY: spaces and wrong info in the book

--- scipy/stats/tests/test_sensitivity_analysis.py
+++ scipy/stats/tests/test_sensitivity_analysis.py
@@ -15,21 +15,21 @@ def ishigami_ref_indices():
     a = 7.
     b = 0.1
 
-    var = 0.5 + a ** 2 / 8 + b * np.pi ** 4 / 5\
-        + b ** 2 * np.pi ** 8 / 18
-    v1 = 0.5 + b * np.pi ** 4 / 5 + b ** 2 * np.pi ** 8 / 50
-    v2 = a ** 2 / 8
+    var = 0.5 + a**2/8 + b*np.pi**4/5 + b**2*np.pi**8/18
+    v1 = 0.5 + b*np.pi**4/5 + b**2*np.pi**8/50
+    v2 = a**2/8
     v3 = 0
     v12 = 0
-    v13 = b ** 2 * np.pi ** 8 * 8 / 225
+    # v13: mistake in the book, see other derivations e.g. in 10.1002/nme.4856
+    v13 = b**2*np.pi**8*8/225
     v23 = 0
 
-    s_first = np.array([v1 / var, v2 / var, v3 / var])
+    s_first = np.array([v1, v2, v3])/var
     s_second = np.array([
-        [0., 0., v13 / var],
-        [v12 / var, 0., v23 / var],
-        [v13 / var, v23 / var, 0.]
-    ])
+        [0., 0., v13],
+        [v12, 0., v23],
+        [v13, v23, 0.]
+    ])/var
     s_total = s_first + s_second.sum(axis=1)
 
     return [s_first.reshape(1, -1), s_total.reshape(1, -1)]
@@ -170,8 +170,8 @@ class TestSobolIndices:
 
             var = np.var(np.vstack([f_A, f_B]), axis=0)
 
-            s = (var - 0.5 * np.mean((f_B - f_AB) ** 2, axis=1)) / var
-            st = np.mean(f_A * (f_A - f_AB), axis=1) / var
+            s = (var - 0.5*np.mean((f_B - f_AB)**2, axis=1)) / var
+            st = np.mean(f_A*(f_A - f_AB), axis=1) / var
 
             return s.T, st.T
 
