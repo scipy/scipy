f3789d84e9bd7be25085b7e29168e305ee4a36d7
Matt Haberland <mhaberla@calpoly.edu>
2023-01-05 16:22:50 -0800

    MAINT: stats.multivariate_t: modify qmvnt for inclusion in SciPy

--- scipy/stats/_qmvnt.py
+++ scipy/stats/_qmvnt.py
@@ -1,35 +1,35 @@
-"""Integration of multivariate normal and t distributions.
+# Integration of multivariate normal and t distributions.
 
-Adapted from the MATLAB original implementations by Dr. Alan Genz.
+# Adapted from the MATLAB original implementations by Dr. Alan Genz.
 
-    http://www.math.wsu.edu/faculty/genz/software/software.html
+#     http://www.math.wsu.edu/faculty/genz/software/software.html
 
-Copyright (C) 2013, Alan Genz,  All rights reserved.
-Python implementation is copyright (C) 2022, Robert Kern,  All rights reserved.
+# Copyright (C) 2013, Alan Genz,  All rights reserved.
+# Python implementation is copyright (C) 2022, Robert Kern,  All rights reserved.
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided the following conditions are met:
-  1. Redistributions of source code must retain the above copyright
-     notice, this list of conditions and the following disclaimer.
-  2. Redistributions in binary form must reproduce the above copyright
-     notice, this list of conditions and the following disclaimer in
-     the documentation and/or other materials provided with the
-     distribution.
-  3. The contributor name(s) may not be used to endorse or promote
-     products derived from this software without specific prior
-     written permission.
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
-OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
-TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-"""
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided the following conditions are met:
+#   1. Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#   2. Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in
+#      the documentation and/or other materials provided with the
+#      distribution.
+#   3. The contributor name(s) may not be used to endorse or promote
+#      products derived from this software without specific prior
+#      written permission.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+# OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+# TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 
 import numpy as np
 
@@ -42,7 +42,7 @@ phi = ndtr
 phinv = ndtri
 
 
-def richtmyer_lattice(n_dim, n_qmc_samples):
+def _richtmyer_lattice(n_dim, n_qmc_samples):
     q = np.sqrt(n_primes(n_dim))
     return q, n_qmc_samples
 
@@ -80,7 +80,7 @@ def _primitive_root(p):
     return r
 
 
-def cbc_lattice(n_dim, n_qmc_samples):
+def _cbc_lattice(n_dim, n_qmc_samples):
     # Round down to the nearest prime number.
     primes = primes_from_2_to(n_qmc_samples + 1)
     n_qmc_samples = primes[-1]
@@ -113,30 +113,30 @@ def cbc_lattice(n_dim, n_qmc_samples):
     return q, n_qmc_samples
 
 
-def get_lattice(lattice, n_dim, n_qmc_samples):
+def _get_lattice(lattice, n_dim, n_qmc_samples):
     if lattice == 'richtmyer':
-        lattice = richtmyer_lattice
+        lattice = _richtmyer_lattice
     elif lattice == 'cbc':
-        lattice = cbc_lattice
+        lattice = _cbc_lattice
     return lattice(n_dim, n_qmc_samples)
 
 
-def qauto(func, covar, low, high, error=1e-3, limit=10_000, **kwds):
+def _qauto(func, covar, low, high, error=1e-3, limit=10_000, **kwds):
     """Automatically rerun the integration to get the required error bound.
 
     Parameters
     ----------
     func : callable
-        Either :func:`qmvn` or :func:`qmvt`.
+        Either :func:`_qmvn` or :func:`_qmvt`.
     covar, low, high : array
-        As specified in :func:`qmvn` and :func:`qmvt`.
+        As specified in :func:`_qmvn` and :func:`_qmvt`.
     error : float > 0
         The desired error bound.
     limit : int > 0:
         The rough limit of the number of integration points to consider. The
         integration will stop looping once this limit has been *exceeded*.
     **kwds :
-        Other keyword arguments to pass to `func`. When using :func:`qmvt`, be
+        Other keyword arguments to pass to `func`. When using :func:`_qmvt`, be
         sure to include ``nu=`` as one of these.
 
     Returns
@@ -170,7 +170,7 @@ def qauto(func, covar, low, high, error=1e-3, limit=10_000, **kwds):
     return prob, est_error, n_samples
 
 
-def qmvn(m, covar, low, high, lattice='cbc', n_batches=10, rng=None):
+def _qmvn(m, covar, low, high, lattice='cbc', n_batches=10, rng=None):
     """Multivariate normal integration over box bounds.
 
     Parameters
@@ -207,7 +207,7 @@ def qmvn(m, covar, low, high, lattice='cbc', n_batches=10, rng=None):
     dci = d - ci
     prob = 0.0
     error_var = 0.0
-    q, n_qmc_samples = get_lattice(lattice, n - 1, max(m // n_batches, 1))
+    q, n_qmc_samples = _get_lattice(lattice, n - 1, max(m // n_batches, 1))
     y = np.zeros((n - 1, n_qmc_samples))
     i_samples = np.arange(n_qmc_samples) + 1
     for j in range(n_batches):
@@ -302,7 +302,7 @@ def _mvn_qmc_integrand(covar, low, high, use_tent=False):
     return integrand, ndim_integrand
 
 
-def qmvt(m, covar, low, high, nu, lattice='cbc', n_batches=10, rng=None):
+def _qmvt(m, covar, low, high, nu, lattice='cbc', n_batches=10, rng=None):
     """Multivariate t integration over box bounds.
 
     Parameters
@@ -341,7 +341,7 @@ def qmvt(m, covar, low, high, nu, lattice='cbc', n_batches=10, rng=None):
     n = cho.shape[0]
     prob = 0.0
     error_var = 0.0
-    q, n_qmc_samples = get_lattice(lattice, n, max(m // n_batches, 1))
+    q, n_qmc_samples = _get_lattice(lattice, n, max(m // n_batches, 1))
     i_samples = np.arange(n_qmc_samples) + 1
     for j in range(n_batches):
         pv = np.ones(n_qmc_samples)
@@ -396,76 +396,6 @@ def _bounds(m, t, alternative):
     else:
         raise ValueError("expected one of {'less', 'greater', 'two-sided'}")
     return low, high
-
-
-def dunnett_test(control_data, treatment_data, alternative='two-sided',
-                 **qmvt_kwds):
-    """Dunnett's test for comparison of multiple means against a control.
-
-    Parameters
-    ----------
-    control_data : float array
-        The measurements for the control group.
-    treatment_data : list of float arrays
-        Arrays of the measurements for each non-control treatment group.
-    alternative : {'two-sided', 'less', 'greater'}, optional
-        Defines the alternative hypothesis.
-        The following options are available (default is 'two-sided'):
-
-        * 'two-sided': the mean of the treatment group is different than the
-          control group mean
-        * 'less': the mean of the treatment group is less than the control
-          group mean
-        * 'greater': the mean of the treatment group is greater than the
-          control group mean
-    **qmvt_kwds :
-        Extra keyword arguments to pass to :func:`qmvt` to control the
-        integration.
-
-    Returns
-    -------
-    ts : float array
-        The t-statistic for each treatment group.
-    ps : float array
-        The multiple-comparison-corrected p-value for the t-statistic for each
-        treatment group.
-    errs : float array
-        The estimated error for the p-value for each treatment group.
-    """
-    control_data = np.asarray(control_data)
-    treatment_data = [np.asarray(x) for x in treatment_data]
-    n0 = len(control_data)
-    ns = [len(x) for x in treatment_data]
-    m = len(treatment_data)
-    nu = sum(ns) + n0 - (m + 1)
-    mu0 = control_data.mean()
-    mus = [x.mean() for x in treatment_data]
-    s2 = np.sum((control_data - mu0)**2) / nu
-    for x, mu in zip(treatment_data, mus):
-        s2 += np.sum((x - mu)**2) / nu
-    s = np.sqrt(s2)
-    ts = np.array([(mu - mu0) / s / np.sqrt(1/ni + 1/n0)
-                   for mu, ni in zip(mus, ns)])
-
-    covar = np.eye(m)
-    for i in range(m - 1):
-        for j in range(i + 1, m):
-            rho = np.sqrt(ns[i] / (ns[i] + n0)) * np.sqrt(ns[j]/(ns[j] + n0))
-            covar[i, j] = covar[j, i] = rho
-
-    ps = []
-    errs = []
-    for t in ts:
-        low, high = _bounds(m, t, alternative)
-        p, err, _ = qmvt(m * 100_000, covar, low, high, nu, **qmvt_kwds)
-        ps.append(p)
-        errs.append(err)
-    ps = np.array(ps)
-    if alternative == 'two-sided':
-        ps = 1 - ps
-    errs = np.array(errs)
-
-    return ts, ps, errs
 
 
 def _permuted_cholesky(covar, low, high, tol=1e-10):
@@ -574,23 +504,3 @@ def _swap_slices(x, slc1, slc2):
     t = x[slc1].copy()
     x[slc1] = x[slc2].copy()
     x[slc2] = t
-
-
-def main():
-    import argparse
-    parser = argparse.ArgumentParser(
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('ndim', type=int, help='The number of dimensions of the problem')
-
-    args = parser.parse_args()
-    covar = np.full((args.ndim, args.ndim), 0.5)
-    covar.flat[::args.ndim + 1] = 1.0
-    low = np.full(args.ndim, -np.inf)
-    high = np.zeros(args.ndim)
-    expected = 1 / (args.ndim + 1)
-    p, e, m = qmvn(args.ndim * 10_000, covar, low, high)
-    z = (p - expected) / (e / 3)
-    print(z)
-
-if __name__ == '__main__':
-    main()
