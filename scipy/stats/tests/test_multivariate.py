9914ada67a8d259a2bad9c918b1d2ed63270aa74
dschmitz89 <danielschmitzsiegen@googlemail.com>
2023-01-06 17:38:58 +0100

    TST: compare VMF and 2D Von Mises

--- scipy/stats/tests/test_multivariate.py
+++ scipy/stats/tests/test_multivariate.py
@@ -26,7 +26,8 @@ from scipy.stats import (multivariate_normal, multivariate_hypergeom,
                          beta, wishart, multinomial, invwishart, chi2,
                          invgamma, norm, uniform, ks_2samp, kstest, binom,
                          hypergeom, multivariate_t, cauchy, normaltest,
-                         random_table, uniform_direction, vonmises_fisher)
+                         random_table, uniform_direction, vonmises_fisher,
+                         vonmises)
 from scipy.stats import _covariance, Covariance
 
 from scipy.integrate import romb
@@ -3068,6 +3069,21 @@ class TestVonMises_Fisher:
                 assert_allclose(current_pdf, all_pdf[i, j])
                 current_logpdf = vmf.logpdf(x[i, j, :])
                 assert_allclose(current_logpdf, all_logpdf[i, j])
+
+    def test_vs_vonmises_2d(self):
+        # test that in 2D, von Mises-Fisher yields the same results
+        # as the von Mises distribution
+        rng = np.random.default_rng(2777937887058094419)
+        mu = np.array([0, 1])
+        mu_angle = np.arctan2(mu[1], mu[0])
+        kappa = 20
+        vmf = vonmises_fisher(mu, kappa)
+        vonmises_dist = vonmises(loc=mu_angle, kappa=kappa)
+        vectors = uniform_direction(2).rvs(10, random_state=rng)
+        angles = np.arctan2(vectors[:, 1], vectors[:, 0])
+        assert_allclose(vonmises_dist.entropy(), vmf.entropy())
+        assert_allclose(vonmises_dist.pdf(angles), vmf.pdf(vectors))
+        assert_allclose(vonmises_dist.logpdf(angles), vmf.logpdf(vectors))
 
     @pytest.mark.parametrize("dim", [2, 3, 6])
     @pytest.mark.parametrize("kappa, mu_tol, kappa_tol",
