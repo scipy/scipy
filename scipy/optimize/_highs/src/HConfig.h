8ca06f282af4988d20cc7cea926ceba13ed8544d
Nicholas McKibben <nicholas.bgp@gmail.com>
2023-01-09 21:17:17 -0700

    DOC: add clarifying note for empty HConfig.h header file

--- scipy/optimize/_highs/src/HConfig.h
+++ scipy/optimize/_highs/src/HConfig.h
@@ -1,4 +1,10 @@
 #ifndef SCIPY_HIGHS_HCONFIG_H
 #define SCIPY_HIGHS_HCONFIG_H
 
+// NOTE: This file is required by HiGHS but intentionally empty
+//       for SciPy integration. It may be used in the future for
+//       SciPy-specific functionality.
+//       The macro SCIPY_HIGHS_CONFIG_H is a header-guard only
+//       and not used anywhere else.
+
 #endif  // SCIPY_HIGHS_HCONFIG_H
