77a648875d57901ac57506563821c594e9ac070f
SoubhagyaKhan <115607099+SoubhagyaKhan@users.noreply.github.com>
2023-01-14 14:53:57 +0530

    Update scipy/spatial/distance.py
    Co-authored-by: Jake Bowhay <60778417+j-bowhay@users.noreply.github.com>

--- scipy/spatial/distance.py
+++ scipy/spatial/distance.py
@@ -605,11 +605,15 @@ def correlation(u, v, w=None, centered=True):
 
     Examples
     --------
-    # To find the correlation distance between two arrays(length of each array must be same) without weights:
+    To find the correlation distance between two arrays (length of each
+    array must be same) without weights:
+    
     >>> from scipy.spatial import distance
     >>> distance.correlation([1, 0, 1], [2, 3, 1])
     1.8660245037844386
-    # To find the correlation distance between two arrays with weights provided:
+    
+    To find the correlation distance between two arrays with weights provided:
+    
     >>> distance.correlation([1, 0, 1], [2, 2, 1], [2, 3, 1])
     1.4472135954999579
 
