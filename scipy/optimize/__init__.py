34773cd95796c16f1eac5a95809409e0b0cf1cef
Jake Bowhay <jb9.bowhay@gmail.com>
2023-01-20 08:58:53 +0000

    MAINT: fix refguide

--- scipy/optimize/__init__.py
+++ scipy/optimize/__init__.py
@@ -200,8 +200,7 @@ functions defined on (a subset of) the complex plane.
 
 .. seealso::
 
-   `scipy.optimize.cython_optimize` -- Typed Cython versions of root finding
-   functions
+   `scipy.optimize.cython_optimize` -- Typed Cython versions of root finding functions
 
 Fixed point finding:
 
@@ -400,7 +399,7 @@ Simple iteration solvers:
    linearmixing
    diagbroyden
 
-"""
+"""  # noqa: E501
 
 from ._optimize import *
 from ._minimize import *
