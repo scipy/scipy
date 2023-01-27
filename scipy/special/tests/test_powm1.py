72aea03810d2bc59db5da91ba3888ea5131a8dd7
Matt Borland <matt@mattborland.com>
2023-01-27 08:24:36 -0800

    BUG: Fix powm1 test case

--- scipy/special/tests/test_powm1.py
+++ scipy/special/tests/test_powm1.py
@@ -44,7 +44,7 @@ def test_powm1(x, y, expected, rtol):
                           (np.inf, -np.inf, -1.0),
                           (np.inf, 0.0, 0.0),
                           (-np.inf, 0.0, 0.0),
-                          (-np.inf, 2.0, np.inf),
+                          (-np.inf, 2.0, -np.inf),
                           (-np.inf, 3.0, -np.inf),
                           (-1.0, float(2**53 - 1), -2.0)])
 def test_powm1_exact_cases(x, y, expected):
