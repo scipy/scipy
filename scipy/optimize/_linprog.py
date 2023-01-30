0759eb7d2dbf9024ce364283025cc6a7055e6c79
Christian Lorentzen <lorentzen.ch@gmail.com>
2023-01-30 09:04:57 +0100

    DOC put comments under the bounds parameter

--- scipy/optimize/_linprog.py
+++ scipy/optimize/_linprog.py
@@ -197,12 +197,8 @@ def linprog(c, A_ub=None, b_ub=None, A_eq=None, b_eq=None,
             A_eq @ x == b_eq
             lb <= x <= ub
 
-    Note that by default ``lb = 0`` and ``ub = None`` unless specified with
-    ``bounds``. There is, however, no need to manually formulate the linear
-    programming problem in terms of positive variables, often termed slack
-    variables. By setting corresponding values of ``min`` in ``bounds``,
-    e.g. a negative value or ``None``, ``linprog`` is well able to handle
-    negative or real valued ``x``.
+    Note that by default ``lb = 0`` and ``ub = None``. Other bounds can be
+    specified with ``bounds``.
 
     Parameters
     ----------
@@ -222,11 +218,13 @@ def linprog(c, A_ub=None, b_ub=None, A_eq=None, b_eq=None,
         the corresponding element of ``b_eq``.
     bounds : sequence, optional
         A sequence of ``(min, max)`` pairs for each element in ``x``, defining
-        the minimum and maximum values of that decision variable. Use ``None``
-        to indicate that there is no bound. By default, bounds are
-        ``(0, None)`` (all decision variables are non-negative).
-        If a single tuple ``(min, max)`` is provided, then ``min`` and
-        ``max`` will serve as bounds for all decision variables.
+        the minimum and maximum values of that decision variable.
+        If a single tuple ``(min, max)`` is provided, then ``min`` and ``max``
+        will serve as bounds for all decision variables.
+        Use ``None`` to indicate that there is no bound. For instance, the
+        default bound ``(0, None)`` means that all decision variables are
+        non-negative, and the pair ``(None, None)`` means no bounds at all,
+        i.e. all variables are allowed to take values form the real numbers.
     method : str, optional
         The algorithm used to solve the standard form problem.
         :ref:`'highs' <optimize.linprog-highs>` (default),
