649bcafd4238722326f462078557b926db5ded1a
Jake Bowhay <jb9.bowhay@gmail.com>
2023-02-12 16:38:36 +0000

    MAINT: fix refguide check

--- scipy/integrate/_quad_vec.py
+++ scipy/integrate/_quad_vec.py
@@ -231,7 +231,7 @@ def quad_vec(f, a, b, epsabs=1e-200, epsrel=1e-8, norm='2', cache_size=100e6, li
     >>> f = lambda x: x**alpha
     >>> x0, x1 = 0, 2
     >>> res = quad_vec(f, x0, x1)
-    >>> plt.plot(alpha, res.y)
+    >>> plt.plot(alpha, res.integral)
     >>> plt.xlabel(r"$\alpha$")
     >>> plt.ylabel(r"$\int_{0}^{2} x^\alpha dx$")
     >>> plt.show()
