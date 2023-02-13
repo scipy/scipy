43fad065ff0aa156e28941e8f22d30ff80eec5a4
steppi <albert_steppi@hms.harvard.edu>
2023-02-12 22:45:13 -0500

    MAINT: Add dists back to fails_cmplx lost in  merge

--- scipy/stats/tests/test_continuous_basic.py
+++ scipy/stats/tests/test_continuous_basic.py
@@ -109,10 +109,10 @@ skip_fit_fix_test = {"MLE": skip_fit_fix_test_mle,
 # cf https://github.com/scipy/scipy/pull/4979 for a discussion.
 
 fails_cmplx = set(['argus', 'beta', 'betaprime', 'chi', 'chi2', 'cosine',
-                   'dgamma', 'dweibull', 'erlang', 'f', 'gamma',
+                   'dgamma', 'dweibull', 'erlang', 'f', 'foldcauchy', 'gamma',
                    'gausshyper', 'gengamma', 'genhyperbolic',
                    'geninvgauss', 'gennorm', 'genpareto',
-                   'halfgennorm', 'invgamma',
+                   'halfcauchy', 'halfgennorm', 'invgamma',
                    'ksone', 'kstwo', 'kstwobign', 'levy_l', 'loggamma',
                    'logistic', 'loguniform', 'maxwell', 'nakagami',
                    'ncf', 'nct', 'ncx2', 'norminvgauss', 'pearson3',
