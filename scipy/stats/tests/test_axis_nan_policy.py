f46ca6c2246b5eec7e3c2db7a1a482884f3a3e2c
Pamphile Roy <roy.pamphile@gmail.com>
2023-02-14 15:57:53 +0100

    ENH: stats: add axis tuple and nan_policy to mstats.sem

--- scipy/stats/tests/test_axis_nan_policy.py
+++ scipy/stats/tests/test_axis_nan_policy.py
@@ -38,6 +38,8 @@ axis_nan_policy_cases = [
     (stats.hmean, tuple(), dict(), 1, 1, False, lambda x: (x,)),
     (stats.pmean, (1.42,), dict(), 1, 1, False, lambda x: (x,)),
     (stats.sem, tuple(), dict(), 1, 1, False, lambda x: (x,)),
+    (stats.mstats.sem, tuple(), dict(), 1, 1, False, lambda x: (x,)),
+    (stats.iqr, tuple(), dict(), 1, 1, False, lambda x: (x,)),
     (stats.kurtosis, tuple(), dict(), 1, 1, False, lambda x: (x,)),
     (stats.skew, tuple(), dict(), 1, 1, False, lambda x: (x,)),
     (stats.kstat, tuple(), dict(), 1, 1, False, lambda x: (x,)),
