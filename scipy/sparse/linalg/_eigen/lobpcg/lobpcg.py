103e09efcf3a41bdfb79c29202cb4ff1fac47e48
Andrew Knyazev <andrew.knyazev@ucdenver.edu>
2023-03-01 12:09:56 -0500

    Update lobpcg.py
    ident fixes

--- scipy/sparse/linalg/_eigen/lobpcg/lobpcg.py
+++ scipy/sparse/linalg/_eigen/lobpcg/lobpcg.py
@@ -80,26 +80,26 @@ def _b_orthonormalize(B, blockVectorV, blockVectorBV=None,
     type = blockVectorV.dtype
     normalization = np.ones(blockVectorV.shape[1], dtype = type)
     if blockVectorBV is None:
-       if B is not None:
-               try:
-                   blockVectorBV = B(blockVectorV)
-               except Exception as e:
-                   if verbosityLevel:
-                       warnings.warn(
-                           f"Secondary MatMul call failed with error\n"
-                           f"{e}\n",
-                           UserWarning, stacklevel=3
-                       )
-                       return None, None, None, normalization
-               if blockVectorBV.shape != blockVectorV.shape:
-                   raise ValueError(
-                       f"The shape {blockVectorV.shape} "
-                       f"of the orthogonalized matrix not preserved\n"
-                       f"and changed to {blockVectorBV.shape} "
-                       f"after multiplying by the secondary matrix.\n"
-                   )
-           else:
-               blockVectorBV = blockVectorV  # Shared data!!!
+        if B is not None:
+            try:
+                blockVectorBV = B(blockVectorV)
+            except Exception as e:
+                if verbosityLevel:
+                    warnings.warn(
+                        f"Secondary MatMul call failed with error\n"
+                        f"{e}\n",
+                        UserWarning, stacklevel=3
+                    )
+                    return None, None, None, normalization
+            if blockVectorBV.shape != blockVectorV.shape:
+                raise ValueError(
+                    f"The shape {blockVectorV.shape} "
+                    f"of the orthogonalized matrix not preserved\n"
+                    f"and changed to {blockVectorBV.shape} "
+                    f"after multiplying by the secondary matrix.\n"
+                )
+        else:
+            blockVectorBV = blockVectorV  # Shared data!!!
     VBV = blockVectorV.T.conj() @ blockVectorBV
     try:
         # VBV is a Cholesky factor from now on...
