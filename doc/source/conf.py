bcdf11e4d5ff34d48421dea3804818cef021ada1
Pamphile Roy <roy.pamphile@gmail.com>
2023-03-02 11:08:37 +0100

    MAINT: icon_links now required.
    [skip cirrus] [skip actions] [skip azp]

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -176,6 +176,7 @@ html_favicon = '_static/favicon.ico'
 html_theme_options = {
   "github_url": "https://github.com/scipy/scipy",
   "twitter_url": "https://twitter.com/SciPy_team",
+  "icon_links": [],
   "navbar_end": ["theme-switcher", "version-switcher", "navbar-icon-links"],
   "switcher": {
       "json_url": "https://scipy.github.io/devdocs/_static/version_switcher.json",
