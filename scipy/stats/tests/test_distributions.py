9f1adac8993d820b1a6aafa42ce04736f92be30d
Matteo Raso <matteo_luigi_raso@protonmail.com>
2023-03-11 14:46:52 -0500

    Removed some test cases

--- scipy/stats/tests/test_distributions.py
+++ scipy/stats/tests/test_distributions.py
@@ -2924,9 +2924,6 @@ class TestInvGamma:
 
     @pytest.mark.parametrize("a, ref",
                             [(100000000.0, -26.21208257605721),
-                            (10000000000.0, -33.11983786163935),
-                            (1e+20, -67.6586142566167),
-                            (1e+75, -257.6218844286255),
                             (1e+100, -343.9688254159022)])
     def test_large_entropy(self, a, ref):
         # The reference values were calculated with mpmath:
