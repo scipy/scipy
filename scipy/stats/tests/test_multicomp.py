a2664b6204e44032fbafebc4ec23b6b7b7f7346b
Pamphile Roy <roy.pamphile@gmail.com>
2023-03-13 22:55:31 +0100

    TST: add more full examples matching R.
    [skip circle] [skip azp] [skip actions]

--- scipy/stats/tests/test_multicomp.py
+++ scipy/stats/tests/test_multicomp.py
@@ -51,39 +51,72 @@ class TestDunnett:
         )
         assert_allclose(res, pvalue, atol=5e-3)
 
-    def test_unbalanced(self):
-        # compare results from Matlab's documentation on multcompare
+    # From Matlab's documentation on multcompare
+    samples_1 = [
+        [
+            24.0, 27.0, 33.0, 32.0, 28.0, 19.0, 37.0, 31.0, 36.0, 36.0,
+            34.0, 38.0, 32.0, 38.0, 32.0
+        ],
+        [26.0, 24.0, 26.0, 25.0, 29.0, 29.5, 16.5, 36.0, 44.0],
+        [25.0, 27.0, 19.0],
+        [25.0, 20.0],
+        [28.0]
+    ]
+    control_1 = [
+        18.0, 15.0, 18.0, 16.0, 17.0, 15.0, 14.0, 14.0, 14.0, 15.0, 15.0,
+        14.0, 15.0, 14.0, 22.0, 18.0, 21.0, 21.0, 10.0, 10.0, 11.0, 9.0,
+        25.0, 26.0, 17.5, 16.0, 15.5, 14.5, 22.0, 22.0, 24.0, 22.5, 29.0,
+        24.5, 20.0, 18.0, 18.5, 17.5, 26.5, 13.0, 16.5, 13.0, 13.0, 13.0,
+        28.0, 27.0, 34.0, 31.0, 29.0, 27.0, 24.0, 23.0, 38.0, 36.0, 25.0,
+        38.0, 26.0, 22.0, 36.0, 27.0, 27.0, 32.0, 28.0, 31.0
+    ]
+    pvalue_1 = [4.727e-06, 0.022346, 0.97912, 0.99953, 0.86579]
+
+    # From Dunnett1995 comparing with R's DescTools: DunnettTest
+    samples_2 = [
+        [9.76, 8.80, 7.68, 9.36], [12.80, 9.68, 12.16, 9.20, 10.55]
+    ]
+    control_2 = [7.40, 8.50, 7.20, 8.24, 9.84, 8.32]
+    pvalue_2 = [0.6201, 0.0058]
+
+    samples_3 = [
+        [55, 64, 64], [55, 49, 52], [50, 44, 41]
+    ]
+    control_3 = [55, 47, 48]
+    pvalue_3 = [0.0364, 0.8966, 0.4091]
+
+    # From Thomson and Short,
+    # Mucociliary function in health, chronic obstructive airway disease,
+    # and asbestosis, Journal of Applied Physiology, 1969. Table 1
+    # Comparing with R's DescTools: DunnettTest
+    samples_4 = [[3.8, 2.7, 4.0, 2.4], [2.8, 3.4, 3.7, 2.2, 2.0]]
+    control_4 = [2.9, 3.0, 2.5, 2.6, 3.2]
+    pvalue_4 = [0.5832, 0.9982]
+
+    @pytest.mark.parametrize(
+        'samples, control, pvalue',
+        [
+            (samples_1, control_1, pvalue_1),
+            (samples_2, control_2, pvalue_2),
+            (samples_3, control_3, pvalue_3),
+            (samples_4, control_4, pvalue_4),
+        ]
+    )
+    def test_unbalanced(self, samples, control, pvalue):
         rng = np.random.default_rng(11681140010308601919115036826969764808)
-        samples = [
-            [
-                24.0, 27.0, 33.0, 32.0, 28.0, 19.0, 37.0, 31.0, 36.0, 36.0,
-                34.0, 38.0, 32.0, 38.0, 32.0
-            ],
-            [26.0, 24.0, 26.0, 25.0, 29.0, 29.5, 16.5, 36.0, 44.0],
-            [25.0, 27.0, 19.0],
-            [25.0, 20.0],
-            [28.0]
-        ]
-        control = [
-            18.0, 15.0, 18.0, 16.0, 17.0, 15.0, 14.0, 14.0, 14.0, 15.0, 15.0,
-            14.0, 15.0, 14.0, 22.0, 18.0, 21.0, 21.0, 10.0, 10.0, 11.0, 9.0,
-            25.0, 26.0, 17.5, 16.0, 15.5, 14.5, 22.0, 22.0, 24.0, 22.5, 29.0,
-            24.5, 20.0, 18.0, 18.5, 17.5, 26.5, 13.0, 16.5, 13.0, 13.0, 13.0,
-            28.0, 27.0, 34.0, 31.0, 29.0, 27.0, 24.0, 23.0, 38.0, 36.0, 25.0,
-            38.0, 26.0, 22.0, 36.0, 27.0, 27.0, 32.0, 28.0, 31.0
-        ]
-        ref = np.array([4.727e-06, 0.022346, 0.97912, 0.99953, 0.86579])
 
         res = stats.dunnett(*samples, control=control, random_state=rng)
 
         assert isinstance(res, DunnettResult)
-        assert_allclose(res.pvalue, ref, atol=1e-3)
+        assert_allclose(res.pvalue, pvalue, atol=1e-3)
 
     @pytest.mark.parametrize(
         'alternative',
         ['two-sided', 'less', 'greater']
     )
     def test_ttest_ind(self, alternative):
+        # check that `dunnett` agrees with `ttest_ind`
+        # when there are only two groups
         rng = np.random.default_rng(114184017807316971636137493526995620351)
 
         for _ in range(10):
