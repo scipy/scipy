c7d32a6c53b75c8200091da00bc7c472d64ee2fe
dschmitz89 <danielschmitzsiegen@googlemail.com>
2023-03-15 21:11:04 +0100

    TST: strengthen isf and ppf tests

--- scipy/stats/_continuous_distns.py
+++ scipy/stats/_continuous_distns.py
@@ -2762,7 +2762,7 @@ class genlogistic_gen(rv_continuous):
         return -sc.expm1(self._logcdf(x, c))
 
     def _isf(self, q, c):
-        return -np.log(sc.powm1(1 - q, -1.0/c))
+        return self._ppf(1 - q, c)
 
     def _stats(self, c):
         mu = _EULER + sc.psi(c)
