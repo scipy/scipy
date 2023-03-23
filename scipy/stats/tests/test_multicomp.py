5be95abef79e84982698511a87225f01689d705d
Pamphile Roy <roy.pamphile@gmail.com>
2023-03-23 19:38:16 +0100

    TST: add another example and restructure cases

--- scipy/stats/tests/test_multicomp.py
+++ scipy/stats/tests/test_multicomp.py
@@ -9,48 +9,6 @@ from scipy.stats._multicomp import _pvalue_dunnett, DunnettResult
 
 
 class TestDunnett:
-
-    @pytest.mark.parametrize(
-        'rho, n_groups, df, statistic, pvalue, alternative',
-        [
-            # From Dunnett1955
-            # Tables 1a and 1b pages 1117-1118
-            (0.5, 1, 10, 1.81, 0.05, "greater"),  # different than two-sided
-            (0.5, 3, 10, 2.34, 0.05, "greater"),
-            (0.5, 2, 30, 1.99, 0.05, "greater"),
-            (0.5, 5, 30, 2.33, 0.05, "greater"),
-            (0.5, 4, 12, 3.32, 0.01, "greater"),
-            (0.5, 7, 12, 3.56, 0.01, "greater"),
-            (0.5, 2, 60, 2.64, 0.01, "greater"),
-            (0.5, 4, 60, 2.87, 0.01, "greater"),
-            (0.5, 4, 60, [2.87, 2.21], [0.01, 0.05], "greater"),
-            # Tables 2a and 2b pages 1119-1120
-            (0.5, 1, 10, 2.23, 0.05, "two-sided"),  # two-sided
-            (0.5, 3, 10, 2.81, 0.05, "two-sided"),
-            (0.5, 2, 30, 2.32, 0.05, "two-sided"),
-            (0.5, 3, 20, 2.57, 0.05, "two-sided"),
-            (0.5, 4, 12, 3.76, 0.01, "two-sided"),
-            (0.5, 7, 12, 4.08, 0.01, "two-sided"),
-            (0.5, 2, 60, 2.90, 0.01, "two-sided"),
-            (0.5, 4, 60, 3.14, 0.01, "two-sided"),
-            (0.5, 4, 60, [3.14, 2.55], [0.01, 0.05], "two-sided"),
-        ],
-    )
-    def test_critical_values(
-        self, rho, n_groups, df, statistic, pvalue, alternative
-    ):
-        rng = np.random.default_rng(165250594791731684851746311027739134893)
-        rho = np.full((n_groups, n_groups), rho)
-        np.fill_diagonal(rho, 1)
-
-        statistic = np.array(statistic)
-        res = _pvalue_dunnett(
-            rho=rho, df=df, statistic=statistic,
-            alternative=alternative,
-            rng=rng
-        )
-        assert_allclose(res, pvalue, atol=5e-3)
-
     # For the following tests, p-values were computed using Matlab, e.g.
     #     sample = [18.  15.  18.  16.  17.  15.  14.  14.  14.  15.  15....
     #               14.  15.  14.  22.  18.  21.  21.  10.  10.  11.  9....
@@ -112,6 +70,7 @@ class TestDunnett:
     #                   linfct=mcp(group="Dunnett"),
     #                   alternative="g")
     #     summary(test)
+    #     confint(test)
     # p-values agreed with those produced by Matlab to at least atol=1e-3
 
     # From Matlab's documentation on multcompare
@@ -139,6 +98,24 @@ class TestDunnett:
     pvalue_1_twosided = [1e-4, 0.02237, 0.97913, 0.99953, 0.86583]
     pvalue_1_greater = [1e-4, 0.011217, 0.768500, 0.896991, 0.577211]
     pvalue_1_less = [1, 1, 0.99660, 0.98398, .99953]
+    ci_1_twosided = [
+        [
+            5.3633917835622, 0.7296142201217, -8.3879817106607,
+            -11.9090753452911, -11.7655021543469
+        ],
+        [
+            15.9709832164378, 13.8936496687672, 13.4556900439941,
+            14.6434503452911, 25.4998771543469
+        ]
+    ]
+    ci_1_less = [
+        5.9036402398526, 1.4000632918725, -7.2754756323636, -10.5567456382391,
+        -9.8675629499576
+    ]
+    ci_1_greater = [
+        15.4306165948619, 13.2230539537359, 12.3429406339544, 13.2908248513211,
+        23.6015228251660
+    ]
 
     # From Dunnett1955 comparing with R's DescTools: DunnettTest
     samples_2 = [
@@ -154,6 +131,12 @@ class TestDunnett:
     control_3 = [55, 47, 48]
     pvalue_3 = [0.0364, 0.8966, 0.4091]
     statistic_3 = [3.09073, 0.56195, -1.40488]
+    ci_3_twosided = [
+        [0.7529028025053, -8.2470971974947, -15.2470971974947],
+        [21.2470971974947, 12.2470971974947, 5.2470971974947]
+    ]
+    ci_3_greater = [19.5984402363662, 10.5984402363662, 3.5984402363662]
+    ci_3_less = [2.4023682323149, -6.5976317676851, -13.5976317676851]
 
     # From Thomson and Short,
     # Mucociliary function in health, chronic obstructive airway disease,
@@ -163,6 +146,47 @@ class TestDunnett:
     control_4 = [2.9, 3.0, 2.5, 2.6, 3.2]
     pvalue_4 = [0.5832, 0.9982]
     statistic_4 = [0.90875, -0.05007]
+
+    @pytest.mark.parametrize(
+        'rho, n_groups, df, statistic, pvalue, alternative',
+        [
+            # From Dunnett1955
+            # Tables 1a and 1b pages 1117-1118
+            (0.5, 1, 10, 1.81, 0.05, "greater"),  # different than two-sided
+            (0.5, 3, 10, 2.34, 0.05, "greater"),
+            (0.5, 2, 30, 1.99, 0.05, "greater"),
+            (0.5, 5, 30, 2.33, 0.05, "greater"),
+            (0.5, 4, 12, 3.32, 0.01, "greater"),
+            (0.5, 7, 12, 3.56, 0.01, "greater"),
+            (0.5, 2, 60, 2.64, 0.01, "greater"),
+            (0.5, 4, 60, 2.87, 0.01, "greater"),
+            (0.5, 4, 60, [2.87, 2.21], [0.01, 0.05], "greater"),
+            # Tables 2a and 2b pages 1119-1120
+            (0.5, 1, 10, 2.23, 0.05, "two-sided"),  # two-sided
+            (0.5, 3, 10, 2.81, 0.05, "two-sided"),
+            (0.5, 2, 30, 2.32, 0.05, "two-sided"),
+            (0.5, 3, 20, 2.57, 0.05, "two-sided"),
+            (0.5, 4, 12, 3.76, 0.01, "two-sided"),
+            (0.5, 7, 12, 4.08, 0.01, "two-sided"),
+            (0.5, 2, 60, 2.90, 0.01, "two-sided"),
+            (0.5, 4, 60, 3.14, 0.01, "two-sided"),
+            (0.5, 4, 60, [3.14, 2.55], [0.01, 0.05], "two-sided"),
+        ],
+    )
+    def test_critical_values(
+        self, rho, n_groups, df, statistic, pvalue, alternative
+    ):
+        rng = np.random.default_rng(165250594791731684851746311027739134893)
+        rho = np.full((n_groups, n_groups), rho)
+        np.fill_diagonal(rho, 1)
+
+        statistic = np.array(statistic)
+        res = _pvalue_dunnett(
+            rho=rho, df=df, statistic=statistic,
+            alternative=alternative,
+            rng=rng
+        )
+        assert_allclose(res, pvalue, atol=5e-3)
 
     @pytest.mark.parametrize(
         'samples, control, pvalue, statistic',
@@ -251,35 +275,42 @@ class TestDunnett:
             assert_allclose(res.pvalue, pvalue, atol=1e-7)
 
     @pytest.mark.parametrize(
-        'alternative, ci_low, ci_high',
+        'samples, control, alternative, ci_low, ci_high',
         [
             (
-                'two-sided',
-                [0.7529028025053, -8.2470971974947, -15.2470971974947],
-                [21.2470971974947, 12.2470971974947, 5.2470971974947]
+                samples_1, control_1, 'two-sided',
+                *ci_1_twosided
              ),
             (
-                'less',
-                [2.4023682323149, -6.5976317676851, -13.5976317676851],
+                samples_1, control_1, 'less',
+                ci_1_less,
+                [np.inf, np.inf, np.inf, np.inf, np.inf]
+            ),
+            (
+                samples_1, control_1, 'greater',
+                [-np.inf, -np.inf, -np.inf, -np.inf, -np.inf],
+                ci_1_greater
+            ),
+            (
+                samples_3, control_3, 'two-sided',
+                *ci_3_twosided
+             ),
+            (
+                samples_3, control_3, 'less',
+                ci_3_less,
                 [np.inf, np.inf, np.inf]
             ),
             (
-                'greater',
+                samples_3, control_3, 'greater',
                 [-np.inf, -np.inf, -np.inf],
-                [19.5984402363662, 10.5984402363662, 3.5984402363662]
+                ci_3_greater
             )
         ]
     )
-    def test_confidence_interval(self, alternative, ci_low, ci_high):
-        # Example (a) from Dunnett1955 using R multcomp `glht`
-        # Same as above with confint(test) and the corresponding alternative
+    def test_confidence_interval(
+        self, samples, control, alternative, ci_low, ci_high
+    ):
         rng = np.random.default_rng(189117774084579816190295271136455278291)
-        samples = [
-            [55, 64, 64],
-            [55, 49, 52],
-            [50, 44, 41]
-        ]
-        control = [55, 47, 48]
 
         res = stats.dunnett(
             *samples, control=control, alternative=alternative,
@@ -298,6 +329,7 @@ class TestDunnett:
         ci_ = res.confidence_interval(confidence_level=0.95)
         assert ci_ is ci
 
+    def toto(self):
         # check some str output
         res_str = str(res)
         assert '(Sample 2 - Control)' in res_str
