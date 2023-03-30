bc73228936de4c68f4f21af48bfa7d07951b9103
Pamphile Roy <roy.pamphile@gmail.com>
2023-03-30 08:57:33 +0200

    ENH: stats.dunnett: add `confidence_interval` and `__str__` methods (#18150)
    * ENH: stats.dunnett: add `confidence_interval` and `__str__` methods
    
    Co-authored-by: Matt Haberland <mhaberla@calpoly.edu>

--- scipy/stats/_multicomp.py
+++ scipy/stats/_multicomp.py
@@ -1,17 +1,20 @@
 from __future__ import annotations
 
-from dataclasses import dataclass
+import warnings
+from dataclasses import dataclass, field
 from typing import TYPE_CHECKING
 
 import numpy as np
 
 from scipy import stats
+from scipy.optimize import minimize_scalar
+from scipy.stats._common import ConfidenceInterval
 from scipy.stats._qmc import check_random_state
 from scipy.stats._stats_py import _var
 
 if TYPE_CHECKING:
     import numpy.typing as npt
-    from scipy._lib._util import SeedType
+    from scipy._lib._util import DecimalNumber, SeedType
     from typing import Literal, Sequence  # noqa: UP035
 
 
@@ -24,10 +27,148 @@ __all__ = [
 class DunnettResult:
     statistic: np.ndarray
     pvalue: np.ndarray
+    _alternative: Literal['two-sided', 'less', 'greater'] = field(repr=False)
+    _rho: np.ndarray = field(repr=False)
+    _df: int = field(repr=False)
+    _std: float = field(repr=False)
+    _mean_samples: np.ndarray = field(repr=False)
+    _mean_control: np.ndarray = field(repr=False)
+    _n_samples: np.ndarray = field(repr=False)
+    _n_control: int = field(repr=False)
+    _rng: SeedType = field(repr=False)
+    _ci: ConfidenceInterval | None = field(default=None, repr=False)
+    _ci_cl: DecimalNumber | None = field(default=None, repr=False)
+
+    def __str__(self):
+        # Note: `__str__` prints the confidence intervals from the most
+        # recent call to `confidence_interval`. If it has not been called,
+        # it will be called with the default CL of .95.
+        if self._ci is None:
+            self.confidence_interval(confidence_level=.95)
+        s = (
+            "Dunnett's test"
+            f" ({self._ci_cl*100:.1f}% Confidence Interval)\n"
+            "Comparison               Statistic  p-value  Lower CI  Upper CI\n"
+        )
+        for i in range(self.pvalue.size):
+            s += (f" (Sample {i} - Control) {self.statistic[i]:>10.3f}"
+                  f"{self.pvalue[i]:>10.3f}"
+                  f"{self._ci.low[i]:>10.3f}"
+                  f"{self._ci.high[i]:>10.3f}\n")
+
+        return s
+
+    def _allowance(
+        self, confidence_level: DecimalNumber = 0.95, tol: DecimalNumber = 1e-3
+    ) -> float:
+        """Allowance.
+
+        It is the quantity to add/subtract from the observed difference
+        between the means of observed groups and the mean of the control
+        group. The result gives confidence limits.
+
+        Parameters
+        ----------
+        confidence_level : float, optional
+            Confidence level for the computed confidence interval.
+            Default is .95.
+        tol : float, optional
+            A tolerance for numerical optimization: the allowance will produce
+            a confidence within ``10*tol*(1 - confidence_level)`` of the
+            specified level, or a warning will be emitted. Tight tolerances
+            may be impractical due to noisy evaluation of the objective.
+            Default is 1e-3.
+
+        Returns
+        -------
+        allowance : float
+            Allowance around the mean.
+        """
+        alpha = 1 - confidence_level
+
+        def pvalue_from_stat(statistic):
+            statistic = np.array(statistic)
+            sf = _pvalue_dunnett(
+                rho=self._rho, df=self._df,
+                statistic=statistic, alternative=self._alternative,
+                rng=self._rng
+            )
+            return abs(sf - alpha)/alpha
+
+        # Evaluation of `pvalue_from_stat` is noisy due to the use of RQMC to
+        # evaluate `multivariate_t.cdf`. `minimize_scalar` is not designed
+        # to tolerate a noisy objective function and may fail to find the
+        # minimum accurately. We mitigate this possibility with the validation
+        # step below, but implementation of a noise-tolerant root finder or
+        # minimizer would be a welcome enhancement. See gh-18150.
+        res = minimize_scalar(pvalue_from_stat, method='brent', tol=tol)
+        critical_value = res.x
+
+        # validation
+        # tol*10 because tol=1e-3 means we tolerate a 1% change at most
+        if res.success is False or res.fun >= tol*10:
+            warnings.warn(
+                "Computation of the confidence interval did not converge to "
+                "the desired level. The confidence level corresponding with "
+                f"the returned interval is approximately {alpha*(1+res.fun)}.",
+                stacklevel=3
+            )
+
+        # From [1] p. 1101 between (1) and (3)
+        allowance = critical_value*self._std*np.sqrt(
+            1/self._n_samples + 1/self._n_control
+        )
+        return abs(allowance)
+
+    def confidence_interval(
+        self, confidence_level: DecimalNumber = 0.95
+    ) -> ConfidenceInterval:
+        """Compute the confidence interval for the specified confidence level.
+
+        Parameters
+        ----------
+        confidence_level : float, optional
+            Confidence level for the computed confidence interval.
+            Default is .95.
+
+        Returns
+        -------
+        ci : ``ConfidenceInterval`` object
+            The object has attributes ``low`` and ``high`` that hold the
+            lower and upper bounds of the confidence intervals for each
+            comparison. The high and low values are accessible for each
+            comparison at index ``i`` for each group ``i``.
+
+        """
+        # check to see if the supplied confidence level matches that of the
+        # previously computed CI.
+        if (self._ci is not None) and (confidence_level == self._ci_cl):
+            return self._ci
+
+        if not (0 < confidence_level < 1):
+            raise ValueError("Confidence level must be between 0 and 1.")
+
+        allowance = self._allowance(confidence_level=confidence_level)
+        diff_means = self._mean_samples - self._mean_control
+
+        low = diff_means-allowance
+        high = diff_means+allowance
+
+        if self._alternative == 'greater':
+            high = [np.inf] * len(diff_means)
+        elif self._alternative == 'less':
+            low = [-np.inf] * len(diff_means)
+
+        self._ci_cl = confidence_level
+        self._ci = ConfidenceInterval(
+            low=low,
+            high=high
+        )
+        return self._ci
 
 
 def dunnett(
-    *samples: npt.ArrayLike,
+    *samples: npt.ArrayLike,  # noqa: D417
     control: npt.ArrayLike,
     alternative: Literal['two-sided', 'less', 'greater'] = "two-sided",
     random_state: SeedType = None
@@ -68,7 +209,7 @@ def dunnett(
 
     Returns
     -------
-    res : DunnettResult
+    res : `~scipy.stats._result_classes.DunnettResult`
         An object containing attributes:
 
         statistic : float ndarray
@@ -79,6 +220,12 @@ def dunnett(
             The computed p-value of the test for each comparison. The element
             at index ``i`` is the p-value for the comparison between
             group ``i`` and the control.
+
+        And the following method:
+
+        confidence_interval(confidence_level=0.95) :
+            Compute the difference in means of the groups
+            with the control +- the allowance.
 
     See Also
     --------
@@ -171,9 +318,13 @@ def dunnett(
         alternative=alternative, random_state=random_state
     )
 
-    rho, df, n_group = _params_dunnett(samples=samples_, control=control_)
+    rho, df, n_group, n_samples, n_control = _params_dunnett(
+        samples=samples_, control=control_
+    )
 
-    statistic = _statistic_dunnett(samples_, control_, df)
+    statistic, std, mean_control, mean_samples = _statistic_dunnett(
+        samples_, control_, df, n_samples, n_control
+    )
 
     pvalue = _pvalue_dunnett(
         rho=rho, df=df, statistic=statistic, alternative=alternative, rng=rng
@@ -181,6 +332,13 @@ def dunnett(
 
     return DunnettResult(
         statistic=statistic, pvalue=pvalue,
+        _alternative=alternative,
+        _rho=rho, _df=df, _std=std,
+        _mean_samples=mean_samples,
+        _mean_control=mean_control,
+        _n_samples=n_samples,
+        _n_control=n_control,
+        _rng=rng
     )
 
 
@@ -218,7 +376,7 @@ def _iv_dunnett(
 
 def _params_dunnett(
     samples: list[np.ndarray], control: np.ndarray
-) -> tuple[np.ndarray, int, int]:
+) -> tuple[np.ndarray, int, int, np.ndarray, int]:
     """Specific parameters for Dunnett's test.
 
     Degree of freedom is the number of observations minus the number of groups
@@ -238,18 +396,17 @@ def _params_dunnett(
     rho = 1/np.sqrt(rho[:, None] * rho[None, :])
     np.fill_diagonal(rho, 1)
 
-    return rho, df, n_groups
+    return rho, df, n_groups, n_samples, n_control
 
 
 def _statistic_dunnett(
-    samples: list[np.ndarray], control: np.ndarray, df: int
-) -> np.ndarray:
+    samples: list[np.ndarray], control: np.ndarray, df: int,
+    n_samples: np.ndarray, n_control: int
+) -> tuple[np.ndarray, float, np.ndarray, np.ndarray]:
     """Statistic of Dunnett's test.
 
     Computation based on the original single-step test from [1].
     """
-    n_control = control.size
-    n_samples = np.array([sample.size for sample in samples])
     mean_control = np.mean(control)
     mean_samples = np.array([np.mean(sample) for sample in samples])
     all_samples = [control] + samples
@@ -258,11 +415,12 @@ def _statistic_dunnett(
     # Variance estimate s^2 from [1] Eq. 1
     s2 = np.sum([_var(sample, mean=mean)*sample.size
                  for sample, mean in zip(all_samples, all_means)]) / df
+    std = np.sqrt(s2)
 
     # z score inferred from [1] unlabeled equation after Eq. 1
     z = (mean_samples - mean_control) / np.sqrt(1/n_samples + 1/n_control)
 
-    return z / np.sqrt(s2)
+    return z / std, std, mean_control, mean_samples
 
 
 def _pvalue_dunnett(
@@ -285,4 +443,4 @@ def _pvalue_dunnett(
     else:
         pvalue = 1 - mvt.cdf(np.inf, lower_limit=statistic)
 
-    return pvalue
+    return np.atleast_1d(pvalue)
