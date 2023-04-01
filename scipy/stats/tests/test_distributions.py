03119f876b7871617da17d996ed4611c706696b1
Neil Girdhar <mistersheik@gmail.com>
2023-04-01 05:01:56 -0400

    ENH: stats.geometric.entropy: implement analytical formula

--- scipy/stats/tests/test_distributions.py
+++ scipy/stats/tests/test_distributions.py
@@ -999,6 +999,12 @@ class TestGeom:
         # this should not underflow
         assert_allclose(stats.geom.ppf(1e-20, 1e-20), 1.0, atol=1e-14)
 
+    def test_entropy(self):
+        # Simple tests of entropy.
+        h = stats.geom(np.array([0.0146, 0.2, 0.5, 0.8])).entropy()
+        expected_h = np.array([5.21939796, 2.50201212, 1.38629436, 0.62550303])
+        assert_allclose(h, expected_h)
+
 
 class TestPlanck:
     def setup_method(self):
