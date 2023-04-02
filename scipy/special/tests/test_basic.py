a61dd36298616f06be434ee1ea1b4e5f77aadd43
Lucas Roberts <rlucas7@vt.edu>
2023-04-02 17:36:31 -0400

    fix off by 1 error

--- scipy/special/tests/test_basic.py
+++ scipy/special/tests/test_basic.py
@@ -3708,7 +3708,7 @@ class TestStirling2:
 
     def test_big_numbers(self):
         # via mpmath
-        ans = [48063331393111, 48004081105038305]
+        ans = asarray([48063331393110, 48004081105038305])
         n = [25, 30]
         k = [17, 4]
         assert array_equal(stirling2(n, k), ans)
