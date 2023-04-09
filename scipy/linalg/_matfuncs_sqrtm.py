BUG: linalg: fix sqrtm precision preservation for complex input.

scipy/linalg/_matfuncs_sqrtm.py
@@ -134,9 +134,8 @@ def sqrtm(A, disp=True, blocksize=64):
     sqrtm : (N, N) ndarray
         Value of the sqrt function at `A`. The dtype is float or complex.
         The precision (data size) is determined based on the precision of
-        input `A`. When the dtype is float, the precision is the same as `A`.
-        When the dtype is complex, the precision is double that of `A`. The
-        precision might be clipped by each dtype precision range.
+        input `A`. The precision might be clipped by each dtype precision
+        range.
 
     errest : float
         (if disp == False)
@@ -181,10 +180,12 @@ def sqrtm(A, disp=True, blocksize=64):
         R = _sqrtm_triu(T, blocksize=blocksize)
         ZH = np.conjugate(Z).T
         X = Z.dot(R).dot(ZH)
-        if not np.iscomplexobj(X):
+        if not np.iscomplexobj(X):  # output is real
             # float byte size range: f2 ~ f16
             X = X.astype(f"f{np.clip(byte_size, 2, 16)}", copy=False)
-        else:
+        elif np.iscomplexobj(A):  # input is complex
+            X = X.astype(f"c{np.clip(byte_size, 8, 32)}", copy=False)
+        else: # input is real, output is complex
             # complex byte size range: c8 ~ c32.
             # c32(complex256) might not be supported in some environments.
             if hasattr(np, 'complex256'):