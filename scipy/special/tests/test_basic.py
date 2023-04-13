b7d786bd4fbc6e9a301ffb4be9f80e9928649b1d
H. Vetinari <h.vetinari@gmx.com>
2023-04-13 13:03:39 +1100

    TST: add test for array-likes

--- scipy/special/tests/test_basic.py
+++ scipy/special/tests/test_basic.py
@@ -2052,6 +2052,25 @@ class TestFactorialFunctions:
         assert_allclose(special.factorialk(n, 3, exact=True),
                         np.array(exp[3], ndmin=dim))
 
+    @pytest.mark.parametrize("exact", [True, False])
+    @pytest.mark.parametrize("level", range(1, 5))
+    def test_factorialx_array_like(self, level, exact):
+        def _nest_me(x, k=1):
+            if k == 0:
+                return x
+            else:
+                return _nest_me([x], k-1)
+
+        n = _nest_me([5], k=level-1)  # nested list
+        exp_nucleus = {1: 120, 2: 15, 3: 10}
+        assert_func = assert_array_equal if exact else assert_allclose
+        assert_func(special.factorial(n, exact=exact),
+                    np.array(exp_nucleus[1], ndmin=level))
+        assert_func(special.factorial2(n, exact=exact),
+                    np.array(exp_nucleus[2], ndmin=level))
+        assert_func(special.factorialk(n, 3, exact=True),
+                    np.array(exp_nucleus[3], ndmin=level))
+
     # note that n=170 is the last integer such that factorial(n) fits float64
     @pytest.mark.parametrize('n', range(30, 180, 10))
     def test_factorial_accuracy(self, n):
