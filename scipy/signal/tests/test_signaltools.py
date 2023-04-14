4c497c8314c4a52050c5bfcf71b5d250df87b5f9
Ralf Gommers <ralf.gommers@gmail.com>
2023-04-14 12:44:52 +0200

    CI: remove `setup.py` based jobs from GitHub Actions and run less on merge to main (#18294)

--- scipy/signal/tests/test_signaltools.py
+++ scipy/signal/tests/test_signaltools.py
@@ -1889,6 +1889,8 @@ class TestLinearFilterObject(_TestLinearFilter):
 def test_lfilter_bad_object():
     # lfilter: object arrays with non-numeric objects raise TypeError.
     # Regression test for ticket #1452.
+    if hasattr(sys, 'abiflags') and 'd' in sys.abiflags:
+        pytest.skip('test is flaky when run with python3-dbg')
     assert_raises(TypeError, lfilter, [1.0], [1.0], [1.0, None, 2.0])
     assert_raises(TypeError, lfilter, [1.0], [None], [1.0, 2.0, 3.0])
     assert_raises(TypeError, lfilter, [None], [1.0], [1.0, 2.0, 3.0])
