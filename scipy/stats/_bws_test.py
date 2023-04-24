21776bed2fd655702c9ac0962f07289d028d07ea
zhimin-z <knightyzhao@gmail.com>
2023-04-24 19:00:26 -0400

    add bws test
    [skip ci]

---
+++ scipy/stats/_bws_test.py
@@ -0,0 +1,230 @@
+import numpy as np
+from ._resampling import permutation_test
+from functools import partial
+from collections import namedtuple
+
+
+def _bws_input_validation(x, y, axis, alternative, variant):
+    ''' Input validation and standardization for bws test'''
+    x, y = np.atleast_1d(x), np.atleast_1d(y)
+    if np.isnan(x).any() or np.isnan(y).any():
+        raise ValueError('`x` and `y` must not contain NaNs.')
+    if np.size(x) == 0 or np.size(y) == 0:
+        raise ValueError('`x` and `y` must be of nonzero size.')
+
+    alternatives = {"two-sided", "less", "greater"}
+    alternative = alternative.lower()
+    if alternative not in alternatives:
+        raise ValueError(f'`alternative` must be one of {alternatives}.')
+
+    if axis != int(axis):
+        raise ValueError('`axis` must be an integer.')
+
+    variants = {"shan", "murakami", "naive"}
+    variant = variant.lower()
+    if variant not in variants:
+        raise ValueError(f'`variant` must be one of {variants}.')
+
+    return x, y, axis, alternative, variant
+
+
+def _bws_statistic(x, y, axis, alternative, variant):
+    '''Compute the BWS test statistic for two independent samples'''
+    # for simplicity, always work along the last axis
+    x, y = np.moveaxis(x, axis, -1), np.moveaxis(y, axis, -1)
+    Ri, Hj = np.sort(x, axis=-1), np.sort(y, axis=-1)
+    n, m = Ri.shape[-1], Hj.shape[-1]
+    i, j = np.arange(1, n+1), np.arange(1, m+1)
+
+    if alternative == 'two-sided':
+        if variant == 'naive':
+            Bx_num = (Ri - (m + n) / n * i) ** 2
+            By_num = (Hj - (m + n) / m * j) ** 2
+            Bx_den = i / (n + 1) * (1 - i / (n + 1)) * m * (m + n) / n
+            By_den = j / (m + 1) * (1 - j / (m + 1)) * n * (m + n) / m
+        else:
+            Bx_num = (Ri - (m + n + 1) / (n + 1) * i) ** 2
+            By_num = (Hj - (m + n + 1) / (m + 1) * j) ** 2
+            Bx_den = i / (n + 1) * (1 - i / (n + 1)) * \
+                m * (m + n + 1) / (n + 2)
+            By_den = j / (m + 1) * (1 - j / (m + 1)) * \
+                n * (m + n + 1) / (m + 2)
+
+        Bx = 1 / n * np.sum(Bx_num / Bx_den, axis=-1)
+        By = 1 / m * np.sum(By_num / By_den, axis=-1)
+
+        B = (Bx + By) * 0.5
+    else:
+        if variant == 'naive':
+            Bx_num = (Ri - (m + n) / n * i) * abs(Ri - (m + n) / n * i)
+            By_num = (Hj - (m + n) / m * j) * abs(Hj - (m + n) / m * j)
+            Bx_den = i / (n + 1) * (1 - i / (n + 1)) * m * (m + n) / n
+            By_den = j / (m + 1) * (1 - j / (m + 1)) * n * (m + n) / m
+        else:
+            Bx_num = (Ri - (m + n + 1) / (n + 1) * i) * \
+                abs(Ri - (m + n + 1) / (n + 1) * i)
+            By_num = (Hj - (m + n + 1) / (m + 1) * j) * \
+                abs(Hj - (m + n + 1) / (m + 1) * j)
+            Bx_den = i / (n + 1) * (1 - i / (n + 1)) * \
+                m * (m + n + 1) / (n + 2)
+            By_den = j / (m + 1) * (1 - j / (m + 1)) * \
+                n * (m + n + 1) / (m + 2)
+
+        Bx = 1 / n * np.sum(Bx_num / Bx_den, axis=-1)
+        By = 1 / m * np.sum(By_num / By_den, axis=-1)
+
+        if alternative == 'greater':
+            B = (Bx - By) * 0.5
+        else:
+            B = (By - Bx) * 0.5
+
+    return B
+
+
+BWSResult = namedtuple('BWSResult', ('statistic', 'pvalue'))
+
+
+def bws_test(x, y, axis=0, alternative="two-sided", variant="naive", n_resamples=9999, random_state=None):
+    r'''Perform the Baumgartner-Weiss-Schindler test on two independent samples.
+
+    The Baumgartner-Weiss-Schindler (BWS) test is a nonparametric test of 
+    the null hypothesis that the distribution underlying sample `x` 
+    is the same as the distribution underlying sample `y`. Unlike 
+    the Kolmogorv-Smirnov, Wilcoxon, and Cramer-Von Mises tests, 
+    the BWS test weights the integral by the variance of the difference
+    in CDFs, emphasizing the tails of the distributions, which increases
+    the power of the test for a lot of applications.
+
+    Parameters
+    ----------
+    x, y : array-like
+        N-d arrays of samples. The arrays must be broadcastable except along
+        the dimension given by `axis`.
+    axis : int, optional
+        Axis along which to perform the test. Default is 0.
+    alternative : {'two-sided', 'less', 'greater'}, optional
+        Defines the alternative hypothesis. Default is 'two-sided'.
+        Let *F(u)* and *G(u)* be the cumulative distribution functions of the
+        distributions underlying `x` and `y`, respectively. Then the following
+        alternative hypotheses are available:
+
+        * 'two-sided': the distributions are not equal, i.e. *F(u) ≠ G(u)* for
+          at least one *u*.
+        * 'less': the distribution underlying `x` is stochastically less
+          than the distribution underlying `y`, i.e. *F(u) >= G(u)* for all *u*.
+        * 'greater': the distribution underlying `x` is stochastically greater
+          than the distribution underlying `y`, i.e. *F(u) <= G(u)* for all *u*.
+
+        Under a more restrictive set of assumptions, the alternative hypotheses
+        can be expressed in terms of the locations of the distributions;
+        see [3] section 5.1.
+    variant : {'shan', 'murakami', 'naive'}, optional
+        Selects the method used to calculate the :math:`B` statistic.
+        Default is 'naive'. The following options are available.
+
+        * ``'naive'``: use the naive method in [1]_ to calculate the :math:`B` statistic.
+        * ``'murakami'``: use the variant method in p.5 (two-sided) or p.7 (one-sided) in
+          [2]_ to calculate the :math:`B` statistic.
+        * ``'shan'``: same as ``'murakami'``.
+    n_resamples : int or np.inf, default: 9999
+        Number of random permutations (resamples) used to approximate the null
+        distribution. If greater than or equal to the number of distinct
+        permutations, the exact null distribution will be computed.
+        Note that the number of distinct permutations grows very rapidly with
+        the sizes of samples, so exact tests are feasible only for very small
+        data sets.
+    random_state : {None, int, `numpy.random.Generator`,
+                    `numpy.random.RandomState`}, optional
+
+        Pseudorandom number generator state used to generate permutations.
+
+        If `random_state` is ``None`` (default), the
+        `numpy.random.RandomState` singleton is used.
+        If `random_state` is an int, a new ``RandomState`` instance is used,
+        seeded with `random_state`.
+        If `random_state` is already a ``Generator`` or ``RandomState``
+        instance then that instance is used.
+
+    Returns
+    -------
+    res : BWSResult
+        An object containing attributes:
+
+        statistic : float
+            The BWS statistic corresponding with sample `x` and `y`.
+        pvalue : float
+            The associated *p*-value for the chosen `alternative`.
+    See Also
+    --------
+    scipy.stats.wilcoxon, scipy.stats.mannwhitneyu, scipy.stats.ttest_ind
+
+    References
+    ----------
+    .. [1] Neuhäuser, M. (2005). Exact Tests Based on the Baumgartner-Wei-Schindler 
+           StatisticA Survey. Statistical Papers, 46(1), 1-29.
+    .. [2] Shan, G., Ma, C., Hutson, A. D., & Wilding, G. E. (2013). Some tests 
+           for detecting trends based on the modified Baumgartner–Weiß–Schindler 
+           statistics. Computational Statistics & Data Analysis, 57(1), 246-261.
+    .. [3] Fay, M. P., & Proschan, M. A. (2010). Wilcoxon-Mann-Whitney or t-test? 
+           On assumptions for hypothesis tests and multiple interpretations of 
+           decision rules. Statistics surveys, 4, 1.
+    .. [4] Murakami, H. (2006). A k-sample rank test based on modified Baumgartner
+           statistic and its power comparison. Journal of the Japanese Society of 
+           Computational Statistics, 19(1), 1-13.
+    .. [5] Neuhäuser, M. (2001). One-sided two-sample and trend tests based on a 
+           modified Baumgartner-Weiß-Schindler statistic. Journal of Nonparametric 
+           Statistics, 13(5), 729-739.
+    .. [6] Good, P. (2013). Permutation tests: a practical guide to resampling 
+           methods for testing hypotheses. Springer Science & Business Media.
+
+    Examples
+    --------
+    We follow the example of table 3 in [1]_: Fourteen children were divided randomly into two groups, 
+    an experimental group and a control group. For each child the number of correct identifications 
+    among 18 new examples was observed.
+
+    >>> control_group_ranks = np.array([1, 2, 3, 4, 6, 7, 8])
+    >>> experimental_group_ranks = np.array([5, 9, 10, 11, 12, 13, 14])
+
+    We use the BWS test to assess whether there is a 
+    statistically significant difference in the methods of training preschool children.
+    The null hypothesis is that the distribution of first method is the same as 
+    the distribution of second one. We decide that a confidence level of 95% is 
+    required to reject the null hypothesis in favor of the alternative that 
+    the distributions are different.
+    Since the number of samples is very small and there are no ties in the
+    data, we can compare the observed test statistic against the *exact*
+    distribution of the test statistic under the null hypothesis.
+
+    >>> from scipy.stats import bws_test
+    >>> B, p = bws_test(control_group_ranks, experimental_group_ranks)
+    >>> print(B)
+    5.132167152575315
+
+    This agrees with :math:`B = 5.132` reported in [1]_. The *p*-value produced
+    by `bws_test` agrees with :math:`p = 0.0029` reported in [1]_.
+
+    >>> print(p)
+    0.002913752913752914
+
+    It normally takes a long time to calculate the exact *p*-value when the passing arrays
+    are large, so we can approximate *p*-value using a resampling permutation test.
+    Here we use `n_resamples=10000` to perform a random permutation test with 10000 samples.
+    Note that *p*-value varies depending on the number of samples.
+
+    >>> B, p = bws_test(control_group_ranks, experimental_group_ranks, n_resamples=10000)
+    >>> print(p)
+    0.003275267196716418
+
+    Under this assumption, the *p*-value would be low enough to reject the
+    null hypothesis in favor of the alternative.
+    '''
+
+    x, y, axis, alternative, variant = _bws_input_validation(
+        x, y, axis, alternative, variant)
+    bws_statistic = partial(_bws_statistic, axis=axis,
+                            alternative=alternative, variant=variant)
+    res = permutation_test((x, y), bws_statistic, alternative='greater',
+                           n_resamples=n_resamples, random_state=random_state)
+
+    return BWSResult(res.statistic, res.pvalue)
