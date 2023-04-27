e7218b645602e9e9b0230c11ec7501ff162c38f4
sagi-ezri <sagi.ezri@gmail.com>
2023-04-27 12:00:21 +0300

    revert black

--- scipy/signal/_fir_filter_design.py
+++ scipy/signal/_fir_filter_design.py
@@ -11,17 +11,7 @@ from scipy.linalg import toeplitz, hankel, solve, LinAlgError, LinAlgWarning, ls
 
 from . import _sigtools
 
-__all__ = [
-    "kaiser_beta",
-    "kaiser_atten",
-    "kaiserord",
-    "firwin",
-    "firwin2",
-    "remez",
-    "firls",
-    "minimum_phase",
-    "fwind1",
-]
+__all__ = ["kaiser_beta", "kaiser_atten", "kaiserord", "firwin", "firwin2", "remez", "firls", "minimum_phase", "fwind1"]
 
 
 def _get_fs(fs, nyq):
