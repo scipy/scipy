2c25c29ff91ecc3eb208af016d4566393d9bc1aa
Jake Bowhay <jb9.bowhay@gmail.com>
2023-05-22 12:58:03 +0100

    BUG: guard against non-finite kd-tree queries

--- scipy/spatial/tests/test_kdtree.py
+++ scipy/spatial/tests/test_kdtree.py
@@ -1468,10 +1468,17 @@ def test_kdtree_nan():
         KDTree(data)
 
 
-def test_gh_18223():
+def test_nonfintite_inputs_gh_18223():
     rng = np.random.default_rng(12345)
     coords = rng.uniform(size=(100, 3), low=0.0, high=0.1)
-    KDTree(coords, balanced_tree=False, compact_nodes=False)
+    t = KDTree(coords, balanced_tree=False, compact_nodes=False)
+    bad_coord = [np.nan for _ in range(3)]
+
+    with pytest.raises(ValueError, match="must be finite"):
+        t.query(bad_coord)
+    with pytest.raises(ValueError, match="must be finite"):
+        t.query_ball_point(bad_coord, 1)
+
     coords[0, :] = np.nan
     with pytest.raises(ValueError, match="must be finite"):
         KDTree(coords, balanced_tree=True, compact_nodes=False)
