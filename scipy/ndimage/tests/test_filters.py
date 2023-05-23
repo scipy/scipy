2ec9a1f99e4e57df86e593b0021c7951260a92ef
Gregory Lee <grlee77@gmail.com>
2023-05-23 18:16:40 -0400

    expand test_filter_axes_kwargs to also work with rank filters

--- scipy/ndimage/tests/test_filters.py
+++ scipy/ndimage/tests/test_filters.py
@@ -744,12 +744,16 @@ class TestNdimageFilters:
                         mode=['reflect', 'nearest', 'constant'])
     kwargs_other = dict(origin=(-1, 0, 1),
                         mode=['reflect', 'nearest', 'constant'])
+    kwargs_rank = dict(origin=(-1, 0, 1))
 
     @pytest.mark.parametrize("filter_func, size0, size, kwargs",
                              [(ndimage.gaussian_filter, 0, 1.0, kwargs_gauss),
                               (ndimage.uniform_filter, 1, 3, kwargs_other),
                               (ndimage.maximum_filter, 1, 3, kwargs_other),
-                              (ndimage.minimum_filter, 1, 3, kwargs_other)])
+                              (ndimage.minimum_filter, 1, 3, kwargs_other),
+                              (ndimage.median_filter, 1, 3, kwargs_rank),
+                              (ndimage.rank_filter, 1, 3, kwargs_rank),
+                              (ndimage.percentile_filter, 1, 3, kwargs_rank)])
     @pytest.mark.parametrize('axes', itertools.combinations(range(-3, 3), 2))
     def test_filter_axes_kwargs(self, filter_func, size0, size, kwargs, axes):
         array = numpy.arange(6 * 8 * 12, dtype=numpy.float64).reshape(6, 8, 12)
@@ -758,20 +762,34 @@ class TestNdimageFilters:
         axes = numpy.array(axes)
         n_axes = axes.size
 
+        if filter_func == ndimage.rank_filter:
+            args = (2,)  # (rank,)
+        elif filter_func == ndimage.percentile_filter:
+            args = (30,)  # (percentile,)
+        else:
+            args = ()
+
         # form kwargs that specify only the axes in `axes`
         reduced_kwargs = {key: val[axes] for key, val in kwargs.items()}
         if len(set(axes % array.ndim)) != len(axes):
             # parametrized cases with duplicate axes raise an error
             with pytest.raises(ValueError, match="axes must be unique"):
-                filter_func(array, [size]*n_axes, axes=axes, **reduced_kwargs)
+                filter_func(array, *args, [size]*n_axes, axes=axes,
+                            **reduced_kwargs)
             return
 
-        output = filter_func(array, [size]*n_axes, axes=axes, **reduced_kwargs)
+        output = filter_func(array, *args, [size]*n_axes, axes=axes,
+                             **reduced_kwargs)
 
         # result should be equivalent to sigma=0.0/size=1 on unfiltered axes
         size_3d = numpy.full(array.ndim, fill_value=size0)
         size_3d[axes] = size
-        expected = filter_func(array, size_3d, **kwargs)
+        if 'origin' in kwargs:
+            # origin should be zero on the axis that has size 0
+            origin = numpy.array([0, 0, 0])
+            origin[axes] = reduced_kwargs['origin']
+            kwargs['origin'] = origin
+        expected = filter_func(array, *args, size_3d, **kwargs)
         assert_allclose(output, expected)
 
     @pytest.mark.parametrize(
