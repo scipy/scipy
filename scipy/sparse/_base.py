b5496a0afbf908532483f454f396d43b63b363ee
Isaac Virshup <ivirshup@gmail.com>
2023-05-23 15:49:33 -0700

    Docs for _get_index_dtype

--- scipy/sparse/_base.py
+++ scipy/sparse/_base.py
@@ -1267,6 +1267,22 @@ class _sparray:
             return np.zeros(self.shape, dtype=self.dtype, order=order)
 
     def _get_index_dtype(self, arrays=(), maxval=None, check_contents=False):
+        """
+        Determine index dtype for array.
+
+        This wraps _sputils.get_index_dtype, providing compatibility for both
+        array and matrix API sparse matrices. Matrix API sparse matrices would
+        attempt to downcast the indices – which can be computationally
+        expensive and undesirable for users. The array API changes this
+        behaviour.
+
+        See discussion: https://github.com/scipy/scipy/issues/16774
+
+        The get_index_dtype import is due to implementation details of the test
+        suite. It allows the decorator ``with_64bit_maxval_limit`` to mock a
+        lower int32 max value for checks on the matrix API's downcasting
+        behaviour.
+        """
         from ._sputils import get_index_dtype
 
         # Don't check contents for array API
