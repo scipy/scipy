62cabc764eb22b64fd46bfc6b8ba80fa2f1279ab
ljwolf <levi.john.wolf@gmail.com>
2023-05-24 15:22:03 -0700

    try stable sort in mst tree ordering

--- scipy/sparse/csgraph/_min_spanning_tree.pyx
+++ scipy/sparse/csgraph/_min_spanning_tree.pyx
@@ -101,7 +101,7 @@ def minimum_spanning_tree(csgraph, overwrite=False):
     rank = np.zeros(N, dtype=ITYPE)
     predecessors = np.arange(N, dtype=ITYPE)
 
-    i_sort = np.argsort(data).astype(ITYPE)
+    i_sort = np.argsort(data, kind='stable').astype(ITYPE)
     row_indices = np.zeros(len(data), dtype=ITYPE)
 
     _min_spanning_tree(data, indices, indptr, i_sort,
