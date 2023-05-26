a03da9bb56950db4fe6a0c85cdf9863b01e24730
Dan Schult <dschult@colgate.edu>
2023-05-26 16:47:03 -0700

    Remove old mmio code that is impossible to get to

--- scipy/io/_mmio.py
+++ scipy/io/_mmio.py
@@ -652,11 +652,6 @@ class MMFile:
                                                     self.entries, self.format,
                                                     self.field, self.symmetry)
 
-        try:
-            from scipy.sparse import coo_matrix
-        except ImportError:
-            coo_matrix = None
-
         dtype = self.DTYPES_BY_FIELD.get(field, None)
 
         has_symmetry = self.has_symmetry
@@ -715,39 +710,6 @@ class MMFile:
             else:
                 if not (i in [0, j] and j == cols):
                     raise ValueError("Parse error, did not read all lines.")
-
-        elif format == self.FORMAT_COORDINATE and coo_matrix is None:
-            # Read sparse matrix to dense when coo_matrix is not available.
-            a = zeros((rows, cols), dtype=dtype)
-            line = 1
-            k = 0
-            while line:
-                line = stream.readline()
-                # line.startswith('%')
-                if not line or line[0] in ['%', 37] or not line.strip():
-                    continue
-                l = line.split()
-                i, j = map(int, l[:2])
-                i, j = i-1, j-1
-                if is_integer:
-                    aij = int(l[2])
-                elif is_unsigned_integer:
-                    aij = int(l[2])
-                elif is_complex:
-                    aij = complex(*map(float, l[2:]))
-                else:
-                    aij = float(l[2])
-                a[i, j] = aij
-                if has_symmetry and i != j:
-                    if is_skew:
-                        a[j, i] = -aij
-                    elif is_herm:
-                        a[j, i] = conj(aij)
-                    else:
-                        a[j, i] = aij
-                k = k + 1
-            if not k == entries:
-                ValueError("Did not read all entries")
 
         elif format == self.FORMAT_COORDINATE:
             # Read sparse COOrdinate format
