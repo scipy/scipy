df477e67532ed538956aa1ba42752ca7c59ce939
Tyler Reddy <tyler.je.reddy@gmail.com>
2023-05-30 15:18:48 -0600

    MAINT: version bounds for 1.11.0rc1
    * the version bounds are easy to mess up--the reference conversation
    from the last release is linked below:
    https://github.com/scipy/scipy/pull/17569
    
    * the adjustment of version strings to include i.e., `rc1` is normally
    left to the `REL` commit proper during the release process

--- setup.py
+++ setup.py
@@ -450,9 +450,9 @@ def setup_package():
     #            in N+1 will turn into errors in N+3
     # For Python versions, if releases is (e.g.) <=3.9.x, set bound to 3.10
     np_minversion = '1.21.6'
-    np_maxversion = '9.9.99'
+    np_maxversion = '1.28.0'
     python_minversion = '3.9'
-    python_maxversion = '3.11'
+    python_maxversion = '3.12'
     if IS_RELEASE_BRANCH:
         req_np = 'numpy>={},<{}'.format(np_minversion, np_maxversion)
         req_py = '>={},<{}'.format(python_minversion, python_maxversion)
