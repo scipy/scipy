70c1d729dd36c8ef7c2739d9effd842dbdfb34bd
Matt Haberland <mhaberla@calpoly.edu>
2023-06-01 12:33:58 -0700

    DEP: stats.iqr: remove deprecated scale='raw'

--- scipy/stats/tests/test_stats.py
+++ scipy/stats/tests/test_stats.py
@@ -3024,12 +3024,6 @@ class TestIQR:
         # Bad scale
         assert_raises(ValueError, stats.iqr, x, scale='foobar')
 
-        with pytest.warns(
-            DeprecationWarning,
-            match="The use of 'scale=\"raw\"'"
-        ):
-            stats.iqr([1], scale='raw')
-
 
 class TestMoments:
     """
