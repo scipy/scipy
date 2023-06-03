df4359a9fed656f4fccb9a170711244a588769c9
Jake Bowhay <jb9.bowhay@gmail.com>
2023-06-03 14:41:35 +0100

    DEP: stats: update deprecation of stats.kendalltau kwarg initial_lexsort

--- scipy/stats/_stats_py.py
+++ scipy/stats/_stats_py.py
@@ -5583,7 +5583,10 @@ def pointbiserialr(x, y):
     return res
 
 
-def kendalltau(x, y, initial_lexsort=None, nan_policy='propagate',
+_NoValue = object()
+
+
+def kendalltau(x, y, initial_lexsort=_NoValue, nan_policy='propagate',
                method='auto', variant='b', alternative='two-sided'):
     r"""Calculate Kendall's tau, a correlation measure for ordinal data.
 
@@ -5606,7 +5609,7 @@ def kendalltau(x, y, initial_lexsort=None, nan_policy='propagate',
 
         .. deprecated:: 1.10.0
            `kendalltau` keyword argument `initial_lexsort` is deprecated as it
-           is unused and will be removed in SciPy 1.12.0.
+           is unused and will be removed in SciPy 1.14.0.
     nan_policy : {'propagate', 'raise', 'omit'}, optional
         Defines how to handle when input contains nan.
         The following options are available (default is 'propagate'):
@@ -5821,7 +5824,7 @@ def kendalltau(x, y, initial_lexsort=None, nan_policy='propagate',
     accurate results.
 
     """
-    if initial_lexsort is not None:
+    if initial_lexsort is not _NoValue:
         msg = ("'kendalltau' keyword argument 'initial_lexsort' is deprecated"
                " as it is unused and will be removed in SciPy 1.12.0.")
         warnings.warn(msg, DeprecationWarning, stacklevel=2)
