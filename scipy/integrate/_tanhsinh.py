b62a592b34adc4ffe370dbb12b32f0d80dae03bd
Matt Haberland <mhaberla@calpoly.edu>
2023-06-04 19:54:17 -0700

    DOC: integrate._tanhsinh: add documentation

--- scipy/integrate/_tanhsinh.py
+++ scipy/integrate/_tanhsinh.py
@@ -2,12 +2,19 @@ from dataclasses import dataclass
 import numpy as np
 
 # todo:
+#  remove maxiter?
+#  make public?
+#  support singularities? interval subdivision? this feature will be added
+#    eventually, but do we adjust the interface now?
+#  test function evaluation count
 #  documentation
 #  cache abscissa/weight pairs
 #  tests - test rtol, maxfun, and minweight
 #  respect data types
 #  callback
 #  log integration
+#  apply np.vectorize as needed?
+#  accept args, kwargs?
 #  warn (somehow) when invalid function values & weight < minweight
 #  vectorize
 
@@ -74,6 +81,140 @@ def _compute_pairs(k):
 
 def _tanhsinh(f, a, b, *, maxfun=5000, maxiter=10, atol=0, rtol=1e-14,
               minweight=1e-100):
+    """Evaluate a convergent integral numerically using tanh-sinh quadrature.
+
+    In practice, tanh-sinh quadrature achieves quadratic convergence for
+    many integrands: the number of accurate *digits* scales roughly linearly
+    with the number of function evaluations [1]_.
+
+    Either or both of the limits of integration may be infinite, and
+    singularities at the endpoints are acceptable. Divergent integrals and
+    integrands with non-finite derivatives or singularities within an interval
+    are out of scope, but may be evaluated be calling `_tanhsinh` on each
+    sub-interval separately.
+
+    Parameters
+    ----------
+    f : callable
+        The function to be integrated. `f` must accept a single argument,
+        the value at which it is to be evaluated. `f` must accept an array
+        as input and evaluate the integrand elementwise.
+    a, b : float
+        Lower and upper limits of integration.
+    maxfun, maxiter: int, default=5000
+        The maximum acceptable number of function evaluations (default: 5000)
+        and iterations (default: 10), respectively. Note that the number of
+        function evaluations is counted as the number of elements at which `f`
+        is evaluated; not the number of calls of `f`.
+        In the first iteration, `f` is called once (twice for doubly-infinite
+        integrals), performing 14 (28) function evaluations. The number of
+        function in each subsequent iteration is approximately double the
+        number in the previous iteration.
+        The function will terminate with nonzero exit status before *either* of
+        these limits is reached. For an increase of one of these parameters to
+        have an effect, *both* of these values must be increased.
+    atol, rtol : float
+        Absolute termination tolerance (default: 0) and relative termination
+        tolerance (default: 1e-15), respectively. The error estimate is as
+        described in [1]_ Section 5; while not theoretically rigorous or
+        conservative, it is said to work well in practice.
+    minweight : float
+        The minimum nonzero weight to be used in the Euler-Maclaurin Summation
+        formula as described in [1]_ Section 4. When evaluating an integral
+        with a singularity at an endpoint, values of the integrand will not be
+        considered if the weight prescribed by the tanh-sinh quadrature scheme
+        falls below this threshold. The default value is 1e-100. For integrals
+        without endpoint singularities, smaller values are acceptable. For
+        integrals with an endpoint singularity, a larger value will protect
+        against overflows of the integrand, but potentially at the expense of
+        integral and error estimate accuracy.
+
+    Returns
+    -------
+    res : QuadratureResult
+        An object with the following attributes.
+
+        integral : float
+            An estimate of the integral
+        error : float
+            An estimate of the error.
+        feval : int
+            The number of function evaluations, i.e., the number of
+            points at which the integrand was evaluated.
+        success : bool
+            Whether the algorithm terminated successfully with an error
+            estimate satisfying the specified `atol` or `rtol`.
+        status : int
+            A numerical code indicating the status of the algorithm at
+            termination. ``0`` indicates successful termination. ``1`` and
+            ``2`` indicate termination due to function or iteration limit
+            (respectively), and ``3`` indicates that an invalid value
+            was encountered within the integration interval.
+        message : str
+            A description of the termination status.
+
+    See Also
+    --------
+    quad, quadrature
+
+    Notes
+    -----
+    Implements the algorithm as described in [1]_ with minor adaptations for
+    fixed-precision arithmetic. The tanh-sinh scheme was originally introduced
+    in [2]_.
+
+    Due to floating-point error in the abscissae, the function may be evaluated
+    at the endpoints of the interval, but the value returned will be ignored.
+
+    If the function returns an invalid value (e.g. infinity, NaN) within the
+    integral and the corresponding weight is greater than `minweight`,
+    `_tanhsinh` will terminate with exit status ``3``. If this is caused by
+    an interior singularity, break the interval into sub-intervals with
+    the singularity at an endpoint, and call `_tanhsinh` on each.
+
+    References
+    ----------
+    [1] Bailey, David H., Karthik Jeyabalan, and Xiaoye S. Li. "A comparison of
+        three high-precision quadrature schemes." Experimental Mathematics 14.3
+        (2005): 317-329.
+    [2] Takahasi, Hidetosi, and Masatake Mori. "Double exponential formulas for
+        numerical integration." Publications of the Research Institute for
+        Mathematical Sciences 9.3 (1974): 721-741.
+
+    Example
+    -------
+    Evaluate the Gaussian integral:
+
+    >>> import numpy as np
+    >>> from scipy import integrate
+    >>> def f(x):
+    ...     return np.exp(-x**2)
+    >>> res = _tanhsinh(f, -np.inf, np.inf)
+    >>> res.integral  # true value is np.sqrt(np.pi), 1.7724538509055159
+    1.772453850905516
+    >>> res.error  # actual error is ~2.220446049250313e-16
+    1.062283726062058e-15
+
+    The value of the Gaussian function (bell curve) is nearly zero for
+    arguments sufficiently far from zero, so the value of the integral
+    over a finite interval is nearly the same.
+
+    >>> _tanhsinh(f, -20, 20).integral
+    1.7724538509055152
+
+    However, with unfavorable integration limits, the integration scheme
+    may not be able to find the important region.
+
+    >>> _tanhsinh(f, -np.inf, 20).integral
+    1.967881377548732e-19
+
+    In such cases, or when there are singularities within the interval,
+    break the integral into parts with endpoints at the important points.
+
+    >>> _tanhsinh(f, -np.inf, 0).integral + _tanhsinh(f, 0, 20).integral
+    1.7724538509055163
+
+    """
 
     # Input validation and standardization
     res = _quadts_iv(f, a, b, maxfun, maxiter, atol, rtol, minweight)
