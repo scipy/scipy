e1e2bd96133941ccff8a25d663c4edfbfba309db
Adam Lugowski <alugowski@gmail.com>
2023-06-05 23:34:06 -0700

    Use Pybind11Extension in setup.py

--- scipy/io/_fast_matrix_market/setup.py
+++ scipy/io/_fast_matrix_market/setup.py
@@ -1,5 +1,10 @@
+import os
+
+import pybind11.setup_helpers
+from pybind11.setup_helpers import Pybind11Extension
+
+
 def configuration(parent_package='', top_path=None):
-    import sys
     from numpy.distutils.misc_util import Configuration
     config = Configuration('_fast_matrix_market', parent_package, top_path)
 
@@ -7,29 +12,32 @@ def configuration(parent_package='', top_path=None):
         ('FMM_FROM_CHARS_INT_SUPPORTED', 1),
         ('FMM_TO_CHARS_INT_SUPPORTED', 1)
     ]
-    extra_compile_args = []
 
-    if sys.platform.startswith('win'):
-        # MSVC
-        extra_compile_args.append('/std:c++17')
-
+    if pybind11.setup_helpers.WIN:
         # MSVC has complete <charconv> support so dependencies can be skipped.
         define_macros.append(('FMM_FROM_CHARS_DOUBLE_SUPPORTED', 1))
         define_macros.append(('FMM_FROM_CHARS_LONG_DOUBLE_SUPPORTED', 1))
         define_macros.append(('FMM_TO_CHARS_DOUBLE_SUPPORTED', 1))
         define_macros.append(('FMM_TO_CHARS_LONG_DOUBLE_SUPPORTED', 1))
-    else:
-        # GCC/Clang
-        extra_compile_args.append('-std=c++17')
-        # Will use fallbacks as floating-point <charconv> methods are not available.
+        # GCC/Clang will use fallbacks as floating-point <charconv> methods are not available.
         # Performance will suffer. Use the meson build instead.
 
-    config.add_extension('_core',
-                         sources=['src/_core.cpp'],
-                         include_dirs=['fast_matrix_market/include'],
-                         define_macros=define_macros,
-                         extra_compile_args=extra_compile_args,
-                         )
+    ext = Pybind11Extension('_core',
+                            sources=[os.path.join(config.package_path, "src", s) for s in (
+                                '_core.cpp',
+                                '_core_read_array.cpp',
+                                '_core_read_coo.cpp',
+                                '_core_write_array.cpp',
+                                '_core_write_coo.cpp',
+                                '_core_write_csc.cpp',
+                            )],
+                            language='c++',
+                            cxx_std=17,
+                            include_dirs=[os.path.join(config.package_path, 'fast_matrix_market', 'include')],
+                            define_macros=define_macros,
+                            )
+
+    config.ext_modules.append(ext)
 
     return config
 
