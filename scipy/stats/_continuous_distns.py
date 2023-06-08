469d39d1c213ac559c5208ba17ac4050907e998a
Omar Salman <omar.salman@arbisoft.com>
2023-06-08 14:19:37 +0500

    Fix: unintended change

--- scipy/stats/_continuous_distns.py
+++ scipy/stats/_continuous_distns.py
@@ -841,7 +841,7 @@ class beta_gen(rv_continuous):
             log_term = 0.5 * (
                 np.log(2*np.pi) + np.log(a) + np.log(b) - 3*np.log(sum_ab) + 1
             )
-            t1 = 100/sum_ab + 20*sum_ab**-2.0 + sum_ab**-3.0 - 2*sum_ab**-4.0
+            t1 = 110/sum_ab + 20*sum_ab**-2.0 + sum_ab**-3.0 - 2*sum_ab**-4.0
             t2 = -50/a - 10*a**-2.0 - a**-3.0 + a**-4.0
             t3 = -50/b - 10*b**-2.0 - b**-3.0 + b**-4.0
             return log_term + (t1 + t2 + t3) / 120
