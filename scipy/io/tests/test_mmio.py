Fix Overflow expectation

scipy/io/tests/test_mmio.py
@@ -63,7 +63,7 @@ class TestMMIOArray:
 
     def test_64bit_integer(self):
         a = array([[2**31, 2**32], [2**63-2, 2**63-1]], dtype=np.int64)
-        if (np.intp(0).itemsize < 8):
+        if (np.intp(0).itemsize < 8) and mmwrite == scipy.io._mmio.mmwrite:
             assert_raises(OverflowError, mmwrite, self.fn, a)
         else:
             self.check_exact(a, (2, 2, 4, 'array', 'integer', 'general'))
@@ -189,7 +189,7 @@ class TestMMIOSparseCSR(TestMMIOArray):
         a = scipy.sparse.csr_matrix(array([[2**32+1, 2**32+1],
                                            [-2**63+2, 2**63-2]],
                                           dtype=np.int64))
-        if (np.intp(0).itemsize < 8):
+        if (np.intp(0).itemsize < 8) and mmwrite == scipy.io._mmio.mmwrite:
             assert_raises(OverflowError, mmwrite, self.fn, a)
         else:
             self.check_exact(a, (2, 2, 4, 'coordinate', 'integer', 'general'))
@@ -360,7 +360,7 @@ class TestMMIOReadLargeIntegers:
         with open(self.fn, 'w') as f:
             f.write(example)
         assert_equal(mminfo(self.fn), info)
-        if (over32 and (np.intp(0).itemsize < 8)) or over64:
+        if (over32 and (np.intp(0).itemsize < 8) and mmwrite == scipy.io._mmio.mmwrite) or over64:
             assert_raises(OverflowError, mmread, self.fn)
         else:
             b = mmread(self.fn)