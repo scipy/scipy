834b257c0947c590d81e05afccffff4ffd10372f
Feras A. Saad <fsaad@cmu.edu>
2023-06-22 17:59:30 -0400

    Add note connecting weibull_min to standard exponential.

--- scipy/stats/_continuous_distns.py
+++ scipy/stats/_continuous_distns.py
@@ -2438,6 +2438,9 @@ class weibull_min_gen(rv_continuous):
     :math:`c=2` where Weibull distribution reduces to the `expon` and
     `rayleigh` distributions respectively.
 
+    Suppose ``X`` is a standard exponentially distributed random variable.
+    Then ``Y = X**k`` is `weibull_min` distributed with shape ``c = 1/k``.
+
     %(after_notes)s
 
     References
