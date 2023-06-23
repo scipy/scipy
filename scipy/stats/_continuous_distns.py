4cdf1b2240993597858a10fa2a9c563c2be92296
Feras Saad <fsaad@cmu.edu>
2023-06-23 16:41:28 -0400

    DOC: stats.weibull_min: note connection to exponential distribution (#18729)
    * DOC: stats.weibull_min: note connection to exponential distribution

--- scipy/stats/_continuous_distns.py
+++ scipy/stats/_continuous_distns.py
@@ -2438,6 +2438,10 @@ class weibull_min_gen(rv_continuous):
     :math:`c=2` where Weibull distribution reduces to the `expon` and
     `rayleigh` distributions respectively.
 
+    Suppose ``X`` is an exponentially distributed random variable with
+    scale ``s``. Then ``Y = X**k`` is `weibull_min` distributed with shape
+    ``c = 1/k`` and scale ``s**k``.
+
     %(after_notes)s
 
     References
