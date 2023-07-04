BUG: make L-BFGS-B work with single precision gradient

scipy/optimize/_lbfgsb_py.py
@@ -183,6 +183,9 @@ def fmin_l_bfgs_b(func, x0, fprime=None, args=(),
         fun = func
         jac = fprime
 
+    # Fortran expects a double precision array for gradient
+    jac=jac.astype(np.float64)
+
     # build options
     callback = _wrap_callback(callback)
     opts = {'disp': disp,
@@ -196,8 +199,8 @@ def fmin_l_bfgs_b(func, x0, fprime=None, args=(),
             'callback': callback,
             'maxls': maxls}
 
-    res = _minimize_lbfgsb(fun, x0, args=args, jac=jac, bounds=bounds,
-                           **opts)
+    res = _minimize_lbfgsb(fun, x0, args=args, jac=jac.astype(np.float64),
+                           bounds=bounds, **opts)
     d = {'grad': res['jac'],
          'task': res['message'],
          'funcalls': res['nfev'],
@@ -348,6 +351,8 @@ def _minimize_lbfgsb(fun, x0, args=(), jac=None, bounds=None,
     n_iterations = 0
 
     while 1:
+        # convert gradient to double precision for Fortran
+        g = g.astype(np.float64)
         # x, f, g, wa, iwa, task, csave, lsave, isave, dsave = \
         _lbfgsb.setulb(m, x, low_bnd, upper_bnd, nbd, f, g, factr,
                        pgtol, wa, iwa, task, iprint, csave, lsave,

scipy/optimize/tests/test_lbfgsb_setulb.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.optimize import _lbfgsb
+from scipy.optimize import _lbfgsb, minimize
 
 
 def objfun(x):
@@ -114,3 +114,11 @@ def test_setulb_floatround():
 
         assert (x <= upper_bnd).all() and (x >= low_bnd).all(), (
             "_lbfgsb.setulb() stepped to a point outside of the bounds")
+
+
+def test_gh_issue18730():
+    def fun(x):
+        return np.sum(x**2), (2*x).astype(np.float32)
+
+    res = minimize(fun, x0=np.array([1.]), jac=True, method="l-bfgs-b")
+    np.testing.assert_allclose(res.x, 0., atol=1e-15)