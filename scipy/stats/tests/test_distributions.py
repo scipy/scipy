ef0545ea527c230c1bf9e9deb3dfa5247f5fc570
Matt Haberland <mhaberla@calpoly.edu>
2023-07-04 13:46:00 -0700

    TST: stats.rv_continuous.sf/isf: test the other tail, too

--- scipy/stats/tests/test_distributions.py
+++ scipy/stats/tests/test_distributions.py
@@ -9334,19 +9334,34 @@ class TestTruncPareto:
             _assert_less_or_close_loglike(stats.truncpareto, data, **kwds)
 
 
-@pytest.mark.parametrize("case", [("loglaplace", None, None, None),
-                                  ("lognorm", None, None, None),
-                                  ("pareto", None, None, None),])
+# Cases are (distribution name, log10 of smallest probability mass to test,
+# log10 of the complement of the largest probability mass to test, atol,
+# rtol). None uses default values.
+@pytest.mark.parametrize("case", [("loglaplace", None, None, None, None),
+                                  ("lognorm", None, None, None, None),
+                                  ("pareto", None, None, None, None),])
 def test_sf_isf_overrides(case):
-    # A stronger test that supplements `test_continuous_basic.check_sf_isf`
-    # for distributions with overridden `sf` and `isf` methods.
-    distname, domain, atol, rtol = case
-    domain = domain or (-290, 0)
+    # Test that SF is the inverse of ISF. Supplements
+    # `test_continuous_basic.check_sf_isf` for distributions with overridden
+    # `sf` and `isf` methods.
+    distname, lp1, lp2, atol, rtol = case
+
+    lpm = np.log10(0.5)  # log10 of the probability mass at the median
+    lp1 = lp1 or -290
+    lp2 = lp2 or -14
     atol = atol or 0
     rtol = rtol or 1e-12
     dist = getattr(stats, distname)
     params = dict(distcont)[distname]
     dist_frozen = dist(*params)
-    ref = np.logspace(*domain)
+
+    # Test (very deep) right tail to median. We can benchmark with random
+    # (loguniform) points, but strictly logspaced points are fine for tests.
+    ref = np.logspace(lp1, lpm)
+    res = dist_frozen.sf(dist_frozen.isf(ref))
+    assert_allclose(res, ref, atol=atol, rtol=rtol)
+
+    # test median to left tail
+    ref = 1 - np.logspace(lp2, lpm, 20)
     res = dist_frozen.sf(dist_frozen.isf(ref))
     assert_allclose(res, ref, atol=atol, rtol=rtol)
