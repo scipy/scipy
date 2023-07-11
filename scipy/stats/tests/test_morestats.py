bcfe91226dc731d0ce1736fb55ad0a851f35de9f
Laurent Sorber <laurent.sorber@gmail.com>
2023-07-11 07:36:04 +0000

    TST: parametrize stats.yeojohnson overflow tests

--- scipy/stats/tests/test_morestats.py
+++ scipy/stats/tests/test_morestats.py
@@ -2230,26 +2230,15 @@ class TestYeojohnson:
         assert_allclose(xt_yeo, xt_box, rtol=1e-6)
         assert_allclose(lam_yeo, lam_box, rtol=1e-6)
     
-    def test_overflow_expr(self):
+    @pytest.mark.parametrize('x', [
+        # Attempt to trigger overflow in power expressions.
+        np.array([2003.0, 1950.0, 1997.0, 2000.0, 2009.0,
+                  2009.0, 1980.0, 1999.0, 2007.0, 1991.0]),
+        # Attempt to trigger overflow with a large optimal lambda.
+        np.array([2003.0, 1950.0, 1997.0, 2000.0, 2009.0])
+    ])
+    def test_overflow(self, x):
         # non-regression test for gh-18389
-        x = np.array([2003.0, 1950.0, 1997.0, 2000.0, 2009.0,
-                      2009.0, 1980.0, 1999.0, 2007.0, 1991.0])
-        
-        def optimizer(fun):
-            out = optimize.fminbound(fun, -lam_yeo, lam_yeo, xtol=1.48e-08)
-            result = optimize.OptimizeResult()
-            result.x = out
-            return result
-
-        with np.errstate(over="raise"):
-            xt_yeo, lam_yeo = stats.yeojohnson(x)
-            xt_box, lam_box = stats.boxcox(x + 1, optimizer=optimizer)
-            assert_allclose(lam_yeo, lam_box, rtol=1e-5)
-            assert_allclose(xt_yeo, xt_box, rtol=1e-5)
-    
-    def test_overflow_lam(self):
-        # non-regression test for gh-18389
-        x = np.array([2003.0, 1950.0, 1997.0, 2000.0, 2009.0])
 
         def optimizer(fun):
             out = optimize.fminbound(fun, -lam_yeo, lam_yeo, xtol=1.48e-08)
@@ -2257,9 +2246,11 @@ class TestYeojohnson:
             result.x = out
             return result
 
-        with np.errstate(over="raise"):
+        with np.errstate(over="raise", under="raise"):
             xt_yeo, lam_yeo = stats.yeojohnson(x)
             xt_box, lam_box = stats.boxcox(x + 1, optimizer=optimizer)
+            assert np.isfinite(np.var(xt_yeo))
+            assert np.isfinite(np.var(xt_box))
             assert_allclose(lam_yeo, lam_box, rtol=1e-5)
             assert_allclose(xt_yeo, xt_box, rtol=1e-5)
 
