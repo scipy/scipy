44d88b288ae6226a4e113997718bc7a146d986fd
Daniel Schmitz <40656107+dschmitz89@users.noreply.github.com>
2023-07-16 00:09:38 +0200

    ENH: add `fit` method to `multivariate_normal` (#18361)

--- scipy/stats/tests/test_multivariate.py
+++ scipy/stats/tests/test_multivariate.py
@@ -803,6 +803,21 @@ class TestMultivariateNormal:
         ref = multivariate_normal.pdf(x, [1, 1, 1], cov_object)
         assert_equal(multivariate_normal.pdf(x, 1, cov=cov_object), ref)
 
+    def test_fit_error(self):
+        data = [1, 3]
+        error_msg = "`x` must be two-dimensional."
+        with pytest.raises(ValueError, match=error_msg):
+            multivariate_normal.fit(data)
+
+    @pytest.mark.parametrize('dim', (3, 5))
+    def test_fit_correctness(self, dim):
+        rng = np.random.default_rng(4385269356937404)
+        x = rng.random((100, dim))
+        mean_est, cov_est = multivariate_normal.fit(x)
+        mean_ref, cov_ref = np.mean(x, axis=0), np.cov(x.T, ddof=0)
+        assert_allclose(mean_est, mean_ref, atol=1e-15)
+        assert_allclose(cov_est, cov_ref, rtol=1e-15)
+
 
 class TestMatrixNormal:
 
