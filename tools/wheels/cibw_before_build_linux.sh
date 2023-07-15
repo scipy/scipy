094404f08a204e1da202bd47c361f817819f143e
steppi <albert.steppi@gmail.com>
2023-07-15 10:24:16 -0400

    MAINT: Add nightly option to script

--- tools/wheels/cibw_before_build_linux.sh
+++ tools/wheels/cibw_before_build_linux.sh
@@ -1,5 +1,24 @@
 set -xe
 
+
+NIGHTLY_FLAG=""
+
+while (( "$#" )); do
+  case "$1" in
+    --nightly)
+      NIGHTLY_FLAG="--nightly"
+      shift
+      ;;
+    --*)
+      echo "Error: Unrecognized flag $1" >&2
+      exit 1
+      ;;
+    *)
+      break
+      ;;
+  esac
+done
+
 PROJECT_DIR="$1"
 PLATFORM=$(PYTHONPATH=tools python -c "import openblas_support; print(openblas_support.get_plat())")
 
@@ -8,6 +27,6 @@ printenv
 cat $PROJECT_DIR/tools/wheels/LICENSE_linux.txt >> $PROJECT_DIR/LICENSE.txt
 
 # Install Openblas
-basedir=$(python tools/openblas_support.py)
+basedir=$(python tools/openblas_support.py $NIGHTLY_FLAG)
 cp -r $basedir/lib/* /usr/local/lib
 cp $basedir/include/* /usr/local/include
