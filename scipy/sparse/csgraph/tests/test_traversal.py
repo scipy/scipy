26b6cde2fc2e960fc4e1bfee94f0e69357434b3d
CJ Carey <perimosocordiae@gmail.com>
2023-07-19 00:22:28 -0400

    BUG: sparse: Support int64 indices in csgraph

--- scipy/sparse/csgraph/tests/test_traversal.py
+++ scipy/sparse/csgraph/tests/test_traversal.py
@@ -1,5 +1,6 @@
 import numpy as np
 from numpy.testing import assert_array_almost_equal
+from scipy.sparse import csr_array
 from scipy.sparse.csgraph import (breadth_first_tree, depth_first_tree,
     csgraph_to_dense, csgraph_from_dense)
 
@@ -66,3 +67,21 @@ def test_graph_depth_first_trivial_graph():
         bfirst_test = depth_first_tree(csgraph, 0, directed)
         assert_array_almost_equal(csgraph_to_dense(bfirst_test),
                                   bfirst)
+
+
+def test_breadth_first_int64_indices():
+    # See https://github.com/scipy/scipy/issues/18716
+    g = csr_array((np.array([1]), np.array([[0], [1]])), shape=(2, 2))
+    assert g.indices.dtype == np.int64
+    for directed in [True, False]:
+        bf = breadth_first_tree(g, 0, directed=directed)
+        assert_array_almost_equal(csgraph_to_dense(bf), [[0, 1], [0, 0]])
+
+
+def test_depth_first_int64_indices():
+    # See https://github.com/scipy/scipy/issues/18716
+    g = csr_array((np.array([1]), np.array([[0], [1]])), shape=(2, 2))
+    assert g.indices.dtype == np.int64
+    for directed in [True, False]:
+        df = depth_first_tree(g, 0, directed=directed)
+        assert_array_almost_equal(csgraph_to_dense(df), [[0, 1], [0, 0]])
