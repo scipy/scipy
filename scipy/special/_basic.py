eff66d09a96f951b5ebb7b970f3957d2d27caeea
Lucas Roberts <rlucas7@vt.edu>
2023-07-20 23:01:12 -0400

    ENH: add stirling2 function to `scipy.special` (#18103)
    * add pure python implementation of stirling2
    
      * removes the c version and implements a
        pure python version which works in numpy
        arrays. Adds exact flag with only true
        branch implemented.
    
    * add missing imports
    
    * add math. to name`
    
    * fully qualify nditer name
    
    * change table creation mechanism
    
    * switch to mpmath.stirling2 with exact=True
    
    * remove typo
    
    * fix off by 1 error
    
    * Update scipy/special/_basic.py
    
    Co-authored-by: Albert Steppi <albert.steppi@gmail.com>
    
    * Update scipy/special/_basic.py
    
    Co-authored-by: Joren <jhammudoglu@gmail.com>
    
    * Apply suggestions from code review
    
    I missed the new 'batch' feature for github suggestions-a welcome addition! I've been wanting this for a bit now :)
    
    Co-authored-by: Albert Steppi <albert.steppi@gmail.com>
    
    * add fixes based on reviews, improve docstring and test case coverage
    
    * raise typeerror on non-int input
    
    * fix flake8 and coerce to int in iter
    
    * Update scipy/special/_basic.py
    
    Co-authored-by: Albert Steppi <albert.steppi@gmail.com>
    
    * Update scipy/special/_basic.py
    
    Co-authored-by: Albert Steppi <albert.steppi@gmail.com>
    
    * Update scipy/special/_basic.py
    
    Co-authored-by: Albert Steppi <albert.steppi@gmail.com>
    
    * Update scipy/special/_basic.py
    
    good catch
    
    Co-authored-by: Albert Steppi <albert.steppi@gmail.com>
    
    * Update scipy/special/_basic.py
    
    nice-thanks
    
    Co-authored-by: Albert Steppi <albert.steppi@gmail.com>
    
    * clean up docstring and comments, add more tests
    
    * add GKP ref and notational expression guidance
    
    * tidy up docstring
    
    * remove stray right square bracket
    
    * Update scipy/special/_basic.py
    
    Co-authored-by: Albert Steppi <albert.steppi@gmail.com>
    
    * Update scipy/special/_basic.py
    
    Co-authored-by: Albert Steppi <albert.steppi@gmail.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: h-vetinari <h.vetinari@gmx.com>
    
    * DOC: Add stirling2 to special __init__.py
    
    * Apply suggestions from code review
    
    commiting a few small docstring fixes
    
    * DOC: Fix indentation in stirling2 docstring
    
    * Make stirling2 docstring a raw string
    
    * DOC: Fix indentation in references
    
    * DOC: Add blank line after bullet list
    
    * Apply suggestions from code review
    
    should fix the 0-dim case and not break the handling for existing scalar case.
    
    Co-authored-by: Daniel Schmitz <40656107+dschmitz89@users.noreply.github.com>
    Co-authored-by: Albert Steppi <albert.steppi@gmail.com>
    
    * Update scipy/special/_basic.py
    
    * Update scipy/special/tests/test_basic.py
    
    change test w/decision to *not* accept input array w/dtype object
    
    ---------
    
    Co-authored-by: Albert Steppi <albert.steppi@gmail.com>
    Co-authored-by: Joren <jhammudoglu@gmail.com>
    Co-authored-by: h-vetinari <h.vetinari@gmx.com>
    Co-authored-by: Daniel Schmitz <40656107+dschmitz89@users.noreply.github.com>

--- scipy/special/_basic.py
+++ scipy/special/_basic.py
@@ -6,6 +6,8 @@ import operator
 import numpy as np
 import math
 import warnings
+from collections import defaultdict
+from heapq import heapify, heappop
 from numpy import (pi, asarray, floor, isscalar, iscomplex, real,
                    imag, sqrt, where, mgrid, sin, place, issubdtype,
                    extract, inexact, nan, zeros, sinc)
@@ -69,6 +71,7 @@ __all__ = [
     'riccati_jn',
     'riccati_yn',
     'sinc',
+    'stirling2',
     'y0_zeros',
     'y1_zeros',
     'y1p_zeros',
@@ -3135,6 +3138,129 @@ def factorialk(n, k, exact=True):
         msg = "factorialk does not support non-integral arrays!"
         raise ValueError(msg + helpmsg)
     return _exact_factorialx_array(n, k=k)
+
+
+def stirling2(N, K, *, exact=True):
+    r"""Generate Stirling number(s) of the second kind.
+
+    Stirling numbers of the second kind count the number of ways to
+    partition a set with N elements into K non-empty subsets.
+
+    The values this function returns are calculated using a dynamic
+    program which avoids redundant computation across the subproblems
+    in the solution. For array-like input, this implementation also 
+    avoids redundant computation across the different Stirling number
+    calculations.
+
+    The numbers are sometimes denoted
+
+    .. math::
+
+        {N \brace{K}}
+
+    see [1]_ for details. This is often expressed-verbally-as
+    "N subset K".
+
+    Parameters
+    ----------
+    N : int, ndarray
+        Number of things.
+    K : int, ndarray
+        Number of non-empty subsets taken.
+    exact : bool, optional
+        This keyword is reserved for a planned future implementation
+        that allows trading speed for accuracy.
+
+    Returns
+    -------
+    val : int, float, ndarray
+        The number of partitions.
+
+    See Also
+    --------
+    comb : The number of combinations of N things taken k at a time.
+
+    Notes
+    -----
+    - If N < 0, or K < 0, then 0 is returned.
+    - If K > N, then 0 is returned.
+
+    The output type will always be `int` or ndarray of `object`.
+    The input must contain either numpy or python integers otherwise a
+    TypeError is raised.
+
+    References
+    ----------
+    .. [1] R. L. Graham, D. E. Knuth and O. Patashnik, "Concrete
+        Mathematics: A Foundation for Computer Science," Addison-Wesley
+        Publishing Company, Boston, 1989. Chapter 6, page 258.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from scipy.special import stirling2
+    >>> k = np.array([3, -1, 3])
+    >>> n = np.array([10, 10, 9])
+    >>> stirling2(n, k)
+    array([9330, 0, 3025], dtype=object)
+
+    """
+    output_is_scalar = np.isscalar(N) and np.isscalar(K)
+    if exact:
+        # make a min-heap of unique (n,k) pairs
+        N, K = asarray(N), asarray(K)
+        if not np.issubdtype(N.dtype, np.integer):
+            raise TypeError("Argument `N` must contain only integers")
+        if not np.issubdtype(K.dtype, np.integer):
+            raise TypeError("Argument `K` must contain only integers")
+        nk_pairs = list(
+            set([(n.take(0), k.take(0))
+                 for n, k in np.nditer([N, K], ['refs_ok'])])
+        )
+        heapify(nk_pairs)
+        # base mapping for small values
+        snsk_vals = defaultdict(int)
+        for pair in [(0, 0), (1, 1), (2, 1), (2, 2)]:
+            snsk_vals[pair] = 1
+        n_old, n_row = 2, [0, 1, 1]
+        # for each pair in the min-heap, calculate the value, store for later
+        while nk_pairs:
+            n, k = heappop(nk_pairs)
+            if n < 2 or k > n or k <= 0:
+                continue
+            elif k == n or k == 1:
+                snsk_vals[(n, k)] = 1
+                continue
+            elif n != n_old:
+                num_iters = n - n_old
+                while num_iters > 0:
+                    n_row.append(1)
+                    # traverse from back to remove second row
+                    for j in range(len(n_row)-2, 1, -1):
+                        n_row[j] = n_row[j]*j + n_row[j-1]
+                    num_iters -= 1
+                snsk_vals[(n, k)] = n_row[k]
+            else:
+                snsk_vals[(n, k)] = n_row[k]
+            n_old, n_row = n, n_row
+        # for each pair in the map, fetch the value, and populate the array
+        it = np.nditer(
+            [N, K, None],
+            ['buffered', 'refs_ok'],
+            [['readonly'], ['readonly'], ['writeonly', 'allocate']],
+            op_dtypes=[object, object, object],
+        )
+        with it:
+            while not it.finished:
+                it[2] = snsk_vals[(int(it[0]), int(it[1]))]
+                it.iternext()
+            output = it.operands[2]
+            # If N and K were both scalars, convert output to scalar.
+            if output_is_scalar:
+                output = output.take(0)
+            return output
+    else:  # this branch will house future Temme approx
+        raise NotImplementedError()
 
 
 def zeta(x, q=None, out=None):
