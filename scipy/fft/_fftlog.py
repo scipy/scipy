397f9d15d2ebca883f66dfcceeb1301627dba064
Lucas Colley <lucas.colley8@gmail.com>
2023-07-29 15:34:06 +0100

    BUG: add `.copy()` to pass pytorch tests

--- scipy/fft/_fftlog.py
+++ scipy/fft/_fftlog.py
@@ -11,11 +11,11 @@ def fht(a, dln, mu, offset=0.0, bias=0.0):
     xp = array_namespace(a)
     a = np.asarray(a)
     b = npfft_log_mm.fht(a, dln, mu, offset=offset, bias=bias)
-    return xp.asarray(b)
+    return xp.asarray(b.copy())
 
 
 def ifht(A, dln, mu, offset=0.0, bias=0.0):
     xp = array_namespace(A)
     A = np.asarray(A)
     B = npfft_log_mm.ifht(A, dln, mu, offset=offset, bias=bias)
-    return xp.asarray(B)
+    return xp.asarray(B.copy())
