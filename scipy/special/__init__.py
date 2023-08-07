d145cc3821f50e9ad8ffed9c307df48e3a5d3779
Matt Haberland <mdhaber@mit.edu>
2023-08-07 09:06:37 -0700

    ENH: special: dispatch to array library
    [skip ci]

--- scipy/special/__init__.py
+++ scipy/special/__init__.py
@@ -770,8 +770,8 @@ Convenience functions
 
 from ._sf_error import SpecialFunctionWarning, SpecialFunctionError
 
-from . import _ufuncs
-from ._ufuncs import *
+from . import _dispatch
+from ._dispatch import *
 
 from . import _basic
 from ._basic import *
@@ -798,7 +798,7 @@ from ._spherical_bessel import (
 # Deprecated namespaces, to be removed in v2.0.0
 from . import add_newdocs, basic, orthogonal, specfun, sf_error, spfun_stats
 
-__all__ = _ufuncs.__all__ + _basic.__all__ + _orthogonal.__all__ + [
+__all__ = _dispatch.__all__ + _basic.__all__ + _orthogonal.__all__ + [
     'SpecialFunctionWarning',
     'SpecialFunctionError',
     'logsumexp',
