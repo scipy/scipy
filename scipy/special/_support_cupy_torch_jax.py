d83dde344649536b752e3743406ae94cb25701c0
Matt Haberland <mdhaber@mit.edu>
2023-08-16 11:55:17 -0700

    MAINT: special: improvements to array library dispatch per review
    [skip ci]

--- scipy/special/_support_cupy_torch_jax.py
+++ scipy/special/_support_cupy_torch_jax.py
@@ -1,38 +1,44 @@
 import sys
-from scipy._lib.array_api_compat.array_api_compat import array_namespace
+import numpy as np
+from scipy._lib._array_api import array_namespace
 from . import _ufuncs
 from ._ufuncs import *
 
 __all__ = _ufuncs.__all__
 
-array_api_comat_prefix = "scipy._lib.array_api_compat.array_api_compat"
+array_api_compat_prefix = "scipy._lib.array_api_compat.array_api_compat"
 
 
-def get_array_special_func(f_name, xp):
-    if xp.__name__ == f"{array_api_comat_prefix}.numpy":
+def get_array_special_func(f_name, xp, n_array_args):
+    # TODO: replace these `__name__` checks with `is_numpy`, etc. after
+    #       gh-19005 merges.
+    if xp.__name__ == f"{array_api_compat_prefix}.numpy":
         f = getattr(_ufuncs, f_name, None)
-    elif xp.__name__ == f"{array_api_comat_prefix}.torch":
+    elif xp.__name__ == f"{array_api_compat_prefix}.torch":
         f = getattr(xp.special, f_name, None)
-    elif xp.__name__ == f"{array_api_comat_prefix}.cupy":
+    elif xp.__name__ == f"{array_api_compat_prefix}.cupy":
         import cupyx
         f = getattr(cupyx.scipy.special, f_name, None)
-    elif xp.__name__ == f"{array_api_comat_prefix}.jax":
+    elif xp.__name__ == f"{array_api_compat_prefix}.jax":
         f = getattr(xp.scipy.special, f_name, None)
     else:
-        f = None
+        f_scipy = getattr(_ufuncs, f_name, None)
+        def f(*args, **kwargs):
+            array_args = args[:n_array_args]
+            other_args = args[n_array_args:]
+            array_args = [np.asarray(arg) for arg in array_args]
+            out = f_scipy(*array_args, *other_args, **kwargs)
+            return xp.asarray(out)
 
-    if f is None:
-        raise ValueError("SciPy cannot dispatch to special function "
-                         f"'{f_name}' in array library '{xp.__name__}'.")
     return f
 
 
 # functools.wraps doesn't work because:
 # 'numpy.ufunc' object has no attribute '__module__'
-def support_cupy_torch_jax(f_name, n_args):
+def support_cupy_torch_jax(f_name, n_array_args):
     def wrapped(*args, **kwargs):
-        xp = array_namespace(*args[:n_args])
-        f = get_array_special_func(f_name, xp)
+        xp = array_namespace(*args[:n_array_args])
+        f = get_array_special_func(f_name, xp, n_array_args)
         return f(*args, **kwargs)
     wrapped.__doc__ = getattr(_ufuncs, f_name).__doc__
     return wrapped
@@ -44,7 +50,7 @@ array_special_func_map = {
     'ndtri': 1
 }
 
-for f_name, n_args in array_special_func_map.items():
+for f_name, n_array_args in array_special_func_map.items():
     sys.modules[__name__].__dict__[f_name] = (
-        support_cupy_torch_jax(f_name, n_args)
+        support_cupy_torch_jax(f_name, n_array_args)
     )
--- scipy/special/tests/test_support_cupy_torch_jax.py
+++ scipy/special/tests/test_support_cupy_torch_jax.py
@@ -1,5 +1,5 @@
 import pytest
-from hypothesis import given, strategies
+from hypothesis import given, strategies, reproduce_failure  # noqa
 import hypothesis.extra.numpy as npst
 from numpy.testing import assert_allclose
 
@@ -8,13 +8,15 @@ from scipy.special._support_cupy_torch_jax import (get_array_special_func,
 from scipy.conftest import array_api_compatible
 from scipy import special
 from scipy._lib.array_api_compat.array_api_compat import numpy as np
+import numpy.array_api as np_array_api
 
 
-def test_dispatch_failure():
-    message = ("SciPy cannot dispatch to special function 'ndtr' "
-               "in array library 'object'.")
-    with pytest.raises(ValueError, match=message):
-        get_array_special_func('ndtr', xp=object)
+def test_dispatch_to_unrecognize_library():
+    f = get_array_special_func('ndtr', xp=np_array_api, n_array_args=1)
+    x = [1, 2, 3]
+    res = f(x)
+    ref = special.ndtr(np.asarray(x))
+    assert_allclose(np.asarray(res), ref)
 
 
 @array_api_compatible
@@ -29,13 +31,20 @@ def test_cupy_torch_jax_support(xp, data, f_name_n_args):
     shapes, final_shape = data.draw(mbs)
     dtype = data.draw(strategies.sampled_from(dtypes))
     dtype = getattr(np, dtype)
+    rtol = np.finfo(dtype).eps * 10
     args_np = [np.asarray(data.draw(npst.arrays(dtype, shape)))
                for shape in shapes]
-    args_xp = [xp.asarray(arg) for arg in args_np]
+    # `torch.asarray(np.asarray(1.))` produces
+    # TypeError: can't convert np.ndarray of type numpy.object_.
+    # So we extract the scalar from 0d arrays.
+    args_xp = [xp.asarray(arg[()]) for arg in args_np]
 
-    ref = f(*args_np)
+    ref = np.asarray(f(*args_np))
     res = f(*args_xp)
-    # having trouble with this locally
-    assert type(res) == type(xp.asarray([]))
+    # When `xp` is NumPy, `res` NumPy scalar, not an array.
+    if res.shape != ():
+        # TODO: use `_assert_matching_namespace` when gh-19005 merges
+        assert type(res) == type(xp.asarray([]))
     assert res.shape == ref.shape
-    assert_allclose(np.asarray(res), ref)
+    # TODO: use `set_assert_allclose` when gh-19005 merges
+    assert_allclose(np.asarray(res), ref, rtol=rtol)
