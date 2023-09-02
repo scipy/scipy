849ca41cf75fb6e01eed8e449fd9771b817221c1
lutefiskhotdish <87871567+lutefiskhotdish@users.noreply.github.com>
2023-09-02 17:04:10 +0100

    DOC: stats: Fix indent in levy_stable doc string
    Extra space at start of one line made it render separately instead of flowing with surrounding text.  Removed that space.
    
    [skip actions] [skip cirrus]

--- scipy/stats/_levy_stable/__init__.py
+++ scipy/stats/_levy_stable/__init__.py
@@ -779,7 +779,7 @@ class levy_stable_gen(rv_continuous):
     equivalent to ``%(name)s.pdf(y, %(shapes)s) / scale`` with
     ``y = (x - loc) / scale``, except in the ``S1`` parameterization if
     ``alpha == 1``.  In that case ``%(name)s.pdf(x, %(shapes)s, loc, scale)``
-     is identically equivalent to ``%(name)s.pdf(y, %(shapes)s) / scale`` with
+    is identically equivalent to ``%(name)s.pdf(y, %(shapes)s) / scale`` with
     ``y = (x - loc - 2 * beta * scale * np.log(scale) / np.pi) / scale``.
     Note that shifting the location of a distribution
     does not make it a "noncentral" distribution.
