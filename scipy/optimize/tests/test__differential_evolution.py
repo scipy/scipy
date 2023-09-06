1684ebb34483fce6da502ec4cde91a247af5c400
Karen Róbertsdóttir <karen.robertsdottir@gmail.com>
2023-09-06 15:49:17 +0000

    ENH: Implemented custom mutation functions in DifferentialEvolution.

--- scipy/optimize/tests/test__differential_evolution.py
+++ scipy/optimize/tests/test__differential_evolution.py
@@ -1574,3 +1574,26 @@ class TestDifferentialEvolutionSolver:
         # after the '=', so if necessary, the text could be reduced to, say,
         # "MAXCV = 0.".
         assert "MAXCV = 0.414" in result.message
+
+    def test_strategy_func(self):
+
+        init = [(-8, 0.5)] * 5
+        bounds = [(-8.0,8.0)] * 2
+
+        def func(x):
+            return abs(x[0]) * abs(x[1])
+            
+        def custom_strategy_fn(candidate, population, rng):
+            assert(np.array_equal(candidate, [-8, 0.5]) or np.array_equal(candidate, [2.0, -1.0]))
+            return [2.0, -1.0]	# Ignore the candidate; just be recognizable.
+
+        result = differential_evolution(func,
+                                        init=init,
+                                        bounds=bounds,
+                                        popsize=5,
+                                        maxiter=2,
+                                        polish=False,
+                                        strategy_func=custom_strategy_fn)
+        assert(result.success is True)
+        assert np.array_equal(result.x, [2.0, -1.0]) 
+
