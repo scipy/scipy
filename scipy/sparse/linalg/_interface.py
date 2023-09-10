e98b6cb1b2b8e8c882aae58b8960c66851e3a70c
Andrew Knyazev <andrew.knyazev@ucdenver.edu>
2023-09-10 19:23:02 -0400

    review comment _interface.py
    [skip actions] [skip cirrus] [skip circle]

--- scipy/sparse/linalg/_interface.py
+++ scipy/sparse/linalg/_interface.py
@@ -175,7 +175,7 @@ class LinearOperator:
         self.shape = shape
 
     def _init_dtype(self):
-        """Determine the dtype from `matvec` by execting it on `int8`.
+        """Determine the dtype by execting `matvec` on an `int8` test vector.
 
         In `np.promote_types` hierarchy, the type `int8` is the smallest,
         so we call `matvec` on `int8` and use the promoted dtype of the output
