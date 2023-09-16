eb1afd0e5ede41d25bce38388def17b6fb35ff6d
Krunal6767 <106827625+Krunal6767@users.noreply.github.com>
2023-09-16 11:31:40 -0400

    Update setup.py

--- scipy/cluster/setup.py
+++ scipy/cluster/setup.py
@@ -1,27 +1,31 @@
+import os
+from numpy.distutils.core import setup
+from numpy.distutils.misc_util import Configuration, get_numpy_include_dirs
+
+# Define common include directories
+include_dirs = [get_numpy_include_dirs()]
+
+# List of extensions to be added
+extensions = [
+    ('_vq', ['_vq.c']),
+    ('_hierarchy', ['_hierarchy.c']),
+    ('_optimal_leaf_ordering', ['_optimal_leaf_ordering.c'])
+]
+
+# Define macros
 DEFINE_MACROS = [("SCIPY_PY3K", None)]
 
-
 def configuration(parent_package='', top_path=None):
-    from numpy.distutils.misc_util import Configuration, get_numpy_include_dirs
     config = Configuration('cluster', parent_package, top_path)
 
+    # Add data directory
     config.add_data_dir('tests')
 
-    config.add_extension('_vq',
-        sources=[('_vq.c')],
-        include_dirs=[get_numpy_include_dirs()])
-
-    config.add_extension('_hierarchy',
-        sources=[('_hierarchy.c')],
-        include_dirs=[get_numpy_include_dirs()])
-
-    config.add_extension('_optimal_leaf_ordering',
-        sources=[('_optimal_leaf_ordering.c')],
-        include_dirs=[get_numpy_include_dirs()])
+    # Add extensions using a loop
+    for ext_name, sources in extensions:
+        config.add_extension(ext_name, sources=sources, include_dirs=include_dirs, define_macros=DEFINE_MACROS)
 
     return config
 
-
 if __name__ == '__main__':
-    from numpy.distutils.core import setup
     setup(**configuration(top_path='').todict())
