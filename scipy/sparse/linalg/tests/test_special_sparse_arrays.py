822105097fcdf55352fbc268366377ed7c6dbaae
Andrew Knyazev <andrew.knyazev@ucdenver.edu>
2023-09-17 17:19:00 -0400

    editorial from review test_special_sparse_arrays.py
    [skip actions] [skip cirrus] [skip circle]

--- scipy/sparse/linalg/tests/test_special_sparse_arrays.py
+++ scipy/sparse/linalg/tests/test_special_sparse_arrays.py
@@ -13,6 +13,7 @@ INT_DTYPES = [np.int8, np.int16, np.int32, np.int64]
 REAL_DTYPES = [np.float32, np.float64]
 COMPLEX_DTYPES = [np.complex64, np.complex128]
 
+
 class TestLaplacianNd:
     """
     LaplacianNd tests
