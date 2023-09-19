7485ebf6f5ad935159fb4f4d41f848bcf77c51c8
Lucas Colley <lucas.colley8@gmail.com>
2023-09-19 18:02:56 +0100

    MAINT: fft: remove `fft2`, `ifft2`, `rfft2`, `irfft2` from `_pocketfft` [skip cirrus] [skip circle]

--- scipy/fft/_pocketfft/tests/test_basic.py
+++ scipy/fft/_pocketfft/tests/test_basic.py
@@ -6,7 +6,7 @@ from numpy.testing import (assert_, assert_equal, assert_array_almost_equal,
 import pytest
 from pytest import raises as assert_raises
 from scipy.fft._pocketfft import (ifft, fft, fftn, ifftn,
-                                  rfft, irfft, rfftn, irfftn, fft2,
+                                  rfft, irfft, rfftn, irfftn,
                                   hfft, ihfft, hfftn, ihfftn)
 
 from numpy import (arange, array, asarray, zeros, dot, exp, pi,
@@ -456,24 +456,6 @@ class TestIRFFTSingle(_TestIRFFTBase):
         self.cdt = np.complex64
         self.rdt = np.float32
         self.ndec = 5
-
-
-class Testfft2:
-    def setup_method(self):
-        np.random.seed(1234)
-
-    def test_regression_244(self):
-        """FFT returns wrong result with axes parameter."""
-        # fftn (and hence fft2) used to break when both axes and shape were
-        # used
-        x = numpy.ones((4, 4, 2))
-        y = fft2(x, s=(8, 8), axes=(-3, -2))
-        y_r = numpy.fft.fftn(x, s=(8, 8), axes=(-3, -2))
-        assert_array_almost_equal(y, y_r)
-
-    def test_invalid_sizes(self):
-        assert_raises(ValueError, fft2, [[]])
-        assert_raises(ValueError, fft2, [[1, 1], [2, 2]], (4, -3))
 
 
 class TestFftnSingle:
