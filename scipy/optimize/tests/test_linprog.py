d301ad10d24d6b5b7614e0f07bdc483eeafbfcd7
Rohit Goswami <rog32@hi.is>
2023-10-01 15:15:47 +0000

    TST: Fixup tests

--- scipy/optimize/tests/test_linprog.py
+++ scipy/optimize/tests/test_linprog.py
@@ -307,7 +307,7 @@ def test_highs_status_message():
     msg = "Optimization terminated successfully."
     assert res.status == 0
     assert res.message.startswith(msg)
-    assert 'HiGHS Status 7' in res.message
+    assert 'HighsModelStatus.kOptimal' in res.message
 
     A, b, c, numbers, M = magic_square(6)
     bounds = [(0, 1)] * len(c)
@@ -318,7 +318,7 @@ def test_highs_status_message():
     msg = "Time limit reached"
     assert res.status == 1
     assert msg in res.message
-    assert 'HiGHS Status 13' in res.message
+    assert 'HighsModelStatus.kTimeLimit' in res.message
 
     options = {"maxiter": 10}
     res = linprog(c=c, A_eq=A, b_eq=b, bounds=bounds, method='highs-ds',
@@ -326,27 +326,26 @@ def test_highs_status_message():
     msg = "Iteration limit reached"
     assert res.status == 1
     assert res.message.startswith(msg)
-    assert 'HiGHS Status 14' in res.message
+    assert 'HighsModelStatus.kIterationLimit' in res.message
 
     res = linprog(1, bounds=(1, -1), method='highs')
     msg = "The problem is infeasible"
     assert res.status == 2
     assert res.message.startswith(msg)
-    assert 'HiGHS Status 8' in res.message
+    assert 'HighsModelStatus.kInfeasible' in res.message
 
-    # TODO: Fix this
-    # res = linprog(-1, method='highs')
-    # msg = "The problem is unbounded."
-    # assert res.status == 3
-    # assert res.message.startswith(msg)
-    # assert 'HiGHS Status 10' in res.message
+    res = linprog(-1, method='highs')
+    msg = "The problem is unbounded."
+    assert res.status == 3
+    assert res.message.startswith(msg)
+    assert 'HighsModelStatus.kUnbounded' in res.message
 
     from scipy.optimize._linprog_highs import HighsStatusMapping
     highs_mapper = HighsStatusMapping()
     status, message = highs_mapper.get_scipy_status(58, "Hello!")
-    # msg = "The HiGHS status code was not recognized. (HiGHS Status 58:"
     assert status == 4
     # TODO: Fix this
+    # msg = "The HiGHS status code was not recognized. (HiGHS Status 58:"
     # assert message.startswith(msg)
     assert "HiGHS Status 58" in message
 
