addd3b222dd18acf3001e711dfb5de9a3093160b
Andrew Knyazev <andrew.knyazev@ucdenver.edu>
2023-10-06 15:57:41 -0400

    all asserts removed sparse_linalg_lobpcg.py

--- benchmarks/benchmarks/sparse_linalg_lobpcg.py
+++ benchmarks/benchmarks/sparse_linalg_lobpcg.py
@@ -9,14 +9,11 @@ with safe_import():
     from scipy.sparse.linalg._special_sparse_arrays import (Sakurai,
                                                             MikotaPair)
 
-# ensure that we are benchmarking a consistent outcome;
-# (e.g. if the code wasn't able to find a solution at all,
-# the timing of the benchmark would become useless).
-msg = ("the benchmark code did not converge as expected, "
-       "the timing is therefore useless")
-
 
 class Bench(Benchmark):
+    # to ensure that we are benchmarking with a consistent accuary
+    # all the benchmark tests are repeated in the unit test suite
+    # `scipy/sparse/linalg/_eigen/lobpcg/tests/test_lobpcg.py`
     params = [
         [],
         ['lobpcg', 'eigsh', 'lapack']
@@ -47,7 +44,6 @@ class Bench(Benchmark):
         self.Bc = mik_m
         self.Ba = mik_m.toarray()
         self.Ab = mik_k.tobanded()
-        self.eigenvalues = mik.eigenvalues
 
         if solver == 'lapack' and n > 512:
             # skip: slow, and not useful to benchmark
@@ -58,21 +54,17 @@ class Bench(Benchmark):
         sakurai_obj = Sakurai(n, dtype='int')
         self.A = sakurai_obj
         self.Aa = sakurai_obj.toarray()
-        self.eigenvalues = sakurai_obj.eigenvalues
 
     def setup_sakurai_inverse(self, n, solver):
         self.shape = (n, n)
         sakurai_obj = Sakurai(n)
-        self.A = sakurai_obj.tobanded().astype(np.float64)
-        self.eigenvalues = sakurai_obj.eigenvalues
+        self.A = sakurai_obj.tobanded().astype(np.float64)s
     
     def time_mikota(self, n, solver):
         def a(x):
             return cho_solve_banded((c, False), x)
 
         m = 10
-        ee = self.eigenvalues(m)
-        tol = m * n * n * n* np.finfo(float).eps
         rng = np.random.default_rng(0)
         X = rng.normal(size=(n, m))
         if solver == 'lobpcg':
@@ -83,10 +75,8 @@ class Bench(Benchmark):
             c = cholesky_banded(self.Ab.astype(np.float32))
             with warnings.catch_warnings():
                 warnings.simplefilter("ignore")
-                el, _ = lobpcg(self.Ac, X, self.Bc, M=a, tol=1e-4,
+                _, _ = lobpcg(self.Ac, X, self.Bc, M=a, tol=1e-4,
                                maxiter=40, largest=False)
-            accuracy = max(abs(ee - el) / ee)
-            assert accuracy < tol, msg
         elif solver == 'eigsh':
             # `eigsh` ARPACK here is called on ``Bx = 1/lambda Ax``
             # to get fast convergence speed similar to that of `lobpcg` above
@@ -98,17 +88,13 @@ class Bench(Benchmark):
             A = LinearOperator((n, n), matvec=self.Ac, matmat=self.Ac, dtype='float64')
             c = cholesky_banded(self.Ab)
             a_l = LinearOperator((n, n), matvec=a, matmat=a, dtype='float64')
-            ea, _ = eigsh(B, k=m, M=A, Minv=a_l, which='LA', tol=1e-4, maxiter=50,
+            _, _ = eigsh(B, k=m, M=A, Minv=a_l, which='LA', tol=1e-4, maxiter=50,
                           v0 = rng.normal(size=(n, 1)))
-            accuracy = max(abs(ee - np.sort(1./ea)) / ee)
-            assert accuracy < tol, msg
         else:
             # `eigh` is the only dense eigensolver for generalized eigenproblems
             # ``Ax = lambda Bx`` and needs both matrices as dense arrays
             # making it very slow for large matrix sizes
-            ed, _ = eigh(self.Aa, self.Ba, subset_by_index=(0, m - 1))
-            accuracy = max(abs(ee - ed) / ee)
-            assert accuracy < tol, msg
+            _, _ = eigh(self.Aa, self.Ba, subset_by_index=(0, m - 1))
 
     def time_sakurai(self, n, solver):
         # the Sakurai matrix ``A`` is ill-conditioned so convergence of both
@@ -116,26 +102,18 @@ class Bench(Benchmark):
         # computing its smallest eigenvalues even from moderate sizes
         # requiring enormous numbers of iterations
         m = 3
-        ee = self.eigenvalues(m)
-        tol = 100 * n * n * n* np.finfo(float).eps
         rng = np.random.default_rng(0)
         X = rng.normal(size=(n, m))
         if solver == 'lobpcg':
             with warnings.catch_warnings():
                 warnings.simplefilter("ignore")
-                el, _ = lobpcg(self.A, X, tol=1e-9, maxiter=5000, largest=False)
-            accuracy = max(abs(ee - el) / ee)
-            assert accuracy < tol, msg
+                _, _ = lobpcg(self.A, X, tol=1e-9, maxiter=5000, largest=False)
         elif solver == 'eigsh':
             a_l = LinearOperator((n, n), matvec=self.A, matmat=self.A, dtype='float64')
-            ea, _ = eigsh(a_l, k=m, which='SA', tol=1e-9, maxiter=15000,
+            _, _ = eigsh(a_l, k=m, which='SA', tol=1e-9, maxiter=15000,
                           v0 = rng.normal(size=(n, 1)))
-            accuracy = max(abs(ee - ea) / ee)
-            assert accuracy < tol, msg
         else:
-            ed, _ = eigh(self.Aa, subset_by_index=(0, m - 1))
-            accuracy = max(abs(ee - ed) / ee)
-            assert accuracy < tol, msg
+            _, _ = eigh(self.Aa, subset_by_index=(0, m - 1))
 
     def time_sakurai_inverse(self, n, solver):
         # apply inverse iterations in  `lobpcg` and `eigsh` ARPACK
@@ -152,17 +130,11 @@ class Bench(Benchmark):
             c = cholesky_banded(self.A)
             with warnings.catch_warnings():
                 warnings.simplefilter("ignore")
-                el, _ = lobpcg(a, X, tol=1e-9, maxiter=8)
-            accuracy = max(abs(ee - 1. / el) / ee)
-            assert accuracy < tol, msg
+                _, _ = lobpcg(a, X, tol=1e-9, maxiter=8)
         elif solver == 'eigsh':
             c = cholesky_banded(self.A)
             a_l = LinearOperator((n, n), matvec=a, matmat=a, dtype='float64')
-            ea, _ = eigsh(a_l, k=m, which='LA', tol=1e-9, maxiter=8,
+            _, _ = eigsh(a_l, k=m, which='LA', tol=1e-9, maxiter=8,
                           v0 = rng.normal(size=(n, 1)))
-            accuracy = max(abs(ee - np.sort(1. / ea)) / ee)
-            assert accuracy < tol, msg
         else:
-            ed, _ = eig_banded(self.A, select='i', select_range=[0, m-1])
-            accuracy = max(abs(ee - ed) / ee)
-            assert accuracy < tol, msg
+            _, _ = eig_banded(self.A, select='i', select_range=[0, m-1])
