a050148114cb18eb285a1a747f1696d9fec71133
Ilhan Polat <ilhanpolat@gmail.com>
2023-10-10 15:51:26 +0200

    ENH:optimize:slsqp:Internal LS functions
    [skip ci] [skip cirrus] [skip circle]

--- scipy/optimize/slsqp/_slsqp.py
+++ scipy/optimize/slsqp/_slsqp.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scipy.linalg as la
-from scipy.optimize import nnls
+from scipy.optimize import _nnls
+from scipy.linalg.blas import dtrsm, dtrsv
 
 
 def linmin(mode: int, ax: float, bx: float, f: float, tol: float):
@@ -162,80 +163,165 @@ def ldl_update(A, n, sigma, z):
 
 
 def ldp(G, h):
+    """Solves the constrained problem, also known as "least distance problem"
+               1
+        min   --- |x|**2  subject to Gx >= h
+         x     2
+
+    via solving the dual problem using Nonnegative Least Squares (nnls)
+    """
     E = np.empty([G.shape[1]+1, G.shape[0]], dtype=G.dtype)
     E[:-1, :] = G.T
     E[-1, :] = h
     # E = np.vstack([G.T, h])
     f = np.zeros([E.shape[0]], dtype=E.dtype)
     f[-1] = 1
-    u, unorm = nnls(E, f)
-    r = E @ u - f
+    u, unorm, mode = _nnls(E, f)
+    if mode == -1:
+        return np.zeros_like(u), u, 3
     if unorm == 0.:
-        return np.zeros(len(r) - 1, dtype=r.dtype)
+        return np.zeros_like(u), u, 4
+    r = E @ u
+    r -= f
+    if np.abs(r[-1]) < np.finfo(np.float64).eps:
+        return np.zeros_like(u), u, 4
 
-    return -r[:-1]/r[-1], 1
+    return -r[:-1]/r[-1], -u/r[-1], 1
 
 
 def lsi(A, b, G, h):
-    """Assumes A full rank. Also b, h is assumed to be 1D"""
+    """Solve inequality constrained least squares problem
+
+        min |Ax - b|  subject to Gx >= h
+         x
+
+    Assumes A to be full rank. Also b, h is expected to be 1D
+    """
     eps = np.spacing(1.)
     Q, R = la.qr(A)
     m, n = A.shape
     # Only deal with full rank problems
     if (np.abs(np.diag(R)) < m*n*eps).any():
-        return np.zeros(n, dtype=A.dtype), 5
+        return np.zeros(n, dtype=A.dtype), np.zeros(n, dtype=A.dtype), 5
 
     bb = Q.T @ b
     # Define x = inv(R) (z - ff)
     # Transform G and h:  X R = G -> R.T X.T = G.T
-    GG = la.solve_triangular(R[:n, :].T, G.T,
-                             lower=True, check_finite=False).T
+    GG = dtrsm(1., R[:n, :], G, side=1)
     hh = h - GG @ bb[:n]
 
-    z, mode = ldp(GG, hh)
-    x = la.solve_triangular(R[:n, :], z + bb[:n], check_finite=False)
-    return x, 1
+    z, mult, mode = ldp(GG, hh)
+    if mode != 1:
+        return np.zeros([R.shape[1]], dtype=R.dtype), mult, mode
+
+    x = dtrsm(1., R[:n, :], z + bb[:n])
+    return x, mult, 1
 
 
 def lsei(A, b, E, f, G, h):
-    """
-    A, E, G: 2D-ndarray
-    b, f, h: 1D-ndarray
+    """Solve inequality and equality constrained least squares problem
 
-    We right triangulize E and apply Q.T to A and G, i.e.;
+        min |Ax - b|  subject to Gx >= h,  Ex = f
+         x
 
-        [E]         [0  Et]   [  ]
-        [A] @ Q.T = [A2 A1] = [AA]
-        [G]         [G2 G1]   [GG]
+    E for (e)quality, and G for "(g)reater than" inequality constraints.
 
-    the indices are swapped due to scipy.linalg.rq returning Et on the right
-    half as upper triangular.
+    Converts the problem into only inequality constrained problem and
+    uses lsi()
+
+    Parameters
+    ----------
+    A, E, G: ndarray
+        2D arrays for the left hand side data of the optimization problem. The
+        second dimension should be all equal. E cannot have more rows than the
+        number of states.
+    b, f, h: ndarray
+        1D arrays for the right hand side data of the optimization problem,
+        sizes compatible with A, E, G.
+
+    Returns
+    -------
+    x: ndarray
+        1D array that contains the solution or the all zero entries depending
+        on the success.
+    mult : ndarray
+        If applicable (E and/or G supplied), returns the multiplier vector.
+    mode: int
+        If 1 then problem is solved successfully otherwise some error occured,
+        error codes following the former fortran code.
+          2 : Size mismatches (returns dummy empty arrays to save memory)
+          5 : Coefficient array A is not full rank.
+          6 : Equality constraint array E is not full rank.
+          7 : NNLS problem failed to find a solution or if no constraints given
+              A is not full rank, up to lstsq precision.
     """
     eps = np.spacing(1.)
+    mineq = G.shape[0]
+    meq, n = E.shape
+    if meq > n:
+        return (np.array([]), np.array([]), 2)
+
+    if meq == 0 and mineq == 0:
+        xx, _, krank, _ = la.lstsq(A, b)
+        if krank != n:
+            return xx, np.array([]), 7
+        # No constraints just least squares, no multipliers
+        return (xx, np.array([]), 1)
+
+    elif meq == 0 and mineq > 0:
+        # No equality constraints, it is an LSI problem
+        return lsi(A, b, G, h)
+
+    nd = n - meq
+
+    # We right triangulize E and, apply Q.T to A and, if given, to G;
+    #
+    #     [E]         [0  Et]    [  ]
+    #     [A] @ Q.T = [A2 A1] := [AA]
+    #     [G]         [G2 G1]    [GG]
+    #
+    # the indices are swapped due to 'scipy.linalg.rq' returning Et on the
+    # right end, as upper triangular.
+
     Et, Q = la.rq(E)
-    m1, n = E.shape
-    nd = n - m1
+
     # Remove the zero part coming from RQ decomposition
     Et = Et[:, nd:]
     AA = A @ Q.T
-    GG = G @ Q.T
+    if mineq > 0:
+        GG = G @ Q.T
 
     # Only deal with full rank problems
-    if (np.abs(np.diag(Et)) < m1*n*eps).any():
-        return np.zeros(E.shape[0], dtype=E.dtype), 6
+    if (np.abs(np.diag(Et)) < meq*n*eps).any():
+        return (np.zeros(len(b), dtype=A.dtype),
+                np.zeros(E.shape[0], dtype=E.dtype),
+                6)
 
-    x1 = la.solve_triangular(Et, f)
+    # Solve Ex = f
+    x1 = dtrsv(Et, f)
 
-    if G.size == 0:  # No inequality constraints
-        x2, _, _, _ = la.lstsq(AA[:, :nd], b-AA[:, nd:]@x1)
+    if mineq == 0:  # No inequality constraints, basic least squares
+        x2, _, _, _ = la.lstsq(AA[:, :nd], b - AA[:, nd:]@x1)
     else:
-        x2, mode = lsi(AA[:, :nd], b - AA[:, nd:]@x1,
-                       GG[:, :nd], h - GG[:, nd:]@f[:m1])
+
+        x2, mult_ls, mode = lsi(AA[:, :nd], b - AA[:, nd:] @ x1,
+                                GG[:, :nd], h - GG[:, nd:] @ x1)
         # If failed
         if mode != 1:
-            return np.zeros(E.shape[0], dtype=E.dtype), mode
+            return (x2, mult_ls, mode)
 
-    # Note: it's [x2 x1] due to RQ swap
-    x = Q.T @ np.hstack([x2, x1])
+    # Solution of the original problem and the multipliers
+    xx = np.hstack([x2, x1])
+    mult = (AA @ xx)
+    mult -= b
+    mult = AA[:, nd:].T @ mult
+    if mineq != 0:
+        mult -= GG[:, nd:].T @ mult_ls
+    dtrsv(Et, mult, trans=1, overwrite_x=True)
+    if mineq != 0:
+        # combine the multipliers of eq and ineq constraints
+        mult = np.hstack([mult, mult_ls])
+    # Note: it is [x2 x1] due to RQ swap
+    xx = Q.T @ xx
 
-    return x, 1
+    return xx, mult, 1
