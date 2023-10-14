407e6a253aa8ccc032edf2fa8fe6f15ef4105142
Andrew Knyazev <andrew.knyazev@ucdenver.edu>
2023-10-14 15:33:27 -0400

    suppress warnings and add k/2 sparse_linalg_svds.py
    [skip actions] [skip cirrus]

--- benchmarks/benchmarks/sparse_linalg_svds.py
+++ benchmarks/benchmarks/sparse_linalg_svds.py
@@ -1,4 +1,5 @@
 import os
+import warnings
 import numpy as np
 from .common import Benchmark, safe_import
 
@@ -17,7 +18,7 @@ class BenchSVDS(Benchmark):
     # Benchmark SVD using the MatrixMarket test matrices recommended by the
     # author of PROPACK at http://sun.stanford.edu/~rmunk/PROPACK/
     params = [
-        [25],
+        [20],
         ["abb313", "illc1033", "illc1850", "qh1484", "rbs480a", "tols4000",
          "well1033", "well1850", "west0479", "west2021"],
         # TODO: re-include propack
@@ -32,16 +33,18 @@ class BenchSVDS(Benchmark):
         matrices = np.load(datafile, allow_pickle=True)
         self.A = matrices[problem][()]
         _, s, _ = svd(self.A.toarray(), full_matrices=False)
-        self.k_singular_values = np.sort(s)[:k]
-        self.tol = 100 * k * np.prod(self.A.shape) * np.finfo(float).eps
+        self.top_singular_values = np.flip(s[:int(k/2)])
+        self.tol = k * np.prod(self.A.shape) * np.finfo(float).eps
         self.rng = np.random.default_rng(0)
 
     def time_svds(self, k, problem, solver):
         if solver == 'svd':
             _, s, _ = svd(self.A.toarray(), full_matrices=False)
-            s = np.sort(s)[:k]
+            s = np.flip(s[:int(k/2)])
         else:
-            _, s, _ = svds(self.A, k=k, solver=solver, random_state=self.rng,
-                           maxiter = 200, tol=self.tol)
-        accuracy = max(abs(self.k_singular_values - s) / self.k_singular_values)
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore")
+                _, s, _ = svds(self.A, k=k, solver=solver, random_state=self.rng,
+                               maxiter = 50, tol=1e-6)
+        accuracy = np.max(np.abs(1 - s[int(k/2):] / self.top_singular_values))
         assert accuracy < self.tol, msg
