6f035ab9ad34bd52d26ff46f251eef5f9c10327b
evbernardes <evbernardes@gmail.com>
2023-10-16 16:05:56 +0200

    minor fix

--- scipy/spatial/transform/_rotation.pyx
+++ scipy/spatial/transform/_rotation.pyx
@@ -2150,7 +2150,7 @@ cdef class Rotation:
             - Third angle belongs to [-180, 180] degrees (both inclusive)
             - Second angle belongs to a set of size 180 degrees, given by:
              ``[-abs(lambda), 180 - abs(lambda)]``, where ``lambda`` is equal to:
-            ``atan2(dot(axis3, cross(axis1, axis2)), dot(axis3, axis1))``
+             ``atan2(dot(axis3, cross(axis1, axis2)), dot(axis3, axis1))``
 
         References
         ----------
