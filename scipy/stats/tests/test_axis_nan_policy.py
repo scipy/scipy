a5984a4764400c49d00784b249c37462c883f66d
Matt Haberland <mhaberla@calpoly.edu>
2023-10-19 13:46:41 -0700

    ENH: stats.bartlett: add axis / nan_policy / keepdims support (#19392)

--- scipy/stats/tests/test_axis_nan_policy.py
+++ scipy/stats/tests/test_axis_nan_policy.py
@@ -68,6 +68,7 @@ axis_nan_policy_cases = [
     (stats.ansari, tuple(), {}, 2, 2, False, None),
     (stats.entropy, tuple(), dict(), 1, 1, False, lambda x: (x,)),
     (stats.entropy, tuple(), dict(), 2, 1, True, lambda x: (x,)),
+    (stats.bartlett, tuple(), {}, 2, 2, False, None),
 ]
 
 # If the message is one of those expected, put nans in
