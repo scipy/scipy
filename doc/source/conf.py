9a8e6a4297f4f8931d55758fe64200cff8206a06
dschmitz89 <danielschmitzsiegen@googlemail.com>
2023-10-21 00:00:36 +0200

    Filter deprecation warning in doc build

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -335,6 +335,7 @@ for key in (
         'scipy.signal.morlet2 is deprecated',
         'scipy.signal.ricker is deprecated',
         'scipy.signal.cwt is deprecated',
+        'scipy.special.sinc is deprecated',
         ):
     warnings.filterwarnings(action='ignore', message='.*' + key + '.*')
 
