Fix kurtosis formula

scipy/stats/_discrete_distns.py
@@ -442,7 +442,7 @@ class betanbinom_gen(rv_discrete):
         def skew(n, a, b):
             return ((2 * n + a - 1) * (2 * b + a - 1)
                     / (a - 3) / sqrt(n * b * (n + a - 1) * (b + a - 1)
-                    / (a -2)))
+                    / (a - 2)))
         if 's' in moments:
             g1 = _lazywhere(a > 3, (n, a, b), f=skew, fillvalue=np.inf)
         def kurtosis(n, a, b):
@@ -451,10 +451,10 @@ class betanbinom_gen(rv_discrete):
                       + 6 * (b - 1.) * b)
                       + 3 * n**2 * ((a + 5.) * b**2 + (a + 5.) * (a - 1.) * b
                       + 2 * (a - 1.)**2) + 3 * (a - 1.) * n
-                      * ((a + 5.) * b**2. + (a + 5.) * (a -1.) * b
-                      + 2 * (a -1.)**2.))
-            denominator = ((a - 4.) * (a -3.) * b * n
-                           * (a - b - 1) * (a + n -1))
+                      * ((a + 5.) * b**2. + (a + 5.) * (a - 1.) * b
+                      + 2 * (a - 1.)**2.))
+            denominator = ((a - 4.) * (a - 3.) * b * n
+                           * (a + b - 1) * (a + n - 1))
             return term * term_2 / denominator
         if 'k' in moments:
             g2 = _lazywhere(a > 4, (n, a, b), f=kurtosis, fillvalue=np.inf)