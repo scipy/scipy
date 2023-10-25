45d2b8aa449744f4526ad8a3e93aeb096652db31
Pablo Rodríguez Pérez <pablorodriper@gmail.com>
2023-10-25 18:32:03 +0000

    Fix tab

--- scipy/stats/_mstats_extras.py
+++ scipy/stats/_mstats_extras.py
@@ -72,12 +72,11 @@ def hdquantiles(data, prob=list([.25,.5,.75]), axis=None, var=False,):
 
     >>> # Display the quantile estimates
     >>> for i, quantile in enumerate(quantiles):
-        >>> print(f"{int(quantile * 100)}th percentile: {quantile_estimates[i]}")
+    >>>     print(f"{int(quantile * 100)}th percentile: {quantile_estimates[i]}")
 
     25th percentile: 3.1505820231763066
     50th percentile: 5.194344084883956
     75th percentile: 7.430626414674935
-    ```
 
     """
     def _hd_1D(data,prob,var):
