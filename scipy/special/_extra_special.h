27f26e02cc3036ed46efe7493fe99b3b1cd6bcec
steppi <albert.steppi@gmail.com>
2023-10-26 12:10:00 -0400

    Use namespaces

--- scipy/special/_extra_special.h
+++ scipy/special/_extra_special.h
@@ -8,6 +8,6 @@
 
 inline npy_cdouble lambertw_scalar(npy_cdouble zp, long k, double tol) {
     std::complex<double> z(npy_creal(zp), npy_cimag(zp));
-    std::complex<double> w = lambertw(z, k, tol);
+    std::complex<double> w = scipy::special::lambertw(z, k, tol);
     return npy_cpack(real(w), imag(w));
 }
