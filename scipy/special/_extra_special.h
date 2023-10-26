15773466ca5505bbdc28eb21e419798551186e18
steppi <albert.steppi@gmail.com>
2023-10-25 21:09:42 -0400

    Change back to copying instead of type punning
    - lead to warning and didn't improve performance anyway

--- scipy/special/_extra_special.h
+++ scipy/special/_extra_special.h
@@ -7,7 +7,7 @@
 
 
 inline npy_cdouble lambertw_scalar(npy_cdouble zp, long k, double tol) {
-    std::complex<double>* z_ptr = reinterpret_cast<std::complex<double>*>(&zp);
-    std::complex<double> w = lambertw(*z_ptr, k, tol);
-    return *(reinterpret_cast<npy_cdouble*>(&w));
+    std::complex<double> z(npy_creal(zp), npy_cimag(zp));
+    std::complex<double> w = lambertw(z, k, tol);
+    return npy_cpack(real(w), imag(w));
 }
