9e0a3ecb2a835b25d08269aa92416ace7a0c468b
Pablo Rodríguez Pérez <pablorodriper@gmail.com>
2023-10-28 09:39:57 +0000

    Fix stats.hdquantiles example

--- scipy/stats/_mstats_extras.py
+++ scipy/stats/_mstats_extras.py
@@ -37,7 +37,7 @@ def hdquantiles(data, prob=list([.25,.5,.75]), axis=None, var=False,):
     data : array_like
         Data array.
     prob : sequence, optional
-        Sequence of quantiles to compute.
+        Sequence of probabilities at which to compute the quantiles.
     axis : int or None, optional
         Axis along which to compute the quantiles. If None, use a flattened
         array.
@@ -63,14 +63,14 @@ def hdquantiles(data, prob=list([.25,.5,.75]), axis=None, var=False,):
     >>> # Sample data
     >>> data = np.array([1.2, 2.5, 3.7, 4.0, 5.1, 6.3, 7.0, 8.2, 9.4])
 
-    >>> # Define quantiles to compute
-    >>> quantiles = [0.25, 0.5, 0.75]
+    >>> # Probabilities at which to compute quantiles
+    >>> probabilities = [0.25, 0.5, 0.75]
 
     >>> # Compute Harrell-Davis quantile estimates
-    >>> quantile_estimates = hdquantiles(data, prob=quantiles)
+    >>> quantile_estimates = hdquantiles(data, prob=probabilities)
 
     >>> # Display the quantile estimates
-    >>> for i, quantile in enumerate(quantiles):
+    >>> for i, quantile in enumerate(probabilities):
     ...     print(f"{int(quantile * 100)}th percentile: {quantile_estimates[i]}")
 
     25th percentile: 3.1505820231763066
