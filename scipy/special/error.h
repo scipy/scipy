f8188c075ca5e687fc5ba5268b5c8af5d8c1b1fc
steppi <albert.steppi@gmail.com>
2023-11-06 14:51:01 -0500

    Make use of sf_error conditional on flag

---
+++ scipy/special/error.h
@@ -0,0 +1,14 @@
+#pragma once
+
+
+namespace scipy {
+    namespace special {
+#ifndef SP_SPECFUN_ERROR
+        void set_error(const char *func_name, int code, const char *fmt, ...) {
+            // nothing
+        }
+#else
+        void set_error(const char *func_name, int code, const char *fmt, ...);
+#endif
+    }
+}
