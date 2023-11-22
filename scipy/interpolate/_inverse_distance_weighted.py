5e438808746285da2ba994a2a451990b355417a2
harshilkamdar <harshilkamdar@ymail.com>
2023-11-22 12:15:52 -0500

    ENH: sketch more

--- scipy/interpolate/_inverse_distance_weighted.py
+++ scipy/interpolate/_inverse_distance_weighted.py
@@ -12,8 +12,10 @@ class InverseDistanceWeightedNDInterpolator(NDInterpolatorBase):
             tree_options = dict()
         self.tree = cKDTree(self.points, **tree_options)
         self.values = np.asarray(y)
+
         self._k = k
         self._local = local
+        self._fill_value = fill_value
 
     def __call__(self, xi, p=2, k=6, distance_upper_bound=np.inf, **query_options):
         xi = _ndim_coords_from_arrays(xi, ndim=self.points.shape[1])
@@ -29,14 +31,22 @@ class InverseDistanceWeightedNDInterpolator(NDInterpolatorBase):
         return interp_values
 
     def _local_idw_interpolation(self, xi, p, k, distance_upper_bound, **query_options):
-        dist, i = self.tree.query(xi, k=self._k, distance_upper_bound=distance_upper_bound, **query_options)
+        dist, i = self.tree.query(xi_flat, **query_options)
+        valid_mask = np.isfinite(dist)
 
         weights = 1.0 / dist ** p
 
-        mask = np.isfinite(dist)
-        ret_array = np.ones(i.shape)
-        ret_array[~mask] = np.nan
-        ret_array[mask] = weights[mask] * self.values[i[mask]]
-        ret_array = np.sum(ret_array, axis=2) / np.sum(weights, axis=2)
+        # create a holder interp_values array with shape (n*m*.., k, l, ...) and fill with nans.
+        interp_shape = flattened_shape[:-1] + self.values.shape[1:] if self.values.ndim > 1 else flattened_shape[:-1]
 
-        return ret_array
+        if np.issubdtype(self.values.dtype, np.complexfloating):
+            interp_values = np.full(interp_shape, self._fill_value, dtype=self.values.dtype)
+        else:
+            interp_values = np.full(interp_shape, self._fill_value)
+
+        if self.values.ndim == 1:
+            interp_values[valid_mask] = np.sum(weights[mask] * self.values[i[mask]], axis=2) / np.sum(weights, axis=2)
+        else:
+            interp_values[valid_mask] = np.sum(weights[mask] * self.values[i[mask], ...], axis=-1) / np.sum(weights, axis=2)
+
+        return interp_values
