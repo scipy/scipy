c81116fdee6209a4678d09f941deaad7418ea705
Tyler Reddy <tyler.je.reddy@gmail.com>
2023-12-10 11:39:17 -0700

    MAINT: version bounds for 1.12.0rc1 [wheel build]
    * draft the release version pins in (we don't upper bound Python
    anymore because of `poetry`, and NumPy `2.0.0` support is
    likely reserved for the next SciPy, `1.13.0`)
    
    * point wheel builds at stable release NumPy, which I think
    should be sufficiently compatible because of:
    https://numpy.org/doc/stable/release/1.25.0-notes.html#compiling-against-the-numpy-c-api-is-now-backwards-compatible-by-default
    
    * additional shims/corrections are almost certain to be
    necessary, but I'll start probing the wheel builds now
    
    * a few release notes fixups I caught in the rendered version
    
    [wheel build]

--- scipy/__init__.py
+++ scipy/__init__.py
@@ -67,7 +67,7 @@ del _distributor_init
 from scipy._lib import _pep440
 # In maintenance branch, change to np_maxversion N+3 if numpy is at N
 np_minversion = '1.22.4'
-np_maxversion = '9.9.99'
+np_maxversion = '1.29.0'
 if (_pep440.parse(__numpy_version__) < _pep440.Version(np_minversion) or
         _pep440.parse(__numpy_version__) >= _pep440.Version(np_maxversion)):
     import warnings
