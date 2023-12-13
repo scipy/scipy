9cd247e02e4b0cad50282b0cc7b840d9916f7272
Xuefeng Xu <xuxf100@qq.com>
2023-12-13 22:08:33 +0800

    fix lint issue

--- scipy/stats/tests/test_morestats.py
+++ scipy/stats/tests/test_morestats.py
@@ -15,7 +15,7 @@ from numpy.testing import (assert_array_equal, assert_almost_equal,
 import pytest
 from pytest import raises as assert_raises
 import re
-from scipy import optimize, stats, special
+from scipy import optimize, stats
 from scipy.stats._morestats import _abw_state, _get_As_weibull, _Avals_weibull
 from .common_tests import check_named_results
 from .._hypotests import _get_wilcoxon_distr, _get_wilcoxon_distr2
