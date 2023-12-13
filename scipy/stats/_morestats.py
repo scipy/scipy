BUG: fix negative overflow in stats.boxcox_normmax

scipy/stats/_morestats.py
@@ -1314,25 +1314,38 @@ def boxcox_normmax(x, brack=None, method='pearsonr', optimizer=None):
     else:
         # Test if the optimal lambda causes overflow
         x = np.asarray(x)
-        x_treme = np.max(x, axis=0) if res > 0 else np.min(x, axis=0)
-        istransinf = np.isinf(special.boxcox(x_treme, res))
         dtype = x.dtype if np.issubdtype(x.dtype, np.floating) else np.float64
-        if np.any(istransinf):
+        # 10000 is a safety factor because `special.boxcox` overflows prematurely.
+        ymax = np.finfo(dtype).max / 10000
+
+        isoverflow = False
+        if np.any(res > 1):
+            xmax = np.max(x)
+            if xmax > 1:
+                mask = special.boxcox(xmax, res) > ymax
+                if np.any(mask):
+                    isoverflow = True
+                    constrained_res = _boxcox_inv_lmbda(xmax, ymax)
+        elif np.any(res < 0):
+            xmin = np.min(x)
+            if xmin < 1:
+                mask = special.boxcox(xmin, res) < -ymax
+                if np.any(mask):
+                    isoverflow = True
+                    constrained_res = _boxcox_inv_lmbda(xmin, -ymax)
+
+        if isoverflow:
             warnings.warn(
-                f"The optimal lambda is {res}, but the returned lambda is "
-                f"the constrained optimum to ensure that the maximum of the "
-                f"transformed data does not cause overflow in {dtype}.",
+                f"The optimal lambda is {res}, but the returned lambda is the"
+                f"constrained optimum to ensure that the maximum or the minimum "
+                f"of the transformed data does not cause overflow in {dtype}.",
                 stacklevel=2
             )
 
             # Return the constrained lambda to ensure the transformation
-            # does not cause overflow. 10000 is a safety factor because
-            # `special.boxcox` overflows prematurely.
-            ymax = np.finfo(dtype).max / 10000
-            constrained_res = _boxcox_inv_lmbda(x_treme, ymax * np.sign(res))
-
+            # does not cause overflow.
             if isinstance(res, np.ndarray):
-                res[istransinf] = constrained_res
+                res[mask] = constrained_res
             else:
                 res = constrained_res
     return res

scipy/stats/tests/test_morestats.py
@@ -1990,12 +1990,11 @@ class TestBoxcox:
             stats.boxcox_normmax(bad_x)
 
     @pytest.mark.parametrize('x', [
-        # Attempt to trigger overflow in power expressions.
-        np.array([2003.0, 1950.0, 1997.0, 2000.0, 2009.0,
-                  2009.0, 1980.0, 1999.0, 2007.0, 1991.0]),
-        # Attempt to trigger overflow with a large optimal lambda.
+        # negative overflow
+        np.array([0.50000471, 0.50004979, 0.50005902, 0.50009312, 0.50001632]),
+        # positive overflow
         np.array([2003.0, 1950.0, 1997.0, 2000.0, 2009.0]),
-        # Attempt to trigger overflow with large data.
+        # positive overflow
         np.array([2003.0e200, 1950.0e200, 1997.0e200, 2000.0e200, 2009.0e200])
     ])
     def test_overflow(self, x):
@@ -2074,20 +2073,6 @@ class TestBoxcoxNormmax:
 
             stats.boxcox_normmax(self.x, brack=(-2.0, 2.0),
                                  optimizer=optimizer)
-
-    @pytest.mark.parametrize(
-        'x', ([2003.0, 1950.0, 1997.0, 2000.0, 2009.0],
-              [0.50000471, 0.50004979, 0.50005902, 0.50009312, 0.50001632]))
-    def test_overflow(self, x):
-        message = "The optimal lambda is..."
-        with pytest.warns(UserWarning, match=message):
-            lmbda = stats.boxcox_normmax(x, method='mle')
-        assert np.isfinite(special.boxcox(x, lmbda)).all()
-        # 10000 is safety factor used in boxcox_normmax
-        ymax = np.finfo(np.float64).max / 10000
-        x_treme = np.max(x) if lmbda > 0 else np.min(x)
-        y_extreme = special.boxcox(x_treme, lmbda)
-        assert_allclose(y_extreme, ymax * np.sign(lmbda))
 
 
 class TestBoxcoxNormplot: