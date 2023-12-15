c07e69b64b43a42130ad7df9408072ddfd6f3887
Takumasa N. <n.takumasa@gmail.com>
2023-12-15 02:45:31 +0000

    Use f-string instead of `format` call

--- scipy/interpolate/_cubic.py
+++ scipy/interpolate/_cubic.py
@@ -462,7 +462,7 @@ class Akima1DInterpolator(CubicHermiteSpline):
 
     def __init__(self, x, y, axis=0, *, method: Literal["akima", "makima"]="akima"):
         if method not in {"akima", "makima"}:
-            raise NotImplementedError("`method`={} is unsupported.".format(method))
+            raise NotImplementedError(f"`method`={method} is unsupported.")
         # Original implementation in MATLAB by N. Shamsundar (BSD licensed), see
         # https://www.mathworks.com/matlabcentral/fileexchange/1814-akima-interpolation
         x, dx, y, axis, _ = prepare_input(x, y, axis)
