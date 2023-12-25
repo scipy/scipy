MAINT: stats: fix axis_nan_policy decorator non-broadcastable paired-sample statistics

scipy/stats/tests/test_axis_nan_policy.py
@@ -8,6 +8,7 @@ from itertools import product, combinations_with_replacement, permutations
 import re
 import pickle
 import pytest
+import datetime
 
 import numpy as np
 from numpy.testing import assert_allclose, assert_equal, suppress_warnings
@@ -710,7 +711,7 @@ def test_empty(hypotest, args, kwds, n_samples, n_outputs, paired, unpacker):
                 # After broadcasting, all arrays are the same shape, so
                 # the shape of the output should be the same as a single-
                 # sample statistic. Use np.mean as a reference.
-                concat = stats._stats_py._broadcast_concatenate(samples, axis)
+                concat = stats._stats_py._broadcast_concatenate(samples, axis, paired=paired)
                 with np.testing.suppress_warnings() as sup:
                     sup.filter(RuntimeWarning, "Mean of empty slice.")
                     sup.filter(RuntimeWarning, "invalid value encountered")
@@ -739,6 +740,48 @@ def test_empty(hypotest, args, kwds, n_samples, n_outputs, paired, unpacker):
                     stats._stats_py._broadcast_concatenate(samples, axis, paired)
                 with pytest.raises(ValueError, match=message):
                     hypotest(*samples, *args, axis=axis, **kwds)
+
+
+@pytest.mark.parametrize("axis", [0, 1])
+@pytest.mark.parametrize(("hypotest", "args", "kwds", "n_samples", "n_outputs",
+                          "paired", "unpacker"), axis_nan_policy_cases)
+def test_non_broadcastable(hypotest, args, kwds, n_samples, n_outputs, paired,
+                           unpacker, axis):
+    # test for correct error message when shapes are not broadcastable
+    if n_samples == 1:  # broadcasting only needed with >1 sample
+        return
+
+    # It's much easier to write a relatively comprehensive test here
+    # by relying on randomly-generated shapes to not be broadcastable.
+    # If we were to use the same seed all the time, the test would
+    # be no stronger than having chosen a single case, so change the
+    # seed every day. Any failing case is still reproducible.
+    seed = int(datetime.date.today().strftime("%Y%m%d"))
+    rng = np.random.default_rng(seed)
+    samples = [rng.random(size=rng.integers(2, 100, size=2))
+               for i in range(n_samples)]
+
+    # Sometimes random shapes will be broadcastable. Take the day off!
+    if _check_arrays_broadcastable(samples, axis=axis):
+        return
+
+    message = "Array shapes are incompatible for broadcasting."
+    with pytest.raises(ValueError, match=message):
+        hypotest(*samples, *args, **kwds)
+
+    if not paired:  # there's another test for paired-sample statistics
+        return
+
+    # Previously, paired sample statistics did not raise an error
+    # message when the shapes were broadcastable except along `axis`
+    # https://github.com/scipy/scipy/pull/19578#pullrequestreview-1766857165
+    shape = rng.integers(2, 10, size=2)
+    most_samples = [rng.random(size=shape) for i in range(n_samples-1)]
+    shape = list(shape)
+    shape[axis] += 1
+    other_sample = rng.random(size=shape)
+    with pytest.raises(ValueError, match=message):
+        hypotest(other_sample, *most_samples, *args, **kwds)
 
 
 def test_masked_array_2_sentinel_array():