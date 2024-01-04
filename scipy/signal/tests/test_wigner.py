f4ea3466e8ee9b905580cab4c887e60b32c077e3
Leander Claes <claes@emt.uni-paderborn.de>
2024-01-04 10:57:20 +0100

    Add test for wigner_ville based on marginals

--- scipy/signal/tests/test_wigner.py
+++ scipy/signal/tests/test_wigner.py
@@ -1,5 +1,6 @@
 import numpy as np
 from numpy.testing import assert_allclose
+from scipy.fft import fft
 from scipy.signal import gausspulse
 
 from scipy.signal import wigner_ville
@@ -23,3 +24,23 @@ class TestWigner:
         max_idx = np.unravel_index(np.argmax(wv), wv.shape)
         assert_allclose(fc, f[max_idx[0]], atol=f[1] - f[0])
         assert_allclose(tc, t[max_idx[1]], atol=t[1] - t[0])
+
+    def test_wigner_ville_power(self):
+        fs = 100
+        fc = 4.2
+        tc = 512 / fs
+        t = np.arange(0, 1024) / fs
+        x_re, x_im = gausspulse(t - tc, fc, bw=0.2, retquad=True)
+        x = x_re + 1j * x_im
+        t, f, wv = wigner_ville(x, fs)
+        assert_allclose(
+            np.sum(wv, axis=0) / len(f), np.abs(x) ** 2
+        )
+        assert_allclose(
+            np.sum(wv, axis=1) * 2,
+            (np.abs(fft(x)) ** 2)[: len(x) // 2],
+            atol=1e-10
+        )
+        assert_allclose(
+            np.sum(np.abs(x) ** 2), np.sum(wv) / len(f)
+        )
