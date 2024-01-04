9f13d60d978c8337f315c0a1b4cd2ad64c41d888
Aman Atman <41161981+willtryagain@users.noreply.github.com>
2024-01-04 18:41:07 +0530

    shorten description
    [docs only]

--- scipy/fft/_fftlog.py
+++ scipy/fft/_fftlog.py
@@ -54,10 +54,9 @@ def fht(a, dln, mu, offset=0.0, bias=0.0):
         A(k) = \int_{0}^{\infty} \! a(r) \, J_\mu(kr) \, k \, dr \;,
 
     where :math:`J_\mu` is the Bessel function of order :math:`\mu`.  The index
-    :math:`\mu` may be any real number, positive or negative.
-
-    Note that the numerical Hankel transform uses an integrand of :math:`k dr`, 
-    while the mathematical Hankel transform is commonly defined using :math:`r dr`.
+    :math:`\mu` may be any real number, positive or negative.  Note that the
+    numerical Hankel transform uses an integrand of :math:`k dr`, while the
+    mathematical Hankel transform is commonly defined using :math:`r dr`.
 
     The input array `a` is a periodic sequence of length :math:`n`, uniformly
     logarithmically spaced with spacing `dln`,
