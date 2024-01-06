a892844f725dda442d139d4b14ef100496172252
anupriyakkumari <126849604+anupriyakkumari@users.noreply.github.com>
2024-01-07 01:40:20 +0530

    Apply suggestions from code review
    Committing suggestions
    
    Co-authored-by: Daniel Schmitz <40656107+dschmitz89@users.noreply.github.com>

--- scipy/optimize/_lbfgsb_py.py
+++ scipy/optimize/_lbfgsb_py.py
@@ -179,37 +179,36 @@ def fmin_l_bfgs_b(func, x0, fprime=None, args=(),
     for this example.
 
     >>> import numpy as np
-    >>> X = np.arange(0,10,1)
+    >>> from scipy.optimize import fmin_l_bfgs_b
+    >>> X = np.arange(0, 10, 1)
     >>> M = 2
     >>> B = 3
-    >>> Y = M*X + B
+    >>> Y = M * X + B
     >>> def func(parameters, *args):
     ...     x = args[0]
     ...     y = args[1]
     ...     m, b = parameters
-    ...     y_model = m*x+b
+    ...     y_model = m * x+b
     ...     error = sum(np.power((y - y_model), 2))
     ...     return error
 
     >>> initial_values = np.array([0.0, 1.0])
     >>> bounds = [(0,5), (5,10)]
 
-    >>> from scipy.optimize import fmin_l_bfgs_b
-    >>> fmin_l_bfgs_b(func, x0 = initial_values, args = (X, Y),
-    ...                              approx_grad=True)
-    (array([1.99999999, 3.00000006]), 1.7746231151323805e-14, # may vary
-     {'grad': array([8.08207139e-10, 8.83131710e-10]), # may vary
-      'task': 'CONVERGENCE: NORM_OF_PROJECTED_GRADIENT_<=_PGTOL',
-      'funcalls': 27, 'nit': 6, 'warnflag': 0})
-    >>> fmin_l_bfgs_b(func, x0 = initial_values, args = (X, Y),
-    ...                              bounds = bounds, approx_grad=True)
-    (array([1.65990508, 5.31649385]), 15.721334516453945, # may vary
-     {'grad': array([14.63034662, 15.72133562]), # may vary
-      'task': 'CONVERGENCE: REL_REDUCTION_OF_F_<=_FACTR*EPSMCH',
-      'funcalls': 39, 'nit': 4, 'warnflag': 0})
-    
-    The first optimize function is unbounded, resulting in optimal parameters but when we use 
-    bounds in the second optimize function, we do not get the optimal parameters anymore. 
+    >>> result = fmin_l_bfgs_b(func, x0 = initial_values, args = (X, Y),
+    ...                                          approx_grad=True)
+    >>> result.x, result.fun
+    array([1.99999999, 3.00000006]), 1.7746231151323805e-14  # may vary
+
+    The optimized parameters in ``result.x`` agree with the ground truth parameters
+    `m` and `b`. Next, let us perform a bound contrained optimization using the `bounds`
+    parameter. 
+
+    >>> bounds = [(0,5), (5,10)]
+    >>> bounded_result =  fmin_l_bfgs_b(func, x0 = initial_values, args = (X, Y),
+    ...                                                            bounds = bounds, approx_grad=True)
+    >>> bounded_result.x, bounded_result.fun
+    array([1.65990508, 5.31649385]), 15.721334516453945  # may vary    
     """
     # handle fprime/approx_grad
     if approx_grad:
