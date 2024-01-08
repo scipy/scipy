91886aae4dd6419012111a78dc634f0eca575d5a
Albert Steppi <albert.steppi@gmail.com>
2024-01-08 14:16:26 -0500

    Add loggamma, sinpi and cospi to special C++ lib
    - along with Cephes dependencies
    - Sinpi and Cospi to avoid conflict with Cuda runtime library

--- scipy/special/special/trig.h
+++ scipy/special/special/trig.h
@@ -3,112 +3,95 @@
  * floating point), it's possible to compute them with greater accuracy
  * than sin(z), cos(z).
  *
- * Originally written in Cython by Josh Wilson (github @person142) 2016. 
+ * Originally written in Cython by Josh Wilson (github @person142) 2016.
  * Translated to C++ by SciPy developers in 2023.
  */
 
-
 #pragma once
 
-
-#include <cmath>
-#include <complex>
-#include <limits>
-
-#include "cephes.hh"
-#include "special/evalpoly.h"
-
-
+#include "cephes/trig.h"
+#include "config.h"
+#include "evalpoly.h"
 
 namespace special {
-	
 
-    inline std::complex<double> sinpi(std::complex<double> z) {
-	double x = z.real();
-	double piy = M_PI * z.imag();
-	double abspiy = std::fabs(piy);
-	double sinpix = cephes::sinpi(x);
-	double cospix = cephes::cospi(x);
+SPECFUN_HOST_DEVICE inline std::complex<double> Sinpi(std::complex<double> z) {
+    double x = z.real();
+    double piy = M_PI * z.imag();
+    double abspiy = std::abs(piy);
+    double sinpix = cephes::Sinpi(x);
+    double cospix = cephes::Cospi(x);
 
-	if (abspiy < 700) {
-	    return std::complex<double>(sinpix*std::cosh(piy),
-					cospix*std::sinh(piy));
-	}
-
-	/* Have to be careful--sinh/cosh could overflow while cos/sin are small.
-	 * At this large of values
-	 *
-	 * cosh(y) ~ exp(y)/2
-	 * sinh(y) ~ sgn(y)*exp(y)/2
-	 *
-	 * so we can compute exp(y/2), scale by the right factor of sin/cos
-	 * and then multiply by exp(y/2) to avoid overflow. */
-	double exphpiy = std::exp(abspiy/2);
-	double coshfac;
-	double sinhfac;
-	if (exphpiy == std::numeric_limits<double>::infinity()) {
-	    if (sinpix == 0.0) {
-		// Preserve the sign of zero.
-		coshfac = std::copysign(0.0, sinpix);
-	    } else {
-		coshfac = std::copysign(std::numeric_limits<double>::infinity(),
-					sinpix);
-	    }
-	    if (cospix == 0.0) {
-		// Preserve the sign of zero.
-		sinhfac = std::copysign(0.0, cospix);
-	    } else {
-		sinhfac = std::copysign(std::numeric_limits<double>::infinity(),
-					cospix);
-	    }
-	    return std::complex<double>(coshfac, sinhfac);
-	}
-	
-	coshfac = 0.5*sinpix*exphpiy;
-	sinhfac = 0.5*cospix*exphpiy;
-	return std::complex<double>(coshfac*exphpiy, sinhfac*exphpiy);
-
-    }
-    
-    inline std::complex<double> cospi(std::complex<double> z) {
-	double x = z.real();
-	double piy = M_PI * z.imag();
-	double abspiy = std::fabs(piy);
-	double sinpix = cephes::sinpi(x);
-	double cospix = cephes::cospi(x);
-	
-	if (abspiy < 700) {
-	    return std::complex<double>(cospix*std::cosh(piy),
-					-sinpix*std::sinh(piy));
-	}
-
-
-	// See csinpi(z) for an idea of what's going on here.
-	double exphpiy = std::exp(abspiy/2);
-	double coshfac;
-	double sinhfac;
-	if (exphpiy == std::numeric_limits<double>::infinity()) {
-	    if (sinpix == 0.0) {
-		// Preserve the sign of zero.
-		coshfac = std::copysign(0.0, cospix);
-	    } else {
-		coshfac = std::copysign(std::numeric_limits<double>::infinity(),
-					cospix);
-	    }
-	    if (cospix == 0.0) {
-		// Preserve the sign of zero.
-		sinhfac = std::copysign(0.0, sinpix);
-	    } else {
-		sinhfac = std::copysign(std::numeric_limits<double>::infinity(),
-					sinpix);
-	    }
-	    return std::complex<double>(coshfac, sinhfac);
-	}
-
-	coshfac = 0.5*cospix*exphpiy;
-	sinhfac = 0.5*sinpix*exphpiy;
-	return std::complex<double>(coshfac*exphpiy, sinhfac*exphpiy);
+    if (abspiy < 700) {
+        return std::complex<double>(sinpix * std::cosh(piy), cospix * std::sinh(piy));
     }
 
+    /* Have to be careful--sinh/cosh could overflow while cos/sin are small.
+     * At this large of values
+     *
+     * cosh(y) ~ exp(y)/2
+     * sinh(y) ~ sgn(y)*exp(y)/2
+     *
+     * so we can compute exp(y/2), scale by the right factor of sin/cos
+     * and then multiply by exp(y/2) to avoid overflow. */
+    double exphpiy = std::exp(abspiy / 2);
+    double coshfac;
+    double sinhfac;
+    if (exphpiy == std::numeric_limits<double>::infinity()) {
+        if (sinpix == 0.0) {
+            // Preserve the sign of zero.
+            coshfac = std::copysign(0.0, sinpix);
+        } else {
+            coshfac = std::copysign(std::numeric_limits<double>::infinity(), sinpix);
+        }
+        if (cospix == 0.0) {
+            // Preserve the sign of zero.
+            sinhfac = std::copysign(0.0, cospix);
+        } else {
+            sinhfac = std::copysign(std::numeric_limits<double>::infinity(), cospix);
+        }
+        return std::complex<double>(coshfac, sinhfac);
+    }
 
+    coshfac = 0.5 * sinpix * exphpiy;
+    sinhfac = 0.5 * cospix * exphpiy;
+    return std::complex<double>(coshfac * exphpiy, sinhfac * exphpiy);
 }
+
+SPECFUN_HOST_DEVICE inline std::complex<double> Cospi(std::complex<double> z) {
+    double x = z.real();
+    double piy = M_PI * z.imag();
+    double abspiy = std::abs(piy);
+    double sinpix = cephes::Sinpi(x);
+    double cospix = cephes::Cospi(x);
+
+    if (abspiy < 700) {
+        return std::complex<double>(cospix * std::cosh(piy), -sinpix * std::sinh(piy));
+    }
+
+    // See csinpi(z) for an idea of what's going on here.
+    double exphpiy = std::exp(abspiy / 2);
+    double coshfac;
+    double sinhfac;
+    if (exphpiy == std::numeric_limits<double>::infinity()) {
+        if (sinpix == 0.0) {
+            // Preserve the sign of zero.
+            coshfac = std::copysign(0.0, cospix);
+        } else {
+            coshfac = std::copysign(std::numeric_limits<double>::infinity(), cospix);
+        }
+        if (cospix == 0.0) {
+            // Preserve the sign of zero.
+            sinhfac = std::copysign(0.0, sinpix);
+        } else {
+            sinhfac = std::copysign(std::numeric_limits<double>::infinity(), sinpix);
+        }
+        return std::complex<double>(coshfac, sinhfac);
+    }
+
+    coshfac = 0.5 * cospix * exphpiy;
+    sinhfac = 0.5 * sinpix * exphpiy;
+    return std::complex<double>(coshfac * exphpiy, sinhfac * exphpiy);
+}
+
+} // namespace special
--- scipy/special/special/zlog1.h
+++ scipy/special/special/zlog1.h
@@ -4,36 +4,32 @@
  * Translated from Cython to C++ by SciPy developers 2023.
  */
 
-
-#include <complex>
-#include <cmath>
-
+#include "config.h"
 
 namespace special {
-    namespace detail {
-	constexpr double tol = 2.220446092504131e-16; // machine epsilon
-	
-	inline std::complex<double> zlog1(std::complex<double> z) {
-	    /* Compute log, paying special attention to accuracy around 1. We
-	     * implement this ourselves because some systems (most notably the
-	     * Travis CI machines) are weak in this regime. */
-	    const double tol = 2.220446092504131e-16; // machine epsilon
-	    std::complex<double> coeff = -1.0;
-	    std::complex<double> res = 0.0;
-	    
-	    if (std::abs(z - 1.0) > 0.1) {
-		return std::log(z);
-	    }
-		
-	    z -= 1.0;
-	    for (int n = 1; n < 17; n++) {
-		coeff *= -z;
-		res += coeff/static_cast<double>(n);
-		if (std::abs(res/coeff) < tol) {
-		    break;
-		}
-	    }
-	    return res;
-	}
+namespace detail {
+
+    SPECFUN_HOST_DEVICE inline std::complex<double> zlog1(std::complex<double> z) {
+        /* Compute log, paying special attention to accuracy around 1. We
+         * implement this ourselves because some systems (most notably the
+         * Travis CI machines) are weak in this regime. */
+        const double tol = 2.220446092504131e-16; // machine epsilon
+        std::complex<double> coeff = -1.0;
+        std::complex<double> res = 0.0;
+
+        if (std::abs(z - 1.0) > 0.1) {
+            return std::log(z);
+        }
+
+        z -= 1.0;
+        for (int n = 1; n < 17; n++) {
+            coeff *= -z;
+            res += coeff / static_cast<double>(n);
+            if (std::abs(res / coeff) < tol) {
+                break;
+            }
+        }
+        return res;
     }
-}
+} // namespace detail
+} // namespace special
