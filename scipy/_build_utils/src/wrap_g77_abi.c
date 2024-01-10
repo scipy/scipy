16fb0cf574ca9daf5e678b274d50bcc11ead0d54
thalassemia <67928790+thalassemia@users.noreply.github.com>
2024-01-10 13:22:03 -0800

    MAINT: Fix int type and missing & in G77 wrappers

--- scipy/_build_utils/src/wrap_g77_abi.c
+++ scipy/_build_utils/src/wrap_g77_abi.c
@@ -16,7 +16,7 @@ typedef float _Complex float_complex;
 #endif
 
 float_complex F_FUNC(wcdotc, WCDOTC)(CBLAS_INT *n, float_complex *cx, \
-        BLAS_INT *incx, float_complex *cy, CBLAS_INT *incy){
+        CBLAS_INT *incx, float_complex *cy, CBLAS_INT *incy){
     float_complex ret;
     /* Prototype in npy_cblas_base.h which is included in npy_cblas.h */
     CBLAS_FUNC(cblas_cdotc_sub)(*n, cx, *incx, cy, *incy,&ret);
@@ -48,26 +48,26 @@ double_complex F_FUNC(wzdotu, WZDOTU)(CBLAS_INT *n, double_complex *zx, \
 }
 
 void BLAS_FUNC(sladiv)(float *xr, float *xi, float *yr, float *yi, \
-    float *retr, float * reti);
+    float *retr, float *reti);
 float_complex F_FUNC(wcladiv, WCLADIV)(float_complex *x, float_complex *y){
     float_complex ret;
     /* float_complex has the same memory layout as float[2], so we can
        cast and use pointer arithmetic to get the real and imaginary parts */
     BLAS_FUNC(sladiv)((float*)(x), (float*)(x)+1, \
         (float*)(y), (float*)(y)+1, \
-        (float*)(ret), (float*)(ret)+1);
+        (float*)(&ret), (float*)(&ret)+1);
     return ret;
 }
 
 void BLAS_FUNC(dladiv)(double *xr, double *xi, double *yr, double *yi, \
-    double *retr, double * reti);
+    double *retr, double *reti);
 double_complex F_FUNC(wzladiv, WZLADIV)(double_complex *x, double_complex *y){
     double_complex ret;
     /* double_complex has the same memory layout as double[2], so we can
        cast and use pointer arithmetic to get the real and imaginary parts */
     BLAS_FUNC(dladiv)((double*)(x), (double*)(x)+1, \
         (double*)(y), (double*)(y)+1, \
-        (double*)(ret), (double*)(ret)+1);
+        (double*)(&ret), (double*)(&ret)+1);
     return ret;
 }
 
