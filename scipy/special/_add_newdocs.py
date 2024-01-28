82d9fb0d3bf2643174dbe66048368aa02372602c
Jake Bowhay <jb9.bowhay@gmail.com>
2024-01-28 13:25:06 +0000

    MAINT: remove unused factorial ufunc

--- scipy/special/_add_newdocs.py
+++ scipy/special/_add_newdocs.py
@@ -14359,11 +14359,6 @@ add_newdoc("owens_t",
     0.10877216734852274
     """)
 
-add_newdoc("_factorial",
-    """
-    Internal function, do not use.
-    """)
-
 add_newdoc("wright_bessel",
     r"""
     wright_bessel(a, b, x, out=None)
--- scipy/special/_factorial.pxd
+++
@@ -1,8 +0,0 @@
-from ._cephes cimport Gamma
-
-
-cdef inline double _factorial(double n) noexcept nogil:
-    if n < 0:
-        return 0
-    else:
-        return Gamma(n + 1)
