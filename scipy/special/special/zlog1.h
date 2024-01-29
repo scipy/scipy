4ecf5638c36c3a87b473056f5fffecfeddffd368
Albert Steppi <albert.steppi@gmail.com>
2024-01-29 09:06:06 -0500

    MAINT: Translate Digamma from Cython into C++ (#19834)
    * Translate digamma function to C++
    
    * Fix typo (reversed inequality) and remove extraneous whitespace
    
    * Flatten nested namespace for digamma under detail
    
    * Use sf_error conditionally
    
    * Add missing pragma once
    
    * Move constants out of toplevel namespace
    
    * Make digamma tol an inline constexpr
    
    * Move digamma to new library
    
    * Make digamma work on cuda
    
    * Fix inequality
    
    * Make style change for more concise return statement
    
    * Use std::numeric_limits for machine epsilon
    
    * Change Sinpi/Cospi back to sinpi/cospi

--- scipy/special/special/zlog1.h
+++ scipy/special/special/zlog1.h
@@ -13,7 +13,6 @@ namespace detail {
         /* Compute log, paying special attention to accuracy around 1. We
          * implement this ourselves because some systems (most notably the
          * Travis CI machines) are weak in this regime. */
-        const double tol = 2.220446092504131e-16; // machine epsilon
         std::complex<double> coeff = -1.0;
         std::complex<double> res = 0.0;
 
@@ -25,7 +24,7 @@ namespace detail {
         for (int n = 1; n < 17; n++) {
             coeff *= -z;
             res += coeff / static_cast<double>(n);
-            if (std::abs(res / coeff) < tol) {
+            if (std::abs(res / coeff) < std::numeric_limits<double>::epsilon()) {
                 break;
             }
         }
