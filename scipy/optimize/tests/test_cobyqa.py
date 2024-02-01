6126c8f5e794a1b69601b4e1c2a19494af775be8
Tom M. Ragonneau <t.ragonneau@gmail.com>
2024-02-01 17:25:18 +0800

    Improve tests of COBYQA

--- scipy/optimize/tests/test_cobyqa.py
+++ scipy/optimize/tests/test_cobyqa.py
@@ -1,28 +1,27 @@
-import math
 import numpy as np
-
 from numpy.testing import assert_allclose, assert_
 
-from scipy.optimize import minimize, Bounds, NonlinearConstraint
+from scipy.optimize import (
+    Bounds,
+    LinearConstraint,
+    NonlinearConstraint,
+    minimize,
+)
 
 
 class TestCOBYQA:
 
     def setup_method(self):
         self.x0 = [4.95, 0.66]
-        self.solution = [math.sqrt(25 - (2.0/3)**2), 2.0/3]
-        self.opts = {
-            'disp': False,
-            'initial_tr_radius': 1,
-            'final_tr_radius': 1e-5,
-            'maxfev': 100,
-        }
+        self.options = {'maxfev': 100}
 
-    def fun(self, x):
-        return x[0]**2 + abs(x[1])**3
+    @staticmethod
+    def fun(x, c=1.0):
+        return x[0]**2 + c * abs(x[1])**3
 
-    def con(self, x):
-        return x[0]**2 + x[1]**2 - 25
+    @staticmethod
+    def con(x):
+        return x[0]**2 + x[1]**2 - 25.0
 
     def test_minimize_simple(self):
         class Callback:
@@ -34,107 +33,89 @@ class TestCOBYQA:
 
         callback = Callback()
 
-        # Minimize with method='COBYQA'
-        cons = NonlinearConstraint(self.con, 0, 0)
-        sol = minimize(self.fun, self.x0, method='cobyqa', constraints=cons,
-                       callback=callback, options=self.opts)
-        assert_allclose(sol.x, self.solution, atol=1e-4)
-        assert_(sol.success, sol.message)
-        assert_(sol.maxcv < 1e-5, sol)
-        assert_(sol.nfev < 70, sol)
-        assert_(sol.fun < self.fun(self.solution) + 1e-3, sol)
-        assert_(sol.nfev == callback.n_calls,
-                "Callback is not called exactly once for every function eval.")
-
-    def test_minimize_constraint_violation(self):
-        np.random.seed(1234)
-        pb = np.random.rand(10, 10)
-        spread = np.random.rand(10)
-
-        def p(w):
-            return pb.dot(w)
-
-        def f(w):
-            return -(w * spread).sum()
-
-        def c(w):
-            return abs(p(w)).sum()
-
-        cons = (
-            NonlinearConstraint(c, -np.inf, 5),
-            NonlinearConstraint(c, 6, np.inf),
+        # Minimize with method='cobyqa'.
+        constraints = NonlinearConstraint(self.con, 0.0, 0.0)
+        sol = minimize(
+            self.fun,
+            self.x0,
+            method='cobyqa',
+            constraints=constraints,
+            callback=callback,
+            options=self.options,
         )
-        w0 = np.zeros((10,))
-        sol = minimize(f, w0, method='cobyqa', constraints=cons,
-                       options={'feasibility_tol': 1e-6})
-        assert_(sol.maxcv > 1e-6)
-        assert_(not sol.success)
+        solution = [np.sqrt(25.0 - 4.0 / 9.0), 2.0 / 3.0]
+        assert_allclose(sol.x, solution, atol=1e-4)
+        assert_(sol.success, sol.message)
+        assert_(sol.maxcv < 1e-8, sol)
+        assert_(sol.nfev <= 100, sol)
+        assert_(sol.fun < self.fun(solution) + 1e-3, sol)
+        assert_(
+            sol.nfev == callback.n_calls,
+            "Callback is not called exactly once for every function eval.",
+        )
 
+    def test_minimize_bounds(self):
+        # Case where the bounds are not active at the solution.
+        bounds = Bounds([4.5, 0.6], [5.0, 0.7])
+        constraints = NonlinearConstraint(self.con, 0.0, 0.0)
+        sol = minimize(
+            self.fun,
+            self.x0,
+            method='cobyqa',
+            bounds=bounds,
+            constraints=constraints,
+            options=self.options,
+        )
+        solution = [np.sqrt(25.0 - 4.0 / 9.0), 2.0 / 3.0]
+        assert_allclose(sol.x, solution, atol=1e-4)
+        assert_(sol.success, sol.message)
+        assert_(sol.maxcv < 1e-8, sol)
+        assert_(sol.nfev <= 100, sol)
+        assert_(sol.fun < self.fun(solution) + 1e-3, sol)
 
-def test_vector_constraints():
-    # test that minimize can take a combination of constraints, some returning a
-    # number and others an array
-    def fun(x):
-        return (x[0] - 1)**2 + (x[1] - 2.5)**2
+        # Case where the bounds are active at the solution.
+        bounds = Bounds([5.0, 0.6], [5.5, 0.65])
+        sol = minimize(
+            self.fun,
+            self.x0,
+            method='cobyqa',
+            bounds=bounds,
+            constraints=constraints,
+            options=self.options,
+        )
+        assert_(not sol.success, sol.message)
+        assert_(sol.maxcv > 0.35, sol)
+        assert_(sol.nfev <= 100, sol)
 
-    def fmin(x):
-        return fun(x) - 1
+    def test_minimize_linear_constraints(self):
+        constraints = LinearConstraint([1.0, 1.0], 1.0, 1.0)
+        sol = minimize(
+            self.fun,
+            self.x0,
+            method='cobyqa',
+            constraints=constraints,
+            options=self.options,
+        )
+        solution = [(4 - np.sqrt(7)) / 3, (np.sqrt(7) - 1) / 3]
+        assert_allclose(sol.x, solution, atol=1e-4)
+        assert_(sol.success, sol.message)
+        assert_(sol.maxcv < 1e-8, sol)
+        assert_(sol.nfev <= 100, sol)
+        assert_(sol.fun < self.fun(solution) + 1e-3, sol)
 
-    def cons1(x):
-        a = np.array([[1, -2, 2], [-1, -2, 6], [-1, 2, 2]])
-        return np.array([a[i, 0] * x[0] + a[i, 1] * x[1] +
-                         a[i, 2] for i in range(len(a))])
-
-    def cons2(x):
-        return x     # identity, acts as bounds x > 0
-
-    x0 = np.array([2, 0])
-    cons_list = [fun, cons1, cons2]
-
-    xsol = [1.4, 1.7]
-    fsol = 0.8
-
-    # testing minimize
-    constraints = [{'type': 'ineq', 'fun': cons} for cons in cons_list]
-    sol = minimize(fun, x0, constraints=constraints, tol=1e-5)
-    assert_allclose(sol.x, xsol, atol=1e-4)
-    assert_(sol.success, sol.message)
-    assert_allclose(sol.fun, fsol, atol=1e-4)
-
-    constraints = {'type': 'ineq', 'fun': fmin}
-    sol = minimize(fun, x0, constraints=constraints, tol=1e-5)
-    assert_allclose(sol.fun, 1, atol=1e-4)
-
-
-class TestBounds:
-    # Test cobyqa support for bounds
-    # Invalid bounds is tested in
-    # test_optimize.TestOptimizeSimple.test_minimize_invalid_bounds
-
-    def test_basic(self):
-        def f(x):
-            return np.sum(x**2)
-
-        lb = [-1, None, 1, None, -0.5]
-        ub = [-0.5, -0.5, None, None, -0.5]
-        bounds = [(a, b) for a, b in zip(lb, ub)]
-        # these are converted to Bounds internally
-
-        res = minimize(f, x0=[1, 2, 3, 4, 5], method='cobyqa', bounds=bounds)
-        ref = [-0.5, -0.5, 1, 0, -0.5]
-        assert res.success
-        assert_allclose(res.x, ref, atol=1e-3)
-
-    def test_unbounded(self):
-        def f(x):
-            return np.sum(x**2)
-
-        bounds = Bounds([-np.inf, -np.inf], [np.inf, np.inf])
-        res = minimize(f, x0=[1, 2], method='cobyqa', bounds=bounds)
-        assert res.success
-        assert_allclose(res.x, 0, atol=1e-3)
-
-        bounds = Bounds([1, -np.inf], [np.inf, np.inf])
-        res = minimize(f, x0=[1, 2], method='cobyqa', bounds=bounds)
-        assert res.success
-        assert_allclose(res.x, [1, 0], atol=1e-3)
+    def test_minimize_args(self):
+        constraints = NonlinearConstraint(self.con, 0.0, 0.0)
+        sol = minimize(
+            self.fun,
+            self.x0,
+            (2.0,),
+            'cobyqa',
+            constraints=constraints,
+            options=self.options,
+        )
+        solution = [np.sqrt(25.0 - 4.0 / 36.0), 2.0 / 6.0]
+        assert_allclose(sol.x, solution, atol=1e-4)
+        assert_(sol.success, sol.message)
+        assert_(sol.maxcv < 1e-8, sol)
+        assert_(sol.nfev <= 100, sol)
+        assert_(sol.fun < self.fun(solution, 2.0) + 1e-3, sol)
