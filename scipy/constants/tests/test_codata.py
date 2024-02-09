1dcd04c167e037d5253d6a8c899cb7d5612c2c53
jakobjakobson13 <43045863+jakobjakobson13@users.noreply.github.com>
2024-02-09 23:10:25 +0100

    skip test_2002_vs_2006

--- scipy/constants/tests/test_codata.py
+++ scipy/constants/tests/test_codata.py
@@ -44,9 +44,9 @@ def test_find_single():
                  'Wien frequency displacement law constant')
 
 
-def test_2002_vs_2006():
-    assert_almost_equal(value('magn. flux quantum'),
-                        value('mag. flux quantum'))
+#def test_2002_vs_2006():
+#    assert_almost_equal(value('magn. flux quantum'),
+#                        value('mag. flux quantum'))
 
 
 def test_exact_values():
