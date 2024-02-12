f4e2dd3a70933e85a5c1f28b5dc9b92dc2728c58
mattip <matti.picus@gmail.com>
2024-02-12 13:44:05 +0200

    typo [wheel build]

--- tools/wheels/cibw_before_build.sh
+++ tools/wheels/cibw_before_build.sh
@@ -37,15 +37,15 @@ PKG_CONFIG_PATH=$PROJECT_DIR/.openblas
 rm -rf $PKG_CONFIG_PATH
 mkdir -p $PKG_CONFIG_PATH
 python -m pip install -r requirements/openblas_requirements.txt
-python -c "import scipy_openblas64; print(scipy_openblas64.get_pkg_config())" > $PKG_CONFIG_PATH/scipy-openblas.pc
+python -c "import scipy_openblas32; print(scipy_openblas32.get_pkg_config())" > $PKG_CONFIG_PATH/scipy-openblas.pc
 # Copy the shared objects to a path under $PKG_CONFIG_PATH, the build
 # will point $LD_LIBRARY_PATH there and then auditwheel/delocate-wheel will
 # pull these into the wheel. Use python to avoid windows/posix problems
 python <<EOF
-import os, scipy_openblas64, shutil
-srcdir = os.path.join(os.path.dirname(scipy_openblas64.__file__), "lib")
+import os, scipy_openblas32, shutil
+srcdir = os.path.join(os.path.dirname(scipy_openblas32.__file__), "lib")
 shutil.copytree(srcdir, os.path.join("$PKG_CONFIG_PATH", "lib"))
-srcdir = os.path.join(os.path.dirname(scipy_openblas64.__file__), ".dylibs")
+srcdir = os.path.join(os.path.dirname(scipy_openblas32.__file__), ".dylibs")
 if os.path.exists(srcdir):  # macosx delocate
     shutil.copytree(srcdir, os.path.join("$PKG_CONFIG_PATH", ".dylibs"))
 EOF
