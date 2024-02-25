6b826ee8d2902e1d036a8e9b4ead2753ed4b0377
Yagiz Olmez <yagiz.olmez@gmail.com>
2024-02-25 00:08:03 -0600

    Used a smaller prime number and removed decimate()

--- scipy/signal/_signaltools.py
+++ scipy/signal/_signaltools.py
@@ -3110,9 +3110,9 @@ def resample(x, num, t=None, axis=0, window=None, domain='time'):
     As noted, `resample` uses FFT transformations, which can be very
     slow if the number of input or output samples is large and prime;
     see `scipy.fft.fft`. In such cases, it can be faster to use the polyphase
-    method in `scipy.signal.resample_poly`. If ``num < len(x)``, you can consider
-    downsampling your signal with `scipy.signal.decimate` before applying
-    `resample`.
+    method in `scipy.signal.resample_poly`. If you need to set the number
+    of samples in the resampled signal to a specific value, you can use
+    `scipy.signal.resample_poly` before using `scipy.signal.resample`.
 
     Examples
     --------
@@ -3132,18 +3132,18 @@ def resample(x, num, t=None, axis=0, window=None, domain='time'):
     >>> plt.legend(['data', 'resampled'], loc='best')
     >>> plt.show()
 
-    If ``num < len(x)``, downsampling with `scipy.signal.decimate` before
-    applying `resample` can provide a significant speed up. Consider the
-    following example where ``len(x)`` is a large prime number:
+    If you need to set the sampling rate to a specific value, you can use 
+    `scipy.signal.resample_poly` before using `scipy.signal.resample`. 
+    Consider the following example where ``len(x)`` is a large prime number:
 
-    >>> N, num = 49379329, 2022590
+    >>> N, num = 55949, 5000
     >>> freq = 100
     >>> x = np.linspace(0, 1, N)
     >>> y = np.cos(2 * np.pi * freq * x)
 
     In this case
 
-    >>> f = signal.resample(signal.decimate(y, N // num), num)
+    >>> f = signal.resample(signal.resample_poly(y, 1, N // num), num)
 
     runs significantly faster than
 
