cea32a44e8587e23a5be7d2b20a1287d1667e8e8
Yoch Melka <yoch.melka@gmail.com>
2024-02-28 10:15:21 +0200

    remove unused imports

--- scipy/sparse/tests/test_dok.py
+++ scipy/sparse/tests/test_dok.py
@@ -1,7 +1,6 @@
-import numpy as np
 from numpy.testing import assert_array_almost_equal
 from scipy.sparse import dok_matrix
-import pytest
+
 
 def test_fromkeys_default():
     # test default value value
