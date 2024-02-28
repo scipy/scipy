c6ca47215148e5303a9dbe7f99a411f2d4309734
Yoch Melka <yoch.melka@gmail.com>
2024-02-28 13:45:40 +0200

    requested changes

--- scipy/sparse/tests/test_dok.py
+++ scipy/sparse/tests/test_dok.py
@@ -1,30 +1,30 @@
-from numpy.testing import assert_array_almost_equal
-from scipy.sparse import dok_matrix
+from numpy.testing import assert_array_equal
+from scipy.sparse import dok_array
 
 
 def test_fromkeys_default():
-    # test default value value
-    edges = [(0,2), (1,0), (2,1)]
-    Xdok = dok_matrix.fromkeys(edges)
-    assert_array_almost_equal(
+    # test with default value
+    edges = [(0, 2), (1, 0), (2, 1)]
+    Xdok = dok_array.fromkeys(edges)
+    assert_array_equal(
+        Xdok.todense(),
         [[0, 0, 1], [1, 0, 0], [0, 1, 0]],
-        Xdok.todense()
     )
 
 def test_fromkeys_positional():
-    # test with value (positional)
-    edges = [(0,2), (1,0), (2,1)]
-    Xdok = dok_matrix.fromkeys(edges, -1)
-    assert_array_almost_equal(
+    # test with positional value
+    edges = [(0, 2), (1, 0), (2, 1)]
+    Xdok = dok_array.fromkeys(edges, -1)
+    assert_array_equal(
+        Xdok.todense(),
         [[0, 0, -1], [-1, 0, 0], [0, -1, 0]],
-        Xdok.todense()
     )
 
 def test_fromkeys_kwargs():
-    # test with value (kwarg)
-    edges = [(0,2), (1,0), (2,1)]
-    Xdok = dok_matrix.fromkeys(edges, value=-1)
-    assert_array_almost_equal(
+    # test with kwarg value
+    edges = [(0,2), (1,0), (2, 1)]
+    Xdok = dok_array.fromkeys(edges, value=-1)
+    assert_array_equal(
+        Xdok.todense(),
         [[0, 0, -1], [-1, 0, 0], [0, -1, 0]],
-        Xdok.todense()
     )
