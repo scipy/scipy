fc66dccffd797e461444467b0083eea367544a1e
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2024-03-01 16:02:19 +0300

    MAINT: appease the linter

--- scipy/conftest.py
+++ scipy/conftest.py
@@ -13,6 +13,9 @@ from scipy._lib._fpumode import get_fpu_mode
 from scipy._lib._testutils import FPUModeChangeWarning
 from scipy._lib._array_api import SCIPY_ARRAY_API, SCIPY_DEVICE
 
+from scpdt.conftest import dt_config
+from contextlib import contextmanager
+
 
 def pytest_configure(config):
     config.addinivalue_line("markers",
@@ -243,11 +246,6 @@ hypothesis.settings.load_profile(SCIPY_HYPOTHESIS_PROFILE)
 ############################################################################
 # doctesting stuff
 
-from scpdt.conftest import dt_config
-from contextlib import contextmanager
-import warnings
-
-
 # FIXME: populate the dict once
 @contextmanager
 def warnings_errors_and_rng(test):
@@ -305,7 +303,6 @@ def warnings_errors_and_rng(test):
     known_warnings.update(dct)
 
     # these legitimately emit warnings in examples
-    from scipy.signal._filter_design import BadCoefficients
     legit = set('scipy.signal.normalize')
 
     # Now, the meat of the matter: filter warnings,
@@ -365,6 +362,4 @@ dt_config.pytest_extra_xfail = {
 # tutorials
 dt_config.pseudocode = set(['integrate.nquad(func,'])
 dt_config.local_resources = {'io.rst': ["octave_a.mat"]}
-
-dt_config.nameerror_after_exception = True
 ############################################################################
