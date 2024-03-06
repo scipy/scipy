42c69a8deeac8f296ff2b7a0b684a53e9f339607
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2024-03-06 19:48:32 +0300

    TST: linalg: undo xfail TestEig::test_singular

--- scipy/linalg/tests/test_decomp.py
+++ scipy/linalg/tests/test_decomp.py
@@ -253,7 +253,6 @@ class TestEig:
         # Compare homogeneous and nonhomogeneous versions
         assert_allclose(sort(wh), sort(w[np.isfinite(w)]))
 
-    @pytest.mark.xfail(reason="See gh-2254")
     def test_singular(self):
         # Example taken from
         # https://web.archive.org/web/20040903121217/http://www.cs.umu.se/research/nla/singular_pairs/guptri/matlab.html
