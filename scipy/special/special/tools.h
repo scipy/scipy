bbb355229664fc268108e334f205094f1521cf3b
Albert Steppi <albert.steppi@gmail.com>
2024-03-19 15:04:37 -0400

    Change to conventional template parameter T

--- scipy/special/special/tools.h
+++ scipy/special/special/tools.h
@@ -9,8 +9,8 @@ namespace special {
 namespace detail {
 
     // Series evaluators.
-    template <typename Generator, typename Number>
-    SPECFUN_HOST_DEVICE Number series_eval(Generator &g, Number init_val, double tol, std::uint64_t max_terms,
+    template <typename Generator, typename T>
+    SPECFUN_HOST_DEVICE T series_eval(Generator &g, T init_val, double tol, std::uint64_t max_terms,
                                            const char *func_name) {
         /* Sum an infinite series to a given precision.
          *
@@ -28,8 +28,8 @@ namespace detail {
          *     will ultimately be used. This is needed to pass to set_error in case
          *     of non-convergence.
          */
-        Number result = init_val;
-        Number term, previous;
+        T result = init_val;
+        T term, previous;
         for (std::uint64_t i = 0; i < max_terms; ++i) {
             term = g();
             result += term;
@@ -39,16 +39,16 @@ namespace detail {
         }
         // Exceeded max terms without converging. Return NaN.
         set_error(func_name, SF_ERROR_NO_RESULT, NULL);
-        if constexpr (std::is_floating_point<Number>::value) {
-            return std::numeric_limits<Number>::quiet_NaN();
+        if constexpr (std::is_floating_point<T>::value) {
+            return std::numeric_limits<T>::quiet_NaN();
         }
         // If result type is not a floating point type, assume it is complex.
-        using FloatingType = typename Number::value_type;
-        return Number(std::numeric_limits<FloatingType>::quiet_NaN(), std::numeric_limits<FloatingType>::quiet_NaN());
+        using FloatingType = typename T::value_type;
+        return T(std::numeric_limits<FloatingType>::quiet_NaN(), std::numeric_limits<FloatingType>::quiet_NaN());
     }
 
-    template <typename Generator, typename Number>
-    SPECFUN_HOST_DEVICE Number series_eval_fixed_length(Generator &g, Number init_val, std::uint64_t num_terms) {
+    template <typename Generator, typename T>
+    SPECFUN_HOST_DEVICE T series_eval_fixed_length(Generator &g, T init_val, std::uint64_t num_terms) {
         /* Sum a fixed number of terms from a series.
          *
          * g : a generator of terms for the series.
@@ -59,7 +59,7 @@ namespace detail {
          * max_terms : The number of terms from the series to sum.
          *
          */
-        Number result = init_val;
+        T result = init_val;
         for (std::uint64_t i = 0; i < num_terms; ++i) {
             result += g();
         }
