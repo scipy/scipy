6b32a01c940b73faf23781804bcc77d4291b2cdb
Atsushi Sakai <asakai.amsl+github@gmail.com>
2024-03-28 22:31:12 +0900

    BUG: optimize: Fix lint CI

--- scipy/optimize/tests/test_linesearch.py
+++ scipy/optimize/tests/test_linesearch.py
@@ -17,8 +17,8 @@ def assert_wolfe(s, phi, derphi, c1=1e-4, c2=0.9, err_msg=""):
     phi0 = phi(0)
     derphi0 = derphi(0)
     derphi1 = derphi(s)
-    msg = "s = {}; phi(0) = {}; phi(s) = {}; phi'(0) = {}; phi'(s) = {}; {}".format(
-        s, phi0, phi1, derphi0, derphi1, err_msg)
+    msg = (f"s = {s}; phi(0) = {phi0}; phi(s) = {phi1}; phi'(0) = {derphi0};"
+           f" phi'(s) = {derphi1}; {err_msg}")
 
     assert phi1 <= phi0 + c1*s*derphi0, "Wolfe 1 failed: " + msg
     assert abs(derphi1) <= abs(c2*derphi0), "Wolfe 2 failed: " + msg
