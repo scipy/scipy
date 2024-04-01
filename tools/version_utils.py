b85940a1b9b8e7f2d4a2965a49753e96d9ec4e52
Tyler Reddy <tyler.je.reddy@gmail.com>
2024-04-01 15:02:36 -0600

    DOC, REL: set 1.13.0 final unreleased
    * Prepare for SciPy `1.13.0` "final" release
    instead of RC2 given urgency of supporting
    NumPy 2 series.
    
    * Remove note about SciPy `1.13.0` not being
    released yet in the release notes, and update
    the `1.13.0` release notes to reflect another
    bump in the OpenBLAS version vendored in for PyPI
    wheels.
    
    * The usual version bumps removing `rc2` version
    flags, and author/pr/issue list updates.

--- tools/version_utils.py
+++ tools/version_utils.py
@@ -8,7 +8,7 @@ MINOR = 13
 MICRO = 0
 ISRELEASED = False
 IS_RELEASE_BRANCH = True
-VERSION = '%d.%d.%drc2' % (MAJOR, MINOR, MICRO)
+VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
 
 def get_version_info(source_root):
