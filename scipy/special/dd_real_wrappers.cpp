c872b7dbc240af05f26a0afd34b3a3d5bc89960a
Albert Steppi <albert.steppi@gmail.com>
2024-04-05 23:30:24 -0400

    Remove C cephes and finishing touches that allow this

--- scipy/special/dd_real_wrappers.cpp
+++ scipy/special/dd_real_wrappers.cpp
@@ -9,6 +9,10 @@ extern "C" double2 dd_create_d(double x) {
     return {x, 0.0};
 }
 
+extern "C" double2 dd_create(double x, double y) {
+    return {x, y};
+}
+
 extern "C" double2 dd_add(const double2* a, const double2* b) {
     special::dd_real::DoubleDouble dd_a(a->hi, a->lo);
     special::dd_real::DoubleDouble dd_b(b->hi, b->lo);
@@ -30,6 +34,20 @@ extern "C" double2 dd_div(const double2* a, const double2* b) {
     return {result.hi, result.lo};
 }
 
+extern "C" double2 dd_exp(const double2* x) {
+    special::dd_real::DoubleDouble dd_x(x->hi, x->lo);
+    special::dd_real::DoubleDouble result = special::dd_real::exp(dd_x);
+    return {result.hi, result.lo};
+}
+
+extern "C" double2 dd_log(const double2* x) {
+    special::dd_real::DoubleDouble dd_x(x->hi, x->lo);
+    special::dd_real::DoubleDouble result = special::dd_real::log(dd_x);
+    return {result.hi, result.lo};
+}
+
 extern "C" double dd_to_double(const double2* a) {
     return a->hi;
 }
+
+
--- scipy/special/dd_real_wrappers.h
+++ scipy/special/dd_real_wrappers.h
@@ -10,9 +10,12 @@ extern "C" {
     } double2;
 
     double2 dd_create_d(double x);
+    double2 dd_create(double x, double y);
     double2 dd_add(const double2* a, const double2* b);
     double2 dd_mul(const double2* a, const double2* b);
     double2 dd_div(const double2* a, const double2* b);
+    double2 dd_exp(const double2* x);
+    double2 dd_log(const double2* x);
     double dd_to_double(const double2* a);
 
 #ifdef __cplusplus
