6155a6342138a2e8b741d8fd9df9e233bbb8f44e
izaid <hi@irwinzaid.com>
2024-04-11 16:08:00 +0100

    Fix for linter

--- scipy/special/_basic.py
+++ scipy/special/_basic.py
@@ -14,7 +14,8 @@ from . import _ufuncs
 from ._ufuncs import (mathieu_a, mathieu_b, iv, jv, gamma,
                       psi, hankel1, hankel2, yv, kv, poch, binom,
                       _stirling2_inexact)
-from ._gufuncs import _lpn, _lpmn, _clpmn, _lqn, _lqmn, _rctj, _rcty, _sph_harm_all as __sph_harm_all
+from ._gufuncs import (_lpn, _lpmn, _clpmn, _lqn, _lqmn, _rctj, _rcty,
+                       _sph_harm_all as __sph_harm_all)
 from . import _specfun
 from ._comb import _comb_int
 from scipy._lib.deprecation import _NoValue, _deprecate_positional_args
