f59c51871eb38b4c3cb6fb6d168e8b7a96796577
izaid <hi@irwinzaid.com>
2024-04-13 21:22:14 +0100

    Cleaned up rank metaprogramming for gufuncs

--- scipy/special/ufunc.h
+++ scipy/special/ufunc.h
@@ -45,26 +45,44 @@ template <typename Func>
 constexpr size_t arity_of_v = arity_of<Func>::value;
 
 template <typename F, size_t I>
-struct argument_rank;
+struct argument_of;
 
-template <typename Res, typename... Args, size_t I>
-struct argument_rank<Res(Args...), I> {
+template <typename Res, typename Arg0, typename... Args>
+struct argument_of<Res(Arg0, Args...), 0> {
+    using type = Arg0;
+};
+
+template <typename Res, typename Arg0, typename... Args, size_t I>
+struct argument_of<Res(Arg0, Args...), I> {
+    using type = typename argument_of<Res(Args...), I - 1>::type;
+};
+
+template <typename F, size_t I>
+using argument_of_t = typename argument_of<F, I>::type;
+
+template <typename T>
+struct rank_of {
     static constexpr size_t value = 0;
 };
 
-template <typename Res, typename T, typename Extents, typename LayoutPolicy, typename AccessorPolicy, typename... Args>
-struct argument_rank<Res(std::mdspan<T, Extents, LayoutPolicy, AccessorPolicy>, Args...), 0> {
+template <typename T, typename Extents, typename LayoutPolicy, typename AccessorPolicy>
+struct rank_of<std::mdspan<T, Extents, LayoutPolicy, AccessorPolicy>> {
     static constexpr size_t value = Extents::rank();
 };
 
-template <typename Res, typename T, typename Extents, typename LayoutPolicy, typename AccessorPolicy, typename... Args,
-          size_t I>
-struct argument_rank<Res(std::mdspan<T, Extents, LayoutPolicy, AccessorPolicy>, Args...), I> {
-    static constexpr size_t value = Extents::rank() + argument_rank<Res(Args...), I - 1>::value;
+template <typename T>
+inline constexpr size_t rank_of_v = rank_of<T>::value;
+
+template <typename Func, size_t I, typename J = std::make_index_sequence<I>>
+struct npy_steps_offset;
+
+template <typename Res, typename... Args, size_t I, size_t... J>
+struct npy_steps_offset<Res(Args...), I, std::index_sequence<J...>> {
+    static constexpr size_t value = (rank_of_v<argument_of_t<Res(Args...), J>> + ... + 0);
 };
 
-template <typename T, size_t N>
-constexpr size_t argument_rank_v = argument_rank<T, N>::value;
+template <typename Func, size_t I>
+constexpr size_t npy_steps_offset_v = npy_steps_offset<Func, I>::value;
 
 // Maps a C++ type to a NumPy type
 template <typename T>
@@ -340,7 +358,8 @@ struct ufunc_traits<Res(Args...), std::index_sequence<I...>> {
     static void loop_func(char **args, const npy_intp *dimensions, const npy_intp *steps, void *data) {
         Res (*func)(Args...) = static_cast<ufunc_data<Res (*)(Args...)> *>(data)->func;
         for (npy_intp i = 0; i < dimensions[0]; ++i) {
-            const Res &res = func(npy_get<Args>(args[I], dimensions + 1, steps + sizeof...(Args) + 1)...);
+            const Res &res = func(npy_get<Args>(args[I], dimensions + 1,
+                                                steps + sizeof...(Args) + 1 + npy_steps_offset_v<Res(Args...), I>)...);
             npy_set(args[sizeof...(Args)], res); // assign to the output pointer
 
             for (npy_uintp j = 0; j < sizeof...(Args); ++j) {
@@ -361,8 +380,8 @@ struct ufunc_traits<void(Args...), std::index_sequence<I...>> {
     static void loop_func(char **args, const npy_intp *dimensions, const npy_intp *steps, void *data) {
         void (*func)(Args...) = static_cast<ufunc_data<void (*)(Args...)> *>(data)->func;
         for (npy_intp i = 0; i < dimensions[0]; ++i) {
-            func(
-                npy_get<Args>(args[I], dimensions + 1, steps + sizeof...(Args) + argument_rank_v<void(Args...), I>)...);
+            func(npy_get<Args>(args[I], dimensions + 1,
+                               steps + sizeof...(Args) + npy_steps_offset_v<void(Args...), I>)...);
 
             for (npy_uintp j = 0; j < sizeof...(Args); ++j) {
                 args[j] += steps[j];
