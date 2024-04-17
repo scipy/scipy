6f6de925353f361cffd64edf8494f26ed9926ec2
H. Vetinari <h.vetinari@gmx.com>
2024-04-17 11:53:39 +1100

    MAINT: remove incidental imports in interpolate.py

--- scipy/interpolate/interpolate.py
+++ scipy/interpolate/interpolate.py
@@ -14,23 +14,18 @@ __all__ = [  # noqa: F822
     'RegularGridInterpolator',
     'array',
     'asarray',
-    'atleast_1d',
-    'atleast_2d',
     'comb',
     'dfitpack',
     'interp1d',
     'interp2d',
     'interpn',
-    'intp',
     'itertools',
     'lagrange',
     'make_interp_spline',
     'poly1d',
     'prod',
-    'ravel',
     'searchsorted',
     'spec',
-    'transpose',
 ]
 
 
