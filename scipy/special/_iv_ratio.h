09ad33811124f367218514823c269a18145e7da4
fancidev <fancidev@gmail.com>
2024-04-18 21:42:02 +0800

    Rearrange formula to simplify code and improve accuracy.

--- scipy/special/_iv_ratio.h
+++ scipy/special/_iv_ratio.h
@@ -4,37 +4,36 @@
 
 #include "special/tools.h"
 #include "special/error.h"
-#include <cstdint>    // for std::uint64_t
-#include <cmath>      // for std::frexp, std::ldexp, std::fmax
-#include <utility>    // for std::pair
+#include <cstdint>    // for uint64_t
+#include <cmath>      // for frexp, ldexp, fmax, fma
+#include <utility>    // for pair
 
-/* Generates the Perron continued fraction for `iv(v, x) / iv(v-1, x)` for
- * v >= 1 and x >= 0.
+/* Generates the "tail" of Perron's continued fraction for `iv(v,x)/iv(v-1,x)`
+ * for v >= 1 and x >= 0.
  *
- * The formulas are given in [1].  We additionally perform an equivalent
- * transform of the c.f. to avoid overflow.
+ * The Perron continued fraction is studied in [1].  It is given by
  *
- *    iv(v, x)      xc    -(2vc+c)(xc) -(2vc+3c)(xc) -(2vc+5c)(xc)
- *   --------- = -------- ------------ ------------- ------------- ...
- *   iv(v-1,x)   2vc+xc + 2(vc+xc)+c + 2(vc+xc)+2c + 2(vc+xc)+3c +
+ *       iv(v, x)      x    -(2v+1)x   -(2v+3)x   -(2v+5)x
+ *   f = --------- = ------ ---------- ---------- ---------- ...
+ *       iv(v-1,x)   2v+x + 2(v+x)+1 + 2(v+x)+2 + 2(v+x)+3 +
+ *
+ * Rearrange the expression by making an equivalent transform to prevent
+ * floating point overflow and extracting the first fraction to simplify
+ * the recurrence relation.  This leads to
+ *
+ *    xc            -(2vc+c)(xc) -(2vc+3c)(xc) -(2vc+5c)(xc)
+ *   --- = 2vc+xc + ------------ ------------- ------------- ...
+ *    f             2(vc+xc)+c + 2(vc+xc)+2c + 2(vc+xc)+3c +
+ *
+ * This class generates the fractions after 2vc+xc.
  *
  * [1] Gautschi, W. and Slavik, J. (1978). "On the computation of modified
- * Bessel function ratios." Mathematics of Computation, 32(143):865-875.
+ *     Bessel function ratios." Mathematics of Computation, 32(143):865-875.
  */
-class IvRatioCFGenerator {
-    using frac_type = std::pair<double, double>;
+struct IvRatioCFTailGenerator {
 
-public:
-
-    // It is assumed that v >= 1, x >= 0, and both are finite.
-    IvRatioCFGenerator(double v, double x) noexcept {
-        int e;
-        std::frexp(std::fmax(v, x), &e);
-        double c = std::ldexp(1, 1-e); // rescaling multiplier
-
-        double vc = v * c;
-        double xc = x * c;
-        frac_ = {xc, 2*vc+xc};
+    // It is assumed that v >= 1, x >= 0, c > 0, and all are finite.
+    IvRatioCFTailGenerator(double vc, double xc, double c) noexcept {
         a0_ = -(2*vc-c)*xc;
         as_ = -2*c*xc;
         b0_ = 2*(vc+xc);
@@ -42,18 +41,15 @@ public:
         k_ = 0;
     }
 
-    frac_type operator()() {
-        frac_type frac = frac_;
-        double k = ++k_;
-        frac_ = {std::fma(k, as_, a0_), std::fma(k, bs_, b0_)};
-        return frac;
+    std::pair<double, double> operator()() {
+        ++k_;
+        return {std::fma(k_, as_, a0_), std::fma(k_, bs_, b0_)};
     }
 
 private:
-    frac_type frac_;  // current fraction
     double a0_, as_;  // a[k] == a0 + as*k, k >= 1
     double b0_, bs_;  // b[k] == b0 + bs*k, k >= 1
-    std::uint64_t k_; // current index (0-based)
+    std::uint64_t k_; // current index
 };
 
 inline double iv_ratio(double v, double x) {
@@ -78,17 +74,25 @@ inline double iv_ratio(double v, double x) {
     }
 
     // Now v >= 0 and x >= 0 and both are finite.
-    IvRatioCFGenerator cf(v+1, x);
+    v += 1;
 
+    int e;
+    std::frexp(std::fmax(v, x), &e);
+    double c = std::ldexp(1, 1-e); // rescaling multiplier
+    double vc = v * c;
+    double xc = x * c;
+
+    IvRatioCFTailGenerator cf(vc, xc, c);
     auto [result, terms] = special::detail::series_eval_kahan(
         special::detail::continued_fraction_series(cf),
         std::numeric_limits<double>::epsilon() * 0.5,
-        1000);
+        1000,
+        2*vc+xc);
 
     if (terms == 0) { // failed to converge; should not happen
         special::set_error("iv_ratio", SF_ERROR_NO_RESULT, NULL);
         return std::numeric_limits<double>::signaling_NaN();
     }
 
-    return result;
+    return xc / result;
 }
