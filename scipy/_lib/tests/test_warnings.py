e359f2d3d4626bf63c89986ad9e184a59350975a
Jake Bowhay <jb9.bowhay@gmail.com>
2024-04-20 13:56:03 +0100

    TST: _lib: remove redundant xfailed test
    [skip ci]

--- scipy/_lib/tests/test_warnings.py
+++ scipy/_lib/tests/test_warnings.py
@@ -115,23 +115,3 @@ def test_warning_calls_filters(warning_calls):
             "found in:\n    {}".format(
                 "\n    ".join(bad_filters)))
 
-
-@pytest.mark.slow
-@pytest.mark.xfail(reason="stacklevels currently missing")
-def test_warning_calls_stacklevels(warning_calls):
-    bad_filters, bad_stacklevels = warning_calls
-
-    msg = ""
-
-    if bad_filters:
-        msg += ("warning ignore filter should not be used, instead, use\n"
-                "numpy.testing.suppress_warnings (in tests only);\n"
-                "found in:\n    {}".format("\n    ".join(bad_filters)))
-        msg += "\n\n"
-
-    if bad_stacklevels:
-        msg += "warnings should have an appropriate stacklevel:\n    {}".format(
-                "\n    ".join(bad_stacklevels))
-
-    if msg:
-        raise AssertionError(msg)
