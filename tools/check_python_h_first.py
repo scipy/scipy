735b62b6d6225c7a6f9025c15182a62444c35b14
DWesl <22566757+DWesl@users.noreply.github.com>
2024-04-20 08:32:08 -0400

    STY: Fix style issues of tools/check_python_h_first.py.

--- tools/check_python_h_first.py
+++ tools/check_python_h_first.py
@@ -1,27 +1,28 @@
-1#!/usr/bin/env python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """Check that Python.h is included before any stdlib headers.
 
 May be a bit overzealous, but it should get the job done.
 """
 import argparse
-import io
 import os.path
 import re
 import sys
 
-HEADER_PATTERN = re.compile(r'^\s*#\s*include\s*[<"]((?:\w+/)*\w+(?:\.h[hp+]{0,2})?)[>"]\s*$')
+HEADER_PATTERN = re.compile(
+    r'^\s*#\s*include\s*[<"]((?:\w+/)*\w+(?:\.h[hp+]{0,2})?)[>"]\s*$'
+)
 
 PYTHON_INCLUDING_HEADERS = [
-  "Python.h",
-  "numpy/arrayobject.h",
-  "numpy/ndarrayobject.h",
-  "numpy/npy_common.h",
-  "numpy/npy_math.h",
-  "numpy/random/distributions.h",
-  "pybind11/pybind11.h",
-  # Python-including headers the sort doesn't pick up
-  "ni_support.h",
+    "Python.h",
+    "numpy/arrayobject.h",
+    "numpy/ndarrayobject.h",
+    "numpy/npy_common.h",
+    "numpy/npy_math.h",
+    "numpy/random/distributions.h",
+    "pybind11/pybind11.h",
+    # Python-including headers the sort doesn't pick up
+    "ni_support.h",
 ]
 
 PARSER = argparse.ArgumentParser(description=__doc__)
@@ -45,7 +46,7 @@ def check_python_h_included_first(name_to_check: str) -> int:
         The number of headers before Python.h
     """
     included_python = False
-    included_other = []
+    included_non_python_header = []
     warned_python_construct = False
     basename_to_check = os.path.basename(name_to_check)
     in_comment = False
@@ -64,43 +65,51 @@ def check_python_h_included_first(name_to_check: str) -> int:
                 continue
             match = re.match(HEADER_PATTERN, line)
             if match:
-                if match.group(1) in PYTHON_INCLUDING_HEADERS:
-                    if included_other and not included_python:
+                this_header = match.group(1)
+                if this_header in PYTHON_INCLUDING_HEADERS:
+                    if included_non_python_header and not included_python:
                         print(
                             f"Header before Python.h in file {name_to_check:s}\n"
-                            f"Python.h on line {i:d}, other header(s) on line(s) {included_other}",
-                            file=sys.stderr
+                            f"Python.h on line {i:d}, other header(s) on line(s)"
+                            f" {included_non_python_header}",
+                            file=sys.stderr,
                         )
                     included_python = True
                     PYTHON_INCLUDING_HEADERS.append(basename_to_check)
-                elif not included_python and ("numpy" in match.group(1) and match.group(1) != "numpy/utils.h"):
-                        print(
-                            f"Python.h not included before python-including header "
-                            f"in file {name_to_check:s}\n"
-                            f"pybind11/pybind11.h on line {i:d}",
-                            file=sys.stderr
-                        )
+                elif not included_python and (
+                    "numpy" in this_header and this_header != "numpy/utils.h"
+                ):
+                    print(
+                        f"Python.h not included before python-including header "
+                        f"in file {name_to_check:s}\n"
+                        f"pybind11/pybind11.h on line {i:d}",
+                        file=sys.stderr,
+                    )
                 elif not included_python:
-                    included_other.append(i)
-            elif (not included_python and not warned_python_construct and ".h" not in basename_to_check) and (
-                "py::" in line or "PYBIND11_" in line or "npy_" in line
-            ):
+                    included_non_python_header.append(i)
+            elif (
+                not included_python
+                and not warned_python_construct
+                and ".h" not in basename_to_check
+            ) and ("py::" in line or "PYBIND11_" in line or "npy_" in line):
                 print(
-                    "Python-including header not used before python constructs in file "
-                    f"{name_to_check:s}\nConstruct on line {i:d}",
-                    file=sys.stderr
+                    "Python-including header not used before python constructs "
+                    f"in file {name_to_check:s}\nConstruct on line {i:d}",
+                    file=sys.stderr,
                 )
                 warned_python_construct = True
-    return included_python and len(included_other)
+    return included_python and len(included_non_python_header)
 
 
 if __name__ == "__main__":
     args = PARSER.parse_args()
     n_out_of_order = 0
     # See which of the headers include Python.h and add them to the list
-    for name_to_check in sorted(args.file_list, key=lambda name: "h" not in name.lower()):
+    for name_to_check in sorted(
+        args.file_list, key=lambda name: "h" not in name.lower()
+    ):
         try:
             n_out_of_order += check_python_h_included_first(name_to_check)
-        except UnicodeDecodeError as err:
+        except UnicodeDecodeError:
             print(f"File {name_to_check:s} not utf-8", sys.stdout)
     sys.exit(n_out_of_order)
