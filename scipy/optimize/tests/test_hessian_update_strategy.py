2943b9c015547ed4103d5e088ec02cc628457013
Jonas Eschle <mayou36@jonas.eschle.com>
2024-04-24 17:08:54 -0400

    tests: use pytests raises

--- scipy/optimize/tests/test_hessian_update_strategy.py
+++ scipy/optimize/tests/test_hessian_update_strategy.py
@@ -1,5 +1,7 @@
 import numpy as np
 from copy import deepcopy
+
+import pytest
 from numpy.linalg import norm
 from numpy.testing import (TestCase, assert_array_almost_equal,
                            assert_array_equal, assert_array_less,
@@ -61,7 +63,7 @@ class TestHessianUpdateStrategy(TestCase):
         init_scales = (
             ('auto', np.eye(ndims)),
             (2, np.eye(ndims) * 2),
-            (np.arange(1, ndims + 1), np.arange(1, ndims + 1) * np.eye(ndims)),
+            (np.arange(1, ndims + 1) * np.eye(ndims), np.arange(1, ndims + 1) * np.eye(ndims)),
             (symmetric_matrix, symmetric_matrix),)
         for approx_type in ['hess', 'inv_hess']:
             for init_scale, true_matrix in init_scales:
@@ -128,7 +130,7 @@ class TestHessianUpdateStrategy(TestCase):
 
                 for qn in quasi_newton:
                     qn.initialize(ndims, approx_type)
-                    with assert_raises(TypeError, match=message):
+                    with pytest.raises(TypeError, match=message):
                         qn.update(np.ones(ndims), np.arange(ndims))
 
     def test_rosenbrock_with_no_exception(self):
