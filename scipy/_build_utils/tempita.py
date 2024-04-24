ca221b03aec104c1d3804f83c358f1d3e2c1b16e
Ralf Gommers <ralf.gommers@gmail.com>
2024-04-24 10:32:54 +0200

    Revert "BLD: improve use of Tempita in meson.build files"
    This reverts commit 629872ac5b2901492cef40602ffc19c7b1456160.

--- scipy/_build_utils/tempita.py
+++ scipy/_build_utils/tempita.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 import sys
 import os
 import argparse
@@ -31,8 +30,6 @@ def main():
                         help="Path to the input file")
     parser.add_argument("-o", "--outdir", type=str,
                         help="Path to the output directory")
-    parser.add_argument("--outfile", type=str,
-                        help="Path to the output file (use either this or outdir)")
     parser.add_argument("-i", "--ignore", type=str,
                         help="An ignored input - may be useful to add a "
                              "dependency between custom targets")
@@ -41,15 +38,12 @@ def main():
     if not args.infile.endswith('.in'):
         raise ValueError(f"Unexpected extension: {args.infile}")
 
-    if not (args.outdir or args.outfile):
-        raise ValueError("Missing `--outdir` or `--outfile` argument to tempita.py")
+    if not args.outdir:
+        raise ValueError("Missing `--outdir` argument to tempita.py")
 
-    if args.outfile:
-        outfile = args.outfile
-    else:
-        outdir_abs = os.path.join(os.getcwd(), args.outdir)
-        outfile = os.path.join(outdir_abs,
-                               os.path.splitext(os.path.split(args.infile)[1])[0])
+    outdir_abs = os.path.join(os.getcwd(), args.outdir)
+    outfile = os.path.join(outdir_abs,
+                           os.path.splitext(os.path.split(args.infile)[1])[0])
 
     process_tempita(args.infile, outfile)
 
