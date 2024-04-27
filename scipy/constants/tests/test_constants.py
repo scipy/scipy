136884537bddc78ad09685cbfc607bbf0dd469b8
Jake Bowhay <jb9.bowhay@gmail.com>
2024-04-27 19:01:44 +0100

    TST: update tests to use xp.asarray

--- scipy/constants/tests/test_constants.py
+++ scipy/constants/tests/test_constants.py
@@ -7,27 +7,33 @@ from scipy._lib._array_api import xp_assert_equal, xp_assert_close
 
 @array_api_compatible
 def test_convert_temperature(xp):
-    xp_assert_equal(sc.convert_temperature(32, 'f', 'Celsius'), 0.0)
-    xp_assert_equal(sc.convert_temperature([0, 0], 'celsius', 'Kelvin'),
-                    [273.15, 273.15])
-    xp_assert_equal(sc.convert_temperature([0, 0], 'kelvin', 'c'),
-                    [-273.15, -273.15])
-    xp_assert_equal(sc.convert_temperature([32, 32], 'f', 'k'), [273.15, 273.15])
-    xp_assert_equal(sc.convert_temperature([273.15, 273.15], 'kelvin', 'F'),
-                    [32.0, 32.0])
-    xp_assert_equal(sc.convert_temperature([0, 0], 'C', 'fahrenheit'), [32.0, 32.0])
-    xp_assert_close(sc.convert_temperature([0, 0], 'c', 'r'), [491.67, 491.67],
+    xp_assert_equal(sc.convert_temperature(xp.asarray(32.), 'f', 'Celsius'),
+                    xp.asarray(0.0))
+    xp_assert_equal(sc.convert_temperature(xp.asarray([0., 0.]), 'celsius', 'Kelvin'),
+                    xp.asarray([273.15, 273.15]))
+    xp_assert_equal(sc.convert_temperature(xp.asarray([0., 0.]), 'kelvin', 'c'),
+                    xp.asarray([-273.15, -273.15]))
+    xp_assert_equal(sc.convert_temperature(xp.asarray([32., 32.]), 'f', 'k'),
+                    xp.asarray([273.15, 273.15]))
+    xp_assert_equal(sc.convert_temperature(xp.asarray([273.15, 273.15]), 'kelvin', 'F'),
+                    xp.asarray([32., 32.]))
+    xp_assert_equal(sc.convert_temperature(xp.asarray([0., 0.]), 'C', 'fahrenheit'),
+                    xp.asarray([32., 32.]))
+    xp_assert_close(sc.convert_temperature(xp.asarray([0., 0.]), 'c', 'r'),
+                    xp.asarray([491.67, 491.67]),
                     rtol=0., atol=1e-13)
-    xp_assert_close(sc.convert_temperature([491.67, 491.67], 'Rankine', 'C'),
-                    [0., 0.], rtol=0., atol=1e-13)
-    xp_assert_close(sc.convert_temperature([491.67, 491.67], 'r', 'F'),
-                    [32., 32.], rtol=0., atol=1e-13)
-    xp_assert_close(sc.convert_temperature([32, 32], 'fahrenheit', 'R'),
-                    [491.67, 491.67], rtol=0., atol=1e-13)
-    xp_assert_close(sc.convert_temperature([273.15, 273.15], 'K', 'R'),
-                    [491.67, 491.67], rtol=0., atol=1e-13)
-    xp_assert_close(sc.convert_temperature([491.67, 0.], 'rankine', 'kelvin'),
-                    [273.15, 0.], rtol=0., atol=1e-13)
+    xp_assert_close(sc.convert_temperature(xp.asarray([491.67, 491.67]),
+                                           'Rankine', 'C'),
+                    xp.asarray([0., 0.]), rtol=0., atol=1e-13)
+    xp_assert_close(sc.convert_temperature(xp.asarray([491.67, 491.67]), 'r', 'F'),
+                    xp.asarray([32., 32.]), rtol=0., atol=1e-13)
+    xp_assert_close(sc.convert_temperature(xp.asarray([32., 32.]), 'fahrenheit', 'R'),
+                    xp.asarray([491.67, 491.67]), rtol=0., atol=1e-13)
+    xp_assert_close(sc.convert_temperature(xp.asarray([273.15, 273.15]), 'K', 'R'),
+                    xp.asarray([491.67, 491.67]), rtol=0., atol=1e-13)
+    xp_assert_close(sc.convert_temperature(xp.asarray([491.67, 0.]),
+                                           'rankine', 'kelvin'),
+                    xp.asarray([273.15, 0.]), rtol=0., atol=1e-13)
 
 
 @array_api_compatible
@@ -40,10 +46,12 @@ def test_convert_temperature_errors(xp):
 
 @array_api_compatible
 def test_lambda_to_nu(xp):
-    xp_assert_equal(sc.lambda2nu([sc.speed_of_light, 1]), [1, sc.speed_of_light])
+    xp_assert_equal(sc.lambda2nu(xp.asarray([sc.speed_of_light, 1])),
+                    xp.asarray([1, sc.speed_of_light]))
 
 
 @array_api_compatible
 def test_nu_to_lambda(xp):
-    xp_assert_equal(sc.nu2lambda([sc.speed_of_light, 1]), [1, sc.speed_of_light])
+    xp_assert_equal(sc.nu2lambda(xp.asarray([sc.speed_of_light, 1])),
+                    xp.asarray([1, sc.speed_of_light]))
 
