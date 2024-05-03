fc75675e60371729bb4e1ac470df9ababe726ac2
Jake Bowhay <60778417+j-bowhay@users.noreply.github.com>
2024-05-03 15:07:34 +0100

    ENH: stats.sem: add array-API support (#20631)
    * ENH: stats.sem: add array-API support
    
    * Apply suggestions from code review
    
    Co-authored-by: Matt Haberland <mhaberla@calpoly.edu>
    
    ---------
    
    Co-authored-by: Matt Haberland <mhaberla@calpoly.edu>

--- scipy/stats/_stats_py.py
+++ scipy/stats/_stats_py.py
@@ -67,7 +67,7 @@ from scipy._lib._bunch import _make_tuple_bunch
 from scipy import stats
 from scipy.optimize import root_scalar
 from scipy._lib._util import normalize_axis_index
-from scipy._lib._array_api import array_namespace, is_numpy
+from scipy._lib._array_api import array_namespace, is_numpy, atleast_nd
 from scipy._lib.array_api_compat import size as xp_size
 
 # In __all__ but deprecated for removal in SciPy 1.13.0
@@ -2827,8 +2827,10 @@ def sem(a, axis=0, ddof=1, nan_policy='propagate'):
     1.2893796958227628
 
     """
+    xp = array_namespace(a)
+    a = atleast_nd(a, ndim=1, xp=xp)
     n = a.shape[axis]
-    s = np.std(a, axis=axis, ddof=ddof) / np.sqrt(n)
+    s = xp.std(a, axis=axis, correction=ddof) / n**0.5
     return s
 
 
