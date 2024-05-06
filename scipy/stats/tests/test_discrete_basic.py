5c1092cf7709bca2fa31203cbc814ba1a7a4bff1
Matt Haberland <mhaberla@calpoly.edu>
2024-05-06 08:59:16 -0700

    MAINT: stats.yulesimon: fix kurtosis

--- scipy/stats/tests/test_discrete_basic.py
+++ scipy/stats/tests/test_discrete_basic.py
@@ -91,7 +91,7 @@ def test_moments(distname, arg):
     check_mean_expect(distfn, arg, m, distname)
     check_var_expect(distfn, arg, m, v, distname)
     check_skew_expect(distfn, arg, m, v, s, distname)
-    if distname not in ['zipf', 'yulesimon', 'betanbinom']:
+    if distname not in ['zipf', 'betanbinom']:
         check_kurt_expect(distfn, arg, m, v, k, distname)
 
     # frozen distr moments
