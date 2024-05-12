94f51dcbb049fa0232e18be05979ed20048a2ef0
Matt Haberland <mhaberla@calpoly.edu>
2024-05-12 09:37:49 -0500

    ENH: special.rel_entr: add generic array API implementation

--- scipy/special/_support_alternative_backends.py
+++ scipy/special/_support_alternative_backends.py
@@ -19,24 +19,61 @@ def get_array_special_func(f_name, xp, n_array_args):
     f = None
     if is_numpy(xp):
         f = getattr(_ufuncs, f_name, None)
-    if is_torch(xp):
+    elif is_torch(xp):
         f = getattr(xp.special, f_name, None)
-    if is_cupy(xp):
+    elif is_cupy(xp):
         import cupyx  # type: ignore[import-not-found]
         f = getattr(cupyx.scipy.special, f_name, None)
-    if xp.__name__ == f"{array_api_compat_prefix}.jax":
+    elif xp.__name__ == f"{array_api_compat_prefix}.jax":
         f = getattr(xp.scipy.special, f_name, None)
 
-    if f is None:
-        f_scipy = getattr(_ufuncs, f_name, None)
-        def f(*args, **kwargs):
+    if f is not None:
+        return f
+
+    # if generic array-API implementation is available, use that;
+    # otherwise, fall back to NumPy/SciPy
+    # Use of ` _f=_f, _xp=xp` is to avoid late-binding gotcha
+    if f_name in _generic_implementations:
+        _f = _generic_implementations[f_name]
+        def f(*args, _f=_f, _xp=xp, **kwargs):
+            return _f(*args, xp=_xp, **kwargs)
+    else:
+        _f = getattr(_ufuncs, f_name, None)
+        def f(*args, _f=_f, _xp=xp, **kwargs):
             array_args = args[:n_array_args]
             other_args = args[n_array_args:]
             array_args = [np.asarray(arg) for arg in array_args]
-            out = f_scipy(*array_args, *other_args, **kwargs)
-            return xp.asarray(out)
+            out = _f(*array_args, *other_args, **kwargs)
+            return _xp.asarray(out)
 
     return f
+
+
+def _get_shape_dtype(*args, xp):
+    args = xp.broadcast_arrays(*args)
+    shape = args[0].shape
+    dtype = xp.result_type(*args)
+    if xp.isdtype(dtype, 'integral'):
+        dtype = xp.float64
+        args = [xp.asarray(arg, dtype=dtype) for arg in args]
+    return args, shape, dtype
+
+
+def _rel_entr(x, y, *, xp):
+    args, shape, dtype = _get_shape_dtype(x, y, xp=xp)
+    x, y = args
+    x_gt_0 = x > 0
+    y_gt_0 = y > 0
+    x_eq_0 = x == 0
+    res = xp.full(x.shape, xp.inf, dtype=dtype)
+    res[x_eq_0 & y_gt_0] = xp.asarray(0, dtype=dtype)
+    i = x_gt_0 & y_gt_0
+    res[i] = x[i] * (xp.log(x[i]) - xp.log(y[i]))
+    return res
+
+
+_generic_implementations = {'rel_entr': _rel_entr}
+
 
 # functools.wraps doesn't work because:
 # 'numpy.ufunc' object has no attribute '__module__'
