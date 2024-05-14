58e66faba645ea3b21b3f82de98d98c39c706866
Matt Haberland <mhaberla@calpoly.edu>
2024-05-14 10:10:59 -0400

    ENH: stats.entropy, special.{entr, rel_entr}: add array API support (#20673)

--- scipy/special/_support_alternative_backends.py
+++ scipy/special/_support_alternative_backends.py
@@ -9,13 +9,14 @@ from . import _ufuncs
 # that these are defined in this file / report an error in __init__.py
 from ._ufuncs import (
     log_ndtr, ndtr, ndtri, erf, erfc, i0, i0e, i1, i1e,  # noqa: F401
-    gammaln, gammainc, gammaincc, logit, expit)  # noqa: F401
+    gammaln, gammainc, gammaincc, logit, expit, entr, rel_entr)  # noqa: F401
 
 _SCIPY_ARRAY_API = os.environ.get("SCIPY_ARRAY_API", False)
 array_api_compat_prefix = "scipy._lib.array_api_compat"
 
 
 def get_array_special_func(f_name, xp, n_array_args):
+    f = None
     if is_numpy(xp):
         f = getattr(_ufuncs, f_name, None)
     elif is_torch(xp):
@@ -25,16 +26,51 @@ def get_array_special_func(f_name, xp, n_array_args):
         f = getattr(cupyx.scipy.special, f_name, None)
     elif xp.__name__ == f"{array_api_compat_prefix}.jax":
         f = getattr(xp.scipy.special, f_name, None)
+
+    if f is not None:
+        return f
+
+    # if generic array-API implementation is available, use that;
+    # otherwise, fall back to NumPy/SciPy
+    # Use of ` _f=_f, _xp=xp` is to avoid late-binding gotcha
+    if f_name in _generic_implementations:
+        _f = _generic_implementations[f_name]
+        def f(*args, _f=_f, _xp=xp, **kwargs):
+            return _f(*args, xp=_xp, **kwargs)
     else:
-        f_scipy = getattr(_ufuncs, f_name, None)
-        def f(*args, **kwargs):
+        _f = getattr(_ufuncs, f_name, None)
+        def f(*args, _f=_f, _xp=xp, **kwargs):
             array_args = args[:n_array_args]
             other_args = args[n_array_args:]
             array_args = [np.asarray(arg) for arg in array_args]
-            out = f_scipy(*array_args, *other_args, **kwargs)
-            return xp.asarray(out)
+            out = _f(*array_args, *other_args, **kwargs)
+            return _xp.asarray(out)
 
     return f
+
+
+def _get_shape_dtype(*args, xp):
+    args = xp.broadcast_arrays(*args)
+    shape = args[0].shape
+    dtype = xp.result_type(*args)
+    if xp.isdtype(dtype, 'integral'):
+        dtype = xp.float64
+        args = [xp.asarray(arg, dtype=dtype) for arg in args]
+    return args, shape, dtype
+
+
+def _rel_entr(x, y, *, xp):
+    args, shape, dtype = _get_shape_dtype(x, y, xp=xp)
+    x, y = args
+    res = xp.full(x.shape, xp.inf, dtype=dtype)
+    res[(x == 0) & (y >= 0)] = xp.asarray(0, dtype=dtype)
+    i = (x > 0) & (y > 0)
+    res[i] = x[i] * (xp.log(x[i]) - xp.log(y[i]))
+    return res
+
+
+_generic_implementations = {'rel_entr': _rel_entr}
+
 
 # functools.wraps doesn't work because:
 # 'numpy.ufunc' object has no attribute '__module__'
@@ -65,6 +101,8 @@ array_special_func_map = {
     'gammaincc': 2,
     'logit': 1,
     'expit': 1,
+    'entr': 1,
+    'rel_entr': 2,
 }
 
 for f_name, n_array_args in array_special_func_map.items():
--- scipy/special/tests/test_support_alternative_backends.py
+++ scipy/special/tests/test_support_alternative_backends.py
@@ -27,6 +27,26 @@ def test_dispatch_to_unrecognize_library():
     xp_assert_close(res, ref, xp=xp)
 
 
+@pytest.mark.parametrize('dtype', ['float32', 'float64', 'int64'])
+@pytest.mark.skipif(not HAVE_ARRAY_API_STRICT,
+                    reason="`array_api_strict` not installed")
+def test_rel_entr_generic(dtype):
+    xp = array_api_strict
+    f = get_array_special_func('rel_entr', xp=xp, n_array_args=2)
+    dtype_np = getattr(np, dtype)
+    dtype_xp = getattr(xp, dtype)
+    x, y = [-1, 0, 0, 1], [1, 0, 2, 3]
+
+    x_xp, y_xp = xp.asarray(x, dtype=dtype_xp), xp.asarray(y, dtype=dtype_xp)
+    res = f(x_xp, y_xp)
+
+    x_np, y_np = np.asarray(x, dtype=dtype_np), np.asarray(y, dtype=dtype_np)
+    ref = special.rel_entr(x_np[-1], y_np[-1])
+    ref = np.asarray([np.inf, 0, 0, ref], dtype=ref.dtype)
+
+    xp_assert_close(res, xp.asarray(ref), xp=xp)
+
+
 @array_api_compatible
 @given(data=strategies.data())
 @pytest.mark.parametrize('f_name_n_args', array_special_func_map.items())
