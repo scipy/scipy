74f01fc71a11c15efcd788a28196e2780ae81073
Daniel Schmitz <40656107+dschmitz89@users.noreply.github.com>
2024-05-15 22:39:27 +0200

    TST: reduce `niter`for COBYQA

--- scipy/optimize/tests/test__basinhopping.py
+++ scipy/optimize/tests/test__basinhopping.py
@@ -212,7 +212,6 @@ class TestBasinHopping:
                                niter=self.niter, disp=self.disp)
             assert_almost_equal(res.x, self.sol[i], self.tol)
 
-    @pytest.mark.fail_slow(40)
     def test_all_nograd_minimizers(self):
         # Test 2-D minimizations without gradient. Newton-CG requires jac=True,
         # so not included here.
@@ -220,11 +219,13 @@ class TestBasinHopping:
         methods = ['CG', 'BFGS', 'L-BFGS-B', 'TNC', 'SLSQP',
                    'Nelder-Mead', 'Powell', 'COBYLA', 'COBYQA']
         minimizer_kwargs = copy.copy(self.kwargs_nograd)
+        # COBYQA takes extensive amount of time on this problem
+        niter = 10 if method == 'COBYQA' else self.niter
         for method in methods:
             minimizer_kwargs["method"] = method
             res = basinhopping(func2d_nograd, self.x0[i],
                                minimizer_kwargs=minimizer_kwargs,
-                               niter=self.niter, disp=self.disp)
+                               niter=niter, disp=self.disp)
             tol = self.tol
             if method == 'COBYLA':
                 tol = 2
