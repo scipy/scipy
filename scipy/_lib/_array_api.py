de48c7418e99287d5d5992e777f7699681ffe7ae
Lucas Colley <lucas.colley8@gmail.com>
2024-05-18 21:05:54 +0100

    MAINT/TST: fft: remove xp backend skips, test `fftfreq` `device` (#19900)

--- scipy/_lib/_array_api.py
+++ scipy/_lib/_array_api.py
@@ -22,9 +22,10 @@ from scipy._lib.array_api_compat import (
     is_array_api_obj,
     size,
     numpy as np_compat,
+    device
 )
 
-__all__ = ['array_namespace', '_asarray', 'size']
+__all__ = ['array_namespace', '_asarray', 'size', 'device']
 
 
 # To enable array API and strict array-like input validation
@@ -393,7 +394,44 @@ def xp_unsupported_param_msg(param: Any) -> str:
 def is_complex(x: Array, xp: ModuleType) -> bool:
     return xp.isdtype(x.dtype, 'complex floating')
 
-def scipy_namespace_for(xp):
+
+def get_xp_devices(xp: ModuleType) -> list[str] | list[None]:
+    """Returns a list of available devices for the given namespace."""
+    devices: list[str] = []
+    if is_torch(xp):
+        devices += ['cpu']
+        import torch # type: ignore[import]
+        num_cuda = torch.cuda.device_count()
+        for i in range(0, num_cuda):
+            devices += [f'cuda:{i}']
+        if torch.backends.mps.is_available():
+            devices += ['mps']
+        return devices
+    elif is_cupy(xp):
+        import cupy # type: ignore[import]
+        num_cuda = cupy.cuda.runtime.getDeviceCount()
+        for i in range(0, num_cuda):
+            devices += [f'cuda:{i}']
+        return devices
+    elif is_jax(xp):
+        import jax # type: ignore[import]
+        num_cpu = jax.device_count(backend='cpu')
+        for i in range(0, num_cpu):
+            devices += [f'cpu:{i}']
+        num_gpu = jax.device_count(backend='gpu')
+        for i in range(0, num_gpu):
+            devices += [f'gpu:{i}']
+        num_tpu = jax.device_count(backend='tpu')
+        for i in range(0, num_tpu):
+            devices += [f'tpu:{i}']
+        return devices
+
+    # given namespace is not known to have a list of available devices;
+    # return `[None]` so that one can use this in tests for `device=None`.
+    return [None] 
+
+
+def scipy_namespace_for(xp: ModuleType) -> ModuleType:
     """
     Return the `scipy` namespace for alternative backends, where it exists,
     such as `cupyx.scipy` and `jax.scipy`. Useful for ad hoc dispatching.
@@ -412,6 +450,7 @@ def scipy_namespace_for(xp):
 
     import scipy
     return scipy
+
 
 # temporary substitute for xp.minimum, which is not yet in all backends
 # or covered by array_api_compat.
