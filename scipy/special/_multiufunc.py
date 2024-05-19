78f4eeedba18eed448187dddc1f588a62e02092b
izaid <hi@irwinzaid.com>
2024-05-19 16:29:27 +0100

    .

--- scipy/special/_multiufunc.py
+++ scipy/special/_multiufunc.py
@@ -48,8 +48,8 @@ class multiufunc:
         out = tuple(np.empty(ufunc_out_shape, dtype = ufunc_out_dtype)
             for ufunc_out_shape, ufunc_out_dtype in zip(ufunc_out_shapes, ufunc_out_dtypes))
 
-        ufunc_out = tuple(np.moveaxis(out, tuple(range(axis)), tuple(range(-axis, 0)))
-            for out, axis in zip(out, ufunc_out_new_dims))
+        ufunc_out = tuple(np.moveaxis(out[i], tuple(range(axis)), tuple(range(-axis, 0)))
+            for i, axis in enumerate(ufunc_out_new_dims))
         ufunc(*ufunc_args, out = ufunc_out)
 
         if (len(out) == 1):
