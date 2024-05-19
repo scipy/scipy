f64c0ba5a5c63df2da39d7c372c8f1780c4fa280
izaid <hi@irwinzaid.com>
2024-05-19 14:39:12 +0100

    Updates on multiufunc

--- scipy/special/_multiufunc.py
+++ scipy/special/_multiufunc.py
@@ -18,36 +18,34 @@ class multiufunc:
         if ((ufunc.nout == 0) or (self._resolve_out_shapes is None)):
             return ufunc(*args)
 
-        resolve_out_shapes_args = args[:-ufunc.nin]
-        args = args[-ufunc.nin:]
+        ufunc_args = args[-ufunc.nin:] # array arguments to be passed to the ufunc
 
-        arg_shapes = tuple(np.shape(arg) for arg in args)
-        out_shapes = self._resolve_out_shapes(*resolve_out_shapes_args, *arg_shapes, ufunc.nout)
+        ufunc_arg_shapes = tuple(np.shape(arg) for arg in ufunc_args)
+        ufunc_out_shapes = self._resolve_out_shapes(*args[:-ufunc.nin], *ufunc_arg_shapes, ufunc.nout)
 
-        arg_dtypes = tuple(arg.dtype if hasattr(arg, 'dtype') else np.dtype(type(arg)) for arg in args)
+        ufunc_arg_dtypes = tuple(arg.dtype if hasattr(arg, 'dtype') else np.dtype(type(arg)) for arg in ufunc_args)
         if hasattr(ufunc, 'resolve_dtypes'):
-            dtypes = arg_dtypes + ufunc.nout * (None,)
-            dtypes = ufunc.resolve_dtypes(dtypes) 
-            out_dtypes = dtypes[-ufunc.nout:]
+            ufunc_dtypes = ufunc_arg_dtypes + ufunc.nout * (None,)
+            ufunc_dtypes = ufunc.resolve_dtypes(ufunc_dtypes) 
+            ufunc_out_dtypes = ufunc_dtypes[-ufunc.nout:]
         else:
-            out_dtype = np.result_type(*arg_dtypes)
-            if (not np.issubdtype(out_dtype, np.inexact)):
-                out_dtype = np.float64
+            ufunc_out_dtype = np.result_type(*ufunc_arg_dtypes)
+            if (not np.issubdtype(ufunc_out_dtype, np.inexact)):
+                ufunc_out_dtype = np.float64
 
-            out_dtypes = ufunc.nout * (out_dtype,)
+            ufunc_out_dtypes = ufunc.nout * (ufunc_out_dtype,)
 
         if self.force_out_complex:
-            out_dtypes = tuple(np.result_type(1j, out_dtype) for out_dtype in out_dtypes)
+            ufunc_out_dtypes = tuple(np.result_type(1j, out_dtype) for out_dtype in ufunc_out_dtypes)
 
-        b_shape = np.broadcast_shapes(*arg_shapes)
+        b = np.broadcast(*ufunc_args)
+        out = tuple(np.empty(out_shape, dtype = out_dtype) for out_shape, out_dtype in zip(ufunc_out_shapes, ufunc_out_dtypes))
 
-        out = tuple(np.empty(out_shape, dtype = out_dtype) for out_shape, out_dtype in zip(out_shapes, out_dtypes))
-
-        new_dims = tuple(len(out_shape) - len(b_shape) for out_shape in out_shapes)
+        new_dims = tuple(len(out_shape) - b.ndim for out_shape in ufunc_out_shapes)
 
         out_ufunc = tuple(np.moveaxis(out, tuple(range(new_dim)), tuple(range(-new_dim, 0)))
             for out, new_dim in zip(out, new_dims))
-        ufunc(*args, out = out_ufunc)
+        ufunc(*ufunc_args, out = out_ufunc)
 
         if (len(out) == 1):
             out, = out
