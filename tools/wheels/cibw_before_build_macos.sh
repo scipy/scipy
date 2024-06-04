dff4dd51d733db480e229009bedc0c29e3c8287a
Andrew Nelson <andyfaff@gmail.com>
2024-06-05 07:11:43 +1000

    BLD: test delocate works by removing original lib (#20870)
    Closes gh-20852
    
    Co-authored-by: Matti Picus <matti.picus@gmail.com>

--- tools/wheels/cibw_before_build_macos.sh
+++ tools/wheels/cibw_before_build_macos.sh
@@ -43,6 +43,7 @@ if [[ $PLATFORM == "x86_64" ]]; then
   export SDKROOT=${SDKROOT:-$(xcrun --show-sdk-path)}
 fi
 
+
 if [[ $PLATFORM == "arm64" ]]; then
   curl -L https://github.com/fxcoudert/gfortran-for-macOS/releases/download/12.1-monterey/gfortran-ARM-12.1-Monterey.dmg -o gfortran.dmg
   GFORTRAN_SHA256=$(shasum -a 256 gfortran.dmg)
@@ -62,3 +63,13 @@ fi
 # Install Openblas
 python -m pip install -r requirements/openblas.txt
 python -c "import scipy_openblas32; print(scipy_openblas32.get_pkg_config())" > $PROJECT_DIR/scipy-openblas.pc
+
+lib_loc=$(python -c"import scipy_openblas32; print(scipy_openblas32.get_lib_dir())")
+# Use the libgfortran from gfortran rather than the one in the wheel
+# since delocate gets confused if there is more than one
+# https://github.com/scipy/scipy/issues/20852
+install_name_tool -change @loader_path/../.dylibs/libgfortran.5.dylib @rpath/libgfortran.5.dylib $lib_loc/libsci*
+install_name_tool -change @loader_path/../.dylibs/libgcc_s.1.1.dylib @rpath/libgcc_s.1.1.dylib $lib_loc/libsci*
+install_name_tool -change @loader_path/../.dylibs/libquadmath.0.dylib @rpath/libquadmath.0.dylib $lib_loc/libsci*
+
+codesign -s - -f $lib_loc/libsci*
