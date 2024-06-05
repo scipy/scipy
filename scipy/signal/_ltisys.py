eac76c59c7d464921e1982efcddd5c32b86c3680
Érico Nogueira <erico.rolim@lnls.br>
2024-06-05 15:29:55 -0300

    DOC: improve signal.dbode docstring.
    Unlike dfreqresp, which from context makes it clear what
    "radian/samples" means, dbode lacks such an example. This can be helpful
    for non-native speakers.
    
    The unit for the returned frequency array w was wrong, since dt is
    specified to be in seconds.
    
    [docs only]

--- scipy/signal/_ltisys.py
+++ scipy/signal/_ltisys.py
@@ -3436,9 +3436,9 @@ def dbode(system, w=None, n=100):
             * 4 (A, B, C, D, dt)
 
     w : array_like, optional
-        Array of frequencies (in radians/sample). Magnitude and phase data is
-        calculated for every value in this array. If not given a reasonable
-        set will be calculated.
+        Array of frequencies (in radians/sample, i.e. 0 to pi covers up to the
+        Nyquist frequency). Magnitude and phase data is calculated for every
+        value in this array. If not given, a reasonable set will be calculated.
     n : int, optional
         Number of frequency points to compute if `w` is not given. The `n`
         frequencies are logarithmically spaced in an interval chosen to
@@ -3447,7 +3447,7 @@ def dbode(system, w=None, n=100):
     Returns
     -------
     w : 1D ndarray
-        Frequency array [rad/time_unit]
+        Frequency array [rad/s]
     mag : 1D ndarray
         Magnitude array [dB]
     phase : 1D ndarray
