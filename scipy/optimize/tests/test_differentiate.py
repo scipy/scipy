72bb98ea0e26ff837c619d12de3316dc421e99f5
Matt Haberland <mhaberla@calpoly.edu>
2024-06-09 10:51:52 -0700

    TST: optimize._differentiate: continue test conversions

--- scipy/optimize/tests/test_differentiate.py
+++ scipy/optimize/tests/test_differentiate.py
@@ -6,23 +6,25 @@ from numpy.testing import assert_array_less, assert_allclose, assert_equal
 from scipy.conftest import array_api_compatible
 import scipy._lib._elementwise_iterative_method as eim
 from scipy._lib._array_api import (xp_assert_close, xp_assert_equal, xp_assert_less,
-                                   is_numpy, is_torch)
+                                   is_numpy, is_torch, array_namespace)
 
 from scipy import stats, optimize, special
 from scipy.optimize._differentiate import (_differentiate as differentiate,
                                            _jacobian as jacobian, _EERRORINCREASE)
 
+
+@array_api_compatible
+@pytest.mark.usefixtures("skip_xp_backends")
+@pytest.mark.skip_xp_backends('array_api_strict', 'jax.numpy',
+                              reasons=[
+                                  'Currently uses fancy indexing assignment.',
+                                  'JAX arrays do not support item assignment.'])
 class TestDifferentiate:
 
     def f(self, x):
         return special.ndtr(x)
 
-    @array_api_compatible
     @pytest.mark.parametrize('x', [0.6, np.linspace(-0.05, 1.05, 10)])
-    @pytest.mark.usefixtures("skip_xp_backends")
-    @pytest.mark.skip_xp_backends('array_api_strict', 'jax.numpy',
-                                  reasons=['Currently uses fancy indexing assignment.',
-                                           'JAX does not support item assignment.'])
     def test_basic(self, x, xp):
         # Invert distribution CDF and compare against distribution `ppf`
         default_dtype = xp.asarray(1.).dtype
@@ -34,6 +36,7 @@ class TestDifferentiate:
         if not is_torch:
             xp_assert_less(xp.abs(res.df - ref), res.error)
 
+    @pytest.mark.skip_xp_backends(np_only=True)
     @pytest.mark.parametrize('case', stats._distr_params.distcont)
     def test_accuracy(self, case):
         distname, params = case
@@ -43,13 +46,8 @@ class TestDifferentiate:
         ref = dist.pdf(x)
         assert_allclose(res.df, ref, atol=1e-10)
 
-    @array_api_compatible
     @pytest.mark.parametrize('order', [1, 6])
     @pytest.mark.parametrize('shape', [tuple(), (12,), (3, 4), (3, 2, 2)])
-    @pytest.mark.usefixtures("skip_xp_backends")
-    @pytest.mark.skip_xp_backends('array_api_strict', 'jax.numpy',
-                                  reasons=['Currently uses fancy indexing assignment.',
-                                           'JAX does not support item assignment.'])
     def test_vectorization(self, order, shape, xp):
         # Test for correct functionality, output shapes, and dtypes for various
         # input shapes.
@@ -78,7 +76,7 @@ class TestDifferentiate:
 
         ref_error = [ref.error for ref in refs]
         xp_assert_close(xp.reshape(res.error, (-1,)), xp.asarray(ref_error),
-                        atol=1e-13)
+                        atol=1e-12)
 
         ref_success = [bool(ref.success) for ref in refs]
         xp_assert_equal(xp.reshape(res.success, (-1,)), xp.asarray(ref_success))
@@ -96,89 +94,89 @@ class TestDifferentiate:
         if not is_numpy(xp):  # can't expect other backends to be exactly the same
             xp.max(res.nit) == f.nit
 
-    def test_flags(self):
+    def test_flags(self, xp):
         # Test cases that should produce different status flags; show that all
         # can be produced simultaneously.
         rng = np.random.default_rng(5651219684984213)
         def f(xs, js):
             f.nit += 1
             funcs = [lambda x: x - 2.5,  # converges
-                     lambda x: np.exp(x)*rng.random(),  # error increases
-                     lambda x: np.exp(x),  # reaches maxiter due to order=2
-                     lambda x: np.full_like(x, np.nan)[()]]  # stops due to NaN
-            res = [funcs[j](x) for x, j in zip(xs, js.ravel())]
+                     lambda x: xp.exp(x)*rng.random(),  # error increases
+                     lambda x: xp.exp(x),  # reaches maxiter due to order=2
+                     lambda x: xp.full_like(x, xp.nan)[()]]  # stops due to NaN
+            res = [funcs[int(j)](x) for x, j in zip(xs, xp.reshape(js, (-1,)))]
             return res
         f.nit = 0
 
-        args = (np.arange(4, dtype=np.int64),)
-        res = differentiate(f, [1]*4, rtol=1e-14, order=2, args=args)
+        args = (xp.arange(4, dtype=xp.int64),)
+        res = differentiate(f, xp.ones(4, dtype=xp.int32), rtol=1e-14,
+                            order=2, args=args)
 
-        ref_flags = np.array([eim._ECONVERGED,
-                              _EERRORINCREASE,
-                              eim._ECONVERR,
-                              eim._EVALUEERR])
-        assert_equal(res.status, ref_flags)
+        ref_flags = xp.asarray([eim._ECONVERGED,
+                                _EERRORINCREASE,
+                                eim._ECONVERR,
+                                eim._EVALUEERR])
+        xp_assert_equal(res.status, ref_flags)
 
-    def test_flags_preserve_shape(self):
+    def test_flags_preserve_shape(self, xp):
         # Same test as above but using `preserve_shape` option to simplify.
         rng = np.random.default_rng(5651219684984213)
         def f(x):
             return [x - 2.5,  # converges
-                    np.exp(x)*rng.random(),  # error increases
+                    xp.exp(x)*rng.random(),  # error increases
                     np.exp(x),  # reaches maxiter due to order=2
-                    np.full_like(x, np.nan)[()]]  # stops due to NaN
+                    np.full_like(x, xp.nan)[()]]  # stops due to NaN
 
-        res = differentiate(f, 1, rtol=1e-14, order=2, preserve_shape=True)
+        res = differentiate(f, xp.asarray(1), rtol=1e-14,
+                            order=2, preserve_shape=True)
 
-        ref_flags = np.array([eim._ECONVERGED,
-                              _EERRORINCREASE,
-                              eim._ECONVERR,
-                              eim._EVALUEERR])
-        assert_equal(res.status, ref_flags)
+        ref_flags = xp.asarray([eim._ECONVERGED,
+                                _EERRORINCREASE,
+                                eim._ECONVERR,
+                                eim._EVALUEERR])
+        xp_assert_equal(res.status, ref_flags)
 
-    def test_preserve_shape(self):
+    def test_preserve_shape(self, xp):
         # Test `preserve_shape` option
         def f(x):
-            return [x, np.sin(3*x), x+np.sin(10*x), np.sin(20*x)*(x-1)**2]
+            return [x, xp.sin(3*x), x+xp.sin(10*x), xp.sin(20*x)*(x-1)**2]
 
-        x = 0
-        ref = [1, 3*np.cos(3*x), 1+10*np.cos(10*x),
-               20*np.cos(20*x)*(x-1)**2 + 2*np.sin(20*x)*(x-1)]
+        x = xp.asarray(0.)
+        ref = xp.asarray([xp.asarray(1), 3*xp.cos(3*x), 1+10*xp.cos(10*x),
+                          20*xp.cos(20*x)*(x-1)**2 + 2*xp.sin(20*x)*(x-1)])
         res = differentiate(f, x, preserve_shape=True)
-        assert_allclose(res.df, ref)
+        xp_assert_close(res.df, ref)
 
-    def test_convergence(self):
+    def test_convergence(self, xp):
         # Test that the convergence tolerances behave as expected
-        dist = stats.norm()
-        x = 1
-        f = dist.cdf
-        ref = dist.pdf(x)
+        x = xp.asarray(1.)
+        f = special.ndtr
+        ref = float(stats.norm.pdf(1.))
         kwargs0 = dict(atol=0, rtol=0, order=4)
 
         kwargs = kwargs0.copy()
         kwargs['atol'] = 1e-3
         res1 = differentiate(f, x, **kwargs)
-        assert_array_less(abs(res1.df - ref), 1e-3)
+        assert abs(res1.df - ref) < 1e-3
         kwargs['atol'] = 1e-6
         res2 = differentiate(f, x, **kwargs)
-        assert_array_less(abs(res2.df - ref), 1e-6)
-        assert_array_less(abs(res2.df - ref), abs(res1.df - ref))
+        assert abs(res2.df - ref) < 1e-6
+        assert abs(res2.df - ref) < abs(res1.df - ref)
 
         kwargs = kwargs0.copy()
         kwargs['rtol'] = 1e-3
         res1 = differentiate(f, x, **kwargs)
-        assert_array_less(abs(res1.df - ref), 1e-3 * np.abs(ref))
+        assert abs(res1.df - ref) < 1e-3 * ref
         kwargs['rtol'] = 1e-6
         res2 = differentiate(f, x, **kwargs)
-        assert_array_less(abs(res2.df - ref), 1e-6 * np.abs(ref))
-        assert_array_less(abs(res2.df - ref), abs(res1.df - ref))
+        assert abs(res2.df - ref) < 1e-6 * ref
+        assert abs(res2.df - ref) < abs(res1.df - ref)
 
-    def test_step_parameters(self):
+    def test_step_parameters(self, xp):
         # Test that step factors have the expected effect on accuracy
-        dist = stats.norm()
-        x = 1
-        f = dist.cdf
-        ref = dist.pdf(x)
+        x = xp.asarray(1.)
+        f = special.ndtr
+        ref = float(stats.norm.pdf(1.))
 
         res1 = differentiate(f, x, initial_step=0.5, maxiter=1)
         res2 = differentiate(f, x, initial_step=0.05, maxiter=1)
@@ -192,36 +190,36 @@ class TestDifferentiate:
         kwargs = dict(order=4, maxiter=1, step_direction=0)
         res = differentiate(f, x, initial_step=0.5, step_factor=0.5, **kwargs)
         ref = differentiate(f, x, initial_step=1, step_factor=2, **kwargs)
-        assert_allclose(res.df, ref.df, rtol=5e-15)
+        xp_assert_close(res.df, ref.df, rtol=5e-15)
 
         # This is a similar test for one-sided difference
         kwargs = dict(order=2, maxiter=1, step_direction=1)
         res = differentiate(f, x, initial_step=1, step_factor=2, **kwargs)
         ref = differentiate(f, x, initial_step=1/np.sqrt(2), step_factor=0.5,
                                    **kwargs)
-        assert_allclose(res.df, ref.df, rtol=5e-15)
+        xp_assert_close(res.df, ref.df, rtol=5e-15)
 
         kwargs['step_direction'] = -1
         res = differentiate(f, x, initial_step=1, step_factor=2, **kwargs)
         ref = differentiate(f, x, initial_step=1/np.sqrt(2), step_factor=0.5,
                                    **kwargs)
-        assert_allclose(res.df, ref.df, rtol=5e-15)
+        xp_assert_close(res.df, ref.df, rtol=5e-15)
 
-    def test_step_direction(self):
+    def test_step_direction(self, xp):
         # test that `step_direction` works as expected
         def f(x):
-            y = np.exp(x)
-            y[(x < 0) + (x > 2)] = np.nan
+            y = xp.exp(x)
+            y[(x < 0) + (x > 2)] = xp.nan
             return y
 
-        x = np.linspace(0, 2, 10)
-        step_direction = np.zeros_like(x)
+        x = xp.linspace(0, 2, 10)
+        step_direction = xp.zeros_like(x)
         step_direction[x < 0.6], step_direction[x > 1.4] = 1, -1
         res = differentiate(f, x, step_direction=step_direction)
-        assert_allclose(res.df, np.exp(x))
-        assert np.all(res.success)
+        xp_assert_close(res.df, xp.exp(x))
+        assert xp.all(res.success)
 
-    def test_vectorized_step_direction_args(self):
+    def test_vectorized_step_direction_args(self, xp):
         # test that `step_direction` and `args` are vectorized properly
         def f(x, p):
             return x ** p
@@ -229,35 +227,35 @@ class TestDifferentiate:
         def df(x, p):
             return p * x ** (p - 1)
 
-        x = np.array([1, 2, 3, 4]).reshape(-1, 1, 1)
-        hdir = np.array([-1, 0, 1]).reshape(1, -1, 1)
-        p = np.array([2, 3]).reshape(1, 1, -1)
+        x = xp.reshape(xp.asarray([1, 2, 3, 4]), (-1, 1, 1))
+        hdir = xp.reshape(xp.asarray([-1, 0, 1]), (1, -1, 1))
+        p = xp.reshape(xp.asarray([2, 3]), (1, 1, -1))
         res = differentiate(f, x, step_direction=hdir, args=(p,))
-        ref = np.broadcast_to(df(x, p), res.df.shape)
-        assert_allclose(res.df, ref)
+        ref = xp.broadcast_to(df(x, p), res.df.shape)
+        ref = xp.asarray(ref, dtype=xp.asarray(1.).dtype)
+        xp_assert_close(res.df, ref)
 
-    def test_maxiter_callback(self):
+    def test_maxiter_callback(self, xp):
         # Test behavior of `maxiter` parameter and `callback` interface
-        x = 0.612814
-        dist = stats.norm()
+        x = xp.asarray(0.612814)
         maxiter = 3
 
         def f(x):
-            res = dist.cdf(x)
+            res = special.ndtr(x)
             return res
 
         default_order = 8
         res = differentiate(f, x, maxiter=maxiter, rtol=1e-15)
-        assert not np.any(res.success)
-        assert np.all(res.nfev == default_order + 1 + (maxiter - 1)*2)
-        assert np.all(res.nit == maxiter)
+        assert not xp.any(res.success)
+        assert xp.all(res.nfev == default_order + 1 + (maxiter - 1)*2)
+        assert xp.all(res.nit == maxiter)
 
         def callback(res):
             callback.iter += 1
             callback.res = res
             assert hasattr(res, 'x')
-            assert res.df not in callback.dfs
-            callback.dfs.add(res.df)
+            assert float(res.df) not in callback.dfs
+            callback.dfs.add(float(res.df))
             assert res.status == eim._EINPROGRESS
             if callback.iter == maxiter:
                 raise StopIteration
@@ -271,21 +269,24 @@ class TestDifferentiate:
         for key in res.keys():
             if key == 'status':
                 assert res[key] == eim._ECONVERR
-                assert callback.res[key] == eim._EINPROGRESS
                 assert res2[key] == eim._ECALLBACK
             else:
                 assert res2[key] == callback.res[key] == res[key]
 
     @pytest.mark.parametrize("hdir", (-1, 0, 1))
     @pytest.mark.parametrize("x", (0.65, [0.65, 0.7]))
-    @pytest.mark.parametrize("dtype", (np.float16, np.float32, np.float64))
-    def test_dtype(self, hdir, x, dtype):
+    @pytest.mark.parametrize("dtype", ('float16', 'float32', 'float64'))
+    def test_dtype(self, hdir, x, dtype, xp):
+        if dtype == 'float16' and not is_numpy(xp):
+            pytest.skip('float16 not tested for alternative backends')
+
         # Test that dtypes are preserved
-        x = np.asarray(x, dtype=dtype)[()]
+        dtype = getattr(xp, dtype)
+        x = xp.asarray(x, dtype=dtype)[()]
 
         def f(x):
             assert x.dtype == dtype
-            return np.exp(x)
+            return xp.exp(x)
 
         def callback(res):
             assert res.x.dtype == dtype
@@ -297,66 +298,68 @@ class TestDifferentiate:
         assert res.x.dtype == dtype
         assert res.df.dtype == dtype
         assert res.error.dtype == dtype
-        eps = np.finfo(dtype).eps
-        assert_allclose(res.df, np.exp(res.x), rtol=np.sqrt(eps))
+        eps = xp.finfo(dtype).eps
+        xp_assert_close(res.df, xp.exp(res.x), rtol=eps**0.5)
 
-    def test_input_validation(self):
+    def test_input_validation(self, xp):
         # Test input validation for appropriate error messages
+        one = xp.asarray(1)
 
         message = '`func` must be callable.'
         with pytest.raises(ValueError, match=message):
-            differentiate(None, 1)
+            differentiate(None, one)
 
         message = 'Abscissae and function output must be real numbers.'
         with pytest.raises(ValueError, match=message):
-            differentiate(lambda x: x, -4+1j)
+            differentiate(lambda x: x, xp.asarray(-4+1j))
 
         message = "When `preserve_shape=False`, the shape of the array..."
         with pytest.raises(ValueError, match=message):
-            differentiate(lambda x: [1, 2, 3], [-2, -3])
+            differentiate(lambda x: [1, 2, 3], xp.asarray([-2, -3]))
 
         message = 'Tolerances and step parameters must be non-negative...'
         with pytest.raises(ValueError, match=message):
-            differentiate(lambda x: x, 1, atol=-1)
+            differentiate(lambda x: x, one, atol=-1)
         with pytest.raises(ValueError, match=message):
-            differentiate(lambda x: x, 1, rtol='ekki')
+            differentiate(lambda x: x, one, rtol='ekki')
         with pytest.raises(ValueError, match=message):
-            differentiate(lambda x: x, 1, initial_step=None)
+            differentiate(lambda x: x, one, initial_step=None)
         with pytest.raises(ValueError, match=message):
-            differentiate(lambda x: x, 1, step_factor=object())
+            differentiate(lambda x: x, one, step_factor=object())
 
         message = '`maxiter` must be a positive integer.'
         with pytest.raises(ValueError, match=message):
-            differentiate(lambda x: x, 1, maxiter=1.5)
+            differentiate(lambda x: x, one, maxiter=1.5)
         with pytest.raises(ValueError, match=message):
-            differentiate(lambda x: x, 1, maxiter=0)
+            differentiate(lambda x: x, one, maxiter=0)
 
         message = '`order` must be a positive integer'
         with pytest.raises(ValueError, match=message):
-            differentiate(lambda x: x, 1, order=1.5)
+            differentiate(lambda x: x, one, order=1.5)
         with pytest.raises(ValueError, match=message):
-            differentiate(lambda x: x, 1, order=0)
+            differentiate(lambda x: x, one, order=0)
 
         message = '`preserve_shape` must be True or False.'
         with pytest.raises(ValueError, match=message):
-            differentiate(lambda x: x, 1, preserve_shape='herring')
+            differentiate(lambda x: x, one, preserve_shape='herring')
 
         message = '`callback` must be callable.'
         with pytest.raises(ValueError, match=message):
-            differentiate(lambda x: x, 1, callback='shrubbery')
+            differentiate(lambda x: x, one, callback='shrubbery')
 
-    def test_special_cases(self):
+    def test_special_cases(self, xp):
         # Test edge cases and other special cases
 
         # Test that integers are not passed to `f`
         # (otherwise this would overflow)
         def f(x):
-            assert np.issubdtype(x.dtype, np.floating)
+            xp_test = array_namespace(x)  # needs `isdtype`
+            assert xp_test.isdtype(x.dtype, 'real floating')
             return x ** 99 - 1
 
-        res = differentiate(f, 7, rtol=1e-10)
+        res = differentiate(f, xp.asarray(7), rtol=1e-10)
         assert res.success
-        assert_allclose(res.df, 99*7.**98)
+        xp_assert_close(res.df, xp.asarray(99*7.**98))
 
         # Test that if success is achieved in the correct number
         # of iterations if function is a polynomial. Ideally, all polynomials
@@ -366,28 +369,29 @@ class TestDifferentiate:
         # extra iteration to detect convergence based on the error estimate.
 
         for n in range(6):
-            x = 1.5
+            x = xp.asarray(1.5)
             def f(x):
                 return 2*x**n
 
             ref = 2*n*x**(n-1)
 
             res = differentiate(f, x, maxiter=1, order=max(1, n))
-            assert_allclose(res.df, ref, rtol=1e-15)
-            assert_equal(res.error, np.nan)
+            xp_assert_close(res.df, ref, rtol=1e-15)
+            xp_assert_equal(res.error, xp.asarray(xp.nan))
 
             res = differentiate(f, x, order=max(1, n))
             assert res.success
             assert res.nit == 2
-            assert_allclose(res.df, ref, rtol=1e-15)
+            xp_assert_close(res.df, ref, rtol=1e-15)
 
         # Test scalar `args` (not in tuple)
         def f(x, c):
             return c*x - 1
 
-        res = differentiate(f, 2, args=3)
-        assert_allclose(res.df, 3)
+        res = differentiate(f, xp.asarray(2), args=xp.asarray(3))
+        xp_assert_close(res.df, xp.asarray(3.))
 
+    @pytest.mark.skip_xp_backends(np_only=True)
     @pytest.mark.xfail
     @pytest.mark.parametrize("case", (  # function, evaluation point
         (lambda x: (x - 1) ** 3, 1),
