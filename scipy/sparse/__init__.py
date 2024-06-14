c6a005e20af618f6d960530d5e1b5494ed548ff9
m-maggi <124086916+m-maggi@users.noreply.github.com>
2024-06-14 19:18:57 +0200

    DOC: Remove slicing in the array note
    [docs only]

--- scipy/sparse/__init__.py
+++ scipy/sparse/__init__.py
@@ -32,8 +32,6 @@ SciPy 2-D sparse array package for numeric data.
      that all *slicing* operations on these objects must produce
      two-dimensional results, or they will result in an error. This
      will be addressed in a future version.
-     Currently *slicing* is not supported by `bsr_array`, `coo_array`,
-     `dia_array`.
 
    The construction utilities (`eye`, `kron`, `random`, `diags`, etc.)
    have not yet been ported, but their results can be wrapped into arrays::
