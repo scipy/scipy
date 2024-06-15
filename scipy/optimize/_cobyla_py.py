02889b07f2c23e439409b12f4d2c044a7518ae28
Nickolai Belakovski <nbelakovski@gmail.com>
2024-06-15 11:12:30 -0400

    MAINT: Remove old COBYLA

--- scipy/optimize/_cobyla_py.py
+++ scipy/optimize/_cobyla_py.py
@@ -14,7 +14,6 @@ import functools
 from threading import RLock
 
 import numpy as np
-from scipy.optimize import _cobyla as cobyla
 from ._optimize import (OptimizeResult, _check_unknown_options,
     _prepare_scalar_function)
 try:
