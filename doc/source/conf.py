d0e7d4646bc55474bf3271ff6f2322f6469cb69a
Tyler Reddy <tyler.je.reddy@gmail.com>
2024-07-04 16:02:01 -0600

    MAINT, DOC: simplify docs warn filter
    * gh-19228 has been resolved for quite some time now
    so let's remove the `conf.py` warnings filter shim
    that was associated with it. Docs build worked locally.
    
    [docs only]

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -184,12 +184,6 @@ warnings.filterwarnings(
     message=r'There is no current event loop',
     category=DeprecationWarning,
 )
-# TODO: remove after gh-19228 resolved:
-warnings.filterwarnings(
-    'ignore',
-    message=r'.*path is deprecated.*',
-    category=DeprecationWarning,
-)
 
 # -----------------------------------------------------------------------------
 # HTML output
