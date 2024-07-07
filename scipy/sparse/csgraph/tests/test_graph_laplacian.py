ab99630995fa94a285aabefd538b26a3ad93dc8a
Andrew Knyazev, Professor Emeritus <andrew.knyazev@ucdenver.edu>
2024-07-07 18:12:55 -0400

    simplify dtypes def test_graph_laplacian.py
    use tuples directly

--- scipy/sparse/csgraph/tests/test_graph_laplacian.py
+++ scipy/sparse/csgraph/tests/test_graph_laplacian.py
@@ -161,11 +161,11 @@ def _check_laplacian_dtype(
                 _assert_allclose_sparse(L, mat)
 
 
-INT_DTYPES = {np.intc, np_long, np.longlong}
-REAL_DTYPES = {np.float32, np.float64, np.longdouble}
-COMPLEX_DTYPES = {np.complex64, np.complex128, np.clongdouble}
+INT_DTYPES = (np.intc, np_long, np.longlong)
+REAL_DTYPES = (np.float32, np.float64, np.longdouble)
+COMPLEX_DTYPES = (np.complex64, np.complex128, np.clongdouble)
 # use sorted list to ensure fixed order of tests
-DTYPES = sorted(INT_DTYPES ^ REAL_DTYPES ^ COMPLEX_DTYPES, key=str)
+DTYPES = INT_DTYPES + REAL_DTYPES + COMPLEX_DTYPES
 
 
 @pytest.mark.parametrize("dtype", DTYPES)
