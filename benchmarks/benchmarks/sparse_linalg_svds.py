7fd28b1b780299040556ce8c0a2e40740c08df17
Andrew Knyazev, Professor Emeritus <andrew.knyazev@ucdenver.edu>
2024-07-19 15:44:17 +0000

    "final" clean version, with propack in

--- benchmarks/benchmarks/sparse_linalg_svds.py
+++ benchmarks/benchmarks/sparse_linalg_svds.py
@@ -12,19 +12,34 @@ with safe_import():
 class BenchSVDS(Benchmark):
     # Benchmark SVDS using the MatrixMarket test matrices recommended by the
     # author of PROPACK at http://sun.stanford.edu/~rmunk/PROPACK/
+    # The dense SVD solve is benchmarked as the baseline.
     # 'arpack' convergence uniformly for all singular values, while
     # 'lobpcg' extreme singular values may converge much faster, so
-    # the assert is only evaluated on the top k/2 singular values.
-    # The assert uses the relative error since for some tested matrices
+    # the accuracy is only checked on the top k/2 singular values.
+    # The check uses the relative error since for some tested matrices
     # the maximal singular values are very large due to poor matrix scaling.
-    # The `maxiter` and `tol` paremeters of `svds` are tuned for fair comaprison.
-    # The dense SVD solve is benchmarked as the base.
+    # On 7/19/24, all accuracy checks pass with ``maxiter = 200, tol=1e-6)``.
+    # If changes are made to a solver, the accuracy check may fail
+    # resulting in the failure of the benchmark for the solver. 
+
+    # use a custom timiout for the benchmark coordinate with the sleep
+    # value in the last line.
+    timeout =  20
+    # problems ``tols4000`` and ``west2021`` take too long and may fail
+    # with some solvers so excluded from the benchmark.
     params = [
         [20],
-        ["abb313", "illc1033", "illc1850", "qh1484", "rbs480a", "tols4000",
-         "well1033", "well1850", "west0479", "west2021"],
-        # TODO: re-include propack
-        ['arpack', 'lobpcg', 'svd']  # 'propack' failing (Aug. 2023)
+        [
+            "abb313",
+            "illc1033",
+            "illc1850",
+            "qh1484",
+            "rbs480a",
+            "well1033",
+            "well1850",
+            "west0479",
+        ],
+        ["propack", "arpack", "lobpcg", "svd"],
     ]
     param_names = ['k', 'problem', 'solver']
 
@@ -35,10 +50,6 @@ class BenchSVDS(Benchmark):
         self.matrices = np.load(datafile, allow_pickle=True)  
 
     def setup(self, k, problem, solver):
-        if solver == 'svd' and problem == "tols4000":
-            # skip: fails breaking the benchmark
-            raise NotImplementedError()
-
         self.A = self.matrices[problem][()]
         _, s, _ = svd(self.A.toarray(), full_matrices=False)
         self.top_singular_values = np.flip(s[:int(k/2)])
@@ -55,16 +66,15 @@ class BenchSVDS(Benchmark):
         else:
             with warnings.catch_warnings():
                 warnings.simplefilter("ignore")
+                # parameters `maxiter` and `tol` are tuned for fair comparison
                 _, s, _ = svds(self.A, k=k, solver=solver, random_state=self.rng,
-                               maxiter = 50, tol=1e-6)
+                            maxiter = 200, tol=1e-6)
             accuracy = np.sum(np.abs(1 - s[int(k/2):] / self.top_singular_values))
             # ensure that we are benchmarking a consistent outcome;
             # (e.g. if the code wasn't able to find a solution accurately
             # enough the timing of the benchmark would become useless).
-            msg = ("the benchmark code did not converge as expected, "
-                   "the timing is therefore useless")
-            assert accuracy < self.tol, msg
-            # if accuracy < self.tol:
-            #     # failed to converge sufficently for timing to be useful
-            #     # ensure we "destroy" performance so that benchmark suite picks up
-            #     time.sleep(100)
+            if accuracy > self.tol:
+                # no convergence sufficently for timing to be relevant;
+                # ensure that the unconverged benchmark fails by sleeping
+                # over the timeout limit.
+                time.sleep(20)
