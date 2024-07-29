e3a2efd2b9eb1a4325dfeeb915e2f9d257f99240
H. Vetinari <h.vetinari@gmx.com>
2024-07-29 23:17:15 +1100

    adapt to ndimage changes

--- scipy/_lib/_array_api_no_0d.py
+++ scipy/_lib/_array_api_no_0d.py
@@ -84,3 +84,21 @@ def xp_assert_less(actual, desired, *, check_0d=False, **kwargs):
     if not check_0d:
         _check_scalar(actual, desired, **kwargs)
     return xp_assert_less_base(actual, desired, check_0d=check_0d, **kwargs)
+
+
+def assert_array_almost_equal(actual, desired, decimal=6, *args, **kwds):
+    """Backwards compatible replacement. In new code, use xp_assert_close instead.
+    """
+    rtol, atol = 0, 1.5*10**(-decimal)
+    return xp_assert_close(actual, desired,
+                           atol=atol, rtol=rtol, check_dtype=False, check_shape=False,
+                           *args, **kwds)
+
+
+def assert_almost_equal(actual, desired, decimal=7, *args, **kwds):
+    """Backwards compatible replacement. In new code, use xp_assert_close instead.
+    """
+    rtol, atol = 0, 1.5*10**(-decimal)
+    return xp_assert_close(actual, desired,
+                           atol=atol, rtol=rtol, check_dtype=False, check_shape=False,
+                           *args, **kwds)
