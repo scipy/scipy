7ec3a349ed9753e9e7caf5364dd09d9a5ed3c3b2
Matt Haberland <mhaberla@calpoly.edu>
2024-08-04 06:50:30 -0700

    MAINT: special.chdtr: fix generic chdtr (#21316)

--- scipy/special/tests/test_support_alternative_backends.py
+++ scipy/special/tests/test_support_alternative_backends.py
@@ -98,3 +98,15 @@ def test_support_alternative_backends(xp, f_name_n_args, dtype, shapes):
 
     eps = np.finfo(dtype_np).eps
     xp_assert_close(res, ref, atol=10*eps)
+
+
+@array_api_compatible
+def test_chdtr_gh21311(xp):
+    # the edge case behavior of generic chdtr was not right; see gh-21311
+    # be sure to test at least these cases
+    # should add `np.nan` into the mix when gh-21317 is resolved
+    x = np.asarray([-np.inf, -1., 0., 1., np.inf])
+    v = x.reshape(-1, 1)
+    ref = special.chdtr(v, x)
+    res = special.chdtr(xp.asarray(v), xp.asarray(x))
+    xp_assert_close(res, xp.asarray(ref))
