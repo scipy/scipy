6fd0e851e7caee7f8dc1aba53eb533575c2c3965
Matt Haberland <mhaberla@calpoly.edu>
2024-08-08 10:34:45 -0700

    TST: linalg.lapack.?trcon: adjust test

--- scipy/linalg/tests/test_lapack.py
+++ scipy/linalg/tests/test_lapack.py
@@ -3,6 +3,7 @@
 #
 
 from functools import reduce
+import random
 
 from numpy.testing import (assert_equal, assert_array_almost_equal, assert_,
                            assert_allclose, assert_almost_equal,
@@ -657,14 +658,18 @@ class TestTbtrs:
 @pytest.mark.parametrize('diag', ['N', 'U'])
 @pytest.mark.parametrize('n', [3, 10])
 def test_trcon(dtype, norm, uplo, diag, n):
-    rng = np.random.default_rng(76563598)
+    # Simple way to get deterministic (unlike `hash`) integer seed based on arguments
+    random.seed(f"{dtype}{norm}{uplo}{diag}{n}")
+    rng = np.random.default_rng(random.randint(0, 9999999999999))
 
     A = rng.random(size=(n, n)) + rng.random(size=(n, n))*1j
+    # make the condition numbers more interesting
+    offset = rng.permuted(np.logspace(0, rng.integers(0, 10), n))
+    A += offset
     A = A.real if np.issubdtype(dtype, np.floating) else A
     A = np.triu(A) if uplo == 'U' else np.tril(A)
     if diag == 'U':
-        i = np.arange(n)
-        A[i, i] = 1
+        A /= np.diag(A)[:, np.newaxis]
     A = A.astype(dtype)
 
     trcon = get_lapack_funcs('trcon', (A,))
@@ -680,7 +685,10 @@ def test_trcon(dtype, norm, uplo, diag, n):
         lu, ipvt, info = getrf(A)
         ref, _ = gecon(lu, anorm, norm=norm)
 
-    rtol = np.finfo(dtype).eps**0.75
+    # This is an estimate of reciprocal condition number; we just need order of
+    # magnitude. In testing, we observed that much smaller rtol is OK in almost
+    # all cases... but sometimes it isn't.
+    rtol = 1  # np.finfo(dtype).eps**0.75
     assert_allclose(res, ref, rtol=rtol)
 
 
