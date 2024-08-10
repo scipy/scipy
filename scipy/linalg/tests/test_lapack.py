99c990758a11e1e35e1b9f5939e9c43651b28e7c
Matt Haberland <mhaberla@calpoly.edu>
2024-08-10 13:55:22 -0700

    ENH: linalg: add Python wrapper of ?trcon (#21331)
    * ENH: linalg: add Python wrapper for ?trcon
    
    * MAINT: linalg: fixup Python wrapper for ?trcon
    
    * MAINT: linalg: consolidate ?trcon wrapper
    
    * DOC: linalg.lapack.?trcon: list as public function
    
    * Update scipy/linalg/flapack_other.pyf.src
    
    * TST: linalg.lapack.?trcon: adjust test

--- scipy/linalg/tests/test_lapack.py
+++ scipy/linalg/tests/test_lapack.py
@@ -3,6 +3,7 @@
 #
 
 from functools import reduce
+import random
 
 from numpy.testing import (assert_equal, assert_array_almost_equal, assert_,
                            assert_allclose, assert_almost_equal,
@@ -648,6 +649,47 @@ class TestTbtrs:
         b = np.ones((ldb, nrhs), dtype=float)
         tbtrs = get_lapack_funcs('tbtrs', dtype=float)
         assert_raises(Exception, tbtrs, ab, b)
+
+
+
+@pytest.mark.parametrize('dtype', DTYPES)
+@pytest.mark.parametrize('norm', ['I', '1', 'O'])
+@pytest.mark.parametrize('uplo', ['U', 'L'])
+@pytest.mark.parametrize('diag', ['N', 'U'])
+@pytest.mark.parametrize('n', [3, 10])
+def test_trcon(dtype, norm, uplo, diag, n):
+    # Simple way to get deterministic (unlike `hash`) integer seed based on arguments
+    random.seed(f"{dtype}{norm}{uplo}{diag}{n}")
+    rng = np.random.default_rng(random.randint(0, 9999999999999))
+
+    A = rng.random(size=(n, n)) + rng.random(size=(n, n))*1j
+    # make the condition numbers more interesting
+    offset = rng.permuted(np.logspace(0, rng.integers(0, 10), n))
+    A += offset
+    A = A.real if np.issubdtype(dtype, np.floating) else A
+    A = np.triu(A) if uplo == 'U' else np.tril(A)
+    if diag == 'U':
+        A /= np.diag(A)[:, np.newaxis]
+    A = A.astype(dtype)
+
+    trcon = get_lapack_funcs('trcon', (A,))
+    res, _ = trcon(A, norm=norm, uplo=uplo, diag=diag)
+
+    if norm == 'I':
+        norm_A = np.linalg.norm(A, ord=np.inf)
+        norm_inv_A = np.linalg.norm(np.linalg.inv(A), ord=np.inf)
+        ref = 1 / (norm_A * norm_inv_A)
+    else:
+        anorm = np.abs(A).sum(axis=0).max()
+        gecon, getrf = get_lapack_funcs(('gecon', 'getrf'), (A,))
+        lu, ipvt, info = getrf(A)
+        ref, _ = gecon(lu, anorm, norm=norm)
+
+    # This is an estimate of reciprocal condition number; we just need order of
+    # magnitude. In testing, we observed that much smaller rtol is OK in almost
+    # all cases... but sometimes it isn't.
+    rtol = 1  # np.finfo(dtype).eps**0.75
+    assert_allclose(res, ref, rtol=rtol)
 
 
 def test_lartg():
