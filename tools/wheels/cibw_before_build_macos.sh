9d1e829f0390c10c08070d28730c098367d0745d
Warren Weckesser <warren.weckesser@gmail.com>
2024-08-14 17:37:34 -0400

    MAINT: Pin pybind11==2.13.1 in more files.

--- tools/wheels/cibw_before_build_macos.sh
+++ tools/wheels/cibw_before_build_macos.sh
@@ -67,7 +67,7 @@ if [[ $FREE_THREADED_BUILD == "True" ]]; then
     python -m pip install -U --pre pip
     python -m pip install -i https://pypi.anaconda.org/scientific-python-nightly-wheels/simple numpy cython
     # python -m pip install git+https://github.com/serge-sans-paille/pythran
-    python -m pip install ninja meson-python pybind11 pythran
+    python -m pip install ninja meson-python pybind11==2.13.1 pythran
 fi
 
 # Install Openblas
