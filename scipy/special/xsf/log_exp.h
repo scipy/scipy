9e9d534b7afac90e8d2fa23be1d0ab1201c1bde1
Warren Weckesser <warren.weckesser@gmail.com>
2024-08-14 23:28:18 -0400

    ENH: special: Improve precision of special.logit. (#21388)
    * ENH: special: Improve precision of special.logit

--- scipy/special/xsf/log_exp.h
+++ scipy/special/xsf/log_exp.h
@@ -27,7 +27,18 @@ inline float exprel(float x) { return exprel(static_cast<double>(x)); }
 
 template <typename T>
 T logit(T x) {
-    return std::log(x / (1 - x));
+    // The standard formula is log(x/(1 - x)), but this expression
+    // loses precision near x=0.5, as does log(x) - log1p(-x).
+    // We use the standard formula away from p=0.5, and use
+    // log1p(2*(x - 0.5)) - log1p(-2*(x - 0.5)) around p=0.5, which
+    // provides very good precision in this interval.
+    if (x < 0.3 || x > 0.65) {
+        return std::log(x/(1 - x));
+    }
+    else {
+        T s = 2*(x - 0.5);
+        return std::log1p(s) - std::log1p(-s);
+    }
 };
 
 //
