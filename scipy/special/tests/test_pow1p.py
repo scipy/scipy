1e2e9e1d85a7ae50004830bb95d8b7225643e259
fancidev <fancidev@gmail.com>
2024-08-20 21:39:24 +0800

    Add test cases for IEEE-754 special values.

--- scipy/special/tests/test_pow1p.py
+++ scipy/special/tests/test_pow1p.py
@@ -21,3 +21,97 @@ class TestPow1p:
         res64 = pow1p(x64, y64)
         assert isinstance(res64, np.float64)
         assert_equal(res32, np.float32(res64))
+
+    @pytest.mark.parametrize('rule, x, y, out', [
+        (
+            "pow(anything, +0) is 1",
+            [1.5, -1.0, -np.inf, np.inf, np.nan], 0.0, 1.0
+        ),
+        (
+            "pow(anything, -0) is 1",
+            [1.5, -1.0, -np.inf, np.inf, np.nan], -0.0, 1.0
+        ),
+        (
+            "pow(±0, negative odd integer) is ±∞ and signals divideByZero",
+            -1, [-3.0, -1.0], np.inf
+        ),
+        (
+            "pow(±0, -∞) is +∞ with no exception",
+            -1.0, -np.inf, np.inf
+        ),
+        (
+            "pow(±0, +∞) is +0 with no exception",
+            -1.0, np.inf, 0.0
+        ),
+        (
+            "pow(±0, positive odd integer) is ±0",
+            -1.0, [1.0, 3.0], 0.0,
+        ),
+        (
+            "pow(-1, ±∞) is 1 with no exception",
+            -2.0, [np.inf, -np.inf], 1.0
+        ),
+        (
+            "pow(+1, anything) is 1",
+            0.0, [1.5, -1.0, -np.inf, np.inf, np.nan], 1.0
+        ),
+        (
+            "pow(|x| < 1, +∞) is +0",
+            [np.nextafter(-2, 0), -1.0, np.nextafter(0, -1)], np.inf, 0.0
+        ),
+        (
+            "pow(|x| > 1, +∞) is +∞",
+            [np.nextafter(-2, -3), np.nextafter(0, 1), -np.inf, np.inf],
+            np.inf, np.inf
+        ),
+        (
+            "pow(|x| < 1, -∞) is +∞",
+            [np.nextafter(-2, 0), -1.0, np.nextafter(0, -1)], -np.inf, np.inf
+        ),
+        (
+            "pow(|x| > 1, -∞) is +0",
+            [np.nextafter(-2, -3), np.nextafter(0, 1), -np.inf, np.inf],
+            -np.inf, 0.0
+        ),
+        (
+            "pow(+∞, y < 0) is +0",
+            np.inf, [-np.inf, -2.5, np.nextafter(0, -1)], 0.0
+        ),
+        (
+            "pow(+∞, y > 0) is +∞",
+            np.inf, [np.inf, +2.5, np.nextafter(0, 1)], np.inf
+        ),
+        (
+            "pow(-∞, negative odd integer y) is -0",
+            -np.inf, [-3.0, -1.0], -0.0
+        ),
+        (
+            "pow(-∞, positive odd integer y) is -∞",
+            -np.inf, [1.0, 3.0], -np.inf
+        ),
+        (
+            "pow(-∞, negative finite y not an odd integer) is +0",
+            -np.inf, [-2.0, -3.5], +0.0,
+        ),
+        (
+            "pow(-∞, positive finite y not an odd integer) is +∞",
+            -np.inf, [2.0, 3.5], np.inf,
+        ),
+        (
+            "pow(±0, negative finite y not an odd integer) is +∞ "
+            "and signals divideByZero",
+            -1, [-2.0, -3.5], np.inf,
+        ),
+        (
+            "pow(±0, positive finite y not an odd integer) is +0",
+            -1, [2.0, 3.5], 0,
+        ),
+        (
+            "pow(finite negative x, finite non-integer y) "
+            "signals the invalid operation exception",
+            -2.5, [1.5, -2.5], np.nan,
+        )
+    ])
+    def test_ieee754_special_values(self, rule, x, y, out):
+        # Special value inputs should conform to IEEE-754 spec of pow().
+        assert_equal(pow1p(x, y), out, rule)
