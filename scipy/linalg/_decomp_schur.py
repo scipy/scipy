839dda0f0f32023ceb5ab107f282ee9f398d6095
Matt Haberland <mhaberla@calpoly.edu>
2024-08-22 22:02:46 -0700

    STY: linalg.schur: fix mypy complaint

--- scipy/linalg/_decomp_schur.py
+++ scipy/linalg/_decomp_schur.py
@@ -173,7 +173,7 @@ def schur(a, output='real', lwork=None, overwrite_a=False, sort=None,
 
     if sort is None:
         sort_t = 0
-        def sfunction(x):
+        def sfunction(x, y=None):
             return None
     else:
         sort_t = 1
