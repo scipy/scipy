4d01f2aee7e18d24035888bfb383d768c940b61e
anushkasuyal <anushkasuyal@hotmail.com>
2024-08-30 15:27:14 +0100

    lots of refactoring for matmul, tensordot, dot

--- scipy/sparse/_base.py
+++ scipy/sparse/_base.py
@@ -679,8 +679,7 @@ class _spbase:
 
             return result
 
-        else:
-            raise ValueError('could not interpret dimensions')
+        return NotImplemented
 
     def __mul__(self, other):
         return self.multiply(other)
