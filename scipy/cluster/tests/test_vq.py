e8f2183ee08367425088c371b6d0e15721064d06
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2024-09-04 15:32:31 -0500

    FIX: Further remove global state from tests in cluster and differentiate

--- scipy/cluster/tests/test_vq.py
+++ scipy/cluster/tests/test_vq.py
@@ -1,6 +1,7 @@
 import warnings
 import sys
 from copy import deepcopy
+from threading import Lock
 
 import numpy as np
 from numpy.testing import (
@@ -97,7 +98,6 @@ class TestWhiten:
 
     @pytest.fixture
     def whiten_lock(self):
-        from threading import Lock
         return Lock()
 
     @skip_xp_backends('jax.numpy',
@@ -353,20 +353,25 @@ class TestKMean:
             kmeans2(data, k, minit='random', rng=rng)
             kmeans2(data[:, 1], k, minit='random', rng=rng)  # special case (1-D)
 
+    @pytest.fixture
+    def krand_lock(self):
+        return Lock()
+
     @pytest.mark.skipif(sys.platform == 'win32',
                         reason='Fails with MemoryError in Wine.')
-    def test_krandinit(self, xp):
+    def test_krandinit(self, xp, krand_lock):
         data = xp.asarray(TESTDATA_2D)
         datas = [xp.reshape(data, (200, 2)),
                  xp.reshape(data, (20, 20))[:10, :]]
         k = int(1e6)
         xp_test = array_namespace(data)
-        for data in datas:
-            rng = np.random.default_rng(1234)
-            init = _krandinit(data, k, rng, xp_test)
-            orig_cov = xpx.cov(data.T, xp=xp_test)
-            init_cov = xpx.cov(init.T, xp=xp_test)
-            xp_assert_close(orig_cov, init_cov, atol=1.1e-2)
+        with krand_lock:
+            for data in datas:
+                rng = np.random.default_rng(1234)
+                init = _krandinit(data, k, rng, xp_test)
+                orig_cov = xpx.cov(data.T, xp=xp_test)
+                init_cov = xpx.cov(init.T, xp=xp_test)
+                xp_assert_close(orig_cov, init_cov, atol=1.1e-2)
 
     def test_kmeans2_empty(self, xp):
         # Regression test for gh-1032.
