448ee40500d5402bd29a46af4a183923bf3f20cd
Albert Steppi <albert.steppi@gmail.com>
2024-09-04 04:32:07 -0400

    Update Cython special to use Boost ncfdtri

--- scipy/special/cython_special.pyx
+++ scipy/special/cython_special.pyx
@@ -1710,10 +1710,6 @@ from ._cdflib_wrappers cimport nbdtrin as _func_nbdtrin
 ctypedef double _proto_nbdtrin_t(double, double, double) noexcept nogil
 cdef _proto_nbdtrin_t *_proto_nbdtrin_t_var = &_func_nbdtrin
 
-from ._cdflib_wrappers cimport ncfdtri as _func_ncfdtri
-ctypedef double _proto_ncfdtri_t(double, double, double, double) noexcept nogil
-cdef _proto_ncfdtri_t *_proto_ncfdtri_t_var = &_func_ncfdtri
-
 from ._cdflib_wrappers cimport ncfdtridfd as _func_ncfdtridfd
 ctypedef double _proto_ncfdtridfd_t(double, double, double, double) noexcept nogil
 cdef _proto_ncfdtridfd_t *_proto_ncfdtridfd_t_var = &_func_ncfdtridfd
@@ -3183,9 +3179,17 @@ cpdef df_number_t ncfdtr(df_number_t x0, df_number_t x1, df_number_t x2, df_numb
         else:
             return NAN
 
-cpdef double ncfdtri(double x0, double x1, double x2, double x3) noexcept nogil:
+cpdef df_number_t ncfdtri(df_number_t x0, df_number_t x1, df_number_t x2, df_number_t x3) noexcept nogil:
     """See the documentation for scipy.special.ncfdtri"""
-    return _func_ncfdtri(x0, x1, x2, x3)
+    if df_number_t is float:
+        return (<float(*)(float, float, float, float) noexcept nogil>scipy.special._ufuncs_cxx._export_ncf_ppf_float)(x0, x1, x2, x3)
+    elif df_number_t is double:
+        return (<double(*)(double, double, double, double) noexcept nogil>scipy.special._ufuncs_cxx._export_ncf_ppf_double)(x0, x1, x2, x3)
+    else:
+        if df_number_t is double:
+            return NAN
+        else:
+            return NAN
 
 cpdef double ncfdtridfd(double x0, double x1, double x2, double x3) noexcept nogil:
     """See the documentation for scipy.special.ncfdtridfd"""
