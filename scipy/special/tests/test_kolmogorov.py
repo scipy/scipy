d7353a905b6d947c0532c2b17707e892daaa0381
Matt Haberland <mhaberla@calpoly.edu>
2024-09-08 00:00:34 -0400

    TST: special._smirnovp: remove test xfail after translation to C++

--- scipy/special/tests/test_kolmogorov.py
+++ scipy/special/tests/test_kolmogorov.py
@@ -303,8 +303,6 @@ class TestSmirnovp:
             _smirnovp, dataset0, (0, 1), 2, rtol=_rtol
         ).check(dtypes=[int, float, float])
 
-    @pytest.mark.xfail(sys.maxsize <= 2**32,
-                       reason="requires 64-bit platform")
     def test_oneovernclose(self):
         # Check derivative at x=1/n
         # (Discontinuous at x=1/n, test on either side: x=1/n +/- 2epsilon)
