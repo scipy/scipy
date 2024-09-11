9776652b8333da4bd7a6547a3872d666d0664b15
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2024-09-11 18:32:36 +0300

    MAINT: interpolate: add docstrings to _dierckxmodule from _bspl.pyx

--- scipy/interpolate/src/_dierckxmodule.cc
+++ scipy/interpolate/src/_dierckxmodule.cc
@@ -302,6 +302,38 @@ py_data_matrix(PyObject *self, PyObject *args)
 }
 
 
+static char doc_coloc[] =
+    "Build the B-spline colocation matrix. \n"
+    "\n"
+    "The colocation matrix is defined as :math:`B_{j,l} = B_l(x_j)`, \n"
+    "so that row ``j`` contains all the B-splines which are non-zero \n"
+    "at ``x_j``. \n"
+    "\n"
+    "The matrix is constructed in the LAPACK banded storage. \n"
+    "Basically, for an N-by-N matrix A with ku upper diagonals and \n"
+    "kl lower diagonals, the shape of the array Ab is (2*kl + ku +1, N), \n"
+    "where the last kl+ku+1 rows of Ab contain the diagonals of A, and \n"
+    "the first kl rows of Ab are not referenced. \n"
+    "For more info see, e.g. the docs for the ``*gbsv`` routine. \n"
+    "\n"
+    "This routine is not supposed to be called directly, and \n"
+    "does no error checking. \n"
+    "\n"
+    "Parameters \n"
+    "---------- \n"
+    "x : ndarray, shape (n,) \n"
+    "    sorted 1D array of x values \n"
+    "t : ndarray, shape (nt + k + 1,) \n"
+    "    sorted 1D array of knots \n"
+    "k : int \n"
+    "    spline order \n"
+    "ab : ndarray, shape (2*kl + ku + 1, nt), F-order \n"
+    "    This parameter is modified in-place. \n"
+    "    On exit: B-spline colocation matrix in the band storage with \n"
+    "    ``ku`` upper diagonals and ``kl`` lower diagonals. \n"
+    "    Here ``kl = ku = k``. \n"
+    "offset : int, optional \n"
+    "    skip this many rows \n";
 /*
  * def _coloc(const double[::1] x,
  *             const double[::1] t,
@@ -355,6 +387,42 @@ py_coloc(PyObject *self, PyObject *args)
     }
 }
 
+static char doc_norm_eq_lsq[] =
+    "Construct the normal equations for the B-spline LSQ problem. \n"
+    " \n"
+    "The observation equations are ``A @ c = y``, and the normal equations are \n"
+    "``A.T @ A @ c = A.T @ y``. This routine fills in the rhs and lhs for the \n"
+    "latter. \n"
+    " \n"
+    "The B-spline collocation matrix is defined as :math:`A_{j,l} = B_l(x_j)`, \n"
+    "so that row ``j`` contains all the B-splines which are non-zero \n"
+    "at ``x_j``. \n"
+    " \n"
+    "The normal eq matrix has at most `2k+1` bands and is constructed in the \n"
+    "LAPACK symmetrix banded storage: ``A[i, j] == ab[i-j, j]`` with `i >= j`. \n"
+    "See the doctsring for `scipy.linalg.cholesky_banded` for more info. \n"
+    " \n"
+    "Parameters \n"
+    "---------- \n"
+    "x : ndarray, shape (n,) \n"
+    "    sorted 1D array of x values \n"
+    "t : ndarray, shape (nt + k + 1,) \n"
+    "    sorted 1D array of knots \n"
+    "k : int \n"
+    "    spline order \n"
+    "y : ndarray, shape (n, s) \n"
+    "    a 2D array of y values. The second dimension contains all trailing \n"
+    "    dimensions of the original array of ordinates. \n"
+    "w : ndarray, shape(n,) \n"
+    "    Weights. \n"
+    "ab : ndarray, shape (k+1, n), in Fortran order. \n"
+    "    This parameter is modified in-place. \n"
+    "    On entry: should be zeroed out. \n"
+    "    On exit: LHS of the normal equations. \n"
+    "rhs : ndarray, shape (n, s), in C order. \n"
+    "    This parameter is modified in-place. \n"
+    "    On entry: should be zeroed out. \n"
+    "    On exit: RHS of the normal equations. \n";
 
 /*
  * def _norm_eq_lsq(const double[::1] x,
@@ -415,6 +483,26 @@ py_norm_eq_lsq(PyObject *self, PyObject *args)
     }
 
 }
+
+
+static char doc_evaluate_spline[] =
+   "Evaluate a spline in the B-spline basis. \n"
+   "\n"
+   "Parameters \n"
+   "---------- \n"
+   "t : ndarray, shape (n+k+1) \n"
+   "     knots \n"
+   " c : ndarray, shape (n, m) \n"
+   "     B-spline coefficients \n"
+   " xp : ndarray, shape (s,) \n"
+   "     Points to evaluate the spline at. \n"
+   " nu : int \n"
+   "     Order of derivative to evaluate. \n"
+   " extrapolate : int, optional \n"
+   "     Whether to extrapolate to ouf-of-bounds points, or to return NaNs. \n"
+   " out : ndarray, shape (s, m) \n"
+   "     Computed values of the spline at each of the input points. \n"
+   "     This argument is modified in-place. \n";
 
 
 /*
@@ -565,11 +653,11 @@ static PyMethodDef DierckxMethods[] = {
     {"data_matrix", py_data_matrix, METH_VARARGS,
      "(m, k+1) array of non-zero b-splines"},
     {"_coloc", py_coloc, METH_VARARGS,
-     "colocation matrix in the F banded storage"},
+      doc_coloc},
     {"_norm_eq_lsq", py_norm_eq_lsq, METH_VARARGS,
-     "lhs and rhs of the normal equations for a spline fit"},
+     doc_norm_eq_lsq},
     {"evaluate_spline", py_evaluate_spline, METH_VARARGS,
-     "evaluate the spline function"},
+     doc_evaluate_spline},
     {"find_interval", py_find_interval, METH_VARARGS,
      doc_find_interval},
     //...
