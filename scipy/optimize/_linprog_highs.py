16ecb99e27fe94213c9ad3cde19b97bb22842224
Rohit Goswami <rog32@hi.is>
2024-09-16 02:06:40 +0000

    BLD: Rework for CI errors
    MAINT: Fixup mypy
    
    BLD: Silence more warnings from highs
    
    MAINT: Update highs commit
    
    MAINT: Bump highs commit
    
    BLD: Rework to have more control over highs build
    
    MAINT: Try fixing mypy again

--- scipy/optimize/_linprog_highs.py
+++ scipy/optimize/_linprog_highs.py
@@ -56,8 +56,9 @@ def _highs_to_scipy_status_message(highs_status, highs_message):
     unrecognized = (4, "The HiGHS status code was not recognized. ")
     scipy_status, scipy_message = (
         scipy_statuses_messages.get(highs_status, unrecognized))
+    hstat = int(highs_status) if highs_status is not None else None
     scipy_message = (f"{scipy_message}"
-                     f"(HiGHS Status {int(highs_status) if highs_status != None else None}: {highs_message})")
+                     f"(HiGHS Status {hstat}: {highs_message})")
     return scipy_status, scipy_message
 
 
