4ccf61e8abd23e8c5ae5c045a70e8e019bc577e8
Max Aehle <max.aehle@scicomp.uni-kl.de>
2024-09-24 12:56:50 +0200

    ENH: add scipy.sparse.linalg.rinvnormest
    Calls scipy.sparse.linalg.splu and then calls the rinvnorm
    function of the resulting LU decomposition.

---
+++ scipy/sparse/linalg/_rinvnormest.py
@@ -0,0 +1,45 @@
+from scipy.sparse.linalg import splu
+
+__all__ = ['rinvnormest']
+
+
+def rinvnormest(A, norm="1"):
+    """
+    Compute an estimate for the reciprocal of the norm of the inverse 
+    of a sparse matrix.
+
+    Parameters
+    ----------
+    A : ndarray or other linear operator
+        A sparse matrix for which an LU matrix can be computed.
+    norm : string, optional
+        "1"/"0" [default] computes the 1-norm, "I" computes the inf-norm.
+
+    Returns
+    -------
+    est : float
+        An estimate of the norm of the inverse matrix.
+
+    Notes
+    -----
+    Computes an LU decomposition and runs the gscon procedure from SuperLU.
+    Use scipy.sparse.linalg.SuperLU.rinvnormest if you already have an LU 
+    decomposition.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from scipy.sparse import csc_matrix
+    >>> from scipy.sparse.linalg import rinvnormest
+    >>> A = csc_matrix([[1., 0., 0.], [5., 8., 2.], [0., -1., 0.]], dtype=float)
+    >>> A.toarray()
+    array([[ 1.,  0.,  0.],
+           [ 5.,  8.,  2.],
+           [ 0., -1.,  0.]])
+    >>> rinvnormest(A,norm="1")
+    0.2
+    >>> 1/np.linalg.norm(np.linalg.inv(A.toarray()), ord=1)
+    0.2
+    """
+    lu_decomposition = splu(A)
+    return lu_decomposition.rinvnormest(norm=norm)
