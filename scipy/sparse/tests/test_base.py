287c8647ebe83a642785f5a2d6fbdcadec2c60ec
Anushka Suyal <126159239+anushkasuyal@users.noreply.github.com>
2024-09-30 19:29:50 +0100

    ENH: sparse: add CSR/CSC `_broadcast_to` method (#21412)
    * broadcast_to added for CSR matrices
    
    * correction made in broadcast_to, tests added
    
    * eliminate_zeros removed
    
    * .copy() added
    
    * added NotImplementedError for CSC
    
    * lint error fixed
    
    * broadcast_to moved to _csr_base in _csr.py
    
    * np.broadcast_shapes used
    
    * add self.sum_duplicates()
    
    * adding copy=False
    
    * copy argument passed in broadcast_to
    
    * reverted ValueError condition, modified tests
    
    * _broadcast_to made helper function
    
    * method name updated in tests
    
    * changes made to _broadcast_to
    
    * Update scipy/sparse/_csr.py
    
    Co-authored-by: CJ Carey <perimosocordiae@gmail.com>
    
    * _broadcast_to allows 1D to 1D/2D
    
    * lint error fixed
    
    * move test_broadcast_to to test_csr.py
    
    * indentation fixed
    
    * unused res_f removed
    
    * shape len equality check removed
    
    * Update scipy/sparse/_csr.py
    
    Co-authored-by: Dan Schult <dschult@colgate.edu>
    
    * Update scipy/sparse/_csr.py
    
    Co-authored-by: Dan Schult <dschult@colgate.edu>
    
    * use np.broadcast_shapes
    
    * Update scipy/sparse/_csr.py
    
    Co-authored-by: Dan Schult <dschult@colgate.edu>
    
    * ValueError modified
    
    * blank line
    
    * update test to cover both csr_matrix and csr_array for 2D
    
    * CSC broadcasting added, _broadcast_to moved to _compressed.py
    
    * lint error
    
    * tests moved
    
    * lint errors
    
    * merge error
    
    ---------
    
    Co-authored-by: CJ Carey <perimosocordiae@gmail.com>
    Co-authored-by: Dan Schult <dschult@colgate.edu>

--- scipy/sparse/tests/test_base.py
+++ scipy/sparse/tests/test_base.py
@@ -5508,3 +5508,30 @@ class Test64BitMatrix(RunAll64Bit):
     @pytest.mark.parametrize('cls,method_name', cases_64bit("spmatrix"))
     def test_resiliency_all_64(self, cls, method_name):
         self._check_resiliency(cls, method_name, fixed_dtype=np.int64)
+
+def test_broadcast_to():
+    a = np.array([[1, 0, 2]])
+    b = np.array([[1], [0], [2]])
+    c = np.array([[1, 0, 2], [0, 3, 0]])
+    d = np.array([[7]])
+    e = np.array([[0]])
+    f = np.array([[0,0,0,0]])
+    for container in (csc_matrix, csc_array, csr_matrix, csr_array):
+        res_a = container(a)._broadcast_to((2,3))
+        res_b = container(b)._broadcast_to((3,4))
+        res_c = container(c)._broadcast_to((2,3))
+        res_d = container(d)._broadcast_to((4,4))
+        res_e = container(e)._broadcast_to((5,6))
+        res_f = container(f)._broadcast_to((2,4))
+        assert_array_equal(res_a.toarray(), np.broadcast_to(a, (2,3)))
+        assert_array_equal(res_b.toarray(), np.broadcast_to(b, (3,4)))
+        assert_array_equal(res_c.toarray(), c)
+        assert_array_equal(res_d.toarray(), np.broadcast_to(d, (4,4)))
+        assert_array_equal(res_e.toarray(), np.broadcast_to(e, (5,6)))
+        assert_array_equal(res_f.toarray(), np.broadcast_to(f, (2,4)))
+
+        with pytest.raises(ValueError, match="cannot be broadcast"):
+            container([[1, 2, 0], [3, 0, 1]])._broadcast_to(shape=(2, 1))
+
+        with pytest.raises(ValueError, match="cannot be broadcast"):
+            container([[0, 1, 2]])._broadcast_to(shape=(3, 2))
