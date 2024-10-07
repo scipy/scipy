3d7697a24656d36eaf6f9b6a03f78e37c3cfdc99
Jake Bowhay <60778417+j-bowhay@users.noreply.github.com>
2024-10-07 10:25:58 +0100

    Update scipy/integrate/_cubature.py
    Co-authored-by: Olly Britton <ollybritton@gmail.com>

--- scipy/integrate/_cubature.py
+++ scipy/integrate/_cubature.py
@@ -664,7 +664,7 @@ class _InfiniteLimitsTransform(_VariableTransform):
                                     / t[:, self._semi_inf_pos])
 
         jacobian_det = self._xp.prod(
-            1/(t[:, self._double_inf_pos | self._semi_inf_pos] ** 2)
+            1/(t[:, self._double_inf_pos | self._semi_inf_pos] ** 2, axis=-1)
         )
 
         f_x = self._f(x, *args, **kwargs)
