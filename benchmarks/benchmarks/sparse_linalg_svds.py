c0a63ac2341db12c079e55ba95f169c0bf4b04cd
Jake Bowhay <jb9.bowhay@gmail.com>
2024-10-08 08:11:51 +0100

    BENCH: use SkipNotImplemented rather than time.sleep

--- benchmarks/benchmarks/sparse_linalg_svds.py
+++ benchmarks/benchmarks/sparse_linalg_svds.py
@@ -1,8 +1,8 @@
 import os
-import time
 import warnings
 import numpy as np
 from .common import Benchmark, safe_import
+from asv_runner.benchmarks.mark import SkipNotImplemented
 
 with safe_import():
     from scipy.linalg import svd
@@ -13,18 +13,15 @@ class BenchSVDS(Benchmark):
     # Benchmark SVDS using the MatrixMarket test matrices recommended by the
     # author of PROPACK at http://sun.stanford.edu/~rmunk/PROPACK/
     # The dense SVD solve is benchmarked as the baseline.
-    # 'arpack' convergence uniformly for all singular values, while
-    # 'lobpcg' extreme singular values may converge much faster, so
+    # `arpack` convergence uniformly for all singular values, while
+    # `lobpcg` extreme singular values may converge much faster, so
     # the accuracy is only checked on the top k/2 singular values.
     # The check uses the relative error since for some tested matrices
     # the maximal singular values are very large due to poor matrix scaling.
-    # On 7/19/24, all accuracy checks pass with ``maxiter = 200, tol=1e-6)``.
+    # On 7/19/24, all accuracy checks pass with ``maxiter = 200, tol=1e-6``.
     # If changes are made to a solver, the accuracy check may fail
-    # resulting in the failure of the benchmark for the solver. 
+    # resulting in the failure of the benchmark for the solver.
 
-    # use a custom timiout for the benchmark coordinate with the sleep
-    # value in the last line.
-    timeout =  20
     # problems ``tols4000`` and ``west2021`` take too long and may fail
     # with some solvers so excluded from the benchmark.
     params = [
@@ -47,14 +44,14 @@ class BenchSVDS(Benchmark):
         dir_path = os.path.dirname(os.path.realpath(__file__))
         datafile = os.path.join(dir_path, "svds_benchmark_files",
                                 "svds_benchmark_files.npz")
-        self.matrices = np.load(datafile, allow_pickle=True)  
+        self.matrices = np.load(datafile, allow_pickle=True)
 
     def setup(self, k, problem, solver):
         self.A = self.matrices[problem][()]
         _, s, _ = svd(self.A.toarray(), full_matrices=False)
         self.top_singular_values = np.flip(s[:int(k/2)])
         self.tol = k * np.max(self.A.shape) * np.finfo(float).eps
-        self.rng = np.random.default_rng(0)
+        self.rng = np.random.default_rng(98360967947894649386)
 
     def time_svds(self, k, problem, solver):
         # The 'svd' solver find all ``m = np.min(self.A.shape) >> k``
@@ -62,19 +59,17 @@ class BenchSVDS(Benchmark):
         # the sparse solvers benchmarked here if m is small enough.
         # It is commonly thus used as a baseline for comparisons.
         if solver == 'svd':
-            _ = svd(self.A.toarray(), full_matrices=False)
+            svd(self.A.toarray(), full_matrices=False)
         else:
             with warnings.catch_warnings():
                 warnings.simplefilter("ignore")
                 # parameters `maxiter` and `tol` are tuned for fair comparison
                 _, s, _ = svds(self.A, k=k, solver=solver, random_state=self.rng,
-                            maxiter = 200, tol=1e-6)
+                               maxiter = 200, tol=1e-6)
             accuracy = np.sum(np.abs(1 - s[int(k/2):] / self.top_singular_values))
             # ensure that we are benchmarking a consistent outcome;
             # (e.g. if the code wasn't able to find a solution accurately
             # enough the timing of the benchmark would become useless).
             if accuracy > self.tol:
-                # no convergence sufficently for timing to be relevant;
-                # ensure that the unconverged benchmark fails by sleeping
-                # over the timeout limit.
-                time.sleep(20)
+                # not convergence sufficiently for timing to be relevant
+                raise SkipNotImplemented("Insufficient accuracy achieved")
