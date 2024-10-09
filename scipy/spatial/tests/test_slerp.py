6a0b02b8542b9a542530b2c7b30ea0067935fd33
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2024-10-09 14:00:31 -0500

    FIX: scipy.spatial tests are running under concurrent loads

--- scipy/spatial/tests/test_slerp.py
+++ scipy/spatial/tests/test_slerp.py
@@ -100,6 +100,7 @@ class TestGeometricSlerp:
                             end=end,
                             t=np.linspace(0, 1, 10))
 
+    @pytest.mark.parallel_threads(1)
     @pytest.mark.parametrize("start, end, expected", [
         # North and South Poles are definitely antipodes
         # but should be handled gracefully now
