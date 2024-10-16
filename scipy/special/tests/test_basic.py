1ea8b5ed11dafb361ccaf0c9bca5414dc1700014
Matt Haberland <mhaberla@calpoly.edu>
2024-10-16 11:41:26 -0700

    MAINT: special.hankel2: fix edge case (#21577)
    * MAINT: special.hankel2: fix edge case
    
    * Apply suggestions from code review
    
    Co-authored-by: Albert Steppi <albert.steppi@gmail.com>
    
    * Update scipy/special/tests/test_basic.py
    
    ---------
    
    Co-authored-by: Albert Steppi <albert.steppi@gmail.com>

--- scipy/special/tests/test_basic.py
+++ scipy/special/tests/test_basic.py
@@ -2717,6 +2717,12 @@ class TestHankel:
         hankrl2e = special.hankel2e(1,.1)
         assert_almost_equal(hank2e,hankrl2e,8)
 
+    def test_hankel2_gh4517(self):
+        # Test edge case reported in https://github.com/scipy/scipy/issues/4517
+        res = special.hankel2(0, 0)
+        assert np.isnan(res.real)
+        assert np.isposinf(res.imag)
+
 
 class TestHyper:
     def test_h1vp(self):
