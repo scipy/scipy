5d524a638807c0f0981920ff159927cbbb74b782
Albert Steppi <albert.steppi@gmail.com>
2024-10-23 13:08:03 -0400

    Add complex zeta test

--- scipy/special/tests/test_zeta.py
+++ scipy/special/tests/test_zeta.py
@@ -1,5 +1,7 @@
 import scipy.special as sc
 import numpy as np
+import pytest
+
 from numpy.testing import assert_equal, assert_allclose
 
 
@@ -47,3 +49,89 @@ def test_riemann_zeta_avoid_overflow():
     s = -260.00000000001
     desired = -5.6966307844402683127e+297  # Computed with Mpmath
     assert_allclose(sc.zeta(s), desired, atol=0, rtol=5e-14)
+
+
+@pytest.mark.parametrize(
+    "z, desired",
+    # Reference cases taken from mpmath with the script:
+    # import numpy as np
+    # import scipy.stats as stats
+
+    # from mpmath import mp
+
+    # # seed = np.random.SeedSequence().entropy
+    # seed = 154689806791763421822480125722191067828
+    # rng = np.random.default_rng(seed)
+
+    # # A small point in each quadrant outside of the critical strip
+    # cases = []
+    # for x_sign, y_sign in [1, 1], [1, -1], [-1, 1], [-1, -1]:
+    #     x = x_sign * rng.uniform(2, 8)
+    #     y = y_sign * rng.uniform(2, 8)
+    #     z = x + y*1j
+    #     reference = complex(mp.zeta(z))
+    #     cases.append((z, reference))
+
+    # # Moderately large imaginary part in each quadrant outside of critical strip
+    # for x_sign, y_sign in [1, 1], [1, -1], [-1, 1], [-1, -1]:
+    #     x = x_sign * rng.uniform(2, 8)
+    #     y = y_sign * rng.uniform(50, 80)
+    #     z = x + y*1j
+    #     reference = complex(mp.zeta(z))
+    #     cases.append((z, reference))
+
+    # # points in critical strip
+    # x = rng.uniform(-1.0, 1.0, size=5)
+    # y = np.exp(rng.uniform(0, 5, size=5))
+    # z = x + y*1j
+    # for t in z:
+    #     reference = complex(mp.zeta(t))
+    #     cases.append((complex(t), reference))
+    # z = x - y*1j
+    # for t in z:
+    #     reference = complex(mp.zeta(t))
+    #     cases.append((complex(t), reference))
+    [
+        # small values in each quadrant outside critical strip
+        ((3.12838509346655+7.111085974836645j),
+         (1.0192654793474945+0.08795174413289127j)),
+        ((7.06791362314716-7.219497492626728j),
+         (1.0020740683598117-0.006752725913243711j)),
+        ((-6.806227077655519+2.724411451005281j),
+         (0.06312488213559667-0.061641496333765956j)),
+        ((-3.0170751511621026-6.3686522550665945j),
+         (-0.10330747857150148-1.214541994832571j)),
+        # Moderately large imaginary part, outside critical strip
+        ((6.133994402212294+60.03091448000761j),
+         (0.9885701843417336+0.009636925981078128j)),
+        ((6.17268142822657-64.74883149743795j),
+         (1.0080474225840865+0.012032804974965354j)),
+        ((-3.462191939791879+76.16258975567534j),
+         (18672.072070850158+2908.5104826247184j)),
+        ((-6.955735216531752-74.75791554155748j),
+         (-77672258.72276545+71625206.0401107j)),
+        # Inside critical strip, varying imaginary part.
+        ((-0.1823923420352156+1.4596830498094384j),
+         (0.15641024400724224-0.29104726162518146j)),
+        ((0.9346987902419266+4.918968547259143j),
+         (0.7452215845376634+0.17593462255710995j)),
+        ((0.7384965359955509+66.6142398421354j),
+         (0.5365014352325163-0.35081725113825424j)),
+        ((-0.14456304559992472+21.783747851715468j),
+         (0.1373304916093494+1.6091267223996006j)),
+        ((-0.5904101064314209+33.17656449538932j),
+         (-4.409223214592868-1.6987333585451423j)),
+         ((-0.1823923420352156-1.4596830498094384j),
+          (0.15641024400724224+0.29104726162518146j)),
+        ((0.9346987902419266-4.918968547259143j),
+         (0.7452215845376634-0.17593462255710995j)),
+        ((0.7384965359955509-66.6142398421354j),
+         (0.5365014352325163+0.35081725113825424j)),
+        ((-0.14456304559992472-21.783747851715468j),
+         (0.1373304916093494-1.6091267223996006j)),
+        ((-0.5904101064314209-33.17656449538932j),
+         (-4.409223214592868+1.6987333585451423j)),
+    ]
+)
+def test_riemann_zeta_complex(z, desired):
+    assert_allclose(sc.zeta(z), desired)
