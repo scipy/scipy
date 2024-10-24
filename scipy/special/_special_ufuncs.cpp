f3025a636ca74b4313bc39efbe5e9884056acd07
Albert Steppi <albert.steppi@gmail.com>
2024-10-24 14:30:13 -0400

    Return NaN for complex input to hurwitz zeta instead of error

--- scipy/special/_special_ufuncs.cpp
+++ scipy/special/_special_ufuncs.cpp
@@ -216,7 +216,8 @@ PyMODINIT_FUNC PyInit__special_ufuncs() {
     PyModule_AddObjectRef(_special_ufuncs, "_sinpi", _sinpi);
 
     PyObject *_zeta = xsf::numpy::ufunc(
-        {static_cast<xsf::numpy::ff_f>(xsf::zeta), static_cast<xsf::numpy::dd_d>(xsf::zeta)}, "_zeta", _zeta_doc);
+	{static_cast<xsf::numpy::ff_f>(xsf::zeta), static_cast<xsf::numpy::Ff_F>(xsf::zeta),
+	 static_cast<xsf::numpy::dd_d>(xsf::zeta), static_cast<xsf::numpy::Dd_D>(xsf::zeta)}, "_zeta", _zeta_doc);
     PyModule_AddObjectRef(_special_ufuncs, "_zeta", _zeta);
 
     PyObject *airy =
