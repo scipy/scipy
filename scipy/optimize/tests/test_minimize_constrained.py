412e8318e43fd9b2c88b1c2daac4577b8f3900b1
Andrew Nelson <andyfaff@gmail.com>
2024-10-27 18:19:40 +1100

    MAINT: remove equality constraint that was impossible to satisfy with starting position

--- scipy/optimize/tests/test_minimize_constrained.py
+++ scipy/optimize/tests/test_minimize_constrained.py
@@ -711,6 +711,9 @@ def test_bug_11886():
 
 
 def test_gh11649():
+    # trust - constr error when attempting to keep bound constrained solutions
+    # feasible. Algorithm attempts to go outside bounds when evaluating finite
+    # differences. (don't give objective an analytic gradient)
     bnds = Bounds(lb=[-1, -1], ub=[1, 1], keep_feasible=True)
 
     def assert_inbounds(x):
@@ -721,20 +724,20 @@ def test_gh11649():
         assert_inbounds(x)
         return np.exp(x[0])*(4*x[0]**2 + 2*x[1]**2 + 4*x[0]*x[1] + 2*x[1] + 1)
 
-    def nce(x):
-        assert_inbounds(x)
-        return x[0]**2 + x[1]
-
-    def nce_jac(x):
-        return np.array([2*x[0], 1])
+    # def nce(x):
+    #     assert_inbounds(x)
+    #     return x[0]**2 + x[1]
+    #
+    # def nce_jac(x):
+    #     return np.array([2*x[0], 1])
 
     def nci(x):
         assert_inbounds(x)
         return x[0]*x[1]
 
     x0 = np.array((0.99, -0.99))
-    nlcs = [NonlinearConstraint(nci, -10, np.inf),
-            NonlinearConstraint(nce, 1, 1, jac=nce_jac)]
+    nlcs = [NonlinearConstraint(nci, -10, np.inf),]
+            # NonlinearConstraint(nce, 1, 1, jac=nce_jac)]
 
     res = minimize(fun=obj, x0=x0, method='trust-constr',
                    bounds=bnds, constraints=nlcs)
