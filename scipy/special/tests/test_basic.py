9f8c815a71519484bfc8b377f95f4d0653283696
Damien LaRocque <phicoltan@gmail.com>
2024-10-28 15:44:34 -0400

    TST: special: remove tests for special.btdtr and special.btdtri

--- scipy/special/tests/test_basic.py
+++ scipy/special/tests/test_basic.py
@@ -177,16 +177,6 @@ class TestCephes:
     def test_besselpoly(self):
         assert_equal(cephes.besselpoly(0,0,0),1.0)
 
-    def test_btdtr(self):
-        with pytest.deprecated_call(match='deprecated in SciPy 1.12.0'):
-            y = special.btdtr(1, 1, 1)
-        assert_equal(y, 1.0)
-
-    def test_btdtri(self):
-        with pytest.deprecated_call(match='deprecated in SciPy 1.12.0'):
-            y = special.btdtri(1, 1, 1)
-        assert_equal(y, 1.0)
-
     def test_btdtria(self):
         assert_equal(cephes.btdtria(1,1,1),5.0)
 
