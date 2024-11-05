a1629c5c8117ef0689174cfb968949974be39338
Matteo Bachetti <matteo@matteobachetti.it>
2024-11-05 11:02:13 +0100

    Test the failure for an invalid mode

--- scipy/signal/tests/test_signaltools.py
+++ scipy/signal/tests/test_signaltools.py
@@ -2135,6 +2135,11 @@ def test_correlation_lags(mode, behind, input_size):
     assert_equal(lags.shape, correlation.shape)
 
 
+def test_correlation_lags_invalid_mode():
+    with pytest.raises(ValueError, match="Mode asdfgh is invalid"):
+        correlation_lags(100, 100, mode="asdfgh")
+
+
 @pytest.mark.parametrize('dt', [np.csingle, np.cdouble,
                                 pytest.param(np.clongdouble, marks=_pmf)])
 class TestCorrelateComplex:
