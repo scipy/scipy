08774bfd00b48f5df123e0268939540b7fad2e49
Albert Steppi <albert.steppi@gmail.com>
2024-11-07 14:27:27 -0500

    BUG: Handle negative inf properly in gammasgn

--- scipy/special/xsf/cephes/gamma.h
+++ scipy/special/xsf/cephes/gamma.h
@@ -379,6 +379,10 @@ namespace cephes {
 	if (x == 0) {
 	    return std::copysign(1.0, x);
 	}
+	if (std::isinf(x)) {
+	    // x > 0 case handled, so x must be negative infinity.
+	    return std::numeric_limits<double>::quiet_NaN();
+	}
 	fx = std::floor(x);
 	if (x - fx == 0.0) {
 	    return std::numeric_limits<double>::quiet_NaN();
