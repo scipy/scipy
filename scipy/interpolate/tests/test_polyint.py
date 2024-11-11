1284aa990cb0869d1f988fa2a6123b662a9f2954
Matt Haberland <mhaberla@calpoly.edu>
2024-11-11 13:35:47 -0800

    Update scipy/interpolate/tests/test_polyint.py
    [skip ci]
    
    Co-authored-by: Jake Bowhay <60778417+j-bowhay@users.noreply.github.com>

--- scipy/interpolate/tests/test_polyint.py
+++ scipy/interpolate/tests/test_polyint.py
@@ -345,6 +345,7 @@ class TestBarycentric:
         self.ys = self.true_poly(self.xs)
 
     def test_lagrange(self):
+        # Ensure backwards compatible post SPEC7
         P = BarycentricInterpolator(self.xs, self.ys, random_state=1)
         xp_assert_close(P(self.test_xs), self.true_poly(self.test_xs))
 
