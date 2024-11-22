b3c739b37822f59d15636e60bd27a480e480a625
Dan Schult <dschult@colgate.edu>
2024-11-22 11:58:46 -0500

    Complete tests and cleanup

--- scipy/sparse/tests/test_base.py
+++ scipy/sparse/tests/test_base.py
@@ -1206,9 +1206,6 @@ class _TestCommon:
         sN = self.spcreator(N, shape=(3,3), dtype=float)
         Nexp = scipy.linalg.expm(N)
 
-#        print(f"{Nexp=}")
-#        print(f"{sN.toarray()=} {sN=}")
-#        print(f"{sN.indptr=} {sN.indices=} {sN.data=}")
         with suppress_warnings() as sup:
             sup.filter(
                 SparseEfficiencyWarning,
@@ -1490,7 +1487,7 @@ class _TestCommon:
             check(dtype)
 
     def test_add_broadcasting(self):
-        if self.datsp.format == "dok":
+        if self.datsp.format in ("dok", "dia", "bsr"):
             return
 
         def check(dtype):
@@ -1503,16 +1500,11 @@ class _TestCommon:
             acol = a[:, [0]]
             arow = a[[0], :]
             bcol = self.spcreator(acol)
-#            print(f"{acol=} {bcol.toarray()=}")
             brow = self.spcreator(arow)
 
-#            print(f"{b=}")
-#            print(f"{bcol=}")
-#            print(f"{bcol.indptr=} {bcol.indices=} {bcol.data=}")
-#            print(f"{b.toarray()=} {bcol.toarray()=} {b.data=} {bcol.data=}")
             c = b + bcol
             assert_array_equal(c.toarray(), b.toarray() + bcol.toarray())
-            return 
+
             # test broadcasting dense
             c = b + a[0]
             assert_array_equal(c, b.toarray() + a[0])
@@ -1675,7 +1667,6 @@ class _TestCommon:
                 except ValueError:
                     assert_raises(ValueError, i.multiply, j)
                     continue
-                print(f"{i.toarray()=} {j.toarray()=} {i.multiply(j).toarray()=}")
                 sp_mult = i.multiply(j)
                 assert_almost_equal(sp_mult.toarray(), dense_mult)
 
@@ -2217,11 +2208,12 @@ class _TestCommon:
         assert_array_equal(dsp.__add__(dsp).toarray(), d.__add__(d))
 
         # bad addition
-        print(f"{a.shape=} {d.shape=} {np.broadcast_shapes(a.shape, d.shape)=}")
-        assert_array_equal(asp.__add__(dsp).toarray(), a.__add__(d))
-        assert_array_equal(bsp.__add__(asp).toarray(), b.__add__(a))
-#        assert_raises(ValueError, asp.__add__, dsp)
-#        assert_raises(ValueError, bsp.__add__, asp)
+        if asp.format in ("csc", "csr", "coo", "lil"):
+            assert_array_equal(asp.__add__(dsp).toarray(), a.__add__(d))
+            assert_array_equal(bsp.__add__(asp).toarray(), b.__add__(a))
+        elif asp.format in ("dok", "dia", "bsr"):
+            assert_raises(ValueError, asp.__add__, dsp)
+            assert_raises(ValueError, bsp.__add__, asp)
 
     def test_size_zero_conversions(self):
         mat = array([])
@@ -2969,7 +2961,6 @@ class _TestSlicingAssign:
                           [0,0,0]])
         block = [[1,0],[0,4]]
 
-        assert False
         with suppress_warnings() as sup:
             sup.filter(SparseEfficiencyWarning, "Changing the sparsity structure")
             B[0,0] = 5
@@ -4324,10 +4315,6 @@ class TestCSR(sparse_test_class()):
         assert_equal(d.indptr.dtype, np.int64)
         assert_equal(e.indptr.dtype, np.int32)
         assert_equal(f.indptr.dtype, np.int64)
-
-        for x in [a, b, c, d, e, f]:
-            assert x.format == "csr"
-            print(f"{x.shape=} {x.indptr=} {x.indices=} {x.data=}", flush=True)
 
         # These shouldn't fail
         for x in [a, b, c, d, e, f]:
