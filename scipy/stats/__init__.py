7fa516864ec1c9270d1596b068ba4eca1004f355
Matt Haberland <mhaberla@calpoly.edu>
2024-11-25 10:51:08 -0800

    ENH: `stats.make_distribution`: generate a `ContinuousDistribution` from existing `rv_continuous`

--- scipy/stats/__init__.py
+++ scipy/stats/__init__.py
@@ -470,6 +470,7 @@ Random Variables
 
    Normal
    Mixture
+   make_distribution
 
 Quasi-Monte Carlo
 =================
@@ -639,7 +640,7 @@ from ._fit import fit, goodness_of_fit
 from ._covariance import Covariance
 from ._sensitivity_analysis import *
 from ._survival import *
-from ._distribution_infrastructure import Mixture
+from ._distribution_infrastructure import Mixture, make_distribution
 from ._new_distributions import Normal
 from ._mgc import multiscale_graphcorr
 from ._correlation import chatterjeexi
