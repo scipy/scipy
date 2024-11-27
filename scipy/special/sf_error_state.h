954dc29656e9572f9fdd3ac0b4bc83799d632cd6
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2024-11-27 15:03:40 -0500

    FIX: try again including pthread before Python headers

--- scipy/special/sf_error_state.h
+++ scipy/special/sf_error_state.h
@@ -3,6 +3,10 @@
 #include "scipy_dll.h"
 #include "xsf/error.h"
 
+#ifdef __MINGW32__
+#include <pthread.h>
+#endif
+
 
 #ifdef __cplusplus
 extern "C" {
@@ -13,7 +17,7 @@ extern "C" {
         SF_ERROR_WARN,        /* Warn on errors */
         SF_ERROR_RAISE        /* Raise on errors */
     } sf_action_t;
-    
+
     SCIPY_DLL void scipy_sf_error_set_action(sf_error_t code, sf_action_t action);
 
     SCIPY_DLL sf_action_t scipy_sf_error_get_action(sf_error_t code);
