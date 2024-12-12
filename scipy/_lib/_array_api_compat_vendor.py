976344be26c86cd6c3a8575fb9968b5c26b84b54
crusaderky <crusaderky@gmail.com>
2024-12-12 15:35:35 +0000

    nits

--- scipy/_lib/_array_api_compat_vendor.py
+++ scipy/_lib/_array_api_compat_vendor.py
@@ -2,10 +2,8 @@
 # This is a hook for array_api_extra/src/array_api_extra/_lib/_compat.py
 # to override functions of array_api_compat.
 
-from __future__ import annotations
-
 from .array_api_compat import *  # noqa: F403
 from ._array_api import array_namespace as scipy_array_namespace
 
 # overrides array_api_compat.array_namespace inside array-api-extra
-array_namespace = scipy_array_namespace
+array_namespace = scipy_array_namespace  # type: ignore[assignment]
