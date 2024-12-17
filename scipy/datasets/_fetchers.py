41869050be81e3065ad54d46c140c3e3a38060c7
Lucas Colley <lucas.colley8@gmail.com>
2024-12-17 12:32:18 +0000

    Update scipy/datasets/_fetchers.py
    [lint only]

--- scipy/datasets/_fetchers.py
+++ scipy/datasets/_fetchers.py
@@ -30,7 +30,9 @@ def fetch_data(dataset_name, data_fetcher=data_fetcher):
                           "for scipy.datasets module. Please use pip or "
                           "conda to install 'pooch'.")
     # https://github.com/scipy/scipy/issues/21879
-    downloader = pooch.HTTPDownloader(headers={"User-Agent": f"SciPy {sys.modules['scipy'].__version__}"})
+    downloader = pooch.HTTPDownloader(
+        headers={"User-Agent": f"SciPy {sys.modules['scipy'].__version__}"}
+    )
     # The "fetch" method returns the full path to the downloaded data file.
     return data_fetcher.fetch(dataset_name, downloader=downloader)
 
