484535d44944c9bd6dab25e674f3cb9136a763a9
Tetsuo Koyama <tkoyama010@gmail.com>
2024-12-17 17:53:21 +0900

    Fix header naming with the SciPy version for the `_fetchers.py`

--- scipy/datasets/_fetchers.py
+++ scipy/datasets/_fetchers.py
@@ -28,7 +28,7 @@ def fetch_data(dataset_name, data_fetcher=data_fetcher):
                           "for scipy.datasets module. Please use pip or "
                           "conda to install 'pooch'.")
     # https://github.com/scipy/scipy/issues/21879
-    downloader = pooch.HTTPDownloader(headers={"User-Agent": "agent"})
+    downloader = pooch.HTTPDownloader(headers={"User-Agent": f"SciPy {sys.modules['scipy'].__version__}"})
     # The "fetch" method returns the full path to the downloaded data file.
     return data_fetcher.fetch(dataset_name, downloader=downloader)
 
