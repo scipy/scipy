f469b7df13102e2510a79cd22b59bae017f9d505
Matt Haberland <mhaberla@calpoly.edu>
2024-12-29 07:23:29 -0800

    MAINT: linalg.cho_solve(_banded): remove batch support for 'lower'

--- scipy/linalg/_decomp_cholesky.py
+++ scipy/linalg/_decomp_cholesky.py
@@ -219,11 +219,11 @@ def cho_solve(c_and_lower, b, overwrite_b=False, check_finite=True):
 
     """
     c, lower = c_and_lower
-    return _cho_solve(c, lower, b, overwrite_b=overwrite_b, check_finite=check_finite)
+    return _cho_solve(c, b, lower, overwrite_b=overwrite_b, check_finite=check_finite)
 
 
-@_apply_over_batch(('c', 2), ('lower', 0), ('b', '1|2'))
-def _cho_solve(c, lower, b, overwrite_b, check_finite):
+@_apply_over_batch(('c', 2), ('b', '1|2'))
+def _cho_solve(c, b, lower, overwrite_b, check_finite):
     if check_finite:
         b1 = asarray_chkfinite(b)
         c = asarray_chkfinite(c)
@@ -380,12 +380,12 @@ def cho_solve_banded(cb_and_lower, b, overwrite_b=False, check_finite=True):
 
     """
     (cb, lower) = cb_and_lower
-    return _cho_solve_banded(cb, lower, b, overwrite_b=overwrite_b,
+    return _cho_solve_banded(cb, b, lower, overwrite_b=overwrite_b,
                              check_finite=check_finite)
 
 
-@_apply_over_batch(('cb', 2), ('lower', 0), ('b', '1|2'))
-def _cho_solve_banded(cb, lower, b, overwrite_b, check_finite):
+@_apply_over_batch(('cb', 2), ('b', '1|2'))
+def _cho_solve_banded(cb, b, lower, overwrite_b, check_finite):
     if check_finite:
         cb = asarray_chkfinite(cb)
         b = asarray_chkfinite(b)
