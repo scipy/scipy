BUG: io: add `precision` argument validation for `mmwrite`

scipy/io/tests/test_mmio.py
@@ -746,7 +746,7 @@ class TestMMIOCoordinate:
 
     def test_precision(self):
         test_values = [pi] + [10**(i) for i in range(0, -10, -1)]
-        test_precisions = range(1, 10)
+        test_precisions = range(1, 15)
         for value in test_values:
             for precision in test_precisions:
                 # construct sparse matrix with test value at last main diagonal
@@ -760,6 +760,12 @@ class TestMMIOCoordinate:
                 assert_array_equal(A.row, [n-1])
                 assert_array_equal(A.col, [n-1])
                 assert_allclose(A.data, [float(f'{value:.{precision}g}')])
+
+    @pytest.mark.parametrize("precision", [-1, 0, 16])
+    def test_invalid_precision(self, precision):
+        A = scipy.sparse.dok_array((2, 2))
+        with pytest.raises(ValueError, match='precision must be in'):
+            mmwrite(self.fn, A, precision=precision)
 
     def test_bad_number_of_coordinate_header_fields(self):
         s = """\