b8bc470418842f036114b2f57315788112d2ec82
Lucas Colley <lucas.colley8@gmail.com>
2025-01-06 17:16:33 +0000

    typo [skip cirrus] [skip circle]

--- scipy/signal/tests/test_splines.py
+++ scipy/signal/tests/test_splines.py
@@ -413,7 +413,7 @@ class TestSymIIR:
         # In that respect, sosfilt is likely doing a better job.
         xp_assert_close(res, exp_res, atol=2e-6, check_dtype=False)
 
-        xp_test = array_namespce(s)  # https://github.com/pytorch/pytorch/issues/51284
+        xp_test = array_namespace(s)  # https://github.com/pytorch/pytorch/issues/51284
         I1 = xp.asarray(
             1 + 1j, dtype=xp_test.result_type(s, xp.complex64)
         )
