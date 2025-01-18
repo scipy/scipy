4f89b5f1a09078603cf6b072ad9ac71fc90f4372
dschmitz89 <danielschmitzsiegen@googlemail.com>
2025-01-18 13:56:13 +0100

    TST: test nctdtrit

--- scipy/special/tests/test_cdflib.py
+++ scipy/special/tests/test_cdflib.py
@@ -552,7 +552,7 @@ def test_ncfdtr(dfn, dfd, nc, f, expected):
     assert_allclose(sp.ncfdtr(dfn, dfd, nc, f), expected, rtol=1e-13, atol=0)
 
 
-class TestNctdtr:
+class TestNoncentralTFunctions:
 
     # Reference values computed with mpmath with the following script
     # Formula from:
@@ -592,7 +592,7 @@ class TestNctdtr:
     #         result = mp.one - f(df, -nc, x)
     #     return float(result)
 
-    @pytest.mark.parametrize("df, nc, x, expected", [
+    @pytest.mark.parametrize("df, nc, x, expected_cdf", [
         (0.98, -3.8, 0.0015, 0.9999279987514815),
         (0.98, -3.8, 0.15, 0.9999528361700505),
         (0.98, -3.8, 1.5, 0.9999908823016942),
@@ -660,13 +660,15 @@ class TestNctdtr:
                         reason="Bug in underlying Boost math implementation")),
         (980, 38, 15, 5.407535300713606e-105)
     ])
-    def test_gh19896(self, df, nc, x, expected):
+    def test_gh19896(self, df, nc, x, expected_cdf):
         # test that gh-19896 is resolved.
         # Originally this was a regression test that used the old Fortran results
         # as a reference. The Fortran results were not accurate, so the reference
         # values were recomputed with mpmath.
-        result = sp.nctdtr(df, nc, x)
-        assert_allclose(result, expected, rtol=1e-13, atol=1e-303)
+        nctdtr_result = sp.nctdtr(df, nc, x)
+        assert_allclose(nctdtr_result, expected_cdf, rtol=1e-13, atol=1e-303)
+        nctdtrit_result = sp.nctdtrit(df, nc, expected_cdf)
+        assert_allclose(x, nctdtrit_result, rtol=1e-12)
 
     def test_nctdtr_gh8344(self):
         # test that gh-8344 is resolved.
