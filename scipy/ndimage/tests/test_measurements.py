ca0c3d861d0efc2de76a189de7f5094aa28b188d
Lucas Colley <lucas.colley8@gmail.com>
2025-01-28 21:54:43 +0000

    generate test data with numpy [skip cirrus] [skip circle]

--- scipy/ndimage/tests/test_measurements.py
+++ scipy/ndimage/tests/test_measurements.py
@@ -550,12 +550,11 @@ def test_value_indices02(xp):
 def test_value_indices03(xp):
     "Test different input array shapes, from 1-D to 4-D"
     for shape in [(36,), (18, 2), (3, 3, 4), (3, 3, 2, 2)]:
-        a = xp.asarray((12*[1]+12*[2]+12*[3]), dtype=xp.int32)
-        a = xp.reshape(a, shape)
+        a = np.asarray((12*[1]+12*[2]+12*[3]), dtype=np.int32)
+        a = np.reshape(a, shape)
 
-        # convert to numpy to prevent issues with data-dependent shapes
-        # from unique for dask
-        trueKeys = np.asarray(xp.unique_values(a))
+        trueKeys = np.unique(a)
+        a = xp.asarray(a)
         vi = ndimage.value_indices(a)
         assert list(vi.keys()) == list(trueKeys)
         for k in [int(x) for x in trueKeys]:
