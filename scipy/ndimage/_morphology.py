BUG: ndimage: `binary_erosion` vs. broadcasted input

scipy/ndimage/_morphology.py
@@ -220,7 +220,8 @@ def _binary_erosion(input, structure, iterations, mask, output,
     except TypeError as e:
         raise TypeError('iterations parameter should be an integer') from e
 
-    input = np.asarray(input)
+    # The Cython code can't cope with broadcasted inputs
+    input = np.asarray(input, order="C")
     ndim = input.ndim
     if np.iscomplexobj(input):
         raise TypeError('Complex type not supported')