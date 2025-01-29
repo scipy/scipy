903c8a789890ff89f6849a1c0534349be83748f7
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2025-01-29 17:54:36 +0100

    TST: signal: skip tests on dask

--- scipy/signal/tests/test_signaltools.py
+++ scipy/signal/tests/test_signaltools.py
@@ -1381,6 +1381,7 @@ padtype_options = ["mean", "median", "minimum", "maximum", "line"]
 padtype_options += _upfirdn_modes
 
 
+@skip_xp_backends("dask.array", reason="XXX something in dask")
 class TestResample:
 
     @skip_xp_backends("jax.numpy", reason="immutable arrays")
