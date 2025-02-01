abaaf77a1280fa8828d895987a173fb3d5e7ca17
Dietrich Brunn <Dietrich.Brunn@web.de>
2025-02-01 21:01:44 +0100

    BUG: signal.ShortTimeFFT: make attributes win and dual_win immutable
    Also, some minor docstr issues were fixed.

--- scipy/signal/tests/test_short_time_fft.py
+++ scipy/signal/tests/test_short_time_fft.py
@@ -278,7 +278,6 @@ def test_exceptions_properties_methods():
         SFT.fft_mode = 'twosided'
         SFT.extent(n=100)
 
-
 @pytest.mark.parametrize('m', ('onesided', 'onesided2X'))
 def test_exceptions_fft_mode_complex_win(m: FFT_MODE_TYPE):
     """Verify that one-sided spectra are not allowed with complex-valued
