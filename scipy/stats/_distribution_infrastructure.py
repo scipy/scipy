7d2c0c230ce64eaeb3610e0c5afd6505aafdf455
steppi <1953382+steppi@users.noreply.github.com>
2025-03-12 20:11:01 -0400

    DOC: Use simpler example of custom distribution with variable support [docs only]

--- scipy/stats/_distribution_infrastructure.py
+++ scipy/stats/_distribution_infrastructure.py
@@ -3618,57 +3618,38 @@ def make_distribution(dist):
 
     Create a custom distribution with variable support.
 
-    >>> class MyGenExtreme:
+    >>> class MyUniformCube:
     ...     @property
     ...     def __make_distribution_version__(self):
     ...         return "1.16.0"
     ...
     ...     @property
     ...     def parameters(self):
-    ...         return {"c": {}, "mu": {}, "sigma": (0, np.inf)}
+    ...         return {"a": (-np.inf, np.inf), "b": (-np.inf, np.inf)}
     ...
     ...     @property
     ...     def support(self):
-    ...         def left(*, c, mu, sigma):
-    ...             c, mu, sigma = np.broadcast_arrays(c, mu, sigma)
-    ...             result = np.empty_like(c)
-    ...             result[c >= 0] = -np.inf
-    ...             result[c < 0] = mu[c < 0] + sigma[c < 0] / c[c < 0]
-    ...             return result[()]
+    ...         def left(*, a, b):
+    ...             a = np.asarray(a)
+    ...             return (a**3)[()]
     ...
-    ...         def right(*, c, mu, sigma):
-    ...             c, mu, sigma = np.broadcast_arrays(c, mu, sigma)
-    ...             result = np.empty_like(c)
-    ...             result[c <= 0] = np.inf
-    ...             result[c > 0] = mu[c > 0] + sigma[c > 0] / c[c > 0]
-    ...             return result[()]
+    ...         def right(*, a, b):
+    ...             b = np.asarray(b)
+    ...             return (b**3)[()]
+    ...         return (left, right)
     ...
-    ...     def pdf(self, x, *, c, mu, sigma):
-    ...         x, c, mu, sigma = np.broadcast_arrays(x, c, mu, sigma)
-    ...         t = np.empty_like(x)
-    ...         mask = (c == 0)
-    ...         t[mask] = np.exp(-(x[mask] - mu[mask])/sigma[mask])
-    ...         t[~mask] = (
-    ...             1  - c[~mask]*(x[~mask] - mu[~mask])/sigma[~mask]
-    ...         )**(1/c[~mask])
-    ...         return np.exp(-t)[()]
+    ...     def pdf(self, x, *, a, b):
+    ...         return 1 / (3*(b - a)*np.cbrt(x)**2)
     ...
-    ...     def cdf(self, x, *, c, mu, sigma):
-    ...         x, c, mu, sigma = np.broadcast_arrays(x, c, mu, sigma)
-    ...         t = np.empty_like(x)
-    ...         mask = (c == 0)
-    ...         t[mask] = np.exp(-(x[mask] - mu[mask])/sigma[mask])
-    ...         t[~mask] = (
-    ...             1  - c[~mask]*(x[~mask] - mu[~mask])/sigma[~mask]
-    ...         )**(1/c[~mask])
-    ...         return np.exp(-t)[()]
+    ...     def cdf(self, x, *, a, b):
+    ...         return (np.cbrt(x) - a) / (b - a)
     >>>
-    >>> MyGenExtreme = stats.make_distribution(MyGenExtreme())
-    >>> Y = MyGenExtreme(c=1, mu=0, sigma=1)
-
-    >>> GenExtreme = stats.make_distribution(stats.genextreme)
-    >>> X = GenExtreme(c=1)
-    >>> np.isclose(Y.cdf(0.1), X.cdf(0.1))
+    >>> MyUniformCube = stats.make_distribution(MyUniformCube())
+    >>> X = MyUniformCube(a=-2, b=2)
+    >>> Y = stats.Uniform(a=-2, b=-2)**3
+    >>> X.support()
+    (-8.0, 8.0)
+    >>> np.isclose(X.cdf(2.1), Y.cdf(2.1))
     np.True_
 
     """
