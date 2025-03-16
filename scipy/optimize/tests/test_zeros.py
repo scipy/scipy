c1dd0846ffdeb0e9620d4844dfa548771f7ee940
Thorsten Alteholz <scipy@alteholz.dev>
2025-03-16 23:44:26 +0100

    TST: optimize: add more tests
    Add more tests for zeros.* to increase coverage.
    The new tests provide strange parameters to some methods and check whether
    the expected exceptions appear.

--- scipy/optimize/tests/test_zeros.py
+++ scipy/optimize/tests/test_zeros.py
@@ -508,6 +508,21 @@ class TestNewton(TestScalarRootFinders):
         assert_allclose(abs(res.root), 2**-0.5)
         assert res.root.dtype == np.dtype(np.float64)
 
+    def test_newton_special_parameters(self):
+        # give zeros.newton() some strange parameters
+        # and check whether an exception appears
+        with pytest.raises(ValueError) as excinfo:
+            _ = zeros.newton(f1, 3, tol=-1e-6)
+        assert "tol too small" in excinfo.exconly()
+
+        with pytest.raises(ValueError) as excinfo:
+            _ = zeros.newton(f1, 3, tol=1e-6, maxiter=-50)
+        assert "maxiter must be greater than 0" in excinfo.exconly()
+
+        with pytest.raises(ValueError) as excinfo:
+            _ = zeros.newton(f1, 3, x1=3)
+        assert "x1 and x0 must be different" in excinfo.exconly()
+
 
 def test_gh_5555():
     root = 0.1
@@ -963,3 +978,23 @@ def test_maxiter_int_check_gh10236(method):
     message = "'float' object cannot be interpreted as an integer"
     with pytest.raises(TypeError, match=message):
         method(f1, 0.0, 1.0, maxiter=72.45)
+
+def test_bisect_special_parameter():
+    # give some zeros method strange parameters
+    # and check whether an exception appears
+    root = 0.1
+    args = (1e-09, 0.004, 10, 0.27456)
+    rtolbad = 4 * np.finfo(float).eps / 2
+
+    def f(x):
+        return x - root
+
+    methods = [zeros.bisect, zeros.ridder, zeros.brentq, zeros.brenth]
+
+    for method in methods:
+        with pytest.raises(ValueError) as excinfo:
+           _ = method(f, -1e8, 1e7, args=args, xtol=-1e-6, rtol=TOL)
+        assert "xtol too small" in excinfo.exconly()
+        with pytest.raises(ValueError) as excinfo:
+           _ = method(f, -1e8, 1e7, args=args, xtol=1e-6, rtol=rtolbad)
+        assert "rtol too small" in excinfo.exconly()
