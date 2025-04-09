22b64cff104241464b76ed8c9957e3fe557bb25c
Lucas Colley <lucas.colley8@gmail.com>
2025-04-09 13:09:06 +0100

    MAINT: simplify pyprima vendoring script

--- tools/vendor_pyprima.sh
+++ tools/vendor_pyprima.sh
@@ -13,17 +13,13 @@ ROOT_DIR="scipy/_lib/pyprima"
 
 rm -rf $ROOT_DIR
 mkdir $ROOT_DIR
-git clone $REPO_URL $ROOT_DIR
-cd $ROOT_DIR
+mkdir $ROOT_DIR/.tmp
+git clone $REPO_URL $ROOT_DIR/.tmp
+pushd $ROOT_DIR/.tmp
 git checkout $COMMIT_HASH
-rm -rf .development
-rm -rf .git
-rm .gitmodules
-rm -rf benchmark/
-rm -rf fortran/
-rm -rf matlab/
-rm -rf c/
-rm -rf python/
-rm -rf .github/
 rm -rf pyprima/profiles_vs_matlab
 rm -rf pyprima/profiles_vs_python_bindings
+popd
+mv -v $ROOT_DIR/.tmp/LICENCE.txt $ROOT_DIR/
+mv -v $ROOT_DIR/.tmp/pyprima/ $ROOT_DIR/pyprima/
+rm -rf $ROOT_DIR/.tmp
