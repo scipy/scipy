d4e9c8196b62a488a774cdfb7c8f2ef003a58998
Ilhan Polat <ilhanpolat@gmail.com>
2025-04-10 02:33:07 +0200

    SQ: header

--- scipy/sparse/linalg/_eigen/arpack/ARPACK/_arpack.h
+++ scipy/sparse/linalg/_eigen/arpack/ARPACK/_arpack.h
@@ -63,7 +63,7 @@ enum ARPACK_ido {
     ido_OPX        = 1,  // OP*x needed
     ido_BX         = 2,  // B*x needed
     ido_USER_SHIFT = 3,  // User shifts are needed
-    ido_OPX_V0     = 4,  // A random vector is needed, This option is not in the original ARPACK
+    ido_OPX_V0     = 4,  // A random vector is needed
     ido_DONE       = 99  // Done
 };
 
