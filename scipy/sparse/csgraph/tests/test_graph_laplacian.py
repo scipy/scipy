571f8a86f4d0778e54d9dac1d4223593cf3e8962
kokwatan <kokwatan@lycorp.co.jp>
2025-05-02 05:24:21 -0600

    FIX: restore original testcase

--- scipy/sparse/csgraph/tests/test_graph_laplacian.py
+++ scipy/sparse/csgraph/tests/test_graph_laplacian.py
@@ -188,6 +188,78 @@ DTYPES = INT_DTYPES + REAL_DTYPES + COMPLEX_DTYPES
 def test_asymmetric_laplacian(use_out_degree, normed,
                               copy, dtype, arr_type, variant):
     # adjacency matrix
+    A = [[0, 1, 0],
+         [4, 2, 0],
+         [0, 0, 0]]
+    A = arr_type(np.array(A), dtype=dtype)
+    A_copy = A.copy()
+
+    if not normed and use_out_degree:
+        # Laplacian matrix using out-degree
+        L = [[1, -1, 0],
+             [-4, 4, 0],
+             [0, 0, 0]]
+        d = [1, 4, 0]
+
+    if normed and use_out_degree:
+        # normalized Laplacian matrix using out-degree
+        L = [[1, -0.5, 0],
+             [-2, 1, 0],
+             [0, 0, 0]]
+        d = [1, 2, 1]
+
+    if not normed and not use_out_degree:
+        # Laplacian matrix using in-degree
+        L = [[4, -1, 0],
+             [-4, 1, 0],
+             [0, 0, 0]]
+        d = [4, 1, 0]
+
+    if normed and not use_out_degree:
+        # normalized Laplacian matrix using in-degree
+        L = [[1, -0.5, 0],
+             [-2, 1, 0],
+             [0, 0, 0]]
+        d = [2, 1, 1]
+
+    _check_laplacian_dtype_none(
+        A,
+        L,
+        d,
+        normed=normed,
+        use_out_degree=use_out_degree,
+        copy=copy,
+        dtype=dtype,
+        arr_type=arr_type,
+        variant=variant,
+    )
+
+    _check_laplacian_dtype(
+        A_copy,
+        L,
+        d,
+        normed=normed,
+        use_out_degree=use_out_degree,
+        copy=copy,
+        dtype=dtype,
+        arr_type=arr_type,
+        variant=variant,
+    )
+
+
+@pytest.mark.parametrize("dtype", DTYPES)
+@pytest.mark.parametrize("arr_type", [np.array,
+                                      sparse.csr_matrix,
+                                      sparse.coo_matrix,
+                                      sparse.csr_array,
+                                      sparse.coo_array])
+@pytest.mark.parametrize("copy", [True, False])
+@pytest.mark.parametrize("normed", [True, False])
+@pytest.mark.parametrize("use_out_degree", [True, False])
+@pytest.mark.parametrize("variant", ["default", "use_abs"])
+def test_asymmetric_laplacian_with_negative_edges(use_out_degree, normed,
+                              copy, dtype, arr_type, variant):
+    # adjacency matrix
     A = [[0, 2, -2],
          [2, 0, 2],
          [-2, 2, 0]]
