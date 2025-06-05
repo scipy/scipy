5320126d6f5090a682016b3bd422e84060299dd9
Ilhan Polat <ilhanpolat@gmail.com>
2025-06-05 11:07:44 +0200

    ENH:sparse.linalg: Complete the rewrite of ARPACK sym solvers
    [skip ci]

--- scipy/sparse/linalg/_eigen/arpack/ARPACK/_arpack_s_double.h
+++ scipy/sparse/linalg/_eigen/arpack/ARPACK/_arpack_s_double.h
@@ -1,3 +1,6 @@
+#ifndef _ARPACK_S_DOUBLE_H
+#define _ARPACK_S_DOUBLE_H
+
 #include "_arpack.h"
 
 // BLAS Routines used
@@ -7,10 +10,10 @@ double ddot_(int* n, double* x, int* incx, double* y, int* incy);
 void dgemv_(char* trans, int* m, int* n, double* alpha, double* a, int* lda, double* x, int* incx, double* beta, double* y, int* incy);
 void dger_(int* m, int* n, double* alpha, double* x, int* incx, double* y, int* incy, double* a, int* lda);
 double dnrm2_(int* n, double* x, int* incx);
+void drot_(int* n, double* sx, int* incx, double* sy, int* incy, double* c, double* s);
 void dscal_(int* n, double* alpha, double* x, int* incx);
 void dswap_(int* n, double* x, int* incx, double* y, int* incy);
 void dtrmm_(char* side, char* uplo, char* transa, char* diag, int* m, int* n, double* alpha, double* a, int* lda, double* b, int* ldb);
-
 
 // LAPACK Routines used
 void dgeqr2_(int* m, int* n, double* a, int* lda, double* tau, double* work, int* info);
@@ -18,7 +21,11 @@ void dlacpy_(char* uplo, int* m, int* n, double* a, int* lda, double* b, int* ld
 void dlaev2_(double* a, double* b, double* c, double* rt1, double* rt2, double* cs1, double* sn1);
 double dlanst_(char* norm, int* n, double* d, double* e);
 void dlartg_(double* f, double* g, double* c, double* s, double* r);
+void dlartgp_(double* f, double* g, double* c, double* s, double* r);
 void dlascl_(char* mtype, int* kl, int* ku, double* cfrom, double* cto, int* m, int* n, double* a, int* lda, int* info);
+void dlaset_(char* uplo, int* m, int* n, double* alpha, double* beta, double* a, int* lda);
 void dlasr_(char* side, char* pivot, char* direct, int* m, int* n, double* c, double* s, double* a, int* lda);
 void dorm2r_(char* side, char* trans, int* m, int* n, int* k, double* a, int* lda, double* tau, double* c, int* ldc, double* work, int* info);
 void dsteqr_(char* compz, int* n, double* d, double* e, double* z, int* ldz, double* work, int* info);
+
+#endif
