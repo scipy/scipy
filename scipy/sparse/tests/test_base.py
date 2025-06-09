2474797139340b7c31b97fe8e83fa6c96c11b184
Dan Schult <dschult@colgate.edu>
2025-06-09 14:08:05 -0400

    TST: sparse: Separate Test64Bit from test_base.py, refactor to allow Test_Classes based marks, ignore some methods (#21743)
    * split rerun-64bit-tests from test_base
    
    * ignore rerun tests that dont call get_index_dtype
    
    set proposed xslow and slow marks
    
    * merge checking ignore with idx_random tests
    
    * fix CI issues
    
    * move new Types 'unscriptable' tests out of test classes. Maybe should be in different module?
    
    * remove feature to ignore tests that don't use get_index_dtype
    
    * fix CI errors

--- scipy/sparse/tests/test_base.py
+++ scipy/sparse/tests/test_base.py
@@ -110,10 +110,9 @@ def with_64bit_maxval_limit(maxval_limit=None, random=False, fixed_dtype=None,
         def new_get_index_dtype(arrays=(), maxval=None, check_contents=False):
             return fixed_dtype
     elif random:
-        counter = np.random.RandomState(seed=1234)
-
+        rng = np.random.default_rng(1234)
         def new_get_index_dtype(arrays=(), maxval=None, check_contents=False):
-            return (np.int32, np.int64)[counter.randint(2)]
+            return (np.int32, np.int64)[rng.integers(2)]
     else:
         def new_get_index_dtype(arrays=(), maxval=None, check_contents=False):
             dtype = np.int32
@@ -4433,11 +4432,11 @@ class TestCSRMatrix(_MatrixMixin, TestCSR):
             sup.filter(SparseEfficiencyWarning, "Changing the sparsity structure")
             return csr_matrix(*args, **kwargs)
     
-    def test_spmatrix_subscriptable(self):
-        result = csr_matrix[np.int8]
-        assert isinstance(result, GenericAlias)
-        assert result.__origin__ is csr_matrix
-        assert result.__args__ == (np.int8,)
+def test_spmatrix_subscriptable():
+    result = csr_matrix[np.int8]
+    assert isinstance(result, GenericAlias)
+    assert result.__origin__ is csr_matrix
+    assert result.__args__ == (np.int8,)
 
 
 TestCSRMatrix.init_class()
@@ -4974,24 +4973,24 @@ class TestCOO(sparse_test_class(getset=False,
         assert_((mat1.reshape((1001, 3000001), order='C') != mat2).nnz == 0)
         assert_((mat2.reshape((3000001, 1001), order='F') != mat1).nnz == 0)
     
-    def test_sparray_subscriptable(self):
-        result = coo_array[np.int8, tuple[int]]
-        assert isinstance(result, GenericAlias)
-        assert result.__origin__ is coo_array
-        assert result.__args__ == (np.int8, tuple[int])
-
-        result = coo_array[np.int8]
-        assert isinstance(result, GenericAlias)
-        assert result.__origin__ is coo_array
-        assert result.__args__ == (np.int8,)
-
-
 class TestCOOMatrix(_MatrixMixin, TestCOO):
     spcreator = coo_matrix
 
 
 TestCOO.init_class()
 TestCOOMatrix.init_class()
+
+
+def test_sparray_subscriptable():
+    result = coo_array[np.int8, tuple[int]]
+    assert isinstance(result, GenericAlias)
+    assert result.__origin__ is coo_array
+    assert result.__args__ == (np.int8, tuple[int])
+
+    result = coo_array[np.int8]
+    assert isinstance(result, GenericAlias)
+    assert result.__origin__ is coo_array
+    assert result.__args__ == (np.int8,)
 
 
 class TestDIA(sparse_test_class(getset=False, slicing=False, slicing_assign=False,
@@ -5644,239 +5643,6 @@ class TestCOONonCanonical(_NonCanonicalMixin, TestCOO):
 class TestCOONonCanonicalMatrix(TestCOONonCanonical, TestCOOMatrix):
     pass
 
-
-#Todo: Revisit 64bit tests: avoid rerun of all tests for each version of get_index_dtype
-def cases_64bit(sp_api):
-    """Yield all tests for all formats that use get_index_dtype
-
-    This is more than testing get_index_dtype. It allows checking whether upcasting
-    or downcasting the index dtypes affects test results. The approach used here
-    does not try to figure out which tests might fail due to 32/64-bit issues.
-    We just run them all.
-    So, each test method in that uses cases_64bit reruns most of the test suite!
-    """
-    if sp_api == "sparray":
-        TEST_CLASSES = [TestBSR, TestCOO, TestCSC, TestCSR, TestDIA,
-                         # lil/dok->other conversion operations use get_index_dtype
-                         # so we include lil & dok test suite even though they do not
-                         # use get_index_dtype within the class. That means many of
-                         # these tests are superfluous, but it's hard to pick which
-                         TestDOK, TestLIL
-                        ]
-    elif sp_api == "spmatrix":
-        TEST_CLASSES = [TestBSRMatrix, TestCOOMatrix, TestCSCMatrix,
-                         TestCSRMatrix, TestDIAMatrix,
-                         # lil/dok->other conversion operations use get_index_dtype
-                         TestDOKMatrix, TestLILMatrix
-                        ]
-    else:
-        raise ValueError(f"parameter {sp_api=} is not one of 'sparray' or 'spmatrix'")
-
-    # The following features are missing, so skip the tests:
-    SKIP_TESTS = {
-        'test_expm': 'expm for 64-bit indices not available',
-        'test_inv': 'linsolve for 64-bit indices not available',
-        'test_solve': 'linsolve for 64-bit indices not available',
-        'test_scalar_idx_dtype': 'test implemented in base class',
-        'test_large_dimensions_reshape': 'test actually requires 64-bit to work',
-        'test_constructor_smallcol': 'test verifies int32 indexes',
-        'test_constructor_largecol': 'test verifies int64 indexes',
-        'test_tocoo_tocsr_tocsc_gh19245': 'test verifies int32 indexes',
-    }
-
-    for cls in TEST_CLASSES:
-        for method_name in sorted(dir(cls)):
-            method = getattr(cls, method_name)
-            if (method_name.startswith('test_') and
-                    not getattr(method, 'slow', False)):
-                marks = []
-
-                msg = SKIP_TESTS.get(method_name)
-                if bool(msg):
-                    marks += [pytest.mark.skip(reason=msg)]
-
-                markers = getattr(method, 'pytestmark', [])
-                for mark in markers:
-                    if mark.name in ('skipif', 'skip', 'xfail', 'xslow'):
-                        marks.append(mark)
-
-                yield pytest.param(cls, method_name, marks=marks)
-
-
-class Test64Bit:
-    # classes that use get_index_dtype
-    MAT_CLASSES = [
-        bsr_matrix, coo_matrix, csc_matrix, csr_matrix, dia_matrix,
-        bsr_array, coo_array, csc_array, csr_array, dia_array,
-    ]
-
-    def _compare_index_dtype(self, m, dtype):
-        dtype = np.dtype(dtype)
-        if m.format in ['csc', 'csr', 'bsr']:
-            return (m.indices.dtype == dtype) and (m.indptr.dtype == dtype)
-        elif m.format == 'coo':
-            return (m.row.dtype == dtype) and (m.col.dtype == dtype)
-        elif m.format == 'dia':
-            return (m.offsets.dtype == dtype)
-        else:
-            raise ValueError(f"matrix {m!r} has no integer indices")
-
-    @pytest.mark.thread_unsafe
-    def test_decorator_maxval_limit(self):
-        # Test that the with_64bit_maxval_limit decorator works
-
-        @with_64bit_maxval_limit(maxval_limit=10)
-        def check(mat_cls):
-            m = mat_cls(np.random.rand(10, 1))
-            assert_(self._compare_index_dtype(m, np.int32))
-            m = mat_cls(np.random.rand(11, 1))
-            assert_(self._compare_index_dtype(m, np.int64))
-
-        for mat_cls in self.MAT_CLASSES:
-            check(mat_cls)
-
-    @pytest.mark.thread_unsafe
-    def test_decorator_maxval_random(self):
-        # Test that the with_64bit_maxval_limit decorator works (2)
-
-        @with_64bit_maxval_limit(random=True)
-        def check(mat_cls):
-            seen_32 = False
-            seen_64 = False
-            for k in range(100):
-                m = mat_cls(np.random.rand(9, 9))
-                seen_32 = seen_32 or self._compare_index_dtype(m, np.int32)
-                seen_64 = seen_64 or self._compare_index_dtype(m, np.int64)
-                if seen_32 and seen_64:
-                    break
-            else:
-                raise AssertionError("both 32 and 64 bit indices not seen")
-
-        for mat_cls in self.MAT_CLASSES:
-            check(mat_cls)
-
-    @pytest.mark.thread_unsafe
-    def test_downcast_intp(self):
-        # Check that bincount and ufunc.reduceat intp downcasts are
-        # dealt with. The point here is to trigger points in the code
-        # that can fail on 32-bit systems when using 64-bit indices,
-        # due to use of functions that only work with intp-size
-        # indices.
-
-        @with_64bit_maxval_limit(fixed_dtype=np.int64, downcast_maxval=1)
-        def check_limited(csc_container, csr_container, coo_container):
-            # These involve indices larger than `downcast_maxval`
-            a = csc_container([[1, 2], [3, 4], [5, 6]])
-            assert_raises(AssertionError, a.count_nonzero, axis=1)
-            assert_raises(AssertionError, a.sum, axis=0)
-
-            a = csr_container([[1, 2, 3], [3, 4, 6]])
-            assert_raises(AssertionError, a.count_nonzero, axis=0)
-            assert_raises(AssertionError, a.sum, axis=1)
-
-            a = coo_container([[1, 2, 3], [3, 4, 5]])
-            assert_raises(AssertionError, a.count_nonzero, axis=0)
-            a.has_canonical_format = False
-            assert_raises(AssertionError, a.sum_duplicates)
-
-        @with_64bit_maxval_limit(fixed_dtype=np.int64)
-        def check_unlimited(csc_container, csr_container, coo_container):
-            # These involve indices smaller than `downcast_maxval`
-            a = csc_container([[1, 2], [3, 4], [5, 6]])
-            a.count_nonzero(axis=1)
-            a.sum(axis=0)
-
-            a = csr_container([[1, 2, 3], [3, 4, 6]])
-            a.count_nonzero(axis=0)
-            a.sum(axis=1)
-
-            a = coo_container([[1, 2, 3], [3, 4, 5]])
-            a.count_nonzero(axis=0)
-            a.has_canonical_format = False
-            a.sum_duplicates()
-
-        check_limited(csc_array, csr_array, coo_array)
-        check_unlimited(csc_array, csr_array, coo_array)
-        check_limited(csc_matrix, csr_matrix, coo_matrix)
-        check_unlimited(csc_matrix, csr_matrix, coo_matrix)
-
-
-# Testing both spmatrices and sparrays for 64bit index dtype handling is
-# expensive and double-checks the same code (e.g. _coobase)
-class RunAll64Bit:
-    def _check_resiliency(self, cls, method_name, **kw):
-        # Resiliency test, to check that sparse matrices deal reasonably
-        # with varying index data types.
-
-        @with_64bit_maxval_limit(**kw)
-        def check(cls, method_name):
-            instance = cls()
-            if hasattr(instance, 'setup_method'):
-                instance.setup_method()
-            try:
-                getattr(instance, method_name)()
-            finally:
-                if hasattr(instance, 'teardown_method'):
-                    instance.teardown_method()
-
-        check(cls, method_name)
-
-
-@pytest.mark.thread_unsafe
-@pytest.mark.slow
-class Test64BitArray(RunAll64Bit):
-    # inheritance of pytest test classes does not separate marks for subclasses.
-    # So we define these functions in both Array and Matrix versions.
-    @pytest.mark.parametrize('cls,method_name', cases_64bit("sparray"))
-    def test_resiliency_limit_10(self, cls, method_name):
-        self._check_resiliency(cls, method_name, maxval_limit=10)
-
-    @pytest.mark.fail_slow(2)
-    @pytest.mark.parametrize('cls,method_name', cases_64bit("sparray"))
-    def test_resiliency_random(self, cls, method_name):
-        # bsr_array.eliminate_zeros relies on csr_array constructor
-        # not making copies of index arrays --- this is not
-        # necessarily true when we pick the index data type randomly
-        self._check_resiliency(cls, method_name, random=True)
-
-    @pytest.mark.parametrize('cls,method_name', cases_64bit("sparray"))
-    def test_resiliency_all_32(self, cls, method_name):
-        self._check_resiliency(cls, method_name, fixed_dtype=np.int32)
-
-    @pytest.mark.parametrize('cls,method_name', cases_64bit("sparray"))
-    def test_resiliency_all_64(self, cls, method_name):
-        self._check_resiliency(cls, method_name, fixed_dtype=np.int64)
-
-
-@pytest.mark.thread_unsafe
-class Test64BitMatrix(RunAll64Bit):
-    # assert_32bit=True only for spmatrix cuz sparray does not check index content
-    @pytest.mark.fail_slow(5)
-    @pytest.mark.parametrize('cls,method_name', cases_64bit("spmatrix"))
-    def test_no_64(self, cls, method_name):
-        self._check_resiliency(cls, method_name, assert_32bit=True)
-
-    # inheritance of pytest test classes does not separate marks for subclasses.
-    # So we define these functions in both Array and Matrix versions.
-    @pytest.mark.parametrize('cls,method_name', cases_64bit("spmatrix"))
-    def test_resiliency_limit_10(self, cls, method_name):
-        self._check_resiliency(cls, method_name, maxval_limit=10)
-
-    @pytest.mark.fail_slow(2)
-    @pytest.mark.parametrize('cls,method_name', cases_64bit("spmatrix"))
-    def test_resiliency_random(self, cls, method_name):
-        # bsr_array.eliminate_zeros relies on csr_array constructor
-        # not making copies of index arrays --- this is not
-        # necessarily true when we pick the index data type randomly
-        self._check_resiliency(cls, method_name, random=True)
-
-    @pytest.mark.parametrize('cls,method_name', cases_64bit("spmatrix"))
-    def test_resiliency_all_32(self, cls, method_name):
-        self._check_resiliency(cls, method_name, fixed_dtype=np.int32)
-
-    @pytest.mark.parametrize('cls,method_name', cases_64bit("spmatrix"))
-    def test_resiliency_all_64(self, cls, method_name):
-        self._check_resiliency(cls, method_name, fixed_dtype=np.int64)
 
 def test_broadcast_to():
     a = np.array([[1, 0, 2]])
