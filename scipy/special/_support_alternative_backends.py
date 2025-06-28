4c1e1ff92d4c2d39e6cd9ee197e2f1e6d6a75935
steppi <1953382+steppi@users.noreply.github.com>
2025-06-28 16:46:53 -0400

    ENH: Add cupy support for bdtrc

--- scipy/special/_support_alternative_backends.py
+++ scipy/special/_support_alternative_backends.py
@@ -292,6 +292,12 @@ _special_funcs = (
                   skip_backends=[
                       ('jax.numpy', "unavailable in jax and fallback fails")]),
               paramtypes=("real", "int", "real")),
+    _FuncInfo(_ufuncs.bdtrc, 3,
+              xp_capabilities(
+                  cpu_only=True, exceptions=['cupy'],
+                  skip_backends=[
+                      ('jax.numpy', "unavailable in jax and fallback fails")]),
+              paramtypes=("real", "int", "real")),
     _FuncInfo(_ufuncs.betainc, 3, _needs_betainc),
     _FuncInfo(_ufuncs.betaincc, 3, _needs_betainc, generic_impl=_betaincc),
     _FuncInfo(_ufuncs.betaln, 2,
