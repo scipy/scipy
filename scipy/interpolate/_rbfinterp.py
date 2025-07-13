781535842669fc08bcd5476c884f28bac76c0737
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2025-07-13 16:36:19 +0200

    MAINT: interpolate: move rbf impl routines to _rbfinterp_np

--- scipy/interpolate/_rbfinterp.py
+++ scipy/interpolate/_rbfinterp.py
@@ -5,13 +5,12 @@ from types import GenericAlias
 from math import comb
 
 import numpy as np
-from numpy.linalg import LinAlgError
 from scipy.spatial import KDTree
-from scipy.linalg.lapack import dgesv  # type: ignore[attr-defined]
 
-from ._rbfinterp_pythran import (_build_system,
-                                 _build_evaluation_coefficients,
-                                 _polynomial_matrix)
+from ._rbfinterp_np import (
+    _build_and_solve_system,
+    _build_evaluation_coefficients,
+)
 
 
 __all__ = ["RBFInterpolator"]
@@ -76,6 +75,7 @@ def _monomial_powers_impl(ndim, degree):
                 out[count][var] += 1
 
             count += 1
+    return out
 
 
 def _monomial_powers(ndim, degree):
@@ -84,58 +84,6 @@ def _monomial_powers(ndim, degree):
     if len(out) == 0:
         out = out.reshape(0, ndim)
     return out
-
-
-def _build_and_solve_system(y, d, smoothing, kernel, epsilon, powers):
-    """Build and solve the RBF interpolation system of equations.
-
-    Parameters
-    ----------
-    y : (P, N) float ndarray
-        Data point coordinates.
-    d : (P, S) float ndarray
-        Data values at `y`.
-    smoothing : (P,) float ndarray
-        Smoothing parameter for each data point.
-    kernel : str
-        Name of the RBF.
-    epsilon : float
-        Shape parameter.
-    powers : (R, N) int ndarray
-        The exponents for each monomial in the polynomial.
-
-    Returns
-    -------
-    coeffs : (P + R, S) float ndarray
-        Coefficients for each RBF and monomial.
-    shift : (N,) float ndarray
-        Domain shift used to create the polynomial matrix.
-    scale : (N,) float ndarray
-        Domain scaling used to create the polynomial matrix.
-
-    """
-    lhs, rhs, shift, scale = _build_system(
-        y, d, smoothing, kernel, epsilon, powers
-        )
-    _, _, coeffs, info = dgesv(lhs, rhs, overwrite_a=True, overwrite_b=True)
-    if info < 0:
-        raise ValueError(f"The {-info}-th argument had an illegal value.")
-    elif info > 0:
-        msg = "Singular matrix."
-        nmonos = powers.shape[0]
-        if nmonos > 0:
-            pmat = _polynomial_matrix((y - shift)/scale, powers)
-            rank = np.linalg.matrix_rank(pmat)
-            if rank < nmonos:
-                msg = (
-                    "Singular matrix. The matrix of monomials evaluated at "
-                    "the data point coordinates does not have full column "
-                    f"rank ({rank}/{nmonos})."
-                    )
-
-        raise LinAlgError(msg)
-
-    return shift, scale, coeffs
 
 
 class RBFInterpolator:
---
+++ scipy/interpolate/_rbfinterp_np.py
@@ -0,0 +1,62 @@
+import numpy as np
+from numpy.linalg import LinAlgError
+from scipy.linalg.lapack import dgesv  # type: ignore[attr-defined]
+
+
+from ._rbfinterp_pythran import (
+    _build_system,
+    _build_evaluation_coefficients,
+    _polynomial_matrix
+)
+
+
+def _build_and_solve_system(y, d, smoothing, kernel, epsilon, powers):
+    """Build and solve the RBF interpolation system of equations.
+
+    Parameters
+    ----------
+    y : (P, N) float ndarray
+        Data point coordinates.
+    d : (P, S) float ndarray
+        Data values at `y`.
+    smoothing : (P,) float ndarray
+        Smoothing parameter for each data point.
+    kernel : str
+        Name of the RBF.
+    epsilon : float
+        Shape parameter.
+    powers : (R, N) int ndarray
+        The exponents for each monomial in the polynomial.
+
+    Returns
+    -------
+    coeffs : (P + R, S) float ndarray
+        Coefficients for each RBF and monomial.
+    shift : (N,) float ndarray
+        Domain shift used to create the polynomial matrix.
+    scale : (N,) float ndarray
+        Domain scaling used to create the polynomial matrix.
+
+    """
+    lhs, rhs, shift, scale = _build_system(
+        y, d, smoothing, kernel, epsilon, powers
+        )
+    _, _, coeffs, info = dgesv(lhs, rhs, overwrite_a=True, overwrite_b=True)
+    if info < 0:
+        raise ValueError(f"The {-info}-th argument had an illegal value.")
+    elif info > 0:
+        msg = "Singular matrix."
+        nmonos = powers.shape[0]
+        if nmonos > 0:
+            pmat = _polynomial_matrix((y - shift)/scale, powers)
+            rank = np.linalg.matrix_rank(pmat)
+            if rank < nmonos:
+                msg = (
+                    "Singular matrix. The matrix of monomials evaluated at "
+                    "the data point coordinates does not have full column "
+                    f"rank ({rank}/{nmonos})."
+                    )
+
+        raise LinAlgError(msg)
+
+    return shift, scale, coeffs
