18c063c1d66cb8884b4b75821041910e295445f5
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-07-22 16:04:39 +0200

    Create WKBIntuitiveMomentumSecondOrder.py

---
+++ WKB/WKBIntuitiveMomentumSecondOrder.py
@@ -0,0 +1,4 @@
+#As previously said in my last graphic engine repository (today july 2025 22th), wz can mix
+#albedo values or vectors with other constants in computer graphics. So here, we can that second order equations
+#(intuitively said) could be refined by quatum mechanics. The easiest way is to use frista simple physicists algorithm before using WKB method.
+#We co not need a model at first sight.
