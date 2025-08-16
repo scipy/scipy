46e4b822c635f4fd553cce3a8b0d3e6084b36b9c
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2025-08-16 17:38:24 +0200

    MAINT: note a workaround for torch.compile CPU threading problem
    Using a matmul under torch.compile ignores all ways to limit the number of threads

--- scipy/interpolate/_rbfinterp_dynamo.py
+++ scipy/interpolate/_rbfinterp_dynamo.py
@@ -10,7 +10,10 @@ from ._rbfinterp_xp import (
 
 import torch
 
+# https://github.com/pytorch/pytorch/issues/160812
 compute_interpolation = torch.compile(fullgraph=True, dynamic=True)(compute_interpolation)
+
+#_build_evaluation_coefficients = torch.compile(fullgraph=True, dynamic=True)(_build_evaluation_coefficients)
 
 # needed for tests
 torch._dynamo.config.cache_size_limit = 160
