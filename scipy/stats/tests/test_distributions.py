5923b32750afa0fb81743f97f71e99ed4637e640
dschmitz89 <danielschmitzsiegen@googlemail.com>
2025-08-22 20:28:32 +0200

    LINT: remove unused function

--- scipy/stats/tests/test_distributions.py
+++ scipy/stats/tests/test_distributions.py
@@ -14,7 +14,7 @@ import platform
 from numpy.testing import (assert_equal, assert_array_equal,
                            assert_almost_equal, assert_array_almost_equal,
                            assert_allclose, assert_,
-                           assert_array_less, assert_array_max_ulp, IS_PYPY)
+                           assert_array_less, assert_array_max_ulp)
 import pytest
 from pytest import raises as assert_raises
 
