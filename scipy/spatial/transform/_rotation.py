911d747a992dbf552a350a19fc6c02a182a3f748
Nevsor <Nevsor@users.noreply.github.com>
2025-09-03 22:46:47 +0200

    Fix typo: unto->onto

--- scipy/spatial/transform/_rotation.py
+++ scipy/spatial/transform/_rotation.py
@@ -1383,7 +1383,7 @@ class Rotation:
             - Second angle belongs to a set of size 180 degrees,
               given by: ``[-lambda, 180 - lambda]``, if ``lambda ≥ 0`` or
               ``[-180 - lambda, -lambda]``, if ``lambda < 0``. Here ``lambda``
-              is the angle in [-180, 180] that rotates axis 3 unto axis 1 
+              is the angle in [-180, 180] that rotates axis 3 onto axis 1 
               using axis 2 as the rotation vector.
 
         References
