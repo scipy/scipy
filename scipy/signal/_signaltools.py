bb5451fa3cfd842b903f7fbf9604a751647e9f33
Ole <38684453+OleBialas@users.noreply.github.com>
2025-09-04 15:11:38 +0200

    DOC: signal.detrend: clarify `overwrite_data` behaviour (#23491)

--- scipy/signal/_signaltools.py
+++ scipy/signal/_signaltools.py
@@ -4072,8 +4072,11 @@ def detrend(data: np.ndarray, axis: int = -1,
         performed for each part of `data` between two break points.
         Break points are specified as indices into `data`. This parameter
         only has an effect when ``type == 'linear'``.
-    overwrite_data : bool, optional
-        If True, perform in place detrending and avoid a copy. Default is False
+    overwrite_data: bool, optional
+        If True, allow in place detrending and avoid a copy. Default is
+        False. In place modification applies only if ``type == 'linear'``
+        and `data` is of the floating point dtype ``float32``, ``float64``,
+        ``complex64`` or ``complex128``.
 
     Returns
     -------
