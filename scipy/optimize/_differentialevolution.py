17fbf6bffca02c9264f270c0d391951ee46220bc
Andrew Nelson <andyfaff@gmail.com>
2025-09-08 19:30:03 +1000

    LINT

--- scipy/optimize/_differentialevolution.py
+++ scipy/optimize/_differentialevolution.py
@@ -1289,7 +1289,8 @@ class DifferentialEvolutionSolver:
             _f = self.original_func
             if not callable(pf):
                 pf = partial(minimize, method=polish_method)
-                _f = lambda x: list(self._mapwrapper(self.func, np.atleast_2d(x)))[0]
+                def _f(x):
+                    return list(self._mapwrapper(self.func, np.atleast_2d(x)))[0]
 
                 if self.disp:
                     print(f"Polishing solution with '{polish_method}'")
