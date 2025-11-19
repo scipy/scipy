cfe21f9d4da60cb2b8fa43aaa683b993a39c331b
Bernard Roesler <bernard.roesler@gmail.com>
2025-11-19 15:21:18 -0500

    TST: Update sksparse.cholmod usage in tests to v0.5.0 API.

--- scipy/optimize/tests/test_linprog.py
+++ scipy/optimize/tests/test_linprog.py
@@ -28,8 +28,7 @@ except ImportError:
 
 has_cholmod = True
 try:
-    import sksparse  # noqa: F401
-    from sksparse.cholmod import cholesky as cholmod  # noqa: F401
+    from sksparse.cholmod import CholmodWarning
 except ImportError:
     has_cholmod = False
 
@@ -1154,6 +1153,10 @@ class LinprogCommonTests:
             # this is an UmfpackWarning but I had trouble importing it
             if has_umfpack:
                 warnings.simplefilter("ignore", UmfpackWarning)
+            if has_cholmod:
+                warnings.filterwarnings(
+                    "ignore", "Matrix is nearly singular", CholmodWarning
+                )
             warnings.filterwarnings(
                 "ignore", "scipy.linalg.solve\nIll...", RuntimeWarning)
             warnings.filterwarnings(
@@ -1401,6 +1404,10 @@ class LinprogCommonTests:
         with warnings.catch_warnings():
             if has_umfpack:
                 warnings.simplefilter("ignore", UmfpackWarning)
+            if has_cholmod:
+                warnings.filterwarnings(
+                    "ignore", "Matrix is nearly singular", CholmodWarning
+                )
             warnings.filterwarnings(
                 "ignore", "Solving system with option 'cholesky'", OptimizeWarning)
             warnings.filterwarnings(
