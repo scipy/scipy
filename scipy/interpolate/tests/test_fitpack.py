fbb3a4a4cf96015e58af7d8b142eedbc461c48da
Gagandeep Singh <gdp.1807@gmail.com>
2025-12-02 19:54:36 +0530

    MAINT: Remove regrid_python from public scope

--- scipy/interpolate/tests/test_fitpack.py
+++ scipy/interpolate/tests/test_fitpack.py
@@ -10,11 +10,12 @@ import pytest
 from scipy._lib._testutils import check_free_memory
 
 from scipy.interpolate import RectBivariateSpline
-from scipy.interpolate import make_splrep, regrid_python
+from scipy.interpolate import make_splrep
 
 from scipy.interpolate._fitpack_py import (splrep, splev, bisplrep, bisplev,
      sproot, splprep, splint, spalde, splder, splantider, insert, dblint)
 from scipy.interpolate._fitpack2 import _regrid_smth as regrid_smth
+from scipy.interpolate._regrid_python import regrid_python
 
 dfitpack_int = np.int32
 
