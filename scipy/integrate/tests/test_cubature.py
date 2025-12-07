393dfc7e1a4770b16be1a5c54843d693cf227a09
Christine P. Chai <star1327p@gmail.com>
2025-12-07 11:41:16 -0800

    Update scipy/integrate/tests/test_cubature.py [docs only]
    Co-authored-by: Matt Haberland <matt.haberland@gmail.com>

--- scipy/integrate/tests/test_cubature.py
+++ scipy/integrate/tests/test_cubature.py
@@ -28,7 +28,7 @@ boolean_index_skip_reason = 'JAX/Dask arrays do not support boolean assignment.'
 #   A.C. Genz, A.A. Malik, Remarks on algorithm 006: An adaptive algorithm for
 #   numerical integration over an N-dimensional rectangular region, Journal of
 #   Computational and Applied Mathematics, Volume 6, Issue 4, 1980, Pages 295-302,
-#   ISSN 0377-0427, :doi:`10.1016/0771-050X(80)90039-X`.
+#   ISSN 0377-0427, https://doi.org/10.1016/0771-050X(80)90039-X.
 
 
 def basic_1d_integrand(x, n, xp):
