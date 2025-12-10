c6b1d3971139c680c61f0a694a7839ea5d58e0f0
Andrew Nelson <andyfaff@gmail.com>
2025-12-11 08:59:29 +1100

    TST: skip zero weighted quantile test for numpy<2.1.3

--- scipy/stats/tests/test_quantile.py
+++ scipy/stats/tests/test_quantile.py
@@ -297,8 +297,8 @@ class TestQuantile:
 
     @pytest.mark.parametrize('zero_weights', [False, True])
     def test_weights_against_numpy(self, zero_weights, xp):
-        if is_numpy(xp) and xp.__version__ < "2.0.1":
-            pytest.skip('`Bug in np.quantile, NumPy < 2.0.1')
+        if is_numpy(xp) and xp.__version__ < "2.1.3":
+            pytest.skip('`Bug in np.quantile (numpy/numpy#27563) fixed in 2.1.3')
         dtype = xp_default_dtype(xp)
         rng = np.random.default_rng(85468924398205602)
         method = 'inverted_cdf'
