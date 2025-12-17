d539019465978ac1c7d372850c3674011577d5d0
Scott Shambaugh <scottshambaugh@users.noreply.github.com>
2025-12-17 15:43:14 -0700

    Automatically promote a Rotation when composing with a RigidTransform

--- scipy/spatial/transform/_rigid_transform.py
+++ scipy/spatial/transform/_rigid_transform.py
@@ -1441,7 +1441,9 @@ class RigidTransform:
     @xp_capabilities(
         skip_backends=[("dask.array", "missing linalg.cross/det functions")]
     )
-    def __mul__(self, other: RigidTransform) -> RigidTransform | NotImplementedType:
+    def __mul__(
+        self, other: RigidTransform | Rotation
+    ) -> RigidTransform | NotImplementedType:
         """Compose this transform with the other.
 
         If ``p`` and ``q`` are two transforms, then the composition of '``q``
@@ -1458,11 +1460,14 @@ class RigidTransform:
         broadcasting rules. The resulting shape for two `RigidTransform` instances
         ``p`` and ``q`` is `np.broadcast_shapes(p.shape, q.shape)`.
 
+        If ``other`` is a `Rotation`, it is automatically promoted to a
+        `RigidTransform` with zero translation.
+
         Parameters
         ----------
-        other : `RigidTransform` instance
-            Transform(s) to be composed with this one. The shapes must be
-            broadcastable.
+        other : `RigidTransform` or `Rotation` instance
+            Transform(s) or rotation(s) to be composed with this one. The shapes
+            must be broadcastable.
 
         Returns
         -------
@@ -1520,7 +1525,9 @@ class RigidTransform:
         >>> tf.translation.shape
         (5, 4, 3)
         """
-        if not isinstance(other, RigidTransform):
+        if isinstance(other, Rotation):
+            other = RigidTransform.from_rotation(other)
+        elif not isinstance(other, RigidTransform):
             # If other is not a RigidTransform, we return NotImplemented to allow other
             # types to implement __rmul__
             return NotImplemented
@@ -1536,6 +1543,31 @@ class RigidTransform:
         if self._single and other._single:
             matrix = matrix[0, ...]
         return RigidTransform(matrix, normalize=True, copy=False)
+
+    @xp_capabilities(
+        skip_backends=[("dask.array", "missing linalg.cross/det functions")]
+    )
+    def __rmul__(self, other: Rotation) -> RigidTransform | NotImplementedType:
+        """Compose a rotation with this transform (rotation applied second).
+
+        See `__mul__` for more details.
+
+        Parameters
+        ----------
+        other : `Rotation` instance
+            The rotation to compose with this transform. The shapes must be
+            broadcastable.
+
+        Returns
+        -------
+        `RigidTransform` instance
+            The composed transform.
+        """
+        if isinstance(other, Rotation):
+            other = RigidTransform.from_rotation(other)
+            return other * self
+        # When other is a RigidTransform __mul__ is called, so we don't handle it here
+        return NotImplemented
 
     @xp_capabilities(
         skip_backends=[("dask.array", "missing linalg.cross/det functions")]
