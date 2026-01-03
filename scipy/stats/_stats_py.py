ac1b108ab6e9cdc4a9b7565b38fdb07fefaedc15
Matt Haberland <mdhaber@mit.edu>
2026-01-03 13:18:42 -0600

    ENH: stats.theilslopes: vectorize implementation (#23830)
    * ENH: stats.theilslopes: vectorize implementation
    
    * MAINT: stats.theilslopes: adjustments to address some CI failures
    
    * MAINT: stats.theilslopes: avoid int(nan) causing invalid indices
    
    * TST: stats.theilslopes: error when 1d slice is too small to facilitate _axis_nan_policy decorator testing
    
    * MAINT: stats.theilslopes: separate stats and mstats implementations
    
    * TST: stats.theilslopes: add test against mststs implementation
    
    * STY: stats:remove unused imports
    
    * MAINT: stats.theilslopes: fix CI failures
    
    * MAINT: stats.theilslopes: adjustments per review
    
    * STY: stats.theilslopes: lint fix

--- scipy/stats/_stats_py.py
+++ scipy/stats/_stats_py.py
@@ -50,7 +50,7 @@ from scipy import linalg  # noqa: F401
 from . import distributions
 from . import _mstats_basic as mstats_basic
 
-from ._stats_mstats_common import theilslopes, siegelslopes
+from ._stats_mstats_common import siegelslopes
 from ._stats import _kendall_dis, _toint64, _weightedrankedtau
 
 from dataclasses import dataclass, field
@@ -101,7 +101,7 @@ __all__ = ['gmean', 'hmean', 'pmean', 'mode', 'tmean', 'tvar',
            'f_oneway', 'pearsonr', 'fisher_exact',
            'spearmanr', 'pointbiserialr',
            'kendalltau', 'weightedtau',
-           'linregress', 'siegelslopes', 'theilslopes', 'ttest_1samp',
+           'linregress', 'siegelslopes', 'ttest_1samp',
            'ttest_ind', 'ttest_ind_from_stats', 'ttest_rel',
            'kstest', 'ks_1samp', 'ks_2samp',
            'chisquare', 'power_divergence',
