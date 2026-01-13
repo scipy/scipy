3c7d2cfa449e59d9294513212406161113ab4fff
Jake Bowhay <jb9.bowhay@gmail.com>
2026-01-13 22:34:48 +0000

    DOC:make numpydoc lint work for class methods
    [docs only]

--- tools/numpydoc_lint.py
+++ tools/numpydoc_lint.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import sys
 import importlib
+import types
+import warnings
 
 from numpydoc.validate import validate
 
@@ -41,32 +43,58 @@ skip_items = [
 ]
 
 
+def walk_class(module_str, class_, public_api):
+    class_str = class_.__name__
+    attrs = {a for a in dir(class_) if not a.startswith("_")}
+    for attr in attrs:
+        item = getattr(class_, attr)
+        if isinstance(item, types.FunctionType):
+            public_api.append(f"{module_str}.{class_str}.{attr}")
+
+
+def walk_module(module_str, public_api=None):
+    if public_api is None:
+        public_api = []
+
+    module = importlib.import_module(module_str)
+
+    for item_str in module.__all__:
+        with warnings.catch_warnings(record=True) as w:
+            warnings.simplefilter("always", category=DeprecationWarning)
+            item = getattr(module, item_str)
+            if w:
+                continue
+
+        if isinstance(item, float | int | dict):  # ignore constants
+            continue
+        elif isinstance(item, types.ModuleType):
+            continue
+        elif isinstance(item, type):  # classes
+            public_api.append(f"{module_str}.{item_str}")
+            walk_class(module_str, item, public_api)  # methods
+        else:  # functions
+            public_api.append(f"{module_str}.{item_str}")
+    return public_api
+
+
 def main():
-    # load in numpydoc config
-    to_check = []
+    public_api = []
 
     # get a list of all public objects
     for module in PUBLIC_MODULES:
-        if "odr" in module:
-            # skip ODR as deprecated
+        if ("mstats" in module or "odr" in module or "fftpack" in module or
+            "cython" in module):
+            # deprecated / legacy modules
             continue
-        mod = importlib.import_module(module)
-        try:
-            to_check.extend(obj for f in mod.__all__
-                            if (obj := f"{module}.{f}") not in PUBLIC_MODULES)
-        except AttributeError:
-            # needed for some deprecated modules
-            continue
+        public_api += walk_module(module)
 
     errors = 0
-    for item in to_check:
+    for item in public_api:
         if str(item) in skip_items:
             continue
         try:
             res = validate(item)
         except AttributeError:
-            continue
-        if res["type"] in ("module", "float", "int", "dict"):
             continue
         for err in res["errors"]:
             if err[0] not in skip_errors:
