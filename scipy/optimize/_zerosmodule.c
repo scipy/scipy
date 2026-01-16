c22fadf0755397cbbe27762c91ec5f740f5f9c08
ilayn <ilhanpolat@gmail.com>
2026-01-16 15:18:39 +0100

    MAINT:optimize: Multiphase initialization for _Zeros extension module

--- scipy/optimize/_zerosmodule.c
+++ scipy/optimize/_zerosmodule.c
@@ -147,11 +147,69 @@ call_solver(solver_type solver, PyObject *self, PyObject *args)
  * These routines interface with the solvers through call_solver
  */
 
+static char doc_bisect[] = (
+    "_bisect(f, a, b, xtol, rtol, maxiter, args, full_output, disp)\n\n"
+    "Find a root of f in [a, b] using bisection.\n\n"
+    "Parameters\n"
+    "----------\n"
+    "f : callable\n"
+    "    Python function returning a float.\n"
+    "a : float\n"
+    "    Left bracket endpoint.\n"
+    "b : float\n"
+    "    Right bracket endpoint.\n"
+    "xtol : float\n"
+    "    Absolute tolerance. Must be >= 0.\n"
+    "rtol : float\n"
+    "    Relative tolerance.\n"
+    "maxiter : int\n"
+    "    Maximum number of iterations. Must be >= 0.\n"
+    "args : tuple\n"
+    "    Extra arguments passed to f.\n"
+    "full_output : int\n"
+    "    Non-zero to return detailed results.\n"
+    "disp : int, optional\n"
+    "    Non-zero to raise RuntimeError on convergence failure.\n\n"
+    "Returns\n"
+    "-------\n"
+    "If full_output is 0: root (float)\n"
+    "If full_output != 0: (root, funcalls, iterations, flag)\n"
+);
+
 static PyObject *
 _bisect(PyObject *self, PyObject *args)
 {
         return call_solver(bisect,self,args);
 }
+
+static char doc_ridder[] = (
+    "_ridder(f, a, b, xtol, rtol, maxiter, args, full_output, disp)\n\n"
+    "Find a root of f in [a, b] using Ridder's method.\n\n"
+    "Parameters\n"
+    "----------\n"
+    "f : callable\n"
+    "    Python function returning a float.\n"
+    "a : float\n"
+    "    Left bracket endpoint.\n"
+    "b : float\n"
+    "    Right bracket endpoint.\n"
+    "xtol : float\n"
+    "    Absolute tolerance. Must be >= 0.\n"
+    "rtol : float\n"
+    "    Relative tolerance.\n"
+    "maxiter : int\n"
+    "    Maximum number of iterations. Must be >= 0.\n"
+    "args : tuple\n"
+    "    Extra arguments passed to f.\n"
+    "full_output : int\n"
+    "    Non-zero to return detailed results.\n"
+    "disp : int, optional\n"
+    "    Non-zero to raise RuntimeError on convergence failure.\n\n"
+    "Returns\n"
+    "-------\n"
+    "If full_output is 0: root (float)\n"
+    "If full_output != 0: (root, funcalls, iterations, flag)\n"
+);
 
 static PyObject *
 _ridder(PyObject *self, PyObject *args)
@@ -159,11 +217,71 @@ _ridder(PyObject *self, PyObject *args)
         return call_solver(ridder,self,args);
 }
 
+static char doc_brenth[] = (
+    "_brenth(f, a, b, xtol, rtol, maxiter, args, full_output, disp)\n\n"
+    "Find a root of f in [a, b] using Brent's method with hyperbolic "
+    "extrapolation.\n\n"
+    "Parameters\n"
+    "----------\n"
+    "f : callable\n"
+    "    Python function returning a float.\n"
+    "a : float\n"
+    "    Left bracket endpoint.\n"
+    "b : float\n"
+    "    Right bracket endpoint.\n"
+    "xtol : float\n"
+    "    Absolute tolerance. Must be >= 0.\n"
+    "rtol : float\n"
+    "    Relative tolerance.\n"
+    "maxiter : int\n"
+    "    Maximum number of iterations. Must be >= 0.\n"
+    "args : tuple\n"
+    "    Extra arguments passed to f.\n"
+    "full_output : int\n"
+    "    Non-zero to return detailed results.\n"
+    "disp : int, optional\n"
+    "    Non-zero to raise RuntimeError on convergence failure.\n\n"
+    "Returns\n"
+    "-------\n"
+    "If full_output is 0: root (float)\n"
+    "If full_output != 0: (root, funcalls, iterations, flag)\n"
+);
+
 static PyObject *
 _brenth(PyObject *self, PyObject *args)
 {
         return call_solver(brenth,self,args);
 }
+
+static char doc_brentq[] = (
+    "_brentq(f, a, b, xtol, rtol, maxiter, args, full_output, disp)\n\n"
+    "Find a root of f in [a, b] using Brent's method with inverse quadratic "
+    "extrapolation.\n\n"
+    "Parameters\n"
+    "----------\n"
+    "f : callable\n"
+    "    Python function returning a float.\n"
+    "a : float\n"
+    "    Left bracket endpoint.\n"
+    "b : float\n"
+    "    Right bracket endpoint.\n"
+    "xtol : float\n"
+    "    Absolute tolerance. Must be >= 0.\n"
+    "rtol : float\n"
+    "    Relative tolerance.\n"
+    "maxiter : int\n"
+    "    Maximum number of iterations. Must be >= 0.\n"
+    "args : tuple\n"
+    "    Extra arguments passed to f.\n"
+    "full_output : int\n"
+    "    Non-zero to return detailed results.\n"
+    "disp : int, optional\n"
+    "    Non-zero to raise RuntimeError on convergence failure.\n\n"
+    "Returns\n"
+    "-------\n"
+    "If full_output is 0: root (float)\n"
+    "If full_output != 0: (root, funcalls, iterations, flag)\n"
+);
 
 static PyObject *
 _brentq(PyObject *self, PyObject *args)
@@ -177,38 +295,52 @@ _brentq(PyObject *self, PyObject *args)
 
 static PyMethodDef
 Zerosmethods[] = {
-	{"_bisect", _bisect, METH_VARARGS, "a"},
-	{"_ridder", _ridder, METH_VARARGS, "a"},
-	{"_brenth", _brenth, METH_VARARGS, "a"},
-	{"_brentq", _brentq, METH_VARARGS, "a"},
+	{"_bisect", _bisect, METH_VARARGS, doc_bisect},
+	{"_ridder", _ridder, METH_VARARGS, doc_ridder},
+	{"_brenth", _brenth, METH_VARARGS, doc_brenth},
+	{"_brentq", _brentq, METH_VARARGS, doc_brentq},
 	{NULL, NULL}
 };
 
-static struct PyModuleDef moduledef = {
-    PyModuleDef_HEAD_INIT,
-    "_zeros",
-    NULL,
-    -1,
-    Zerosmethods,
-    NULL,
-    NULL,
-    NULL,
-    NULL
+static int module_exec(PyObject *module)
+{
+#if Py_GIL_DISABLED
+    PyUnstable_Module_SetGIL(module, Py_MOD_GIL_NOT_USED);
+#endif
+    return 0;
+}
+
+
+static PyModuleDef_Slot module_slots[] = {
+    {Py_mod_exec, module_exec},
+    {Py_mod_multiple_interpreters, Py_MOD_PER_INTERPRETER_GIL_SUPPORTED},
+#if PY_VERSION_HEX >= 0x030d00f0  /* Python 3.13+ */
+    {Py_mod_gil, Py_MOD_GIL_NOT_USED},
+#endif
+    {0, NULL}
 };
+
+
+static char module_doc[] =
+    "Low-level scalar root-finding functions.\n\n"
+    "This module provides C implementations of bracketing root-finding\n"
+    "algorithms: bisection, Ridder's method, and two variants of Brent's\n"
+    "method (brentq with inverse quadratic extrapolation, brenth with\n"
+    "hyperbolic extrapolation).\n";
+
+
+static struct PyModuleDef moduledef = {
+    .m_base = PyModuleDef_HEAD_INIT,
+    .m_name = "_zeros",
+    .m_doc = module_doc,
+    .m_size = 0,
+    .m_methods = Zerosmethods,
+    .m_slots = module_slots
+};
+
 
 PyMODINIT_FUNC
 PyInit__zeros(void)
 {
-    PyObject *module;
-
-    module = PyModule_Create(&moduledef);
-    if (module == NULL) {
-        return module;
-    }
-
-#if Py_GIL_DISABLED
-    PyUnstable_Module_SetGIL(module, Py_MOD_GIL_NOT_USED);
-#endif
-
-    return module;
+    return PyModuleDef_Init(&moduledef);
 }
