3b309253b057db9cfddd8e7a34c9b43460d84d70
Jake Bowhay <jb9.bowhay@gmail.com>
2026-01-17 08:51:29 +0000

    lint
    [lint only]

--- scipy/optimize/_qap.py
+++ scipy/optimize/_qap.py
@@ -1,11 +1,8 @@
 import numpy as np
 import operator
-import warnings
-import numbers
 from . import (linear_sum_assignment, OptimizeResult)
 from ._optimize import _check_unknown_options
 
-from scipy._lib._util import check_random_state
 import itertools
 
 QUADRATIC_ASSIGNMENT_METHODS = ['faq', '2opt']
