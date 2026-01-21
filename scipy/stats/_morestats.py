6f88e4662e2f950cb89975718c5967c522ff5598
Matt Haberland <matt.haberland@gmail.com>
2026-01-21 06:39:56 -0800

    MAINT: stats.fligner: fix _axis_nan_policy test failure by specifying rankdata axis

--- scipy/stats/_morestats.py
+++ scipy/stats/_morestats.py
@@ -3545,7 +3545,7 @@ def fligner(*samples, center='median', proportiontocut=0.05, axis=0):
     Xibar = [func(sample) for sample in samples]
     Xij_Xibar = [xp.abs(sample - Xibar_) for sample, Xibar_ in zip(samples, Xibar)]
     Xij_Xibar = xp.concat(Xij_Xibar, axis=-1)
-    ranks = stats.rankdata(Xij_Xibar, method='average')
+    ranks = stats.rankdata(Xij_Xibar, method='average', axis=-1)
     ranks = xp.astype(ranks, dtype)
     a_Ni = special.ndtri(ranks / (2*(N + 1.0)) + 0.5)
 
