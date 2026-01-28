8fd9210eba6f3f3aaed209b4f5bb7202006808ba
Christian Lorentzen <lorentzen.ch@gmail.com>
2026-01-28 22:43:00 +0100

    DOC next try with refguide-check

--- scipy/signal/_whittaker.py
+++ scipy/signal/_whittaker.py
@@ -151,9 +151,10 @@ def whittaker_henderson(signal, *, lamb="reml", order=2, weights=None):
     >>> w = np.ones_like(y)
     >>> w[np.isnan(y)] = 0
     >>> z = whittaker_henderson(y, weights=w)
-    >>> y[:5], z[:5]
-    (array([-0.19, -0.25, -0.1 , -0.17, -0.11])),
-     array([-0.18743163, -0.18403575, -0.18066452, -0.17797578, -0.17585308]))
+    >>> y[:5]
+    array([-0.19, -0.25, -0.1 , -0.17, -0.11])
+    >>> z[:5]
+    array([-0.18743163, -0.18403575, -0.18066452, -0.17797578, -0.17585308]))
 
     Let us plot measurements and Whittaker-Henderson smoothing.
 
