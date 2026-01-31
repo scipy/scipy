335c49b8edd8ff5416fd28e6f06af245c49c17f3
Jake Bowhay <jb9.bowhay@gmail.com>
2026-01-31 16:40:13 +0000

    DOC: add missing parameter descriptions and enable PR07 rule
    [docs only]

--- scipy/ndimage/_filters.py
+++ scipy/ndimage/_filters.py
@@ -1089,7 +1089,8 @@ def gaussian_laplace(input, sigma, output=None, mode="reflect",
     axes : tuple of int or None
         The axes over which to apply the filter. If `sigma` or `mode` tuples
         are provided, their length must match the number of axes.
-    Extra keyword arguments will be passed to gaussian_filter().
+    **kwargs
+        Extra keyword arguments will be passed to `gaussian_filter`.
 
     Returns
     -------
@@ -1214,7 +1215,8 @@ def gaussian_gradient_magnitude(input, sigma, output=None,
     axes : tuple of int or None
         The axes over which to apply the filter. If `sigma` or `mode` tuples
         are provided, their length must match the number of axes.
-    Extra keyword arguments will be passed to gaussian_filter().
+    **kwargs
+        Extra keyword arguments will be passed to `gaussian_filter`.
 
     Returns
     -------
