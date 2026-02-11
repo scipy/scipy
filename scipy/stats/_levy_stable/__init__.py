9841f30c5b2ec8b14b83cb3a3d04e55f5166e024
Matt Haberland <matt.haberland@gmail.com>
2026-02-11 15:54:59 -0800

    Update scipy/stats/_levy_stable/__init__.py
    [skip ci]

--- scipy/stats/_levy_stable/__init__.py
+++ scipy/stats/_levy_stable/__init__.py
@@ -282,7 +282,7 @@ def _pdf_single_value_piecewise_post_rounding_Z0(x0, alpha, beta, quad_eps,
             # exp_height = 1 is handled by peak
         ]
         # Symmetry is used to ensure x > zeta as above. Integration 
-        # becomesnumerically unstable so let `quad` handle 
+        # becomes numerically unstable so let `quad` handle 
         # integration points
         if beta != -1:
             intg_points = [0, peak] + tail_points
