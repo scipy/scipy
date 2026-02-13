dbf3e2be0ea133ff21f3431a4de900866df08979
Yaocheng Chen <yaocheng.chencyc@gmail.com>
2026-02-13 13:34:37 +0900

    DOC: fixed some typo

--- scipy/stats/_axis_nan_policy.py
+++ scipy/stats/_axis_nan_policy.py
@@ -374,15 +374,15 @@ def _axis_nan_policy_factory(tuple_to_result, default_axis=0,
         Function that unpacks the results of the function being wrapped into
         a tuple. This is essentially the inverse of `tuple_to_result`. Default
         is `None`, which is appropriate for statistical tests that return a
-        statistic, pvalue tuple (rather than, e.g., a non-iterable datalass).
+        statistic, pvalue tuple (rather than, e.g., a non-iterable dataclass).
     too_small : int or callable, default: 0
-        The largest unnacceptably small sample for the function being wrapped.
+        The largest unacceptably small sample for the function being wrapped.
         For example, some functions require samples of size two or more or they
         raise an error. This argument prevents the error from being raised when
         input is not 1D and instead places a NaN in the corresponding element
         of the result. If callable, it must accept a list of samples, axis,
         and a dictionary of keyword arguments passed to the wrapper function as
-        arguments and return a bool indicating weather the samples passed are
+        arguments and return a bool indicating whether the samples passed are
         too small.
     n_outputs : int or callable, default: 2
         The number of outputs produced by the function given 1d sample(s). For
