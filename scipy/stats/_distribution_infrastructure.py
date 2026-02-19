3c686c6db135f9ab15cd9676dad3fe3eb7fef93b
Matt Haberland <matt.haberland@gmail.com>
2026-02-18 23:12:26 -0800

    MAINT: stats.UnivariateDistribution: use public elementwise iterative methods

--- scipy/stats/_distribution_infrastructure.py
+++ scipy/stats/_distribution_infrastructure.py
@@ -9,14 +9,13 @@ import numpy as np
 from numpy import inf
 
 from scipy._lib._array_api import xp_capabilities, xp_promote
-from scipy._lib._util import _rng_spawn, _RichResult
+from scipy._lib._util import _rng_spawn
 from scipy._lib._docscrape import ClassDoc, NumpyDocString
 from scipy._external import array_api_extra as xpx
 from scipy import special, stats
 from scipy.special._ufuncs import _log1mexp
 from scipy.integrate import tanhsinh as _tanhsinh, nsum
-from scipy.optimize._bracket import _bracket_root, _bracket_minimum
-from scipy.optimize._chandrupatla import _chandrupatla, _chandrupatla_minimize
+from scipy.optimize import elementwise
 from scipy.stats._probability_distribution import _ProbabilityDistribution
 from scipy.stats import qmc
 
@@ -2038,16 +2037,16 @@ class UnivariateDistribution(_ProbabilityDistribution):
 
         xl0, xr0 = _guess_bracket(xmin, xmax)
 
-        res = _bracket_root(f2, xl0=xl0, xr0=xr0, xmin=xmin, xmax=xmax,
-                            args=(p,), kwargs=params)
+        res = elementwise.bracket_root(f2, xl0=xl0, xr0=xr0, xmin=xmin, xmax=xmax,
+                                       args=(p,), kwargs=params)
         # For now, we ignore the status, but I want to use the bracket width
         # as an error estimate.
 
         xrtol = None if _isnull(self.tol) else self.tol
         xatol = None if xatol is None else xatol
         tolerances = dict(xrtol=xrtol, xatol=xatol, fatol=0, frtol=0)
-        return _chandrupatla(f2, a=res.xl, b=res.xr,
-                             args=(p,), kwargs=params, **tolerances)
+        return elementwise.find_root(f2, res.bracket, args=(p,),
+                                     kwargs=params, tolerances=tolerances)
 
     ## Other
 
@@ -2092,7 +2091,7 @@ class UnivariateDistribution(_ProbabilityDistribution):
     # params - a dictionary of distribution shape parameters passed by
     #          the public method.
     # Dispatch methods accept `params` rather than relying on the state of the
-    # object because iterative algorithms like `_tanhsinh` and `_chandrupatla`
+    # object because iterative algorithms like `tanhsinh` and `find_root`
     # need their callable to follow a strict elementwise protocol: each element
     # of the output is determined solely by the values of the inputs at the
     # corresponding location. The public methods do not satisfy this protocol
@@ -2271,18 +2270,18 @@ class UnivariateDistribution(_ProbabilityDistribution):
         def f(x, **params):
             return -self._pxf_dispatch(x, **params)
 
-        res_b = _bracket_minimum(f, m, xmin=a, xmax=b, kwargs=params)
-        res = _chandrupatla_minimize(f, res_b.xl, res_b.xm, res_b.xr,
-                                     kwargs=params, xatol=xatol)
+        res_b = elementwise.bracket_minimum(f, m, xmin=a, xmax=b, kwargs=params)
+        res = elementwise.find_minimum(f, res_b.bracket, kwargs=params,
+                                       tolerances=dict(xatol=xatol))
         mode = np.asarray(res.x)
 
-        # _bracket_minimum fails with status == -1 if the mode is at a boundary.
+        # bracket_minimum fails with status == -1 if the mode is at a boundary.
         # We have to consider this as a special case; there's no way to include
-        # this logic in _chandrupatla_minimize because _bracket_minimum doesn't
-        # produce a valid bracket.
+        # this logic in find_minimum; it has to treat the bracket as invalid.
         mode_at_boundary = res_b.status == -1
-        mode_at_left = mode_at_boundary & (res_b.fl <= res_b.fm)
-        mode_at_right = mode_at_boundary & (res_b.fr < res_b.fm)
+        fl, fm, fr = res_b.bracket
+        mode_at_left = mode_at_boundary & (fl <= fm)
+        mode_at_right = mode_at_boundary & (fr < fm)
         mode[mode_at_left] = a[mode_at_left]
         mode[mode_at_right] = b[mode_at_right]
         return mode[()]
@@ -3654,7 +3653,7 @@ class DiscreteDistribution(UnivariateDistribution):
         # F(floor(xr) - 1) < p (strictly) because floor(xr) - 1 < xl and F decreases
         # monotonically as the argument decreases. So we choose x = floor(xr), and
         # later we'll choose between x* = x and x* = x + 1.
-        x = np.asarray(np.floor(res.xr))
+        x = np.asarray(np.floor(res.bracket[-1]))
         # This is also suitable for case 2b. Because G is a *decreasing* function, we
         # know that G(xr) <= p (and G(xl) >= p), so G(floor(xr) + 1) <= p.
         # G(floor(xr)) *may* be <= p, but we can't know until we evaluate it.
@@ -3673,7 +3672,7 @@ class DiscreteDistribution(UnivariateDistribution):
         #    floor(res.x) + 1 is again the solution to the discrete problem.
         # Either way, we can choose x = res.x, and at the end we'll choose between
         # x* = x and x* = x + 1.
-        mask = res.fun == 0
+        mask = res.f_x == 0
         x[mask] = np.floor(res.x[mask])
 
         # For case 3, let xmin be the left endpoint of the support, and note that in
@@ -5249,8 +5248,8 @@ class Mixture(_ProbabilityDistribution):
         self._raise_if_method(method)
         a, b = self.support()
         def f(x): return -self.pdf(x)
-        res = _bracket_minimum(f, 1., xmin=a, xmax=b)
-        res = _chandrupatla_minimize(f, res.xl, res.xm, res.xr)
+        res = elementwise.bracket_minimum(f, 1., xmin=a, xmax=b)
+        res = elementwise.find_minimum(f, res.bracket)
         return res.x
 
     def median(self, *, method=None):
@@ -5352,8 +5351,9 @@ class Mixture(_ProbabilityDistribution):
         fun = getattr(self, fun)
         f = lambda x, p: fun(x) - p  # noqa: E731 is silly
         xl0, xr0 = _guess_bracket(xmin, xmax)
-        res = _bracket_root(f, xl0=xl0, xr0=xr0, xmin=xmin, xmax=xmax, args=(p,))
-        return _chandrupatla(f, a=res.xl, b=res.xr, args=(p,)).x
+        res = elementwise.bracket_root(f, xl0=xl0, xr0=xr0,
+                                       xmin=xmin, xmax=xmax, args=(p,))
+        return elementwise.find_root(f, res.bracket, args=(p,)).x
 
     def icdf(self, p, /, *, method=None):
         self._raise_if_method(method)
