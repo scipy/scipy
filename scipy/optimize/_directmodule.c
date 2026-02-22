6dda8c13c534a61bc56c7b11bb5841efbd37d9dd
Tyler Reddy <tyler.je.reddy@gmail.com>
2026-02-21 19:13:58 -0700

    BUG: PR 24584 revisions
    * A preprocessor guard was missing in `scipy/optimize/_directmodule.c`,
    causing the release branch to fail to build with CPython `3.11.x`.

--- scipy/optimize/_directmodule.c
+++ scipy/optimize/_directmodule.c
@@ -93,7 +93,9 @@ static int module_exec(PyObject *module) {
 
 static struct PyModuleDef_Slot direct_slots[] = {
     {Py_mod_exec, module_exec},
+#if PY_VERSION_HEX >= 0x030c00f0
     {Py_mod_multiple_interpreters, Py_MOD_PER_INTERPRETER_GIL_SUPPORTED},
+#endif
 #if PY_VERSION_HEX >= 0x030d00f0  /* Python 3.13+ */
     /* signal that this module supports running without an active GIL */
     {Py_mod_gil, Py_MOD_GIL_NOT_USED},
