5df8c8d43664de0ab4ed9a3cc0d1197368c1346c
ManasVardhan <manasvardhan@gmail.com>
2026-02-27 09:08:37 -0800

    BENCH: fix disabled (broken) benchmarks
    Re-enable benchmarks that were disabled in gh-19052:
    
    - optimize_milp.py: Re-enable time_milp for piperout-27 with a
      time_limit of 30s to prevent indefinite timeouts.
    - optimize_milp.py: Re-enable magic square benchmark for size 5.
      Size 6 remains excluded as it is still too slow.
    - stats.py: Remove skip for pareto (loc+scale fixed) and invgauss
      (loc fixed) fit benchmarks. These were disabled in Aug 2023 and
      the underlying issues have since been resolved.
    
    Note: sparse_linalg_svds.py benchmarks (propack solver) were already
    fixed in a previous update.
    
    Closes gh-19389

--- benchmarks/benchmarks/optimize_milp.py
+++ benchmarks/benchmarks/optimize_milp.py
@@ -48,18 +48,15 @@ class MilpMiplibBenchmarks(Benchmark):
         self.integrality = integrality
 
     def time_milp(self, prob):
-        # TODO: fix this benchmark (timing out in Aug. 2023); see gh-19389
-        # res = milp(c=self.c, constraints=self.constraints, bounds=self.bounds,
-        #           integrality=self.integrality)
-        # assert res.success
-        pass
+        res = milp(c=self.c, constraints=self.constraints,
+                   bounds=self.bounds, integrality=self.integrality,
+                   options={"time_limit": 30})
+        assert res.success
 
 
 class MilpMagicSquare(Benchmark):
 
-    # TODO: look at 5,6 - timing out and disabled in Apr'24 (5) and Aug'23 (6)
-    #       see gh-19389 for details
-    params = [[3, 4]]
+    params = [[3, 4, 5]]
     param_names = ['size']
 
     def setup(self, n):
