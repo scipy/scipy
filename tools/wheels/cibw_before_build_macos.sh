db9bfddef178c141facd2033d228deb4556df987
Andrew Nelson <andyfaff@gmail.com>
2026-03-02 12:23:49 +1100

    MAINT: fallback to older gfortran compiler for arm64

--- tools/wheels/cibw_before_build_macos.sh
+++ tools/wheels/cibw_before_build_macos.sh
@@ -16,12 +16,6 @@ if [[ $PLATFORM == "x86_64" ]]; then
   curl -L https://github.com/isuruf/gcc/releases/download/gcc-15.2.0/gfortran-darwin-x86_64-native.tar.gz -o gfortran.tar.gz
   KNOWN_SHA256="fb03c1f37bf0258ada6e3e41698e3ad416fff4dad448fd746e01d8ccf1efdc0f  gfortran.tar.gz"
   GFORTRAN_LOC=/opt/gfortran-darwin-x86_64-native
-fi
-if [[ $PLATFORM == "arm64" ]]; then
-  curl -L https://github.com/isuruf/gcc/releases/download/gcc-15.2.0/gfortran-darwin-arm64-native.tar.gz -o gfortran.tar.gz
-  KNOWN_SHA256="999a91eef894d32f99e3b641520bef9f475055067f301f0f1947b8b716b5922a  gfortran.tar.gz"
-  GFORTRAN_LOC=/opt/gfortran-darwin-arm64-native
-fi
 
   GFORTRAN_SHA256=$(shasum -a 256 gfortran.tar.gz)
   if [ "$GFORTRAN_SHA256" != "$KNOWN_SHA256" ]; then
@@ -30,9 +24,6 @@ fi
   fi
 
   sudo mkdir -p /opt/
-  # the following directories may not exist on a default image
-  sudo mkdir -p /usr/local/bin
-  sudo mkdir -p /usr/local/lib
   # places gfortran in /opt/gfortran-darwin-x86_64-native. There's then
   # bin, lib, include, libexec underneath that.
   sudo tar -xv -C /opt -f gfortran.tar.gz
@@ -49,6 +40,22 @@ fi
   # environment (so it mirrors what is done in the conda-forge compiler
   # activation scripts)
   export SDKROOT=${SDKROOT:-$(xcrun --show-sdk-path)}
+fi
+
+if [[ $PLATFORM == "arm64" ]]; then
+  curl -L https://github.com/fxcoudert/gfortran-for-macOS/releases/download/12.1-monterey/gfortran-ARM-12.1-Monterey.dmg -o gfortran.dmg
+  GFORTRAN_SHA256=$(shasum -a 256 gfortran.dmg)
+  KNOWN_SHA256="e2e32f491303a00092921baebac7ffb7ae98de4ca82ebbe9e6a866dd8501acdf  gfortran.dmg"
+
+  if [ "$GFORTRAN_SHA256" != "$KNOWN_SHA256" ]; then
+      echo sha256 mismatch
+      exit 1
+  fi
+
+  hdiutil attach -mountpoint /Volumes/gfortran gfortran.dmg
+  sudo installer -pkg /Volumes/gfortran/gfortran.pkg -target /
+  type -p gfortran
+fi
 
 # Install OpenBLAS
 python -m pip install -r requirements/openblas.txt
