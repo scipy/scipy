cf4f0a24e7068d88741a6f2654f6e8bc0ca90c03
Jeff Smith <jeffreyksmithjr@gmail.com>
2026-03-13 10:33:31 +0000

    MAINT: remove unused `assert_array_almost_equal` import

--- scipy/stats/tests/test_contingency.py
+++ scipy/stats/tests/test_contingency.py
@@ -1,7 +1,6 @@
 import numpy as np
 from numpy.testing import (assert_equal, assert_array_equal,
-                           assert_array_almost_equal, assert_approx_equal,
-                           assert_allclose)
+                           assert_approx_equal, assert_allclose)
 import pytest
 from pytest import raises as assert_raises
 from scipy import stats
