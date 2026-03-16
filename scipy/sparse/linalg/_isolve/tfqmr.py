88995610d4db69ef7ce80e88957f879458b5443f
geraldinegalindo <g3.wth17@gmail.com>
2026-03-16 10:54:06 +0100

    fixed validation to occur only if a non-default is given for maxiter

--- scipy/sparse/linalg/_isolve/tfqmr.py
+++ scipy/sparse/linalg/_isolve/tfqmr.py
@@ -93,8 +93,6 @@ def tfqmr(A, b, x0=None, *, rtol=1e-5, atol=0., maxiter=None, M=None,
     True
     """
 
-    # Check maxiter
-    _validate_int(maxiter, 'maxiter', minimum=1)
     # Check data type
     dtype = A.dtype
     if np.issubdtype(dtype, np.int64):
@@ -113,6 +111,8 @@ def tfqmr(A, b, x0=None, *, rtol=1e-5, atol=0., maxiter=None, M=None,
     ndofs = A.shape[0]
     if maxiter is None:
         maxiter = min(10000, ndofs * 10)
+    # Check maxiter if a value is given
+    _validate_int(maxiter, 'maxiter', minimum=1)
 
     if x0 is None:
         r = b.copy()
