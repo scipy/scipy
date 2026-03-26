7e70bce81aac56b1edf78bd996a689d4078be2ee
dan <danielwragg07@gmail.com>
2026-03-26 19:04:08 +0000

    DOC: roots_sh_legendre Examples added

--- scipy/special/_orthogonal.py
+++ scipy/special/_orthogonal.py
@@ -2548,6 +2548,35 @@ def roots_sh_legendre(n, mu=False):
         Handbook of Mathematical Functions with Formulas,
         Graphs, and Mathematical Tables. New York: Dover, 1972.
 
+    Examples
+    --------
+
+    Find the roots of a 7th order Legendre polynomial.
+
+    >>> import numpy as np
+    >>> from scipy.special import roots_sh_legendre, eval_sh_legendre
+    >>> roots, weights = roots_sh_legendre(7)
+
+    ``roots`` holds the roots, and ``weights`` holds the weights for
+    Gauss-Legendre quadrature.
+
+    >>> roots
+    array([0.02544604, 0.12923441, 0.29707742, 0.5, 0.70292258, 0.87076559,
+        0.97455396])
+    >>> weights
+    array([0.06474248, 0.1398527, 0.19091503, 0.20897959, 0.19091503, 0.1398527,
+        0.06474248])
+
+    Verify that we have the roots by evaluating the degree 7 Shifted Legendre
+    polynomial at ``roots``.  All the values are approximately zero:
+
+    >>> eval_sh_legendre(7, roots)
+    array([5.55111512e-16, 1.11022302e-16,  3.33066907e-16,  0.00000000e+00,
+        -2.22044605e-16, -1.11022302e-16, -1.85962357e-15])
+
+    ``weights`` can be used for Guass-Legendre quadrature to estimate integrals over
+    the interval :math:`[0, 1]`.
+
     """
     x, w = roots_legendre(n)
     x = (x + 1) / 2
