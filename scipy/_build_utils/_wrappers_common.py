85cf17a710b8610d1d1bc506656c332977875789
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2026-03-26 08:37:12 -0500

    include npy_math.h for cython codegen

--- scipy/_build_utils/_wrappers_common.py
+++ scipy/_build_utils/_wrappers_common.py
@@ -39,6 +39,7 @@ WRAPPED_FUNCS = ['cdotc', 'cdotu', 'zdotc', 'zdotu', 'cladiv', 'zladiv']
 USE_OLD_ACCELERATE = ['lsame', 'dcabs1']
 
 C_PREAMBLE = """
+#include "numpy/npy_math.h"  /* for npy_complex{64,128} only */
 #include "scipy_blas_defines.h"
 #include "fortran_defs.h"
 
