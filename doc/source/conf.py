8a4633fa0e01d62e9ccdd06ebe5bb30551cfa056
Dan Schult <dschult@colgate.edu>
2026-04-07 17:08:23 -0400

    DOC: separate sparse spmatrix docs to their own pages and add doc warnings  (#24961)
    * split spmatrix from rest of sparse docs and reword [docs only]
    
    * switch spmatrix from tutorials to sparray [docs only]
    
    * add doc warnings to class constructors and functions [docs only]
    
    * ensure refguide-check passes even with spmatrix docs separated
    
    * fix title bar lengths
    
    * make review suggestions happen

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -121,7 +121,7 @@ add_function_parentheses = False
 # Ensure all our internal links work
 nitpicky = True
 nitpick_ignore = [
-    # This ignores errors for classes (OptimizeResults, sparse.dok_matrix)
+    # This ignores errors for classes (OptimizeResults, sparse.dok_array)
     # which inherit methods from `dict`. missing references to builtins get
     # ignored by default (see https://github.com/sphinx-doc/sphinx/pull/7254),
     # but that fix doesn't work for inherited methods.
