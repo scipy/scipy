623c22709129d0e0df8c76aabd18f9c74eba6157
Dan Schult <dschult@colgate.edu>
2026-05-01 12:44:09 -0400

    remove spmatrix from scipy/sparse/csgraph

--- scipy/sparse/csgraph/tests/test_traversal.py
+++ scipy/sparse/csgraph/tests/test_traversal.py
@@ -2,7 +2,7 @@ import warnings
 import numpy as np
 import pytest
 from numpy.testing import assert_array_almost_equal
-from scipy.sparse import csr_array, csr_matrix, coo_array, coo_matrix
+from scipy.sparse import csr_array, coo_array
 from scipy.sparse.csgraph import (breadth_first_tree, depth_first_tree,
     csgraph_to_dense, csgraph_from_dense, csgraph_masked_from_dense)
 
@@ -79,42 +79,6 @@ def test_return_type():
     for directed in [True, False]:
         assert isinstance(depth_first_tree(csgraph, 0, directed), csr_array)
         assert isinstance(breadth_first_tree(csgraph, 0, directed), csr_array)
-
-    # start of testing with matrix/spmatrix types
-    with warnings.catch_warnings():
-        warnings.filterwarnings("ignore", "the matrix subclass.*", DeprecationWarning)
-        warnings.filterwarnings(
-            "ignore", "the matrix subclass.*", PendingDeprecationWarning)
-
-        nm_csgraph = np.matrix([[0, 1, 2, 0, 0],
-                                [1, 0, 0, 0, 3],
-                                [2, 0, 0, 7, 0],
-                                [0, 0, 7, 0, 1],
-                                [0, 3, 0, 1, 0]])
-
-    csgraph = csr_matrix(nm_csgraph)
-    assert isinstance(laplacian(csgraph), coo_matrix)
-    assert isinstance(minimum_spanning_tree(csgraph), csr_matrix)
-    for directed in [True, False]:
-        assert isinstance(depth_first_tree(csgraph, 0, directed), csr_matrix)
-        assert isinstance(breadth_first_tree(csgraph, 0, directed), csr_matrix)
-
-    csgraph = csgraph_from_dense(nm_csgraph, null_value=0)
-    assert isinstance(csgraph, csr_matrix)
-    assert isinstance(laplacian(csgraph), coo_matrix)
-    assert isinstance(minimum_spanning_tree(csgraph), csr_matrix)
-    for directed in [True, False]:
-        assert isinstance(depth_first_tree(csgraph, 0, directed), csr_matrix)
-        assert isinstance(breadth_first_tree(csgraph, 0, directed), csr_matrix)
-
-    mm_csgraph = csgraph_masked_from_dense(nm_csgraph, null_value=0)
-    assert isinstance(mm_csgraph, np.ma.MaskedArray)
-    # laplacian doesnt work with masked arrays so not here
-    assert isinstance(minimum_spanning_tree(csgraph), csr_matrix)
-    for directed in [True, False]:
-        assert isinstance(depth_first_tree(csgraph, 0, directed), csr_matrix)
-        assert isinstance(breadth_first_tree(csgraph, 0, directed), csr_matrix)
-    # end of testing with matrix/spmatrix types
 
 
 def test_graph_breadth_first_trivial_graph():
