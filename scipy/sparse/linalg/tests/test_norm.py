2636b609f2ecfdf8352b64bf43e35506c335c986
Dan Schult <dschult@colgate.edu>
2026-05-02 00:16:53 -0400

    linting for sparse/linalg/tests/test_norm.py

--- scipy/sparse/linalg/tests/test_norm.py
+++ scipy/sparse/linalg/tests/test_norm.py
@@ -5,7 +5,6 @@ import numpy as np
 from numpy.linalg import norm as npnorm
 from numpy.testing import assert_allclose, assert_equal
 from pytest import raises as assert_raises
-import pytest
 
 import scipy.sparse
 from scipy.sparse.linalg import norm as spnorm
