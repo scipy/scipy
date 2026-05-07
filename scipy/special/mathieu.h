3c6deb0af0ecc8a4633f0ea888753cf10407d07e
steppi <1953382+steppi@users.noreply.github.com>
2026-05-07 17:39:32 -0400

    Apply clang-format

--- scipy/special/mathieu.h
+++ scipy/special/mathieu.h
@@ -12,72 +12,68 @@ namespace special {
 
 double mathieu_a(double m, double q) {
     if ((m < 0) || (m != std::floor(m))) {
-      xsf::set_error("mathieu_a", SF_ERROR_DOMAIN, NULL);
-      return std::numeric_limits<double>::quiet_NaN();
+        xsf::set_error("mathieu_a", SF_ERROR_DOMAIN, NULL);
+        return std::numeric_limits<double>::quiet_NaN();
     }
     if (m > 500) {
-      // Fallback to Zhang and Jin implementation for very high order.
-      return xsf::cem_cva(m, q);
+        // Fallback to Zhang and Jin implementation for very high order.
+        return xsf::cem_cva(m, q);
     }
 
     auto int_m = static_cast<CBLAS_INT>(m);
     auto N = int_m + 25;
 
     std::vector<double> D(N);
-    std::vector<double> E(N-1);
+    std::vector<double> E(N - 1);
 
     if (int_m % 2) {
-      xsf::mathieu::make_matrix_eo(q, xsf::numpy::as_mdspan(D), xsf::numpy::as_mdspan(E));
+        xsf::mathieu::make_matrix_eo(q, xsf::numpy::as_mdspan(D), xsf::numpy::as_mdspan(E));
     } else {
-      xsf::mathieu::make_matrix_ee(q, xsf::numpy::as_mdspan(D), xsf::numpy::as_mdspan(E));
+        xsf::mathieu::make_matrix_ee(q, xsf::numpy::as_mdspan(D), xsf::numpy::as_mdspan(E));
     }
 
     auto status = eigvalsh_tridiagonal(D, E);
     if (status != SF_ERROR_OK) {
-      xsf::set_error("mathieu_a", status, NULL);
+        xsf::set_error("mathieu_a", status, NULL);
     }
 
     auto idx = int_m % 2 ? (int_m - 1) / 2 : int_m / 2;
     return D[idx];
 }
 
-float mathieu_a(float m, float q) {
-    return mathieu_a(static_cast<double>(m), static_cast<double>(q)); 
-}
+float mathieu_a(float m, float q) { return mathieu_a(static_cast<double>(m), static_cast<double>(q)); }
 
 double mathieu_b(double m, double q) {
     if ((m <= 0) || (m != std::floor(m))) {
-      xsf::set_error("mathieu_b", SF_ERROR_DOMAIN, NULL);
-      return std::numeric_limits<double>::quiet_NaN();
+        xsf::set_error("mathieu_b", SF_ERROR_DOMAIN, NULL);
+        return std::numeric_limits<double>::quiet_NaN();
     }
     if (m > 500) {
-      // Fallback to Zhang and Jin implementation for very high order.
-      return xsf::sem_cva(m, q);
+        // Fallback to Zhang and Jin implementation for very high order.
+        return xsf::sem_cva(m, q);
     }
 
     auto int_m = static_cast<CBLAS_INT>(m);
     auto N = int_m + 25;
 
     std::vector<double> D(N);
-    std::vector<double> E(N-1);
+    std::vector<double> E(N - 1);
 
     if (int_m % 2) {
-      xsf::mathieu::make_matrix_oo(q, xsf::numpy::as_mdspan(D), xsf::numpy::as_mdspan(E));
+        xsf::mathieu::make_matrix_oo(q, xsf::numpy::as_mdspan(D), xsf::numpy::as_mdspan(E));
     } else {
-      xsf::mathieu::make_matrix_oe(q, xsf::numpy::as_mdspan(D), xsf::numpy::as_mdspan(E));
+        xsf::mathieu::make_matrix_oe(q, xsf::numpy::as_mdspan(D), xsf::numpy::as_mdspan(E));
     }
 
     auto status = eigvalsh_tridiagonal(D, E);
     if (status != SF_ERROR_OK) {
-      xsf::set_error("mathieu_b", status, NULL);
+        xsf::set_error("mathieu_b", status, NULL);
     }
 
     auto idx = int_m % 2 ? (int_m - 1) / 2 : (int_m - 2) / 2;
     return D[idx];
 }
 
-float mathieu_b(float m, float q) {
-    return mathieu_b(static_cast<double>(m), static_cast<double>(q)); 
-}
+float mathieu_b(float m, float q) { return mathieu_b(static_cast<double>(m), static_cast<double>(q)); }
 
-}
+} // namespace special
