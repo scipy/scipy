28bdccf69e8725f85bc0c39d35e7b6a47037bfdb
steppi <1953382+steppi@users.noreply.github.com>
2026-05-07 23:51:33 -0400

    Add mathieu_coeffs

--- scipy/special/mathieu.h
+++ scipy/special/mathieu.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <algorithm>
 #include <vector>
 
 #include <xsf/error.h>
@@ -60,6 +61,39 @@ double mathieu_cv(double m, double q) {
 
     auto idx = cv_index<FuncParity>(int_m);
     return D[idx];
+}
+
+template <xsf::mathieu::Parity FuncParity>
+sf_error_t mathieu_coeffs(double q, int m, std::vector<double> &AA) {
+    using namespace xsf::mathieu;
+    auto constexpr Even = Parity::Even;
+    auto constexpr Odd = Parity::Odd;
+
+    auto N = AA.size();
+
+    if (N == 0) {
+        return SF_ERROR_OK;
+    }
+
+    std::vector<double> D(N);
+    std::vector<double> E(N - 1);
+    std::vector<double> Z(N * N);
+
+    if (m % 2) {
+        make_matrix<FuncParity, Odd>(q, xsf::numpy::as_mdspan(D), xsf::numpy::as_mdspan(E));
+    } else {
+        make_matrix<FuncParity, Even>(q, xsf::numpy::as_mdspan(D), xsf::numpy::as_mdspan(E));
+    }
+
+    auto status = eigh_tridiagonal(D, E, Z);
+    if (status != SF_ERROR_OK) {
+        return status;
+    }
+
+    auto idx = cv_index<FuncParity>(m);
+    std::copy_n(Z.begin() + idx * N, N, AA.begin());
+
+    return SF_ERROR_OK;
 }
 
 double mathieu_a(double m, double q) { return mathieu_cv<xsf::mathieu::Parity::Even>(m, q); }
