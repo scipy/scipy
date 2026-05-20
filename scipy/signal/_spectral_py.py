3145c66bcab8f2da15af2fe02fa1d376c0e0eb29
fbourgey <bourgeyflorian@gmail.com>
2026-05-20 14:47:29 -0400

    MAINT: remove dtype cast

--- scipy/signal/_spectral_py.py
+++ scipy/signal/_spectral_py.py
@@ -691,10 +691,7 @@ def welch(x, fs=1.0, window='hann_periodic', nperseg=None, noverlap=None, nfft=N
                            return_onesided=return_onesided, scaling=scaling,
                            axis=axis, average=average)
 
-    Pxx = xp.asarray(Pxx_np.real)
-    freqs = xp.asarray(freqs_np, dtype=Pxx.dtype)
-
-    return freqs, Pxx
+    return xp.asarray(freqs_np), xp.asarray(Pxx_np.real)
 
 
 def csd(x, y, fs=1.0, window='hann_periodic', nperseg=None, noverlap=None, nfft=None,
