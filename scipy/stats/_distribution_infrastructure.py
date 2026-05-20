sthagen/scipy-scipy

commit f81af9d67fc0bcc074cf3acb40cbf0a6ba08b50e

ENH: optimize.elementwise: add kwargs support (#24624)

scipy/stats/_distribution_infrastructure.py
@@ -9,14 +9,13 @@ import numpy as np
 from numpy import inf
 
 from scipy._lib._array_api import xp_capabilities, xp_promote
-from scipy._lib._util import _rng_spawn, _RichResult
+from scipy._lib._util import _rng_spawn
 from scipy._lib._docscrape import ClassDoc, NumpyDocString
 from scipy._external import array_api_extra as xpx
 from scipy import special, stats
 from scipy.special._ufuncs import _log1mexp
 from scipy.integrate import tanhsinh as _tanhsinh, nsum
-from scipy.optimize._bracket import _bracket_root, _bracket_minimum
-from scipy.optimize._chandrupatla import _chandrupatla, _chandrupatla_minimize
+from scipy.optimize import elementwise
 from scipy.stats._probability_distribution import _ProbabilityDistribution
 from scipy.stats import qmc
 
@@ -1197,28 +1196,6 @@ def _fiinfo(x):
         return np.iinfo(x)
 
 
-def _kwargs2args(f, args=None, kwargs=None):
-    # Wraps a function that accepts a primary argument `x`, secondary
-    # arguments `args`, and secondary keyward arguments `kwargs` such that the
-    # wrapper accepts only `x` and `args`. The keyword arguments are extracted
-    # from `args` passed into the wrapper, and these are passed to the
-    # underlying function as `kwargs`.
-    # This is a temporary workaround until the scalar algorithms `_tanhsinh`,
-    # `_chandrupatla`, etc., support `kwargs` or can operate with compressing
-    # arguments to the callable.
-    args = args or []
-    kwargs = kwargs or {}
-    names = list(kwargs.keys())
-    n_args = len(args)
-
-    def wrapped(x, *args):
-        return f(x, *args[:n_args], **dict(zip(names, args[n_args:])))
-
-    args = tuple(args) + tuple(kwargs.values())
-
-    return wrapped, args
-
-
 def _logexpxmexpy(x, y):
     """ Compute the log of the difference of the exponentials of two arguments.
 
@@ -2053,42 +2030,23 @@ class UnivariateDistribution(_ProbabilityDistribution):
 
     def _solve_bounded(self, f, p, *, bounds=None, params=None, xatol=None):
         # Finds the argument of a function that produces the desired output.
-        # Much of this should be added to _bracket_root / _chandrupatla.
         xmin, xmax = self._support(**params) if bounds is None else bounds
-        params = {} if params is None else params
-
-        p, xmin, xmax = np.broadcast_arrays(p, xmin, xmax)
-        if not p.size:
-            # might need to figure out result type based on p
-            res = _RichResult()
-            empty = np.empty(p.shape, dtype=self._dtype)
-            res.xl, res.x, res.xr = empty, empty, empty
-            res.fl, res.fr = empty, empty
 
         def f2(x, _p, **kwargs):  # named `_p` to avoid conflict with shape `p`
             return f(x, **kwargs) - _p
 
-        f3, args = _kwargs2args(f2, args=[p], kwargs=params)
-        # If we know the median or mean, should use it
-
-        # Any operations between 0d array and a scalar produces a scalar, so...
-        shape = xmin.shape
-        xmin, xmax = np.atleast_1d(xmin, xmax)
-
         xl0, xr0 = _guess_bracket(xmin, xmax)
-        xmin = xmin.reshape(shape)
-        xmax = xmax.reshape(shape)
-        xl0 = xl0.reshape(shape)
-        xr0 = xr0.reshape(shape)
 
-        res = _bracket_root(f3, xl0=xl0, xr0=xr0, xmin=xmin, xmax=xmax, args=args)
+        res = elementwise.bracket_root(f2, xl0=xl0, xr0=xr0, xmin=xmin, xmax=xmax,
+                                       args=(p,), kwargs=params)
         # For now, we ignore the status, but I want to use the bracket width
         # as an error estimate.
 
         xrtol = None if _isnull(self.tol) else self.tol
         xatol = None if xatol is None else xatol
         tolerances = dict(xrtol=xrtol, xatol=xatol, fatol=0, frtol=0)
-        return _chandrupatla(f3, a=res.xl, b=res.xr, args=args, **tolerances)
+        return elementwise.find_root(f2, res.bracket, args=(p,),
+                                     kwargs=params, tolerances=tolerances)
 
     ## Other
 
@@ -2133,7 +2091,7 @@ class UnivariateDistribution(_ProbabilityDistribution):
     # params - a dictionary of distribution shape parameters passed by
     #          the public method.
     # Dispatch methods accept `params` rather than relying on the state of the
-    # object because iterative algorithms like `_tanhsinh` and `_chandrupatla`
+    # object because iterative algorithms like `tanhsinh` and `find_root`
     # need their callable to follow a strict elementwise protocol: each element
     # of the output is determined solely by the values of the inputs at the
     # corresponding location. The public methods do not satisfy this protocol
@@ -2306,21 +2264,24 @@ class UnivariateDistribution(_ProbabilityDistribution):
         raise NotImplementedError(self._not_implemented)
 
     def _mode_optimization(self, xatol=None, **params):
-        if not self._size:
-            return np.empty(self._shape, dtype=self._dtype)
-
         a, b = self._support(**params)
         m = self._median_dispatch(**params)
 
-        f, args = _kwargs2args(lambda x, **params: -self._pxf_dispatch(x, **params),
-                               args=(), kwargs=params)
-        res_b = _bracket_minimum(f, m, xmin=a, xmax=b, args=args)
-        res = _chandrupatla_minimize(f, res_b.xl, res_b.xm, res_b.xr,
-                                     args=args, xatol=xatol)
+        def f(x, **params):
+            return -self._pxf_dispatch(x, **params)
+
+        res_b = elementwise.bracket_minimum(f, m, xmin=a, xmax=b, kwargs=params)
+        res = elementwise.find_minimum(f, res_b.bracket, kwargs=params,
+                                       tolerances=dict(xatol=xatol))
         mode = np.asarray(res.x)
+
+        # bracket_minimum fails with status == -1 if the mode is at a boundary.
+        # We have to consider this as a special case; there's no way to include
+        # this logic in find_minimum; it has to treat the bracket as invalid.
         mode_at_boundary = res_b.status == -1
-        mode_at_left = mode_at_boundary & (res_b.fl <= res_b.fm)
-        mode_at_right = mode_at_boundary & (res_b.fr < res_b.fm)
+        fl, fm, fr = res_b.bracket
+        mode_at_left = mode_at_boundary & (fl <= fm)
+        mode_at_right = mode_at_boundary & (fr < fm)
         mode[mode_at_left] = a[mode_at_left]
         mode[mode_at_right] = b[mode_at_right]
         return mode[()]
@@ -3692,7 +3653,7 @@ class DiscreteDistribution(UnivariateDistribution):
         # F(floor(xr) - 1) < p (strictly) because floor(xr) - 1 < xl and F decreases
         # monotonically as the argument decreases. So we choose x = floor(xr), and
         # later we'll choose between x* = x and x* = x + 1.
-        x = np.asarray(np.floor(res.xr))
+        x = np.asarray(np.floor(res.bracket[-1]))
         # This is also suitable for case 2b. Because G is a *decreasing* function, we
         # know that G(xr) <= p (and G(xl) >= p), so G(floor(xr) + 1) <= p.
         # G(floor(xr)) *may* be <= p, but we can't know until we evaluate it.
@@ -3711,7 +3672,7 @@ class DiscreteDistribution(UnivariateDistribution):
         #    floor(res.x) + 1 is again the solution to the discrete problem.
         # Either way, we can choose x = res.x, and at the end we'll choose between
         # x* = x and x* = x + 1.
-        mask = res.fun == 0
+        mask = res.f_x == 0
         x[mask] = np.floor(res.x[mask])
 
         # For case 3, let xmin be the left endpoint of the support, and note that in
@@ -5286,8 +5247,8 @@ class Mixture(_ProbabilityDistribution):
         self._raise_if_method(method)
         a, b = self.support()
         def f(x): return -self.pdf(x)
-        res = _bracket_minimum(f, 1., xmin=a, xmax=b)
-        res = _chandrupatla_minimize(f, res.xl, res.xm, res.xr)
+        res = elementwise.bracket_minimum(f, 1., xmin=a, xmax=b)
+        res = elementwise.find_minimum(f, res.bracket)
         return res.x
 
     def median(self, *, method=None):
@@ -5389,8 +5350,9 @@ class Mixture(_ProbabilityDistribution):
         fun = getattr(self, fun)
         f = lambda x, p: fun(x) - p  # noqa: E731 is silly
         xl0, xr0 = _guess_bracket(xmin, xmax)
-        res = _bracket_root(f, xl0=xl0, xr0=xr0, xmin=xmin, xmax=xmax, args=(p,))
-        return _chandrupatla(f, a=res.xl, b=res.xr, args=(p,)).x
+        res = elementwise.bracket_root(f, xl0=xl0, xr0=xr0,
+                                       xmin=xmin, xmax=xmax, args=(p,))
+        return elementwise.find_root(f, res.bracket, args=(p,)).x
 
     def icdf(self, p, /, *, method=None):
         self._raise_if_method(method)

scipy/optimize/_bracket.py
@@ -6,7 +6,7 @@ from scipy._lib._array_api import array_namespace, xp_ravel, xp_promote
 _ELIMITS = -1  # used in _bracket_root
 _ESTOPONESIDE = 2  # used in _bracket_root
 
-def _bracket_root_iv(func, xl0, xr0, xmin, xmax, factor, args, maxiter):
+def _bracket_root_iv(func, xl0, xr0, xmin, xmax, factor, args, kwargs, maxiter):
 
     if not callable(func):
         raise ValueError('`func` must be callable.')
@@ -54,7 +54,7 @@ def _bracket_root_iv(func, xl0, xr0, xmin, xmax, factor, args, maxiter):
     # Calculate the default value of xr0 if a value has not been supplied.
     # Be careful to ensure xr0 is not larger than xmax.
     if xr0_not_supplied:
-        xr0 = xl0 + xp.minimum((xmax - xl0)/ 8, 1.0)
+        xr0 = xl0 + xp.minimum((xmax - xl0)/ 8, xp.ones_like(xmax))
         xr0 = xp.astype(xr0, xl0.dtype, copy=False)
 
     maxiter = xp.asarray(maxiter)
@@ -66,11 +66,11 @@ def _bracket_root_iv(func, xl0, xr0, xmin, xmax, factor, args, maxiter):
     if not maxiter == maxiter_int or maxiter < 0:
         raise ValueError(message)
 
-    return func, xl0, xr0, xmin, xmax, factor, args, maxiter, xp
+    return func, xl0, xr0, xmin, xmax, factor, args, kwargs, maxiter, xp
 
 
 def _bracket_root(func, xl0, xr0=None, *, xmin=None, xmax=None, factor=None,
-                  args=(), maxiter=1000):
+                  args=(), kwargs=None, maxiter=1000):
     """Bracket the root of a monotonic scalar function of one variable
 
     This function works elementwise when `xl0`, `xr0`, `xmin`, `xmax`, `factor`, and
@@ -102,6 +102,8 @@ def _bracket_root(func, xl0, xr0=None, *, xmin=None, xmax=None, factor=None,
         bracketed requires arguments that are not broadcastable with these
         arrays, wrap that callable with `func` such that `func` accepts
         only `x` and broadcastable arrays.
+    kwargs : dict of str:array_like, optional
+        Additional keyword arguments to be passed to `func`. See `args`.
     maxiter : int, optional
         The maximum number of iterations of the algorithm to perform.
 
@@ -171,11 +173,11 @@ def _bracket_root(func, xl0, xr0=None, *, xmin=None, xmax=None, factor=None,
     # - allow factor < 1?
 
     callback = None  # works; I just don't want to test it
-    temp = _bracket_root_iv(func, xl0, xr0, xmin, xmax, factor, args, maxiter)
-    func, xl0, xr0, xmin, xmax, factor, args, maxiter, xp = temp
+    temp = _bracket_root_iv(func, xl0, xr0, xmin, xmax, factor, args, kwargs, maxiter)
+    func, xl0, xr0, xmin, xmax, factor, args, kwargs, maxiter, xp = temp
 
     xs = (xl0, xr0)
-    temp = eim._initialize(func, xs, args)
+    temp = eim._initialize(func, xs, args, kwargs=kwargs)
     func, xs, fs, args, shape, dtype, xp = temp  # line split for PEP8
     xl0, xr0 = xs
     xmin = xp_ravel(xp.astype(xp.broadcast_to(xmin, shape), dtype, copy=False), xp=xp)
@@ -414,7 +416,7 @@ def _bracket_root(func, xl0, xr0=None, *, xmin=None, xmax=None, factor=None,
                      xp)
 
 
-def _bracket_minimum_iv(func, xm0, xl0, xr0, xmin, xmax, factor, args, maxiter):
+def _bracket_minimum_iv(func, xm0, xl0, xr0, xmin, xmax, factor, args, kwargs, maxiter):
 
     if not callable(func):
         raise ValueError('`func` must be callable.')
@@ -489,11 +491,11 @@ def _bracket_minimum_iv(func, xm0, xl0, xr0, xmin, xmax, factor, args, maxiter):
     if not maxiter == maxiter_int or maxiter < 0:
         raise ValueError(message)
 
-    return func, xm0, xl0, xr0, xmin, xmax, factor, args, maxiter, xp
+    return func, xm0, xl0, xr0, xmin, xmax, factor, args, kwargs, maxiter, xp
 
 
 def _bracket_minimum(func, xm0, *, xl0=None, xr0=None, xmin=None, xmax=None,
-                     factor=None, args=(), maxiter=1000):
+                     factor=None, args=(), kwargs=None, maxiter=1000):
     """Bracket the minimum of a unimodal scalar function of one variable
 
     This function works elementwise when `xm0`, `xl0`, `xr0`, `xmin`, `xmax`,
@@ -529,6 +531,8 @@ def _bracket_minimum(func, xm0, *, xl0=None, xr0=None, xmin=None, xmax=None,
         callable to be bracketed requires arguments that are not broadcastable
         with these arrays, wrap that callable with `func` such that `func`
         accepts only ``x`` and broadcastable arrays.
+    kwargs : dict of str:array_like, optional
+        Additional keyword arguments to be passed to `f`. See `args`.
     maxiter : int, optional
         The maximum number of iterations of the algorithm to perform. The number
         of function evaluations is three greater than the number of iterations.
@@ -596,11 +600,12 @@ def _bracket_minimum(func, xm0, *, xl0=None, xr0=None, xmin=None, xmax=None,
     """  # noqa: E501
     callback = None  # works; I just don't want to test it
 
-    temp = _bracket_minimum_iv(func, xm0, xl0, xr0, xmin, xmax, factor, args, maxiter)
-    func, xm0, xl0, xr0, xmin, xmax, factor, args, maxiter, xp = temp
+    temp = _bracket_minimum_iv(func, xm0, xl0, xr0, xmin, xmax,
+                               factor, args, kwargs, maxiter)
+    func, xm0, xl0, xr0, xmin, xmax, factor, args, kwargs, maxiter, xp = temp
 
     xs = (xl0, xm0, xr0)
-    temp = eim._initialize(func, xs, args)
+    temp = eim._initialize(func, xs, args, kwargs=kwargs)
     func, xs, fs, args, shape, dtype, xp = temp
 
     xl0, xm0, xr0 = xs

scipy/optimize/_chandrupatla.py
@@ -4,12 +4,8 @@ import scipy._lib._elementwise_iterative_method as eim
 from scipy._lib._util import _RichResult
 from scipy._lib._array_api import xp_copy
 
-# TODO:
-# - (maybe?) don't use fancy indexing assignment
-# - figure out how to replace the new `try`/`except`s
 
-
-def _chandrupatla(func, a, b, *, args=(), xatol=None, xrtol=None,
+def _chandrupatla(func, a, b, *, args=(), kwargs=None, xatol=None, xrtol=None,
                   fatol=None, frtol=0, maxiter=None, callback=None):
     """Find the root of an elementwise function using Chandrupatla's algorithm.
 
@@ -34,6 +30,8 @@ def _chandrupatla(func, a, b, *, args=(), xatol=None, xrtol=None,
         broadcastable with one another.
     args : tuple, optional
         Additional positional arguments to be passed to `func`.
+    kwargs : dict of str:array_like, optional
+        Additional keyword arguments to be passed to `func`.
     xatol, xrtol, fatol, frtol : float, optional
         Absolute and relative tolerances on the root and function value.
         See Notes for details.
@@ -125,12 +123,12 @@ def _chandrupatla(func, a, b, *, args=(), xatol=None, xrtol=None,
     array([1.8932892 , 2.        , 2.09455148])
 
     """
-    res = _chandrupatla_iv(func, args, xatol, xrtol,
+    res = _chandrupatla_iv(func, args, kwargs, xatol, xrtol,
                            fatol, frtol, maxiter, callback)
-    func, args, xatol, xrtol, fatol, frtol, maxiter, callback = res
+    func, args, kwargs, xatol, xrtol, fatol, frtol, maxiter, callback = res
 
     # Initialization
-    temp = eim._initialize(func, (a, b), args)
+    temp = eim._initialize(func, (a, b), args, kwargs=kwargs)
     func, xs, fs, args, shape, dtype, xp = temp
     x1, x2 = xs
     f1, f2 = fs
@@ -243,7 +241,7 @@ def _chandrupatla(func, a, b, *, args=(), xatol=None, xrtol=None,
                      xp=xp)
 
 
-def _chandrupatla_iv(func, args, xatol, xrtol,
+def _chandrupatla_iv(func, args, kwargs, xatol, xrtol,
                      fatol, frtol, maxiter, callback):
     # Input validation for `_chandrupatla`
 
@@ -270,10 +268,10 @@ def _chandrupatla_iv(func, args, xatol, xrtol,
     if callback is not None and not callable(callback):
         raise ValueError('`callback` must be callable.')
 
-    return func, args, xatol, xrtol, fatol, frtol, maxiter, callback
+    return func, args, kwargs, xatol, xrtol, fatol, frtol, maxiter, callback
 
 
-def _chandrupatla_minimize(func, x1, x2, x3, *, args=(), xatol=None,
+def _chandrupatla_minimize(func, x1, x2, x3, *, args=(), kwargs=None, xatol=None,
                            xrtol=None, fatol=None, frtol=None, maxiter=100,
                            callback=None):
     """Find the minimizer of an elementwise function.
@@ -306,6 +304,8 @@ def _chandrupatla_minimize(func, x1, x2, x3, *, args=(), xatol=None,
         differentiated requires arguments that are not broadcastable with `x`,
         wrap that callable with `func` such that `func` accepts only `x` and
         broadcastable arrays.
+    kwargs : dict of str:array_like, optional
+        Additional keyword arguments to be passed to `f`. See `args`.
     xatol, xrtol, fatol, frtol : float, optional
         Absolute and relative tolerances on the minimizer and function value.
         See Notes for details.
@@ -390,13 +390,13 @@ def _chandrupatla_minimize(func, x1, x2, x3, *, args=(), xatol=None,
     >>> res.x
     array([1. , 1.5, 2. ])
     """
-    res = _chandrupatla_iv(func, args, xatol, xrtol,
+    res = _chandrupatla_iv(func, args, kwargs, xatol, xrtol,
                            fatol, frtol, maxiter, callback)
-    func, args, xatol, xrtol, fatol, frtol, maxiter, callback = res
+    func, args, kwargs, xatol, xrtol, fatol, frtol, maxiter, callback = res
 
     # Initialization
     xs = (x1, x2, x3)
-    temp = eim._initialize(func, xs, args)
+    temp = eim._initialize(func, xs, args, kwargs=kwargs)
     func, xs, fs, args, shape, dtype, xp = temp  # line split for PEP8
     x1, x2, x3 = xs
     f1, f2, f3 = fs

scipy/optimize/_elementwise.py
@@ -8,7 +8,8 @@ from scipy._lib._array_api import xp_capabilities
     skip_backends=[('dask.array', 'boolean indexing assignment'),
                    ('array_api_strict', 'Currently uses fancy indexing assignment.'),
                    ('jax.numpy', 'JAX arrays do not support item assignment.')])
-def find_root(f, init, /, *, args=(), tolerances=None, maxiter=None, callback=None):
+def find_root(f, init, /, *, args=(), kwargs=None,
+              tolerances=None, maxiter=None, callback=None):
     """Find the root of a monotonic, real-valued function of a real variable.
 
     For each element of the output of `f`, `find_root` seeks the scalar
@@ -51,6 +52,8 @@ def find_root(f, init, /, *, args=(), tolerances=None, maxiter=None, callback=No
         If the callable for which the root is desired requires arguments that are
         not broadcastable with `x`, wrap that callable with `f` such that `f`
         accepts only `x` and broadcastable ``*args``.
+    kwargs : dict of str:array_like, optional
+        Additional keyword arguments to be passed to `f`. See `args`.
     tolerances : dictionary of floats, optional
         Absolute and relative tolerances on the root and function value.
         Valid keys of the dictionary are:
@@ -231,7 +234,7 @@ def find_root(f, init, /, *, args=(), tolerances=None, maxiter=None, callback=No
     else:
         _callback = callback
 
-    res = _chandrupatla(f, xl, xr, args=args, **tolerances,
+    res = _chandrupatla(f, xl, xr, args=args, kwargs=kwargs, **tolerances,
                         maxiter=maxiter, callback=_callback)
     return reformat_result(res)
 
@@ -240,7 +243,8 @@ def find_root(f, init, /, *, args=(), tolerances=None, maxiter=None, callback=No
     skip_backends=[('dask.array', 'boolean indexing assignment'),
                    ('array_api_strict', 'Currently uses fancy indexing assignment.'),
                    ('jax.numpy', 'JAX arrays do not support item assignment.')])
-def find_minimum(f, init, /, *, args=(), tolerances=None, maxiter=100, callback=None):
+def find_minimum(f, init, /, *, args=(), kwargs=None,
+                 tolerances=None, maxiter=100, callback=None):
     """Find the minimum of a unimodal, real-valued function of a real variable.
 
     For each element of the output of `f`, `find_minimum` seeks the scalar minimizer
@@ -285,6 +289,8 @@ def find_minimum(f, init, /, *, args=(), tolerances=None, maxiter=100, callback=
         If the callable for which the root is desired requires arguments that are
         not broadcastable with `x`, wrap that callable with `f` such that `f`
         accepts only `x` and broadcastable ``*args``.
+    kwargs : dict of str:array_like, optional
+        Additional keyword arguments to be passed to `f`. See `args`.
     tolerances : dictionary of floats, optional
         Absolute and relative tolerances on the root and function value.
         Valid keys of the dictionary are:
@@ -460,7 +466,7 @@ def find_minimum(f, init, /, *, args=(), tolerances=None, maxiter=100, callback=
     else:
         _callback = callback
 
-    res = _chandrupatla_minimize(f, xl, xm, xr, args=args, **tolerances,
+    res = _chandrupatla_minimize(f, xl, xm, xr, args=args, kwargs=kwargs, **tolerances,
                                  maxiter=maxiter, callback=_callback)
     return reformat_result(res)
 
@@ -469,8 +475,8 @@ def find_minimum(f, init, /, *, args=(), tolerances=None, maxiter=100, callback=
     skip_backends=[('dask.array', 'boolean indexing assignment'),
                    ('array_api_strict', 'Currently uses fancy indexing assignment.'),
                    ('jax.numpy', 'JAX arrays do not support item assignment.')])
-def bracket_root(f, xl0, xr0=None, *, xmin=None, xmax=None, factor=None, args=(),
-                 maxiter=1000):
+def bracket_root(f, xl0, xr0=None, *, xmin=None, xmax=None, factor=None,
+                 args=(), kwargs=None, maxiter=1000):
     """Bracket the root of a monotonic, real-valued function of a real variable.
 
     For each element of the output of `f`, `bracket_root` seeks the scalar
@@ -511,6 +517,8 @@ def bracket_root(f, xl0, xr0=None, *, xmin=None, xmax=None, factor=None, args=()
         If the callable for which the root is desired requires arguments that are
         not broadcastable with `x`, wrap that callable with `f` such that `f`
         accepts only `x` and broadcastable ``*args``.
+    kwargs : dict of str:array_like, optional
+        Additional keyword arguments to be passed to `f`. See `args`.
     maxiter : int, default: 1000
         The maximum number of iterations of the algorithm to perform.
 
@@ -624,7 +632,7 @@ def bracket_root(f, xl0, xr0=None, *, xmin=None, xmax=None, factor=None, args=()
     """  # noqa: E501
 
     res = _bracket_root(f, xl0, xr0=xr0, xmin=xmin, xmax=xmax, factor=factor,
-                        args=args, maxiter=maxiter)
+                        args=args, kwargs=kwargs, maxiter=maxiter)
     res.bracket = res.xl, res.xr
     res.f_bracket = res.fl, res.fr
     del res.xl
@@ -640,7 +648,7 @@ def bracket_root(f, xl0, xr0=None, *, xmin=None, xmax=None, factor=None, args=()
                    ('jax.numpy', 'JAX arrays do not support item assignment.'),
                    ('torch', 'data-apis/array-api-compat#271')])
 def bracket_minimum(f, xm0, *, xl0=None, xr0=None, xmin=None, xmax=None,
-                     factor=None, args=(), maxiter=1000):
+                     factor=None, args=(), kwargs=None, maxiter=1000):
     """Bracket the minimum of a unimodal, real-valued function of a real variable.
 
     For each element of the output of `f`, `bracket_minimum` seeks the scalar
@@ -682,6 +690,8 @@ def bracket_minimum(f, xm0, *, xl0=None, xr0=None, xmin=None, xmax=None,
         If the callable for which the root is desired requires arguments that are
         not broadcastable with `x`, wrap that callable with `f` such that `f`
         accepts only `x` and broadcastable ``*args``.
+    kwargs : dict of str:array_like, optional
+        Additional keyword arguments to be passed to `f`. See `args`.
     maxiter : int, default: 1000
         The maximum number of iterations of the algorithm to perform.
 
@@ -804,7 +814,7 @@ def bracket_minimum(f, xm0, *, xl0=None, xr0=None, xmin=None, xmax=None,
     """  # noqa: E501
 
     res = _bracket_minimum(f, xm0, xl0=xl0, xr0=xr0, xmin=xmin, xmax=xmax,
-                           factor=factor, args=args, maxiter=maxiter)
+                           factor=factor, args=args, kwargs=kwargs, maxiter=maxiter)
     res.bracket = res.xl, res.xm, res.xr
     res.f_bracket = res.fl, res.fm, res.fr
     del res.xl

scipy/optimize/tests/test_bracket.py
@@ -3,7 +3,8 @@ import pytest
 import numpy as np
 
 from scipy.optimize._bracket import _ELIMITS
-from scipy.optimize.elementwise import bracket_root, bracket_minimum
+from scipy.optimize.elementwise import (bracket_root, bracket_minimum,
+                                        find_root, find_minimum)
 import scipy._lib._elementwise_iterative_method as eim
 from scipy import stats
 from scipy._lib._array_api_no_0d import (xp_assert_close, xp_assert_equal,
@@ -375,6 +376,19 @@ class TestBracketRoot:
         res = _bracket_root(lambda x: x + 0.25, xl0=-0.5, xmin=-np.inf, xmax=0)
         assert res.success
 
+    @pytest.mark.parametrize('dtype', ['float32', 'float64'])
+    def test_kwargs(self, xp, dtype):
+        # test that `kwargs` is used, broadcasts correctly, and affects dtype
+        def f(x, c, *, p):
+            return x - (p + c)
+
+        a = xp.zeros((), dtype=xp.float32)
+        c = xp.asarray([1, 2, 3], dtype=xp.float32)
+        p = xp.asarray([2, 3, 4], dtype=getattr(xp, dtype))[:, xp.newaxis]
+        bracket = bracket_root(f, a, args=(c,), kwargs={'p': p})
+        res = find_root(f, bracket.bracket, args=(c,), kwargs={'p': p})
+        ref = p + c
+        xp_assert_close(res.x, ref)
 
 @make_xp_test_case(bracket_minimum)
 class TestBracketMinimum:
@@ -886,3 +900,17 @@ class TestBracketMinimum:
         result = _bracket_minimum(f, xp.asarray(-0.5535723499480897),
                                   xmin=xmin, xmax=xmax)
         xp_assert_close(result.xr, xmax)
+
+    @pytest.mark.parametrize('dtype', ['float32', 'float64'])
+    def test_kwargs(self, xp, dtype):
+        # test that `kwargs` is used, broadcasts correctly, and affects dtype
+        def f(x, c, *, p):
+            return (x - (p + c))**2
+
+        a = xp.zeros((), dtype=xp.float32)
+        c = xp.asarray([1, 2, 3], dtype=xp.float32)
+        p = xp.asarray([2, 3, 4], dtype=getattr(xp, dtype))[:, xp.newaxis]
+        bracket = bracket_minimum(f, a, args=(c,), kwargs={'p': p})
+        res = find_minimum(f, bracket.bracket, args=(c,), kwargs={'p': p})
+        ref = p + c
+        xp_assert_close(res.x, ref)

scipy/scipy

commit 4c7b1818ed09e78afe2b4235c0ca47588de88b81

BUG: stats: fix _mode_optimization boundary case and _LogUniform mode
Two bugs were introduced in f81af9d67f when refactoring _mode_optimization
to use the new elementwise.bracket_minimum API:

1. The boundary condition check incorrectly used res_b.bracket (x positions)
   instead of res_b.f_bracket (function values). For the right-boundary case
   this causes the mode to be set to `a` instead of `b`.

2. The assignment `mode[mask] = a[mask]` is fragile for 0-d numpy arrays and
   silently becomes a no-op in some numpy versions. Replaced with np.where.

Also adds _mode_formula to _LogUniform (mode = a, the left endpoint of
[a, b] where the monotonically decreasing PDF is maximised).

Fixes test_funcs[mode-methods3-None-_LogUniform].

scipy/stats/_distribution_infrastructure.py
@@ -2266,11 +2266,11 @@ class UnivariateDistribution(_ProbabilityDistribution):
         # We have to consider this as a special case; there's no way to include
         # this logic in find_minimum; it has to treat the bracket as invalid.
         mode_at_boundary = res_b.status == -1
-        fl, fm, fr = res_b.bracket
+        fl, fm, fr = res_b.f_bracket
         mode_at_left = mode_at_boundary & (fl <= fm)
         mode_at_right = mode_at_boundary & (fr < fm)
-        mode[mode_at_left] = a[mode_at_left]
-        mode[mode_at_right] = b[mode_at_right]
+        mode = np.where(mode_at_left, a, mode)
+        mode = np.where(mode_at_right, b, mode)
         return mode[()]
 
     def mean(self, *, method=None):

scipy/stats/_new_distributions.py
@@ -338,6 +338,9 @@ class _LogUniform(ContinuousDistribution):
     def _pdf_formula(self, x, *, log_a, log_b, **kwargs):
         return ((log_b - log_a)*x)**-1
 
+    def _mode_formula(self, *, a, **kwargs):
+        return a
+
     def _moment_raw_formula(self, order, log_a, log_b, **kwargs):
         if order == 0:
             return self._one