aae76c7b4bc8e9319c9b65d6a548d1ab2226f883
Jake Vanderplas <jakevdp@google.com>
2026-05-26 13:44:30 -0700

    BUG: linalg.fiedler_companion: fix output shape for length-1 input (#25227)

--- scipy/linalg/_special_matrices.py
+++ scipy/linalg/_special_matrices.py
@@ -1099,6 +1099,8 @@ def fiedler_companion(a):
     if a.size <= 2:
         if a.size == 2:
             return np.array([[-(a/a[0])[-1]]])
+        if a.size == 1:
+            return np.empty((0, 0), dtype=a.dtype)
         return np.array([], dtype=a.dtype)
 
     if a[0] == 0.:
