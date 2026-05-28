d402d8c375bb958d7098b388aa0360391073e098
Warren Weckesser <warren.weckesser@gmail.com>
2026-05-28 15:16:58 -0400

    CI: Increase the test timeout for an interpolate test in a Windows job. (#25238)

--- scipy/interpolate/tests/test_fitpack2.py
+++ scipy/interpolate/tests/test_fitpack2.py
@@ -1,5 +1,6 @@
 # Created by Pearu Peterson, June 2003
 import itertools
+import os
 import sys
 import warnings
 
@@ -1558,6 +1559,7 @@ class TestRectBivariateSpline:
         return spl(x, y)
 
     @pytest.mark.slow()
+    @pytest.mark.timeout(timeout=float(os.getenv("WINDOWS_CI_LONG_TIMEOUT", "60")))
     @pytest.mark.parametrize('shape', [(350, 850), (2000, 170)])
     @pytest.mark.parametrize('s_tols', [(0, 1e-12, 1e-7),
                                         (1, 7e-3, 1e-4),
