b96466c15871d183deb2dba9b53bfac721a4192c
Warren Weckesser <warren.weckesser@gmail.com>
2026-05-28 07:19:41 -0400

    fixup: increase the timeout only for the specific problematic test.

--- scipy/interpolate/tests/test_fitpack2.py
+++ scipy/interpolate/tests/test_fitpack2.py
@@ -1412,6 +1412,7 @@ class TestRectBivariateSpline:
         return spl(x, y)
 
     @pytest.mark.slow()
+    @pytest.mark.timeout(timeout=float(os.getenv("WINDOWS_CI_LONG_TIMEOUT", "60")))
     @pytest.mark.parametrize('shape', [(350, 850), (2000, 170)])
     @pytest.mark.parametrize('s_tols', [(0, 1e-12, 1e-7),
                                         (1, 7e-3, 1e-4),
