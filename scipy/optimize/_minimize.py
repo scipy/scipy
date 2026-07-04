a3b94cdf1c32ce9279f21d017a40a635d7f47567
Andrew Nelson <andyfaff@gmail.com>
2026-07-04 11:55:27 +1000

    Apply suggestion from @andyfaff

--- scipy/optimize/_minimize.py
+++ scipy/optimize/_minimize.py
@@ -14,7 +14,7 @@ from warnings import warn
 
 import numpy as np
 from scipy._lib._util import wrapped_inspect_signature
-from scipy._lib._array_api import array_namespace, xp_copy
+from scipy._lib._array_api import array_namespace
 
 # unconstrained minimization
 from ._optimize import (_minimize_neldermead, _minimize_powell, _minimize_cg,
