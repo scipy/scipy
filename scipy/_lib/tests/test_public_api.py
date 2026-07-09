ee5a890c8f704d2511fff0b1f29debafa8ae4bcd
Jake Bowhay <jb9.bowhay@gmail.com>
2026-07-09 14:58:17 +0100

    DEP: stats: remove private but present

--- scipy/_lib/tests/test_public_api.py
+++ scipy/_lib/tests/test_public_api.py
@@ -35,13 +35,6 @@ PRIVATE_BUT_PRESENT_MODULES = [
     'scipy.misc.common',
     'scipy.misc.doccer',
     'scipy.special.cython_special',
-    'scipy.stats.biasedurn',
-    'scipy.stats.kde',
-    'scipy.stats.morestats',
-    'scipy.stats.mstats_basic',
-    'scipy.stats.mstats_extras',
-    'scipy.stats.mvn',
-    'scipy.stats.stats',
 ]
 
 
@@ -194,39 +187,3 @@ def test_api_importable():
         raise AssertionError("Modules that are not really public but looked "
                              "public and can not be imported: "
                              f"{module_names}")
-
-
-@pytest.mark.parametrize(("module_name", "correct_module"),
-                          ('scipy.stats.biasedurn', None),
-                          ('scipy.stats.kde', None),
-                          ('scipy.stats.morestats', None),
-                          ('scipy.stats.mstats_basic', 'mstats'),
-                          ('scipy.stats.mstats_extras', 'mstats'),
-                          ('scipy.stats.mvn', None),
-                          ('scipy.stats.stats', None)])
-def test_private_but_present_deprecation(module_name, correct_module):
-    # gh-18279, gh-17572, gh-17771 noted that deprecation warnings
-    # for imports from private modules
-    # were misleading. Check that this is resolved.
-    module = import_module(module_name)
-    if correct_module is None:
-        import_name = f'scipy.{module_name.split(".")[1]}'
-    else:
-        import_name = f'scipy.{module_name.split(".")[1]}.{correct_module}'
-
-    correct_import = import_module(import_name)
-
-    # Attributes that were formerly in `module_name` can still be imported from
-    # `module_name`, albeit with a deprecation warning.
-    for attr_name in module.__all__:
-        # ensure attribute is present where the warning is pointing
-        assert getattr(correct_import, attr_name, None) is not None
-        message = f"Please import `{attr_name}` from the `{import_name}`..."
-        with pytest.deprecated_call(match=message):
-            getattr(module, attr_name)
-
-    # Attributes that were not in `module_name` get an error notifying the user
-    # that the attribute is not in `module_name` and that `module_name` is deprecated.
-    message = f"`{module_name}` is deprecated..."
-    with pytest.raises(AttributeError, match=message):
-        getattr(module, "ekki")
