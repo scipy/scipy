2ddb10681642467b25e75920ec2b961fe6337858
Martin Schuck <martin.schuck@tum.de>
2026-07-23 00:18:52 +0200

    Avoid allocation in _compose_quat_single

--- scipy/spatial/transform/_rotation_cy.pyx
+++ scipy/spatial/transform/_rotation_cy.pyx
@@ -319,12 +319,14 @@ cdef double[:, :] _compute_davenport_from_quat(
 @cython.wraparound(False)
 cdef inline void _compose_quat_single( # calculate p * q into r
     const double[:] p, const double[:] q, double[:] r
-) noexcept:
-    cdef double[:] cross = _cross3(p[:3], q[:3])
+) noexcept nogil:
+    cdef double cx = p[1]*q[2] - p[2]*q[1]
+    cdef double cy = p[2]*q[0] - p[0]*q[2]
+    cdef double cz = p[0]*q[1] - p[1]*q[0]
 
-    r[0] = p[3]*q[0] + q[3]*p[0] + cross[0]
-    r[1] = p[3]*q[1] + q[3]*p[1] + cross[1]
-    r[2] = p[3]*q[2] + q[3]*p[2] + cross[2]
+    r[0] = p[3]*q[0] + q[3]*p[0] + cx
+    r[1] = p[3]*q[1] + q[3]*p[1] + cy
+    r[2] = p[3]*q[2] + q[3]*p[2] + cz
     r[3] = p[3]*q[3] - p[0]*q[0] - p[1]*q[1] - p[2]*q[2]
 
 @cython.boundscheck(False)
