6c994a21b1eaa28da68a84a05ff2cf3d6003f61f
Chris Ninham <61634310+Nin17@users.noreply.github.com>
2026-08-22 16:50:23 +0200

    MAINT: remove whitespace

--- scipy/fft/tests/test_helper.py
+++ scipy/fft/tests/test_helper.py
@@ -562,7 +562,7 @@ class TestRFFTFreq:
             y = fft.rfftfreq(9, xp=xp, device=d)
             x = xp.empty(0, device=d)
             assert xp_device(y) == xp_device(x)
-    
+
     def test_dtype(self, xp):
         for dtype in (xp.float32, xp.float64):
             assert fft.rfftfreq(9, xp=xp, dtype=dtype).dtype == dtype
