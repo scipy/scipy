1e55a6604d86592adbcbd0ca428631d5cb34f5e5
Albert Steppi <1953382+steppi@users.noreply.github.com>
2026-08-27 12:07:03 -0500

    ENH: special: add UFunc wrapper which preserves UFunc behavior relevant for special functions and use when wrapping ufuncs (#26032)
    * ENH: add UFuncLikeWrapper shim
    
    * MAINT: simplifications for ufunc-like wrapper
    
    * ENH: use ufunclike wrapper in support alternative backends
    
    * get all tests passing
    
    * remove out support for alternative backends
    
    * add qualname to wrapper for better error messages
    
    * replace masked array with mock subclass
    
    * fix errors found by linter
    
    * fix bug
    
    * remove documentation of removed .ufunc attribute
    
    * make fixes
    
    * fix bug
    
    * update mathieu_xem testing restriction
    
    * remove stale sentence from docstring
    
    * remove assertions that vary based on SCIPY_ARRAY_API
    
    * add a battery of tests for _make_ufunc_like_wrapper
    
    * add more _make_ufunc_like_wrapper tests
    
    * more sane handling for `out`
    
    * distinguish out=None from out unsupplied
    
    * more improvements to out handling
    
    * add extra_note explaining how ufunc kwargs work across backends
    
    * add more sophisticated ufunc extra note
    
    * Fix indentation of extra note
    
    * Don't give source link to nowhere for ufunc like wrapper
    
    * some more improvements
    
    * some clean up
    
    * more clean up
    
    * add comment on tuple_aware_xp_assert_close
    
    * more clean up
    
    * more clean up
    
    * change `WrapperClass` name for better docs inspection
    
    * allow ignore of type_checker false positive

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -19,6 +19,7 @@ from scipy._lib._util import _rng_html_rewrite
 # Workaround for sphinx-doc/sphinx#6573
 # ua._Function should not be treated as an attribute
 import scipy._lib.uarray as ua
+from scipy.special._ufunc_tools import _UFuncWrapper
 from scipy.stats._distn_infrastructure import rv_generic
 from scipy.stats._multivariate import multi_rv_generic
 
@@ -474,6 +475,9 @@ def linkcode_resolve(domain, info):
     # class since it contains the implementation of all the methods.
     if isinstance(obj, rv_generic | multi_rv_generic):
         obj = obj.__class__
+    # Much like ufuncs, ufunc_wrappers have no source location.
+    if isinstance(obj, _UFuncWrapper):
+        return None
     try:
         fn = inspect.getsourcefile(obj)
     except Exception:
