2834b243ac309a06fadfeec2b8d95ceb2aea6909
fbourgey <bourgeyflorian@gmail.com>
2026-09-01 06:07:17 -0400

    ENH: use _iv_ratioinv

--- scipy/stats/_continuous_distns.py
+++ scipy/stats/_continuous_distns.py
@@ -11366,31 +11366,7 @@ class vonmises_gen(rv_continuous):
                 # some large kappa such that r[0](kappa) = 1.0 numerically.
                 return 1e16
             elif r > 0:
-                def solve_for_kappa(kappa):
-                    return sc.i1e(kappa)/sc.i0e(kappa) - r
-
-                # The bounds of the root of r[0](kappa) = r are derived from
-                # selected bounds of r[0](x) given in [1, Eq. 11 & 16].  See
-                # gh-20102 for details.
-                #
-                # [1] Amos, D. E. (1973).  Computation of Modified Bessel
-                #     Functions and Their Ratios.  Mathematics of Computation,
-                #     28(125): 239-251.
-                lower_bound = r/(1-r)/(1+r)
-                upper_bound = 2*lower_bound
-
-                # The bounds are violated numerically for certain values of r,
-                # where solve_for_kappa evaluated at the bounds have the same
-                # sign.  This indicates numerical imprecision of i1e()/i0e().
-                # Return the violated bound in this case as it's more accurate.
-                if solve_for_kappa(lower_bound) >= 0:
-                    return lower_bound
-                elif solve_for_kappa(upper_bound) <= 0:
-                    return upper_bound
-                else:
-                    root_res = root_scalar(solve_for_kappa, method="brentq",
-                                           bracket=(lower_bound, upper_bound))
-                    return root_res.root
+                return scu._iv_ratioinv(1, r)
             else:
                 # if the provided floc is very far from the circular mean,
                 # the mean resultant length r can become negative.
