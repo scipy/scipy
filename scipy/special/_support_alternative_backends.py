d440b739932dc25ba9fc5fcfa428aae3e1430770
steppi <1953382+steppi@users.noreply.github.com>
2026-09-01 22:34:15 -0400

    appease linter

--- scipy/special/_support_alternative_backends.py
+++ scipy/special/_support_alternative_backends.py
@@ -23,7 +23,9 @@ def _special_namespace_for(xp):
     return getattr(spx, "special", None)
 
 
-def _ufunc_kwargs_extra_note(name=None, out_unsupported_backends=(), additional_note=None):
+def _ufunc_kwargs_extra_note(
+        name=None, out_unsupported_backends=(), additional_note=None
+):
     if (name is None) != (not out_unsupported_backends):
         raise ValueError(
             "`name` and `out_unsupported_backends` must either both be supplied "
@@ -54,9 +56,9 @@ def _ufunc_kwargs_extra_note(name=None, out_unsupported_backends=(), additional_
         "For the NumPy backend, this function supports all\n"
         "    `NumPy ufunc keyword arguments "
         "<https://numpy.org/doc/stable/reference/ufuncs.html#optional-keyword-arguments>`_.\n"
-        "    Other backends may support ``out``, but typically none of the other ufunc\n"
-        "    kwargs. ``out`` is typically supported for CuPy and PyTorch, but not\n"
-        "    currently in cases where SciPy relies on a generic Array API\n"
+        "    Other backends may support ``out``, but typically none of the other\n"
+        "    ufunc kwargs. ``out`` is typically supported for CuPy and PyTorch, but\n"
+        "    not currently in cases where SciPy relies on a generic Array API\n"
         "    implementation or, for PyTorch on CPU, falls back to the NumPy backend.\n"
         "    ``out`` is never supported for JAX because JAX arrays are immutable.\n"
         f"    {out_note}\n"
