151ce8c4b9edafab3935b4b3af73c283f8b76a92
Matt Haberland <matt.haberland@gmail.com>
2026-09-09 12:15:18 -0700

    MAINT: refactor signature of mutually_broadcastable_shapes helper to match hypothesis

--- scipy/sparse/linalg/tests/test_interface.py
+++ scipy/sparse/linalg/tests/test_interface.py
@@ -36,12 +36,15 @@ pytestmark = make_xp_pytest_marks(
     aslinearoperator
 )
 
-def generate_broadcastable_shapes(nshapes, *, ndim=2, min=0, max=10, rng=None):
+def mutually_broadcastable_shapes(nshapes, *, base_shape=None, min_dims=2,
+                                  max_dims=None, min_side=0, max_side=10, rng=None):
     rng = np.random.default_rng(rng)
-    min = np.broadcast_to(min, ndim)  # so min and max can be scalars or array-like
-    max = np.broadcast_to(max, ndim)
-    batch_shape = tuple(rng.integers(min_, max_+1) for min_, max_ in zip(min, max))
-    shapes = np.repeat([batch_shape], nshapes, axis=0)
+    ndim = rng.integers(min_dims, max_dims or min_dims, endpoint=True)
+    min_side = np.broadcast_to(min_side, ndim)  # so min_side and max_side
+    max_side = np.broadcast_to(max_side, ndim)  # can be scalars or array-like
+    base_shape = tuple(rng.integers(min_, max_+1) for min_, max_ in
+                       zip(min_side, max_side)) if base_shape is None else base_shape
+    shapes = np.repeat([base_shape], nshapes, axis=0)
 
     # make some elements of some shapes 1 (while preserving overall batch shape)
     for column in shapes.T:
@@ -56,7 +59,7 @@ def generate_broadcastable_shapes(nshapes, *, ndim=2, min=0, max=10, rng=None):
             shapes[i] = shape[rng.integers(j+1):]
             break
 
-    assert np.broadcast_shapes(*shapes) == batch_shape
+    assert np.broadcast_shapes(*shapes) == base_shape
     return [tuple(int(el) for el in shape) for shape in shapes]
 
 @pytest.mark.xfail_xp_backends('dask.array', reason=(
@@ -408,8 +411,8 @@ class TestDotTests:
         dtype = np.dtype(data_dtype)
         *batch_shape, M, N = op.shape
 
-        u_broadcast, v_broadcast = generate_broadcastable_shapes(
-            2, ndim=3, min=0, max=5, rng=63
+        u_broadcast, v_broadcast = mutually_broadcastable_shapes(
+            2, min_dims=3, max_dims=3, min_side=0, max_side=5, rng=63
         )
 
         for u_shape, v_shape in (
