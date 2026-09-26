c57449a25c386f0a8652da766b7f57265c2ed50d
Jake Bowhay <jb9.bowhay@gmail.com>
2026-09-26 09:24:58 +0100

    simplify

--- scipy/linalg/_special_matrices.py
+++ scipy/linalg/_special_matrices.py
@@ -4,7 +4,7 @@ import numpy as np
 from numpy.lib.stride_tricks import as_strided
 from scipy._lib._util import _apply_over_batch
 from scipy._lib._array_api import (
-    array_namespace, xp_capabilities, xp_size, xp_promote, xp_device
+    array_namespace, xp_capabilities, xp_promote, xp_device
 )
 import scipy._external.array_api_extra as xpx
 
@@ -1012,13 +1012,7 @@ def fiedler(a):
     xp = array_namespace(a)
     a = xpx.atleast_nd(xp.asarray(a), ndim=1)
 
-    if xp_size(a) == 0:
-        batch_shape, n = a.shape[:-1], a.shape[-1]
-        return xp.empty(batch_shape + (n, n), dtype=a.dtype, device=xp_device(a))
-    elif xp_size(a) == 1:
-        return xp.asarray([[0.]], dtype=a.dtype, device=xp_device(a),)
-    else:
-        return xp.abs(a[..., :, xp.newaxis] - a[..., xp.newaxis, :])
+    return xp.abs(a[..., :, xp.newaxis] - a[..., xp.newaxis, :])
 
 
 @xp_capabilities(np_only=True)
